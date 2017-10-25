8d6951ba489838bf99be128e00885d583d2c8f69
C. Andrew Warren <andrew@andrewland.co>
2017-10-25 16:10:49 -0400

    Upload current state of recidiviz scraper
    GitOrigin-RevId: 61365a79e0c31c6badc87d862c6172d54c64ecad

---
+++ appengine_config.py
@@ -0,0 +1,8 @@
+# Copyright 2017 Andrew Corp <andrew@andrewland.co> 
+# All rights reserved.
+
+# appengine_config.py
+from google.appengine.ext import vendor
+
+# Add any libraries installed in the "lib" folder.
+vendor.add('lib')
