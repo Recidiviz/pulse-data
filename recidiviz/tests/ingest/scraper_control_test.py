ce966881f221c4056c0412837e3b00a17fa354b7
Colin Adams <adamscolinc@gmail.com>
2018-11-07 19:32:41 -0500

    Scraper implementation for Mesa County, Colorado (Recidiviz/recidiviz-data#168)
    GitOrigin-RevId: b94b3e9bc71510b4044c1d18521f4c4f23bccb32

--- recidiviz/tests/ingest/scraper_control_test.py
+++ recidiviz/tests/ingest/scraper_control_test.py
@@ -284,7 +284,7 @@ def test_get_and_validate_params_all():
     params = [("region", "all"), ("scrape_type", "all")]
 
     results = scraper_control.get_and_validate_params(params)
-    assert results == (["us_ny", "us_pa_greene", "us_vt"],
+    assert results == (["us_co_mesa", "us_ny", "us_pa_greene", "us_vt"],
                        ["background", "snapshot"], params)
 
 
