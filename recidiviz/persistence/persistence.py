19d79ecebce2e9e1c30806a9209b0859e99c82df
Zachary Lawrence <zacharyclawrence@gmail.com>
2018-11-26 15:04:59 -0500

    Create relationships between ORM classes (Recidiviz/recidiviz-data#203)
    Update the ORM objects to include relationships between each table.
    
    This also includes adding a test that reads a person and all of their relationships. Consequently, this PR also includes a dummy _convert_ingest_info() to enable tests that verify that relationships are correctly matched. This logic will need to be completely updated with the data converter logic (ie. ingest_info -> database schema) which is currently being written.
    
    PAIR = @terinpw
    GitOrigin-RevId: 96043f635ed43923cd310aca368ecf231f61d501

--- recidiviz/persistence/persistence.py
+++ recidiviz/persistence/persistence.py
@@ -18,7 +18,8 @@
 
 from recidiviz import Session
 from recidiviz.persistence import entity_matching
-from recidiviz.persistence.database.schema import Person
+from recidiviz.persistence.database.schema import Person, Booking, Arrest, \
+    Charge, Bond, Sentence
 
 
 def write(ingest_info):
@@ -30,7 +31,7 @@ def write(ingest_info):
          ingest_info: The IngestInfo containing each person
     """
     for ingest_info_person in ingest_info.person:
-        person = _convert_person(ingest_info_person)
+        person = _convert_ingest_info(ingest_info_person)
         session = Session()
         try:
             existing_person = entity_matching.get_entity_match(session, person)
@@ -49,9 +50,44 @@ def write(ingest_info):
             session.close()
 
 
-# TODO: This will be done fully in another class
-def _convert_person(person):
+# TODO: This converter needs to be built fully in another class
+def _convert_ingest_info(ingest_info):
     """Converts person."""
-    return Person(surname=person.surname,
-                  birthdate=person.birthdate,
-                  place_of_residence=person.place_of_residence)
+    return Person(surname=ingest_info.surname,
+                  birthdate=ingest_info.birthdate,
+                  given_names=ingest_info.given_names,
+                  place_of_residence=ingest_info.place_of_residence,
+                  bookings=_convert_bookings(ingest_info.booking))
+
+
+def _convert_bookings(bookings):
+    """Converts booking."""
+    return [Booking(facility=booking.facility,
+                    arrest=_convert_arrest(booking.arrest),
+                    charges=_convert_charge(booking.charge))
+            for booking in bookings]
+
+
+def _convert_arrest(arrest):
+    """Converts arrest."""
+    return Arrest(officer_name=arrest.officer_name)
+
+
+def _convert_charge(charges):
+    """Converts charge."""
+    return [Charge(name=charge.name,
+                   bond=_convert_bond(charge.bond),
+                   sentence=_convert_sentence(charge.sentence))
+            for charge in charges]
+
+
+# TODO: Use the same object if 2 charges point to the same bond
+def _convert_bond(bond):
+    """Converts bond."""
+    return Bond(bond_type=bond.bond_type)
+
+
+# TODO: Use the same object if 2 charges point to the same sentence
+def _convert_sentence(sentence):
+    """Converts sentence."""
+    return Sentence(is_life=sentence.is_life)
