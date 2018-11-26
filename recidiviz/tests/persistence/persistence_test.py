685fac2bbfdb27b8cee76640b40a26576a58ad62
Zachary Lawrence <zacharyclawrence@gmail.com>
2018-11-26 15:04:59 -0500

    Create relationships between ORM classes (Recidiviz/recidiviz-data#203)
    Update the ORM objects to include relationships between each table.
    
    This also includes adding a test that reads a person and all of their relationships. Consequently, this PR also includes a dummy _convert_ingest_info() to enable tests that verify that relationships are correctly matched. This logic will need to be completely updated with the data converter logic (ie. ingest_info -> database schema) which is currently being written.
    
    PAIR = @terinpw
    GitOrigin-RevId: 96043f635ed43923cd310aca368ecf231f61d501

--- recidiviz/tests/persistence/persistence_test.py
+++ recidiviz/tests/persistence/persistence_test.py
@@ -19,17 +19,25 @@
 from datetime import datetime
 
 from recidiviz import Session
-from recidiviz.ingest.models.ingest_info import IngestInfo
+from recidiviz.ingest.models.ingest_info import IngestInfo, _Bond
 from recidiviz.persistence import persistence
 from recidiviz.persistence.database import database
 from recidiviz.tests.utils import fakes
 
-SURNAME_1 = 'test_surname_1'
-SURNAME_2 = 'test_surname_2'
 BIRTHDATE_1 = datetime(year=1993, month=11, day=15)
 BIRTHDATE_2 = datetime(year=1996, month=2, day=11)
+BOND_TYPE = 'TEST_BOND_TYPE'
+CHARGE_NAME_1 = 'TEST_CHARGE_1'
+CHARGE_NAME_2 = 'TEST_CHARGE_2'
+FACILITY = 'TEST_FACILITY'
+GIVEN_NAME = "TEST_GIVEN_NAME"
+IS_LIFE_1 = False
+IS_LIFE_2 = True
+OFFICER_NAME = 'TEST_OFFICER_NAME'
 PLACE_1 = 'TEST_PLACE_1'
 PLACE_2 = 'TEST_PLACE_2'
+SURNAME_1 = 'TEST_SURNAME_1'
+SURNAME_2 = 'TEST_SURNAME_2'
 
 
 class TestPersistence(object):
@@ -67,10 +75,13 @@ class TestPersistence(object):
         assert len(result) == 1
         assert result[0].surname == SURNAME_1
 
+    # TODO: Consider not replace everything with the newly scraped data, instead
+    # update only the newly scraped fields (keeping old data)
     def test_sameTwoPeople_matchesPeopleAndReplacesWithNewerData(self):
         # Arrange
         ingest_info = IngestInfo()
-        ingest_info.create_person(surname=SURNAME_1, place_of_residence=PLACE_1)
+        ingest_info.create_person(surname=SURNAME_1, place_of_residence=PLACE_1,
+                                  given_names=GIVEN_NAME)
         persistence.write(ingest_info)
 
         ingest_info = IngestInfo()
@@ -84,6 +95,7 @@ class TestPersistence(object):
         assert len(result) == 1
         assert result[0].surname == SURNAME_1
         assert result[0].place_of_residence == PLACE_2
+        assert result[0].given_names is None
 
     def test_readSinglePersonByName(self):
         # Arrange
@@ -99,3 +111,51 @@ class TestPersistence(object):
         assert len(result) == 1
         assert result[0].surname == SURNAME_1
         assert result[0].birthdate == BIRTHDATE_1
+
+    # TODO: Rewrite this test to directly test __eq__ between the two People
+    def test_readPersonAndAllRelationships(self):
+        # Arrange
+        ingest_info = IngestInfo()
+        person = ingest_info.create_person(surname=SURNAME_1)
+        booking = person.create_booking(facility=FACILITY)
+        booking.create_arrest(officer_name=OFFICER_NAME)
+
+        shared_bond = _Bond(bond_type=BOND_TYPE)
+
+        charge_1 = booking.create_charge(name=CHARGE_NAME_1)
+        charge_1.bond = shared_bond
+        charge_1.create_sentence(is_life=IS_LIFE_1)
+
+        charge_2 = booking.create_charge(name=CHARGE_NAME_2)
+        charge_2.bond = shared_bond
+        charge_2.create_sentence(is_life=IS_LIFE_2)
+
+        # Act
+        persistence.write(ingest_info)
+        result = database.read_people(Session())
+
+        # Assert
+        assert len(result) == 1
+        result_person = result[0]
+        assert result_person.surname == SURNAME_1
+
+        assert len(result_person.bookings) == 1
+        result_booking = result_person.bookings[0]
+        assert result_booking.facility == FACILITY
+
+        result_arrest = result_booking.arrest
+        assert result_arrest.officer_name == OFFICER_NAME
+
+        result_charges = result_booking.charges
+        assert len(result_charges) == 2
+        assert result_charges[0].name == CHARGE_NAME_1
+        assert result_charges[1].name == CHARGE_NAME_2
+
+        bond_1 = result_charges[0].bond
+        bond_2 = result_charges[1].bond
+        assert bond_1.bond_type == bond_2.bond_type
+
+        sentence_1 = result_charges[0].sentence
+        sentence_2 = result_charges[1].sentence
+        assert sentence_1.is_life == IS_LIFE_1
+        assert sentence_2.is_life == IS_LIFE_2
