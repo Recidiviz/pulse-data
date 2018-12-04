786d01b67ee32411acfaa8ba6a6511e9d1447bf6
Arian Karbasi <ratboy33@gmail.com>
2018-12-04 10:45:42 -0500

    Remove generic_scraper and generic_scraper related constants (Recidiviz/recidiviz-data#242)
    * Remove generic_scraper and generic_scraper related constants, everyone uses base_scraper now
    
    * Remove generic scraper from jailtracker vendor page as well
    
    GitOrigin-RevId: d8ba806534e7dec64758962a012725a93c32f1ea

--- recidiviz/ingest/constants.py
+++ recidiviz/ingest/constants.py
@@ -19,19 +19,11 @@
 
 # These constants tell the generic scraper what functions to perform
 INITIAL_TASK = 0x1
-SCRAPE_PERSON = 0x2
-SCRAPE_RECORD = 0X4
-GET_MORE_TASKS = 0x8
+SCRAPE_DATA = 0x2
+GET_MORE_TASKS = 0x4
 
 # Convenience definitions for scraper task types
 INITIAL_TASK_AND_MORE = INITIAL_TASK | GET_MORE_TASKS
-SCRAPE_PERSON_AND_MORE = SCRAPE_PERSON | GET_MORE_TASKS
-SCRAPE_PERSON_AND_RECORD = SCRAPE_PERSON | SCRAPE_RECORD
-SCRAPE_RECORD_AND_MORE = SCRAPE_RECORD | GET_MORE_TASKS
-
-# Constants used for the new base scraper. These should eventually replace all
-# Record and Person related constants above.
-SCRAPE_DATA = 0x2
 SCRAPE_DATA_AND_MORE = SCRAPE_DATA | GET_MORE_TASKS
 
 # Scrape types
