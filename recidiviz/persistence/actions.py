7ebc06b241b90ad8f33e894d1ac8b105e6fdc66d
Zachary Lawrence <zacharyclawrence@gmail.com>
2018-12-06 17:59:05 -0500

    Add a flask endpoint for the persistence layer (recidiviz/recidiviz-data#255)
    GitOrigin-RevId: 15974f91d5333947e0144196490390dcab70dba3

---
+++ recidiviz/persistence/actions.py
@@ -0,0 +1,37 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2018 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Contains all HTTP endpoints for communicating with the persistence layer."""
+
+import httplib
+
+from flask import Flask
+
+from recidiviz.persistence import persistence
+from recidiviz.utils.auth import authenticate_request
+
+
+app = Flask(__name__)
+
+
+@app.route("/v1/records", methods=['POST'])
+@authenticate_request
+def write_record():
+    # TODO: Something like `ingest_info = protobuf.read(request.data)`
+    ingest_info = None
+    persistence.write(ingest_info)
+
+    return '', httplib.NOT_IMPLEMENTED
