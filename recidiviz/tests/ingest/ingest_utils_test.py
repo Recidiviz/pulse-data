da6ac804e6c3f843b9a44cd3138b235539085eab
Jonas Jongejan <227529+HalfdanJ@users.noreply.github.com>
2018-12-20 07:50:13 -0500

    Added us_fl_martin_county scraper (Recidiviz/recidiviz-data#327)
    * Added us_fl_martin_county scraper
    
    * us_fl_martin_county fixes
    
    * Simplify martin_county get_more_tasks
    
    * Change us_fl_martin_county to use dataExtractor, but with errors
    
    * Fix us_fl_martin_county
    
    * Add us_fl_martin_county to test
    
    * Linting fixes
    
    * Linting fix
    
    * Linting fix
    
    GitOrigin-RevId: 1deca4daeb531b4258f22a59aef07cc44a0ea98c

--- recidiviz/tests/ingest/ingest_utils_test.py
+++ recidiviz/tests/ingest/ingest_utils_test.py
@@ -28,7 +28,8 @@ def test_validate_regions_one_ok():
 def test_validate_regions_one_all():
     assert set(ingest_utils.validate_regions(["all"])) == {
         "us_ny", "us_pa_greene", "us_vt", "us_co_mesa", "us_mt_gallatin",
-        "us_pa_dauphin", "us_mo_stone", "us_ar_van_buren", "us_pa"
+        "us_pa_dauphin", "us_mo_stone", "us_ar_van_buren", "us_pa",
+        "us_fl_martin_county"
     }
 
 
@@ -48,7 +49,8 @@ def test_validate_regions_multiple_invalid():
 def test_validate_regions_multiple_all():
     assert set(ingest_utils.validate_regions(["us_vt", "all"])) == {
         "us_ny", "us_pa_greene", "us_vt", "us_co_mesa", "us_mt_gallatin",
-        "us_pa_dauphin", "us_mo_stone", "us_ar_van_buren", "us_pa"
+        "us_pa_dauphin", "us_mo_stone", "us_ar_van_buren", "us_pa",
+        "us_fl_martin_county"
     }
 
 
