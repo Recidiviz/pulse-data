069e4cea23dce49086152fabf63349721a56664a
Arian Karbasi <ratboy33@gmail.com>
2018-12-27 16:26:44 -0500

    Cli script (Recidiviz/recidiviz-data#420)
    * Remove the need for proxies locally and load header
    
    * Add run scraper
    
    * fix lint
    
    * fix lint
    
    * fix pytest
    
    * fix linter
    
    * address comments
    
    GitOrigin-RevId: b41a84594f9a2c5c29a42f244fbbac0ef6e109e0

--- recidiviz/tests/ingest/scraper_utils_test.py
+++ recidiviz/tests/ingest/scraper_utils_test.py
@@ -130,17 +130,6 @@ class TestGetProxies(object):
         secrets.CACHED_SECRETS.clear()
 
     @patch('recidiviz.utils.environment.in_prod')
-    def test_get_proxies_local(self, mock_in_prod):
-        mock_in_prod.return_value = False
-
-        write_secret('proxy_url', 'proxy.biz/')
-        write_secret('test_proxy_user', 'user')
-        write_secret('test_proxy_password', 'password')
-
-        proxies = scraper_utils.get_proxies()
-        assert proxies == {'http': 'http://user:password@proxy.biz/'}
-
-    @patch('recidiviz.utils.environment.in_prod')
     def test_get_proxies_prod(self, mock_in_prod):
         mock_in_prod.return_value = True
 
@@ -163,27 +152,11 @@ class TestGetProxies(object):
         assert exception.value.message == 'No proxy user/pass'
 
     @patch('recidiviz.utils.environment.in_prod')
-    def test_get_proxies_local_no_password(self, mock_in_prod):
+    def test_get_proxies_local(self, mock_in_prod):
         mock_in_prod.return_value = False
 
-        write_secret('proxy_url', 'proxy.biz/')
-        write_secret('test_proxy_user', 'user')
-
-        with pytest.raises(Exception) as exception:
-            scraper_utils.get_proxies()
-        assert exception.value.message == 'No proxy user/pass'
-
-    @patch('recidiviz.utils.environment.in_prod')
-    def test_get_proxies_local_no_url(self, mock_in_prod):
-        mock_in_prod.return_value = False
-
-        write_secret('test_proxy_user', 'user')
-        write_secret('test_proxy_password', 'password')
-
-        with pytest.raises(Exception) as exception:
-            scraper_utils.get_proxies()
-        assert exception.value.message == 'No proxy url'
-
+        proxies = scraper_utils.get_proxies()
+        assert proxies is None
 
 class TestGetHeaders(object):
     """Tests for the get_headers method in the module."""
@@ -201,17 +174,31 @@ class TestGetHeaders(object):
         self.testbed.deactivate()
         secrets.CACHED_SECRETS.clear()
 
-    def test_get_headers(self):
+    @patch('recidiviz.utils.environment.in_prod')
+    def test_get_headers(self, mock_in_prod):
+        # This is prod behaviour
+        mock_in_prod.return_value = True
         user_agent = 'test_user_agent'
         write_secret('user_agent', user_agent)
 
         headers = scraper_utils.get_headers()
         assert headers == {'User-Agent': user_agent}
 
-    def test_get_headers_missing_user_agent(self):
+    @patch('recidiviz.utils.environment.in_prod')
+    def test_get_headers_missing_user_agent_in_prod(self, mock_in_prod):
+        mock_in_prod.return_value = True
         with pytest.raises(Exception) as exception:
             scraper_utils.get_headers()
         assert exception.value.message == 'No user agent string'
+
+    @patch('recidiviz.utils.environment.in_prod')
+    def test_get_headers_local(self, mock_in_prod):
+        mock_in_prod.return_value = False
+        headers = scraper_utils.get_headers()
+        assert headers == {
+            'User-Agent': ('For any issues, concerns, or rate constraints,'
+                           'e-mail alerts@recidiviz.com')
+        }
 
 
 def write_secret(name, value):
