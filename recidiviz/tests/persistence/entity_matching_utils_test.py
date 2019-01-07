7d60fa9f0c412a8a6f7f4f204825311fcb0d86fc
Colin Adams <adamscolinc@gmail.com>
2019-01-07 10:09:21 -0500

    Use some python 3 features (Recidiviz/recidiviz-data#501)
    * Use some python 3 features
    
    * concurrent.futures can't be used, as they aren't 'real' futures
    
    * fix naming
    
    GitOrigin-RevId: b163842b8e7922ac301b4d0f62e4a7104ff6712e

--- recidiviz/tests/persistence/entity_matching_utils_test.py
+++ recidiviz/tests/persistence/entity_matching_utils_test.py
@@ -21,26 +21,26 @@ from unittest import TestCase
 from recidiviz.persistence import entities, entity_matching_utils
 
 _PERSON_ID = '2'
-_PERSON_ID_ANOTHER = '3'
+_PERSON_ID_OTHER = '3'
 _BOOKING_ID = '12'
-_BOOKING_ID_ANOTHER = '13'
+_BOOKING_ID_OTHER = '13'
 _CHARGE_ID = '112'
-_CHARGE_ID_ANOTHER = '113'
+_CHARGE_ID_OTHER = '113'
 _BOND_ID = '212'
-_BOND_ID_ANOTHER = '213'
+_BOND_ID_OTHER = '213'
 _SENTENCE_ID = '312'
-_SENTENCE_ID_ANOTHER = '313'
+_SENTENCE_ID_OTHER = '313'
 _REGION = 'region'
 _EXTERNAL_ID = 'external_id'
-_EXTERNAL_ID_ANOTHER = 'external_id_another'
+_EXTERNAL_ID_OTHER = 'external_id_another'
 _FACILITY = 'facility'
 _GIVEN_NAMES = 'given_names'
-_GIVEN_NAMES_ANOTHER = 'different names'
+_GIVEN_NAMES_OTHER = 'different names'
 _SURNAME = 'surname'
 _PLACE_1 = 'place'
 _PLACE_2 = 'another'
 _DATE = datetime(2018, 12, 13)
-_DATE_ANOTHER = datetime(2017, 12, 13)
+_DATE_OTHER = datetime(2017, 12, 13)
 _CHARGE_NAME = "Charge1"
 _CHARGE_NAME_2 = "Charge2"
 _CHARGE_NAME_3 = "Charge3"
@@ -61,10 +61,10 @@ class TestEntityMatchingUtils(TestCase):
             birthdate=_DATE
         )
         self.assertTrue(entity_matching_utils.is_person_match(
-            db_person, ingested_person))
-        ingested_person.birthdate = _DATE_ANOTHER
+            db_entity=db_person, ingested_entity=ingested_person))
+        ingested_person.birthdate = _DATE_OTHER
         self.assertFalse(entity_matching_utils.is_person_match(
-            db_person, ingested_person))
+            db_entity=db_person, ingested_entity=ingested_person))
 
     def test_person_match_external_id(self):
         db_person = entities.Person.new_with_none_defaults(
@@ -74,53 +74,53 @@ class TestEntityMatchingUtils(TestCase):
             external_id=_EXTERNAL_ID
         )
         self.assertTrue(entity_matching_utils.is_person_match(
-            db_person, ingested_person))
-        ingested_person.external_id = _EXTERNAL_ID_ANOTHER
+            db_entity=db_person, ingested_entity=ingested_person))
+        ingested_person.external_id = _EXTERNAL_ID_OTHER
         self.assertFalse(entity_matching_utils.is_person_match(
-            db_person, ingested_person))
+            db_entity=db_person, ingested_entity=ingested_person))
 
     def test_booking_match_external_id(self):
         db_booking = entities.Booking(external_id=_EXTERNAL_ID)
         ingested_booking = entities.Booking(external_id=_EXTERNAL_ID)
         self.assertTrue(entity_matching_utils.is_booking_match(
-            db_booking, ingested_booking))
-        ingested_booking.external_id = _EXTERNAL_ID_ANOTHER
+            db_entity=db_booking, ingested_entity=ingested_booking))
+        ingested_booking.external_id = _EXTERNAL_ID_OTHER
         self.assertFalse(entity_matching_utils.is_booking_match(
-            db_booking, ingested_booking))
+            db_entity=db_booking, ingested_entity=ingested_booking))
 
     def test_booking_match_admission_date(self):
         db_booking = entities.Booking(admission_date=_DATE)
         ingested_booking = entities.Booking(admission_date=_DATE)
         self.assertTrue(entity_matching_utils.is_booking_match(
-            db_booking, ingested_booking))
+            db_entity=db_booking, ingested_entity=ingested_booking))
         ingested_booking.admission_date = None
         self.assertFalse(entity_matching_utils.is_booking_match(
-            db_booking, ingested_booking))
+            db_entity=db_booking, ingested_entity=ingested_booking))
 
     def test_booking_match_open_bookings(self):
         db_booking = entities.Booking(admission_date=_DATE,
                                       admission_date_inferred=True)
         ingested_booking = entities.Booking()
         self.assertTrue(entity_matching_utils.is_booking_match(
-            db_booking, ingested_booking))
+            db_entity=db_booking, ingested_entity=ingested_booking))
         ingested_booking.release_date = _DATE
         self.assertFalse(entity_matching_utils.is_booking_match(
-            db_booking, ingested_booking))
+            db_entity=db_booking, ingested_entity=ingested_booking))
 
     def test_charge_match(self):
         # TODO(350): expand tests after more robust equality function
         self.assertTrue(entity_matching_utils.is_charge_match(
-            entities.Charge(charge_id=_CHARGE_ID),
-            entities.Charge(charge_id=_CHARGE_ID_ANOTHER)))
+            db_entity=entities.Charge(charge_id=_CHARGE_ID),
+            ingested_entity=entities.Charge(charge_id=_CHARGE_ID_OTHER)))
 
     def test_bond_match(self):
         # TODO(350): expand tests after more robust equality function
         self.assertTrue(entity_matching_utils.is_bond_match(
-            entities.Bond(bond_id=_BOND_ID),
-            entities.Bond(bond_id=_BOND_ID_ANOTHER)))
+            db_entity=entities.Bond(bond_id=_BOND_ID),
+            ingested_entity=entities.Bond(bond_id=_BOND_ID_OTHER)))
 
     def test_sentence_match(self):
         # TODO(350): expand tests after more robust equality function
         self.assertTrue(entity_matching_utils.is_sentence_match(
-            entities.Sentence(sentence_id=_SENTENCE_ID),
-            entities.Sentence(sentence_id=_SENTENCE_ID_ANOTHER)))
+            db_entity=entities.Sentence(sentence_id=_SENTENCE_ID),
+            ingested_entity=entities.Sentence(sentence_id=_SENTENCE_ID_OTHER)))
