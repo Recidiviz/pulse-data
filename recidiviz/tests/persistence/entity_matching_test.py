9f6cc42f2d3110468c592a90fe8dea95959c5144
terinpw <terinpw@google.com>
2019-01-09 16:16:26 -0500

    entity match on all people (not just ones w open bookings) if external_ids are present (Recidiviz/recidiviz-data#550)
    * if external ids are present, allow matching on closed bookings
    
    * return tpye
    
    * remove unused pylint ingores
    
    GitOrigin-RevId: a0214a1125e56e4f0e47d1e2b20070205253be04

--- recidiviz/tests/persistence/entity_matching_test.py
+++ recidiviz/tests/persistence/entity_matching_test.py
@@ -104,14 +104,12 @@ class TestEntityMatching(TestCase):
         session.add(schema_person_another)
         session.commit()
 
-        # pylint: disable=protected-access
         ingested_existing_charge = database_utils.convert_charge(
             schema_charge)
         ingested_existing_charge.charge_id = None
         ingested_new_charge = entities.Charge(status=ChargeStatus.PENDING,
                                               name=_CHARGE_NAME_3)
 
-        # pylint: disable=protected-access
         ingested_open_booking = database_utils.convert_booking(
             open_schema_booking)
         ingested_open_booking.admission_date = None
@@ -130,7 +128,6 @@ class TestEntityMatching(TestCase):
         expected_existing_charge = copy.deepcopy(ingested_existing_charge)
         expected_new_charge = copy.deepcopy(ingested_new_charge)
 
-        # pylint: disable=protected-access
         expected_dropped_charge = database_utils.convert_charge(
             schema_charge_another)
         expected_dropped_charge.status = ChargeStatus.DROPPED
@@ -138,11 +135,48 @@ class TestEntityMatching(TestCase):
         expected_open_booking.charges = [expected_existing_charge,
                                          expected_new_charge,
                                          expected_dropped_charge]
-        # pylint: disable=protected-access
         expected_closed_booking = \
             database_utils.convert_booking(closed_schema_booking)
         expected_person = copy.deepcopy(ingested_person)
         expected_person.bookings = \
             [expected_open_booking, expected_closed_booking]
+
+        assert ingested_person == expected_person
+
+    def test_match_entities_external_ids(self):
+        closed_schema_booking = schema.Booking(
+            admission_date=_DATE_ANOTHER,
+            booking_id=_BOOKING_ID,
+            release_date=_DATE,
+            custody_status=CustodyStatus.RELEASED.value,
+            last_seen_time=_DATE)
+
+        schema_person = schema.Person(person_id=_PERSON_ID,
+                                      external_id=_EXTERNAL_ID,
+                                      surname=_SURNAME,
+                                      given_names=_GIVEN_NAMES,
+                                      birthdate=_DATE,
+                                      place_of_residence=_PLACE_1,
+                                      region=_REGION,
+                                      bookings=[closed_schema_booking])
+
+        session = Session()
+        session.add(schema_person)
+        session.commit()
+
+        ingested_closed_booking = database_utils.convert_booking(
+            closed_schema_booking)
+        ingested_closed_booking.booking_id = None
+        ingested_closed_booking.facility = _FACILITY
+
+        ingested_person = database_utils.convert_person(schema_person)
+        ingested_person.person_id = None
+        ingested_person.bookings = [ingested_closed_booking]
+
+        entity_matching.match_entities(Session(), _REGION, [ingested_person])
+
+        expected_closed_booking = copy.deepcopy(ingested_closed_booking)
+        expected_person = copy.deepcopy(ingested_person)
+        expected_person.bookings = [expected_closed_booking]
 
         assert ingested_person == expected_person
