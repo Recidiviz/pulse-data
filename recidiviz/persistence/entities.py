29b8ee75ecc13e3c34c85191a42c54d0c5e88796
Zachary Lawrence <zacharyclawrence@gmail.com>
2019-01-10 12:33:40 -0500

    Make entities Optional[] type (Recidiviz/recidiviz-data#561)
    * Make entities Optional[] type
    
    * use typing.Union instead of manually checking type
    
    * fix
    
    * Ignore type for private __args__ reference
    
    * Move enum_cls to where it's used
    
    GitOrigin-RevId: d4e5158d7f5fa3ef680ce4217927fb0fa3608459

--- recidiviz/persistence/entities.py
+++ recidiviz/persistence/entities.py
@@ -20,7 +20,7 @@ Note: These classes mirror the SQL Alchemy ORM objects but are kept separate.
 This allows these persistence layer objects additional flexibility that the SQL
 Alchemy ORM objects can't provide.
 """
-from typing import List
+from typing import List, Optional
 
 import datetime
 import attr
@@ -36,110 +36,110 @@ from recidiviz.common.constants.person import Race, Ethnicity, Gender
 
 @attr.s
 class Person(BuildableAttr):
-    external_id: str = attr.ib()
-    surname: str = attr.ib()
-    given_names: str = attr.ib()
-    birthdate: datetime.date = attr.ib()
-    birthdate_inferred_from_age: bool = attr.ib()
-    gender: Gender = attr.ib()
-    race: Race = attr.ib()
-    region: str = attr.ib()
-    ethnicity: Ethnicity = attr.ib()
-    place_of_residence: str = attr.ib()
+    external_id: Optional[str] = attr.ib()
+    surname: Optional[str] = attr.ib()
+    given_names: Optional[str] = attr.ib()
+    birthdate: Optional[datetime.date] = attr.ib()
+    birthdate_inferred_from_age: Optional[bool] = attr.ib()
+    gender: Optional[Gender] = attr.ib()
+    race: Optional[Race] = attr.ib()
+    region: Optional[str] = attr.ib()
+    ethnicity: Optional[Ethnicity] = attr.ib()
+    place_of_residence: Optional[str] = attr.ib()
 
-    person_id: int = attr.ib(default=None)
+    person_id: Optional[int] = attr.ib(default=None)
     bookings: List['Booking'] = attr.ib(factory=list)
 
 
 @attr.s
 class Booking(BuildableAttr):
-    external_id: str = attr.ib()
-    admission_date: datetime.date = attr.ib()
-    admission_date_inferred: bool = attr.ib()
-    release_date: datetime.date = attr.ib()
-    release_date_inferred: bool = attr.ib()
-    projected_release_date: datetime.date = attr.ib()
-    release_reason: ReleaseReason = attr.ib()
-    custody_status: CustodyStatus = attr.ib()
-    facility: str = attr.ib()
-    classification: Classification = attr.ib()
-    last_seen_time: datetime.datetime = attr.ib()
+    external_id: Optional[str] = attr.ib()
+    admission_date: Optional[datetime.date] = attr.ib()
+    admission_date_inferred: Optional[bool] = attr.ib()
+    release_date: Optional[datetime.date] = attr.ib()
+    release_date_inferred: Optional[bool] = attr.ib()
+    projected_release_date: Optional[datetime.date] = attr.ib()
+    release_reason: Optional[ReleaseReason] = attr.ib()
+    custody_status: Optional[CustodyStatus] = attr.ib()
+    facility: Optional[str] = attr.ib()
+    classification: Optional[Classification] = attr.ib()
+    last_seen_time: Optional[datetime.datetime] = attr.ib()
 
-    booking_id: int = attr.ib(default=None)
+    booking_id: Optional[int] = attr.ib(default=None)
     holds: List['Hold'] = attr.ib(factory=list)
-    arrest: 'Arrest' = attr.ib(default=None)
+    arrest: Optional['Arrest'] = attr.ib(default=None)
     charges: List['Charge'] = attr.ib(factory=list)
 
 
 @attr.s
 class Hold:
-    external_id: str = attr.ib(default=None)
-    jurisdiction_name: str = attr.ib(default=None)
-    hold_status: str = attr.ib(default=None)
+    external_id: Optional[str] = attr.ib(default=None)
+    jurisdiction_name: Optional[str] = attr.ib(default=None)
+    hold_status: Optional[str] = attr.ib(default=None)
 
-    hold_id: int = attr.ib(default=None)
+    hold_id: Optional[int] = attr.ib(default=None)
 
 
 @attr.s
 class Arrest(BuildableAttr):
-    external_id: str = attr.ib()
-    date: datetime.date = attr.ib()
-    location: str = attr.ib()
-    agency: str = attr.ib()
-    officer_name: str = attr.ib()
-    officer_id: str = attr.ib()
+    external_id: Optional[str] = attr.ib()
+    date: Optional[datetime.date] = attr.ib()
+    location: Optional[str] = attr.ib()
+    agency: Optional[str] = attr.ib()
+    officer_name: Optional[str] = attr.ib()
+    officer_id: Optional[str] = attr.ib()
 
-    arrest_id: int = attr.ib(default=None)
+    arrest_id: Optional[int] = attr.ib(default=None)
 
 
 @attr.s
 class Charge(BuildableAttr):
-    external_id: str = attr.ib()
-    offense_date: datetime.date = attr.ib()
-    statute: str = attr.ib()
-    name: str = attr.ib()
-    attempted: bool = attr.ib()
-    degree: ChargeDegree = attr.ib()
-    charge_class: ChargeClass = attr.ib()
-    level: str = attr.ib()
-    fee_dollars: int = attr.ib()
-    charging_entity: str = attr.ib()
-    status: ChargeStatus = attr.ib()
-    court_type: CourtType = attr.ib()
-    case_number: str = attr.ib()
-    next_court_date: datetime.date = attr.ib()
-    judge_name: str = attr.ib()
+    external_id: Optional[str] = attr.ib()
+    offense_date: Optional[datetime.date] = attr.ib()
+    statute: Optional[str] = attr.ib()
+    name: Optional[str] = attr.ib()
+    attempted: Optional[bool] = attr.ib()
+    degree: Optional[ChargeDegree] = attr.ib()
+    charge_class: Optional[ChargeClass] = attr.ib()
+    level: Optional[str] = attr.ib()
+    fee_dollars: Optional[int] = attr.ib()
+    charging_entity: Optional[str] = attr.ib()
+    status: Optional[ChargeStatus] = attr.ib()
+    court_type: Optional[CourtType] = attr.ib()
+    case_number: Optional[str] = attr.ib()
+    next_court_date: Optional[datetime.date] = attr.ib()
+    judge_name: Optional[str] = attr.ib()
 
-    charge_id: int = attr.ib(default=None)
-    bond: 'Bond' = attr.ib(default=None)
-    sentence: 'Sentence' = attr.ib(default=None)
+    charge_id: Optional[int] = attr.ib(default=None)
+    bond: Optional['Bond'] = attr.ib(default=None)
+    sentence: Optional['Sentence'] = attr.ib(default=None)
 
 
 @attr.s
 class Bond:
-    external_id: str = attr.ib(default=None)
-    amount_dollars: int = attr.ib(default=None)
-    bond_type: BondType = attr.ib(default=None)
-    status: BondStatus = attr.ib(default=None)
+    external_id: Optional[Optional[str]] = attr.ib(default=None)
+    amount_dollars: Optional[int] = attr.ib(default=None)
+    bond_type: Optional[BondType] = attr.ib(default=None)
+    status: Optional[BondStatus] = attr.ib(default=None)
 
-    bond_id: int = attr.ib(default=None)
+    bond_id: Optional[int] = attr.ib(default=None)
 
 
 @attr.s
 class Sentence:
-    external_id: str = attr.ib(default=None)
-    date_imposed: datetime.date = attr.ib(default=None)
-    sentencing_region: str = attr.ib(default=None)
-    min_length_days: int = attr.ib(default=None)
-    max_length_days: int = attr.ib(default=None)
-    is_life: bool = attr.ib(default=None)
-    is_probation: bool = attr.ib(default=None)
-    is_suspended: bool = attr.ib(default=None)
-    fine_dollars: int = attr.ib(default=None)
-    parole_possible: bool = attr.ib(default=None)
-    post_release_supervision_length_days: int = attr.ib(default=None)
+    external_id: Optional[str] = attr.ib(default=None)
+    date_imposed: Optional[datetime.date] = attr.ib(default=None)
+    sentencing_region: Optional[str] = attr.ib(default=None)
+    min_length_days: Optional[int] = attr.ib(default=None)
+    max_length_days: Optional[int] = attr.ib(default=None)
+    is_life: Optional[bool] = attr.ib(default=None)
+    is_probation: Optional[bool] = attr.ib(default=None)
+    is_suspended: Optional[bool] = attr.ib(default=None)
+    fine_dollars: Optional[int] = attr.ib(default=None)
+    parole_possible: Optional[bool] = attr.ib(default=None)
+    post_release_supervision_length_days: Optional[int] = attr.ib(default=None)
 
-    sentence_id: str = attr.ib(default=None)
+    sentence_id: Optional[str] = attr.ib(default=None)
 
     # To avoid recursive references, store only 1 level of related_sentences
     # (ie. don't store related_sentences of these related_sentences).
