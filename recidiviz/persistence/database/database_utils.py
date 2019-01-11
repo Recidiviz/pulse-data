d8368dcaf63514a2a3043276c3bb76fee5cfd661
Zachary Lawrence <zacharyclawrence@gmail.com>
2019-01-10 21:54:03 -0500

    Make hold a BuildableEntity (recidiviz/recidiviz-data#576)
    GitOrigin-RevId: c5895926a8e80fb2d2ee59359b1b39f2655f59d8

--- recidiviz/persistence/database/database_utils.py
+++ recidiviz/persistence/database/database_utils.py
@@ -170,7 +170,7 @@ def _convert(src):
 
 def _should_use_builder(entity_cls):
     return entity_cls in {entities.Person, entities.Booking, entities.Arrest,
-                          entities.Charge}
+                          entities.Charge, entities.Hold}
 
 
 def _is_enum(attr_type):
