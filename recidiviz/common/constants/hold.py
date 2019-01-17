01f1b44cf5da074a74729ece83b73b3f94463986
jamwalla <james.wallacelee@gmail.com>
2019-01-17 09:21:52 -0500

    Holds in IngestInfo and Converter (Recidiviz/recidiviz-data#631)
    GitOrigin-RevId: 36fc084c3699a98cd85808225cacecc9a2fa08de

---
+++ recidiviz/common/constants/hold.py
@@ -0,0 +1,35 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+"""Constants related to a hold entity."""
+import recidiviz.common.constants.enum_canonical_strings as enum_strings
+from recidiviz.common.constants.mappable_enum import MappableEnum
+
+
+class HoldStatus(MappableEnum):
+    ACTIVE = enum_strings.hold_status_active
+    INACTIVE = enum_strings.hold_status_inactive
+
+    @staticmethod
+    def _get_default_map():
+        return _HOLD_STATUS_MAP
+
+
+_HOLD_STATUS_MAP = {
+    'ACTIVE': HoldStatus.ACTIVE,
+    'INACTIVE': HoldStatus.INACTIVE
+}
