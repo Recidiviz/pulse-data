db0d8895765bee97ff319e41d243e74d1098090b
Zachary Lawrence <zacharyclawrence@gmail.com>
2019-01-22 18:03:12 -0500

    Add GA aggregate scraper (recidiviz/recidiviz-data#704)
    recidiviz/recidiviz-data#129
    
    GitOrigin-RevId: d3e6416b9cc11a8d68fd4b72a65070e89d869127

---
+++ recidiviz/ingest/aggregate/regions/ga/ga_aggregate_ingest.py
@@ -0,0 +1,107 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2018 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Parse the GA Aggregated Statistics PDF."""
+import datetime
+import locale
+from typing import Dict
+
+import pandas as pd
+import tabula
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
+import recidiviz.common.constants.enum_canonical_strings as enum_strings
+from recidiviz.ingest.aggregate import aggregate_ingest_utils
+from recidiviz.persistence.database.schema import GaCountyAggregate
+
+
+def parse(filename: str, date_scraped: datetime.datetime) \
+        -> Dict[DeclarativeMeta, pd.DataFrame]:
+    _setup()
+
+    ga_county_table = _parse_table(filename)
+
+    # TODO(#698): Set county fips based on the county_name
+    ga_county_table['fips'] = None
+    ga_county_table['report_date'] = date_scraped
+    ga_county_table['report_granularity'] = enum_strings.monthly_granularity
+
+    return {
+        GaCountyAggregate: ga_county_table
+    }
+
+
+def _parse_table(filename: str) -> pd.DataFrame:
+    """Parses the last table in the GA PDF."""
+
+    # Set column names since the pdf makes them hard to parse directly
+    column_names = [
+        'Index',
+        'Jurisdiction',
+        'Total Number of Inmates In Jail',
+        'Jail Capacity',
+        'Inmates as % of Capacity',
+        'Number of Inmates Sentenced to State [Number]',
+        'Number of Inmates Sentenced to State [% of Total]',
+        'Number of Inmates Awaiting Trial in Jail [Number]',
+        'Number of Inmates Awaiting Trial in Jail [% of Total]',
+        'Number of Inmates Serving County Sentence [Number]',
+        'Number of Inmates Serving County Sentence [% of Total]',
+        'Number of Other Inmates [Number]',
+        'Number of Other Inmates [% of Total]'
+    ]
+
+    # Tables at the end of the doc contain all data we want to parse
+    pages = [8, 9, 10, 11]
+
+    # Use lattice parsing since default parsing fails to parse columns on
+    # the right half of the page
+    use_lattice = True
+
+    result = tabula.read_pdf(
+        filename,
+        pages=pages,
+        lattice=use_lattice,
+        pandas_options={
+            'names': column_names,
+            'skiprows': _every_43,  # Skip header rows at the top of each page
+            'skipfooter': 1  # The last row is the grand totals
+        })
+
+    result = aggregate_ingest_utils.rename_columns_and_select(result, {
+        'Jurisdiction': 'county_name',
+        'Total Number of Inmates In Jail': 'total_number_of_inmates_in_jail',
+        'Jail Capacity': 'jail_capacity',
+        'Number of Inmates Sentenced to State [Number]':
+            'number_of_inmates_sentenced_to_state',
+        'Number of Inmates Awaiting Trial in Jail [Number]':
+            'number_of_inmates_awaiting_trial',
+        'Number of Inmates Serving County Sentence [Number]':
+            'number_of_inmates_serving_county_sentence',
+        'Number of Other Inmates [Number]':
+            'number_of_other_inmates'
+    })
+
+    return result
+
+
+def _every_43(index: int) -> bool:
+    return index % 43 == 0
+
+
+def _setup() -> None:
+    # This allows us to call `locale.atoi` when converting str -> int
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
