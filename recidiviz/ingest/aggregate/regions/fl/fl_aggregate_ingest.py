5cde45ef6732ce3c5b7c0f3579a199e6cfa3a51f
Zachary Lawrence <zacharyclawrence@gmail.com>
2019-01-22 18:10:43 -0500

    Add 2nd table to FL aggregate parser (Recidiviz/recidiviz-data#700)
    This PR is a continuation of Recidiviz/recidiviz-data#648 by adding parsing logic for the 2nd table. This completes all FL aggregate scrape logic because the 3rd and final table will not be parsed (since it contains no county level data).
    
    Recidiviz/recidiviz-data#129
    
    GitOrigin-RevId: 6c2ce9a24e9758efbd4b3d87d1525706febe757a

--- recidiviz/ingest/aggregate/regions/fl/fl_aggregate_ingest.py
+++ recidiviz/ingest/aggregate/regions/fl/fl_aggregate_ingest.py
@@ -17,7 +17,7 @@
 """Parse the FL Aggregated Statistics PDF."""
 import datetime
 import locale
-from typing import Dict
+from typing import Dict, Optional
 
 import pandas as pd
 import tabula
@@ -25,26 +25,35 @@ from sqlalchemy.ext.declarative import DeclarativeMeta
 
 import recidiviz.common.constants.enum_canonical_strings as enum_strings
 from recidiviz.ingest.aggregate import aggregate_ingest_utils
-from recidiviz.persistence.database.schema import FlCountyAggregate
+from recidiviz.persistence.database.schema import FlCountyAggregate, \
+    FlFacilityAggregate
 
 
 def parse(filename: str, date_scraped: datetime.datetime) \
         -> Dict[DeclarativeMeta, pd.DataFrame]:
     _setup()
 
-    fl_county_table = _parse_table_1(filename)
-
     # TODO(#698): Set county fips based on the county_name
+    fl_county_table = _parse_county_table(filename)
     fl_county_table['fips'] = None
-    fl_county_table['report_date'] = date_scraped
-    fl_county_table['report_granularity'] = enum_strings.monthly_granularity
 
-    return {
-        FlCountyAggregate: fl_county_table
+    # TODO(#698, #689): Set facility fips based on facility_name
+    fl_facility_table = _parse_facility_table(filename)
+    fl_facility_table['fips'] = None
+
+    result = {
+        FlCountyAggregate: fl_county_table,
+        FlFacilityAggregate: fl_facility_table
     }
 
+    for table in result.values():
+        table['report_date'] = date_scraped
+        table['report_granularity'] = enum_strings.monthly_granularity
 
-def _parse_table_1(filename: str) -> pd.DataFrame:
+    return result
+
+
+def _parse_county_table(filename: str) -> pd.DataFrame:
     """Parses the FL County - Table 1 in the PDF."""
     part1 = tabula.read_pdf(
         filename,
@@ -74,6 +83,64 @@ def _parse_table_1(filename: str) -> pd.DataFrame:
     result['date_reported'] = pd.to_datetime(result['date_reported'])
 
     return result
+
+
+def _parse_facility_table(filename: str) -> pd.DataFrame:
+    """Parse the FL County Pretrial Inmate Report - Table 2 in the PDF."""
+    # Set column names directly since the pdf format makes them hard to parse
+    column_names = [
+        'Detention Facility Name',
+        'Average Daily Population',
+        'Number Felony Pretrial',
+        'Number Misdemeanor Pretrial',
+        'Total Percent Pretrial']
+
+    part1 = tabula.read_pdf(
+        filename,
+        pages=[5],
+        pandas_options={
+            'skiprows': [0, 1, 2],
+            'names': column_names,
+        })
+    part2 = tabula.read_pdf(
+        filename,
+        pages=[6],
+        pandas_options={
+            'skiprows': [0, 1, 2],
+            'usecols': [0, 2, 3, 4, 5],  # Column 1 contains no data
+            'names': column_names,
+            'skipfooter': 2  # The last 2 rows are the totals
+        })
+    result = part1.append(part2, ignore_index=True)
+
+    result = aggregate_ingest_utils.rename_columns_and_select(result, {
+        'Detention Facility Name': 'facility_name',
+        'Average Daily Population': 'average_daily_population',
+        'Number Felony Pretrial': 'number_felony_pretrial',
+        'Number Misdemeanor Pretrial': 'number_misdemeanor_pretrial'
+    })
+
+    result['average_daily_population'] = result[
+        'average_daily_population'].apply(_use_stale_adp).apply(_to_int)
+    for column_name in {'number_felony_pretrial',
+                        'number_misdemeanor_pretrial'}:
+        result[column_name] = result[column_name].apply(_to_int)
+
+    return result
+
+
+def _use_stale_adp(adp_str: str) -> str:
+    """Use adp values listed in Table 2 that are listed as stale."""
+    # Stale values are marked with an '*', so strip all '*'
+    return adp_str.rstrip('*')
+
+
+def _to_int(int_str: str) -> Optional[int]:
+    try:
+        return locale.atoi(int_str)
+    except ValueError:
+        # Values containing a '-' have no reported data, so just return None
+        return None
 
 
 def _setup() -> None:
