15dad4740594134a246f6f58f520f59c2e6d0db9
Colin Adams <adamscolinc@gmail.com>
2019-01-24 14:45:41 -0500

    change run scraper to use queue (Recidiviz/recidiviz-data#717)
    * change run scraper to use queue
    
    * minor arg changes
    
    * fix fail fast
    
    * remove strtobool
    
    * clarify task num log message
    
    * address comments
    
    GitOrigin-RevId: 0beb168a12b1ba678d36f8efc46ea754d6bf649a

--- recidiviz/ingest/run_scraper.py
+++ recidiviz/ingest/run_scraper.py
@@ -19,7 +19,7 @@
 
 usage: run_scraper.py [-h] --region REGION [--num_tasks NUM_TASKS]
                       [--sleep_between_requests SLEEP_BETWEEN_REQUESTS]
-                      [--run_forever RUN_FOREVER] [--fail_fast FAIL_FAST]
+                      [--run_forever RUN_FOREVER] [--no_fail_fast]
                       [--log LOG]
 
 Example:
@@ -33,106 +33,103 @@ import logging
 import time
 import traceback
 import types
+from collections import deque
 from datetime import datetime
-from distutils.util import strtobool  # pylint: disable=no-name-in-module
+from functools import partial
 
 from recidiviz.ingest import constants
 from recidiviz.ingest.task_params import QueueRequest
 from recidiviz.utils import regions
 
-# Sleep 1 seconds per task
-sleep_between_requests = 1
 
-# Number of people to scrape by default
-num_tasks_left = 5
+# This function acts as a bound method to the scraper instance.
+def add_task(queue, _self, task_name, request):
+    """Overwritten version of `add_task` which adds the task to an in-memory
+    queue.
+    """
 
-# Alternatively, if run_forever is true, ignore num_tasks and run to completion
-run_forever = False
+    # Serialize and deserialize the request. Simply to replicate production
+    # and catch any potential issues.
+    serialized = json.dumps(request.to_serializable())
+    request = QueueRequest.from_serializable(json.loads(serialized))
 
-# Fail the first time we hit an error. Set to False to log errors and continue.
-fail_fast = True
-
-# Default logging level
-logging_level = logging.INFO
-
-
-# This function acts as a bound method to the scraper instance.  It is
-# overwriting the functionality of add_task to just run the task instead of
-# throwing it on the taskqueue.  By binding it in this fashion, we get access
-# to self as if this function was defined inline in the class, which is useful
-# in faking the run.
-def add_task(self, task_name, params):
-    """Overwritten version of add task which simply runs the task
-    and ignores the usage of the task_queue."""
-    logging.info('***')
-    # These must be global because we are binding the function and we therefore
-    # have no control over how they're called.  The choices would be to make
-    # the bound function inline, or use the global counters in this fashion.
-    # Doing it inline doesn't make much sense in this use case because thhis is
-    # a CLI with all static functions, and the entrypoint is module level.
-    global num_tasks_left
-    global sleep_between_requests
-
-    if not run_forever:
-        num_tasks_left -= 1
-        # If we are done, we can exit.
-        if num_tasks_left == 0:
-            logging.info('Completed the test run!')
-            exit()
-        logging.info('%s tasks left to complete', num_tasks_left)
-
-    logging.info('Sleeping %s seconds before sending another request',
-                 sleep_between_requests)
-    time.sleep(sleep_between_requests)
-    logging.info('***')
-    fn = getattr(self, task_name)
-    try:
-        # Serialize and deserialize the request. Simply to replicate production
-        # and catch any potential issues.
-        serialized = json.dumps(params.to_serializable())
-        request = QueueRequest.from_serializable(json.loads(serialized))
-
-        # Run the task
-        fn(request)
-    except Exception as e:
-        if fail_fast or e is KeyboardInterrupt:
-            raise
-        traceback.print_exc()
+    # Add it to the queue
+    queue.append((task_name, request))
 
 
-def start_scrape(self, scrape_type):
-    fn = getattr(self, self.get_initial_task_method())
-    fn(QueueRequest(scrape_type=scrape_type,
-                    scraper_start_time=datetime.now(),
-                    next_task=self.get_initial_task()))
+def start_scrape(queue, self, scrape_type):
+    add_task(queue, self, self.get_initial_task_method(),
+             QueueRequest(scrape_type=scrape_type,
+                          scraper_start_time=datetime.now(),
+                          next_task=self.get_initial_task()))
+
+
+def run_scraper(args):
+    """Runs the scraper for the given region
+
+    Creates and manages an in-memory FIFO queue to replicate production.
+    """
+    region = regions.Region(args.region)
+    scraper = region.get_scraper()
+
+    task_queue = deque()
+
+    # We use this to bind the method to the instance.
+    scraper.add_task = types.MethodType(
+        partial(add_task, task_queue), scraper)
+    scraper.start_scrape = types.MethodType(
+        partial(start_scrape, task_queue), scraper)
+
+    scraper.start_scrape(constants.ScrapeType.BACKGROUND)
+
+    num_tasks_run = 0
+    while task_queue and (num_tasks_run < args.num_tasks or args.run_forever):
+        logging.info('***')
+        logging.info('Running task %d of %s tasks', num_tasks_run,
+                     'infinite' if args.run_forever else args.num_tasks)
+
+        # run the task
+        method, request = task_queue.popleft()
+        try:
+            getattr(scraper, method)(request)
+        except Exception as e:
+            if args.fail_fast or e is KeyboardInterrupt:
+                raise
+            traceback.print_exc()
+
+        # increment and sleep
+        num_tasks_run += 1
+        logging.info('Sleeping %s seconds before sending another request',
+                     args.sleep_between_requests)
+        time.sleep(args.sleep_between_requests)
+
+    logging.info('Completed the test run!')
 
 
 def _create_parser():
     """Creates the CLI argument parser."""
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--region', required=True, help='The region to test')
     parser.add_argument(
-        '--num_tasks', required=False,
-        default=num_tasks_left, type=int,
-        help='The number of tasks to complete, default is {}'.format(
-            num_tasks_left))
+        '--num_tasks', required=False, default=5, type=int,
+        help='The number of tasks to complete'
+    )
     parser.add_argument(
-        '--sleep_between_requests', required=False,
-        default=sleep_between_requests, type=float,
-        help='The number of seconds to sleep in between requests,'
-             'default is {}'.format(sleep_between_requests))
+        '--sleep_between_requests', required=False, default=1, type=float,
+        help='The number of seconds to sleep in between requests'
+    )
     parser.add_argument(
-        '-run_forever', required=False, action='store_true',
+        '--run_forever', required=False, action='store_true',
         help='If set, ignore num_tasks and run until completion'
     )
     parser.add_argument(
-        '--fail_fast', required=False, default=str(fail_fast),
-        help='Stop running after an error, default is {}'.format(fail_fast)
+        '--no_fail_fast', required=False, dest='fail_fast',
+        action='store_false', help='Continue running after an error'
     )
     parser.add_argument(
-        '--log', required=False, default=logging_level,
-        help='Set the logging level, default is '
-             '{}'.format(logging.getLevelName(logging_level))
+        '--log', required=False, default='INFO', type=logging.getLevelName,
+        help='Set the logging level'
     )
     return parser
 
@@ -144,21 +141,8 @@ def _configure_logging(level):
 
 if __name__ == "__main__":
     arg_parser = _create_parser()
-    args = arg_parser.parse_args()
+    arguments = arg_parser.parse_args()
 
-    num_tasks_left = args.num_tasks
-    sleep_between_requests = args.sleep_between_requests
-    run_forever = args.run_forever
-    fail_fast = bool(strtobool(args.fail_fast))
-    logging_level = args.log
+    _configure_logging(arguments.log)
 
-    _configure_logging(logging_level)
-
-    region = regions.Region(args.region)
-    scraper = region.get_scraper()
-
-    # We use this to bind the method to the instance.
-    scraper.add_task = types.MethodType(add_task, scraper)
-    scraper.start_scrape = types.MethodType(start_scrape, scraper)
-
-    scraper.start_scrape(constants.ScrapeType.BACKGROUND)
+    run_scraper(arguments)
