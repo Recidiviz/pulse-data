b9ace3d20bdbe49ab1e3c7c38aed739eefcc4ee1
Zachary Garcia <zacharydgarcia@gmail.com>
2019-01-30 10:10:30 -0500

    Add migration to change type of aggregate report date (Recidiviz/recidiviz-data#775)
    * Generate migration for aggregate report date
    
    * Fix comments
    
    GitOrigin-RevId: 42e43b08a403f5519fbf930b0240fc2bf35cf0ba

--- recidiviz/persistence/database/migrations/env.py
+++ recidiviz/persistence/database/migrations/env.py
@@ -53,7 +53,10 @@ def run_migrations_offline():
     """
     url = get_sqlalchemy_url()
     context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True)
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        compare_type=True)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -71,8 +74,8 @@ def run_migrations_online():
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata
-        )
+            target_metadata=target_metadata,
+            compare_type=True)
 
         with context.begin_transaction():
             context.run_migrations()
