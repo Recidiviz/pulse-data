4ec4a32e73235517e124bfe12245870d47303896
Arian Karbasi <ratboy33@gmail.com>
2019-02-04 10:01:21 -0500

    Aggregate site ingests for kentucky and hawaii (Recidiviz/recidiviz-data#823)
    * Add hi site scraper
    
    * Add ky site scraper
    
    * fix pylint
    
    * Add to scrape_aggregate_reports
    
    GitOrigin-RevId: f769c271a8b87bc95bdc51f6e953c7eda66c5c4b

--- recidiviz/ingest/aggregate/scrape_aggregate_reports.py
+++ recidiviz/ingest/aggregate/scrape_aggregate_reports.py
@@ -29,8 +29,8 @@ import gcsfs
 
 from recidiviz.ingest.aggregate.regions.fl import fl_aggregate_site_scraper
 from recidiviz.ingest.aggregate.regions.ga import ga_aggregate_site_scraper
-# from recidiviz.ingest.aggregate.regions.hi import hi_aggregate_site_scraper
-# from recidiviz.ingest.aggregate.regions.ky import ky_aggregate_site_scraper
+from recidiviz.ingest.aggregate.regions.hi import hi_aggregate_site_scraper
+from recidiviz.ingest.aggregate.regions.ky import ky_aggregate_site_scraper
 # from recidiviz.ingest.aggregate.regions.ny import ny_aggregate_site_scraper
 from recidiviz.ingest.aggregate.regions.tx import tx_aggregate_site_scraper
 from recidiviz.utils import metadata
@@ -60,8 +60,8 @@ def scrape_aggregate_reports():
     state_to_scraper = {
         'florida': fl_aggregate_site_scraper.get_urls_to_download,
         'georgia': ga_aggregate_site_scraper.get_urls_to_download,
-        # 'hawaii': hi_aggregate_site_scraper.get_urls_to_download,
-        # 'kentucky': ky_aggregate_site_scraper.get_urls_to_download,
+        'hawaii': hi_aggregate_site_scraper.get_urls_to_download,
+        'kentucky': ky_aggregate_site_scraper.get_urls_to_download,
         # 'new_york': ny_aggregate_site_scraper.get_urls_to_download,
         'texas': tx_aggregate_site_scraper.get_urls_to_download,
     }
---
+++ recidiviz/tests/ingest/aggregate/regions/hi/hi_aggregate_site_scraper_test.py
@@ -0,0 +1,43 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2018 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for hi_aggregate_ingest.py."""
+from unittest import TestCase
+from mock import patch, Mock
+import requests
+
+from recidiviz.ingest.aggregate.regions.hi import hi_aggregate_site_scraper
+from recidiviz.tests.ingest import fixtures
+
+REPORTS_HTML = fixtures.as_string('aggregate/regions/hi', 'report.html')
+
+
+class TestHiAggregateSiteScraper(TestCase):
+    """Test that fl_aggregate_site_scraper correctly scrapes urls."""
+
+    @patch.object(requests, 'get')
+    def testGetAllUrls(self, mockget):
+        mockresponse = Mock()
+        mockget.return_value = mockresponse
+        mockresponse.text = REPORTS_HTML
+        url1 = ('https://dps.hawaii.gov/wp-content/uploads/2019/01/'
+                'Pop-Reports-EOM-2018-12-31.pdf')
+        url2 = ('http://dps.hawaii.gov/wp-content/uploads/2016/01/'
+                'Pop-Reports-EOM-2014-12-31.pdf')
+        expected_urls = {url1, url2}
+
+        urls = hi_aggregate_site_scraper.get_urls_to_download()
+        self.assertEqual(expected_urls, urls)
