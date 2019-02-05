9e61d06a8f710429d0b93fa97adddb8ff54dc1ab
Zachary Lawrence <zacharyclawrence@gmail.com>
2019-02-05 17:07:08 -0500

    Add programmatically determining each df in HI (recidiviz/recidiviz-data#853)
    Fix HI error: `Length mismatch expected axis has 14 elements new values have 22 elements`
    
    GitOrigin-RevId: 224b6e69cea9da4e4ac458db85e36b85b77e674a

--- recidiviz/tests/ingest/aggregate/regions/hi/hi_aggregate_ingest_test.py
+++ recidiviz/tests/ingest/aggregate/regions/hi/hi_aggregate_ingest_test.py
@@ -34,12 +34,16 @@ from recidiviz.tests.ingest import fixtures
 from recidiviz.tests.utils import fakes
 
 DATE_SCRAPED = datetime.date(year=2018, month=11, day=30)
+DATE_SCRAPED_2 = datetime.date(year=2017, month=9, day=30)
+
 
 # Cache the parsed pdf between tests since it's expensive to compute
 @pytest.fixture(scope="class")
 def parsed_pdf(request):
     request.cls.parsed_pdf = hi_aggregate_ingest.parse(
         fixtures.as_filepath('Pop-Reports-EOM-2018-11-30.pdf'))
+    request.cls.parsed_pdf_2 = hi_aggregate_ingest.parse(
+        fixtures.as_filepath('pop-reports-eom-2017-09-30-17.pdf'))
 
 
 @pytest.mark.usefixtures("parsed_pdf")
@@ -110,6 +114,71 @@ class TestHiAggregateIngest(TestCase):
             'probation_violation_female_population': [0, 8],
             'fips': [4021, 15003],
             'report_date': 2 * [DATE_SCRAPED],
+            'report_granularity': 2 * [enum_strings.monthly_granularity]
+        }, index=range(10, 12))
+        assert_frame_equal(result.tail(n=2), expected_tail, check_names=False)
+
+    def testParseNewTableOrder_ParsesHeadAndTail(self):
+        result = self.parsed_pdf_2[HiFacilityAggregate]
+
+        # Assert Head
+        expected_head = pd.DataFrame({
+            'facility_name': ['Hawaii Community Correctional Center',
+                              'Halawa Correctional Facility - Special Needs'],
+            'design_bed_capacity': [206., 90.],
+            'operation_bed_capacity': [226., 132.],
+            'total_population': [379, 113],
+            'male_population': [310, 113],
+            'female_population': [69, 0],
+            'sentenced_felony_male_population': [32, 113],
+            'sentenced_felony_female_population': [5, 0],
+            'sentenced_felony_probation_male_population': [52, 0],
+            'sentenced_felony_probation_female_population': [14, 0],
+            'sentenced_misdemeanor_male_population': [19, 0],
+            'sentenced_misdemeanor_female_population': [6, 0],
+            'sentenced_pretrial_felony_male_population': [107, 0],
+            'sentenced_pretrial_felony_female_population': [16, 0],
+            'sentenced_pretrial_misdemeanor_male_population': [30, 0],
+            'sentenced_pretrial_misdemeanor_female_population': [11, 0],
+            'held_for_other_jurisdiction_male_population': [4, 0],
+            'held_for_other_jurisdiction_female_population': [0, 0],
+            'parole_violation_male_population': [4, 0],
+            'parole_violation_female_population': [1, 0],
+            'probation_violation_male_population': [62, 0],
+            'probation_violation_female_population': [16, 0],
+            'fips': [15001, 15003],
+            'report_date': 2 * [DATE_SCRAPED_2],
+            'report_granularity': 2 * [enum_strings.monthly_granularity]
+        })
+        assert_frame_equal(result.head(n=2), expected_head, check_names=False)
+
+        # Assert Tail
+        expected_tail = pd.DataFrame({
+            'facility_name': ['Saguaro Correctional Center, AZ',
+                              'Federal Detention Center, Honolulu'],
+            'design_bed_capacity': [NaN, NaN],
+            'operation_bed_capacity': [NaN, NaN],
+            'total_population': [1618, 114],
+            'male_population': [1618, 104],
+            'female_population': [0, 10],
+            'sentenced_felony_male_population': [1528, 2],
+            'sentenced_felony_female_population': [0, 0],
+            'sentenced_felony_probation_male_population': [0, 6],
+            'sentenced_felony_probation_female_population': [0, 4],
+            'sentenced_misdemeanor_male_population': [0, 5],
+            'sentenced_misdemeanor_female_population': [0, 0],
+            'sentenced_pretrial_felony_male_population': [0, 5],
+            'sentenced_pretrial_felony_female_population': [0, 2],
+            'sentenced_pretrial_misdemeanor_male_population': [0, 3],
+            'sentenced_pretrial_misdemeanor_female_population': [0, 0],
+            'held_for_other_jurisdiction_male_population': [0, 1],
+            'held_for_other_jurisdiction_female_population': [0, 0],
+            'parole_violation_male_population': [90, 10],
+            'parole_violation_female_population': [0, 0],
+            'probation_violation_male_population': [0, 72],
+            'probation_violation_female_population': [0, 4],
+            'fips': [4021, 15003],
+            'report_date': 2 * [DATE_SCRAPED_2],
             'report_granularity': 2 * [enum_strings.monthly_granularity]
         }, index=range(10, 12))
         assert_frame_equal(result.tail(n=2), expected_tail, check_names=False)
