647005210b6692ab485fd9b716432b9f8bb269ec
ohinds <ohinds@gmail.com>
2019-02-08 10:40:13 -0500

    Add revoked to bond status (Recidiviz/recidiviz-data#908)
    GitOrigin-RevId: 346c2ee99b2fe8cc554b5f714d562c18fee86885

--- recidiviz/persistence/database/schema.py
+++ recidiviz/persistence/database/schema.py
@@ -130,6 +130,7 @@ bond_status_values = (enum_strings.bond_status_denied,
                       enum_strings.bond_status_not_required,
                       enum_strings.bond_status_pending,
                       enum_strings.bond_status_posted,
+                      enum_strings.bond_status_revoked,
                       enum_strings.unknown_removed_from_source,
                       enum_strings.bond_status_set)
 
