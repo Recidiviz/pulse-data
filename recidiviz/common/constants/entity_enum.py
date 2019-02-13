c387e099f91953ef271145d13244fde2b94e641f
jamwalla <james.wallacelee@gmail.com>
2019-02-13 15:46:34 -0500

    enum overrides (Recidiviz/recidiviz-data#954)
    Co-Authored-By: Zachary Lawrence <zacharylawrence@users.noreply.github.com>
    GitOrigin-RevId: 6455a15b980beadd90ecfa756dc91566fab1b87b

--- recidiviz/common/constants/entity_enum.py
+++ recidiviz/common/constants/entity_enum.py
@@ -14,7 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""Contains logic related to MappableEnums"""
+
+"""Contains logic related to EntityEnums."""
+
 import re
 import string
 from enum import Enum, EnumMeta
---
+++ recidiviz/tests/common/constants/enum_overrides_test.py
@@ -0,0 +1,72 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for EnumOverrides class."""
+
+import unittest
+
+from recidiviz.common.constants.bond import BondStatus, BondType
+from recidiviz.common.constants.charge import ChargeClass, ChargeDegree
+from recidiviz.common.constants.enum_overrides import EnumOverrides
+from recidiviz.common.constants.person import Race, Ethnicity
+
+
+class EnumOverridesTest(unittest.TestCase):
+    """Tests for EnumOverrides class."""
+
+    def test_add(self):
+        overrides_builder = EnumOverrides.Builder()
+        overrides_builder.add('A', Race.ASIAN)
+        overrides_builder.add('A', ChargeDegree.FIRST)
+
+        overrides = overrides_builder.build()
+
+        self.assertIsNone(overrides.parse('A', ChargeClass))
+        self.assertEqual(overrides.parse('A', Race), Race.ASIAN)
+        self.assertEqual(overrides.parse('A', ChargeDegree), ChargeDegree.FIRST)
+
+    def test_add_fromDifferentEnum(self):
+        overrides_builder = EnumOverrides.Builder()
+
+        overrides = overrides_builder.build()
+
+        self.assertIsNone(overrides.parse('LATINO', Ethnicity))
+
+    def test_add_matchPredicate(self):
+        is_pending = lambda s: s.startswith('PENDING')
+
+        overrides_builder = EnumOverrides.Builder()
+        overrides_builder.add(is_pending, BondStatus.PENDING)
+
+        overrides = overrides_builder.build()
+
+        self.assertIsNone(overrides.parse('PEND', BondStatus))
+        self.assertEqual(overrides.parse('PENDING', BondStatus),
+                         BondStatus.PENDING)
+        self.assertEqual(overrides.parse('PENDING - WAITING TO SEE MAGISTRATE',
+                                         BondStatus), BondStatus.PENDING)
+
+    def test_ignore(self):
+        overrides_builder = EnumOverrides.Builder()
+        overrides_builder.ignore('A', ChargeClass)
+        overrides_builder.ignore('NONE')
+
+        overrides = overrides_builder.build()
+
+        self.assertTrue(overrides.should_ignore('NONE', ChargeClass))
+        self.assertTrue(overrides.should_ignore('NONE', ChargeClass))
+        self.assertTrue(overrides.should_ignore('A', ChargeClass))
+        self.assertFalse(overrides.should_ignore('A', BondType))
