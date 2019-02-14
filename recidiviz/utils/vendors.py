4ea035c9acddccf77afff6f2e9f1dd25b967db59
Colin Adams <adamscolinc@gmail.com>
2019-02-14 11:53:56 -0500

    Shared jailtracker queue (recidiviz/recidiviz-data#981)
    * create vendor scrapers
    
    * jailtracker queue file
    
    * make jailtracker regions use shared queue
    
    * more
    
    * remove todos
    
    * add shared queue to new jailtracker regions
    
    * fix flaky test - set order
    
    GitOrigin-RevId: 4779476e14bf8cd27f9fd3287581034b491f7d51

---
+++ recidiviz/utils/vendors.py
@@ -0,0 +1,40 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Utility functions related to vendors"""
+import os
+import pkgutil
+from typing import Any, Dict, Optional, Set
+
+import yaml
+
+BASE_VENDOR_PATH = 'recidiviz/ingest/scrape/vendors'
+def get_vendors() -> Set[str]:
+    return {vendor_module.name for vendor_module
+            in pkgutil.iter_modules([BASE_VENDOR_PATH])}
+
+def get_vendor_queue_params(vendor: str) -> Optional[Dict[str, Any]]:
+    """Gets the queue params for the given region.
+
+    Returns:
+        - None, if queue.yaml does not exist (and a queue should not be created)
+        - dict, if queue.yaml does exist (and a queue should be created)
+    """
+    queue_param_path = os.path.join(BASE_VENDOR_PATH, vendor, 'queue.yaml')
+    if not os.path.exists(queue_param_path):
+        return None
+    with open(queue_param_path) as queue_params:
+        return yaml.load(queue_params) or {}
