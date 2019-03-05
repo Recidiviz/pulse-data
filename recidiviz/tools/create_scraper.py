37f482b89e923d68e72ecc5a679e4bbed7b30dd4
Zachary Lawrence <zacharyclawrence@gmail.com>
2019-03-05 16:35:40 -0500

    Create main() for create_scraper (recidiviz/recidiviz-data#1193)
    Co-Authored-By: terinpw <terinpw@users.noreply.github.com>
    GitOrigin-RevId: 35e7a0020eadb3f51857e0ad56e1fae2dcf37dfa

--- recidiviz/tools/create_scraper.py
+++ recidiviz/tools/create_scraper.py
@@ -46,81 +46,8 @@ import recidiviz.tests.ingest.scrape.regions
 from recidiviz.utils import regions
 
 
-def populate_file(template_path, target_path, subs):
-    with open(template_path) as template:
-        template = Template(template.read())
-        contents = template.substitute(subs)
-
-    with open(target_path, 'w') as target:
-        target.write(contents)
-
-
-def create_scraper_files(subs, vendor: Optional[str]):
-    """Creates __init__.py, region_name_scraper.py, and region_name.yaml files
-    in recidiviz/ingest/scrape/regions/region_name
-    """
-
-    def create_scraper(template):
-        target = os.path.join(target_dir, subs['region'] + '_scraper.py')
-        populate_file(template, target, subs)
-
-    def create_extractor_yaml(template):
-        target = os.path.join(target_dir, subs['region'] + '.yaml')
-        populate_file(template, target, subs)
-
-    def create_manifest_yaml(template):
-        target = os.path.join(target_dir, 'manifest.yaml')
-        populate_file(template, target, subs)
-
-    regions_dir = os.path.dirname(recidiviz.ingest.scrape.regions.__file__)
-    if not os.path.exists(regions_dir):
-        raise OSError("Couldn't find directory "
-                      "recidiviz/ingest/scrape/regions.")
-    template_dir = os.path.join(os.path.dirname(__file__), 'scraper_template')
-    target_dir = os.path.join(regions_dir, subs['region'])
-    if os.path.exists(target_dir):
-        raise OSError('directory %s already exists' % target_dir)
-    os.mkdir(target_dir)
-
-    init_template = os.path.join(template_dir, '__init__.txt')
-    init_target = os.path.join(target_dir, '__init__.py')
-    populate_file(init_template, init_target, subs)
-
-    if vendor:
-        template_dir = os.path.join(template_dir, vendor)
-    scraper_template = os.path.join(template_dir, 'region_scraper.txt')
-    create_scraper(scraper_template)
-    create_manifest_yaml(os.path.join(template_dir, 'manifest.txt'))
-
-    if not vendor:
-        yaml_template = os.path.join(template_dir, 'region.txt')
-        create_extractor_yaml(yaml_template)
-
-
-def create_test_files(subs, vendor: Optional[str]):
-    def create_test(template):
-        test_target_file_name = subs['region'] + '_scraper_test.py'
-        test_target = os.path.join(target_test_dir, test_target_file_name)
-        populate_file(template, test_target, subs)
-
-    ingest_dir = os.path.dirname(recidiviz.ingest.scrape.regions.__file__)
-    test_dir = os.path.dirname(recidiviz.tests.ingest.scrape.regions.__file__)
-    if not os.path.exists(ingest_dir):
-        raise OSError('Couldn\'t find directory '
-                      'recidiviz/tests/ingest/scrape/regions.')
-    target_test_dir = os.path.join(test_dir, subs['region'])
-    if os.path.exists(target_test_dir):
-        raise OSError('directory %s already exists' % target_test_dir)
-    os.mkdir(target_test_dir)
-
-    template_dir = os.path.join(os.path.dirname(__file__), 'scraper_template')
-    if vendor:
-        template_dir = os.path.join(template_dir, vendor)
-    test_template = os.path.join(template_dir, 'region_scraper_test.txt')
-    create_test(test_template)
-
-
-if __name__ == '__main__':
+def main():
+    """Main entry point for create_scraper."""
     parser = argparse.ArgumentParser()
     parser.add_argument('county')
     parser.add_argument('state')
@@ -165,5 +92,83 @@ if __name__ == '__main__':
         substitutions[optional_arg] = arg_value if arg_value is not None else ''
 
     if not args.tests_only:
-        create_scraper_files(substitutions, args.vendor)
-    create_test_files(substitutions, args.vendor)
+        _create_scraper_files(substitutions, args.vendor)
+    _create_test_files(substitutions, args.vendor)
+
+
+def _create_scraper_files(subs, vendor: Optional[str]):
+    """Creates __init__.py, region_name_scraper.py, and region_name.yaml files
+    in recidiviz/ingest/scrape/regions/region_name
+    """
+
+    def create_scraper(template):
+        target = os.path.join(target_dir, subs['region'] + '_scraper.py')
+        _populate_file(template, target, subs)
+
+    def create_extractor_yaml(template):
+        target = os.path.join(target_dir, subs['region'] + '.yaml')
+        _populate_file(template, target, subs)
+
+    def create_manifest_yaml(template):
+        target = os.path.join(target_dir, 'manifest.yaml')
+        _populate_file(template, target, subs)
+
+    regions_dir = os.path.dirname(recidiviz.ingest.scrape.regions.__file__)
+    if not os.path.exists(regions_dir):
+        raise OSError("Couldn't find directory "
+                      "recidiviz/ingest/scrape/regions.")
+    template_dir = os.path.join(os.path.dirname(__file__), 'scraper_template')
+    target_dir = os.path.join(regions_dir, subs['region'])
+    if os.path.exists(target_dir):
+        raise OSError('directory %s already exists' % target_dir)
+    os.mkdir(target_dir)
+
+    init_template = os.path.join(template_dir, '__init__.txt')
+    init_target = os.path.join(target_dir, '__init__.py')
+    _populate_file(init_template, init_target, subs)
+
+    if vendor:
+        template_dir = os.path.join(template_dir, vendor)
+    scraper_template = os.path.join(template_dir, 'region_scraper.txt')
+    create_scraper(scraper_template)
+    create_manifest_yaml(os.path.join(template_dir, 'manifest.txt'))
+
+    if not vendor:
+        yaml_template = os.path.join(template_dir, 'region.txt')
+        create_extractor_yaml(yaml_template)
+
+
+def _create_test_files(subs, vendor: Optional[str]):
+    def create_test(template):
+        test_target_file_name = subs['region'] + '_scraper_test.py'
+        test_target = os.path.join(target_test_dir, test_target_file_name)
+        _populate_file(template, test_target, subs)
+
+    ingest_dir = os.path.dirname(recidiviz.ingest.scrape.regions.__file__)
+    test_dir = os.path.dirname(recidiviz.tests.ingest.scrape.regions.__file__)
+    if not os.path.exists(ingest_dir):
+        raise OSError('Couldn\'t find directory '
+                      'recidiviz/tests/ingest/scrape/regions.')
+    target_test_dir = os.path.join(test_dir, subs['region'])
+    if os.path.exists(target_test_dir):
+        raise OSError('directory %s already exists' % target_test_dir)
+    os.mkdir(target_test_dir)
+
+    template_dir = os.path.join(os.path.dirname(__file__), 'scraper_template')
+    if vendor:
+        template_dir = os.path.join(template_dir, vendor)
+    test_template = os.path.join(template_dir, 'region_scraper_test.txt')
+    create_test(test_template)
+
+
+def _populate_file(template_path, target_path, substitutions):
+    with open(template_path) as template:
+        template = Template(template.read())
+        contents = template.substitute(substitutions)
+
+    with open(target_path, 'w') as target:
+        target.write(contents)
+
+
+if __name__ == '__main__':
+    main()
