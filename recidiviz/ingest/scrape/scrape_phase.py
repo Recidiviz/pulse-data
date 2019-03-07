e4be57e31fe504324b5d201b664a439340407694
Colin Adams <adamscolinc@gmail.com>
2019-03-06 20:02:50 -0500

    Run stop, infer release when scraper is finished (Recidiviz/recidiviz-data#1161)
    * start
    
    * launch stop when finished
    
    * updates
    
    * fixes
    
    * fix tests
    
    * scraper-control -> scraper-phase
    
    * fix fix fix
    
    * type
    
    * fix tests
    
    GitOrigin-RevId: 4ed28abec531a9c26438925abec1c68ddce5cea6

---
+++ recidiviz/ingest/scrape/scrape_phase.py
@@ -0,0 +1,31 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2018 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""Defines the order of processes to run once a scrape has finished"""
+
+from typing import Optional
+
+_next_phase = {
+    'scraper_status.check_for_finished_scrapers':
+        'scraper_control.scraper_stop',
+    'scraper_control.scraper_stop': 'infer_release.infer_release',
+    'infer_release.infer_release': None,
+}
+
+def next_phase(current_phase: Optional[str]) -> Optional[str]:
+    if current_phase is None:
+        raise ValueError('Unable to identify current scrape phase.')
+    return _next_phase[current_phase]
