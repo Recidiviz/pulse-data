f4d6921ffc5af1831f0064d7d011e6e641f72a08
Zachary Lawrence <zacharyclawrence@gmail.com>
2019-03-11 11:17:15 -0400

    Update create_scraper to set jid (Recidiviz/recidiviz-data#1203)
    Co-Authored-By: terinpw terinpw@users.noreply.github.com
    GitOrigin-RevId: 5885ebe9e048d22ffd1b0cc4aaf7133a3bce9c75

---
+++ recidiviz/common/jid.py
@@ -0,0 +1,79 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Common utility functions used to manipulate jurisdiction_ids.
+
+Jurisdiction Ids are unique identifiers (ints) which are mapped 1:1 to each
+jurisdiction in the United States. Each Jurisdiction Id starts with the FIPS
+county code that that jurisdiction falls into. This means that a single
+jurisdiction may map to multiple jurisdiction_ids if that jurisdiction falls
+into multiple counties.
+
+The `data_sets/jid.csv` file contains a mapping of (agency_name, state)
+to jurisdiction_id. It's important to note that when joining scraped data with
+a jurisdiction_id, we must take the county's state into account since
+multiple states share the same county names.
+"""
+
+import pandas as pd
+import us
+from more_itertools import one
+
+from recidiviz.common.errors import FipsMergingError
+from recidiviz.common.fips import get_fips_for
+from recidiviz.common.fips_fuzzy_matching import best_match
+from recidiviz.tests.ingest.fixtures import as_filepath
+
+# Float between [0, 1] which sets the required fuzzy matching certainty
+_FUZZY_MATCH_CUTOFF = 0.75
+
+_JID = pd.read_csv(as_filepath('jid.csv', subdir='data_sets'))
+
+
+def get(county_name: str, state: us.states) -> str:
+    """Return the matching jurisdiction_id if one jurisdiction_id can be
+    matched to the county_name, otherwise raise a FipsMergingError."""
+    county_fips = _to_county_fips(county_name, state)
+    return _to_jurisdiction_id(county_fips)
+
+
+def _to_county_fips(county_name: str, state: us.states) -> int:
+    """Lookup fips by county_name, filtering within the given state"""
+    all_fips_for_state_df = get_fips_for(state)
+    actual_county_name = best_match(
+        county_name, all_fips_for_state_df.index, _FUZZY_MATCH_CUTOFF)
+
+    try:
+        return all_fips_for_state_df.at[actual_county_name, 'fips']
+    except KeyError:
+        raise FipsMergingError
+
+
+def _to_jurisdiction_id(county_fips: int) -> str:
+    """Lookup jurisdiction_id by manifest_agency_name, filtering within the
+    given county_fips"""
+    jids_matching_county_fips = _JID.loc[_JID['fips'] == county_fips]
+
+    # Some jurisdictions in jid.csv have no listed names.
+    jids_matching_county_fips = \
+        jids_matching_county_fips.dropna(subset=['name'])
+
+    # If only one jurisdiction in the county, assume it's a match
+    if len(jids_matching_county_fips) == 1:
+        return one(jids_matching_county_fips['jid'])
+
+    raise FipsMergingError
