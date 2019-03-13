8ef4701ac7f526dca6fe837bcb00bcacdbb29847
Zachary Garcia <zacharydgarcia@gmail.com>
2019-03-13 18:36:37 -0400

    Add sentence date fields (Recidiviz/recidiviz-data#1287)
    * Add fields to schema
    
    * Add converter logic
    
    * Fix date conversion
    
    * [skip validation] dummy commit
    
    * Undo dummy commit
    
    GitOrigin-RevId: b13a568674ec70741ecba016d15b5b0c5cd02072

--- recidiviz/persistence/converter/sentence.py
+++ recidiviz/persistence/converter/sentence.py
@@ -15,8 +15,12 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ============================================================================
 """Converts an ingest_info proto Sentence to a persistence entity."""
+from datetime import date
+from typing import Optional, Tuple
+
 from recidiviz.common.common_utils import normalize
 from recidiviz.common.constants.sentence import SentenceStatus
+from recidiviz.common.date import parse_date
 from recidiviz.common.ingest_metadata import IngestMetadata
 from recidiviz.persistence import entities
 from recidiviz.persistence.converter.converter_utils import (fn, parse_bool,
@@ -40,9 +44,30 @@ def convert(proto, metadata: IngestMetadata) -> entities.Sentence:
     new.parole_possible = fn(parse_bool, 'parole_possible', proto)
     new.post_release_supervision_length_days = \
         fn(parse_days, 'post_release_supervision_length', proto)
+    new.date_imposed = fn(parse_date, 'date_imposed', proto)
+    new.completion_date, new.projected_completion_date = _parse_completion_date(
+        proto, metadata)
     new.status = fn(SentenceStatus.parse, 'status', proto,
                     metadata.enum_overrides,
                     default=SentenceStatus.UNKNOWN_FOUND_IN_SOURCE)
     new.status_raw_text = fn(normalize, 'status', proto)
 
     return new.build()
+
+def _parse_completion_date(
+        proto,
+        metadata: IngestMetadata) -> Tuple[Optional[date], Optional[date]]:
+    """Reads completion_date and projected_completion_date from |proto|.
+
+    If completion_date is in the future relative to scrape time, will be
+    treated as projected_completion_date instead.
+    """
+    completion_date = fn(parse_date, 'completion_date', proto)
+    projected_completion_date = fn(
+        parse_date, 'projected_completion_date', proto)
+
+    if completion_date and completion_date > metadata.last_seen_time.date():
+        projected_completion_date = completion_date
+        completion_date = None
+
+    return completion_date, projected_completion_date
--- recidiviz/tests/persistence/converter/sentence_test.py
+++ recidiviz/tests/persistence/converter/sentence_test.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """Tests for converting sentences."""
+import datetime
 import unittest
 
 from recidiviz.common.constants.sentence import SentenceStatus
@@ -32,18 +33,45 @@ class SentenceConverterTest(unittest.TestCase):
         ingest_sentence = ingest_info_pb2.Sentence(
             sentence_id='SENTENCE_ID',
             min_length='1',
+            date_imposed='2000-1-1',
             post_release_supervision_length=''
         )
 
         # Act
         result = sentence.convert(ingest_sentence,
-                                  IngestMetadata.new_with_defaults())
+                                  IngestMetadata.new_with_defaults(
+                                      last_seen_time=datetime.datetime(
+                                          year=2018, month=6, day=8)))
 
         # Assert
         expected_result = entities.Sentence.new_with_defaults(
             external_id='SENTENCE_ID',
             min_length_days=1,
             post_release_supervision_length_days=0,
+            date_imposed=datetime.date(year=2000, month=1, day=1),
+            status=SentenceStatus.UNKNOWN_FOUND_IN_SOURCE
+        )
+
+        self.assertEqual(result, expected_result)
+
+    def testCompletionDateInFuture(self):
+        # Arrange
+        ingest_sentence = ingest_info_pb2.Sentence(
+            sentence_id='SENTENCE_ID',
+            completion_date='2020-1-1',
+        )
+
+        # Act
+        result = sentence.convert(ingest_sentence,
+                                  IngestMetadata.new_with_defaults(
+                                      last_seen_time=datetime.datetime(
+                                          year=2018, month=6, day=8)))
+
+        # Assert
+        expected_result = entities.Sentence.new_with_defaults(
+            external_id='SENTENCE_ID',
+            completion_date=None,
+            projected_completion_date=datetime.date(year=2020, month=1, day=1),
             status=SentenceStatus.UNKNOWN_FOUND_IN_SOURCE
         )
 
