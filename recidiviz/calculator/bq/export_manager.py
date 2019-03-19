b50837fa96d446e396adf5b18765ab29037aac1f
rasmi <rrelasmar@gmail.com>
2019-03-19 11:57:03 -0400

    Add BQ export to tasks and cron. (recidiviz/recidiviz-data#1345)
    * Added bq queue config settings.
    
    * Added function for adding BQ tasks.
    
    * Added date to bq export task name.
    
    * Added url parameter and docstring.
    
    * Added status returns to bq_load functions.
    
    * Added BQ export task and cron integrations.
    
    * Added bq task test.
    
    * Removed unnecessary auth check.
    
    * Remove try-except since Flask handles exceptions.
    
    GitOrigin-RevId: 115c8cccac5c193cbafaf9c5f66e07ba009afce5

--- recidiviz/calculator/bq/export_manager.py
+++ recidiviz/calculator/bq/export_manager.py
@@ -17,9 +17,13 @@
 
 """Export data from Cloud SQL and load it into BigQuery."""
 
+from http import HTTPStatus
+import json
 import logging
 from typing import Optional
 
+import flask
+from flask import request
 # Importing only for typing.
 from google.cloud import bigquery
 
@@ -27,11 +31,13 @@ from recidiviz.calculator.bq import bq_load
 from recidiviz.calculator.bq import bq_utils
 from recidiviz.calculator.bq import cloudsql_export
 from recidiviz.calculator.bq import export_config
+from recidiviz.common import queues
+from recidiviz.utils.auth import authenticate_request
 
 
 def export_table_then_load_table(
         table: str,
-        dataset_ref: Optional[bigquery.dataset.DatasetReference] = None):
+        dataset_ref: Optional[bigquery.dataset.DatasetReference] = None)-> bool:
     """Exports a Cloud SQL table to CSV, then loads it into BigQuery.
 
     Waits until the BigQuery load is completed.
@@ -42,18 +48,21 @@ def export_table_then_load_table(
         dataset_ref: The BigQuery dataset to load the table into.
             Gets created if it does not already exist.
             Uses export_config.BASE_TABLES_BQ_DATASET if not specified.
+    Returns:
+        True if load succeeds, else False.
     """
     if not dataset_ref:
         dataset_ref = bq_utils.client().dataset(
             export_config.BASE_TABLES_BQ_DATASET)
 
     export_success = cloudsql_export.export_table(table)
-    if export_success:
-        bq_load.start_table_load_and_wait(
-            dataset_ref, table)
+    if export_success: # pylint: disable=no-else-return
+        load_success = bq_load.start_table_load_and_wait(dataset_ref, table)
+        return load_success
     else:
         logging.error('Skipping BigQuery load of table "%s", '
                       'which failed to export.', table)
+        return False
 
 
 def export_then_load_all_sequentially():
@@ -95,6 +104,42 @@ def export_all_then_load_all():
         export_config.BASE_TABLES_BQ_DATASET)
     bq_load.load_all_tables_concurrently(
         BASE_TABLES_DATASET_REF, export_config.TABLES_TO_EXPORT)
+
+
+export_manager_blueprint = flask.Blueprint('export_manager', __name__)
+
+@export_manager_blueprint.route('/export', methods=['POST'])
+@authenticate_request
+def handle_bq_export_task():
+    """Worker function to handle BQ export task requests.
+
+    Form data must be a bytes-encoded JSON object with parameters listed below.
+
+    URL Parameters:
+        table_name: Table to export then import. Table must be defined
+            in export_config.TABLES_TO_EXPORT.
+    """
+    json_data = request.get_data(as_text=True)
+    data = json.loads(json_data)
+    table_name = data['table_name']
+
+    success = export_table_then_load_table(table_name)
+
+    return ('', HTTPStatus.OK if success else HTTPStatus.INTERNAL_SERVER_ERROR)
+
+
+@export_manager_blueprint.route('/create_export_tasks')
+@authenticate_request
+def create_all_bq_export_tasks():
+    """Creates an export task for each table to be exported.
+
+    A task is created for each table defined in export_config.TABLES_TO_EXPORT.
+
+    Re-creates all tasks if any task fails to be created.
+    """
+    for table in export_config.TABLES_TO_EXPORT:
+        queues.create_bq_task(table.name, '/export_manager/export')
+    return ('', HTTPStatus.OK)
 
 
 if __name__ == '__main__':
