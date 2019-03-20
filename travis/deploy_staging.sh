5b91747086405cab00c57365114ad92af8a2bffd
Colin Adams <adamscolinc@gmail.com>
2019-03-20 15:53:10 -0400

    actually set variables in travis (recidiviz/recidiviz-data#1374)
    * actually set variables in travis
    
    * echo new vars
    
    GitOrigin-RevId: 5be24c22f98bc09dba681df92131111f98a64093

--- travis/deploy_staging.sh
+++ travis/deploy_staging.sh
@@ -25,14 +25,16 @@ docker cp recidiviz:/app/queue.yaml . 2>&1 | ind
 gcloud -q app deploy queue.yaml --project=recidiviz-staging 2>&1 | ind
 
 # App engine doesn't allow '.' in the version name
-VERSION=$(echo $TRAVIS_TAG | tr '.' '-') 2>&1 | ind
+VERSION=$(echo $TRAVIS_TAG | tr '.' '-')
+echo $VERSION 2>&1 | ind
 
 # Authorize docker to acess GCR
 # use instead of 'auth configure-docker' as travis has an old gcloud
 gcloud -q docker --authorize-only 2>&1 | ind
 
 # Push the docker image to GCR
-IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.$VERSION:latest 2>&1 | ind
+IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.$VERSION:latest
+echo $IMAGE_URL 2>&1 | ind
 docker tag recidiviz-image $IMAGE_URL 2>&1 | ind
 docker push $IMAGE_URL 2>&1 | ind
 
