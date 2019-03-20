80cfc8ea84057187d4196d2481945dc1e1a2cbd6
Colin Adams <adamscolinc@gmail.com>
2019-03-20 11:23:30 -0400

    disable prompts in all gcloud commands (recidiviz/recidiviz-data#1369)
    GitOrigin-RevId: 3b14e234ecacf915f5056bb41229d5ca7ce2c62b

--- travis/deploy_staging.sh
+++ travis/deploy_staging.sh
@@ -5,23 +5,23 @@
 
 # Create encryption keys and authenticate with GAE
 openssl aes-256-cbc -K $encrypted_f7b0b16e1068_key -iv $encrypted_f7b0b16e1068_iv -in client-secret-staging.json.enc -out client-secret-staging.json -d
-gcloud auth activate-service-account recidiviz-staging@appspot.gserviceaccount.com --key-file client-secret-staging.json
+gcloud -q auth activate-service-account recidiviz-staging@appspot.gserviceaccount.com --key-file client-secret-staging.json
 
 # Deploy cron.yaml
-gcloud app deploy cron.yaml --project=recidiviz-staging
+gcloud -q app deploy cron.yaml --project=recidiviz-staging
 
 # Generate and deploy queue.yaml
 echo "Starting deploy of queue.yaml"
 docker exec -it recidiviz pipenv run python -m recidiviz.tools.build_queue_config --environment all
 docker cp recidiviz:/app/queue.yaml .
-gcloud app deploy queue.yaml --project=recidiviz-staging
+gcloud -q app deploy queue.yaml --project=recidiviz-staging
 
 # App engine doesn't allow '.' in the version name
 VERSION=$(echo $TRAVIS_TAG | tr '.' '-')
 
 # Authorize docker to acess GCR
 # use instead of 'auth configure-docker' as travis has an old gcloud
-gcloud docker --authorize-only
+gcloud -q docker --authorize-only
 
 # Push the docker image to GCR
 IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.$VERSION:latest
@@ -29,4 +29,4 @@ docker tag recidiviz-image $IMAGE_URL
 docker push $IMAGE_URL
 
 # Deploy application
-gcloud app deploy staging.yaml --project recidiviz-staging --version $VERSION --image-url $IMAGE_URL
+gcloud -q app deploy staging.yaml --project recidiviz-staging --version $VERSION --image-url $IMAGE_URL
