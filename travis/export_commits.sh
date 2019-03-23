7b44f9b9b67f88c0e07c712d614fff8b41e95041
Colin Adams <adamscolinc@gmail.com>
2019-03-23 14:23:55 -0400

    Fix travis deploy script issues (Recidiviz/recidiviz-data#1453)
    * Prevent timeouts on deploy
    
    * fix Recidiviz/recidiviz-data#1392
    
    GitOrigin-RevId: cce939f6808f1ec6a38a1ed5d0f87d5bd21b09d1

--- travis/export_commits.sh
+++ travis/export_commits.sh
@@ -1,21 +1,23 @@
-echo "setup"
-git config user.email "helperbot@recidiviz.com"
-git config user.name "Helper Bot"
-echo "https://helperbot-recidiviz:${GH_TOKEN}@github.com" > .git/credentials
+source travis/base.sh
+
+# Setup git
+git config user.email "helperbot@recidiviz.com" 2>&1 | ind
+git config user.name "Helper Bot" 2>&1 | ind
+echo "https://helperbot-recidiviz:${GH_TOKEN}@github.com" > .git/credentials 2>&1 | ind
 
 # TODO(#1361): Use pre-built copybara image when it's available.
-echo "build"
-docker build github.com/google/copybara.git -t copybara:latest
+# Build the copybara image
+docker build github.com/google/copybara.git -t copybara:latest 2>&1 | ind
 
-echo "run"
+# Run copybara
 docker run -e COPYBARA_CONFIG='mirror/copy.bara.sky' \
            -e COPYBARA_WORKFLOW='exportSourceToPublic' \
            -v "$(pwd)/.git/config":/root/.gitconfig \
            -v "$(pwd)/.git/credentials":/root/.git-credentials \
            -v "$(pwd)":/usr/src/app \
-           -it copybara copybara
+           -it copybara copybara 2>&1 | ind
 
-echo "cleanup"
-git config --unset user.email
-git config --unset user.name
-rm .git/credentials
+# Cleanup git
+git config --unset user.email 2>&1 | ind
+git config --unset user.name 2>&1 | ind
+rm .git/credentials 2>&1 | ind
