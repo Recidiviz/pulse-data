d420f8fb3bd1edc30e3869236a2162d6881bc3b4
Arian Karbasi <ratboy33@gmail.com>
2019-03-23 15:12:09 -0400

    fix no proxy bool (Recidiviz/recidiviz-data#1454)
    * fix no proxy bool
    
    * add test for default proxy
    
    GitOrigin-RevId: fd5b34bad141ddb821327072dff9e8166041c161

--- recidiviz/ingest/scrape/base_scraper.py
+++ recidiviz/ingest/scrape/base_scraper.py
@@ -108,7 +108,7 @@ class BaseScraper(Scraper):
         logging.info('Fetching content with endpoint: %s', endpoint)
 
         # Create cookie jar to pass to fetch
-        should_proxy = bool(self.get_region().should_proxy)
+        should_proxy = self.get_region().should_proxy
         response = self.fetch_page(
             endpoint, headers=headers, cookies=cookies, params=params,
             post_data=post_data, json_data=json_data, should_proxy=should_proxy)
