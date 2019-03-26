ef8b939a56b528f2e149fdfd3ce37796b592a60c
Colin Adams <adamscolinc@gmail.com>
2019-03-26 15:33:32 -0400

    travis allow copybara no-ops (Recidiviz/recidiviz-data#1514)
    * tests
    
    * travis allow copybara no-ops
    
    GitOrigin-RevId: f69d526fe64f0213edf54e133f8d06c202f92115

--- travis/export_commits.sh
+++ travis/export_commits.sh
@@ -10,12 +10,24 @@ echo "https://helperbot-recidiviz:${GH_TOKEN}@github.com" > .git/credentials 2>&
 docker build github.com/google/copybara.git -t copybara:latest 2>&1 | ind
 
 # Run copybara
+#
+# Note: Copybara returns exit code 4 if the command was a no-op. This will
+# happen if the change only modified files that are excluded from the public
+# mirror. In this case we still want the script to pass, so we capture the exit
+# code and only exit if it is not 0 or 4.
+set +e
 docker run -e COPYBARA_CONFIG='mirror/copy.bara.sky' \
            -e COPYBARA_WORKFLOW='exportSourceToPublic' \
            -v "$(pwd)/.git/config":/root/.gitconfig \
            -v "$(pwd)/.git/credentials":/root/.git-credentials \
            -v "$(pwd)":/usr/src/app \
            -it copybara copybara 2>&1 | ind
+code=$? 
+if [ "$code" -ne 0 -a "$code" -ne 4 ];
+then
+   exit $code
+fi;
+set -e
 
 # Cleanup git
 git config --unset user.email 2>&1 | ind
