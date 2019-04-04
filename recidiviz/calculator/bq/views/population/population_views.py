d110d051b462d08d300ed2d889b2911e7197d799
rasmi <rrelasmar@gmail.com>
2019-04-04 13:23:37 -0400

    Add 'all' race/gender to population count views, add resident pop counts. (Recidiviz/recidiviz-data#1601)
    * Added race_gender_all View.
    
    * Added total_jail_person_count to admissions_releases_race_gender_all.
    
    * Updated docstring.
    
    * Added resident_population_counts View.
    
    * Added resident_population_counts to population_views.
    
    * Added jail_pop_and_resident_pop view.
    
    * Clarify UNKNOWN.
    
    * Clarify UNKNOWN gender.
    
    GitOrigin-RevId: 9c656bbe82979aa783d230dd3d9312a2acfa59f7

--- recidiviz/calculator/bq/views/population/population_views.py
+++ recidiviz/calculator/bq/views/population/population_views.py
@@ -19,11 +19,20 @@
 
 from recidiviz.calculator.bq.views.population.population_admissions_releases_race_gender import \
     POPULATION_ADMISSIONS_RELEASES_RACE_GENDER_VIEW
+from recidiviz.calculator.bq.views.population.population_admissions_releases_race_gender_all import \
+    POPULATION_ADMISSIONS_RELEASES_RACE_GENDER_ALL_VIEW
 from recidiviz.calculator.bq.views.population.population_admissions_releases import \
     POPULATION_ADMISSIONS_RELEASES_VIEW
+from recidiviz.calculator.bq.views.population.resident_population_counts import \
+    RESIDENT_POPULATION_COUNTS_VIEW
+from recidiviz.calculator.bq.views.population.jail_pop_and_resident_pop import \
+    JAIL_POP_AND_RESIDENT_POP_VIEW
 
 
 POPULATION_VIEWS = [
+    RESIDENT_POPULATION_COUNTS_VIEW,
     POPULATION_ADMISSIONS_RELEASES_RACE_GENDER_VIEW,
-    POPULATION_ADMISSIONS_RELEASES_VIEW
+    POPULATION_ADMISSIONS_RELEASES_RACE_GENDER_ALL_VIEW,
+    POPULATION_ADMISSIONS_RELEASES_VIEW,
+    JAIL_POP_AND_RESIDENT_POP_VIEW
 ]
---
+++ recidiviz/calculator/bq/views/population/resident_population_counts.py
@@ -0,0 +1,124 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Resident population counts by race and gender for each year-fips."""
+# pylint: disable=line-too-long
+
+from recidiviz.calculator.bq.views import bqview
+
+from recidiviz.calculator.bq.views.vera import vera_view_constants
+
+from recidiviz.utils import metadata
+
+
+PROJECT_ID = metadata.project_id()
+
+RESIDENT_POPULATION_COUNTS_VIEW_NAME = 'resident_population_counts'
+
+RESIDENT_POPULATION_COUNTS_DESCRIPTION = \
+"""
+Creates a resident population count table from
+Vera's Incarceration Trends dataset. For every
+year-fips-race-gender combination, there will be a `resident_pop` column.
+Additionally, a `total_resident_pop` column is created that sums the entire
+resident population for each year-fips combination.
+
+Combines 'OTHER' and 'EXTERNAL_UNKNOWN' Race into 'OTHER/UNKNOWN'.
+
+Create fake 'ALL' enums for Race and Gender fields.
+
+Gender: 'ALL' sums Male, Female, and Unknown for each race.
+
+Similarly, Race: 'ALL' sums every race for each gender.
+
+DO NOT sum along race or gender, or you will double-count by including 'ALL'.
+"""
+
+RESIDENT_POPULATION_COUNTS_QUERY = \
+"""
+/*{description}*/
+
+WITH
+
+RaceGender AS (
+  SELECT
+    year,
+    fips,
+    IF(race = 'OTHER' OR race = 'EXTERNAL_UNKNOWN', 'OTHER/UNKNOWN', race) AS race,
+    gender,
+    resident_pop
+  FROM `{project_id}.{vera_dataset}.{iob_race_gender_pop_table}`
+),
+
+AllRace AS (
+  SELECT
+    year,
+    fips,
+    'ALL' AS race,
+    gender,
+    SUM(resident_pop) AS resident_pop
+  FROM RaceGender
+  GROUP BY year, fips, gender
+),
+
+AllGender AS (
+  SELECT
+    year,
+    fips,
+    race,
+    'ALL' AS gender,
+    SUM(resident_pop) AS resident_pop
+    FROM RaceGender
+  GROUP BY year, fips, race
+),
+
+TotalPop AS (
+  SELECT
+    year,
+    fips,
+    SUM(resident_pop) AS total_resident_pop
+    FROM RaceGender
+  GROUP BY year, fips
+)
+
+SELECT CombinedRaceGender.*, TotalPop.total_resident_pop FROM (
+  SELECT * FROM AllRace
+  UNION ALL
+  SELECT * FROM AllGender
+  UNION ALL
+  SELECT * FROM RaceGender
+) CombinedRaceGender
+LEFT JOIN
+  TotalPop
+ON
+  CombinedRaceGender.year = TotalPop.year AND CombinedRaceGender.fips = TotalPop.fips
+
+ORDER BY year DESC, fips, race, gender
+""".format(
+    description=RESIDENT_POPULATION_COUNTS_DESCRIPTION,
+    project_id=PROJECT_ID,
+    vera_dataset=vera_view_constants.VERA_DATASET,
+    iob_race_gender_pop_table=vera_view_constants.IOB_RACE_GENDER_POP_TABLE
+)
+
+RESIDENT_POPULATION_COUNTS_VIEW = bqview.BigQueryView(
+    view_id=RESIDENT_POPULATION_COUNTS_VIEW_NAME,
+    view_query=RESIDENT_POPULATION_COUNTS_QUERY
+)
+
+if __name__ == '__main__':
+    print(RESIDENT_POPULATION_COUNTS_VIEW.view_id)
+    print(RESIDENT_POPULATION_COUNTS_VIEW.view_query)
