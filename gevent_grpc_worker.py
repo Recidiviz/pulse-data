cb0328563e082804905b0ba59e409e548b2260f6
Joshua Essex <essexjosh@gmail.com>
2019-04-06 21:57:44 -0500

    Cleans up a handful of doc typos and such. (Recidiviz/recidiviz-data#1614)
    GitOrigin-RevId: 42fb13714e6638846f65de1d446b8dfa2b66e5cd

--- gevent_grpc_worker.py
+++ gevent_grpc_worker.py
@@ -16,7 +16,7 @@
 # =============================================================================
 """Provides a gevent worker that also patches grpc to be asynchronous.
 
-Note: This must be a the top level and called before any recidiviz code so that
+Note: This must be at the top level and called before any recidiviz code so that
 nothing is imported prior to being patched. If it is placed inside of the
 recidiviz directory, then the __init__.py file will be called first.
 """
