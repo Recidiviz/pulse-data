c68860010e4c1438a2e99c6ac8fbf3877ee5c59e
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2019-05-06 10:25:07 -0700

    State Ingest Schema - Entity definitions (Recidiviz/recidiviz-data#1703)
    This change includes entity and related constant definitions for the new state ingest schema. The first part of the work towards completing Recidiviz/recidiviz-data#1625.
    
    Several key distinguishing features:
    
    * All state-specific entities and constants live in /state subdirectories
    * All state Entity fields have been organized within the object to follow a strict structure so it's easier to tell if we are missing relevant information about this entity.
    * Unlike the county schema, the state schema separates information on the prescribed sentence and the actual execution of that sentence into multiple objects. Sentences can have one of two types: IncarcerationSentence or SupervisionSentence. Each of those sentences is carried out through a series of either IncarcerationPeriod and SupervisionPeriod.
    * Charges and Sentences have a many:many relationship, allowing flexibility in how we represent a variety of edge cases in how sentencing works. A sentence might encompass multiple charges, but also a charge might be associated with multiple sentences, for example, a SUSPENDED incarceration sentence and a related supervision sentence that is being served in lieu of incarceration.
    * New objects representing ParoleHearings and SupervisionViolations - these and their child objects help us represent revocation and also transition from incarceration to supervision.
    * The Person object has been updated to reflect the eventual complexity that multiple data sources with information about a given person will provide. A person may be listed as multiple different races or have multiple 'external_ids' (one for each state where they've had an encounter with the justice system, for example).
    
    GitOrigin-RevId: 739816f006cea018660301be615edf7ad1148d2f

---
+++ recidiviz/common/constants/state/enum_canonical_strings.py
@@ -0,0 +1,128 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+"""
+Strings used as underlying representation of enum values for state-specific
+application and database code.
+
+NOTE: Changing ANY STRING VALUE in this file will require a database migration.
+The Python values pointing to the strings can be renamed without issue.
+
+SQLAlchemy represents SQL enums as strings, and uses the string representation
+to pass values to the database. This means any change to the string values
+requires a database migration. Therefore in order to keep the code as flexible
+as possible, the string values should never be used directly. Storing the
+strings in this file and only referring to them by their values here allows
+us to structure the application layer code any way we want, while only
+requiring the database to be updated when an enum value is created or removed.
+"""
+
+# assessment.py
+assessment_class_mental_health = 'MENTAL_HEALTH'
+assessment_class_risk = 'RISK'
+assessment_class_security_classification = 'SECURITY_CLASSIFICATION'
+assessment_class_substance_abuse = 'SUBSTANCE_ABUSE'
+
+assessment_type_asi = 'ASI'
+assessment_type_lsir = 'LSIR'
+assessment_type_oras = 'ORAS'
+assessment_type_psa = 'PSA'
+
+# charge.py
+charge_class_civil = 'CIVIL'
+charge_class_felony = 'FELONY'
+charge_class_infraction = 'INFRACTION'
+charge_class_misdemeanor = 'MISDEMEANOR'
+charge_class_other = 'OTHER'
+
+# court_case.py
+# TODO(1697): Add enum strings here
+
+# fine.py
+fine_status_paid = 'PAID'
+fine_status_unpaid = 'UNPAID'
+
+# incarceration.py
+incarceration_type_county_jail = 'COUNTY_JAIL'
+incarceration_type_state_prison = 'STATE_PRISON'
+
+# incarceration_incident.py
+incarceration_incident_offense_contraband = 'CONTRABAND'
+incarceration_incident_offense_violent = 'VIOLENT'
+
+incarceration_incident_outcome_privilege_loss = 'PRIVILEGE_LOSS'
+incarceration_incident_outcome_solitary = 'SOLITARY'
+incarceration_incident_outcome_warning = 'WARNING'
+incarceration_incident_outcome_write_up = 'WRITE_UP'
+
+# incarceration_period.py
+incarceration_period_status_in_custody = 'IN_CUSTODY'
+incarceration_period_status_not_in_custody = 'NOT_IN_CUSTODY'
+
+incarceration_facility_security_level_maximum = 'MAXIMUM'
+incarceration_facility_security_level_medium = 'MEDIUM'
+incarceration_facility_security_level_minimum = 'MINIMUM'
+
+incarceration_period_termination_reason_death = 'DEATH'
+incarceration_period_termination_reason_escaped = 'ESCAPED'
+incarceration_period_termination_reason_released = 'RELEASED'
+incarceration_period_termination_reason_transfer = 'TRANSFER'
+
+incarceration_period_release_type_conditional = 'CONDITIONAL'
+incarceration_period_release_type_death = 'DEATH'
+incarceration_period_release_type_earned_time = 'EARNED_TIME'
+incarceration_period_release_type_good_time = 'GOOD_TIME'
+incarceration_period_release_type_parole = 'PAROLE'
+incarceration_period_release_type_probation = 'PROBATION'
+incarceration_period_release_type_sentence_served = 'SERVED'
+
+# sentence.py
+sentence_status_commuted = 'COMMUTED'
+sentence_status_completed = 'COMPLETED'
+sentence_status_serving = 'SERVING'
+sentence_status_suspended = 'SUSPENDED'
+
+# supervision.py
+supervision_type_conditional = 'CONDITIONAL'
+supervision_type_halfway_house = 'HALFWAY_HOUSE'
+supervision_type_parole = 'PAROLE'
+supervision_type_post_confinement = 'POST_CONFINEMENT'
+supervision_type_pre_confinement = 'PRE_CONFINEMENT'
+supervision_type_probation = 'PROBATION'
+
+# supervision_period.py
+supervision_period_admission_type_conditional_release = 'CONDITIONAL_RELEASE'
+supervision_period_admission_type_court_sentence = 'COURT_SENTENCE'
+supervision_period_admission_type_return_from_absconsion = \
+    'RETURN_FROM_ABSCONSION'
+supervision_period_admission_type_return_from_suspension = \
+    'RETURN_FROM_SUPSENSION'
+
+supervision_period_status_terminated = 'TERMINATED'
+supervision_period_status_under_supervision = 'UNDER_SUPERVISION'
+
+supervision_period_termination_type_absconsion = 'ABSCONSION'
+supervision_period_termination_type_discharge = 'DISCHARGE'
+supervision_period_termination_type_revocation = 'REVOCATION'
+supervision_period_termination_type_suspension = 'SUSPENSION'
+
+# supervision_violation.py
+supervision_violation_type_absconded = 'ABSCONDED'
+supervision_violation_type_felony = 'FELONY'
+supervision_violation_type_misdemeanor = 'MISDEMEANOR'
+supervision_violation_type_municipal = 'MUNICIPAL'
+supervision_violation_type_technical = 'TECHNICAL'
---
+++ recidiviz/common/constants/state/incarceration_period.py
@@ -0,0 +1,135 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+"""Constants related to a IncarcerationPeriod."""
+from typing import Dict
+
+import recidiviz.common.constants.enum_canonical_strings as enum_strings
+import recidiviz.common.constants.state.enum_canonical_strings as \
+    state_enum_strings
+from recidiviz.common.constants.entity_enum import EntityEnum, EntityEnumMeta
+
+
+class IncarcerationPeriodStatus(EntityEnum, metaclass=EntityEnumMeta):
+    EXTERNAL_UNKNOWN = enum_strings.external_unknown
+    IN_CUSTODY = state_enum_strings.incarceration_period_status_in_custody
+    NOT_IN_CUSTODY = \
+        state_enum_strings.incarceration_period_status_not_in_custody
+
+    @staticmethod
+    def _get_default_map():
+        return _INCARCERATION_PERIOD_STATUS_MAP
+
+
+class IncarcerationFacilitySecurityLevel(EntityEnum, metaclass=EntityEnumMeta):
+    MAXIMUM = state_enum_strings.incarceration_facility_security_level_maximum
+    MEDIUM = state_enum_strings.incarceration_facility_security_level_medium
+    MINIMUM = state_enum_strings.incarceration_facility_security_level_minimum
+
+    @staticmethod
+    def _get_default_map():
+        return _INCARCERATION_FACILITY_SECURITY_LEVEL_MAP
+
+
+class IncarcerationPeriodAdmissionReason(EntityEnum, metaclass=EntityEnumMeta):
+
+    # TODO(1697): Add values here
+
+    @staticmethod
+    def _get_default_map():
+        return _INCARCERATION_PERIOD_ADMISSION_REASON_MAP
+
+
+class IncarcerationPeriodTerminationReason(EntityEnum,
+                                           metaclass=EntityEnumMeta):
+    DEATH = state_enum_strings.incarceration_period_termination_reason_death
+    ESCAPED = state_enum_strings.incarceration_period_termination_reason_escaped
+    RELEASED = \
+        state_enum_strings.incarceration_period_termination_reason_released
+    TRANSFER = \
+        state_enum_strings.incarceration_period_termination_reason_transfer
+
+    @staticmethod
+    def _get_default_map():
+        return _INCARCERATION_PERIOD_TERMINATION_REASON_MAP
+
+
+class IncarcerationPeriodReleaseType(EntityEnum, metaclass=EntityEnumMeta):
+    # This release type corresponds to any release into some sort of
+    # supervision.
+    CONDITIONAL = \
+        state_enum_strings.incarceration_period_release_type_conditional
+    SENTENCE_SERVED = \
+        state_enum_strings.incarceration_period_release_type_sentence_served
+
+    @staticmethod
+    def _get_default_map():
+        return _INCARCERATION_PERIOD_RELEASE_TYPE_MAP
+
+
+_INCARCERATION_FACILITY_SECURITY_LEVEL_MAP = {
+    'MAXIMUM': IncarcerationFacilitySecurityLevel.MAXIMUM,
+    'MAX': IncarcerationFacilitySecurityLevel.MAXIMUM,
+    'MEDIUM': IncarcerationFacilitySecurityLevel.MEDIUM,
+    'MED': IncarcerationFacilitySecurityLevel.MEDIUM,
+    'MINIMUM': IncarcerationFacilitySecurityLevel.MINIMUM,
+    'MIN': IncarcerationFacilitySecurityLevel.MINIMUM,
+}
+
+
+_INCARCERATION_PERIOD_RELEASE_TYPE_MAP = {
+    'CONDITIONAL': IncarcerationPeriodReleaseType.CONDITIONAL,
+    'CONDITIONAL RELEASE': IncarcerationPeriodReleaseType.CONDITIONAL,
+    'EARNED TIME': IncarcerationPeriodReleaseType.SENTENCE_SERVED,
+    'TIME EARNED': IncarcerationPeriodReleaseType.SENTENCE_SERVED,
+    'GOOD TIME': IncarcerationPeriodReleaseType.SENTENCE_SERVED,
+    'PAROLE': IncarcerationPeriodReleaseType.CONDITIONAL,
+    'RELEASE TO PAROLE': IncarcerationPeriodReleaseType.CONDITIONAL,
+
+    # TODO(1697): Should this actually be SENTENCE_SERVED? The probation is now
+    #  a new stacked sentence that is being served?
+    'PROBATION': IncarcerationPeriodReleaseType.CONDITIONAL,
+    'RELEASE TO PROBATION': IncarcerationPeriodReleaseType.CONDITIONAL,
+    'SERVED': IncarcerationPeriodReleaseType.SENTENCE_SERVED,
+    'TIME SERVED': IncarcerationPeriodReleaseType.SENTENCE_SERVED,
+    'EXPIRED': IncarcerationPeriodReleaseType.SENTENCE_SERVED,
+}
+
+
+_INCARCERATION_PERIOD_STATUS_MAP = {
+    'CUSTODY': IncarcerationPeriodStatus.IN_CUSTODY,
+    'IN CUSTODY': IncarcerationPeriodStatus.IN_CUSTODY,
+
+    # TODO(1697): What strings correspond to
+    #  IncarcerationPeriodStatus.NOT_IN_CUSTODY?
+}
+
+_INCARCERATION_PERIOD_ADMISSION_REASON_MAP: \
+    Dict[str, IncarcerationPeriodAdmissionReason] = {
+        # TODO(1697): Add values here
+    }
+
+_INCARCERATION_PERIOD_TERMINATION_REASON_MAP = {
+    'DEATH': IncarcerationPeriodTerminationReason.DEATH,
+    'ESCAPED': IncarcerationPeriodTerminationReason.ESCAPED,
+    'ESCAPE': IncarcerationPeriodTerminationReason.ESCAPED,
+    'RELEASED': IncarcerationPeriodTerminationReason.RELEASED,
+
+    # TODO(1697): Is this actually what these mean?
+    'HELD ELSEWHERE': IncarcerationPeriodTerminationReason.TRANSFER,
+    'HOLD': IncarcerationPeriodTerminationReason.TRANSFER,
+}
