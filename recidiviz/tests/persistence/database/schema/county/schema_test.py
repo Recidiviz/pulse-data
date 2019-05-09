62d002076942c4d23e83163a7cbfa73424b06b8b
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2019-05-08 17:12:38 -0700

    Move shared schema enums to shared file, update tests (Recidiviz/recidiviz-data#1730)
    As part of Recidiviz/recidiviz-data#1625, move SQLAlchemy enums that both the county and state schemas use to a shared file, and refactor schema enum tests to have separate tests for state, county, and aggregate enums.
    
    Also:
    * Added line to confirm that the tests are actually testing files with enums in them, since we'd silently broken the enum tests to be testing a file without any enums in it.
    * Created a schema subdir in recidiviz/tests/persistence/database to match structure in production code.
    * Added several new enums to the state schema so that file would have enums in it to test.
    GitOrigin-RevId: 20ec21bcef3b9b3d3dd85fc4ea2d27aab3939f7e

---
+++ recidiviz/tests/persistence/database/schema/county/schema_test.py
@@ -0,0 +1,51 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for county-specific SQLAlchemy enums."""
+
+from recidiviz.common.constants.county import booking, sentence, charge, hold
+from recidiviz.persistence.database.schema.county import schema
+from recidiviz.tests.persistence.database.schema.shared_enums_test import (
+    SchemaEnumsTest
+)
+
+
+class TestCountySchemaEnums(SchemaEnumsTest):
+    """Tests for validating state schema enums are defined correctly"""
+
+    # Test case ensuring enum values match between persistence layer enums and
+    # schema enums
+    def testPersistenceAndSchemaEnumsMatch(self):
+        # Mapping between name of schema enum and persistence layer enum. This
+        # map controls which pairs of enums are tested.
+        #
+        # If a schema enum does not correspond to a persistence layer enum,
+        # it should be mapped to None.
+        county_enums_mapping = {
+            'admission_reason': booking.AdmissionReason,
+            'classification': booking.Classification,
+            'custody_status': booking.CustodyStatus,
+            'release_reason': booking.ReleaseReason,
+            'hold_status': hold.HoldStatus,
+            'sentence_status': sentence.SentenceStatus,
+            'sentence_relationship_type': None,
+            'charge_class': charge.ChargeClass,
+        }
+
+        merged_mapping = {**self.SHARED_ENUMS_TEST_MAPPING,
+                          **county_enums_mapping}
+
+        self.check_persistence_and_schema_enums_match(merged_mapping, schema)
