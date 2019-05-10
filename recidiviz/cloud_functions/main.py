5b6a788124cbd5eed694d98ecc92dabbddca2b8c
ohinds <ohinds@vera.org>
2019-05-10 15:45:13 -0400

    Add a cloud function to trigger direct ingest on a new file. (Recidiviz/recidiviz-data#1740)
    GitOrigin-RevId: ab21539fad69ebdbfba55233ef7fee2efbf0129c

--- recidiviz/cloud_functions/main.py
+++ recidiviz/cloud_functions/main.py
@@ -22,8 +22,12 @@ import os
 
 from cloud_function_utils import make_iap_request
 
-_CLOUD_FUNCTION_URL = ('http://{}.appspot.com/cloud_function/state_aggregate?'
-                       'bucket={}&state={}&filename={}')
+_STATE_AGGREGATE_CLOUD_FUNCTION_URL = (
+    'http://{}.appspot.com/cloud_function/state_aggregate?bucket={}&state={}'
+    '&filename={}')
+_DIRECT_INGEST_CLOUD_FUNCTION_URL = (
+    'http://{}.appspot.com/cloud_function/direct?bucket={}&region={}'
+    '&filename={}')
 _CLIENT_ID = {
     'recidiviz-staging': ('984160736970-flbivauv2l7sccjsppe34p7436l6890m.apps.'
                           'googleusercontent.com'),
@@ -47,9 +51,33 @@ def parse_state_aggregate(data, _):
     logging.info(
         "Running cloud function for bucket %s, state %s, filename %s",
         bucket, state, filename)
-    url = _CLOUD_FUNCTION_URL.format(
+    url = _STATE_AGGREGATE_CLOUD_FUNCTION_URL.format(
         project_id, bucket, state, filename)
     # Hit the cloud function backend, which persists the table data to our
+    # database.
+    response = make_iap_request(url, _CLIENT_ID[project_id])
+    logging.info("The response status is %s", response.status_code)
+
+
+def direct_ingest(data, _):
+    """This function is triggered when a file is dropped into the direct
+    ingest bucket and makes a request to parse and write the data to
+    the database.
+
+    data: A cloud storage object that holds name information and other metadata
+    related to the file that was dropped into the bucket.
+    _: (google.cloud.functions.Context): Metadata of triggering event.
+
+    """
+    bucket = data['bucket']
+    region, filename = data['name'].split('/')
+    project_id = os.environ.get('GCP_PROJECT')
+    logging.info(
+        "Running cloud function for bucket %s, region %s, filename %s",
+        bucket, region, filename)
+    url = _DIRECT_INGEST_CLOUD_FUNCTION_URL.format(
+        project_id, bucket, region, filename)
+    # Hit the cloud function backend, which parses the data and persists to our
     # database.
     response = make_iap_request(url, _CLIENT_ID[project_id])
     logging.info("The response status is %s", response.status_code)
