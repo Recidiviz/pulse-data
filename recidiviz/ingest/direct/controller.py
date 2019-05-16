dd2b2a6cfdf5db27be92c6567042ce4d3dbecaca
ohinds <ohinds@vera.org>
2019-05-16 19:02:51 -0400

    A few fixes to the db -> gcs copy. (Recidiviz/recidiviz-data#1771)
    GitOrigin-RevId: 796209f09ee47030a81d5bbf6ef4073ae8aae8ce

--- recidiviz/ingest/direct/controller.py
+++ recidiviz/ingest/direct/controller.py
@@ -46,6 +46,8 @@ def mv_from_db_to_gcs(region_name):
     Args
         region_name: (str) the name of the region to move files for.
     """
+    logging.info("Processing %s", region_name)
+
     token = os.environ.get('DB_TOKEN')
     if not token:
         logging.error("No Dropbox token found, check your environment.")
@@ -56,27 +58,30 @@ def mv_from_db_to_gcs(region_name):
     fs = gcsfs.GCSFileSystem(project=project_id, cache_timeout=-1)
 
     db_folder_path = '/{}'.format(region_name)
-    processed_db_folder_path = db_folder_path + '/processed'
+    processed_db_folder_path = db_folder_path + '/processed/'
     response = dbx.files_list_folder(path=db_folder_path)
     for entry in response.entries:
         if isinstance(entry, (DeletedMetadata, FolderMetadata)):
             continue
 
         db_path = entry.path_lower
-        logging.info("The path to download from dropbox is %s", db_path)
+        filename = os.path.basename(db_path)
+        logging.info("Processing %s", filename)
         _, resp = dbx.files_download(db_path)
 
         # Note that this assumes that the bucket name is prefixed with
         # the region_name, which is a good idea regardless because
         # bucket names must be unique across Google(!).
         bucket = '{}-{}'.format(project_id, _DIRECT_INGEST_GCS_BUCKET_NAME)
-        gcs_path = os.path.join(bucket, region_name)
-        gcs_file = fs.GCSFile(gcs_path, mode='wb')
-        gcs_file.write(resp.content)
-        gcs_file.close()
+        gcs_path = os.path.join(bucket, region_name, filename)
+        with fs.open(gcs_path, mode='wb') as gcs_fp:
+            gcs_fp.write(resp.content)
 
-        dbx.files_move(db_path, processed_db_folder_path,
+        dbx.files_move(db_path,
+                       os.path.join(processed_db_folder_path, filename),
                        allow_shared_folder=True, allow_ownership_transfer=True)
+
+        logging.info("Completed processing %s", filename)
 
 
 @check_direct_ingest_upload_blueprint.route('/check_direct_ingest_upload')
