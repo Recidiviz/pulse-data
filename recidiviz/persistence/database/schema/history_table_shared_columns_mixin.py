6edf7810276dcd48ed5b855e5e80e87a7f436ac7
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2019-06-13 11:29:52 -0700

    Move HistoryTableSharedColumns to a shared location for use by both schemas (Recidiviz/recidiviz-data#1877)
    Killing another small state schema cleanup TODO.
    
    GitOrigin-RevId: 4fa2ad74aae0337270b37a32676fc0c8e609a4da

---
+++ recidiviz/persistence/database/schema/history_table_shared_columns_mixin.py
@@ -0,0 +1,32 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""SQLAlchemy table mixin that defines all columns common on ahistory table"""
+
+from sqlalchemy import Column, DateTime
+
+
+class HistoryTableSharedColumns:
+    """A mixin which defines all columns common to any history table"""
+
+    # Consider this class a mixin and only allow instantiating subclasses
+    def __new__(cls, *_, **__):
+        if cls is HistoryTableSharedColumns:
+            raise Exception(f'[{cls}] cannot be instantiated')
+        return super().__new__(cls)
+
+    valid_from = Column(DateTime, nullable=False)
+    valid_to = Column(DateTime)
