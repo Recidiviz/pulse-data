323ca5e840dad4957b7fe0ef784f8683cd295e53
Oliver Hinds <ohinds@vera.org>
2019-09-04 11:22:45 -0400

    Remove BQ sort that was causing extracts to fail.
    GitOrigin-RevId: 50ff48ac5bb5d2894da5845a76a5ee1e0f85c6fe

--- recidiviz/calculator/bq/views/charges/charge_text_counts.py
+++ recidiviz/calculator/bq/views/charges/charge_text_counts.py
@@ -106,7 +106,6 @@ JOIN
   `{project_id}.{views_dataset}.{county_names_view}` CountyNames
 ON
   PersonCountTable.fips = CountyNames.fips
-ORDER BY day DESC, fips, person_count DESC
 """.format(
     description=CHARGE_TEXT_COUNTS_DESCRIPTION,
     project_id=PROJECT_ID,
