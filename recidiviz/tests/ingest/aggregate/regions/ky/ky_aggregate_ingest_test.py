e7ce88eeac91295903ca42e47da9edabfd0b9e0f
ohinds <ohinds@vera.org>
2019-09-13 09:18:56 -0500

    Account for changing column names in the KY PDFs. (Recidiviz/recidiviz-data#2416)
    GitOrigin-RevId: 20c03e535bf5b4ca8084605b4e9544950c17a2de

--- recidiviz/tests/ingest/aggregate/regions/ky/ky_aggregate_ingest_test.py
+++ recidiviz/tests/ingest/aggregate/regions/ky/ky_aggregate_ingest_test.py
@@ -39,6 +39,7 @@ from recidiviz.tests.utils import fakes
 
 DATE_SCRAPED = datetime.date(year=2018, month=12, day=20)
 DATE_SCRAPED_2 = datetime.date(year=2018, month=8, day=23)
+DATE_SCRAPED_3 = datetime.date(year=2019, month=8, day=22)
 
 # Cache the parsed pdf between tests since it's expensive to compute
 @pytest.fixture(scope="class")
@@ -47,6 +48,8 @@ def parsed_pdf(request):
         '', fixtures.as_filepath('12-20-18.pdf'))
     request.cls.parsed_pdf_2 = ky_aggregate_ingest.parse(
         '', fixtures.as_filepath('08-23-18.pdf'))
+    request.cls.parsed_pdf_3 = ky_aggregate_ingest.parse(
+        '', fixtures.as_filepath('08-22-19.pdf'))
 
 
 @pytest.mark.usefixtures("parsed_pdf")
@@ -161,6 +164,61 @@ class TestKyAggregateIngest(TestCase):
             'federal_female_population': [2, 0],
             'fips': ['21239', '21239'],
             'report_date': 2 * [DATE_SCRAPED_2],
+            'aggregation_window': 2 * [enum_strings.daily_granularity],
+            'report_frequency': 2 * [enum_strings.weekly_granularity]
+        }, index=range(122, 124))
+        assert_frame_equal(result.tail(n=2), expected_tail, check_names=False)
+
+    def testParse3_ParsesHeadAndTail(self):
+        result = self.parsed_pdf_3[KyFacilityAggregate]
+
+        # Assert Head
+        expected_head = pd.DataFrame({
+            'facility_name': ['Adair', 'Adair RCC'],
+            'total_jail_beds': [51, 31],
+            'reported_population': [113, 31],
+            'male_population': [91, 31],
+            'class_d_male_population': [8, 29],
+            'community_custody_male_population': [0, 2],
+            'alternative_sentence_male_population': [7, 0],
+            'controlled_intake_male_population': [6, 0],
+            'parole_violators_male_population': [6, 0],
+            'federal_male_population': [0, 0],
+            'female_population': [22, 0],
+            'class_d_female_population': [0, 0],
+            'community_custody_female_population': [0, 0],
+            'alternative_sentence_female_population': [0, 0],
+            'controlled_intake_female_population': [3, 0],
+            'parole_violators_female_population': [0, 0],
+            'federal_female_population': [0, 0],
+            'fips': ['21001', '21001'],
+            'report_date': 2 * [DATE_SCRAPED_3],
+            'aggregation_window': 2 * [enum_strings.daily_granularity],
+            'report_frequency': 2 * [enum_strings.weekly_granularity]
+        })
+        assert_frame_equal(result.head(n=2), expected_head, check_names=False)
+
+        # Assert Tail
+        expected_tail = pd.DataFrame({
+            'facility_name': ['Woodford', 'Woodford JRC'],
+            'total_jail_beds': [63, 32],
+            'reported_population': [107, 36],
+            'male_population': [86, 36],
+            'class_d_male_population': [5, 13],
+            'community_custody_male_population': [0, 23],
+            'alternative_sentence_male_population': [0, 0],
+            'controlled_intake_male_population': [3, 0],
+            'parole_violators_male_population': [2, 0],
+            'federal_male_population': [47, 0],
+            'female_population': [21, 0],
+            'class_d_female_population': [2, 0],
+            'community_custody_female_population': [2, 0],
+            'alternative_sentence_female_population': [0, 0],
+            'controlled_intake_female_population': [0, 0],
+            'parole_violators_female_population': [0, 0],
+            'federal_female_population': [5, 0],
+            'fips': ['21239', '21239'],
+            'report_date': 2 * [DATE_SCRAPED_3],
             'aggregation_window': 2 * [enum_strings.daily_granularity],
             'report_frequency': 2 * [enum_strings.weekly_granularity]
         }, index=range(122, 124))
