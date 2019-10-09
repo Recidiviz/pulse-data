af8b4fee2fe01e5adf806b9d164e399da545413f
Julia Dressel <julia@recidiviz.org>
2019-10-09 13:24:50 -0700

    Running pipeline deploy commands from within the pipenv (Recidiviz/recidiviz-data#2516)
    GitOrigin-RevId: 0875af61d75735ef7b88facb40d6ef9fd6fa1804

--- recidiviz/tools/deploy_pipeline_from_yaml.sh
+++ recidiviz/tools/deploy_pipeline_from_yaml.sh
@@ -1,4 +1,14 @@
-source ./run_commands.sh
+function run_cmd {
+    cmd="$1"
+    echo "Running \`$cmd\`"
+    $cmd
+
+    ret_code=$?
+    if [ $ret_code -ne 0 ]; then
+        echo "Failed with exit code $ret_code"
+        exit $ret_code
+    fi
+}
 
 function deploy_pipelines {
   cmd=$1
@@ -6,17 +16,17 @@ function deploy_pipelines {
   bucket=$3
   file=$4
 
-  pipeline_count="$(cat $file | yq -r .pipeline_count)"
+  pipeline_count="$(cat $file | pipenv run yq -r .pipeline_count)"
   END=$pipeline_count
   ((END = END - 1))
   i=0
   # Deploy each pipeline in the file to a template
   while [[ $i -le $END ]]
   do
-      pipeline="$(cat $file | yq -r .pipelines[$i].pipeline)"
-      job_name="$(cat $file | yq -r .pipelines[$i].job_name)"
-      input="$(cat $file | yq -r .pipelines[$i].input)"
-      output="$(cat $file | yq -r .pipelines[$i].output)"
+      pipeline="$(cat $file | pipenv run yq -r .pipelines[$i].pipeline)"
+      job_name="$(cat $file | pipenv run yq -r .pipelines[$i].job_name)"
+      input="$(cat $file | pipenv run yq -r .pipelines[$i].input)"
+      output="$(cat $file | pipenv run yq -r .pipelines[$i].output)"
       run_cmd "$cmd $project $bucket $pipeline $job_name $input $output"
       ((i = i + 1))
   done
