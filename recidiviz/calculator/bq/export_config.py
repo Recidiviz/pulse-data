c6ab133af427b980e930d51703e15cd527226847
Julia Dressel <julia@recidiviz.org>
2019-10-10 16:14:45 -0700

    Pointing to state instead of recidiviz_state_scratch_space_2 (Recidiviz/recidiviz-data#2522)
    GitOrigin-RevId: 422e1507c125f290674dd6028c199ff27dd5060a

--- recidiviz/calculator/bq/export_config.py
+++ recidiviz/calculator/bq/export_config.py
@@ -78,8 +78,7 @@ STATE_TABLES_TO_EXPORT = tuple(
 # As of right now, we aren't excluding any columns from the state schema export.
 STATE_COLUMNS_TO_EXCLUDE: Dict[str, List[str]] = {}
 
-# TODO(1632): Change this to 'state' once nightly ingest is going to BQ
-STATE_BASE_TABLES_BQ_DATASET = 'recidiviz_state_scratch_space_2'
+STATE_BASE_TABLES_BQ_DATASET = 'state'
 
 def gcs_export_uri(table_name: str) -> str:
     """Return export URI location in Google Cloud Storage given a table name."""
