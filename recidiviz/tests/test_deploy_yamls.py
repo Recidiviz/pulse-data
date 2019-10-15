be8cd40cb152906216dd0426b4e7eba7a17dc975
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2019-10-15 16:16:16 -0700

    Pause cron job and add yaml parsing tests (Recidiviz/recidiviz-data#2538)
    Pause export of state data so we can run a full re-run in prod. Also adds tests that would have caught the yaml parsing bug fixed in Recidiviz/recidiviz-data#2468.
    
    GitOrigin-RevId: e2fda63a8b3ce7e49245e5fa29acd69949ef0cda

---
+++ recidiviz/tests/test_deploy_yamls.py
@@ -0,0 +1,52 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests checking that all the yamls deployed by our builds parse successfully.
+"""
+import os
+import unittest
+
+import yaml
+
+
+class TestDeployYamls(unittest.TestCase):
+    """Tests checking that all the yamls deployed by our builds parse
+    successfully.
+    """
+    def path_for_build_file(self, file_name: str) -> str:
+        return os.path.join(os.path.dirname(__file__),
+                            '..', '..', file_name)
+
+    def test_cron_yaml_parses(self):
+        cron_file_path = self.path_for_build_file('cron.yaml')
+        with open(cron_file_path, 'r') as ymlfile:
+            file_contents = yaml.full_load(ymlfile)
+
+        self.assertTrue(file_contents)
+
+    def test_prod_yaml_parses(self):
+        cron_file_path = self.path_for_build_file('prod.yaml')
+        with open(cron_file_path, 'r') as ymlfile:
+            file_contents = yaml.full_load(ymlfile)
+
+        self.assertTrue(file_contents)
+
+    def test_staging_yaml_parses(self):
+        cron_file_path = self.path_for_build_file('staging.yaml')
+        with open(cron_file_path, 'r') as ymlfile:
+            file_contents = yaml.full_load(ymlfile)
+
+        self.assertTrue(file_contents)
