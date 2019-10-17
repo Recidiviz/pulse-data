aabd488aac7f24272b87ff0bf8b2180ab09d8de9
terinpw <terin@recidiviz.org>
2019-10-17 14:14:33 -0700

    Queries necessary for FTR descriptive stats (Recidiviz/recidiviz-data#2544)
    * ftr referral rate queries
    
    * pr changes
    
    * docstrings and age calc update
    
    * pylint fix
    
    GitOrigin-RevId: 92a18917c02dcaaf5b327a8c66a32b053e6e4551

--- recidiviz/calculator/bq/dashboard/views/reference/reference_views.py
+++ recidiviz/calculator/bq/dashboard/views/reference/reference_views.py
@@ -20,14 +20,26 @@ from recidiviz.calculator.bq.dashboard.views.reference.most_recent_calculate_job
     MOST_RECENT_CALCULATE_JOB_VIEW
 from recidiviz.calculator.bq.dashboard.views.reference.incarceration_admissions_60_days import \
     INCARCERATION_ADMISSIONS_60_DAYS_VIEW
+from recidiviz.calculator.bq.dashboard.views.reference.persons_on_supervision_60_days import \
+    PERSONS_ON_SUPERVISION_60_DAYS_VIEW
+from recidiviz.calculator.bq.dashboard.views.reference.persons_with_age import \
+    PERSONS_WITH_AGE_VIEW
+from recidiviz.calculator.bq.dashboard.views.reference.us_nd.ftr_referrals_60_days import \
+    FTR_REFERRAL_VIEW
 from recidiviz.calculator.bq.dashboard.views.reference.state_person_race_and_ethnicity import \
     STATE_PERSON_RACE_AND_ETHNICITY_VIEW
 from recidiviz.calculator.bq.dashboard.views.reference.supervision_termination_by_person import \
     SUPERVISION_TERMINATION_BY_PERSON_VIEW
+from recidiviz.calculator.bq.dashboard.views.reference.persons_with_most_recent_lsir import \
+    PERSONS_WITH_MOST_RECENT_LSIR_VIEW
 
 REF_VIEWS = [
     MOST_RECENT_CALCULATE_JOB_VIEW,
     INCARCERATION_ADMISSIONS_60_DAYS_VIEW,
     STATE_PERSON_RACE_AND_ETHNICITY_VIEW,
-    SUPERVISION_TERMINATION_BY_PERSON_VIEW
+    SUPERVISION_TERMINATION_BY_PERSON_VIEW,
+    PERSONS_ON_SUPERVISION_60_DAYS_VIEW,
+    PERSONS_WITH_MOST_RECENT_LSIR_VIEW,
+    FTR_REFERRAL_VIEW,
+    PERSONS_WITH_AGE_VIEW,
 ]
