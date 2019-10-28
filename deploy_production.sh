89bdbfe2e8a46513e77d2316857b7d916176dd9a
ohinds <ohinds@vera.org>
2019-10-28 11:14:06 -0600

    Don't require user approval before continuing. (recidiviz/recidiviz-data#2571)
    GitOrigin-RevId: 88c29a04beed749159c89b4960e0170659073327

--- deploy_production.sh
+++ deploy_production.sh
@@ -8,7 +8,7 @@ echo "Checking out tag $1"
 git checkout tags/$1 -b $1
 
 echo "Starting deploy of cron.yaml"
-gcloud app deploy cron.yaml --project=recidiviz-123
+gcloud -q app deploy cron.yaml --project=recidiviz-123
 
 echo "Starting task queue initialization"
 pipenv run python -m recidiviz.tools.initialize_google_cloud_task_queues --project_id recidiviz-123 --google_auth_token $(gcloud auth print-access-token)
@@ -22,5 +22,5 @@ STAGING_IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.$VERSION:latest
 PROD_IMAGE_URL=us.gcr.io/recidiviz-123/appengine/default.$VERSION:latest
 
 echo "Starting deploy of main app"
-gcloud container images add-tag $STAGING_IMAGE_URL $PROD_IMAGE_URL
-gcloud app deploy prod.yaml --project=recidiviz-123 --version=$VERSION --image-url=$PROD_IMAGE_URL
+gcloud -q container images add-tag $STAGING_IMAGE_URL $PROD_IMAGE_URL
+gcloud -q app deploy prod.yaml --project=recidiviz-123 --version=$VERSION --image-url=$PROD_IMAGE_URL
