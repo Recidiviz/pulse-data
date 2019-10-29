c8b43ee399f75ff7de3c9bb3152dc5bac85dd12e
Julia Dressel <julia@recidiviz.org>
2019-10-29 12:11:47 -0700

    Views for average change in LSI-R over time (Recidiviz/recidiviz-data#2579)
    All views necessary to produce the calculations for the average change in LSI-R scores between first reassessment and termination of supervision
    
    GitOrigin-RevId: 7088ed794bd3e64586ba3ff3a3df4f97b6455a04

--- recidiviz/calculator/bq/dashboard/views/reference/supervision_success_by_person.py
+++ recidiviz/calculator/bq/dashboard/views/reference/supervision_success_by_person.py
@@ -47,12 +47,12 @@ FROM
 (SELECT IFNULL(succ.state_code, fail.state_code) as state_code, IFNULL(succ.projected_year, fail.projected_year) as projected_year, IFNULL(succ.projected_month, fail.projected_month) as projected_month, IFNULL(succ.person_id, fail.person_id) as person_id, IFNULL(success_count, 0) as success_count, IFNULL(failure_count, 0) as failure_count
 FROM
 (SELECT state_code, projected_year, projected_month, person_id, count(*) as success_count FROM
-(SELECT * FROM `{project_id}.{views_dataset}.supervision_termination_by_person`)
+(SELECT * FROM `{project_id}.{views_dataset}.supervision_termination_by_person_and_projected_completion`)
 WHERE termination_reason in ('DISCHARGE', 'EXPIRATION')
 GROUP BY state_code, projected_year, projected_month, person_id) succ
 FULL OUTER JOIN
 (SELECT state_code, projected_year, projected_month, person_id, count(*) as failure_count FROM
-(SELECT * FROM `{project_id}.{views_dataset}.supervision_termination_by_person`)
+(SELECT * FROM `{project_id}.{views_dataset}.supervision_termination_by_person_and_projected_completion`)
 WHERE termination_reason in ('ABSCONSION', 'REVOCATION', 'SUSPENSION')
 GROUP BY state_code, projected_year, projected_month, person_id) fail
 ON succ.projected_year = fail.projected_year and succ.projected_month = fail.projected_month and succ.person_id = fail.person_id)
