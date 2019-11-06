1f54b07e8d5b5725750752c451b7936dec125e8d
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2019-11-06 14:58:20 +0700

    [MO yaml mappings] Add StateIncarcerationSentence mappings (Recidiviz/recidiviz-data#2609)
    GitOrigin-RevId: a0bfe4d468225322ddc52639320a8aab4c7a7c73

--- recidiviz/ingest/direct/state_shared_row_posthooks.py
+++ recidiviz/ingest/direct/state_shared_row_posthooks.py
@@ -19,14 +19,17 @@
 All functions added to this file should have an associated to-do outlining how
 we might update our yaml mapping support to handle this case more generally.
 """
-from typing import Dict, List, Callable
+import importlib
+import logging
+from typing import Dict, List, Callable, Type, Optional, Union
 
 from recidiviz.common.constants.state.state_person_alias import \
     StatePersonAliasType
 from recidiviz.ingest.direct.direct_ingest_controller_utils import \
     create_if_not_exists
 from recidiviz.ingest.models.ingest_info import IngestObject, StateAlias, \
-    StatePerson, StatePersonExternalId
+    StatePerson, StatePersonExternalId, StateSentenceGroup, \
+    StateSupervisionSentence, StateIncarcerationSentence
 from recidiviz.ingest.models.ingest_object_cache import IngestObjectCache
 
 
@@ -78,3 +81,134 @@ def gen_label_single_external_id_hook(external_id_type: str) -> Callable:
                 found = True
 
     return _label_external_id
+
+
+def _load_county_codes_map_for_state(state_code: str) -> Dict[str, str]:
+    module_name = \
+        f'recidiviz.ingest.direct.regions.{state_code.lower()}' \
+        f'.{state_code.lower()}_county_code_reference'
+
+    module = importlib.import_module(module_name)
+
+    return getattr(module, 'COUNTY_CODES')
+
+
+def _default_normalized_county_code(
+        county_code: Optional[str],
+        county_codes_map: Dict[str, str]) -> Optional[str]:
+    if not county_code:
+        return None
+
+    normalized_code = county_codes_map.get(county_code.upper())
+    if not normalized_code:
+        logging.warning("Found new county code not in reference cache: [%s]",
+                        county_code)
+        return county_code
+
+    return normalized_code
+
+
+def gen_normalize_county_codes_posthook(
+        state_code: str,
+        col_name: str,
+        ingest_type: Type[IngestObject],
+        custom_normalize_fn: Optional[Callable[[str,
+                                                Dict[str, str]],
+                                               Optional[str]]] = None
+) -> Callable:
+    normalize_fn = custom_normalize_fn \
+        if custom_normalize_fn is not None \
+        else _default_normalized_county_code
+
+    county_codes_map = _load_county_codes_map_for_state(state_code)
+
+    def _normalize_county_codes(
+            _file_tag: str,
+            row: Dict[str, str],
+            extracted_objects: List[IngestObject],
+            _cache: IngestObjectCache):
+
+        county_code = row[col_name]
+        normalized_code = normalize_fn(county_code, county_codes_map)
+
+        for extracted_object in extracted_objects:
+            if isinstance(extracted_object, ingest_type):
+                extracted_object.__setattr__('county_code', normalized_code)
+
+    return _normalize_county_codes
+
+
+def gen_map_ymd_counts_to_max_length_field_posthook(
+        years_col_name: str,
+        months_col_name: str,
+        days_col_name: str,
+        ingest_type: Type[Union[StateSentenceGroup,
+                                StateIncarcerationSentence,
+                                StateSupervisionSentence]],
+        test_for_fallback: Optional[Callable] = None,
+        fallback_parser: Optional[Callable] = None
+):
+    """Generates a row post-hook that will hydrate the max_length field as
+    formatted duration string on objects with the specified sentence type,
+    based on the values in separate year, month, and day counts columns.
+
+    Example output of posthook: '10Y 5M 0D'
+
+    The two optional callables are used for state-specific fallback parsing to
+    be used in the event that the generated length string is unsuitable. If the
+    |test_for_fallback| is given and it returns False on the generated length
+    string, then it will pass the length string into the given |fallback_parser|
+    and use that output instead.
+    """
+
+    def _normalize_ymd_codes(
+            _file_tag: str,
+            row: Dict[str, str],
+            extracted_objects: List[IngestObject],
+            _cache: IngestObjectCache):
+
+        def normalize_numerical_str(numerical_str: str) -> str:
+            no_commas_str = numerical_str.replace(',', '')
+
+            if not no_commas_str:
+                return '0'
+
+            return no_commas_str
+
+        years = normalize_numerical_str(row[years_col_name])
+        months = normalize_numerical_str(row[months_col_name])
+        days = normalize_numerical_str(row[days_col_name])
+
+        length_str = f'{years}Y {months}M {days}D'
+        if test_for_fallback and not test_for_fallback(length_str):
+            if fallback_parser:
+                length_str = fallback_parser(length_str)
+
+        for extracted_object in extracted_objects:
+            if isinstance(extracted_object, ingest_type):
+                extracted_object.__setattr__('max_length', length_str)
+
+    return _normalize_ymd_codes
+
+
+def gen_set_is_life_sentence_hook(
+        sen_calc_type_col: str,
+        is_life_val: str,
+        ingest_type: Type[Union[StateSentenceGroup,
+                                StateIncarcerationSentence]]) -> Callable:
+    def _set_is_life_sentence(_file_tag: str,
+                              row: Dict[str, str],
+                              extracted_objects: List[IngestObject],
+                              _cache: IngestObjectCache):
+        is_life_sentence = row[sen_calc_type_col] == is_life_val
+
+        if not is_life_sentence and ingest_type == StateSentenceGroup:
+            # Don't set is_life on the StateSentenceGroup rollup unless we
+            # have positive confirmation.
+            return
+
+        for extracted_object in extracted_objects:
+            if isinstance(extracted_object, ingest_type):
+                extracted_object.__setattr__('is_life', str(is_life_sentence))
+
+    return _set_is_life_sentence
