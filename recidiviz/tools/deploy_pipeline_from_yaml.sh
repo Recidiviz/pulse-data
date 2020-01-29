bfc530eb3f426add49063a576ab2045566d5f424
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-01-28 18:23:20 -0800

    Fail deploy scripts if jq is not installed (recidiviz/recidiviz-data#2809)
    This change makes it so the deploy scripts don't fail silently when the deploy calculate pipeline script fails.
    
    GitOrigin-RevId: 31ed060cbf3d745505f4db0547f1166f0b215009

--- recidiviz/tools/deploy_pipeline_from_yaml.sh
+++ recidiviz/tools/deploy_pipeline_from_yaml.sh
@@ -10,6 +10,11 @@ function run_cmd {
     fi
 }
 
+function check_not_empty {
+    # Check if variable is empty and exit if it is
+    [ -z "$1" ] && echo "Variable is unset" && exit 1
+}
+
 function deploy_pipelines {
   cmd=$1
   project=$2
@@ -17,6 +22,8 @@ function deploy_pipelines {
   file=$4
 
   pipeline_count="$(cat $file | pipenv run yq -r .pipeline_count)"
+  check_not_empty pipeline_count
+
   END=$pipeline_count
   ((END = END - 1))
   i=0
@@ -24,9 +31,14 @@ function deploy_pipelines {
   while [[ $i -le $END ]]
   do
       pipeline="$(cat $file | pipenv run yq -r .pipelines[$i].pipeline)"
+      check_not_empty pipeline
       job_name="$(cat $file | pipenv run yq -r .pipelines[$i].job_name)"
+      check_not_empty job_name
       input="$(cat $file | pipenv run yq -r .pipelines[$i].input)"
+      check_not_empty input
       output="$(cat $file | pipenv run yq -r .pipelines[$i].output)"
+      check_not_empty output
+
       run_cmd "$cmd $project $bucket $pipeline $job_name $input $output"
       ((i = i + 1))
   done
