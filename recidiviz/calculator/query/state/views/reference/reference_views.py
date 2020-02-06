9fedb38e6b2ffe01f5a8e4f0656ef782dd73cc54
Julia Dressel <julia@recidiviz.org>
2020-02-05 18:32:00 -0800

    Adding persons_with_last_known_address (Recidiviz/recidiviz-data#2835)
    * Adding persons_with_last_known_address
    
    GitOrigin-RevId: 69d26d8b68e1b1302c3a08c3af50da584718078d

--- recidiviz/calculator/query/state/views/reference/reference_views.py
+++ recidiviz/calculator/query/state/views/reference/reference_views.py
@@ -19,6 +19,8 @@ from recidiviz.calculator.query.state.views.reference.most_recent_job_id_by_metr
     MOST_RECENT_JOB_ID_BY_METRIC_AND_STATE_CODE_VIEW
 from recidiviz.calculator.query.state.views.reference.persons_to_recent_county_of_residence import \
     PERSONS_TO_RECENT_COUNTY_OF_RESIDENCE_VIEW
+from recidiviz.calculator.query.state.views.reference.persons_with_last_known_address import \
+    PERSONS_WITH_LAST_KNOWN_ADDRESS_VIEW
 from recidiviz.calculator.query.state.views.reference.ssvr_to_agent_association import \
     SSVR_TO_AGENT_ASSOCIATION_VIEW
 from recidiviz.calculator.query.state.views.reference.supervision_period_to_agent_association import \
@@ -28,5 +30,6 @@ REF_VIEWS = [
     MOST_RECENT_JOB_ID_BY_METRIC_AND_STATE_CODE_VIEW,
     SSVR_TO_AGENT_ASSOCIATION_VIEW,
     SUPERVISION_PERIOD_TO_AGENT_ASSOCIATION_VIEW,
+    PERSONS_WITH_LAST_KNOWN_ADDRESS_VIEW,
     PERSONS_TO_RECENT_COUNTY_OF_RESIDENCE_VIEW,
 ]
