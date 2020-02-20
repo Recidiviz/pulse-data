5b421ebb5f7004afd68cb96b5e2691e988880616
terinpw <terin@recidiviz.org>
2020-02-19 17:16:48 -0800

    Regenerate migration because of hash conflict (recidiviz/recidiviz-data#2872)
    This PR regenerates the migration from recidiviz/recidiviz-data#2866 so that the hashes agree.
    
    Co-authored-by: Helper Bot <48495666+helperbot-recidiviz@users.noreply.github.com>
    GitOrigin-RevId: 3529e06021ffc2e9498d4460d7f2cc4b508bc707

--- recidiviz/persistence/database/migrations/state/versions/2020_02_19_1815_a0398559d189_add_internal_unknown_incarceration_admission_reason.py
+++ recidiviz/persistence/database/migrations/state/versions/2020_02_19_1815_a0398559d189_add_internal_unknown_incarceration_admission_reason.py
@@ -1,8 +1,8 @@
 """add_internal_unknown_incarceration_admission_reason
 
-Revision ID: 491547b72334
-Revises: 1b4d14ad4a70
-Create Date: 2020-02-18 18:33:31.502400
+Revision ID: a0398559d189
+Revises: a6fb0ed2091c
+Create Date: 2020-02-19 18:15:56.393793
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '491547b72334'
-down_revision = '1b4d14ad4a70'
+revision = 'a0398559d189'
+down_revision = 'a6fb0ed2091c'
 branch_labels = None
 depends_on = None
 
@@ -49,3 +49,4 @@ def downgrade():
                     type_=sa.Enum(*old_values, name='state_incarceration_period_admission_reason'),
                     postgresql_using='admission_reason::text::state_incarceration_period_admission_reason')
     op.execute('DROP TYPE state_incarceration_period_admission_reason_old;')
+
