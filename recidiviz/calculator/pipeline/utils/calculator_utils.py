a5656fbb2d319527ff83b3f1d58597ccbe8852c5
Julia Dressel <julia@recidiviz.org>
2020-04-13 17:19:47 -0700

    Race and ethnicity values lists for person-level metrics (Recidiviz/recidiviz-data#3062)
    * Makes race/ethnicity output a comma separated list for person-level metrics
    
    GitOrigin-RevId: 7340d97a35c56a23698cb30a8ad06298fc597311

--- recidiviz/calculator/pipeline/utils/calculator_utils.py
+++ recidiviz/calculator/pipeline/utils/calculator_utils.py
@@ -31,8 +31,7 @@ from recidiviz.common.constants.state.state_supervision_violation import \
     StateSupervisionViolationType
 from recidiviz.common.constants.state.state_supervision_violation_response \
     import StateSupervisionViolationResponseDecision
-from recidiviz.persistence.entity.state.entities import StatePerson, StatePersonRace, StatePersonEthnicity,\
-    StateSupervisionViolation
+from recidiviz.persistence.entity.state.entities import StatePerson, StateSupervisionViolation
 
 # Relevant metric period month lengths for dashboard person-based calculations
 METRIC_PERIOD_MONTHS = [36, 12, 6, 3]
@@ -69,51 +68,55 @@ VIOLATION_TYPE_SEVERITY_ORDER = [
 ]
 
 
-def for_characteristics_races_ethnicities(
-        races: List[StatePersonRace], ethnicities: List[StatePersonEthnicity],
-        characteristics: Dict[str, Any]) -> List[Dict[str, Any]]:
-    """Produces the list of all combinations of the given metric
-    characteristics, given the fact that there can be multiple races and
-    ethnicities present.
+def for_characteristics_races_ethnicities(characteristics: Dict[str, Any]) -> List[Dict[str, Any]]:
+    """Produces the list of all combinations of the given metric characteristics, given the fact that there can be
+    multiple races and ethnicities present.
 
     For example, this function call:
-        for_characteristics_races_ethnicities(races: [Race.BLACK, Race.WHITE],
-        ethnicities: [Ethnicity.HISPANIC, Ethnicity.NOT_HISPANIC],
-        characteristics: {'gender': Gender.FEMALE, 'age': '<25'})
+        for_characteristics_races_ethnicities(characteristics: {
+            'gender': Gender.FEMALE, 'age': '<25', 'race': [Race.BLACK, Race.WHITE],
+            'ethnicity': [Ethnicity.HISPANIC, Ethnicity.NOT_HISPANIC]
+            }
+        )
 
-    First computes all combinations for the given characteristics. Then, for
-    each race present, adds a copy of each combination augmented with the race.
-    For each ethnicity present, adds a copy of each combination augmented with
-    the ethnicity. Finally, for every combination of race and ethnicity, adds a
+    First computes all combinations for the given base characteristics, excluding the race and ethnicity values. Then,
+    for each race present, adds a copy of each combination augmented with the race. For each ethnicity present, adds a
+    copy of each combination augmented with the ethnicity. Finally, for every combination of race and ethnicity, adds a
     copy of each combination augmented with both the race and the ethnicity.
     """
+    base_characteristics = characteristics.copy()
+
+    # Remove and store the race and ethnicity lists
+    races = base_characteristics.pop('race', [])
+    ethnicities = base_characteristics.pop('ethnicity', [])
+
     # Initial combinations
-    combos: List[Dict[str, Any]] = for_characteristics(characteristics)
+    combos: List[Dict[str, Any]] = for_characteristics(base_characteristics)
 
     # Race additions
     race_combos: List[Dict[Any, Any]] = []
-    for race_object in races:
+    for race in races:
         for combo in combos:
             augmented_combo = combo.copy()
-            augmented_combo['race'] = race_object.race
+            augmented_combo['race'] = race
             race_combos.append(augmented_combo)
 
     # Ethnicity additions
     ethnicity_combos: List[Dict[Any, Any]] = []
-    for ethnicity_object in ethnicities:
+    for ethnicity in ethnicities:
         for combo in combos:
             augmented_combo = combo.copy()
-            augmented_combo['ethnicity'] = ethnicity_object.ethnicity
+            augmented_combo['ethnicity'] = ethnicity
             ethnicity_combos.append(augmented_combo)
 
     # Multi-race and ethnicity additions
     race_ethnicity_combos: List[Dict[Any, Any]] = []
-    for race_object in races:
-        for ethnicity_object in ethnicities:
+    for race in races:
+        for ethnicity in ethnicities:
             for combo in combos:
                 augmented_combo = combo.copy()
-                augmented_combo['race'] = race_object.race
-                augmented_combo['ethnicity'] = ethnicity_object.ethnicity
+                augmented_combo['race'] = race
+                augmented_combo['ethnicity'] = ethnicity
                 race_ethnicity_combos.append(augmented_combo)
 
     combos = combos + race_combos + ethnicity_combos + race_ethnicity_combos
@@ -151,6 +154,32 @@ def for_characteristics(characteristics) -> List[Dict[str, Any]]:
         for combo in i_combinations:
             combos.append(combo)
     return combos
+
+
+def add_demographic_characteristics(characteristics: Dict[str, Any],
+                                    person: StatePerson,
+                                    inclusions: Dict[str, bool],
+                                    event_date: date) -> Dict[str, Any]:
+    """Adds the person's demographic characteristics to the given |characteristics| dictionary, including only the
+    attributes that are explicitly set to be included in the |inclusions| dictionary. For the 'age_bucket' field,
+    calculates the person's age on the |event_date|.
+    """
+    if inclusions.get('age_bucket'):
+        event_age = age_at_date(person, event_date)
+        event_age_bucket = age_bucket(event_age)
+        if event_age_bucket is not None:
+            characteristics['age_bucket'] = event_age_bucket
+    if inclusions.get('gender'):
+        if person.gender is not None:
+            characteristics['gender'] = person.gender
+    if inclusions.get('race'):
+        if person.races:
+            characteristics['race'] = [race_object.race for race_object in person.races]
+    if inclusions.get('ethnicity'):
+        if person.ethnicities:
+            characteristics['ethnicity'] = [ethnicity_object.ethnicity for ethnicity_object in person.ethnicities]
+
+    return characteristics
 
 
 def age_at_date(person: StatePerson, check_date: date) -> Optional[int]:
