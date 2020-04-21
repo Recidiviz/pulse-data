f64d23520fedd847c8d382a10d23cea9268d2651
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-04-21 14:24:35 -0700

    [MO calculate] Add person_id to us_mo_sentence_statuses table (recidiviz/recidiviz-data#3112)
    Adds a person_id field to all us_mo_sentence_status rows so we can group by person_id when importing into the pipeline.
    
    Also:
    * Filters out any rows that don't have an associated sentence in our DB
    * Removes intermediate table us_mo_tak026_tak025_sentence_statuses from query (was derived differently in staging and prod) in favor of referencing raw data tables.
    
    Necessary for getting MO calc run going in prod.
    
    GitOrigin-RevId: decbcb322aed992a12a67e28cf3990b765796611

--- recidiviz/calculator/query/state/views/reference/us_mo_sentence_statuses.py
+++ recidiviz/calculator/query/state/views/reference/us_mo_sentence_statuses.py
@@ -15,13 +15,14 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """BQ View containing US_MO state statuses from TAK026"""
-from recidiviz.calculator.query import bqview
+from recidiviz.calculator.query import bqview, export_config
 from recidiviz.calculator.query.state import view_config
 
 from recidiviz.utils import metadata
 
 PROJECT_ID = metadata.project_id()
 REFERENCE_TABLES_DATASET = view_config.REFERENCE_TABLES_DATASET
+BASE_DATASET = export_config.STATE_BASE_TABLES_BQ_DATASET
 
 US_MO_SENTENCE_STATUSES_VIEW_NAME = \
     'us_mo_sentence_statuses'
@@ -34,21 +35,42 @@ US_MO_SENTENCE_STATUSES_QUERY = \
     """
     /*{description}*/
     SELECT
-      CONCAT(DOC, '-', CYC, '-', SEO) as sentence_external_id,
-      CONCAT(DOC, '-', CYC, '-', SEO, '-', SSO) as sentence_status_external_id,
-      SCD AS status_code,
-      SY AS status_date,
-      SDE AS status_description
-    FROM
-      `{project_id}.{reference_tables_dataset}.us_mo_tak026_tak025_sentence_statuses`
-    WHERE
-      DOC IS NOT NULL AND
-      CYC IS NOT NULL AND
-      SEO IS NOT NULL AND
-      SSO IS NOT NULL
+        COALESCE(incarceration_sentences.person_id, supervision_sentences.person_id) AS person_id,
+        all_statuses.*
+    FROM (
+        SELECT
+          CONCAT(DOC, '-', CYC, '-', SEO) as sentence_external_id,
+          CONCAT(DOC, '-', CYC, '-', SEO, '-', SSO) as sentence_status_external_id,
+          SCD AS status_code,
+          SY AS status_date,
+          SDE AS status_description
+        FROM
+          `{project_id}.{reference_tables_dataset}.us_mo_tak026_sentence_status`
+        LEFT OUTER JOIN
+          `{project_id}.{reference_tables_dataset}.us_mo_tak025_sentence_status_xref`
+        USING (DOC, CYC, SSO)
+        LEFT OUTER JOIN
+          `{project_id}.{reference_tables_dataset}.us_mo_tak146_status_code_descriptions`
+        USING (SCD)
+        WHERE
+          DOC IS NOT NULL AND
+          CYC IS NOT NULL AND
+          SEO IS NOT NULL AND
+          SSO IS NOT NULL
+    ) all_statuses
+    LEFT OUTER JOIN
+      `{project_id}.{base_dataset}.state_supervision_sentence` supervision_sentences
+    ON
+        supervision_sentences.external_id = sentence_external_id
+    LEFT OUTER JOIN
+      `{project_id}.{base_dataset}.state_incarceration_sentence` incarceration_sentences
+    ON
+        incarceration_sentences.external_id = sentence_external_id
+    WHERE (incarceration_sentences.person_id IS NOT NULL OR supervision_sentences.person_id IS NOT NULL);
 """.format(
         description=US_MO_SENTENCE_STATUSES_DESCRIPTION,
         project_id=PROJECT_ID,
+        base_dataset=BASE_DATASET,
         reference_tables_dataset=REFERENCE_TABLES_DATASET,
     )
 
