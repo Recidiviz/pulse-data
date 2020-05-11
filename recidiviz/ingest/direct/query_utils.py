95b5c951c52456abe8ea07bcf0071b2f64d6621c
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-05-11 12:25:31 -0700

    [SQL preprocessing] Implement raw file YAML for ID (Recidiviz/recidiviz-data#3186)
    Implements raw data yaml for Idaho, removing the need for the US_ID_RAW_TABLES_TO_PKS list.
    
    Moved the yaml parsing logic out of DirectIngestRawFileImportManager and into its own class DirectIngestRegionRawFileConfig to give us yaml parsing functionality without having to instantiate a file system object.
    
    This PR also includes adding a BigQuery client as an instance var to DirectIngestRawFileImportManager - that will be used in a PR to follow.
    
    Work towards Recidiviz/recidiviz-data#3020
    
    GitOrigin-RevId: 884beec45633b55910ee215a58e5c097d2d9d22d

--- recidiviz/ingest/direct/query_utils.py
+++ recidiviz/ingest/direct/query_utils.py
@@ -16,9 +16,9 @@
 # =============================================================================
 """Util functions useful for any region specific SQL queries."""
 import os
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Set
 
-from recidiviz.ingest.direct.controllers.direct_ingest_raw_file_import_manager import DirectIngestRawFileConfig
+from recidiviz.ingest.direct.controllers.direct_ingest_raw_file_import_manager import DirectIngestRegionRawFileConfig
 
 
 def output_sql_queries(query_name_to_query_list: List[Tuple[str, str]], dir_path: Optional[str] = None):
@@ -48,109 +48,16 @@ def _print_all_queries_to_console(query_name_to_query_list: List[Tuple[str, str]
         print(query_str)
 
 
-def get_raw_table_config(region_code: str,
-                         raw_table_name: str):
-    return DirectIngestRawFileConfig(
-        file_tag=raw_table_name,
-        primary_key_cols=get_primary_keys_for_table_name(region_code, raw_table_name),
-        # TODO(3020): None of the stop-gap utils that use get_raw_table_config() actually use these fields below, so we
-        #   just include arbitrary default values. This function should be eliminated entirely once SQL preprocessing
-        #   work is complete.
-        separator=',',
-        encoding='UTF-8',
-        ignore_quotes=False
-    )
-
-# TODO(3020): Move all PK logic into Raw Yaml class
-US_ID_RAW_TABLES_TO_PKS = [
-    ('applc_usr', ['usr_id']),
-    ('assess_qstn', ['assess_qstn_num', 'tst_sctn_num', 'assess_tst_id']),
-    ('assess_qstn_choice', ['qstn_choice_num', 'assess_qstn_num', 'tst_sctn_num', 'assess_tst_id']),
-    ('assess_tst', ['assess_tst_id']),
-    ('behavior_evaluation_source_cd', ['behavior_evaluation_source_cd']),
-    ('body_loc_cd', ['body_loc_cd']),
-    ('casemgr', ['move_srl', 'case_dtd']),
-    ('cis_codepersonnamesuffix', ['id']),
-    ('cis_codepersonnametype', ['id']),
-    ('cis_offender', ['id']),
-    ('cis_personname', ['id']),
-    ('clssfctn_cust_lvl', ['clssfctn_cust_lvl_cd']),
-    ('cntc_rslt_cd', ['cntc_rslt_cd']),
-    ('cntc_typ_cd', ['cntc_typ_cd']),
-    ('cntc_typ_subtyp_cd', ['cntc_typ_cd', 'cnt_subtyp_cd']),
-    ('county', ['cnty_cd']),
-    ('early_discharge', ['early_discharge_id']),
-    ('early_discharge_form_typ', ['early_discharge_form_typ_id']),
-    ('early_discharge_sent', ['early_discharge_sent_id']),
-    ('ethnic', ['ethnic_cd']),
-    ('facility', ['fac_cd']),
-    ('hrchy', ['staff_usr_id']),
-    ('judge', ['judge_cd']),
-    ('jurisdiction_decision_code', ['jurisdiction_decision_code_id']),
-    ('lgl_stat_cd', ['lgl_stat_cd']),
-    ('loc_typ_cd', ['loc_typ_cd']),
-    ('location', ['loc_cd']),
-    ('lvgunit', ['fac_cd', 'lu_cd']),
-    ('mittimus', ['mitt_srl']),
-    ('movement', ['move_srl']),
-    ('offender', ['docno']),
-    ('offense', ['off_cat', 'off_cd', 'off_deg']),
-    ('offstat', ['docno', 'incrno', 'statno']),
-    ('ofndr_agnt', ['ofndr_num']),
-    ('ofndr_behavior_evaluation', ['ofndr_behavior_evaluation_id']),
-    ('ofndr_classifications', ['classification_id']),
-    ('ofndr_dob', ['ofndr_num']),
-    ('ofndr_sctn_eval', ['ofndr_tst_id', 'tst_sctn_num', 'assess_tst_id']),
-    ('ofndr_tst', ['ofndr_tst_id']),
-    ('ofndr_tst_cert', ['ofndr_tst_id']),
-    ('ofndr_wrkld', ['ofndr_wrkld_id']),
-    ('sentdisp', ['sent_disp']),
-    ('sentence', ['mitt_srl', 'sent_no']),
-    ('sentprob', ['mitt_srl', 'sent_no']),
-    ('sentretn', ['mitt_srl', 'sent_no', 'retn_no']),
-    ('sexcod', ['sex_cd']),
-    ('sprvsn_cntc', ['sprvsn_cntc_id']),
-    ('statrls', ['stat_rls_typ', 'stat_cd']),
-    ('statstrt', ['stat_strt_typ']),
-    ('tst_qstn_rspns', ['tst_qstn_rspns_id']),
-    ('wrkld_cat', ['wrkld_cat_id']),
-]
-
-RAW_TABLES_TO_PKS_BY_STATE = {
-    'us_id': US_ID_RAW_TABLES_TO_PKS,
+RAW_TABLE_CONFIGS_BY_STATE = {
+    'us_id': DirectIngestRegionRawFileConfig('us_id'),
 }
 
 
-def get_raw_tables_for_state(state_code: str) -> List[str]:
+def get_raw_table_config(region_code: str,
+                         raw_table_name: str):
+    return RAW_TABLE_CONFIGS_BY_STATE[region_code].raw_file_configs[raw_table_name]
+
+
+def get_raw_tables_for_state(state_code: str) -> Set[str]:
     """Returns the names of all raw data tables for the given |state_code|."""
-    table_names = []
-    for table_name, _ in get_raw_tables_and_pk_tuples_for_state(state_code=state_code):
-        table_names.append(table_name)
-    return table_names
-
-
-def get_raw_tables_and_pk_tuples_for_state(state_code: str) -> List[Tuple[str, List[str]]]:
-    """For the provided |state_code|, returns a list of tuples which contain the raw data table name and a corresponding
-    list of primary keys for that table.
-    """
-    raw_tables_to_pks = RAW_TABLES_TO_PKS_BY_STATE.get(state_code)
-    if raw_tables_to_pks is None:
-        raise ValueError(f'No found tables to primary key associations for state {state_code}')
-    return raw_tables_to_pks
-
-
-def get_primary_keys_for_table_name(state_code, table_name) -> List[str]:
-    """Returns a list of the primary keys for the provided |state_code| and |table_name|"""
-    raw_tables_to_pks = get_raw_tables_and_pk_tuples_for_state(state_code)
-    for raw_table_name, pks in raw_tables_to_pks:
-        if raw_table_name == table_name:
-            return pks
-    return []
-
-
-def get_primary_key_str_for_table_name(state_code, table_name) -> str:
-    """Returns the primary keys for the provided |state_code| and |table_name| in a concatenated string meant for
-    SQL queries.
-    """
-    primary_keys = get_primary_keys_for_table_name(state_code, table_name)
-    return ", ".join(primary_keys)
+    return RAW_TABLE_CONFIGS_BY_STATE[state_code].raw_file_tags
