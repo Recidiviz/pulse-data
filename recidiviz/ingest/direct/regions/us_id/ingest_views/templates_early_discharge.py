fac1b7cea748855213a87deed8e12016b54799cc
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-05-14 09:21:05 -0700

    [SQL preprocessing] New BigQueryView class for region ingest view queries (Recidiviz/recidiviz-data#3208)
    Adds DirectIngestPreProcessedIngestView for programatically generating SQL query views on raw data tables. This view will be used by the soon-ish-to-come ingest view export manager.
    
    In this PR:
    * Rename DirectIngestRawDataTableLatestBigQueryView ->
    DirectIngestRawDataTableLatestView and DirectIngestRawDataTableUpToDateBigQueryView -> DirectIngestRawDataTableUpToDateView to be less verbose and have more naming consistency
    * DirectIngestPreProcessedIngestView implementation
    * DirectIngestPreProcessedIngestView tests
    * Implemented several US_ID queries in new format and verified these generate valid BQ queries.
    * Implemented `local_project_id_override` for use in local execution
    
    Work towards Recidiviz/recidiviz-data#3020
    
    GitOrigin-RevId: b21e345a7c59f735d12fa24951437f5c7d584fba

---
+++ recidiviz/ingest/direct/regions/us_id/ingest_views/templates_early_discharge.py
@@ -0,0 +1,128 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Helper templates for the US_ID early discharge queries."""
+from enum import auto, Enum
+
+
+class EarlyDischargeType(Enum):
+    INCARCERATION = auto()
+    SUPERVISION = auto()
+
+
+INCARCERATION_SENTENCE_IDS_QUERY = """
+      SELECT
+          mitt_srl,
+          incrno,
+          sent_no
+      FROM
+          {sentence}
+      LEFT JOIN
+          {mittimus}
+      USING
+          (mitt_srl)
+      LEFT JOIN
+          {sentprob}
+      USING
+          (mitt_srl, sent_no)
+      WHERE
+          {sentprob}.mitt_srl IS NULL
+"""
+
+PROBATION_SENTENCE_IDS_QUERY = """
+      SELECT
+          mitt_srl,
+          incrno,
+          sent_no
+      FROM
+          {sentence}
+      LEFT JOIN
+          {mittimus}
+      USING
+          (mitt_srl)
+      LEFT JOIN
+          {sentprob}
+      USING
+          (mitt_srl, sent_no)
+      WHERE
+          {sentprob}.mitt_srl IS NOT NULL
+"""
+
+EARLY_DISCHARGE_QUERY_TEMPLATE = """
+WITH 
+relevant_sentences AS ({relevant_sentence_query}),
+filtered_early_discharge AS (
+  SELECT 
+    * 
+  EXCEPT (
+    jurisdiction_decision_code_id, 
+    supervisor_review_date,
+    jurisdiction_authize_date)  # Ignore fields which are always NULL.
+  FROM 
+    {{early_discharge}}
+),
+form_type AS (
+  SELECT 
+    early_discharge_form_typ_id,
+    early_discharge_form_typ_desc,
+  FROM 
+    {{early_discharge_form_typ}}
+),
+jurisdiction_code AS (
+  SELECT
+    jurisdiction_decision_code_id,
+    jurisdiction_decision_description
+  FROM 
+   {{jurisdiction_decision_code}}
+)
+SELECT 
+  *
+FROM 
+  {{early_discharge_sent}}
+LEFT JOIN 
+  filtered_early_discharge
+USING 
+  (early_discharge_id)
+LEFT JOIN 
+  form_type
+USING 
+  (early_discharge_form_typ_id)
+LEFT JOIN 
+  jurisdiction_code
+USING 
+  (jurisdiction_decision_code_id)
+JOIN 
+  relevant_sentences
+USING
+  (mitt_srl, sent_no)
+ORDER BY 
+  ofndr_num, early_discharge_id;
+"""
+
+
+def _get_relevant_sentence_query_for_type(discharge_type: EarlyDischargeType) -> str:
+    if discharge_type == EarlyDischargeType.INCARCERATION:
+        return INCARCERATION_SENTENCE_IDS_QUERY
+    if discharge_type == EarlyDischargeType.SUPERVISION:
+        return PROBATION_SENTENCE_IDS_QUERY
+
+    raise ValueError(f'Unexpected discharge type {discharge_type}')
+
+
+def early_discharge_view_template(discharge_type: EarlyDischargeType) -> str:
+    return EARLY_DISCHARGE_QUERY_TEMPLATE.format(
+        relevant_sentence_query=_get_relevant_sentence_query_for_type(discharge_type)
+    )
