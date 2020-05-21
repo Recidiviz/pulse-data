2d89f0c54330fde3a00e56980993371711e9b4f9
Michael Parker <parker.michael@gmail.com>
2020-05-21 17:13:21 -0500

    MVP of the email reporting pipeline (Recidiviz/recidiviz-data#2952)
    GitOrigin-RevId: bf3b5955cba10be4e2966d69d7e27950e2237e3f

---
+++ recidiviz/cloud_functions/email_reporting/email_generation.py
@@ -0,0 +1,109 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+"""Functionality for the generation of email reports.
+
+Generates the HTML for email reports and stores it for later delivery.
+"""
+
+import json
+import logging
+
+from string import Template
+
+from google.cloud import pubsub_v1
+
+import email_reporting_utils as utils
+from report_context import ReportContext
+
+
+def generate(report_context: ReportContext) -> None:
+    """Generates an email for the identified recipient.
+
+    Receives the full user data, applies it to the HTML template and stores the result in Cloud Storage.
+
+    Args:
+        report_context: The context for a single recipient
+    """
+    prepared_data = report_context.get_prepared_data()
+    check_for_required_keys(prepared_data)
+
+    if report_context.has_chart():
+        start_chart_generation(report_context)
+
+    data_bucket = utils.get_data_storage_bucket_name()
+    template_filename = ''
+    try:
+        template_filename = utils.get_template_filename(report_context.state_code, report_context.get_report_type())
+        html_template = utils.load_string_from_storage(data_bucket, template_filename)
+    except Exception:
+        logging.error("Unable to load email template at %s/%s", data_bucket, template_filename)
+        raise
+
+    try:
+        template = Template(html_template)
+        final_email = template.substitute(prepared_data)
+    except KeyError as err:
+        logging.error("Attribute required for HTML template missing from recipient data: "
+                      "batch id = %s, email address = %s, attribute = %s",
+                      prepared_data[utils.KEY_BATCH_ID], prepared_data[utils.KEY_EMAIL_ADDRESS], err)
+        raise
+    except Exception:
+        logging.error("Unexpected error during templating. Recipient data = %s", prepared_data)
+        raise
+
+    html_bucket = utils.get_html_bucket_name()
+    html_filename = ''
+    try:
+        html_filename = utils.get_html_filename(prepared_data[utils.KEY_BATCH_ID],
+                                                prepared_data[utils.KEY_EMAIL_ADDRESS])
+        utils.upload_string_to_storage(html_bucket, html_filename, final_email, "text/html")
+    except Exception:
+        logging.error("Error while attempting upload of %s/%s", html_bucket, html_filename)
+        raise
+
+
+def check_for_required_keys(recipient_data: dict) -> None:
+    """Checks recipient_data for required information and raises errors.
+
+    Raises errors if email address, state id, or batch id are missing from recipient_data. This module cannot
+    function without them so callers should allow the raised errors to propagate up the stack.
+    """
+    keys = [utils.KEY_EMAIL_ADDRESS, utils.KEY_STATE_CODE, utils.KEY_BATCH_ID]
+
+    for key in keys:
+        if key not in recipient_data:
+            raise KeyError(f"Unable to generate email due to missing key {key}. "
+                           f"Recipient data = {json.dumps(recipient_data)}")
+
+
+def start_chart_generation(report_context: ReportContext) -> None:
+    """Starts chart generation for a recipient.
+
+    Uses Pub/Sub to send a message to the chart function. The message contains all of the recipient's data since the
+    chart function will pass it along to the next step in the generation process.
+
+    Args:
+        report_context: The report context containing the data and chart type
+    """
+    publisher = pubsub_v1.PublisherClient()
+
+    prepared_data = report_context.get_prepared_data()
+    payload = json.dumps(prepared_data)  # no error checking here since we already validated the JSON previously
+    # TODO(3260): Generalize this with report context
+    topic = utils.get_chart_topic()
+    publisher.publish(topic, payload.encode("utf-8"))
---
+++ recidiviz/cloud_functions/email_reporting/email_reporting_utils.py
@@ -0,0 +1,168 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+""" Utilities and constants shared across python modules in this package
+"""
+from datetime import datetime
+import logging
+import os
+
+from google.cloud import storage
+
+
+def get_env_var(key: str) -> str:
+    """Retrieve an environment variable
+
+    Args:
+        key: The key of the env var to retrieve
+
+    Returns:
+        The value of the variable. Guaranteed to return if it is set.
+
+    Raises:
+        KeyError if the environment variable does not exist
+    """
+    value = os.environ.get(key)
+    if value is None:
+        raise KeyError(f"The environment variable '{key}' is not set")
+    return value
+
+
+# Keys used to identify elements in dictionaries
+KEY_BATCH_ID = "batch_id"
+KEY_REPORT_TYPE = "report_type"
+KEY_EMAIL_ADDRESS = "email_address"
+KEY_STATE_CODE = "state_code"
+
+
+def get_project_id() -> str:
+    return get_env_var('GCP_PROJECT')
+
+
+def get_data_storage_bucket_name() -> str:
+    return f'{get_project_id()}-report-data'
+
+
+def get_data_archive_bucket_name() -> str:
+    return f'{get_project_id()}-report-data-archive'
+
+
+def get_html_bucket_name() -> str:
+    return f'{get_project_id()}-report-html'
+
+
+def get_images_bucket_name() -> str:
+    return f'{get_project_id()}-report-images'
+
+
+def get_cdn_static_ip() -> str:
+    return get_env_var('CDN_STATIC_IP')
+
+
+# TODO(3265): Convert from HTTP to HTTPS when a certificate has been set up
+def get_static_image_path(state_code: str, report_type: str) -> str:
+    return f'http://{get_cdn_static_ip()}/{state_code}/{report_type}/static'
+
+
+def get_data_filename(state_code: str, report_type: str) -> str:
+    return f'{state_code}/{report_type}/data.json'
+
+
+def get_data_archive_filename(batch_id: str) -> str:
+    return f'{batch_id}.json'
+
+
+def get_properties_filename(state_code: str, report_type: str) -> str:
+    return f'{state_code}/{report_type}/properties.json'
+
+
+def get_html_filename(batch_id: str, email_address: str) -> str:
+    return f'{batch_id}/{email_address}.html'
+
+
+def get_template_filename(state_code: str, report_type: str) -> str:
+    return f'{state_code}/{report_type}/template.html'
+
+
+# TODO(3260): Make this general-purpose to work for any report type's chart
+def get_chart_topic() -> str:
+    return f'projects/{get_project_id()}/topics/report_po_comparison_chart'
+
+
+def load_string_from_storage(bucket_name: str, filename: str) -> str:
+    """Load object from Cloud Storage and return as string.
+
+    Args:
+        bucket_name: The identifier of the Cloud Storage bucket
+        filename: The identifier of the object within the bucket
+
+    Returns:
+        String form of the object decoded using UTF-8
+
+    Raises:
+        All errors.  Callers are expected to handle.
+    """
+    storage_client = storage.Client()
+
+    logging.debug("Downloading %s/%s...", bucket_name, filename)
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(filename)
+    contents = blob.download_as_string().decode("utf-8")
+
+    logging.debug("Downloaded %s/%s.", bucket_name, filename)
+    return contents
+
+
+def upload_string_to_storage(bucket_name: str, filename: str, contents: str, content_type: str = 'text/plain') -> None:
+    """Upload a string into Cloud Storage.
+
+    Creates a new object in the given bucket with the given filename.
+
+    Args:
+        bucket_name: The identifier of the Cloud Storage bucket
+        filename: The identifier of the object within the bucket
+        contents: A string to put in the object
+        content_type: Optional parameter if the content is something other than plain text
+
+    Raises:
+        All errors.  Callers are expected to handle.
+    """
+    storage_client = storage.Client()
+
+    logging.debug("Uploading %s/%s...", bucket_name, filename)
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(filename)
+    blob.upload_from_string(contents, content_type=content_type)
+
+    logging.debug("Uploaded %s/%s.", bucket_name, filename)
+
+
+def generate_batch_id() -> str:
+    """Create a new batch id.
+
+    Create a batch ID using the current date and time.
+
+    Returns:
+        String in the format YYYYMMDDHHMMSS. Note that this is a string not
+        and integer because callers will exclusively use this in situations
+        requiring a string.
+    """
+    dt = datetime.now()
+    format_str = "%Y%m%d%H%M%S"
+    return dt.strftime(format_str)
