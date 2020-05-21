e9d517b65e8ef3397bc70df45dcb8868ddb8c0f1
Michael Parker <parker.michael@gmail.com>
2020-05-21 17:13:21 -0500

    MVP of the email reporting pipeline (Recidiviz/recidiviz-data#2952)
    GitOrigin-RevId: c3b9d854885772106ab9abf8476027bc90e9ae23

---
+++ recidiviz/cloud_functions/email_reporting/available_context.py
@@ -0,0 +1,36 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+"""Utilities for selecting from available report contexts."""
+
+from po_monthly_report.context import PoMonthlyReportContext
+from report_context import ReportContext
+
+
+def get_report_context(state_code: str, report_type: str, recipient_data: dict) -> ReportContext:
+    """Returns the appropriate report context for the given parameters, choosing the correct ReportContext
+    implementation.
+
+    Args:
+        state_code: State identifier for the recipient
+        report_type: The type of report to be sent to the recipient
+        recipient_data: The retrieved data for this recipient
+    """
+    if report_type == "po_monthly_report":
+        return PoMonthlyReportContext(state_code, recipient_data)
+
+    raise KeyError(f"Unrecognized report type: {report_type}")
