a3a39f9c118fb2a2fb24d8cee28da06ed978a68e
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-05-26 18:45:39 -0700

    [Build] Fix pylint script to handle when there are no python files changed (recidiviz/recidiviz-data#3285)
    I was not handling the case where you only update non-python files, this fix updates the run_pylint script to handle that case and skip pylint entirely.
    
    If pylint passes on this build, then the fix works.
    
    GitOrigin-RevId: 2b04e781f16fa87b4a402469e7aabcf264111b69

--- recidiviz/tools/travis/run_pylint.sh
+++ recidiviz/tools/travis/run_pylint.sh
@@ -2,8 +2,9 @@
 
 merge_base_hash=$(git merge-base master HEAD)
 current_head_hash=$(git rev-parse HEAD)
-current_head_branch_name=$(git rev-parse HEAD --abbrev-ref)
+current_head_branch_name=$(git rev-parse --abbrev-ref HEAD)
 changed_files_cmd="git diff --name-only $(git merge-base HEAD master)"
+changed_files=$(${changed_files_cmd})
 
 # Look for changes in Pipfile.lock and .pylintrc - changes in these files could mean that python files that have not
 # been touched now have new lint errors.
@@ -19,6 +20,13 @@ then
     echo "Running pylint for branch $current_head_branch_name. Pylint config changed=[$pylint_config_may_have_changed]";
     pylint recidiviz;
 else
+    changed_python_files=$(${changed_files_cmd} | grep '.*\.py$')
+    if [[ -z ${changed_python_files} ]]
+    then
+        echo "Found no python files changed - not running pylint."
+        exit 0
+    fi
+
     echo "Running differential pylint for branch $current_head_branch_name";
-    pylint $(${changed_files_cmd} | grep '.*\.py$');
+    pylint ${changed_python_files};
 fi
