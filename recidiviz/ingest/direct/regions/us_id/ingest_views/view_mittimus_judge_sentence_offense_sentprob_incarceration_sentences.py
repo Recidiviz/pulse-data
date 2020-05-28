1328ee0dbcefe545031d68a5ebaf97e2f23b29a4
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-05-28 15:15:49 -0700

    [SQL Preprocessing] Migrate remaining US_ID queries to the new ingest view infrastructure (Recidiviz/recidiviz-data#3286)
    Migrates all of the remaining US_ID SQL queries to the new ingest_views directory so they can be executed as part of SQL preprocessing.
    
    GitOrigin-RevId: 147e8d8638c7513bca593c3f4fc344d045ef5947

---
+++ recidiviz/ingest/direct/regions/us_id/ingest_views/view_mittimus_judge_sentence_offense_sentprob_incarceration_sentences.py
@@ -0,0 +1,34 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query for incarceration sentences."""
+from recidiviz.ingest.direct.controllers.direct_ingest_big_query_view_types import \
+    DirectIngestPreProcessedIngestViewBuilder
+from recidiviz.ingest.direct.regions.us_id.ingest_views.templates_sentences import sentence_view_template, SentenceType
+from recidiviz.utils.environment import GAE_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = sentence_view_template(sentence_type=SentenceType.INCARCERATION)
+
+VIEW_BUILDER = DirectIngestPreProcessedIngestViewBuilder(
+    region='us_id',
+    ingest_view_name='mittimus_judge_sentence_offense_sentprob_incarceration_sentences',
+    view_query_template=VIEW_QUERY_TEMPLATE
+)
+
+if __name__ == '__main__':
+    with local_project_id_override(GAE_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
---
+++ recidiviz/ingest/direct/regions/us_id/ingest_views/view_ofndr_tst_ofndr_tst_cert.py
@@ -0,0 +1,49 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query that generates info for all LSIR assessments."""
+
+from recidiviz.ingest.direct.controllers.direct_ingest_big_query_view_types import \
+    DirectIngestPreProcessedIngestViewBuilder
+from recidiviz.utils.environment import GAE_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """SELECT
+        *
+    EXCEPT
+        (updt_usr_id, updt_dt)
+    FROM
+        {ofndr_tst}
+    LEFT JOIN
+        {ofndr_tst_cert}
+    USING
+        (ofndr_tst_id, assess_tst_id)
+    WHERE
+        {ofndr_tst}.assess_tst_id = '2'  # LSIR assessments
+        AND {ofndr_tst_cert}.cert_pass_flg = 'Y'  # Test score has been certified
+    ORDER BY
+        {ofndr_tst}.ofndr_num
+"""
+
+VIEW_BUILDER = DirectIngestPreProcessedIngestViewBuilder(
+    region='us_id',
+    ingest_view_name='ofndr_tst_ofndr_tst_cert',
+    view_query_template=VIEW_QUERY_TEMPLATE,
+)
+
+if __name__ == '__main__':
+    with local_project_id_override(GAE_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
