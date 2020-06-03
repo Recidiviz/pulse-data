afacfb5fc54f238cbf530ef59524c3f17a23b97d
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-06-02 22:13:24 -0700

    [SQL preprocessing] Update file check-in script to upload to ingest bucket (Recidiviz/recidiviz-data#3327)
    Updated the script to check new files into storage to upload to the ingest bucket, since "checking a raw file in" now involves uploading to a raw data table.
    
    Also threw in what I believe is an actual fix for the pulse-data build issue.
    
    GitOrigin-RevId: 78b24b5aba013ff5471cc1efb9aa3b4d158b632a

--- recidiviz/tests/ingest/direct/regions/us_pa/us_pa_controller_test.py
+++ recidiviz/tests/ingest/direct/regions/us_pa/us_pa_controller_test.py
@@ -20,8 +20,6 @@ import datetime
 import json
 from typing import Type
 
-from mock import patch
-
 from recidiviz import IngestInfo
 from recidiviz.common.constants.charge import ChargeStatus
 from recidiviz.common.constants.person_characteristics import Gender, Race, Ethnicity, ResidencyStatus
@@ -37,12 +35,10 @@ from recidiviz.ingest.direct.regions.us_pa.us_pa_controller import UsPaControlle
 from recidiviz.ingest.models.ingest_info import StatePerson, StatePersonExternalId, StatePersonRace, StateAlias, \
     StatePersonEthnicity, StateAssessment, StateSentenceGroup, StateIncarcerationSentence, StateCharge, \
     StateCourtCase, StateAgent
-from recidiviz.ingest.scrape.regions.us_pa.us_pa_scraper import UsPaScraper
 from recidiviz.persistence.entity.state import entities
 from recidiviz.tests.ingest.direct.regions.base_state_direct_ingest_controller_tests import \
     BaseStateDirectIngestControllerTests
 from recidiviz.tests.ingest.direct.regions.utils import populate_person_backedges
-from recidiviz.utils import regions
 
 _STATE_CODE_UPPER = 'US_PA'
 
@@ -972,25 +968,3 @@ class TestUsPaController(BaseStateDirectIngestControllerTests):
         self._do_ingest_job_rerun_for_tags(self.controller.get_file_tag_rank_list())
 
         self.assert_expected_db_people(expected_people)
-
-    # Note: These tests really belong in regions_test.py, but since our copybara job that copies code to the pulse-data
-    # repo deletes everything in a regions directory, references to UsPaScraper and UsPaController will fail the build.
-    @patch("recidiviz.utils.environment.get_gae_environment")
-    def test_get_region_same_name_different_ingest_type(self, mock_environment):
-        mock_environment.return_value = 'staging'
-
-        us_pa_scraper_region = regions.get_region('us_pa', is_direct_ingest=False)
-        self.assertEqual(us_pa_scraper_region.get_ingestor_class(), UsPaScraper)
-
-        us_pa_direct_ingest_region = regions.get_region('us_pa', is_direct_ingest=True)
-        self.assertEqual(us_pa_direct_ingest_region.get_ingestor_class(), UsPaController)
-
-    @patch("recidiviz.utils.environment.get_gae_environment")
-    def test_get_region_same_name_different_ingest_type_reverse(self, mock_environment):
-        mock_environment.return_value = 'staging'
-
-        us_pa_direct_ingest_region = regions.get_region('us_pa', is_direct_ingest=True)
-        self.assertEqual(us_pa_direct_ingest_region.get_ingestor_class(), UsPaController)
-
-        us_pa_scraper_region = regions.get_region('us_pa', is_direct_ingest=False)
-        self.assertEqual(us_pa_scraper_region.get_ingestor_class(), UsPaScraper)
