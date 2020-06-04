9ddbfe965d22e0ae5ca91a3ffe2ca8f18ece78c0
jamwalla <james.wallacelee@gmail.com>
2020-06-04 12:48:18 -0400

    KY agg site scraper - skip 9-12-10 (Recidiviz/recidiviz-data#3333)
    GitOrigin-RevId: a29991abe2d63d1683e10e86258c293b89c0b286

--- recidiviz/ingest/aggregate/regions/ky/ky_aggregate_site_scraper.py
+++ recidiviz/ingest/aggregate/regions/ky/ky_aggregate_site_scraper.py
@@ -39,6 +39,9 @@ def get_urls_to_download() -> Set[str]:
     for link in links:
         link = unquote(link)
         if 'weekly jail' in link.lower() and 'pdf' in link.lower():
+            # This report is missing and the page gives a 404
+            if link.endswith('09-12-19.pdf'):
+                continue
             # Fix typo in link for ​​October 17, 2019
             if link.endswith('12-17-19.pdf'):
                 link = link.replace('12-17-19', '10-17-19')
