7d75ccc651517fea67dea45fad053e571d391d8f
Julia Dressel <julia@recidiviz.org>
2020-07-13 12:59:25 -0700

    Views for probation and parole pages of public dashboard (Recidiviz/recidiviz-data#3538)
    BigQuery views to support the Probation and Parole pages of the US_ND Public Dashboard. Includes the view export to the new public_dashboard_views dataset, and file export to the new {project_id}-public-dashboard-data GCS bucket.
    
    GitOrigin-RevId: 553f2b0391fa92c7e5d629b764deab4ef2015478

---
+++ recidiviz/calculator/query/state/views/public_dashboard/supervision/supervision_views.py
@@ -0,0 +1,39 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Public dashboard views related to supervision."""
+# pylint: disable=line-too-long
+from typing import List
+
+from recidiviz.big_query.big_query_view import BigQueryViewBuilder
+from recidiviz.calculator.query.state.views.public_dashboard.supervision.active_program_participation_by_region import \
+    ACTIVE_PROGRAM_PARTICIPATION_BY_REGION_VIEW_BUILDER
+from recidiviz.calculator.query.state.views.public_dashboard.supervision.supervision_population_by_district_by_demographics import \
+    SUPERVISION_POPULATION_BY_DISTRICT_BY_DEMOGRAPHICS_VIEW_BUILDER
+from recidiviz.calculator.query.state.views.public_dashboard.supervision.supervision_revocations_by_month_by_type_by_demographics import \
+    SUPERVISION_REVOCATIONS_BY_MONTH_BY_TYPE_BY_DEMOGRAPHICS_VIEW_VIEW_BUILDER
+from recidiviz.calculator.query.state.views.public_dashboard.supervision.supervision_success_by_month import \
+    SUPERVISION_SUCCESS_BY_MONTH_VIEW_BUILDER
+from recidiviz.calculator.query.state.views.public_dashboard.supervision.supervision_success_by_period_by_demographics import \
+    SUPERVISION_SUCCESS_BY_PERIOD_BY_DEMOGRAPHICS_VIEW_BUILDER
+
+SUPERVISION_VIEW_BUILDERS: List[BigQueryViewBuilder] = [
+    ACTIVE_PROGRAM_PARTICIPATION_BY_REGION_VIEW_BUILDER,
+    SUPERVISION_POPULATION_BY_DISTRICT_BY_DEMOGRAPHICS_VIEW_BUILDER,
+    SUPERVISION_REVOCATIONS_BY_MONTH_BY_TYPE_BY_DEMOGRAPHICS_VIEW_VIEW_BUILDER,
+    SUPERVISION_SUCCESS_BY_MONTH_VIEW_BUILDER,
+    SUPERVISION_SUCCESS_BY_PERIOD_BY_DEMOGRAPHICS_VIEW_BUILDER
+]
