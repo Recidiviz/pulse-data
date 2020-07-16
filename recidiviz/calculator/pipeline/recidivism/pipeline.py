170e680232949888290411bba89373777cdd7cd4
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-07-16 16:33:30 -0700

    [PA ingest] Bugfixes to enable PA incarceration pipeline run (Recidiviz/recidiviz-data#3603)
    Fixes several bugs in the incarceration pipeline to enable a full PA pipeline run:
    
    1) Don't crash if a person has only placeholder incarceration periods - get the state code from the full list of periods BEFORE we've filtered out placeholders.
    2) Filter the results of ip_to_judicial_district_query (and other similar reference input queries) by state code so we don't crash in person_and_kwargs_for_identifier b/c there are no people.
    
    This is not necessarily a comprehensive set of fixes to get PA running, but Recidiviz/recidiviz-data#2 is a bug breaking other states in master...
    
    Work towards Recidiviz/recidiviz-data#3449.
    
    GitOrigin-RevId: 26519d5d25bd4dbcc35eb07e3a19b962251145ce

--- recidiviz/calculator/pipeline/recidivism/pipeline.py
+++ recidiviz/calculator/pipeline/recidivism/pipeline.py
@@ -45,7 +45,7 @@ from recidiviz.calculator.pipeline.utils.beam_utils import ConvertDictToKVTuple
 from recidiviz.calculator.pipeline.utils.entity_hydration_utils import \
     SetViolationResponseOnIncarcerationPeriod, SetViolationOnViolationsResponse
 from recidiviz.calculator.pipeline.utils.execution_utils import get_job_id, person_and_kwargs_for_identifier, \
-    select_all_query
+    select_all_by_person_query
 from recidiviz.calculator.pipeline.utils.extractor_utils import BuildRootEntity
 from recidiviz.calculator.pipeline.utils.metric_utils import \
     json_serializable_metric_key
@@ -433,7 +433,14 @@ def run(apache_beam_pipeline_options: PipelineOptions,
         )
 
         # Bring in the table that associates people and their county of residence
-        person_id_to_county_query = select_all_query(reference_dataset, PERSONS_TO_RECENT_COUNTY_OF_RESIDENCE_VIEW_NAME)
+        person_id_to_county_query = select_all_by_person_query(
+            reference_dataset,
+            PERSONS_TO_RECENT_COUNTY_OF_RESIDENCE_VIEW_NAME,
+            # TODO(3602): Once we put state_code on StatePerson objects, we can update the
+            # persons_to_recent_county_of_residence query to have a state_code field, allowing us to also filter the
+            # output by state_code.
+            state_code_filter=None,
+            person_id_filter_set=person_id_filter_set)
 
         person_id_to_county_kv = (
             p
