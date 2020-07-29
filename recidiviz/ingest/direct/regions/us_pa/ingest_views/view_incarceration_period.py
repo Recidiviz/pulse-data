5e065a4d1095bd9a8f636cf94b8a07e9738e83ac
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-07-28 17:51:21 -0700

    [PA ingest] PA incarceration periods validation bugfixes (recidiviz/recidiviz-data#3678)
    This PR includes a number of query and ingest mapping bugfixes that should result in approximate incarceration population numbers with a less than 2% person-level error rate. This is based on a query that approximates how the calculation pipeline breaks periods into month buckets. Will need to re-ingest periods and rerun the calculation pipeline to confirm accuracy.
    
    As part of this fix, I added sentence types so we can differentiate treatment admissions from revocations and also updated admission reason mappings so that board hold admissions are not treated as revocations until the parole status changes to indicate they have been found guilty.
    
    Query updates in more detail:
    * Make sure a death status is the last status in sequence number ordering no matter what
    * Treat people on a bus or out on WRIT/ATA as being still attributed to their previous facility's population (this seems to be how PA does accounting)
    * Do not close incarceration periods if someone got put on a bus and hasn't been marked as being in a new facility yet (i.e. a person needs to have an explicit delete status row to be removed from the population)
    * Use the control number from `dbo_tblSearchInmateInfo` instead of `dbo_Movrec` because there are some control numbers in `dbo_Movrec` that just aren't in `dbo_tblSearchInmateInfo` at all (why.....I cannot say)
    * Do the same thing with control numbers in the `dbo_Miscon` query, since that table has the same issue
    
    Work towards recidiviz/recidiviz-data#3456
    
    GitOrigin-RevId: 93eb8886b8aaeab429ef8a7b495c8ffaec29c737

--- recidiviz/ingest/direct/regions/us_pa/ingest_views/view_incarceration_period.py
+++ recidiviz/ingest/direct/regions/us_pa/ingest_views/view_incarceration_period.py
@@ -25,28 +25,54 @@ from recidiviz.utils.metadata import local_project_id_override
 VIEW_QUERY_TEMPLATE = """
 WITH movements_base AS (
   SELECT 
-      m.mov_cnt_num AS control_number,
+      -- In 3 cases from the 80s, we see an inmate number with no corresponding row match in dbo_tblSearchInmateInfo -
+      -- in this case, we fall back on the control number in dbo_Movrec
+      COALESCE(ids.control_number, m.mov_cnt_num) AS control_number,
       m.mov_cur_inmt_num AS inmate_number,
       m.mov_move_date AS move_date,
-      m.mov_move_time AS move_time,
       m.mov_sent_stat_cd AS sentence_status_code,
       m.parole_stat_cd AS parole_status_code,
       m.mov_move_code AS movement_code,
-      CAST(mov_seq_num AS INT64) AS sequence_number,
+      -- Sort death statuses after all other entries. No bringing people back to life.
+      IF(m.mov_sent_stat_cd IN ('DA', 'DN', 'DS', 'DX', 'DZ'), 99999, CAST(mov_seq_num AS INT64)) AS sequence_number,
       CASE 
-        -- When the person is on parole, we null out any location that may have ended up in the move_to_loc col
-        WHEN m.mov_move_to_loc = '' OR  m.mov_sent_stat_cd = 'P' THEN NULL 
+        WHEN
+          -- When the person is on parole, we null out any location that may have ended up in the move_to_loc col - we 
+          -- only want to keep incarceration facilities here, but PA uses this column to record the county/state that
+          --  this person is serving parole in as well.
+          m.mov_sent_stat_cd = 'P'
+
+          -- Any deletion row means they are going to a location that is not an incarceration facility
+          OR m.mov_move_code IN ('D', 'DA', 'DIT')
+
+          -- This person is on a bus being transferred between locations - do not use this location. We will count this
+          -- person towards their previous location until they are checked into a new location.
+          OR m.mov_move_to_loc = 'BUS'
+          -- If the person is out on a WRIT/ATA, they may have moved to a county jail temporarily, but we count them as 
+          -- being under the jurisdiction of the PA DOC and their permanent location is tracked as the facility they
+          -- came from - ignore this person's location change for now.
+          OR m.mov_sent_stat_cd = 'WT'
+        THEN NULL 
         ELSE m.mov_move_to_loc END
       AS move_location,
-      m.mov_sent_stat_cd = 'P' AS is_on_parole,
-      m.parole_stat_cd IN ('TPV', 'CPV', 'TCV') AS is_confirmed_parole_violator_parole_status
+      m.mov_move_code IN ('D', 'DA', 'DIT') AS is_delete_movement,
+      m.parole_stat_cd IN ('TPV', 'CPV', 'TCV') AS is_confirmed_parole_violator_parole_status,
   FROM {dbo_Movrec} m
+  LEFT OUTER JOIN
+  -- In 40-ish cases, the control_number in dbo_Movrec does not correspond to the control_number we see for that 
+  -- inmate number in dbo_tblSearchInmateInfo (doesn't show up in dbo_tblSearchInmateInfo at all). We generally
+  -- want to rely on dbo_tblSearchInmateInfo, since that's the file we use to ingest person id links.
+  (SELECT DISTINCT control_number, inmate_number FROM {dbo_tblSearchInmateInfo}) ids
+  ON m.mov_cur_inmt_num = ids.inmate_number
+  WHERE 
+    -- This is the 'Bogus record' flag, if 'Y', indicates if a record should be ignored, 'N' otherwise.
+    m.mov_rec_del_flag = 'N'
 ),
 movements AS (
   SELECT 
     *,
     LAST_VALUE(move_location IGNORE NULLS) OVER (
-        PARTITION BY control_number, is_on_parole
+        PARTITION BY control_number
         ORDER BY sequence_number ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
     ) AS location,
     LAG(is_confirmed_parole_violator_parole_status) OVER (
@@ -55,41 +81,51 @@ movements AS (
     ) AS previous_is_confirmed_parole_violator_parole_status
   FROM movements_base
 ),
-movements_with_location_change_identifier AS (
+movements_with_inflection_indicators AS (
     SELECT 
       *,
-      CASE WHEN sequence_number - previous_sequence_number_within_location = 1 THEN 0 ELSE 1 END AS new_location
+      CASE WHEN sequence_number - previous_sequence_number_within_location = 1 THEN 0 ELSE 1 END AS new_location,
+      (NOT previous_is_confirmed_parole_violator_parole_status 
+        AND is_confirmed_parole_violator_parole_status) AS is_new_revocation,
     FROM (
       SELECT 
         *,
         LAG(sequence_number) OVER (PARTITION BY control_number, location 
-                                   ORDER BY sequence_number) AS previous_sequence_number_within_location
+                                   ORDER BY sequence_number) AS previous_sequence_number_within_location,
+        LAG(is_delete_movement) OVER (PARTITION BY control_number ORDER BY sequence_number) AS prev_is_delete_movement,
       FROM movements 
     )
 ),
 critical_movements AS (
   SELECT
     *,
-    LAG(sequence_number) OVER (
-        PARTITION BY control_number
-        ORDER BY sequence_number
-    ) AS prev_critical_movement_sequence_number,
-    (
-        NOT previous_is_confirmed_parole_violator_parole_status AND is_confirmed_parole_violator_parole_status
-    ) AS is_confirmed_parole_violation_movement
-  FROM movements_with_location_change_identifier
+    LAG(sequence_number) OVER (PARTITION BY control_number
+                               ORDER BY sequence_number) AS prev_critical_movement_sequence_number
+  FROM movements_with_inflection_indicators
   WHERE
     -- This person changed locations or was released to parole - we will create a new period when this happens
     new_location = 1 
     -- This person has transitioned from Parole Violator Pending to actual convicted parole violator
-    OR (NOT previous_is_confirmed_parole_violator_parole_status AND is_confirmed_parole_violator_parole_status)
+    OR is_new_revocation
+    -- This is a release and does not indicate a status change while in the same location
+    OR is_delete_movement
+    -- This record comes after the person has been released - we need to make sure to include this (could be same
+    -- facility as the release row, so new_location = 1 may not indicate that they have changed)
+    OR prev_is_delete_movement
+),
+sentence_types AS (
+  SELECT 
+    curr_inmate_num AS inmate_number,
+    -- It seems there is only ever one sentence per inmate number, so this is just a precaution
+    MAX(type_of_sent) AS sentence_type
+  FROM {dbo_Senrec}
+  GROUP BY curr_inmate_num
 ),
 periods AS (
   SELECT
     start_movement.control_number,
     start_movement.inmate_number,
     start_movement.sequence_number,
-    start_movement.is_on_parole,
     start_movement.move_date AS start_movement_date,
     end_movement.move_date AS end_movement_date,
     start_movement.location,
@@ -99,32 +135,21 @@ periods AS (
     end_movement.parole_status_code AS end_parole_status_code,
     start_movement.movement_code AS start_movement_code,
     end_movement.movement_code AS end_movement_code,
-    'S' AS sentence_type,
-    CASE WHEN start_movement.is_confirmed_parole_violation_movement THEN 1 ELSE 0 END AS is_parole_violation_admission
+    start_movement.is_new_revocation AS start_is_new_revocation,
+    sentence_types.sentence_type,
   FROM 
     critical_movements start_movement
   LEFT OUTER JOIN
     critical_movements end_movement
   ON start_movement.control_number = end_movement.control_number 
     AND end_movement.prev_critical_movement_sequence_number = start_movement.sequence_number
-),
-filtered_periods AS (
-  SELECT * EXCEPT (is_on_parole, is_parole_violation_admission)
-  FROM periods
-  WHERE
-    NOT is_on_parole
-    -- Termination movement codes
-    AND start_movement_code NOT IN ('D', 'DA', 'DIT')
-    -- This person is on a bus being transferred between locations
-    AND location != 'BUS'
-    -- TODO(3447): Is this the right thing to do here? Do we want to include these people in the incarceration count?
-    AND start_sentence_status_code != 'WT'
+  LEFT OUTER JOIN
+    sentence_types
+  ON start_movement.inmate_number = sentence_types.inmate_number 
+  WHERE NOT start_movement.is_delete_movement
 )
--- TODO(3447): This query produces numbers that are off by about 2.5 - should be 47K for 12/31/2017 per this site: 
--- https://nicic.gov/state-statistics/2017/pennsylvania#:~:text=As%20of%20December%2031%2C%202017,and%20budget%20of%20%242.28%20billion.
--- TODO(3447): Investigate why there are a bunch of non-prison location codes in the results    
 SELECT *
-FROM filtered_periods
+FROM periods
 """
 
 VIEW_BUILDER = DirectIngestPreProcessedIngestViewBuilder(
