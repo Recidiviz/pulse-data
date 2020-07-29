73ec346ae0c276904ad7c5fece7239f725cda72d
Julia Dressel <julia@recidiviz.org>
2020-07-28 18:01:12 -0700

    Updating enum mapping for US_ND transfers (recidiviz/recidiviz-data#3660)
    Admission and release enum mapping changes to support transfers out of state
    
    GitOrigin-RevId: 1eb6b35e28d635a525e96166f5307ba4b93edd5d

---
+++ recidiviz/persistence/database/migrations/state/versions/2020_07_23_2305_0aeeb9565367_update_transfer_oos_release_reason.py
@@ -0,0 +1,44 @@
+# pylint: skip-file
+"""update_transfer_oos_release_reason
+
+Revision ID: 0aeeb9565367
+Revises: 5d0aa76119c5
+Create Date: 2020-07-23 23:05:57.383679
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0aeeb9565367'
+down_revision = '5d0aa76119c5'
+branch_labels = None
+depends_on = None
+
+
+RELEASE_REASON_QUERY = "SELECT incarceration_period_id FROM" \
+                       " state_incarceration_period" \
+                       " WHERE state_code = 'US_ND' AND release_reason_raw_text = 'TRN'"
+
+
+UPDATE_QUERY = "UPDATE state_incarceration_period SET release_reason = '{new_value}'" \
+               " WHERE incarceration_period_id IN ({ids_query});"
+
+
+def upgrade():
+    connection = op.get_bind()
+
+    updated_release_reason = 'TRANSFERRED_OUT_OF_STATE'
+
+    connection.execute(UPDATE_QUERY.format(new_value=updated_release_reason,
+                                           ids_query=RELEASE_REASON_QUERY))
+
+
+def downgrade():
+    connection = op.get_bind()
+
+    updated_release_reason = 'TRANSFER'
+
+    connection.execute(UPDATE_QUERY.format(new_value=updated_release_reason,
+                                           ids_query=RELEASE_REASON_QUERY))
