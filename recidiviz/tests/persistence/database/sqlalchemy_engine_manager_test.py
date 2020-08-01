004c44e9e23045a35d70bdf430c4b74baacf3355
Colin Adams <colinadams@google.com>
2020-07-31 19:56:46 -0700

    Allow direct ingest to run simultaneously for multiple states (recidiviz/recidiviz-data#3709)
    * add notes to persistence
    
    * allow multiple states to run simultaneously
    
    * add issue number
    
    * fix test
    
    * pylint
    
    * increase queue 4 -> 50
    
    * use serializable isolation mode for states
    
    * retry persistence transaction on serialization failure
    
    * test
    
    * fix
    
    GitOrigin-RevId: 79f49aab4f42f47974c234c52124efcebc6a4bf0

---
+++ recidiviz/tests/persistence/database/sqlalchemy_engine_manager_test.py
@@ -0,0 +1,67 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for sqlalchemy_engine_manager.py"""
+
+from unittest.case import TestCase
+from mock import call, patch
+
+from recidiviz.persistence.database.sqlalchemy_engine_manager import \
+    SQLAlchemyEngineManager
+
+
+class SQLAlchemyEngineManagerTest(TestCase):
+    """Tests"""
+    def tearDown(self):
+        SQLAlchemyEngineManager.teardown_engines()
+
+    @patch('sqlalchemy.create_engine')
+    @patch('recidiviz.environment.in_gae_production')
+    @patch('recidiviz.environment.in_gae')
+    @patch.object(SQLAlchemyEngineManager, '_get_server_postgres_instance_url', lambda **_: 'path')
+    def testInitEngines_usesCorrectIsolationLevels(self, mock_in_gae, mock_in_production, mock_create_engine):
+        # Arrange
+        mock_in_gae.return_value = True
+        mock_in_production.return_value = True
+
+        # Act
+        SQLAlchemyEngineManager.init_engines_for_server_postgres_instances()
+
+        # Assert
+        assert mock_create_engine.call_args_list == [
+            call('path', isolation_level=None),
+            call('path', isolation_level=None),
+            call('path', isolation_level=None),
+        ]
+
+    @patch('sqlalchemy.create_engine')
+    @patch('recidiviz.environment.in_gae_staging')
+    @patch('recidiviz.environment.in_gae')
+    @patch.object(SQLAlchemyEngineManager, '_get_server_postgres_instance_url', lambda **_: 'path')
+    def testInitEngines_usesCorrectIsolationLevelsInStaging(self, mock_in_gae, mock_in_staging, mock_create_engine):
+        # Arrange
+        mock_in_gae.return_value = True
+        mock_in_staging.return_value = True
+
+        # Act
+        SQLAlchemyEngineManager.init_engines_for_server_postgres_instances()
+
+        # Assert
+        assert mock_create_engine.call_args_list == [
+            call('path', isolation_level=None),
+            call('path', isolation_level='SERIALIZABLE'),
+            call('path', isolation_level=None),
+        ]
