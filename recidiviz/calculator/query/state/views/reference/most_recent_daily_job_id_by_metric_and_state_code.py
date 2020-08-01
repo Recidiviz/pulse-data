840510177a829e6040943f50d11acb3868b778db
Julia Dressel <julia@recidiviz.org>
2020-08-01 14:06:53 -0700

    Sentenced population and other daily pop view updates (Recidiviz/recidiviz-data#3688)
    Primarily, this PR supports the sentence_type_by_district_by_demographics view, which shows the current incarcerated and supervised population broken down by sentence type, judicial district, and demographics. Building this view required some optimization updates so that we aren't over-querying the supervision and incarceration population metrics tables too many times (the daily output in these tables make these queries much more expensive). So, this PR includes three new materialized views most_recent_daily_job_id_by_metric_and_state_code_view, most_recent_daily_incarceration_population and most_recent_daily_supervision_population_view that are now used by all public dashboard views that are showing most recent single day population counts. This means the sentence_type_by_district_by_demographics view will process ~10MB instead of ~100GB. (The most_recent_daily_supervision_population_view is expensive to run, ~100GB, which is why it's happening only once in the materialization).
    
    GitOrigin-RevId: 353f23548023b2741b501648de714188f2f24cf2

---
+++ recidiviz/calculator/query/state/views/reference/most_recent_daily_job_id_by_metric_and_state_code.py
@@ -0,0 +1,67 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Most recent calculate job_id for the most recent metric date for daily metrics, by metric type and state code."""
+# pylint: disable=trailing-whitespace, line-too-long
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.utils.environment import GAE_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+MOST_RECENT_DAILY_JOB_ID_BY_METRIC_AND_STATE_CODE_VIEW_NAME = \
+    'most_recent_daily_job_id_by_metric_and_state_code_view'
+
+MATERIALIZED_VIEW_TABLE_NAME = \
+    'most_recent_daily_job_id_by_metric_and_state_code'
+
+MOST_RECENT_DAILY_JOB_ID_BY_METRIC_AND_STATE_CODE_DESCRIPTION = \
+    """ Job ID of the most recent calculate job for the most recent metric date for daily metrics, by metric and state
+    code.
+
+    All job_ids begin with the format: 'YYYY-MM-DD_HH_MM_SS', so ordering by job_id gives us the most recent job. This
+    format is true of both jobs run locally and run on Dataflow.
+    """
+
+MOST_RECENT_DAILY_JOB_ID_BY_METRIC_AND_STATE_CODE_QUERY_TEMPLATE = \
+    """
+    /*{description}*/
+    WITH all_job_ids AS (
+      (SELECT DISTINCT job_id, state_code, date_of_stay as metric_date, 'INCARCERATION_POPULATION' as metric_type
+      FROM `{project_id}.{metrics_dataset}.incarceration_population_metrics`)
+      UNION ALL
+      (SELECT DISTINCT job_id, state_code, date_of_supervision as metric_date, 'SUPERVISION_POPULATION' as metric_type
+      FROM `{project_id}.{metrics_dataset}.supervision_population_metrics`)
+    ), ranked_job_ids AS (
+      SELECT *, row_number() OVER (PARTITION BY state_code, metric_type ORDER BY metric_date DESC, job_id DESC) AS recency_rank
+      FROM all_job_ids
+    )
+    
+    SELECT * FROM ranked_job_ids WHERE recency_rank = 1
+    ORDER BY metric_type, state_code
+    """
+
+MOST_RECENT_DAILY_JOB_ID_BY_METRIC_AND_STATE_CODE_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=dataset_config.REFERENCE_TABLES_DATASET,
+    view_id=MOST_RECENT_DAILY_JOB_ID_BY_METRIC_AND_STATE_CODE_VIEW_NAME,
+    materialized_view_table_id=MATERIALIZED_VIEW_TABLE_NAME,
+    view_query_template=MOST_RECENT_DAILY_JOB_ID_BY_METRIC_AND_STATE_CODE_QUERY_TEMPLATE,
+    description=MOST_RECENT_DAILY_JOB_ID_BY_METRIC_AND_STATE_CODE_DESCRIPTION,
+    metrics_dataset=dataset_config.DATAFLOW_METRICS_DATASET
+)
+
+if __name__ == '__main__':
+    with local_project_id_override(GAE_PROJECT_STAGING):
+        MOST_RECENT_DAILY_JOB_ID_BY_METRIC_AND_STATE_CODE_VIEW_BUILDER.build_and_print()
---
+++ recidiviz/calculator/query/state/views/reference/most_recent_daily_supervision_population.py
@@ -0,0 +1,79 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Event based supervision population for the most recent date of supervision."""
+# pylint: disable=trailing-whitespace, line-too-long
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.query import bq_utils
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.utils.environment import GAE_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+MOST_RECENT_DAILY_SUPERVISION_POPULATION_VIEW_NAME = \
+    'most_recent_daily_supervision_population_view'
+
+MATERIALIZED_VIEW_TABLE_NAME = 'most_recent_daily_supervision_population'
+
+MOST_RECENT_DAILY_SUPERVISION_POPULATION_DESCRIPTION = \
+    """Event based supervision population for the most recent date of supervision."""
+
+MOST_RECENT_DAILY_SUPERVISION_POPULATION_QUERY_TEMPLATE = \
+    """
+    /*{description}*/
+    WITH most_recent_job_id AS (
+      SELECT
+        state_code,
+        metric_date as date_of_supervision,
+        job_id
+      FROM
+        `{project_id}.{reference_dataset}.most_recent_daily_job_id_by_metric_and_state_code`
+      WHERE metric_type = 'SUPERVISION_POPULATION'
+    )
+    
+    SELECT
+      state_code,
+      person_id,
+      supervision_type,
+      race_or_ethnicity,
+      IFNULL(gender, 'EXTERNAL_UNKNOWN') as gender,
+      IFNULL(age_bucket, 'EXTERNAL_UNKNOWN') as age_bucket,
+      IFNULL(judicial_district_code, 'EXTERNAL_UNKNOWN') as judicial_district_code,
+      IFNULL(supervising_district_external_id, 'EXTERNAL_UNKNOWN') as supervising_district_external_id,
+      date_of_supervision
+    FROM
+       `{project_id}.{metrics_dataset}.supervision_population_metrics`
+    INNER JOIN
+        most_recent_job_id
+    USING (state_code, job_id, date_of_supervision),
+    {race_or_ethnicity_dimension}
+    WHERE metric_period_months = 0
+    AND methodology = 'EVENT'
+    """
+
+MOST_RECENT_DAILY_SUPERVISION_POPULATION_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=dataset_config.REFERENCE_TABLES_DATASET,
+    view_id=MOST_RECENT_DAILY_SUPERVISION_POPULATION_VIEW_NAME,
+    materialized_view_table_id=MATERIALIZED_VIEW_TABLE_NAME,
+    view_query_template=MOST_RECENT_DAILY_SUPERVISION_POPULATION_QUERY_TEMPLATE,
+    description=MOST_RECENT_DAILY_SUPERVISION_POPULATION_DESCRIPTION,
+    metrics_dataset=dataset_config.DATAFLOW_METRICS_DATASET,
+    reference_dataset=dataset_config.REFERENCE_TABLES_DATASET,
+    race_or_ethnicity_dimension=bq_utils.unnest_race_and_ethnicity()
+)
+
+if __name__ == '__main__':
+    with local_project_id_override(GAE_PROJECT_STAGING):
+        MOST_RECENT_DAILY_SUPERVISION_POPULATION_VIEW_BUILDER.build_and_print()
