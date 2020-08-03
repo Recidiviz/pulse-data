9885033cf091a41dbbc22ff431cb8d0e1f357497
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-08-02 19:05:29 -0700

    [ND Public Dashboard] Internal consistency validation for tables with mutually-exclusive breakdowns (Recidiviz/recidiviz-data#3741)
     Internal consistency validation for tables with mutually-exclusive breakdowns
    
    GitOrigin-RevId: fea75c59e9f0fd48d15fd6b8158b6488885dd52b

---
+++ recidiviz/validation/views/state/incarceration_population_by_facility_by_demographics_internal_consistency.py
@@ -0,0 +1,63 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""A view that can be used in validation to ensure internal consistency across breakdowns in the
+incarceration_population_by_facility_by_demographics view.
+"""
+
+# pylint: disable=trailing-whitespace
+
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.query.state import dataset_config as state_dataset_config
+from recidiviz.calculator.query.state.views.public_dashboard.incarceration.\
+    incarceration_population_by_facility_by_demographics import \
+    INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_VIEW_NAME
+from recidiviz.utils.environment import GAE_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+from recidiviz.validation.views import dataset_config
+from recidiviz.validation.views.utils.internal_consistency_templates import internal_consistency_query
+
+INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_INTERNAL_CONSISTENCY_VIEW_NAME = \
+    'incarceration_population_by_facility_by_demographics_internal_consistency'
+
+INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_INTERNAL_CONSISTENCY_DESCRIPTION = \
+    """ Builds validation table to ensure internal consistency across breakdowns in the 
+ incarceration_population_by_facility_by_demographics view."""
+
+
+PARTITION_COLUMNS = ['state_code', 'date_of_stay', 'facility']
+CALCULATED_COLUMNS_TO_VALIDATE = ['total_population']
+MUTUALLY_EXCLUSIVE_BREAKDOWN_COLUMNS = ['age_bucket', 'race_or_ethnicity', 'gender']
+
+INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_INTERNAL_CONSISTENCY_QUERY_TEMPLATE = f"""
+/*{{description}}*/
+{internal_consistency_query(partition_columns=PARTITION_COLUMNS,
+                            mutually_exclusive_breakdown_columns=MUTUALLY_EXCLUSIVE_BREAKDOWN_COLUMNS,
+                            calculated_columns_to_validate=CALCULATED_COLUMNS_TO_VALIDATE)}
+"""
+
+INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_INTERNAL_CONSISTENCY_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=dataset_config.VIEWS_DATASET,
+    view_id=INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_INTERNAL_CONSISTENCY_VIEW_NAME,
+    view_query_template=INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_INTERNAL_CONSISTENCY_QUERY_TEMPLATE,
+    description=INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_INTERNAL_CONSISTENCY_DESCRIPTION,
+    validated_table_dataset_id=state_dataset_config.PUBLIC_DASHBOARD_VIEWS_DATASET,
+    validated_table_id=INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_VIEW_NAME
+)
+
+if __name__ == '__main__':
+    with local_project_id_override(GAE_PROJECT_STAGING):
+        INCARCERATION_POPULATION_BY_FACILITY_BY_DEMOGRAPHICS_INTERNAL_CONSISTENCY_VIEW_BUILDER.build_and_print()
