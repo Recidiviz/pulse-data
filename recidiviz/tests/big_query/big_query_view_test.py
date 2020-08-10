7ce326389f9ebc00aa67836a9fda70130182bed3
Julia Dressel <julia@recidiviz.org>
2020-08-10 12:25:01 -0700

    Throwing if raw project_id strings are in templates (Recidiviz/recidiviz-data#3783)
    * Adds a new validation step to the creation of a BigQueryView that asserts none of our project_id values are in the view's view_query_template.
    
    GitOrigin-RevId: 66f164ce1faeb61d48c5fb17a94b14d92f76cb14

--- recidiviz/tests/big_query/big_query_view_test.py
+++ recidiviz/tests/big_query/big_query_view_test.py
@@ -17,6 +17,7 @@
 """Tests for BigQueryView"""
 import unittest
 
+import pytest
 from mock import patch
 
 from recidiviz.big_query.big_query_view import BigQueryView
@@ -25,7 +26,7 @@ from recidiviz.big_query.big_query_view import BigQueryView
 class BigQueryViewTest(unittest.TestCase):
     """Tests for BigQueryView"""
 
-    PROJECT_ID = 'project-id'
+    PROJECT_ID = 'recidiviz-project-id'
 
     def setUp(self) -> None:
         self.metadata_patcher = patch('recidiviz.utils.metadata.project_id')
@@ -46,8 +47,8 @@ class BigQueryViewTest(unittest.TestCase):
         self.assertEqual('view_dataset', view.dataset_id)
         self.assertEqual('my_view', view.table_id)
         self.assertEqual('my_view', view.view_id)
-        self.assertEqual('SELECT * FROM `project-id.some_dataset.table`', view.view_query)
-        self.assertEqual('SELECT * FROM `project-id.view_dataset.my_view`', view.select_query)
+        self.assertEqual(f"SELECT * FROM `{self.PROJECT_ID}.some_dataset.table`", view.view_query)
+        self.assertEqual(f"SELECT * FROM `{self.PROJECT_ID}.view_dataset.my_view`", view.select_query)
 
     def test_simple_view_overwrite_project_id(self):
         view = BigQueryView(
@@ -64,6 +65,16 @@ class BigQueryViewTest(unittest.TestCase):
         self.assertEqual('SELECT * FROM `other-project.some_dataset.table`', view.view_query)
         self.assertEqual('SELECT * FROM `other-project.view_dataset.my_view`', view.select_query)
 
+    @patch('recidiviz.big_query.big_query_view.GAE_PROJECTS', [PROJECT_ID])
+    def test_simple_view_invalid_raw_project_id(self):
+        with pytest.raises(ValueError):
+            _ = BigQueryView(
+                project_id='other-project',
+                dataset_id='view_dataset',
+                view_id='my_view',
+                view_query_template=f"SELECT * FROM `{self.PROJECT_ID}.some_dataset.table`"
+            )
+
     def test_extra_format_args(self):
         view = BigQueryView(
             dataset_id='view_dataset',
@@ -78,8 +89,8 @@ class BigQueryViewTest(unittest.TestCase):
         self.assertEqual('view_dataset', view.dataset_id)
         self.assertEqual('my_view', view.table_id)
         self.assertEqual('my_view', view.view_id)
-        self.assertEqual('SELECT name, date FROM `project-id.a_dataset.table`', view.view_query)
-        self.assertEqual('SELECT * FROM `project-id.view_dataset.my_view`', view.select_query)
+        self.assertEqual(f"SELECT name, date FROM `{self.PROJECT_ID}.a_dataset.table`", view.view_query)
+        self.assertEqual(f"SELECT * FROM `{self.PROJECT_ID}.view_dataset.my_view`", view.select_query)
 
     def test_missing_format_arg_throws_on_instantiation(self):
         with self.assertRaises(KeyError):
