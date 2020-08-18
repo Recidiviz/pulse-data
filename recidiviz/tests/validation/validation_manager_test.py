50e078d7619b1bf107b5c78f9fbbce4d3d9bb34b
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-08-18 10:30:01 -0700

    [Validation] Several small validation fixes (Recidiviz/recidiviz-data#3857)
    This change contains several small fixes that I'm making on the path to adding PA external validation data to BigQuery.
    
    This PR:
    * Updates the (apparently broken) `po_report_missing_fields` to remove a column that has been removed from the underlying source view
    * Updates the validation manager to track outright job failures separately from validation errors and log metrics for those job failures
    * Update the validation manager to only run validations on states that are launched in a particular region - allowing us to add external validation data for PA to the prod tables without tripping any alerts.
    * Updates some of the tests to pass when run with unittest (aka make them runnable in Pycharm)
    * Removes the hack in BigQueryView that allowed test code to not have properly mocked a project id
    
    Work towards Recidiviz/recidiviz-data#3690
    
    GitOrigin-RevId: 477c6728c4b0393da390da3ae696e4e8a7bbb9fe

--- recidiviz/tests/validation/validation_manager_test.py
+++ recidiviz/tests/validation/validation_manager_test.py
@@ -25,38 +25,41 @@ from mock import patch
 
 from recidiviz.big_query.big_query_view import BigQueryView
 from recidiviz.tests.utils.matchers import UnorderedCollection
+from recidiviz.utils.environment import GaeEnvironment
 from recidiviz.validation.checks.existence_check import ExistenceDataValidationCheck
-from recidiviz.validation.configured_validations import get_all_validations, STATES_TO_VALIDATE
+from recidiviz.validation.configured_validations import get_all_validations, get_state_codes_to_validate
 from recidiviz.validation.validation_manager import validation_manager_blueprint, _fetch_validation_jobs_to_perform
 from recidiviz.validation.validation_models import DataValidationJob, DataValidationJobResult
 from recidiviz.validation.views import view_config
 
-_TEST_VALIDATIONS: List[DataValidationJob] = [
-    DataValidationJob(region_code='US_UT',
-                      validation=ExistenceDataValidationCheck(
-                          view=BigQueryView(dataset_id='my_dataset',
-                                            view_id='test_1',
-                                            view_query_template='select * from literally_anything')
-                      )),
-    DataValidationJob(region_code='US_UT',
-                      validation=ExistenceDataValidationCheck(
-                          view=BigQueryView(dataset_id='my_dataset',
-                                            view_id='test_2',
-                                            view_query_template='select * from literally_anything')
-                      )),
-    DataValidationJob(region_code='US_VA',
-                      validation=ExistenceDataValidationCheck(
-                          view=BigQueryView(dataset_id='my_dataset',
-                                            view_id='test_1',
-                                            view_query_template='select * from literally_anything')
-                      )),
-    DataValidationJob(region_code='US_VA',
-                      validation=ExistenceDataValidationCheck(
-                          view=BigQueryView(dataset_id='my_dataset',
-                                            view_id='test_2',
-                                            view_query_template='select * from literally_anything')
-                      )),
-]
+
+def get_test_validations() -> List[DataValidationJob]:
+    return [
+        DataValidationJob(region_code='US_UT',
+                          validation=ExistenceDataValidationCheck(
+                              view=BigQueryView(dataset_id='my_dataset',
+                                                view_id='test_1',
+                                                view_query_template='select * from literally_anything')
+                          )),
+        DataValidationJob(region_code='US_UT',
+                          validation=ExistenceDataValidationCheck(
+                              view=BigQueryView(dataset_id='my_dataset',
+                                                view_id='test_2',
+                                                view_query_template='select * from literally_anything')
+                          )),
+        DataValidationJob(region_code='US_VA',
+                          validation=ExistenceDataValidationCheck(
+                              view=BigQueryView(dataset_id='my_dataset',
+                                                view_id='test_1',
+                                                view_query_template='select * from literally_anything')
+                          )),
+        DataValidationJob(region_code='US_VA',
+                          validation=ExistenceDataValidationCheck(
+                              view=BigQueryView(dataset_id='my_dataset',
+                                                view_id='test_2',
+                                                view_query_template='select * from literally_anything')
+                          )),
+    ]
 
 
 _API_RESPONSE_IF_NO_FAILURES = "Failed validations:\n"
@@ -66,18 +69,27 @@ class TestHandleRequest(TestCase):
     """Tests for the Validation Manager API endpoint."""
 
     def setUp(self) -> None:
+        self.metadata_patcher = patch('recidiviz.utils.metadata.project_id')
+        self.mock_project_id_fn = self.metadata_patcher.start()
+        self.mock_project_id_fn.return_value = 'recidiviz-456'
+
         app = Flask(__name__)
         app.register_blueprint(validation_manager_blueprint)
         app.config['TESTING'] = True
         self.client = app.test_client()
 
+        self._TEST_VALIDATIONS = get_test_validations()
+
+    def tearDown(self):
+        self.metadata_patcher.stop()
+
     @patch("recidiviz.validation.validation_manager._emit_failures")
     @patch("recidiviz.validation.validation_manager._run_job")
     @patch("recidiviz.validation.validation_manager._fetch_validation_jobs_to_perform")
     def test_handle_request_happy_path_no_failures(self, mock_fetch_validations, mock_run_job, mock_emit_failures):
-        mock_fetch_validations.return_value = _TEST_VALIDATIONS
+        mock_fetch_validations.return_value = self._TEST_VALIDATIONS
         mock_run_job.return_value = DataValidationJobResult(
-            validation_job=_TEST_VALIDATIONS[0], was_successful=True, failure_description=None)
+            validation_job=self._TEST_VALIDATIONS[0], was_successful=True, failure_description=None)
 
         headers = {'X-Appengine-Cron': 'test-cron'}
         response = self.client.get('/validate', headers=headers)
@@ -86,28 +98,58 @@ class TestHandleRequest(TestCase):
         self.assertEqual(_API_RESPONSE_IF_NO_FAILURES, response.get_data().decode())
 
         self.assertEqual(4, mock_run_job.call_count)
-        for job in _TEST_VALIDATIONS:
+        for job in self._TEST_VALIDATIONS:
             mock_run_job.assert_any_call(job)
 
-        mock_emit_failures.assert_called_with([])
+        mock_emit_failures.assert_not_called()
+
+    @patch("recidiviz.validation.validation_manager._emit_failures")
+    @patch("recidiviz.validation.validation_manager._run_job")
+    @patch("recidiviz.validation.validation_manager._fetch_validation_jobs_to_perform")
+    def test_handle_request_with_job_failures_and_validation_failures(
+            self, mock_fetch_validations, mock_run_job, mock_emit_failures):
+        mock_fetch_validations.return_value = self._TEST_VALIDATIONS
+        first_failure = DataValidationJobResult(
+            validation_job=self._TEST_VALIDATIONS[1], was_successful=False, failure_description='Oh no')
+        second_failure = DataValidationJobResult(
+            validation_job=self._TEST_VALIDATIONS[2], was_successful=False, failure_description='How awful')
+        mock_run_job.side_effect = [
+            DataValidationJobResult(
+                validation_job=self._TEST_VALIDATIONS[0], was_successful=True, failure_description=None),
+            first_failure,
+            second_failure,
+            ValueError('Job failed to run!')
+        ]
+        headers = {'X-Appengine-Cron': 'test-cron'}
+        response = self.client.get('/validate', headers=headers)
+
+        self.assertEqual(200, response.status_code)
+        self.assertNotEqual(_API_RESPONSE_IF_NO_FAILURES, response.get_data().decode())
+
+        self.assertEqual(4, mock_run_job.call_count)
+        for job in self._TEST_VALIDATIONS:
+            mock_run_job.assert_any_call(job)
+
+        mock_emit_failures.assert_called_with(UnorderedCollection([self._TEST_VALIDATIONS[3]]),
+                                              UnorderedCollection([first_failure, second_failure]))
 
     @patch("recidiviz.validation.validation_manager._emit_failures")
     @patch("recidiviz.validation.validation_manager._run_job")
     @patch("recidiviz.validation.validation_manager._fetch_validation_jobs_to_perform")
     def test_handle_request_happy_path_some_failures(self, mock_fetch_validations, mock_run_job, mock_emit_failures):
-        mock_fetch_validations.return_value = _TEST_VALIDATIONS
+        mock_fetch_validations.return_value = self._TEST_VALIDATIONS
 
         first_failure = DataValidationJobResult(
-            validation_job=_TEST_VALIDATIONS[1], was_successful=False, failure_description='Oh no')
+            validation_job=self._TEST_VALIDATIONS[1], was_successful=False, failure_description='Oh no')
         second_failure = DataValidationJobResult(
-            validation_job=_TEST_VALIDATIONS[2], was_successful=False, failure_description='How awful')
+            validation_job=self._TEST_VALIDATIONS[2], was_successful=False, failure_description='How awful')
         mock_run_job.side_effect = [
             DataValidationJobResult(
-                validation_job=_TEST_VALIDATIONS[0], was_successful=True, failure_description=None),
+                validation_job=self._TEST_VALIDATIONS[0], was_successful=True, failure_description=None),
             first_failure,
             second_failure,
             DataValidationJobResult(
-                validation_job=_TEST_VALIDATIONS[3], was_successful=True, failure_description=None),
+                validation_job=self._TEST_VALIDATIONS[3], was_successful=True, failure_description=None),
         ]
 
         headers = {'X-Appengine-Cron': 'test-cron'}
@@ -117,10 +159,10 @@ class TestHandleRequest(TestCase):
         self.assertNotEqual(_API_RESPONSE_IF_NO_FAILURES, response.get_data().decode())
 
         self.assertEqual(4, mock_run_job.call_count)
-        for job in _TEST_VALIDATIONS:
+        for job in self._TEST_VALIDATIONS:
             mock_run_job.assert_any_call(job)
 
-        mock_emit_failures.assert_called_with(UnorderedCollection([first_failure, second_failure]))
+        mock_emit_failures.assert_called_with([], UnorderedCollection([first_failure, second_failure]))
 
     @patch("recidiviz.validation.validation_manager._emit_failures")
     @patch("recidiviz.validation.validation_manager._run_job")
@@ -138,7 +180,7 @@ class TestHandleRequest(TestCase):
         self.assertEqual(_API_RESPONSE_IF_NO_FAILURES, response.get_data().decode())
 
         mock_run_job.assert_not_called()
-        mock_emit_failures.assert_called_with([])
+        mock_emit_failures.assert_not_called()
 
     @patch("recidiviz.big_query.view_update_manager.create_dataset_and_update_views_for_view_builders")
     @patch("recidiviz.validation.validation_manager._emit_failures")
@@ -158,17 +200,44 @@ class TestHandleRequest(TestCase):
         self.assertEqual(_API_RESPONSE_IF_NO_FAILURES, response.get_data().decode())
 
         mock_run_job.assert_not_called()
-        mock_emit_failures.assert_called_with([])
+        mock_emit_failures.assert_not_called()
         mock_update_views.assert_called_with(view_config.VIEW_BUILDERS_FOR_VIEWS_TO_UPDATE)
 
 
 class TestFetchValidations(TestCase):
     """Tests the _fetch_validation_jobs_to_perform function."""
 
-    def test_cross_product_states_and_checks(self):
+    def setUp(self) -> None:
+        self.metadata_patcher = patch('recidiviz.utils.metadata.project_id')
+        self.mock_project_id_fn = self.metadata_patcher.start()
+        self.mock_project_id_fn.return_value = 'recidiviz-456'
+
+    def tearDown(self):
+        self.metadata_patcher.stop()
+
+    @patch("recidiviz.utils.environment.get_gae_environment", return_value=GaeEnvironment.STAGING.value)
+    def test_cross_product_states_and_checks_staging(self, _mock_get_environment):
         all_validations = get_all_validations()
-        all_states = STATES_TO_VALIDATE
+        all_states = get_state_codes_to_validate()
         expected_length = len(all_validations) * len(all_states)
+
+        result = _fetch_validation_jobs_to_perform()
+        self.assertEqual(expected_length, len(result))
+
+    @patch("recidiviz.utils.environment.get_gae_environment", return_value=GaeEnvironment.PRODUCTION.value)
+    def test_cross_product_states_and_checks_production(self, _mock_get_environment):
+        all_validations = get_all_validations()
+        state_codes_to_validate = get_state_codes_to_validate()
+
+        # When you promote a state to production, we will start running validations against that state - add it to this
+        # list to confirm you've updated all relevant external data validation tables in production to include
+        # validation data for the newly promoted region.
+        self.assertEqual(state_codes_to_validate, [
+            'US_MO',
+            'US_ND'
+        ])
+
+        expected_length = len(all_validations) * len(state_codes_to_validate)
 
         result = _fetch_validation_jobs_to_perform()
         self.assertEqual(expected_length, len(result))
