a58c3fc443cce1a0e01b1fe7c8cc8bef8ab8b51a
Julia Dressel <julia@recidiviz.org>
2020-08-18 10:16:31 -0700

    Adding prompts to prod deploy (Recidiviz/recidiviz-data#3858)
    GitOrigin-RevId: 9d627bc2a77699a4ab66c8bcd74ad10163323381

--- recidiviz/tools/deploy/deploy_production.sh
+++ recidiviz/tools/deploy/deploy_production.sh
@@ -8,6 +8,19 @@ then
     exit 1
 fi
 
+GIT_VERSION_TAG=$(echo $1 | tr '-' '.')
+LAST_DEPLOYED_GIT_VERSION_TAG=$(gcloud app versions list --project=recidiviz-123 --hide-no-traffic --service=default --format=yaml | yq .id | tr -d \" | tr '-' '.')
+
+echo "Commits since last deploy:"
+git log --oneline $LAST_DEPLOYED_GIT_VERSION_TAG..$GIT_VERSION_TAG
+
+read -p "Have you completed all Pre-Deploy tasks listed at http://go/deploy-checklist? (y/n):" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
 echo "Fetching all tags"
 git fetch --all --tags --prune
 
@@ -26,10 +39,17 @@ pipenv run python -m recidiviz.calculator.calculation_data_storage_manager --pro
 echo "Deploying prod-ready calculation pipelines to templates in recidiviz-123."
 pipenv run python -m recidiviz.tools.deploy.deploy_pipeline_templates --project_id recidiviz-123 --templates_to_deploy production
 
-VERSION=$(echo $1 | tr '.' '-')
-STAGING_IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.$VERSION:latest
-PROD_IMAGE_URL=us.gcr.io/recidiviz-123/appengine/default.$VERSION:latest
+GAE_VERSION=$(echo $1 | tr '.' '-')
+STAGING_IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.GAE_VERSION:latest
+PROD_IMAGE_URL=us.gcr.io/recidiviz-123/appengine/default.GAE_VERSION:latest
 
 echo "Starting deploy of main app"
 gcloud -q container images add-tag $STAGING_IMAGE_URL $PROD_IMAGE_URL
-gcloud -q app deploy prod.yaml --project=recidiviz-123 --version=$VERSION --image-url=$PROD_IMAGE_URL
+gcloud -q app deploy prod.yaml --project=recidiviz-123 --version=GAE_VERSION --image-url=$PROD_IMAGE_URL
+
+read -p "Have you completed all Post-Deploy tasks listed at http://go/deploy-checklist? (y/n):" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
