67069207ae7778212ff16e7ca7720452d4d8593d
Julia Dressel <julia@recidiviz.org>
2020-08-26 17:42:47 -0700

    Validation and copy_bq_views script fixes and updates (recidiviz/recidiviz-data#3890)
    * Fixes the copy_bq_views script, which was no-longer working after the update to not allow raw project_id strings in view BigQueryView view queries. This will now change the project_id values in all of the views in the source dataset to the --destination_project_id, which is super helpful when copying views between staging and prod.
    * Fixes the case_termination_by_type_comparison validation view that was referencing views from the incorrect dataset.
    * Adds an non_mutually_exclusive_breakdown_columns argument to the internal_consistency_query validation helper. This allows for views to have breakdown categories like age or race where a person can contribute to multiple categories.
    * Adds the age_bucket breakdown as a non_mutually_exclusive_breakdown_column for the supervision_success_by_period_by_demographics_internal_consistency validation, since we have decided that counting someone towards multiple age groups as they age during the course of a 36month time period is the desired behavior.
    
    GitOrigin-RevId: 34b79e2e9ed39fc13eefc6dfb06a135f200673bc

--- recidiviz/tools/copy_bq_views.py
+++ recidiviz/tools/copy_bq_views.py
@@ -14,19 +14,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""
-Script for copying all views from one dataset to another in BigQuery.
+"""Script for copying all views from one dataset to another in BigQuery.
 
 Example usage (run from `pipenv shell`):
 
 python -m recidiviz.tools.copy_bq_views \
-    --source-project-id recidiviz-123 \
-    --source-dataset-id my-dataset
-    --destination-project-id recidiviz-staging
-    --destination-dataset-id new-dataset
+    --source_project_id recidiviz-123 \
+    --source_dataset_id my-dataset
+    --destination_project_id recidiviz-staging
+    --destination_dataset_id new-dataset
 """
 import argparse
 import logging
+import sys
 
 from google.cloud import bigquery
 
@@ -34,7 +34,10 @@ from recidiviz.big_query.big_query_client import BigQueryClientImpl
 from recidiviz.big_query.big_query_view import BigQueryView
 
 
-def main(*, source_project_id, source_dataset_id, destination_project_id, destination_dataset_id):
+def copy_bq_views(source_project_id: str,
+                  source_dataset_id: str,
+                  destination_project_id: str,
+                  destination_dataset_id: str):
     """Copies all views from the source_project_id.source_dataset_id to the
     destination_project_id.destination_dataset_id."""
 
@@ -43,39 +46,57 @@ def main(*, source_project_id, source_dataset_id, destination_project_id, destin
 
     # Construct a BigQuery client with the destination_project_id
     destination_client = BigQueryClientImpl(project_id=destination_project_id)
-
     destination_dataset = bigquery.DatasetReference(destination_project_id, destination_dataset_id)
-
     tables_in_source_dataset = source_client.list_tables(source_dataset_id)
 
     for table_ref in tables_in_source_dataset:
         table = source_client.get_table(source_client.dataset_ref_for_id(table_ref.dataset_id),
                                         table_ref.table_id)
+        view_query = table.view_query
 
         # Only copy this view if there is a view_query to replicate and the view doesn't already exist in the
         # destination dataset
-        if table.view_query and not destination_client.table_exists(destination_dataset, table_id=table.table_id):
+        if view_query and not destination_client.table_exists(destination_dataset, table_id=table.table_id):
+            # Remove any references to the source_project_id from the view_query
+            updated_view_query = view_query.replace(source_project_id, '{project_id}')
+
             # Retrieve all of the information about the view
-            source_client.copy_view(view=BigQueryView(dataset_id=table_ref.dataset_id,
+            source_client.copy_view(view=BigQueryView(project_id=destination_project_id,
+                                                      dataset_id=destination_dataset_id,
                                                       view_id=table.table_id,
-                                                      view_query_template=table.view_query),
+                                                      view_query_template=updated_view_query),
                                     destination_client=destination_client,
                                     destination_dataset_ref=destination_dataset)
 
 
+def parse_arguments(argv):
+    """Parses the required arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--source_project_id',
+                        required=True,
+                        help='The project_id where the existing views live')
+
+    parser.add_argument('--source_dataset_id',
+                        required=True,
+                        help='The name of the dataset to copy.')
+
+    parser.add_argument('--destination_project_id',
+                        required=True,
+                        help='The project_id where the new views should live. Can be the same as source_project_id.')
+
+    parser.add_argument('--destination_dataset_id',
+                        required=True,
+                        help='The dataset where the new views should live.')
+
+    return parser.parse_known_args(argv)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    logging.getLogger().setLevel(logging.INFO)
+    known_args, _ = parse_arguments(sys.argv)
 
-    parser.add_argument('--source-project-id', required=True, help='The project_id where the existing views live')
-
-    parser.add_argument('--source-dataset-id', required=True, help='The name of the dataset to copy.')
-
-    parser.add_argument('--destination-project-id', required=True,
-                        help='The project_id where the new views should live. Can be the same as source-project-id.')
-
-    parser.add_argument('--destination-dataset-id', required=True, help='The dataset where the new views should live.')
-
-    args = parser.parse_args()
-    logging.basicConfig(level=logging.INFO, format='%(message)s')
-    main(source_project_id=args.source_project_id, source_dataset_id=args.source_dataset_id,
-         destination_project_id=args.destination_project_id, destination_dataset_id=args.destination_dataset_id)
+    copy_bq_views(source_project_id=known_args.source_project_id,
+                  source_dataset_id=known_args.source_dataset_id,
+                  destination_project_id=known_args.destination_project_id,
+                  destination_dataset_id=known_args.destination_dataset_id)
