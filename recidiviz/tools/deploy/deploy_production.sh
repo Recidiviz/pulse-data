60b84e2d2517d1f181a998523fb5767b4d593474
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-09-03 09:37:14 -0700

    [Deploy] Lots of deploy script unification and safety checks (Recidiviz/recidiviz-data#3989)
    This PR does a fair amount of consolidation of how we a) handle script output and b) handle failures in scripts.
    
    Main changes:
    * Renamed `run_command.sh` to `script_base.sh` - this should be imported for all command-line scripts
    * Set several global settings in `script_base.sh` so that we fail if any command in a pipeline of commands fails and so we fail on unset variables.
    * Updated the `run_cmd` function to take in a list of command args that it can run directly, rather than interpreting a string as a command. This allows IDEs to catch errors more easily.
    * The `run_cmd` function now actually properly indents output with a blue `> ` (before that part of the command failed since we weren't properly importing the `ind` function).
    * Broke exiting on failure functionality into a separate `exit_on_fail` function which we can use wherever we use a command to assign a value to a variable. There was no mechanism before for exiting the script when these commands failed.
    * Added new `script_prompt` utility for command line prompting of yes/no questions
    
    * Changes specifically to `deploy_production.sh`
        * Updated to use `run_cmd` for all commands
        * Updated to use `script_prompt` for prompts
        * Added usage printout on usage error
        * Enforced that git tag for deploy matches vX.X.X pattern and does not contain `-alpha` (Recidiviz/recidiviz-data#3967)
        * Enforced that deployed tag is greater than last deployed tag
        * Enforced that git status is completely clean before checking out the tag so we don't actually push bad state to cron.yaml etc.
        * Small style changes recommended by IDE
    
    * Changes specifically to `deploy_local_to_staging.sh`
       * Unquoted all commands passed to `run_cmd`
       * Small style changes recommended by IDE
    
    * Changes specifically to `deploy_staging.sh` and `export_commits.sh`
        * Import `script_base.sh` instead of `travis/base.sh`. Functionally, this means we do not have `set -v` and `set -e` options set - instead we rely on `run_cmd` and `echo` statements to handle exiting and printing progress, respectively.
        * Update to use `run_cmd` and `echo` statements in a number of places
        * Small style changes recommended by IDE
    
    Closes Recidiviz/recidiviz-data#3891.
    Closes Recidiviz/recidiviz-data#3967.
    
    GitOrigin-RevId: e987efccc898134dcce52c751279661975d135d5

--- recidiviz/tools/deploy/deploy_production.sh
+++ recidiviz/tools/deploy/deploy_production.sh
@@ -1,59 +1,74 @@
 #!/usr/bin/env bash
-BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
+#
+# Script for deploying tagged versions to production. Must be run within the pipenv shell.
+#
 
-read -p "Have you run any new migrations added since the last release for all prod DBs (jails, state, operations)? (y/n):" -n 1 -r
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+if [[ x"$1" == x ]]; then
+    echo "usage: $0 <version_tag>"
     exit 1
 fi
 
-GIT_VERSION_TAG=$(echo $1 | tr '-' '.')
-LAST_DEPLOYED_GIT_VERSION_TAG=$(gcloud app versions list --project=recidiviz-123 --hide-no-traffic --service=default --format=yaml | yq .id | tr -d \" | tr '-' '.')
+BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
+source ${BASH_SOURCE_DIR}/../script_base.sh
+
+GIT_VERSION_TAG=$(echo $1 | tr '-' '.') || exit_on_fail
+LAST_DEPLOYED_GIT_VERSION_TAG=$(gcloud app versions list --project=recidiviz-123 --hide-no-traffic --service=default --format=yaml | yq .id | tr -d \" | tr '-' '.') || exit_on_fail
+
+if [[ ! ${GIT_VERSION_TAG} =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Invalid release version [$GIT_VERSION_TAG] - must match regex: v[0-9]+\.[0-9]+\.[0-9]+"
+    exit 1
+fi
+
+if ! version_less_than ${LAST_DEPLOYED_GIT_VERSION_TAG} ${GIT_VERSION_TAG}; then
+    echo "Deploy version [$GIT_VERSION_TAG] must be greater than last deployed tag [$LAST_DEPLOYED_GIT_VERSION_TAG]."
+    exit 1
+fi
+
+echo "Beginning deploy of version [$GIT_VERSION_TAG] to production. Last deployed version: [$LAST_DEPLOYED_GIT_VERSION_TAG]."
+script_prompt "Do you want to continue?"
+
+script_prompt "Have you run any new migrations added since the last release for all prod DBs (jails, state, operations)\
+ or were there no new migrations to run?"
 
 echo "Commits since last deploy:"
-git log --oneline tags/$LAST_DEPLOYED_GIT_VERSION_TAG..tags/$GIT_VERSION_TAG
+run_cmd git log --oneline tags/${LAST_DEPLOYED_GIT_VERSION_TAG}..tags/${GIT_VERSION_TAG}
 
-read -p "Have you completed all Pre-Deploy tasks listed at http://go/deploy-checklist? (y/n):" -n 1 -r
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+script_prompt "Have you completed all Pre-Deploy tasks listed at http://go/deploy-checklist?"
+
+echo "Fetching all tags"
+run_cmd git fetch --all --tags --prune
+
+echo "Checking for clean git status"
+if [[ ! -z "$(git status --porcelain)" ]]; then
+    echo "Git status not clean - please commit or stash changes before retrying."
     exit 1
 fi
 
-echo "Fetching all tags"
-git fetch --all --tags --prune
-
-echo "Checking out tag $GIT_VERSION_TAG"
-if ! git checkout tags/$GIT_VERSION_TAG -b $GIT_VERSION_TAG
+echo "Checking out tag [$GIT_VERSION_TAG]"
+if ! git checkout tags/${GIT_VERSION_TAG} -b ${GIT_VERSION_TAG}
 then
     echo "Attempting to reuse existing branch $GIT_VERSION_TAG"
-    git checkout $GIT_VERSION_TAG
+    run_cmd git checkout ${GIT_VERSION_TAG}
 fi
 
 echo "Starting deploy of cron.yaml"
-gcloud -q app deploy cron.yaml --project=recidiviz-123
+run_cmd gcloud -q app deploy cron.yaml --project=recidiviz-123
 
 echo "Starting task queue initialization"
-pipenv run python -m recidiviz.tools.initialize_google_cloud_task_queues --project_id recidiviz-123 --google_auth_token $(gcloud auth print-access-token)
+run_cmd python -m recidiviz.tools.initialize_google_cloud_task_queues --project_id recidiviz-123 --google_auth_token $(gcloud auth print-access-token)
 
 echo "Updating the BigQuery Dataflow metric table schemas to match the metric classes"
-pipenv run python -m recidiviz.calculator.calculation_data_storage_manager --project_id recidiviz-123 --function_to_execute update_schemas
+run_cmd python -m recidiviz.calculator.calculation_data_storage_manager --project_id recidiviz-123 --function_to_execute update_schemas
 
 echo "Deploying prod-ready calculation pipelines to templates in recidiviz-123."
-pipenv run python -m recidiviz.tools.deploy.deploy_pipeline_templates --project_id recidiviz-123 --templates_to_deploy production
+run_cmd python -m recidiviz.tools.deploy.deploy_pipeline_templates --project_id recidiviz-123 --templates_to_deploy production
 
-GAE_VERSION=$(echo $GIT_VERSION_TAG | tr '.' '-')
-STAGING_IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.$GAE_VERSION:latest
-PROD_IMAGE_URL=us.gcr.io/recidiviz-123/appengine/default.$GAE_VERSION:latest
+GAE_VERSION=$(echo ${GIT_VERSION_TAG} | tr '.' '-') || exit_on_fail
+STAGING_IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.${GAE_VERSION}:latest || exit_on_fail
+PROD_IMAGE_URL=us.gcr.io/recidiviz-123/appengine/default.${GAE_VERSION}:latest || exit_on_fail
 
 echo "Starting deploy of main app"
-gcloud -q container images add-tag $STAGING_IMAGE_URL $PROD_IMAGE_URL
-gcloud -q app deploy prod.yaml --project=recidiviz-123 --version=$GAE_VERSION --image-url=$PROD_IMAGE_URL
+run_cmd gcloud -q container images add-tag ${STAGING_IMAGE_URL} ${PROD_IMAGE_URL}
+run_cmd gcloud -q app deploy prod.yaml --project=recidiviz-123 --version=${GAE_VERSION} --image-url=${PROD_IMAGE_URL}
 
-read -p "Have you completed all Post-Deploy tasks listed at http://go/deploy-checklist? (y/n):" -n 1 -r
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
+script_prompt "Have you completed all Post-Deploy tasks listed at http://go/deploy-checklist?"
--- travis/deploy_staging.sh
+++ travis/deploy_staging.sh
@@ -1,30 +1,37 @@
-source travis/base.sh
+#!/usr/bin/env bash
+source recidiviz/tools/script_base.sh
 
-# Create encryption keys and authenticate with GAE
-openssl aes-256-cbc -K $encrypted_7ad4439b42dc_key -iv $encrypted_7ad4439b42dc_iv -in client-secret-staging.json.enc -out client-secret-staging.json -d 2>&1 | ind
-gcloud -q auth activate-service-account recidiviz-staging@appspot.gserviceaccount.com --key-file client-secret-staging.json 2>&1 | ind
+echo "Creating encryption keys"
+run_cmd openssl aes-256-cbc -K ${encrypted_7ad4439b42dc_key} -iv ${encrypted_7ad4439b42dc_iv} -in client-secret-staging.json.enc -out client-secret-staging.json -d
 
-# Deploy cron.yaml
-gcloud -q app deploy cron.yaml --project=recidiviz-staging 2>&1 | ind
+echo "Authenticating wtih GAE"
+run_cmd gcloud -q auth activate-service-account recidiviz-staging@appspot.gserviceaccount.com --key-file client-secret-staging.json
 
-# Initialize task queues
+echo "Deploying cron.yaml"
+run_cmd gcloud -q app deploy cron.yaml --project=recidiviz-staging
+
+echo "Initializing task queues"
 # TODO(3369): If we figure out how to properly authenticate the docker environment, we shouldn't have to pass an auth token here
-docker exec -it recidiviz pipenv run python -m recidiviz.tools.initialize_google_cloud_task_queues --project_id recidiviz-staging --google_auth_token $(gcloud auth print-access-token) 2>&1 | ind
+run_cmd docker exec -it recidiviz pipenv run python -m recidiviz.tools.initialize_google_cloud_task_queues --project_id recidiviz-staging --google_auth_token $(gcloud auth print-access-token)
 
 # App engine doesn't allow '.' in the version name
-VERSION=$(echo $TRAVIS_TAG | tr '.' '-')
-echo $VERSION 2>&1 | ind
+VERSION=$(echo ${TRAVIS_TAG} | tr '.' '-') || exit_on_fail
+
+echo "Deploying version [$VERSION]"
 
 # TODO(3369): Move this earlier so we can authenticate python commands properly?
-# Authorize docker to acess GCR
-# use instead of 'auth configure-docker' as travis has an old gcloud
-gcloud -q docker --authorize-only 2>&1 | ind
+echo "Authorizing docker to access GCR"
+# Use this command instead of 'auth configure-docker' as travis has an old gcloud
+run_cmd gcloud -q docker --authorize-only
 
 # Push the docker image to GCR
-IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.$VERSION:latest
-echo $IMAGE_URL 2>&1 | ind
-docker tag recidiviz-image $IMAGE_URL 2>&1 | ind
-docker push $IMAGE_URL 2>&1 | ind
+IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.${VERSION}:latest || exit_on_fail
 
-# Deploy application
-gcloud -q app deploy staging.yaml --project recidiviz-staging --version $VERSION --image-url $IMAGE_URL 2>&1 | ind
+echo "Tagging image url [$IMAGE_URL] as recidiviz-image"
+run_cmd docker tag recidiviz-image ${IMAGE_URL}
+
+echo "Pushing image url [$IMAGE_URL]"
+run_cmd docker push ${IMAGE_URL}
+
+echo "Deploying application"
+run_cmd gcloud -q app deploy staging.yaml --project recidiviz-staging --version ${VERSION} --image-url ${IMAGE_URL}
