d4b49926539d4e3d338a4a37fc41b31e17942101
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-09-03 09:37:14 -0700

    [Deploy] Lots of deploy script unification and safety checks (recidiviz/recidiviz-data#3989)
    This PR does a fair amount of consolidation of how we a) handle script output and b) handle failures in scripts.
    
    Main changes:
    * Renamed `run_command.sh` to `script_base.sh` - this should be imported for all command-line scripts
    * Set several global settings in `script_base.sh` so that we fail if any command in a pipeline of commands fails and so we fail on unset variables.
    * Updated the `run_cmd` function to take in a list of command args that it can run directly, rather than interpreting a string as a command. This allows IDEs to catch errors more easily.
    * The `run_cmd` function now actually properly indents output with a blue `> ` (before that part of the command failed since we weren't properly importing the `ind` function).
    * Broke exiting on failure functionality into a separate `exit_on_fail` function which we can use wherever we use a command to assign a value to a variable. There was no mechanism before for exiting the script when these commands failed.
    * Added new `script_prompt` utility for command line prompting of yes/no questions
    
    * Changes specifically to `deploy_production.sh`
        * Updated to use `run_cmd` for all commands
        * Updated to use `script_prompt` for prompts
        * Added usage printout on usage error
        * Enforced that git tag for deploy matches vX.X.X pattern and does not contain `-alpha` (recidiviz/recidiviz-data#3967)
        * Enforced that deployed tag is greater than last deployed tag
        * Enforced that git status is completely clean before checking out the tag so we don't actually push bad state to cron.yaml etc.
        * Small style changes recommended by IDE
    
    * Changes specifically to `deploy_local_to_staging.sh`
       * Unquoted all commands passed to `run_cmd`
       * Small style changes recommended by IDE
    
    * Changes specifically to `deploy_staging.sh` and `export_commits.sh`
        * Import `script_base.sh` instead of `travis/base.sh`. Functionally, this means we do not have `set -v` and `set -e` options set - instead we rely on `run_cmd` and `echo` statements to handle exiting and printing progress, respectively.
        * Update to use `run_cmd` and `echo` statements in a number of places
        * Small style changes recommended by IDE
    
    Closes recidiviz/recidiviz-data#3891.
    Closes recidiviz/recidiviz-data#3967.
    
    GitOrigin-RevId: 2a2221c346d896003894ef0ced00d978e8e2cb70

--- recidiviz/tools/deploy/deploy_local_to_staging.sh
+++ recidiviz/tools/deploy/deploy_local_to_staging.sh
@@ -1,43 +1,42 @@
 #!/usr/bin/env bash
 
-BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
-
-source ${BASH_SOURCE_DIR}/../run_commands.sh
-
 deploy_name=$1
 version_tag=$(echo $2 | tr '.' '-')
 
-if [ x"$deploy_name" == x -o x"$version_tag" == x ]; then
+if [[ x"$deploy_name" == x || x"$version_tag" == x ]]; then
     echo "usage: $0 <deploy_name> <version_tag>"
     exit 1
 fi
 
+BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
+source ${BASH_SOURCE_DIR}/../script_base.sh
+
 echo "Updating the BigQuery Dataflow metric table schemas to match the metric classes"
-run_cmd "pipenv run python -m recidiviz.calculator.calculation_data_storage_manager --project_id recidiviz-staging --function_to_execute update_schemas"
+run_cmd pipenv run python -m recidiviz.calculator.calculation_data_storage_manager --project_id recidiviz-staging --function_to_execute update_schemas
 
 echo "Deploying stage-only calculation pipelines to templates in recidiviz-staging."
-run_cmd "pipenv run python -m recidiviz.tools.deploy.deploy_pipeline_templates --project_id recidiviz-staging --templates_to_deploy staging"
+run_cmd pipenv run python -m recidiviz.tools.deploy.deploy_pipeline_templates --project_id recidiviz-staging --templates_to_deploy staging
 
 echo "Deploying prod-ready calculation pipelines to templates in recidiviz-staging."
-run_cmd "pipenv run python -m recidiviz.tools.deploy.deploy_pipeline_templates --project_id recidiviz-staging --templates_to_deploy production"
+run_cmd pipenv run python -m recidiviz.tools.deploy.deploy_pipeline_templates --project_id recidiviz-staging --templates_to_deploy production
 
 echo "Initializing task queues"
-run_cmd "pipenv run python -m recidiviz.tools.initialize_google_cloud_task_queues --project_id recidiviz-staging --google_auth_token $(gcloud auth print-access-token)"
+run_cmd pipenv run python -m recidiviz.tools.initialize_google_cloud_task_queues --project_id recidiviz-staging --google_auth_token $(gcloud auth print-access-token)
 
 echo "Building docker image"
-run_cmd "docker build -t recidiviz-image ."
+run_cmd docker build -t recidiviz-image .
 
 echo "Tagging release"
-run_cmd "docker tag recidiviz-image us.gcr.io/recidiviz-staging/appengine/$deploy_name"
+run_cmd docker tag recidiviz-image us.gcr.io/recidiviz-staging/appengine/${deploy_name}
 
 echo "Pushing image"
-run_cmd "docker push us.gcr.io/recidiviz-staging/appengine/$deploy_name"
+run_cmd docker push us.gcr.io/recidiviz-staging/appengine/${deploy_name}
 
 echo "Running deploy"
-run_cmd "gcloud -q app deploy --no-promote staging.yaml
+run_cmd gcloud -q app deploy --no-promote staging.yaml
        --project recidiviz-staging
-       --version $version_tag-$deploy_name
-       --image-url us.gcr.io/recidiviz-staging/appengine/$deploy_name
-       --verbosity=debug"
+       --version ${version_tag}-${deploy_name}
+       --image-url us.gcr.io/recidiviz-staging/appengine/${deploy_name}
+       --verbosity=debug
 
 echo "App deployed (but not promoted) to \`$version_tag-$deploy_name\`.recidiviz-staging.appspot.com"
