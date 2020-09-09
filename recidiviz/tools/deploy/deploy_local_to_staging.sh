16a1232228c566423f53cfcf512880f00c0295f4
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-09-08 21:14:43 -0700

    [Deploy] Safety updates to ./deploy_local_to_staging.sh (Recidiviz/recidiviz-data#3992)
    Updates the ./deploy_local_to_staging.sh script to do the following:
    a) Enforce that the current branch at HEAD contains the most recently pushed version tag and prompt the user to rebase if this is not the case.
    b) Derive the version tag number directly from the latest version tag. This will update the scheme to use the NEXT alpha version, rather than the previous version, which may not be an alpha version at all.
    c) Enforces that only version name arg is passed in now.
    
    Closes Recidiviz/recidiviz-data#3972
    
    GitOrigin-RevId: ff7f134d12c6126f8baca74cb66d544e5aacdfa7

--- recidiviz/tools/deploy/deploy_local_to_staging.sh
+++ recidiviz/tools/deploy/deploy_local_to_staging.sh
@@ -1,15 +1,45 @@
 #!/usr/bin/env bash
 
-deploy_name=$1
-version_tag=$(echo $2 | tr '.' '-')
+BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
+source ${BASH_SOURCE_DIR}/../script_base.sh
+source ${BASH_SOURCE_DIR}/deploy_helpers.sh
 
-if [[ x"$deploy_name" == x || x"$version_tag" == x ]]; then
-    echo "usage: $0 <deploy_name> <version_tag>"
+DEBUG_BUILD_NAME=''
+
+function print_usage {
+    echo_error "usage: $0 -d DEBUG_BUILD_NAME"
+    echo_error "  -d: Name to append to the version for a debug local deploy (e.g. anna-test1)."
+    exit 1
+}
+
+while getopts "d:" flag; do
+  case "${flag}" in
+    d) DEBUG_BUILD_NAME="$OPTARG" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+if [[ -z ${DEBUG_BUILD_NAME} ]]; then
+    print_usage
     exit 1
 fi
 
 BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
 source ${BASH_SOURCE_DIR}/../script_base.sh
 
+echo "Fetching all tags"
+run_cmd git fetch --all --tags --prune
+
+LAST_VERSION_TAG_ON_CURRENT_BRANCH=$(last_version_tag_on_branch HEAD)
+LAST_VERSION_TAG_ON_MASTER=$(last_version_tag_on_branch master)
+
+if [[ ${LAST_VERSION_TAG_ON_CURRENT_BRANCH} != ${LAST_VERSION_TAG_ON_MASTER} ]]; then
+    echo_error "Current branch does not contain latest version tag on master [$LAST_VERSION_TAG_ON_MASTER] - please rebase."
+    exit 1
+fi
+
+VERSION_TAG=$(next_alpha_version ${LAST_VERSION_TAG_ON_MASTER}) || exit_on_fail
+
 # Deploys a debug version to staging without promoting traffic to it
-${BASH_SOURCE_DIR}/base_deploy_to_staging.sh -v ${version_tag} -d ${deploy_name} -n
+${BASH_SOURCE_DIR}/base_deploy_to_staging.sh -v ${VERSION_TAG} -d ${DEBUG_BUILD_NAME} -n
