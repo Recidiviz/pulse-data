93b724d5ac74bf9a491daa5fab614eb59950fd95
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-09-08 19:55:05 -0700

    [Deploy] Refactor deploy scripts for new release process (recidiviz/recidiviz-data#3991)
    Implements all deploy script changes necessary to support new deploy / cherry-pick process.
    
    Major changes:
    * Disabled entirely the ability to trigger a deploy by creating a tag in Github in `.travis.yml`
    * Accordingly, deleted the `deploy_staging.sh` script that runs in Travis, which will be replaced by the new local `deploy_alpha_to_staging.sh` script.
    * Wrote `deploy_alpha_to_staging.sh` which, notably, *chooses the next appropriate alpha version for the user*.
    * Implemented basic permissions checking via `verify_deploy_permissions` function. Added to all scripts except `deploy_local_to_staging.sh` (will be easy to add later if we want).
    * Consolidated the core pieces of staging deploy scripts into a single `base_deploy_staging.sh` script.
    * Moves all functionality related to deploying cron.yaml, pipeline templates, etc to a single helper function `pre_deploy_configure_infrastructure` - this means that we will finally start updating pipeline templates with every staging deploy.
    * Adds new `cut_release_candidate.sh` script which
        * Determines next release tag based on whether we're cutting a release from master or a release branch (i.e. a cherry-pick
        * Creates and pushes a new tag from the tip of the branch to release from (master or releases/*)
        * Deploys the new version to staging
        * If not a cherry-pick version, creates a new releases/* branch to be used in case we need to do a cherry-pick later
    
    Closes recidiviz/recidiviz-data#3968
    Closes recidiviz/recidiviz-data#3971
    Closes recidiviz/recidiviz-data#3369 (bonus!)
    
    GitOrigin-RevId: e7efe5c2fd8ffb6a93e7c46bd79eca694c2dde9e

---
+++ recidiviz/tools/deploy/base_deploy_to_staging.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+#
+# Base script for deploying a new version to staging, whether it be a cherry-pick, a standard release, or a new alpha
+# version.
+#
+
+BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
+source ${BASH_SOURCE_DIR}/../script_base.sh
+source ${BASH_SOURCE_DIR}/deploy_helpers.sh
+
+VERSION_TAG=''
+DEBUG_BUILD_NAME=''
+PROMOTE=''
+NO_PROMOTE=''
+PROMOTE_FLAGS=''
+
+function print_usage {
+    echo_error "usage: $0 -v VERSION [-p -n -d DEBUG_BUILD_NAME]"
+    echo_error "  -v: Version tag to deploy (e.g. v1.2.0)"
+    echo_error "  -p: Indicates that we should promote traffic to the newly deployed version. Can not be used with -n."
+    echo_error "  -n: Indicates that we should not promote traffic to the newly deployed version. Can not be used with -p."
+    echo_error "  -d: Name to append to the version for a debug local deploy (e.g. anna-test1)."
+    exit 1
+}
+
+while getopts "v:pnd:" flag; do
+  case "${flag}" in
+    v) VERSION_TAG=$(echo $OPTARG | tr '.' '-') ;;
+    p) PROMOTE_FLAGS='--promote' PROMOTE='true';;
+    n) PROMOTE_FLAGS='--no-promote' NO_PROMOTE='true';;
+    d) DEBUG_BUILD_NAME="$OPTARG" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+if [[ -z ${VERSION_TAG} ]]; then
+    echo_error "Missing/empty version tag argument"
+    print_usage
+    exit 1
+fi
+
+if [[ (! -z ${PROMOTE} && ! -z ${NO_PROMOTE}) ||  ( -z ${PROMOTE} && -z ${NO_PROMOTE}) ]]; then
+    echo_error "Must pass exactly one of either -p (promote) or -n (no-promote) flags"
+    print_usage
+    exit 1
+fi
+
+if [[ ! -z ${PROMOTE} && ! -z ${DEBUG_BUILD_NAME} ]]; then
+    echo_error "Debug releases must only have  -n (no-promote) option."
+    print_usage
+    exit 1
+fi
+
+if [[ ! -z ${PROMOTE} || ! -z ${DEBUG_BUILD_NAME} ]]; then
+    pre_deploy_configure_infrastructure 'recidiviz-staging'
+else
+    echo "Skipping configuration and pipeline deploy steps for no promote release build."
+fi
+
+echo "Building docker image"
+run_cmd docker build -t recidiviz-image .
+
+if [[ ! -z ${DEBUG_BUILD_NAME} ]]; then
+    VERSION=${VERSION_TAG}-${DEBUG_BUILD_NAME}
+else
+    VERSION=${VERSION_TAG}
+fi
+
+IMAGE_URL=us.gcr.io/recidiviz-staging/appengine/default.${VERSION}:latest || exit_on_fail
+
+echo "Tagging image url [$IMAGE_URL] as recidiviz-image"
+run_cmd docker tag recidiviz-image ${IMAGE_URL}
+
+echo "Pushing image url [$IMAGE_URL]"
+run_cmd docker push ${IMAGE_URL}
+
+echo "Deploying application"
+run_cmd gcloud -q app deploy ${PROMOTE_FLAGS} staging.yaml \
+       --project recidiviz-staging \
+       --version ${VERSION} \
+       --image-url ${IMAGE_URL} \
+       --verbosity=debug
+
+if [[ ! -z ${PROMOTE} ]]; then
+    echo "App deployed to \`${VERSION}\`.recidiviz-staging.appspot.com"
+else
+    echo "App deployed (but not promoted) to \`${VERSION}\`.recidiviz-staging.appspot.com"
+fi
---
+++ recidiviz/tools/deploy/deploy_helpers.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
+source ${BASH_SOURCE_DIR}/../script_base.sh
+
+VERSION_REGEX="^v([0-9]+)\.([0-9]+)\.([0-9]+)(-alpha.([0-9]+))?$"
+
+# Returns the last version tag on the given branch. Fails if that tag does not match the acceptable version regex.
+function last_version_tag_on_branch {
+    BRANCH=$1
+
+    LAST_VERSION_TAG_ON_BRANCH=$(git tag --sort=-v:refname --merged ${BRANCH} | head -n 1) || exit_on_fail
+
+    # Check that the version parses
+    _=$(parse_version ${LAST_VERSION_TAG_ON_BRANCH}) || exit_on_fail
+
+    echo ${LAST_VERSION_TAG_ON_BRANCH}
+}
+
+# Parses a version tag and output a space-separated string of the version regex capture groups.
+# Example usage:
+#    $ VERSION_PARTS=($(parse_version v1.2.0-alpha.0))
+#    $ echo "MAJOR VERSION NUMBER: ${VERSION_PARTS[1]}"
+function parse_version {
+    VERSION=$1
+    if [[ ! ${VERSION} =~ ${VERSION_REGEX} ]]
+    then
+        echo_error "Expected a version tag matching regex $VERSION_REGEX. Instead found [$VERSION]."
+        exit 1
+    fi
+
+    echo ${BASH_REMATCH[@]}
+}
+
+function next_alpha_version {
+    PREVIOUS_VERSION=$1
+    PREVIOUS_VERSION_PARTS=($(parse_version ${PREVIOUS_VERSION})) || exit_on_fail
+
+    MAJOR=${PREVIOUS_VERSION_PARTS[1]}
+    MINOR=${PREVIOUS_VERSION_PARTS[2]}
+    PATCH=${PREVIOUS_VERSION_PARTS[3]}
+    ALPHA=${PREVIOUS_VERSION_PARTS[4]-}  # Optional
+    ALPHA_VERSION=${PREVIOUS_VERSION_PARTS[5]-}  # Optional
+
+    if [[ -z ${ALPHA} ]]; then
+        # If the previous version was a release version, bump the minor version and build a fresh alpha version
+        NEW_VERSION="v$MAJOR.$(($MINOR + 1)).0-alpha.0"
+    else
+        # If the previous version was an alpha version, just increment alpha version
+        NEW_VERSION="v$MAJOR.$MINOR.$PATCH-alpha.$(($ALPHA_VERSION + 1))"
+    fi
+
+    echo ${NEW_VERSION}
+}
+
+function verify_deploy_permissions {
+    # TODO(3996): Actually check Github for Owner-level repo permissions
+
+    GIT_USER_EMAIL="$(git config user.email)"
+    EMAIL_PATTERN=".*\@recidiviz\.org$"
+
+    if [[ ! ${GIT_USER_EMAIL} =~ ${EMAIL_PATTERN} ]]
+    then
+        echo_error "User [$GIT_USER_EMAIL] does not have sufficient permissions to deploy."
+        echo_error "Please reach out to Recidiviz administrators if you need to deploy."
+        exit 1
+    fi
+}
+
+# Prints an error and exits if the provided commit does not have a green build on Travis.
+function check_commit_is_green {
+    COMMIT=$1
+
+    # This returns an SVG with an element that will have the text "passing" if the build is passing.
+    URL="https://api.travis-ci.com/Recidiviz/pulse-data.svg?token=pa7kG645RqXUvoHE2g9n&commit=$COMMIT"
+    TRAVIS_BUILD_STATUS_SVG=$(curl ${URL})
+
+    PASSING_PATTERN=">passing<"
+
+    if [[ ! ${TRAVIS_BUILD_STATUS_SVG} =~ ${PASSING_PATTERN} ]]
+    then
+        echo_error "Commit [$COMMIT] is not passing on Travis. You must wait for a green build to deploy."
+        exit 1
+    fi
+
+    echo "Build is passing for commit [$COMMIT]."
+}
+
+# Helper for deploying any infrastructure changes before we deploy a new version of the application. Requires that we
+# have checked out the commit for the version that will be deployed.
+function pre_deploy_configure_infrastructure {
+    PROJECT=$1
+
+    echo "Deploying cron.yaml"
+    run_cmd gcloud -q app deploy cron.yaml --project=${PROJECT}
+
+    echo "Updating the BigQuery Dataflow metric table schemas to match the metric classes"
+    run_cmd pipenv run python -m recidiviz.calculator.calculation_data_storage_manager --project_id ${PROJECT} --function_to_execute update_schemas
+
+    if [[ ${PROJECT} == 'recidiviz-staging' ]]; then
+        echo "Deploying stage-only calculation pipelines to templates in ${PROJECT}."
+        run_cmd pipenv run python -m recidiviz.tools.deploy.deploy_pipeline_templates --project_id ${PROJECT} --templates_to_deploy staging
+    fi
+
+    echo "Deploying prod-ready calculation pipelines to templates in ${PROJECT}."
+    run_cmd pipenv run python -m recidiviz.tools.deploy.deploy_pipeline_templates --project_id ${PROJECT} --templates_to_deploy production
+
+    echo "Initializing task queues"
+    run_cmd pipenv run python -m recidiviz.tools.initialize_google_cloud_task_queues --project_id ${PROJECT} --google_auth_token $(gcloud auth print-access-token)
+}
