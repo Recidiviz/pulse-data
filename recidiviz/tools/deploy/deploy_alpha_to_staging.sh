362465578ccfd9dd2255cccc66855b1d79baf05c
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-09-08 19:55:05 -0700

    [Deploy] Refactor deploy scripts for new release process (Recidiviz/recidiviz-data#3991)
    Implements all deploy script changes necessary to support new deploy / cherry-pick process.
    
    Major changes:
    * Disabled entirely the ability to trigger a deploy by creating a tag in Github in `.travis.yml`
    * Accordingly, deleted the `deploy_staging.sh` script that runs in Travis, which will be replaced by the new local `deploy_alpha_to_staging.sh` script.
    * Wrote `deploy_alpha_to_staging.sh` which, notably, *chooses the next appropriate alpha version for the user*.
    * Implemented basic permissions checking via `verify_deploy_permissions` function. Added to all scripts except `deploy_local_to_staging.sh` (will be easy to add later if we want).
    * Consolidated the core pieces of staging deploy scripts into a single `base_deploy_staging.sh` script.
    * Moves all functionality related to deploying cron.yaml, pipeline templates, etc to a single helper function `pre_deploy_configure_infrastructure` - this means that we will finally start updating pipeline templates with every staging deploy.
    * Adds new `cut_release_candidate.sh` script which
        * Determines next release tag based on whether we're cutting a release from master or a release branch (i.e. a cherry-pick
        * Creates and pushes a new tag from the tip of the branch to release from (master or releases/*)
        * Deploys the new version to staging
        * If not a cherry-pick version, creates a new releases/* branch to be used in case we need to do a cherry-pick later
    
    Closes Recidiviz/recidiviz-data#3968
    Closes Recidiviz/recidiviz-data#3971
    Closes Recidiviz/recidiviz-data#3369 (bonus!)
    
    GitOrigin-RevId: 910b8f8859b814451508c3586bcee70f5efb1253

---
+++ recidiviz/tools/deploy/deploy_alpha_to_staging.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#
+# Script for deploying an alpha version to stage. A version deployed with this script will not be deployed to
+# production.
+#
+
+BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
+source ${BASH_SOURCE_DIR}/../script_base.sh
+
+echo "Verifying deploy permissions"
+run_cmd verify_deploy_permissions
+
+echo "Fetching all tags"
+run_cmd git fetch --all --tags --prune
+
+echo "Checking for existing tags at tip of master"
+check_for_tags_at_branch_tip master
+
+run_cmd safe_git_checkout_branch master
+
+LAST_VERSION_TAG_ON_MASTER=$(last_version_tag_on_branch master) || exit_on_fail
+NEW_VERSION=$(next_alpha_version ${LAST_VERSION_TAG_ON_MASTER}) || exit_on_fail
+
+script_prompt "Will create tag and deploy version [$NEW_VERSION] at commit [$(git rev-parse HEAD)] which is the \
+tip of branch [master]. Continue?"
+
+echo "Creating local tag ${NEW_VERSION}"
+run_cmd git tag -m "Version $NEW_VERSION release - $(date +'%Y-%m-%d %H:%M:%S')" ${NEW_VERSION}
+
+echo "Pushing tags to remote"
+run_cmd git push origin --tags
+
+${BASH_SOURCE_DIR}/base_deploy_to_staging.sh -v ${NEW_VERSION} -p
--- recidiviz/tools/script_base.sh
+++ recidiviz/tools/script_base.sh
@@ -4,14 +4,14 @@
 BLUE_OUTPUT_ESCAPE_CODE=$(tput setaf 4)
 
 # Get the escape sequence to reset from the current color to whatever the default is
-RESET_OUTPUT_COLOR_ESCAPE_CODE=$(tput sgr0)
+RESET_OUTPUT_COLOR_ESCAPE_CODE=$(tput -T ansi sgr0)
 
 # Prepends a blue '> ' to each line of output piped to this function.
 #
 # In order to use this function, append `2>&1 | indent_output` to all commands so that all output goes to stdout and
 # gets run through this function.
 function indent_output {
-    sed "s/^/$BLUE_OUTPUT_ESCAPE_CODE>$RESET_OUTPUT_COLOR_ESCAPE_CODE /"
+    sed "s/^/$BLUE_OUTPUT_ESCAPE_CODE> $RESET_OUTPUT_COLOR_ESCAPE_CODE /"
 }
 
 # Fails the command if any command in a pipeline fails. For example: `a | b | c` will fail if any of commands a, b
@@ -23,21 +23,32 @@ set -u
 
 ACCEPTABLE_RETURN_CODES=(0)
 
+# Prints a message to stderr, pre-pending the file number and line number of the caller.
+function echo_error {
+    CALLER_INFO=($(caller))
+    CALLER_LINE_NUMBER=${CALLER_INFO[0]}
+    CALLER_FILE=${CALLER_INFO[1]}
+    ECHO_ARGS=$@
+    echo "[$CALLER_FILE:$CALLER_LINE_NUMBER] $ECHO_ARGS" 1>&2;
+}
+
 # Checks the return code of the most recent command and prints a message then exits with that return code if it is
 # not in the array of acceptable codes. Should be used whenever a command is captured into a variable:
 #   MY_VAR=$(some command) || exit_on_fail
 function exit_on_fail {
-
     # Gets the return code from the previous pipeline of commands (will be non-zero if any fail).
     ret_code=$?
     # Check if return code is in set of acceptable return codes
     if [[ ! " ${ACCEPTABLE_RETURN_CODES[@]} " =~ " ${ret_code} " ]]; then
-        echo "Failed with exit code $ret_code"
+        echo "Failed with exit code $ret_code" 1>&2
         exit ${ret_code}
     fi
 }
 
 # All script commands should be run via `run_cmd`, which handles output formatting and exiting on error.
+#
+# NOTE: This should be used with extreme caution when using pipes (|) or file descriptor redirection (> or >>) as the
+# order of operations can be tricky to get right.
 function run_cmd {
     # Runs the full array of arguments passed to `run_cmd` as a command, piping output to stdout so we can indent it.
     $@ 2>&1 | indent_output
@@ -70,4 +81,39 @@ function sort_versions {
 function version_less_than {
     min_version=$(echo -e "$1\n$2" | sort_versions | head -n1) || exit_on_fail
     [[ "$1" = "$2" ]] && return 1 || [[  "$1" = "$min_version" ]]
+}
+
+function safe_git_checkout_branch {
+    BRANCH=$1
+    echo "Checking for clean git status"
+    if [[ ! -z "$(git status --porcelain)" ]]; then
+        echo_error "Git status not clean - please commit or stash changes before retrying."
+        exit 1
+    fi
+
+    echo "Checking out [$BRANCH]"
+    if ! git checkout -t origin/${BRANCH}
+    then
+        echo "Branch [$BRANCH] already exists - reusing"
+        run_cmd git checkout ${BRANCH}
+        echo "Pulling latest from [$BRANCH]"
+        run_cmd git pull
+    fi
+}
+
+function check_for_tags_at_branch_tip {
+    BRANCH=$1
+    ALLOW_ALPHA=${2-}  # Optional argument
+    if [[ ! -z ${ALLOW_ALPHA} ]]; then
+        TAGS_AT_TIP_OF_BRANCH=$(git tag --points-at ${BRANCH} | grep -v alpha) || exit_on_fail
+    else
+        TAGS_AT_TIP_OF_BRANCH=$(git tag --points-at ${BRANCH}) || exit_on_fail
+    fi
+
+    if [[ ! -z ${TAGS_AT_TIP_OF_BRANCH} ]]; then
+        echo_error "The tip of branch [$BRANCH] is already tagged - exiting."
+        echo_error "Tags: ${TAGS_AT_TIP_OF_BRANCH}"
+        exit 1
+    fi
+
 }
