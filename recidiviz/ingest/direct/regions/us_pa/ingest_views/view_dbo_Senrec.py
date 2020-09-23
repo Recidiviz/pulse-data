e2c08e02e24ca88fcf30d6c57fe8c490565fb539
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-09-23 10:16:57 -0700

    [PA ingest] Fix comment syntax error in dbo_Senrec view query (Recidiviz/recidiviz-data#4142)
    Somehow I committed a syntax error to this query in July and we haven't tried to use it until now... I will need to cherry-pick this into v1.8 to get the PA ingest run going.
    
    Work towards Recidiviz/recidiviz-data#4039
    
    GitOrigin-RevId: 2591398e80710c99c59d7c72b5ae99fc2a3f8fd8

--- recidiviz/ingest/direct/regions/us_pa/ingest_views/view_dbo_Senrec.py
+++ recidiviz/ingest/direct/regions/us_pa/ingest_views/view_dbo_Senrec.py
@@ -25,7 +25,7 @@ VIEW_QUERY_TEMPLATE = """SELECT ids.control_number AS control_number, sentences.
 FROM 
     {dbo_Senrec} sentences
 -- As of 2020-06-10, there are only 6 sentences with no control numbers - we omit these since there's not way for us to
-link to a person.
+-- link to a person.
 JOIN
     (SELECT DISTINCT control_number, inmate_number FROM {dbo_tblSearchInmateInfo}) ids
 ON ids.inmate_number = sentences.curr_inmate_num
