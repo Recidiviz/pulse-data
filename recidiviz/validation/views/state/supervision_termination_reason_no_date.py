4ed5a991f9f57b0dac61389df6da0216ae09949b
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-09-24 19:22:38 -0700

    [PA ingest] Fixes to PA supervision_period query to handle raw data updates (Recidiviz/recidiviz-data#4162)
    Fixes two kinda gross bugs with the supervision_periods ingest view query for PA.
    
    1) When people are no longer on supervision anymore, their info gets moved from  `dbo_Release` to `dbo_Hist_Release`. Our query on these tables was written originally on the assumption that someone will only ever exist in one table for a given parole_count_id. HOWEVER, since our raw data tables are append-only, if a person was on parole in June when we got our initial historical dump, then moved off supervision before the refresh, we will see rows for this person both in `dbo_Release` and `dbo_Hist_Release`. We want to pick the row that's in the history table over the one in the active table, since that has the final info.
    
    2) I was setting the termination reason to 'TRANSFER_WITHIN_STATE' on un-terminated supervision periods.
    
    Includes validations for Recidiviz/recidiviz-data#2 and the parallel issue on incarceration periods.
    
    Work towards Recidiviz/recidiviz-data#4039.
    
    GitOrigin-RevId: f442db2002341123f7345d253c17266bee324121

---
+++ recidiviz/validation/views/state/supervision_termination_reason_no_date.py
@@ -0,0 +1,51 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""A view revealing when state supervision periods have termination reasons but no date."""
+
+# pylint: disable=trailing-whitespace
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.query.state import dataset_config as state_dataset_config
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+from recidiviz.validation.views import dataset_config
+
+SUPERVISION_TERMINATION_REASON_NO_DATE_VIEW_NAME = 'supervision_termination_reason_no_date'
+
+SUPERVISION_TERMINATION_REASON_NO_DATE_DESCRIPTION = \
+    """Supervision periods with termination reasons but no termination date."""
+
+SUPERVISION_TERMINATION_REASON_NO_DATE_QUERY_TEMPLATE = \
+    """
+    /*{description}*/
+    SELECT *, state_code as region_code
+    FROM `{project_id}.{state_dataset}.state_supervision_period`
+    WHERE termination_date IS NULL
+    AND termination_reason IS NOT NULL
+    ORDER BY region_code, termination_reason, external_id
+"""
+
+SUPERVISION_TERMINATION_REASON_NO_DATE_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=dataset_config.VIEWS_DATASET,
+    view_id=SUPERVISION_TERMINATION_REASON_NO_DATE_VIEW_NAME,
+    view_query_template=SUPERVISION_TERMINATION_REASON_NO_DATE_QUERY_TEMPLATE,
+    description=SUPERVISION_TERMINATION_REASON_NO_DATE_DESCRIPTION,
+    state_dataset=state_dataset_config.STATE_BASE_DATASET,
+)
+
+if __name__ == '__main__':
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        SUPERVISION_TERMINATION_REASON_NO_DATE_VIEW_BUILDER.build_and_print()
