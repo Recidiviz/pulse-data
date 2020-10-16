863736654526e8cc12db63255cd009931738fb3e
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-10-16 13:40:36 -0700

    [Dataflow] Pin dill version loaded on workers to match Pipfile version (Recidiviz/recidiviz-data#4322)
    The issue: we had updated dill in our Pipfile.lock to a version newer than the version loaded on Dataflow workers. All pipeline deploys were serializing the pipeline with a newer dill version that was not compatible with the version the machine was using to deserialize it.
    
    This change pins the dill version we deploy to workers to match our local pipenv version.
    
    Closes Recidiviz/recidiviz-data#4321
    
    GitOrigin-RevId: b81076f3d08379667f4618c57d5996b811de2255

--- setup.py
+++ setup.py
@@ -32,6 +32,10 @@ REQUIRED_PACKAGES = [
     'apache-beam',
     'cattrs',
     'dateparser',
+    # Must stay up-to-date with latest dill version in the Pipfile - this library is used for template serialization and
+    # it's critical that the local version used to serialize the templates and the version used to deserialize on the
+    # remote workers match.
+    'dill==0.3.2',
     'Flask',
     'google-api-core',
     'google-api-python-client',
