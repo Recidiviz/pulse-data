16cec90a60656b1d844c5d06ded2f74923255b7d
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-10-29 16:57:28 -0700

    [Monitoring] Fail more loudly for non-validation metric export errors (plus monitoring) (Recidiviz/recidiviz-data#4413)
    In Recidiviz/recidiviz-data#4179 we updated the metric view export code to validate exported files and refrain from copying them to a final location if they failed to validate.
    
    In doing so, we suppressed ValueErrors thrown during the export process so one validation failure wouldn't fail the whole export. However, this meant we were suppressing ALL errors, which may be due to reasons other than a failed validation (broken code or view query code, for example).
    
    This PR updates the logic to only suppress specific validation exceptions but not other value errors, and also adds monitoring metrics for both types of failures.
    
    Follow-up to Recidiviz/recidiviz-data#4407.
    
    GitOrigin-RevId: 9a6d4360d6d47bf81f9a03fd7160d56fe1bd0ff0

--- recidiviz/tests/big_query/export/composite_big_query_view_exporter_test.py
+++ recidiviz/tests/big_query/export/composite_big_query_view_exporter_test.py
@@ -24,7 +24,7 @@ import pytest
 from mock import call, create_autospec, patch
 
 from recidiviz.big_query.big_query_client import BigQueryClient
-from recidiviz.big_query.export.big_query_view_exporter import BigQueryViewExporter
+from recidiviz.big_query.export.big_query_view_exporter import BigQueryViewExporter, ViewExportValidationError
 from recidiviz.big_query.export.composite_big_query_view_exporter import CompositeBigQueryViewExporter
 from recidiviz.big_query.export.export_query_config import ExportBigQueryViewConfig
 from recidiviz.cloud_storage.gcs_file_system import GCSFileSystem
@@ -43,7 +43,7 @@ class CompositeBigQueryViewExporterTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.metadata_patcher.stop()
 
-    def test_export_happy_path(self):
+    def test_export_happy_path(self) -> None:
         metric_view_one = MetricBigQueryViewBuilder(
             dataset_id='dataset',
             view_id='view1',
@@ -137,7 +137,7 @@ class CompositeBigQueryViewExporterTest(unittest.TestCase):
             call(GcsfsFilePath(bucket_name='bucket2', blob_name='US_XX/view2.txt')),
         ])
 
-    def test_export_staging_delegate_validator_failed(self):
+    def test_export_staging_delegate_validator_crashed(self) -> None:
         metric_view_one = MetricBigQueryViewBuilder(
             dataset_id='dataset',
             view_id='view1',
@@ -205,7 +205,75 @@ class CompositeBigQueryViewExporterTest(unittest.TestCase):
 
         self.assertIn('Validation failed', str(e.value))
 
-    def test_export_final_existence_validation_failed(self):
+    def test_export_staging_delegate_validation_failed(self) -> None:
+        metric_view_one = MetricBigQueryViewBuilder(
+            dataset_id='dataset',
+            view_id='view1',
+            view_query_template='select * from table',
+            dimensions=['a', 'b', 'c'],
+        ).build()
+
+        export_config_one = ExportBigQueryViewConfig(
+            view=metric_view_one,
+            view_filter_clause='WHERE state_code = \'US_XX\'',
+            intermediate_table_name='intermediate_table',
+            output_directory=GcsfsDirectoryPath.from_absolute_path('gs://bucket1/US_XX'),
+        )
+        export_config_one_staging = ExportBigQueryViewConfig(
+            view=metric_view_one,
+            view_filter_clause='WHERE state_code = \'US_XX\'',
+            intermediate_table_name='intermediate_table',
+            output_directory=GcsfsDirectoryPath.from_absolute_path('gs://bucket1/staging/US_XX'),
+        )
+
+        metric_view_two = MetricBigQueryViewBuilder(
+            dataset_id='dataset',
+            view_id='view2',
+            view_query_template='select * from view2',
+            dimensions=['d', 'e', 'f'],
+        ).build()
+
+        export_config_two = ExportBigQueryViewConfig(
+            view=metric_view_two,
+            view_filter_clause='WHERE state_code = \'US_XX\'',
+            intermediate_table_name='intermediate_table2',
+            output_directory=GcsfsDirectoryPath.from_absolute_path('gs://bucket2/US_XX'),
+        )
+        export_config_two_staging = ExportBigQueryViewConfig(
+            view=metric_view_two,
+            view_filter_clause='WHERE state_code = \'US_XX\'',
+            intermediate_table_name='intermediate_table2',
+            output_directory=GcsfsDirectoryPath.from_absolute_path('gs://bucket2/staging/US_XX'),
+        )
+
+        mock_bq_client = create_autospec(BigQueryClient)
+        mock_fs = create_autospec(GCSFileSystem)
+
+        delegate_one = create_autospec(BigQueryViewExporter)
+        delegate_two = create_autospec(BigQueryViewExporter)
+
+        delegate_one.export_and_validate.return_value = [export_config_one_staging.output_path('json'),
+                                                         export_config_two_staging.output_path('json')]
+        delegate_two.export_and_validate.return_value = [export_config_one_staging.output_path('txt'),
+                                                         export_config_two_staging.output_path('txt')]
+
+        delegate_one = create_autospec(BigQueryViewExporter)
+        delegate_one_staging_paths = [export_config_one_staging.output_path('json'),
+                                      export_config_two_staging.output_path('json')]
+        delegate_one.export_and_validate.return_value = delegate_one_staging_paths
+
+        delegate_two = create_autospec(BigQueryViewExporter)
+        delegate_two.export_and_validate.side_effect = ViewExportValidationError('Validation failed')
+
+        # Make the actual call
+        exporter = CompositeBigQueryViewExporter(mock_bq_client, mock_fs, [delegate_one, delegate_two])
+
+        with pytest.raises(ViewExportValidationError) as e:
+            exporter.export_and_validate([export_config_one, export_config_two])
+
+        self.assertIn('Validation failed', str(e.value))
+
+    def test_export_final_existence_validation_failed(self) -> None:
         metric_view_one = MetricBigQueryViewBuilder(
             dataset_id='dataset',
             view_id='view1',
@@ -265,7 +333,7 @@ class CompositeBigQueryViewExporterTest(unittest.TestCase):
         # Make the actual call
         exporter = CompositeBigQueryViewExporter(mock_bq_client, mock_fs, [delegate_one, delegate_two])
 
-        with pytest.raises(ValueError) as e:
+        with pytest.raises(ViewExportValidationError) as e:
             exporter.export_and_validate([export_config_one, export_config_two])
 
         # We get an error at the very end of the export chain because even though delegate validations passed, the
