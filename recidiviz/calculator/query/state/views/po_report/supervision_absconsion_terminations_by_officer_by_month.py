d825d7f6ce78bcee5e40f28dd9b1b97400daba25
Daniela Schiano di Cola <daschi87@gmail.com>
2020-10-30 18:49:25 -0400

    Add missing fields to po_monthly_report_data for UXR updates (Recidiviz/recidiviz-data#4432)
    * Add average aggregations for absconsion, crime revocation, technical revocation
    
    * Add new averages to the po_monthly_report_data
    
    * Add the officer's first name to the po_monthly_report_data
    
    * Add missing comma
    
    * Add pos_discharges_change and total_revocations
    
    * Add US_ID given names to augmented_agent_info, use in po_monthly_report_data
    
    * Trim officer name in subclause
    
    GitOrigin-RevId: 8ba469d67a38d559e43c621c9f2590001fd91cd7

--- recidiviz/calculator/query/state/views/po_report/supervision_absconsion_terminations_by_officer_by_month.py
+++ recidiviz/calculator/query/state/views/po_report/supervision_absconsion_terminations_by_officer_by_month.py
@@ -18,6 +18,7 @@
 # pylint: disable=trailing-whitespace
 
 from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.query import bq_utils
 from recidiviz.calculator.query.state import dataset_config
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
@@ -49,14 +50,38 @@ SUPERVISION_ABSCONSION_TERMINATIONS_BY_OFFICER_BY_MONTH_QUERY_TEMPLATE = \
         -- Only the following supervision types should be included in the PO report
         AND supervision_period_supervision_type IN ('DUAL', 'PROBATION', 'PAROLE', 'INTERNAL_UNKNOWN')
       GROUP BY state_code, year, month, officer_external_id
+    ),
+    avg_absconsions_by_district_state AS (
+        -- Get the average monthly absconsions by district and state
+        SELECT
+          state_code, year, month,
+          district,
+          AVG(IFNULL(absconsion_count, 0)) AS avg_absconsions
+        FROM `{project_id}.{po_report_dataset}.officer_supervision_district_association_materialized`
+        LEFT JOIN absconsions_per_officer
+            USING (state_code, year, month, officer_external_id),
+        {district_dimension}
+        GROUP BY state_code, year, month, district   
     )
     SELECT
       state_code, year, month,
       officer_external_id, district,
-      IFNULL(absconsions_per_officer.absconsion_count, 0) AS absconsions
+      IFNULL(absconsions_per_officer.absconsion_count, 0) AS absconsions,
+      district_avg.avg_absconsions AS absconsions_district_average,
+      state_avg.avg_absconsions AS absconsions_state_average
     FROM `{project_id}.{po_report_dataset}.officer_supervision_district_association_materialized`
     LEFT JOIN absconsions_per_officer
       USING (state_code, year, month, officer_external_id)
+    LEFT JOIN(
+        SELECT * FROM avg_absconsions_by_district_state
+        WHERE district != 'ALL'
+    ) district_avg
+        USING (state_code, year, month, district)
+    LEFT JOIN (
+      SELECT * EXCEPT (district) FROM avg_absconsions_by_district_state
+      WHERE district = 'ALL'
+    ) state_avg
+      USING (state_code, year, month)
     ORDER BY state_code, year, month, district, officer_external_id
     """
 
@@ -68,6 +93,7 @@ SUPERVISION_ABSCONSION_TERMINATIONS_BY_OFFICER_BY_MONTH_VIEW_BUILDER = SimpleBig
     description=SUPERVISION_ABSCONSION_TERMINATIONS_BY_OFFICER_BY_MONTH_DESCRIPTION,
     reference_views_dataset=dataset_config.REFERENCE_VIEWS_DATASET,
     state_dataset=dataset_config.STATE_BASE_DATASET,
+    district_dimension=bq_utils.unnest_district(district_column='district'),
     po_report_dataset=dataset_config.PO_REPORT_DATASET
 )
 
