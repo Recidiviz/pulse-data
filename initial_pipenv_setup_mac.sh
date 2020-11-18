4f61b0d50a2ad135756f595473c00417239edfdc
Colin Adams <colinadams@google.com>
2020-11-17 16:03:52 -0800

    Sync githooks for everyone on ./initial_pipenv_setup_mac.sh (recidiviz/recidiviz-data#4629)
    GitOrigin-RevId: 4cc81fa4486c041fb152349612855b2c6dcff38f

--- initial_pipenv_setup_mac.sh
+++ initial_pipenv_setup_mac.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# Add hooks
+cp -R githooks/ .git/hooks/
+chmod +x .git/hooks/*
+
+# Sync with openssl linked properly
 export LDFLAGS="-L/usr/local/opt/openssl/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl/include"
 pipenv sync --dev
