94fbbfeda73aa2b1b61e5ccc38238135ea5a1752
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2020-11-23 19:41:31 -0500

    Create script to pull secrets and use them to run upgrades. (Recidiviz/recidiviz-data#4598)
    * Create skeleton to pull secrets and use them to run upgrades.
    
    * Respond to reviewer feedback
    
    GitOrigin-RevId: e7c325e472bf6739786d8f1ad74b5010816448ac

---
+++ stubs/pygit2/repository.pyi
@@ -0,0 +1,31 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+from typing import Optional
+
+from .reference import Reference
+
+
+class BaseRepository:
+    pass
+
+
+class Repository(BaseRepository):
+    def __init__(self, path: Optional[str] = None, flags: int = 0): ...
+
+    @property
+    def head(self) -> Reference: ...
