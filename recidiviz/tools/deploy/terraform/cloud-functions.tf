9b9d74137fb90bb205309d853e5cd3f9af60bc44
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2020-11-24 18:28:45 -0500

    Add initial terraform scripts for managing cloud functions (recidiviz/recidiviz-data#4706)
    GitOrigin-RevId: a8c0fa24bc84c33c042e1dfcc2f44af30f45db98

---
+++ recidiviz/tools/deploy/terraform/cloud-functions.tf
@@ -0,0 +1,213 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+# NOTE: county-map-updater is not tracked here since its code is defined
+# directly in the Google Cloud Functions UI.
+
+# All the "pipeline" cloud functions are omitted because those processes
+# are triggered by Airflow directly.
+
+# The execute-covid-aggregation is not included because it will eventually
+# be deprecated. See #4464.
+
+data "google_secret_manager_secret_version" "sendgrid_api_key" {
+  secret = "sendgrid_api_key"
+}
+
+data "google_secret_manager_secret_version" "po_report_cdn_static_ip" {
+  secret = "po_report_cdn_static_IP"
+}
+
+locals {
+  repo_url      = "https://source.developers.google.com/projects/${var.project_id}/repos/github_Recidiviz_pulse-data/moveable-aliases/${var.release_tag}/paths/recidiviz/cloud_functions"
+  is_production = (var.project_id == "recidiviz-123")
+}
+
+resource "google_cloudfunctions_function" "direct-ingest-county" {
+  name    = "direct-ingest-county"
+  runtime = "python37"
+  labels = {
+    "deployment-tool" = "console-cloud"
+  }
+
+  entry_point           = "direct_ingest_county"
+  environment_variables = {}
+
+  source_repository {
+    url = local.repo_url
+  }
+
+  timeouts {}
+}
+
+
+resource "google_cloudfunctions_function" "direct-ingest-states" {
+  # TODO(#4690): The cloud function for US_MO ingest on prod apparently oscillates
+  # between a "handle_state_direct_ingest_file" and
+  # "handle_state_direct_ingest_file_rename_only" entrypoint. As a result, prod
+  # and staging drift :(. Once that's more stable, we can add it to this list.
+  for_each = toset(["US_ID", "US_ND", "US_PA"])
+
+  name    = "direct-ingest-state-${replace(lower(each.key), "_", "-")}"
+  runtime = "python37"
+  labels = {
+    "deployment-tool" = "console-cloud"
+  }
+
+  entry_point           = "handle_state_direct_ingest_file"
+  environment_variables = {}
+
+  source_repository {
+    url = local.repo_url
+  }
+
+  timeouts {}
+}
+
+
+resource "google_cloudfunctions_function" "export_metric_view_data" {
+  name    = "export_metric_view_data"
+  runtime = "python37"
+  labels = {
+    "deployment-tool" = "console-cloud"
+  }
+
+  entry_point           = "export_metric_view_data"
+  environment_variables = {}
+
+  source_repository {
+    url = local.repo_url
+  }
+
+  timeouts {}
+}
+
+
+resource "google_cloudfunctions_function" "handle-covid-source-upload" {
+  name    = "handle-covid-source-upload"
+  runtime = "python37"
+  labels = {
+    "deployment-tool" = "console-cloud"
+  }
+
+  available_memory_mb = 2048
+
+  entry_point           = "handle_covid_ingest_on_upload"
+  environment_variables = {}
+
+  source_repository {
+    url = local.repo_url
+  }
+
+  timeout = 500
+
+  timeouts {}
+}
+
+# TODO(#4690): Once @jamwalla confirms that "parse-state-aggregate" in prod is the
+# same as "state-aggregate-function" in staging, then we rename staging, import the
+# two states and add this to the terraform workflow.
+
+# resource "google_cloudfunctions_function" "parse-state-aggregate" {
+#   name    = "parse-state-aggregate"
+#   runtime = "python37"
+#   labels = {
+#     "deployment-tool" = "console-cloud"
+#   }
+
+#   entry_point           = "parse_state_aggregate"
+#   environment_variables = {}
+
+#   source_repository {
+#     url = local.repo_url
+#   }
+
+#   timeout = 120
+
+#   timeouts {}
+# }
+
+
+resource "google_cloudfunctions_function" "report_deliver_emails_for_batch" {
+  name    = "report_deliver_emails_for_batch"
+  runtime = "python37"
+  labels = {
+    "deployment-tool" = "console-cloud"
+  }
+
+  entry_point = "handle_deliver_emails_for_batch_email_reporting"
+  environment_variables = {
+    "FROM_EMAIL_ADDRESS" = "reports@recidiviz.org"
+    "FROM_EMAIL_NAME"    = "Recidiviz Reports"
+    "SENDGRID_API_KEY"   = data.google_secret_manager_secret_version.sendgrid_api_key.secret_data
+  }
+  trigger_http = true
+
+  source_repository {
+    url = local.repo_url
+  }
+
+  timeout = 300
+
+  timeouts {}
+}
+
+
+resource "google_cloudfunctions_function" "report_start_new_batch" {
+  name    = "report_start_new_batch"
+  runtime = "python37"
+  labels = {
+    "deployment-tool" = "console-cloud"
+  }
+
+  entry_point = "handle_start_new_batch_email_reporting"
+  environment_variables = {
+    "CDN_STATIC_IP" = data.google_secret_manager_secret_version.po_report_cdn_static_ip.secret_data
+  }
+  trigger_http = true
+
+  source_repository {
+    url = local.repo_url
+  }
+
+  timeout = 300
+
+  timeouts {}
+}
+
+# TODO(#4690): Re-enable this after "run-calc-pipelines" in staging has been renamed.
+# Note: It doesn't seem like it's possible to rename cloud functions, so we will need
+# to copy it and then delete the old one.
+
+# resource "google_cloudfunctions_function" "run_calculation_pipelines" {
+#   name    = "run_calculation_pipelines"
+#   runtime = "python37"
+#   labels = {
+#     "deployment-tool" = "console-cloud"
+#   }
+
+#   entry_point = "trigger_calculation_pipeline_dag"
+#   environment_variables = {
+#     "WEBSERVER_ID" = "p03ca791d5f21b85cp-tp"
+#   }
+
+#   source_repository {
+#     url = local.repo_url
+#   }
+
+#   timeouts {}
+# }
