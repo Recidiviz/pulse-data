e98b918a660a6b951a0b339a133bb5dcbbd7a615
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2020-12-10 13:40:55 -0500

    Re-enable BQ queries for ingest metadata (Recidiviz/recidiviz-data#4877)
    * Revert "Revert "Add BQ views for viewing state/enum breakdowns (Recidiviz/recidiviz-data#4760)" (Recidiviz/recidiviz-data#4874)"
    
    This reverts commit 3963ad92641e99acb050b843ed6bf7b7232710a4.
    
    * Add missing arg
    
    GitOrigin-RevId: 9e334b607e3cdc6cf30f2569f82a6ef9a013992c

---
+++ recidiviz/ingest/views/enum_counter.py
@@ -0,0 +1,92 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""A view collector that generates BigQuery views that count the instances of
+particular enum values for a given table.
+
+Its results are split by state and by whether it is a placeholder object.
+"""
+from typing import List
+
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.big_query.big_query_view_collector import BigQueryViewCollector
+from recidiviz.ingest.views.dataset_config import VIEWS_DATASET
+from recidiviz.ingest.views.metadata_helpers import (
+    METADATA_EXCLUDED_PROPERTIES,
+    METADATA_TABLES_WITH_CUSTOM_COUNTERS,
+    get_enum_property_names,
+    get_state_tables,
+)
+
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+ENUM_COUNTER_STATE_QUERY_TEMPLATE = \
+    """
+SELECT
+  state_code,
+  IF({column_name} IS NULL, 'NULL', {column_name}) AS {column_name},
+  COUNT(*) AS total_count,
+  0 AS placeholder_count
+FROM
+  `{project_id}.state.{table_name}`
+GROUP BY state_code, `{column_name}`
+ORDER BY state_code, `{column_name}`;
+"""
+
+ENUM_COUNTER_WITH_PLACEHOLDERS_STATE_QUERY_TEMPLATE = \
+    """
+SELECT
+  state_code,
+  IF({column_name} IS NULL, 'NULL', {column_name}) AS {column_name},
+  COUNT(*) AS total_count,
+  IFNULL(SUM(CASE WHEN external_id IS NULL THEN 1 END), 0) AS placeholder_count
+FROM
+  `{project_id}.state.{table_name}`
+GROUP BY state_code, `{column_name}`
+ORDER BY state_code, `{column_name}`;
+"""
+
+
+class StateTableEnumCounterBigQueryViewCollector(BigQueryViewCollector[SimpleBigQueryViewBuilder]):
+    def collect_view_builders(self) -> List[SimpleBigQueryViewBuilder]:
+        builders = []
+        for entity, table_name in get_state_tables():
+            if table_name in METADATA_TABLES_WITH_CUSTOM_COUNTERS:
+                continue
+            has_placeholders = 'external_id' in entity.get_column_property_names()
+            for col in get_enum_property_names(entity):
+                if col in METADATA_EXCLUDED_PROPERTIES:
+                    continue
+                template = ENUM_COUNTER_WITH_PLACEHOLDERS_STATE_QUERY_TEMPLATE if has_placeholders \
+                    else ENUM_COUNTER_STATE_QUERY_TEMPLATE
+                builders.append(
+                    SimpleBigQueryViewBuilder(
+                        dataset_id=VIEWS_DATASET,
+                        view_id=f'ingest_state_metadata__{table_name}__{col}',
+                        view_query_template=template,
+                        table_name=table_name,
+                        column_name=col,
+                    )
+                )
+        return builders
+
+
+if __name__ == '__main__':
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        collector = StateTableEnumCounterBigQueryViewCollector()
+        for builder in collector.collect_view_builders():
+            builder.build_and_print()
---
+++ recidiviz/ingest/views/view_config.py
@@ -0,0 +1,34 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Ingest metadata view configuration."""
+
+from typing import Dict, Sequence
+
+from recidiviz.big_query.big_query_view import BigQueryViewBuilder
+from recidiviz.ingest.views.dataset_config import VIEWS_DATASET
+from recidiviz.ingest.views.enum_counter import StateTableEnumCounterBigQueryViewCollector
+from recidiviz.ingest.views.non_enum_counter import StateTableNonEnumCounterBigQueryViewCollector
+from recidiviz.ingest.views.state_person_counter import StatePersonBigQueryViewCollector
+
+
+VIEW_BUILDERS_FOR_VIEWS_TO_UPDATE: Dict[str, Sequence[BigQueryViewBuilder]] = {
+    VIEWS_DATASET: (
+        StateTableEnumCounterBigQueryViewCollector().collect_view_builders() +
+        StateTableNonEnumCounterBigQueryViewCollector().collect_view_builders() +
+        StatePersonBigQueryViewCollector().collect_view_builders()
+    ),
+}
