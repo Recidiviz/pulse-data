d8aaf586a54ebeb62d8da1454d6fb04165f99b0a
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2020-12-14 10:34:36 -0500

    Add helper script to run prod migrations for all four databases (Recidiviz/recidiviz-data#4906)
    * Add helper script to run prod migrations for all four databases
    
    * Rename with underscores
    
    GitOrigin-RevId: b43640fd614b63ee195fc4d5aeed06318adf637e

---
+++ recidiviz/tools/migrations/run_all_prod_migrations.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
+source ${BASH_SOURCE_DIR}/../script_base.sh
+
+echo 'Running prod migrations against all databases.'
+script_prompt 'Are you running this script from prod-data-client?'
+
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JAILS --project-id recidiviz-123 --ssl-cert-path ~/prod_data_certs/
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database STATE --project-id recidiviz-123 --ssl-cert-path ~/prod_state_data_certs/
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database OPERATIONS --project-id recidiviz-123 --ssl-cert-path ~/prod_operations_data_certs/
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JUSTICE_COUNTS --project-id recidiviz-123 --ssl-cert-path ~/prod_justice_counts_data_certs/
