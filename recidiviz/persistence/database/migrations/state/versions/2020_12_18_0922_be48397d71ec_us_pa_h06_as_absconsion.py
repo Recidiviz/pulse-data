f8368bb31cac7ffd832f19071bcbe1927a5ffdab
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2020-12-22 12:41:00 -0800

    [US_PA] Migrate violation type H06 to count as absconsion (Recidiviz/recidiviz-data#4976)
    The good folks at the PADOC have requested that the H06 violation type be counted as absconsion. Once this commit is deployed to prod and the BQ refresh/metric file export run, the change will take effect (no data rerun required).
    
    Closes Recidiviz/recidiviz-data#4671
    
    GitOrigin-RevId: a795274f9ae51fdd64a7ad62fa040706cf6c9e15

---
+++ recidiviz/persistence/database/migrations/state/versions/2020_12_18_0922_be48397d71ec_us_pa_h06_as_absconsion.py
@@ -0,0 +1,59 @@
+# pylint: skip-file
+"""us_pa_H06_as_absconsion
+
+Revision ID: be48397d71ec
+Revises: a54c66450b78
+Create Date: 2020-12-18 09:22:11.699720
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'be48397d71ec'
+down_revision = 'a54c66450b78'
+branch_labels = None
+depends_on = None
+
+
+UPDATE_TO_NEW_VALUE_QUERY = "UPDATE {table_id}" \
+                            " SET violation_type = '{new_violation_type_enum_value}'" \
+                            " WHERE supervision_violation_type_entry_id IN (" \
+                            "   SELECT supervision_violation_type_entry_id FROM" \
+                            "   state_supervision_violation_type_entry" \
+                            "   WHERE state_code = 'US_PA' AND violation_type_raw_text = 'H06'" \
+                            ");"
+
+
+def upgrade():
+    connection = op.get_bind()
+
+    new_enum_value = 'ABSCONDED'
+    connection.execute(
+        UPDATE_TO_NEW_VALUE_QUERY.format(
+            table_id='state_supervision_violation_type_entry',
+            new_violation_type_enum_value=new_enum_value)
+    )
+    connection.execute(
+        UPDATE_TO_NEW_VALUE_QUERY.format(
+            table_id='state_supervision_violation_type_entry_history',
+            new_violation_type_enum_value=new_enum_value)
+    )
+
+
+def downgrade():
+    connection = op.get_bind()
+
+    new_enum_value = 'TECHNICAL'
+
+    connection.execute(
+        UPDATE_TO_NEW_VALUE_QUERY.format(
+            table_id='state_supervision_violation_type_entry',
+            new_violation_type_enum_value=new_enum_value)
+    )
+    connection.execute(
+        UPDATE_TO_NEW_VALUE_QUERY.format(
+            table_id='state_supervision_violation_type_entry_history',
+            new_violation_type_enum_value=new_enum_value)
+    )
