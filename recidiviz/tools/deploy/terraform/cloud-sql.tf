f3e48d98589290059c07e371b06aaf1d07cca51d
Dan Hansen <dan@recidiviz.org>
2021-01-06 10:43:22 -0800

    Add existing CloudSQL resources to Terraform (Recidiviz/recidiviz-data#5204)
    * Recidiviz/recidiviz-data#5163 Remove ad-hoc script
    
    * Reorganize `copy_certificates_to_prod_data_client`
    
    * Add existing CloudSQL resources to Terraform
    
    * Add ad-hoc script to import existing CloudSQL resources into Terraform state
    
    GitOrigin-RevId: 713d88d50e37795b8f75edad16bddb3b0805293b

--- recidiviz/tools/deploy/terraform/cloud-sql.tf
+++ recidiviz/tools/deploy/terraform/cloud-sql.tf
@@ -1,4 +1,3 @@
-
 # Recidiviz - a data platform for criminal justice reform
 # Copyright (C) 2020 Recidiviz, Inc.
 #
@@ -16,6 +15,11 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 
+locals {
+  is_staging    = var.project_id == "recidiviz-staging"
+  is_production = var.project_id == "recidiviz-123"
+}
+
 module "case_triage_database" {
   source            = "./modules/cloud-sql"
   base_secret_name  = "case_triage"
@@ -26,3 +30,59 @@ module "case_triage_database" {
   require_ssl_connection = true
 }
 
+
+module "jails_database" {
+  source           = "./modules/cloud-sql"
+  base_secret_name = "sqlalchemy"
+  database_version = "POSTGRES_9_6"
+  region           = "us-east4"
+  zone             = local.is_staging ? "us-east4-c" : "us-east4-b"
+  # 1 vCPU, 3.75GB Memory | 4 vCPU, 15GB Memory
+  tier             = local.is_staging ? "db-custom-1-3840" : "db-custom-4-15360"
+  has_readonly_user = local.is_production
+  # TODO(#5135): Consolidate to `true` once all certificates are provisioned
+  # Hack to decouple SSL certificate / secret creation from SSL enforcement
+  require_ssl_connection = local.is_production
+}
+
+
+module "justice_counts_database" {
+  source                         = "./modules/cloud-sql"
+  base_secret_name               = "justice_counts"
+  region                         = "us-east1"
+  zone                           = "us-east1-c"
+  tier                           = "db-custom-1-3840" # 1 vCPU, 3.75GB Memory
+  has_readonly_user              = local.is_production
+  point_in_time_recovery_enabled = local.is_production
+  # TODO(#5135): Consolidate to `true` once all certificates are provisioned
+  # Hack to decouple SSL certificate / secret creation from SSL enforcement
+  require_ssl_connection         = local.is_production
+}
+
+
+module "operations_database" {
+  source            = "./modules/cloud-sql"
+  base_secret_name  = "operations"
+  database_version  = "POSTGRES_11"
+  region            = "us-east1"
+  zone              = "us-east1-b"
+  tier              = "db-custom-1-3840" # 1 vCPU, 3.75GB Memory
+  has_readonly_user = local.is_production
+  # TODO(#5135): Consolidate to `true` once all certificates are provisioned
+  # Hack to decouple SSL certificate / secret creation from SSL enforcement
+  require_ssl_connection = local.is_production
+}
+
+
+module "state_database" {
+  source            = "./modules/cloud-sql"
+  base_secret_name  = "state"
+  database_version  = "POSTGRES_9_6"
+  region            = "us-east1"
+  zone              = "us-east1-c"
+  tier              = "db-custom-4-16384" # 4 vCPUs, 16GB Memory
+  has_readonly_user = local.is_production
+  # TODO(#5135): Consolidate to `true` once all certificates are provisioned
+  # Hack to decouple SSL certificate / secret creation from SSL enforcement
+  require_ssl_connection = local.is_production
+}
