9779c9f9c543cbfc6342b3510f0f1c260bd2c29f
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-01-19 18:58:09 -0500

    Create SelectedColumnsBigQueryView and first client list implementation (Recidiviz/recidiviz-data#5376)
    GitOrigin-RevId: 3712ceaed63e842c2bf4de0bad7ba8140c1eb701

---
+++ recidiviz/case_triage/views/client_list_view.py
@@ -0,0 +1,71 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Creates the view builder and view for listing all clients."""
+
+from recidiviz.big_query.selected_columns_big_query_view import SelectedColumnsBigQueryViewBuilder
+from recidiviz.case_triage.views.dataset_config import VIEWS_DATASET
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+CLIENT_LIST_QUERY_TEMPLATE = """
+WITH
+  most_recent_client_dates AS (
+  SELECT
+    supervising_officer_external_id,
+    po_report_recipients.state_code,
+    MAX(date_of_supervision) AS date_of_supervision
+  FROM
+    `{project_id}.dataflow_metrics_materialized.most_recent_supervision_population_metrics_materialized` most_recent_metrics
+  LEFT JOIN
+    `{project_id}.static_reference_tables.po_report_recipients` po_report_recipients
+  ON
+    po_report_recipients.state_code = most_recent_metrics.state_code
+    AND po_report_recipients.officer_external_id = most_recent_metrics.supervising_officer_external_id
+  GROUP BY
+    supervising_officer_external_id,
+    state_code )
+SELECT
+  {columns}
+FROM
+  most_recent_client_dates
+INNER JOIN
+  `{project_id}.dataflow_metrics_materialized.most_recent_supervision_population_metrics_materialized`
+USING
+  (supervising_officer_external_id,
+    state_code,
+    date_of_supervision)
+WHERE
+  supervision_level IS NOT NULL
+"""
+
+CLIENT_LIST_VIEW_BUILDER = SelectedColumnsBigQueryViewBuilder(
+    dataset_id=VIEWS_DATASET,
+    view_id='client_list',
+    view_query_template=CLIENT_LIST_QUERY_TEMPLATE,
+    columns=[
+        'supervising_officer_external_id',
+        'person_external_id',
+        'supervision_type',
+        'case_type',
+        'supervision_level',
+        'state_code',
+    ],
+)
+
+if __name__ == '__main__':
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        CLIENT_LIST_VIEW_BUILDER.build_and_print()
