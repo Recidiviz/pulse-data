ac9c6ee64e0060204655fc053a38b190cd604ac4
Dan Hansen <dan@recidiviz.org>
2021-01-22 12:06:11 -0800

    Run migrations for CASE_TRIAGE (recidiviz/recidiviz-data#5433)
    GitOrigin-RevId: c246093ada6e7662c95711021355198881fc6dee

--- recidiviz/tools/migrations/run_all_prod_migrations.sh
+++ recidiviz/tools/migrations/run_all_prod_migrations.sh
@@ -10,3 +10,4 @@ run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database J
 run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database STATE --project-id recidiviz-123 --ssl-cert-path ~/prod_state_data_certs/
 run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database OPERATIONS --project-id recidiviz-123 --ssl-cert-path ~/prod_operations_data_certs/
 run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JUSTICE_COUNTS --project-id recidiviz-123 --ssl-cert-path ~/prod_justice_counts_data_certs/
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database CASE_TRIAGE --project-id recidiviz-123 --ssl-cert-path ~/prod_case_triage_certs/
