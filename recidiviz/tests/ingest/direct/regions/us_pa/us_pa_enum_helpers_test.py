759c7b16f8e3b67a565ecfda8a8cbd56e4eafb0d
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2021-02-05 16:48:10 -0800

    [PA ingest] Add a bunch of old supervision_level enum mappings (recidiviz/recidiviz-data#5729)
    Add enum mappings for a bunch of legacy supervision level strings in PA that we had missed / ignored in previous ingest runs where the enum error threshold was not 0.
    
    Did some sanity checking of other enums in this file and added tests for parsing all the possible values.
    
    Work towards recidiviz/recidiviz-data#5500
    
    GitOrigin-RevId: 2e8111e8055d156c01271b179da2affb8bd8bc64

--- recidiviz/tests/ingest/direct/regions/us_pa/us_pa_enum_helpers_test.py
+++ recidiviz/tests/ingest/direct/regions/us_pa/us_pa_enum_helpers_test.py
@@ -18,12 +18,16 @@
 import unittest
 from recidiviz.common.constants.state.state_incarceration_period import StateIncarcerationPeriodAdmissionReason, \
     StateIncarcerationPeriodReleaseReason, StateSpecializedPurposeForIncarceration
+from recidiviz.common.constants.state.state_supervision_period import StateSupervisionLevel, \
+    StateSupervisionPeriodSupervisionType, StateSupervisionPeriodAdmissionReason, \
+    StateSupervisionPeriodTerminationReason
+from recidiviz.ingest.direct.regions.us_pa.us_pa_controller import UsPaController
 from recidiviz.ingest.direct.regions.us_pa.us_pa_enum_helpers import incarceration_period_admission_reason_mapper, \
     incarceration_period_release_reason_mapper, incarceration_period_purpose_mapper
 from recidiviz.tests.ingest import fixtures
 
 
-class TestSetDateSpecificAssessmentLevel(unittest.TestCase):
+class TestUsPaEnumMappings(unittest.TestCase):
     """Tests the mapper functions from the us_pa_enum_helpers file
     which parse raw text from the incarceration period ingest view"""
     def test_incarceration_period_admission_reason_mapper(self) -> None:
@@ -58,3 +62,43 @@ class TestSetDateSpecificAssessmentLevel(unittest.TestCase):
                 mapping = incarceration_period_purpose_mapper(purpose_str)
                 self.assertIsNotNone(mapping)
                 self.assertIsInstance(mapping, StateSpecializedPurposeForIncarceration)
+
+    def test_supervision_level_parsing(self) -> None:
+        enum_overrides = UsPaController.generate_enum_overrides()
+        fixture_path = fixtures.as_filepath('supervision_level_raw_text.csv')
+        with open(fixture_path, 'r') as f:
+            while True:
+                supervision_level_str = f.readline().strip()
+                if not supervision_level_str:
+                    break
+                _ = StateSupervisionLevel.parse(supervision_level_str, enum_overrides)
+
+    def test_supervision_period_supervision_type_parsing(self) -> None:
+        enum_overrides = UsPaController.generate_enum_overrides()
+        fixture_path = fixtures.as_filepath('supervision_period_supervision_type_raw_text.csv')
+        with open(fixture_path, 'r') as f:
+            while True:
+                supervision_type_str = f.readline().strip()
+                if not supervision_type_str:
+                    break
+                _ = StateSupervisionPeriodSupervisionType.parse(supervision_type_str, enum_overrides)
+
+    def test_supervision_period_admission_reason_parsing(self) -> None:
+        enum_overrides = UsPaController.generate_enum_overrides()
+        fixture_path = fixtures.as_filepath('supervision_period_admission_reason_raw_text.csv')
+        with open(fixture_path, 'r') as f:
+            while True:
+                supervision_type_str = f.readline().strip()
+                if not supervision_type_str:
+                    break
+                _ = StateSupervisionPeriodAdmissionReason.parse(supervision_type_str, enum_overrides)
+
+    def test_supervision_period_termination_reason_parsing(self) -> None:
+        enum_overrides = UsPaController.generate_enum_overrides()
+        fixture_path = fixtures.as_filepath('supervision_period_termination_reason_raw_text.csv')
+        with open(fixture_path, 'r') as f:
+            while True:
+                supervision_type_str = f.readline().strip()
+                if not supervision_type_str:
+                    break
+                _ = StateSupervisionPeriodTerminationReason.parse(supervision_type_str, enum_overrides)
