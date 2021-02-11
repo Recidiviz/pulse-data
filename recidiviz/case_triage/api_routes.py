004ac873e6568018aa1fa124fecb93833daf3d3b
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-02-11 09:25:40 -0500

    Reorganize routes and add flask tests (Recidiviz/recidiviz-data#5737)
    GitOrigin-RevId: 10db8d994ed46b2d8117b8524ed238471de4660a

---
+++ recidiviz/case_triage/api_routes.py
@@ -0,0 +1,104 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Implements API routes for the Case Triage app."""
+from flask import Blueprint, g, jsonify, request
+from flask_sqlalchemy_session import current_session
+
+from recidiviz.case_triage.case_updates.interface import CaseUpdatesInterface
+from recidiviz.case_triage.case_updates.types import CaseUpdateActionType
+from recidiviz.case_triage.exceptions import CaseTriageBadRequestException
+from recidiviz.case_triage.querier.querier import CaseTriageQuerier, PersonDoesNotExistError
+
+
+api = Blueprint('api', __name__)
+
+
+@api.route('/clients')
+def clients() -> str:
+    return jsonify([
+        client.to_json() for client in CaseTriageQuerier.clients_for_officer(
+            current_session,
+            g.current_user,
+        )
+    ])
+
+
+@api.route('/record_client_action', methods=['POST'])
+def record_client_action() -> str:
+    """Records individual clients actions. Expects JSON body of the form:
+    {
+        personExternalId: str,
+        actions: List[str],
+        otherText: Optional[str],
+    }
+    `actions` must be non-empty and only contain approved actions
+    """
+    if not request.json:
+        raise CaseTriageBadRequestException(
+            code='missing_body',
+            description='A json body must be provided for the request',
+        )
+    if 'personExternalId' not in request.json:
+        raise CaseTriageBadRequestException(
+            code='missing_arg',
+            description='`personExternalId` keyword is missing from request',
+        )
+    if 'actions' not in request.json:
+        raise CaseTriageBadRequestException(
+            code='missing_arg',
+            description='`actions` keyword is missing from request',
+        )
+
+    person_external_id = request.json['personExternalId']
+    actions = request.json['actions']
+    other_text = request.json.get('otherText')
+
+    if not isinstance(actions, list):
+        raise CaseTriageBadRequestException(
+            code='improper_type',
+            description='`actions` must be a list of CaseUpdateActionTypes',
+        )
+
+    try:
+        user_initiated_actions = [CaseUpdateActionType(a) for a in actions]
+    except ValueError as e:
+        raise CaseTriageBadRequestException(
+            code='improper_type',
+            description='`actions` must be a list of CaseUpdateActionTypes',
+        ) from e
+
+    state_code = g.current_user.state_code
+    try:
+        client = CaseTriageQuerier.etl_client_with_id_and_state_code(
+            current_session,
+            person_external_id,
+            state_code,
+        )
+    except PersonDoesNotExistError as e:
+        raise CaseTriageBadRequestException(
+            code='invalid_arg',
+            description=f'`personExternalId` does not correspond to a known person: {person_external_id}',
+        ) from e
+
+    CaseUpdatesInterface.update_case_for_person(
+        current_session,
+        g.current_user,
+        client,
+        user_initiated_actions,
+        other_text,
+    )
+    return ''
--- recidiviz/case_triage/server.py
+++ recidiviz/case_triage/server.py
@@ -19,20 +19,15 @@ import json
 import os
 from typing import Dict
 
-from flask import Flask, Response, g, jsonify, request, session
-from flask_sqlalchemy_session import current_session, flask_scoped_session
-from sqlalchemy.orm import sessionmaker
+from flask import Flask, Response, g, jsonify, send_from_directory, session
+from flask_sqlalchemy_session import current_session
 
+from recidiviz.case_triage.api_routes import api
 from recidiviz.case_triage.authorization import AuthorizationStore
-from recidiviz.case_triage.case_updates.interface import CaseUpdatesInterface
-from recidiviz.case_triage.case_updates.types import CaseUpdateActionType
-from recidiviz.case_triage.exceptions import (
-    CaseTriageAuthorizationError,
-    CaseTriageBadRequestException,
-)
-from recidiviz.case_triage.querier.querier import CaseTriageQuerier, PersonDoesNotExistError
+from recidiviz.case_triage.exceptions import CaseTriageAuthorizationError
+from recidiviz.case_triage.scoped_sessions import setup_scoped_sessions
+from recidiviz.case_triage.querier.querier import CaseTriageQuerier
 from recidiviz.case_triage.util import get_local_secret
-from recidiviz.persistence.database.base_schema import CaseTriageBase
 from recidiviz.persistence.database.sqlalchemy_engine_manager import SQLAlchemyEngineManager, SchemaType
 from recidiviz.tools.postgres import local_postgres_helpers
 from recidiviz.utils.auth.auth0 import (
@@ -44,23 +39,24 @@ from recidiviz.utils.environment import in_development, in_test
 from recidiviz.utils.flask_exception import FlaskException
 from recidiviz.utils.timer import RepeatedTimer
 
+
+# Flask setup
 static_folder = os.path.abspath(os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     "../../frontends/case-triage/build/",
 ))
 
-app = Flask(__name__, static_folder=static_folder, static_url_path="/")
+app = Flask(__name__, static_folder=static_folder)
 app.secret_key = get_local_secret("case_triage_secret_key")
 
 if in_development():
     db_url = local_postgres_helpers.postgres_db_url_from_env_vars()
 else:
     db_url = SQLAlchemyEngineManager.get_server_postgres_instance_url(schema_type=SchemaType.CASE_TRIAGE)
-engine = SQLAlchemyEngineManager.init_engine_for_postgres_instance(db_url, CaseTriageBase)
-session_factory = sessionmaker(bind=engine)
-app.scoped_session = flask_scoped_session(session_factory, app)
+setup_scoped_sessions(app, db_url)
 
 
+# Auth setup
 def on_successful_authorization(_payload: Dict[str, str], token: str) -> None:
     """
     Memoize the user's info (email_address, picture, etc) into our session
@@ -74,8 +70,6 @@ def on_successful_authorization(_payload: Dict[str, str], token: str) -> None:
             code="unauthorized",
             description="You are not authorized to access this application",
         )
-
-    g.current_user = CaseTriageQuerier.officer_for_email(current_session, session['user_info']['email'])
 
 
 auth0_configuration = get_local_secret("case_triage_auth0")
@@ -93,6 +87,19 @@ if not in_test():
     store_refresh.start()
 
 
+# Routes & Blueprints
+@api.before_request
+@requires_authorization
+def fetch_user_info() -> None:
+    """This method both fetches the current user and (by virtue of the decorator) enforces authorization
+    for all API routes."""
+    if not getattr(g, 'current_user', None):
+        g.current_user = CaseTriageQuerier.officer_for_email(current_session, session['user_info']['email'])
+
+
+app.register_blueprint(api, url_prefix='/api')
+
+
 @app.errorhandler(FlaskException)
 def handle_auth_error(ex: FlaskException) -> Response:
     response = jsonify({
@@ -103,93 +110,16 @@ def handle_auth_error(ex: FlaskException) -> Response:
     return response
 
 
-if not in_development():
-    with open(os.path.join(static_folder, 'index.html'), 'r') as index_file:
-        index_html = index_file.read()
-else:
-    index_html = ''
-
-
-@app.route('/')
-def index() -> str:
-    return index_html
-
-
 @app.route('/auth0_public_config.js')
 def auth0_public_config() -> str:
     # Expose ONLY the necessary variables to configure our Auth0 frontend
     return f'window.AUTH0_CONFIG = {authorization_config.as_public_config()};'
 
 
-@app.route('/api/clients')
-@requires_authorization
-def clients() -> str:
-    return jsonify([
-        client.to_json() for client in CaseTriageQuerier.clients_for_officer(
-            current_session,
-            g.current_user,
-        )
-    ])
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path: str = "") -> Response:
+    if path != "" and os.path.exists(os.path.join(static_folder, path)):
+        return send_from_directory(os.path.join(static_folder), path)
 
-
-@app.route('/api/record_client_action', methods=['POST'])
-@requires_authorization
-def record_client_action() -> str:
-    """Records individual clients actions. Expects JSON body of the form:
-    {
-        personExternalId: str,
-        actions: List[str],
-        otherText: Optional[str],
-    }
-    `actions` must be non-empty and only contain approved actions
-    """
-    if 'personExternalId' not in request.json:
-        raise CaseTriageBadRequestException(
-            code='missing_arg',
-            description='`personExternalId` keyword is missing from request',
-        )
-    if 'actions' not in request.json:
-        raise CaseTriageBadRequestException(
-            code='missing_arg',
-            description='`actions` keyword is missing from request',
-        )
-
-    person_external_id = request.json['personExternalId']
-    actions = request.json['actions']
-    other_text = request.json.get('otherText')
-
-    if not isinstance(actions, list):
-        raise CaseTriageBadRequestException(
-            code='improper_type',
-            description='`actions` must be a list of CaseUpdateActionTypes',
-        )
-
-    try:
-        user_initiated_actions = [CaseUpdateActionType(a) for a in actions]
-    except ValueError as e:
-        raise CaseTriageBadRequestException(
-            code='improper_type',
-            description='`actions` must be a list of CaseUpdateActionTypes',
-        ) from e
-
-    state_code = g.current_user.state_code
-    try:
-        client = CaseTriageQuerier.etl_client_with_id_and_state_code(
-            current_session,
-            person_external_id,
-            state_code,
-        )
-    except PersonDoesNotExistError as e:
-        raise CaseTriageBadRequestException(
-            code='invalid_arg',
-            description=f'`personExternalId` does not correspond to a known person: {person_external_id}',
-        ) from e
-
-    CaseUpdatesInterface.update_case_for_person(
-        current_session,
-        g.current_user,
-        client,
-        user_initiated_actions,
-        other_text,
-    )
-    return ''
+    return send_from_directory(os.path.join(static_folder), 'index.html')
