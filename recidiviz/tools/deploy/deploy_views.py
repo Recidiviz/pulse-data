6b079e95675f824359d166a69b323499d886b2ae
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2021-02-10 18:58:56 -0800

    [View update] Only materialize views that may have changed (retry) (Recidiviz/recidiviz-data#5798)
    All changes from Recidiviz/recidiviz-data#5756, plus added a new deploy_views.py script for use in the deploy.
    
    GitOrigin-RevId: 402dd6622d06fc6a633dcc6d5467b0c4212cd01f

---
+++ recidiviz/tools/deploy/deploy_views.py
@@ -0,0 +1,54 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Script for deploying updated views to BigQuery. Should not be run outside the context of a deploy.
+Run locally with the following command:
+    python -m recidiviz.big_query.view_update_manager --project_id [PROJECT_ID]
+"""
+import argparse
+import logging
+import sys
+from typing import List, Tuple
+
+from recidiviz.big_query.view_update_manager import VIEW_BUILDERS_BY_NAMESPACE, \
+    create_dataset_and_deploy_views_for_view_builders
+from recidiviz.utils.environment import GCP_PROJECT_STAGING, GCP_PROJECT_PRODUCTION
+from recidiviz.utils.metadata import local_project_id_override
+
+
+def parse_arguments(argv: List[str]) -> Tuple[argparse.Namespace, List[str]]:
+    """Parses the required arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--project_id',
+                        dest='project_id',
+                        type=str,
+                        choices=[GCP_PROJECT_STAGING, GCP_PROJECT_PRODUCTION],
+                        required=True)
+
+    return parser.parse_known_args(argv)
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    known_args, _ = parse_arguments(sys.argv)
+
+    with local_project_id_override(known_args.project_id):
+        for namespace, builders in VIEW_BUILDERS_BY_NAMESPACE.items():
+            # TODO(#5785): Clarify use case of BigQueryViewNamespace filter (see ticket for more)
+            create_dataset_and_deploy_views_for_view_builders(bq_view_namespace=namespace,
+                                                              view_builders_to_update=builders)
