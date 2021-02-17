4febcfbdc44c03ade8fdd141f95fbd7929368a91
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2021-02-16 19:29:34 -0500

    Move read_potential_match_db_persons to BaseStateMatchingDelegate with default (recidiviz/recidiviz-data#5869)
    GitOrigin-RevId: 61ddf8e82e1398e5012d8cd9ea12d1dffed0a8d4

--- recidiviz/persistence/entity_matching/state/us_pa/us_pa_matching_delegate.py
+++ recidiviz/persistence/entity_matching/state/us_pa/us_pa_matching_delegate.py
@@ -17,37 +17,21 @@
 """Contains logic for US_PA specific entity matching overrides."""
 
 import logging
-from typing import List, Type, Optional
+from typing import List, Optional
 
-from recidiviz.persistence.database.database_entity import DatabaseEntity
 from recidiviz.persistence.database.schema.state import schema
-from recidiviz.persistence.database.session import Session
 from recidiviz.persistence.entity_matching import entity_matching_utils
 from recidiviz.persistence.entity_matching.entity_matching_types import EntityTree
 from recidiviz.persistence.entity_matching.state.base_state_matching_delegate import BaseStateMatchingDelegate
 from recidiviz.persistence.entity_matching.state.state_date_based_matching_utils import \
     move_violations_onto_supervision_periods_for_person
-from recidiviz.persistence.entity_matching.state.state_matching_utils import read_persons_by_root_entity_cls, \
-    nonnull_fields_entity_match
+from recidiviz.persistence.entity_matching.state.state_matching_utils import nonnull_fields_entity_match
 
 
 class UsPaMatchingDelegate(BaseStateMatchingDelegate):
     """Class that contains matching logic specific to US_PA."""
     def __init__(self):
-        super().__init__('us_pa')
-
-    def read_potential_match_db_persons(
-            self,
-            session: Session,
-            ingested_persons: List[schema.StatePerson]
-    ) -> List[schema.StatePerson]:
-        """Reads and returns all persons from the DB that are needed for entity matching in this state, given the
-        |ingested_persons|.
-        """
-        allowed_root_entity_classes: List[Type[DatabaseEntity]] = [schema.StatePerson, schema.StateSentenceGroup]
-        db_persons = read_persons_by_root_entity_cls(
-            session, self.region_code, ingested_persons, allowed_root_entity_classes)
-        return db_persons
+        super().__init__('us_pa', [schema.StatePerson, schema.StateSentenceGroup])
 
     def get_non_external_id_match(
             self, ingested_entity_tree: EntityTree,
--- recidiviz/persistence/entity_matching/templates/us_xx/us_xx_matching_delegate.py
+++ recidiviz/persistence/entity_matching/templates/us_xx/us_xx_matching_delegate.py
@@ -16,29 +16,10 @@
 # =============================================================================
 """Contains logic for US_XX specific entity matching overrides."""
 
-from typing import List, Type
-
-from recidiviz.persistence.database.database_entity import DatabaseEntity
-from recidiviz.persistence.database.schema.state import schema
-from recidiviz.persistence.database.session import Session
 from recidiviz.persistence.entity_matching.state.base_state_matching_delegate import BaseStateMatchingDelegate
-from recidiviz.persistence.entity_matching.state.state_matching_utils import read_persons_by_root_entity_cls
 
 
 class UsXxMatchingDelegate(BaseStateMatchingDelegate):
     """Class that contains matching logic specific to US_XX."""
     def __init__(self) -> None:
         super().__init__('us_xx')
-
-    def read_potential_match_db_persons(
-            self,
-            session: Session,
-            ingested_persons: List[schema.StatePerson]
-    ) -> List[schema.StatePerson]:
-        """Reads and returns all persons from the DB that are needed for entity matching in this state, given the
-        |ingested_persons|.
-        """
-        allowed_root_entity_classes: List[Type[DatabaseEntity]] = [schema.StatePerson, schema.StateSentenceGroup]
-        db_persons = read_persons_by_root_entity_cls(
-            session, self.region_code, ingested_persons, allowed_root_entity_classes)
-        return db_persons
