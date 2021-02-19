83a9526836ec8db113d4efd27e32cf911a59e75c
Colin Adams <colinadams@google.com>
2021-02-19 09:36:51 -0800

    Only patch if apache_beam is present (Recidiviz/recidiviz-data#5925)
    GitOrigin-RevId: 4a93f5a9242fb03cd70ccb7efed4cada22322013

--- recidiviz/calculator/pipeline/__init__.py
+++ recidiviz/calculator/pipeline/__init__.py
@@ -17,32 +17,35 @@
 """Pipeline-based calculations."""
 import logging
 
-from apache_beam.io.gcp.bigquery_tools import BigQueryWrapper
-from apache_beam.io.gcp.internal.clients import bigquery
+try:
+    from apache_beam.io.gcp.bigquery_tools import BigQueryWrapper
+    from apache_beam.io.gcp.internal.clients import bigquery
 
-def _start_job(self: BigQueryWrapper, request: bigquery.BigqueryJobsInsertRequest) -> bigquery.Job:
-    """Inserts a BigQuery job.
+    def _start_job(self: BigQueryWrapper, request: bigquery.BigqueryJobsInsertRequest) -> bigquery.Job:
+        """Inserts a BigQuery job.
 
-    Raises an error if the insert fails, even if the job already exists.
-    """
-    logging.info("Using patched _start_job")
-    try:
-        response: bigquery.Job = self.client.jobs.Insert(request)
-        logging.info(
-            "Started BigQuery job: %s\n "
-            "bq show -j --format=prettyjson --project_id=%s %s",
-            response.jobReference,
-            response.jobReference.projectId,
-            response.jobReference.jobId)
-        return response
-    except Exception as exn:
-        logging.error(
-            "Failed to insert job %s: %s", request.job.jobReference, exn)
-        raise
+        Raises an error if the insert fails, even if the job already exists.
+        """
+        logging.info("Using patched _start_job")
+        try:
+            response: bigquery.Job = self.client.jobs.Insert(request)
+            logging.info(
+                "Started BigQuery job: %s\n "
+                "bq show -j --format=prettyjson --project_id=%s %s",
+                response.jobReference,
+                response.jobReference.projectId,
+                response.jobReference.jobId)
+            return response
+        except Exception as exn:
+            logging.error(
+                "Failed to insert job %s: %s", request.job.jobReference, exn)
+            raise
 
-# Replaces the default implementation of _start_job with one that raises all errors.
-# The default implementation suppresses 409s which can cause our dataflow pipelines to silently omit data. See
-# #5152, as well as the default implementation:
-# https://github.com/apache/beam/blob/master/sdks/python/apache_beam/io/gcp/bigquery_tools.py#L440
-logging.info('Patching BigQueryWrapper._start_job')
-setattr(BigQueryWrapper, "_start_job", _start_job)
+    # Replaces the default implementation of _start_job with one that raises all errors.
+    # The default implementation suppresses 409s which can cause our dataflow pipelines to silently omit data. See
+    # #5152, as well as the default implementation:
+    # https://github.com/apache/beam/blob/master/sdks/python/apache_beam/io/gcp/bigquery_tools.py#L440
+    logging.info('Patching BigQueryWrapper._start_job')
+    setattr(BigQueryWrapper, "_start_job", _start_job)
+except ImportError:
+    logging.info('Skipping patch; apache_beam is not present')
