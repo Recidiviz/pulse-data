3c907a040e9349e67e405c512e6d5d0374f9b64d
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-03-01 17:32:26 -0500

    Check that end date is enforced (Recidiviz/recidiviz-data#6124)
    GitOrigin-RevId: b2d82c75dbabe9641eeac74b13d96fcf23477cb0

--- recidiviz/case_triage/views/view_config.py
+++ recidiviz/case_triage/views/view_config.py
@@ -20,7 +20,7 @@ from typing import Sequence
 from recidiviz.big_query.big_query_view import BigQueryViewBuilder
 from recidiviz.big_query.selected_columns_big_query_view import SelectedColumnsBigQueryViewBuilder
 from recidiviz.case_triage.views.client_list_view import CLIENT_LIST_VIEW_BUILDER
-from recidiviz.case_triage.views.employment_periods import EMPLOYMENT_PERIODS_VIEW_BUILDER
+from recidiviz.case_triage.views.employment_periods import CURRENT_EMPLOYMENT_PERIODS_VIEW_BUILDER
 from recidiviz.case_triage.views.latest_assessments import LATEST_ASSESSMENTS_VIEW_BUILDER
 from recidiviz.case_triage.views.officer_list_view import OFFICER_LIST_VIEW_BUILDER
 
@@ -31,7 +31,7 @@ CASE_TRIAGE_EXPORTED_VIEW_BUILDERS: Sequence[SelectedColumnsBigQueryViewBuilder]
 
 VIEW_BUILDERS_FOR_VIEWS_TO_UPDATE: Sequence[BigQueryViewBuilder] = [
     CLIENT_LIST_VIEW_BUILDER,
-    EMPLOYMENT_PERIODS_VIEW_BUILDER,
+    CURRENT_EMPLOYMENT_PERIODS_VIEW_BUILDER,
     LATEST_ASSESSMENTS_VIEW_BUILDER,
     OFFICER_LIST_VIEW_BUILDER,
 ]
