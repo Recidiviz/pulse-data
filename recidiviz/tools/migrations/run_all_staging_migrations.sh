3f1041c325953d09f61df7523112a7dac9b699b0
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-03-10 13:16:52 -0500

    Adds script to run all staging migrations (Recidiviz/recidiviz-data#6324)
    GitOrigin-RevId: 3b71edb3e6312f9cd787c70dfd3aa8c6a85e7aa1

---
+++ recidiviz/tools/migrations/run_all_staging_migrations.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
+source ${BASH_SOURCE_DIR}/../script_base.sh
+
+echo 'Running staging migrations against all databases.'
+script_prompt 'Are you running this script from prod-data-client?'
+
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JAILS --project-id recidiviz-staging --ssl-cert-path ~/dev_data_certs/
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database STATE --project-id recidiviz-staging --ssl-cert-path ~/dev_state_data_certs/
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database OPERATIONS --project-id recidiviz-staging --ssl-cert-path ~/dev_operations_data_certs/
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JUSTICE_COUNTS --project-id recidiviz-staging --ssl-cert-path ~/dev_justice_counts_data_certs/
+run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database CASE_TRIAGE --project-id recidiviz-staging --ssl-cert-path ~/dev_case_triage_certs/
