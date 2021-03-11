aa0e40e422ab689e62815a9876023df853f2938d
Colin Adams <colinadams@google.com>
2021-03-11 11:00:36 -0800

    Use pre-commit library to manage pre commits (Recidiviz/recidiviz-data#6323)
    GitOrigin-RevId: 98c5d8c2f82685766d54f32e8e284b7f384f0de1

--- recidiviz/tools/docs/documentation_generator.py
+++ recidiviz/tools/docs/documentation_generator.py
@@ -20,8 +20,10 @@
 import logging
 import os
 import subprocess
+import sys
 
-from typing import Set
+import argparse
+from typing import List, Optional, Sequence, Set
 
 import recidiviz
 from recidiviz.ingest.direct import regions as regions_module
@@ -32,11 +34,13 @@ from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
 
-def generate_raw_data_documentation_for_region(region_code: str) -> None:
+def generate_raw_data_documentation_for_region(region_code: str) -> bool:
     """
     Parses the files available under `recidiviz/ingest/direct/regions/{region_code}/raw_data/` to produce documentation
     which is suitable to be added to the region ingest specification. Overwrites or creates the markdown file
     for a given region.
+
+    Returns True if files were modified, False otherwise.
     """
     documentation_generator = DirectIngestDocumentationGenerator()
     documentation = documentation_generator.generate_raw_file_docs_for_region(
@@ -46,41 +50,61 @@ def generate_raw_data_documentation_for_region(region_code: str) -> None:
     markdown_file_path = os.path.join(
         ingest_docs_path, f"{region_code.lower()}/raw_data.md"
     )
-    with open(markdown_file_path, "w") as raw_data_md_file:
-        raw_data_md_file.write(documentation)
-    res = subprocess.Popen(
-        f"git add {markdown_file_path}", shell=True, stdout=subprocess.PIPE
+    with open(markdown_file_path, "r") as raw_data_md_file:
+        prior_documentation = raw_data_md_file.read()
+    if prior_documentation != documentation:
+        with open(markdown_file_path, "w") as raw_data_md_file:
+            raw_data_md_file.write(documentation)
+            return True
+    return False
+
+
+def get_touched_raw_data_regions(touched_files: Optional[List[str]]) -> Set[str]:
+    """Returns the touched regions' codes.
+
+    If modified files are not provided, greps for touched files in the direct ingest
+    regions directories.
+    """
+
+    if not touched_files:
+        regions_dir_path = os.path.relpath(
+            os.path.dirname(regions_module.__file__),
+            os.path.dirname(os.path.dirname(recidiviz.__file__)),
+        )
+        res = subprocess.Popen(
+            f'git diff --cached --name-only | grep "{regions_dir_path}"',
+            shell=True,
+            stdout=subprocess.PIPE,
+        )
+        stdout, _stderr = res.communicate()
+        touched_files = stdout.decode().splitlines()
+
+    return {file.split("/")[4] for file in touched_files if os.path.isdir(file)}
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "filenames",
+        nargs="*",
+        help="Modified files to indicate which regions need their docs to be regenerated. "
+        "Paths must be relative to the root of the repository. "
+        "If none are provided, will use `git diff` to determine modified files.",
     )
-    _stdout, _stderr = res.communicate()
+    args = parser.parse_args(argv)
 
-
-def get_touched_raw_data_regions() -> Set[str]:
-    """Greps for touched files in the direct ingest regions directories and returns the touched regions' codes"""
-
-    regions_dir_path = os.path.relpath(
-        os.path.dirname(regions_module.__file__),
-        os.path.dirname(os.path.dirname(recidiviz.__file__)),
-    )
-    res = subprocess.Popen(
-        f'git diff --cached --name-only | grep "{regions_dir_path}" | cut -d / -f 5',
-        shell=True,
-        stdout=subprocess.PIPE,
-    )
-    stdout, _stderr = res.communicate()
-    return set(stdout.decode().upper().splitlines())
-
-
-def main() -> None:
     # Arbitrary project ID - we just need to build views in order to obtain raw table dependencies
     with local_project_id_override(GCP_PROJECT_STAGING):
-        touched_raw_data_regions = get_touched_raw_data_regions()
+        modified = False
+        touched_raw_data_regions = get_touched_raw_data_regions(args.filenames)
         for region_code in touched_raw_data_regions:
             logging.info(
                 "Generating raw data documentation for region [%s]", region_code
             )
-            generate_raw_data_documentation_for_region(region_code)
+            modified |= generate_raw_data_documentation_for_region(region_code)
+        return 1 if modified else 0
 
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
-    main()
+    sys.exit(main())
