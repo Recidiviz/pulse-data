ec61e93286ef9e50efdb9b47ca7bf94eeeb81bdf
Emily Chao <emily@recidiviz.org>
2021-03-12 08:56:19 -0800

    expanding connection pool limits (recidiviz/recidiviz-data#6398)
    GitOrigin-RevId: 51cdda7bae8ba8b904c575e8c1318819873309eb

--- recidiviz/cloud_storage/gcsfs_factory.py
+++ recidiviz/cloud_storage/gcsfs_factory.py
@@ -15,12 +15,25 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """Factory for GCSFileSystem objects"""
+import requests
+
 from google.cloud import storage
 
 from recidiviz.cloud_storage.gcs_file_system import GCSFileSystem, GCSFileSystemImpl
+
+# Adjust these variables if there are connection pool full errors to storage.googleapis
+POOL_CONNECTIONS = 128
+POOL_MAXSIZE = 128
 
 
 class GcsfsFactory:
     @classmethod
     def build(cls) -> GCSFileSystem:
-        return GCSFileSystemImpl(storage.Client())
+        storage_client = storage.Client()
+        adapter = requests.adapters.HTTPAdapter(
+            pool_connections=POOL_CONNECTIONS, pool_maxsize=POOL_MAXSIZE
+        )
+        # pylint: disable=protected-access
+        storage_client._http.mount("https://", adapter)
+        storage_client._http._auth_request.session.mount("https://", adapter)
+        return GCSFileSystemImpl(storage_client)
