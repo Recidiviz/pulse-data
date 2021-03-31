ab5debd9caf5346aeba20317e422fafb6770d1de
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2021-03-31 16:53:28 -0400

    [US_ND] Add StateProgramAssignmentParticipationStatus enum and update mappings (Recidiviz/recidiviz-data#6657)
    * [skip validation state ingest_docs] add refused as participation status and update mappings
    
    * fix enums in migration
    
    * add to schema
    
    GitOrigin-RevId: 7a3789b69c4ac623c28e929be2edf3c5df416f6d

---
+++ recidiviz/persistence/database/migrations/state/versions/2021_03_30_1123_bed035a0cd79_us_nd_update_participation_status.py
@@ -0,0 +1,65 @@
+# pylint: skip-file
+"""us_nd_update_participation_status
+
+Revision ID: bed035a0cd79
+Revises: 26f50952aaab
+Create Date: 2021-03-30 11:23:44.284084
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "bed035a0cd79"
+down_revision = "26f50952aaab"
+branch_labels = None
+depends_on = None
+
+
+REFUSED_STATUS_QUERY = (
+    "SELECT external_id FROM"
+    " {table_name}"
+    " WHERE participation_status_raw_text LIKE '%%REFUSED%%' AND state_code = 'US_ND'"
+)
+
+
+UPDATE_QUERY = (
+    "UPDATE {table_name} SET participation_status = '{new_value}'"
+    " WHERE external_id IN ({ids_query});"
+)
+
+PROGRAM_ASSIGNMENT_TABLE_NAME = "state_program_assignment"
+PROGRAM_ASSIGNMENT_HISTORY_TABLE_NAME = "state_program_assignment_history"
+
+
+def upgrade():
+    updated_participation_status = "REFUSED"
+    with op.get_context().autocommit_block():
+        for table_name in [
+            PROGRAM_ASSIGNMENT_TABLE_NAME,
+            PROGRAM_ASSIGNMENT_HISTORY_TABLE_NAME,
+        ]:
+            op.execute(
+                UPDATE_QUERY.format(
+                    table_name=table_name,
+                    new_value=updated_participation_status,
+                    ids_query=REFUSED_STATUS_QUERY.format(table_name=table_name),
+                )
+            )
+
+
+def downgrade():
+    updated_participation_status = "EXTERNAL_UNKNOWN"
+    with op.get_context().autocommit_block():
+        for table_name in [
+            PROGRAM_ASSIGNMENT_TABLE_NAME,
+            PROGRAM_ASSIGNMENT_HISTORY_TABLE_NAME,
+        ]:
+            op.execute(
+                UPDATE_QUERY.format(
+                    table_name=table_name,
+                    new_value=updated_participation_status,
+                    ids_query=REFUSED_STATUS_QUERY.format(table_name=table_name),
+                )
+            )
