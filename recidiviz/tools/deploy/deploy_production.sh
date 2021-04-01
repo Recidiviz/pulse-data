b78cecdfa2a021234dcce5cbd6d8b9d2fffa5626
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-04-01 18:01:25 -0400

    Run gcloud compute ssh without run_cmd (recidiviz/recidiviz-data#6738)
    GitOrigin-RevId: 0cae94d8dde6c650142c577715cfb25c1d1c875c

--- recidiviz/tools/deploy/deploy_production.sh
+++ recidiviz/tools/deploy/deploy_production.sh
@@ -52,8 +52,10 @@ then
 fi
 
 echo "Running migrations on prod-data-client. You may have to enter the passphrase for your ssh key to continue."
-run_cmd gcloud compute ssh --ssh-flag="-t" prod-data-client --command "cd pulse-data && git fetch && git checkout $LAST_DEPLOYED_GIT_VERSION_TAG \
+# The remote migration execution script doesn't play nice with run_cmd
+gcloud compute ssh --ssh-flag="-t" prod-data-client --command "cd pulse-data && git fetch && git checkout $LAST_DEPLOYED_GIT_VERSION_TAG \
     && pipenv sync --dev && pipenv run ./recidiviz/tools/migrations/run_all_prod_migrations.sh"
+exit_on_fail
 
 # Use rev-list to get the hash of the commit that the tag points to, rev-parse parse
 # returns the hash of the tag itself.
