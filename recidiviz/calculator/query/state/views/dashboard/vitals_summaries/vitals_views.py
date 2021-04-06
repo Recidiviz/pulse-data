10000902c3a994056fccf829b525e22594f9a134
caroletouma <carole@recidiviz.org>
2021-04-06 10:13:17 -0400

    Add Vitals Summaries Views to Dashboard (recidiviz/recidiviz-data#6704)
    * initial setup
    
    * add vitals summaries views
    
    * fix queries
    
    * more fixes
    
    * more fixes
    
    * add state level and entity type
    
    * add to vitals_views
    
    * nit fix
    
    * fix comments, update for test fix
    
    * remove indiv files and refactor query
    
    * fix query
    
    * address tony's comments
    
    GitOrigin-RevId: eb236d590b0db5bf524ba204bf8e604fa468bb17

---
+++ recidiviz/calculator/query/state/views/dashboard/vitals_summaries/vitals_views.py
@@ -0,0 +1,26 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Dashboard views related to vitals."""
+from typing import List
+
+from recidiviz.calculator.query.state.views.dashboard.vitals_summaries.vitals_summaries import (
+    VITALS_SUMMARIES_VIEW_BUILDER,
+)
+from recidiviz.metrics.metric_big_query_view import MetricBigQueryViewBuilder
+
+
+VITALS_VIEW_BUILDERS: List[MetricBigQueryViewBuilder] = [VITALS_SUMMARIES_VIEW_BUILDER]
