07673ef5b1d9ed89b9d73579d54881fee52c6d7e
caroletouma <carole@recidiviz.org>
2021-04-06 10:55:16 -0400

    Add New `StateSpecializedPurposeForIncarceration.TEMPORARY_CUSTODY` (Recidiviz/recidiviz-data#6648)
    * add new StateSpecializedPurposeForIncarceration
    
    * add temp custody
    
    * populate temp custody
    
    * add to default mapping
    
    * remove db migration
    
    * add new migration
    
    * populate migration
    
    Co-authored-by: helperbot-recidiviz <helperbot@recidiviz.com>
    GitOrigin-RevId: dd348db6046c869c526155733ba3c745e9faca64

---
+++ recidiviz/persistence/database/migrations/state/versions/2021_04_06_1000_29880de90cc5_add_new_temporary_custody.py
@@ -0,0 +1,82 @@
+# pylint: skip-file
+"""add_new_temporary_custody
+
+Revision ID: 29880de90cc5
+Revises: 60e6b436fe15
+Create Date: 2021-04-06 10:00:10.124594
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "29880de90cc5"
+down_revision = "60e6b436fe15"
+branch_labels = None
+depends_on = None
+
+# Without `TEMPORARY_CUSTODY`
+old_values = [
+    "EXTERNAL_UNKNOWN",
+    "INTERNAL_UNKNOWN",
+    "GENERAL",
+    "PAROLE_BOARD_HOLD",
+    "SHOCK_INCARCERATION",
+    "TREATMENT_IN_PRISON",
+]
+
+# With `TEMPORARY_CUSTODY`
+new_values = [
+    "EXTERNAL_UNKNOWN",
+    "INTERNAL_UNKNOWN",
+    "GENERAL",
+    "PAROLE_BOARD_HOLD",
+    "SHOCK_INCARCERATION",
+    "TREATMENT_IN_PRISON",
+    "TEMPORARY_CUSTODY",
+]
+
+
+def upgrade():
+    op.execute(
+        "ALTER TYPE state_specialized_purpose_for_incarceration RENAME TO state_specialized_purpose_for_incarceration_old;"
+    )
+    sa.Enum(*new_values, name="state_specialized_purpose_for_incarceration").create(
+        bind=op.get_bind()
+    )
+    op.alter_column(
+        "state_incarceration_period",
+        column_name="specialized_purpose_for_incarceration",
+        type_=sa.Enum(*new_values, name="state_specialized_purpose_for_incarceration"),
+        postgresql_using="specialized_purpose_for_incarceration::text::state_specialized_purpose_for_incarceration",
+    )
+    op.alter_column(
+        "state_incarceration_period_history",
+        column_name="specialized_purpose_for_incarceration",
+        type_=sa.Enum(*new_values, name="state_specialized_purpose_for_incarceration"),
+        postgresql_using="specialized_purpose_for_incarceration::text::state_specialized_purpose_for_incarceration",
+    )
+    op.execute("DROP TYPE state_specialized_purpose_for_incarceration_old;")
+
+
+def downgrade():
+    op.execute(
+        "ALTER TYPE state_specialized_purpose_for_incarceration RENAME TO state_specialized_purpose_for_incarceration_old;"
+    )
+    sa.Enum(*old_values, name="state_specialized_purpose_for_incarceration").create(
+        bind=op.get_bind()
+    )
+    op.alter_column(
+        "state_incarceration_period",
+        column_name="specialized_purpose_for_incarceration",
+        type_=sa.Enum(*old_values, name="state_specialized_purpose_for_incarceration"),
+        postgresql_using="specialized_purpose_for_incarceration::text::state_specialized_purpose_for_incarceration",
+    )
+    op.alter_column(
+        "state_incarceration_period_history",
+        column_name="specialized_purpose_for_incarceration",
+        type_=sa.Enum(*old_values, name="state_specialized_purpose_for_incarceration"),
+        postgresql_using="specialized_purpose_for_incarceration::text::state_specialized_purpose_for_incarceration",
+    )
+    op.execute("DROP TYPE state_specialized_purpose_for_incarceration_old;")
