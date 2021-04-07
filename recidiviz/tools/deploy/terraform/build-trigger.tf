21dee4e957ecb5d1c01727b09b0c0079e7ff3f83
Emily Chao <emily@recidiviz.org>
2021-04-07 09:37:43 -0700

    Setting up Terraform for the cloud build trigger (Recidiviz/recidiviz-data#6799)
    * setting up terraform for the cloud build trigger
    
    * adding tags and also updating branch regex
    
    GitOrigin-RevId: ae27a3132e51555704edadd500eeea7234c1e1f4

---
+++ recidiviz/tools/deploy/terraform/build-trigger.tf
@@ -0,0 +1,41 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+resource "google_cloudbuild_trigger" "staging_release_build_trigger" {
+  provider    = "google-beta"
+  description = "Builds a remote Docker image for staging on every push to master."
+  count       = var.project_id == "recidiviz-staging" ? 1 : 0
+
+  github {
+    owner = "Recidiviz"
+    name  = "pulse-data"
+    push {
+      branch = "^master$|^releases/v[0-9]+.[0-9]+-rc$"
+    }
+  }
+
+  build {
+    step {
+      name = "gcr.io/kaniko-project/executor:latest"
+      args = ["--destination=us.gcr.io/$PROJECT_ID/appengine/build:$COMMIT_SHA", "--cache=true"]
+    }
+    options {
+      machine_type = "N1_HIGHCPU_32"
+    }
+    tags = ["$COMMIT_SHA", "$BRANCH_NAME"]
+    timeout = "3600s"
+  }
+}
