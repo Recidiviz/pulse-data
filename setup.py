8c9444fc0672cdf1673d9733cde008a3a9fbfa66
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-04-08 17:02:40 -0400

    Rollback beam (Recidiviz/recidiviz-data#6853)
    * Rollback apache-beam to 2.27.
    
    GitOrigin-RevId: d2d0b3d6de85e0bcc4a7c5441e8a699217102b44

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ REQUIRED_PACKAGES = [
     "opencensus-correlation",
     "opencensus-ext-stackdriver",
     # Must stay up-to-date with latest protobuf version in the Pipfile
-    "protobuf==3.15.7",
+    "protobuf==3.15.8",
     "SQLAlchemy",
     "us",
 ]
