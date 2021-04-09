2aa54f2ce0f6542d2eb45021db3ec895297a2e01
Emily Chao <emily@recidiviz.org>
2021-04-09 12:17:02 -0600

    Adding stubs for `google.auth` (Recidiviz/recidiviz-data#6880)
    * adding stubs for google.auth
    
    * addressing feedback comments
    
    * addressing review comments
    
    * ignored default in sqladmin
    
    GitOrigin-RevId: 0dfdb5ca694aa5ec92096f1cf9169e2cb126be31

--- stubs/google/auth/app_engine.pyi
+++ stubs/google/auth/app_engine.pyi
@@ -1,5 +1,5 @@
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2019 Recidiviz, Inc.
+# Copyright (C) 2021 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,17 +14,3 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""Accessor for a global instance of the sqladmin client."""
-
-import google
-from googleapiclient import discovery
-
-_client = None
-
-
-def sqladmin_client() -> discovery.Resource:
-    global _client
-    if not _client:
-        credentials, _ = google.auth.default()  # type: ignore[attr-defined]
-        _client = discovery.build("sqladmin", "v1beta4", credentials=credentials)
-    return _client
