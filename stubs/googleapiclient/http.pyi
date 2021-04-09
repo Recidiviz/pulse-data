09c1a64e92c74c90d3444ad914099d0efa73f494
Emily Chao <emily@recidiviz.org>
2021-04-09 11:31:08 -0600

    Adding stubs for `googleapiclient` (recidiviz/recidiviz-data#6881)
    * adding stubs for googleapiclient
    
    * partial stubbing
    
    GitOrigin-RevId: 9f58422c9acda7669dc0a169779edefe074e25f1

---
+++ stubs/googleapiclient/http.pyi
@@ -0,0 +1,27 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+import io
+from typing import Tuple
+
+class HttpRequest: ...
+
+class MediaDownloadProgress:
+    def progress(self) -> float: ...
+
+class MediaIoBaseDownload:
+    def __init__(self, fh: io.FileIO, request: HttpRequest) -> None: ...
+    def next_chunk(self) -> Tuple[MediaDownloadProgress, bool]: ...
