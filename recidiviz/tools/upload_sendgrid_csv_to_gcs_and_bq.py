2206324d95e6fddecd373c784d3b245271118e99
madeleine-charity <madeleine.charity@gmail.com>
2021-04-13 18:40:33 -0500

    Adding columns to our sendgrid data storage (Recidiviz/recidiviz-data#6948)
    GitOrigin-RevId: f8c11289cace22b2c7dd04a32386086a62040e64

--- recidiviz/tools/upload_sendgrid_csv_to_gcs_and_bq.py
+++ recidiviz/tools/upload_sendgrid_csv_to_gcs_and_bq.py
@@ -19,11 +19,19 @@
 Script to upload raw SendGrid CSV to Google Cloud Storage and add new rows to BigQuery
 table.
 
+Also has --backfill flag for when schema is updated or files are updated. Use flag and
+all rows in current permanent BQ table will be deleted and filled with updated rows
+from CSVs in GCS.
+
+Intended use is only one of --backfill or --local-filepath flags to be used at a time.
+However when both flags are given, the new file will be uploaded and then the permanent
+table will be backfilled with all files in GCS, including the new file.
+
 To get data, go to https://app.sendgrid.com/, from the side bar, Activity > search for
 all activity (in last 30 days) > Export CSV. The site will send you an email with a
 link to download the CSV from. Upload that CSV using this script.
 
-Usage:
+Most common usage, this uploads a file from sendgrid to GCS and BQ:
 
 python -m recidiviz.tools.upload_sendgrid_csv_to_gcs_and_bq
     --project-id [project-id]
@@ -34,6 +42,12 @@ Example usage (run from `pipenv shell`):
 python -m recidiviz.tools.upload_sendgrid_csv_to_gcs_and_bq
     --project-id recidiviz-staging
     --local-filepath ~/Downloads/example.csv
+
+Back fill usage:
+
+python -m recidiviz.tools.upload_sendgrid_csv_to_gcs_and_bq
+    --project-id [project-id]
+    --backfill true
 """
 
 import argparse
@@ -45,35 +59,86 @@ from typing import List
 from google.cloud import bigquery
 from google.cloud.bigquery import WriteDisposition
 
-from recidiviz.cloud_storage.gcs_file_system import GcsfsFileContentsHandle
+from recidiviz.cloud_storage.gcs_file_system import (
+    GcsfsFileContentsHandle,
+    GCSFileSystem,
+)
 from recidiviz.big_query.big_query_client import BigQueryClientImpl
 from recidiviz.cloud_storage.gcsfs_factory import GcsfsFactory
 from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
 from recidiviz.persistence.database.bq_refresh.bq_refresh import wait_for_table_load
+from recidiviz.utils.environment import GCP_PROJECT_STAGING, GCP_PROJECT_PRODUCTION
 
 BUCKET_SUFFIX = "-sendgrid-data"
 DATASET_ID = "sendgrid_email_data"
 FINAL_DESTINATION_TABLE = "raw_sendgrid_email_data"
 TEMP_DESTINATION_TABLE = "temp_for_upload"
 DATE_FORMAT = "%Y.%m.%d"
-QUERY_TEMPLATE = """
-SELECT processed, message_id, event, email 
-FROM `{project_id}.sendgrid_email_data.{temp_table}` 
+INSERT_QUERY_TEMPLATE = """
+SELECT processed, subject, api_key_id, message_id, event, email 
+FROM `{project_id}.{dataset_id}.{temp_table}` 
 EXCEPT DISTINCT 
-SELECT processed, message_id, event, email 
-FROM `{project_id}.sendgrid_email_data.{final_table}` 
+SELECT processed, subject, api_key_id, message_id, event, email 
+FROM `{project_id}.{dataset_id}.{final_table}` 
 ORDER BY processed"""
 
 
-def main(*, project_id: str, local_filepath: str) -> None:
-    """Uploads file at filepath to Google Cloud Storage and adds rows that do not
-    already exist in BigQuery table"""
+def main(*, project_id: str, local_filepath: str, backfill: bool) -> None:
+    """If filepath, uploads file at filepath to Google Cloud Storage and adds rows that
+     do not already exist in BigQuery table.
+    If backfill, loads all rows in CSVs in GCS to BQ table.
+    """
 
-    # Upload raw Sendgrid CSV to GCS
     fs = GcsfsFactory.build()
+    bq_client = BigQueryClientImpl(project_id=project_id)
+
+    # If backfill, clear out all data in BQ table and load in new rows
+    if backfill and (
+        input(
+            "Are you sure? This action will delete all data from current raw data "
+            "table and replace it with rows from CSVs currently in GCS. "
+            "Enter 'backfill' if you are sure. \n"
+        )
+        == "backfill"
+    ):
+        # Clear out old rows from table
+        bq_client.delete_from_table_async(
+            dataset_id=DATASET_ID,
+            table_id=FINAL_DESTINATION_TABLE,
+            filter_clause="WHERE TRUE",
+        )
+        # For each file in table, load into BQ
+        for blob in fs.ls_with_blob_prefix(f"{project_id}{BUCKET_SUFFIX}", ""):
+            if isinstance(blob, GcsfsFilePath):
+                logging.info(
+                    "Back filling from blob [%s] in bucket [%s]",
+                    blob.file_name,
+                    f"{project_id}{BUCKET_SUFFIX}",
+                )
+                load_from_gcs_to_temp_table(bq_client, project_id, blob.file_name)
+                load_from_temp_to_permanent_table(bq_client, project_id)
+
+    # If local file path was provided, upload that file to GCS and load data into BQ
+    if local_filepath:
+        # If local file path provided, upload file at file path into GCS
+        upload_raw_file_to_gcs(fs, local_filepath, f"{project_id}{BUCKET_SUFFIX}")
+        logging.info("Found local file path, uploading from [%s]", local_filepath)
+
+        # Load data to temporary table and then to permanent
+        load_from_gcs_to_temp_table(
+            bq_client, project_id, date.today().strftime(DATE_FORMAT)
+        )
+        load_from_temp_to_permanent_table(bq_client, project_id)
+
+
+def upload_raw_file_to_gcs(
+    fs: GCSFileSystem, local_filepath: str, bucket_name: str
+) -> None:
+    """Upload raw Sendgrid CSV to GCS"""
+
     fs.upload_from_contents_handle_stream(
         path=GcsfsFilePath(
-            bucket_name=project_id + BUCKET_SUFFIX,
+            bucket_name=bucket_name,
             blob_name=date.today().strftime(DATE_FORMAT),
         ),
         contents_handle=GcsfsFileContentsHandle(
@@ -84,16 +149,18 @@ def main(*, project_id: str, local_filepath: str) -> None:
     logging.info(
         "Uploaded file [%s] to Google Cloud Storage bucket name=[%s] blob name=[%s]",
         local_filepath,
-        project_id + BUCKET_SUFFIX,
+        bucket_name,
         date.today().strftime(DATE_FORMAT),
     )
 
-    bq_client = BigQueryClientImpl(project_id=project_id)
 
-    # Upload to BQ from GCS
+def load_from_gcs_to_temp_table(
+    bq_client: BigQueryClientImpl, project_id: str, blob_name: str
+) -> None:
+    """Upload raw data from GCS to temporary BQ table """
+    bucket_name = f"{project_id}{BUCKET_SUFFIX}"
     load_job = bq_client.load_table_from_cloud_storage_async(
-        source_uri=f"gs://{project_id}{BUCKET_SUFFIX}/"
-        f"{date.today().strftime(DATE_FORMAT)}",
+        source_uri=f"gs://{bucket_name}/" f"{blob_name}",
         destination_dataset_ref=bigquery.DatasetReference(
             project=project_id,
             dataset_id=DATASET_ID,
@@ -141,7 +208,11 @@ def main(*, project_id: str, local_filepath: str) -> None:
         logging.info("Copy from cloud storage to temporary table failed")
         return
 
-    # Upload non-duplicate rows to existing table
+
+def load_from_temp_to_permanent_table(
+    bq_client: BigQueryClientImpl, project_id: str
+) -> None:
+    """Query temporary table and persist view to permanent table"""
     num_rows_before = bq_client.get_table(
         dataset_ref=bigquery.DatasetReference(
             project=project_id,
@@ -153,8 +224,9 @@ def main(*, project_id: str, local_filepath: str) -> None:
     insert_job = bq_client.insert_into_table_from_query(
         destination_dataset_id=DATASET_ID,
         destination_table_id=FINAL_DESTINATION_TABLE,
-        query=QUERY_TEMPLATE.format(
+        query=INSERT_QUERY_TEMPLATE.format(
             project_id=project_id,
+            dataset_id=DATASET_ID,
             temp_table=TEMP_DESTINATION_TABLE,
             final_table=FINAL_DESTINATION_TABLE,
         ),
@@ -179,13 +251,22 @@ def parse_arguments(argv: List[str]) -> argparse.Namespace:
     parser.add_argument(
         "--project-id",
         required=True,
+        choices=[GCP_PROJECT_STAGING, GCP_PROJECT_PRODUCTION],
         help="The project_id for the destination table",
     )
 
     parser.add_argument(
         "--local-filepath",
-        required=True,
+        required=False,
         help="The local filepath for the local csv to upload.",
+    )
+
+    parser.add_argument(
+        "--backfill",
+        required=False,
+        action="store_true",
+        help="Whether or not to backfill bigquery with all the files in GCS. "
+        "Set to 'true' to backfill",
     )
 
     known_args, _ = parser.parse_known_args(argv)
@@ -196,4 +277,8 @@ def parse_arguments(argv: List[str]) -> argparse.Namespace:
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
     args = parse_arguments(sys.argv)
-    main(local_filepath=args.local_filepath, project_id=args.project_id)
+    main(
+        local_filepath=args.local_filepath,
+        project_id=args.project_id,
+        backfill=args.backfill,
+    )
