784848953dbf9fbb24f0f7bda59d43f8d32152e9
Dan Hansen <dan@recidiviz.org>
2021-04-18 12:41:47 -0700

    Add functionality for caching ingest files as Parquet (Recidiviz/recidiviz-data#6958)
    * Add functionality for caching ingest files as Parquet
    
    * mypy stub ttl
    
    * pylint
    
    * Review feedback
    
    GitOrigin-RevId: a20192d3902db574181541fb811c3d1875d23ab7

---
+++ recidiviz/admin_panel/data_discovery/cache_ingest_file_as_parquet.py
@@ -0,0 +1,111 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+""" Functionality for converting CSVs to Parquet format and storing in Redis
+"""
+from typing import Generator
+from io import BytesIO
+
+import pandas
+import pandas as pd
+import redis
+
+from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
+from recidiviz.ingest.direct.controllers.gcsfs_csv_reader import (
+    GcsfsCsvReaderDelegate,
+)
+from recidiviz.ingest.direct.controllers.gcsfs_direct_ingest_utils import (
+    filename_parts_from_path,
+)
+
+
+class SingleIngestFileParquetCache:
+    """Class responsible for converting dataframes to Parquet and storing to Redis.
+    A single ingest file may be broken into multiple chunks, so we store the Parquet files as a Redis list
+    """
+
+    def __init__(
+        self, cache: redis.Redis, file: GcsfsFilePath, *, expiry: int = 0
+    ) -> None:
+        self.cache = cache
+        self.expiry = expiry
+        self.cache_key = f"{file.uri()}.pq"
+
+    def clear(self) -> None:
+        """ Removes the entry from the cache """
+        self.cache.delete(self.cache_key)
+
+    def add_chunk(self, df: pandas.DataFrame) -> None:
+        """ Serializes a dataframe to parquet format; pushes to redis """
+        self.cache.rpush(self.cache_key, df.to_parquet(compression="GZIP"))
+        self.cache.expire(self.cache_key, self.expiry)
+
+    def get_parquet_files(self) -> Generator[BytesIO, None, None]:
+        file_count = self.cache.llen(self.cache_key)
+
+        for i in range(file_count):
+            parquet_file = self.cache.lindex(self.cache_key, i)
+
+            if not isinstance(parquet_file, bytes):
+                raise ValueError(
+                    f"Expected Parquet file at {self.cache_key} index {i} to be bytes type"
+                )
+
+            yield BytesIO(parquet_file)
+
+
+class CacheIngestFileAsParquetDelegate(GcsfsCsvReaderDelegate):
+    """ GcsfsCsvReaderDelegate for streaming reads to Redis"""
+
+    def __init__(
+        self, parquet_cache: SingleIngestFileParquetCache, path: GcsfsFilePath
+    ):
+        self.path = path
+        self.file_parts = filename_parts_from_path(path)
+        self.parquet_cache = parquet_cache
+        self.parquet_cache.clear()
+
+    def on_dataframe(self, _encoding: str, _chunk_num: int, df: pd.DataFrame) -> bool:
+        """ For each chunked dataframe, append the processing date column, and push to Redis """
+        df["ingest_processing_date"] = self.file_parts.utc_upload_datetime.strftime(
+            "%D"
+        )
+
+        self.parquet_cache.add_chunk(df)
+
+        # Continue to the next chunk
+        return True
+
+    def on_unicode_decode_error(self, _encoding: str, _e: UnicodeError) -> bool:
+        # The file cannot be decoded; delete cache key and don't retry
+        self.parquet_cache.clear()
+
+        return False
+
+    def on_exception(self, _encoding: str, e: Exception) -> bool:
+        # If the file cannot be successfully parsed, remove from cache
+        if isinstance(e, pandas.errors.ParserError):
+            print(e)
+            self.parquet_cache.clear()
+
+        # Re-raise exception
+        return True
+
+    def on_file_read_success(self, encoding: str) -> None:
+        pass
+
+    def on_start_read_with_encoding(self, encoding: str) -> None:
+        pass
---
+++ recidiviz/tests/admin_panel/data_discovery/cache_ingest_file_as_parquet_test.py
@@ -0,0 +1,99 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+""" Tests for cache_ingest_file_as_parquet """
+from unittest import TestCase, mock
+
+import fakeredis
+import pandas
+from freezegun import freeze_time
+
+from recidiviz.admin_panel.data_discovery.cache_ingest_file_as_parquet import (
+    SingleIngestFileParquetCache,
+    CacheIngestFileAsParquetDelegate,
+)
+from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
+
+
+class TestParquetFileCache(TestCase):
+    """ Tests for ParquetFileCache """
+
+    def setUp(self) -> None:
+        self.fakeredis = fakeredis.FakeRedis()
+
+    @freeze_time("2021-04-13 23:00:00")
+    def test_get_parquet_files(self) -> None:
+        """ Dataframes can be added to the cache; read from the cache """
+        file_path = GcsfsFilePath.from_absolute_path(
+            "gs://recidiviz-staging-bucket/file.csv"
+        )
+        expiry = 999
+        parquet_file_cache = SingleIngestFileParquetCache(
+            self.fakeredis, file_path, expiry=expiry
+        )
+        first_chunk = pandas.DataFrame(data=[[1], [2]], columns=["column"])
+        second_chunk = pandas.DataFrame(data=[[3], [4]], columns=["column"])
+
+        parquet_file_cache.add_chunk(first_chunk)
+        parquet_file_cache.add_chunk(second_chunk)
+
+        self.assertEqual(self.fakeredis.ttl(parquet_file_cache.cache_key), expiry)
+
+        dataframes = [
+            pandas.read_parquet(parquet_file)
+            for parquet_file in parquet_file_cache.get_parquet_files()
+        ]
+        self.assertEqual(len(dataframes), 2)
+        self.assertTrue(first_chunk.equals(dataframes[0]))
+        self.assertTrue(second_chunk.equals(dataframes[1]))
+
+
+class TestCacheIngestFileAsParquetDelegate(TestCase):
+    """ Tests for the CacheIngestFileAsParquetDelegate """
+
+    def setUp(self) -> None:
+        self.redis = fakeredis.FakeRedis()
+        path = GcsfsFilePath.from_absolute_path(
+            "gs://test_bucket/us_id/ingest_view/2021/01/11/unprocessed_2021-01-11T00:00:00:000000_ingest_view_test_view.csv"
+        )
+        self.cache = mock.MagicMock()
+        self.delegate = CacheIngestFileAsParquetDelegate(self.cache, path)
+
+    def test_initialize(self) -> None:
+        df = pandas.DataFrame(data=[1], columns=["x"])
+        self.assertEqual(self.delegate.on_dataframe("UTF-8", 0, df), True)
+        self.assertEqual(df.iloc[0]["ingest_processing_date"], "01/11/21")
+        self.cache.add_chunk.assert_called()
+
+    def test_on_exception(self) -> None:
+        # Clear is called upon instantiation
+        self.cache.reset_mock()
+
+        self.assertTrue(self.delegate.on_exception("UTF-8", ValueError()))
+        self.cache.clear.assert_not_called()
+
+        # Cache is cleared upon pandas parsing exception
+        self.assertTrue(
+            self.delegate.on_exception("UTF-8", pandas.errors.ParserError())
+        )
+        self.cache.clear.assert_called()
+
+    def test_on_unicode_decode_error(self) -> None:
+        # Don't retry processing with a different data type on decode errors
+        self.assertFalse(self.delegate.on_unicode_decode_error("UTF-8", UnicodeError()))
+
+        # Cache is cleared upon decode error
+        self.cache.clear.assert_called()
