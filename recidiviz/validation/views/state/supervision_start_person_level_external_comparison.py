6e7b935237e0d69940b9e0e05ea79b2f6c4441a3
Kris Gerig <kris.gerig@gmail.com>
2021-04-23 16:02:31 -0500

    Update supervision external validation views (Recidiviz/recidiviz-data#7108)
    * add supervision start external validation view
    
    * update supervision termination external validation view to exclude out of state transfers
    
    * add TODO notes to remove filters once transfer logic is updated
    
    GitOrigin-RevId: 0aaaff2eda2d6a2b927a6b7156081a8e35b4aac2

--- recidiviz/validation/views/state/supervision_start_person_level_external_comparison.py
+++ recidiviz/validation/views/state/supervision_start_person_level_external_comparison.py
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 
-"""A view comparing person-level supervision termination metrics to the person-level values from external metrics
+"""A view comparing person-level supervision start metrics to the person-level values from external metrics
 provided by the state.
 """
 
@@ -26,57 +26,57 @@ from recidiviz.utils.metadata import local_project_id_override
 from recidiviz.validation.views import dataset_config
 
 
-SUPERVISION_TERMINATION_PERSON_LEVEL_EXTERNAL_COMPARISON_VIEW_NAME = (
-    "supervision_termination_person_level_external_comparison"
+SUPERVISION_START_PERSON_LEVEL_EXTERNAL_COMPARISON_VIEW_NAME = (
+    "supervision_start_person_level_external_comparison"
 )
 
-SUPERVISION_TERMINATION_PERSON_LEVEL_EXTERNAL_COMPARISON_DESCRIPTION = """
-Comparison of internal and external lists of supervision terminations.
+SUPERVISION_START_PERSON_LEVEL_EXTERNAL_COMPARISON_DESCRIPTION = """
+Comparison of internal and external lists of supervision starts.
 """
 
-SUPERVISION_TERMINATION_PERSON_LEVEL_EXTERNAL_COMPARISON_QUERY_TEMPLATE = """
+SUPERVISION_START_PERSON_LEVEL_EXTERNAL_COMPARISON_QUERY_TEMPLATE = """
 /*{description}*/
     WITH external_data AS (
       -- NOTE: You can replace this part of the query with your own query to test the SELECT query you will use to
-      -- generate data to insert into the `supervision_termination_person_level` table.
-      SELECT region_code, person_external_id, termination_date
-      FROM `{project_id}.{external_accuracy_dataset}.supervision_termination_person_level`
+      -- generate data to insert into the `supervision_start_person_level` table.
+      SELECT region_code, person_external_id, start_date
+      FROM `{project_id}.{external_accuracy_dataset}.supervision_start_person_level`
     ), internal_data AS (
-      SELECT state_code as region_code, person_external_id, termination_date,
-      FROM `{project_id}.{materialized_metrics_dataset}.most_recent_supervision_termination_metrics_materialized`
-      -- TODO(#5422): Remove termination_reason filter once we have removed transfers from events
-      -- Transfers terminations are not included in the external termination data --
-      WHERE termination_reason NOT IN ('TRANSFER_WITHIN_STATE', 'TRANSFER_OUT_OF_STATE')
+      SELECT state_code as region_code, person_external_id, start_date,
+      FROM `{project_id}.{materialized_metrics_dataset}.most_recent_supervision_start_metrics_materialized`
+      -- TODO(#5422): Remove admission_reason filter once we have removed transfers from events
+      WHERE admission_reason NOT IN ('TRANSFER_WITHIN_STATE', 'TRANSFER_OUT_OF_STATE')
+        AND supervision_type IN ('PROBATION', 'PAROLE', 'DUAL')
     ), internal_metrics_for_valid_regions_and_dates AS (
       SELECT * FROM
       -- Only compare regions and months for which we have external validation data
-      (SELECT DISTINCT region_code, termination_date FROM external_data)
+      (SELECT DISTINCT region_code, start_date FROM external_data)
       LEFT JOIN
         internal_data
-      USING (region_code, termination_date)
+      USING (region_code, start_date)
     )
 
     SELECT
       region_code,
-      termination_date,
+      start_date,
       external_data.person_external_id AS external_person_external_id,
       internal_data.person_external_id AS internal_person_external_id,
     FROM
       external_data
     FULL OUTER JOIN
       internal_metrics_for_valid_regions_and_dates internal_data
-    USING(region_code, termination_date, person_external_id)
+    USING(region_code, start_date, person_external_id)
 """
 
-SUPERVISION_TERMINATION_PERSON_LEVEL_EXTERNAL_COMPARISON_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+SUPERVISION_START_PERSON_LEVEL_EXTERNAL_COMPARISON_VIEW_BUILDER = SimpleBigQueryViewBuilder(
     dataset_id=dataset_config.VIEWS_DATASET,
-    view_id=SUPERVISION_TERMINATION_PERSON_LEVEL_EXTERNAL_COMPARISON_VIEW_NAME,
-    view_query_template=SUPERVISION_TERMINATION_PERSON_LEVEL_EXTERNAL_COMPARISON_QUERY_TEMPLATE,
-    description=SUPERVISION_TERMINATION_PERSON_LEVEL_EXTERNAL_COMPARISON_DESCRIPTION,
+    view_id=SUPERVISION_START_PERSON_LEVEL_EXTERNAL_COMPARISON_VIEW_NAME,
+    view_query_template=SUPERVISION_START_PERSON_LEVEL_EXTERNAL_COMPARISON_QUERY_TEMPLATE,
+    description=SUPERVISION_START_PERSON_LEVEL_EXTERNAL_COMPARISON_DESCRIPTION,
     external_accuracy_dataset=dataset_config.EXTERNAL_ACCURACY_DATASET,
     materialized_metrics_dataset=state_dataset_config.DATAFLOW_METRICS_MATERIALIZED_DATASET,
 )
 
 if __name__ == "__main__":
     with local_project_id_override(GCP_PROJECT_STAGING):
-        SUPERVISION_TERMINATION_PERSON_LEVEL_EXTERNAL_COMPARISON_VIEW_BUILDER.build_and_print()
+        SUPERVISION_START_PERSON_LEVEL_EXTERNAL_COMPARISON_VIEW_BUILDER.build_and_print()
