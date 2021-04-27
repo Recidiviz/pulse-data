9c378590ac1b9b30538f374e004d4ecd12234a7f
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-04-27 13:11:12 -0400

    [Case Triage] Be more defensive about policy info (Recidiviz/recidiviz-data#7164)
    GitOrigin-RevId: 93f59f3f8dbbd3670584ac5f08d60b00a4a7927d

--- frontends/case-triage/src/components/ClientList/ClientListCard.tsx
+++ frontends/case-triage/src/components/ClientList/ClientListCard.tsx
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 import * as React from "react";
-import moment from "moment";
 import { IconSVG, NeedState } from "@recidiviz/design-system";
 import { DecoratedClient } from "../../stores/ClientsStore";
 import { useRootStore } from "../../stores";
