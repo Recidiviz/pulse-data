46f184862cb8bc4549bcc30d2a385c9bd4db58db
Emily Chao <emily@recidiviz.org>
2021-04-30 13:01:31 -0700

    Adding supervision contacts to be ingested for US_PA (Recidiviz/recidiviz-data#7233)
    * Adding supervision contacts to be ingested for US_PA
    
    * removing debug flag
    
    * Addressing Anna's feedback
    
    * addressing lint
    
    * addressing more lint
    
    GitOrigin-RevId: 50706bf89a359d18b4b792ea60acdee13bf5a8a7

---
+++ recidiviz/ingest/direct/regions/us_pa/ingest_views/view_supervision_contacts.py
@@ -0,0 +1,57 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query containing the supervision contacts made during a person's supervision period(s).
+"""
+
+from recidiviz.ingest.direct.views.direct_ingest_big_query_view_types import (
+    DirectIngestPreProcessedIngestViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+SELECT
+    FACT_PAROLEE_CNTC_SUMRY_ID as contact_id,
+    PERSON_ID as person_id,
+    PAROLE_NUMBER as parole_number,
+    CAST(LEFT(START_DATE, 10) AS DATE) AS contact_start_date,
+    CAST(LEFT(END_DATE, 10) AS DATE) AS contact_end_date,
+    DURATION_MINS as duration,
+    CONTACT_TYPE as contact_type,
+    METHOD as method,
+    ATTEMPTED as contact_attempt,
+    COLLATERAL_TYPE as collateral_type,
+    ASSISTED as assisted,
+    NOTE_TEXT as note,
+    PRL_AGNT_EMPL_NO as agent_number,
+    PRL_AGNT_FIRST_NAME as agent_first_name,
+    PRL_AGNT_LAST_NAME as agent_last_name,
+    PRL_AGNT_ORG_NAME as parole_org
+  FROM
+    {PRS_FACT_PAROLEE_CNTC_SUMRY}
+"""
+
+VIEW_BUILDER = DirectIngestPreProcessedIngestViewBuilder(
+    region="us_pa",
+    ingest_view_name="supervision_contacts",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+    order_by_cols="parole_number ASC, contact_id ASC",
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
