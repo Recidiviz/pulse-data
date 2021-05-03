dc84c8144923ed8327eb85b95069b69dee14a0ba
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2021-05-03 15:24:40 -0400

    Add links to ingest logs (Recidiviz/recidiviz-data#7260)
    GitOrigin-RevId: 0c3fad26f992c3e20e30c7e84f216595372fa0a9

---
+++ frontends/admin-panel/src/components/IngestOperationsView/IngestLogsCard.tsx
@@ -0,0 +1,50 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2021 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+import * as React from "react";
+import { Card, Descriptions } from "antd";
+
+interface IngestLogsCardProps {
+  stateCode: string;
+  env: string;
+}
+
+const IngestLogsCard: React.FC<IngestLogsCardProps> = ({ stateCode, env }) => {
+  let logsUrl = "";
+  let non200Url = "";
+  if (env === "staging" || env === "development") {
+    logsUrl = `http://go/staging-ingest-logs/${stateCode.toLowerCase()}`;
+    non200Url = `http://go/staging-non-200-ingest-responses/${stateCode.toLowerCase()}`;
+  } else if (env === "production") {
+    logsUrl = `https://go/prod-ingest-logs/${stateCode.toLowerCase()}`;
+    non200Url = `http://go/prod-non-200-ingest-responses/${stateCode.toLowerCase()}`;
+  }
+
+  return (
+    <Card title="Summary" style={{ width: 1000 }}>
+      <Descriptions>
+        <Descriptions.Item label="Logs Explorer" span={3}>
+          <a href={logsUrl}>{logsUrl}</a>
+        </Descriptions.Item>
+        <Descriptions.Item label="Non-200 Ingest Responses" span={3}>
+          <a href={non200Url}>{non200Url}</a>
+        </Descriptions.Item>
+      </Descriptions>
+    </Card>
+  );
+};
+
+export default IngestLogsCard;
