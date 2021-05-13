f9cef365e0c0db60806452664753b26a71a525e3
Ian MacFarland <macfarlandian@users.noreply.github.com>
2021-05-13 13:18:54 -0700

    E2E test setup for case triage (Recidiviz/recidiviz-data#7429)
    GitOrigin-RevId: f5bbb9e55ae71bd2511ddee0eaa34d1e09b60008

---
+++ frontends/case-triage/e2e/pages/HomePage.ts
@@ -0,0 +1,31 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2021 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+
+import Page from "./Page";
+
+/* eslint-disable class-methods-use-this */
+class HomePage extends Page {
+  open(): void {
+    super.open("/");
+  }
+
+  get clientListHeading() {
+    return $("h2=Up Next");
+  }
+}
+
+export default new HomePage();
