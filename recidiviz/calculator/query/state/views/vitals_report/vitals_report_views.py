548a7c655dd54865d5491c325d2065563eeea882
Anthony Kesich <anthony@recidiviz.org>
2021-05-21 14:35:48 -0500

    Calculate contact rate in vitals views (Recidiviz/recidiviz-data#7530)
    * Add contacts calculation to vitals exports
    
    * Add comment about why we limit supervision level in contact calculation
    
    * Update query to only check f2f contacts
    
    GitOrigin-RevId: a86463aa69bb9d5f36d8c376ef2fa245a7669e1a

--- recidiviz/calculator/query/state/views/vitals_report/vitals_report_views.py
+++ recidiviz/calculator/query/state/views/vitals_report/vitals_report_views.py
@@ -27,12 +27,17 @@ from recidiviz.calculator.query.state.views.vitals_report.supervision_population
 from recidiviz.calculator.query.state.views.vitals_report.supervision_population_due_for_release_by_po_by_day import (
     SUPERVISION_POPULATION_DUE_FOR_RELEASE_BY_PO_BY_DAY_VIEW_BUILDER,
 )
+from recidiviz.calculator.query.state.views.vitals_report.timely_contact_by_po_by_day import (
+    TIMELY_CONTACT_BY_PO_BY_DAY_VIEW_BUILDER,
+)
 
 
 # NOTE: These views must be listed in order of dependency. For example, if view Y depends on view X, then view X should
 # appear in the list before view Y.
+
 VITALS_REPORT_VIEW_BUILDERS: List[BigQueryViewBuilder] = [
     SUPERVISION_POPULATION_BY_PO_BY_DAY_VIEW_BUILDER,
     OVERDUE_LSIR_BY_PO_BY_DAY_VIEW_BUILDER,
     SUPERVISION_POPULATION_DUE_FOR_RELEASE_BY_PO_BY_DAY_VIEW_BUILDER,
+    TIMELY_CONTACT_BY_PO_BY_DAY_VIEW_BUILDER,
 ]
