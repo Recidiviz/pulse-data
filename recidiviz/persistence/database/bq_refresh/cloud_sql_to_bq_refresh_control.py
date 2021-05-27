24896753bad556f287482572fe27ae5a51f12e56
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2021-05-27 09:32:02 -0700

    [Multi-DB] Clean up legacy BQ refresh code (Recidiviz/recidiviz-data#7647)
    The change here is two-fold:
    1) Delete a bunch of legacy BQ refresh code that is no-longer used after Recidiviz/recidiviz-data#7646 lands
    2) Renames the endpoint `/cloud_sql_to_bq/create_refresh_bq_tasks` to `/cloud_sql_to_bq/create_refresh_bq_schema_task` to reflect the fact that it only queues one task now.
    
    Closes Recidiviz/recidiviz-data#7397
    
    GitOrigin-RevId: 60931e1b58753db4f39c7a64f0d0426c8b7ae763

--- recidiviz/persistence/database/bq_refresh/cloud_sql_to_bq_refresh_control.py
+++ recidiviz/persistence/database/bq_refresh/cloud_sql_to_bq_refresh_control.py
@@ -24,7 +24,6 @@ from typing import Tuple
 import flask
 from flask import request
 
-from recidiviz.big_query.big_query_client import BigQueryClientImpl
 from recidiviz.cloud_storage.gcs_pseudo_lock_manager import GCSPseudoLockDoesNotExist
 from recidiviz.ingest.direct.direct_ingest_control import kick_all_schedulers
 from recidiviz.persistence.database.bq_refresh.bq_refresh_cloud_task_manager import (
@@ -36,9 +35,6 @@ from recidiviz.persistence.database.bq_refresh.cloud_sql_to_bq_lock_manager impo
 from recidiviz.persistence.database.bq_refresh.cloud_sql_to_bq_refresh_config import (
     CloudSqlToBQConfig,
 )
-from recidiviz.persistence.database.bq_refresh.cloud_sql_to_bq_refresh_runner import (
-    export_table_then_load_table,
-)
 from recidiviz.persistence.database.bq_refresh.federated_cloud_sql_to_bq_refresh import (
     federated_bq_schema_refresh,
 )
@@ -49,101 +45,16 @@ from recidiviz.utils.auth.gae import requires_gae_auth
 cloud_sql_to_bq_blueprint = flask.Blueprint("export_manager", __name__)
 
 
-@cloud_sql_to_bq_blueprint.route("/refresh_bq_table", methods=["POST"])
-@requires_gae_auth
-def refresh_bq_table() -> Tuple[str, int]:
-    """Worker function to handle BQ export task requests.
-
-    Form data must be a bytes-encoded JSON object with parameters listed below.
-
-    URL Parameters:
-        table_name: Table to export then import. Table must be defined
-            in one of the base schema types.
-    """
-    json_data = request.get_data(as_text=True)
-    data = json.loads(json_data)
-    table_name = data["table_name"]
-    schema_type_str = data["schema_type"]
-
-    try:
-        schema_type = SchemaType(schema_type_str.upper())
-    except ValueError:
-        return (f"Unknown schema type [{schema_type_str}]", HTTPStatus.BAD_REQUEST)
-
-    if not CloudSqlToBQConfig.is_valid_schema_type(schema_type):
-        return (
-            f"Unsuppported schema type: [{schema_type}]",
-            HTTPStatus.BAD_REQUEST,
-        )
-
-    bq_client = BigQueryClientImpl()
-
-    logging.info("Starting BQ export task for table: %s", table_name)
-
-    export_table_then_load_table(bq_client, table_name, schema_type)
-    return ("", HTTPStatus.OK)
-
-
-@cloud_sql_to_bq_blueprint.route("/monitor_refresh_bq_tasks", methods=["POST"])
-@requires_gae_auth
-def monitor_refresh_bq_tasks() -> Tuple[str, int]:
-    """Worker function to publish a message to a Pub/Sub topic once all tasks in
-    the BIGQUERY_QUEUE queue have completed.
-    """
-    json_data = request.get_data(as_text=True)
-    data = json.loads(json_data)
-    schema = data["schema"]
-    topic = data["topic"]
-    message = data["message"]
-
-    try:
-        schema_type = SchemaType(schema.upper())
-    except ValueError:
-        return (f"Unknown schema type [{schema}]", HTTPStatus.BAD_REQUEST)
-
-    task_manager = BQRefreshCloudTaskManager()
-
-    # If any of the tasks in the queue have task_name containing schema, consider BQ tasks in queue
-    bq_tasks_in_queue = False
-    bq_task_list = task_manager.get_bq_queue_info().task_names
-    for task_name in bq_task_list:
-        task_id = task_name[task_name.find("/tasks/") :]
-        if schema in task_id:
-            bq_tasks_in_queue = True
-
-    # If there are BQ tasks in the queue, then re-queue this task in a minute
-    if bq_tasks_in_queue:
-        logging.info("Tasks still in bigquery queue. Re-queuing bq monitor" " task.")
-        task_manager.create_bq_refresh_monitor_task(schema, topic, message)
-        # TODO(#6257): Add an expiration refresh to our lock in case the total export
-        # takes more than an hour.
-        return "", HTTPStatus.OK
-
-    # Publish a message to the Pub/Sub topic once state BQ export is complete
-    if topic:
-        pubsub_helper.publish_message_to_topic(message=message, topic=topic)
-
-    # Unlock export lock when all BQ exports complete
-    lock_manager = CloudSqlToBQLockManager()
-    lock_manager.release_lock(schema_type)
-    logging.info(
-        "Done running export for %s, unlocking Postgres to BigQuery export", schema
-    )
-
-    # Kick scheduler to restart ingest
-    kick_all_schedulers()
-
-    return ("", HTTPStatus.OK)
-
-
 @cloud_sql_to_bq_blueprint.route(
-    "/create_refresh_bq_tasks/<schema_arg>", methods=["GET", "POST"]
+    "/create_refresh_bq_schema_task/<schema_arg>", methods=["GET", "POST"]
 )
 @requires_gae_auth
 def wait_for_ingest_to_create_tasks(schema_arg: str) -> Tuple[str, HTTPStatus]:
-    """Worker function to wait until ingest is not running to create_all_bq_refresh_tasks_for_schema.
-    When ingest is not running/locked, creates task to create_all_bq_refresh_tasks_for_schema.
-    When ingest is running/locked, re-enqueues this task to run again in 60 seconds.
+    """Worker function to wait until ingest is not running to queue a task to run
+    /refresh_bq_schema. Before doing anything, grabs the refresh lock to indicate that
+    a refresh wants to start and ingest should yield ASAP. Then:
+    * When ingest is not running/locked, creates task to run /refresh_bq_schema.
+    * When ingest is running/locked, re-enqueues this task to run again in 60 seconds.
     """
     try:
         schema_type = SchemaType(schema_arg.upper())
@@ -175,35 +86,6 @@ def wait_for_ingest_to_create_tasks(schema_arg: str) -> Tuple[str, HTTPStatus]:
     logging.info("No regions running, triggering BQ refresh.")
     task_manager.create_refresh_bq_schema_task(schema_type=schema_type)
     return "", HTTPStatus.OK
-
-
-# TODO(#7397): Ship federated export to production and delete this code.
-def create_all_bq_refresh_tasks_for_schema(schema_type: SchemaType) -> None:
-    """Creates an export task for each table to be exported.
-
-    A task is created for each table defined in the schema.
-
-    Re-creates all tasks if any task fails to be created.
-    """
-    logging.info("Beginning BQ export for %s schema tables.", schema_type.value)
-
-    task_manager = BQRefreshCloudTaskManager()
-
-    cloud_sql_to_bq_config = CloudSqlToBQConfig.for_schema_type(schema_type)
-
-    for table in cloud_sql_to_bq_config.get_tables_to_export():
-        task_manager.create_refresh_bq_table_task(table.name, schema_type)
-
-    if schema_type is SchemaType.STATE:
-        pub_sub_topic = "v1.calculator.trigger_daily_pipelines"
-        pub_sub_message = "State export to BQ complete"
-    else:
-        pub_sub_topic = ""
-        pub_sub_message = ""
-
-    task_manager.create_bq_refresh_monitor_task(
-        schema_type.value, pub_sub_topic, pub_sub_message
-    )
 
 
 @cloud_sql_to_bq_blueprint.route(
--- recidiviz/tests/persistence/database/bq_refresh/cloud_sql_to_bq_refresh_config_test.py
+++ recidiviz/tests/persistence/database/bq_refresh/cloud_sql_to_bq_refresh_config_test.py
@@ -23,17 +23,13 @@ from unittest import mock
 from typing import Dict, List
 
 import sqlalchemy
-from google.cloud import bigquery
 from parameterized import parameterized
 
 from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
 from recidiviz.persistence.database.bq_refresh.cloud_sql_to_bq_refresh_config import (
     CloudSqlToBQConfig,
 )
-from recidiviz.persistence.database.schema_table_region_filtered_query_builder import (
-    BigQuerySchemaTableRegionFilteredQueryBuilder,
-)
-from recidiviz.persistence.database.schema_utils import is_association_table, SchemaType
+from recidiviz.persistence.database.schema_utils import SchemaType
 from recidiviz.tests.cloud_storage.fake_gcs_file_system import FakeGCSFileSystem
 from recidiviz.view_registry.datasets import (
     VIEW_SOURCE_TABLE_DATASETS,
@@ -147,81 +143,6 @@ county_columns_to_exclude:
                         f"COUNTY_COLUMNS_TO_EXCLUDE` for this table {table.name}.",
                     )
 
-    def test_get_bq_schema_for_table(self) -> None:
-        """Test that get_bq_schema_for_table returns a list
-        of SchemaField objects when given a valid table_name
-
-        Assert that excluded columns are not in the schema.
-        """
-        for schema_type in self.enabled_schema_types:
-            config = CloudSqlToBQConfig.for_schema_type(schema_type)
-            assert config is not None
-            table_name = config.sorted_tables[0].name
-            schema = config.get_bq_schema_for_table(table_name)
-            for schema_field in schema:
-                self.assertIsInstance(schema_field, bigquery.SchemaField)
-
-            for table_name, excluded_columns in config.columns_to_exclude.items():
-                schema = config.get_bq_schema_for_table(table_name)
-                for column in excluded_columns:
-                    self.assertTrue(
-                        column not in {s.name for s in schema},
-                        msg=f"Column {column} should not be included. It is found in "
-                        f"COUNTY_COLUMNS_TO_EXCLUDE` for this table {table_name}.",
-                    )
-
-    def test_get_bq_schema_for_table_region_code_in_schema(self) -> None:
-        """Assert that the region code is included in the schema for association tables in the State schema."""
-        association_table_name = (
-            "state_supervision_period_program_assignment_association"
-        )
-        config = CloudSqlToBQConfig.for_schema_type(SchemaType.STATE)
-        assert config is not None
-        region_code_col = "state_code"
-        schema = config.get_bq_schema_for_table(association_table_name)
-
-        self.assertIn(region_code_col, [schema_field.name for schema_field in schema])
-
-    def test_get_gcs_export_uri_for_table(self) -> None:
-        """Test that get_gcs_export_uri_for_table generates a GCS URI
-        with the correct project ID and table name.
-        """
-        config = CloudSqlToBQConfig.for_schema_type(SchemaType.JAILS)
-        assert config is not None
-        fake_table = "my_fake_table"
-        bucket = "{}-dbexport".format(self.mock_project_id)
-        gcs_export_uri = "gs://{bucket}/{table_name}.csv".format(
-            bucket=bucket, table_name=fake_table
-        )
-        self.assertEqual(
-            gcs_export_uri, config.get_gcs_export_uri_for_table(fake_table)
-        )
-
-    def test_get_table_export_query(self) -> None:
-        """For each SchemaType and for each table:
-        1. Assert that each export query is of type string
-        2. Assert that excluded columns are not in the query
-        """
-        for schema_type in self.enabled_schema_types:
-            config = CloudSqlToBQConfig.for_schema_type(schema_type)
-            assert config is not None
-            config.region_codes_to_exclude = []
-
-            for table in config.sorted_tables:
-                query = config.get_table_export_query(table.name)
-                self.assertIsInstance(query, str)
-
-                for column in config.columns_to_exclude:
-                    search_pattern = "[ ]*{}[, ]+".format(f"{table.name}.{column}")
-                    self.assertNotRegex(
-                        query,
-                        search_pattern,
-                        msg='Column "{}" not excluded properly from table "{}".'
-                        " Check cloud_sql_to_bq_export_config.COUNTY_TABLE_COLUMNS_TO_EXPORT".format(
-                            column, table.name
-                        ),
-                    )
-
     def test_get_tables_to_export(self) -> None:
         """Assertions for the method get_tables_to_export
         1. Assert that it returns a list of type sqlalchemy.Table
@@ -269,62 +190,6 @@ county_columns_to_exclude:
 
             for char in dataset_id:
                 self.assertIn(char, allowed_characters)
-
-    def test_get_stale_bq_rows_for_excluded_regions_query_builder(self) -> None:
-        """Given a table name, assert that it returns a query builder that filters for rows
-        excluded from the export query"""
-        config = CloudSqlToBQConfig.for_schema_type(SchemaType.STATE)
-        assert config is not None
-        config.region_codes_to_exclude = ["US_VA", "us_id", "US_hi"]
-        for table in config.get_tables_to_export():
-            if is_association_table(table.name):
-                # This is tested in the CloudSqlSchemaTableRegionFilteredQueryBuilder class
-                continue
-            filter_clause = "WHERE state_code IN ('US_VA','US_ID','US_HI')"
-            query_builder = config.get_stale_bq_rows_for_excluded_regions_query_builder(
-                table.name
-            )
-
-            self.assertIsInstance(
-                query_builder, BigQuerySchemaTableRegionFilteredQueryBuilder
-            )
-            self.assertIn(filter_clause, query_builder.full_query())
-
-    def test_get_stale_bq_rows_for_excluded_regions_query_builder_no_excluded_regions(
-        self,
-    ) -> None:
-        """Given a table name and no excluded region codes, assert that it returns a query builder
-        that returns no rows"""
-        config = CloudSqlToBQConfig.for_schema_type(SchemaType.STATE)
-        assert config is not None
-        config.region_codes_to_exclude = []
-        filter_clause = "WHERE FALSE"
-        for table in config.get_tables_to_export():
-            query_builder = config.get_stale_bq_rows_for_excluded_regions_query_builder(
-                table.name
-            )
-
-            self.assertIsInstance(
-                query_builder, BigQuerySchemaTableRegionFilteredQueryBuilder
-            )
-            self.assertEqual(filter_clause, query_builder.filter_clause())
-
-    def test_get_stale_bq_rows_for_excluded_regions_query_builder_jails_schema(
-        self,
-    ) -> None:
-        """Given a JAILS schema, a table name and None for region_codes_to_exclude, assert that it returns a
-        query builder that returns no rows"""
-        filter_clause = "WHERE FALSE"
-        config = CloudSqlToBQConfig.for_schema_type(SchemaType.JAILS)
-        assert config is not None
-        for table in config.get_tables_to_export():
-            query_builder = config.get_stale_bq_rows_for_excluded_regions_query_builder(
-                table.name
-            )
-            self.assertIsInstance(
-                query_builder, BigQuerySchemaTableRegionFilteredQueryBuilder
-            )
-            self.assertEqual(filter_clause, query_builder.filter_clause())
 
     @mock.patch(
         "recidiviz.persistence.database.bq_refresh.cloud_sql_to_bq_refresh_config.metadata.project_id",
