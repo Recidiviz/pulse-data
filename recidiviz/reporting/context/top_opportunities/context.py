f05d920348d70ce60b9c2cb1f223f0daa6454092
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-06-14 06:41:27 -1000

    [Emails] Make "Learn More" optional (Recidiviz/recidiviz-data#7814)
    * [Emails] Make "Learn More" optional
    
    * Re-introduce properties.json
    
    * Fix lint
    
    GitOrigin-RevId: 8faabc98cb010f9ae726e70b70d23e5e3baf439e

--- recidiviz/reporting/context/top_opportunities/context.py
+++ recidiviz/reporting/context/top_opportunities/context.py
@@ -28,8 +28,15 @@ from typing import List
 
 from jinja2 import Environment, FileSystemLoader, Template
 
+import recidiviz.reporting.email_reporting_utils as utils
+from recidiviz.reporting.context.po_monthly_report.constants import (
+    DEFAULT_MESSAGE_BODY_KEY,
+    OFFICER_GIVEN_NAME,
+)
 from recidiviz.reporting.context.report_context import ReportContext
 from recidiviz.reporting.recipient import Recipient
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
 
 
 class TopOpportunitiesReportContext(ReportContext):
@@ -44,10 +51,14 @@ class TopOpportunitiesReportContext(ReportContext):
         )
 
     def get_required_recipient_data_fields(self) -> List[str]:
-        return []
+        return [OFFICER_GIVEN_NAME, "mismatches"]
 
     def get_report_type(self) -> str:
         return "top_opportunities"
+
+    def get_properties_filepath(self) -> str:
+        """Returns path to the properties.json, assumes it is in the same directory as the context."""
+        return os.path.join(os.path.dirname(__file__), "properties.json")
 
     @property
     def html_template(self) -> Template:
@@ -56,53 +67,71 @@ class TopOpportunitiesReportContext(ReportContext):
     def prepare_for_generation(self) -> dict:
         """Executes PO Monthly Report data preparation."""
         self.prepared_data = copy.deepcopy(self.recipient_data)
+
+        self.prepared_data["static_image_path"] = utils.get_static_image_path(
+            self.state_code, self.get_report_type()
+        )
+        self.prepared_data[
+            "greeting"
+        ] = f'Hi {self.recipient_data["officer_given_name"]}:'
+
+        if message_override := self.recipient_data["message_body_override"]:
+            self.prepared_data["message_body"] = message_override
+        else:
+            self.prepared_data["message_body"] = self.properties[
+                DEFAULT_MESSAGE_BODY_KEY
+            ]
+
         return self.prepared_data
 
 
 if __name__ == "__main__":
-    jinja_env = Environment(
-        loader=FileSystemLoader(
-            os.path.join(os.path.dirname(os.path.dirname(__file__)), "templates")
-        )
+    context = TopOpportunitiesReportContext(
+        "US_ID",
+        Recipient.from_report_json(
+            {
+                utils.KEY_EMAIL_ADDRESS: "test@recidiviz.org",
+                utils.KEY_STATE_CODE: "US_ID",
+                utils.KEY_DISTRICT: "US_ID_D3",
+                OFFICER_GIVEN_NAME: "Clementine",
+                "mismatches": {
+                    "high_to_medium": [
+                        {
+                            "name": "Nikhil Bhargava",
+                            "person_external_id": "189472",
+                        },
+                    ],
+                    "high_to_low": [
+                        {
+                            "name": "Serena Chang",
+                            "person_external_id": "47228",
+                        },
+                        {
+                            "name": "Juan Agron",
+                            "person_external_id": "132878",
+                        },
+                        {
+                            "name": "Emily Chao",
+                            "person_external_id": "147872",
+                        },
+                    ],
+                    "medium_to_low": [
+                        {
+                            "name": "Annalise Irby",
+                            "person_external_id": "74827",
+                        },
+                        {
+                            "name": "Dan Hansen",
+                            "person_external_id": "32224",
+                        },
+                    ],
+                },
+            }
+        ),
     )
 
-    template = jinja_env.get_template("top_opportunities/email.html.jinja2")
-    print(
-        template.render(
-            greeting="Hi Clementine,",
-            message_body="We've been searching your caseload for opportunities to lighten your load. We've found a couple of cases where we think your clients are eligible for reduced supervision. Please let us know if anything seems wrong!",
-            static_image_path="./recidiviz/reporting/context/static",
-            mismatches={
-                "high_to_medium": [
-                    {
-                        "name": "Nikhil Bhargava",
-                        "person_external_id": "189472",
-                    },
-                ],
-                "high_to_low": [
-                    {
-                        "name": "Serena Chang",
-                        "person_external_id": "47228",
-                    },
-                    {
-                        "name": "Juan Agron",
-                        "person_external_id": "132878",
-                    },
-                    {
-                        "name": "Emily Chao",
-                        "person_external_id": "147872",
-                    },
-                ],
-                "medium_to_low": [
-                    {
-                        "name": "Annalise Irby",
-                        "person_external_id": "74827",
-                    },
-                    {
-                        "name": "Dan Hansen",
-                        "person_external_id": "32224",
-                    },
-                ],
-            },
-        )
-    )
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        prepared_data = context.prepare_for_generation()
+        prepared_data["static_image_path"] = "./recidiviz/reporting/context/static"
+
+    print(context.html_template.render(**prepared_data))
