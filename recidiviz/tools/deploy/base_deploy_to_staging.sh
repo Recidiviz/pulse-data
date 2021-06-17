08889071447ceecabf9c5027c30598c4ceeea628
Dana Hoffman <danahoffman@google.com>
2021-06-17 12:54:41 -0700

    make project configurable in deploy scripts (recidiviz/recidiviz-data#7894)
    GitOrigin-RevId: 351717463ba80a43dab45ac1cb2f9d5e4ca8473f

--- recidiviz/tools/deploy/base_deploy_to_staging.sh
+++ recidiviz/tools/deploy/base_deploy_to_staging.sh
@@ -16,19 +16,21 @@ DEBUG_BUILD_NAME=''
 PROMOTE=''
 NO_PROMOTE=''
 PROMOTE_FLAGS=''
+PROJECT_ID='recidiviz-staging'
 
 function print_usage {
-    echo_error "usage: $0 -v VERSION -c COMMIT_SHA [-p -n -d DEBUG_BUILD_NAME]"
+    echo_error "usage: $0 -v VERSION -c COMMIT_SHA [-p -n -d DEBUG_BUILD_NAME -r PROJECT_ID]"
     echo_error "  -v: Version tag to deploy (e.g. v1.2.0)"
     echo_error "  -c: Full SHA of the commit that is to be deployed."
     echo_error "  -b: Name of the branch from which we're deploying. (required if promoting)"
     echo_error "  -p: Indicates that we should promote traffic to the newly deployed version. Can not be used with -n."
     echo_error "  -n: Indicates that we should not promote traffic to the newly deployed version. Can not be used with -p."
     echo_error "  -d: Name to append to the version for a debug local deploy (e.g. anna-test1)."
+    echo_error "  -r: Project ID to deploy to. Defaults to recidiviz-staging."
     run_cmd exit 1
 }
 
-while getopts "b:v:c:pnd:" flag; do
+while getopts "b:v:c:pnd:r:" flag; do
   case "${flag}" in
     v) VERSION_TAG="$OPTARG" ;;
     c) COMMIT_HASH="$OPTARG" ;;
@@ -36,6 +38,7 @@ while getopts "b:v:c:pnd:" flag; do
     p) PROMOTE_FLAGS='--promote' PROMOTE='true';;
     n) PROMOTE_FLAGS='--no-promote' NO_PROMOTE='true';;
     d) DEBUG_BUILD_NAME="$OPTARG" ;;
+    r) PROJECT_ID="$OPTARG" ;;
     *) print_usage
        run_cmd exit 1 ;;
   esac
@@ -71,11 +74,11 @@ if [[ ! -z ${PROMOTE} && ! -z ${DEBUG_BUILD_NAME} ]]; then
     run_cmd exit 1
 fi
 
-CALC_CHANGES_SINCE_LAST_DEPLOY=$(calculation_pipeline_changes_since_last_deploy 'recidiviz-staging')
+CALC_CHANGES_SINCE_LAST_DEPLOY=$(calculation_pipeline_changes_since_last_deploy $PROJECT_ID)
 
 echo "Performing pre-deploy verification"
 verify_hash $COMMIT_HASH
-run_cmd verify_can_deploy recidiviz-staging
+run_cmd verify_can_deploy $PROJECT_ID
 
 if [[ ! -z ${DEBUG_BUILD_NAME} ]]; then
     DOCKER_IMAGE_TAG=${VERSION_TAG}-${DEBUG_BUILD_NAME}
@@ -85,10 +88,10 @@ else
     GAE_VERSION=$(echo $VERSION_TAG | tr '.' '-')
 fi
 
-IMAGE_BASE=us.gcr.io/recidiviz-staging/appengine/default
+IMAGE_BASE=us.gcr.io/$PROJECT_ID/appengine/default
 IMAGE_URL=$IMAGE_BASE:${DOCKER_IMAGE_TAG} || exit_on_fail
 
-REMOTE_BUILD_BASE=us.gcr.io/recidiviz-staging/appengine/build
+REMOTE_BUILD_BASE=us.gcr.io/$PROJECT_ID/appengine/build
 REMOTE_BUILD_URL=$REMOTE_BUILD_BASE:${COMMIT_HASH}
 
 if [[ ! -z ${DEBUG_BUILD_NAME} ]]; then
@@ -127,7 +130,7 @@ else
     done
 
     if [[ "${FOUND_REMOTE_BUILD}" == "false" ]]; then
-        echo "Unable to find remote build for ${COMMIT_HASH} within the timeout - you might need to manually trigger it in Cloud Build (https://console.cloud.google.com/cloud-build/triggers?project=recidiviz-staging). Exiting..."
+        echo "Unable to find remote build for ${COMMIT_HASH} within the timeout - you might need to manually trigger it in Cloud Build (https://console.cloud.google.com/cloud-build/triggers?project=$PROJECT_ID). Exiting..."
         run_cmd exit 1
     fi
 
@@ -152,7 +155,7 @@ fi
 
 if [[ ! -z ${PROMOTE} ]]; then
     verify_hash $COMMIT_HASH
-    pre_deploy_configure_infrastructure 'recidiviz-staging' "${DOCKER_IMAGE_TAG}" "$COMMIT_HASH"
+    pre_deploy_configure_infrastructure "$PROJECT_ID" "${DOCKER_IMAGE_TAG}" "$COMMIT_HASH"
 else
     echo "Skipping configuration and pipeline deploy steps for debug or no promote release build."
 fi
@@ -161,7 +164,7 @@ fi
 echo "Deploying application - default"
 verify_hash $COMMIT_HASH
 run_cmd gcloud -q app deploy ${PROMOTE_FLAGS} staging.yaml \
-       --project recidiviz-staging \
+       --project $PROJECT_ID \
        --version ${GAE_VERSION} \
        --image-url ${IMAGE_URL} \
        --verbosity=debug
@@ -170,7 +173,7 @@ if [[ -z ${DEBUG_BUILD_NAME} ]]; then
     echo "Deploying application - scrapers"
     verify_hash $COMMIT_HASH
     run_cmd gcloud -q app deploy ${PROMOTE_FLAGS} staging-scrapers.yaml \
-        --project recidiviz-staging \
+        --project $PROJECT_ID \
         --version ${GAE_VERSION} \
         --image-url ${IMAGE_URL} \
         --verbosity=debug
@@ -179,15 +182,15 @@ else
 fi
 
 if [[ ! -z ${PROMOTE} ]]; then
-    echo "App deployed to \`${GAE_VERSION}\`.recidiviz-staging.appspot.com"
+    echo "App deployed to \`${GAE_VERSION}\`.$PROJECT_ID.appspot.com"
 else
-    echo "App deployed (but not promoted) to \`${GAE_VERSION}\`.recidiviz-staging.appspot.com"
+    echo "App deployed (but not promoted) to \`${GAE_VERSION}\`.$PROJECT_ID.appspot.com"
 fi
 
 if [[ ! -z ${PROMOTE} ]]; then
     echo "Deploy succeeded - triggering post-deploy jobs."
     verify_hash $COMMIT_HASH
-    post_deploy_triggers 'recidiviz-staging' $CALC_CHANGES_SINCE_LAST_DEPLOY
+    post_deploy_triggers $PROJECT_ID $CALC_CHANGES_SINCE_LAST_DEPLOY
 else
     echo "Deploy succeeded - skipping post deploy triggers for no promote build."
 fi
