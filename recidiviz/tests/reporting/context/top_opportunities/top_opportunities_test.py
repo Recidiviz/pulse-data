4f95d3f598ba85eb8e92fc114977328ec78c500e
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-06-22 07:39:51 -0700

    [Top Opp Email] Generate emails in test (Recidiviz/recidiviz-data#7923)
    GitOrigin-RevId: 10915268b31455fa1016888280befa9c6521a6e8

---
+++ recidiviz/tests/reporting/context/top_opportunities/top_opportunities_test.py
@@ -0,0 +1,51 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for top_opportunities/context.py."""
+
+from typing import Optional
+from unittest.case import TestCase
+
+from recidiviz.persistence.database.schema_utils import SchemaType
+from recidiviz.persistence.database.sqlalchemy_database_key import SQLAlchemyDatabaseKey
+from recidiviz.tools.postgres import local_postgres_helpers
+
+
+class TopOpportunitiesContextTest(TestCase):
+    """Implements tests for the TopOpportunities context."""
+
+    # Stores the location of the postgres DB for this test run
+    temp_db_dir: Optional[str]
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.temp_db_dir = local_postgres_helpers.start_on_disk_postgresql_database()
+
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.database_key = SQLAlchemyDatabaseKey.for_schema(SchemaType.CASE_TRIAGE)
+        local_postgres_helpers.use_on_disk_postgresql_database(self.database_key)
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        local_postgres_helpers.teardown_on_disk_postgresql_database(self.database_key)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        local_postgres_helpers.stop_and_clear_on_disk_postgresql_database(
+            cls.temp_db_dir
+        )
