1474d061160efb640815ce5e71b22c98f1569413
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2021-06-24 17:08:06 -0400

    Script for running CloudSQL -> BQ refresh to sandbox (Recidiviz/recidiviz-data#7982)
    Adds a script for loading the current state of a Postgres instance into a sandbox BigQuery dataset. Can be used to test changes to the CloudSQL -> BigQuery refresh. Or when debugging issues in Postgres when the data has not yet been loaded to BigQuery (either because refresh for a given region is paused, or the daily refresh just hasn't run yet).
    
    I've been using this code in a less-refined form to test changes to the federated export and figured it should be a script for everyone!
    
    Related to Recidiviz/recidiviz-data#6067.
    
    GitOrigin-RevId: 7002d268e0a36651c1a0d046618aa881c6ad22a4

---
+++ recidiviz/tools/postgres/load_postgres_to_bq_sandbox.py
@@ -0,0 +1,134 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""A script for loading the current state of a Postgres instance into a sandbox BigQuery
+dataset. Can be used to test changes to the CloudSQL -> BigQuery refresh. Or when
+debugging issues in Postgres when the data has not yet been loaded to BigQuery (either
+because refresh for a given region is paused, or the daily refresh just hasn't run yet).
+
+NOTE: This will only load data from the PRIMARY databases in the STATE schema.
+
+Usage:
+    python -m recidiviz.tools.postgres.load_postgres_to_bq_sandbox \
+        --project_id [PROJECT_ID] \
+        --sandbox_dataset_prefix [SANDBOX_DATASET_PREFIX] \
+        --schema [SCHEMA]
+Example:
+    python -m recidiviz.tools.postgres.load_postgres_to_bq_sandbox \
+        --project_id recidiviz-staging \
+        --sandbox_dataset_prefix my_prefix \
+        --schema STATE
+
+"""
+import argparse
+import logging
+import sys
+from typing import List, Tuple
+from unittest import mock
+
+from recidiviz.persistence.database.bq_refresh import cloud_sql_to_bq_refresh_config
+from recidiviz.persistence.database.bq_refresh.cloud_sql_to_bq_refresh_config import (
+    CloudSqlToBQConfig,
+)
+from recidiviz.persistence.database.bq_refresh.federated_cloud_sql_to_bq_refresh import (
+    federated_bq_schema_refresh,
+)
+from recidiviz.persistence.database.schema_utils import SchemaType
+from recidiviz.tests.cloud_storage.fake_gcs_file_system import FakeGCSFileSystem
+from recidiviz.utils.environment import GCP_PROJECT_PRODUCTION, GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+STANDARD_YAML_CONTENTS = """
+region_codes_to_exclude: []
+state_history_tables_to_include:
+- state_person_history
+county_columns_to_exclude:
+person:
+- full_name
+- birthdate_inferred_from_age
+"""
+
+
+def main(sandbox_dataset_prefix: str, schema_type: SchemaType) -> None:
+    logging.info(
+        "Prefixing all output datasets with [%s_].", known_args.sandbox_dataset_prefix
+    )
+    fake_gcs = FakeGCSFileSystem()
+
+    # We mock the export config to a version that does not have any paused regions.
+    with mock.patch(
+        f"{cloud_sql_to_bq_refresh_config.__name__}.GcsfsFactory.build",
+        return_value=fake_gcs,
+    ):
+        fake_gcs.upload_from_string(
+            path=CloudSqlToBQConfig.default_config_path(),
+            contents=STANDARD_YAML_CONTENTS,
+            content_type="text/yaml",
+        )
+        federated_bq_schema_refresh(
+            schema_type=schema_type,
+            dataset_override_prefix=sandbox_dataset_prefix,
+        )
+        config = CloudSqlToBQConfig.for_schema_type(schema_type)
+        final_destination_dataset = config.unioned_multi_region_dataset(
+            dataset_override_prefix=sandbox_dataset_prefix
+        )
+
+    logging.info(
+        "Load complete. Data loaded to dataset [%s].", final_destination_dataset
+    )
+
+
+def parse_arguments(argv: List[str]) -> Tuple[argparse.Namespace, List[str]]:
+    """Parses the required arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--project_id",
+        dest="project_id",
+        type=str,
+        choices=[GCP_PROJECT_STAGING, GCP_PROJECT_PRODUCTION],
+        required=True,
+    )
+
+    parser.add_argument(
+        "--sandbox_dataset_prefix",
+        dest="sandbox_dataset_prefix",
+        help="A prefix to append to all names of the datasets where data will be loaded.",
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        "--schema",
+        type=SchemaType,
+        choices=list(SchemaType),
+        help="Specifies which schema to generate migrations for.",
+        required=True,
+    )
+
+    return parser.parse_known_args(argv)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    known_args, _ = parse_arguments(sys.argv)
+
+    with local_project_id_override(known_args.project_id):
+        main(
+            sandbox_dataset_prefix=known_args.sandbox_dataset_prefix,
+            schema_type=known_args.schema,
+        )
