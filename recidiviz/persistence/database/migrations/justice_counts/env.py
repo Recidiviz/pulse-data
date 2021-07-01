9ce0a493f07c7c7b131772110c1ed105481b9f81
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2021-07-01 15:06:19 -0700

    [Database] Update alembic url code to properly escape values (Recidiviz/recidiviz-data#8081)
    We were manually building database URLs that are used for running alembic migrations. Some version of alembic or sqlalchemy changed the url parsing behavior so that an `@` symbol in the password would be interpreted as the delimiter between the password and the host. The case triage DB had an `@` symbol in the password, which resulted in this error after I updated the pipenv in my `prod-data-client`:
    
    ```
    >  INFO:root:*** Starting postgres migrations for schema [SchemaType.CASE_TRIAGE], db_name [postgres] ***
    >  ERROR [root] Migrations failed to run: (psycopg2.OperationalError) could not translate host name "nvw@35.222.188.122" to address: Name or service not known
    >
    >  (Background on this error at: http://sqlalche.me/e/14/e3q8)
    Failed with exit code 1
    ```
    
    This PR updates to use `sqlalchemy.URL` objects to construct the URL and also consolidates this logic into a single file.
    
    I verified that `run_all_staging_migrations.sh` works with this change.
    
    GitOrigin-RevId: 4968db3ee64c13be910f910e8072a58f3bdd1ddf

--- recidiviz/persistence/database/migrations/justice_counts/env.py
+++ recidiviz/persistence/database/migrations/justice_counts/env.py
@@ -18,19 +18,13 @@ sys.path.insert(0, os.path.dirname(module_path))
 
 from logging.config import fileConfig
 
-from sqlalchemy import create_engine
-
 from alembic import context
-from recidiviz.persistence.database.constants import (
-    SQLALCHEMY_DB_USER,
-    SQLALCHEMY_DB_PASSWORD,
-    SQLALCHEMY_DB_HOST,
-    SQLALCHEMY_DB_NAME,
-    SQLALCHEMY_USE_SSL,
-    SQLALCHEMY_SSL_KEY_PATH,
-    SQLALCHEMY_SSL_CERT_PATH,
-)
+
 from recidiviz.persistence.database.base_schema import JusticeCountsBase
+from recidiviz.persistence.database.migrations.base_env import (
+    run_migrations_offline,
+    run_migrations_online,
+)
 
 # Import anything from the state schema.py files to ensure the table class
 # declarations are run within the Alembic environment
@@ -48,100 +42,7 @@ fileConfig(config.config_file_name)
 # Metadata from schema
 target_metadata = JusticeCountsBase.metadata
 
-# String defining database implementation used by SQLAlchemy engine
-_DB_TYPE = "postgresql"
-
-
-def get_sqlalchemy_url() -> str:
-    """Returns string needed to connect to database"""
-
-    # Boolean int (0 or 1) indicating whether to use SSL to connect to the
-    # database
-    use_ssl = int(os.getenv(SQLALCHEMY_USE_SSL, "0"))
-
-    if use_ssl == 1:
-        return _get_sqlalchemy_url_with_ssl()
-    if use_ssl == 0:
-        return _get_sqlalchemy_url_without_ssl()
-    raise RuntimeError("Invalid value for use_ssl: {use_ssl}".format(use_ssl=use_ssl))
-
-
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = get_sqlalchemy_url()
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        transaction_per_migration=True,
-        literal_binds=True,
-        compare_type=True,
-    )
-
-    context.run_migrations()
-
-
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    # Create a new connection if we don't already have one configured from Alembic
-    connectable = config.attributes.get(
-        "connection", create_engine(get_sqlalchemy_url())
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            transaction_per_migration=True,
-            compare_type=True,
-        )
-
-        context.run_migrations()
-
-
-def _get_sqlalchemy_url_without_ssl() -> str:
-    """Returns string used for SQLAlchemy engine, without SSL params"""
-
-    user = os.getenv(SQLALCHEMY_DB_USER)
-    password = os.getenv(SQLALCHEMY_DB_PASSWORD)
-    host = os.getenv(SQLALCHEMY_DB_HOST)
-    db_name = os.getenv(SQLALCHEMY_DB_NAME)
-
-    return "{db_type}://{user}:{password}@{host}/{db_name}".format(
-        db_type=_DB_TYPE, user=user, password=password, host=host, db_name=db_name
-    )
-
-
-def _get_sqlalchemy_url_with_ssl() -> str:
-    """Returns string used for SQLAlchemy engine, with SSL params"""
-
-    ssl_key_path = os.getenv(SQLALCHEMY_SSL_KEY_PATH)
-    ssl_cert_path = os.getenv(SQLALCHEMY_SSL_CERT_PATH)
-
-    ssl_params = "?sslkey={ssl_key_path}&sslcert={ssl_cert_path}".format(
-        ssl_key_path=ssl_key_path, ssl_cert_path=ssl_cert_path
-    )
-
-    url_without_ssl = _get_sqlalchemy_url_without_ssl()
-
-    return url_without_ssl + ssl_params
-
-
 if context.is_offline_mode():
-    run_migrations_offline()
+    run_migrations_offline(target_metadata)
 else:
-    run_migrations_online()
+    run_migrations_online(target_metadata, config)
