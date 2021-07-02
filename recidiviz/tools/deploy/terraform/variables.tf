b9dd545772e2410d4b7e4e071d9b05f9d53bca78
Dana Hoffman <danawillowhoffman@gmail.com>
2021-07-02 07:11:07 -0700

    add state-aggregate-reports GCS module + another small var (Recidiviz/recidiviz-data#7995)
    * add state-aggregate-reports GCS module + another small var
    
    * add oneoff import script
    
    * add character count info to var description
    
    * newline
    
    GitOrigin-RevId: 6cd05475893cbdefd08091d4e95296dd11acb809

--- recidiviz/tools/deploy/terraform/variables.tf
+++ recidiviz/tools/deploy/terraform/variables.tf
@@ -71,6 +71,18 @@ variable "github_username" {
   description = "GitHub user to mirror the pulse-data repo from."
 }
 
+variable "direct_ingest_state_storage_secondary_bucket_name_suffix" {
+  type    = string
+  default = "direct-ingest-state-storage-secondary"
+  description = <<EOT
+Name suffix of the direct ingest state storage secondary bucket.
+Used when deploying to a project that causes the full bucket name to be >63 characters
+(see https://cloud.google.com/storage/docs/naming-buckets#requirements).
+This affects projects whose names are >=26 characters.
+Defaults to direct-ingest-state-storage-secondary.
+EOT
+}
+
 locals {
   repo_url = "https://source.developers.google.com/projects/${var.project_id}/repos/github_${var.github_username}_pulse-data/revisions/${var.git_hash}/paths/recidiviz/cloud_functions"
 }
