5b0ec0d1e4ca5280fabb3c7a5b7098842281fc37
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-07-14 16:51:42 -0700

    [Terraform] Adds CloudSQL connections for secondary databases (Recidiviz/recidiviz-data#8242)
    GitOrigin-RevId: beed4e9130499bfbe08ba3ca4f6bbf7ce205d499

--- recidiviz/tools/deploy/terraform/modules/state-direct-ingest-resources/databases.tf
+++ recidiviz/tools/deploy/terraform/modules/state-direct-ingest-resources/databases.tf
@@ -29,13 +29,32 @@ resource "google_bigquery_connection" "state_primary_bq_connection" {
   provider = google-beta
 
   connection_id = "state_${lower(var.state_code)}_primary_cloudsql"
-  friendly_name = "${var.state_code} State Cloud SQL Postgres"
+  friendly_name = "${var.state_code} State Cloud SQL Postgres (Primary)"
   location      = var.cloudsql_instance_region
-  description   = "Connection to the ${var.state_code} State Cloud SQL database"
+  description   = "Connection to the ${var.state_code} State Cloud SQL database (primary)"
 
   cloud_sql {
     instance_id = var.cloudsql_instance_id
     database    = google_sql_database.state_primary.name
+    type        = "POSTGRES"
+    credential {
+      username = var.cloudsql_instance_user_name
+      password = var.cloudsql_instance_user_password
+    }
+  }
+}
+
+resource "google_bigquery_connection" "state_secondary_bq_connection" {
+  provider = google-beta
+
+  connection_id = "state_${lower(var.state_code)}_secondary_cloudsql"
+  friendly_name = "${var.state_code} State Cloud SQL Postgres (Secondary)"
+  location      = var.cloudsql_instance_region
+  description   = "Connection to the ${var.state_code} State Cloud SQL database (secondary)"
+
+  cloud_sql {
+    instance_id = var.cloudsql_instance_id
+    database    = google_sql_database.state_secondary.name
     type        = "POSTGRES"
     credential {
       username = var.cloudsql_instance_user_name
