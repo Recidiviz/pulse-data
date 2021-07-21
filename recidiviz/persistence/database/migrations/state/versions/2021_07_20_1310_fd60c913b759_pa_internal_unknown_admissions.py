ad23bc1d6d49fda6528c55274e015e80e3a16888
Julia Dressel <julia@recidiviz.org>
2021-07-20 17:06:52 -0700

    Handling `ADMITTED_FROM_SUPERVISION` admissions US_PA (Recidiviz/recidiviz-data#8352)
    This PR does the following:
    - Classifies all admissions to parole board holds as `TEMPORARY_CUSTODY` admissions. This is consistent with expected IP values.
    - Reclassifies all current non-board hold `ADMITTED_FROM_SUPERVISION` admissions as `INTERNAL_UNKNOWN`
    - Migrations to set correct admission reason values on existing IPs for the above changes.
    
    GitOrigin-RevId: 519ab4b8ad2a4fcfea228ce6f2190f15acf34f0c

---
+++ recidiviz/persistence/database/migrations/state/versions/2021_07_20_1310_fd60c913b759_pa_internal_unknown_admissions.py
@@ -0,0 +1,75 @@
+# pylint: skip-file
+"""pa_internal_unknown_admissions
+
+Revision ID: fd60c913b759
+Revises: b86b7c2bacb1
+Create Date: 2021-07-20 13:10:48.348816
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "fd60c913b759"
+down_revision = "b86b7c2bacb1"
+branch_labels = None
+depends_on = None
+
+RAW_TEXT_VALUES_TO_MIGRATE = [
+    "COV-FALSE-APV-FALSE",
+    "COV-FALSE-APV-NONE",
+    "CPV-FALSE-APV-FALSE",
+    "CPV-NONE-APV-FALSE",
+    "CPV-NONE-APV-NONE",
+    "NONE-FALSE-APD-FALSE",
+    "NONE-FALSE-APV-FALSE",
+    "NONE-FALSE-APV-NONE",
+    "TCV-FALSE-APV-FALSE",
+    "TPV-FALSE-APV-FALSE",
+    "TPV-NONE-APV-FALSE",
+    "TPV-NONE-APV-NONE",
+]
+
+RAW_TEXT_VALUES_AS_LIST_STRING = ",".join(
+    [f"'{value}'" for value in RAW_TEXT_VALUES_TO_MIGRATE]
+)
+
+TABLES_TO_UPDATE = ["state_incarceration_period", "state_incarceration_period_history"]
+
+UPDATE_QUERY = (
+    "UPDATE {table_name} SET admission_reason = '{new_value}'"
+    " WHERE state_code = 'US_PA'"
+    " AND admission_reason_raw_text IN ({raw_text_values});"
+)
+
+
+def upgrade() -> None:
+    connection = op.get_bind()
+
+    new_value = "INTERNAL_UNKNOWN"
+
+    with op.get_context().autocommit_block():
+        for table_name in TABLES_TO_UPDATE:
+            connection.execute(
+                UPDATE_QUERY.format(
+                    table_name=table_name,
+                    new_value=new_value,
+                    raw_text_values=RAW_TEXT_VALUES_AS_LIST_STRING,
+                )
+            )
+
+
+def downgrade() -> None:
+    connection = op.get_bind()
+
+    new_value = "ADMITTED_FROM_SUPERVISION"
+
+    with op.get_context().autocommit_block():
+        for table_name in TABLES_TO_UPDATE:
+            connection.execute(
+                UPDATE_QUERY.format(
+                    table_name=table_name,
+                    new_value=new_value,
+                    raw_text_values=RAW_TEXT_VALUES_AS_LIST_STRING,
+                )
+            )
