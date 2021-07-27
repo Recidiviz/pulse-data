9697d171df21fe69ce726e17789b643aa13004f0
Colin Adams <colinadams@google.com>
2021-07-27 16:55:32 -0700

    Remove gcsfs (Recidiviz/recidiviz-data#8429)
    GitOrigin-RevId: 37307027f4ce807c491279f29578040f0a175b50

--- recidiviz/tests/ingest/aggregate/scraper_aggregate_reports_test.py
+++ recidiviz/tests/ingest/aggregate/scraper_aggregate_reports_test.py
@@ -15,29 +15,36 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """Tests for tx_aggregate_ingest.py."""
-from typing import Any
-from unittest import TestCase
 import builtins
 import datetime
-import os
-import tempfile
-from flask import Flask
-from mock import patch, Mock, call
-import requests
-import gcsfs
-import pytz
+from typing import Any
+from unittest import TestCase
 
-from recidiviz.cloud_functions.cloud_function_utils import GCSFS_NO_CACHING
-from recidiviz.ingest.aggregate import scrape_aggregate_reports
+import pytz
+import requests
+from flask import Flask
+from mock import ANY, Mock, call, patch
+
+from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
 from recidiviz.ingest.aggregate.regions.ca import ca_aggregate_site_scraper
 from recidiviz.ingest.aggregate.regions.ny import ny_aggregate_site_scraper
 from recidiviz.ingest.aggregate.regions.tx import tx_aggregate_site_scraper
+from recidiviz.ingest.aggregate.scrape_aggregate_reports import (
+    HISTORICAL_BUCKET,
+    UPLOAD_BUCKET,
+    build_path,
+    scrape_aggregate_reports_blueprint,
+)
+from recidiviz.tests.cloud_storage.fake_gcs_file_system import (
+    FakeGCSFileSystem,
+    FakeGCSFileSystemDelegate,
+)
 from recidiviz.utils import metadata
 
 APP_ID = "recidiviz-scraper-aggregate-report-test"
 
 app = Flask(__name__)
-app.register_blueprint(scrape_aggregate_reports.scrape_aggregate_reports_blueprint)
+app.register_blueprint(scrape_aggregate_reports_blueprint)
 app.config["TESTING"] = True
 
 SERVER_MODIFIED_TIME = datetime.datetime(year=2019, month=1, day=1, tzinfo=pytz.UTC)
@@ -64,6 +71,11 @@ def _MockGet(url: str, **_: Any) -> Mock:
     return ret
 
 
+class FailOnUploadDelegate(FakeGCSFileSystemDelegate):
+    def on_file_added(self, path: GcsfsFilePath) -> None:
+        raise ValueError(f"{path.uri()} was added.")
+
+
 @patch.object(metadata, "project_id", Mock(return_value=TEST_ENV))
 @patch.object(metadata, "project_number", Mock(return_value=TEST_ENV))
 class TestScraperAggregateReports(TestCase):
@@ -71,22 +83,23 @@ class TestScraperAggregateReports(TestCase):
 
     def setUp(self) -> None:
         self.client = app.test_client()
-        self.historical_bucket = scrape_aggregate_reports.HISTORICAL_BUCKET.format(
-            TEST_ENV
-        )
-        self.upload_bucket = scrape_aggregate_reports.UPLOAD_BUCKET.format(TEST_ENV)
 
-    @patch.object(gcsfs, "GCSFileSystem")
+        self.fs = FakeGCSFileSystem()
+        self.gcs_factory_patcher = patch(
+            "recidiviz.ingest.aggregate.scrape_aggregate_reports.GcsfsFactory.build"
+        )
+        self.gcs_factory_patcher.start().return_value = self.fs
+
     @patch.object(requests, "get")
     @patch.object(builtins, "open")
     @patch.object(tx_aggregate_site_scraper, "get_urls_to_download")
     def testExistsNoUpload(
-        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock, mock_fs: Mock
+        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock
     ) -> None:
+        historical_path = build_path(HISTORICAL_BUCKET, "texas", EXISTING_PDF_NAME)
         # Make the info call return an older modified time than the server time.
-        mock_fs_return = Mock()
-        mock_fs.return_value = mock_fs_return
-        mock_fs_return.exists.return_value = True
+        self.fs.test_add_path(historical_path, local_path=None)
+        self.fs.test_set_delegate(FailOnUploadDelegate())
         mock_get_all_tx.return_value = {EXISTING_TEST_URL}
         mock_get.side_effect = _MockGet
 
@@ -94,26 +107,19 @@ class TestScraperAggregateReports(TestCase):
         response = self.client.get("/scrape_state?state=texas", headers=headers)
         self.assertEqual(response.status_code, 200)
 
-        mock_fs.assert_called_with(project=TEST_ENV, cache_timeout=GCSFS_NO_CACHING)
-        mock_fs_return.exists.assert_called_with(
-            os.path.join(self.historical_bucket, "texas", EXISTING_PDF_NAME)
-        )
-        self.assertEqual(mock_fs_return.put.called, False)
+        self.assertListEqual(self.fs.all_paths, [historical_path])
         self.assertEqual(mock_open.called, False)
 
-    @patch.object(gcsfs, "GCSFileSystem")
     @patch.object(requests, "get")
     @patch.object(builtins, "open")
     @patch.object(ny_aggregate_site_scraper, "get_urls_to_download")
     def testExistsIsNyUpload(
-        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock, mock_fs: Mock
+        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock
     ) -> None:
-        upload_bucket = os.path.join(self.upload_bucket, "new_york", EXISTING_PDF_NAME)
-        temploc = os.path.join(tempfile.gettempdir(), EXISTING_PDF_NAME)
+        historical_path = build_path(HISTORICAL_BUCKET, "new_york", EXISTING_PDF_NAME)
+        upload_path = build_path(UPLOAD_BUCKET, "new_york", EXISTING_PDF_NAME)
         # Make the info call return an older modified time than the server time.
-        mock_fs_return = Mock()
-        mock_fs.return_value = mock_fs_return
-        mock_fs_return.exists.return_value = True
+        self.fs.test_add_path(historical_path, local_path=None)
         mock_get_all_tx.return_value = {EXISTING_TEST_URL}
         mock_get.side_effect = _MockGet
 
@@ -121,25 +127,18 @@ class TestScraperAggregateReports(TestCase):
         response = self.client.get("/scrape_state?state=new_york", headers=headers)
         self.assertEqual(response.status_code, 200)
 
-        mock_fs.assert_called_with(project=TEST_ENV, cache_timeout=GCSFS_NO_CACHING)
-        self.assertFalse(mock_fs_return.exists.called)
-        mock_fs_return.put.assert_called_with(temploc, upload_bucket)
-        mock_open.assert_called_with(temploc, "wb")
+        self.assertListEqual(self.fs.all_paths, [historical_path, upload_path])
+        mock_open.assert_called_with(ANY, "wb")
         mock_get.assert_called_with(EXISTING_TEST_URL, verify=True)
 
-    @patch.object(gcsfs, "GCSFileSystem")
     @patch.object(requests, "get")
     @patch.object(builtins, "open")
     @patch.object(tx_aggregate_site_scraper, "get_urls_to_download")
     def testNoExistsUpload200(
-        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock, mock_fs: Mock
+        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock
     ) -> None:
-        upload_bucket = os.path.join(self.upload_bucket, "texas", EXISTING_PDF_NAME)
-        temploc = os.path.join(tempfile.gettempdir(), EXISTING_PDF_NAME)
+        upload_path = build_path(UPLOAD_BUCKET, "texas", EXISTING_PDF_NAME)
         # Make the info call return an older modified time than the server time.
-        mock_fs_return = Mock()
-        mock_fs.return_value = mock_fs_return
-        mock_fs_return.exists.return_value = False
         mock_get_all_tx.return_value = {EXISTING_TEST_URL}
         mock_get.side_effect = _MockGet
 
@@ -147,27 +146,18 @@ class TestScraperAggregateReports(TestCase):
         response = self.client.get("/scrape_state?state=texas", headers=headers)
         self.assertEqual(response.status_code, 200)
 
-        mock_fs.assert_called_with(project=TEST_ENV, cache_timeout=GCSFS_NO_CACHING)
-        mock_fs_return.exists.assert_called_with(
-            os.path.join(self.historical_bucket, "texas", EXISTING_PDF_NAME)
-        )
-        mock_fs_return.put.assert_called_with(temploc, upload_bucket)
-        mock_open.assert_called_with(temploc, "wb")
+        self.assertListEqual(self.fs.all_paths, [upload_path])
+        mock_open.assert_called_with(ANY, "wb")
         mock_get.assert_called_with(EXISTING_TEST_URL, verify=True)
 
-    @patch.object(gcsfs, "GCSFileSystem")
     @patch.object(requests, "post")
     @patch.object(builtins, "open")
     @patch.object(ca_aggregate_site_scraper, "get_urls_to_download")
     def testCaNoExistsUpload200(
-        self, mock_get_all_ca: Mock, mock_open: Mock, mock_post: Mock, mock_fs: Mock
+        self, mock_get_all_ca: Mock, mock_open: Mock, mock_post: Mock
     ) -> None:
-        upload_bucket = os.path.join(self.upload_bucket, "california", EXISTING_CA_NAME)
-        temploc = os.path.join(tempfile.gettempdir(), EXISTING_CA_NAME)
+        upload_path = build_path(UPLOAD_BUCKET, "california", EXISTING_CA_NAME)
         # Make the info call return an older modified time than the server time.
-        mock_fs_return = Mock()
-        mock_fs.return_value = mock_fs_return
-        mock_fs_return.exists.return_value = False
         mock_get_all_ca.return_value = [(EXISTING_TEST_URL_CA, CA_POST_DATA)]
         mock_post.side_effect = _MockGet
 
@@ -175,31 +165,21 @@ class TestScraperAggregateReports(TestCase):
         response = self.client.get("/scrape_state?state=california", headers=headers)
         self.assertEqual(response.status_code, 200)
 
-        mock_fs.assert_called_with(project=TEST_ENV, cache_timeout=GCSFS_NO_CACHING)
-        mock_fs_return.exists.assert_called_with(
-            os.path.join(self.historical_bucket, "california", EXISTING_CA_NAME)
-        )
-        mock_fs_return.put.assert_called_with(temploc, upload_bucket)
-        mock_open.assert_called_with(temploc, "wb")
+        self.assertListEqual(self.fs.all_paths, [upload_path])
+        mock_open.assert_called_with(ANY, "wb")
         mock_post.assert_called_with(
             EXISTING_TEST_URL_CA, data=CA_POST_DATA, verify=True
         )
 
-    @patch.object(gcsfs, "GCSFileSystem")
     @patch.object(requests, "get")
     @patch.object(builtins, "open")
     @patch.object(tx_aggregate_site_scraper, "get_urls_to_download")
     def testMultipleUrlsAll200(
-        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock, mock_fs: Mock
+        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock
     ) -> None:
-        upload_bucket1 = os.path.join(self.upload_bucket, "texas", EXISTING_PDF_NAME)
-        upload_bucket2 = os.path.join(self.upload_bucket, "texas", EXISTING_PDF_NAME2)
-        temploc1 = os.path.join(tempfile.gettempdir(), EXISTING_PDF_NAME)
-        temploc2 = os.path.join(tempfile.gettempdir(), EXISTING_PDF_NAME2)
+        upload_path1 = build_path(UPLOAD_BUCKET, "texas", EXISTING_PDF_NAME)
+        upload_path2 = build_path(UPLOAD_BUCKET, "texas", EXISTING_PDF_NAME2)
         # Make the info call return an older modified time than the server time.
-        mock_fs_return = Mock()
-        mock_fs.return_value = mock_fs_return
-        mock_fs_return.exists.return_value = False
         mock_get_all_tx.return_value = {EXISTING_TEST_URL, EXISTING_TEST_URL2}
         mock_get.side_effect = _MockGet
 
@@ -207,51 +187,27 @@ class TestScraperAggregateReports(TestCase):
         response = self.client.get("/scrape_state?state=texas", headers=headers)
         self.assertEqual(response.status_code, 200)
 
-        mock_fs.assert_called_with(project=TEST_ENV, cache_timeout=GCSFS_NO_CACHING)
-        self.assertEqual(mock_fs.call_count, 1)
-        expected_exists_calls = [
-            call(os.path.join(self.historical_bucket, "texas", EXISTING_PDF_NAME)),
-            call(os.path.join(self.historical_bucket, "texas", EXISTING_PDF_NAME2)),
-        ]
+        self.assertCountEqual(self.fs.all_paths, [upload_path1, upload_path2])
+        self.assertListEqual(mock_open.call_args_list, [call(ANY, "wb")] * 2)
         self.assertCountEqual(
-            mock_fs_return.exists.call_args_list, expected_exists_calls
+            mock_get.call_args_list,
+            [
+                call(EXISTING_TEST_URL, verify=True),
+                call(EXISTING_TEST_URL2, verify=True),
+            ],
         )
-        expected_put_calls = [
-            call(temploc1, upload_bucket1),
-            call(temploc2, upload_bucket2),
-        ]
-        self.assertCountEqual(mock_fs_return.put.call_args_list, expected_put_calls)
-        expected_open_calls = [call(temploc1, "wb"), call(temploc2, "wb")]
-        self.assertCountEqual(mock_open.call_args_list, expected_open_calls)
 
-    @patch.object(gcsfs, "GCSFileSystem")
     @patch.object(requests, "get")
     @patch.object(builtins, "open")
     @patch.object(tx_aggregate_site_scraper, "get_urls_to_download")
     def testMultipleUrlsOne200OneNoExists(
-        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock, mock_fs: Mock
+        self, mock_get_all_tx: Mock, mock_open: Mock, mock_get: Mock
     ) -> None:
-        historical_path1 = os.path.join(
-            self.historical_bucket, "texas", EXISTING_PDF_NAME
-        )
-        historical_path2 = os.path.join(
-            self.historical_bucket, "texas", EXISTING_PDF_NAME2
-        )
+        historical_path1 = build_path(HISTORICAL_BUCKET, "texas", EXISTING_PDF_NAME)
+        upload_path2 = build_path(UPLOAD_BUCKET, "texas", EXISTING_PDF_NAME2)
 
-        def _exists_return(path: str) -> bool:
-            ret_bool = False
-            if path == historical_path1:
-                ret_bool = True
-            elif path == historical_path2:
-                ret_bool = False
-            return ret_bool
-
-        upload_bucket2 = os.path.join(self.upload_bucket, "texas", EXISTING_PDF_NAME2)
-        temploc2 = os.path.join(tempfile.gettempdir(), EXISTING_PDF_NAME2)
         # Make the info call return an older modified time than the server time.
-        mock_fs_return = Mock()
-        mock_fs.return_value = mock_fs_return
-        mock_fs_return.exists.side_effect = _exists_return
+        self.fs.test_add_path(historical_path1, local_path=None)
         mock_get_all_tx.return_value = {EXISTING_TEST_URL, EXISTING_TEST_URL2}
         mock_get.side_effect = _MockGet
 
@@ -259,13 +215,8 @@ class TestScraperAggregateReports(TestCase):
         response = self.client.get("/scrape_state?state=texas", headers=headers)
         self.assertEqual(response.status_code, 200)
 
-        mock_fs.assert_called_with(project=TEST_ENV, cache_timeout=GCSFS_NO_CACHING)
-        self.assertEqual(mock_fs.call_count, 1)
-        expected_exists_calls = [call(historical_path1), call(historical_path2)]
+        self.assertCountEqual(self.fs.all_paths, [historical_path1, upload_path2])
+        self.assertListEqual(mock_open.call_args_list, [call(ANY, "wb")])
         self.assertCountEqual(
-            mock_fs_return.exists.call_args_list, expected_exists_calls
+            mock_get.call_args_list, [call(EXISTING_TEST_URL2, verify=True)]
         )
-        self.assertEqual(mock_fs_return.put.call_count, 1)
-        mock_fs_return.put.assert_called_with(temploc2, upload_bucket2)
-        mock_open.assert_called_with(temploc2, "wb")
-        self.assertEqual(mock_open.call_count, 1)
