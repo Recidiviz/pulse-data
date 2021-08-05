1c6022ee7809ded35b8547ca6b604bdc011fcb24
Dan Hansen <dan@recidiviz.org>
2021-08-04 18:58:10 -0700

    Add a VPC access connector in `us-central1` (recidiviz/recidiviz-data#8618)
    * Add a VPC access connector in us-central1
    
    * Use secret data
    
    GitOrigin-RevId: c24a0157068ceb8bcd3dadc5560819b27510b32c

--- recidiviz/tools/deploy/terraform/cloud-run.tf
+++ recidiviz/tools/deploy/terraform/cloud-run.tf
@@ -93,7 +93,7 @@ resource "google_cloud_run_service" "case-triage" {
         "autoscaling.knative.dev/maxScale"        = var.max_case_triage_instances
         "run.googleapis.com/cloudsql-instances"   = local.joined_connection_string
         "run.googleapis.com/sandbox"              = "gvisor"
-        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.redis_vpc_connector.name
+        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.us_central_redis_vpc_connector.name
 
       }
 
