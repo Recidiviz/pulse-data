42c8cc601da3b35f7c83e8d86ddcf0b8384e7ab0
Nikhil Bhargava <993682+nbhargava@users.noreply.github.com>
2021-08-05 13:37:51 -0400

    [Terraform] Clean up annotations to include auto-generated ones (Recidiviz/recidiviz-data#8624)
    GitOrigin-RevId: f8b8ec2a83919788dc34300ce1ed2b645692dd94

--- recidiviz/tools/deploy/terraform/cloud-run.tf
+++ recidiviz/tools/deploy/terraform/cloud-run.tf
@@ -94,7 +94,7 @@ resource "google_cloud_run_service" "case-triage" {
         "run.googleapis.com/cloudsql-instances"   = local.joined_connection_string
         "run.googleapis.com/sandbox"              = "gvisor"
         "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.us_central_redis_vpc_connector.name
-
+        "run.googleapis.com/vpc-access-egress"    = "private-ranges-only"
       }
 
       name = "case-triage-web-${replace(var.docker_image_tag, ".", "-")}"
