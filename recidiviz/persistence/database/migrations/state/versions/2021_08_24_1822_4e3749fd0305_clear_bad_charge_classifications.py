fb51fe6c1d43391678b241bcec84ccadc1394146
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2021-08-25 13:55:22 -0700

    [Ingest mappings] Add deserializing factory for StateCharge (recidiviz/recidiviz-data#8995)
    Added a StateCharge entity factory and back ported it for use in legacy ingest info proto parsing code.
    
    In the process, eliminated two pieces of (yikes) global StateCharge parsing logic.
    
    1. First piece of global logic attempted to pull in a description from `ncic.csv` if one did not already exist.
    ```
        if new.description is None and new.ncic_code is not None:
            ncic_description = ncic.get_description(new.ncic_code)
            if ncic_description:
                new.description = normalize(ncic_description)
    ```
    
    Only MO and ND hydrate the `ncic_code` field, so these are the only states that could be impacted by this change.
    ```
    SELECT DISTINCT state_code FROM `recidiviz-123.state.state_charge` WHERE ncic_code IS NOT NULL;
    ```
    * ND actually already has controller logic to grab the description from `ncic.csv`, so removing this logic globally has no impact.
    * MO hydrates the `ncic_code` and `description` fields from ingest view `tak022_tak024_tak025_tak026_offender_sentence_supervision`. I queried that view query and only 6 rows had a null value in `BS_COD`, the field used to hydrate description. All of the corresponding NCIC does don't show up in `ncic.csv` (e.g. 9999), so removing this logic globally has no impact.
    
    2. Second piece of global logic attempted to infer the `StateChargeClassificationType` from the charge description if a classification type did not already exist.
    ```
        if new.classification_type is None:
            new.classification_type = StateChargeClassificationType.find_in_string(
                new.description
            )
    ```
    This is very unpredictable and problematic because we have classification types like OTHER and CIVIL which definitely should not be inferred from the description. I did some digging and concluded that we can and should just remove this logic for all states.
    
    All this is based on queries for StateCharge where `classification_type` is not NULL and `classification_type_raw_text` IS NULL.
    
    * US_ID - 1052 out of 309609 charges have a `StateChargeClassificationType` due to this shared logic. All are incorrect inferences of CIVIL or OTHER. Rest of StateCharge objects have no classification type.
    * US_ND - 0 charges have a `StateChargeClassificationType` due to this shared logic.
    * US_PA - 6 charges have a `StateChargeClassificationType` due to this shared logic. All are incorrect mappings of OTHER.
    * US_MO - 31493 charges have a StateChargeClassificationType due to this shared logic (1.9%). 78120 have no classification type (4.6%). 1575203 have a fully hydrated classification type (93.5%). Of the 31493 that are hydrated based on this logic, 30123 have a null `offense_date` so we likely don't use them meaningfully in calcs.
    
    I added a migration to clear out the bad classification_types.
    
    Work towards recidiviz/recidiviz-data#8909.
    
    GitOrigin-RevId: ace7569f2b7243ecbb7d195c987914d53fc74950

---
+++ recidiviz/persistence/database/migrations/state/versions/2021_08_24_1822_4e3749fd0305_clear_bad_charge_classifications.py
@@ -0,0 +1,35 @@
+# pylint: skip-file
+"""clear_bad_charge_classifications
+
+Revision ID: 4e3749fd0305
+Revises: 6178a06afa16
+Create Date: 2021-08-24 18:22:24.398018
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "4e3749fd0305"
+down_revision = "6178a06afa16"
+branch_labels = None
+depends_on = None
+
+
+UPDATE_QUERY = (
+    "UPDATE {table_name} SET classification_type = NULL"
+    " WHERE classification_type_raw_text IS NULL;"
+)
+
+TABLES_TO_UPDATE = ["state_charge", "state_charge_history"]
+
+
+def upgrade() -> None:
+    with op.get_context().autocommit_block():
+        for table in TABLES_TO_UPDATE:
+            op.execute(UPDATE_QUERY.format(table_name=table))
+
+
+def downgrade() -> None:
+    # Migration is lossy, can't be undone.
+    pass
--- recidiviz/persistence/ingest_info_converter/state/entity_helpers/state_charge.py
+++ recidiviz/persistence/ingest_info_converter/state/entity_helpers/state_charge.py
@@ -15,24 +15,13 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ============================================================================
 """Converts an ingest_info proto StateCharge to a persistence entity."""
-from recidiviz.common import ncic
+from recidiviz.common import common_utils
 from recidiviz.common.constants.charge import ChargeStatus
+from recidiviz.common.constants.enum_parser import EnumParser
 from recidiviz.common.constants.state.state_charge import StateChargeClassificationType
 from recidiviz.common.ingest_metadata import IngestMetadata
-from recidiviz.common.str_field_utils import (
-    normalize,
-    parse_bool,
-    parse_date,
-    parse_int,
-)
 from recidiviz.ingest.models.ingest_info_pb2 import StateCharge
 from recidiviz.persistence.entity.state import entities
-from recidiviz.persistence.ingest_info_converter.utils.converter_utils import (
-    fn,
-    parse_external_id,
-    parse_region_code_with_override,
-)
-from recidiviz.persistence.ingest_info_converter.utils.enum_mappings import EnumMappings
 
 
 # TODO(#8905): Delete this file once all states have been migrated to v2 ingest
@@ -45,46 +34,38 @@ def copy_fields_to_builder(
 
     Note: This will not copy children into the Builder!
     """
-
-    enum_fields = {
-        "status": ChargeStatus,
-        "classification_type": StateChargeClassificationType,
-    }
-    enum_mappings = EnumMappings(proto, enum_fields, metadata.enum_overrides)
-
     # Enum values
-    new.status = enum_mappings.get(
-        ChargeStatus, default=ChargeStatus.PRESENT_WITHOUT_INFO
+    new.status = EnumParser(
+        getattr(proto, "status"), ChargeStatus, metadata.enum_overrides
     )
-    new.status_raw_text = fn(normalize, "status", proto)
-    new.classification_type = enum_mappings.get(StateChargeClassificationType)
-    new.classification_type_raw_text = fn(normalize, "classification_type", proto)
+    new.status_raw_text = getattr(proto, "status")
+    new.classification_type = EnumParser(
+        getattr(proto, "classification_type"),
+        StateChargeClassificationType,
+        metadata.enum_overrides,
+    )
+    new.classification_type_raw_text = getattr(proto, "classification_type")
 
     # 1-to-1 mappings
-    new.external_id = fn(parse_external_id, "state_charge_id", proto)
-    new.offense_date = fn(parse_date, "offense_date", proto)
-    new.date_charged = fn(parse_date, "date_charged", proto)
-    new.state_code = parse_region_code_with_override(proto, "state_code", metadata)
-    new.county_code = fn(normalize, "county_code", proto)
-    new.statute = fn(normalize, "statute", proto)
+    state_charge_id = getattr(proto, "state_charge_id")
+    new.external_id = (
+        None if common_utils.is_generated_id(state_charge_id) else state_charge_id
+    )
+    new.offense_date = getattr(proto, "offense_date")
+    new.date_charged = getattr(proto, "date_charged")
+    new.state_code = metadata.region
+    new.county_code = getattr(proto, "county_code")
+    new.statute = getattr(proto, "statute")
 
-    new.ncic_code = fn(normalize, "ncic_code", proto)
-    new.description = fn(normalize, "description", proto)
-    if new.description is None and new.ncic_code is not None:
-        ncic_description = ncic.get_description(new.ncic_code)
-        if ncic_description:
-            new.description = normalize(ncic_description)
+    new.ncic_code = getattr(proto, "ncic_code")
+    new.description = getattr(proto, "description")
 
-    new.attempted = fn(parse_bool, "attempted", proto)
-    if new.classification_type is None:
-        new.classification_type = StateChargeClassificationType.find_in_string(
-            new.description
-        )
-    new.classification_subtype = fn(normalize, "classification_subtype", proto)
-    new.offense_type = fn(normalize, "offense_type", proto)
-    new.is_violent = fn(parse_bool, "is_violent", proto)
-    new.is_sex_offense = fn(parse_bool, "is_sex_offense", proto)
-    new.counts = fn(parse_int, "counts", proto)
-    new.charge_notes = fn(normalize, "charge_notes", proto)
-    new.is_controlling = fn(parse_bool, "is_controlling", proto)
-    new.charging_entity = fn(normalize, "charging_entity", proto)
+    new.attempted = getattr(proto, "attempted")
+    new.classification_subtype = getattr(proto, "classification_subtype")
+    new.offense_type = getattr(proto, "offense_type")
+    new.is_violent = getattr(proto, "is_violent")
+    new.is_sex_offense = getattr(proto, "is_sex_offense")
+    new.counts = getattr(proto, "counts")
+    new.charge_notes = getattr(proto, "charge_notes")
+    new.is_controlling = getattr(proto, "is_controlling")
+    new.charging_entity = getattr(proto, "charging_entity")
