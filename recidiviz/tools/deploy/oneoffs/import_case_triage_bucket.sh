94ba65e79a34c65248cc217f1690f1eea1eb9bd0
Chinedu Ozodi <chin2413@gmail.com>
2021-09-14 10:57:52 -0700

    deleted one-off script (recidiviz/recidiviz-data#9188)
    Co-authored-by: Chinedu Ozodi <chinedu@recidiv.org>
    GitOrigin-RevId: bf3594798c6ab8cdf352b84f560c4baf45b354df

--- recidiviz/tools/deploy/oneoffs/import_case_triage_bucket.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
-source ${BASH_SOURCE_DIR}/terraform_oneoffs.sh
-terraform_import module.case-triage-data.google_storage_bucket.bucket $PROJECT_ID/$PROJECT_ID-case-triage-data
