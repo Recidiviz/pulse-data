89089b7de8198066059c5daf70aaef357297619a
Emily Chao (they/them) <emily@recidiviz.org>
2021-09-15 17:31:37 -0700

    Adjusting ingest for ND to match new supervision contacts schema (Recidiviz/recidiviz-data#9184)
    * ingest changes for ND
    
    * addressing anna and caroles comments
    
    * addressing comment
    
    GitOrigin-RevId: b20c75cfe8d2399e3231af333fe83bd3d1d3b61d

---
+++ recidiviz/ingest/direct/regions/us_nd/ingest_views/view_docstars_contacts_v2.py
@@ -0,0 +1,81 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query containing contacts information.
+
+   TODO(#5057): Update description once we ingest contact codes 2 - 6.
+   NOTE: This is only capturing contacts logged by the supervising officer."""
+
+from recidiviz.ingest.direct.views.direct_ingest_big_query_view_types import (
+    DirectIngestPreProcessedIngestViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+WITH contacts_with_split_supervisor_name AS (
+  SELECT
+    RecID,
+    SID,
+    CONTACT_DATE,
+    CONTACT_CODE,
+    C2 AS CONTACT_CODE_2,
+    C3 AS CONTACT_CODE_3,
+    C4 AS CONTACT_CODE_4,
+    C5 AS CONTACT_CODE_5,
+    C6 AS CONTACT_CODE_6,
+    CATEGORY,
+    SPLIT(ORIGINATOR, ', ')[OFFSET(0)] LNAME,
+    SPLIT(ORIGINATOR, ', ')[OFFSET(1)] FNAME,
+  FROM {docstars_contacts}
+  WHERE CONTACT_CODE IS NOT NULL
+  # Exclude system generated entries, as those don't represent contacts.
+  AND CONTACT_CODE != "SG"
+  # ND includes supervision and FTR contacts. Only include supervision contacts
+  AND CATEGORY = "Supervision"
+ )
+SELECT 
+  RecID,
+  SID,
+  CONTACT_DATE,
+  CONTACT_CODE AS CONTACT_CODE_1,
+  CONTACT_CODE_2,
+  CONTACT_CODE_3,
+  CONTACT_CODE_4,
+  CONTACT_CODE_5,
+  CONTACT_CODE_6,
+  CATEGORY,
+  contacts_with_split_supervisor_name.LNAME,
+  contacts_with_split_supervisor_name.FNAME,
+  OFFICER, 
+  FROM contacts_with_split_supervisor_name LEFT JOIN
+  {docstars_officers} officers
+  ON (
+    contacts_with_split_supervisor_name.LNAME = officers.LNAME AND 
+    contacts_with_split_supervisor_name.FNAME = officers.FNAME
+  )
+"""
+
+VIEW_BUILDER = DirectIngestPreProcessedIngestViewBuilder(
+    region="us_nd",
+    ingest_view_name="docstars_contacts_v2",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+    order_by_cols="SID",
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
