648bedb74d687db0cebdfa05cf1e22b8496cfc7b
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2021-09-29 09:48:49 -0700

    [Ingest mappings] Support for enum literal fields (Recidiviz/recidiviz-data#9424)
    Add support for directly setting an enum literal value. For enum literals, we will leave the raw text blank.
    
    Closes Recidiviz/recidiviz-data#9067
    Closes Recidiviz/recidiviz-data#8958
    
    GitOrigin-RevId: 63c7b284e51875d520fb3c51818491ec16a34d64

---
+++ recidiviz/persistence/database/migrations/state/versions/2021_09_28_1341_bcd42781c590_clear_pa_raw_texts.py
@@ -0,0 +1,69 @@
+# pylint: skip-file
+"""clear_pa_raw_texts
+
+Revision ID: bcd42781c590
+Revises: c5e0b7ec2b9a
+Create Date: 2021-09-28 13:41:32.099729
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "bcd42781c590"
+down_revision = "c5e0b7ec2b9a"
+branch_labels = None
+depends_on = None
+
+
+UPDATE_QUERY = (
+    "UPDATE {table_name} SET {raw_text_col} = NULL"
+    " WHERE state_code = '{state_code}' AND {enum_col} IN ('{raw_text_values_str}');"
+)
+
+DOWNGRADE_QUERY = (
+    "UPDATE {table_name} SET {raw_text_col} = {enum_col}"
+    " WHERE state_code = '{state_code}' AND {enum_col} IN ('{raw_text_values_str}');"
+)
+
+
+MIGRATION_INFO = {
+    "US_PA": [
+        ("state_person_alias", "alias_type", ["GIVEN_NAME"]),
+        ("state_incarceration_incident", "incident_type", ["REPORT", "CONTRABAND"]),
+    ]
+}
+
+
+def upgrade() -> None:
+    with op.get_context().autocommit_block():
+        for state_code, update_info_list in MIGRATION_INFO.items():
+            for update_info in update_info_list:
+                base_table, enum_col, raw_text_values_to_clear = update_info
+                for table in [base_table, f"{base_table}_history"]:
+                    op.execute(
+                        UPDATE_QUERY.format(
+                            table_name=table,
+                            state_code=state_code,
+                            enum_col=enum_col,
+                            raw_text_col=f"{enum_col}_raw_text",
+                            raw_text_values_str="', '".join(raw_text_values_to_clear),
+                        )
+                    )
+
+
+def downgrade() -> None:
+    with op.get_context().autocommit_block():
+        for state_code, update_info_list in MIGRATION_INFO.items():
+            for update_info in update_info_list:
+                base_table, enum_col, raw_text_values = update_info
+                for table in [base_table, f"{base_table}_history"]:
+                    op.execute(
+                        DOWNGRADE_QUERY.format(
+                            table_name=table,
+                            state_code=state_code,
+                            enum_col=enum_col,
+                            raw_text_col=f"{enum_col}_raw_text",
+                            raw_text_values_str="', '".join(raw_text_values),
+                        )
+                    )
