762af0bb078ef4456d135c0559049d905dede0a8
Julia Dressel <julia@recidiviz.org>
2021-09-30 14:51:45 -0700

    [Schema Cleanup] Deprecating `supervision_type` on SPs for all states (recidiviz/recidiviz-data#9430)
    Deprecates field supervision_type on StateSupervisionPeriod for all states. Only state using this was US_ND, so does the full deprecation migration (and controller updates) for that state.
    
    GitOrigin-RevId: 2651877266be80301871e463be6360104eec3f53

---
+++ recidiviz/persistence/database/migrations/state/versions/2021_09_28_1721_b81aa5311fb7_drop_supervision_type_us_nd.py
@@ -0,0 +1,42 @@
+# pylint: skip-file
+"""drop_supervision_type_us_nd
+
+Revision ID: b81aa5311fb7
+Revises: fa392d2c4eb1
+Create Date: 2021-09-28 17:21:07.793446
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "b81aa5311fb7"
+down_revision = "fa392d2c4eb1"
+branch_labels = None
+depends_on = None
+
+UPDATE_QUERY = (
+    "UPDATE {table_name} SET {nullify_cols_statement} WHERE state_code IN ('US_ND');"
+)
+
+COLS_TO_NULLIFY = ["supervision_type", "supervision_type_raw_text"]
+
+TABLES_TO_UPDATE = ["state_supervision_period", "state_supervision_period_history"]
+
+
+def upgrade() -> None:
+    with op.get_context().autocommit_block():
+        for table in TABLES_TO_UPDATE:
+            nullify_cols_statement = ", ".join(
+                [f"{col} = NULL" for col in COLS_TO_NULLIFY]
+            )
+            op.execute(
+                UPDATE_QUERY.format(
+                    table_name=table, nullify_cols_statement=nullify_cols_statement
+                )
+            )
+
+
+def downgrade() -> None:
+    # Field deprecation cannot be undone
+    pass
