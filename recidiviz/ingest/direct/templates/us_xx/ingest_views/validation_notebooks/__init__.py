1a959d98c28e8ed02b28a87fcad238c477cfac3e
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2021-11-05 16:51:23 -0700

    [Ingest] Add validation_notebooks directories to Copybara exclude-list (recidiviz/recidiviz-data#9913)
    We've been using Jupyter notebooks more for ingest view validation but it's very easy to forget to clear them before committing them to the codebase! I'm adding the validation_notebooks folder to the copybara exclude-list so we can't accidentally leak PII to pulse-data in these notebooks.
    
    GitOrigin-RevId: 7b50371764d8c31de4a4fca9a446669e8218ff67

--- recidiviz/ingest/direct/templates/us_xx/ingest_views/validation_notebooks/__init__.py
+++ recidiviz/ingest/direct/templates/us_xx/ingest_views/validation_notebooks/__init__.py
@@ -14,3 +14,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
+"""Put Jupyter notebooks for ingest view validation in this folder."""
