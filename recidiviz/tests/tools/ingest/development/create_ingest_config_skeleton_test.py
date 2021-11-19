67edd44fe8025de77105d796b4d87bd9a8e00480
Daniela Schiano di Cola <daniela@recidiviz.org>
2021-11-19 11:08:56 -0800

    Remove description fields with TODOs when generating raw data config skeleton (Recidiviz/recidiviz-data#10027)
    * Remove description fields with TODOs when generating raw data config skeleton
    
    * Add option for description placeholders
    
    * Add test for description placeholders
    
    * Remove extra print
    
    * Fix TODO format
    
    GitOrigin-RevId: 9a6ecdb4d77f80b377eac7c910d83bf826cba2b2

--- recidiviz/tests/tools/ingest/development/create_ingest_config_skeleton_test.py
+++ recidiviz/tests/tools/ingest/development/create_ingest_config_skeleton_test.py
@@ -23,6 +23,7 @@ import unittest
 from recidiviz.ingest.direct.controllers.direct_ingest_raw_file_import_manager import (
     DirectIngestRegionRawFileConfig,
 )
+from recidiviz.tests.ingest.direct.direct_ingest_util import PLACEHOLDER_TO_DO_STRING
 from recidiviz.tools.ingest.development.create_ingest_config_skeleton import (
     create_ingest_config_skeleton,
     make_config_directory,
@@ -34,27 +35,35 @@ FAKE_STATE = "us_xx"
 
 
 class CreateIngestConfigSkeletonTest(unittest.TestCase):
+    """Tests for create_ingest_config_skeleton.py"""
+
+    def setUp(self) -> None:
+        self.input_path = os.path.join(
+            os.path.dirname(__file__),
+            "create_ingest_config_skeleton_test_fixtures",
+            INPUT_TABLE + ".txt",
+        )
+        self.state_code = FAKE_STATE
+        self.delimiter = "|"
+        self.allow_overwrite = False
+        self.initialize_state = True
+        self.add_description_placeholders = False
+
     def tearDown(self) -> None:
         shutil.rmtree(os.path.split(make_config_directory(FAKE_STATE))[0])
 
     def test_create_structure_and_check(self) -> None:
         """Create a raw file config from a test input, try to ingest it, and check the results."""
-        state_code = FAKE_STATE
-        delimiter = "|"
-        allow_overwrite = False
-        initialize_state = True
-
-        input_path = os.path.join(
-            os.path.dirname(__file__),
-            "create_ingest_config_skeleton_test_fixtures",
-            INPUT_TABLE + ".txt",
-        )
-
         create_ingest_config_skeleton(
-            [input_path], state_code, delimiter, allow_overwrite, initialize_state
+            [self.input_path],
+            self.state_code,
+            self.delimiter,
+            self.allow_overwrite,
+            self.initialize_state,
+            self.add_description_placeholders,
         )
 
-        config = DirectIngestRegionRawFileConfig(region_code=state_code)
+        config = DirectIngestRegionRawFileConfig(region_code=self.state_code)
 
         self.assertIsNotNone(config)
 
@@ -63,4 +72,34 @@ class CreateIngestConfigSkeletonTest(unittest.TestCase):
         self.assertEqual(
             [field.name for field in table_config.columns],
             ["field1", "field2", "field3", "field_4"],
+        )
+        self.assertEqual(
+            [field.description for field in table_config.columns],
+            [None, None, None, None],
+        )
+
+    def test_create_ingest_config_skeleton_description_placeholders(self) -> None:
+        """Create a raw file config with description placeholders"""
+        create_ingest_config_skeleton(
+            [self.input_path],
+            self.state_code,
+            self.delimiter,
+            self.allow_overwrite,
+            self.initialize_state,
+            add_description_placeholders=True,
+        )
+
+        config = DirectIngestRegionRawFileConfig(region_code=self.state_code)
+        table_config = config.raw_file_configs[INPUT_TABLE]
+
+        self.assertIsNotNone(config)
+
+        self.assertEqual(
+            [field.description for field in table_config.columns],
+            [
+                PLACEHOLDER_TO_DO_STRING,
+                PLACEHOLDER_TO_DO_STRING,
+                PLACEHOLDER_TO_DO_STRING,
+                PLACEHOLDER_TO_DO_STRING,
+            ],
         )
