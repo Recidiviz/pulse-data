65e30c36bce1b10d4b5218d774cfc45f66e0774b
Julia Dressel <julia@recidiviz.org>
2021-11-22 17:26:08 -0800

    [US_ND] Update is_period_included_in_state_population logic US_ND (Recidiviz/recidiviz-data#10141)
    Adds state-specific implementation of is_period_included_in_state_population for US_ND. We do not currently set custodial_authority values on IPs in US_ND. Once we start doing this, we will want to divert all non-STATE_PRISON periods to the metrics for periods that aren't included in the state's pop.
    
    GitOrigin-RevId: 992b3456976343957e7e7cb0956b736034203f32

---
+++ recidiviz/tests/calculator/pipeline/utils/state_utils/us_nd/us_nd_incarceration_delegate_test.py
@@ -0,0 +1,109 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests us_nd_incarceration_delegate.py."""
+import unittest
+from datetime import date
+
+from recidiviz.calculator.pipeline.utils.state_utils.us_nd.us_nd_incarceration_delegate import (
+    UsNdIncarcerationDelegate,
+)
+from recidiviz.common.constants.state.shared_enums import StateCustodialAuthority
+from recidiviz.common.constants.state.state_incarceration import StateIncarcerationType
+from recidiviz.common.constants.state.state_incarceration_period import (
+    StateIncarcerationPeriodAdmissionReason,
+    StateIncarcerationPeriodReleaseReason,
+    StateIncarcerationPeriodStatus,
+)
+from recidiviz.persistence.entity.state.entities import StateIncarcerationPeriod
+
+STATE_CODE = "US_ND"
+
+
+class TestUsNdIncarcerationDelegate(unittest.TestCase):
+    """Tests functions in UsNdIncarcerationDelegate."""
+
+    def setUp(self) -> None:
+        self.delegate = UsNdIncarcerationDelegate()
+
+    def test_is_period_included_in_state_population_state_prison_custodial_authority(
+        self,
+    ) -> None:
+        incarceration_period = StateIncarcerationPeriod.new_with_defaults(
+            incarceration_period_id=1112,
+            external_id="2",
+            incarceration_type=StateIncarcerationType.STATE_PRISON,
+            status=StateIncarcerationPeriodStatus.NOT_IN_CUSTODY,
+            custodial_authority=StateCustodialAuthority.STATE_PRISON,
+            state_code=STATE_CODE,
+            admission_date=date(2008, 12, 20),
+            admission_reason=StateIncarcerationPeriodAdmissionReason.NEW_ADMISSION,
+            release_date=date(2010, 12, 21),
+            release_reason=StateIncarcerationPeriodReleaseReason.CONDITIONAL_RELEASE,
+        )
+
+        self.assertTrue(
+            self.delegate.is_period_included_in_state_population(incarceration_period)
+        )
+
+    def test_is_period_included_in_state_population_not_state_prison_custodial_authority(
+        self,
+    ) -> None:
+        incarceration_period = StateIncarcerationPeriod.new_with_defaults(
+            incarceration_period_id=1112,
+            external_id="2",
+            incarceration_type=StateIncarcerationType.STATE_PRISON,
+            status=StateIncarcerationPeriodStatus.NOT_IN_CUSTODY,
+            state_code=STATE_CODE,
+            admission_date=date(2008, 12, 20),
+            admission_reason=StateIncarcerationPeriodAdmissionReason.NEW_ADMISSION,
+            release_date=date(2010, 12, 21),
+            release_reason=StateIncarcerationPeriodReleaseReason.CONDITIONAL_RELEASE,
+        )
+
+        for custodial_authority in StateCustodialAuthority:
+            if custodial_authority == StateCustodialAuthority.STATE_PRISON:
+                continue
+
+            incarceration_period.custodial_authority = custodial_authority
+
+            self.assertFalse(
+                self.delegate.is_period_included_in_state_population(
+                    incarceration_period
+                )
+            )
+
+    def test_is_period_included_in_state_population_null_custodial_authority(
+        self,
+    ) -> None:
+        incarceration_period = StateIncarcerationPeriod.new_with_defaults(
+            incarceration_period_id=1112,
+            external_id="2",
+            incarceration_type=StateIncarcerationType.STATE_PRISON,
+            status=StateIncarcerationPeriodStatus.NOT_IN_CUSTODY,
+            custodial_authority=None,
+            state_code=STATE_CODE,
+            admission_date=date(2008, 12, 20),
+            admission_reason=StateIncarcerationPeriodAdmissionReason.NEW_ADMISSION,
+            release_date=date(2010, 12, 21),
+            release_reason=StateIncarcerationPeriodReleaseReason.CONDITIONAL_RELEASE,
+        )
+
+        # TODO(#3641): Change this to assert False once we're setting the
+        #  custodial_authority field at ingest for US_ND
+        self.assertTrue(
+            self.delegate.is_period_included_in_state_population(incarceration_period)
+        )
