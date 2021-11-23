a75cb3387dbc02d91212836c94ee5d723cef21f1
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2021-11-23 13:32:38 -0800

    [Bugfix] Stop `purge_state_db` script from hanging (Recidiviz/recidiviz-data#10149)
    The `alembic` package setup hijacks logging in a way that disconnects the communication between the `purge_state_db` and `purge_state_db_remote_helper`.
    
    This change adds a way to turn off alembic logging configuration and also turns it off for this script. I have confirmed that the purge_state_db script no longer hangs now!
    
    Closes Recidiviz/recidiviz-data#9699
    
    GitOrigin-RevId: 4110d5c2848498e2fc0c4187d4ab90a1500eed67

--- recidiviz/persistence/database/migrations/justice_counts/env.py
+++ recidiviz/persistence/database/migrations/justice_counts/env.py
@@ -37,7 +37,8 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+if config.attributes.get("configure_logger", True):
+    fileConfig(config.config_file_name)
 
 # Metadata from schema
 target_metadata = JusticeCountsBase.metadata
