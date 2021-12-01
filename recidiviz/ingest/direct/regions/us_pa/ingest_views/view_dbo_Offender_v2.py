7b9f047be32527aa7c70359d17e344dfa88ca56a
Colin Adams <colin@recidiviz.org>
2021-12-01 11:56:46 -0800

    [PA] Ensure all people from dbo_Offender are included in output (Recidiviz/recidiviz-data#10209)
    GitOrigin-RevId: 3bf66ee91347435a9f22b97afcede992eb46fa6f

--- recidiviz/ingest/direct/regions/us_pa/ingest_views/view_dbo_Offender_v2.py
+++ recidiviz/ingest/direct/regions/us_pa/ingest_views/view_dbo_Offender_v2.py
@@ -16,42 +16,13 @@
 # =============================================================================
 """Query containing person demographic and identifier information from PBPP."""
 
-from recidiviz.ingest.direct.regions.us_pa.ingest_views.templates_person_external_ids import (
-    PRIMARY_STATE_IDS_FRAGMENT_V2,
-)
 from recidiviz.ingest.direct.views.direct_ingest_big_query_view_types import (
     DirectIngestPreProcessedIngestViewBuilder,
 )
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
-# TODO(#10073): Delete this view
-VIEW_QUERY_TEMPLATE = f"""WITH
-{PRIMARY_STATE_IDS_FRAGMENT_V2},
-base_query AS (
-  SELECT 
-    ids.recidiviz_primary_person_id, ParoleNumber, OffRaceEthnicGroup, OffSex,
-    ROW_NUMBER() OVER (PARTITION BY recidiviz_primary_person_id ORDER BY LastModifiedDate DESC) AS recency_rank
-  FROM {{dbo_Offender}} offender
-  JOIN
-  (SELECT DISTINCT recidiviz_primary_person_id, parole_number FROM recidiviz_primary_person_ids) ids
-  ON ids.parole_number = offender.ParoleNumber
-),
-races_ethnicities AS (
-  SELECT 
-    recidiviz_primary_person_id,
-    STRING_AGG(DISTINCT OffRaceEthnicGroup, ',' ORDER BY OffRaceEthnicGroup) AS races_ethnicities_list
-  FROM base_query
-  GROUP BY recidiviz_primary_person_id
-),
-people_with_races_ethnicities as (
-  SELECT * EXCEPT (recency_rank)
-  FROM base_query
-  LEFT OUTER JOIN
-  races_ethnicities
-  USING (recidiviz_primary_person_id)
-  WHERE recency_rank = 1
-),
+VIEW_QUERY_TEMPLATE = """WITH
 -- TODO(#8895): Don't pull in contacts here once we have a real source for names.
 names_from_parole_contacts as (
   SELECT
@@ -69,22 +40,24 @@ names_from_parole_contacts as (
         suffix,
         created_date
       FROM
-        {{dbo_PRS_FACT_PAROLEE_CNTC_SUMRY}}
+        {dbo_PRS_FACT_PAROLEE_CNTC_SUMRY}
       WHERE
         first_name IS NOT NULL
         AND last_name IS NOT NULL ) )
   WHERE
     recency_rank = 1
 )
-SELECT people.*, names.first_name, names.last_name, names.suffix
-FROM people_with_races_ethnicities people
+SELECT 
+  offender.ParoleNumber, offender.OffRaceEthnicGroup, offender.OffSex,
+  names.first_name, names.last_name, names.suffix
+FROM {dbo_Offender} offender
 LEFT JOIN names_from_parole_contacts names
-ON people.ParoleNumber = names.parole_number
+ON offender.ParoleNumber = names.parole_number
 """
 
 VIEW_BUILDER = DirectIngestPreProcessedIngestViewBuilder(
     region="us_pa",
-    ingest_view_name="dbo_Offender",
+    ingest_view_name="dbo_Offender_v2",
     view_query_template=VIEW_QUERY_TEMPLATE,
     order_by_cols=None,
     materialize_raw_data_table_views=True,
