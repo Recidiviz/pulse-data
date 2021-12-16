59fd08a10b463d40afec8e21eb12a4e2264950ef
Julia Dressel <julia@recidiviz.org>
2021-12-15 20:00:20 -0800

    Revert "[US_ND] Remove calc support for v1 incarceration periods (Recidiviz/recidiviz-data#10315)" (Recidiviz/recidiviz-data#10390)
    This reverts commit e09fa2a, which introduced breaking changes to the pipelines.
    
    GitOrigin-RevId: cd665280aa4cdbd6a8beb39dd488decb940cb97c

--- recidiviz/ingest/direct/regions/us_nd/us_nd_legacy_enum_helpers.py
+++ recidiviz/ingest/direct/regions/us_nd/us_nd_legacy_enum_helpers.py
@@ -39,6 +39,9 @@ from recidiviz.common.constants.state.state_incarceration_incident import (
     StateIncarcerationIncidentOutcomeType,
     StateIncarcerationIncidentType,
 )
+from recidiviz.common.constants.state.state_incarceration_period import (
+    StateIncarcerationPeriodAdmissionReason,
+)
 from recidiviz.common.constants.state.state_person_alias import StatePersonAliasType
 from recidiviz.common.constants.state.state_program_assignment import (
     StateProgramAssignmentParticipationStatus,
@@ -132,6 +135,10 @@ def supervision_contact_location_mapper(
 IGNORES: Dict[Type[Enum], List[str]] = {
     # TODO(#2305): What are the appropriate court case statuses?
     StateCourtCaseStatus: ["A", "STEP"],
+    # TODO(#10152): Delete the StateIncarcerationPeriodAdmissionReason mappings
+    #  once the dependency on this logic has been removed from US_ND Dataflow
+    #  pre-processing.
+    StateIncarcerationPeriodAdmissionReason: ["COM", "CONT", "CONV", "NTAD"],
 }
 
 
@@ -160,6 +167,46 @@ def generate_enum_overrides() -> EnumOverrides:
         StateSentenceStatus.SERVING: ["O"],
         StateChargeClassificationType.FELONY: ["IF"],
         StateChargeClassificationType.MISDEMEANOR: ["IM"],
+        # TODO(#10152): Delete the StateIncarcerationPeriodAdmissionReason mappings
+        #  once the dependency on this logic has been removed from US_ND Dataflow
+        #  pre-processing.
+        StateIncarcerationPeriodAdmissionReason.ADMITTED_IN_ERROR: ["ADM ERROR"],
+        StateIncarcerationPeriodAdmissionReason.EXTERNAL_UNKNOWN: ["OTHER", "PREA"],
+        StateIncarcerationPeriodAdmissionReason.NEW_ADMISSION: [
+            "ADMN",
+            "RAB",
+            "DEF",
+        ],
+        StateIncarcerationPeriodAdmissionReason.REVOCATION: [
+            "PARL",
+            "PV",
+            "NPRB",
+            "NPROB",
+            "PRB",
+            "RPRB",
+        ],
+        StateIncarcerationPeriodAdmissionReason.RETURN_FROM_ESCAPE: ["REC", "RECA"],
+        StateIncarcerationPeriodAdmissionReason.RETURN_FROM_ERRONEOUS_RELEASE: [
+            "READMN"
+        ],
+        StateIncarcerationPeriodAdmissionReason.TRANSFER: [
+            "CONF",
+            "CRT",
+            "DETOX",
+            "FED",
+            "HOSP",
+            "HOSPS",
+            "HOSPU",
+            "INT",
+            "JOB",
+            "MED",
+            "PROG",
+            "RB",
+            "SUPL",
+        ],
+        StateIncarcerationPeriodAdmissionReason.TRANSFER_FROM_OTHER_JURISDICTION: [
+            "OOS"
+        ],
         StateIncarcerationIncidentType.DISORDERLY_CONDUCT: [
             "DAMAGE",
             "DISCON",
