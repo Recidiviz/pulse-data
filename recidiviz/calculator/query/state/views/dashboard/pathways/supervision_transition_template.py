536f262338e858e7cc825ffeb6d155053c07395d
Jen Overgaag <jovergaag@gmail.com>
2021-12-16 15:43:30 -0800

    add person_count column to supervision_transition_template (Recidiviz/recidiviz-data#10395)
    GitOrigin-RevId: 5c81431776b917cdcd8a3a1e7d953dc4efe200c6

--- recidiviz/calculator/query/state/views/dashboard/pathways/supervision_transition_template.py
+++ recidiviz/calculator/query/state/views/dashboard/pathways/supervision_transition_template.py
@@ -71,7 +71,7 @@ def supervision_transition_template(status: str, transition_type: str) -> str:
       FROM blanks_filled
     )
 
-    SELECT *
+    SELECT *, {transition_type} as person_count,
     FROM averaged_{transition_type}
     WHERE DATE(year, month, 1) BETWEEN
       DATE_SUB(CURRENT_DATE('US/Pacific'), INTERVAL 60 MONTH) AND CURRENT_DATE('US/Pacific')
