4edc6bcc3802c528d9d3f2ecc6457e0d950c8ac0
Daniela Schiano di Cola <daniela@recidiviz.org>
2022-01-13 13:45:20 -0800

    [US_ME] Add US_ME state-specific calculation delegates (Recidiviz/recidiviz-data#10645)
    * Add US_ME pre-processing placeholder classes
    
    * Add missing __init__ file
    
    * Add 2022 copyright and alphabetize switch statement
    
    GitOrigin-RevId: eec79e03dd7db2ab2c253f0e0b2f2e1aeee3aac8

---
+++ recidiviz/calculator/pipeline/utils/state_utils/us_me/us_me_incarceration_period_pre_processing_delegate.py
@@ -0,0 +1,28 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Contains state-specific logic for certain aspects of pre-processing US_ME
+StateIncarcerationPeriod entities so that they are ready to be used in pipeline
+calculations."""
+from recidiviz.calculator.pipeline.utils.incarceration_period_pre_processing_manager import (
+    StateSpecificIncarcerationPreProcessingDelegate,
+)
+
+
+class UsMeIncarcerationPreProcessingDelegate(
+    StateSpecificIncarcerationPreProcessingDelegate
+):
+    """US_ME implementation of the StateSpecificIncarcerationPreProcessingDelegate."""
