763cf6713887ebe20b8c3fd30885d7ca0481a5b4
Daniela Schiano di Cola <daniela@recidiviz.org>
2022-01-14 10:18:14 -0800

    [US_ME] Add tests for state-specific calculation delegates (Recidiviz/recidiviz-data#10658)
    * Add tests for state pre-processing delegates
    
    * Rename from utils to delegate
    
    GitOrigin-RevId: 4ff0027d90505bddac7bbad58b2c8256bc75f086

---
+++ recidiviz/tests/calculator/pipeline/utils/state_utils/us_me/us_me_supervision_period_pre_processing_delegate_test.py
@@ -0,0 +1,34 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2022 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""Tests us_me_supervision_period_pre_processing_delegate.py."""
+import unittest
+
+from recidiviz.calculator.pipeline.utils.state_utils.us_me.us_me_supervision_period_pre_processing_delegate import (
+    UsMeSupervisionPreProcessingDelegate,
+)
+from recidiviz.common.constants.states import StateCode
+
+_STATE_CODE = StateCode.US_ME.value
+
+
+class TestUsMeSupervisionPreProcessingDelegate(unittest.TestCase):
+    """Tests functions in UsMeSupervisionPreProcessingDelegate."""
+
+    def setUp(self) -> None:
+        self.delegate = UsMeSupervisionPreProcessingDelegate()
+
+    # ~~ Add new tests here ~~
