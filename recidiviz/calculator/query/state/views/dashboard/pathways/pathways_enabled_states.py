eebd4bc3305a1450c9c084258312d4445dff3cc2
Ian MacFarland <macfarlandian@users.noreply.github.com>
2022-01-18 10:49:54 -0800

    Pathways supervision snapshot view (Recidiviz/recidiviz-data#10633)
    GitOrigin-RevId: 675b8855a9280c55b37b0ccca8dab00eafb2b72c

--- recidiviz/calculator/query/state/views/dashboard/pathways/pathways_enabled_states.py
+++ recidiviz/calculator/query/state/views/dashboard/pathways/pathways_enabled_states.py
@@ -18,4 +18,9 @@
 
 from recidiviz.common.constants.states import StateCode
 
-ENABLED_STATES = [StateCode.US_TN.value, StateCode.US_ID.value, StateCode.US_ND.value]
+ENABLED_STATES = [
+    StateCode.US_ID.value,
+    StateCode.US_ME.value,
+    StateCode.US_ND.value,
+    StateCode.US_TN.value,
+]
