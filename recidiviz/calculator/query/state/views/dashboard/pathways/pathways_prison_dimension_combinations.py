8b40ed46f389773242b1359283c0194efd26aa5d
Jen Overgaag <jovergaag@gmail.com>
2022-01-25 10:54:07 -0800

    [Pathways] prison_to_supervision_count_by_month view (Recidiviz/recidiviz-data#10760)
    * prison_to_supervision_count_by_month
    
    * map location name
    
    * change person_count to event_count
    
    * docs
    
    * prison_to_supervision_transitions
    
    * generalize transition_template and rename pathways_supervision_dimension_combinations
    
    * prison_to_supervision_count_by_month refactor
    
    * get_pathways_constant_dimensions_unnested
    
    * split compartment_location_end
    
    * pylint
    
    GitOrigin-RevId: 92abd683cad6c4ed217a3c41c7adafb4f677b857

---
+++ recidiviz/calculator/query/state/views/dashboard/pathways/pathways_prison_dimension_combinations.py
@@ -0,0 +1,86 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Helper view providing all possible combinations of dimension values. Useful for building exhaustive views."""
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.calculator.query.state.views.dashboard.pathways.pathways_constant_dimensions import (
+    get_constant_dimensions_unnested,
+)
+from recidiviz.calculator.query.state.views.dashboard.pathways.pathways_enabled_states import (
+    ENABLED_STATES,
+)
+from recidiviz.calculator.query.state.views.dashboard.pathways.pathways_supervision_dimension_combinations import (
+    state_specific_joins,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+PATHWAYS_PRISON_DIMENSION_COMBINATIONS_VIEW_NAME = (
+    "pathways_prison_dimension_combinations"
+)
+
+PATHWAYS_PRISON_DIMENSION_COMBINATIONS_DESCRIPTION = "Helper view providing all possible combinations of incarceration dimension values. Useful for building exhaustive views."
+
+PATHWAYS_PRISON_DIMENSION_COMBINATIONS_QUERY_TEMPLATE = """
+    /*{description}*/
+    WITH 
+    constant_dimensions AS (
+        SELECT
+            *
+        FROM {constant_dimensions}
+    )
+    , facilities AS (
+        SELECT
+            state_code,
+            location_name AS facility,
+        FROM `{project_id}.{dashboard_views_dataset}.pathways_incarceration_location_name_map`
+
+        UNION ALL
+
+        SELECT
+            state_code,
+            "ALL" as facility,
+        FROM UNNEST({enabled_states}) as state_code
+    )
+    , state_specific_dimensions AS (
+        SELECT
+            state_code,
+            facility
+        FROM facilities
+        {state_specific_joins}
+    )
+
+    SELECT
+        *
+    FROM constant_dimensions
+    FULL OUTER JOIN state_specific_dimensions USING (state_code)
+"""
+
+PATHWAYS_PRISON_DIMENSION_COMBINATIONS_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=dataset_config.DASHBOARD_VIEWS_DATASET,
+    view_id=PATHWAYS_PRISON_DIMENSION_COMBINATIONS_VIEW_NAME,
+    view_query_template=PATHWAYS_PRISON_DIMENSION_COMBINATIONS_QUERY_TEMPLATE,
+    description=PATHWAYS_PRISON_DIMENSION_COMBINATIONS_DESCRIPTION,
+    dashboard_views_dataset=dataset_config.DASHBOARD_VIEWS_DATASET,
+    constant_dimensions=",".join(get_constant_dimensions_unnested({})),
+    enabled_states=str(ENABLED_STATES),
+    state_specific_joins=" ".join(state_specific_joins),
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        PATHWAYS_PRISON_DIMENSION_COMBINATIONS_VIEW_BUILDER.build_and_print()
---
+++ recidiviz/calculator/query/state/views/dashboard/pathways/prison_to_supervision_count_by_month.py
@@ -0,0 +1,82 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2021 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""Prison to supervision count by month"""
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.calculator.query.state.dataset_config import DASHBOARD_VIEWS_DATASET
+from recidiviz.calculator.query.state.views.dashboard.pathways.pathways_prison_dimension_combinations import (
+    PATHWAYS_PRISON_DIMENSION_COMBINATIONS_VIEW_NAME,
+)
+from recidiviz.calculator.query.state.views.dashboard.pathways.transition_template import (
+    transition_monthly_aggregate_template,
+)
+from recidiviz.metrics.metric_big_query_view import MetricBigQueryViewBuilder
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+PRISON_TO_SUPERVISION_COUNT_BY_MONTH_NAME = "prison_to_supervision_count_by_month"
+
+PRISON_TO_SUPERVISION_COUNT_BY_MONTH_DESCRIPTION = (
+    """Prison to supervision count by month"""
+)
+
+aggregate_query = """
+    SELECT
+        transitions.state_code,
+        EXTRACT(YEAR FROM transition_date) as year,
+        EXTRACT(MONTH FROM transition_date) as month,
+        gender,
+        age_group,
+        facility,
+        COUNT(1) as event_count
+    FROM
+        `{project_id}.{reference_dataset}.prison_to_supervision_transitions` transitions,
+        UNNEST ([gender, 'ALL']) AS gender,
+        UNNEST ([age_group, 'ALL']) AS age_group
+    LEFT JOIN `{project_id}.{dashboard_views_dataset}.pathways_incarceration_location_name_map` location
+        ON transitions.state_code = location.state_code 
+        AND transitions.level_1_location_external_id = location.location_id,
+    UNNEST ([IFNULL(location_name, level_1_location_external_id), "ALL"]) AS facility
+    GROUP BY 1, 2, 3, 4, 5, 6
+"""
+
+dimensions = [
+    "gender",
+    "age_group",
+    "facility",
+]
+
+PRISON_TO_SUPERVISION_COUNT_BY_MONTH_QUERY_TEMPLATE = (
+    transition_monthly_aggregate_template(
+        aggregate_query,
+        dimensions,
+        PATHWAYS_PRISON_DIMENSION_COMBINATIONS_VIEW_NAME,
+    )
+)
+
+PRISON_TO_SUPERVISION_COUNT_BY_MONTH_VIEW_BUILDER = MetricBigQueryViewBuilder(
+    dataset_id=dataset_config.DASHBOARD_VIEWS_DATASET,
+    view_id=PRISON_TO_SUPERVISION_COUNT_BY_MONTH_NAME,
+    view_query_template=PRISON_TO_SUPERVISION_COUNT_BY_MONTH_QUERY_TEMPLATE,
+    description=PRISON_TO_SUPERVISION_COUNT_BY_MONTH_DESCRIPTION,
+    dimensions=("state_code", "year", "month", *dimensions),
+    dashboard_views_dataset=DASHBOARD_VIEWS_DATASET,
+    reference_dataset=dataset_config.REFERENCE_VIEWS_DATASET,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        PRISON_TO_SUPERVISION_COUNT_BY_MONTH_VIEW_BUILDER.build_and_print()
