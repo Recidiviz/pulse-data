f61886c587b2290db707f9360eddeda4f3495b2d
Daniela Schiano di Cola <daniela@recidiviz.org>
2022-02-02 17:23:48 -0800

    Add us_xx_validation to state-direct-ingest-resources and add oneoff import script (Recidiviz/recidiviz-data#10939)
    GitOrigin-RevId: 7c2c5bb76e98a9d636b2fa3113fb2ef4abda6b94

--- recidiviz/tools/deploy/terraform/modules/state-direct-ingest-resources/bigquery.tf
+++ recidiviz/tools/deploy/terraform/modules/state-direct-ingest-resources/bigquery.tf
@@ -21,3 +21,9 @@ module "state-specific-scratch-dataset" {
   dataset_id  =  "${lower(var.state_code)}_scratch"
   description = "State-specific scratch space dataset that can be used to save one-off queries related to ${upper(var.state_code)} data. May provide a temporary staging ground for some ingest external validation data."
 }
+
+module "state-specific-validation-dataset" {
+  source      = "../big_query_dataset"
+  dataset_id  = "${lower(var.state_code)}_validation"
+  description = "State-specific validation dataset for external data."
+}
