8b175f742f4ea88a50d9f131e52521e4c0b2648f
Dan Hansen <dan@recidiviz.org>
2022-02-08 16:00:56 -0800

    Fix pylint issues found during complete pylint run (Recidiviz/recidiviz-data#11042)
    GitOrigin-RevId: 1af527e23d42385a8c78debbb3ccd75d2ce9fc3f

--- recidiviz/tests/fixtures/google_auth_patcher.py
+++ recidiviz/tests/fixtures/google_auth_patcher.py
@@ -1,4 +1,22 @@
-from collections import Generator
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+""" Fixture for ensuring our tests don't reach out to GCP """
+
+from typing import Generator
 from unittest.mock import patch
 
 import pytest
@@ -12,14 +30,14 @@ def google_auth_patcher(
 ) -> Generator[None, None, None]:
     """For tests without the emulator, prevent them from trying to create google cloud clients."""
 
-    google_auth_patcher = None
+    patcher = None
 
     if (
         not pytestconfig.getoption("with_emulator", default=None)
         and "emulator" not in request.fixturenames
     ):
-        google_auth_patcher = patch("google.auth.default")
-        mock_google_auth = google_auth_patcher.start()
+        patcher = patch("google.auth.default")
+        mock_google_auth = patcher.start()
         mock_google_auth.side_effect = AssertionError(
             "Unit test may not instantiate a Google client. Please mock the appropriate client class inside this test "
             " (e.g. `patch('google.cloud.bigquery.Client')`)."
@@ -27,5 +45,5 @@ def google_auth_patcher(
 
     yield
 
-    if google_auth_patcher is not None:
-        google_auth_patcher.stop()
+    if patcher is not None:
+        patcher.stop()
