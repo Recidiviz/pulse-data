619e44128d052f7d7910c26aae94c53ad009ef5d
Daniela Schiano di Cola <daniela@recidiviz.org>
2022-02-17 09:58:08 -0800

    [US_ME][Validations] Transform US_ME facility name for validation view (Recidiviz/recidiviz-data#11195)
    * Add US_ME state specific query string to external comparison queries
    
    * Add missing state_code field
    
    * Add entire case statement into state specific str
    
    GitOrigin-RevId: 37fa91a5727b4bc48961079f558e7e6a2876e5b3

---
+++ recidiviz/validation/views/utils/state_specific_query_strings.py
@@ -0,0 +1,39 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""State-specific query strings for validation views.
+"""
+
+
+def state_specific_dataflow_facility_name_tranformation() -> str:
+    """
+    Facility name transformations for dataflow output when comparing against external validation data.
+
+    For US_ME: The dataflow results will list SOUTHERN MAINE WOMEN'S REENTRY CENTER as a facility name, which is mapped from
+    the value of the housing_unit in the ingest mapping CURRENT_STATUS_incarceration_periods.yaml. However, US_ME's
+    validation reports lists this facility as Maine Correctional Center because of how the facility was created in
+    their system."""
+    return """
+        IFNULL(
+            CASE state_code
+                WHEN 'US_PA' THEN UPPER(LEFT(facility, 3))
+                -- TODO(#11192): Remove state-specific facility mapping when we have housing_unit in external validation data
+                WHEN 'US_ME' 
+                    THEN IF(facility = "SOUTHERN MAINE WOMEN'S REENTRY CENTER", "MAINE CORRECTIONAL CENTER", facility)
+            ELSE facility END,
+            'EXTERNAL_UNKNOWN'
+        ) AS facility
+    """
