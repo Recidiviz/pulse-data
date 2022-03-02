5c03bd21247c7d9bb73d3bcac832132f10d06178
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2022-03-02 11:44:02 -0800

    [US_TN] Adjust admission/release reasons for WEEKEND_CONFINEMENT IPs (Recidiviz/recidiviz-data#11342)
    Updates admission / release reasons on WEEKEND_CONFINEMENT incarceration periods to TEMPORARY_CUSTODY and RELEASE_FROM_TEMPORARY_CUSTODY, per this doc.
    
    Closes Recidiviz/recidiviz-data#11315
    
    GitOrigin-RevId: 9b26911a162769c422f45ce4ac04188e9b154483

---
+++ recidiviz/persistence/database/migrations/state/versions/2022_02_28_1530_7c6df6895461_add_ip_weekend_confinement_adm_rel.py
@@ -0,0 +1,87 @@
+# pylint: skip-file
+"""add_ip_weekend_confinement_adm_rel
+
+Revision ID: 7c6df6895461
+Revises: f459d62ed8cb
+Create Date: 2022-02-28 15:30:29.579031
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+from recidiviz.utils.string import StrictStringFormatter
+
+revision = "7c6df6895461"
+down_revision = "f459d62ed8cb"
+branch_labels = None
+depends_on = None
+
+
+ADMISSION_REASON_UPGRADE_QUERY = """UPDATE {table_name} 
+SET
+  admission_reason = 'TEMPORARY_CUSTODY'
+WHERE state_code = 'US_TN' 
+  AND admission_reason_raw_text IN ('PRFA-WKEND','CCFA-WKEND');
+"""
+
+RELEASE_REASON_UPGRADE_QUERY = """UPDATE {table_name} 
+SET
+  release_reason = 'RELEASED_FROM_TEMPORARY_CUSTODY'
+WHERE state_code = 'US_TN' 
+  AND release_reason_raw_text IN ('FAPR-WKRET','FACC-WKRET');
+"""
+
+
+ADMISSION_REASON_DOWNGRADE_QUERY = """UPDATE {table_name} 
+SET
+  admission_reason = 'SANCTION_ADMISSION'
+WHERE state_code = 'US_TN' 
+  AND admission_reason_raw_text IN ('PRFA-WKEND','CCFA-WKEND');
+"""
+
+RELEASE_REASON_DOWNGRADE_QUERY = """UPDATE {table_name} 
+SET
+  release_reason = 'RELEASED_TO_SUPERVISION'
+WHERE state_code = 'US_TN' 
+  AND release_reason_raw_text IN ('FAPR-WKRET','FACC-WKRET');
+"""
+
+INCARCERATION_PERIOD_TABLE_NAME = "state_incarceration_period"
+INCARCERATION_PERIOD_HISTORY_TABLE_NAME = "state_incarceration_period"
+
+
+def upgrade() -> None:
+    with op.get_context().autocommit_block():
+        for table_name in [
+            INCARCERATION_PERIOD_TABLE_NAME,
+            INCARCERATION_PERIOD_HISTORY_TABLE_NAME,
+        ]:
+            for update_query in [
+                ADMISSION_REASON_UPGRADE_QUERY,
+                RELEASE_REASON_UPGRADE_QUERY,
+            ]:
+                op.execute(
+                    StrictStringFormatter().format(
+                        update_query,
+                        table_name=table_name,
+                    )
+                )
+
+
+def downgrade() -> None:
+    with op.get_context().autocommit_block():
+        for table_name in [
+            INCARCERATION_PERIOD_TABLE_NAME,
+            INCARCERATION_PERIOD_HISTORY_TABLE_NAME,
+        ]:
+            for update_query in [
+                ADMISSION_REASON_DOWNGRADE_QUERY,
+                RELEASE_REASON_DOWNGRADE_QUERY,
+            ]:
+                op.execute(
+                    StrictStringFormatter().format(
+                        update_query,
+                        table_name=table_name,
+                    )
+                )
