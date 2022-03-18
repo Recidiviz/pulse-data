9f233ac00f70cc58fefdca6afcc74e5cd44aa1ea
Mahmoud O <59492998+mxosman@users.noreply.github.com>
2022-03-18 17:29:07 -0500

    [Justice Counts] Setting up justice-counts frontend subdirectories and skeleton app (Recidiviz/recidiviz-data#11622)
    * setting up justice-counts frontend subdirectories and skeleton app
    
    * Fixing configuration details in package.json and tsconfig.json
    
    * Set up simple test, fixed minor linting errors, added test and linting instructions to README
    
    Co-authored-by: Mahmoud <mahmoud@Mahmouds-MacBook-Pro.local>
    GitOrigin-RevId: 312c8f3b3cf636e6668f6b2373193a2b8527b953

---
+++ frontends/justice-counts/control-panel/src/index.tsx
@@ -0,0 +1,33 @@
+import { fonts, palette } from "@recidiviz/design-system";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createGlobalStyle } from "styled-components/macro";
+
+import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    width: 100%;
+    font-family: ${fonts.sans};
+    background-color: ${palette.marble2};
+    color: ${palette.text.normal};
+  }
+`;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
