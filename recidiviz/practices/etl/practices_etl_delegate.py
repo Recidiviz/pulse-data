3024db54d6b1d05788cb8a333db3fcd0894746f7
Anthony Kesich <anthony@recidiviz.org>
2022-03-25 11:53:37 -0500

    [Practices] Create ETL delegates (Recidiviz/recidiviz-data#11753)
    * [Practices] Create ETL delegates
    
    GitOrigin-RevId: d9c62165c521348f2644858ea58719b3febffb48

---
+++ recidiviz/practices/etl/practices_etl_delegate.py
@@ -0,0 +1,120 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2022 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""Abstract classes defining delegate interfaces for ETLing data into the
+practices frontend database(s)."""
+import abc
+import logging
+import os
+from typing import Iterator, TextIO, Tuple
+
+from recidiviz.cloud_storage.gcsfs_factory import GcsfsFactory
+from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
+from recidiviz.firestore.firestore_client import FirestoreClientImpl
+from recidiviz.metrics.export.export_config import PRACTICES_VIEWS_OUTPUT_DIRECTORY_URI
+from recidiviz.utils import metadata
+from recidiviz.utils.string import StrictStringFormatter
+
+# Firestore client caps us at 500 records per batch
+MAX_FIRESTORE_RECORDS_PER_BATCH = 499
+
+
+class PracticesETLDelegate(abc.ABC):
+    """Abstract class containing the ETL logic for a specific exported practices view."""
+
+    @property
+    @abc.abstractmethod
+    def EXPORT_FILENAME(self) -> str:
+        """Name of the file this delegate is watching for."""
+
+    @abc.abstractmethod
+    def run_etl(self) -> None:
+        """Runs the ETL logic for the view."""
+
+    def filename_matches(self, filename: str) -> bool:
+        """Checks if the given filename matches the expected filename for this delegate."""
+        return filename == self.EXPORT_FILENAME
+
+    def get_filepath(self) -> GcsfsFilePath:
+        return GcsfsFilePath.from_absolute_path(
+            os.path.join(
+                StrictStringFormatter().format(
+                    PRACTICES_VIEWS_OUTPUT_DIRECTORY_URI,
+                    project_id=metadata.project_id(),
+                ),
+                self.EXPORT_FILENAME,
+            )
+        )
+
+    def get_file_stream(self) -> Iterator[TextIO]:
+        """Returns a stream of the contents of the file this delegate is watching for."""
+        client = GcsfsFactory.build()
+
+        filepath = self.get_filepath()
+
+        if not client.is_file(filepath.abs_path()):
+            raise FileNotFoundError(
+                f"File {filepath.abs_path()} does not exist in GCS."
+            )
+
+        with client.open(filepath) as f:
+            yield f
+
+
+class PracticesFirestoreETLDelegate(PracticesETLDelegate):
+    """Abstract class containing the ETL logic for exporting a specified practices view to Firestore."""
+
+    @property
+    @abc.abstractmethod
+    def COLLECTION_NAME(self) -> str:
+        """Name of the Firestore collection this delegate will ETL into."""
+
+    @abc.abstractmethod
+    def transform_row(self, row: str) -> Tuple[str, dict]:
+        """Prepares a row of the exported file for Firestore ingestion.
+        Returns a tuple of the document ID and the document contents."""
+
+    def run_etl(self) -> None:
+        logging.info(
+            'Starting export of gs://%s to the Firestore collection "%s".',
+            self.get_filepath().abs_path(),
+            self.COLLECTION_NAME,
+        )
+        firestore_client = FirestoreClientImpl()
+        firestore_client.delete_collection(self.COLLECTION_NAME)
+        batch = firestore_client.batch()
+        firestore_collection = firestore_client.get_collection(self.COLLECTION_NAME)
+        num_records_to_write = 0
+        total_records_written = 0
+
+        for file_stream in self.get_file_stream():
+            while line := file_stream.readline():
+                row_id, new_document = self.transform_row(line)
+                batch.set(firestore_collection.document(row_id), new_document)
+                num_records_to_write += 1
+                total_records_written += 1
+
+                if num_records_to_write >= MAX_FIRESTORE_RECORDS_PER_BATCH:
+                    batch.commit()
+                    batch = firestore_client.batch()
+                    num_records_to_write = 0
+
+        batch.commit()
+        logging.info(
+            '%d records written to Firestore collection "%s".',
+            total_records_written,
+            self.COLLECTION_NAME,
+        )
---
+++ recidiviz/tests/practices/etl/practices_firestore_etl_delegate_test.py
@@ -0,0 +1,95 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2022 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""Tests for the Firestore ETL Delegate."""
+from typing import Optional, Tuple
+from unittest import TestCase
+from unittest.mock import patch
+
+import mock.mock
+
+from recidiviz.practices.etl.practices_etl_delegate import (
+    MAX_FIRESTORE_RECORDS_PER_BATCH,
+    PracticesFirestoreETLDelegate,
+)
+from recidiviz.utils.metadata import local_project_id_override
+
+
+class TestETLDelegate(PracticesFirestoreETLDelegate):
+    EXPORT_FILENAME = "test_export.json"
+    COLLECTION_NAME = "test_collection"
+
+    def transform_row(self, row: str) -> Tuple[str, dict]:
+        return row, {"data": row}
+
+
+class FakeBatchWriter(TestCase):
+    def __init__(self) -> None:
+        super().__init__()
+        self.doc_count = 0
+
+    def set(self, doc_id: str, doc: dict) -> None:  # pylint: disable=unused-argument
+        self.doc_count += 1
+        self.assertGreaterEqual(MAX_FIRESTORE_RECORDS_PER_BATCH, self.doc_count)
+
+    def commit(self) -> None:
+        self.assertGreaterEqual(MAX_FIRESTORE_RECORDS_PER_BATCH, self.doc_count)
+        self.doc_count = 0
+
+
+class FakeFileStream:
+    def __init__(self, total_values: int) -> None:
+        self.total_values = total_values
+        self.values_written = 0
+
+    def readline(self) -> Optional[str]:
+        if self.values_written >= self.total_values:
+            return None
+
+        self.values_written += 1
+        return str(self.values_written)
+
+
+class PracticesFirestoreEtlDelegateTest(TestCase):
+    """Tests for the Firestore ETL Delegate."""
+
+    @patch("google.cloud.firestore.Client")
+    @patch("recidiviz.firestore.firestore_client.FirestoreClientImpl.get_collection")
+    @patch("recidiviz.firestore.firestore_client.FirestoreClientImpl.batch")
+    @patch("recidiviz.firestore.firestore_client.FirestoreClientImpl.delete_collection")
+    @patch(
+        "recidiviz.practices.etl.practices_etl_delegate.PracticesETLDelegate.get_file_stream"
+    )
+    def test_run_etl_respects_batching(
+        self,
+        mock_get_file_stream: mock.MagicMock,
+        mock_delete_collection: mock.MagicMock,
+        mock_batch_writer: mock.MagicMock,
+        mock_get_collection: mock.MagicMock,
+        mock_firestore_client: mock.MagicMock,  # pylint: disable=unused-argument
+    ) -> None:
+        """Tests that the ETL Delegate respects the max batch size for writing to Firestore."""
+        # mock_firestore_client.batch.return_value = FakeBatchWriter()
+        mock_batch_writer.return_value = FakeBatchWriter()
+        mock_get_file_stream.return_value = [FakeFileStream(3000)]
+        with local_project_id_override("test-project"):
+            delegate = TestETLDelegate()
+            delegate.run_etl()
+
+        # mock_firestore_client.delete_collection.assert_called_once_with("test_collection")
+        # mock_firestore_client.get_collection.assert_called_once_with("test_collection")
+        mock_delete_collection.assert_called_once_with("test_collection")
+        mock_get_collection.assert_called_once_with("test_collection")
