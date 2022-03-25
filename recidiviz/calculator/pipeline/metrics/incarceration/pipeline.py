b437346e74c99f41ae88c4b1b778e4dd450e1109
Julia Dressel <julia@recidiviz.org>
2022-03-25 14:28:08 -0700

    [Bug Fix] Fixing missing US_MO status table (Recidiviz/recidiviz-data#11792)
    I prematurely dropped the US_MO sentence status table from the incarceration and supervision pipelines. This will fix the deployed pipelines. Follow-up PR (hopefully) to stop bringing sentences into these pipelines, and to remove their dependency on the US_MO sentence status table.
    
    GitOrigin-RevId: 6f83ec8ce04d00803cca4de91f705ee74c357c98

--- recidiviz/calculator/pipeline/metrics/incarceration/pipeline.py
+++ recidiviz/calculator/pipeline/metrics/incarceration/pipeline.py
@@ -57,6 +57,10 @@ from recidiviz.calculator.query.state.views.reference.persons_to_recent_county_o
 from recidiviz.calculator.query.state.views.reference.supervision_period_to_agent_association import (
     SUPERVISION_PERIOD_TO_AGENT_ASSOCIATION_VIEW_NAME,
 )
+from recidiviz.calculator.query.state.views.reference.us_mo_sentence_statuses import (
+    US_MO_SENTENCE_STATUSES_VIEW_NAME,
+)
+from recidiviz.common.constants.states import StateCode
 from recidiviz.persistence.entity.state import entities
 
 
@@ -95,7 +99,11 @@ class IncarcerationMetricsPipelineRunDelegate(MetricPipelineRunDelegate):
                 StateSpecificSupervisionDelegate,
                 StateSpecificViolationDelegate,
             ],
-            state_specific_required_reference_tables={},
+            state_specific_required_reference_tables={
+                # We need to bring in the US_MO sentence status table to load
+                # state-specific versions of sentences
+                StateCode.US_MO: [US_MO_SENTENCE_STATUSES_VIEW_NAME]
+            },
         )
 
     @classmethod
