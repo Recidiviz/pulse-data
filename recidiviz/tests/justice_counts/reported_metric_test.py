b226b470e35c62cc44f89c8f2ea9c009e86992d8
lilidworkin <99899591+lilidworkin@users.noreply.github.com>
2022-04-06 14:24:18 -0400

    [Justice Counts][1/n] Adding `ReportedMetric` class to represent metric values reported by agencies (Recidiviz/recidiviz-data#11844)
    The main contribution of this PR is to create a new ReportedMetric class, which represents an agency's reported values for an official Tier 1 Justice Counts metric. There will be a many:1 mapping between instances of this class and instances of MetricDefinition, since agencies will report the same metric over different time periods on a recurring basis.
    
    GitOrigin-RevId: 04737220a5d7915dc1551f1fc03e2dc71eba9f95

---
+++ recidiviz/tests/justice_counts/reported_metric_test.py
@@ -0,0 +1,106 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""This class implements tests for Justice Counts ReportedMetric class."""
+
+from unittest import TestCase
+
+from recidiviz.justice_counts.dimensions.law_enforcement import SheriffBudgetType
+from recidiviz.justice_counts.dimensions.person import Gender
+from recidiviz.justice_counts.metrics import law_enforcement
+from recidiviz.justice_counts.metrics.constants import ContextKey
+from recidiviz.justice_counts.metrics.reported_metric import (
+    ReportedAggregatedDimension,
+    ReportedContext,
+    ReportedMetric,
+)
+
+
+class TestJusticeCountsMetricDefinition(TestCase):
+    """Implements tests for the Justice Counts ReportedMetric class."""
+
+    def setUp(self) -> None:
+        self.reported_budget = ReportedMetric(
+            key=law_enforcement.annual_budget.key,
+            value=100000,
+            contexts=[
+                ReportedContext(
+                    key=ContextKey.PRIMARY_FUNDING_SOURCE, value="government"
+                )
+            ],
+            aggregated_dimensions=[
+                ReportedAggregatedDimension(
+                    dimension_to_value={
+                        SheriffBudgetType.DETENTION: 50000,
+                        SheriffBudgetType.PATROL: 50000,
+                    }
+                )
+            ],
+        )
+
+    def test_init(self) -> None:
+        self.assertEqual(
+            self.reported_budget.metric_definition.display_name, "Annual Budget"
+        )
+
+        with self.assertRaisesRegex(
+            ValueError, "Not all dimension instances belong to the same class"
+        ):
+            ReportedMetric(
+                key=law_enforcement.annual_budget.key,
+                value=100000,
+                aggregated_dimensions=[
+                    ReportedAggregatedDimension(
+                        dimension_to_value={
+                            SheriffBudgetType.DETENTION: 50000,
+                            Gender.get("FEMALE"): 100,
+                        }
+                    )
+                ],
+            )
+
+        with self.assertRaisesRegex(
+            ValueError,
+            "Not all members of the dimension enum have a reported value",
+        ):
+            ReportedMetric(
+                key=law_enforcement.annual_budget.key,
+                value=100000,
+                aggregated_dimensions=[
+                    ReportedAggregatedDimension(
+                        dimension_to_value={
+                            SheriffBudgetType.DETENTION: 50000,
+                        }
+                    )
+                ],
+            )
+
+        with self.assertRaisesRegex(
+            ValueError,
+            "Sums across dimension metric/law_enforcement/budget/type do not equal the total",
+        ):
+            ReportedMetric(
+                key=law_enforcement.annual_budget.key,
+                value=100000,
+                aggregated_dimensions=[
+                    ReportedAggregatedDimension(
+                        dimension_to_value={
+                            SheriffBudgetType.DETENTION: 50000,
+                            SheriffBudgetType.PATROL: 10,
+                        }
+                    )
+                ],
+            )
