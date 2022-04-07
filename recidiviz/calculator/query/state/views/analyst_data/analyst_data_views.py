aee5593cdb38e6eba7057f9c0b2f8dd648f8a563
Damini Sharma <damini@recidiviz.org>
2022-04-07 09:06:08 -0700

    add sentencing logic view separately to avoid complex query errors and bug fix on aligning columns for union all (Recidiviz/recidiviz-data#12048)
    GitOrigin-RevId: b360d82a28512d2faf11597c613cae065bfc330f

--- recidiviz/calculator/query/state/views/analyst_data/analyst_data_views.py
+++ recidiviz/calculator/query/state/views/analyst_data/analyst_data_views.py
@@ -105,6 +105,9 @@ from recidiviz.calculator.query.state.views.analyst_data.us_tn.us_tn_compliant_r
 from recidiviz.calculator.query.state.views.analyst_data.us_tn.us_tn_overdue_for_discharge import (
     US_TN_OVERDUE_FOR_DISCHARGE_VIEW_BUILDER,
 )
+from recidiviz.calculator.query.state.views.analyst_data.us_tn.us_tn_sentencing_logic import (
+    US_TN_SENTENCE_LOGIC_VIEW_BUILDER,
+)
 from recidiviz.calculator.query.state.views.analyst_data.us_tn.us_tn_supervision_geographies import (
     US_TN_SUPERVISION_GEOGRAPHIES_VIEW_BUILDER,
 )
@@ -143,4 +146,5 @@ ANALYST_DATA_VIEW_BUILDERS: List[SimpleBigQueryViewBuilder] = [
     US_TN_COMPLIANT_REPORTING_REFERRAL_VIEW_BUILDER,
     US_TN_SUPERVISION_PLAN_LOGIC_VIEW_BUILDER,
     US_TN_SUPERVISION_GEOGRAPHIES_VIEW_BUILDER,
+    US_TN_SENTENCE_LOGIC_VIEW_BUILDER,
 ] + SUPERVISION_OFFICER_OFFICE_AND_DISTRICT_VIEW_BUILDERS
