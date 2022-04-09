7d6a50aa8c543adf7be2ce22f2e2c0a914697a00
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2022-04-08 18:59:25 -0700

    [Postgres deprecation] Remove connection and database resources for v1 (Recidiviz/recidiviz-data#12031)
    Removes BQ connections and databases from the deprecated v1 postgres instances. Out of an abundance of caution, will land this in a prod release AFTER Recidiviz/recidiviz-data#12030 (aka will merge this PR on Friday after the staging release goes out).
    
    Part of Recidiviz/recidiviz-data#11326
    
    GitOrigin-RevId: 4160896ec47e660514a41cdfd3c9a8349bd11585

--- recidiviz/tools/deploy/terraform/modules/state-direct-ingest-resources/databases.tf
+++ recidiviz/tools/deploy/terraform/modules/state-direct-ingest-resources/databases.tf
@@ -15,54 +15,6 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 
-resource "google_sql_database" "state_primary" {
-  name     = "${lower(var.state_code)}_primary"
-  instance = var.cloudsql_instance_name
-}
-
-resource "google_sql_database" "state_secondary" {
-  name     = "${lower(var.state_code)}_secondary"
-  instance = var.cloudsql_instance_name
-}
-
-resource "google_bigquery_connection" "state_primary_bq_connection" {
-  provider = google-beta
-
-  connection_id = "state_${lower(var.state_code)}_primary_cloudsql"
-  friendly_name = "${var.state_code} State Cloud SQL Postgres (Primary)"
-  location      = var.cloudsql_instance_region
-  description   = "Connection to the ${var.state_code} State Cloud SQL database (primary)"
-
-  cloud_sql {
-    instance_id = var.cloudsql_instance_id
-    database    = google_sql_database.state_primary.name
-    type        = "POSTGRES"
-    credential {
-      username = var.cloudsql_instance_user_name
-      password = var.cloudsql_instance_user_password
-    }
-  }
-}
-
-resource "google_bigquery_connection" "state_secondary_bq_connection" {
-  provider = google-beta
-
-  connection_id = "state_${lower(var.state_code)}_secondary_cloudsql"
-  friendly_name = "${var.state_code} State Cloud SQL Postgres (Secondary)"
-  location      = var.cloudsql_instance_region
-  description   = "Connection to the ${var.state_code} State Cloud SQL database (secondary)"
-
-  cloud_sql {
-    instance_id = var.cloudsql_instance_id
-    database    = google_sql_database.state_secondary.name
-    type        = "POSTGRES"
-    credential {
-      username = var.cloudsql_instance_user_name
-      password = var.cloudsql_instance_user_password
-    }
-  }
-}
-
 # Postgres Version 13 Upgrade Databases
 resource "google_sql_database" "state_v2_primary" {
   name     = "${lower(var.state_code)}_primary"
