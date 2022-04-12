9c26287ae655c506e512d70fda1fb9362bb6f7cd
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2022-04-11 17:21:31 -0700

    [Auth] Delete unused /auth/update_auth0_user_metadata endpoint (Recidiviz/recidiviz-data#12075)
    The work done by the auth/update_auth0_user_metadata endpoint is no longer something we need to do because we request user metadata on every login. Deleting the endpoint and code that triggers the endpoint since this endpoint crashes semi-regularly in prod.
    
    GitOrigin-RevId: 3385fae573f38f58ba3ddb0ece07ac22c5cf4c97

--- recidiviz/auth/auth_endpoint.py
+++ recidiviz/auth/auth_endpoint.py
@@ -20,13 +20,13 @@
 import logging
 import os
 from http import HTTPStatus
-from typing import Any, Dict, List, Mapping, Optional, Tuple, Union
+from typing import Any, Dict, List, Mapping, Tuple, Union
 
 import sqlalchemy.orm.exc
 from flask import Blueprint, request
 from sqlalchemy import func
 
-from recidiviz.auth.auth0_client import Auth0AppMetadata, Auth0Client
+from recidiviz.auth.auth0_client import Auth0AppMetadata
 from recidiviz.calculator.query.state.views.reference.dashboard_user_restrictions import (
     DASHBOARD_USER_RESTRICTIONS_VIEW_BUILDER,
 )
@@ -215,105 +215,6 @@ def dashboard_user_restrictions_by_email() -> Tuple[
 
     finally:
         session.close()
-
-
-@auth_endpoint_blueprint.route("/update_auth0_user_metadata", methods=["GET"])
-@requires_gae_auth
-def update_auth0_user_metadata() -> Tuple[str, HTTPStatus]:
-    """This endpoint is triggered from a GCS bucket when a new user restrictions file is created. It downloads the
-     user restrictions file and updates each user's app_metadata with the restrictions from the file.
-
-    This function first queries the Auth0 Management API to request all users and their user_ids by the
-    list of email addresses that exist in the downloaded file. Then it iterates over those users and updates their
-    app_metadata.
-
-    Query parameters:
-        region_code: (required) The region code that has the updated user restrictions file, e.g. US_MO
-
-    Returns:
-         Text indicating the results of the run and an HTTP status
-
-    Raises:
-        Nothing. Catch everything so that we can always return a response to the request
-    """
-    region_code = get_only_str_param_value(
-        "region_code", request.args, preserve_case=True
-    )
-
-    if not region_code:
-        return (
-            "Missing required region_code param",
-            HTTPStatus.BAD_REQUEST,
-        )
-
-    try:
-        _validate_region_code(region_code)
-    except ValueError as error:
-        logging.error(error)
-        return str(error), HTTPStatus.BAD_REQUEST
-
-    database_key = SQLAlchemyDatabaseKey.for_schema(schema_type=SchemaType.CASE_TRIAGE)
-    with SessionFactory.using_database(
-        database_key=database_key, autocommit=False
-    ) as session:
-        try:
-            all_user_restrictions = (
-                session.query(
-                    DashboardUserRestrictions.restricted_user_email,
-                    DashboardUserRestrictions.allowed_supervision_location_ids,
-                    DashboardUserRestrictions.allowed_supervision_location_level,
-                    DashboardUserRestrictions.can_access_leadership_dashboard,
-                    DashboardUserRestrictions.can_access_case_triage,
-                    DashboardUserRestrictions.should_see_beta_charts,
-                    DashboardUserRestrictions.routes,
-                    DashboardUserRestrictions.user_hash,
-                )
-                .filter(DashboardUserRestrictions.state_code == region_code.upper())
-                .order_by(DashboardUserRestrictions.restricted_user_email)
-                .all()
-            )
-            user_restrictions_by_email: Dict[str, Auth0AppMetadata] = {}
-            for user_restrictions in all_user_restrictions:
-                email = user_restrictions["restricted_user_email"].lower()
-                user_restrictions_by_email[email] = _format_db_results(
-                    user_restrictions
-                )
-
-            auth0 = Auth0Client()
-            email_addresses = list(user_restrictions_by_email.keys())
-            users = auth0.get_all_users_by_email_addresses(email_addresses)
-            num_updated_users = 0
-
-            for user in users:
-                email = user.get("email", "")
-                current_app_metadata = user.get("app_metadata", {})
-                new_restrictions: Optional[
-                    Auth0AppMetadata
-                ] = user_restrictions_by_email.get(email)
-                current_restrictions = _normalize_current_restrictions(
-                    current_app_metadata
-                )
-
-                if (
-                    new_restrictions is not None
-                    and current_restrictions != new_restrictions
-                ):
-                    num_updated_users += 1
-                    auth0.update_user_app_metadata(
-                        user_id=user.get("user_id", ""), app_metadata=new_restrictions
-                    )
-
-            return (
-                f"Finished updating {num_updated_users} auth0 users with restrictions for region {region_code}",
-                HTTPStatus.OK,
-            )
-
-        except Exception as error:
-            logging.error(error)
-            return (
-                f"Error using Auth0 management API to update users: {error}",
-                HTTPStatus.INTERNAL_SERVER_ERROR,
-            )
 
 
 def _format_db_results(
