5d4af9a21c9c987000e308da3b7c8850134f9f28
Nichelle <nichelle.a.hall@gmail.com>
2022-04-14 16:26:35 -0700

    [Justice Counts] [1/n] Add create_report method to ReportInterface (Recidiviz/recidiviz-data#12183)
    * Justice Counts /api/reports endpoint
    - adds new /api/reports endpoint that returns reports associated with an agency_id on GET
    - updates server_test.py to test endpoint
    - adds new ReportInterface
    - adds tests for ReportInterface
    
    * Address Lili's comment
    
    * Add create_report method to ReportInterface
    
    * addressing PR feedback
    
    * [Justice Counts] Change /api/reports [POST] endpoint to recieve a month and year versus date strings
    
    - updates create_report to get month and year instead of datetime strings
    - updates create_report tests
    
    GitOrigin-RevId: edf9cb5a6b712162a65b1a55ed248176a5c68f83

--- recidiviz/tests/justice_counts/report_test.py
+++ recidiviz/tests/justice_counts/report_test.py
@@ -17,7 +17,14 @@
 """This class implements tests for the Justice Counts ReportInterface."""
 
 
+import datetime
+
+from recidiviz.justice_counts.agency import AgencyInterface
 from recidiviz.justice_counts.report import ReportInterface
+from recidiviz.justice_counts.user_account import UserAccountInterface
+from recidiviz.persistence.database.schema.justice_counts.schema import (
+    ReportingFrequency,
+)
 from recidiviz.persistence.database.session_factory import SessionFactory
 from recidiviz.tests.justice_counts.utils import (
     JusticeCountsDatabaseTestCase,
@@ -38,25 +45,75 @@ class TestReportInterface(JusticeCountsDatabaseTestCase):
             annual_report = self.test_schema_objects.test_report_annual
             session.add_all(
                 [
-                    self.test_schema_objects.test_user_A,
-                    self.test_schema_objects.test_user_B,
                     monthly_report,
                     annual_report,
                 ]
             )
 
         with SessionFactory.using_database(self.database_key) as session:
+            user_A = UserAccountInterface.get_user_by_auth0_user_id(
+                session=session, auth0_user_id="auth0_id_A"
+            )
+            agency_A = AgencyInterface.get_agency_by_name(
+                session=session, name="Agency Alpha"
+            )
             reports_agency_A = ReportInterface.get_reports_by_agency_id(
                 session=session,
-                agency_id=1,
-                user_account_id=1,
+                agency_id=agency_A.id,
+                user_account_id=user_A.id,
             )
-            self.assertEqual(len(reports_agency_A), 1)
-            self.assertEqual(reports_agency_A[0].id, 1)
+            self.assertEqual(reports_agency_A[0].source_id, agency_A.id)
+            user_B = UserAccountInterface.get_user_by_auth0_user_id(
+                session=session, auth0_user_id="auth0_id_B"
+            )
+            agency_B = AgencyInterface.get_agency_by_name(
+                session=session, name="Agency Beta"
+            )
             reports_agency_B = ReportInterface.get_reports_by_agency_id(
                 session=session,
-                agency_id=2,
-                user_account_id=2,
+                agency_id=agency_B.id,
+                user_account_id=user_B.id,
             )
-            self.assertEqual(len(reports_agency_B), 1)
-            self.assertEqual(reports_agency_B[0].id, 2)
+            self.assertEqual(reports_agency_B[0].source_id, agency_B.id)
+
+    def test_create_report(self) -> None:
+        with SessionFactory.using_database(self.database_key) as session:
+            session.add(
+                self.test_schema_objects.test_agency_A,
+                self.test_schema_objects.test_user_A,
+            )
+
+        with SessionFactory.using_database(self.database_key) as session:
+            agency_id = 1
+            new_monthly_report = ReportInterface.create_report(
+                session=session,
+                agency_id=agency_id,
+                user_account_id=1,
+                month=2,
+                year=2022,
+                frequency=ReportingFrequency.MONTHLY.value,
+            )
+            self.assertEqual(new_monthly_report.source_id, agency_id)
+            self.assertEqual(new_monthly_report.type, ReportingFrequency.MONTHLY.value)
+            self.assertEqual(
+                new_monthly_report.date_range_start, datetime.date(2022, 2, 1)
+            )
+            self.assertEqual(
+                new_monthly_report.date_range_end, datetime.date(2022, 3, 1)
+            )
+            new_annual_report = ReportInterface.create_report(
+                session=session,
+                agency_id=agency_id,
+                user_account_id=1,
+                year=2022,
+                month=3,
+                frequency=ReportingFrequency.ANNUAL.value,
+            )
+            self.assertEqual(new_annual_report.source_id, agency_id)
+            self.assertEqual(new_annual_report.type, ReportingFrequency.ANNUAL.value)
+            self.assertEqual(
+                new_annual_report.date_range_start, datetime.date(2022, 3, 1)
+            )
+            self.assertEqual(
+                new_annual_report.date_range_end, datetime.date(2023, 3, 1)
+            )
