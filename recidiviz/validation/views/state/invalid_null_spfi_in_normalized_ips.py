f293c05e37e24c51be33c22389b4616f059cacef
Julia Dressel <julia@recidiviz.org>
2022-04-15 14:08:21 -0700

    [Normalization/Validations] Updating normalized entity validations (Recidiviz/recidiviz-data#12153)
    Updates a bunch of validations that alert to problems in IP normalization logic to pull from the normalized_state dataset instead of the metrics. Also adds the normalized_state dataset to the two "freshness" validations.
    
    Confirmed that validation output was the same. Had to update a few validations to handle TRANSFER admission/release reasons, since these are excluded from metrics but are present in normalized IPs.
    
    GitOrigin-RevId: 5ce60e0f1021d4acb95778ce690e8dc1f2af2da7

--- recidiviz/validation/views/state/invalid_null_spfi_in_normalized_ips.py
+++ recidiviz/validation/views/state/invalid_null_spfi_in_normalized_ips.py
@@ -1,5 +1,5 @@
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2021 Recidiviz, Inc.
+# Copyright (C) 2022 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,9 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""A view revealing when normalized incarceration periods have an admission_reason
-value of ADMITTED_FROM_SUPERVISION. This is an ingest-only enum and should not be
-observed after normalization.
+"""A view revealing when normalized incarceration periods have a null
+specialized_purpose_for_incarceration value, which should never happen.
 
 Existence of any rows indicates a bug in IP normalization logic.
 """
@@ -25,9 +24,6 @@ from recidiviz.calculator.pipeline.normalization.utils.normalized_entities impor
     NormalizedStateIncarcerationPeriod,
 )
 from recidiviz.calculator.query.state import dataset_config as state_dataset_config
-from recidiviz.common.constants.state.state_incarceration_period import (
-    StateIncarcerationPeriodAdmissionReason,
-)
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 from recidiviz.validation.views import dataset_config
@@ -35,31 +31,26 @@ from recidiviz.validation.views.utils.normalized_entities_validation_utils impor
     validation_query_for_normalized_entity,
 )
 
-INVALID_ADMITTED_FROM_SUPERVISION_ADMISSION_REASON_VIEW_NAME = (
-    "invalid_admitted_from_supervision_admission_reason"
+INVALID_NULL_SPFI_NORMALIZED_IPS_VIEW_NAME = "invalid_null_spfi_normalized_ips"
+
+INVALID_NULL_SPFI_NORMALIZED_IPS_DESCRIPTION = (
+    """Metrics with invalid null values for specialized_purpose_for_incarceration."""
 )
 
-INVALID_ADMITTED_FROM_SUPERVISION_ADMISSION_REASON_DESCRIPTION = """Normalized
-incarceration periods with invalid, ingest-only admission_reason values of
-ADMITTED_FROM_SUPERVISION."""
 
-INVALID_ROWS_FILTER_CLAUSE = f"""WHERE admission_reason =
-'{StateIncarcerationPeriodAdmissionReason.ADMITTED_FROM_SUPERVISION.value}'"""
-
-
-INVALID_ADMITTED_FROM_SUPERVISION_ADMISSION_REASON_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+INVALID_NULL_SPFI_NORMALIZED_IPS_VIEW_BUILDER = SimpleBigQueryViewBuilder(
     dataset_id=dataset_config.VIEWS_DATASET,
-    view_id=INVALID_ADMITTED_FROM_SUPERVISION_ADMISSION_REASON_VIEW_NAME,
+    view_id=INVALID_NULL_SPFI_NORMALIZED_IPS_VIEW_NAME,
     view_query_template=validation_query_for_normalized_entity(
         normalized_entity_class=NormalizedStateIncarcerationPeriod,
-        additional_columns_to_select=["person_id", "admission_date"],
-        invalid_rows_filter_clause=INVALID_ROWS_FILTER_CLAUSE,
-        validation_description=INVALID_ADMITTED_FROM_SUPERVISION_ADMISSION_REASON_DESCRIPTION,
+        additional_columns_to_select=["person_id"],
+        invalid_rows_filter_clause="WHERE specialized_purpose_for_incarceration IS NULL",
+        validation_description=INVALID_NULL_SPFI_NORMALIZED_IPS_DESCRIPTION,
     ),
-    description=INVALID_ADMITTED_FROM_SUPERVISION_ADMISSION_REASON_DESCRIPTION,
+    description=INVALID_NULL_SPFI_NORMALIZED_IPS_DESCRIPTION,
     normalized_state_dataset=state_dataset_config.NORMALIZED_STATE_DATASET,
 )
 
 if __name__ == "__main__":
     with local_project_id_override(GCP_PROJECT_STAGING):
-        INVALID_ADMITTED_FROM_SUPERVISION_ADMISSION_REASON_VIEW_BUILDER.build_and_print()
+        INVALID_NULL_SPFI_NORMALIZED_IPS_VIEW_BUILDER.build_and_print()
