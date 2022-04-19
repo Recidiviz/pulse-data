a11e22beb953623038a228c8d3e3ecd23aff1e48
Jen Overgaag <jovergaag@gmail.com>
2022-04-18 17:14:42 -0700

    [Pathways][US_ME] Update state specific district filter (Recidiviz/recidiviz-data#12327)
    [Pathways][US_ME] Update state specific district filter
    
    GitOrigin-RevId: bef6e37aee4f0d8b7b656e8f1ddbd93bc8ee7f46

--- recidiviz/calculator/query/state/state_specific_query_strings.py
+++ recidiviz/calculator/query/state/state_specific_query_strings.py
@@ -479,14 +479,13 @@ def pathways_state_specific_supervision_district_filter() -> str:
         CASE
             -- TODO(#12239): Remove this filtering once we figure out why there are DOC facilities in supervision periods
             WHEN state_code = "US_ME" THEN
-                district NOT IN (
+                UPPER(district) NOT IN (
                     -- Filter out unknown locations, using both the location mapping name and the external id
-                    "Non-Committed Adult",
                     "NON-COMMITTED ADULT",
                     "TEMP SOCIETY OUT ADULT",
-                    "Temp Society Out",
+                    "TEMP SOCIETY OUT",
                     -- Filter out DOC Facilities
-                    "MAINE CORRECTIONAL FACILITY",
+                    "MAINE CORRECTIONAL CENTER",
                     "MOUNTAIN VIEW CORRECTIONAL FACILITY",
                     "SOUTHERN MAINE WOMEN'S REENTRY CENTER",
                     "BOLDUC CORRECTIONAL FACILITY",
