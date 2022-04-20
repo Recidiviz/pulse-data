ae0556e36a1e278187f1a1cef17524e62d24bc15
Chinedu Ozodi <chinedu@recidiviz.org>
2022-04-20 12:07:17 -0700

    [Bug Fix] fix deploy migrations script (Recidiviz/recidiviz-data#12392)
    * fix migrations script
    
    * removed misc white space
    
    * add correct tab space
    
    GitOrigin-RevId: afaef6bbab98a84c9d3cdf489f29c314f740a69f

--- recidiviz/tools/migrations/run_all_migrations.sh
+++ recidiviz/tools/migrations/run_all_migrations.sh
@@ -8,8 +8,19 @@ echo 'Running migrations against all databases.'
 MIGRATIONS_HASH=$1
 PROJECT_ID=$2
 
-run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JAILS --project-id "$PROJECT_ID" --ssl-cert-path ~/prod_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
-run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database STATE --project-id "$PROJECT_ID" --ssl-cert-path ~/prod_state_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
-run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database OPERATIONS --project-id "$PROJECT_ID" --ssl-cert-path ~/prod_operations_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
-run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JUSTICE_COUNTS --project-id "$PROJECT_ID" --ssl-cert-path ~/prod_justice_counts_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
-run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database CASE_TRIAGE --project-id "$PROJECT_ID" --ssl-cert-path ~/prod_case_triage_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+if [[ "$PROJECT_ID" = 'recidiviz-123' ]]; then
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JAILS --project-id recidiviz-123 --ssl-cert-path ~/prod_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database STATE --project-id recidiviz-123 --ssl-cert-path ~/prod_state_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database OPERATIONS --project-id recidiviz-123 --ssl-cert-path ~/prod_operations_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JUSTICE_COUNTS --project-id recidiviz-123 --ssl-cert-path ~/prod_justice_counts_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database CASE_TRIAGE --project-id recidiviz-123 --ssl-cert-path ~/prod_case_triage_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+elif [[ "$PROJECT_ID" = 'recidiviz-staging' ]]; then
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JAILS --project-id recidiviz-staging --ssl-cert-path ~/dev_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database STATE --project-id recidiviz-staging --ssl-cert-path ~/dev_state_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database OPERATIONS --project-id recidiviz-staging --ssl-cert-path ~/dev_operations_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database JUSTICE_COUNTS --project-id recidiviz-staging --ssl-cert-path ~/dev_justice_counts_data_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+    run_cmd python -m recidiviz.tools.migrations.run_migrations_to_head --database CASE_TRIAGE --project-id recidiviz-staging --ssl-cert-path ~/dev_case_triage_certs/ --skip-db-name-check --confirm-hash "$MIGRATIONS_HASH"
+else
+    echo_error "Unrecognized project id for migration: ${PROJECT_ID}"
+    exit 1
+fi
