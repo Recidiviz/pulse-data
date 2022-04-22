023eb30a94255d57315baf64e08b48c149df1672
Mahmoud O <59492998+mxosman@users.noreply.github.com>
2022-04-22 15:42:53 -0500

    [Justice Counts] Initial work on data entry view, form components, and styling (Recidiviz/recidiviz-data#12350)
    * Initial work on form components
    
    * Clean up comments
    
    * Extends TextInput to optionally have the styling of the Additional Context field
    
    * Fix padding on Input
    
    * Create px to rem conversion util, styling clean up and matching 1/n
    
    * Finish styling clean up
    
    * Another styling spot check, clean up comments
    
    * Sort out resetBinaryInputs logic, comment clean up, styling clean up
    
    * -
    
    * Styling adjustments
    
    * Fix input padding (right)
    
    * Add a bit of responsiveness
    
    * Remove max-width
    
    * Add tests
    
    * Fix linting issues
    
    * Temporarily re-add FormComponents route for visibility - will be merged with ReportDataEntry in next PR
    
    Co-authored-by: Mahmoud <mahmoud@Mahmouds-MacBook-Pro.local>
    GitOrigin-RevId: 25538e248bdcebb5417149ebf74be8d4e1a9a31b

---
+++ frontends/justice-counts/control-panel/src/components/Forms/Forms.test.tsx
@@ -0,0 +1,66 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2022 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import { TextInput } from ".";
+
+test("Optional input label without value expected to be default position, font-size and color", async () => {
+  render(
+    <TextInput
+      type="text"
+      label="Total Staff"
+      valueLabel="People"
+      context="Measures the number of full-time staff employed by the agency."
+      error=""
+      value=""
+      readOnly
+    />
+  );
+
+  const label = screen.getByText(/Total Staff/i);
+
+  expect(window.getComputedStyle(label).top).toBe("26px");
+  expect(window.getComputedStyle(label).fontSize).toBe("1.25rem");
+  expect(window.getComputedStyle(label).color).toBe("rgb(119, 119, 119)");
+
+  expect.hasAssertions();
+});
+
+test("Required input label with value expected to shrink position, font-size and change color", async () => {
+  render(
+    <TextInput
+      type="text"
+      label="Total Staff"
+      valueLabel="People"
+      context="Measures the number of full-time staff employed by the agency."
+      error=""
+      value="100"
+      readOnly
+      required
+    />
+  );
+
+  const label = screen.getByText(/Total Staff/i);
+
+  expect(window.getComputedStyle(label).top).toBe("12px");
+  expect(window.getComputedStyle(label).fontSize).toBe("0.75rem");
+  expect(window.getComputedStyle(label).color).toBe("rgb(0, 115, 229)");
+
+  expect.hasAssertions();
+});
