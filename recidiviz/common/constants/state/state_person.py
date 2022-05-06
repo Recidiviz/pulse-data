063c2a620dd60300638e2121caca7c90f47c9893
Julia Dressel <julia@recidiviz.org>
2022-05-06 14:44:31 -0700

    [Enum Docs] Fixed `StatePerson` enum PR (Recidiviz/recidiviz-data#12827)
    A fixed version of Recidiviz/recidiviz-data#12748, which had a broken migration. That PR initially removed the StateResidencyStatus.TRANSIENT, because I thought it was entirely unused. Turns out there is special logic to parse residency statuses that looks for words in someone's address, and that there were instances of this value in the state_person_history table. Decided to update the PR to not remove this enum value.
    
    Also, multiple PRs were merged where the enum docs in the PRs did not reflect the PR edits that had happened in that PR because the pre-commit config wasn't set up to enforce new enum/entity doc generation if the recidiviz/common/constants/state/* files had been updated. This PR includes those enum doc updates, and the fix to the pre-commit hook.
    
    GitOrigin-RevId: cbf6c60d985cd07e52f9c0277d7e01b9542f4084

--- recidiviz/common/constants/state/state_person.py
+++ recidiviz/common/constants/state/state_person.py
@@ -27,7 +27,7 @@ from recidiviz.common.constants.state.state_entity_enum import StateEntityEnum
 class StateGender(StateEntityEnum):
     FEMALE = state_enum_strings.state_gender_female
     MALE = state_enum_strings.state_gender_male
-    OTHER = state_enum_strings.state_gender_other
+    NON_BINARY = state_enum_strings.state_gender_non_binary
     TRANS = state_enum_strings.state_gender_trans
     TRANS_FEMALE = state_enum_strings.state_gender_trans_female
     TRANS_MALE = state_enum_strings.state_gender_trans_male
@@ -38,6 +38,23 @@ class StateGender(StateEntityEnum):
     def _get_default_map() -> Dict[str, Optional["StateGender"]]:
         return _STATE_GENDER_MAP
 
+    @classmethod
+    def get_enum_description(cls) -> str:
+        return "The gender of the person."
+
+    @classmethod
+    def get_value_descriptions(cls) -> Dict["StateEntityEnum", str]:
+        return _STATE_GENDER_VALUE_DESCRIPTIONS
+
+
+_STATE_GENDER_VALUE_DESCRIPTIONS: Dict[StateEntityEnum, str] = {
+    StateGender.FEMALE: "Female",
+    StateGender.MALE: "Male",
+    StateGender.NON_BINARY: "Non-binary",
+    StateGender.TRANS: "Trans",
+    StateGender.TRANS_FEMALE: "Trans female",
+    StateGender.TRANS_MALE: "Trans male",
+}
 
 # TODO(#8905): Delete _get_default_map() once all state ingest views have been
 #  migrated to v2 mappings.
@@ -55,6 +72,25 @@ class StateRace(StateEntityEnum):
     def _get_default_map() -> Dict[str, "StateRace"]:
         return _STATE_RACE_MAP
 
+    @classmethod
+    def get_enum_description(cls) -> str:
+        return "A racial identity of the person."
+
+    @classmethod
+    def get_value_descriptions(cls) -> Dict["StateEntityEnum", str]:
+        return _STATE_RACE_VALUE_DESCRIPTIONS
+
+
+_STATE_RACE_VALUE_DESCRIPTIONS: Dict[StateEntityEnum, str] = {
+    StateRace.AMERICAN_INDIAN_ALASKAN_NATIVE: "American Indian Alaskan Native",
+    StateRace.ASIAN: "Asian",
+    StateRace.BLACK: "Black",
+    StateRace.NATIVE_HAWAIIAN_PACIFIC_ISLANDER: "Native Hawaiian Pacific Islander",
+    StateRace.OTHER: "Used when the state data explicitly indicates that the person’s "
+    "race is “Other”. If there is a race value that does not map to one of our "
+    "`StateRace` enum values, then `INTERNAL_UNKNOWN` should be used.",
+    StateRace.WHITE: "White",
+}
 
 # TODO(#8905): Delete _get_default_map() once all state ingest views have been
 #  migrated to v2 mappings.
@@ -68,6 +104,19 @@ class StateEthnicity(StateEntityEnum):
     def _get_default_map() -> Dict[str, "StateEthnicity"]:
         return STATE_ETHNICITY_MAP
 
+    @classmethod
+    def get_enum_description(cls) -> str:
+        return "An ethnicity of the person."
+
+    @classmethod
+    def get_value_descriptions(cls) -> Dict["StateEntityEnum", str]:
+        return _STATE_ETHNICITY_VALUE_DESCRIPTIONS
+
+
+_STATE_ETHNICITY_VALUE_DESCRIPTIONS: Dict[StateEntityEnum, str] = {
+    StateEthnicity.HISPANIC: "Used when a person is Hispanic.",
+    StateEthnicity.NOT_HISPANIC: "Used when a person is not Hispanic.",
+}
 
 # TODO(#8905): Delete _get_default_map() once all state ingest views have been
 #  migrated to v2 mappings.
@@ -82,6 +131,22 @@ class StateResidencyStatus(StateEntityEnum):
     def _get_default_map() -> Dict[str, "StateResidencyStatus"]:
         return STATE_RESIDENCY_STATUS_SUBSTRING_MAP
 
+    @classmethod
+    def get_enum_description(cls) -> str:
+        return "The status of the person’s permanent residency."
+
+    @classmethod
+    def get_value_descriptions(cls) -> Dict["StateEntityEnum", str]:
+        return _STATE_RESIDENCY_STATUS_VALUE_DESCRIPTIONS
+
+
+_STATE_RESIDENCY_STATUS_VALUE_DESCRIPTIONS: Dict[StateEntityEnum, str] = {
+    StateResidencyStatus.HOMELESS: "Used when the person is unhoused.",
+    StateResidencyStatus.TRANSIENT: "Used when the person is living in temporary "
+    "housing.",
+    StateResidencyStatus.PERMANENT: "Used when the person is living in a permanent "
+    "residence.",
+}
 
 _STATE_GENDER_MAP = {
     "EXTERNAL UNKNOWN": StateGender.EXTERNAL_UNKNOWN,
@@ -90,8 +155,8 @@ _STATE_GENDER_MAP = {
     "M": StateGender.MALE,
     "MALE": StateGender.MALE,
     "NOT SPECIFIED": None,
-    "O": StateGender.OTHER,
-    "OTHER": StateGender.OTHER,
+    "NB": StateGender.NON_BINARY,
+    "NON BINARY": StateGender.NON_BINARY,
     "TRANS": StateGender.TRANS,
     "TRANS FEMALE": StateGender.TRANS_FEMALE,
     "TRANS FEMALE TRANS WOMAN": StateGender.TRANS_FEMALE,
