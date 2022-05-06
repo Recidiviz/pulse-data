bc26d0adb02b0abaa5a995112fdba69e4506a9c3
Julia Dressel <julia@recidiviz.org>
2022-05-06 11:57:24 -0700

    [Ingest Fix] Fixing US_ME `StateChargeClassificationType` mapping (Recidiviz/recidiviz-data#12783)
    Fixing wrong StateChargeClassificationType mapping in US_ME. Should be merged before the PR that documents the StateChargeClassificationType enum so that we can delete the (now) unused OTHER value.
    
    GitOrigin-RevId: f1da41626275475b98385bd0479952db2f73e415

---
+++ recidiviz/persistence/database/migrations/state/versions/2022_05_06_1722_a6a86d0718db_us_me_charge_fix.py
@@ -0,0 +1,68 @@
+# pylint: skip-file
+"""us_me_charge_fix
+
+Revision ID: a6a86d0718db
+Revises: 6d2226525eb4
+Create Date: 2022-05-05 10:22:09.175135
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+from recidiviz.utils.string import StrictStringFormatter
+
+revision = "a6a86d0718db"
+down_revision = "6d2226525eb4"
+branch_labels = None
+depends_on = None
+
+
+UPDATE_QUERY = (
+    "UPDATE {table_name} SET classification_type = '{new_value}'"
+    " WHERE state_code = 'US_ME'"
+    " AND classification_type = '{old_value}';"
+)
+
+TABLES = [
+    "state_charge",
+    "state_charge_history",
+]
+
+
+def upgrade() -> None:
+    old_value = "OTHER"
+    new_value = "INFRACTION"
+
+    for table in TABLES:
+        op.execute(
+            StrictStringFormatter().format(
+                UPDATE_QUERY,
+                table_name=table,
+                new_value=new_value,
+                old_value=old_value,
+            )
+        )
+
+
+def downgrade() -> None:
+    # NOTE: THIS IS NOT A PERFECT DOWNGRADE.
+    # There are some `INFRACTION` instances that will be changed to `OTHER` in this
+    # downgrade that would not have been mapped to `OTHER` in ingest. However,
+    # it is not possible to distinguish between the StateCharge entities that
+    # originated from the us_me_supervision_sentences view versus the
+    # us_me_incarceration_sentences view. As of 5/5/2022, this will only impact 1
+    # StateCharge entity.
+
+    old_value = "INFRACTION"
+    new_value = "OTHER"
+
+    for table in TABLES:
+        op.execute(
+            StrictStringFormatter().format(
+                UPDATE_QUERY,
+                table_name=table,
+                new_value=new_value,
+                old_value=old_value,
+            )
+        )
