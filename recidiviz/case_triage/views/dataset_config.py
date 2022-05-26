6997e33371a9bbe202424f54735ad87d4fb46726
Dan Hansen <dan@recidiviz.org>
2022-05-26 15:11:50 -0700

    [Case Triage] Disable Case Triage CloudSQL -> BQ refresh (Recidiviz/recidiviz-data#13139)
    GitOrigin-RevId: 4e7da717d562eecb520d4d0b42e386204f122f68

--- recidiviz/case_triage/views/dataset_config.py
+++ recidiviz/case_triage/views/dataset_config.py
@@ -18,14 +18,6 @@
 
 CASE_TRIAGE_DATASET: str = "case_triage"
 
-# Transitional dataset in the same region (e.g. us-east1) as the Case Triage CloudSQL
-# instance where Case Triage CloudSQL data is stored before the CloudSQL -> BQ refresh
-# copies it to a dataset in the 'US' multi-region.
-CASE_TRIAGE_FEDERATED_REGIONAL_DATASET: str = "case_triage_federated_regional"
-
-# Where data exported from CloudSQL -> BQ federated export lives
-CASE_TRIAGE_FEDERATED_DATASET: str = "case_triage_federated"
-
 # Location the Cloud SQL connection resides in
 CASE_TRIAGE_CLOUDSQL_LOCATION: str = "us-central1"
 
