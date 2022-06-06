1a7748e5faadd9f3f388993df82df1439b6c6177
lilidworkin <99899591+lilidworkin@users.noreply.github.com>
2022-06-06 14:03:14 -0400

    [Justice Counts] Extend fixtures to generate bigger and better fake data (Recidiviz/recidiviz-data#13277)
    GitOrigin-RevId: 31cb8089e563839ee27bb519e144aed8588c97d5

--- recidiviz/tools/justice_counts/control_panel/generate_fixtures.py
+++ recidiviz/tools/justice_counts/control_panel/generate_fixtures.py
@@ -18,10 +18,130 @@
 # =============================================================================
 """Utilities for generating Justice Counts fixtures."""
 
-from typing import List
+import datetime
+import itertools
+import random
+from typing import Any, Iterable, List, cast
 
+from faker import Faker
+
+from recidiviz.common.constants.justice_counts import ValueType
+from recidiviz.justice_counts.dimensions.base import DimensionBase
+from recidiviz.justice_counts.metrics.metric_definition import (
+    AggregatedDimension,
+    Context,
+    MetricDefinition,
+)
 from recidiviz.justice_counts.report import ReportInterface
 from recidiviz.persistence.database.schema.justice_counts import schema
+
+random.seed(0)
+Faker.seed(0)
+FAKE = Faker(locale=["en-US"])
+
+
+def _create_aggregate_datapoint(
+    report: schema.Report, metric_definition: MetricDefinition
+) -> schema.Datapoint:
+    return schema.Datapoint(
+        value=random.randint(10000, 100000),
+        metric_definition_key=metric_definition.key,
+        value_type=ValueType.NUMBER,
+        start_date=report.date_range_start,
+        end_date=report.date_range_end,
+        dimension_identifier_to_member=None,
+        report=report,
+    )
+
+
+def _create_dimension_datapoints(
+    report: schema.Report,
+    metric_definition: MetricDefinition,
+    dimension: AggregatedDimension,
+) -> List[schema.Datapoint]:
+    return [
+        schema.Datapoint(
+            value=random.randint(10000, 100000),
+            metric_definition_key=metric_definition.key,
+            value_type=ValueType.NUMBER,
+            start_date=report.date_range_start,
+            end_date=report.date_range_end,
+            dimension_identifier_to_member={
+                dimension.dimension_identifier(): d.dimension_value
+            },
+            report=report,
+        )
+        for d in cast(Iterable[DimensionBase], dimension.dimension)
+    ]
+
+
+def _create_context_datapoint(
+    report: schema.Report,
+    metric_definition: MetricDefinition,
+    context: Context,
+) -> schema.Datapoint:
+    value: Any
+    if context.value_type == ValueType.NUMBER:
+        value = random.randint(10000, 100000)
+    elif context.value_type == ValueType.BOOLEAN:
+        value = random.choice(["True", "False"])
+    elif context.value_type == ValueType.TEXT:
+        value = FAKE.text(max_nb_chars=50)
+    else:
+        raise ValueError("Invalid ValueType")
+    return schema.Datapoint(
+        value=value,
+        metric_definition_key=metric_definition.key,
+        context_key=context.key.value,
+        value_type=context.value_type,
+        start_date=report.date_range_start,
+        end_date=report.date_range_end,
+        dimension_identifier_to_member=None,
+        report=report,
+    )
+
+
+def _get_datapoints_for_report(
+    report: schema.Report, system: schema.System
+) -> List[schema.Datapoint]:
+    metric_definitions = ReportInterface.get_metric_definitions_by_report_type(
+        report_type=report.type,
+        system=system,
+    )
+    return list(
+        itertools.chain(
+            *[
+                [
+                    _create_aggregate_datapoint(
+                        report=report,
+                        metric_definition=metric_definition,
+                    )
+                ]
+                + [
+                    _create_context_datapoint(
+                        report=report,
+                        metric_definition=metric_definition,
+                        context=context,
+                    )
+                    for context in metric_definition.contexts
+                ]
+                + list(
+                    itertools.chain(
+                        *[
+                            _create_dimension_datapoints(
+                                report=report,
+                                metric_definition=metric_definition,
+                                dimension=dimension,
+                            )
+                            for dimension in metric_definition.aggregated_dimensions
+                            or []
+                        ]
+                    )
+                )
+                for metric_definition in metric_definitions
+            ]
+        )
+    )
 
 
 def generate_fixtures() -> List[schema.JusticeCountsBase]:
@@ -37,8 +157,16 @@ def generate_fixtures() -> List[schema.JusticeCountsBase]:
     object_groups = []
 
     # First add a group of Users
+    num_users = 5
     object_groups.append(
-        [schema.UserAccount(id=0, email_address="john@fake.com", name="John Smith")]
+        [
+            schema.UserAccount(
+                id=i,
+                email_address=f"{FAKE.name().replace(' ','').lower()}@gmail.com",
+                name=None,
+            )
+            for i in range(num_users)
+        ]
     )
 
     # Next add a group of Agencies
@@ -68,23 +196,40 @@ def generate_fixtures() -> List[schema.JusticeCountsBase]:
 
     # Finally add a group of reports (which depend on agencies already being in the DB)
     reports = []
-    for agency_id, _, agency_name in agency_tuples:
-        # For each agency, create two reports, one monthly and one annual.
-        monthly_report = ReportInterface.create_report_object(
-            agency_id=agency_id,
-            user_account_id=0,
-            year=2022,
-            month=1,
-            frequency="MONTHLY",
-        )
-        annual_report = ReportInterface.create_report_object(
-            agency_id=agency_id,
-            user_account_id=0,
-            year=2022,
-            month=1,
-            frequency="ANNUAL",
-        )
-        reports.extend([monthly_report, annual_report])
+    for agency_id, agency_system, agency_name in agency_tuples:
+        for year in range(2020, 2023):
+            annual_report = ReportInterface.create_report_object(
+                agency_id=agency_id,
+                user_account_id=random.randrange(num_users),
+                year=year,
+                month=1,
+                frequency="ANNUAL",
+                last_modified_at=FAKE.date_time_between(
+                    start_date=datetime.datetime(2020, 1, 1)
+                ),
+            )
+            annual_report.datapoints = _get_datapoints_for_report(
+                report=annual_report,
+                system=agency_system,
+            )
+            reports.append(annual_report)
+
+            for month in range(1, 13):
+                monthly_report = ReportInterface.create_report_object(
+                    agency_id=agency_id,
+                    user_account_id=random.randrange(num_users),
+                    year=year,
+                    month=month,
+                    frequency="MONTHLY",
+                    last_modified_at=FAKE.date_time_between(
+                        start_date=datetime.datetime(2020, 1, 1)
+                    ),
+                )
+                monthly_report.datapoints = _get_datapoints_for_report(
+                    report=monthly_report,
+                    system=agency_system,
+                )
+                reports.append(monthly_report)
     object_groups.append(reports)
 
     return object_groups
