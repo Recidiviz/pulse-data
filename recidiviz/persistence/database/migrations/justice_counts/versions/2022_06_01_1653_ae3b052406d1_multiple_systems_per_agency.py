3c9471ed5fda96a1ac2eab7f456c1c42f1b9ed3c
lilidworkin <99899591+lilidworkin@users.noreply.github.com>
2022-06-06 12:54:52 -0400

    [Justice Counts][1/n] Allowing agencies to below to multiple systems (Recidiviz/recidiviz-data#13282)
    GitOrigin-RevId: b7e3bab0aa9fadd7e0c7b9f3266f6a1aec751598

---
+++ recidiviz/persistence/database/migrations/justice_counts/versions/2022_06_01_1653_ae3b052406d1_multiple_systems_per_agency.py
@@ -0,0 +1,86 @@
+# pylint: skip-file
+"""multiple systems per agency
+
+Revision ID: ae3b052406d1
+Revises: cbbab0a044d3
+Create Date: 2022-06-01 16:53:52.487197
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "ae3b052406d1"
+down_revision = "cbbab0a044d3"
+branch_labels = None
+depends_on = None
+
+old_values = [
+    "DEFENSE",
+    "JAILS",
+    "LAW_ENFORCEMENT",
+    "COURT_PROCESSES",
+    "COURTS_AND_PRETRIAL",
+    "COMMUNITY_SUPERVISION_AND_REENTRY",
+    "CORRECTIONS",
+    "PRISONS",
+    "PROSECUTION",
+    "SUPERVISION",
+]
+
+new_values = [
+    "DEFENSE",
+    "JAILS",
+    "LAW_ENFORCEMENT",
+    "COURT_PROCESSES",
+    "COURTS_AND_PRETRIAL",
+    "COMMUNITY_SUPERVISION_AND_REENTRY",
+    "CORRECTIONS",
+    "PRISONS",
+    "PROSECUTION",
+    "SUPERVISION",
+    "PAROLE",
+    "PROBATION",
+]
+
+
+def upgrade() -> None:
+    op.execute("ALTER TYPE system RENAME TO System_old;")
+    sa.Enum(*new_values, name="system").create(bind=op.get_bind())
+    op.alter_column(
+        "source",
+        column_name="system",
+        type_=sa.Enum(*new_values, name="system"),
+        postgresql_using="system::text::system",
+    )
+    op.alter_column(
+        "report_table_definition",
+        column_name="system",
+        type_=sa.Enum(*new_values, name="system"),
+        postgresql_using="system::text::system",
+    )
+    op.execute("DROP TYPE System_old;")
+
+    op.add_column(
+        "source", sa.Column("systems", sa.ARRAY(sa.String(length=255)), nullable=True)
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("source", "systems")
+
+    op.execute("ALTER TYPE system RENAME TO System_old;")
+    sa.Enum(*old_values, name="system").create(bind=op.get_bind())
+    op.alter_column(
+        "source",
+        column_name="system",
+        type_=sa.Enum(*old_values, name="system"),
+        postgresql_using="system::text::system",
+    )
+    op.alter_column(
+        "report_table_definition",
+        column_name="system",
+        type_=sa.Enum(*old_values, name="system"),
+        postgresql_using="system::text::system",
+    )
+    op.execute("DROP TYPE System_old;")
