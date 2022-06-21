15b5049a005ad0e034363b3a81464f53286ab793
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2022-06-21 15:36:44 -0700

    [Pathways] enable application-data-import service in prod (Recidiviz/recidiviz-data#13242)
    GitOrigin-RevId: 0c14b2d89511aac2822184cfca73e1a41aaa8f19

--- recidiviz/tools/deploy/terraform/cloud-storage-notifications.tf
+++ recidiviz/tools/deploy/terraform/cloud-storage-notifications.tf
@@ -1,10 +1,12 @@
 module "dashboard_data" {
-  # TODO(#12449): Remove staging-only check when we create production environment
-  count  = var.project_id == "recidiviz-123" ? 0 : 1
   source = "./modules/cloud-storage-notification"
 
   bucket_name           = module.dashboard-event-level-data.name
-  push_endpoint         = "${google_cloud_run_service.application-data-import[count.index].status.0.url}/import/trigger_pathways"
+  push_endpoint         = "${google_cloud_run_service.application-data-import.status.0.url}/import/trigger_pathways"
   service_account_email = google_service_account.application_data_import_cloud_run.email
   filter                = "NOT hasPrefix(attributes.objectId, \"staging/\")"
+}
+moved {
+  from = module.dashboard_data[0]
+  to   = module.dashboard_data
 }
