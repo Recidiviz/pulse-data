9d655a7931595e4613c78025f0b0a952e3379e17
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2022-06-29 10:48:40 -0700

    [Orchestration] Add dummy view rematerialization as blocking step in the DAG (Recidiviz/recidiviz-data#13611)
    This PR inserts a dummy view rematerialization step as a blocking step in the orchestration DAG. The dummy step does not actually rematerialize views, but it does write to a rematerialization_tracker table on success. This new step comes after all dataflow pipelines have completed but before ANY metric exports run.
    
    To do this:
    
    * Created a CloudTasksTaskCreateOperator which will queue a cloud task to run the rematerialization endpoint
    * Implemented a new custom sensor operator BQResultSensor which checks every 60 seconds for the success row in the rematerialization_tracker table from that queued cloud task. This operator will fail after 2 hours of no success.
    * Inserted the CloudTasksTaskCreateOperator and BQResultSensor as steps between pipelines and exports in the DAG.
    
    The view materialization step will run even if some of the upstream tasks fail, but no metric exports will run if the view materialization fails. Metric exports for a given state also still won't run if pipelines for that state failed.
    
    We will not actually see any performance gains with this PR on its own. A PR to follow will actually remove the rematerialization step from the metric export endpoint and update the DAG to actually rematerialize the views.
    
    Part of Recidiviz/recidiviz-data#13307.
    
    GitOrigin-RevId: c7e688ae7758ec906090350c908e2769af91113d

---
+++ recidiviz/airflow/dags/calculation/finished_rematerialization_task_query_generator.py
@@ -0,0 +1,85 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Implementation of the BQResultSensorQueryGenerator that returns a query
+that produces a row when the most recently triggered view rematerialization task has
+completed.
+"""
+
+from typing import Dict
+
+from airflow.utils.log.logging_mixin import LoggingMixin
+from google.cloud.tasks_v2 import CloudTasksClient
+
+# Custom Airflow operators in the recidiviz.airflow.dags.operators package are imported
+# into the Cloud Composer environment at the top-level. However, for unit tests, we
+# still need to import the recidiviz-top-level.
+try:
+    from operators.bq_result_sensor import (  # type: ignore
+        BQResultSensor,
+        BQResultSensorQueryGenerator,
+    )
+except ImportError:
+    from recidiviz.airflow.dags.operators.bq_result_sensor import (
+        BQResultSensor,
+        BQResultSensorQueryGenerator,
+    )
+
+
+class FinishedRematerializationTaskQueryGenerator(
+    BQResultSensorQueryGenerator, LoggingMixin
+):
+    """Implementation of the BQResultSensorQueryGenerator that returns a query
+    that produces a row when the most recently triggered view rematerialization task has
+    completed.
+    """
+
+    def __init__(
+        self, project_id: str, cloud_task_create_operator_task_id: str
+    ) -> None:
+        super().__init__()
+        self.project_id = project_id
+        self.cloud_task_create_operator_task_id = cloud_task_create_operator_task_id
+
+    def get_query(self, operator: BQResultSensor, context: Dict) -> str:
+        task_path = self.get_last_queued_task_path(
+            operator, context, self.cloud_task_create_operator_task_id
+        )
+        task_path_parts = CloudTasksClient.parse_task_path(task_path)
+        cloud_task_id = task_path_parts["task"]
+
+        return f"""SELECT *
+FROM `{self.project_id}.view_update_metadata.rematerialization_tracker`
+WHERE cloud_task_id = '{cloud_task_id}';"""
+
+    def get_last_queued_task_path(
+        self,
+        operator: BQResultSensor,
+        context: Dict,
+        cloud_task_create_operator_task_id: str,
+    ) -> str:
+        self.log.info(
+            "Fetching last task queued via the [%s] Airflow task.",
+            cloud_task_create_operator_task_id,
+        )
+        create_cloud_task_result = operator.xcom_pull(
+            context,
+            key="return_value",
+            task_ids=cloud_task_create_operator_task_id,
+        )
+        task_path = create_cloud_task_result["name"]
+        self.log.info("Found last queued task [%s].", task_path)
+        return task_path
--- recidiviz/airflow/dags/calculation_pipeline_dags.py
+++ recidiviz/airflow/dags/calculation_pipeline_dags.py
@@ -19,17 +19,27 @@ The DAG configuration to run the calculation pipelines in Dataflow simultaneousl
 This file is uploaded to GCS on deploy.
 """
 import datetime
+import json
 import os
+import uuid
+from collections import defaultdict
 from typing import Any, Dict, List, NamedTuple, Optional
 
 from airflow.decorators import dag
 from airflow.providers.google.cloud.operators.pubsub import PubSubPublishMessageOperator
+from airflow.providers.google.cloud.operators.tasks import CloudTasksTaskCreateOperator
 from airflow.utils.trigger_rule import TriggerRule
+from google.cloud import tasks_v2
+from google.cloud.tasks_v2 import CloudTasksClient
 
 # Custom Airflow operators in the recidiviz.airflow.dags.operators package are imported into the
 # Cloud Composer environment at the top-level. However, for unit tests, we still need to
 # import the recidiviz-top-level.
 try:
+    from calculation.finished_rematerialization_task_query_generator import (  # type: ignore
+        FinishedRematerializationTaskQueryGenerator,
+    )
+    from operators.bq_result_sensor import BQResultSensor  # type: ignore
     from operators.iap_httprequest_operator import (  # type: ignore
         IAPHTTPRequestOperator,
     )
@@ -41,6 +51,12 @@ try:
         PIPELINE_AGNOSTIC_EXPORTS,
     )
 except ImportError:
+    from recidiviz.airflow.dags.calculation.finished_rematerialization_task_query_generator import (
+        FinishedRematerializationTaskQueryGenerator,
+    )
+    from recidiviz.airflow.dags.operators.bq_result_sensor import (
+        BQResultSensor,
+    )
     from recidiviz.airflow.dags.utils.export_tasks_config import (
         CASE_TRIAGE_STATES,
         PIPELINE_AGNOSTIC_EXPORTS,
@@ -136,6 +152,36 @@ def dataflow_operator_for_pipeline(
     )
 
 
+def trigger_rematerialize_views_operator() -> CloudTasksTaskCreateOperator:
+    queue_location = "us-east1"
+    queue_name = "bq-view-update"
+    task_path = CloudTasksClient.task_path(
+        project_id,
+        location=queue_location,
+        queue=queue_name,
+        task=uuid.uuid4().hex,
+    )
+    task = tasks_v2.types.Task(
+        name=task_path,
+        app_engine_http_request={
+            "http_method": "POST",
+            # TODO(#13307): Change to /view_update/rematerialize_all_deployed_views
+            #  once we're ready to actaully run full rematerialization.
+            "relative_uri": "/view_update/test_rematerialize_all_deployed_views",
+            "body": json.dumps({}).encode(),
+        },
+    )
+    return CloudTasksTaskCreateOperator(
+        task_id="trigger_rematerialize_views_task",
+        location=queue_location,
+        queue_name=queue_name,
+        task=task,
+        # This will trigger the task regardless of the failure or success of the
+        # upstream pipelines.
+        trigger_rule=TriggerRule.ALL_DONE,
+    )
+
+
 def execute_calculations(use_historical: bool, should_trigger_exports: bool) -> None:
     """This represents the overall execution of our calculation pipelines.
 
@@ -155,9 +201,17 @@ def execute_calculations(use_historical: bool, should_trigger_exports: bool) -> 
         trigger_rule=TriggerRule.ALL_DONE,
     )
 
-    normalization_pipelines = YAMLDict.from_path(config_file).pop_dicts(
-        "normalization_pipelines"
+    trigger_view_rematerialize = trigger_rematerialize_views_operator()
+
+    wait_for_rematerialize = BQResultSensor(
+        task_id="wait_for_view_rematerialization_success",
+        query_generator=FinishedRematerializationTaskQueryGenerator(
+            project_id=project_id,
+            cloud_task_create_operator_task_id=trigger_view_rematerialize.task_id,
+        ),
     )
+
+    update_normalized_state >> trigger_view_rematerialize >> wait_for_rematerialize
 
     # TODO(#9010): Have the historical DAG mirror incremental DAG in everything but
     #  calculation month counts
@@ -167,37 +221,9 @@ def execute_calculations(use_historical: bool, should_trigger_exports: bool) -> 
         else "incremental_metric_pipelines"
     )
 
-    supplemental_dataset_pipelines = YAMLDict.from_path(config_file).pop_dicts(
-        "supplemental_dataset_pipelines"
-    )
-
-    states_to_trigger = {
-        pipeline.peek("state_code", str) for pipeline in metric_pipelines
-    }
-
-    metric_pipelines_by_state: Dict[str, List] = {
-        state_code: [] for state_code in states_to_trigger
-    }
-
-    state_trigger_export_operators = (
-        {
-            state_code: trigger_export_operator(state_code)
-            for state_code in states_to_trigger
-        }
-        if should_trigger_exports
-        else {}
-    )
-
-    for state_export in state_trigger_export_operators.values():
-        # This ensures that the normalized_state dataset will be updated before the
-        # export for a state is triggered
-        update_normalized_state >> state_export
-
-    if should_trigger_exports:
-        case_triage_export = trigger_export_operator("CASE_TRIAGE")
-
-        # Case triage export also relies on an updated normalized_state dataset
-        update_normalized_state >> case_triage_export
+    metric_pipelines_by_state: Dict[
+        str, List[RecidivizDataflowTemplateOperator]
+    ] = defaultdict(list)
 
     for metric_pipeline in metric_pipelines:
         pipeline_config_args = get_pipeline_config_args(metric_pipeline)
@@ -206,20 +232,18 @@ def execute_calculations(use_historical: bool, should_trigger_exports: bool) -> 
             metric_pipeline_operator = dataflow_operator_for_pipeline(
                 pipeline_config_args, metric_pipeline
             )
-            # Add the pipeline to the list of metric pipelines for this state
-            metric_pipelines_by_state[pipeline_config_args.state_code] += [
-                metric_pipeline_operator
-            ]
 
-            # This >> ensures that all the calculation pipelines will run before the
-            # Pub / Sub message is published saying the pipelines are done.
-            if should_trigger_exports:
-                (
-                    metric_pipeline_operator
-                    >> state_trigger_export_operators[pipeline_config_args.state_code]
-                )
-                if pipeline_config_args.state_code in CASE_TRIAGE_STATES:
-                    metric_pipeline_operator >> case_triage_export
+            # Metric pipelines should complete before view rematerialization starts
+            metric_pipeline_operator >> trigger_view_rematerialize
+
+            # Add the pipeline to the list of metric pipelines for this state
+            metric_pipelines_by_state[pipeline_config_args.state_code].append(
+                metric_pipeline_operator
+            )
+
+    normalization_pipelines = YAMLDict.from_path(config_file).pop_dicts(
+        "normalization_pipelines"
+    )
 
     for normalization_pipeline in normalization_pipelines:
         pipeline_config_args = get_pipeline_config_args(normalization_pipeline)
@@ -229,6 +253,7 @@ def execute_calculations(use_historical: bool, should_trigger_exports: bool) -> 
                 pipeline_config_args, normalization_pipeline
             )
 
+            # Normalization pipelines should complete before normalized_state dataset is refreshed
             normalization_calculation_pipeline >> update_normalized_state
 
             for metric_pipeline in metric_pipelines_by_state[
@@ -238,18 +263,45 @@ def execute_calculations(use_historical: bool, should_trigger_exports: bool) -> 
                 # run before the metric pipelines for the state are triggered.
                 normalization_calculation_pipeline >> metric_pipeline
 
+    supplemental_dataset_pipelines = YAMLDict.from_path(config_file).pop_dicts(
+        "supplemental_dataset_pipelines"
+    )
     for supplemental_pipeline in supplemental_dataset_pipelines:
         pipeline_config_args = get_pipeline_config_args(supplemental_pipeline)
 
         if project_id == GCP_PROJECT_STAGING or not pipeline_config_args.staging_only:
-            _ = dataflow_operator_for_pipeline(
+            supplemental_pipeline_operator = dataflow_operator_for_pipeline(
                 pipeline_config_args, supplemental_pipeline
             )
 
-    # These exports don't depend on pipeline output.
+            supplemental_pipeline_operator >> trigger_view_rematerialize
+
     if should_trigger_exports:
-        for export in PIPELINE_AGNOSTIC_EXPORTS:
-            _ = trigger_export_operator(export)
+        states_to_trigger = {
+            pipeline.peek("state_code", str) for pipeline in metric_pipelines
+        }
+        state_trigger_export_operators = {
+            state_code: trigger_export_operator(state_code)
+            for state_code in states_to_trigger
+        }
+
+        case_triage_export_operator = trigger_export_operator("CASE_TRIAGE")
+        all_trigger_export_operators = {
+            *state_trigger_export_operators.values(),
+            case_triage_export_operator,
+            *[trigger_export_operator(export) for export in PIPELINE_AGNOSTIC_EXPORTS],
+        }
+
+        for export_operator in all_trigger_export_operators:
+            wait_for_rematerialize >> export_operator
+
+        for state_code, metric_pipeline_operators in metric_pipelines_by_state.items():
+            for metric_pipeline_operator in metric_pipeline_operators:
+                # If any metric pipeline for a particular state fails, then the exports
+                # for that state should not proceed.
+                (metric_pipeline_operator >> state_trigger_export_operators[state_code])
+                if state_code in CASE_TRIAGE_STATES:
+                    (metric_pipeline_operator >> case_triage_export_operator)
 
 
 # By setting catchup to False and max_active_runs to 1, we ensure that at
