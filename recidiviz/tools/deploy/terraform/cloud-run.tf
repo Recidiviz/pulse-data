216182c61cfc660497f516525ba7401965601d90
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2022-07-20 10:18:31 -0700

    [import service] update memory to match other cloud run services (Recidiviz/recidiviz-data#14077)
    GitOrigin-RevId: 2f400d1c2b8affc653fe8ecdd3d10b52093dc5f1

--- recidiviz/tools/deploy/terraform/cloud-run.tf
+++ recidiviz/tools/deploy/terraform/cloud-run.tf
@@ -256,6 +256,13 @@ resource "google_cloud_run_service" "application-data-import" {
           name  = "RECIDIVIZ_ENV"
           value = var.project_id == "recidiviz-123" ? "production" : "staging"
         }
+
+        resources {
+          limits = {
+            cpu    = "1000m"
+            memory = "1024Mi"
+          }
+        }
       }
 
       service_account_name = google_service_account.application_data_import_cloud_run.email
