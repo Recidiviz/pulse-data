0a77164796dd5bca27e3d82afd67b6088400123e
Chinedu Ozodi <chinedu@recidiviz.org>
2022-08-05 14:22:12 -0700

    [Admin Panel] create new component ingest raw file processing status table (Recidiviz/recidiviz-data#14363)
    * initial implementation of new component
    
    * finished initial implementation of new component
    
    * PR update
    
    * PR Updates 2
    
    GitOrigin-RevId: 2227c0b2fcdb6da8b92ab20e2381e173458aafee

---
+++ frontends/admin-panel/src/components/IngestOperationsView/IngestRawFileProcessingStatusTable.tsx
@@ -0,0 +1,82 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2022 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+import { Table } from "antd";
+import { ColumnsType } from "antd/lib/table";
+import * as React from "react";
+import {
+  booleanSort,
+  optionalNumberSort,
+  optionalStringSort,
+} from "../Utilities/GeneralUtilities";
+import { IngestRawFileProcessingStatus } from "./constants";
+
+interface IngestRawFileProcessingStatusTableProps {
+  ingestRawFileProcessingStatus: IngestRawFileProcessingStatus[];
+}
+
+const IngestRawFileProcessingStatusTable: React.FC<IngestRawFileProcessingStatusTableProps> =
+  ({ ingestRawFileProcessingStatus }) => {
+    const columns: ColumnsType<IngestRawFileProcessingStatus> = [
+      {
+        title: "Raw Data File Tag",
+        dataIndex: "fileTag",
+        key: "fileTag",
+        sorter: {
+          compare: (a, b) =>
+            a.hasConfig === b.hasConfig
+              ? a.fileTag.localeCompare(b.fileTag)
+              : booleanSort(a.hasConfig, b.hasConfig),
+          multiple: 2,
+        },
+        defaultSortOrder: "ascend",
+      },
+      {
+        title: "Last Recieved",
+        dataIndex: "latestDiscoveryTime",
+        key: "latestDiscoveryTime",
+        sorter: (a, b) =>
+          a.latestDiscoveryTime.localeCompare(b.latestDiscoveryTime),
+      },
+      {
+        title: "Last Processed",
+        dataIndex: "latestProcessedTime",
+        key: "latestProcessedTime",
+        sorter: (a, b) =>
+          optionalStringSort(a.latestProcessedTime, b.latestProcessedTime),
+      },
+      {
+        title: "# Pending Upload",
+        dataIndex: "numberUnprocessedFiles",
+        key: "numberUnprocessedFiles",
+        sorter: {
+          compare: (a, b) =>
+            optionalNumberSort(
+              a.numberUnprocessedFiles,
+              b.numberUnprocessedFiles
+            ),
+          multiple: 1,
+        },
+        defaultSortOrder: "descend",
+      },
+    ];
+
+    return (
+      <Table dataSource={ingestRawFileProcessingStatus} columns={columns} />
+    );
+  };
+
+export default IngestRawFileProcessingStatusTable;
--- frontends/admin-panel/src/components/Utilities/GeneralUtilities.tsx
+++ frontends/admin-panel/src/components/Utilities/GeneralUtilities.tsx
@@ -12,6 +12,12 @@ export function optionalNumberSort(
   return (a || 0) - (b || 0);
 }
 
+export function booleanSort(a: boolean, b: boolean): number {
+  const aNumber = a ? 1 : 0;
+  const bNumber = b ? 1 : 0;
+  return aNumber - bNumber;
+}
+
 export function getUniqueValues<T>(values: T[]): T[] {
   return Array.from(new Set(values));
 }
