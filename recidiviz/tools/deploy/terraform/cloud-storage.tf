2f483b4a37cba722445371435fbfc5f42f50f9e1
lilidworkin <99899591+lilidworkin@users.noreply.github.com>
2022-08-11 12:28:39 -0400

    [Justice Counts] Fix permissions for new JC GCS bucket Recidiviz/recidiviz-data#14584
    GitOrigin-RevId: b89029fdc2710d7fef24875166d418a15cd9d839

--- recidiviz/tools/deploy/terraform/cloud-storage.tf
+++ recidiviz/tools/deploy/terraform/cloud-storage.tf
@@ -38,7 +38,7 @@ module "justice-counts-control-panel-ingest" {
 resource "google_storage_bucket_iam_member" "justice-counts-control-panel-ingest-bucket-member" {
   bucket = module.justice-counts-control-panel-ingest.name
   role   = "roles/storage.admin"
-  member = "serviceAccount:${data.google_service_account.justice_counts_cloud_run.email}"
+  member = "group:s-justicecounts@recidiviz.org"
 }
 
 module "direct-ingest-state-storage" {
