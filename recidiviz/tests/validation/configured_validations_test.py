c24ab352ab3ba9ed005d331818521dcb9a80b107
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2022-09-07 17:49:26 -0700

    [Validation] Add test that checks that all validation views are configured in a validation job (Recidiviz/recidiviz-data#15054)
    Includes a pretty hacky but effective test that checks that all defined validation view builders in the validation_views dataset are actually registered in a validation job.
    
    Had to exempt a number views which this test found and filed a task to either move or register those views.
    
    Part of Recidiviz/recidiviz-data#15080.
    
    GitOrigin-RevId: adcaa1dca16875bf9331b0837748ca36df1a8813

--- recidiviz/tests/validation/configured_validations_test.py
+++ recidiviz/tests/validation/configured_validations_test.py
@@ -16,11 +16,52 @@
 # =============================================================================
 """Unit test for all configured data validations"""
 import unittest
+from typing import Set
 from unittest.mock import MagicMock, patch
 
 import attr
 
+from recidiviz.big_query.big_query_view import BigQueryViewBuilder
+from recidiviz.big_query.big_query_view_collector import BigQueryViewCollector
+from recidiviz.validation import views as views_module
 from recidiviz.validation.configured_validations import get_all_validations
+from recidiviz.validation.views.dataset_config import VIEWS_DATASET
+from recidiviz.validation.views.state.po_report_clients import (
+    PO_REPORT_CLIENTS_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.prod_staging_comparison.incarceration_admission_external_prod_staging_comparison import (
+    INCARCERATION_ADMISSION_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.prod_staging_comparison.incarceration_population_external_prod_staging_comparison import (
+    INCARCERATION_POPULATION_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.prod_staging_comparison.incarceration_release_external_prod_staging_comparison import (
+    INCARCERATION_RELEASE_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.prod_staging_comparison.sessions_justice_counts_comparison import (
+    SESSIONS_JUSTICE_COUNTS_COMPARISON_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.prod_staging_comparison.sessions_justice_counts_prod_staging_comparison import (
+    SESSIONS_JUSTICE_COUNTS_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.prod_staging_comparison.supervision_population_external_prod_staging_comparison import (
+    SUPERVISION_POPULATION_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.prod_staging_comparison.supervision_start_external_prod_staging_comparison import (
+    SUPERVISION_START_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.prod_staging_comparison.supervision_termination_external_prod_staging_comparison import (
+    SUPERVISION_TERMINATION_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.sessions_validation.reincarcerations_from_dataflow_to_dataflow_disaggregated import (
+    REINCARCERATIONS_FROM_DATAFLOW_TO_DATAFLOW_DISAGGREGATED_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.sessions_validation.reincarcerations_from_sessions_to_dataflow_disaggregated import (
+    REINCARCERATIONS_FROM_SESSIONS_TO_DATAFLOW_DISAGGREGATED_VIEW_BUILDER,
+)
+from recidiviz.validation.views.state.sessions_validation.revocation_sessions_to_dataflow_disaggregated import (
+    REVOCATION_SESSIONS_TO_DATAFLOW_DISAGGREGATED_VIEW_BUILDER,
+)
 
 
 @patch("recidiviz.utils.metadata.project_id", MagicMock(return_value="test-project"))
@@ -42,3 +83,67 @@ class TestConfiguredValidations(unittest.TestCase):
         for validation in validations:
             validation_view = validation.view_builder.build()
             assert "region_code" in validation_view.view_query
+
+    def test_all_validation_builders_configured_in_a_validation(self) -> None:
+        found_builders = set(
+            BigQueryViewCollector.collect_view_builders_in_module(
+                # TODO(python/mypy#5374): Remove the ignore type when abstract class
+                #  assignments are supported.
+                builder_type=BigQueryViewBuilder,  # type: ignore[misc]
+                view_dir_module=views_module,
+                recurse=True,
+                view_builder_attribute_name_regex=".*_VIEW_BUILDER",
+                expect_builders_in_all_files=False,
+            )
+        )
+
+        found_validation_builders = set(
+            builder
+            for builder in found_builders
+            if builder.address.dataset_id == VIEWS_DATASET
+            # TODO(#15080): Configure validations for these views which were
+            #  created without an associated configured validation, or move
+            #  out of the validation_views dataset.
+            and builder
+            not in [
+                # Polaris
+                PO_REPORT_CLIENTS_VIEW_BUILDER,
+                # External validation data
+                INCARCERATION_ADMISSION_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+                INCARCERATION_RELEASE_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+                INCARCERATION_POPULATION_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+                SUPERVISION_START_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+                SUPERVISION_TERMINATION_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+                SUPERVISION_POPULATION_EXTERNAL_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+                # Sessions
+                SESSIONS_JUSTICE_COUNTS_COMPARISON_VIEW_BUILDER,
+                SESSIONS_JUSTICE_COUNTS_PROD_STAGING_COMPARISON_VIEW_BUILDER,
+                REINCARCERATIONS_FROM_SESSIONS_TO_DATAFLOW_DISAGGREGATED_VIEW_BUILDER,
+                REINCARCERATIONS_FROM_DATAFLOW_TO_DATAFLOW_DISAGGREGATED_VIEW_BUILDER,
+                REVOCATION_SESSIONS_TO_DATAFLOW_DISAGGREGATED_VIEW_BUILDER,
+            ]
+        )
+
+        validations = get_all_validations()
+        builders_in_validations: Set[BigQueryViewBuilder] = {
+            v.view_builder for v in validations
+        }
+
+        if in_validation_but_not_found := (
+            builders_in_validations - found_validation_builders
+        ):
+            raise ValueError(
+                f"Found validation registered in a configured validation in "
+                f"configured_validations.py, but which is not defined in the validation "
+                f"views directory: "
+                f"{[b.address for b in in_validation_but_not_found]}"
+            )
+
+        if not_registered_in_validation := (
+            found_validation_builders - builders_in_validations
+        ):
+            raise ValueError(
+                f"Found validation view builders that are not registered in a "
+                f"configured validation in configured_validations.py: "
+                f"{[b.address for b in not_registered_in_validation]}"
+            )
