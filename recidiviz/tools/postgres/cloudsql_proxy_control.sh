908fb1f568420c14f54a6df80f9c25e3d8a8cd81
Dan Hansen <dan@recidiviz.org>
2022-09-13 13:26:18 -0700

    [Cloud SQL] Add verify method which checks if the proxy is running / did run successfully (Recidiviz/recidiviz-data#15252)
    GitOrigin-RevId: 8f516419e55755f8201964fa71ca3f5121ad1b58

--- recidiviz/tools/postgres/cloudsql_proxy_control.sh
+++ recidiviz/tools/postgres/cloudsql_proxy_control.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2021 Recidiviz, Inc.
+# Copyright (C) 2022 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,48 +16,79 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 
+
+# Example usages:
+# # Start the proxy
+# ✗ ./recidiviz/tools/postgres/cloudsql_proxy_control.sh -p 5440 -c PROJECT_ID:GCP_REGION:INSTANCE_NAME
+# Starting Cloud SQL Proxy container for PROJECT_ID:GCP_REGION:INSTANCE_NAME...
+# Started Cloud SQL Proxy container with name: dazzling_mclean
+#
+# # See if the proxy can be started by performing a dry run
+# ✗ ./recidiviz/tools/postgres/cloudsql_proxy_control.sh -p 5440 -c PROJECT_ID:GCP_REGION:INSTANCE_NAME -d
+# Connection to 127.0.0.1 port 5440 [tcp/*] succeeded!
+# The following processes are using port :5440. Please quit the process bound to the port and try again.
+# COMMAND    PID      USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
+# com.docke 4747 danhansen  137u  IPv4 0xe91a990d7862af27      0t0  TCP localhost:5440->localhost:49207 (CLOSE_WAIT)
+# com.docke 4747 danhansen  139u  IPv4 0xe91a990d7bbc19c7      0t0  TCP localhost:5440 (LISTEN)
+# Run the following command to kill the docker containers (if applicable):
+# ./recidiviz/tools/postgres/cloudsql_proxy_control.sh -q -p 5440
+#
+# # Verify proxy is running
+# ✗ ./recidiviz/tools/postgres/cloudsql_proxy_control.sh -p 5440 -c PROJECT_ID:GCP_REGION:INSTANCE_NAME -v
+# The Cloud SQL Proxy is running and accepting connections.
+#
+# # Quit proxy
+# ✗ ./recidiviz/tools/postgres/cloudsql_proxy_control.sh -p 5440 -c PROJECT_ID:GCP_REGION:INSTANCE_NAME -q
+# Killing Cloud SQL Proxy container on :5440 with name: dazzling_mclean
+#
+# # Verify that the container executed without encountering network errors
+# ✗ ./recidiviz/tools/postgres/cloudsql_proxy_control.sh -p 5440 -c PROJECT_ID:GCP_REGION:INSTANCE_NAME -v
+# No known errors encountered in the Cloud SQL Proxy container.
+
+
 BASH_SOURCE_DIR=$(dirname "$BASH_SOURCE")
 source ${BASH_SOURCE_DIR}/../script_base.sh
 source ${BASH_SOURCE_DIR}/script_helpers.sh
 
-CLOUD_SQL_PROXY_IMAGE="gcr.io/cloudsql-docker/gce-proxy:1.31.0"
-
-DATABASE_CONNECTION_STRING=''
-DATABASE_PORT=''
-DRY_RUN=false
-CONTROL=start
-
 function print_usage {
-    echo_error "usage: $0 -p LOCAL_PORT [-dq] [-c CONNECTION_STRING]"
+    echo_error "usage: $0 -p LOCAL_PORT [-dqv] [-c CONNECTION_STRING]"
     echo_error "  -p: (required) Port to expose the local database server on."
     echo_error "  -c: Connection string of the Cloud SQL instance. format: PROJECT_ID:REGION:INSTANCE_NAME"
     echo_error "      The connection string can be retrieved using gcloud secrets versions access latest --project=PROJECT_ID --secret=SCHEMA_TYPE_cloudsql_instance_id"
     echo_error "  -d: Dry run to check that the proxy can be started."
+    echo_error "  -v: Verify that the container behaved as we expected. Exits with non-zero if not."
     echo_error "  -q: Quit any running Cloud SQL Proxy containers"
     run_cmd exit 1
 }
 
-function get_container_name {
-  PORT=$1
-  docker ps --format '{{.Names}}' --filter ancestor=${CLOUD_SQL_PROXY_IMAGE} --filter expose=${PORT}
+function prune_old_containers {
+  # Prunes CloudSQL Proxy containers from more than 1 week ago
+  EXITED_CONTAINERS=$(get_exited_containers)
+  if [[ $EXITED_CONTAINERS != "" ]]; then
+    docker rm ${EXITED_CONTAINERS} > /dev/null
+  fi
 }
 
-while getopts "dqc:p:" flag; do
-  case "${flag}" in
-    d) DRY_RUN=true ;;
-    c) DATABASE_CONNECTION_STRING="$OPTARG" ;;
-    p) DATABASE_PORT="$OPTARG" ;;
-    q) CONTROL=QUIT ;;
-    *) print_usage
-  esac
-done
+function get_ancestor_filters {
+  # Gets `docker ps` filter arguments for all versions of the Cloud SQL proxy image
+  LOCAL_IMAGES=$(docker images --filter "reference=${CLOUDSQL_PROXY_IMAGE_NAME}:*" --format "{{.ID}}")
+  echo $LOCAL_IMAGES | xargs -I {} echo '--filter ancestor={}'
+}
 
-if [ "$DATABASE_PORT" == "" ]; then
-  echo_error "Missing required argument -p"
-  print_usage
-fi
 
-if [ $CONTROL == "QUIT" ]; then
+function get_container_name {
+  PORT=$1
+  docker ps --format '{{.Names}}' $(get_ancestor_filters) --filter expose=${PORT}
+}
+
+function get_exited_containers {
+  # Exited containers no longer have ports associated, so they don't match the `expose` filter in `get_container_name`
+  # Duplicating the ps call here
+  docker ps --format '{{.Names}}' $(get_ancestor_filters) --filter "status=exited"
+}
+
+function control_quit {
+  # Quit running proxies on the specified port
   DOCKER_CONTAINER_ID=$(get_container_name $DATABASE_PORT)
 
   if [ "$DOCKER_CONTAINER_ID" == "" ]; then
@@ -68,44 +99,114 @@ if [ $CONTROL == "QUIT" ]; then
   fi
 
   exit 0
-fi
+}
 
+function control_verify {
+  # Verify that the proxy is running, if it is not, double check the logs to verify that it ran successfully
+  DOCKER_CONTAINER_ID=$(get_container_name $DATABASE_PORT)
 
+  if [ $DOCKER_CONTAINER_ID ]; then
+    wait_for_postgres $DATABASE_HOST $DATABASE_PORT
+    echo "The Cloud SQL Proxy is running and accepting connections."
+    exit 0
+  fi
 
-if [ "$DATABASE_CONNECTION_STRING" == "" ]; then
-  print_usage
-fi
+  DOCKER_CONTAINER_ID=$(get_exited_containers | head -n 1)
 
+  if [ -z $DOCKER_CONTAINER_ID ]; then
+    echo "Could not find any existing proxy containers for port ${DATABASE_PORT}"
+    exit $CLOUDSQL_PROXY_NEVER_STARTED_ERROR_EXIT_CODE
+  fi
 
-if [ $DRY_RUN == "true" ]; then
-  if nc -z $CLOUDSQL_PROXY_HOST $DATABASE_PORT; then
-    echo "The following processes are using port :${DATABASE_PORT}. Please quit the process bound to the port and try again."
-    lsof -i :${DATABASE_PORT}
+  CONTAINER_LOGS=$(docker logs ${DOCKER_CONTAINER_ID} 2>&1)
 
-    echo "Run the following command to kill the docker containers (if applicable):"
-    echo "$0 -q"
-    exit 1
+  # Check for any client networks errors, such as losing connection, or connection being reset
+  ENCOUNTERED_NETWORK_ERROR=$(echo -e "$CONTAINER_LOGS" | grep -ie "connection refused" -e "connection reset by peer")
+
+  if [[ $ENCOUNTERED_NETWORK_ERROR != "" ]]; then
+    echo "The Cloud SQL Proxy container encountered a network error:"
+    echo "${ENCOUNTERED_NETWORK_ERROR}"
+    exit $CLOUDSQL_PROXY_NETWORK_ERROR_EXIT_CODE
+  else
+    echo "No known errors encountered in the Cloud SQL Proxy container."
+    echo "${CONTAINER_LOGS}"
   fi
 
   exit 0
+}
+
+function control_start {
+   if [ "$DATABASE_CONNECTION_STRING" == "" ]; then
+    print_usage
+  fi
+
+
+  if [ $DRY_RUN == "true" ]; then
+    if nc -z $DATABASE_HOST $DATABASE_PORT; then
+      echo "The following processes are using port :${DATABASE_PORT}. Please quit the process bound to the port and try again."
+      lsof -i :${DATABASE_PORT}
+
+      echo "Run the following command to kill the docker containers (if applicable):"
+      echo "$0 -q -p ${DATABASE_PORT}"
+      exit 1
+    fi
+
+    exit 0
+  fi
+
+  echo "Starting Cloud SQL Proxy container for ${DATABASE_CONNECTION_STRING}..."
+
+  # The docker container runs with ~/.config/gcloud mounted as a volume to /config
+  # It assumes the existence of application_default_credentials.json
+  # Run `gcloud auth application-default login` if you are running into authentication errors
+  docker run -d \
+    -p $DATABASE_HOST:$DATABASE_PORT:$DATABASE_PORT \
+    -v ~/.config/gcloud:/config \
+    $CLOUDSQL_PROXY_IMAGE /cloud_sql_proxy \
+    -instances=${DATABASE_CONNECTION_STRING}=tcp:0.0.0.0:$DATABASE_PORT \
+    -credential_file=/config/application_default_credentials.json > /dev/null 2>&1
+
+  CONTAINER_NAME=$(get_container_name $DATABASE_PORT)
+
+  if [ $CONTAINER_NAME == "" ]; then
+    echo "Failed to start the Cloud SQL Proxy. Check the docker logs for the most recently started container"
+    exit $CLOUDSQL_PROXY_NEVER_STARTED_ERROR_EXIT_CODE
+  else
+    echo "Started Cloud SQL Proxy container with name: ${CONTAINER_NAME}"
+  fi
+
+  # Wait for Postgres to be ready before continuing
+  wait_for_postgres $DATABASE_HOST $DATABASE_PORT
+}
+
+# Script body
+check_docker_running
+
+DATABASE_CONNECTION_STRING=''
+DATABASE_HOST=$CLOUDSQL_PROXY_HOST
+DATABASE_PORT=''
+DRY_RUN=false
+CONTROL=START
+
+while getopts "dqvc:p:" flag; do
+  case "${flag}" in
+    d) DRY_RUN=true ;;
+    c) DATABASE_CONNECTION_STRING="$OPTARG" ;;
+    p) DATABASE_PORT="$OPTARG" ;;
+    q) CONTROL=QUIT ;;
+    v) CONTROL=VERIFY ;;
+    *) print_usage
+  esac
+done
+
+if [ "$DATABASE_PORT" == "" ]; then
+  echo_error "Missing required argument -p"
+  print_usage
 fi
 
-echo "Starting Cloud SQL Proxy container for ${DATABASE_CONNECTION_STRING}..."
-
-
-# The docker container runs with ~/.config/gcloud mounted as a volume to /config
-# It assumes the existence of application_default_credentials.json
-# Run `gcloud auth application-default login` if you are running into authentication errors
-docker run -d \
-  -p 127.0.0.1:$DATABASE_PORT:$DATABASE_PORT \
-  -v ~/.config/gcloud:/config \
-  $CLOUD_SQL_PROXY_IMAGE /cloud_sql_proxy \
-  -instances=${DATABASE_CONNECTION_STRING}=tcp:0.0.0.0:$DATABASE_PORT \
-  -credential_file=/config/application_default_credentials.json > /dev/null 2>&1
-
-CONTAINER_NAME=$(get_container_name $DATABASE_PORT)
-if [ $CONTAINER_NAME == "" ]; then
-  echo "Failed to start the Cloud SQL Proxy. Check the docker logs for the most recently started container"
-else
-  echo "Started Cloud SQL Proxy container with name: ${CONTAINER_NAME}"
-fi
+case $CONTROL in
+  QUIT) control_quit ;;
+  VERIFY) control_verify ;;
+  START) control_start ;;
+  **) print_usage ;;
+esac
