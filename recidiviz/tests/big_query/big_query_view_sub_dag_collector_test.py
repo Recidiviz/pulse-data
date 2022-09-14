6ac9279ee1a2d9666e8068292e4f2bdc8d483213
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2022-09-14 11:44:56 -0700

    [BigQuery] Update load_views_to_sandbox to allow for specifying specific views and datasets at the same time (Recidiviz/recidiviz-data#15202)
    Refactors the `load_views_to_sandbox` script logic so that there is a core internal function that takes in a `BigQueryViewCollector` function that produces the view builders we actually want to load into the sandbox.
    
    Created two implementations of builder collectors:
    * `BigQueryViewSubDagCollector` - collects only view builders connected to a collection of views or the views in a collection of datasets. Updated the collection logic to allow for both specific views AND datasets to be specified, because there is no good reason why that shouldn't be allowed and I heard @justkunz hacks the script to do this sometimes. Also added tests for this logic for the first time!
    * `_AllButSomeBigQueryViewsCollector` - Returns all deployed views but allows you to filter out views in specific datasets. This is only used to filter out the dataflow materialized views datasets. It's possible that we won't have to do that anymore once @elchao96 is done reducing the size of the dataflow datasets 😁
    
    As a follow-up step, will add a third collector for automatically detecting views changed on the current branch.
    
    Part of Recidiviz/recidiviz-data#6722.
    
    GitOrigin-RevId: b848473c88917f9fa1acd3730ec69d3ad7de1ba7

---
+++ recidiviz/tests/big_query/big_query_view_sub_dag_collector_test.py
@@ -0,0 +1,147 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for BigQueryViewSubDagCollector"""
+import unittest
+from unittest.mock import Mock, patch
+
+from recidiviz.big_query.big_query_view_sub_dag_collector import (
+    BigQueryViewSubDagCollector,
+)
+from recidiviz.tests.big_query.big_query_view_dag_walker_test import (
+    DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST,
+)
+
+
+@patch("recidiviz.utils.metadata.project_id", Mock(return_value="recidiviz-456"))
+class BigQueryViewSubDagCollectorTest(unittest.TestCase):
+    """Tests for BigQueryViewSubDagCollector"""
+
+    def test_single_view_subdag(self) -> None:
+        collector = BigQueryViewSubDagCollector(
+            view_builders_in_full_dag=DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST,
+            view_addresses_in_sub_dag={
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[0].address
+            },
+            dataset_ids_in_sub_dag=None,
+            include_ancestors=False,
+            include_descendants=False,
+            datasets_to_exclude=set(),
+        )
+
+        self.assertCountEqual(
+            [DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[0]],
+            collector.collect_view_builders(),
+        )
+
+    def test_single_dataset_in_subdag(self) -> None:
+        collector = BigQueryViewSubDagCollector(
+            view_builders_in_full_dag=DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST,
+            view_addresses_in_sub_dag=None,
+            dataset_ids_in_sub_dag={
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[3].dataset_id
+            },
+            include_ancestors=False,
+            include_descendants=False,
+            datasets_to_exclude=set(),
+        )
+
+        self.assertCountEqual(
+            [DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[3]],
+            collector.collect_view_builders(),
+        )
+
+    def test_dataset_and_address_specified(self) -> None:
+        collector = BigQueryViewSubDagCollector(
+            view_builders_in_full_dag=DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST,
+            view_addresses_in_sub_dag={
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[0].address
+            },
+            dataset_ids_in_sub_dag={
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[3].dataset_id
+            },
+            include_ancestors=False,
+            include_descendants=False,
+            datasets_to_exclude=set(),
+        )
+
+        self.assertCountEqual(
+            [
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[0],
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[3],
+            ],
+            collector.collect_view_builders(),
+        )
+
+    def test_include_ancestors(self) -> None:
+        collector = BigQueryViewSubDagCollector(
+            view_builders_in_full_dag=DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST,
+            view_addresses_in_sub_dag={
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[2].address
+            },
+            dataset_ids_in_sub_dag=None,
+            include_ancestors=True,
+            include_descendants=False,
+            datasets_to_exclude=set(),
+        )
+
+        self.assertCountEqual(
+            [
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[0],
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[1],
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[2],
+            ],
+            collector.collect_view_builders(),
+        )
+
+    def test_include_descendants(self) -> None:
+        collector = BigQueryViewSubDagCollector(
+            view_builders_in_full_dag=DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST,
+            view_addresses_in_sub_dag=None,
+            dataset_ids_in_sub_dag={
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[2].dataset_id
+            },
+            include_ancestors=False,
+            include_descendants=True,
+            datasets_to_exclude=set(),
+        )
+
+        self.assertCountEqual(
+            [
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[2],
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[3],
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[4],
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[5],
+            ],
+            collector.collect_view_builders(),
+        )
+
+    def test_include_ancestors_and_descendants(self) -> None:
+        collector = BigQueryViewSubDagCollector(
+            view_builders_in_full_dag=DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST,
+            view_addresses_in_sub_dag=None,
+            dataset_ids_in_sub_dag={
+                DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST[2].dataset_id
+            },
+            include_ancestors=True,
+            include_descendants=True,
+            datasets_to_exclude=set(),
+        )
+
+        self.assertCountEqual(
+            DIAMOND_SHAPED_DAG_VIEW_BUILDERS_LIST,
+            collector.collect_view_builders(),
+        )
