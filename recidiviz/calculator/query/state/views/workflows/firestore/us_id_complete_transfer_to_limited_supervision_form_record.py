362b2e92690b8fa51aa627422f173e97a21714b3
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2022-09-21 14:14:29 -0700

    [US_ID][Workflows] Add LSU form (Recidiviz/recidiviz-data#15335)
    ## Description of the change
    
    - Adds additional requested info to LSU form
    - change record name for ED
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#15331
    Closes Recidiviz/recidiviz-data#15394
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: b8047a4c562d436e9337e21243d8a9428bf5f536

---
+++ recidiviz/calculator/query/state/views/workflows/firestore/us_id_complete_transfer_to_limited_supervision_form_record.py
@@ -0,0 +1,341 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query for information needed to autopopulate the transfer chrono as well as
+for relevant case notes needed to determine eligibility for transfer to limited unit
+supervision in Idaho
+"""
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.pipeline.supplemental.dataset_config import (
+    SUPPLEMENTAL_DATA_DATASET,
+)
+from recidiviz.calculator.query.bq_utils import nonnull_end_date_clause
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.calculator.query.state.dataset_config import (
+    NORMALIZED_STATE_DATASET,
+    SESSIONS_DATASET,
+)
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.raw_data.dataset_config import (
+    raw_latest_views_dataset_for_region,
+)
+from recidiviz.task_eligibility.dataset_config import (
+    task_eligibility_spans_state_specific_dataset,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+COMPLETE_TRANSFER_TO_LIMITED_SUPERVISION_FORM_US_ID_RECORD_VIEW_NAME = (
+    "us_id_complete_transfer_to_limited_supervision_form_record"
+)
+
+COMPLETE_TRANSFER_TO_LIMITED_SUPERVISION_FORM_US_ID_RECORD_DESCRIPTION = """
+    View of the limited supervision unit record for transfer chrono completion and 
+    for determining eligibility for transfer to limited unit supervision in Idaho
+    for individuals that may be eligible 
+    """
+COMPLETE_TRANSFER_TO_LIMITED_SUPERVISION_FORM_US_ID_RECORD_QUERY_TEMPLATE = f"""
+ /*{{description}}*/
+ WITH sentence_charge_description AS (
+      SELECT
+        sent.state_code,
+        sent.person_id,
+        sent.date_imposed,
+        sent.projected_completion_date_max,
+        sent.description, 
+      FROM `{{project_id}}.{{sessions_dataset}}.sentence_spans_materialized`,
+        UNNEST (sentences_preprocessed_id_array) sentences_preprocessed_id
+      INNER JOIN `{{project_id}}.{{sessions_dataset}}.sentences_preprocessed_materialized` sent
+        USING (state_code, person_id, sentences_preprocessed_id)
+      WHERE state_code = "US_ID"
+      AND CURRENT_DATE BETWEEN start_date AND COALESCE(end_date, "9999-12-31")
+    ),
+    person_info AS (
+      SELECT
+        spi.person_id,
+        spi.state_code,
+        spi.current_address,
+        spi.current_email_address,
+        spi.current_phone_number
+      FROM `{{project_id}}.{{normalized_state_dataset}}.state_person` spi
+      WHERE state_code = 'US_ID'
+    ),
+    latest_assessment_score AS (
+      SELECT
+        person_id,
+        state_code,
+        assessment_date,
+        assessment_score,
+      FROM `{{project_id}}.{{sessions_dataset}}.assessment_score_sessions_materialized` score
+      WHERE assessment_type = 'LSIR'
+        AND score_end_date IS NULL
+        AND state_code = 'US_ID'
+      QUALIFY ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY assessment_date DESC)=1
+    ),
+    latest_employment_info AS (
+      SELECT
+        pei.person_id,
+        'US_ID' AS state_code,
+        emp.name,
+        CONCAT(emp.line1,', ', emp.city, ', ', emp.zipcode) AS employer_address,
+        SAFE_CAST(startdate AS DATETIME) AS start_date,
+        SAFE_CAST(verifydate AS DATETIME) AS verify_date,
+      FROM `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.cis_employment_latest` e
+      INNER JOIN `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.cis_offender_latest` o
+        ON e.personemploymentid = o.id
+      INNER JOIN `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` pei
+        ON o.offendernumber = pei.external_id
+        AND pei.id_type = "US_ID_DOC"
+        AND pei.state_code = 'US_ID'
+      LEFT JOIN `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.cis_employer_latest` emp
+        ON e.employerid = emp.id
+      QUALIFY ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY verifydate DESC)=1
+    ),
+    latest_drug_screen AS (
+      SELECT
+        person_id,
+        state_code,
+        drug_screen_date,
+        is_positive_result,
+      FROM `{{project_id}}.{{sessions_dataset}}.drug_screens_preprocessed_materialized` 
+        WHERE sample_type = "URINE"
+        AND state_code = 'US_ID'
+      QUALIFY ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY drug_screen_date DESC)=1
+    ),
+    latest_ncic_ilets_check AS(
+      SELECT 
+        a.state_code,
+        a.person_id,
+        a.create_dt AS review_date,
+        cn.agnt_note_title AS note_title, 
+        cn.agnt_note_txt AS note_body, 
+      FROM `{{project_id}}.{{supplemental_dataset}}.us_id_case_note_matched_entities` a
+      LEFT JOIN `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.agnt_case_updt_latest` cn
+        USING(agnt_case_updt_id)
+      WHERE ncic_ilets_nco_check
+      QUALIFY ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY review_date DESC)=1
+    ),
+    latest_treatment_discharge AS(
+      SELECT 
+        p.state_code,
+        p.person_id,
+        p.discharge_date,
+        cn.agnt_note_title AS note_title,
+        cn.agnt_note_txt AS note_body,
+      FROM `{{project_id}}.{{normalized_state_dataset}}.state_program_assignment` p
+      LEFT JOIN `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.agnt_case_updt_latest` cn
+          ON cn.agnt_case_updt_id = SPLIT((p.external_id), '-')[SAFE_OFFSET(1)]
+      --only select treatment completion records
+      WHERE p.discharge_date IS NOT NULL
+      QUALIFY ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY discharge_date DESC)=1
+    ),
+   notes AS(
+    --violations
+      SELECT 
+        v.external_id,
+        v.state_code,
+        v.person_id,
+        COALESCE(vr.response_date,v.violation_date) AS event_date,
+        COALESCE(vt.violation_type, cn.agnt_note_title) AS note_title,
+        IF(cn.agnt_note_txt IS NULL, "--",cn.agnt_note_txt) AS note_body,
+        "Violations" AS criteria,
+      FROM `{{project_id}}.{{normalized_state_dataset}}.state_supervision_violation` v
+      LEFT JOIN `{{project_id}}.{{normalized_state_dataset}}.state_supervision_violation_type_entry` vt
+        USING (supervision_violation_id, person_id, state_code)
+      LEFT JOIN `{{project_id}}.{{normalized_state_dataset}}.state_supervision_violation_response` vr
+        USING (supervision_violation_id, person_id, state_code)
+      LEFT JOIN `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.agnt_case_updt_latest` cn
+        ON cn.agnt_case_updt_id = SPLIT((v.external_id), '-')[SAFE_OFFSET(1)]
+      WHERE (violation_type NOT IN ('TECHNICAL') OR violation_type IS NULL)
+        AND DATE_ADD(COALESCE(vr.response_date,v.violation_date), INTERVAL 90 DAY) >= CURRENT_DATE('US/Pacific')
+    --treatment 
+      UNION ALL 
+      SELECT 
+        p.external_id,
+        p.state_code,
+        p.person_id,
+        GREATEST(COALESCE(p.discharge_date, p.start_date)) AS event_date,
+        cn.agnt_note_title AS note_title,
+        cn.agnt_note_txt AS note_body,
+        "Treatment" AS criteria
+      FROM `{{project_id}}.{{normalized_state_dataset}}.state_program_assignment` p
+      LEFT JOIN `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.agnt_case_updt_latest` cn
+        ON cn.agnt_case_updt_id = SPLIT((p.external_id), '-')[SAFE_OFFSET(1)]
+    --ncic/ilets and new crime violations
+      UNION ALL
+      SELECT 
+        a.agnt_case_updt_id AS external_id,
+        a.state_code,
+        a.person_id,
+        a.create_dt AS event_date,
+        cn.agnt_note_title AS note_title, 
+        cn.agnt_note_txt AS note_body, 
+        CASE 
+            WHEN ncic_ilets_nco_check THEN "No new criminal activity check"
+            WHEN new_crime THEN "Violations"
+            ELSE NULL
+        END AS criteria
+      FROM `{{project_id}}.{{supplemental_dataset}}.us_id_case_note_matched_entities` a
+      LEFT JOIN `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.agnt_case_updt_latest` cn
+        USING(agnt_case_updt_id)
+      WHERE (ncic_ilets_nco_check OR new_crime)
+      --only select ncic checks and new crime within the past 90 days
+        AND DATE_ADD(a.create_dt, INTERVAL 90 DAY) >= CURRENT_DATE('US/Pacific')
+    --Community service, Interlock, Special Conditions 
+      UNION ALL
+      SELECT 
+        a.agnt_case_updt_id AS external_id,
+        a.state_code,
+        a.person_id,
+        a.create_dt AS event_date,
+        cn.agnt_note_title AS note_title, 
+        cn.agnt_note_txt AS note_body,
+        CASE 
+            WHEN community_service THEN "Community service"
+            WHEN case_plan THEN "Special Conditions"
+            WHEN interlock THEN "Interlock"
+            ELSE NULL
+        END AS criteria
+      FROM `{{project_id}}.{{supplemental_dataset}}.us_id_case_note_matched_entities` a
+      LEFT JOIN `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.agnt_case_updt_latest` cn
+        USING(agnt_case_updt_id)
+      WHERE community_service
+        OR case_plan
+        OR interlock
+    --DUI notes
+      UNION ALL
+      SELECT 
+        a.agnt_case_updt_id AS external_id,
+        a.state_code,
+        a.person_id,
+        a.create_dt AS event_date,
+        cn.agnt_note_title AS note_title, 
+        cn.agnt_note_txt AS note_body,
+        "DUI" AS criteria
+      FROM `{{project_id}}.{{supplemental_dataset}}.us_id_case_note_matched_entities` a
+      LEFT JOIN `{{project_id}}.{{us_id_raw_data_up_to_date_dataset}}.agnt_case_updt_latest` cn
+        USING(agnt_case_updt_id)
+      WHERE dui 
+        AND NOT not_m_dui
+         --only include DUI notes within the past 12 months 
+        AND DATE_ADD(a.create_dt, INTERVAL 12 MONTH) >= CURRENT_DATE('US/Pacific')
+    ),
+    form AS (
+      SELECT
+          pei.external_id,
+          tes.state_code,
+          tes.start_date AS eligible_start_date,
+          ses.start_date AS supervision_start_date,
+          DATE_DIFF(proj.projected_completion_date_max, CURRENT_DATE('US/Pacific'), DAY) AS days_remaining_on_supervision,
+          --aggregate all relevant charge descriptions 
+          ARRAY_AGG(charge.description IGNORE NULLS) AS form_information_charge_descriptions,
+          pi.current_address AS form_information_current_address,
+          pi.current_phone_number AS form_information_current_phone_number,
+          pi.current_email_address AS form_information_email_address,
+          score.assessment_date AS form_information_assessment_date,
+          score.assessment_score AS form_information_assessment_score,
+          ei.name AS form_information_employer_name,
+          ei.employer_address AS form_information_employer_address,
+          ei.start_date AS form_information_employment_start_date,
+          ei.verify_date AS form_information_employment_date_verified,
+          ds.drug_screen_date AS form_information_drug_screen_date,
+          ds.is_positive_result AS form_information_drug_screen_result,
+          ncic.review_date AS form_information_ncic_review_date,
+          ncic.note_title AS form_information_ncic_note_title,
+          ncic.note_body AS form_information_ncic_note_body,
+          tx.discharge_date AS form_information_tx_discharge_date,
+          tx.note_title AS form_information_tx_note_title,
+          tx.note_body AS form_information_tx_note_body,
+          ARRAY_AGG(tes.reasons)[ORDINAL(1)] AS reasons,
+          TO_JSON(ARRAY_AGG(IF(n.note_title IS NOT NULL, STRUCT(n.note_title, n.note_body, n.event_date, n.criteria),NULL) IGNORE NULLS)) AS case_notes,
+      FROM `{{project_id}}.{{task_eligibility_dataset}}.complete_transfer_to_limited_supervision_form_materialized` tes
+      INNER JOIN `{{project_id}}.{{sessions_dataset}}.compartment_sessions_materialized` ses
+        ON tes.state_code = ses.state_code
+        AND tes.person_id = ses.person_id 
+        AND tes.start_date BETWEEN ses.start_date AND {nonnull_end_date_clause('ses.end_date')}
+        AND tes.state_code = 'US_ID'
+      INNER JOIN `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` pei
+        ON tes.state_code = pei.state_code 
+        AND tes.person_id = pei.person_id
+        AND pei.id_type = "US_ID_DOC"
+      LEFT JOIN `{{project_id}}.{{sessions_dataset}}.supervision_projected_completion_date_spans_materialized` proj
+        ON proj.state_code = ses.state_code
+        AND proj.person_id = ses.person_id
+        --use the projected completion date from the current span
+        AND CURRENT_DATE('US/Pacific') BETWEEN proj.start_date AND proj.end_date
+      LEFT JOIN sentence_charge_description charge
+        ON tes.state_code = charge.state_code
+        AND tes.person_id = charge.person_id
+        --select charges that were imposed before the relevant supervision session and that 
+        --expire after the eligible period has started
+        AND ses.start_date >= charge.date_imposed 
+        AND tes.start_date <= COALESCE(charge.projected_completion_date_max, "9999-12-31")
+      LEFT JOIN person_info pi
+        ON tes.state_code = pi.state_code
+        AND tes.person_id = pi.person_id
+      LEFT JOIN latest_assessment_score score
+        ON tes.state_code = score.state_code
+        AND tes.person_id = score.person_id
+        --join all latest? or only latest when assessment_score_date <= ses.start_date
+      LEFT JOIN latest_employment_info ei
+        ON tes.state_code = ei.state_code
+        AND tes.person_id = ei.person_id
+        AND ei.start_date >= ses.start_date
+      LEFT JOIN latest_drug_screen ds
+        ON tes.state_code = ds.state_code
+        AND tes.person_id = ds.person_id
+        --join all latest? or only latest when drug_screen_date <= ses.start_date
+      LEFT JOIN latest_ncic_ilets_check ncic
+        ON tes.state_code = ncic.state_code
+        AND tes.person_id = ncic.person_id
+        AND DATE_ADD(review_date, INTERVAL 90 DAY) >= CURRENT_DATE('US/Pacific')
+      LEFT JOIN latest_treatment_discharge tx
+        ON tes.state_code = tx.state_code
+        AND tes.person_id = tx.person_id
+        --select only treatment discharges that occured within the last supervision session
+        AND tx.discharge_date BETWEEN ses.start_date AND (COALESCE(ses.end_date, "9999-12-31"))
+      LEFT JOIN notes n
+        ON tes.state_code = n.state_code
+        AND tes.person_id = n.person_id
+        --select notes that occur within that supervision session
+        AND n.event_date >= ses.start_date
+      WHERE CURRENT_DATE('US/Pacific') BETWEEN tes.start_date AND {nonnull_end_date_clause('tes.end_date')}
+        AND tes.is_eligible
+        AND tes.state_code = 'US_ID'
+      GROUP BY 1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
+     )
+     SELECT * from form 
+     ORDER BY external_id
+"""
+
+COMPLETE_TRANSFER_TO_LIMITED_SUPERVISION_FORM_US_ID_RECORD_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=dataset_config.WORKFLOWS_VIEWS_DATASET,
+    view_id=COMPLETE_TRANSFER_TO_LIMITED_SUPERVISION_FORM_US_ID_RECORD_VIEW_NAME,
+    view_query_template=COMPLETE_TRANSFER_TO_LIMITED_SUPERVISION_FORM_US_ID_RECORD_QUERY_TEMPLATE,
+    description=COMPLETE_TRANSFER_TO_LIMITED_SUPERVISION_FORM_US_ID_RECORD_DESCRIPTION,
+    normalized_state_dataset=NORMALIZED_STATE_DATASET,
+    supplemental_dataset=SUPPLEMENTAL_DATA_DATASET,
+    sessions_dataset=SESSIONS_DATASET,
+    task_eligibility_dataset=task_eligibility_spans_state_specific_dataset(
+        StateCode.US_ID
+    ),
+    should_materialize=True,
+    us_id_raw_data_up_to_date_dataset=raw_latest_views_dataset_for_region("us_id"),
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        COMPLETE_TRANSFER_TO_LIMITED_SUPERVISION_FORM_US_ID_RECORD_VIEW_BUILDER.build_and_print()
