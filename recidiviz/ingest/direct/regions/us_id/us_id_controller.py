b1c7e3005c262c8a0dcb5aa804329e5de84d330e
Kris Gerig <kris@recidiviz.org>
2022-09-21 14:38:51 -0500

    [US_ID] Update mappings for SupervisionLevel (Recidiviz/recidiviz-data#15505)
    ## Description of the change
    
    On 9/21/22 we saw `PROBATION VIOLATOR` show up in Idaho raw data in the
    field we normally map to `supervision_level`. This PR maps `PROBATION
    VIOLATOR` to `INTERNAL_UNKNOWN` when it appears in this field.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#15502
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: e9038bc28b5d83d184f7912a3cdfe4ba9603d613

--- recidiviz/ingest/direct/regions/us_id/us_id_controller.py
+++ recidiviz/ingest/direct/regions/us_id/us_id_controller.py
@@ -352,6 +352,7 @@ class UsIdController(BaseDirectIngestController, LegacyIngestViewProcessorDelega
             "D7 DUI COURT",
             "EXPANDED CRC",
             "BENCH WARRANT",  # SupervisionType, not SupervisionLevel
+            "PROBATION VIOLATOR",  # SupervisionType, not SupervisionLevel
             "ADMINISTRATIVE",  # Used for a non-standardized set of situations.
             "GOLD SEAL PENDING",  # Pending supervision termination (after parole board has approved termination)
             # No longer under IDOC authority
