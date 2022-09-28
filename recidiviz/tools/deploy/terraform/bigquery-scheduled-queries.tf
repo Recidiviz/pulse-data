7dd8bc2f72912321a488270b2bf2beb8310d7e6f
Colin Adams <colin@recidiviz.org>
2022-09-28 16:05:14 -0700

    Fix permissions for terraform scheduled queries (Recidiviz/recidiviz-data#15683)
    ## Description of the change
    
    We need a service account to run the scheduled queries. This creates a
    service account and gives it BQ admin permissions.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    N/A
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 007232ae1abd093569882c0f9d2ff1a44fddd9ee

--- recidiviz/tools/deploy/terraform/bigquery-scheduled-queries.tf
+++ recidiviz/tools/deploy/terraform/bigquery-scheduled-queries.tf
@@ -15,13 +15,48 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 
-# Materializes tables that are too slow to materialize as part of our regular flow.
+# This sets up scheduled queries to materialize tables that are too slow to materialize
+# as part of our regular flow.
+
+# Allow us to access the project number
+data "google_project" "project" {}
+
+# Enable the data transfer service
+resource "google_project_service" "data_transfer_service" {
+  project                    = var.project_id
+  service                    = "bigquerydatatransfer.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+
+# Create a service account with the proper permissions
+resource "google_service_account" "bigquery_scheduled_queries" {
+  account_id   = "bigquery-scheduled-queries"
+  display_name = "BigQuery Scheduled Queries Service Account"
+  description  = "Account that runs BigQuery scheduled queries."
+}
+
+resource "google_project_iam_member" "bigquery_scheduled_queries_permissions" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountShortTermTokenMinter"
+  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-bigquerydatatransfer.iam.gserviceaccount.com"
+}
+resource "google_project_iam_binding" "bigquery_datatransfer_admin" {
+  project = var.project_id
+  role    = "roles/bigquery.admin"
+  members = ["serviceAccount:${google_service_account.bigquery_scheduled_queries.email}"]
+}
+
+# Create a dataset
 module "unmanaged_views_dataset" {
   source      = "./modules/big_query_dataset"
   dataset_id  = "unmanaged_views"
   description = "This dataset contains materialized versions of views that are managed by scheduled queries instead of our typical view materialization."
+
+  depends_on = [google_project_iam_member.bigquery_scheduled_queries_permissions]
 }
 
+# Actually create the scheduled queries
 resource "google_bigquery_data_transfer_config" "supervision_district_metrics_materialized" {
   display_name           = "supervision_district_metrics_materialized"
   location               = "US"
@@ -33,6 +68,8 @@ resource "google_bigquery_data_transfer_config" "supervision_district_metrics_ma
     write_disposition               = "WRITE_TRUNCATE"
     query                           = "SELECT * FROM `${var.project_id}.analyst_data.supervision_district_metrics"
   }
+
+  depends_on = [google_project_iam_member.bigquery_scheduled_queries_permissions]
 }
 
 resource "google_bigquery_data_transfer_config" "supervision_office_metrics_materialized" {
@@ -46,6 +83,8 @@ resource "google_bigquery_data_transfer_config" "supervision_office_metrics_mate
     write_disposition               = "WRITE_TRUNCATE"
     query                           = "SELECT * FROM `${var.project_id}.analyst_data.supervision_office_metrics"
   }
+
+  depends_on = [google_project_iam_member.bigquery_scheduled_queries_permissions]
 }
 
 resource "google_bigquery_data_transfer_config" "supervision_officer_metrics_materialized" {
@@ -59,6 +98,8 @@ resource "google_bigquery_data_transfer_config" "supervision_officer_metrics_mat
     write_disposition               = "WRITE_TRUNCATE"
     query                           = "SELECT * FROM `${var.project_id}.analyst_data.supervision_officer_metrics"
   }
+
+  depends_on = [google_project_iam_member.bigquery_scheduled_queries_permissions]
 }
 
 resource "google_bigquery_data_transfer_config" "supervision_state_metrics_materialized" {
@@ -72,4 +113,6 @@ resource "google_bigquery_data_transfer_config" "supervision_state_metrics_mater
     write_disposition               = "WRITE_TRUNCATE"
     query                           = "SELECT * FROM `${var.project_id}.analyst_data.supervision_state_metrics"
   }
+
+  depends_on = [google_project_iam_member.bigquery_scheduled_queries_permissions]
 }
