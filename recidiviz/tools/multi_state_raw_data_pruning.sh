0fd26a8bab41903532e331181635d20a8c12901c
caroletouma <carole@recidiviz.org>
2022-10-11 16:00:45 -0400

    Add bash script that runs raw data pruning for multiple states/projects (Recidiviz/recidiviz-data#15946)
    ## Description of the change
    
    Add script that iterates through raw data pruning script to enable
    multi-state usage. This will first do a dry-run, and then will prompt
    for confirmation that dry-run output looks good before proceeding with
    actual raw data pruning. This will conduct raw data pruning in both
    _staging_ and _prod._
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 8ec2f7d9e16ff1d24228a1463bfa38f62cc7c180

---
+++ recidiviz/tools/multi_state_raw_data_pruning.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#  Usage: bash recidiviz/tools/multi_state_raw_data_pruning.sh
+
+declare -a states=("US_TN" "US_MI" "US_ID" "US_ND")
+
+echo "Running DRY-RUN raw data pruning script in STAGING for the following states: ${states[*]}."
+echo "Output can be found in prune-staging.txt."
+for state in "${states[@]}"
+do
+  echo "Starting DRY-RUN in STAGING for $state"
+  python -m recidiviz.tools.ingest.one_offs.clear_redundant_raw_data_on_bq --dry-run True --project-id=recidiviz-staging --state-code="$state" >> prune-staging.txt
+done
+
+echo "Running DRY-RUN raw data pruning script in PROD for the following states: ${states[*]}."
+echo "Output can be found in prune-prod.txt."
+for state in "${states[@]}"
+do
+  echo "Starting DRY-RUN in PROD for $state"
+  python -m recidiviz.tools.ingest.one_offs.clear_redundant_raw_data_on_bq --dry-run True --project-id=recidiviz-123 --state-code="$state" >> prune-prod.txt
+done
+
+read -p "Look at prune-staging.txt and prune-prod.txt and confirm that dry-run output look correct. Are you ready to proceed with raw data pruning? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    for state in "${states[@]}"
+    do
+      echo "Starting raw data pruning in STAGING for $state"
+      python -m recidiviz.tools.ingest.one_offs.clear_redundant_raw_data_on_bq --dry-run False --project-id=recidiviz-staging --state-code="$state"
+      echo "Starting raw data pruning in PROD for $state"
+      python -m recidiviz.tools.ingest.one_offs.clear_redundant_raw_data_on_bq --dry-run False --project-id=recidiviz-123 --state-code="$state"
+    done
+fi
+
+echo "Deleting temporary output files..."
+rm prune-prod.txt
+rm prune-staging.txt
+
+echo "Done with raw data pruning script for the following states: ${states[*]}."
