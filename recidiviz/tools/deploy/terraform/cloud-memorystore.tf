5f903d7ebc15403772199695260fb2aaae703453
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2022-10-26 10:13:12 -0700

    [Terraform] add infra to allow case triage to make external requests from a static IP (Recidiviz/recidiviz-data#16282)
    GitOrigin-RevId: 684bb2574610ed34d6524a9939d05ff972ac154c

--- recidiviz/tools/deploy/terraform/cloud-memorystore.tf
+++ recidiviz/tools/deploy/terraform/cloud-memorystore.tf
@@ -15,10 +15,6 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 
-data "google_secret_manager_secret_version" "vpc_access_connector_us_central_cidr" { secret = "vpc_access_connector_us_central_cidr" }
-
-data "google_secret_manager_secret_version" "vpc_access_connector_us_east_cidr" { secret = "vpc_access_connector_us_east_cidr" }
-
 resource "google_redis_instance" "case_triage_rate_limiter_cache" {
   name           = "rate-limit-cache"
   region         = var.app_engine_region
@@ -26,7 +22,6 @@ resource "google_redis_instance" "case_triage_rate_limiter_cache" {
   tier           = "BASIC"
   redis_version  = "REDIS_5_0"
 }
-
 
 resource "google_redis_instance" "case_triage_sessions_cache" {
   name           = "case-triage-sessions-cache"
@@ -42,31 +37,6 @@ resource "google_redis_instance" "pathways_metric_cache" {
   memory_size_gb = 1
   tier           = "BASIC"
   redis_version  = "REDIS_5_0"
-}
-
-resource "google_project_service" "vpc_access_connector" {
-  service = "vpcaccess.googleapis.com"
-
-  disable_dependent_services = true
-  disable_on_destroy         = true
-}
-
-# VPC Connector is required for app engine to connect to Redis
-resource "google_vpc_access_connector" "redis_vpc_connector" {
-  name           = "redis-vpc-connector"
-  region         = var.app_engine_region
-  ip_cidr_range  = data.google_secret_manager_secret_version.vpc_access_connector_us_east_cidr.secret_data
-  network        = "default"
-  max_throughput = 1000
-}
-
-# VPC Connector is required for Cloud Run to connect to Redis
-resource "google_vpc_access_connector" "us_central_redis_vpc_connector" {
-  name           = "us-central-redis-vpc-ac" # Name has a 23 character limit
-  region         = var.region
-  ip_cidr_range  = data.google_secret_manager_secret_version.vpc_access_connector_us_central_cidr.secret_data
-  network        = "default"
-  max_throughput = 1000
 }
 
 
