4e57c71e0ecc33a87e586247c1b2b2f1447beb88
lilidworkin <99899591+lilidworkin@users.noreply.github.com>
2022-11-01 13:01:04 -0400

    [Justice Counts][1/n] Script to run Cloud Build Trigger (Recidiviz/recidiviz-data#16370)
    ## Description of the change
    
    I'm working on a deploy script for Justice Counts. One part of the
    script will be manually triggering our [Google Cloud Build
    trigger](https://console.cloud.google.com/cloud-build/triggers;region=global/edit/84614bd8-b45a-4788-af4f-950f6f6f526e?project=recidiviz-staging),
    which builds our Docker image.
    
    After some research, I learned that the best way to programmatically
    trigger a Google Cloud Build trigger was via this
    [google-cloud-build](https://cloud.google.com/python/docs/reference/cloudbuild/latest)
    library (and in particular,
    [run_build_trigger](https://cloud.google.com/python/docs/reference/cloudbuild/latest/google.cloud.devtools.cloudbuild_v1.services.cloud_build.CloudBuildClient#google_cloud_devtools_cloudbuild_v1_services_cloud_build_CloudBuildClient_run_build_trigger)).
    (While you can technically use this [alpha
    CLI](https://cloud.google.com/sdk/gcloud/reference/alpha/builds/triggers/run),
    it is alpha and therefore subject to change, and also doesn't support
    runtime variable substitutions.)
    
    Here is the script to do the manual trigger. A few things to note:
    * To simplify things, I have actually created two different Cloud Build
    triggers, one for our Publisher app and one for our Agency Dashboard
    app. They work almost identically (because both apps are actually built
    using the same
    [Dockerfile](https://github.com/Recidiviz/recidiviz-data/blob/main/Dockerfile.justice-counts)).
    The script decides which trigger to run based on a user provided
    `frontend_app` argument.
    * Our Dockerfile uses a build arg FRONTEND_URL to decide which version
    of the frontend code to pull into the image. Our Cloud Build Trigger
    therefore also takes a FRONTEND_URL argument, which it passes to the
    Docker build step. This URL can be any Github tarball. Github creates
    tarballs for both branches and tags, which slightly different URLs. This
    script formats the URL for the user based on whether they provide a
    `frontend_branch` or `frontend_tag` argument.
    * Cloud Build Triggers can be run from either a backend branch or tag,
    so this script allows either.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Contributes to Recidiviz/recidiviz-data#16325
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: e70f5bd756cf094983761435e325eb3ad7a7f502

---
+++ recidiviz/tools/deploy/justice_counts/run_cloud_build_trigger.py
@@ -0,0 +1,166 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Script to run one of two Justice Counts Cloud Build Triggers (either Build-JC-Publisher or Build-JC-Agency-Dashboard).
+Generally meant to be called from the Justice Counts deploy_to_staging script.
+You need to specify:
+- name of the JC app you're deploying (either Publisher or Agency Dashboard)
+- version of the backend code to use (either the name of a branch or tag)
+- version of the frontend code to use (either the name of a branch or tag)
+- subdirectory in which to place the built Docker image
+
+Example usage (run from `pipenv shell`):
+python -m recidiviz.tools.deploy.justice_counts.run_cloud_build_trigger \
+    --backend-branch main \
+    --frontend-branch main \
+    --frontend-app agency-dashboard \
+    --subdirectory justice-counts/agency-dashboard
+
+python -m recidiviz.tools.deploy.justice_counts.run_cloud_build_trigger \
+    --backend-tag jc.publisher.v1.0.0 \
+    --frontend-tag publisher.v1.0.0 \
+    --frontend-app publisher \
+    --subdirectory justice-counts/publisher
+"""
+import argparse
+import logging
+from typing import Optional
+
+# using a from import causes mypy errors
+# pylint: disable=consider-using-from-import
+import google.cloud.devtools.cloudbuild_v1 as cloudbuild_v1
+
+from recidiviz.utils.metadata import local_project_id_override
+
+PROJECT_ID = "recidiviz-staging"
+
+PUBLISHER_CLOUD_BUILD_TRIGGER = "Build-JC-Publisher"
+AGENCY_DASHBOARD_CLOUD_BUILD_TRIGGER = "Build-JC-Agency-Dashboard"
+
+
+def create_parser() -> argparse.ArgumentParser:
+    """Returns an argument parser for the script."""
+    parser = argparse.ArgumentParser(
+        description="Deploy the Justice Counts Publisher application to staging Cloud Run."
+    )
+
+    backend_group = parser.add_mutually_exclusive_group(required=True)
+    backend_group.add_argument(
+        "--backend-tag", help="Name of the backend tag to deploy."
+    )
+    backend_group.add_argument(
+        "--backend-branch", help="Name of the backend branch to deploy."
+    )
+
+    frontend_group = parser.add_mutually_exclusive_group(required=True)
+    frontend_group.add_argument(
+        "--frontend-tag", help="Name of the frontend tag to deploy."
+    )
+    frontend_group.add_argument(
+        "--frontend-branch", help="Name of the frontend branch to deploy."
+    )
+
+    parser.add_argument(
+        "--frontend-app",
+        help="Name of the frontend app to deploy. Must be either `publisher` or `agency_dashboard`.",
+        choices=["publisher", "agency-dashboard"],
+        required=True,
+    )
+    parser.add_argument(
+        "--subdirectory",
+        help="Name of the subdirectory in Container Registry in which to place the built image.",
+        required=True,
+    )
+    return parser
+
+
+def main(
+    backend_tag: Optional[str],
+    backend_branch: Optional[str],
+    frontend_tag: Optional[str],
+    frontend_branch: Optional[str],
+    frontend_app: str,
+    subdirectory: str,
+) -> None:
+    """Run the Justice Counts Build-JC-Docker-Image Cloud Build Trigger."""
+    if frontend_branch:
+        frontend_url = f"https://github.com/Recidiviz/justice-counts/archive/{frontend_branch}.tar.gz"
+    elif frontend_tag:
+        frontend_url = f"https://github.com/Recidiviz/justice-counts/archive/refs/tags/{frontend_tag}.tar.gz"
+    else:
+        raise ValueError(
+            "Must specify either the `frontend_branch` or `frontend_tag` argument."
+        )
+
+    substitutions = {
+        "_FRONTEND_URL": frontend_url,
+        "_SUBDIRECTORY": subdirectory,
+    }
+
+    if backend_branch:
+        repo_source = cloudbuild_v1.RepoSource(
+            branch_name=backend_branch, substitutions=substitutions
+        )
+    elif backend_tag:
+        repo_source = cloudbuild_v1.RepoSource(
+            tag_name=backend_tag, substitutions=substitutions
+        )
+    else:
+        raise ValueError(
+            "Must specify either the `backend_branch` or `backend_tag` argument."
+        )
+
+    if frontend_app == "publisher":
+        trigger_id = PUBLISHER_CLOUD_BUILD_TRIGGER
+    elif frontend_app == "agency-dashboard":
+        trigger_id = AGENCY_DASHBOARD_CLOUD_BUILD_TRIGGER
+    else:
+        raise ValueError(
+            "Invalid value for `frontend_app` -- must be either `publisher` or `agency-dashboard`."
+        )
+
+    request = cloudbuild_v1.RunBuildTriggerRequest(
+        project_id=PROJECT_ID,
+        trigger_id=trigger_id,
+        source=repo_source,
+    )
+
+    logging.info(
+        "Submitting request to Cloud Build Trigger %s in %s with the substitutions %s...",
+        trigger_id,
+        PROJECT_ID,
+        substitutions,
+    )
+    client = cloudbuild_v1.CloudBuildClient()
+    operation = client.run_build_trigger(request=request)
+    response = operation.result()
+    logging.info("Cloud Build Trigger job finished with status %s.", response.status)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+
+    args = create_parser().parse_args()
+    with local_project_id_override(PROJECT_ID):
+        main(
+            backend_tag=args.backend_tag,
+            backend_branch=args.backend_branch,
+            frontend_tag=args.frontend_tag,
+            frontend_branch=args.frontend_branch,
+            frontend_app=args.frontend_app,
+            subdirectory=args.subdirectory,
+        )
