d293665a4db96cecb791ca815d71e61799f2959a
Justine <justine@recidiviz.org>
2022-11-04 15:21:41 -0400

    Validate sentence date_imposed is hydrated (Recidiviz/recidiviz-data#16438)
    Add a validation to flag sentences that are missing a value for
    `date_imposed`
    
    GitOrigin-RevId: bfe2c68bcea04ffce3005354b480a02ac8d734fb

--- recidiviz/validation/configured_validations.py
+++ recidiviz/validation/configured_validations.py
@@ -223,6 +223,9 @@ from recidiviz.validation.views.state.sentence_type_by_district_by_demographics_
 from recidiviz.validation.views.state.sessions_validation.sentence_end_dates_before_start_dates import (
     SENTENCE_END_DATES_BEFORE_START_DATES_VIEW_BUILDER,
 )
+from recidiviz.validation.views.state.sessions_validation.sentences_missing_date_imposed import (
+    SENTENCES_MISSING_DATE_IMPOSED_VIEW_BUILDER,
+)
 from recidiviz.validation.views.state.sessions_validation.session_liberty_releases_with_no_sentence_completion_date import (
     SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_VIEW_BUILDER,
 )
@@ -483,6 +486,10 @@ def get_all_validations() -> List[DataValidationCheck]:
         ),
         ExistenceDataValidationCheck(
             view_builder=SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_VIEW_BUILDER,
+            validation_category=ValidationCategory.INVARIANT,
+        ),
+        ExistenceDataValidationCheck(
+            view_builder=SENTENCES_MISSING_DATE_IMPOSED_VIEW_BUILDER,
             validation_category=ValidationCategory.INVARIANT,
         ),
         SamenessDataValidationCheck(
