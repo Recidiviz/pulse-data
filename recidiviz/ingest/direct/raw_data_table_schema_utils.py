44a917503fbb9fb5cfab0336b137d7ea2eeb84d6
caroletouma <carole@recidiviz.org>
2022-11-14 16:05:13 -0500

    Refactor `update_raw_data_table_schema` so that the logic within can be called from an ingest endpoint (Recidiviz/recidiviz-data#16588)
    ## Description of the change
    
    Refactor `update_raw_data_table_schema` so that the logic within can be
    called from an ingest endpoint.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#16565
    Closes Recidiviz/recidiviz-data#16567
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 5df096ee721e0258d47772de848105bfd6551abd

---
+++ recidiviz/ingest/direct/raw_data_table_schema_utils.py
@@ -0,0 +1,100 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2022 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""Utilities to update raw data table schemas."""
+import logging
+from typing import List
+
+from google.cloud import bigquery
+
+from recidiviz.big_query.big_query_client import BigQueryClientImpl
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.raw_data.dataset_config import (
+    raw_tables_dataset_for_region,
+)
+from recidiviz.ingest.direct.raw_data.direct_ingest_raw_file_import_manager import (
+    DirectIngestRawFileImportManager,
+    get_region_raw_file_config,
+)
+from recidiviz.ingest.direct.types.direct_ingest_constants import (
+    FILE_ID_COL_NAME,
+    UPDATE_DATETIME_COL_NAME,
+)
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+
+
+def _update_raw_data_table_schema_to_new_schema(
+    state_code: StateCode,
+    instance: DirectIngestInstance,
+    raw_file_tag: str,
+    schema: List[bigquery.SchemaField],
+) -> None:
+    """Update the schema of a given raw data table to match the passed in `schema`."""
+    bq_client = BigQueryClientImpl()
+
+    raw_data_dataset_id = raw_tables_dataset_for_region(
+        state_code=state_code, instance=instance
+    )
+    raw_data_dataset_ref = bq_client.dataset_ref_for_id(raw_data_dataset_id)
+
+    try:
+        if bq_client.table_exists(raw_data_dataset_ref, raw_file_tag):
+            bq_client.update_schema(
+                raw_data_dataset_id, raw_file_tag, schema, allow_field_deletions=False
+            )
+        else:
+            bq_client.create_table_with_schema(
+                raw_data_dataset_id, raw_file_tag, schema
+            )
+    except Exception as e:
+        logging.exception(
+            "Failed to update schema for `%s.%s`", raw_data_dataset_id, raw_file_tag
+        )
+        raise ValueError(
+            f"Failed to update schema for `{raw_data_dataset_id}.{raw_file_tag}`."
+        ) from e
+
+
+def _get_raw_data_table_schema(
+    state_code: StateCode, raw_file_tag: str
+) -> bigquery.SchemaField:
+    """Retrieve the raw data BQ schema for a given raw file tag."""
+    region_config = get_region_raw_file_config(state_code.value.lower())
+    region_raw_file_config = region_config.raw_file_configs
+
+    raw_data_config = region_raw_file_config[raw_file_tag]
+    columns = [column.name for column in raw_data_config.columns] + [
+        FILE_ID_COL_NAME,
+        UPDATE_DATETIME_COL_NAME,
+    ]
+    schema = DirectIngestRawFileImportManager.create_raw_table_schema_from_columns(
+        columns
+    )
+
+    return schema
+
+
+def update_raw_data_table_schema(
+    state_code: StateCode, instance: DirectIngestInstance, raw_file_tag: str
+) -> None:
+    """Update the raw data table for a given state, instance, and raw file tag by obtaining the appropriate raw file
+    config updating the schema based on the columns defined in the YAML."""
+    _update_raw_data_table_schema_to_new_schema(
+        state_code=state_code,
+        instance=instance,
+        raw_file_tag=raw_file_tag,
+        schema=_get_raw_data_table_schema(state_code, raw_file_tag),
+    )
