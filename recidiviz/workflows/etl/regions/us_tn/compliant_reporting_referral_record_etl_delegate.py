041af066904aea9a5761c1fdaa1b6cc1fac97380
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2022-11-17 08:51:36 -0800

    [Workflows] clean up compliant reporting etl delegate (Recidiviz/recidiviz-data#16716)
    GitOrigin-RevId: b052f0d7ac58275ec959c07df60962f6c0cea758

--- recidiviz/workflows/etl/regions/us_tn/compliant_reporting_referral_record_etl_delegate.py
+++ recidiviz/workflows/etl/regions/us_tn/compliant_reporting_referral_record_etl_delegate.py
@@ -17,9 +17,10 @@
 """Delegate class to ETL compliant reporting referral records for workflows into Firestore."""
 import json
 import logging
-from typing import Optional, Tuple
+from typing import Any, Optional, Tuple
 
-from recidiviz.common.str_field_utils import parse_int
+from recidiviz.common.common_utils import convert_nested_dictionary_keys
+from recidiviz.common.str_field_utils import snake_to_camel
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 from recidiviz.workflows.etl.workflows_etl_delegate import (
@@ -40,168 +41,55 @@ class CompliantReportingReferralRecordETLDelegate(WorkflowsSingleStateETLDelegat
         if "compliant_reporting_eligible" not in data:
             return None, None
 
-        # First fill the non-nullable fields
-        new_document = {
-            "stateCode": data["state_code"],
-            "poFirstName": data["po_first_name"],
-            "poLastName": data["po_last_name"],
-            "clientFirstName": data["client_first_name"],
-            "clientLastName": data.get(
-                "client_last_name", ""
-            ),  # this has few enough null entries in the data that we shouldn't really treat it as optional
-            "dateToday": data["date_today"],
-            "tdocId": data["tdoc_id"],
-            "eligibilityCategory": data["compliant_reporting_eligible"],
-            "remainingCriteriaNeeded": parse_int(data["remaining_criteria_needed"]),
-            "currentOffenses": json.loads(data["current_offenses"]),
-            "pastOffenses": data.get("past_offenses"),
-            "lifetimeOffensesExpired": data.get("lifetime_offenses_expired"),
-            "judicialDistrict": data.get("judicial_district"),
-            "drugScreensPastYear": [
-                {
-                    "result": screen["ContactNoteType"],
-                    "date": screen["contact_date"],
-                }
-                for screen in data["drug_screens_past_year"]
-            ],
-            "sanctionsPastYear": data.get("sanctions_past_year"),
-            "finesFeesEligible": data["fines_fees_eligible"],
-            "supervisionType": data["supervision_type"],
-            "supervisionFeeAssessed": data.get("supervision_fee_assessed", ""),
-            "supervisionFeeArrearaged": data["supervision_fee_arrearaged"],
-            "supervisionFeeArrearagedAmount": data.get(
-                "supervision_fee_arrearaged_amount", ""
-            ),
-            "supervisionFeeExemptionType": json.loads(
-                data["supervision_fee_exemption_type"]
-            ),
-            "courtCostsPaid": data["court_costs_paid"],
-            "specialConditionsAlcDrugScreen": data[
-                "special_conditions_alc_drug_screen"
-            ],
-            "specialConditionsAlcDrugAssessmentComplete": data[
-                "special_conditions_alc_drug_assessment_complete"
-            ],
-            "specialConditionsAlcDrugTreatment": data[
-                "special_conditions_alc_drug_treatment"
-            ],
-            "specialConditionsAlcDrugTreatmentCurrent": data[
-                "special_conditions_alc_drug_treatment_current"
-            ],
-            "specialConditionsCounseling": data["special_conditions_counseling"],
-            "specialConditionsCounselingAngerManagementCurrent": data[
-                "special_conditions_counseling_anger_management_current"
-            ],
-            "specialConditionsCommunityService": data[
-                "special_conditions_community_service"
-            ],
-            "specialConditionsCommunityServiceCurrent": data[
-                "special_conditions_community_service_current"
-            ],
-            "specialConditionsProgramming": data["special_conditions_programming"],
-            "specialConditionsProgrammingCognitiveBehavior": data[
-                "special_conditions_programming_cognitive_behavior"
-            ],
-            "specialConditionsProgrammingCognitiveBehaviorCurrent": data[
-                "special_conditions_programming_cognitive_behavior_current"
-            ],
-            "specialConditionsProgrammingSafe": data[
-                "special_conditions_programming_safe"
-            ],
-            "specialConditionsProgrammingSafeCurrent": data[
-                "special_conditions_programming_safe_current"
-            ],
-            "specialConditionsProgrammingVictimImpact": data[
-                "special_conditions_programming_victim_impact"
-            ],
-            "specialConditionsProgrammingVictimImpactCurrent": data[
-                "special_conditions_programming_victim_impact_current"
-            ],
-            "specialConditionsProgrammingFsw": data[
-                "special_conditions_programming_fsw"
-            ],
-            "specialConditionsProgrammingFswCurrent": data[
-                "special_conditions_programming_fsw_current"
-            ],
-        }
-
-        # add nullable fields
-        if "physical_address" in data:
-            # Addresses arrive in the form: "123 Fake st., Metropolis, Tn 59545
-            # This fixes the misformatted state code
-            new_document["physicalAddress"] = data["physical_address"].replace(
-                " Tn ", " TN "
-            )
-
-        if "current_employer" in data:
-            new_document["currentEmployer"] = data["current_employer"]
-
-        if "drivers_license" in data:
-            new_document["driversLicense"] = data["drivers_license"]
-
-        if "drivers_license_suspended" in data:
-            new_document["driversLicenseSuspended"] = data["drivers_license_suspended"]
-
-        if "drivers_license_revoked" in data:
-            new_document["driversLicenseRevoked"] = data["drivers_license_revoked"]
-
-        if "conviction_county" in data:
-            new_document["convictionCounty"] = data["conviction_county"]
-
-        if "court_name" in data:
-            new_document["courtName"] = data["court_name"]
-
-        if "all_dockets" in data:
-            new_document["allDockets"] = data["all_dockets"]
-
-        if "sentence_start_date" in data:
-            new_document["sentenceStartDate"] = data["sentence_start_date"]
-
-        if "expiration_date" in data:
-            new_document["expirationDate"] = data["expiration_date"]
-
-        if "sentence_length_days" in data:
-            sentence_length_days = data["sentence_length_days"]
-            if int(sentence_length_days) >= 0:
-                new_document["sentenceLengthDays"] = sentence_length_days
+        new_document: dict[str, Any] = {}
+        for key, value in data.items():
+            if key == "compliant_reporting_eligible":
+                new_document["eligibilityCategory"] = value
+            elif key in [
+                "current_offenses",
+                "supervision_fee_exemption_type",
+                "restitution_monthly_payment_to",
+            ]:
+                new_document[key] = json.loads(value)
+            elif key == "physical_address":
+                # Addresses arrive in the form: "123 Fake st., Metropolis, Tn 59545
+                # This fixes the misformatted state code
+                new_document[key] = value.replace(" Tn ", " TN ")
+            elif key == "drug_screens_past_year":
+                new_document[key] = [
+                    {
+                        "result": screen["ContactNoteType"],
+                        "date": screen["contact_date"],
+                    }
+                    for screen in value
+                ]
+            elif key == "zero_tolerance_codes":
+                new_document[key] = [
+                    {
+                        "contactNoteType": code["ContactNoteType"],
+                        "contactNoteDate": code["contact_date"],
+                    }
+                    for code in value
+                ]
+            elif key.startswith("almost_eligible_") or key in [
+                "date_supervision_level_eligible",
+                "cr_rejections_past_3_months",
+                "date_serious_sanction_eligible",
+            ]:
+                # The keys for almost eligible don't line up with what the data is transformed to,
+                # so handle them separately.
+                continue
             else:
+                new_document[key] = value
+
+        if "sentence_length_days" in new_document:
+            sentence_length_days = new_document["sentence_length_days"]
+            if int(sentence_length_days) < 0:
                 # If the sentence length is negative, assume something has gone wrong and clear out
                 # the start/expiration dates as well
-                new_document.pop("sentenceStartDate", None)
-                new_document.pop("expirationDate", None)
-
-        if "special_conditions_flag" in data:
-            new_document["specialConditionsFlag"] = data["special_conditions_flag"]
-
-        if "next_special_conditions_check" in data:
-            new_document["nextSpecialConditionsCheck"] = data[
-                "next_special_conditions_check"
-            ]
-
-        if "last_special_conditions_note" in data:
-            new_document["lastSpecialConditionsNote"] = data[
-                "last_special_conditions_note"
-            ]
-
-        if "special_conditions_terminated_date" in data:
-            new_document["specialConditionsTerminatedDate"] = data[
-                "special_conditions_terminated_date"
-            ]
-
-        if "eligible_level_start" in data:
-            new_document["eligibleLevelStart"] = data["eligible_level_start"]
-
-        if "most_recent_arrest_check" in data:
-            new_document["mostRecentArrestCheck"] = data["most_recent_arrest_check"]
-
-        if "zero_tolerance_codes" in data:
-            new_document["zeroToleranceCodes"] = [
-                {
-                    "contactNoteType": code["ContactNoteType"],
-                    "contactNoteDate": code["contact_date"],
-                }
-                for code in data["zero_tolerance_codes"]
-            ]
+                new_document.pop("sentence_length_days", None)
+                new_document.pop("sentence_start_date", None)
+                new_document.pop("expiration_date", None)
 
         almost_eligible_criteria = {}
 
@@ -239,118 +127,7 @@ class CompliantReportingReferralRecordETLDelegate(WorkflowsSingleStateETLDelegat
         if almost_eligible_criteria:
             new_document["almostEligibleCriteria"] = almost_eligible_criteria
 
-        if "supervision_fee_exemption_expir_date" in data:
-            new_document["supervisionFeeExemptionExpirDate"] = data[
-                "supervision_fee_exemption_expir_date"
-            ]
-
-        if "supervision_fee_waived" in data:
-            new_document["supervisionFeeWaived"] = data["supervision_fee_waived"]
-
-        if "court_costs_monthly_amt_1" in data:
-            new_document["courtCostsMonthlyAmt1"] = data["court_costs_monthly_amt_1"]
-
-        if "court_costs_monthly_amt_2" in data:
-            new_document["courtCostsMonthlyAmt2"] = data["court_costs_monthly_amt_2"]
-
-        if "restitution_amt" in data:
-            new_document["restitutionAmt"] = data["restitution_amt"]
-
-        if "restitution_monthly_payment" in data:
-            new_document["restitutionMonthlyPayment"] = data[
-                "restitution_monthly_payment"
-            ]
-
-        if "restitution_monthly_payment_to" in data:
-            new_document["restitutionMonthlyPaymentTo"] = json.loads(
-                data["restitution_monthly_payment_to"]
-            )
-
-        if "special_conditions_alc_drug_screen_date" in data:
-            new_document["specialConditionsAlcDrugScreenDate"] = data[
-                "special_conditions_alc_drug_screen_date"
-            ]
-
-        if "special_conditions_alc_drug_assessment" in data:
-            new_document["specialConditionsAlcDrugAssessment"] = data[
-                "special_conditions_alc_drug_assessment"
-            ]
-
-        if "court_costs_balance" in data:
-            new_document["courtCostsBalance"] = data["court_costs_balance"]
-
-        if "special_conditions_alc_drug_assessment_complete_date" in data:
-            new_document["specialConditionsAlcDrugAssessmentCompleteDate"] = data[
-                "special_conditions_alc_drug_assessment_complete_date"
-            ]
-
-        if "special_conditions_alc_drug_treatment_in_out" in data:
-            new_document["specialConditionsAlcDrugTreatmentInOut"] = data[
-                "special_conditions_alc_drug_treatment_in_out"
-            ]
-
-        if "special_conditions_alc_drug_treatment_complete_dat" in data:
-            new_document["specialConditionsAlcDrugTreatmentCompleteDate"] = data[
-                "special_conditions_alc_drug_treatment_complete_dat"
-            ]
-
-        if "special_conditions_counseling_anger_management" in data:
-            new_document["specialConditionsCounselingAngerManagement"] = data[
-                "special_conditions_counseling_anger_management"
-            ]
-
-        if "special_conditions_counseling_anger_management_complete_date" in data:
-            new_document[
-                "specialConditionsCounselingAngerManagementCompleteDate"
-            ] = data["special_conditions_counseling_anger_management_complete_date"]
-
-        if "special_conditions_counseling_mental_health" in data:
-            new_document["specialConditionsCounselingMentalHealth"] = data[
-                "special_conditions_counseling_mental_health"
-            ]
-
-        if "special_conditions_counseling_mental_health_current" in data:
-            new_document["specialConditionsCounselingMentalHealthCurrent"] = data[
-                "special_conditions_counseling_mental_health_current"
-            ]
-
-        if "special_conditions_counseling_mental_health_complete_date" in data:
-            new_document["specialConditionsCounselingMentalHealthCompleteDate"] = data[
-                "special_conditions_counseling_mental_health_complete_date"
-            ]
-
-        if "special_conditions_community_service_hours" in data:
-            new_document["specialConditionsCommunityServiceHours"] = data[
-                "special_conditions_community_service_hours"
-            ]
-
-        if "special_conditions_programming_cognitive_behavior_completion_date" in data:
-            new_document[
-                "specialConditionsProgrammingCognitiveBehaviorCompletionDate"
-            ] = data[
-                "special_conditions_programming_cognitive_behavior_completion_date"
-            ]
-
-        if "special_conditions_community_service_completion_date" in data:
-            new_document["specialConditionsCommunityServiceCompletionDate"] = data[
-                "special_conditions_community_service_completion_date"
-            ]
-
-        if "special_conditions_programming_safe_completion_date" in data:
-            new_document["specialConditionsProgrammingSafeCompletionDate"] = data[
-                "special_conditions_programming_safe_completion_date"
-            ]
-
-        if "special_conditions_programming_victim_impact_completion_date" in data:
-            new_document[
-                "specialConditionsProgrammingVictimImpactCompletionDate"
-            ] = data["special_conditions_programming_victim_impact_completion_date"]
-
-        if "special_conditions_programming_fsw_completion_date" in data:
-            new_document["specialConditionsProgrammingFswCompletionDate"] = data[
-                "special_conditions_programming_fsw_completion_date"
-            ]
-
+        new_document = convert_nested_dictionary_keys(new_document, snake_to_camel)
         return data["tdoc_id"], new_document
 
 
