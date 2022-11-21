5698afc27f238d4ec081c17686c79e0e46d8c261
lilidworkin <99899591+lilidworkin@users.noreply.github.com>
2022-11-21 10:48:50 -0500

    [Justice Counts] Updates to deploy scripts (Recidiviz/recidiviz-data#16704)
    ## Description of the change
    
    Here I'm making two main changes to our deploy scripts:
    1) For the playtesting script, I'm making it possible to specify a URL
    tag -- e.g. whether you want to deploy to `playtesting--...` or
    `terrytest--...` or `mahmoudtest--...`, etc. This makes it possible for
    us each to have our own playtesting versions live!
    2. I'm also making it possible to deploy Agency Dashboards to
    playtesting, staging, or prod!
    
    @terryttsai , @mxosman , @nichelle-hall could you all test out the
    playtesting for me, and approve if the script works for you? Here's how
    you can call it:
    
    1. Go to `~/recidiviz/recidiviz-data`
    2. Run
    ```
    ./recidiviz/tools/deploy/justice_counts/deploy_for_playtesting.sh -b <name of backend branch> -f <name of frontend branch> -a <either publisher of agency-dashboard> -t <URL tag>
    ```
    
    For example, @terryttsai can run the following:
    ```
    ./recidiviz/tools/deploy/justice_counts/deploy_for_playtesting.sh -b main -f <name of frontend branch he's working on> -a agency-dashboard -t terrytest
    ```
    
    @nichelle-hall could run the following:
    ```
    ./recidiviz/tools/deploy/justice_counts/deploy_for_playtesting.sh -b <name of backend branch she's working on> -f main -a publisher -t nichelletest
    ```
    
    @mxosman could run the following:
    ```
    ./recidiviz/tools/deploy/justice_counts/deploy_for_playtesting.sh -b main -f <name of frontend branch he's working on> -a publisher -t mahmoudtest
    ```
    
    Let me know if you run into any errors! There might be security kinks we
    need to work out.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 9626250d3c29509152ccb41d6c74c158eb55feda

--- recidiviz/tools/deploy/justice_counts/deploy_for_playtesting.sh
+++ recidiviz/tools/deploy/justice_counts/deploy_for_playtesting.sh
@@ -5,10 +5,10 @@ Performs the following actions:
 1) Runs our Cloud Build Trigger to build a Docker image off of the specified branches
    in pulse-data and justice-counts.
 2) Deploys a new Cloud Run revision from this image without allocating traffic,
-   but with the playtesting tag.
+   but with the specified tag.
 
 Example usage:
-./recidiviz/tools/deploy/justice_counts/deploy_for_playtesting.sh -b main -f settings-feature -a publisher
+./recidiviz/tools/deploy/justice_counts/deploy_for_playtesting.sh -b main -f settings-feature -a publisher -t playtesting
 "
 
 BASH_SOURCE_DIR=$(dirname "${BASH_SOURCE[0]}")
@@ -19,20 +19,24 @@ PROJECT_ID='recidiviz-staging'
 BACKEND_BRANCH=''
 FRONTEND_BRANCH=''
 FRONTEND_APP=''
+URL_TAG=''
+CLOUD_RUN_SERVICE=''
 
 function print_usage {
-    echo_error "usage: $0 -b BACKEND_BRANCH -f FRONTEND_BRANCH -a FRONTEND_APP"
+    echo_error "usage: $0 -b BACKEND_BRANCH -f FRONTEND_BRANCH -a FRONTEND_APP -t URL_TAG"
     echo_error "  -b: Backend branch from which to build the Docker image."
     echo_error "  -f: Frontend branch from which to build the Docker image."
     echo_error "  -a: Frontend app to deploy (either publisher or agency-dashboard)."
+    echo_error "  -t: Tag to add to the deployed revision (e.g. playtesting or <yourname>test)."
     run_cmd exit 1
 }
 
-while getopts "b:f:a:" flag; do
+while getopts "b:f:a:t:" flag; do
   case "${flag}" in
     b) BACKEND_BRANCH="$OPTARG" ;;
     f) FRONTEND_BRANCH="$OPTARG" ;;
     a) FRONTEND_APP="$OPTARG" ;;
+    t) URL_TAG="$OPTARG" ;;
     *) print_usage
        run_cmd exit 1 ;;
   esac
@@ -57,13 +61,23 @@ if [[ -z ${FRONTEND_APP} ]]; then
     run_cmd exit 1
 fi
 
-if [[ ${FRONTEND_APP} != 'publisher' && ${FRONTEND_APP} != 'agency-dashboard' ]]; then
+if [[ -z ${URL_TAG} ]]; then
+    echo_error "Missing/empty tag argument"
+    print_usage
+    run_cmd exit 1
+fi
+
+if [[ ${FRONTEND_APP} == 'publisher' ]]; then
+    CLOUD_RUN_SERVICE="justice-counts-web"
+elif [[ ${FRONTEND_APP} == 'agency-dashboard' ]]; then
+    CLOUD_RUN_SERVICE="justice-counts-agency-dashboard-web"
+else
     echo_error "Invalid frontend application - must be either publisher or agency-dashboard"
     run_cmd exit 1
 fi
 
 # This is where Cloud Build will put the new Docker image
-SUBDIRECTORY=justice-counts/playtesting/${FRONTEND_APP}
+SUBDIRECTORY=justice-counts/playtesting/${FRONTEND_APP}/${URL_TAG}
 REMOTE_IMAGE_BASE=us.gcr.io/${PROJECT_ID}/${SUBDIRECTORY}
 
 echo "Building Docker image off of ${BACKEND_BRANCH} in pulse-data and ${FRONTEND_BRANCH} in justice-counts..."
@@ -79,13 +93,12 @@ RECIDIVIZ_DATA_COMMIT_HASH=$(gcloud container images list-tags "${REMOTE_IMAGE_B
 # Use that to get the URL of the built Docker image
 REMOTE_IMAGE_URL=${REMOTE_IMAGE_BASE}:${RECIDIVIZ_DATA_COMMIT_HASH}
 
-# TODO(#16325): If FRONTEND_APP is agency-dashboard, deploy to a different Cloud Run service
 echo "Deploying new Cloud Run revision with image ${REMOTE_IMAGE_URL} to playtesting URL..."
-run_cmd gcloud -q run deploy justice-counts-web \
+run_cmd gcloud -q run deploy "${CLOUD_RUN_SERVICE}" \
     --project "${PROJECT_ID}" \
     --image "${REMOTE_IMAGE_URL}" \
     --region "us-central1" \
-    --tag "playtesting" \
+    --tag "${URL_TAG}" \
     --no-traffic
 
 echo "Deploy of ${FRONTEND_APP} for playtesting succeeded."
