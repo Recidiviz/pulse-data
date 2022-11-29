ce1f5677deb56580414d2f2a7b9d738e19873705
Michelle Orden <82831800+morden35@users.noreply.github.com>
2022-11-29 10:32:00 -0600

    [Justice Counts] Endpoints to save agency purpose and functions settings (Recidiviz/recidiviz-data#16801)
    ## Description of the change
    
    This PR builds the backend endpoints that allow users to fill out
    "purpose and functions" information about their agency. It does so
    through a new endpoint found in api.py: update_agency(). update_agency()
    checks for the "settings" parameter in the request JSON. If the
    "settings" parameter is present,
    AgencySettingInterface.create_agency_setting() is called for each
    setting.
    
    Additionally, the frontend can now retrieve a previously saved setting
    value via the get_agency_settings() endpoint in api.py. The endpoint
    calls AgencySettingInterface.get_agency_settings() and returns a
    JSON-ified version of all settings we found in the DB.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#16725
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: f3ecefece1f8f2e0e69b7af5f55695cdebf0fd6c

--- recidiviz/tests/justice_counts/control_panel/server_test.py
+++ recidiviz/tests/justice_counts/control_panel/server_test.py
@@ -35,6 +35,7 @@ from recidiviz.cloud_storage.gcsfs_factory import GcsfsFactory
 from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
 from recidiviz.common.constants.justice_counts import ContextKey
 from recidiviz.justice_counts.agency import AgencyInterface
+from recidiviz.justice_counts.agency_setting import AgencySettingType
 from recidiviz.justice_counts.bulk_upload.bulk_upload import BulkUploader
 from recidiviz.justice_counts.control_panel.config import Config
 from recidiviz.justice_counts.control_panel.constants import ControlPanelPermission
@@ -1984,4 +1985,48 @@ class TestJusticeCountsControlPanelAPI(JusticeCountsDatabaseTestCase):
         self.assertEqual(
             set(agency.systems),
             {schema.System.PAROLE.value, schema.System.SUPERVISION.value},
+        )
+
+    def test_update_and_get_agency_settings(self) -> None:
+        user = self.test_schema_objects.test_user_A
+        agency = self.test_schema_objects.test_agency_C
+        self.session.add_all([user, agency])
+        self.session.commit()
+        self.session.refresh(agency)
+        agency_id = agency.id
+
+        with self.app.test_request_context():
+            g.user_context = UserContext(
+                auth0_user_id=user.auth0_user_id, agency_ids=[agency_id]
+            )
+
+            # First, update agency settings
+            update_response = self.client.patch(
+                f"/api/agencies/{agency_id}",
+                json={
+                    "settings": [
+                        {
+                            "setting_type": AgencySettingType.PURPOSE_AND_FUNCTIONS.value,
+                            "value": "My agency has the following purpose and functions ...",
+                        }
+                    ]
+                },
+            )
+            # Next, get the agency setting
+            get_response = self.client.get(f"/api/agencies/{agency_id}")
+
+        self.assertEqual(update_response.status_code, 200)
+        self.assertEqual(get_response.status_code, 200)
+
+        self.assertEqual(
+            get_response.json,
+            {
+                "settings": [
+                    {
+                        "setting_type": AgencySettingType.PURPOSE_AND_FUNCTIONS.value,
+                        "value": "My agency has the following purpose and functions ...",
+                        "source_id": agency_id,
+                    }
+                ]
+            },
         )
