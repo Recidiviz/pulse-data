7bbde5740ae83565e5009d79d16a85485386f0d3
Jen Overgaag <jovergaag@gmail.com>
2022-11-30 10:48:28 -0800

    [Workflows] US_ME resident record (Recidiviz/recidiviz-data#16767)
    ## Description of the change
    
    `resident_record` for US_ME residents. This includes all fields, except
    for hard coding `['sccp']` as `all_eligible_opportunities` until the
    sccp query is finished, at which point I will submit another PR to
    properly mark the eligible residents.
    
    This has been run in the `jovergaag_16252_*` sandbox.
    
    The secret has been added to both staging and production.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#16252
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 9897a17c95169d620f1c604781c504899edf7dbe

---
+++ recidiviz/calculator/query/state/views/workflows/us_me/incarceration_residents_template.py
@@ -0,0 +1,122 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2022 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""View logic to prepare US_ID Workflows supervision clients."""
+
+from recidiviz.calculator.query.bq_utils import array_concat_with_null
+
+US_ME_INCARCERATION_RESIDENTS_QUERY_TEMPLATE = f"""
+    us_me_incarceration_cases AS (
+        SELECT
+            dataflow.state_code,
+            dataflow.person_id,
+            person_external_id,
+            sp.full_name AS person_name,
+            dataflow.facility AS facility_id,
+            dataflow.start_date_inclusive AS admission_date
+        FROM `{{project_id}}.{{dataflow_dataset}}.most_recent_incarceration_population_span_metrics_materialized` dataflow
+        INNER JOIN `{{project_id}}.{{sessions_dataset}}.compartment_sessions_materialized` sessions
+            ON dataflow.state_code = sessions.state_code
+            AND dataflow.person_id = sessions.person_id
+            AND sessions.compartment_level_1 = "INCARCERATION"
+            AND sessions.end_date IS NULL
+        INNER JOIN `{{project_id}}.{{normalized_state_dataset}}.state_person` sp 
+            ON dataflow.person_id = sp.person_id
+        WHERE dataflow.state_code = 'US_ME' AND dataflow.included_in_state_population
+            AND dataflow.end_date_exclusive IS NULL
+    )
+    , release_dates AS (
+        SELECT
+            person_id,
+            MAX(projected_release.projected_max_release_date) AS release_date
+        FROM `{{project_id}}.{{normalized_state_dataset}}.state_incarceration_sentence` projected_release
+        WHERE completion_date is NULL
+            AND status != "COMPLETED"
+        GROUP BY 1
+    )
+    , custody_level AS (
+        SELECT
+            pei.person_id,
+            UPPER(cs.CLIENT_SYS_DESC) AS custody_level,
+        FROM `{{project_id}}.{{us_me_raw_data_dataset}}.CIS_112_CUSTODY_LEVEL` cl
+        INNER JOIN `{{project_id}}.{{us_me_raw_data_up_to_date_dataset}}.CIS_1017_CLIENT_SYS_latest` cs
+            ON cl.CIS_1017_CLIENT_SYS_CD = cs.CLIENT_SYS_CD
+        INNER JOIN `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` pei
+            ON cl.CIS_100_CLIENT_ID = pei.external_id
+            AND pei.state_code = "US_ME"
+        WHERE TRUE
+        QUALIFY ROW_NUMBER() OVER (
+            PARTITION BY person_id
+            ORDER BY CAST(LEFT(cl.CUSTODY_DATE, 19) AS DATETIME) DESC
+        ) = 1
+        ORDER BY
+            pei.person_id,
+            CAST(LEFT(cl.CUSTODY_DATE, 19) AS DATETIME)
+    )
+    , housing_unit AS (
+      SELECT
+        person_id,
+        housing_unit AS unit_id,
+        ROW_NUMBER() over (partition by person_id order by admission_date desc) rn 
+      FROM `{{project_id}}.{{normalized_state_dataset}}.state_incarceration_period` 
+      where
+          release_date is null
+          and state_code='US_ME'
+      qualify rn = 1
+    )
+    , officer_assignments AS (
+        SELECT DISTINCT
+            Cis_900_Employee_Id as officer_id,
+            Cis_100_Client_Id as person_external_id,
+            row_number() OVER (PARTITION BY Cis_100_Client_Id ORDER BY Assignment_Date DESC) rn
+        FROM `{{project_id}}.{{us_me_raw_data_up_to_date_dataset}}.CIS_124_SUPERVISION_HISTORY_latest` sp
+        WHERE Supervision_End_Date IS NULL
+        QUALIFY rn = 1
+    )
+    , resident_info AS (
+        SELECT DISTINCT
+            ic.state_code,
+            ic.person_name,
+            ic.person_id,
+            ic.person_external_id,
+            officer_id,
+            facility_id,
+            unit_id,
+            custody_level.custody_level,
+            ic.admission_date,
+            rd.release_date,
+        FROM
+            us_me_incarceration_cases ic
+        LEFT JOIN release_dates rd
+            USING(person_id)
+        LEFT JOIN custody_level
+            USING(person_id)
+        LEFT JOIN housing_unit
+          USING(person_id)
+        LEFT JOIN officer_assignments
+          USING(person_external_id)
+
+    ),
+    me_residents AS (
+        SELECT
+            #* EXCEPT(eligible_opportunities),
+            # {array_concat_with_null(["me_sccp_eligibility.eligible_opportunities"])} AS all_eligible_opportunities,
+            *,
+            ["usMeSCCP"] as all_eligible_opportunities
+        FROM resident_info
+        WHERE officer_id IS NOT NULL
+    )
+"""
