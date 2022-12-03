a5fcd47b9778d40c9121147bae0276526752ba00
Jen Overgaag <jovergaag@gmail.com>
2022-12-02 17:00:45 -0800

    [Workflows] Update ETL to support ME SCCP, enable production export (Recidiviz/recidiviz-data#16777)
    ## Description of the change
    
    Updates the ETL to support the ME SCCP launch. Also enables the
    production export for ME Workflows
    
    - adds `has_facility_caseload` to staff ETL.
    - adds resident ETL delegate
    - adds a script to run the ETL from a local branch. Is this a bad idea?
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#16256
    Closes Recidiviz/recidiviz-data#16251
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 7a1bff770fd6f5fcd987ab5df85748f13b4f1cb8

---
+++ recidiviz/tools/workflows/run_etl_from_local_branch.py
@@ -0,0 +1,82 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""A script for running the Workflows ETL for a given file and state
+
+To run the ETL for a specific file and state, run:
+    python -m recidiviz.tools.workflows.run_etl_from_local_branch \
+       --filename [file name]
+       --state_code [state_code]
+
+"""
+import argparse
+import logging
+
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+from recidiviz.workflows.etl.routes import get_workflows_delegates
+
+
+def main(
+    filename: str,
+    state_code: str,
+) -> None:
+    logging.getLogger().setLevel(logging.INFO)
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        for delegate in get_workflows_delegates():
+            try:
+                if delegate.supports_file(state_code, filename):
+                    delegate.run_etl(state_code, filename)
+            except ValueError:
+                logging.info(
+                    "Error running Firestore ETL for file %s for state_code %s",
+                    filename,
+                    state_code,
+                )
+
+
+def parse_arguments() -> argparse.Namespace:
+    """Parses the required arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--filename",
+        dest="filename",
+        help="file name for the collection you want to update",
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        "--state_code",
+        dest="state_code",
+        help="State code of the collection you want to update",
+        type=str,
+        required=True,
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    args = parse_arguments()
+
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
+    main(
+        filename=args.filename,
+        state_code=args.state_code,
+    )
---
+++ recidiviz/workflows/etl/workflows_resident_etl_delegate.py
@@ -0,0 +1,57 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2022 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""Delegate class to ETL resident records for Workflows into Firestore."""
+import json
+from typing import List, Tuple
+
+from recidiviz.common.str_field_utils import person_name_case, snake_to_camel
+from recidiviz.workflows.etl.workflows_etl_delegate import WorkflowsFirestoreETLDelegate
+
+
+class WorkflowsResidentETLDelegate(WorkflowsFirestoreETLDelegate):
+    """Delegate class to ETL the resident_record.json file into Firestore."""
+
+    COLLECTION_BY_FILENAME = {"resident_record.json": "residents"}
+
+    def get_supported_files(self, state_code: str) -> List[str]:
+        return ["resident_record.json"]
+
+    def transform_row(self, row: str) -> Tuple[str, dict]:
+        data = json.loads(row)
+
+        new_document = {
+            "pseudonymizedId": data["pseudonymized_id"],
+            "personExternalId": data["person_external_id"],
+            "stateCode": data["state_code"],
+            "personName": {
+                snake_to_camel(k): person_name_case(v)
+                for k, v in json.loads(data["person_name"]).items()
+            },
+            "officerId": data["officer_id"],
+            "facilityId": data.get("facility_id"),
+            "unitId": data.get("unit_id"),
+            "custodyLevel": data.get("custody_level"),
+            "admissionDate": data.get("admission_date"),
+            "releaseDate": data.get("release_date"),
+        }
+
+        if "all_eligible_opportunities" in data:
+            new_document["allEligibleOpportunities"] = data[
+                "all_eligible_opportunities"
+            ]
+
+        return data["person_external_id"], new_document
