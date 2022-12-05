4d71d5197ca385b4491586855398acec427941bb
lilidworkin <99899591+lilidworkin@users.noreply.github.com>
2022-12-05 12:40:43 -0500

    Revert "[Justice Counts] Update `agency.to_json()` to include state n… (Recidiviz/recidiviz-data#17047)
    …ame (Recidiviz/recidiviz-data#16903)"
    
    This reverts commit 168e542244fa79e74a68c0510fd77188577ae1b3.
    
    ## Description of the change
    
    Causing this error on staging:
    ```
    Traceback (most recent call last): File "/app/recidiviz/justice_counts/control_panel/routes/api.py", line 219, in create_user_if_necessary "agencies": [agency.to_json() for agency in agencies or []], File "/app/recidiviz/justice_counts/control_panel/routes/api.py", line 219, in <listcomp> "agencies": [agency.to_json() for agency in agencies or []], File "/app/recidiviz/persistence/database/schema/justice_counts/schema.py", line 315, in to_json "state": self.get_state_name(), File "/app/recidiviz/persistence/database/schema/justice_counts/schema.py", line 296, in get_state_name state_name = StateCode(self.state_code.upper()).get_state().name AttributeError: 'NoneType' object has no attribute 'upper'
    ```
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: ee5775c534c167f9f62812b8dcee5b8f44f3e230

--- recidiviz/persistence/database/schema/justice_counts/schema.py
+++ recidiviz/persistence/database/schema/justice_counts/schema.py
@@ -292,10 +292,6 @@ class Agency(Source):
         fips.validate_county_code(fips_county_code)
         return fips_county_code
 
-    def get_state_name(self) -> str:
-        state_name = StateCode(self.state_code.upper()).get_state().name
-        return state_name
-
     __mapper_args__ = {
         "polymorphic_identity": "agency",
     }
@@ -312,7 +308,6 @@ class Agency(Source):
             ],
             "state_code": self.state_code,
             "fips_county_code": self.fips_county_code,
-            "state": self.get_state_name(),
         }
 
 
