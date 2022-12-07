897a0351dea7cf72cad6af5ff95d7930ba57c218
adeenal <74085696+adeenal@users.noreply.github.com>
2022-12-07 15:16:53 -0800

    [US_PA] Fix dbo_tblInmTestScore and dbo_tblInmTestScoreHist duplications  (Recidiviz/recidiviz-data#17101)
    ## Description of the change
    
    > Fix dbo_tblInmTestScore and dbo_tblInmTestScoreHist duplications of
    external IDs
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#17060
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6ac3f9a77077bed2ba5363cbffec6441ef8be774

--- recidiviz/ingest/direct/regions/us_pa/ingest_views/view_dbo_tblInmTestScore.py
+++ recidiviz/ingest/direct/regions/us_pa/ingest_views/view_dbo_tblInmTestScore.py
@@ -23,36 +23,47 @@ from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_QUERY_TEMPLATE = """
-WITH all_test_scores AS (
+    WITH all_test_scores as (
+      SELECT Test_Id, Control_Number, Test_Desc, Inmate_number, Test_Dt, Fac_Cd, Test_Score, ModBy_EmpNum, LstMod_Dt, AsmtVer_Num, Fab_ind, RSTRvsd_Flg
+  FROM (
     SELECT 
-      Test_Id,
-      Control_Number,
-      Test_Desc,
-      Inmate_number,
-      Test_Dt,
-      Fac_Cd,
-      Test_Score,
-      ModBy_EmpNum,
-      LstMod_Dt,
-      AsmtVer_Num,
-      Fab_ind,
-      RSTRvsd_Flg
-    FROM {dbo_tblInmTestScore}
-    UNION DISTINCT
-    SELECT 
-      Test_Id,
-      Control_Number,
-      Test_Desc,
-      Inmate_number,
-      Test_Dt,
-      Fac_Cd,
-      Test_Score,
-      ModBy_EmpNum,
-      LstMod_Dt,
-      AsmtVer_Num,
-      Fab_ind,
-      RSTRvsd_Flg
-    FROM {dbo_tblInmTestScoreHist}
+      *, 
+      ROW_NUMBER() OVER (PARTITION BY Control_Number, Inmate_number, Test_Id, AsmtVer_Num ORDER BY is_history_row ASC) AS entry_priority
+    FROM (
+      SELECT
+        t.Test_Id,
+        t.Control_Number,
+        t.Test_Desc,
+        t.Inmate_number,
+        t.Test_Dt,
+        t.Fac_Cd,
+        t.Test_Score,
+        t.ModBy_EmpNum,
+        t.LstMod_Dt,
+        t.AsmtVer_Num,
+        t.Fab_ind,
+        t.RSTRvsd_Flg,
+        0 AS is_history_row
+    FROM {dbo_tblInmTestScore} t
+      UNION ALL
+      SELECT
+        ht.Test_Id,
+        ht.Control_Number,
+        ht.Test_Desc,
+        ht.Inmate_number,
+        ht.Test_Dt,
+        ht.Fac_Cd,
+        ht.Test_Score,
+        ht.ModBy_EmpNum,
+        ht.LstMod_Dt,
+        ht.AsmtVer_Num,
+        ht.Fab_ind,
+        ht.RSTRvsd_Flg,
+        1 AS is_history_row
+    FROM {dbo_tblInmTestScoreHist} ht
+    ) as programs
+  ) as programs_with_priority
+  WHERE entry_priority = 1
 )
 SELECT *
 FROM all_test_scores
