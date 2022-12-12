c70a0de78784585dfbe7c70651162c1dfff62974
brendali121 <brenda@recidiviz.org>
2022-12-12 12:33:07 -0600

    [US_IX] Sentences and charges ingest view and mappings (Recidiviz/recidiviz-data#16026)
    ## Description of the change
    
    This is the incarceration and supervision sentence for ID ATLAS data and
    the corresponding tests. Also pulled in the state charge view and
    mappings and corresponding tests.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#15892
    Closes Recidiviz/recidiviz-data#15893
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [X] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [X] This pull request has a descriptive title and information useful
    to a reviewer
    - [X] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [X] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    Co-authored-by: lydiamasri2 <lydia@recidiviz.org>
    GitOrigin-RevId: cc065aad7a00302e988f0ece64365658afad2c7c

---
+++ recidiviz/ingest/direct/regions/us_ix/ingest_views/view_incarceration_sentence.py
@@ -0,0 +1,53 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query for incarceration sentences."""
+from recidiviz.ingest.direct.regions.us_ix.ingest_views.template_sentence import (
+    sentence_view_template,
+)
+from recidiviz.ingest.direct.views.direct_ingest_big_query_view_types import (
+    DirectIngestPreProcessedIngestViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+sentences_base = sentence_view_template()
+
+
+VIEW_QUERY_TEMPLATE = f"""
+    SELECT 
+        SentenceId, 
+        OffenderId, 
+        CountyId,
+        (DATE(EffectiveDate)) as EffectiveDate, 
+        (DATE(SentenceDate)) as SentenceDate,
+        (DATE(DpedApprovedDate)) as DpedApprovedDate,
+        (DATE(FtrdApprovedDate)) as FtrdApprovedDate,
+        TermStatusDesc,
+    FROM ({sentences_base}) sent
+    WHERE SentenceOrderCategoryId = '2'
+"""
+
+VIEW_BUILDER = DirectIngestPreProcessedIngestViewBuilder(
+    region="us_ix",
+    ingest_view_name="incarceration_sentence",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+    order_by_cols="OffenderId, SentenceId",
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
