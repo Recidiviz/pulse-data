3cc04bef591a3596b12dfb4a63b32146bd0d697a
brendali121 <brenda@recidiviz.org>
2022-12-20 15:37:26 -0600

    [US_IX] Revise state charge to link correctly to sentences (Recidiviz/recidiviz-data#17403)
    ## Description of the change
    
    Revise state charge to link correctly to sentences, sepcifically:
    - Pull `SentenceOrderCategoryId` into the ingest view and revise the
    YAML mapping to attach to the appropriate type of sentence
    (incarceration vs supervision) based on `SentenceOrderCategoryId`.
    - Pull in the `SentenceId` for the _sentence_ attached to the same
    sentence order as each charge to construct a sentence `external_id` we
    can use to attach a charge to a sentence.
    - For context on the original issue, sentences and charges come from the
    same table `scl_Sentence` and are separate rows in that table (we
    differentiate which rows are charges vs sentences using
    `SentenceLinkClassId`) but to link charges to sentences we were
    accidentally using the `SentenceId` for the charge row rather than the
    `SentenceId` for the sentence row
    - Updates all associated tests for the above changes
    
    This should address both issues of not having charge <> supervision
    sentence associations as well as not seeing charges attached to open
    incarceration sentences.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: e7bbea26c2e6c9c7b2ff7bfc0f665ea019e1a816

--- recidiviz/ingest/direct/regions/us_ix/ingest_views/view_charges.py
+++ recidiviz/ingest/direct/regions/us_ix/ingest_views/view_charges.py
@@ -37,7 +37,6 @@ VIEW_QUERY_TEMPLATE = """
 
   SELECT 
     sent.OffenderId,
-    sentlink.SentenceId,
     sentoff.OffenseId,
     (DATE(off.OffenseDate)) as OffenseDate,
     off.OffenseTypeId,
@@ -51,14 +50,38 @@ VIEW_QUERY_TEMPLATE = """
     leg.MiddleName, 
     leg.LastName, 
     leg.NameSuffix,
-    ord.CountyId
+    ord.CountyId,
+    ord_type.SentenceOrderCategoryId,
+    sentlink2.SentenceId as _sentence_external_id
 FROM {scl_Sentence} sent
   LEFT JOIN {scl_SentenceLink} sentlink ON sent.SentenceId = sentlink.SentenceId
   LEFT JOIN {scl_SentenceLinkOffense} sentoff ON sentlink.SentenceLinkId = sentoff.SentenceLinkId
   LEFT JOIN {scl_Offense} off ON sentoff.OffenseId = off.OffenseId
   LEFT JOIN {scl_OffenseType} offtyp ON off.OffenseTypeId = offtyp.OffenseTypeId
   LEFT JOIN {scl_SentenceOrder} ord ON off.SentenceOrderId = ord.SentenceOrderId
+  LEFT JOIN {scl_SentenceOrderType} ord_type ON ord.SentenceOrderTypeId = ord_type.SentenceOrderTypeId
   LEFT JOIN {scl_Legist} leg ON ord.JudgeLegistId = leg.LegistId
+  -- 
+  -- Charge records and sentence information are all stored in the same tables (scl_Sentence and scl_SentenceLink) but 
+  -- denoted differently by SentenceLinkClassId and use different association tables (scl_SentenceLinkSentenceOrder 
+  -- and scl_SentenceLinkOffense).  It broadly looks like this:
+  -- 
+  --                        scl_Sentence
+  --                              |
+  --                       scl_SentenceLink
+  --                      |               |
+  -- scl_SentenceLinkSentenceOrder     scl_SentenceLinkOffense
+  --                       |                      | 
+  --                scl_SentenceOrder  ---  scl_Offense
+  -- 
+  -- So here we're taking the SentenceOrder we have attached to the charge record and
+  -- joining on scl_SentenceLinkSentenceOrder and scl_SentenceLink (again) 
+  -- to go from SentenceOrder > SentenceOrder/SentenceLink > SentenceLink > Sentence in order to
+  -- grab the SentenceId for the sentence record attached to the same sentence order 
+  -- that this charge record is attached to.  In other words, from sent and sentlink we grab information
+  -- about the charge, whereas from sentlink2 we grab information about the associated sentence.
+  LEFT JOIN {scl_SentenceLinkSentenceOrder} linkorder ON ord.SentenceOrderId = linkorder.SentenceOrderId
+  LEFT JOIN {scl_SentenceLink} sentlink2 ON sentlink2.SentenceLinkId = linkorder.SentenceLinkId
 WHERE sentlink.SentenceLinkClassId = '1' -- only look at offense sentence records
     AND sent.OffenderId IS NOT NULL
     AND off.OffenseId IS NOT NULL
