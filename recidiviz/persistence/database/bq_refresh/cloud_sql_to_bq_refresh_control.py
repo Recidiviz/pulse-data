65c9f92b4eeda2fc03244bf0f235116a1379b91a
Chinedu Ozodi <chinedu@recidiviz.org>
2023-01-05 14:50:38 -0800

    [DAG] Cleanup code after case triage refresh move to dag (Recidiviz/recidiviz-data#17612)
    ## Description of the change
    
    Removed dry run tag for DAG CASE TRIAGE refresh. Cleanup of code after
    CASE TRIAGE refresh move to DAG.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#15930
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 552d4ded817fc897b0d73deff35dbb065405eaf9

--- recidiviz/persistence/database/bq_refresh/cloud_sql_to_bq_refresh_control.py
+++ recidiviz/persistence/database/bq_refresh/cloud_sql_to_bq_refresh_control.py
@@ -19,7 +19,6 @@ import datetime
 import json
 import logging
 import time
-import uuid
 from http import HTTPStatus
 from typing import Optional, Tuple
 
@@ -30,16 +29,8 @@ from recidiviz.big_query.big_query_client import BigQueryClientImpl
 from recidiviz.big_query.rematerialization_success_persister import (
     RefreshBQDatasetSuccessPersister,
 )
-from recidiviz.calculator.pipeline.pipeline_type import MetricPipelineRunType
-from recidiviz.cloud_functions.cloudsql_to_bq_refresh_utils import (
-    PIPELINE_RUN_TYPE_REQUEST_ARG,
-)
 from recidiviz.cloud_storage.gcs_pseudo_lock_manager import GCSPseudoLockDoesNotExist
 from recidiviz.cloud_tasks.utils import get_current_cloud_task_id
-from recidiviz.ingest.direct.direct_ingest_control import kick_all_schedulers
-from recidiviz.persistence.database.bq_refresh.bq_refresh_cloud_task_manager import (
-    BQRefreshCloudTaskManager,
-)
 from recidiviz.persistence.database.bq_refresh.cloud_sql_to_bq_lock_manager import (
     CloudSqlToBQLockManager,
 )
@@ -53,142 +44,6 @@ from recidiviz.persistence.database.schema_utils import SchemaType
 from recidiviz.utils.auth.gae import requires_gae_auth
 
 cloud_sql_to_bq_blueprint = flask.Blueprint("export_manager", __name__)
-
-# TODO(#15930): This function will be deleted once the all dependencies on this endpoint are moved to the DAG
-@cloud_sql_to_bq_blueprint.route(
-    "/create_refresh_bq_schema_task/<schema_arg>", methods=["GET", "POST"]
-)
-@requires_gae_auth
-def wait_for_ingest_to_create_tasks(
-    schema_arg: str,
-) -> Tuple[str, HTTPStatus]:
-    """Worker function to wait until ingest is not running to queue a task to run
-    /refresh_bq_schema. Before doing anything, grabs the refresh lock to indicate that
-    a refresh wants to start and ingest should yield ASAP. Then:
-    * When ingest is not running/locked, creates task to run /refresh_bq_schema.
-    * When ingest is running/locked, re-enqueues this task to run again in 60 seconds.
-    """
-    try:
-        schema_type = SchemaType(schema_arg.upper())
-    except ValueError:
-        return (
-            f"Unexpected value for schema_arg: [{schema_arg}]",
-            HTTPStatus.BAD_REQUEST,
-        )
-    if not CloudSqlToBQConfig.is_valid_schema_type(schema_type):
-        return (
-            f"Unsuppported schema type: [{schema_type}]",
-            HTTPStatus.BAD_REQUEST,
-        )
-    if schema_type == SchemaType.STATE:
-        return (
-            f"Schema type should only be refreshed through DAG: [{schema_type}]",
-            HTTPStatus.BAD_REQUEST,
-        )
-
-    pipeline_run_type_arg = get_value_from_request(PIPELINE_RUN_TYPE_REQUEST_ARG)
-
-    if pipeline_run_type_arg:
-        try:
-            _ = MetricPipelineRunType(pipeline_run_type_arg.upper())
-        except ValueError:
-            return (
-                f"Unexpected value for pipeline_run_type: [{pipeline_run_type_arg}]",
-                HTTPStatus.BAD_REQUEST,
-            )
-
-    lock_id = get_value_from_request("lock_id", str(uuid.uuid4()))
-    if not lock_id:
-        raise ValueError(
-            "Response from get_value_from_request() should not return "
-            "None when provided with a default value."
-        )
-    logging.info("Request lock id: %s", lock_id)
-
-    lock_manager = CloudSqlToBQLockManager()
-    lock_manager.acquire_lock(schema_type=schema_type, lock_id=lock_id)
-
-    task_manager = BQRefreshCloudTaskManager()
-    if not lock_manager.can_proceed(schema_type):
-        logging.info("Regions running, re-enqueuing this task.")
-        task_manager.create_reattempt_create_refresh_tasks_task(
-            lock_id=lock_id,
-            schema=schema_arg,
-            pipeline_run_type=pipeline_run_type_arg,
-        )
-        return "", HTTPStatus.OK
-
-    logging.info("No regions running, triggering BQ refresh.")
-    task_manager.create_refresh_bq_schema_task(
-        schema_type=schema_type,
-        pipeline_run_type=pipeline_run_type_arg,
-    )
-    return "", HTTPStatus.OK
-
-
-# TODO(#15930): This function will be deleted once the all dependencies on this endpoint are moved to the DAG
-@cloud_sql_to_bq_blueprint.route(
-    "/refresh_bq_schema/<schema_arg>", methods=["GET", "POST"]
-)
-@requires_gae_auth
-def refresh_bq_schema(schema_arg: str) -> Tuple[str, HTTPStatus]:
-    """Performs a full refresh of BigQuery data for a given schema, pulling data from
-    the appropriate CloudSQL Postgres instance.
-
-    On completion, triggers Dataflow pipelines (when necessary), releases the refresh
-    lock and restarts any paused ingest work.
-    """
-    try:
-        schema_type = SchemaType(schema_arg.upper())
-    except ValueError:
-        return (
-            f"Unexpected value for schema_arg: [{schema_arg}]",
-            HTTPStatus.BAD_REQUEST,
-        )
-    if not CloudSqlToBQConfig.is_valid_schema_type(schema_type):
-        return (
-            f"Unsupported schema type: [{schema_type}]",
-            HTTPStatus.BAD_REQUEST,
-        )
-    if schema_type == SchemaType.STATE:
-        return (
-            f"Schema type should only be refreshed through DAG: [{schema_type}]",
-            HTTPStatus.BAD_REQUEST,
-        )
-
-    lock_manager = CloudSqlToBQLockManager()
-
-    try:
-        can_proceed = lock_manager.can_proceed(schema_type)
-    except GCSPseudoLockDoesNotExist as e:
-        logging.exception(e)
-        return (
-            f"Expected lock for [{schema_arg}] BQ refresh to already exist.",
-            HTTPStatus.EXPECTATION_FAILED,
-        )
-
-    if not can_proceed:
-        return (
-            f"Expected to be able to proceed with refresh before this endpoint was "
-            f"called for [{schema_arg}].",
-            HTTPStatus.EXPECTATION_FAILED,
-        )
-
-    federated_bq_schema_refresh(schema_type=schema_type)
-
-    # Unlock export lock when all BQ exports complete
-    lock_manager = CloudSqlToBQLockManager()
-    lock_manager.release_lock(schema_type)
-    logging.info(
-        "Done running refresh for [%s], unlocking Postgres to BigQuery export",
-        schema_type.value,
-    )
-
-    # Kick scheduler to restart ingest
-    if schema_type in {SchemaType.STATE, SchemaType.OPERATIONS}:
-        kick_all_schedulers()
-
-    return "", HTTPStatus.OK
 
 
 @cloud_sql_to_bq_blueprint.route("/acquire_lock/<schema_arg>", methods=["GET", "POST"])
