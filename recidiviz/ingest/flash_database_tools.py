1acaa811a50a519fe58dd1a6559706aa69a7b265
caroletouma <carole@recidiviz.org>
2023-01-05 08:58:10 -0800

    Include Raw Data Sections in Flashing Checklist (Recidiviz/recidiviz-data#16522)
    ## Description of the change
    
    This includes (conditionally) raw data sections to the flashing and
    cancellation checklists if the raw data source is `SECONDARY`.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#12882
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 3071770c69b8b3a103380640e3af0deaf5d4445e

--- recidiviz/ingest/flash_database_tools.py
+++ recidiviz/ingest/flash_database_tools.py
@@ -15,7 +15,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """Utilities for flash database checklist """
+from typing import Iterator
 
+from google.cloud import bigquery
 
 from recidiviz.big_query.big_query_client import BigQueryClient
 from recidiviz.big_query.view_update_manager import (
@@ -26,7 +28,6 @@ from recidiviz.ingest.direct.ingest_view_materialization.instance_ingest_view_co
     InstanceIngestViewContentsImpl,
 )
 from recidiviz.ingest.direct.raw_data.dataset_config import (
-    raw_latest_views_dataset_for_region,
     raw_tables_dataset_for_region,
 )
 from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
@@ -114,14 +115,14 @@ def move_ingest_view_results_between_instances(
     )
 
 
-def move_raw_data_between_instances(
+def copy_raw_data_between_instances(
     state_code: StateCode,
     ingest_instance_source: DirectIngestInstance,
     ingest_instance_destination: DirectIngestInstance,
     big_query_client: BigQueryClient,
 ) -> None:
-    """Move raw data to a BQ dataset that is the opposite ingest instance with no expiration date, deletes source
-    dataset"""
+    """Copy raw data to a BQ dataset that is the opposite ingest instance with no expiration date, but does not delete
+    the source dataset"""
     source_raw_dataset_id = raw_tables_dataset_for_region(
         state_code=state_code, instance=ingest_instance_source
     )
@@ -144,64 +145,36 @@ def move_raw_data_between_instances(
         overwrite_destination_tables=True,
     )
 
-    # Delete original datasets
-    big_query_client.delete_dataset(
-        dataset_ref=big_query_client.dataset_ref_for_id(
-            dataset_id=source_raw_dataset_id
-        ),
-        delete_contents=True,
-    )
 
-
-def delete_contents_in_raw_data_dataset(
+def delete_contents_of_raw_data_tables(
     state_code: StateCode,
     ingest_instance: DirectIngestInstance,
     big_query_client: BigQueryClient,
 ) -> None:
-    """Clears the raw data dataset."""
+    """Deletes the contents of each table within the raw data dataset (but does not delete the tables themselves)."""
     raw_dataset_id = raw_tables_dataset_for_region(
         state_code=state_code, instance=ingest_instance
     )
-    big_query_client.delete_dataset(
-        dataset_ref=big_query_client.dataset_ref_for_id(dataset_id=raw_dataset_id),
-        delete_contents=True,
-    )
+    list_of_tables: Iterator[
+        bigquery.table.TableListItem
+    ] = big_query_client.list_tables(dataset_id=raw_dataset_id)
+
+    for table in list_of_tables:
+        big_query_client.delete_from_table_async(
+            dataset_id=raw_dataset_id, table_id=table.table_id, filter_clause=""
+        )
 
 
-def delete_contents_in_latest_view_dataset(
+def copy_raw_data_to_backup(
     state_code: StateCode,
     ingest_instance: DirectIngestInstance,
     big_query_client: BigQueryClient,
 ) -> None:
-    """Clears the raw data latest view dataset."""
-    latest_view_dataset_id = raw_latest_views_dataset_for_region(
-        state_code=state_code, instance=ingest_instance
-    )
-    big_query_client.delete_dataset(
-        dataset_ref=big_query_client.dataset_ref_for_id(
-            dataset_id=latest_view_dataset_id
-        ),
-        delete_contents=True,
-    )
-
-
-def move_raw_data_to_backup(
-    state_code: StateCode,
-    ingest_instance: DirectIngestInstance,
-    big_query_client: BigQueryClient,
-) -> None:
-    """Copies raw data for a single ingest instance to a backup BQ dataset, deletes source dataset"""
+    """Copies raw data for a single ingest instance to a backup BQ dataset"""
     source_raw_dataset_id = raw_tables_dataset_for_region(
         state_code=state_code, instance=ingest_instance
     )
 
     big_query_client.backup_dataset_tables_if_dataset_exists(
         dataset_id=source_raw_dataset_id
-    )
-
-    big_query_client.delete_dataset(
-        dataset_ref=big_query_client.dataset_ref_for_id(
-            dataset_id=source_raw_dataset_id
-        ),
-        delete_contents=True,
     )
