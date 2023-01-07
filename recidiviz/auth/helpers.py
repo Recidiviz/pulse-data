8d38b90692ffe7050f6117636b0dbfc77ff0e61f
Jen Overgaag <jovergaag@gmail.com>
2023-01-06 16:54:33 -0800

    [Admin panel] Allow users GET endpoint to get single user (Recidiviz/recidiviz-data#17648)
    ## Description of the change
    
    Modified the `/auth/users` endpoint to take an email address as a
    parameter to get a single user.
    
    This will be used by Auth0 when getting permissions for a user.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#17566
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    Co-authored-by: Dana Hoffman <1130694+danawillow@users.noreply.github.com>
    GitOrigin-RevId: 3b46d878429b9f58dd5c77f67a001d677901d563

--- recidiviz/auth/helpers.py
+++ recidiviz/auth/helpers.py
@@ -18,7 +18,33 @@
 
 import base64
 import hashlib
+from typing import Any
 
 
 def generate_user_hash(email: str) -> bytes:
     return base64.b64encode(hashlib.sha256(email.encode("utf-8")).digest())
+
+
+def format_user_info(user: Any) -> dict[str, str]:
+    return {
+        "emailAddress": user.email_address,
+        "stateCode": user.state_code,
+        "externalId": user.external_id,
+        "role": user.role,
+        "district": user.district,
+        "firstName": user.first_name,
+        "lastName": user.last_name,
+        "allowedSupervisionLocationIds": user.district
+        if user.state_code == "US_MO"
+        else "",
+        "allowedSupervisionLocationLevel": "level_1_supervision_location"
+        if user.state_code == "US_MO" and user.district is not None
+        else "",
+        "canAccessLeadershipDashboard": user.can_access_leadership_dashboard,
+        "canAccessCaseTriage": user.can_access_case_triage,
+        "shouldSeeBetaCharts": user.should_see_beta_charts,
+        "routes": user.routes,
+        "featureVariants": user.feature_variants,
+        "blocked": user.blocked,
+        "userHash": user.user_hash,
+    }
