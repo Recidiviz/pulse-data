0105af20a713cf12a8014e473b5172136c2dbf51
brendali121 <brenda@recidiviz.org>
2023-01-25 15:38:38 -0800

    [US_MI] Ingest employment periods (Recidiviz/recidiviz-data#18054)
    ## Description of the change
    
    Ingest employment periods for Michigan
    
    My only note is that there is both a `position` and `occupation` column
    in the raw data and sometimes both are valued, sometimes only one is
    valued, and sometimes neither are valued. But when they're both valued,
    they're mostly the same or closely related. So I'm just arbitrarily
    choosing to use `position` if both are valued.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: b06384aa91f338074144b14292c3dd0996a99f86

---
+++ recidiviz/ingest/direct/regions/us_mi/ingest_views/view_employment_period.py
@@ -0,0 +1,51 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query containing MDOC employment period information."""
+
+from recidiviz.ingest.direct.views.direct_ingest_big_query_view_types import (
+    DirectIngestPreProcessedIngestViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+SELECT
+  emp.offender_booking_id,
+  sequence_number,
+  (DATE(employment_date)) as employment_date,
+  (DATE(termination_date)) as termination_date,
+  employer_name,
+  position,
+  employment_status_id,
+  termination_reason_code,
+  occupation
+FROM {ADH_OFFENDER_EMPLOYMENT} emp
+INNER JOIN {ADH_OFFENDER_BOOKING} book on emp.offender_booking_id = book.offender_booking_id
+
+"""
+
+VIEW_BUILDER = DirectIngestPreProcessedIngestViewBuilder(
+    region="us_mi",
+    ingest_view_name="employment_period",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+    materialize_raw_data_table_views=False,
+    order_by_cols="offender_booking_id, sequence_number",
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
