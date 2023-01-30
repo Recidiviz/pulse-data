f4478106a109b9b909b3fe5657e826d884915b24
Emily Chao (they/them) <emily@recidiviz.org>
2023-01-30 09:29:45 -0800

    Mark SFTP files as discovered if they are not already (Recidiviz/recidiviz-data#18194)
    ## Description of the change
    
    This PR first takes the server files and removes any entries that
    already have a discovered time before inserting all of them into the
    remote file metadata table with the current date as the discovery time.
    This does not mean we remove them from the list to be downloaded because
    they still need to be downloaded, just not inserted into the database.
    
    ## Type of change
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#17333
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [X] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Helper Bot <helperbot@recidiviz.org>
    GitOrigin-RevId: ad8849ccab3afcaa52828e80472205b9ade07952

---
+++ recidiviz/airflow/tests/sftp/mark_files_discovered_sql_query_generator_test.py
@@ -0,0 +1,115 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Unit tests for MarkFilesDiscoveredSqlQueryGenerator"""
+import datetime
+import unittest
+from typing import Set, Tuple
+from unittest.mock import create_autospec
+
+import freezegun
+import pandas as pd
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.context import Context
+
+from recidiviz.airflow.dags.operators.cloud_sql_query_operator import (
+    CloudSqlQueryOperator,
+)
+from recidiviz.airflow.dags.sftp.mark_files_discovered_sql_query_generator import (
+    MarkFilesDiscoveredSqlQueryGenerator,
+)
+
+
+class TestMarkFilesDiscoveredSqlQueryGenerator(unittest.TestCase):
+    """Unit tests for MarkFilesDiscoveredSqlQueryGenerator"""
+
+    def setUp(self) -> None:
+        self.generator = MarkFilesDiscoveredSqlQueryGenerator(
+            region_code="US_XX", filter_downloaded_files_task_id="test_task_id"
+        )
+
+    def tests_generates_exists_sql_correctly(self) -> None:
+        sample_data_set: Set[Tuple[str, int]] = {("file1.csv", 1), ("file2.csv", 1)}
+        expected_query = """
+SELECT remote_file_path, sftp_timestamp FROM direct_ingest_sftp_remote_file_metadata
+ WHERE file_download_time IS NULL AND (remote_file_path, sftp_timestamp) IN (('file1.csv', 1),('file2.csv', 1));"""
+
+        self.assertEqual(
+            self.generator.exists_sql_query(sample_data_set), expected_query
+        )
+
+    @freezegun.freeze_time(datetime.datetime(2023, 1, 26, 0, 0, 0, 0))
+    def tests_generates_insert_sql_correctly(self) -> None:
+        sample_data_set: Set[Tuple[str, int]] = {("file1.csv", 1), ("file2.csv", 1)}
+        expected_query = """
+INSERT INTO direct_ingest_sftp_remote_file_metadata (region_code, remote_file_path, sftp_timestamp, file_discovery_time)
+VALUES
+('US_XX', 'file1.csv', 1, '2023-01-26 00:00:00.000000 UTC'),
+('US_XX', 'file2.csv', 1, '2023-01-26 00:00:00.000000 UTC');"""
+
+        self.assertEqual(
+            self.generator.insert_sql_query(sample_data_set), expected_query
+        )
+
+    @freezegun.freeze_time(datetime.datetime(2023, 1, 26, 0, 0, 0, 0))
+    def test_marks_files_discovered_correctly(self) -> None:
+        mock_operator = create_autospec(CloudSqlQueryOperator)
+        mock_postgres = create_autospec(PostgresHook)
+        mock_context = create_autospec(Context)
+
+        sample_data = [
+            {"remote_file_path": "file1.csv", "sftp_timestamp": 1},
+            {"remote_file_path": "file2.csv", "sftp_timestamp": 2},
+        ]
+
+        mock_operator.xcom_pull.return_value = sample_data
+        mock_postgres.get_pandas_df.return_value = pd.DataFrame(
+            [{"remote_file_path": "file2.csv", "sftp_timestamp": 2}]
+        )
+
+        results = self.generator.execute_postgres_query(
+            mock_operator, mock_postgres, mock_context
+        )
+
+        mock_postgres.run.assert_called_with(
+            """
+INSERT INTO direct_ingest_sftp_remote_file_metadata (region_code, remote_file_path, sftp_timestamp, file_discovery_time)
+VALUES
+('US_XX', 'file1.csv', 1, '2023-01-26 00:00:00.000000 UTC');"""
+        )
+
+        self.assertListEqual(results, sample_data)
+
+    def test_marks_files_discovered_no_files_to_mark(self) -> None:
+        mock_operator = create_autospec(CloudSqlQueryOperator)
+        mock_postgres = create_autospec(PostgresHook)
+        mock_context = create_autospec(Context)
+
+        sample_data = [
+            {"remote_file_path": "file1.csv", "sftp_timestamp": 1},
+            {"remote_file_path": "file2.csv", "sftp_timestamp": 2},
+        ]
+
+        mock_operator.xcom_pull.return_value = sample_data
+        mock_postgres.get_pandas_df.return_value = pd.DataFrame(sample_data)
+
+        results = self.generator.execute_postgres_query(
+            mock_operator, mock_postgres, mock_context
+        )
+
+        mock_postgres.run.assert_not_called()
+
+        self.assertListEqual(results, sample_data)
