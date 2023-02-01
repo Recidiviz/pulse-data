f48672c9585aa98fd72c5d0ad624a6b15f1f4011
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2023-02-01 13:44:11 -0800

    [US_MI][Workflows] Create stubs for early discharge from parole and probation  (Recidiviz/recidiviz-data#18303)
    ## Description of the change
    
    Following the criteria outlined in the copy, stubs were created for
    early discharge from parole/dual and probation.
    
    The following opportunities were drafted, and the associated criteria
    are outlined in the [reasons
    doc](https://paper.dropbox.com/doc/MI-Workflows-Reasons--Bx3YSotHi5jFBkV4F3LGmAGqAQ-dlXEAU8Yu8wCnfLxT9brb):
    `complete_discharge_early_from_parole_dual_supervision_request`
    `complete_discharge_early_from_probation_supervision_request`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6d2537454ac012e84158c7018b5f50b9c333b72a

---
+++ recidiviz/task_eligibility/eligibility_spans/us_mi/complete_discharge_early_from_parole_dual_supervision_request.py
@@ -0,0 +1,71 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Builder for a task eligibility spans view that shows the spans of time during which
+someone in MI is eligible for early discharge from parole or dual supervision.
+"""
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.candidate_populations.general import (
+    parole_dual_supervision_population,
+)
+from recidiviz.task_eligibility.completion_events import early_discharge
+from recidiviz.task_eligibility.criteria.general import (
+    parole_dual_supervision_past_half_original_term,
+    serving_at_least_one_year_on_parole_dual_supervision,
+    supervision_not_past_full_term_completion_date,
+)
+from recidiviz.task_eligibility.criteria.state_specific.us_mi import (
+    no_active_ppo,
+    no_new_ineligible_offenses_on_supervision,
+    no_owi_violation_on_parole_dual_supervision,
+    not_on_intensive_supervision,
+    not_paroled_from_sai,
+    not_serving_ineligible_offenses_on_parole_dual_supervision,
+    parole_dual_supervision_past_early_discharge_date,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Shows the spans of time during which someone in MI is eligible
+to request early discharge from supervision through the parole board.
+"""
+
+VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
+    state_code=StateCode.US_MI,
+    task_name="COMPLETE_DISCHARGE_EARLY_FROM_PAROLE_DUAL_SUPERVISION_REQUEST",
+    description=_DESCRIPTION,
+    candidate_population_view_builder=parole_dual_supervision_population.VIEW_BUILDER,
+    criteria_spans_view_builders=[
+        supervision_not_past_full_term_completion_date.VIEW_BUILDER,
+        serving_at_least_one_year_on_parole_dual_supervision.VIEW_BUILDER,
+        parole_dual_supervision_past_early_discharge_date.VIEW_BUILDER,
+        not_serving_ineligible_offenses_on_parole_dual_supervision.VIEW_BUILDER,
+        no_new_ineligible_offenses_on_supervision.VIEW_BUILDER,
+        not_paroled_from_sai.VIEW_BUILDER,
+        not_on_intensive_supervision.VIEW_BUILDER,
+        no_active_ppo.VIEW_BUILDER,
+        no_owi_violation_on_parole_dual_supervision.VIEW_BUILDER,
+        parole_dual_supervision_past_half_original_term.VIEW_BUILDER,
+    ],
+    completion_event_builder=early_discharge.VIEW_BUILDER,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
---
+++ recidiviz/task_eligibility/eligibility_spans/us_mi/complete_discharge_early_from_probation_supervision_request.py
@@ -0,0 +1,59 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Builder for a task eligibility spans view that shows the spans of time during which
+someone in MI is eligible for early discharge from probation supervision.
+"""
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.candidate_populations.general import (
+    probation_supervision_population,
+)
+from recidiviz.task_eligibility.completion_events import early_discharge
+from recidiviz.task_eligibility.criteria.general import (
+    supervision_past_half_full_term_release_date,
+)
+from recidiviz.task_eligibility.criteria.state_specific.us_mi import (
+    no_active_ppo,
+    no_new_ineligible_offenses_on_supervision,
+    not_serving_ineligible_offenses_on_probation_supervision,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Shows the spans of time during which someone in MI is eligible
+to request early discharge from probation supervision.
+"""
+
+VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
+    state_code=StateCode.US_MI,
+    task_name="COMPLETE_DISCHARGE_EARLY_FROM_PROBATION_SUPERVISION_REQUEST",
+    description=_DESCRIPTION,
+    candidate_population_view_builder=probation_supervision_population.VIEW_BUILDER,
+    criteria_spans_view_builders=[
+        not_serving_ineligible_offenses_on_probation_supervision.VIEW_BUILDER,
+        no_active_ppo.VIEW_BUILDER,
+        no_new_ineligible_offenses_on_supervision.VIEW_BUILDER,
+        supervision_past_half_full_term_release_date.VIEW_BUILDER,
+    ],
+    completion_event_builder=early_discharge.VIEW_BUILDER,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
