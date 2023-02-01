144820fdc6715c64571dcb1eb5af465845e5988d
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2023-01-31 19:45:07 -0600

    [US_ME] [Workflows] Updated eligibility spans so that folks are eligible 3 months before their actual eligibility date (Recidiviz/recidiviz-data#18206)
    ## Description of the change
    
    > Folks interested in SCCP can apply (send paperwork) 3 months before
    their eligibility date. Before this PR, people were labeled as
    uneligible until the actual date arrived. This PR hopes to change this
    so that they are eligible 3 months before that date arrives, this way
    they can start their paperwork 3 months in advance. **The actual
    eligibility date, which is displayed on our workflows tool, will not be
    changed.**
    
    This will move people previously classified as 'Almost Eligible' to
    'Eligible'
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#17945
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 2e058421aee0db43a0f65634362d921f3087cc6e

--- recidiviz/task_eligibility/criteria/state_specific/us_me/x_months_remaining_on_sentence.py
+++ recidiviz/task_eligibility/criteria/state_specific/us_me/x_months_remaining_on_sentence.py
@@ -20,6 +20,13 @@ which clients are 30/24 months away from expected release date. When
 average state-wide case load is lower than 90, residents are allowed
 to be released within their last 30 months ; otherwise, residents are only
 allowed to be released 24 months before their sentence ends.
+
+Since folks can start their paperwork 3 months before they are 30/24 months
+away from their release date, we differentiate
+between the time people can start their paperwork ("critical_date":
+3 months before they are 30/24 months away) and the actual time when
+they can be released from prison (real_eligible_date: when they are 30/24
+months away from their release date)
 """
 
 from recidiviz.calculator.query.bq_utils import nonnull_end_date_clause
@@ -51,16 +58,23 @@ from recidiviz.utils.metadata import local_project_id_override
 _CRITERIA_NAME = "US_ME_X_MONTHS_REMAINING_ON_SENTENCE"
 
 _DESCRIPTION = """Defines a criteria view that shows spans of time for
-which clients are 30/24 months away from expected release date. When 
+which clients are 30/24 months away from expected release date. When
 average state-wide case load is lower than 90, residents are allowed
-to be released within their last 30 months ; otherwise, residents are only 
+to be released within their last 30 months ; otherwise, residents are only
 allowed to be released 24 months before their sentence ends.
+
+Since folks can start their paperwork 3 months before they are 30/24 months
+away from their release date, we differentiate
+between the time people can start their paperwork ("critical_date":
+3 months before they are 30/24 months away) and the actual time when
+they can be released from prison (real_eligible_date: when they are 30/24
+months away from their release date)
 """
 
 _QUERY_TEMPLATE = f"""
 WITH case_load AS (
   -- Current ratio of supervision clients/supervision officers
-  SELECT 
+  SELECT
     supervision_clients/supervision_officers AS curr_supervision_client_ratio
   FROM (
         SELECT
@@ -68,49 +82,64 @@ WITH case_load AS (
             COUNT(DISTINCT person_id) AS supervision_clients
         FROM `{{project_id}}.{{sessions_dataset}}.supervision_officer_sessions_materialized`
         WHERE state_code = 'US_ME'
-            AND CURRENT_DATE('US/Pacific')  <= {nonnull_end_date_clause('end_date')} 
+            AND CURRENT_DATE('US/Pacific')  <= {nonnull_end_date_clause('end_date')}
             -- TODO(#16408) we need to confirm who to subset here
-            AND supervising_officer_external_id != '0' -- Seems to be old/migration data
-            -- TODO(#16823) should drop these 0s in normalization maybe?
         )
 ),
 {cis_319_term_cte()},
-term_cte_crit_date AS (
+term_crit_date AS (
     -- Calculate the critical date as a function of the statewide case load
-    SELECT 
+    SELECT
         * EXCEPT(curr_supervision_client_ratio),
         CASE
             WHEN curr_supervision_client_ratio > 90 THEN DATE_SUB(end_date, INTERVAL 24 MONTH)
-            ELSE DATE_SUB(end_date, INTERVAL 30 MONTH) 
+            ELSE DATE_SUB(end_date, INTERVAL 30 MONTH)
         END critical_date,
     FROM term_cte, case_load
 ),
-{create_sub_sessions_with_attributes('term_cte_crit_date')},
+term_crit_date_plus_real AS(
+    SELECT
+        * EXCEPT (critical_date),
+        critical_date AS real_eligible_date,
+        -- folks can start their paperwork 3 months before their eligibility date
+        DATE_SUB(critical_date, INTERVAL 3 MONTH) AS critical_date,
+    FROM term_crit_date
+    WHERE start_date != {nonnull_end_date_clause('end_date')}
+),
+{create_sub_sessions_with_attributes('term_crit_date_plus_real')},
 critical_date_spans AS (
     {cis_319_after_csswa()}
 ),
+
+save_real_eligible_date AS (
+    SELECT
+        DISTINCT person_id, state_code, critical_date, real_eligible_date
+    FROM critical_date_spans
+),
 {critical_date_has_passed_spans_cte()}
 SELECT
-    state_code,
-    person_id,
-    CASE                                            
-        WHEN (start_date IS NULL) AND (critical_date_has_passed) THEN critical_date  
+    cd.state_code,
+    cd.person_id,
+    CASE
+        WHEN (start_date IS NULL) AND (critical_date_has_passed) THEN cd.critical_date
                                 -- When there was no intake date in CIS_319_TERM,
-                                -- start_date of our subsession is NULL for the 
+                                -- start_date of our subsession is NULL for the
                                 -- period for which the criteria is met. But
-                                -- we know the end date and we can calculate the 
-                                -- start_date (eligible_date) 
-        ELSE start_date                             
-    END start_date,   
-        -- if the most recent subsession is True, then end_date should be NULL                              
-    IF((ROW_NUMBER() OVER (PARTITION BY person_id, state_code
-                           ORDER BY start_date DESC) =  1) 
-            AND (critical_date_has_passed), 
-        NULL, 
-        end_date) AS end_date,                    
+                                -- we know the end date and we can calculate the
+                                -- start_date (eligible_date)
+        ELSE start_date
+    END start_date,
+        -- if the most recent subsession is True, then end_date should be NULL
+    IF((ROW_NUMBER() OVER (PARTITION BY cd.person_id, cd.state_code
+                           ORDER BY start_date DESC) =  1)
+            AND (critical_date_has_passed),
+        NULL,
+        end_date) AS end_date,
     critical_date_has_passed AS meets_criteria,
-    TO_JSON(STRUCT(critical_date AS eligible_date)) AS reason,
-FROM critical_date_has_passed_spans
+    TO_JSON(STRUCT(xps.real_eligible_date AS eligible_date)) AS reason,
+FROM critical_date_has_passed_spans cd
+LEFT JOIN save_real_eligible_date xps
+    USING (person_id, state_code, critical_date)
 """
 
 VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
