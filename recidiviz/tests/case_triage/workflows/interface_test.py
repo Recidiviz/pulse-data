d70db7fa1717b76c9ed418f1497255ccf7256d67
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2023-02-07 10:38:23 -0800

    [Workflows] Add recidiviz to condition for using TOMIS test-ids (Recidiviz/recidiviz-data#18448)
    GitOrigin-RevId: fd0c70b115bea7f561ffde223c267bd1dfd848e9

--- recidiviz/tests/case_triage/workflows/interface_test.py
+++ recidiviz/tests/case_triage/workflows/interface_test.py
@@ -66,7 +66,7 @@ class TestWorkflowsInterface(TestCase):
             # update_document is called 5 times: page 1 in progress, page 1 success, page 2 in progress, page 2 success,
             # and entire note status success
             # TODO(#2938): Explore more granular testing for firestore calls
-            assert mock_client.update_document.call_count == 5
+            self.assertEqual(mock_client.update_document.call_count, 5)
 
     @patch("recidiviz.case_triage.workflows.interface.get_secret")
     @patch("recidiviz.case_triage.workflows.interface.FirestoreClientImpl")
@@ -81,7 +81,7 @@ class TestWorkflowsInterface(TestCase):
                 WorkflowsUsTnExternalRequestInterface().insert_tepe_contact_note(
                     PERSON_EXTERNAL_ID, USER_ID, CONTACT_NOTE_DATE_TIME, {1: []}
                 )
-            assert mock_client.update_document.call_count == 3
+            self.assertEqual(mock_client.update_document.call_count, 3)
 
     @patch("requests.put")
     @patch("recidiviz.case_triage.workflows.interface.get_secret")
@@ -100,6 +100,31 @@ class TestWorkflowsInterface(TestCase):
         mock_client.update_document.assert_called_once()
 
     @patch("recidiviz.case_triage.workflows.interface.get_secret")
+    @patch("recidiviz.case_triage.workflows.interface.FirestoreClientImpl")
+    @patch("recidiviz.case_triage.workflows.interface.in_gcp_production")
+    def test_insert_contact_note_prod_and_recidiviz(
+        self,
+        mock_in_prod: MagicMock,
+        mock_client: MagicMock,
+        mock_get_secret: MagicMock,
+    ) -> None:
+        response_json = {"status": "OK"}
+        mock_get_secret.return_value = self.fake_url
+        mock_client = mock_client.return_value
+        mock_in_prod.return_value = True
+        with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
+            rsps.add(responses.PUT, self.fake_url, json=response_json)
+            WorkflowsUsTnExternalRequestInterface().insert_tepe_contact_note(
+                PERSON_EXTERNAL_ID, "RECIDIVIZ", CONTACT_NOTE_DATE_TIME, {1: [], 2: []}
+            )
+
+            # Check third call to get_secret gets test url
+            mock_get_secret.mock_calls[2].assert_called_with(
+                "workflows_us_tn_insert_contact_note_test_url"
+            )
+            self.assertEqual(mock_client.update_document.call_count, 5)
+
+    @patch("recidiviz.case_triage.workflows.interface.get_secret")
     @patch("recidiviz.case_triage.workflows.interface.in_gcp_production")
     @freezegun.freeze_time("2000-12-30")
     def test_workflows_us_tn_write_tepe_note_to_tomis_request_format_request(
@@ -110,6 +135,38 @@ class TestWorkflowsInterface(TestCase):
         request = WorkflowsUsTnWriteTEPENoteToTomisRequest(
             offender_id=PERSON_EXTERNAL_ID,
             user_id=USER_ID,
+            contact_note_date_time=datetime.datetime.now().isoformat(),
+            contact_sequence_number=1,
+            comments=["line 1", "line 2"],
+        )
+
+        actual = request.format_request()
+
+        expected = json.dumps(
+            {
+                "ContactNoteDateTime": "2000-12-30T00:00:00",
+                "OffenderId": "test-id",
+                "UserId": "test-id",
+                "ContactTypeCode1": "TEPE",
+                "ContactSequenceNumber": 1,
+                "Comment1": "line 1",
+                "Comment2": "line 2",
+            }
+        )
+
+        self.assertEqual(actual, expected)
+
+    @patch("recidiviz.case_triage.workflows.interface.get_secret")
+    @patch("recidiviz.case_triage.workflows.interface.in_gcp_production")
+    @freezegun.freeze_time("2000-12-30")
+    def test_workflows_us_tn_write_tepe_note_to_tomis_request_format_request_recidiviz(
+        self, mock_in_prod: MagicMock, mock_secret: MagicMock
+    ) -> None:
+        mock_in_prod.return_value = True
+        mock_secret.return_value = "test-id"
+        request = WorkflowsUsTnWriteTEPENoteToTomisRequest(
+            offender_id=PERSON_EXTERNAL_ID,
+            user_id="RECIDIVIZ",
             contact_note_date_time=datetime.datetime.now().isoformat(),
             contact_sequence_number=1,
             comments=["line 1", "line 2"],
