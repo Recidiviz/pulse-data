f0d592a9eb31ac3eebf9ec956fe1b4c62383b258
Michelle Orden <82831800+morden35@users.noreply.github.com>
2023-02-08 10:09:11 -0600

    [Justice Counts] excepting KeyError and raising Missing Column exception (Recidiviz/recidiviz-data#18399)
    ## Description of the change
    
    Rather than excepting a TypeError and continuing, excepting both
    KeyError and TypeError. This prevents a 500 when uploading excel file
    without 'value' column. Here is what the page looks like when uploading
    a file without 'value' column:
    <img width="1432" alt="Screenshot 2023-02-07 at 2 49 12 PM"
    src="https://user-images.githubusercontent.com/82831800/217363425-66332424-c0a4-4b90-b4af-7a2dc164d7ac.png">
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#16572
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 29dc914902392e7c4598f7f71f306e3291ad1f6d

--- recidiviz/justice_counts/bulk_upload/bulk_upload.py
+++ recidiviz/justice_counts/bulk_upload/bulk_upload.py
@@ -158,12 +158,13 @@ class BulkUploader:
             # Drop any rows that contain any NaN values
             try:
                 df = df.dropna(axis=0, how="any", subset=["value"])
-            except TypeError:
+            except (KeyError, TypeError):
                 # We will be in this case if the value column is missing,
                 # and it's safe to ignore the error because we'll raise
                 # an error about the missing value column later on in
                 # _get_column_value.
                 pass
+
             # Convert dataframe to a list of dictionaries
             rows = df.to_dict("records")
             metric_key_to_datapoint_jsons, metric_key_to_errors = self._upload_rows(
