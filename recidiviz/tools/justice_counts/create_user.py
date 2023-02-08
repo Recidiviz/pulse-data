171758ee2204fd9d3c8288dea71da39fb8cedf55
lilidworkin <99899591+lilidworkin@users.noreply.github.com>
2023-02-08 17:08:03 -0500

    [Justice Counts] Scripts to create/update users (Recidiviz/recidiviz-data#18506)
    ## Description of the change
    
    Adding two scripts that we can use to manage users in the staging and
    prod DBs:
    * create_user: Add users to the DB. They MUST first have a corresponding
    account in Auth0.
    * update_user_role: Update the role of given user+agency. Both user and
    agency must already exist in the DB. If the user is not already part of
    the agency, they are added.
    
    I tested a bit manually, e.g.:
    
    ```
    (recidiviz-data) lilidworkin@Lilis-MBP:~/recidiviz/recidiviz-data$ python -m recidiviz.tools.justice_counts.update_user_role --email=lilidworkin@gmail.com --agency="Law Enforcement" --role=JUSTICE_COUNTS_ADMIN --project-id=recidiviz-staging --dry-run=true
    INFO:root:Test environment, proceeding.
    INFO:root:Starting Cloud SQL Proxy container for recidiviz-staging:us-east1:dev-justice-counts-data...
    INFO:root:Started Cloud SQL Proxy container with name: angry_keller
    INFO:root:Test environment, proceeding.
    INFO:root:Test environment, proceeding.
    INFO:root:User is currently a member of this agency with role None
    INFO:root:Updating role to UserAccountRole.JUSTICE_COUNTS_ADMIN
    INFO:root:Killing Cloud SQL Proxy container on :5440 with name: angry_keller
    ```
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: ae971236722beb1711d8509aed91ab1bb4a085bc

---
+++ recidiviz/tools/justice_counts/create_user.py
@@ -0,0 +1,121 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+"""
+Creates a new UserAccount in our DB for given email address.
+
+python -m recidiviz.tools.justice_counts.create_user \
+  --email=EMAIL_ADDRESS \
+  --project-id=PROJECT_ID \
+  --dry-run=true
+"""
+
+import argparse
+import logging
+
+from recidiviz.admin_panel.routes.justice_counts_tools import _get_auth0_client
+from recidiviz.justice_counts.user_account import UserAccountInterface
+from recidiviz.persistence.database.schema.justice_counts import schema
+from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.persistence.database.session_factory import SessionFactory
+from recidiviz.persistence.database.sqlalchemy_database_key import SQLAlchemyDatabaseKey
+from recidiviz.tools.postgres.cloudsql_proxy_control import cloudsql_proxy_control
+from recidiviz.utils.environment import GCP_PROJECT_PRODUCTION, GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+from recidiviz.utils.params import str_to_bool
+
+logger = logging.getLogger(__name__)
+
+
+def create_parser() -> argparse.ArgumentParser:
+    """Returns an argument parser for the script."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--project-id",
+        choices=[GCP_PROJECT_STAGING, GCP_PROJECT_PRODUCTION],
+        help="Used to select which GCP project in which to run this script.",
+        required=True,
+    )
+    parser.add_argument(
+        "--email",
+        type=str,
+        help="Email address of the user to create.",
+        required=True,
+    )
+    parser.add_argument("--dry-run", type=str_to_bool, default=True)
+    return parser
+
+
+def create_user_account(dry_run: bool, email: str) -> None:
+    """
+    Creates a new UserAccount in our DB for given email address.
+    NOTE: There must already be an Auth0 account for this email address.
+    """
+    schema_type = SchemaType.JUSTICE_COUNTS
+    database_key = SQLAlchemyDatabaseKey.for_schema(schema_type)
+
+    auth0_client = _get_auth0_client()
+    matching_users = auth0_client.get_all_users_by_email_addresses(
+        email_addresses=[email]
+    )
+    if len(matching_users) != 1:
+        raise ValueError(f"{len(matching_users)} found in Auth0.")
+
+    matching_user = matching_users[0]
+    name = matching_user.get("name")
+    auth0_user_id = matching_user["user_id"]
+
+    logging.info(
+        "Found matching user in Auth0 with auth0_user_id=%s and name=%s",
+        auth0_user_id,
+        name,
+    )
+
+    with cloudsql_proxy_control.connection(schema_type=schema_type):
+        with SessionFactory.for_proxy(database_key) as session:
+            db_user = UserAccountInterface.get_user_by_email(
+                session=session, email=email
+            )
+            if db_user:
+                raise ValueError("User with this email already exists in our DB.")
+
+            user = schema.UserAccount(
+                name=name,
+                auth0_user_id=auth0_user_id,
+                email=email,
+            )
+
+            logger.info(
+                "Creating new user with name=%s, auth0_user_id=%s, and email=%s",
+                name,
+                auth0_user_id,
+                email,
+            )
+
+            if not dry_run:
+                session.add(user)
+                session.commit()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    args = create_parser().parse_args()
+    with local_project_id_override(args.project_id):
+        create_user_account(
+            email=args.email,
+            dry_run=args.dry_run,
+        )
