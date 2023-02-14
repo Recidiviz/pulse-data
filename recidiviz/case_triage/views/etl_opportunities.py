9af4e809269d9fba6b5226a183d41d678cd843cd
Dan Hansen <dan@recidiviz.org>
2023-02-13 16:25:13 -0800

    [Case Triage] Drop unused LSU / Earned Discharge opportunities (Recidiviz/recidiviz-data#18661)
    ## Description of the change
    
    These views are going over our BigQuery resource consumption and are
    currently unused, so I'm removing them to unblock view materialization.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#18616
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 42f2504c44731a10c5992cf0924061ce2089a448

--- recidiviz/case_triage/views/etl_opportunities.py
+++ recidiviz/case_triage/views/etl_opportunities.py
@@ -55,65 +55,6 @@ WITH overdue_downgrades AS (
     )
     AND recommended_supervision_downgrade_level IS NOT NULL
 ),
-earned_discharge_eligible AS (
-  SELECT
-    state_code,
-    supervising_officer_external_id,
-    person_external_id,
-    '{OpportunityType.EARLY_DISCHARGE.value}' AS opportunity_type,
-    TO_JSON_STRING(STRUCT(
-        num_open_earned_discharge_requests AS openEarnedDischargeRequests,
-        days_served AS daysServed,
-        prop_sentence_served AS proportionSentenceServed,
-        supervision_level AS currentSupervisionLevel,
-        days_since_last_positive_urine_analysis_date AS daysSincePositiveUA,
-        positive_urine_analysis_results_past_year_count AS countPositiveUAOneYear,
-        is_employed AS hasEmployment,
-        last_verified_employment_date AS latestVerifiedEmploymentDate,
-        days_employed AS daysSinceEmploymentBegan,
-        days_employed_in_session AS daysSinceEmploymentBeganOnSupervision,
-        critical_contacts_count AS countCriticalContacts
-        )) AS opportunity_metadata
-  FROM 
-    `{{project_id}}.{{case_triage_dataset}}.client_eligibility_criteria_materialized`
-  WHERE 
-    state_code = 'US_ID'
-    # TODO(#7303): Add treatment completion as an additional blocking criteria
-    AND case_type = 'GENERAL'
-    AND supervision_type NOT IN ('INTERNAL_UNKNOWN', 'INFORMAL_PROBATION')
-    AND days_served >= 365
-    AND num_open_earned_discharge_requests = 0
-    AND supervision_level IN ('MINIMUM', 'MEDIUM')
-    AND critical_contacts_count = 0
-),
-limited_supervision_eligible AS (
-  SELECT
-    state_code,
-    supervising_officer_external_id,
-    person_external_id,
-    '{OpportunityType.LIMITED_SUPERVISION_UNIT.value}' AS opportunity_type,
-    TO_JSON_STRING(STRUCT(
-        days_served AS daysServed,
-        prop_sentence_served AS proportionSentenceServed,
-        supervision_level AS currentSupervisionLevel,
-        days_since_last_positive_urine_analysis_date AS daysSincePositiveUA,
-        positive_urine_analysis_results_past_year_count AS countPositiveUAOneYear,
-        is_employed AS hasEmployment,
-        last_verified_employment_date AS latestVerifiedEmploymentDate,
-        days_employed AS daysSinceEmploymentBegan,
-        days_employed_in_session AS daysSinceEmploymentBeganOnSupervision,
-        critical_contacts_count AS countCriticalContacts
-        )) AS opportunity_metadata  
-  FROM
-    `{{project_id}}.{{case_triage_dataset}}.client_eligibility_criteria_materialized`
-  WHERE 
-    state_code = 'US_ID'
-    # TODO(#7303): Add treatment completion as an additional blocking criteria
-    AND case_type = 'GENERAL'
-    AND supervision_type NOT IN ('INTERNAL_UNKNOWN', 'INFORMAL_PROBATION')
-    AND supervision_level = 'MINIMUM'
-    AND days_at_current_supervision_level >= 365 
-),
 export_time AS (
   SELECT CURRENT_TIMESTAMP AS exported_at
 ),
@@ -122,16 +63,6 @@ unioned_results AS (
     *
   FROM
     overdue_downgrades
-  UNION ALL
-  SELECT
-    *
-  FROM
-    earned_discharge_eligible
-  UNION ALL
-  SELECT
-    *
-  FROM
-    limited_supervision_eligible
 )
 SELECT
   {{columns}}
