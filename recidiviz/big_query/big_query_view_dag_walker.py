a3c0cd478d08994a9638533b78c52cb25802575e
jmielke-recidiviz <118864563+jmielke-recidiviz@users.noreply.github.com>
2023-02-14 18:30:44 -0700

    [DagWalker] Update processing/processed set membership check (Recidiviz/recidiviz-data#18660)
    ## Description of the change
    
    Problem:
    ---
    - While processing adjacent nodes, DagWalker checks to see if nodes
    already exist in processing or processed sets
    - But the sets are made of addresses instead of nodes
    
    Solution:
    ---
    - Check if the associated view address is in the processing or processed
    sets
    
    Notes:
    ---
    - Logically, it seems these conditions will not be hit, so we throw if they are, as that indicates an error in programming or understanding
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: ab142eeb6bb40cb1dd2ecc787c483d3b4486272a

--- recidiviz/big_query/big_query_view_dag_walker.py
+++ recidiviz/big_query/big_query_view_dag_walker.py
@@ -543,12 +543,17 @@ class BigQueryViewDagWalker:
                     )
                     for adjacent_address in adjacent_addresses:
                         adjacent_node = self.nodes_by_address[adjacent_address]
-                        if adjacent_node in processed or adjacent_node in queue:
+                        if (
+                            adjacent_node.view.address in processed
+                            or adjacent_node in queue
+                        ):
                             raise ValueError(
                                 f"Unexpected situation where adjacent node has already been processed: {adjacent_address}"
                             )
-                        if adjacent_node in processing:
-                            continue
+                        if adjacent_node.view.address in processing:
+                            raise ValueError(
+                                f"Unexpected situation where adjacent node is already processing: {adjacent_address}"
+                            )
 
                         previous_level_all_processed = True
                         previous_level_results = {}
