80c5d9c2943523a656203b85ad88573cf4889e1c
Daniela Schiano di Cola <daniela@recidiviz.org>
2023-02-16 11:26:34 -0800

    [Workflows]Add us_ix_supervision_tasks_record query (Recidiviz/recidiviz-data#18713)
    ## Description of the change
    
    This adds a `us_ix_supervision_tasks_record` query for Workflows US_IX
    supervision tasks feature.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#18568
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: cec9de1848acb10993322995a802c281de597116

---
+++ recidiviz/calculator/query/state/views/workflows/firestore/us_ix_supervision_tasks_record.py
@@ -0,0 +1,146 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query for supervision tasks that are either overdue or upcoming within 30 days for Idaho"""
+from typing import Optional
+
+import attr
+
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+US_IX_SUPERVISION_TASKS_RECORD_VIEW_NAME = "us_ix_supervision_tasks_record"
+
+US_IX_SUPERVISION_TASKS_RECORD_DESCRIPTION = """
+    View containing tasks that are either overdue or due within the next 30 days for each client on supervision in 
+    US_IX.
+"""
+
+
+@attr.s
+class UsIxSupervisionTaskQueryConfig:
+    due_date_column: str = attr.ib()
+    task_details_struct: str = attr.ib()
+    task_details_joins: Optional[str] = attr.ib(default="")
+
+
+CLIENT_RECORD_JOIN = """
+    INNER JOIN `{project_id}.{workflows_views}.client_record_materialized` client
+    USING(person_external_id)
+"""
+
+ASSESSMENT_SCORE_JOIN = """
+    LEFT JOIN `{project_id}.{sessions}.assessment_score_sessions_materialized` ss 
+    USING(person_id)
+"""
+
+SUPERVISION_TASK_CONFIGS = [
+    UsIxSupervisionTaskQueryConfig(
+        due_date_column="next_recommended_home_visit_date",
+        task_details_struct="""STRUCT(
+              'homeVisit' as type,
+              MAX(next_recommended_home_visit_date) AS due_date,
+              STRUCT(MAX(client.supervision_level) AS supervision_level, MAX(client.address) AS current_address) AS details
+            )
+        """,
+    ),
+    UsIxSupervisionTaskQueryConfig(
+        due_date_column="next_recommended_face_to_face_date",
+        task_details_struct="""STRUCT(
+              'contact' as type,
+              MAX(next_recommended_face_to_face_date) AS due_date,
+              STRUCT(MAX(client.supervision_level) AS supervision_level) AS details
+            )
+        """,
+    ),
+    UsIxSupervisionTaskQueryConfig(
+        due_date_column="next_recommended_assessment_date",
+        task_details_struct="""STRUCT(
+          'assessment' as type,
+          MAX(next_recommended_assessment_date) AS due_date,
+          STRUCT(MAX(ss.assessment_level_raw_text) AS risk_level, MAX(ss.assessment_date) AS last_assessed_on) AS details
+          )
+        """,
+        task_details_joins="\n".join([ASSESSMENT_SCORE_JOIN]),
+    ),
+]
+
+
+def get_case_compliance_task_ctes() -> str:
+    """This CTE returns a union of each supervision task and each state_code defined in the
+    SupervisionTaskQueryConfig list."""
+    cte_body = "\n        UNION ALL\n".join(
+        [
+            f"""
+            SELECT
+                cc.person_external_id,
+                cc.state_code,
+                MAX(client.officer_id) AS officer_id,
+                IF(
+                    MAX({config.due_date_column}) <= DATE_ADD(CURRENT_DATE('US/Eastern'), INTERVAL 1 MONTH),
+                    TO_JSON({config.task_details_struct}),
+                    NULL
+                ) AS task
+            FROM `{{project_id}}.{{dataflow_metrics_materialized}}.most_recent_supervision_case_compliance_metrics_materialized` cc
+
+            INNER JOIN `{{project_id}}.{{workflows_views}}.client_record_materialized` client
+            USING(person_external_id)
+    
+            {config.task_details_joins}
+            
+            WHERE cc.state_code = 'US_IX'
+            AND date_of_evaluation = (
+                SELECT MAX(date_of_evaluation)
+                FROM `{{project_id}}.{{dataflow_metrics_materialized}}.most_recent_supervision_case_compliance_metrics_materialized` 
+            )
+            AND {config.due_date_column} IS NOT NULL
+            GROUP BY 1,2
+            """
+            for config in SUPERVISION_TASK_CONFIGS
+        ]
+    )
+    return cte_body
+
+
+US_IX_SUPERVISION_TASKS_RECORD_QUERY_TEMPLATE = f"""
+    WITH all_supervision_tasks AS ({get_case_compliance_task_ctes()})
+
+    SELECT
+        t.person_external_id,
+        t.state_code,
+        t.officer_id,
+        ARRAY_AGG(task IGNORE NULLS) AS tasks
+    FROM all_supervision_tasks t
+    WHERE task IS NOT NULL
+    GROUP BY 1,2,3
+"""
+
+US_IX_SUPERVISION_TASKS_RECORD_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=dataset_config.WORKFLOWS_VIEWS_DATASET,
+    view_id=US_IX_SUPERVISION_TASKS_RECORD_VIEW_NAME,
+    view_query_template=US_IX_SUPERVISION_TASKS_RECORD_QUERY_TEMPLATE,
+    description=US_IX_SUPERVISION_TASKS_RECORD_DESCRIPTION,
+    dataflow_metrics_materialized=dataset_config.DATAFLOW_METRICS_MATERIALIZED_DATASET,
+    workflows_views=dataset_config.WORKFLOWS_VIEWS_DATASET,
+    sessions=dataset_config.SESSIONS_DATASET,
+    should_materialize=True,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        US_IX_SUPERVISION_TASKS_RECORD_VIEW_BUILDER.build_and_print()
