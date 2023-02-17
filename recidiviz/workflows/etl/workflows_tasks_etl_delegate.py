810ef7cb5736fe1ad88a53fa0b4b85613756c8e3
Daniela Schiano di Cola <daniela@recidiviz.org>
2023-02-17 11:33:57 -0800

    [Workflows] Add tasks ETL delegate (Recidiviz/recidiviz-data#18761)
    ## Description of the change
    
    This adds the class `WorkflowsTasksETLDelegate` and sets up exports for
    `us_ix_supervision_tasks_record` and imports to Firestore.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#18569
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 2c25d2246dafc049a26b97609dc750d6c665999e

---
+++ recidiviz/workflows/etl/workflows_tasks_etl_delegate.py
@@ -0,0 +1,66 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2022 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""Delegate class to ETL Supervision Tasks records for Workflows into Firestore."""
+import json
+from typing import Any, Dict, List, Optional, Tuple
+
+from recidiviz.common.common_utils import convert_nested_dictionary_keys
+from recidiviz.common.constants.states import StateCode
+from recidiviz.common.str_field_utils import snake_to_camel
+from recidiviz.workflows.etl.workflows_etl_delegate import WorkflowsFirestoreETLDelegate
+
+
+class WorkflowsTasksETLDelegate(WorkflowsFirestoreETLDelegate):
+    """Generic delegate for loading Workflows' tasks records into Firestore."""
+
+    def __init__(self, state_code: StateCode):
+        super().__init__(state_code)
+
+    @property
+    def COLLECTION_BY_FILENAME(self) -> Dict[str, str]:
+        return {"us_ix_supervision_tasks_record.json": "US-ID_supervisionTasks"}
+
+    def get_supported_files(self) -> List[str]:
+        return ["us_ix_supervision_tasks_record.json"]
+
+    @property
+    def timestamp_key(self) -> str:
+        """Name of the key this delegate will insert into each document to record when it was loaded."""
+        return "__loadedAt"
+
+    def filepath_url(self, filename: str) -> str:
+        return f"gs://{self.get_filepath(filename).abs_path()}"
+
+    def format_state_code(self, state_code: str) -> str:
+        if state_code == "US_IX":
+            return "US_ID"
+        return state_code
+
+    def transform_row(self, row: str) -> Tuple[Optional[str], Optional[dict]]:
+        data = json.loads(row)
+
+        new_document: dict[str, Any] = {
+            "externalId": data["person_external_id"],
+            "officerId": data.get("officer_id"),
+            "stateCode": self.format_state_code(data["state_code"]),
+            "tasks": [
+                convert_nested_dictionary_keys(task, snake_to_camel)
+                for task in data.get("tasks")
+            ],
+        }
+
+        return data["person_external_id"], new_document
