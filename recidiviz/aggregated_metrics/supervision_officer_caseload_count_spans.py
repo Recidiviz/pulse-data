4b209237a289d83fd004ac8c447e3c738c866908
mayukas <30325262+mayukas@users.noreply.github.com>
2023-02-24 10:19:50 -0500

    Calculate caseload count spans and caseload coverage over analysis period for supervision officers (Recidiviz/recidiviz-data#18720)
    ## Description of the change
    
    This change creates a new spans view called
    `supervision_officer_caseload_count_spans` in `aggregated_metrics`,
    indexed on supervision officer id, where the start of a new session is
    triggered by a change in the officer's caseload size. This
    caseload_count_spans view is used to generate another metric called
    `prop_period_with_caseload` for supervision officers, representing the
    proportion an analysis period over which an officer has over N clients.
    (For now, N = 10 for all states, though future research may call for a
    different threshold). This metric can be used to filter officers in the
    outliers analysis who have not been observed with a valid caseload for a
    significant enough amount of time, which often skews average daily
    population.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Related to Recidiviz/recidiviz-data#18711
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: mayukas <mayuka@stanford.edu>
    GitOrigin-RevId: 98bae3689ed81a6e5209adf0e5537be33c8b5fa1

---
+++ recidiviz/aggregated_metrics/supervision_officer_caseload_count_spans.py
@@ -0,0 +1,92 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""View of continuous spans at a given client count for the specified assignment sessions"""
+
+from recidiviz.aggregated_metrics.dataset_config import AGGREGATED_METRICS_DATASET_ID
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_VIEW_NAME = "supervision_officer_caseload_count_spans"
+
+_VIEW_DESCRIPTION = (
+    "Spans defined per officer based on periods of a continuous caseload size"
+)
+
+_QUERY_TEMPLATE = """
+WITH assignment_spans AS (
+    SELECT
+        state_code,
+        officer_id,
+        person_id,
+        assignment_date AS start_date,
+        end_date
+    FROM
+        `{project_id}.{aggregated_metrics_dataset}.supervision_officer_metrics_assignment_sessions_materialized`
+)
+,
+population_change_dates AS (
+    -- Start dates increase population by 1
+    SELECT
+        state_code,
+        officer_id,
+        start_date AS change_date,
+        1 AS change_value
+    FROM assignment_spans
+    UNION ALL
+    -- End dates decrease population by 1
+    SELECT
+        state_code,
+        officer_id,
+        end_date AS change_date,
+        -1 AS change_value
+    FROM assignment_spans
+    WHERE end_date IS NOT NULL
+)
+,
+population_change_dates_agg AS (
+    SELECT
+        state_code, 
+        officer_id, 
+        change_date, 
+        SUM(change_value) AS change_value
+    FROM 
+        population_change_dates
+    GROUP BY 1, 2, 3
+)
+SELECT
+    state_code,
+    officer_id,
+    change_date AS start_date,
+    LEAD(change_date) OVER (PARTITION BY state_code, officer_id ORDER BY change_date) AS end_date,
+    SUM(change_value) OVER (PARTITION BY state_code, officer_id ORDER BY change_date) AS caseload_count,
+FROM 
+    population_change_dates_agg
+"""
+
+SUPERVISION_OFFICER_CASELOAD_COUNT_SPANS_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=AGGREGATED_METRICS_DATASET_ID,
+    view_id=_VIEW_NAME,
+    view_query_template=_QUERY_TEMPLATE,
+    description=_VIEW_DESCRIPTION,
+    aggregated_metrics_dataset=AGGREGATED_METRICS_DATASET_ID,
+    should_materialize=True,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        SUPERVISION_OFFICER_CASELOAD_COUNT_SPANS_VIEW_BUILDER.build_and_print()
