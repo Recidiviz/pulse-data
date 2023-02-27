436a020b308701dc2fd0123f20f19ec1752139ab
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2023-02-27 12:39:30 -0500

    [Workflows] add MO users to staff record (Recidiviz/recidiviz-data#19065)
    GitOrigin-RevId: 1d0776dd3474bcefe0234f01c341fa7a22845815

--- recidiviz/calculator/query/state/views/workflows/us_mo/incarceration_staff_template.py
+++ recidiviz/calculator/query/state/views/workflows/us_mo/incarceration_staff_template.py
@@ -26,7 +26,8 @@ US_MO_INCARCERATION_STAFF_TEMPLATE = """
             state_code,
             -- TODO(#18886) Use facility name as name instead of ID
             facility_id AS name,
-            CAST(NULL AS STRING) AS district,
+            -- TODO(#19062) Make it more clear where facility IDs go
+            facility_id AS district,
             CAST(NULL AS STRING) AS email,
             false AS has_caseload,
             true AS has_facility_caseload,
@@ -35,9 +36,31 @@ US_MO_INCARCERATION_STAFF_TEMPLATE = """
         FROM `{project_id}.{workflows_dataset}.resident_record_materialized`
         WHERE state_code = "US_MO"
         AND facility_id IS NOT NULL
+    ),
+    staff_without_caseloads AS (
+        SELECT DISTINCT 
+            IFNULL(id, LOWER(email)) as id,
+            "US_MO" as state_code,
+            UPPER(first_name || " " || last_name) as name,
+            -- TODO(#19062) Make it more clear where facility IDs go
+            IF(LOWER(facility)="all", CAST(NULL as STRING), facility) as district,
+            LOWER(email) AS email,
+            false AS has_caseload,
+            false AS has_facility_caseload,
+            UPPER(first_name) as given_names,
+            UPPER(last_name) as surname,
+        -- This table is intentionally named to be specific to this workflow and is not intended to
+        -- be the long-term place where we manage MO workflows users (the new users-in-admin-panel
+        -- infrastructure will be).
+        FROM `{project_id}.{static_reference_tables_dataset}.us_mo_rh_workflows_roster`
     )
-    -- TODO(#18886) Add people that should have access to tool
     SELECT 
         {columns}
     FROM caseload_staff
+
+    UNION ALL
+    
+    SELECT 
+        {columns}
+    FROM staff_without_caseloads
 """
