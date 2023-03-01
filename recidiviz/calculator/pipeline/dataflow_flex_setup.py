a71b5901aecd8ab023688a2d27e6c0e3da8ece98
Gloria Liu <gloria@recidiviz.org>
2023-03-01 10:13:06 -0500

    update dockerfile, cloudbuild file and dataflow-flex terraform file (Recidiviz/recidiviz-data#19124)
    ## Description of the change
    
    **dockerfile change** (** both these changes are ESSENTIAL for the flex
    pipeline to run, or else there will be memory/start up issues with the
    workers **):
    1) "--no-cache-dir" - this means the pip cache won't be used (only makes
    the image larger unnecessarily, when the docker cache can be used)
    2) setting PIP_NO_DEPS to True to prevent trying to install dependencies
    again
    
    we also are still on apache-beam version 2.41.0 so this needs to
    specified to prevent a higher version from being installed which may
    cause typing errors
    
    **cloudbuild file change**:
    this file is only used for development and submitting the build
    manually. instead of pointing at the colin test image, point it to a dev
    directory in the artifact registry
    
    **dataflow-flex.tf change**:
    the path created by
    "flex_template_metadata/${basename(dirname(each.value))}_${basename(each.value)}"
    ends up looking something pretty repetitive like
    
    `"recidiviz-staging-dataflow-flex-templates/flex_template_metadata/template_metadata_metrics.json"`
     simplifying it so it looks like
    
    `"recidiviz-staging-dataflow-flex-templates/template_metadata/metrics.json"`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#18109 and Recidiviz/recidiviz-data#18116
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 9e643496d923e5c24acf21a3195590a07c3d4389

--- recidiviz/calculator/pipeline/dataflow_flex_setup.py
+++ recidiviz/calculator/pipeline/dataflow_flex_setup.py
@@ -52,7 +52,7 @@ REQUIRED_PACKAGES = [
     "protobuf==3.20.3",
     # Needed for thefuzz to avoid "Using slow pure-python SequenceMatcher" warning
     "python-Levenshtein",
-    "SQLAlchemy",
+    "SQLAlchemy==1.4.46",
     "thefuzz",
     "us",
 ]
