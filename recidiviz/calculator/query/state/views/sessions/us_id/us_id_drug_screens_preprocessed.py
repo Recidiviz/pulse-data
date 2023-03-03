5eda28ec2e8ab486aae587c0dd1db3eb54c386b4
Daniel Allen <daniel@recidiviz.org>
2023-03-03 12:11:16 -0500

    [US_TN] update TN drug screens to incorporate tests from `DrugTestDrugClass` (Recidiviz/recidiviz-data#15226)
    ## Description of the change
    
    Update the `us_tn_drug_screens_preprocessed` view to more
    comprehensively document drug screens using the existing
    `Contacts`-based tests, and the additional `DrugTestDrugClass` tests.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#14879
    Closes Recidiviz/recidiviz-data#16426
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 258d2d598d6fffe58e8091501a101d653d119b5e

--- recidiviz/calculator/query/state/views/sessions/us_id/us_id_drug_screens_preprocessed.py
+++ recidiviz/calculator/query/state/views/sessions/us_id/us_id_drug_screens_preprocessed.py
@@ -86,7 +86,7 @@ US_ID_DRUG_SCREENS_PREPROCESSED_QUERY_TEMPLATE = """
         ARRAY_AGG(COALESCE(result_raw_text, 'UNKNOWN')) OVER w AS result_raw_text,
         CAST(NULL AS STRING) AS substance_detected,
         med_invalidate_flg,
-        FALSE AS is_inferred,
+        FALSE AS is_inferred
     FROM drug_screens_raw_cte
     QUALIFY ROW_NUMBER() OVER w = 1
     WINDOW w AS (PARTITION BY person_id, state_code, drug_screen_date, sample_type)
