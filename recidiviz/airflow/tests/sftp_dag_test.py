5f40d89c5e494394eb9dec7dd302359322f2e5f7
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2023-03-21 11:01:24 -0700

    [SFTP] Unpause queues that were running, not those that were paused (Recidiviz/recidiviz-data#19637)
    ## Description of the change
    
    We were still failing to unpause queues after pausing them in the SFTP
    DAG. This PR fixes by flipping PAUSED to RUNNING in one place in the
    code, adds unittests for that helper function, and also updates some
    naming to be slightly more clear (for my brain at least).
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Follow-up to Recidiviz/recidiviz-data#19620
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 85736c3867bca1168d8d3bab913af791ee929116

--- recidiviz/airflow/tests/sftp_dag_test.py
+++ recidiviz/airflow/tests/sftp_dag_test.py
@@ -21,7 +21,9 @@ import unittest
 from unittest.mock import patch
 
 from airflow.models import DagBag
+from google.cloud.tasks_v2.types.queue import Queue
 
+from recidiviz.airflow.dags.sftp_dag import get_running_queue_instances
 from recidiviz.airflow.tests.test_utils import AIRFLOW_WORKING_DIRECTORY, DAG_FOLDER
 from recidiviz.calculator import pipeline
 
@@ -138,3 +140,60 @@ class TestSftpPipelineDag(unittest.TestCase):
             for task_type in task_types_with_retries:
                 if task_type in task.task_id:
                     self.assertEqual(3, task.retries)
+
+    def test_get_running_queue_instances(self) -> None:
+        primary_queue_status = {
+            "name": "projects/recidiviz-staging/locations/us-east1/queues/direct-ingest-state-us-me-scheduler",
+            "rate_limits": {
+                "max_dispatches_per_second": 100.0,
+                "max_burst_size": 20,
+                "max_concurrent_dispatches": 1,
+            },
+            "retry_config": {
+                "max_attempts": 5,
+                "min_backoff": "0.100s",
+                "max_backoff": "3600s",
+                "max_doublings": 16,
+            },
+            "state": Queue.State.RUNNING,
+            "purge_time": "2022-01-24T18:39:30.777006Z",
+            "stackdriver_logging_config": {"sampling_ratio": 1.0},
+        }
+
+        secondary_queue_status = {
+            "name": "projects/recidiviz-staging/locations/us-east1/queues/direct-ingest-state-us-me-scheduler-secondary",
+            "rate_limits": {
+                "max_dispatches_per_second": 100.0,
+                "max_burst_size": 20,
+                "max_concurrent_dispatches": 1,
+            },
+            "retry_config": {
+                "max_attempts": 5,
+                "min_backoff": "0.100s",
+                "max_backoff": "3600s",
+                "max_doublings": 16,
+            },
+            "state": Queue.State.RUNNING,
+            "purge_time": "2022-01-26T01:07:46.518478Z",
+            "stackdriver_logging_config": {"sampling_ratio": 1.0},
+        }
+
+        instances = get_running_queue_instances(
+            primary_queue_status, secondary_queue_status
+        )
+        self.assertEqual(["primary", "secondary"], instances)
+
+        # Now start with primary paused
+        primary_queue_status["state"] = Queue.State.PAUSED
+
+        instances = get_running_queue_instances(
+            primary_queue_status, secondary_queue_status
+        )
+        self.assertEqual(["secondary"], instances)
+
+        # Now start with secondary paused as well
+        secondary_queue_status["state"] = Queue.State.PAUSED
+        instances = get_running_queue_instances(
+            primary_queue_status, secondary_queue_status
+        )
+        self.assertEqual([], instances)
