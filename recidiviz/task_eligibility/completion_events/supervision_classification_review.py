152f881e3e89ac6ad75ca7513e0fae7edf68a63a
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2023-03-22 15:18:08 -0700

    [US_MI][Workflows] Add stubs for classification review opportunities (Recidiviz/recidiviz-data#19663)
    ## Description of the change
    
    This PR creates stubs for the classification review opportunities and
    criteria outlined in [MI Workflows
    Reasons](https://paper.dropbox.com/doc/MI-Workflows-Reasons--B04xVTqcwbwlLdUrnFqonS26AQ-dlXEAU8Yu8wCnfLxT9brb).
    
    Additionally, this adds the
    `probation_parole_dual_active_supervision_population` candidate
    population query as outlined in the [candidate population refactor
    doc](https://docs.google.com/document/d/1KB0cNNA_mFTOZMR9pLPVgtVsWkSB1wvwYIBTzpGbZCg/edit?usp=sharing).
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 2f3e9fd13984e8be220378ea19b425282f978550

---
+++ recidiviz/task_eligibility/completion_events/supervision_classification_review.py
@@ -0,0 +1,52 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines a view that shows all classification review dates for clients in Michigan.
+"""
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.task_eligibility.task_completion_event_big_query_view_builder import (
+    TaskCompletionEventBigQueryViewBuilder,
+    TaskCompletionEventType,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Defines a view that shows all classification review dates for clients in Michigan. Classification
+reviews happen every 6 months after the initial classification review and should result in a supervision level 
+downgrade unless there are extenuating circumstances. 
+"""
+
+_QUERY_TEMPLATE = """
+SELECT
+    state_code,
+    person_id,
+    start_date AS completion_event_date,
+FROM `{project_id}.{sessions_dataset}.compartment_sessions_materialized`
+WHERE FALSE
+"""
+
+VIEW_BUILDER: TaskCompletionEventBigQueryViewBuilder = (
+    TaskCompletionEventBigQueryViewBuilder(
+        completion_event_type=TaskCompletionEventType.SUPERVISION_CLASSIFICATION_REVIEW,
+        description=_DESCRIPTION,
+        completion_event_query_template=_QUERY_TEMPLATE,
+        sessions_dataset=dataset_config.SESSIONS_DATASET,
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
