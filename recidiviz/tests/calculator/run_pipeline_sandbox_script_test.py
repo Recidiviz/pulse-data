96b1d6988ba4716b50ec223daa88196c630c7cd8
Gloria Liu <gloria@recidiviz.org>
2023-03-22 14:20:51 -0400

    delete legacy sandbox script [c]  (Recidiviz/recidiviz-data#19669)
    ## Description of the change
    
    delete legacy sandbox script and rename new flex sandbox script to
    run_sandbox_calculation_pipeline
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Related to https://github.com/Recidiviz/recidiviz-data/issues/17989
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 64458a2265c0f9bca3b60c4d8c088485f3ea0eb0

--- recidiviz/tests/calculator/run_pipeline_sandbox_script_test.py
+++ recidiviz/tests/calculator/run_pipeline_sandbox_script_test.py
@@ -14,12 +14,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""Tests for run_sandbox_flex_calculation_pipeline.py"""
+"""Tests for run_sandbox_calculation_pipeline.py"""
 
 import os
 import unittest
 
-from recidiviz.tools.calculator.run_sandbox_flex_calculation_pipeline import (
+from recidiviz.tools.calculator.run_sandbox_calculation_pipeline import (
     get_cloudbuild_path,
     get_template_path,
 )
