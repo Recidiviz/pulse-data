dc854a16adec107a2861e470869fab02a7a2548b
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2023-03-23 09:51:41 -0700

    [US_PA] Remove unnecessary ingest_info reference from enum parsers code (Recidiviz/recidiviz-data#19700)
    ## Description of the change
    
    The `assessment_level_mapper` custom enum parser still uses
    `recidiviz.ingest.models.ingest_info.StateAssessment` which is a class
    that should have only been used by v1 mappings code. This change updates
    that function to remove the unnecessary dependency.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#8905.
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 315b32491d2bc7964644b55d07282bfbebd2e091

--- recidiviz/ingest/direct/regions/us_pa/us_pa_custom_enum_parsers.py
+++ recidiviz/ingest/direct/regions/us_pa/us_pa_custom_enum_parsers.py
@@ -46,7 +46,6 @@ from recidiviz.common.str_field_utils import parse_int
 from recidiviz.ingest.direct.legacy_ingest_mappings.direct_ingest_controller_utils import (
     invert_enum_to_str_mappings,
 )
-from recidiviz.ingest.models.ingest_info import StateAssessment
 
 # dictionary of specific dates for set_date_specific_lsir_fields
 _DATE_SPECIFIC_ORDERED_LSIR_LEVELS = OrderedDict(
@@ -411,66 +410,50 @@ def supervision_contact_location_mapper(
 
 
 def assessment_level_mapper(raw_text: str) -> StateAssessmentLevel:
-    """Maps an assessment_level_raw_text code to the corresponding StateAssessmentLevel, if applicable."""
-    date, desc, score = raw_text.split("††")
-    state_assessment = StateAssessment(
-        assessment_date=date, assessment_type=desc, assessment_score=score
-    )
-    output = set_date_specific_lsir_fields(state_assessment)
-    level = str(output.assessment_level)
-    if level.startswith("UNKNOWN") or "NONE" in score:
-        return StateAssessmentLevel.EXTERNAL_UNKNOWN
-    return StateAssessmentLevel(level)
+    """Maps an assessment_level_raw_text code to the corresponding StateAssessmentLevel,
+    if applicable.
 
-
-def set_date_specific_lsir_fields(assessment: StateAssessment) -> StateAssessment:
-    """Over time, US_PA has updated the mapping between an LSIR score and the associated assessment level. This function
-    sets the appropriate assessment_level and assessment_score according to the score and the date of the |assessment|,
-    as defined by _DATE_SPECIFIC_ORDERED_LSIR_LEVELS.
-    Returns the updated StateAssessment object.
+    Over time, US_PA has updated the mapping between an LSIR score and the associated
+    assessment level. This function returns the appropriate assessment_level according
+    to the score and the date of the |assessment|, as defined by
+    _DATE_SPECIFIC_ORDERED_LSIR_LEVELS.
     """
-    if not assessment.assessment_score or "NONE" in assessment.assessment_score:
-        return assessment
-    assessment_score = parse_int(assessment.assessment_score)
+    assessment_date_raw, _desc, assessment_score_raw = raw_text.split("††")
+
+    if not assessment_score_raw or "NONE" in assessment_score_raw:
+        return StateAssessmentLevel.EXTERNAL_UNKNOWN
+
+    assessment_score = parse_int(assessment_score_raw)
     if assessment_score == 60:
-        # This value indicates the scoring was not completed
-        assessment.assessment_score = None
-        assessment.assessment_level = "UNKNOWN (60-ATTEMPTED_INCOMPLETE)"
-    elif assessment_score == 70:
-        # This person either refused to be assessed or did not need to be assessed because they chose not to be released
-        # onto parole
-        assessment.assessment_score = None
-        assessment.assessment_level = "UNKNOWN (70-REFUSED)"
-    elif assessment_score > 55:
-        # Assessment score number is over the max value of 54, and isn't one of the expected special-case
-        # codes (60, 70, 55)
-        assessment.assessment_level = f"UNKNOWN ({assessment_score}-SCORE_OUT_OF_RANGE)"
-        assessment.assessment_score = None
-    else:
-        if assessment_score == 55:
-            # This should be treated as a 54
-            assessment_score = 54
-            assessment.assessment_score = "54"
-        assessment_date_raw = assessment.assessment_date
-        assessment_date = (
-            str_field_utils.parse_date(assessment_date_raw)
-            if assessment_date_raw
-            else None
-        )
-        if not assessment_date:
-            # At this point we need a valid assessment_date to determine the date-specific LSIR level
-            assessment.assessment_level = "UNKNOWN (NO_DATE)"
-            return assessment
-        for cutoff_date, score_level_map in _DATE_SPECIFIC_ORDERED_LSIR_LEVELS.items():
-            if assessment_date <= cutoff_date:
-                for cutoff_score, level in score_level_map.items():
-                    if assessment_score <= cutoff_score:
-                        assessment.assessment_level = level.value
-                        return assessment
-        raise ValueError(
-            f"Unhandled assessment_score {assessment_score} with assessment_date {assessment_date}"
-        )
-    return assessment
+        # This value indicates the scoring was not completed (ATTEMPTED INCOMPLETE)
+        return StateAssessmentLevel.EXTERNAL_UNKNOWN
+    if assessment_score == 70:
+        # This person either refused to be assessed or did not need to be assessed
+        # because they chose not to be released onto parole (REFUSED)
+        return StateAssessmentLevel.EXTERNAL_UNKNOWN
+    if assessment_score > 55:
+        # Assessment score number is over the max value of 54, and isn't one of the
+        # expected special-case codes (60, 70, 55) (SCORE_OUT_OF_RANGE)
+        return StateAssessmentLevel.EXTERNAL_UNKNOWN
+
+    if assessment_score == 55:
+        # This should be treated as a 54
+        assessment_score = 54
+    assessment_date = (
+        str_field_utils.parse_date(assessment_date_raw) if assessment_date_raw else None
+    )
+    if not assessment_date:
+        # At this point we need a valid assessment_date to determine the date-specific
+        # LSIR level
+        return StateAssessmentLevel.EXTERNAL_UNKNOWN
+    for cutoff_date, score_level_map in _DATE_SPECIFIC_ORDERED_LSIR_LEVELS.items():
+        if assessment_date <= cutoff_date:
+            for cutoff_score, level in score_level_map.items():
+                if assessment_score <= cutoff_score:
+                    return level
+    raise ValueError(
+        f"Unhandled assessment_score {assessment_score} with assessment_date {assessment_date}"
+    )
 
 
 def force_adm_reason_internal_unknown(
