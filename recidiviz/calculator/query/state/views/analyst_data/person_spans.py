1db2ddf7f4b37debbed1f32fe877ad0fb45a3e1b
Zack Goodman <46683734+zagoodman@users.noreply.github.com>
2023-03-29 10:35:56 -0700

    Adjusts person_spans and person_events (Recidiviz/recidiviz-data#19723)
    ## Description of the change
    
    This PR:
    
    1. Removes unnecessary `ARRAY_AGG()` and `GROUP BY` from person_spans
    and person_events
    2. Adds a `SUPERVISION_OFFICER_SESSION` to person_spans to close a
    ticket and assist in closing a Looker ticket.
    
    The first change has implications for Recidiviz/recidiviz-data#19577 since most events and spans
    no longer require ARRAY_AGG.
    
    ~I would appreciate a sanity check that this function is actually not
    required where I removed it...my intuition is that if we're grouping by
    all fields in `event_attributes` that guarantees the array always
    contains exactly one entry. In other cases, if the set of group by
    columns uniquely identifies rows in the source table (e.g. person_id
    plus start_date in most sessions tables) then there should be exactly
    one entry in the array.~
    
    Update: `person_events` is unique on person-event-date-attributes, as
    expected (see query below c/o @justkunz):
    
    <details>
    <summary>Query</summary>
    
    ```
    WITH duplicate_records AS (
      SELECT
        state_code,
        person_id,
        event,
        event_date,
        event_attributes,
        COUNT(*) AS total_rows
      FROM `recidiviz-staging.zackg_analyst_data.person_events_materialized`
      GROUP BY 1, 2, 3, 4, 5
      HAVING total_rows > 1
    )
    SELECT
      event,
      COUNT(*) AS repeat_count
    FROM duplicate_records
    GROUP BY 1
    ORDER BY repeat_count DESC
    LIMIT 50
    ```
    
    </details>
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#15777
    Related to recidiviz/lookerRecidiviz/recidiviz-data#123
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 89568ba855829e845fb51b125ce0d34a82c07e4a

--- recidiviz/calculator/query/state/views/analyst_data/person_spans.py
+++ recidiviz/calculator/query/state/views/analyst_data/person_spans.py
@@ -1,5 +1,5 @@
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2022 Recidiviz, Inc.
+# Copyright (C) 2023 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@ PERSON_SPANS_VIEW_NAME = "person_spans"
 PERSON_SPANS_VIEW_DESCRIPTION = (
     "View concatenating client (person) spans in a common format. Note that end_dates "
     "are exclusive, i.e. the last full day of the span (if any) was the day prior to "
-    "the end_date."
+    "the end_date. Spans of the same type may be overlapping."
 )
 
 PERSON_SPANS_QUERY_TEMPLATE = """
@@ -45,14 +45,13 @@ SELECT
     "COMPARTMENT_SESSION" AS span,
     start_date,
     DATE_ADD(end_date, INTERVAL 1 DAY) AS end_date,
-    TO_JSON_STRING(ARRAY_AGG(STRUCT(
+    TO_JSON_STRING(STRUCT(
         compartment_level_1,
         compartment_level_2,
         case_type_start
-    ))[OFFSET(0)]) AS span_attributes,
+    )) AS span_attributes,
 FROM
     `{project_id}.{sessions_dataset}.compartment_sessions_materialized`
-GROUP BY 1, 2, 3, 4, 5
 
 UNION ALL
 
@@ -61,16 +60,15 @@ SELECT
     state_code,
     person_id,
     "PERSON_DEMOGRAPHICS" AS span,
-    MIN(birthdate) AS start_date,
+    birthdate AS start_date,
     DATE_ADD(CURRENT_DATE("US/Eastern"), INTERVAL 1 DAY) AS end_date,
-    TO_JSON_STRING(ARRAY_AGG(STRUCT(
+    TO_JSON_STRING(STRUCT(
         birthdate,
-        pd.gender,
-        pd.prioritized_race_or_ethnicity
-    ))[OFFSET(0)]) AS span_attributes,
+        gender,
+        prioritized_race_or_ethnicity
+    )) AS span_attributes,
 FROM
-    `{project_id}.{sessions_dataset}.person_demographics_materialized` pd
-GROUP BY 1, 2, 3
+    `{project_id}.{sessions_dataset}.person_demographics_materialized`
 
 UNION ALL
 
@@ -81,18 +79,17 @@ SELECT
     "ASSESSMENT_SCORE_SESSION" AS span,
     assessment_date AS start_date,
     DATE_ADD(score_end_date, INTERVAL 1 DAY) AS end_date,
-    TO_JSON_STRING(ARRAY_AGG(STRUCT(
+    TO_JSON_STRING(STRUCT(
         assessment_type,
         assessment_score,
         assessment_level
-    ))[OFFSET(0)]) AS span_attributes,
+    )) AS span_attributes,
 FROM
     `{project_id}.{sessions_dataset}.assessment_score_sessions_materialized`
 WHERE
     assessment_date IS NOT NULL
     AND assessment_type IS NOT NULL
     AND assessment_score IS NOT NULL
-GROUP BY 1, 2, 3, 4, 5
 
 UNION ALL
 
@@ -103,15 +100,14 @@ SELECT
     "EMPLOYMENT_PERIOD" AS span,
     start_date,
     end_date,
-    TO_JSON_STRING(ARRAY_AGG(STRUCT(
+    TO_JSON_STRING(STRUCT(
         employer_name
-    ))[OFFSET(0)]) AS span_attributes,
+    )) AS span_attributes,
 FROM
     `{project_id}.{normalized_state_dataset}.state_employment_period`
 WHERE
     start_date IS NOT NULL
     AND employment_status != "UNEMPLOYED"
-GROUP BY 1, 2, 3, 4, 5
 
 UNION ALL
 
@@ -122,14 +118,13 @@ SELECT
     "EMPLOYMENT_STATUS_SESSION" AS span,
     employment_status_start_date AS start_date,
     DATE_ADD(employment_status_end_date, INTERVAL 1 DAY) AS end_date,
-    TO_JSON_STRING(ARRAY_AGG(STRUCT(
+    TO_JSON_STRING(STRUCT(
         CAST(is_employed AS STRING) AS is_employed
-    ))[OFFSET(0)]) AS span_attributes,
+    )) AS span_attributes,
 FROM
     `{project_id}.{sessions_dataset}.supervision_employment_status_sessions_materialized`
 WHERE
     employment_status_start_date IS NOT NULL
-GROUP BY 1, 2, 3, 4, 5
 
 UNION ALL
 
@@ -182,16 +177,29 @@ SELECT
     "SUPERVISION_LEVEL_DOWNGRADE_ELIGIBLE" AS span,
     start_date,
     DATE_ADD(end_date, INTERVAL 1 DAY) AS end_date,
-    TO_JSON_STRING(ARRAY_AGG(STRUCT(
+    TO_JSON_STRING(STRUCT(
         "SUPERVISION_DOWNGRADE" AS task_name,
         CAST(mismatch_corrected AS STRING) AS mismatch_corrected,
         recommended_supervision_downgrade_level
-    ))[OFFSET(0)]) AS span_attributes,
+    )) AS span_attributes,
 FROM
     `{project_id}.{sessions_dataset}.supervision_downgrade_sessions_materialized`
 WHERE
     recommended_supervision_downgrade_level IS NOT NULL
-GROUP BY 1, 2, 3, 4, 5
+
+UNION ALL
+
+SELECT
+    state_code,
+    person_id,
+    "SUPERVISION_OFFICER_SESSION" AS span,
+    start_date,
+    DATE_ADD(end_date, INTERVAL 1 DAY) AS end_date,
+    TO_JSON_STRING(STRUCT(
+        supervising_officer_external_id
+    )) AS span_attributes,
+FROM
+    `{project_id}.{sessions_dataset}.supervision_officer_sessions_materialized`
 
 UNION ALL
 
@@ -202,19 +210,18 @@ SELECT
     "TASK_ELIGIBILITY_SESSION" AS span,
     start_date,
     end_date,
-    TO_JSON_STRING(ARRAY_AGG(STRUCT(
+    TO_JSON_STRING(STRUCT(
         task_name,
         completion_event_type AS task_type,
         is_eligible,
         ineligible_criteria
-    ))[OFFSET(0)]) AS span_attributes
+    )) AS span_attributes
 FROM
     `{project_id}.{task_eligibility_dataset}.all_tasks_materialized`
 INNER JOIN
     `{project_id}.{reference_views_dataset}.task_to_completion_event`
 USING
     (task_name)
-GROUP BY 1, 2, 3, 4, 5, task_name
 
 """
 
