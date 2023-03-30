07b1428032f5cd937e175c0a738d55cbc72342f1
jmielke-recidiviz <118864563+jmielke-recidiviz@users.noreply.github.com>
2023-03-30 09:12:42 -0600

    [Cleanup] Remove legacy map_fn_with_progress_bar (Recidiviz/recidiviz-data#19782)
    ## Description of the change
    
    Problem:
    ---
    - There used to be only one user of the legacy
    `map_fn_with_progress_bar`
      - The deploy_all_pipeline_templates script
      - Now there are no users
    - The function has been superceded by `map_fn_with_progress_bar_results`
      - To track successes and failures
    
    Solution:
    ---
    - Remove the old
    -
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 35703471a83dd7638be3675a7b2cebf0841e7be4

--- recidiviz/utils/future_executor.py
+++ recidiviz/utils/future_executor.py
@@ -14,122 +14,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-""" Class for awaiting results of futures """
+"""Function for awaiting results of futures with progress bar, tracking exceptions"""
 
 import sys
 from concurrent import futures
-from concurrent.futures import Future
-from contextlib import contextmanager
-from typing import Any, Callable, Dict, Generator, List, Optional, Tuple
+from typing import Any, Callable, Dict, List, Tuple
 
-import attr
 from progress.bar import Bar
 
-from recidiviz.tools.deploy.logging import redirect_logging_to_file
 from recidiviz.utils import structured_logging
-
-
-@attr.s(frozen=True)
-class FutureExecutorProgress:
-    """Data class for future execution progress"""
-
-    running: int = attr.ib()
-    completed: int = attr.ib()
-    total: int = attr.ib()
-
-    def format(self) -> str:
-        """Format to progress string"""
-        return f"{self.running} running - {self.completed} / {self.total}"
-
-
-class FutureExecutor:
-    """Given a list of task futures, exposes a progress generator, and the results of futures"""
-
-    PROGRESS_TIMEOUT = 120  # 2 minutes
-
-    def __init__(self, task_futures: List[Future]):
-        self.task_futures = task_futures
-        self.futures_count = len(self.task_futures)
-
-    def progress(
-        self, timeout: Optional[int] = PROGRESS_TIMEOUT
-    ) -> Generator[FutureExecutorProgress, None, None]:
-        """Publishes progress of the futures until all futures are completed
-        Raises a timeout error if futures have not completed after `timeout` seconds
-        Raises exceptions that the futures may have encountered
-        """
-
-        completed = 0
-
-        for _ in futures.as_completed(self.task_futures, timeout=timeout):
-            completed += 1
-            running = len([future for future in self.task_futures if future.running()])
-
-            yield FutureExecutorProgress(
-                running=running, completed=completed, total=self.futures_count
-            )
-
-        # Aggregate results, raising any exceptions that a future may have encountered
-        self.results()
-
-    def wait_with_progress_bar(
-        self, message: str, timeout: Optional[int] = PROGRESS_TIMEOUT
-    ) -> None:
-        progress_bar = Bar(message, max=self.futures_count, check_tty=False)
-        progress_bar.start()
-
-        for progress in self.progress(timeout=timeout):
-            progress_bar.goto(progress.completed)
-
-        progress_bar.finish()
-
-    def results(self) -> List:
-        return [task_future.result() for task_future in self.task_futures]
-
-    @staticmethod
-    @contextmanager
-    def build(
-        func: Callable,
-        kwargs_list: List[Dict[str, Any]],
-        max_workers: Optional[int] = None,
-    ) -> Generator["FutureExecutor", None, None]:
-        """Creates a ThreadPoolExecutor and corresponding FutureExecutor"""
-        with futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-            future_execution = FutureExecutor(
-                [
-                    executor.submit(structured_logging.with_context(func), **kwargs)
-                    for kwargs in kwargs_list
-                ]
-            )
-
-            yield future_execution
-
-
-def map_fn_with_progress_bar(
-    fn: Callable,
-    kwargs_list: List[Dict[str, Any]],
-    progress_bar_message: str,
-    max_workers: int,
-    timeout_sec: int,
-    logging_redirect_filename: str,
-) -> None:
-    """This will call `fn` with each set of args in `kwargs_list`, distributing the
-    work across `max_workers` threads.
-
-    Additionally, it will write out a progress bar to keep track of how many of the
-    calls have completed, and redirect logging for the individual calls to
-    `logging_redirect_filename`.
-    """
-    with redirect_logging_to_file(logging_redirect_filename), FutureExecutor.build(
-        fn,
-        kwargs_list,
-        max_workers,
-    ) as execution:
-
-        execution.wait_with_progress_bar(
-            progress_bar_message,
-            timeout=timeout_sec,
-        )
 
 
 def map_fn_with_progress_bar_results(
