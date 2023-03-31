c77a6661534dfb52e6fec4d3b9373e1c1464611e
Zack Goodman <46683734+zagoodman@users.noreply.github.com>
2023-03-31 12:07:54 -0700

    Initializes NC raw data ingest (Recidiviz/recidiviz-data#19859)
    ## Description of the change
    
    This PR adds the necessary files to start ingesting NC raw data. In
    addition to the files in this PR I added raw data CSVs to [this
    bucket](https://console.cloud.google.com/storage/browser/recidiviz-staging-direct-ingest-state-us-nc;tab=objects?authuser=0&project=recidiviz-staging&prefix=&forceOnObjectsSortingFiltering=false).
    
    Why: NC is well-studied (because their case-level data are [publicly
    available](https://webapps.doc.state.nc.us/opi/downloads.do?method=view)),
    and it would be nice to validate research numbers calculated using our
    downstream tables off what other folks have published. I also lived in
    NC for a while so got a soft spot for the state.
    
    I parsed NC's data docs & converted the downloaded `.dat` files to
    `.csv` with appropriate headings in recidiviz/recidiviz-researchRecidiviz/recidiviz-data#860.
    
    I may have been a bit too strict on `is_pii`, and I took my best guess
    on primary keys. Will validate soon.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes None
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: ffb01cd50eb573fb9289a7b74186d6be8189626a

--- recidiviz/tests/server_config_test.py
+++ recidiviz/tests/server_config_test.py
@@ -66,7 +66,7 @@ class TestServerConfig(unittest.TestCase):
         self,
     ) -> None:
         self.assertEqual(
-            28, len(server_config.database_keys_for_schema_type(SchemaType.STATE))
+            30, len(server_config.database_keys_for_schema_type(SchemaType.STATE))
         )
 
     @patch("recidiviz.utils.environment.in_gcp_production")
@@ -77,5 +77,5 @@ class TestServerConfig(unittest.TestCase):
 
         # Should skip primary/secondary in US_IX and US_OZ
         self.assertEqual(
-            26, len(server_config.database_keys_for_schema_type(SchemaType.STATE))
+            28, len(server_config.database_keys_for_schema_type(SchemaType.STATE))
         )
