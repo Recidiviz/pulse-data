01141de46b742c7b93ecd903fe7322e863ae9a55
caroletouma <carole@recidiviz.org>
2023-04-04 15:06:32 -0400

    [2] Add Raw Data Table Query Diff Template and Builder (Recidiviz/recidiviz-data#18945)
    ## Description of the change
    
    Add a template query that identifies added, deleted, and changed rows
    between two historical raw data tables.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#18920
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6088bb4e66a52cc04b7d1855d195e6b0ac3ad8c8

---
+++ recidiviz/ingest/direct/views/raw_data_diff_query_builder.py
@@ -0,0 +1,154 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2023 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""Defines a class for building SQL queries to compute the diffs between existing data in a raw data table and
+updated data for that table."""
+from types import ModuleType
+
+from recidiviz.big_query.big_query_query_builder import BigQueryQueryBuilder
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct import regions
+from recidiviz.ingest.direct.raw_data.raw_file_configs import DirectIngestRawFileConfig
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.ingest.direct.views.raw_table_query_builder import RawTableQueryBuilder
+from recidiviz.utils.string import StrictStringFormatter
+
+DELETED_DIFF_JOIN_CLAUSE_TEMPLATE = """
+    (
+        (current_table_rows.{column_name} IS NULL AND new_table_rows.{column_name} IS NULL) OR 
+        (current_table_rows.{column_name} = new_table_rows.{column_name})
+    )"""
+
+DELETED_DIFF_WHERE_CLAUSE_TEMPLATE = """
+new_table_rows.{column_name} IS NULL
+"""
+
+QUERY_TEMPLATE = """
+WITH current_table_rows AS 
+( 
+ {latest_current_raw_data_query}
+), 
+new_table_rows AS ( 
+    SELECT 
+      * 
+    FROM `{project_id}.{new_raw_data_dataset}.{table_name}`
+),
+added_or_updated_diff AS ( 
+  SELECT * FROM new_table_rows 
+  EXCEPT DISTINCT 
+  SELECT * FROM current_table_rows
+), 
+deleted_diff AS ( 
+  SELECT 
+    current_table_rows.* 
+  FROM 
+    current_table_rows
+  LEFT OUTER JOIN
+    new_table_rows
+  ON 
+    {deleted_diff_join_clause} 
+  WHERE 
+    {deleted_diff_where_clause}
+) 
+SELECT 
+  *, 
+  false AS is_deleted 
+FROM added_or_updated_diff 
+
+UNION ALL 
+
+SELECT 
+  *, 
+  true AS is_deleted 
+FROM deleted_diff
+"""
+
+
+class RawDataDiffQueryBuilder:
+    """Class for building SQL queries to compute the diffs between existing data in a raw data table and updated
+    data for that table."""
+
+    def __init__(
+        self,
+        project_id: str,
+        state_code: StateCode,
+        raw_data_instance: DirectIngestInstance,
+        raw_file_config: DirectIngestRawFileConfig,
+        new_raw_data_dataset: str,
+        region_module: ModuleType = regions,
+    ):
+        """Builds a query for computing the diffs between existing data in a raw data table and updated data for
+        that table."""
+        self._query_builder = BigQueryQueryBuilder(address_overrides=None)
+        self.project_id = project_id
+        self.state_code = state_code
+        self.raw_data_instance = raw_data_instance
+        self.raw_file_config = raw_file_config
+        self.table_name = self.raw_file_config.file_tag
+
+        self.latest_current_raw_data_query = RawTableQueryBuilder(
+            project_id=self.project_id,
+            region_code=self.state_code.value,
+            raw_data_source_instance=self.raw_data_instance,
+        ).build_query(
+            raw_file_config=raw_file_config,
+            address_overrides=None,
+            normalized_column_values=False,
+            raw_data_datetime_upper_bound=None,
+            filter_to_latest=True,
+        )
+        self.primary_key_cols = self.raw_file_config.primary_key_cols
+
+        self.new_raw_data_dataset = new_raw_data_dataset
+        self._region_module = region_module
+
+    def build_query(self) -> str:
+        query_kwargs = {
+            "project_id": self.project_id,
+            "table_name": self.table_name,
+            "latest_current_raw_data_query": self.latest_current_raw_data_query,
+            "new_raw_data_dataset": self.new_raw_data_dataset,
+            "deleted_diff_join_clause": self._build_is_deleted_join_clause(),
+            "deleted_diff_where_clause": self._build_is_deleted_where_clause(),
+        }
+        return self._query_builder.build_query(
+            project_id=self.project_id,
+            query_template=QUERY_TEMPLATE,
+            query_format_kwargs=query_kwargs,
+        )
+
+    def build_and_print(self) -> None:
+        print(self.build_query())
+
+    def _build_is_deleted_join_clause(self) -> str:
+        join_statements = []
+        for col in self.primary_key_cols:
+            join_statements.append(
+                StrictStringFormatter().format(
+                    DELETED_DIFF_JOIN_CLAUSE_TEMPLATE, column_name=col
+                )
+            )
+        return " AND".join(join_statements)
+
+    def _build_is_deleted_where_clause(self) -> str:
+        where_statements = []
+        for col in self.primary_key_cols:
+            where_statements.append(
+                StrictStringFormatter().format(
+                    DELETED_DIFF_WHERE_CLAUSE_TEMPLATE, column_name=col
+                )
+            )
+        return " AND".join(where_statements)
