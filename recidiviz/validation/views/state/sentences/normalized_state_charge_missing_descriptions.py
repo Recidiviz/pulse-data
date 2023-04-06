331199c17c042b79e919ace3bb739c8217c9e831
Justine <justine@recidiviz.org>
2023-04-06 16:34:36 -0400

    [Sentence validation] flag charges missing uniform labels (Recidiviz/recidiviz-data#19857)
    ## Description of the change
    
    Add a validation that checks the hydration level for the uniform offense
    labels on `normalized_state.state_charge` that fails if the number of
    rows with `nbirs_category_uniform` is null or `"EXTERNAL_UNKNOWN"` goes
    above the set threshold. There's already a ticket to fix this for US_MO,
    and I've added a ticket to investigate US_PA as well since the uniform
    label is missing ~5% of all charges. All of the other states have
    relatively low numbers (all well under 1% missing) so I don't think they
    are worth investigating (error rate query below).
    
    ```sql
    SELECT
    region_code,
    COUNT(*) AS total_rows,
    total_charges,
    100 * COUNT(*) / total_charges AS percent_error
    FROM `recidiviz-staging.jkunz_validation_views.normalized_state_charge_missing_uniform_offense_labels_materialized`
    INNER JOIN (
      SELECT state_code AS region_code, COUNT(*) AS total_charges,
      FROM `recidiviz-staging.normalized_state.state_charge`
      GROUP BY 1
    ) USING (region_code)
    GROUP BY 1, 3
    ORDER BY 1
    ```
    
    I also moved the sentence-specific validations into a new python package
    within the validation module since we have a handful of them now that
    aren't all sessions-specific, but make sense to have grouped together.
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#19819
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [X] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Justine Kunz <justine@Justines-MacBook-Pro.local>
    GitOrigin-RevId: c1ef9261def2a455afc1cfedd9538bc0f9552d60

--- recidiviz/validation/views/state/sentences/normalized_state_charge_missing_descriptions.py
+++ recidiviz/validation/views/state/sentences/normalized_state_charge_missing_descriptions.py
@@ -1,5 +1,5 @@
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2022 Recidiviz, Inc.
+# Copyright (C) 2023 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,42 +14,43 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""A validation view that contains all of the sentences with no date imposed."""
+"""A validation view that contains all of the normalized state charges that are missing
+offense descriptions."""
 
 from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
-from recidiviz.calculator.query.state.dataset_config import SESSIONS_DATASET
+from recidiviz.calculator.query.state.dataset_config import NORMALIZED_STATE_DATASET
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 from recidiviz.validation.views import dataset_config
 
-SENTENCES_MISSING_DATE_IMPOSED_VIEW_NAME = "sentences_missing_date_imposed"
+NORMALIZED_STATE_CHARGE_MISSING_DESCRIPTIONS_VIEW_NAME = (
+    "normalized_state_charge_missing_descriptions"
+)
 
-SENTENCES_MISSING_DATE_IMPOSED_DESCRIPTION = """
-Return all sentences that have no `date_imposed` value, indicating they cannot be linked
-to any corresponding sessions.
+NORMALIZED_STATE_CHARGE_MISSING_DESCRIPTIONS_DESCRIPTION = """
+Normalized state charge rows with null offense descriptions, impacting
+Workflows auto-filled forms and any analysis by offense type.
 """
 
-SENTENCES_MISSING_DATE_IMPOSED_QUERY_TEMPLATE = """
+NORMALIZED_STATE_CHARGE_MISSING_DESCRIPTIONS_QUERY_TEMPLATE = """
 SELECT
   state_code AS region_code,
-  sentence_type,
   person_id,
-  sentence_id,
+  charge_id,
   external_id,
-FROM `{project_id}.{sessions_dataset}.sentences_preprocessed_materialized`
-WHERE date_imposed IS NULL
+FROM `{project_id}.{normalized_state_dataset}.state_charge`
+WHERE description IS NULL
 """
 
-
-SENTENCES_MISSING_DATE_IMPOSED_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+NORMALIZED_STATE_CHARGE_MISSING_DESCRIPTIONS_VIEW_BUILDER = SimpleBigQueryViewBuilder(
     dataset_id=dataset_config.VIEWS_DATASET,
-    view_id=SENTENCES_MISSING_DATE_IMPOSED_VIEW_NAME,
-    view_query_template=SENTENCES_MISSING_DATE_IMPOSED_QUERY_TEMPLATE,
-    description=SENTENCES_MISSING_DATE_IMPOSED_DESCRIPTION,
-    sessions_dataset=SESSIONS_DATASET,
+    view_id=NORMALIZED_STATE_CHARGE_MISSING_DESCRIPTIONS_VIEW_NAME,
+    view_query_template=NORMALIZED_STATE_CHARGE_MISSING_DESCRIPTIONS_QUERY_TEMPLATE,
+    description=NORMALIZED_STATE_CHARGE_MISSING_DESCRIPTIONS_DESCRIPTION,
+    normalized_state_dataset=NORMALIZED_STATE_DATASET,
     should_materialize=True,
 )
 
 if __name__ == "__main__":
     with local_project_id_override(GCP_PROJECT_STAGING):
-        SENTENCES_MISSING_DATE_IMPOSED_VIEW_BUILDER.build_and_print()
+        NORMALIZED_STATE_CHARGE_MISSING_DESCRIPTIONS_VIEW_BUILDER.build_and_print()
--- recidiviz/validation/views/state/sentences/session_new_admissions_with_no_sentence_date_imposed.py
+++ recidiviz/validation/views/state/sentences/session_new_admissions_with_no_sentence_date_imposed.py
@@ -1,5 +1,5 @@
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2022 Recidiviz, Inc.
+# Copyright (C) 2023 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,8 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""A validation view that contains all of the sessions releases that do not
-have a sentence completed on that end date."""
+"""A validation view that contains all of the new admission sessions that do not
+have a sentence imposed on that start date."""
 
 from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
 from recidiviz.calculator.query.state.dataset_config import SESSIONS_DATASET
@@ -23,30 +23,26 @@ from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 from recidiviz.validation.views import dataset_config
 
-SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_VIEW_NAME = (
-    "session_liberty_releases_with_no_sentence_completion_date"
+SESSION_NEW_ADMISSIONS_WITH_NO_SENTENCE_DATE_IMPOSED_VIEW_NAME = (
+    "session_new_admissions_with_no_sentence_date_imposed"
 )
 
-SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_DESCRIPTION = """
-Return all sessions releases (outflowing to liberty or death) that
-do not have a corresponding sentence completed on the same date.
+SESSION_NEW_ADMISSIONS_WITH_NO_SENTENCE_DATE_IMPOSED_DESCRIPTION = """
+Return all new admission sessions (inflowing from liberty or investigation) that
+do not have a corresponding sentence imposed on the same date.
 """
 
-SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_QUERY_TEMPLATE = """
+SESSION_NEW_ADMISSIONS_WITH_NO_SENTENCE_DATE_IMPOSED_QUERY_TEMPLATE = """
 WITH sessions_to_sentences AS (
     SELECT
         ses.state_code AS region_code,
         ses.person_id,
         ses.session_id,
         ses.compartment_level_1,
-        DATE_ADD(ses.end_date, INTERVAL 1 DAY) AS session_end_date,
-        sen.completion_date AS sentence_completion_date,
+        ses.start_date AS session_start_date,
+        sen.date_imposed AS sentence_date_imposed,
         COALESCE(
-            DATE_DIFF(
-                DATE_ADD(ses.end_date, INTERVAL 1 DAY),
-                sen.completion_date,
-                DAY
-            ),
+            DATE_DIFF(ses.start_date, sen.date_imposed, DAY),
             9999
         ) AS sentence_to_session_offset_days,
     FROM `{project_id}.{sessions_dataset}.compartment_sessions_materialized` ses
@@ -55,38 +51,33 @@ WITH sessions_to_sentences AS (
         AND ses.person_id = sen.person_id
         -- Only join sentences that were imposed before the session ended
         AND sen.date_imposed < COALESCE(DATE_ADD(ses.end_date, INTERVAL 1 DAY), CURRENT_DATE('US/Eastern'))
-    WHERE outflow_to_level_1 IN ("LIBERTY", "DEATH")
+    WHERE inflow_from_level_1 IN ("LIBERTY", "INVESTIGATION")
         AND compartment_level_1 IN ("SUPERVISION", "INCARCERATION")
     -- Pick the closest sentence imposed for each session start
-    QUALIFY ROW_NUMBER() OVER (
-        PARTITION BY region_code, person_id, start_date
-        ORDER BY ABS(DATE_DIFF(
-            DATE_ADD(ses.end_date, INTERVAL 1 DAY),
-            sen.completion_date,
-            DAY
-        )) ASC) = 1
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY region_code, person_id, start_date
+            ORDER BY ABS(DATE_DIFF(ses.start_date, sen.date_imposed, DAY)) ASC) = 1
 )
 SELECT
     region_code,
     person_id,
     session_id,
     compartment_level_1,
-    session_end_date,
-    sentence_completion_date,
+    session_start_date,
+    sentence_date_imposed,
 FROM sessions_to_sentences
 WHERE sentence_to_session_offset_days != 0
 """
 
 
-SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+SESSION_NEW_ADMISSIONS_WITH_NO_SENTENCE_DATE_IMPOSED_VIEW_BUILDER = SimpleBigQueryViewBuilder(
     dataset_id=dataset_config.VIEWS_DATASET,
-    view_id=SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_VIEW_NAME,
-    view_query_template=SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_QUERY_TEMPLATE,
-    description=SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_DESCRIPTION,
+    view_id=SESSION_NEW_ADMISSIONS_WITH_NO_SENTENCE_DATE_IMPOSED_VIEW_NAME,
+    view_query_template=SESSION_NEW_ADMISSIONS_WITH_NO_SENTENCE_DATE_IMPOSED_QUERY_TEMPLATE,
+    description=SESSION_NEW_ADMISSIONS_WITH_NO_SENTENCE_DATE_IMPOSED_DESCRIPTION,
     sessions_dataset=SESSIONS_DATASET,
     should_materialize=True,
 )
 
 if __name__ == "__main__":
     with local_project_id_override(GCP_PROJECT_STAGING):
-        SESSION_LIBERTY_RELEASES_WITH_NO_SENTENCE_COMPLETION_DATE_VIEW_BUILDER.build_and_print()
+        SESSION_NEW_ADMISSIONS_WITH_NO_SENTENCE_DATE_IMPOSED_VIEW_BUILDER.build_and_print()
