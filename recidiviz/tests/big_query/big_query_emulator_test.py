ab0d2af5c5dda1d74076265ab7200f2f9ed26b6b
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2023-04-10 15:53:21 -0700

    [Testing] Add more test cases for the BigQueryEmulator (Recidiviz/recidiviz-data#20055)
    ## Description of the change
    
    While cleaning up a bunch of old local branches in preparation for
    getting a new computer, I found a set of BQ emulator sanity check tests
    I had written while I was trying to get TES BQ emulator tests to work.
    Might as well commit them!
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    N/A
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: b4369e8b6994ff47d083acfec880a6b82f1ec396

--- recidiviz/tests/big_query/big_query_emulator_test.py
+++ recidiviz/tests/big_query/big_query_emulator_test.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """Tests capabilities of the BigQuery emulator."""
+import datetime
 from datetime import date
 
 from google.api_core import exceptions
@@ -311,6 +312,52 @@ QUALIFY ROW_NUMBER() OVER (ORDER BY b DESC) = 1;
             expected_result=[{"min_a": date(2022, 1, 1), "max_b": date(2022, 4, 4)}],
         )
 
+    def test_query_min_max_dates_with_partition(self) -> None:
+        """Tests resolution of https://github.com/goccy/bigquery-emulator/issues/31."""
+        address = BigQueryAddress(dataset_id=_DATASET_1, table_id=_TABLE_1)
+        self.create_mock_table(
+            address,
+            schema=[
+                bigquery.SchemaField(
+                    "a",
+                    field_type=bigquery.enums.SqlTypeNames.DATE.value,
+                    mode="REQUIRED",
+                ),
+                bigquery.SchemaField(
+                    "b",
+                    field_type=bigquery.enums.SqlTypeNames.DATE.value,
+                    mode="NULLABLE",
+                ),
+            ],
+        )
+        self.load_rows_into_table(
+            address,
+            data=[
+                {"a": "2022-01-01", "b": "2022-03-03"},
+                {"a": "2022-02-02", "b": "2022-03-03"},
+                {"a": "2022-02-02", "b": "2022-04-04"},
+            ],
+        )
+
+        self.run_query_test(
+            f"SELECT MIN(a) OVER (PARTITION BY b) AS min_a, MAX(b) OVER (PARTITION BY a) AS max_b "
+            f"FROM `{self.project_id}.{address.dataset_id}.{address.table_id}`;",
+            expected_result=[
+                {
+                    "min_a": datetime.date(2022, 1, 1),
+                    "max_b": datetime.date(2022, 3, 3),
+                },
+                {
+                    "min_a": datetime.date(2022, 1, 1),
+                    "max_b": datetime.date(2022, 4, 4),
+                },
+                {
+                    "min_a": datetime.date(2022, 2, 2),
+                    "max_b": datetime.date(2022, 4, 4),
+                },
+            ],
+        )
+
     def test_query_min_with_parition(self) -> None:
         """Tests resolution of https://github.com/goccy/bigquery-emulator/issues/19."""
         self.run_query_test(
@@ -371,3 +418,222 @@ FROM UNNEST([STRUCT(1 AS a, 2 AS b)]);""",
                 """SELECT SAFE.PARSE_DATE("%m/%d/%Y", "2008-12-25") as a;""",
                 expected_result=[{"a": None}],
             )
+
+    def test_array_to_json(self) -> None:
+        # Tests resolution to https://github.com/goccy/bigquery-emulator/issues/24.
+        query = "SELECT TO_JSON([1, 2, 3]) as a;"
+        self.run_query_test(
+            query,
+            expected_result=[{"a": "[1,2,3]"}],
+        )
+
+    def test_to_json(self) -> None:
+        query = """SELECT TO_JSON(
+  STRUCT("foo" AS a, 1 AS b)
+) AS result;"""
+
+        self.run_query_test(
+            query,
+            expected_result=[{"result": '{"a":"foo","b":1}'}],
+        )
+
+    def test_to_json_nested(self) -> None:
+        query = """SELECT TO_JSON(
+  STRUCT("foo" AS a, TO_JSON(STRUCT("bar" AS c)) AS b)
+) AS result;"""
+
+        self.run_query_test(
+            query,
+            expected_result=[{"result": '{"a":"foo","b":{"c":"bar"}}'}],
+        )
+
+    def test_to_json_nested_cte(self) -> None:
+        query = """WITH inner_json AS (
+  SELECT TO_JSON(STRUCT("bar" AS c)) AS b
+)
+SELECT TO_JSON(STRUCT("foo" as a, b)) AS result
+FROM inner_json;"""
+
+        self.run_query_test(
+            query,
+            expected_result=[{"result": '{"a":"foo","b":{"c":"bar"}}'}],
+        )
+
+    def test_to_json_nested_cte_column_rename(self) -> None:
+        query = """WITH inner_json AS (
+  SELECT TO_JSON(STRUCT("bar" AS c)) AS b
+)
+SELECT TO_JSON(STRUCT("foo" AS a, b AS b_2)) AS result
+FROM inner_json;"""
+
+        self.run_query_test(
+            query,
+            expected_result=[{"result": '{"a":"foo","b_2":{"c":"bar"}}'}],
+        )
+
+    def test_to_json_nested_cte_numbers(self) -> None:
+        query = """WITH inner_json AS (
+    SELECT TO_JSON(STRUCT(1 AS c)) AS b
+)
+SELECT TO_JSON(STRUCT(2 as a, b)) AS result
+FROM inner_json;"""
+
+        self.run_query_test(
+            query,
+            expected_result=[{"result": '{"a":2,"b":{"c":1}}'}],
+        )
+
+    def test_to_json_nested_outer_array(self) -> None:
+        query = """WITH inner_json AS (
+    SELECT TO_JSON(STRUCT(1 AS c)) AS b
+)
+SELECT
+TO_JSON([
+    TO_JSON(STRUCT('foo' AS a, b))
+]) AS result
+FROM inner_json;"""
+
+        self.run_query_test(
+            query,
+            expected_result=[{"result": '[{"a":"foo","b":{"c":1}}]'}],
+        )
+
+    def test_nested_json_array_agg(self) -> None:
+        query = """WITH inner_table AS (
+  SELECT * 
+  FROM UNNEST([
+    STRUCT(
+      "foo" AS a, TO_JSON(STRUCT(1 AS b)) AS c
+    )
+  ])
+)
+SELECT TO_JSON(ARRAY_AGG(
+    TO_JSON(STRUCT(a, c))
+    ORDER BY a
+)) AS result
+FROM inner_table;"""
+        self.run_query_test(
+            query,
+            expected_result=[{"result": '[{"a":"foo","c":{"b":1}}]'}],
+        )
+
+    def test_array_agg(self) -> None:
+        query = """
+SELECT b, ARRAY_AGG(a) AS a_list
+FROM UNNEST([
+   STRUCT(1 AS a, 2 AS b),
+   STRUCT(3 AS a, 2 AS b)
+])
+GROUP BY b;
+"""
+        self.run_query_test(
+            query,
+            expected_result=[{"a_list": [1, 3], "b": 2}],
+        )
+
+    def test_array_agg_ignore_nulls_no_nulls(self) -> None:
+        query = """
+SELECT b, ARRAY_AGG(a IGNORE NULLS) AS a_list
+FROM UNNEST([
+   STRUCT(1 AS a, 2 AS b),
+   STRUCT(3 AS a, 2 AS b)
+])
+GROUP BY b;
+"""
+        self.run_query_test(
+            query,
+            expected_result=[{"a_list": [1, 3], "b": 2}],
+        )
+
+    # TODO(https://github.com/goccy/bigquery-emulator/issues/34): File task for this
+    def test_array_agg_ignore_nulls(self) -> None:
+        address = BigQueryAddress(dataset_id=_DATASET_1, table_id=_TABLE_1)
+        self.create_mock_table(
+            address,
+            schema=[
+                bigquery.SchemaField(
+                    "a",
+                    field_type=bigquery.enums.SqlTypeNames.INTEGER.value,
+                    mode="REQUIRED",
+                ),
+                bigquery.SchemaField(
+                    "b",
+                    field_type=bigquery.enums.SqlTypeNames.INTEGER.value,
+                    mode="NULLABLE",
+                ),
+            ],
+        )
+        self.load_rows_into_table(
+            address,
+            data=[{"a": None, "b": 2}, {"a": 3, "b": 2}],
+        )
+        query = f"""
+SELECT b, ARRAY_AGG(a IGNORE NULLS) AS a_list
+FROM `{self.project_id}.{address.dataset_id}.{address.table_id}`
+GROUP BY b;
+"""
+        self.run_query_test(
+            query,
+            expected_result=[{"a_list": [3], "b": 2}],
+        )
+
+    def test_array_agg_with_nulls(self) -> None:
+        """Tests fix for https://github.com/goccy/bigquery-emulator/issues/33"""
+        address = BigQueryAddress(dataset_id=_DATASET_1, table_id=_TABLE_1)
+        self.create_mock_table(
+            address,
+            schema=[
+                bigquery.SchemaField(
+                    "a",
+                    field_type=bigquery.enums.SqlTypeNames.INTEGER.value,
+                    mode="REQUIRED",
+                ),
+                bigquery.SchemaField(
+                    "b",
+                    field_type=bigquery.enums.SqlTypeNames.INTEGER.value,
+                    mode="NULLABLE",
+                ),
+            ],
+        )
+        self.load_rows_into_table(
+            address,
+            data=[{"a": None, "b": 2}, {"a": 3, "b": 2}],
+        )
+        query = f"""
+SELECT b, ARRAY_AGG(a) AS a_list
+FROM `{self.project_id}.{address.dataset_id}.{address.table_id}`
+GROUP BY b;
+"""
+        with self.assertRaisesRegex(
+            Exception, r"ARRAY_AGG: input value must be not null"
+        ):
+            self.run_query_test(
+                query,
+                expected_result=[{"a_list": [3], "b": 2}],
+            )
+
+    def test_null_in_unnest(self) -> None:
+        """Tests resolution of https://github.com/goccy/bigquery-emulator/issues/30."""
+        query = """
+SELECT a
+FROM UNNEST([
+   STRUCT(NULL AS a)
+]);
+"""
+        self.run_query_test(
+            query,
+            expected_result=[{"a": None}],
+        )
+
+    def test_date_in_unnest(self) -> None:
+        """Tests resolution of https://github.com/goccy/bigquery-emulator/issues/30."""
+        query = """
+SELECT a
+FROM UNNEST([
+  STRUCT(DATE(2022, 1, 1) AS a)
+]);
+"""
+        self.run_query_test(
+            query,
+            expected_result=[{"a": datetime.date(2022, 1, 1)}],
+        )
