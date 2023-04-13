f45d98194b1b84f20c9ce5bed50c364a31c1dcdf
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2023-04-13 13:37:37 -0400

    [US_NC][Not-yet-partner] Hydrate StateIncarcerationPeriod (Recidiviz/recidiviz-data#20141)
    ## Description of the change
    
    Extra notes included in the view and mappings files.
    
    This hydrates StateIncarcerationPeriod information based on sentence
    information found in raw data table `INMT4BB1` ("Sentence
    Computations"). Some of these sentences have started and ended, and
    others are currently active. Sentences are joined with raw data table
    `INMT4AA1` ("Inmate Profile") which includes details about _only the
    most recent_ sentence a person has received, and the _single most recent
    movement_ that person has made. So, if a person's last movement in the
    correctional system happened on the date that their sentence began or
    ended, the nature of that movement is stored as their `admission_reason`
    or `release_reason`.
    
    There are only a few cases where the results of this join to hydrate
    `admission_reason` and `release_reason` don't make logical sense; they
    are noted in the mapping .yaml, and they seem to result largely from
    individual differences in how movement type codes are used.
    
    The one exception seems to happen in the case when someone was released
    to supervision before the end date of their sentence, and then _more
    recently_ were released from parole on the date their sentence ended. In
    this case, the "most recent movement type" is `TERMINATED PAROLE` rather
    than `RELEASE TO PAROLE`. This issue would be resolved if we had access
    to _all_ of a person's movements rather than only their most recent one.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 65ce90a5e514fc3d05210ac010b2fe74449b31a9

---
+++ recidiviz/ingest/direct/regions/us_nc/ingest_views/view_incarceration_periods.py
@@ -0,0 +1,129 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query containing information about incarceration periods in NC."""
+
+from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
+    DirectIngestViewQueryBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+WITH sentences AS (
+-- This CTE joins historical sentence data with the details we can find in the Inmate Profile
+-- raw data (INMT4AA1) about an individual's latest period of incarceration. Most of the
+-- fields added here are blank for most of the rows in the resulting dataset, because most
+-- rows here do not correspond to a person's latest period of incarceration. This
+-- makes it easier to find instance where people were released to parole.
+-- This also updates end_dates when people died before their sentence end date.
+  SELECT 
+    sentences.CIDORNUM as doc_id,
+    GIEFFDT as sentence_begin_date,
+    CASE 
+      WHEN PARBEGDT != '0001-01-01 00:00:00' THEN LEAST(PARBEGDT, CIRELDAT) 
+      WHEN CILAMVTY = 'DEATH' THEN LEAST(CILAMVDT, CIRELDAT)
+      ELSE CIRELDAT 
+    END AS end_date,
+    CIPREFIX as sentence_prefix,
+    PARBEGDT as parole_begin_date,
+    CILAMVTY as movement_type,
+    CILAMVDT as movement_date,
+    CICURLOC as facility,
+    CICCLASS as custody_level,
+  FROM {INMT4BB1} sentences
+  LEFT JOIN {INMT4AA1} profiles
+  ON sentences.CIDORNUM = profiles.CIDORNUM
+  AND sentences.CIPREFIX = profiles.CIPREFX2
+  -- sentence start date is not in the future
+  WHERE GIEFFDT < CAST(@update_timestamp as string)
+),
+-- This CTE creates a new period in any case where a person was 'RETURNED FROM PAROLE'
+-- after being released to supervision. Since their new 'end_date' is the
+-- date on which their supervision period began, any movement involving a return from
+-- parole after that is assumed to be a revocation.
+-- This CTE adjusts the movement reason field for rows in which a person was released
+-- to parole before their sentence end date.
+new_revocation_periods AS (
+  SELECT 
+    doc_id,
+    sentence_begin_date,
+    end_date,
+    CASE
+      WHEN parole_begin_date != '0001-01-01 00:00:00'
+      AND end_date = parole_begin_date 
+      THEN 'PAROLE/RETURN TO PAR'
+      ELSE movement_type 
+    END AS movement_type,
+    movement_date,
+    facility,
+    custody_level,
+    parole_begin_date
+  FROM sentences
+
+  UNION ALL 
+
+  SELECT 
+    doc_id, 
+    movement_date as sentence_begin_date,
+    null as end_date,
+    movement_type,
+    movement_date,
+    facility,
+    custody_level,
+    parole_begin_date
+  FROM sentences
+  -- if this person was revoked after being released to parole
+  WHERE sentences.movement_date > end_date 
+  AND sentences.movement_type IN ('RETURNED FROM PAROLE')
+), 
+-- This CTE separates movement type into period start or end reasons based on which
+-- date the movement occurred on.
+sentences_with_reasons_split AS (
+  SELECT 
+    doc_id,
+    sentence_begin_date as start_date,
+    end_date,
+    CASE 
+      WHEN ABS(DATE_DIFF(movement_date, sentence_begin_date, DAY)) < 7 
+      THEN movement_type 
+      ELSE NULL 
+    END AS start_reason,
+    CASE
+      WHEN ABS(DATE_DIFF(movement_date, end_date, DAY)) < 7 
+      THEN movement_type
+      ELSE NULL
+    END AS end_reason,
+    facility,
+    custody_level
+FROM new_revocation_periods
+WHERE sentence_begin_date != end_date
+AND sentence_begin_date < end_date)
+
+SELECT * 
+FROM sentences_with_reasons_split
+"""
+
+VIEW_BUILDER = DirectIngestViewQueryBuilder(
+    region="us_nc",
+    ingest_view_name="incarceration_periods",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+    order_by_cols="doc_id",
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
