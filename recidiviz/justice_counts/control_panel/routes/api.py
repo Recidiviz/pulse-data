0a7cb43f0eddf71a81dac7af3e7fc241a9140f83
Michelle Orden <82831800+morden35@users.noreply.github.com>
2023-04-17 17:04:34 -0500

    [Justice Counts] Jurisdictions GET refactor (Recidiviz/recidiviz-data#20233)
    ## Description of the change
    
    This PR deletes the jurisdictions get endpoint and moves the logic to
    the get_agency_settings endpoint. This will reduce the number of calls
    the frontend makes to the backend. This is also more uniform and matches
    the logic of the put flow.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Related to Recidiviz/recidiviz-data#19010
    Related to [Recidiviz/recidiviz-data#574](https://github.com/Recidiviz/justice-counts/pull/574)
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 234d809b8bfdccc66faee245e1cee194ee147724

--- recidiviz/justice_counts/control_panel/routes/api.py
+++ recidiviz/justice_counts/control_panel/routes/api.py
@@ -170,27 +170,15 @@ def get_api_blueprint(
                 session=current_session,
                 agency_id=agency_id,
             )
-            return jsonify(
-                {"settings": [setting.to_json() for setting in agency_settings]}
-            )
-        except Exception as e:
-            raise _get_error(error=e) from e
-
-    @api_blueprint.route("/agencies/<agency_id>/jurisdictions", methods=["GET"])
-    @auth_decorator
-    def get_agency_jurisdictions(agency_id: int) -> Response:
-        try:
-            request_json = assert_type(request.values, dict)
-            user = UserAccountInterface.get_user_by_auth0_user_id(
-                session=current_session,
-                auth0_user_id=get_auth0_user_id(request_dict=request_json),
-            )
-            raise_if_user_is_not_in_agency(user=user, agency_id=agency_id)
-
             jurisdictions = AgencyJurisdictionInterface.to_json(
                 session=current_session, agency_id=agency_id
             )
-            return jsonify(jurisdictions)
+            return jsonify(
+                {
+                    "settings": [setting.to_json() for setting in agency_settings],
+                    "jurisdictions": jurisdictions,
+                }
+            )
         except Exception as e:
             raise _get_error(error=e) from e
 
