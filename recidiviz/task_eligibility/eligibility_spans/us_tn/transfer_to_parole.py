be7338bfa884f9a31578023e6de7dffc95b66a09
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2023-04-18 15:58:56 -0500

    [TES] Past Parole Eligibility Date general criteria and task queries for Parole and Geriatric Parole (Recidiviz/recidiviz-data#20223)
    ## Description of the change
    
    - Created general criteria to identify periods when incarcerated clients
    are past their PED
    - Task queries for Parole Eligibility in the following states:
        - CO
        - PA
        - MI
        - IX
        - MO
        - TN
    - Task queries for Geriatric Parole Eligibility in the following states:
        - CO
        - PA
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#20118
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 7dd48b2b3bf745d1f5563d2e757a7658496ad520

---
+++ recidiviz/task_eligibility/eligibility_spans/us_tn/transfer_to_parole.py
@@ -0,0 +1,54 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Builder for a task eligiblity spans view that shows the spans of time during which
+someone in TN is eligible parole.
+"""
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.candidate_populations.general import (
+    incarceration_population,
+)
+from recidiviz.task_eligibility.completion_events import release_to_parole
+from recidiviz.task_eligibility.criteria.general import (
+    incarcerated_at_least_1_year,
+    incarceration_past_parole_eligibility_date,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Builder for a task eligiblity spans view that shows the spans of time during which
+someone in TN is eligible parole.
+"""
+
+VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
+    state_code=StateCode.US_TN,
+    task_name="TRANSFER_TO_PAROLE",
+    description=_DESCRIPTION,
+    candidate_population_view_builder=incarceration_population.VIEW_BUILDER,
+    criteria_spans_view_builders=[
+        incarceration_past_parole_eligibility_date.VIEW_BUILDER,
+        incarcerated_at_least_1_year.VIEW_BUILDER,
+        # TODO(#20245) add half time criteria
+    ],
+    completion_event_builder=release_to_parole.VIEW_BUILDER,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
