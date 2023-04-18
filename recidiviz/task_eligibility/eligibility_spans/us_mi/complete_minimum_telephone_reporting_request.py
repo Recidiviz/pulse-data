5bb75bc9c409587fd8ee52e11ec48abf421904b3
Shalin Brahmbhatt <63821972+shalindb@users.noreply.github.com>
2023-04-17 17:00:32 -0700

    [US_MI] [Workflows] Minimum Telephone Reporting Request (Recidiviz/recidiviz-data#20272)
    ## Description of the change
    
    Added stubs for MI minimum telephone reporting:
    General
    - on active supervision at least 6 months
    - initial assessment level minimum or medium
    - supervision not within 90 days of full term completion date
    State Specific
    - not serving an OUIL or OWI
    - not serving ineligible offenses on supervision
    - supervision level is minimum low or minimum in person
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x ] Tests have been written to cover the code changed/added as part
    of this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: d5425345e051ffaf30ebe4f1d3b64a71a7e89369

---
+++ recidiviz/task_eligibility/eligibility_spans/us_mi/complete_minimum_telephone_reporting_request.py
@@ -0,0 +1,65 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Builder for a task eligibility spans view that shows the spans of time during which
+someone in MI is eligible for minimum telephone reporting.
+"""
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.candidate_populations.general import (
+    probation_parole_dual_active_supervision_population,
+)
+from recidiviz.task_eligibility.completion_events import supervision_level_downgrade
+from recidiviz.task_eligibility.criteria.general import (
+    initial_assessment_level_minimum_or_medium,
+    on_active_supervision_at_least_six_months,
+    supervision_not_within_90_days_of_full_term_completion_date,
+)
+from recidiviz.task_eligibility.criteria.state_specific.us_mi import (
+    not_serving_an_ouil_or_owi,
+    not_serving_ineligible_offenses_for_telephone_reporting,
+    supervision_level_is_minimum_low_or_minimum_in_person,
+    not_required_to_register_under_sora,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Shows the spans of time during which
+someone in MI is eligible for minimum telephone reporting.
+"""
+
+VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
+    state_code=StateCode.US_MI,
+    task_name="complete_minimum_telephone_reporting_request",
+    description=_DESCRIPTION,
+    candidate_population_view_builder=probation_parole_dual_active_supervision_population.VIEW_BUILDER,
+    criteria_spans_view_builders=[
+        not_serving_an_ouil_or_owi.VIEW_BUILDER,
+        not_serving_ineligible_offenses_for_telephone_reporting.VIEW_BUILDER,
+        supervision_level_is_minimum_low_or_minimum_in_person.VIEW_BUILDER,
+        initial_assessment_level_minimum_or_medium.VIEW_BUILDER,
+        not_required_to_register_under_sora.VIEW_BUILDER,
+        on_active_supervision_at_least_six_months.VIEW_BUILDER,
+        supervision_not_within_90_days_of_full_term_completion_date.VIEW_BUILDER,
+    ],
+    completion_event_builder=supervision_level_downgrade.VIEW_BUILDER,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
