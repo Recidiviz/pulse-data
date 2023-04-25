ad716058e9275a58507f07cb2c55071c4054a715
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2023-04-24 17:18:44 -0700

    [Outliers] Set up cloudsql instance for outliers (Recidiviz/recidiviz-data#20391)
    GitOrigin-RevId: 4f449a4b56528a03dc762206ccd169e75524448d

---
+++ recidiviz/calculator/query/state/views/outliers/outliers_enabled_states.py
@@ -0,0 +1,49 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2023 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""Lists the states for which outliers is enabled."""
+import os
+from typing import List
+
+import yaml
+
+import recidiviz
+from recidiviz.common.constants.states import StateCode
+
+yaml_path = os.path.join(
+    os.path.dirname(recidiviz.__file__),
+    "tools/deploy/terraform/config/outliers_enabled_states.yaml",
+)
+
+_outliers_enabled_states: List[str] = []
+
+
+def get_outliers_enabled_states() -> List[str]:
+    global _outliers_enabled_states
+
+    if _outliers_enabled_states:
+        return _outliers_enabled_states
+
+    with open(yaml_path, "r", encoding="utf-8") as ymlfile:
+        outliers_enabled_states: List[str] = yaml.full_load(ymlfile)
+
+    _outliers_enabled_states = [
+        StateCode[state_code].value
+        for state_code in outliers_enabled_states
+        if StateCode.is_state_code(state_code)
+    ]
+
+    return _outliers_enabled_states
