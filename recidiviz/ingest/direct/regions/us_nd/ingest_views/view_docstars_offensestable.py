529d6dbbeb0bde5d07dc30f6b64f29aee0625811
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2023-04-27 10:51:13 -0400

    [US_ND] Coalesce NCIC/CST codes in state_charge (Recidiviz/recidiviz-data#20541)
    ## Description of the change
    
    After investigating using the `docstars_cst_ncic_xref` table that ND
    sent (not ingested), it turns out that the `Common_Statute_NCIC_Code`
    field in `docstars_offensestable` raw data uses the same NCIC system as
    the `CODE` field. It is unclear why these are separated. Regardless,
    this change to the view and mapping for ingest of
    `docstars_offensestable` coalesces the two codes into the `ncic_code`
    field in the mapping, where the description and violence flag are pulled
    from the common `ncic.csv` reference table.
    
    These codes are mutually exclusive, so the order of the `COALESCE`
    should never matter, but just in case rows ever start coming that have
    both codes, the view prioritizes NCIC codes stored in the `CODE` field.
    
    This also updates the raw data config to make clear that the `CODE` and
    `Common_Statute_NCIC_Code` both contain legacy NCIC codes.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#20328
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] This pull request has been moved out of a Draft state, has no
    "Work In Progress" label, and has assigned reviewers
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 3b0430e3eaf846667f84d1dc5f982a7f534dc025

--- recidiviz/ingest/direct/regions/us_nd/ingest_views/view_docstars_offensestable.py
+++ recidiviz/ingest/direct/regions/us_nd/ingest_views/view_docstars_offensestable.py
@@ -18,10 +18,8 @@
 
 The raw data refers to offenses by either the legacy NCIC code or the Common Statute
 NCIC Code. These are mututally exclusive - there are currently no rows in the raw data
-that have both fields populated. The descriptions and violence indicators for these codes
-come from distinct reference files. Because the legacy NCIC code reference is shared 
-across states and the CST code reference file is ND-specific, information for rows with
-CST codes is populated in the view; info for legacy NCIC codes is populated in the mapping.
+that have both fields populated. The Common_Statute_NCIC_Code field stores legacy NCIC
+codes, so these two fields are coalesced before being passed to the ingest mapping. 
 
 The Common_Statute_Number field is populated in the raw data if and only if the CST code
 is used, so it can be used to determine if a given row uses the legacy or CST system."""
@@ -33,37 +31,29 @@ from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_QUERY_TEMPLATE = """
-WITH base_offenses AS (
-SELECT ot.*,
-oc.JUDGE,
+SELECT  
+  ot.SID, 
+  ot.CASE_NUMBER, 
+  ot.RecID, 
+  ot.LEVEL, 
+  ot.COUNTS, 
+  ot.OFFENSEDATE, 
+  ot.COUNTY, 
+  ot.COUNT, 
+  ot.COURT_NUMBER, 
+  ot.INACTIVEDATE, 
+  ot.RecDate, 
+  ot.YEAR, 
+  ot.LAST_UPDATE, 
+  ot.CREATED_BY, 
+  ot.MASTER_OFFENSE_IND,
+  ot.REQUIRES_REGISTRATION,
+  ot.Common_Statute_Number, 
+  COALESCE(CODE, Common_Statute_NCIC_Code) as ncic_code,
+  oc.JUDGE
 FROM {docstars_offensestable} ot
 LEFT JOIN {docstars_offendercasestable} oc
 USING (CASE_NUMBER)
-),
-offenses_with_desc AS (
-  SELECT
-    base.*,
-    UPPER(cst.DESCRIPTION) AS description
-  FROM base_offenses base
-  LEFT JOIN {recidiviz_docstars_cst_ncic_code} cst
-  ON base.Common_Statute_NCIC_Code = cst.CODE
-),
-offenses_with_violence_flag AS (
-  SELECT off.*,
-  CASE 
-  WHEN Common_Statute_NCIC_Code IS NOT NULL AND CrimCodeTxt IS NULL AND description IS NOT NULL THEN 'N'
-  WHEN Common_Statute_NCIC_Code IS NOT NULL AND CrimCodeTxt IS NOT NULL AND description IS NOT NULL THEN 'Y'
-  ELSE NULL
-  END AS violence_flag
-FROM offenses_with_desc off
-LEFT JOIN {RECIDIVIZ_REF_violent_crimes_codes} vc
-ON off.Common_Statute_NCIC_Code = vc.CrimCodeTxt
-)
-SELECT  LEVEL, COUNTS, OFFENSEDATE, COUNTY, SID, CASE_NUMBER, RecID, COUNT, COURT_NUMBER, 
-INACTIVEDATE, RecDate, YEAR, LAST_UPDATE, CREATED_BY, MASTER_OFFENSE_IND,REQUIRES_REGISTRATION,
-Common_Statute_Number, JUDGE, description, violence_flag,
-COALESCE(Common_Statute_NCIC_Code, CODE) as ncic_code
-FROM offenses_with_violence_flag
 """
 
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
