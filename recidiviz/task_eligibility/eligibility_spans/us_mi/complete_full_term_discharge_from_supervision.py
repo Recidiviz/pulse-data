95bd2b28b388b00f4eb14602373e3ee5b26000be
Shalin Brahmbhatt <63821972+shalindb@users.noreply.github.com>
2023-05-03 16:32:44 -0700

    [US_MI] [Workflows] Changed candidate population query for complete_full_term_discharge_from_supervision (Recidiviz/recidiviz-data#20709)
    ## Description of the change
    
    Changed candidate population query from
    `probation_parole_dual_active_supervision_population` to
    `parole_dual_active_supervision_population` in the
    `complete_full_term_discharge_from_supervision` opportunity
    
    Eligible clients down from 452 to 128 (324 less)
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#20655
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: b5fd45ffb2afcd2396943b9ac36d9dca8902cad5

--- recidiviz/task_eligibility/eligibility_spans/us_mi/complete_full_term_discharge_from_supervision.py
+++ recidiviz/task_eligibility/eligibility_spans/us_mi/complete_full_term_discharge_from_supervision.py
@@ -19,7 +19,7 @@ someone in MI is eligible for full term discharge from supervision.
 """
 from recidiviz.common.constants.states import StateCode
 from recidiviz.task_eligibility.candidate_populations.general import (
-    probation_parole_dual_active_supervision_population,
+    parole_dual_active_supervision_population,
 )
 from recidiviz.task_eligibility.completion_events.general import full_term_discharge
 from recidiviz.task_eligibility.criteria.general import (
@@ -39,7 +39,7 @@ VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
     state_code=StateCode.US_MI,
     task_name="COMPLETE_FULL_TERM_DISCHARGE_FROM_SUPERVISION",
     description=_DESCRIPTION,
-    candidate_population_view_builder=probation_parole_dual_active_supervision_population.VIEW_BUILDER,
+    candidate_population_view_builder=parole_dual_active_supervision_population.VIEW_BUILDER,
     criteria_spans_view_builders=[
         supervision_past_full_term_completion_date.VIEW_BUILDER,
     ],
