117e994cdc99fadf14f5afa0d1e00ff321f3daf9
Gloria Liu <gloria@recidiviz.org>
2023-05-10 11:15:48 -0400

    --skip-build argument to not rebuild and submit pipeline (Recidiviz/recidiviz-data#20824)
    ## Description of the change
    
    skip building+submitting the pipeline image and flex template if
    --skip-build is set
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#20803
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Anna Geiduschek <anna@recidiviz.org>
    GitOrigin-RevId: 76f1af814e3288f81163820ac6a82f565df00653

--- recidiviz/tools/calculator/run_sandbox_calculation_pipeline.py
+++ recidiviz/tools/calculator/run_sandbox_calculation_pipeline.py
@@ -31,6 +31,7 @@ usage: python -m recidiviz.tools.calculator.run_sandbox_calculation_pipeline \
           [--data_input INPUT] \
           [--reference_view_input REFERENCE_VIEW_INPUT] \
           [--static_reference_input STATIC_REFERENCE_VIEW_INPUT] \
+          [--skip_build] \
           # Note: The --metric_types arg must be last since it is a list
           [--metric_types METRIC_TYPES]
 
@@ -72,7 +73,7 @@ import json
 import logging
 import os
 import time
-from typing import Type
+from typing import List, Tuple, Type
 
 import google.auth
 import google.auth.transport.requests
@@ -92,7 +93,7 @@ from recidiviz.calculator.pipeline.supplemental.pipeline_parameters import (
 from recidiviz.tools.utils.script_helpers import prompt_for_confirmation, run_command
 
 
-def parse_run_arguments() -> PipelineParameters:
+def parse_run_arguments() -> Tuple[argparse.Namespace, List[str]]:
     """Parses the arguments needed to start a sandbox pipeline to a Namespace."""
     parser = argparse.ArgumentParser()
 
@@ -105,17 +106,32 @@ def parse_run_arguments() -> PipelineParameters:
         required=True,
     )
 
-    known_args, remaining_args = parser.parse_known_args()
+    parser.add_argument(
+        "--skip_build",
+        dest="skip_build",
+        help="If set to true, the image will not be rebuilt and submitted. "
+        "Useful for if you did not change pipeline code and want to run "
+        "pipelines with different parameters (e.g. for different states) "
+        "for the same pipeline image",
+        required=False,
+        action=argparse.BooleanOptionalAction,
+    )
 
+    return parser.parse_known_args()
+
+
+def parse_pipeline_parameters(
+    pipeline_type: str, remaining_args: List[str]
+) -> PipelineParameters:
     parameter_cls: Type[PipelineParameters]
-    if known_args.pipeline_type == "metrics":
+    if pipeline_type == "metrics":
         parameter_cls = MetricsPipelineParameters
-    elif known_args.pipeline_type == "normalization":
+    elif pipeline_type == "normalization":
         parameter_cls = NormalizationPipelineParameters
-    elif known_args.pipeline_type == "supplemental":
+    elif pipeline_type == "supplemental":
         parameter_cls = SupplementalPipelineParameters
     else:
-        raise ValueError(f"Unexpected pipeline_type [{known_args.pipeline_type}]")
+        raise ValueError(f"Unexpected pipeline_type [{pipeline_type}]")
 
     return parameter_cls.parse_from_args(remaining_args, sandbox_pipeline=True)
 
@@ -146,7 +162,8 @@ def get_template_path(pipeline_type: str) -> str:
 
 def run_sandbox_calculation_pipeline() -> None:
     """Runs the pipeline designated by the given --pipeline argument."""
-    params = parse_run_arguments()
+    known_args, remaining_args = parse_run_arguments()
+    params = parse_pipeline_parameters(known_args.pipeline_type, remaining_args)
     _, username = os.path.split(params.template_metadata_subdir)
     launch_body = params.flex_template_launch_body()
     template_gcs_path = params.template_gcs_path(params.project)
@@ -162,31 +179,36 @@ def run_sandbox_calculation_pipeline() -> None:
         f"`{params.output}` using `create_or_update_dataflow_sandbox`?"
     )
 
-    submit_build_start = time.time()
+    if not known_args.skip_build:
+        submit_build_start = time.time()
 
-    # Build and submit the image to "us-docker.pkg.dev/recidiviz-staging/dataflow-dev/{username}-build:latest"
-    print(
-        "Submitting build (this takes a few minutes, or longer on the first run).....\n"
-    )
-    run_command(
-        f"gcloud builds submit --project={params.project} --config {cloudbuild_absolute_path} --substitutions=_IMAGE_PATH={artifact_reg_image_path}",
-        timeout_sec=900,
-    )
+        # Build and submit the image to "us-docker.pkg.dev/recidiviz-staging/dataflow-dev/{username}-build:latest"
+        print(
+            "Submitting build (this takes a few minutes, or longer on the first run).....\n"
+        )
+        run_command(
+            f"gcloud builds submit --project={params.project} --config {cloudbuild_absolute_path} --substitutions=_IMAGE_PATH={artifact_reg_image_path}",
+            timeout_sec=900,
+        )
 
-    submit_build_exec_seconds = time.time() - submit_build_start
-    build_minutes, build_seconds = divmod(submit_build_exec_seconds, 60)
-    print(f"Submitted build in {build_minutes} minutes and {build_seconds} seconds.\n")
+        submit_build_exec_seconds = time.time() - submit_build_start
+        build_minutes, build_seconds = divmod(submit_build_exec_seconds, 60)
+        print(
+            f"Submitted build in {build_minutes} minutes and {build_seconds} seconds.\n"
+        )
 
-    # Upload the flex template json, tagged with the image that should be used
-    # This step is only necessary when the template_metadata.json file or image path has been changed
-    print(f"Building flex template (uploading to {template_gcs_path}) .....\n")
-    run_command(
-        f"gcloud dataflow flex-template build \
-        {template_gcs_path} \
-        --image {artifact_reg_image_path} \
-        --sdk-language PYTHON \
-        --metadata-file {template_absolute_path}"
-    )
+        # Upload the flex template json, tagged with the image that should be used
+        # This step is only necessary when the template_metadata.json file or image path has been changed
+        print(f"Building flex template (uploading to {template_gcs_path}) .....\n")
+        run_command(
+            f"gcloud dataflow flex-template build \
+            {template_gcs_path} \
+            --image {artifact_reg_image_path} \
+            --sdk-language PYTHON \
+            --metadata-file {template_absolute_path}"
+        )
+    else:
+        print("--skip_build is set... skipping build...")
 
     # Run the dataflow job
     pipeline_launch_body = json.dumps(launch_body, indent=2)
