bd6df796e9284493db785c209c843caa3736c73a
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2023-05-10 13:55:27 -0700

    [Outliers] Refactor outliers config objects (Recidiviz/recidiviz-data#20862)
    GitOrigin-RevId: e2b6815ecb8d41b19efe3d4ec4ea524a7afe83fc

--- recidiviz/calculator/query/state/views/outliers/supervision_metrics_helpers.py
+++ recidiviz/calculator/query/state/views/outliers/supervision_metrics_helpers.py
@@ -17,54 +17,67 @@
 """Helpers for building supervision metrics views """
 from typing import Dict, List
 
+import attr
+
 from recidiviz.calculator.query.bq_utils import list_to_query_string
 from recidiviz.calculator.query.state.views.analyst_data.models.metric_unit_of_analysis_type import (
     MetricUnitOfAnalysis,
+    MetricUnitOfAnalysisType,
 )
 from recidiviz.common.constants.states import StateCode
 
-METRICS_TO_EXPORT: Dict[str, List[str]] = {
-    "incarceration_starts_and_inferred_technical_violation": [
-        StateCode.US_PA.value,
-    ],
-    "incarceration_starts_technical_violation": [
-        StateCode.US_IX.value,
-    ],
-    "absconsions_bench_warrants": [
-        StateCode.US_IX.value,
-    ],
-    "incarceration_starts": [
-        StateCode.US_IX.value,
-    ],
-    "task_completions_early_discharge": [
-        StateCode.US_IX.value,
-    ],
-    "task_completions_transfer_to_limited_supervision": [
-        StateCode.US_IX.value,
-    ],
-    "task_completions_full_term_discharge": [
-        StateCode.US_IX.value,
-    ],
+
+@attr.s
+class OutliersConfig:
+    # List of metrics that are relevant for this state,
+    # where each element corresponds to a column name in an aggregated_metrics views
+    metrics: List[str] = attr.ib()
+
+    # Location exclusions; a unit of analysis mapped to a list of ids to exclude
+    unit_of_analysis_to_exclusion: Dict[MetricUnitOfAnalysisType, List[str]] = attr.ib(
+        default=None
+    )
+
+
+OUTLIERS_CONFIGS_BY_STATE: Dict[StateCode, OutliersConfig] = {
+    StateCode.US_IX: OutliersConfig(
+        metrics=[
+            "incarceration_starts_technical_violation",
+            "absconsions_bench_warrants",
+            "incarceration_starts",
+            "task_completions_early_discharge",
+            "task_completions_transfer_to_limited_supervision",
+            "task_completions_full_term_discharge",
+        ],
+    ),
+    StateCode.US_PA: OutliersConfig(
+        metrics=["incarceration_starts_and_inferred"],
+        unit_of_analysis_to_exclusion={
+            MetricUnitOfAnalysisType.SUPERVISION_DISTRICT: ["FAST", "CO"]
+        },
+    ),
 }
 
 
 def supervision_metric_query_template(
     unit_of_analysis: MetricUnitOfAnalysis,
 ) -> str:
-    query = "\nUNION ALL\n".join(
-        [
-            f"""
-SELECT 
+    subqueries = []
+    for state_code, config in OUTLIERS_CONFIGS_BY_STATE.items():
+        for metric in config.metrics:
+            subqueries.append(
+                f"""
+SELECT
     {list_to_query_string(unit_of_analysis.primary_key_columns)},
     period,
     end_date,
     "{metric}" AS metric_id,
     {metric} AS metric_value,
     avg_daily_population,
-FROM `{{project_id}}.{{aggregated_metrics_dataset}}.supervision_{unit_of_analysis.level_name_short}_aggregated_metrics_materialized` 
-WHERE state_code IN ({list_to_query_string(states, quoted=True)})
-            """
-            for metric, states in METRICS_TO_EXPORT.items()
-        ]
-    )
+FROM `{{project_id}}.{{aggregated_metrics_dataset}}.supervision_{unit_of_analysis.level_name_short}_aggregated_metrics_materialized`
+WHERE state_code = '{state_code.value}'
+"""
+            )
+
+    query = "\nUNION ALL\n".join(subqueries)
     return query
