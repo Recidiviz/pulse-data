1bcaad88a094f7c27702929a63ca3edeef124cae
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2023-06-09 10:28:59 -0700

    [Build] Install cmake in initial_pipenv_setup_mac.sh (Recidiviz/recidiviz-data#21463)
    ## Description of the change
    
    One of our dependencies now requires that `cmake` is installed locally.
    Adds this installation to the `initial_pipenv_setup_mac.sh` script so
    nobody forgets to install on future setup.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Follow up to this thread:
    https://recidiviz.slack.com/archives/GJDCVR2AY/p1685994967464229
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: f711364fe21d1824aae4394845d1b88887bcfe17

--- initial_pipenv_setup_mac.sh
+++ initial_pipenv_setup_mac.sh
@@ -7,9 +7,15 @@ chmod +x .git/hooks/*
 # Set git blame to ignore noisy commits
 git config blame.ignoreRevsFile .git-blame-ignore-revs
 
-# Sync with openssl linked properly
+# Ensure openssl is linked properly
 export LDFLAGS="-L/usr/local/opt/openssl/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl/include"
+
+# Install cmake to avoid qdldl build error: 'RuntimeError: CMake must be
+# installed to build qdldl'
+brew install cmake
+
+# Sync dependencies now that environment is setup properly
 pipenv sync --dev
 
 # Install pre-commit hooks
