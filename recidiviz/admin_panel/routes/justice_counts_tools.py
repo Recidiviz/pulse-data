c69fb77f167c91282457c39f28afab992fa6643c
Michelle Orden <82831800+morden35@users.noreply.github.com>
2023-06-26 14:55:32 -0500

    [Justice Counts] Add CSG users to prod agencies as read_only (Recidiviz/recidiviz-data#21773)
    ## Description of the change
    
    Whenever we create a new agency in production, all CSG users (users with
    '@csg.org' email) will be automatically added to the agency as
    'READ_ONLY'.
    
    It's a hard to test this since we only want to add users in production
    on non-test agencies. However, I commented out that condition and tested
    in dev using test emails and it seemed to work:
    
    <img width="1299" alt="Screenshot 2023-06-26 at 12 35 13 PM"
    src="https://github.com/Recidiviz/recidiviz-data/assets/82831800/e353aae2-e488-4c0e-894f-5aee35c8333f">
    
    <img width="1300" alt="Screenshot 2023-06-26 at 12 35 18 PM"
    src="https://github.com/Recidiviz/recidiviz-data/assets/82831800/a9a04f6b-090a-451f-96ae-6b58b68818f8">
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Related to Recidiviz/recidiviz-data#21506
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    Co-authored-by: Michelle Orden <michelleorden@michelles-mbp.lan>
    GitOrigin-RevId: 482c969c8674397e978a807cdfe230bc39f7cffe

--- recidiviz/admin_panel/routes/justice_counts_tools.py
+++ recidiviz/admin_panel/routes/justice_counts_tools.py
@@ -34,6 +34,7 @@ from recidiviz.persistence.database.schema_type import SchemaType
 from recidiviz.persistence.database.session_factory import SessionFactory
 from recidiviz.persistence.database.sqlalchemy_database_key import SQLAlchemyDatabaseKey
 from recidiviz.utils.auth.gae import requires_gae_auth
+from recidiviz.utils.environment import in_gcp_production
 from recidiviz.utils.types import assert_type, non_optional
 
 _auth0_client = None
@@ -98,6 +99,18 @@ def add_justice_counts_tools_routes(bp: Blueprint) -> None:
                     state_code=state_code,
                     fips_county_code=fips_county_code,
                 )
+
+                # Add all CSG accounts as READ_ONLY in production
+                if "test" not in name.lower() and in_gcp_production():
+                    csg_users = UserAccountInterface.get_csg_users(session=session)
+                    for csg_user in csg_users:
+                        AgencyUserAccountAssociationInterface.update_user_role(
+                            role="READ_ONLY",
+                            user=csg_user,
+                            agency=agency,
+                            session=session,
+                        )
+
                 return (
                     jsonify({"agency": agency.to_json()}),
                     HTTPStatus.OK,
