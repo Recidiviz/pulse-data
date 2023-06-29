0d37333adea85e6db29b1c281bf6ce6d7629dfac
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2023-06-28 19:21:52 -0500

    [US_CA] SLD criteria, opportunity and form queries (Recidiviz/recidiviz-data#21603)
    ## Description of the change
    
    Created the following for the CA Supervision Level Downgrade
    opportunity:
    
    1. Individual criteria queries:
    - `supervision_level_is_high_for_6_months.py`: checks that the client is
    Category A (supervision_level = HIGH) for 6 months before being eligible
    - `assessment_level_3_or_lower.py`: checks that the last CSRA of the
    client is below 4.
    - `housing_type_is_not_transient.py`: only created the file with a
    placeholder/stub query. @jkgerig is planning to write it soon.
    - `no_violation_within_6_months.py`: only created the file with a
    placeholder/stub query. @not-a-doctor-stromberg is planning to write the
    query soon.
    2. Candidate population query: I created a new view that only cares
    about the parole active population
    (`parole_active_supervision_population.py`). Does this make sense for
    you, @jkgerig, and @not-a-doctor-stromberg? I.e.
    ```
            AND compartment_level_2 NOT IN ('INTERNAL_UNKNOWN', 'ABSCONSION', 'BENCH_WARRANT')
            AND correctional_level NOT IN ('IN_CUSTODY','WARRANT','ABSCONDED','ABSCONSION','EXTERNAL_UNKNOWN')
    ```
    3. The task query itself:
    `us_ca_supervision_level_downgrade_form_record.py`, this doesn't contain
    much yet. I'm assuming we'll add more info as case notes and almost
    eligible features once we gather more feedback from testers.
    
    I've uploaded all into a sandbox with prefix `hsalas`.
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#21489
    Closes Recidiviz/recidiviz-data#21895
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: cd1ee5b721febd026f1c8bac9caf4f7a6ec2fdf1

---
+++ recidiviz/calculator/query/state/views/workflows/firestore/us_ca_supervision_level_downgrade_form_record.py
@@ -0,0 +1,79 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Queries information needed to fill out the SLD form in CA
+"""
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.query.bq_utils import nonnull_end_date_exclusive_clause
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.calculator.query.state.dataset_config import NORMALIZED_STATE_DATASET
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.dataset_config import (
+    task_eligibility_spans_state_specific_dataset,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+US_CA_SUPERVISION_LEVEL_DOWNGRADE_VIEW_NAME = (
+    "US_CA_SUPERVISION_LEVEL_DOWNGRADE_FORM_RECORD"
+)
+
+US_CA_SUPERVISION_LEVEL_DOWNGRADE_DESCRIPTION = """
+    Queries information needed to fill out the SLD form in CA
+    """
+
+
+US_CA_SUPERVISION_LEVEL_DOWNGRADE_QUERY_TEMPLATE = f"""
+WITH current_parole_pop_cte AS (
+    -- Keep only people in Parole
+    SELECT pei.external_id,
+        tes.state_code,
+        tes.reasons,
+        tes.ineligible_criteria,
+        tes.is_eligible,
+    FROM `{{project_id}}.{{task_eligibility_dataset}}.supervision_level_downgrade_materialized` tes
+    LEFT JOIN `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` pei
+        USING(person_id)
+    WHERE 
+      CURRENT_DATE('US/Pacific') BETWEEN tes.start_date AND 
+                                         {nonnull_end_date_exclusive_clause('tes.end_date')}
+      AND tes.state_code = 'US_CA'
+)
+
+SELECT 
+    external_id,
+    state_code,
+    reasons,
+    ineligible_criteria,
+FROM current_parole_pop_cte
+WHERE is_eligible
+"""
+
+US_CA_SUPERVISION_LEVEL_DOWNGRADE_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=dataset_config.WORKFLOWS_VIEWS_DATASET,
+    view_id=US_CA_SUPERVISION_LEVEL_DOWNGRADE_VIEW_NAME,
+    view_query_template=US_CA_SUPERVISION_LEVEL_DOWNGRADE_QUERY_TEMPLATE,
+    description=US_CA_SUPERVISION_LEVEL_DOWNGRADE_DESCRIPTION,
+    normalized_state_dataset=NORMALIZED_STATE_DATASET,
+    task_eligibility_dataset=task_eligibility_spans_state_specific_dataset(
+        StateCode.US_CA
+    ),
+    should_materialize=True,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        US_CA_SUPERVISION_LEVEL_DOWNGRADE_VIEW_BUILDER.build_and_print()
