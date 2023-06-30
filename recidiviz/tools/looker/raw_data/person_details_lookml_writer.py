7ad7a0a0b686d0e0c202e6d550e747f88c2fe6a5
Ryan <24993797+rzgn@users.noreply.github.com>
2023-06-30 15:35:52 -0700

    Setup files for raw data LookML view generation (Recidiviz/recidiviz-data#21925)
    ## Description of the change
    
    > Create `rediciviz/tools/looker/raw_data`, which will contain code that
    generates LookML files for raw data exploration, currently the main
    script and LookML View generator in skeleton format. Also create
    `recidiviz/tests/tools/looker/raw_data`, for unit test files for the
    view generation code.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | **Type: Non-breaking refactor** | **change addresses some tech debt
    item or prepares for a later change, but does not change functionality**
    |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    n/a
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Ryan Guan <rzgn@users.noreply.github.com>
    GitOrigin-RevId: 394c7f189a074f06f4ea8b241880b2ef231bc90d

---
+++ recidiviz/tools/looker/raw_data/person_details_lookml_writer.py
@@ -0,0 +1,56 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""A script for building a set of LookML views, explores and dashboards 
+for raw data tables and writing them to files.
+
+Run the following to write files to the specified directory DIR:
+python -m recidiviz.tools.looker.raw_data.person_details_lookml_writer --looker_repo_root [DIR]
+"""
+
+import argparse
+
+from recidiviz.tools.looker.raw_data.person_details_view_generator import (
+    generate_lookml_views,
+)
+
+
+def parse_arguments() -> argparse.Namespace:
+    """Parses the required arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--looker_repo_root",
+        dest="looker_dir",
+        help="Specifies local path to the Looker repo, where all files will be saved to",
+        type=str,
+        required=True,
+    )
+
+    return parser.parse_args()
+
+
+def main(looker_dir: str) -> None:
+    # TODO(#21937): Generate views here
+    # TODO(#21938): Generate explores here
+    # TODO(#21939): Generate dashboards here
+    print(looker_dir)
+    generate_lookml_views()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args.looker_dir)
