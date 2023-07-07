25993016b7d4928a3244347e3ad56e4111c456ac
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2023-07-07 14:35:30 -0500

    [US_ME] [Workflows] Adjustment to half-time date - ET Workflows (Recidiviz/recidiviz-data#21710)
    ## Description of the change
    
    We received a message from ME suggesting we surfaced the wrong start
    date for some people's probation terms. Specifically, people going from
    SCCP -> Probation. They were wrong because we were taking the SCCP start
    time as the start of the probation term. In this PR, I hope to fix this
    problem.
    
    Before, we used `compartment_level_1_super_sessions` to determine these
    start dates. We will use the same table, but I will partition it with
    SCCP sessions (using `compartment_sessions`). This PR generates two
    relevant changes:
    
    1. Eligibility criteria: I changed the criteria view that calculated our
    half time: (now)
    `supervision_past_half_full_term_release_date_from_probation_start`.
    This caused a drop in people eligible for ME ET from 1059 to 1050. This
    is expected. I would greatly appreciate a review from @samanthahuff on
    this logic.
    2. Client record: I changed how we calculate our `start_date`. This
    change did not significantly impact our client_record other than pushing
    start_dates further in the future for 161/5469 clients. I can use a
    review from someone on the Polaris team in this regard (@Catacola,
    @danielsmc).
    
    You can find everything in a sandbox with the prefix` hsalas`.
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#21654
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: bf1350b73dcca2a5f578007d66ced4697a9466e5

--- recidiviz/task_eligibility/criteria/state_specific/us_me/supervision_past_half_full_term_release_date_from_probation_start.py
+++ recidiviz/task_eligibility/criteria/state_specific/us_me/supervision_past_half_full_term_release_date_from_probation_start.py
@@ -17,41 +17,47 @@
 
 """
 Defines a criteria span view that calculates half time across all supervision sentences; 
-where start_date is the first time a person began supervision (based on super_sessions) 
-and end_date is the final projected_release_date all supervision sentences considered.
+where start_date is the first time a person began probation (based on super_sessions 
+after partitioning ME SCCP sessions) and end_date is the final projected_release_date 
+all supervision sentences considered.
 
 This view was created because SUPERVISION_PAST_HALF_FULL_TERM_RELEASE_DATE didn't use
-the right start_date in ME. Once the ingest mappings are fixed in ME, we will hopefully 
-deprecate this criteria view builder.
+the right start_date in ME.
 """
 
 from recidiviz.calculator.query.bq_utils import nonnull_end_date_clause
 from recidiviz.calculator.query.state.dataset_config import SESSIONS_DATASET
+from recidiviz.common.constants.states import StateCode
 from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
-    StateAgnosticTaskCriteriaBigQueryViewBuilder,
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
 )
 from recidiviz.task_eligibility.utils.critical_date_query_fragments import (
     critical_date_has_passed_spans_cte,
 )
+from recidiviz.task_eligibility.utils.preprocessed_views_query_fragments import (
+    compartment_level_1_super_sessions_without_me_sccp,
+)
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
-_CRITERIA_NAME = "SUPERVISION_PAST_HALF_FULL_TERM_RELEASE_DATE_FROM_SUPERVISION_START"
+_CRITERIA_NAME = (
+    "US_ME_SUPERVISION_PAST_HALF_FULL_TERM_RELEASE_DATE_FROM_PROBATION_START"
+)
 
 _DESCRIPTION = """
 Defines a criteria span view that calculates half time across all supervision sentences; 
-where start_date is the first time a person began supervision (based on super_sessions) 
-and end_date is the final projected_release_date all supervision sentences considered.
+where start_date is the first time a person began probation (based on super_sessions 
+after partitioning ME SCCP sessions) and end_date is the final projected_release_date 
+all supervision sentences considered.
 
 This view was created because SUPERVISION_PAST_HALF_FULL_TERM_RELEASE_DATE didn't use
-the right start_date in ME. Once the ingest mappings are fixed in ME, we will hopefully 
-deprecate this criteria view builder.
+the right start_date in ME.
 """
 
 _QUERY_TEMPLATE = f"""
-#TODO(#19981) Deprecate this criteria builder once the supervision sentence effective date aligns with the probation session start date
+WITH {compartment_level_1_super_sessions_without_me_sccp()},
 
-WITH critical_date_spans AS (
+critical_date_spans AS (
     SELECT
         sss.state_code,
         sss.person_id,
@@ -64,13 +70,14 @@ WITH critical_date_spans AS (
         DATE_ADD(sss.start_date,
                 INTERVAL CAST(FLOOR(DATE_DIFF(projected_completion_date_max, sss.start_date, DAY)/2) AS INT64) DAY)
         AS critical_date,
-    FROM `{{project_id}}.{{sessions_dataset}}.compartment_level_1_super_sessions_materialized` sss
+    FROM partitioning_compartment_l1_ss_with_sccp sss
     LEFT JOIN `{{project_id}}.{{sessions_dataset}}.supervision_projected_completion_date_spans_materialized` proj_end
         ON sss.compartment_level_1 = "SUPERVISION"
             AND sss.state_code = proj_end.state_code 
             AND sss.person_id = proj_end.person_id 
             AND sss.start_date < {nonnull_end_date_clause('proj_end.end_date_exclusive')}
             AND proj_end.start_date < {nonnull_end_date_clause('sss.end_date_exclusive')}
+    WHERE COALESCE(compartment_level_2, "") != 'COMMUNITY_CONFINEMENT'
 ),
 
 {critical_date_has_passed_spans_cte()}
@@ -86,10 +93,11 @@ SELECT
 FROM critical_date_has_passed_spans cd
 """
 
-VIEW_BUILDER: StateAgnosticTaskCriteriaBigQueryViewBuilder = (
-    StateAgnosticTaskCriteriaBigQueryViewBuilder(
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
+    StateSpecificTaskCriteriaBigQueryViewBuilder(
         criteria_name=_CRITERIA_NAME,
         description=_DESCRIPTION,
+        state_code=StateCode.US_ME,
         criteria_spans_query_template=_QUERY_TEMPLATE,
         sessions_dataset=SESSIONS_DATASET,
     )
