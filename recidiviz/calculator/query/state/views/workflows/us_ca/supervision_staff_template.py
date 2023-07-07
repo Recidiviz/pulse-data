80a0808e0a70e67568b76d1c404901eba7caffcd
Daniel McLaughlin <danielm@recidiviz.org>
2023-07-07 13:54:01 -0400

    [US_CA] Workflows Staff Record (Recidiviz/recidiviz-data#22017)
    ## Description of the change
    
    Creates a staff_record for CA parole officers. Emails are currently
    placeholders, and the district assignment logic may need to be refined.
    Right now officers with caseloads are assigned to the district where
    they have the most clients, and agents without caseloads are assigned to
    a district if there's only one possibility in the raw data.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#21532
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 5731ec2a7ff45baa7788929468fe030574dbe916

---
+++ recidiviz/calculator/query/state/views/workflows/us_ca/supervision_staff_template.py
@@ -0,0 +1,64 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+"""View logic to prepare US_CA supervision staff data for Workflows"""
+
+US_CA_SUPERVISION_STAFF_TEMPLATE = """
+    WITH
+    -- Assign officers with caseloads to the district where they have the most clients
+    caseload_districts AS (
+        SELECT BadgeNumber, ParoleDistrict as district
+            FROM `{project_id}.{us_ca_raw_tables_dataset}.PersonParole`
+            WHERE
+                update_datetime = '2023-05-10T00:00:00'
+                AND ParoleDistrict IS NOT NULL
+            GROUP BY BadgeNumber, ParoleDistrict
+            QUALIFY ROW_NUMBER() OVER (PARTITION BY BadgeNumber ORDER BY COUNT(*) desc) = 1
+    ),
+    -- Assign officers without caseloads only if they're associated with a singular district
+    no_caseload_districts AS (
+        SELECT BadgeNumber, ANY_VALUE(ParoleDistrict) AS district
+            FROM `{project_id}.{us_ca_raw_data_up_to_date_dataset}.AgentParole_latest`
+            WHERE ParoleDistrict IS NOT NULL
+            GROUP BY BadgeNumber, ParoleDistrict
+            HAVING count(DISTINCT ParoleDistrict)=1
+    )
+    SELECT
+        state_staff.state_code,
+        external_id AS id,
+        JSON_VALUE(full_name, "$.given_names") AS given_names,
+        JSON_VALUE(full_name, "$.surname") AS surname,
+        UPPER(JSON_VALUE(full_name, "$.given_names") || " " || JSON_VALUE(full_name, "$.middle_names") || JSON_VALUE(full_name, "$.surname")) AS name,
+        "PLACEHOLDER." || JSON_VALUE(full_name, "$.given_names") || "." || JSON_VALUE(full_name, "$.surname") || "@cdcr.ca.gov" AS email,
+        external_id IN (
+            SELECT DISTINCT
+                officer_id
+            FROM `{project_id}.{workflows_dataset}.client_record_materialized`
+            WHERE state_code="US_CA"
+                AND officer_id IS NOT NULL
+        ) AS has_caseload,
+        FALSE AS has_facility_caseload,
+        IFNULL(caseload_districts.district, no_caseload_districts.district) AS district
+
+    FROM `{project_id}.{normalized_state_dataset}.state_staff` state_staff
+    LEFT JOIN `{project_id}.{normalized_state_dataset}.state_staff_external_id` eid
+        USING(staff_id)
+    LEFT JOIN caseload_districts ON eid.external_id = caseload_districts.BadgeNumber
+    LEFT JOIN no_caseload_districts ON eid.external_id = no_caseload_districts.BadgeNumber
+    WHERE
+        state_staff.state_code = "US_CA"
+ """
