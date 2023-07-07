455f9617058fd8aeccb7db643a53c49002afb8ae
Dan Hansen <dan@recidiviz.org>
2023-07-07 11:32:52 -0700

    [Airflow] Use `project_id()` in the `update_all_managed_views` entrypoint (Recidiviz/recidiviz-data#22052)
    ## Description of the change
    
    GKE Pods have access to the metadata server.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 3bab0cf44d3a512bb225a9cc38ab9c9674c942e7

--- recidiviz/entrypoints/view_update/update_all_managed_views.py
+++ recidiviz/entrypoints/view_update/update_all_managed_views.py
@@ -22,6 +22,7 @@ import sys
 from typing import List, Tuple
 
 from recidiviz.big_query.view_update_manager import execute_update_all_managed_views
+from recidiviz.utils.metadata import project_id
 
 
 def parse_arguments(argv: List[str]) -> Tuple[argparse.Namespace, List[str]]:
@@ -43,4 +44,4 @@ if __name__ == "__main__":
 
     known_args, _ = parse_arguments(sys.argv)
 
-    execute_update_all_managed_views(known_args.project_id, known_args.sandbox_prefix)
+    execute_update_all_managed_views(project_id(), known_args.sandbox_prefix)
