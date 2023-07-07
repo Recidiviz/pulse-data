0100daa9a8bd98b95d47c63ae6859c270f850a35
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2023-07-07 10:33:31 -0500

    [US_CA] [Workflows] Housing criteria + supervision level fix  (Recidiviz/recidiviz-data#22002)
    ## Description of the change
    
    Two changes:
    1. Fixed a bug in one of the criteria -
    `supervision_level_is_high_for_6_months`.
    2. Filled the housing criteria so that only individuals with sustainable
    housing are eligible for the SLD opportunity in CA -
    `housing_type_is_not_transient`
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#21996
    Closes Recidiviz/recidiviz-data#21486
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: a48f2acfd2bf59eafe2bf99b4e04ec0b8cbfbfa6

---
+++ recidiviz/task_eligibility/criteria/state_specific/us_ca/housing_type_is_not_transient.py
@@ -0,0 +1,78 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines a criteria span view that shows spans of time during which someone has a stable
+housing type (is not transient)
+"""
+
+from recidiviz.calculator.query.bq_utils import nonnull_end_date_clause
+from recidiviz.calculator.query.state.dataset_config import (
+    ANALYST_VIEWS_DATASET,
+    NORMALIZED_STATE_DATASET,
+)
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "US_CA_HOUSING_TYPE_IS_NOT_TRANSIENT"
+
+_DESCRIPTION = """Defines a criteria span view that shows spans of time during which someone has a stable
+housing type (is not transient)
+"""
+
+_QUERY_TEMPLATE = f"""
+# TODO(#21353): switch to supervision periods and make a state agnostic criteria
+WITH sustainable_housing AS (
+  SELECT 
+    peid.person_id,
+    CAST(sp.start_date AS DATE) AS start_date,
+    CAST(sp.end_date AS DATE) AS end_date, 
+  FROM `{{project_id}}.{{analyst_dataset}}.us_ca_sustainable_housing_status_periods_materialized` sp
+  LEFT JOIN `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` peid
+    ON peid.state_code = 'US_CA'
+      AND sp.OffenderId = peid.external_id
+  # Sustainable housing is defined as not homeless/transient.
+  WHERE sustainable_housing = 1
+)
+
+SELECT 
+  "US_CA" AS state_code,
+  person_id, 
+  start_date,
+  end_date,
+  TRUE AS meets_criteria,
+  TO_JSON(STRUCT('Not transient' AS current_housing)) AS reason
+FROM sustainable_housing
+WHERE start_date != {nonnull_end_date_clause('end_date')}
+"""
+
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
+    StateSpecificTaskCriteriaBigQueryViewBuilder(
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+        state_code=StateCode.US_CA,
+        criteria_spans_query_template=_QUERY_TEMPLATE,
+        analyst_dataset=ANALYST_VIEWS_DATASET,
+        normalized_state_dataset=NORMALIZED_STATE_DATASET,
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
