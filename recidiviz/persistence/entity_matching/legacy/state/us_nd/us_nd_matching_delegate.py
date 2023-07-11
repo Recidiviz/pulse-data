a45ae43df9db67ecb28fc835fb6bc4333843eb40
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2023-07-11 11:48:03 -0400

    [US_ND] Combine incarceration sentences views into one (Recidiviz/recidiviz-data#21888)
    ## Description of the change
    
    The following five ingest views all hydrate various parts of the
    incarceration sentence / charge portions of our schema:
    
    - `elite_offendersentenceaggs`
    - `elite_offendersentences`
    - `elite_offendersentenceterms`
    - `elite_state_charge`
    - `elite_orderstable`
    
    This change replaces them with a single ingest view query that joins
    those 5 tables together:
    
    - `elite_incarceration_sentences`
    
    This new view hydrates `StateIncarcerationSentence`, as well as
    associated `StateCharge` objects, the same way that the previous set of
    views did, with the following exception:
    - `StateIncarcerationSentence.date_imposed` is hydrated from
    `elite_offendersentenceterms.START_DATE`, which contains the start date
    of a collection of sentence terms all tied to the same
    `StateIncarcerationSentence` object.
    
    I tried to use `elite_orderstable.COURT_DATE` to hydrate
    `StateIncarcerationSentence.date_imposed`. The `COURT_DATE` field has
    the raw data description "When the court order began" and is identical
    to the `CONVICTION_DATE` field in 99.98% of rows in the same file. This
    ended up being infeasible because the `date_imposed` field is tied to a
    sentence, and the `COURT_DATE` field is at a charge-level of
    granularity, so there would be multiple `date_imposed` values tied to a
    single `StateIncarcerationSentence.external_id`.
    
    This PR also removes the check for `external_id` hydration on the
    `StateIncarcerationSentence` object in the ND state DB because we are no
    longer be producing incarceration sentences without a hydrated
    `external_id`.
    
    Finally, this change removes the five views listed at the top of this
    description, as well as their associated mappings and tests.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#14801
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 09243d1cb9bf00386b0dde871aec39640e91013a

--- recidiviz/persistence/entity_matching/legacy/state/us_nd/us_nd_matching_delegate.py
+++ recidiviz/persistence/entity_matching/legacy/state/us_nd/us_nd_matching_delegate.py
@@ -55,11 +55,6 @@ class UsNdMatchingDelegate(StateSpecificEntityMatchingDelegate):
                 # We will be able to remove StateAgent from this list when we stop
                 # generating supervision officer StateAgent objects with no external_id.
                 schema.StateAgent,
-                # TODO(#14801): As part of a rewrite of the ND incarceration sentence
-                # ingest view, we should no longer be producing incarceration sentences
-                # without a hydrated external_id, so we should be able to remove
-                # StateIncarcerationSentence from this list.
-                schema.StateIncarcerationSentence,
                 # We will be able to remove StateSupervisionViolationResponse from this
                 # list when we stop generating StateSupervisionViolationResponse objects
                 # with no external_id.
