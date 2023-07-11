67603c63de7b3ef9d27f299b11a87bc5285a9ffc
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2023-07-11 16:19:58 -0400

    Revert "[US_ND] Combine incarceration sentences views into one (Recidiviz/recidiviz-data#21888)" (Recidiviz/recidiviz-data#22155)
    This reverts commit 09243d1cb9bf00386b0dde871aec39640e91013a.
    
    ## Description of the change
    
    This view needs a few more tweaks to datetime parsing before it can be
    deployed without causing errors.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: be2a438e10c6f6a1090dfb1fecf641770f4c4a38

--- recidiviz/ingest/direct/regions/us_nd/us_nd_controller.py
+++ recidiviz/ingest/direct/regions/us_nd/us_nd_controller.py
@@ -23,7 +23,6 @@ from recidiviz.ingest.direct.controllers.base_direct_ingest_controller import (
     BaseDirectIngestController,
 )
 from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
-from recidiviz.utils import environment
 
 
 class UsNdController(BaseDirectIngestController):
@@ -51,20 +50,10 @@ class UsNdController(BaseDirectIngestController):
             "elite_offenders",
             "elite_alias",
             "elite_offenderbookingstable",
-        ]
-
-        # gate legacy incarceration sentence views to production
-        if environment.in_gcp_production():
-            tags.extend(
-                [
-                    "elite_offendersentenceaggs",
-                    "elite_offendersentences",
-                    "elite_offendersentenceterms",
-                    "elite_state_charges",
-                ]
-            )
-
-        tags += [
+            "elite_offendersentenceaggs",
+            "elite_offendersentences",
+            "elite_offendersentenceterms",
+            "elite_state_charges",
             "elite_externalmovements_incarceration_periods",
             "elite_offense_in_custody_and_pos_report_data",
             # Docstars - supervision-focused
@@ -78,12 +67,7 @@ class UsNdController(BaseDirectIngestController):
             "docstars_staff",
             "docstars_staff_location_periods",
             "docstars_staff_role_periods",
-            "docstars_staff_supervisor_periods",
+            "docstars_staff_supervisor_periods"
+            # TODO(#1918): Integrate bed assignment / location history
         ]
-
-        # gate new combined incarceration sentence view to staging
-        if not environment.in_gcp_production():
-            tags.extend(["elite_incarceration_sentences"])
-
-        # TODO(#1918): Integrate bed assignment / location history
         return tags
