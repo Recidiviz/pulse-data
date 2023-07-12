5407a3bd5a60ee19fe025327bbacc8c46aee2719
Claire Camacho <104117954+claire25camacho@users.noreply.github.com>
2023-07-12 11:03:32 -0700

    [Justice Counts] Fixed template_generator.py with comments taken into account (Recidiviz/recidiviz-data#22176)
    …late function
    
    ## Description of the change
    
    Fixed template_generator.py with comments taken into account. Will
    update with datetime fix soon.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Works on Recidiviz/recidiviz-data#21384
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: d92be296567b3d9263f582ad777eed3d3dedda6e

---
+++ recidiviz/justice_counts/bulk_upload/template_generator.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warragit addnty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Utilities for generating templates for Bulk Upload.
+"""
+
+
+from typing import Any, Dict
+
+import pandas as pd
+
+from recidiviz.justice_counts.metricfiles.metricfile_registry import (
+    SYSTEM_TO_METRICFILES,
+)
+from recidiviz.persistence.database.schema.justice_counts import schema
+
+
+def generate_bulk_upload_template(system: schema.System, file_path: str) -> None:
+    """Generates a Bulk Upload template for a particular agency."""
+    metricfiles = SYSTEM_TO_METRICFILES[system]
+    filename_to_rows = {}
+    for metricfile in metricfiles:  # pylint: disable=too-many-nested-blocks
+        rows = []
+        if (
+            metricfile.definition.reporting_frequency
+            == schema.ReportingFrequency.ANNUAL
+        ):
+            if metricfile.definition.system == schema.System.SUPERVISION:
+                for s in [s.name for s in schema.System.supervision_subsystems()] + [
+                    "ALL"
+                ]:
+                    for year in [2021, 2022]:
+                        row = {"year": str(year), "system": s, "value": ""}
+                        rows.append(row)
+            else:
+                for year in [2021, 2022]:
+                    row = {"year": str(year), "value": ""}
+                    rows.append(row)
+        else:
+            if metricfile.definition.system == schema.System.SUPERVISION:
+                for s in [s.name for s in schema.System.supervision_subsystems()] + [
+                    "ALL"
+                ]:
+                    for year in [2021, 2022]:
+                        for month in range(1, 13):
+                            row = {
+                                "year": str(year),
+                                "month": str(month),
+                                "system": s,
+                                "value": "",
+                            }
+                            rows.append(row)
+            else:
+                for year in [2021, 2022]:
+                    for month in range(1, 13):
+                        row = {"year": str(year), "month": str(month), "value": ""}
+                        rows.append(row)
+
+        new_rows = []
+        if metricfile.disaggregation:
+            for row in rows:
+                row.pop("value")  # move value column last
+                for (
+                    dimension
+                ) in metricfile.disaggregation:  # type: ignore[attr-defined]
+                    new_row: Dict[str, Any] = {
+                        **row,
+                        metricfile.disaggregation_column_name: dimension.value,  # type: ignore
+                        "value": "",
+                    }
+                    new_rows.append(new_row)
+        else:
+            new_rows = rows
+
+        filename_to_rows[metricfile.canonical_filename] = new_rows
+
+    with pd.ExcelWriter(  # pylint: disable=abstract-class-instantiated
+        file_path
+    ) as writer:
+        for filename, rows in filename_to_rows.items():
+            df = pd.DataFrame.from_dict(rows)
+            df.to_excel(writer, sheet_name=filename, index=False)
+            worksheet = writer.sheets[filename]
+            for idx, col in enumerate(df):
+                series = df[col]
+                # set column length to max string length in column
+                # default column length is 8
+                max_len = max(series.astype(str).map(len).max(), 8)
+                worksheet.set_column(idx, idx, max_len)
