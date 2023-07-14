eca90118c9399e9b030fc4a6d92efc42f93a9fa1
Dan Hansen <dan@recidiviz.org>
2023-07-14 14:05:53 -0700

    [Admin Panel] Add on-call error logs explorer (Recidiviz/recidiviz-data#22249)
    ## Description of the change
    
    This adds a rudimentary version of an error log explorer to the admin
    panel. The functionality is somewhat duplicative of what Sentry would
    provide us. Adoption of Sentry would require another vendor security
    review as we would be leaking some PII in our exception tracing. As a
    result I decided to roll our own lightweight error tracker. It also
    allows us to implement some of our implementation-specific logic that
    other error trackers may not do for us.
    
    The few on-call log review pain-points that this aims to answer are the
    following:
    * What is the exception that occurred?
    * When did it start failing?
    * Has this endpoint since started succeeding?
    
    <img width="2048" alt="Screenshot 2023-07-13 at 6 09 44 PM"
    src="https://github.com/Recidiviz/recidiviz-data/assets/437360/3937b6ba-5ad6-4cd3-a996-3cadd11ad063">
    
    Exceptions are grouped by the URL. Links to view logs for any of the
    failing requests are included in the "traces" tab. The tag showing when
    the exception last occurred will turn green if there was a 200 response
    for the endpoint after most recent error response.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 8445ea16789b1341e22b11ec6423cdc053aa3ca9

---
+++ recidiviz/admin_panel/routes/on_call.py
@@ -0,0 +1,42 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines routes for the on-call section of the admin panel."""
+
+from flask import Blueprint, Response, jsonify, request
+
+from recidiviz.admin_panel.on_call.on_call_logs_search import LogsView, OnCallLogsSearch
+from recidiviz.utils.auth.gae import requires_gae_auth
+
+
+def add_on_call_routes(blueprint: Blueprint) -> None:
+    @blueprint.route("/on_call/logs", methods=["POST"])
+    @requires_gae_auth
+    def logs() -> Response:
+        """
+        Request Body:
+            view: (string) LogsView enum value controls which logs are returned
+        Returns:
+            JSON representation of the logs
+        """
+        view = LogsView.DIRECT_INGEST
+        if request.json and "view" in request.json:
+            try:
+                view = LogsView(request.json["view"])
+            except ValueError:
+                pass
+
+        return jsonify(OnCallLogsSearch().query(view))
