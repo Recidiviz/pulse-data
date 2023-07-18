afe6ed34bfde8ed877ae0d8b65f58d09a23c8416
n-damiani <113454156+n-damiani@users.noreply.github.com>
2023-07-18 12:20:27 -0500

    [US_MO] Add date validation to StateStaffRolePeriod normalization (Recidiviz/recidiviz-data#22361)
    ## Description of the change
    
    Some of the data used to construct StateStaffRolePeriod in MO contains
    invalid dates, which has been causing normalization to fail. This adds a
    MO-specific `normalize_role_periods` method which removes dates that are
    < 1900 or are > 10 years in the future. There are also some role period
    data where start date > end date; both dates are invalidated in these
    cases.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#22227
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: ba196243bca562106c1f4f352b476ea3b28133e5

--- recidiviz/pipelines/utils/state_utils/us_mo/us_mo_staff_role_period_normalization_delegate.py
+++ recidiviz/pipelines/utils/state_utils/us_mo/us_mo_staff_role_period_normalization_delegate.py
@@ -15,6 +15,11 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """Contains US_MO implementation of the StateSpecificStaffRolePeriodNormalizationDelegate."""
+import datetime
+from typing import List, Optional
+
+from recidiviz.persistence.entity.entity_utils import deep_entity_update
+from recidiviz.persistence.entity.state.entities import StateStaffRolePeriod
 from recidiviz.pipelines.normalization.utils.normalization_managers.staff_role_period_normalization_manager import (
     StateSpecificStaffRolePeriodNormalizationDelegate,
 )
@@ -24,3 +29,43 @@ class UsMoStaffRolePeriodNormalizationDelegate(
     StateSpecificStaffRolePeriodNormalizationDelegate
 ):
     """US_MO implementation of the StateSpecificStaffRolePeriodNormalizationDelegate."""
+
+    def normalize_role_periods(
+        self, role_periods: List[StateStaffRolePeriod]
+    ) -> List[StateStaffRolePeriod]:
+
+        return [
+            deep_entity_update(
+                rp,
+                start_date=rp.start_date
+                if self._is_date_valid(rp.start_date)
+                and self._period_bounds_valid(rp.start_date, rp.end_date)
+                else None,
+                end_date=rp.end_date
+                if self._is_date_valid(rp.end_date)
+                and self._period_bounds_valid(rp.start_date, rp.end_date)
+                else None,
+            )
+            for rp in role_periods
+        ]
+
+    @staticmethod
+    def _period_bounds_valid(
+        start_date: Optional[datetime.date], end_date: Optional[datetime.date]
+    ) -> bool:
+        """For MO, there are numerous cases where a role period is recorded with an
+        end date earlier than the start date. Since at least one of the dates must be
+        erroneous, we invalidate them both."""
+        if start_date and end_date:
+            return end_date >= start_date
+        return True
+
+    @staticmethod
+    def _is_date_valid(date_bound: Optional[datetime.date]) -> bool:
+        """For MO, there are numerous cases where the role period start/end date appears
+        to be entered incorrectly, but nonetheless can be parsed into a date in the future
+        or the distant past. Any dates before 1900 are treated as invalid, and dates in
+        the future are removed as well to indicate open periods."""
+        return not date_bound or (
+            date_bound.year >= 1900 and date_bound <= datetime.date.today()
+        )
