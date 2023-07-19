de05a1aa0e30d2cf7dd83b95b5e6ea6ea15c2887
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2023-07-19 18:41:03 -0400

    Revert "Double-write housing unit type and category (Recidiviz/recidiviz-data#22251)" (Recidiviz/recidiviz-data#22441)
    This reverts commit 42b194ad2f1cb51e050240b0e62ab879865c84c5.
    
    ## Description of the change
    
    > Description here
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: fb8a9406e9f149505c79312945d29090567c4d12

--- recidiviz/calculator/query/state/views/sessions/justice_impact_sessions.py
+++ recidiviz/calculator/query/state/views/sessions/justice_impact_sessions.py
@@ -66,13 +66,10 @@ WITH weights AS (
             WHEN compartment_level_1 IN (
                 "DEATH", "ERRONEOUS_RELEASE", "INTERNAL_UNKNOWN", "LIBERTY", "SUSPENSION"
             ) THEN 0
-            -- TODO(#22252): Delete this reference to `housing_unit_type` once 
-            -- `housing_unit_category` replaces it as the super-type for housing placement
             WHEN housing_unit_type IN (
                 "TEMPORARY_SOLITARY_CONFINEMENT",
                 "PERMANENT_SOLITARY_CONFINEMENT"
-            ) OR housing_unit_category IN ("SOLITARY_CONFINEMENT")
-            THEN {SOLITARY_CONFINEMENT_WEIGHT}
+            ) THEN {SOLITARY_CONFINEMENT_WEIGHT}
             WHEN compartment_level_1 IN (
                 "INCARCERATION", "INCARCERATION_OUT_OF_STATE", "PENDING_CUSTODY"
             ) THEN
