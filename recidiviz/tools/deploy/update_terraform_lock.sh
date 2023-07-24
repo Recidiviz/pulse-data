464eccca3f7c568ac19242b876c4edd6eb1f0777
phenggeler <10621768+phenggeler@users.noreply.github.com>
2023-07-24 18:41:48 +0000

    [Workspace Setup] Support Terraform Runs in VSCode Containers (Recidiviz/recidiviz-data#22554)
    ## Description of the change
    
    > We have to support the Linux architecture that is used by VS Code
    Containers
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 9b7889649d3013f7649beca1483f0fc3cadf93df

--- recidiviz/tools/deploy/update_terraform_lock.sh
+++ recidiviz/tools/deploy/update_terraform_lock.sh
@@ -38,4 +38,6 @@ check_terraform_installed
 
 terraform -chdir=recidiviz/tools/deploy/terraform providers lock \
   -platform=darwin_amd64 \
-  -platform=darwin_arm64
+  -platform=darwin_arm64 \
+  -platform=linux_arm64 \
+  -platform=linux_amd64
