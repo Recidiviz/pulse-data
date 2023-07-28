65714c0595095acfd3552b2db0080014b2ea4fa4
Daniel McLaughlin <danielm@recidiviz.org>
2023-07-28 12:17:50 -0400

    [US_CA] Staff record fixes (Recidiviz/recidiviz-data#22724)
    GitOrigin-RevId: eb314a9e60199f38aeec091c8be566458d727314

--- recidiviz/calculator/query/state/views/workflows/us_ca/supervision_staff_template.py
+++ recidiviz/calculator/query/state/views/workflows/us_ca/supervision_staff_template.py
@@ -19,25 +19,14 @@
 
 US_CA_SUPERVISION_STAFF_TEMPLATE = """
     WITH
-    -- Assign officers with caseloads to the district where they have the most clients
+    -- Assign officers to the district where they have the most clients
     caseload_districts AS (
-        SELECT officer_id as BadgeNumber, ParoleUnit as district
+        SELECT BadgeNumber, ParoleUnit as district
             FROM `{project_id}.{us_ca_raw_data_up_to_date_dataset}.PersonParole_latest`
-            -- TODO(#22427) Once PersonParole has badge numbers, delete this join on client record
-            INNER JOIN `{project_id}.{workflows_dataset}.client_record_materialized`
-                ON OffenderId=person_external_id
             WHERE
                 ParoleUnit IS NOT NULL
-            GROUP BY officer_id, ParoleUnit
-            QUALIFY ROW_NUMBER() OVER (PARTITION BY officer_id ORDER BY COUNT(*) desc) = 1
-    ),
-    -- Assign officers without caseloads only if they're associated with a singular district
-    no_caseload_districts AS (
-        SELECT BadgeNumber, ANY_VALUE(ParoleUnit) AS district
-            FROM `{project_id}.{us_ca_raw_data_up_to_date_dataset}.AgentParole_latest`
-            WHERE ParoleUnit IS NOT NULL
             GROUP BY BadgeNumber, ParoleUnit
-            HAVING count(DISTINCT ParoleUnit)=1
+            QUALIFY ROW_NUMBER() OVER (PARTITION BY BadgeNumber ORDER BY COUNT(*) desc) = 1
     )
     SELECT
         state_staff.state_code,
@@ -45,22 +34,19 @@ US_CA_SUPERVISION_STAFF_TEMPLATE = """
         JSON_VALUE(full_name, "$.given_names") AS given_names,
         JSON_VALUE(full_name, "$.surname") AS surname,
         UPPER(JSON_VALUE(full_name, "$.given_names") || " " || JSON_VALUE(full_name, "$.middle_names") || JSON_VALUE(full_name, "$.surname")) AS name,
-        "PLACEHOLDER." || JSON_VALUE(full_name, "$.given_names") || "." || JSON_VALUE(full_name, "$.surname") || "@cdcr.ca.gov" AS email,
-        external_id IN (
-            SELECT DISTINCT
-                officer_id
-            FROM `{project_id}.{workflows_dataset}.client_record_materialized`
-            WHERE state_code="US_CA"
-                AND officer_id IS NOT NULL
-        ) AS has_caseload,
+        JSON_VALUE(full_name, "$.given_names") || "." || JSON_VALUE(full_name, "$.surname") || "@cdcr.ca.gov" AS email,
+        TRUE AS has_caseload,
         FALSE AS has_facility_caseload,
-        IFNULL(caseload_districts.district, no_caseload_districts.district) AS district
+        caseload_districts.district
 
     FROM `{project_id}.{normalized_state_dataset}.state_staff` state_staff
     LEFT JOIN `{project_id}.{normalized_state_dataset}.state_staff_external_id` eid
         USING(staff_id)
     LEFT JOIN caseload_districts ON eid.external_id = caseload_districts.BadgeNumber
-    LEFT JOIN no_caseload_districts ON eid.external_id = no_caseload_districts.BadgeNumber
-    WHERE
-        state_staff.state_code = "US_CA"
+    INNER JOIN (
+        SELECT DISTINCT state_code, officer_id as external_id
+        FROM `{project_id}.{workflows_dataset}.client_record_materialized`
+        WHERE state_code="US_CA"
+    ) USING(external_id)
+    WHERE state_staff.state_code = "US_CA"
  """
