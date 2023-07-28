e35e54a415527a6e7066f37ff3855428e7e10511
Dan Hansen <dan@recidiviz.org>
2023-07-28 09:22:59 -0700

    [Airflow] Alter incident unique id; add more conditions for skipping triggering incidents (Recidiviz/recidiviz-data#22710)
    ## Description of the change
    This updates the `AirflowAlertingIncident` to take a list of all failed
    `execution_dates` rather than just the most recent failure. The unique
    id now uses the earliest failed `execution_date` for the incident rather
    than the `last succeeded` date.
    
    I've also added a handful of predicates for incidents that we wish to
    suppress.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: ab1d5c4a78b927c506ec4554060181a628a417e3

---
+++ recidiviz/airflow/dags/monitoring/alerting_trigger_predicates.py
@@ -0,0 +1,126 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Predicates for whether an incident should be sent to PagerDuty"""
+import enum
+import os
+from datetime import datetime, timedelta, timezone
+from typing import Callable, List, Optional, Tuple
+
+import attr
+
+from recidiviz.airflow.dags.monitoring.airflow_alerting_incident import (
+    AirflowAlertingIncident,
+)
+
+
+class TriggerPredicateMethod(enum.Enum):
+    SILENCE = "silence"
+    PRECONDITION = "precondition"
+
+
+@attr.s(auto_attribs=True)
+class AlertingIncidentTriggerPredicate:
+    """Predicate containing rules for incident inclusion"""
+
+    method: TriggerPredicateMethod
+    condition: Callable[
+        [AirflowAlertingIncident],
+        bool,
+    ]
+    failure_message: str
+    dag_id: Optional[str] = attr.ib(default=None)
+
+
+def incident_has_been_updated_recently(incident: AirflowAlertingIncident) -> bool:
+    """Incidents are only reported to PagerDuty if they are "active" (opened, still occurring, or resolved) within
+    the last two days.
+    This has no intentional functional difference and is only done to save on SendGrid email quota.
+    edge case: if the monitoring DAG is down for more than 2 days, some auto-resolve emails will be missed
+    """
+    now_utc = datetime.now(tz=timezone.utc)
+    most_recent_update = max(
+        incident.previous_success_date or datetime.fromtimestamp(0, tz=timezone.utc),
+        incident.most_recent_failure,
+        incident.next_success_date or datetime.fromtimestamp(0, tz=timezone.utc),
+    )
+
+    return timedelta(days=2) > now_utc - most_recent_update
+
+
+def get_trigger_predicates() -> List[AlertingIncidentTriggerPredicate]:
+    project_id = os.environ["GCP_PROJECT"]
+    return [
+        AlertingIncidentTriggerPredicate(
+            method=TriggerPredicateMethod.PRECONDITION,
+            condition=incident_has_been_updated_recently,
+            failure_message="incident has not occurred recently",
+        ),
+        AlertingIncidentTriggerPredicate(
+            method=TriggerPredicateMethod.PRECONDITION,
+            dag_id=f"{project_id}_calculation_dag",
+            condition=lambda incident: (
+                "ingest_instance" in incident.conf_obj
+                and incident.conf_obj["ingest_instance"] == "PRIMARY"
+            ),
+            failure_message="incident is not for the primary ingest instance",
+        ),
+        AlertingIncidentTriggerPredicate(
+            method=TriggerPredicateMethod.SILENCE,
+            dag_id=f"{project_id}_calculation_dag",
+            condition=lambda incident: "state_code_branch_end" in incident.task_id,
+            failure_message="state_code_branch_end is not an actionable failure",
+        ),
+        AlertingIncidentTriggerPredicate(
+            method=TriggerPredicateMethod.SILENCE,
+            dag_id=f"{project_id}_sftp_dag",
+            condition=lambda incident: (
+                incident.task_id == "US_IX.remote_file_download.download_sftp_files"
+                and len(incident.failed_execution_dates) == 1
+            ),
+            failure_message="must fail at least twice",
+        ),
+    ]
+
+
+def should_trigger_airflow_alerting_incident(
+    incident: AirflowAlertingIncident,
+) -> Tuple[bool, List[str]]:
+    """Returns whether the incident should be triggered and a list of reasons why if it should not
+    In order for an incident to be triggered:
+    * All TriggerPredicateMethod.PRECONDITION predicate conditions must be true
+    * All TriggerPredicateMethod.SILENCE predicate conditions must be false
+    """
+    predicates = [
+        predicate
+        for predicate in get_trigger_predicates()
+        # Gather predicates that apply to all DAGs, or ones specific to this DAG
+        if predicate.dag_id is None or incident.dag_id == predicate.dag_id
+    ]
+
+    failure_messages = [
+        predicate.failure_message
+        for predicate in predicates
+        if (
+            predicate.method == TriggerPredicateMethod.PRECONDITION
+            and not predicate.condition(incident)
+        )
+        or (
+            predicate.method == TriggerPredicateMethod.SILENCE
+            and predicate.condition(incident)
+        )
+    ]
+
+    return len(failure_messages) == 0, failure_messages
---
+++ recidiviz/airflow/tests/monitoring/alerting_trigger_predicates_test.py
@@ -0,0 +1,106 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Tests for trigger predicates"""
+import json
+import os
+import unittest
+from datetime import datetime, timedelta, timezone
+from unittest.mock import patch
+
+import attr
+
+from recidiviz.airflow.dags.monitoring.airflow_alerting_incident import (
+    AirflowAlertingIncident,
+)
+from recidiviz.airflow.dags.monitoring.alerting_trigger_predicates import (
+    should_trigger_airflow_alerting_incident,
+)
+
+
+@patch.dict(os.environ, {"GCP_PROJECT": "test_project"})
+class TestPredicates(unittest.TestCase):
+    """Tests for Airflow alerting trigger predicates"""
+
+    def test_happy_path(self) -> None:
+        incident = AirflowAlertingIncident(
+            dag_id="test_project_calculation_dag",
+            conf=json.dumps({"ingest_instance": "PRIMARY"}),
+            task_id="update_managed_views_all.execute_entrypoint_operator",
+            failed_execution_dates=[datetime.now(tz=timezone.utc)],
+        )
+
+        should_trigger, messages = should_trigger_airflow_alerting_incident(incident)
+        self.assertTrue(should_trigger, messages)
+
+    def test_sftp(self) -> None:
+        incident = AirflowAlertingIncident(
+            dag_id="test_project_sftp_dag",
+            conf="{}",
+            task_id="US_IX.remote_file_download.download_sftp_files",
+            failed_execution_dates=[datetime.now(tz=timezone.utc)],
+        )
+
+        should_trigger, messages = should_trigger_airflow_alerting_incident(incident)
+        self.assertFalse(should_trigger, messages)
+        self.assertIn("must fail at least twice", messages)
+
+        incident = attr.evolve(
+            incident,
+            failed_execution_dates=[
+                datetime.now(tz=timezone.utc),
+                datetime.now(tz=timezone.utc),
+            ],
+        )
+
+        should_trigger, messages = should_trigger_airflow_alerting_incident(incident)
+        self.assertTrue(should_trigger, messages)
+        self.assertEqual(len(messages), 0)
+
+    def test_state_code_branch_end(self) -> None:
+        incident = AirflowAlertingIncident(
+            dag_id="test_project_calculation_dag",
+            conf="{}",
+            task_id="post_normalization_pipelines.state_code_branch_end",
+            failed_execution_dates=[datetime.now(tz=timezone.utc)],
+        )
+
+        should_trigger, messages = should_trigger_airflow_alerting_incident(incident)
+        self.assertFalse(should_trigger, messages)
+        self.assertIn("state_code_branch_end is not an actionable failure", messages)
+
+    def test_secondary_dag(self) -> None:
+        incident = AirflowAlertingIncident(
+            dag_id="test_project_calculation_dag",
+            conf=json.dumps({"ingest_instance": "SECONDARY"}),
+            task_id="update_managed_views_all.execute_entrypoint_operator",
+            failed_execution_dates=[datetime.now(tz=timezone.utc)],
+        )
+
+        should_trigger, messages = should_trigger_airflow_alerting_incident(incident)
+        self.assertFalse(should_trigger, messages)
+        self.assertIn("incident is not for the primary ingest instance", messages)
+
+    def test_stale_incident(self) -> None:
+        incident = AirflowAlertingIncident(
+            dag_id="test_project_calculation_dag",
+            conf=json.dumps({"ingest_instance": "PRIMARY"}),
+            task_id="update_managed_views_all.execute_entrypoint_operator",
+            failed_execution_dates=[datetime.now(tz=timezone.utc) - timedelta(days=14)],
+        )
+
+        should_trigger, messages = should_trigger_airflow_alerting_incident(incident)
+        self.assertFalse(should_trigger, messages)
+        self.assertIn("incident has not occurred recently", messages)
