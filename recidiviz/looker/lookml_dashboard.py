af74ebdd80cce0ba93cc0f712a665e7bc9d2ef55
Ryan <24993797+rzgn@users.noreply.github.com>
2023-07-27 17:30:15 -0700

    Add dashboards to LookML generation library (Recidiviz/recidiviz-data#22676)
    ## Description of the change
    
    > Adds new classes `LookMLDashboard` and `LookMLDashboardParameter` to
    the LookML generation library, supporting everything neded to generate a
    dashboard like [this
    one](https://github.com/Recidiviz/looker/blob/08b2aed94aaa044ad96ac277a9d4236ae347b1db/dashboards/state_raw_data/us_nd/us_nd_raw_data_person_details_template.dashboard.lookml)
    except for the `elements` and `filters`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | **Type: Feature** | **non-breaking change that adds functionality** |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#21939
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Ryan Guan <rzgn@users.noreply.github.com>
    GitOrigin-RevId: 369ba5657ce0aeaa8ee54985202a8933077ec435

---
+++ recidiviz/looker/lookml_dashboard.py
@@ -0,0 +1,72 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines a class representing LookML dashboard files, which provide
+   settings for a data dashboard."""
+from typing import List, Optional
+
+import attr
+
+from recidiviz.looker.lookml_dashboard_parameter import LookMLDashboardParameter
+from recidiviz.looker.lookml_utils import write_lookml_file
+from recidiviz.utils.string import StrictStringFormatter
+
+DASHBOARD_TEMPLATE = """- dashboard: {dashboard_name}
+  {parameters}
+"""
+
+
+@attr.define
+class LookMLDashboard:
+    """
+    Produces LookML dashboard text that satisfies the syntax described in
+    https://cloud.google.com/looker/docs/reference/param-lookml-dashboard
+    """
+
+    dashboard_name: str
+    parameters: List[LookMLDashboardParameter]
+    load_configuration_wait: bool = attr.field(default=False)
+    extended_dashboard: Optional[str] = attr.field(default=None)
+    extension_required: bool = attr.field(default=False)
+
+    def build(self) -> str:
+        all_parameters = [param.build() for param in self.parameters]
+        if self.load_configuration_wait:
+            all_parameters.append("load_configuration: wait")
+        if self.extended_dashboard:
+            all_parameters.append(f"extends: {self.extended_dashboard}")
+        if self.extension_required:
+            all_parameters.append("extension: required")
+        parameters_clause = "\n  ".join(all_parameters)
+
+        return StrictStringFormatter().format(
+            DASHBOARD_TEMPLATE,
+            dashboard_name=self.dashboard_name,
+            parameters=parameters_clause,
+        )
+
+    def write(self, output_directory: str, source_script_path: str) -> None:
+        """
+        Writes LookML dashboard file into the specified output directory with a
+        header indicating the date and script source of the auto-generated dashboard.
+        """
+        file_name = f"{self.dashboard_name}.dashboard.lookml"
+        write_lookml_file(
+            output_directory=output_directory,
+            file_name=file_name,
+            source_script_path=source_script_path,
+            file_body=self.build(),
+        )
