81738b69681d6313a22d5de213fd74bc0662f5a3
brendali121 <brenda@recidiviz.org>
2023-08-01 13:32:02 -0500

    [US_IX] Fix some more state staff things (Recidiviz/recidiviz-data#22815)
    ## Description of the change
    
    Fix the following US_IX state staff things:
    
    - Hard code `supervisor_staff_external_id` for one officer from
    `us_ix_dual_supervisors_officers` whose supervisor info was missing in
    `supervision_officer_attributes_sessions`
    - In `state_staff` view, revise priority ordering for which name record
    to use to pick the one with the highest EmployeeId (previously attempted
    to do this but because the EmployeeId I was sorting was in STRING
    format, it wasn't sorting as I intended)
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: b031dae1deb3e47b8f642327cac7f38affa4f3c0

--- recidiviz/calculator/query/state/views/reference/us_ix_dual_supervisors_officers.py
+++ recidiviz/calculator/query/state/views/reference/us_ix_dual_supervisors_officers.py
@@ -29,31 +29,40 @@ US_IX_DUAL_SUPERVISORS_OFFICERS_QUERY_TEMPLATE = """
   SELECT
     DISTINCT
     attrs.state_code,
-    officer_id as external_id,
+    attrs.officer_id as external_id,
+    TO_HEX(SHA256(attrs.officer_id)) as officer_id_hash,
     staff_id,
     staff.full_name,
     staff.email,
-    supervisor_staff_external_id as supervisor_external_id,
+    CASE WHEN TO_HEX(SHA256(attrs.officer_id)) = '92a7194ae5db4ecb83f724e83d0d50b3c216561849b48f4d60946b3b0a301a3a'
+         THEN missing_supervisor.officer_id
+      ELSE supervisor_staff_external_id
+      END as supervisor_external_id,
     supervision_district,
     supervision_unit,
     specialized_caseload_type
   FROM `{project_id}.sessions.supervision_officer_attribute_sessions_materialized` attrs
   INNER JOIN `{project_id}.{normalized_state_dataset}.state_staff` staff USING(staff_id)
+  LEFT JOIN (
+    select officer_id 
+    from `{project_id}.sessions.supervision_officer_attribute_sessions_materialized` 
+    where TO_HEX(SHA256(officer_id)) = 'a33b4b5dd71ecdc0d1e24f636fdaaff8902a3a0f8b0604bcfadb6ae9adea15de'
+  ) missing_supervisor ON 1=1
   WHERE 
     attrs.state_code = 'US_IX' AND
-    officer_id IN (
-      '3183',
-      '3159',
-      '2786',
-      '3174',
-      '2771',
-      '2617',
-      '2654',
-      '2764',
-      '2791',
-      '2789'
+    TO_HEX(SHA256(attrs.officer_id)) IN (
+    '0a6b81782b5c6d04236250f24ed2b3a27c3afd1a80371ea238ba029cbe5aca0d',
+    '13d61412db18277db74db4337a034d6972da028bdcf41c2286c07f452753fc8f',
+    '144dc53d5dff011b70e273ff282f86c2976c8268811315f548ae013705941ba8',
+    '6bcc05d40b1752992b7142914608fbac9adb28af8ca29b7d14b1e46428be693e',
+    '7929065522441d4053cba7ebffb2d224585a110b13840ff69cf0e89b725af9e7',
+    '92a7194ae5db4ecb83f724e83d0d50b3c216561849b48f4d60946b3b0a301a3a',
+    'dbaf899fbd964344fefdd5d21bb73a9b0b1654799a0a430106e87ec5b6300a77',
+    'df979f8f4738e382b5e7c616a20b341ca41cece8dce7532c2c50490109bf43f0',
+    'e172e24d3024bb7c229c66312fe18a56c169c8da74e48c666e1590141cec58e5',
+    'ef5839d610a3567ed8e2b6df29dce544e3fb9ffbaf45f507b3a69fbc5e81f507'
     )
-    AND (CURRENT_DATE("US/Pacific") BETWEEN start_date AND IFNULL(DATE_SUB(end_date_exclusive, INTERVAL 1 DAY), "9999-12-31") or officer_id = '3183')
+    AND (CURRENT_DATE("US/Pacific") BETWEEN start_date AND IFNULL(DATE_SUB(end_date_exclusive, INTERVAL 1 DAY), "9999-12-31") or TO_HEX(SHA256(attrs.officer_id))= '92a7194ae5db4ecb83f724e83d0d50b3c216561849b48f4d60946b3b0a301a3a')
     AND supervision_district IS NOT NULL
 """
 
