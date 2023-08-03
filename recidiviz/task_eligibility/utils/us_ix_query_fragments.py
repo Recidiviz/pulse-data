16b17ee94abb5d038476abf96fb580ac970827cf
Shalin Brahmbhatt <63821972+shalindb@users.noreply.github.com>
2023-08-03 14:49:09 -0700

    [US_IX] Added TPD criteria (Recidiviz/recidiviz-data#22941)
    ## Description of the change
    
    Added the criteria:
    TPD within 18 months
    TPD within 1 year
    TPD within 7 years
    Uploaded to sandbox under `shalin`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#22730
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: a6667517ba9e341d96c24a453ec145092d7d42cb

---
+++ recidiviz/task_eligibility/utils/us_ix_query_fragments.py
@@ -0,0 +1,72 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Helper SQL queries for Idaho
+"""
+from recidiviz.task_eligibility.utils.critical_date_query_fragments import (
+    critical_date_has_passed_spans_cte,
+)
+from recidiviz.calculator.query.bq_utils import nonnull_start_date_clause
+
+
+def date_within_time_span(
+    meets_criteria_leading_window_days: int = 0, critical_date_column: str = ""
+) -> str:
+    """
+    Generates a BigQuery SQL query that identifies time spans where a critical date
+    falls within a specified leading window of days.
+
+    Args:
+        meets_criteria_leading_window_days (int, optional): The leading window of days
+            used to determine if a critical date falls within. Default is 0.
+        critical_date_column (str, optional): The column representing the critical date
+            to be checked within the time spans. Default is an empty string.
+
+    Returns:
+        str: A formatted BigQuery SQL query that selects time spans and determines if the
+        critical date has passed within the specified leading window.
+
+    Example usage:
+        query = date_within_time_span(meets_criteria_leading_window_days=5, critical_date_column="custom_date")
+        # Execute the generated query using your preferred method
+    """
+    return f"""
+    WITH
+      critical_date_spans AS (
+          SELECT
+            state_code,
+            person_id,
+            {nonnull_start_date_clause('start_date')} as start_datetime,
+            end_date as end_datetime,
+            {critical_date_column} AS critical_date
+          FROM
+            `{{project_id}}.{{analyst_dataset}}.us_ix_parole_dates_spans_preprocessing_materialized`
+          WHERE {critical_date_column} IS NOT NULL AND start_date IS NOT NULL),
+      {critical_date_has_passed_spans_cte(meets_criteria_leading_window_days)}
+    SELECT
+      state_code,
+      person_id,
+      start_date,
+      end_date,
+      critical_date_has_passed AS meets_criteria,
+      TO_JSON(STRUCT(critical_date AS {critical_date_column})) AS reason,
+    FROM
+      critical_date_has_passed_spans
+    """
+
+
+print(date_within_time_span(2555, "tentative_end_date"))
