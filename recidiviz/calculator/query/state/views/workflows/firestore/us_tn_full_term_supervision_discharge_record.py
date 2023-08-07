f05a9552db9863302ccf01e0bad431702777c43d
Damini Sharma <damini@recidiviz.org>
2023-08-07 10:00:44 -0700

    [US_TN] TEPE Form and Sidebar updates for DRUNs, DRUPs and NCAC/NCAF (Recidiviz/recidiviz-data#22994)
    ## Description of the change
    
    This PR makes some changes to what's in the form field and the side bar
    for the TEPE note based on UXR changes. The updates are captured in the
    linked ticket and highlighted
    [here](https://docs.google.com/spreadsheets/d/1enzAosYDOrSvshOhJVwm4NZuPSdcIaxV3Ec2_cyzjOw/edit#gid=1757109567)
    
    The changes are:
    - Changing the join condition for ContactNotes and ContactComments to be
    a bit more complete but also possibly noisier
    - Aggregating all relevant contact types and comments received on any
    given day to be a) more aligned with how POs actually enter comments and
    b) hopefully allow for a bit more consolidation on the side bar so
    comments aren't repeated on the same day
    - Adds two fields `form_information_new_offenses` and
    `form_information_alcohol_history` which are arrays of STRUCTs because
    we want to add all to the form, not just the latest
    - Adds an additional `"ALCOHOL DRUG HISTORY - NEGATIVE SCREENS"` for the
    sidebar, where we want to display all negative screens and their dates,
    ideally in some condensed format, without displaying the comments
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#22788
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 14a811670a9583dab0be693f70a07d43aa693b66

--- recidiviz/calculator/query/state/views/workflows/firestore/us_tn_full_term_supervision_discharge_record.py
+++ recidiviz/calculator/query/state/views/workflows/firestore/us_tn_full_term_supervision_discharge_record.py
@@ -69,24 +69,49 @@ latest_system_session AS ( # get latest system session date to bring in relevant
   QUALIFY ROW_NUMBER() OVER(PARTITION BY person_id ORDER BY system_session_id DESC) = 1
 ),
 relevant_codes AS (
+  /*
+    ContactNoteComment is unique on OffenderID and ContactNoteDateTime while ContactNoteType is unique on OffenderID,
+    ContactNoteDateTime, and ContactType. Which means POs enter one comment but a collection of ContactTypes at any 
+    given time, and there can be multiple per day. 
+    
+    The trade-off between joining on Contact Date vs. ContactDateTime is the following:
+    - For drug tests, we sometimes see a DRUL (drug test sent to lab) entered on Date X and Time Y, with the matching 
+    comment describing what the person tested positive for, awaiting confirmation from the lab. Later on Date X Time Z, 
+    there is a DRUP contact to confirm the positive test but not matching comment. For this reason, joining on Contact
+    Date provides more complete information
+    - A PO might enter completely unrelated contact notes on Date X at Time Y and Time Z - in this case, joining on date
+    might just add some noise since we only care about the comment from Time Y but will get both
+    
+    Below, we join on contact date and aggregate all relevant contact types and comments for a given date which may be
+    noisier than ideal but also hopefully more complete  
+  */ 
   SELECT person_id,
-         CAST(CAST(ContactNoteDateTime AS datetime) AS DATE) AS contact_date,
-         ContactNoteType AS contact_type,
-         Comment AS contact_comment
-  FROM `{{project_id}}.{{us_tn_raw_data_up_to_date_dataset}}.ContactNoteType_latest` contact
-  LEFT JOIN `{{project_id}}.{{us_tn_raw_data_up_to_date_dataset}}.ContactNoteComment_latest`
-    USING (OffenderID, ContactNoteDateTime)
+         contact_date,
+         ARRAY_AGG(DISTINCT contact.ContactNoteType IGNORE NULLS) AS contact_type,
+         ARRAY_AGG(DISTINCT comment.Comment IGNORE NULLS) AS contact_comment
+  FROM
+    (
+        SELECT * EXCEPT(ContactNoteDateTime),
+                CAST(CAST(ContactNoteDateTime AS datetime) AS DATE) AS contact_date,
+        FROM `{{project_id}}.{{us_tn_raw_data_up_to_date_dataset}}.ContactNoteType_latest`
+    ) contact
+  LEFT JOIN (
+        SELECT * EXCEPT(ContactNoteDateTime),
+                CAST(CAST(ContactNoteDateTime AS datetime) AS DATE) AS contact_date,
+        FROM `{{project_id}}.{{us_tn_raw_data_up_to_date_dataset}}.ContactNoteComment_latest`
+    ) comment
+    USING (OffenderID, contact_date)
   INNER JOIN `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` pei
       ON contact.OffenderID = pei.external_id
       AND pei.state_code = 'US_TN'
   LEFT JOIN latest_system_session
     USING(person_id)
-  WHERE CAST(CAST(ContactNoteDateTime AS datetime) AS DATE) >= latest_system_session_start_date
+  WHERE contact_date >= latest_system_session_start_date
      AND (
            ContactNoteType LIKE '%PSE%'
            OR ContactNoteType IN ('VRPT','VWAR','COHC','ARRP')
            OR ContactNoteType LIKE "%ABS%"
-           OR ContactNoteType = 'DRUP'
+           OR ContactNoteType IN ('DRUN','DRUP','DRUM','DRUX')
            OR ContactNoteType LIKE "%FSW%"
            OR ContactNoteType LIKE "%EMP%"
            OR ContactNoteType IN ("SPEC","SPET")
@@ -94,7 +119,9 @@ relevant_codes AS (
            OR ContactNoteType IN ("FAC1","FAC2")
            OR ContactNoteType LIKE 'FEE%'
            OR ContactNoteType = 'TEPE'
+           OR ContactNoteType IN ('NCAC','NCAF')
      )
+  GROUP BY 1,2
 ),
 sex_offense_pse_code AS ( #latest PSE code
   SELECT
@@ -104,9 +131,26 @@ sex_offense_pse_code AS ( #latest PSE code
       contact_type,
       contact_comment
     ) AS latest_pse
-  FROM relevant_codes
+  FROM relevant_codes,
+  UNNEST(contact_type) AS contact_type
   WHERE contact_type LIKE '%PSE%'
   QUALIFY ROW_NUMBER() OVER(PARTITION BY person_id ORDER BY contact_date DESC) = 1
+),
+new_offense_codes AS ( #latest PSE code
+  SELECT
+    person_id,
+    -- Create array because we want to keep all, not just latest
+    ARRAY_AGG(
+        STRUCT(
+          contact_date,
+          contact_type,
+          contact_comment
+        )
+        ) AS new_offenses
+  FROM relevant_codes,
+  UNNEST(contact_type) AS contact_type
+  WHERE contact_type IN ('NCAC','NCAF')
+  GROUP BY 1
 ),
 emp_code AS ( #latest EMP code
   SELECT
@@ -116,7 +160,8 @@ emp_code AS ( #latest EMP code
       contact_type,
       contact_comment
     ) AS latest_emp
-  FROM relevant_codes
+  FROM relevant_codes,
+  UNNEST(contact_type) AS contact_type
   WHERE contact_type LIKE '%EMP%'
   QUALIFY ROW_NUMBER() OVER(PARTITION BY person_id ORDER BY contact_date DESC) = 1
 ),
@@ -128,7 +173,8 @@ spe_code AS ( #latest SPE code
       contact_type,
       contact_comment
     ) AS latest_spe
-  FROM relevant_codes
+  FROM relevant_codes,
+  UNNEST(contact_type) AS contact_type
   WHERE contact_type IN ("SPEC","SPET")
   QUALIFY ROW_NUMBER() OVER(PARTITION BY person_id ORDER BY contact_date DESC) = 1
 ),
@@ -139,7 +185,8 @@ vrr_code AS ( #latest SPE code
       contact_date,
       contact_type
     ) AS latest_vrr
-  FROM relevant_codes
+  FROM relevant_codes,
+  UNNEST(contact_type) AS contact_type
   WHERE contact_type LIKE 'VRR%'
   QUALIFY ROW_NUMBER() OVER(PARTITION BY person_id ORDER BY contact_date DESC) = 1
 ),
@@ -151,7 +198,8 @@ fee_code AS ( #latest SPE code
       contact_type,
       contact_comment
     ) AS latest_fee
-  FROM relevant_codes
+  FROM relevant_codes,
+  UNNEST(contact_type) AS contact_type
   WHERE contact_type LIKE 'FEE%'
   QUALIFY ROW_NUMBER() OVER(PARTITION BY person_id ORDER BY contact_date DESC) = 1
 ),
@@ -159,9 +207,25 @@ tepe_code AS ( # latest TEPE code
     SELECT
     person_id,
     contact_date AS latest_tepe,
-  FROM relevant_codes
+  FROM relevant_codes,
+  UNNEST(contact_type) AS contact_type
   WHERE contact_type = 'TEPE'
   QUALIFY ROW_NUMBER() OVER(PARTITION BY person_id ORDER BY contact_date DESC) = 1
+),
+alc_history_codes AS (
+    SELECT  person_id,
+            -- Create array because we want to keep all, not just latest
+            ARRAY_AGG(
+                STRUCT(
+                  contact_date,
+                  contact_type,
+                  contact_comment
+                )
+            ) AS alc_history
+    FROM relevant_codes,
+    UNNEST(contact_type) AS contact_type
+    WHERE contact_type IN ('DRUP') OR contact_type LIKE "%FSW%"
+    GROUP BY 1
 ),
 sidebar_contact_notes_union AS ( #cte to union all contact notes to be displayed in sidebar
     SELECT person_id,
@@ -169,7 +233,8 @@ sidebar_contact_notes_union AS ( #cte to union all contact notes to be displayed
           contact_date AS event_date,
           contact_comment AS note_body,
           "REVOCATION HEARINGS" AS criteria,
-    FROM relevant_codes
+    FROM relevant_codes,
+    UNNEST(contact_type) AS contact_type
     WHERE contact_type IN ('VRPT','VWAR','COHC','AARP')
           OR contact_type LIKE "%ABS%"
 
@@ -180,8 +245,20 @@ sidebar_contact_notes_union AS ( #cte to union all contact notes to be displayed
             contact_date AS event_date,
             contact_comment AS note_body,
             "ALCOHOL DRUG HISTORY" AS criteria,
-    FROM relevant_codes
-    WHERE contact_type = 'DRUP' OR contact_type LIKE "%FSW%"
+    FROM relevant_codes,
+    UNNEST(contact_type) AS contact_type
+    WHERE contact_type IN ('DRUP') OR contact_type LIKE "%FSW%"
+    
+    UNION ALL
+
+    SELECT  person_id,
+            contact_type AS note_title,
+            contact_date AS event_date,
+            contact_comment AS note_body,
+            "ALCOHOL DRUG HISTORY - NEGATIVE SCREENS" AS criteria,
+    FROM relevant_codes,
+    UNNEST(contact_type) AS contact_type
+    WHERE contact_type IN ('DRUN','DRUM','DRUX')
 
     UNION ALL
 
@@ -191,8 +268,9 @@ sidebar_contact_notes_union AS ( #cte to union all contact notes to be displayed
             contact_comment AS note_body,
             "INTAKE NOTES" AS criteria,
     FROM (
-      SELECT *
-      FROM relevant_codes
+      SELECT person_id, contact_type, contact_date, contact_comment
+      FROM relevant_codes,
+      UNNEST(contact_type) AS contact_type
       WHERE contact_type IN ("FAC1","FAC2")
       -- Keep latest FAC1 and latest FAC2
       QUALIFY ROW_NUMBER() OVER(PARTITION BY person_id, contact_type ORDER BY contact_date DESC) = 1
@@ -208,6 +286,7 @@ sidebar_contact_notes_array AS (
                     NULL
                 )
             IGNORE NULLS
+            ORDER BY criteria, event_date
             )
         ) AS case_notes
     FROM sidebar_contact_notes_union
@@ -256,6 +335,8 @@ tes_cte AS (
          arr.case_notes,
          pse.latest_pse AS form_information_latest_pse,
          sex_offenses.sex_offenses AS form_information_sex_offenses,
+         new_off.new_offenses AS form_information_new_offenses,
+         alc.alc_history AS form_information_alcohol_history,
          emp.latest_emp AS form_information_latest_emp,
          spe.latest_spe AS form_information_latest_spe,
          vrr.latest_vrr AS form_information_latest_vrr,
@@ -269,6 +350,10 @@ tes_cte AS (
   LEFT JOIN sidebar_contact_notes_array arr
     USING(person_id)
   LEFT JOIN sex_offense_pse_code pse
+    USING(person_id)
+  LEFT JOIN new_offense_codes new_off
+    USING(person_id)
+  LEFT JOIN alc_history_codes alc
     USING(person_id)
   LEFT JOIN sex_offenses
     USING(person_id)
