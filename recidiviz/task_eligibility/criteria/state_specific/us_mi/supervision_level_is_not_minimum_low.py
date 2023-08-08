e0231f3a95e9c9ccefbf73cc898883576038197e
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2023-08-08 15:41:44 -0700

    [US_MI][Workflows] Exclude clients on minimum low supervision from classification review (Recidiviz/recidiviz-data#23009)
    ## Description of the change
    
    Clients on minimum low are not eligible for classification review
    
    **eligibility changes**:
    ~140 are no longer eligible for CR
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: a54d37570ad8d627c73600fdc46a6b3868b919ad

---
+++ recidiviz/task_eligibility/criteria/state_specific/us_mi/supervision_level_is_not_minimum_low.py
@@ -0,0 +1,79 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""This criteria view builder defines spans of time that a client is on Minimum Low Supervision
+"""
+from recidiviz.calculator.query.sessions_query_fragments import aggregate_adjacent_spans
+from recidiviz.calculator.query.state.dataset_config import (
+    ANALYST_VIEWS_DATASET,
+    SESSIONS_DATASET,
+)
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "US_MI_SUPERVISION_LEVEL_IS_NOT_MINIMUM_LOW"
+
+_DESCRIPTION = """This criteria view builder defines spans of time that a client is on Minimum Low Supervision
+"""
+
+_QUERY_TEMPLATE = f"""
+#TODO(#22511) refactor to build off of a general criteria view builder
+WITH minimum_low_spans AS (
+    SELECT
+        state_code,
+        person_id,
+        start_date,
+        end_date_exclusive AS end_date,
+    #TODO(#20035) replace with supervision level raw text sessions once views agree
+    FROM `{{project_id}}.{{sessions_dataset}}.compartment_sub_sessions_materialized` sls
+    /* Using regex to match digits in sls.correctional_level_raw_text
+   so imputed values with the form d*##IMPUTED are correctly joined */
+    LEFT JOIN `{{project_id}}.{{analyst_data_dataset}}.us_mi_supervision_level_raw_text_mappings` map
+        ON REGEXP_EXTRACT(sls.correctional_level_raw_text, r'(\\d+)') \
+         = map.supervision_level_raw_text
+    WHERE state_code = "US_MI"
+    AND compartment_level_1 IN ('SUPERVISION')
+    AND map.is_minimum_low
+)
+    SELECT 
+        state_code,
+        person_id,
+        start_date,
+        end_date,
+        FALSE AS meets_criteria,
+    TO_JSON(STRUCT(TRUE AS supervision_level_is_minimum_low)) AS reason,
+    FROM ({aggregate_adjacent_spans(table_name='minimum_low_spans')})
+"""
+
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
+    StateSpecificTaskCriteriaBigQueryViewBuilder(
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+        criteria_spans_query_template=_QUERY_TEMPLATE,
+        state_code=StateCode.US_MI,
+        sessions_dataset=SESSIONS_DATASET,
+        analyst_data_dataset=ANALYST_VIEWS_DATASET,
+        meets_criteria_default=True,
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
