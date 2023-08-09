65f7bf68e5be2dcb4b06803d96e1b116efebe4a6
Shalin Brahmbhatt <63821972+shalindb@users.noreply.github.com>
2023-08-09 16:34:13 -0700

    [US_IX] Writing State-Specific Detainer Criteria (Recidiviz/recidiviz-data#23071)
    ## Description of the change
    
    Added Analyst View for Detainers, 2 criteria for XCRC and CRC US_IX, and
    a query fragment
    
    Uploaded to sandbox under `shalin`
    No Validation Views Failing
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 2d1134e404121e24c928b8581598ec536a5ae271

---
+++ recidiviz/calculator/query/state/views/analyst_data/us_ix/us_ix_detainer_spans.py
@@ -0,0 +1,85 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+""" This view has one record per term and person. It contains detainer related information for residents in
+IX. This will be used to create criteria queries for the CRC and XCRC workflows. Does not include historical
+spans, just a present view of detainers """
+
+from recidiviz.common.constants.states import StateCode
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.ingest.direct.dataset_config import raw_latest_views_dataset_for_region
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.calculator.query.state.dataset_config import (
+    ANALYST_VIEWS_DATASET,
+    NORMALIZED_STATE_DATASET,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+US_IX_DETAINER_SPANS_VIEW_NAME = "us_ix_detainer_spans"
+
+US_IX_DETAINER_SPANS_VIEW_DESCRIPTION = """ This view has one record per term and person. It contains detainer related information for residents in
+IX. This will be used to create criteria queries for the CRC and XCRC workflows. Does not include historical
+spans, just a present view of detainers """
+
+US_IX_DETAINER_SPANS_QUERY_TEMPLATE = """
+    SELECT
+      pei.person_id,
+      pei.state_code,
+      SAFE_CAST(LEFT(IssuedDate, 10) AS DATE) AS start_date,
+      SAFE_CAST(LEFT(NULL, 10) AS DATE) AS end_date,
+      det_type.DetainerTypeDesc,
+      det_type.DetainerTypeId,
+      det_status.DetainerStatusDesc,
+      det.CriminalChargesDesc,
+      det.Comments,
+      det.AgencyContactName,
+      det_status.Abbreviation,
+    FROM
+      `{project_id}.{us_ix_raw_data_up_to_date_dataset}.scl_Detainer_latest` det
+    LEFT JOIN
+      `{project_id}.{normalized_state_dataset}.state_person_external_id` pei
+    ON
+      pei.external_id = OffenderId
+    LEFT JOIN
+      `{project_id}.{us_ix_raw_data_up_to_date_dataset}.scl_DetainerType_latest` det_type
+    ON
+      det.DetainerTypeId = det_type.DetainerTypeId
+    LEFT JOIN
+      `{project_id}.{us_ix_raw_data_up_to_date_dataset}.scl_DetainerStatus_latest` det_status
+    ON
+      det.DetainerStatusId = det_status.DetainerStatusId
+    WHERE
+      pei.state_code = 'US_IX'
+      AND pei.id_type = 'US_IX_DOC'
+      AND DetainerStatusDesc != "Closed" 
+"""
+
+US_IX_DETAINER_SPANS_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=ANALYST_VIEWS_DATASET,
+    view_id=US_IX_DETAINER_SPANS_VIEW_NAME,
+    description=US_IX_DETAINER_SPANS_VIEW_DESCRIPTION,
+    view_query_template=US_IX_DETAINER_SPANS_QUERY_TEMPLATE,
+    normalized_state_dataset=NORMALIZED_STATE_DATASET,
+    should_materialize=True,
+    us_ix_raw_data_up_to_date_dataset=raw_latest_views_dataset_for_region(
+        state_code=StateCode.US_IX, instance=DirectIngestInstance.PRIMARY
+    ),
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        US_IX_DETAINER_SPANS_VIEW_BUILDER.build_and_print()
