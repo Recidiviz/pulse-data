93bd7d5404b0da60e8dfbf8efb399ef3faed7e1e
Damini Sharma <damini@recidiviz.org>
2023-08-10 16:41:26 -0700

    General incarceration candidate population with facility filter (Recidiviz/recidiviz-data#22921)
    ## Description of the change
    
    While doing some headroom calculations for TN facilities, I found it
    would be useful to have a candidate population view that limits to non
    County Jail facilities. I've adjusted the query in
    `candidate_population_builders` to allow for this option and re-used the
    [pathways state specific
    strings](https://github.com/Recidiviz/recidiviz-data/blob/8518d52dd5ca72a6a7f7f84c5e15659b7b4d918f/recidiviz/calculator/query/state/state_specific_query_strings.py#L698)
    
    As seen below, the biggest difference between the new
    `GENERAL_INCARCERATION_POPULATION_FACILITY_FILTER` view and the
    `GENERAL_INCARCERATION_POPULATION` view is for TN, and some difference
    for MI
    
    ```
    WITH sandbox AS (
      SELECT state_code, count(*) as sandbox_c
      FROM `recidiviz-123.dsharm20230801_task_eligibility_candidates_general.general_incarceration_population_facility_filter_materialized`
      WHERE current_date BETWEEN start_date AND coalesce(end_date,'9999-01-01')
      GROUP BY 1
      ORDER BY 1
    ),
    main AS (
        SELECT state_code, count(*)  as main_c
      FROM `recidiviz-123.dsharm20230801_main_task_eligibility_candidates_general.general_incarceration_population_materialized`
      WHERE current_date BETWEEN start_date AND coalesce(end_date,'9999-01-01')
      GROUP BY 1
      ORDER BY 1
    )
    select *, sandbox.sandbox_c - main.main_c AS diff
    from sandbox inner join main using(state_code)
    order by 4 desc
    ```
    
    state_code | sandbox_c | main_c | diff
    -- | -- | -- | --
    | US_ID | 7462 | 7462 | 0 |  
    | US_CO | 16684 | 16684 | 0 |  
    | US_IX | 7252 | 7252 | 0 |  
    | US_MO | 19774 | 19774 | 0 |  
    | US_ME | 1810 | 1810 | 0 |  
    | US_PA | 36505 | 36505 | 0 |  
    | US_ND | 1848 | 1849 | -1 |  
    | US_MI | 32922 | 33000 | -78 |  
    | US_TN | 19105 | 27689 | -8584
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: a03891647e0eeca0b1d52bd13aa388ea602a48dd

---
+++ recidiviz/task_eligibility/candidate_populations/general/general_incarceration_population_facility_filter.py
@@ -0,0 +1,53 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines all spans of time in which a person is in INCARCERATION GENERAL
+as tracked by data in our `sessions` dataset, and uses state-specific filters for facilities.
+The only type of incarceration included is compartment_level_2='GENERAL'.
+"""
+from recidiviz.calculator.query.state import state_specific_query_strings
+from recidiviz.task_eligibility.task_candidate_population_big_query_view_builder import (
+    StateAgnosticTaskCandidatePopulationBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.candidate_population_builders import (
+    state_agnostic_candidate_population_view_builder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_POPULATION_NAME = "GENERAL_INCARCERATION_POPULATION_FACILITY_FILTER"
+
+_DESCRIPTION = """Selects all spans of time in which a person is in INCARCERATION GENERAL
+as tracked by data in our `sessions` dataset, and uses state-specific filters for facilities. Specifically,
+this excludes facilities that should not be included in pathways metrics
+The only type of incarceration included is compartment_level_2='GENERAL'.
+"""
+
+VIEW_BUILDER: StateAgnosticTaskCandidatePopulationBigQueryViewBuilder = (
+    state_agnostic_candidate_population_view_builder(
+        population_name=_POPULATION_NAME,
+        description=_DESCRIPTION,
+        additional_filters=[
+            'compartment_level_2 = "GENERAL" ',
+            f"{state_specific_query_strings.pathways_state_specific_facility_filter()}",
+        ],
+        compartment_level_1=["INCARCERATION"],
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
