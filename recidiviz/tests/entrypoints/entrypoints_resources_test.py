e7f548fff64a0d4caf2da51ba6451b916a99d4dc
Dan Hansen <dan@recidiviz.org>
2023-08-14 10:28:50 -0700

    [Airflow] Add functionality for pod resource sizing (Recidiviz/recidiviz-data#23172)
    ## Description of the change
    
    This adds a new YAML configuration for defining pod resourcing.
    
    https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/
    
    The new group flow can be found in
    [experiment-2](https://0e2e27c34f014b43b93fca80835153d5-dot-us-central1.composer.googleusercontent.com/dags/recidiviz-staging_hourly_monitoring_dag/grid).
    
    <img width="534" alt="Screenshot 2023-08-13 at 9 44 29 PM"
    src="https://github.com/Recidiviz/recidiviz-data/assets/437360/ff98e449-015f-47f5-b583-93d196e6f222">
    
    The resource configuration is selected via the
    `entrypoints_resources.py` entrypoint in our `appengine` image so that
    we can utilize the entrypoints' `argparse` parsers.
    
    ```yaml
    # Configuration for determining pod resource sizing
    # If many configurations match, the configuration that is closer to the end of the list is given priority
    # https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/
    # entrypoint: (str, required)
    # args: (dict[str, any], optional)
    # resources: (dict[str, any], required)
    #   requests:
    #     cpu:
    #     memory:
    #   limits:
    #     cpu:
    #     memory:
    ```
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 0848f3614bd69fc293634d33a5a5db3fcbac7fdf

---
+++ recidiviz/tests/entrypoints/entrypoints_resources_test.py
@@ -0,0 +1,98 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for entrypoint resource allocation"""
+import argparse
+import os
+import unittest
+from typing import Any
+
+import yaml
+
+import recidiviz
+from recidiviz.entrypoints.entrypoint_executor import (
+    ENTRYPOINTS_BY_NAME,
+    parse_arguments,
+)
+from recidiviz.entrypoints.entrypoint_resources import (
+    DEFAULT_RESOURCES,
+    KubernetesEntrypointResourceAllocator,
+)
+from recidiviz.entrypoints.metric_export.metric_view_export import (
+    MetricViewExportEntrypoint,
+)
+
+
+class TestKubernetesResourceAllocator(unittest.TestCase):
+    """Tests for KubernetesResourceAllocator."""
+
+    entrypoint_args_fixture: Any
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        with open(
+            os.path.join(
+                os.path.dirname(recidiviz.__file__),
+                "airflow/tests/fixtures/entrypoints_args.yaml",
+            ),
+            "r",
+            encoding="utf-8",
+        ) as fixture_file:
+            cls.entrypoint_args_fixture = yaml.safe_load(fixture_file)
+
+    def test_known_entrypoints(self) -> None:
+        allocator = KubernetesEntrypointResourceAllocator()
+
+        for argv in self.entrypoint_args_fixture.values():
+            executor_args, entrypoint_argv = parse_arguments(argv)
+            entrypoint = ENTRYPOINTS_BY_NAME[executor_args.entrypoint]
+            self.assertIsNotNone(
+                allocator.get_resources(
+                    executor_args=executor_args,
+                    entrypoint_args=entrypoint.get_parser().parse_args(entrypoint_argv),
+                )
+            )
+
+    def test_resources(self) -> None:
+        allocator = KubernetesEntrypointResourceAllocator()
+        args, _ = parse_arguments(["--entrypoint=ValidationEntrypoint"])
+        self.assertEqual(
+            allocator.get_resources(
+                executor_args=args,
+                entrypoint_args=argparse.Namespace(),
+            ),
+            DEFAULT_RESOURCES,
+        )
+
+        args, entrypoint_args = parse_arguments(
+            [
+                "--entrypoint=MetricViewExportEntrypoint",
+                "--export_job_name=LANTERN",
+                "--state_code=US_PA",
+            ]
+        )
+        self.assertEqual(
+            allocator.get_resources(
+                executor_args=args,
+                entrypoint_args=MetricViewExportEntrypoint.get_parser().parse_args(
+                    entrypoint_args
+                ),
+            ),
+            {
+                "requests": {"cpu": "2000m", "memory": "1.5Gi"},
+                "limits": {"cpu": "2000m", "memory": "2.5Gi"},
+            },
+        )
