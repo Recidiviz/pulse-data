2d22cd2f137ad9034f43c245d6c833bd10e60d2e
Gloria Liu <gloria@recidiviz.org>
2023-08-21 14:51:44 -0400

    [Validation] stable counts validations by date (Recidiviz/recidiviz-data#21543)
    ## Description of the change
    
    this validation alerts if any of the previous 6 months the change in
    entity counts by the specified date column changes by more than 10%. the
    entities and date columns we are checking for are in a dictionary in
    code as `ENTITIES_WITH_EXPECTED_STABLE_COUNTS_OVER_TIME`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Related to Recidiviz/recidiviz-data#21297
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: e3dcd02ba9a6e92822db703a0e016dedd43e4570

---
+++ recidiviz/tests/validation/stable_counts_validations_test.py
@@ -0,0 +1,83 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Unit test for stable counts validations"""
+import unittest
+from datetime import date
+
+from recidiviz.common.constants.states import StateCode
+from recidiviz.persistence.database.schema.state import schema
+from recidiviz.persistence.database.schema_utils import (
+    get_database_entity_by_table_name,
+    get_state_table_classes,
+)
+from recidiviz.validation.views.state.stable_counts.entity_by_column_count_stable_counts import (
+    exemptions_string_builder,
+)
+from recidiviz.validation.views.state.stable_counts.stable_counts import (
+    ENTITIES_WITH_EXPECTED_STABLE_COUNTS_OVER_TIME,
+)
+
+
+class TestStableCountsValidations(unittest.TestCase):
+    """Unit tests to check that all entity names and columns in ENTITIES_WITH_EXPECTED_STABLE_COUNTS_OVER_TIME
+    are valid"""
+
+    def test_valid_expected_entities(self) -> None:
+        found_tables = [key.name for key in get_state_table_classes()]
+        for entity, _ in ENTITIES_WITH_EXPECTED_STABLE_COUNTS_OVER_TIME.items():
+            self.assertIn(entity, found_tables)
+
+    def test_valid_date_columns(self) -> None:
+        for (
+            entity_name,
+            config,
+        ) in ENTITIES_WITH_EXPECTED_STABLE_COUNTS_OVER_TIME.items():
+            entity = get_database_entity_by_table_name(schema, entity_name)
+            for date_col in config.date_columns_to_check:
+                self.assertTrue(hasattr(entity, date_col.date_column_name))
+
+
+class TestStableCountsExemptions(unittest.TestCase):
+    """Unit tests to test methods related to stable counts exemptions."""
+
+    def test_exemptions_string_builder_single_exemption(self) -> None:
+        exemptions = {StateCode.US_MI: [date(2023, 4, 1)]}
+        output = exemptions_string_builder(exemptions)
+        expected_output = 'NOT (region_code = "US_MI" AND month IN ("2023-04-01"))'
+        self.assertEqual(output, expected_output)
+
+    def test_exemptions_string_builder_multiple_exemptions(self) -> None:
+        exemptions = {
+            StateCode.US_MO: [date(2023, 3, 1), date(2023, 4, 1), date(2023, 5, 1)],
+        }
+        output = exemptions_string_builder(exemptions)
+        expected_output = 'NOT (region_code = "US_MO" AND month IN ("2023-03-01", "2023-04-01", "2023-05-01"))'
+
+        self.assertEqual(output, expected_output)
+
+    def test_exemptions_string_builder_multiple_states(self) -> None:
+        exemptions = {
+            StateCode.US_PA: [date(2023, 2, 1), date(2023, 3, 1)],
+            StateCode.US_MO: [date(2023, 3, 1), date(2023, 4, 1), date(2023, 5, 1)],
+        }
+        output = exemptions_string_builder(exemptions)
+        expected_output = (
+            'NOT (region_code = "US_PA" AND month IN ("2023-02-01", "2023-03-01"))'
+            '\n      AND NOT (region_code = "US_MO" AND month IN ("2023-03-01", "2023-04-01", "2023-05-01"))'
+        )
+
+        self.assertEqual(output, expected_output)
