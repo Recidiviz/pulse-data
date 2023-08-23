7c0daf5a463ebd5bbdaa7ec574021f6dc774c6c0
Chinedu Ozodi <chinedu@recidiviz.org>
2023-08-23 12:24:20 -0700

    [Airflow] Created integration tests for `initialize_calculation_dag_group.py` (Recidiviz/recidiviz-data#23112)
    ## Description of the change
    
    Created integration tests for `initialize_calculation_dag_group.py`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#23110
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 625f13784e44688f252cac5221e86b2b759bcc8e

--- recidiviz/airflow/tests/test_utils.py
+++ recidiviz/airflow/tests/test_utils.py
@@ -15,15 +15,21 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """Test utilities for DAG tests"""
+import logging
 import os
+import sys
 import unittest
 from datetime import datetime
-from typing import Any, Optional
+from typing import Any, Dict, Optional, Set
 from unittest.mock import patch
 
+import attr
 from airflow import DAG, settings
 from airflow.models import BaseOperator, DagRun, TaskInstance
+from airflow.utils import timezone
 from airflow.utils.db import initdb, resetdb
+from airflow.utils.state import DagRunState, State, TaskInstanceState
+from airflow.utils.types import DagRunType
 from airflow.www.fab_security.sqla.models import (
     RegisterUser,
     User,
@@ -31,6 +37,7 @@ from airflow.www.fab_security.sqla.models import (
     add_index_on_ab_user_username_postgres,
 )
 from sqlalchemy import event
+from sqlalchemy.orm import Session
 
 from recidiviz import airflow as recidiviz_airflow_module
 from recidiviz.tools.postgres import local_postgres_helpers
@@ -69,6 +76,16 @@ BUGGY_AIRFLOW_SQLALCHEMY_EVENTS = [
 ]
 
 
+@attr.s(auto_attribs=True)
+class DagTestResult:
+    """Stores the results of a dag test run"""
+
+    dag_run_state: DagRunState
+
+    failure_messages: Dict[str, str]
+    """A dictionary of task_id -> failure message if the task failed"""
+
+
 class AirflowIntegrationTest(unittest.TestCase):
     """Sets up the airflow database and provides a SQLAlchemy session builder"""
 
@@ -99,7 +116,6 @@ class AirflowIntegrationTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.engine = settings.engine
-
         for invalid_event in BUGGY_AIRFLOW_SQLALCHEMY_EVENTS:
             table, event_name, fn = invalid_event
             if event.contains(table, event_name, fn):
@@ -110,3 +126,181 @@ class AirflowIntegrationTest(unittest.TestCase):
     def tearDown(self) -> None:
         resetdb(skip_init=True)
         self.engine.dispose()
+
+    def run_dag_test(
+        self,
+        dag: DAG,
+        session: Session,
+        run_conf: Optional[Dict[str, Any]] = None,
+        expected_failure_task_ids: Optional[Set[str]] = None,
+        expected_skipped_task_ids: Optional[Set[str]] = None,
+    ) -> DagTestResult:
+        """
+        A Modified version of 'dag.test'. Will run the full dag to allow
+        looking up statuses in the postgres database. Failure messages are stored in self.failure_messages.
+        """
+
+        if not expected_failure_task_ids:
+            expected_failure_task_ids = set()
+        if not expected_skipped_task_ids:
+            expected_skipped_task_ids = set()
+
+        failure_messages: Dict[str, str] = {}
+
+        def add_logger_if_needed(ti: TaskInstance) -> None:
+            """
+            Add a formatted logger to the taskinstance so all logs are surfaced to the command line instead
+            of into a task file. Since this is a local test run, it is much better for the user to see logs
+            in the command line, rather than needing to search for a log file.
+            Args:
+                ti: The taskinstance that will receive a logger
+
+            """
+            formatter = logging.Formatter(
+                "[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s"
+            )
+            handler = logging.StreamHandler(sys.stdout)
+            handler.level = logging.INFO
+            handler.setFormatter(formatter)
+            # only add log handler once
+            if not any(isinstance(h, logging.StreamHandler) for h in ti.log.handlers):
+                dag.log.debug("Adding Streamhandler to taskinstance %s", ti.task_id)
+                ti.log.addHandler(handler)
+
+        execution_date = timezone.utcnow()
+        dag.log.debug(
+            "Clearing existing task instances for execution date %s", execution_date
+        )
+        dag.clear(
+            start_date=execution_date,
+            end_date=execution_date,
+            dag_run_state=False,  # type: ignore
+            session=session,
+        )
+
+        dag.log.debug("Getting dagrun for dag %s", dag.dag_id)
+        dr: DagRun = self._get_or_create_dagrun(
+            dag=dag,
+            session=session,
+            start_date=execution_date,
+            execution_date=execution_date,
+            run_id=DagRun.generate_run_id(DagRunType.MANUAL, execution_date),
+            conf=run_conf,
+        )
+
+        tasks = dag.task_dict
+        dag.log.debug("starting dagrun")
+        # Instead of starting a scheduler, we run the minimal loop possible to check
+        # for task readiness and dependency management. This is notably faster
+        # than creating a BackfillJob and allows us to surface logs to the user
+        while dr.state == State.RUNNING:
+            schedulable_tis, _ = dr.update_state(session=session)
+            for ti in schedulable_tis:
+                add_logger_if_needed(ti)
+                ti.task = tasks[ti.task_id]
+                failure_message = self._run_task(ti, session)
+                if failure_message:
+                    failure_messages[ti.task_id] = failure_message
+
+        dag.run(ignore_first_depends_on_past=True, verbose=True)
+
+        for task_instance in dag.tasks:
+            task_state = self._get_task_instance_state(task_instance.task_id, session)
+            if (
+                task_state == TaskInstanceState.SKIPPED
+                and task_instance.task_id not in expected_skipped_task_ids
+            ):
+                raise ValueError(
+                    f"Task [{task_instance.task_id}] was skipped unexpectedly"
+                )
+            if (
+                task_state == TaskInstanceState.FAILED
+                and task_instance.task_id not in expected_failure_task_ids
+            ):
+                raise ValueError(f"Task [{task_instance.task_id}] failed unexpectedly")
+            if (
+                task_state == TaskInstanceState.SUCCESS
+                and task_instance.task_id
+                in expected_failure_task_ids | expected_skipped_task_ids
+            ):
+                raise ValueError(
+                    f"Task [{task_instance.task_id}] succeeded unexpectedly"
+                )
+
+        return DagTestResult(self._get_dag_run_state(session), failure_messages)
+
+    def _get_or_create_dagrun(
+        self,
+        dag: DAG,
+        session: Session,
+        conf: Optional[Dict[Any, Any]],
+        start_date: datetime,
+        execution_date: datetime,
+        run_id: str,
+    ) -> DagRun:
+        """
+        Create a DAGRun, but only after clearing the previous instance of said dagrun to prevent collisions.
+        This is a modified version of airflow.models.dag._get_or_create_dagrun
+        """
+        logging.info("dagrun id: %s", dag.dag_id)
+        dr: DagRun = (
+            session.query(DagRun)
+            .filter(
+                DagRun.dag_id == dag.dag_id, DagRun.execution_date == execution_date
+            )
+            .first()
+        )
+        if dr:
+            session.delete(dr)
+            session.commit()
+        dr = dag.create_dagrun(
+            state=DagRunState.RUNNING,
+            execution_date=execution_date,
+            run_id=run_id,
+            start_date=start_date or execution_date,
+            session=session,
+            conf=conf,  # type: ignore
+            data_interval=(execution_date, execution_date),
+        )
+        logging.info("created dagrun %s", str(dr))
+        return dr
+
+    def _run_task(self, ti: TaskInstance, session: Session) -> Optional[str]:
+        """
+        Run a single task instance, and push result to Xcom for downstream tasks. This is a modified version of
+        airflow.models.dag._run_task. Returns a failure message if the task fails.
+        """
+        logging.info("*****************************************************")
+        if ti.map_index > 0:
+            logging.info("Running task %s index %d", ti.task_id, ti.map_index)
+        else:
+            logging.info("Running task %s", ti.task_id)
+        try:
+            ti._run_raw_task(session=session)  # pylint: disable=protected-access
+            session.flush()
+            logging.info("%s ran successfully!", ti.task_id)
+        except Exception as e:
+            logging.info(e)
+            return str(e)
+        logging.info("*****************************************************")
+        return None
+
+    def _get_dag_run_state(self, session: Session) -> DagRunState:
+        """Get the state of the most recent dag run."""
+        rows = session.query(DagRun.state).first()
+        if not rows:
+            raise ValueError("DagRun not found")
+        return DagRunState(rows[0])
+
+    def _get_task_instance_state(
+        self, task_id: str, session: Session
+    ) -> TaskInstanceState:
+        """Get the state of the task instance with task_id from the most recent dag run."""
+        rows = (
+            session.query(TaskInstance.state)
+            .filter(TaskInstance.task_id == task_id)
+            .first()
+        )
+        if not rows:
+            raise ValueError(f"Task [{task_id}] not found")
+        return TaskInstanceState(rows[0])
