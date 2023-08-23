bfc589d653ea1b66d4ab81cabdbd76298c3a8418
brendali121 <brenda@recidiviz.org>
2023-08-23 12:14:03 -0500

    [US_MI] Ingest employment periods for COMS (Recidiviz/recidiviz-data#23098)
    ## Description of the change
    
    On 8-14-2023, all employment data moved to COMS. To address that, this
    PR:
    - Ends all employment periods ingested from their old system (OMNI) that
    were still open as of 8-14-2023 on that date via a mappings change
    - Adds a new `employment_periods_coms` ingest view that ingests all
    employment periods data from COMS, and hydrates the employment period
    start date as either 8-14-2023 or the start date that appears in
    `COMS_Employment` depending on which comes earlier.
    
    Additional notes:
    - Basic validations:
    - 24% of people who have an active employment period as of 8/14/2023 in
    either OMNI or COMS show up in both systems
    - 69% of people who have an active employment period as of 8/14/2023
    only show up in OMNI
    - MI has warned us that employment data data quality in OMNI is
    questionable because it is unclear how thorough officers are at closing
    out employment periods. So this may not actually be that concerning from
    a COMS data quality standpoint.
    - 7% of people who have an active employment period as of 8/14/2023 only
    show up in COMS
    - Currently, employment periods are only used in Workflows as case notes
    displayed for Classification Review. Unclear how useful it actually is
    to display them, especially since we no longer get employer name in
    COMS, so we will need to revise the
    `us_mi_complete_classification_review_form_record` to either display
    employment accounting for COMS (aka without employer names) or remove
    employment info from being displayed altogether. Ticket: Recidiviz/recidiviz-data#23425
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: e071fe4fcc63b4ad65065e6769357e131a334d2c

--- recidiviz/ingest/direct/regions/us_mi/ingest_views/view_employment_period_coms.py
+++ recidiviz/ingest/direct/regions/us_mi/ingest_views/view_employment_period_coms.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""Query containing MDOC employment period information using data from OMNI."""
+"""Query containing MDOC employment period information using data from COMS."""
 
 from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
     DirectIngestViewQueryBuilder,
@@ -23,27 +23,29 @@ from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_QUERY_TEMPLATE = """
-SELECT
-  emp.offender_booking_id,
-  sequence_number,
-  (DATE(employment_date)) as employment_date,
-  (DATE(termination_date)) as termination_date,
-  employer_name,
-  position,
-  employment_status_id,
-  termination_reason_code,
-  occupation
-FROM {ADH_OFFENDER_EMPLOYMENT} emp
-INNER JOIN {ADH_OFFENDER_BOOKING} book on emp.offender_booking_id = book.offender_booking_id
-
+SELECT DISTINCT
+  Employment_Id,
+  LTRIM(emp.offender_number, '0') as Offender_Number,
+  Job_Status,
+  Job_Title,
+  -- Let's start all employment periods on 8-14-2023 since that's when employment periods data got moved to COMS
+  CASE 
+    WHEN DATE(Employment_Start_Date) < DATE(2023,8,14) THEN DATE(2023,8,14)
+    ELSE DATE(Employment_Start_Date)
+    END AS Employment_Start_Date,
+  DATE(Employment_End_Date) as Employment_End_Date,
+  Reason_For_Leaving
+FROM {COMS_Employment} emp
+LEFT JOIN {ADH_OFFENDER} off on LTRIM(emp.offender_number, '0') = off.Offender_Number
+INNER JOIN {ADH_OFFENDER_BOOKING} book on off.offender_id = book.offender_id
 """
 
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
     region="us_mi",
-    ingest_view_name="employment_period",
+    ingest_view_name="employment_period_coms",
     view_query_template=VIEW_QUERY_TEMPLATE,
     materialize_raw_data_table_views=False,
-    order_by_cols="offender_booking_id, sequence_number",
+    order_by_cols="Employment_Id",
 )
 
 if __name__ == "__main__":
