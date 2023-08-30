c4c2b02a05354ceb82d8322328ac4c34fa8b387f
Santy Mendoza <55068232+santymendoza@users.noreply.github.com>
2023-08-30 15:25:29 -0700

    [US_CA] Add TES views for employment milestones (Recidiviz/recidiviz-data#23276)
    ## Description of the change
    
    Added TES views for the employment milestones. Someone will be eligible
    for employment milestones/kudos for their periods of continuous
    employment. The count for these milestones only restarts when a person
    goes into unemployment. The reason blob of each span passes the name of
    all employers the person worked for during the period (e.g. the previous
    6mo for the 6-8mo milestone). Specifically, we build the following:
    - 0-2mo employment kudos
    - 6-8mo employment kudos
    - 12-14mo employment kudos
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#23199
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Hugo S <hugo@recidiviz.org>
    GitOrigin-RevId: 6f6ccef20cf01dc79e72569126963e486c9ed50a

---
+++ recidiviz/task_eligibility/eligibility_spans/us_ca/kudos_employment_12_to_14_months.py
@@ -0,0 +1,57 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Shows the spans of time during which someone in CA is eligible
+for an maintained 12 months of employment kudos. This span only lasts for two months 
+after the event happened and is only available to clients on PAROLE and not IN_CUSTODY.
+"""
+
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.candidate_populations.general import (
+    parole_active_supervision_population,
+)
+from recidiviz.task_eligibility.completion_events.general import (
+    supervision_level_downgrade,
+)
+from recidiviz.task_eligibility.criteria.state_specific.us_ca import (
+    employment_for_12_to_14_months,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Shows the spans of time during which someone in CA is eligible
+for an maintained 12 months of employment kudos. This span only lasts for two months 
+after the event happened and is only available to clients on PAROLE and not IN_CUSTODY.
+"""
+
+VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
+    state_code=StateCode.US_CA,
+    task_name="kudos_employment_12_to_14_months",
+    description=_DESCRIPTION,
+    candidate_population_view_builder=parole_active_supervision_population.VIEW_BUILDER,
+    criteria_spans_view_builders=[
+        employment_for_12_to_14_months.VIEW_BUILDER,
+    ],
+    completion_event_builder=supervision_level_downgrade.VIEW_BUILDER,
+    # TODO(#22285) - Change to the right completion_event_builder
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
---
+++ recidiviz/task_eligibility/utils/us_ca_query_fragments.py
@@ -0,0 +1,113 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Helper SQL queries for California
+"""
+
+from recidiviz.calculator.query.bq_utils import nonnull_end_date_clause
+from recidiviz.calculator.query.sessions_query_fragments import aggregate_adjacent_spans
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.dataset_config import TASK_ELIGIBILITY_CRITERIA_GENERAL
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.critical_date_query_fragments import (
+    critical_date_has_passed_spans_cte,
+)
+
+
+def employment_milestone_builder(
+    criteria_name: str, description: str, from_x_months: int, to_x_months: int
+) -> StateSpecificTaskCriteriaBigQueryViewBuilder:
+    """Returns a state-specific criteria view builder indicating the spans of time when
+    a person has been employed for a certain number of months. It also returns every employer
+    name that was active from the start of the employment period until the desired milestone.
+
+    Args:
+        criteria_name (str): The name of the criteria
+        description (str): The description of the criteria
+        from_x_months (int): The number of months to start counting employment
+        to_x_months (int): The number of months to end counting employment
+    """
+
+    criteria_query = f"""WITH has_employment AS (
+    SELECT 
+        state_code,
+        person_id,
+        start_date,
+        end_date,
+    FROM `{{project_id}}.{{tes_criteria_general_dataset}}.has_employment_materialized`
+    WHERE state_code = 'US_CA'
+),
+
+has_employment_agg_adj_spans AS (
+    # We need adjacent spans so we start the milestone count only when a person goes from 
+    #   not having employment to having employment.
+    {aggregate_adjacent_spans(table_name='has_employment')}
+),
+
+has_employment_unnested AS (
+    SELECT 
+        state_code,
+        person_id,
+        start_date,
+        end_date,
+        status_employer_start_date,
+    FROM `{{project_id}}.{{tes_criteria_general_dataset}}.has_employment_materialized`,
+    UNNEST(JSON_VALUE_ARRAY(reason.status_employer_start_date)) AS status_employer_start_date
+    WHERE state_code = 'US_CA'
+),
+
+critical_date_spans AS (
+    SELECT 
+        adj.state_code,
+        adj.person_id,
+        DATE_ADD(adj.start_date, INTERVAL {from_x_months} MONTH) AS start_datetime,
+        adj.end_date AS end_datetime,
+        DATE_ADD(adj.start_date, INTERVAL {to_x_months} MONTH) AS critical_date,
+        ARRAY_AGG(DISTINCT un.status_employer_start_date) AS status_employer_start_date,
+    FROM has_employment_agg_adj_spans adj
+    LEFT JOIN has_employment_unnested un
+    ON adj.state_code = un.state_code
+        AND adj.person_id = un.person_id
+        AND adj.start_date < {nonnull_end_date_clause('un.end_date')}
+        # Instead of using adj.end_date, we use the critical date as the end date.
+        #   This is because we want to grab all employer names that were active
+        #   from the start date to the critical date.
+        AND un.start_date < DATE_ADD(adj.start_date, INTERVAL {to_x_months} MONTH)
+    GROUP BY 1,2,3,4,5
+
+),
+{critical_date_has_passed_spans_cte(attributes=['status_employer_start_date'])}
+
+SELECT 
+    cd.state_code,
+    cd.person_id,
+    cd.start_date,
+    cd.end_date,
+    NOT cd.critical_date_has_passed AS meets_criteria,
+    TO_JSON(STRUCT(status_employer_start_date AS status_employer_start_date)) AS reason
+FROM critical_date_has_passed_spans cd
+WHERE cd.start_date < {nonnull_end_date_clause('cd.end_date')}"""
+
+    return StateSpecificTaskCriteriaBigQueryViewBuilder(
+        criteria_name=criteria_name,
+        description=description,
+        state_code=StateCode.US_CA,
+        criteria_spans_query_template=criteria_query,
+        tes_criteria_general_dataset=TASK_ELIGIBILITY_CRITERIA_GENERAL,
+    )
