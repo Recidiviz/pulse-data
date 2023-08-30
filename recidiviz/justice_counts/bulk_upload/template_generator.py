5a05cd37c86794df9fc8252a3e17b630e60a7513
Nichelle <nichelle.a.hall@gmail.com>
2023-08-30 12:05:45 -0500

    [Justice Counts] Create Custom Sheets for Super Agencies  (Recidiviz/recidiviz-data#23556)
    ## Description of the change
    
    Updates template generator to create templates with an `agency` column
    for super agencies. This PR also involves a small refactor of the
    template generator code where the logic is separated out into smaller
    helper methods.
    
    https://github.com/Recidiviz/recidiviz-data/assets/19961693/76eadb8f-d118-4d75-9451-081e349dd5d0
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#617
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: c99e72ff6ecd1b476df0052f8e941bbe713058d1

--- recidiviz/justice_counts/bulk_upload/template_generator.py
+++ recidiviz/justice_counts/bulk_upload/template_generator.py
@@ -9,7 +9,7 @@
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warragit addnty of
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
@@ -21,16 +21,22 @@
 
 
 import datetime
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional
 
 import pandas as pd
 from sqlalchemy.orm import Session
 
+from recidiviz.justice_counts.agency import AgencyInterface
 from recidiviz.justice_counts.datapoint import DatapointInterface
+from recidiviz.justice_counts.metricfile import MetricFile
 from recidiviz.justice_counts.metricfiles.metricfile_registry import (
     SYSTEM_TO_METRICFILES,
 )
+from recidiviz.justice_counts.metrics.metric_interface import MetricInterface
 from recidiviz.persistence.database.schema.justice_counts import schema
+
+THIS_YEAR = datetime.date.today().year
+LAST_YEAR = THIS_YEAR - 1
 
 
 def generate_bulk_upload_template(
@@ -52,10 +58,14 @@ def generate_bulk_upload_template(
     metricfiles = SYSTEM_TO_METRICFILES[system]
     filename_to_rows = {}
 
-    this_year = datetime.date.today().year
-    last_year = this_year - 1
+    child_agencies = (
+        AgencyInterface.get_child_agencies_for_agency(session=session, agency=agency)
+        if agency.is_superagency is True
+        else []
+    )
 
     for metricfile in metricfiles:  # pylint: disable=too-many-nested-blocks
+        rows: List[Dict[str, str]] = []
         metric_interface = metric_key_to_interface.get(metricfile.definition.key)
         enabled_status = (
             metric_interface.is_metric_enabled if metric_interface is not None else True
@@ -64,87 +74,30 @@ def generate_bulk_upload_template(
         if enabled_status is False:
             continue
 
-        rows = []
         if (
             metricfile.definition.reporting_frequency
             == schema.ReportingFrequency.ANNUAL
         ):
-            if metricfile.definition.system == schema.System.SUPERVISION:
-                for s in [s.name for s in schema.System.supervision_subsystems()] + [
-                    "ALL"
-                ]:
-                    for year in [last_year, this_year]:
-                        row = {"year": str(year), "system": s, "value": ""}
-                        rows.append(row)
-            else:
-                for year in [last_year, this_year]:
-                    row = {"year": str(year), "value": ""}
-                    rows.append(row)
-        else:
-            if metricfile.definition.system == schema.System.SUPERVISION:
-                for s in [s.name for s in schema.System.supervision_subsystems()] + [
-                    "ALL"
-                ]:
-                    for year in [last_year, this_year]:
-                        for month in range(1, 13):
-                            row = {
-                                "year": str(year),
-                                "month": str(month),
-                                "system": s,
-                                "value": "",
-                            }
-                            rows.append(row)
-            else:
-                for year in [last_year, this_year]:
-                    for month in range(1, 13):
-                        row = {"year": str(year), "month": str(month), "value": ""}
-                        rows.append(row)
+            rows = _add_rows_for_annual_metric(rows=rows, metricfile=metricfile)
 
-        new_rows = []
-        if metricfile.disaggregation is not None:
-            dimension_id_to_dimension_metric_interface = (
-                {
-                    aggregated_dim.dimension_identifier(): aggregated_dim
-                    for aggregated_dim in metric_interface.aggregated_dimensions
-                }
-                if metric_interface is not None
-                else {}
-            )
-            dimension_metric_interface = dimension_id_to_dimension_metric_interface.get(
-                metricfile.disaggregation.dimension_identifier()
-            )
-            for row in rows:
-                row.pop("value")  # move value column last
-                for (
-                    dimension
-                ) in metricfile.disaggregation:  # type: ignore[attr-defined]
-                    if (
-                        dimension_metric_interface is not None
-                        and dimension_metric_interface.dimension_to_enabled_status
-                        is not None
-                    ):
-                        if (
-                            dimension_metric_interface.dimension_to_enabled_status.get(
-                                dimension
-                            )
-                            is False
-                        ):
-                            # If a dimension is turned off, don't include it in the spreadsheet.
-                            continue
-                    new_row: Dict[str, Any] = {
-                        **row,
-                        metricfile.disaggregation_column_name: dimension.value,  # type: ignore
-                        "value": "",
-                    }
-                    new_rows.append(new_row)
         else:
-            new_rows = rows
+            rows = _add_rows_for_monthly_metric(rows=rows, metricfile=metricfile)
+
+        if metricfile.disaggregation is not None:
+            rows = _add_rows_for_disaggregated_metric(
+                rows=rows,
+                metricfile=metricfile,
+                metric_interface=metric_interface,
+            )
+
+        if agency.is_superagency is True:
+            rows = _add_rows_for_super_agency(rows=rows, child_agencies=child_agencies)
 
         # If no new rows were added, don't add the sheet. This will happen in
         # the case where a metric is all dimensions of an dissaggregation
         # are disabled.
-        if len(new_rows) > 0:
-            filename_to_rows[metricfile.canonical_filename] = new_rows
+        if len(rows) > 0:
+            filename_to_rows[metricfile.canonical_filename] = rows
 
     with pd.ExcelWriter(  # pylint: disable=abstract-class-instantiated
         file_path
@@ -159,3 +112,98 @@ def generate_bulk_upload_template(
                 # default column length is 8
                 max_len = max(series.astype(str).map(len).max(), 8)
                 worksheet.set_column(idx, idx, max_len)
+
+
+def _add_rows_for_annual_metric(
+    rows: List[Dict[str, str]], metricfile: MetricFile
+) -> List[Dict[str, str]]:
+    if metricfile.definition.system == schema.System.SUPERVISION:
+        for s in [s.name for s in schema.System.supervision_subsystems()] + ["ALL"]:
+            for year in [LAST_YEAR, THIS_YEAR]:
+                row = {"year": str(year), "system": s, "value": ""}
+                rows.append(row)
+    else:
+        for year in [LAST_YEAR, THIS_YEAR]:
+            row = {"year": str(year), "value": ""}
+            rows.append(row)
+    return rows
+
+
+def _add_rows_for_monthly_metric(
+    rows: List[Dict[str, str]], metricfile: MetricFile
+) -> List[Dict[str, str]]:
+    if metricfile.definition.system == schema.System.SUPERVISION:
+        for s in [s.name for s in schema.System.supervision_subsystems()] + ["ALL"]:
+            for year in [LAST_YEAR, THIS_YEAR]:
+                for month in range(1, 13):
+                    row = {
+                        "year": str(year),
+                        "month": str(month),
+                        "system": s,
+                        "value": "",
+                    }
+                    rows.append(row)
+    else:
+        for year in [LAST_YEAR, THIS_YEAR]:
+            for month in range(1, 13):
+                row = {"year": str(year), "month": str(month), "value": ""}
+                rows.append(row)
+    return rows
+
+
+def _add_rows_for_disaggregated_metric(
+    rows: List[Dict[str, str]],
+    metricfile: MetricFile,
+    metric_interface: Optional[MetricInterface] = None,
+) -> List[Dict[str, str]]:
+    """Adds rows for a sheet representing a metric's disaggregation"""
+    new_rows = []
+    dimension_id_to_dimension_metric_interface = (
+        {
+            aggregated_dim.dimension_identifier(): aggregated_dim
+            for aggregated_dim in metric_interface.aggregated_dimensions
+        }
+        if metric_interface is not None
+        else {}
+    )
+    dimension_metric_interface = dimension_id_to_dimension_metric_interface.get(
+        metricfile.disaggregation.dimension_identifier()  # type: ignore[union-attr]
+    )
+    for row in rows:
+        row.pop("value")  # move value column last
+        for dimension in metricfile.disaggregation:  # type: ignore[union-attr]
+            if (
+                dimension_metric_interface is not None
+                and dimension_metric_interface.dimension_to_enabled_status is not None
+            ):
+                if (
+                    dimension_metric_interface.dimension_to_enabled_status.get(
+                        dimension
+                    )
+                    is False
+                ):
+                    # If a dimension is turned off, don't include it in the spreadsheet.
+                    continue
+            new_row: Dict[str, Any] = {
+                **row,
+                metricfile.disaggregation_column_name: dimension.value,  # type: ignore
+                "value": "",
+            }
+            new_rows.append(new_row)
+    return new_rows
+
+
+def _add_rows_for_super_agency(
+    rows: List[Dict[str, str]], child_agencies: List[schema.Agency]
+) -> List[Dict[str, str]]:
+    new_rows = []
+    for row in rows:
+        row.pop("value")  # move value column last
+        for agency in child_agencies:
+            new_row: Dict[str, Any] = {
+                **row,
+                "agency": agency.name,
+                "value": "",
+            }
+            new_rows.append(new_row)
+    return new_rows
