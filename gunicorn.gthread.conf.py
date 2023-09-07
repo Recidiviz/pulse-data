8a3fec563beecf5f01298428fc5377e6f31f0ceb
Dan Hansen <dan@recidiviz.org>
2023-09-07 09:59:06 -0700

    [App Engine] Use `gthread` worker instead of `gevent` (Recidiviz/recidiviz-data#23747)
    ## Description of the change
    
    This adds a new `gunicorn.gthread.conf.py` file which uses a threaded
    worker rather than gevent. The old configuration is maintained until we
    know that we want to permanently switch. Recidiviz/recidiviz-data#23745 contains the list of
    metrics we'll be monitoring to see how the threaded workers perform.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#23646 Recidiviz/recidiviz-data#23745
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 8ca6ddac891fc3fce3c08cef8f6145e84d16d4f2

---
+++ gunicorn.gthread.conf.py
@@ -0,0 +1,34 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Configures gunicorn"""
+import multiprocessing
+
+# Note: if we adjust the number of gunicorn workers per cpu upwards,
+# we may have to adjust the number of max connections in our postgres instances.
+# See the dicussion in #5497 for more context, and see the docs:
+# https://cloud.google.com/sql/docs/quotas#postgresql for more.
+
+# To avoid consuming too much instance memory, limit the number of workers.
+# http://docs.gunicorn.org/en/stable/design.html#how-many-workers
+workers = (2 * multiprocessing.cpu_count()) + 1
+# Use a threaded worker
+worker_class = "gthread"
+timeout = 3600  # 60 min timeout
+loglevel = "debug"
+accesslog = "gunicorn-access.log"
+errorlog = "gunicorn-error.log"
+keepalive = 650
