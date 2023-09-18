39f00b27336fc4b31ca1fb134f9b8751145b4814
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2023-09-18 11:59:15 -0500

    [US_IX] Correct leading_window_days (Recidiviz/recidiviz-data#24021)
    ## Description of the change
    
    > Correct leading_window_days
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 780468258e89c33846777dcf87b093185c737344

--- recidiviz/task_eligibility/criteria/general/incarceration_within_7_years_of_full_term_completion_date.py
+++ recidiviz/task_eligibility/criteria/general/incarceration_within_7_years_of_full_term_completion_date.py
@@ -37,7 +37,7 @@ someone is incarcerated within 7 years of their full term completion date.
 
 _QUERY_TEMPLATE = f"""
 {is_past_full_term_completion_date(compartment_level_1_filter='INCARCERATION',
-                                   meets_criteria_leading_window_days=1825,
+                                   meets_criteria_leading_window_days=2555,
                                    critical_date_name_in_reason='full_term_completion_date')}
 """
 
