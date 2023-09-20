6100aac177e780a80abeccfede9758961f41f124
Kris Gerig <kris@recidiviz.org>
2023-09-20 11:24:19 -0500

    [US_CA] Add employer name to employment periods (Recidiviz/recidiviz-data#23954)
    ## Description of the change
    
    This PR adds employer name to employment periods in CA for use in
    downstream products (specifically milestones).
    
    During review of the employer name field, I discovered that they
    frequently list the employer name as `UNEMPLOYED`. Previously we
    hard-coded all periods as `EMPLOYED`, but now we have to change our
    logic to assign either `UNEMPLOYED` or `EMPLOYED` to employment status
    based on the value of the employer name field, so this will require a
    rerun.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: adf8c67c29e0803b7792188be567b5df70a94106

---
+++ recidiviz/ingest/direct/regions/us_ca/us_ca_custom_parsers.py
@@ -0,0 +1,47 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Custom parsers functions for US_CA. Can be referenced in an ingest view manifest
+like this:
+
+my_flat_field:
+    $custom:
+        $function: us_ca_custom_parsers.<function name>
+        $args:
+            arg_1: <expression>
+            arg_2: <expression>
+"""
+
+
+from recidiviz.common.constants.state.state_employment_period import (
+    StateEmploymentPeriodEmploymentStatus,
+)
+from recidiviz.ingest.direct.regions.us_ca.us_ca_custom_enum_parsers import (
+    parse_employment_status,
+)
+
+
+def is_employed(employer_name: str) -> bool:
+    parsed_employment_status = parse_employment_status(employer_name)
+
+    if parsed_employment_status in (
+        StateEmploymentPeriodEmploymentStatus.EMPLOYED,
+        StateEmploymentPeriodEmploymentStatus.EMPLOYED_FULL_TIME,
+        StateEmploymentPeriodEmploymentStatus.EMPLOYED_PART_TIME,
+    ):
+        return True
+
+    return False
