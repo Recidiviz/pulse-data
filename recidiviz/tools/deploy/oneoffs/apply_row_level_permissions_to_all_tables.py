6315f636ef1ffc1ce69f519424ea2ba7581f12eb
Colin Adams <colin@recidiviz.org>
2023-09-22 10:58:48 -0700

    [BQ] Parallelize row level permission scripts (Recidiviz/recidiviz-data#24177)
    ## Description of the change
    
    This uses a threadpool to set and drop row level permissions against the
    tables in parallel.
    
    I refactored the expiration script I made to pull the parallelism piece
    out and reuse that, so now each of the scripts just define the
    operations to run and the datasets they care about.
    
    I ran this against 5 sandbox datasets and it went from ~2 mins to apply
    the permissions to ~10 seconds. There's some constant time up front to
    query all of the datasets, it only took 3 seconds to actually drop the
    permissions from the tables once we knew which datasets existed.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#24165
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: fb55f004f101c899fd63e3922d3c597d8938c3fe

--- recidiviz/tools/deploy/oneoffs/apply_row_level_permissions_to_all_tables.py
+++ recidiviz/tools/deploy/oneoffs/apply_row_level_permissions_to_all_tables.py
@@ -1,25 +1,32 @@
 """Applies relevant row-level permissions to all tables in the given project. 
 Will overwrite all existing row-level permissions.
 Usage:
-    python -m recidiviz.tools/deploy.oneoffs.apply_row_level_permissions_to_all_tables --project_id recidiviz-staging
+    python -m recidiviz.tools.deploy.oneoffs.apply_row_level_permissions_to_all_tables --project_id recidiviz-staging
 """
 import argparse
 import logging
+from typing import Optional
 
-from recidiviz.big_query.big_query_client import BigQueryClientImpl
+from recidiviz.big_query.big_query_client import BigQueryClient, BigQueryClientImpl
+from recidiviz.tools.utils.bigquery_helpers import run_operation_for_tables
 from recidiviz.utils.environment import GCP_PROJECTS
 from recidiviz.utils.metadata import local_project_id_override
 
 
-def apply_row_level_permissions_to_all_tables() -> None:
-    client = BigQueryClientImpl()
-    datasets = list(client.list_datasets())
-    for dataset in datasets:
-        dataset_ref = client.dataset_ref_for_id(dataset.dataset_id)
-        tables = client.list_tables(dataset.dataset_id)
-        for t in tables:
-            table = client.get_table(dataset_ref=dataset_ref, table_id=t.table_id)
-            client.apply_row_level_permissions(table)
+def apply_row_level_permissions_to_all_tables(dataset_prefix: Optional[str]) -> None:
+    def _apply_permissions_for_table(
+        client: BigQueryClient, dataset_id: str, table_id: str
+    ) -> None:
+        dataset_ref = client.dataset_ref_for_id(dataset_id)
+        table = client.get_table(dataset_ref=dataset_ref, table_id=table_id)
+        client.apply_row_level_permissions(table)
+
+    run_operation_for_tables(
+        client=BigQueryClientImpl(),
+        prompt="Apply row level permissions",
+        operation=_apply_permissions_for_table,
+        dataset_prefix=dataset_prefix,
+    )
 
 
 if __name__ == "__main__":
@@ -37,7 +44,14 @@ if __name__ == "__main__":
         required=True,
     )
 
+    parser.add_argument(
+        "--dataset-prefix",
+        help="Optionally, only apply permissions to datasets matching this prefix",
+        type=str,
+        required=False,
+    )
+
     args = parser.parse_args()
 
     with local_project_id_override(args.project_id):
-        apply_row_level_permissions_to_all_tables()
+        apply_row_level_permissions_to_all_tables(args.dataset_prefix)
--- recidiviz/tools/deploy/oneoffs/rollback_row_level_permissions_from_all_tables.py
+++ recidiviz/tools/deploy/oneoffs/rollback_row_level_permissions_from_all_tables.py
@@ -3,26 +3,33 @@ applying these row-level permissions. Should we find that row-level permissions 
 causing an issue, this script is intended to rollback all row-level permissions. 
 This script will drop row-level permissions on ALL tables in the given project.
 Usage:
-    python -m recidiviz.tools-deploy.oneoffs.rollback_row_level_permissions_from_all_tables --project_id recidiviz-staging
+    python -m recidiviz.tools.deploy.oneoffs.rollback_row_level_permissions_from_all_tables --project_id recidiviz-staging
 """
 
 import argparse
 import logging
+from typing import Optional
 
-from recidiviz.big_query.big_query_client import BigQueryClientImpl
+from recidiviz.big_query.big_query_client import BigQueryClient, BigQueryClientImpl
+from recidiviz.tools.utils.bigquery_helpers import run_operation_for_tables
 from recidiviz.utils.environment import GCP_PROJECTS
 from recidiviz.utils.metadata import local_project_id_override
 
 
-def rollback_row_level_permissions_on_all_tables() -> None:
-    client = BigQueryClientImpl()
-    datasets = list(client.list_datasets())
-    for dataset in datasets:
-        dataset_ref = client.dataset_ref_for_id(dataset.dataset_id)
-        tables = client.list_tables(dataset.dataset_id)
-        for t in tables:
-            table = client.get_table(dataset_ref=dataset_ref, table_id=t.table_id)
-            client.drop_row_level_permissions(table)
+def rollback_row_level_permissions_on_all_tables(dataset_prefix: Optional[str]) -> None:
+    def _drop_permissions_for_table(
+        client: BigQueryClient, dataset_id: str, table_id: str
+    ) -> None:
+        dataset_ref = client.dataset_ref_for_id(dataset_id)
+        table = client.get_table(dataset_ref=dataset_ref, table_id=table_id)
+        client.drop_row_level_permissions(table)
+
+    run_operation_for_tables(
+        client=BigQueryClientImpl(),
+        prompt="Drop row level permissions",
+        operation=_drop_permissions_for_table,
+        dataset_prefix=dataset_prefix,
+    )
 
 
 if __name__ == "__main__":
@@ -40,7 +47,14 @@ if __name__ == "__main__":
         required=True,
     )
 
+    parser.add_argument(
+        "--dataset-prefix",
+        help="Optionally, only drop permissions from datasets matching this prefix",
+        type=str,
+        required=False,
+    )
+
     args = parser.parse_args()
 
     with local_project_id_override(args.project_id):
-        rollback_row_level_permissions_on_all_tables()
+        rollback_row_level_permissions_on_all_tables(args.dataset_prefix)
