85b5535e17761b50afce89bb40d915eccd4de1ed
Dan Hansen <dan@recidiviz.org>
2023-09-22 11:23:44 -0700

    [Airflow] Output dataflow pipeline errors in task logs (Recidiviz/recidiviz-data#24073)
    ## Description of the change
    
    > Description here
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#23505
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: d4e1f36f7323debcbbcbfdd5e45eea3a375a1c95

---
+++ recidiviz/airflow/tests/operators/recidiviz_dataflow_operator_test.py
@@ -0,0 +1,101 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for RecidivizDataflowFlexTemplateOperator."""
+import unittest
+from datetime import datetime
+from unittest.mock import create_autospec, patch
+
+from airflow import DAG
+from airflow.providers.google.cloud.hooks.dataflow import (
+    DataflowHook,
+    DataflowJobStatus,
+)
+from google.cloud.logging import Client as LoggingClient
+
+from recidiviz.airflow.dags.operators.recidiviz_dataflow_operator import (
+    RecidivizDataflowFlexTemplateOperator,
+)
+from recidiviz.airflow.tests.test_utils import execute_task
+
+
+class TestRecidivizDataflowFlexTemplateOperator(unittest.TestCase):
+    """Tests for RecidivizDataflowFlexTemplateOperator."""
+
+    def setUp(self) -> None:
+        self.mock_hook = create_autospec(DataflowHook)
+        self.mock_hook_patcher = patch(
+            "recidiviz.airflow.dags.operators.recidiviz_dataflow_operator.DataflowHook",
+        )
+        self.mock_hook_patcher.start().return_value = self.mock_hook
+
+        self.mock_logs_client = create_autospec(LoggingClient)
+        self.mock_logs_client_patcher = patch(
+            "recidiviz.airflow.dags.operators.recidiviz_dataflow_operator.LoggingClient",
+        )
+        self.mock_logs_client_patcher.start().return_value = self.mock_logs_client
+
+        self.dag = DAG(dag_id="test_dag", start_date=datetime.now())
+        self.dataflow_task = RecidivizDataflowFlexTemplateOperator(
+            task_id="test_task",
+            project_id="test-project",
+            body={},
+            location="us-central1",
+        )
+
+    def tearDown(self) -> None:
+        self.mock_hook_patcher.stop()
+        self.mock_logs_client_patcher.stop()
+
+    def test_execute_basic(self) -> None:
+        # Arrange
+        self.mock_hook.is_job_dataflow_running.return_value = False
+
+        # Act
+        _ = execute_task(self.dag, self.dataflow_task)
+
+        self.mock_hook.start_flex_template.assert_called()
+
+    def test_execute_on_retry(self) -> None:
+        # Arrange
+        self.mock_hook.is_job_dataflow_running.return_value = True
+
+        # Act
+        _ = execute_task(self.dag, self.dataflow_task)
+
+        self.mock_hook.wait_for_done.assert_called()
+
+    def test_execute_job_failure(self) -> None:
+        self.mock_hook.is_job_dataflow_running.return_value = False
+        self.mock_hook.start_flex_template.side_effect = Exception("Job has failed!")
+        self.mock_hook.get_job.return_value = {
+            "currentState": DataflowJobStatus.JOB_STATE_FAILED,
+            "id": "2023-09-18_07_09_47-16912541725945987225",
+            "creationTime": "2023-09-18T14:09:47.864426Z",
+        }
+
+        with self.assertRaises(Exception):
+            _ = execute_task(self.dag, self.dataflow_task)
+
+        self.mock_logs_client.list_entries.assert_called_with(
+            resource_names=["projects/test-project"],
+            filter_="\n"
+            '(log_id("dataflow.googleapis.com/job-message") OR log_id("dataflow.googleapis.com/launcher"))\n'
+            'resource.type="dataflow_step"\n'
+            'resource.labels.job_id="2023-09-18_07_09_47-16912541725945987225"\n'
+            'timestamp >= "2023-09-18T14:09:47.864426Z"\n'
+            "severity >= ERROR\n",
+        )
