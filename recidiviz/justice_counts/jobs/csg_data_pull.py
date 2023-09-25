b9b06ab06bf15f012589c6341367966f93488693
Nichelle <nichelle.a.hall@gmail.com>
2023-09-25 15:48:15 -0500

    [Justice Counts] Automate data pull for CSG (Recidiviz/recidiviz-data#24039)
    ## Description of the change
    
    Job for CSG agency data pull. This will be run weekly as a Cloud Run
    Job. I tested manually in recidiviz-staging and it was successful!
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#23883
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6a71d11de14ae5aa9fb795e1b2c110a27664b7aa

---
+++ recidiviz/justice_counts/jobs/csg_data_pull.py
@@ -0,0 +1,300 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Job that writes to the Justice Counts Data Pull google spreadsheet with data about
+all agencies with Publisher accounts. Includes fields like "num_record_with_data",
+"num_metrics_configured", etc.
+
+python -m recidiviz.justice_counts.jobs.csg_data_pull.py \
+  --project-id=PROJECT_ID \
+"""
+import datetime
+import logging
+from collections import defaultdict
+from itertools import groupby
+from typing import Any, Dict, List
+
+import pandas as pd
+from googleapiclient.discovery import build
+from oauth2client.client import GoogleCredentials
+from sqlalchemy.engine import Engine
+
+from recidiviz.auth.auth0_client import Auth0Client
+from recidiviz.persistence.database.constants import JUSTICE_COUNTS_DB_SECRET_PREFIX
+from recidiviz.persistence.database.schema.justice_counts import schema
+from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.persistence.database.session import Session
+from recidiviz.persistence.database.sqlalchemy_database_key import SQLAlchemyDatabaseKey
+from recidiviz.persistence.database.sqlalchemy_engine_manager import (
+    SQLAlchemyEngineManager,
+)
+
+# Spreadsheet Name: Justice Counts Data Pull Spreadsheet
+# https://docs.google.com/spreadsheets/d/1Vcz110SWJoTE345w3buPd8oYnwqu-Q_mIJ4C-9z_CC0/edit#gid=870547342
+SPREADSHEET_ID = "1Vcz110SWJoTE345w3buPd8oYnwqu-Q_mIJ4C-9z_CC0"
+
+CREATED_AT = "created_at"
+LAST_LOGIN = "last_login"
+LAST_UPDATE = "last_update"
+NUM_RECORDS_WITH_DATA = "num_records_with_data"
+NUM_METRICS_WITH_DATA = "num_metrics_with_data"
+NUM_METRICS_CONFIGURED = "num_metrics_configured"
+NUM_METRICS_AVAILABLE = "num_metrics_available"
+NUM_METRICS_UNAVAILABLE = "num_metrics_unavailable"
+IS_ALPHA_PARTNER = "is_alpha_partner"
+IS_SUPERAGENCY = "is_superagency"
+
+# To add:
+# NUM_METRICS_UNCONFIGURED = "num_metrics_unconfigured"
+# NUM_METRICS_DEFINED = "num_metrics_defined"
+
+# These are the IDs of our alpha partner agencies in production
+ALPHA_PARTNER_IDS = {74, 75, 73, 77, 79, 80, 81, 83, 84, 85}
+
+logger = logging.getLogger(__name__)
+
+
+def summarize(datapoints: List[schema.Datapoint]) -> Dict[str, Any]:
+    """Given a list of Datapoints belonging to a particular agency,
+    return a dictionary containing summary statistics.
+    """
+    report_id_to_datapoints = defaultdict(list)
+    metric_key_to_datapoints = defaultdict(list)
+    metrics_configured = set()
+    metrics_available = set()
+    metrics_unavailable = set()
+
+    last_update = None
+    for datapoint in datapoints:
+        # For now, an agency's last_update is the max(dp["created_at"])
+        # over all of its datapoints. That means if a user edits an existing datapoint,
+        # it won't count towards last_update.
+        # TODO(#22370) Add updated_at to schema.Datapoint and use that instead
+        if datapoint["created_at"]:
+            datapoint_created_at = datapoint["created_at"].date()
+            if not last_update or (last_update and datapoint_created_at > last_update):
+                last_update = datapoint_created_at
+
+        # Process report datapoints (i.e. those that contain data for a time period)
+        if datapoint["is_report_datapoint"] and datapoint["value"] is not None:
+            # Group datapoints by report (i.e. time period)
+            report_id_to_datapoints[datapoint["report_id"]].append(datapoint)
+            # Group datapoints by metric
+            metric_key_to_datapoints[datapoint["metric_definition_key"]].append(
+                datapoint
+            )
+        # Process non-report datapoints (i.e. tthose that contain info about metric configuration)
+        elif (
+            not datapoint["is_report_datapoint"]
+            # The following filters to non-report datapoints that contain information about whether
+            # the top-level metric is turned on or off
+            and not datapoint["dimension_identifier_to_member"]
+            and not datapoint["context_key"]
+            and not datapoint["includes_excludes_key"]
+            and datapoint["enabled"] is not None
+        ):
+            # We consider a metric configured if it is either turned on or off
+            metrics_configured.add(datapoint["metric_definition_key"])
+            if datapoint["enabled"] is True:
+                metrics_available.add(datapoint["metric_definition_key"])
+            elif datapoint["enabled"] is False:
+                metrics_unavailable.add(datapoint["metric_definition_key"])
+
+    return {
+        LAST_UPDATE: last_update or "",
+        NUM_RECORDS_WITH_DATA: len(report_id_to_datapoints),
+        NUM_METRICS_WITH_DATA: len(metric_key_to_datapoints),
+        NUM_METRICS_CONFIGURED: len(metrics_configured),
+        NUM_METRICS_AVAILABLE: len(metrics_available),
+        NUM_METRICS_UNAVAILABLE: len(metrics_unavailable),
+    }
+
+
+def generate_agency_summary_csv(engine: Engine) -> None:
+    """Generates a CSV with data about all agencies with Publisher accounts."""
+    session = Session(bind=engine)
+
+    # Authenticate with the Google Sheets API
+    credentials = GoogleCredentials.get_application_default()
+    # GoogleCredentials.get_application_default() will use the service account
+    # assigned to the Cloud Run Job. The service account has access to the spreadsheet
+    # with editor permissions.
+    spreadsheet_service = build("sheets", "v4", credentials=credentials)
+    auth0_client = Auth0Client(  # nosec
+        domain_secret_name="justice_counts_auth0_api_domain",
+        client_id_secret_name="justice_counts_auth0_api_client_id",
+        client_secret_secret_name="justice_counts_auth0_api_client_secret",
+    )
+    auth0_users = auth0_client.get_all_users()
+    auth0_user_id_to_user = {user["user_id"]: user for user in auth0_users}
+
+    agencies = session.execute("select * from source where type = 'agency'").all()
+    agency_user_account_associations = session.execute(
+        "select * from agency_user_account_association"
+    ).all()
+    users = session.execute("select * from user_account").all()
+    datapoints = session.execute("select * from datapoint").all()
+
+    user_id_to_auth0_user = {
+        user["id"]: auth0_user_id_to_user.get(user["auth0_user_id"]) for user in users
+    }
+
+    agency_id_to_users = defaultdict(list)
+    for assoc in agency_user_account_associations:
+        auth0_user = user_id_to_auth0_user[assoc["user_account_id"]]
+        if (
+            # Skip over CSG and Recidiviz users -- we shouldn't
+            # count as a true login!
+            auth0_user
+            and "csg" not in auth0_user["email"]
+            and "recidiviz" not in auth0_user["email"]
+        ):
+            agency_id_to_users[assoc["agency_id"]].append(auth0_user)
+
+    # We have some test agencies in production, currently distinguished only in name
+    test_agency_ids = {
+        a["id"] for a in agencies if "TEST" in a["name"] or "Test" in a["name"]
+    }
+    # Skip child agencies, since a superagency might have hundreds of them
+    child_agency_ids = {a["id"] for a in agencies if a["super_agency_id"]}
+
+    original_agencies = agencies
+    agencies = [
+        dict(a)
+        for a in original_agencies
+        if a["id"] not in (test_agency_ids | child_agency_ids)
+    ]
+    agency_id_to_agency = {a["id"]: a for a in agencies}
+
+    logger.info("Number of agencies: %s", len(agencies))
+
+    for agency in agencies:
+        users = agency_id_to_users[agency["id"]]
+        agency_created_at = agency.get("created_at")
+        last_login = None
+        first_user_created_at = None
+        for user in users:
+            # If the agency was created before the agency.created_at field was
+            # added to our schema, we use the date of the first created user
+            # that belongs to that agency
+            if agency_created_at is None:
+                user_created_at = datetime.datetime.strptime(
+                    user["created_at"].split("T")[0], "%Y-%m-%d"
+                ).date()
+                if first_user_created_at is None or (
+                    user_created_at < first_user_created_at
+                ):
+                    first_user_created_at = user_created_at
+            # The agency's last_login is the most recent login date of
+            # any of its users.
+            if "last_login" not in user:
+                continue
+            user_last_login = datetime.datetime.strptime(
+                user["last_login"].split("T")[0], "%Y-%m-%d"
+            ).date()
+            if not last_login or (user_last_login > last_login):
+                last_login = user_last_login
+
+        agency[LAST_LOGIN] = last_login or ""
+        agency[CREATED_AT] = agency_created_at or first_user_created_at or ""
+        agency[LAST_UPDATE] = ""
+        agency[NUM_RECORDS_WITH_DATA] = 0
+        agency[NUM_METRICS_WITH_DATA] = 0
+        agency[NUM_METRICS_CONFIGURED] = 0
+        agency[NUM_METRICS_AVAILABLE] = 0
+        agency[NUM_METRICS_UNAVAILABLE] = 0
+        agency[IS_ALPHA_PARTNER] = agency["id"] in ALPHA_PARTNER_IDS
+        agency[IS_SUPERAGENCY] = bool(agency["is_superagency"])
+
+    agency_id_to_datapoints_groupby = groupby(
+        sorted(datapoints, key=lambda x: x["source_id"]),
+        key=lambda x: x["source_id"],
+    )
+    agency_id_to_datapoints = {k: list(v) for k, v in agency_id_to_datapoints_groupby}
+
+    for agency_id, datapoints in agency_id_to_datapoints.items():
+        if agency_id not in agency_id_to_agency:
+            continue
+
+        data = summarize(datapoints=datapoints)
+        agency_id_to_agency[agency_id] = dict(agency_id_to_agency[agency_id], **data)
+
+    agencies = list(agency_id_to_agency.values())
+
+    columns = [
+        "name",
+        "state",
+        CREATED_AT,
+        LAST_LOGIN,
+        LAST_UPDATE,
+        NUM_RECORDS_WITH_DATA,
+        NUM_METRICS_WITH_DATA,
+        NUM_METRICS_CONFIGURED,
+        NUM_METRICS_AVAILABLE,
+        NUM_METRICS_UNAVAILABLE,
+        IS_ALPHA_PARTNER,
+        IS_SUPERAGENCY,
+        "systems",
+    ]
+    df = (
+        pd.DataFrame.from_records(agencies)
+        # Sort agencies alphabetically by name
+        .sort_index().rename(columns={"state_code": "state"})
+        # Put columns in desired order
+        .reindex(columns=columns)
+    )
+
+    now = datetime.datetime.now()
+    new_sheet_title = f"{now.month}-{now.day}-{now.year}"
+
+    # Create a new worksheet in the spreadsheet
+    request = {"addSheet": {"properties": {"title": new_sheet_title, "index": 1}}}
+
+    # Create new sheet
+    spreadsheet_service.spreadsheets().batchUpdate(
+        spreadsheetId=SPREADSHEET_ID, body={"requests": [request]}
+    ).execute()
+
+    logger.info("New Sheet Created")
+
+    # Format data to fit Google API specifications.
+    # Google API spec requires a list of lists,
+    # each list representing a row.
+    data_to_write = [columns]
+    data_to_write.extend(df.astype(str).values.tolist())
+    body = {"values": data_to_write}
+    range_name = f"{new_sheet_title}!A1"  #
+    spreadsheet_service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID,
+        range=range_name,
+        valueInputOption="RAW",
+        body=body,
+    ).execute()
+
+    logger.info("Sheet '%s' added and data written.", new_sheet_title)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    database_key = SQLAlchemyDatabaseKey.for_schema(
+        SchemaType.JUSTICE_COUNTS,
+    )
+    justice_counts_engine = SQLAlchemyEngineManager.init_engine(
+        database_key=database_key,
+        secret_prefix_override=JUSTICE_COUNTS_DB_SECRET_PREFIX,
+    )
+
+    generate_agency_summary_csv(justice_counts_engine)
