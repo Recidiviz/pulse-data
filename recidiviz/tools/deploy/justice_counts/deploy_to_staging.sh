a5a8b762ebdfe51caca5e4e4134d0646045299f8
Nichelle <nichelle.a.hall@gmail.com>
2023-09-26 16:59:45 -0500

    [Justice Counts] update staging and production deploy scrips to deploy data pull jobs (Recidiviz/recidiviz-data#24235)
    Turns out I updated the staging script incorrectly and it was updating
    the csg-data-pull image in production, not staging! This PR now
    correctly updates the right images in the right environments. I tested
    the updated call in `deploy_to_staging` in a bash script as well and
    everything worked as expected.
    
    ## Related issues
    
    Related to Recidiviz/recidiviz-data#24039
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: c79d4eafd9987d91c6211f30cb0ef418f1ee1855

--- recidiviz/tools/deploy/justice_counts/deploy_to_staging.sh
+++ recidiviz/tools/deploy/justice_counts/deploy_to_staging.sh
@@ -161,7 +161,7 @@ run_cmd gcloud -q container images add-tag "${LATEST_DOCKER_TAG}" "${REMOTE_IMAG
 echo "Updating Image for Cloud Run Jobs"
 run_cmd gcloud run jobs update justice-counts-recurring-reports --image "${LATEST_DOCKER_TAG}" --region "us-central1"
 run_cmd gcloud run jobs update justice-counts-staging-recurring-new-mexico-courts-dataxchange --image "${LATEST_DOCKER_TAG}" --region "us-central1"
-run_cmd gcloud run jobs update justice-counts-csg-data-pull --image "${LATEST_DOCKER_TAG}" --region "us-central1" --project "justice-counts-production"
+run_cmd gcloud run jobs update csg-data-pull --image "${LATEST_DOCKER_TAG}" --region "us-central1" --project "justice-counts-staging"
 
 # TODO(#16325): Create release candidate branches to facilitate cherry-picks.
 
