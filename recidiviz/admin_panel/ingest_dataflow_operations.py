b9e272119a65b7ee692567b3ddbb69c613d2234a
Gloria Liu <gloria@recidiviz.org>
2023-09-27 15:28:39 -0400

    get_all_latest_ingest_jobs with google-cloud-dataflow-client (Recidiviz/recidiviz-data#24134)
    ## Description of the change
    
    implement `get_all_latest_ingest_jobs` to get the latest job of each job
    name with the google cloud dataflow client
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Helper Bot <helperbot@recidiviz.org>
    Co-authored-by: Anna Geiduschek <anna@recidiviz.org>
    GitOrigin-RevId: 5d95b7bd36965e1dbee001ca073430f11e75a871

---
+++ recidiviz/admin_panel/ingest_dataflow_operations.py
@@ -0,0 +1,150 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Functions and classes for calling the dataflow API for ingest pipeline metadata."""
+import concurrent.futures
+import os
+from collections import defaultdict
+from concurrent import futures
+from typing import Dict, Optional
+
+import attr
+from google.cloud import dataflow_v1beta3
+from google.cloud.dataflow_v1beta3 import JobState, ListJobsRequest
+
+import recidiviz
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.regions.direct_ingest_region_utils import (
+    get_direct_ingest_states_launched_in_env,
+)
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.utils import metadata
+from recidiviz.utils.yaml_dict import YAMLDict
+
+
+@attr.define(kw_only=True)
+class DataflowPipelineMetadataResponse:
+    id: str
+    project_id: str
+    name: str
+    create_time: float
+    start_time: float
+    termination_time: float
+    termination_state: str
+    location: str
+
+    @property
+    def duration(self) -> float:
+        return self.termination_time - self.start_time
+
+
+PIPELINE_CONFIG_YAML_PATH = os.path.join(
+    os.path.dirname(recidiviz.__file__), "pipelines/calculation_pipeline_templates.yaml"
+)
+
+
+def get_latest_job_for_state_instance(
+    state_code: StateCode, instance: DirectIngestInstance
+) -> Optional[DataflowPipelineMetadataResponse]:
+    return get_all_latest_ingest_jobs()[state_code][instance]
+
+
+def get_latest_jobs_from_location_by_name(
+    location: str,
+) -> Dict[str, DataflowPipelineMetadataResponse]:
+    """Fetch most recent job for each job name in the specified location (us-west1, us-central3, etc)."""
+    client = dataflow_v1beta3.JobsV1Beta3Client()
+
+    request = dataflow_v1beta3.ListJobsRequest(
+        filter=ListJobsRequest.Filter.TERMINATED,
+        project_id=metadata.project_id(),
+        location=location,
+    )
+
+    # Returns a list of all jobs, ordered by termination time, descending
+    page_result = client.list_jobs(request=request)
+
+    jobs_by_name = {}
+    for response in page_result:
+        if response.name in jobs_by_name:
+            # We have already found a more recent job
+            continue
+        if response.current_state in (
+            JobState.JOB_STATE_DONE,
+            JobState.JOB_STATE_FAILED,
+        ):
+            job = DataflowPipelineMetadataResponse(
+                id=response.id,
+                project_id=response.project_id,
+                name=response.name,
+                create_time=response.create_time.timestamp(),  # type: ignore[attr-defined]
+                start_time=response.start_time.timestamp(),  # type: ignore[attr-defined]
+                termination_time=response.create_time.timestamp(),  # type: ignore[attr-defined]
+                termination_state=response.current_state.name,
+                location=response.location,
+            )
+            jobs_by_name[response.name] = job
+
+    return jobs_by_name
+
+
+def ingest_pipeline_name(state_code: StateCode, instance: DirectIngestInstance) -> str:
+    state_code_part = state_code.value.lower().replace("_", "-")
+    instance_part = "-secondary" if instance is DirectIngestInstance.SECONDARY else ""
+    pipeline_type_part = "-ingest"
+    return f"{state_code_part}{pipeline_type_part}{instance_part}"
+
+
+def get_all_latest_ingest_jobs() -> Dict[
+    StateCode, Dict[DirectIngestInstance, Optional[DataflowPipelineMetadataResponse]]
+]:
+    """Get the latest job for each ingest pipeline."""
+    pipeline_configs = YAMLDict.from_path(PIPELINE_CONFIG_YAML_PATH)
+
+    all_locations = {
+        ingest_pipeline.peek("region", str)
+        for ingest_pipeline in pipeline_configs.pop_dicts("ingest_pipelines")
+    }
+
+    # TODO(#24241) cache results inside the ingest operations store
+    with futures.ThreadPoolExecutor(max_workers=4) as executor:
+        latest_job_by_name = {}
+        locations_futures = [
+            executor.submit(get_latest_jobs_from_location_by_name, location)
+            for location in all_locations
+        ]
+        for future in concurrent.futures.as_completed(locations_futures):
+            data = future.result()
+            for job_name, job in data.items():
+                latest_job_by_name[job_name] = job
+
+    jobs_by_state_instance: Dict[
+        StateCode,
+        Dict[DirectIngestInstance, Optional[DataflowPipelineMetadataResponse]],
+    ] = defaultdict(lambda: defaultdict(None))
+    for state_code in get_direct_ingest_states_launched_in_env():
+        for instance in DirectIngestInstance:
+            pipeline_name = ingest_pipeline_name(state_code, instance)
+            latest_job = latest_job_by_name.get(pipeline_name)
+            jobs_by_state_instance[state_code][instance] = latest_job
+    return jobs_by_state_instance
+
+
+def is_ingest_enabled_in_secondary(
+    state_code: StateCode,  # pylint: disable=unused-argument
+) -> bool:
+    # TODO(#24240): Look at mappings to determine whether there is instance-specific logic
+    return False
---
+++ recidiviz/tests/admin_panel/ingest_dataflow_operations_test.py
@@ -0,0 +1,203 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+"""Implements tests for the IngestDataflowOperations."""
+import datetime
+import os
+from unittest import mock
+from unittest.case import TestCase
+from unittest.mock import patch
+
+import recidiviz
+from recidiviz.admin_panel.ingest_dataflow_operations import (
+    DataflowPipelineMetadataResponse,
+    get_all_latest_ingest_jobs,
+    ingest_pipeline_name,
+)
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.tests import pipelines as recidiviz_pipelines_tests_module
+from recidiviz.utils.yaml_dict import YAMLDict
+
+PIPELINES_TESTS_WORKING_DIRECTORY = os.path.dirname(
+    recidiviz_pipelines_tests_module.__file__
+)
+
+FAKE_PIPELINE_CONFIG_YAML_PATH = os.path.join(
+    PIPELINES_TESTS_WORKING_DIRECTORY,
+    "fake_calculation_pipeline_templates.yaml",
+)
+
+
+class IngestDataflowOperations(TestCase):
+    """Implements tests for get_all_latest_ingest_dataflow_jobs and helpers."""
+
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.latest_jobs_from_location_patcher = mock.patch(
+            "recidiviz.admin_panel.ingest_dataflow_operations.get_latest_jobs_from_location_by_name"
+        )
+        self.latest_jobs_from_location_patcher.start()
+
+        self.state_code_list_patcher = mock.patch(
+            "recidiviz.admin_panel.ingest_dataflow_operations.get_direct_ingest_states_launched_in_env",
+            return_value=[
+                StateCode.US_XX,
+                StateCode.US_YY,
+            ],
+        )
+        self.state_code_list_patcher.start()
+
+        self.pipeline_config_yaml_path_patcher = patch(
+            "recidiviz.admin_panel.ingest_dataflow_operations.PIPELINE_CONFIG_YAML_PATH",
+            FAKE_PIPELINE_CONFIG_YAML_PATH,
+        )
+        self.pipeline_config_yaml_path_patcher.start()
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.pipeline_config_yaml_path_patcher.stop()
+        self.state_code_list_patcher.stop()
+
+    @mock.patch(
+        "recidiviz.admin_panel.ingest_dataflow_operations.is_ingest_enabled_in_secondary",
+        True,
+    )
+    def test_get_all_latest_ingest_jobs_simple(self) -> None:
+
+        pipeline = DataflowPipelineMetadataResponse(
+            id="1234",
+            project_id="test-project",
+            name="us-xx-ingest",
+            create_time=datetime.datetime(2023, 7, 10).timestamp(),
+            start_time=datetime.datetime(2023, 7, 10).timestamp(),
+            termination_time=datetime.datetime(2023, 7, 11).timestamp(),
+            termination_state="JOB_STATE_FAILED",
+            location="us-east1",
+        )
+
+        pipeline2 = DataflowPipelineMetadataResponse(
+            id="1235",
+            project_id="test-project",
+            name="us-xx-ingest-secondary",
+            create_time=datetime.datetime(2023, 9, 10).timestamp(),
+            start_time=datetime.datetime(2023, 9, 10).timestamp(),
+            termination_time=datetime.datetime(2023, 9, 11).timestamp(),
+            termination_state="JOB_STATE_FAILED",
+            location="us-east1",
+        )
+
+        pipeline3 = DataflowPipelineMetadataResponse(
+            id="1236",
+            project_id="test-project",
+            name="us-yy-ingest",
+            create_time=datetime.datetime(2023, 8, 10).timestamp(),
+            start_time=datetime.datetime(2023, 8, 10).timestamp(),
+            termination_time=datetime.datetime(2023, 8, 11).timestamp(),
+            termination_state="JOB_STATE_DONE",
+            location="us-east1",
+        )
+
+        pipeline4 = DataflowPipelineMetadataResponse(
+            id="1237",
+            project_id="test-project",
+            name="us-yy-ingest-secondary",
+            create_time=datetime.datetime(2023, 10, 10).timestamp(),
+            start_time=datetime.datetime(2023, 10, 10).timestamp(),
+            termination_time=datetime.datetime(2023, 10, 11).timestamp(),
+            termination_state="JOB_STATE_DONE",
+            location="us-east1",
+        )
+        expected = {
+            StateCode.US_XX: {
+                DirectIngestInstance.PRIMARY: pipeline,
+                DirectIngestInstance.SECONDARY: pipeline2,
+            },
+            StateCode.US_YY: {
+                DirectIngestInstance.PRIMARY: pipeline3,
+                DirectIngestInstance.SECONDARY: pipeline4,
+            },
+        }
+        with mock.patch(
+            "recidiviz.admin_panel.ingest_dataflow_operations.get_latest_jobs_from_location_by_name",
+            return_value={
+                "us-xx-ingest": pipeline,
+                "us-xx-ingest-secondary": pipeline2,
+                "us-yy-ingest": pipeline3,
+                "us-yy-ingest-secondary": pipeline4,
+            },
+        ):
+            self.assertEqual(expected, get_all_latest_ingest_jobs())
+
+    def test_get_all_latest_ingest_jobs_no_secondary(self) -> None:
+
+        pipeline = DataflowPipelineMetadataResponse(
+            id="1234",
+            project_id="test-project",
+            name="us-xx-ingest",
+            create_time=datetime.datetime(2023, 7, 10).timestamp(),
+            start_time=datetime.datetime(2023, 7, 10).timestamp(),
+            termination_time=datetime.datetime(2023, 7, 11).timestamp(),
+            termination_state="JOB_STATE_FAILED",
+            location="us-east1",
+        )
+
+        pipeline2 = DataflowPipelineMetadataResponse(
+            id="1236",
+            project_id="test-project",
+            name="us-yy-ingest",
+            create_time=datetime.datetime(2023, 8, 10).timestamp(),
+            start_time=datetime.datetime(2023, 8, 10).timestamp(),
+            termination_time=datetime.datetime(2023, 8, 11).timestamp(),
+            termination_state="JOB_STATE_FAILED",
+            location="us-east1",
+        )
+
+        expected = {
+            StateCode.US_XX: {
+                DirectIngestInstance.PRIMARY: pipeline,
+                DirectIngestInstance.SECONDARY: None,
+            },
+            StateCode.US_YY: {
+                DirectIngestInstance.PRIMARY: pipeline2,
+                DirectIngestInstance.SECONDARY: None,
+            },
+        }
+        with mock.patch(
+            "recidiviz.admin_panel.ingest_dataflow_operations.get_latest_jobs_from_location_by_name",
+            return_value={
+                "us-xx-ingest": pipeline,
+                "us-yy-ingest": pipeline2,
+            },
+        ):
+            self.assertEqual(expected, get_all_latest_ingest_jobs())
+
+    def test_ingest_pipelines_correct_job_names(self) -> None:
+        PIPELINE_CONFIG_YAML_PATH = os.path.join(
+            os.path.dirname(recidiviz.__file__),
+            "pipelines/calculation_pipeline_templates.yaml",
+        )
+        pipeline_configs = YAMLDict.from_path(PIPELINE_CONFIG_YAML_PATH)
+
+        for ingest_pipeline in pipeline_configs.pop_dicts("ingest_pipelines"):
+            state_code = ingest_pipeline.peek("state_code", str)
+            expected_name = ingest_pipeline_name(
+                StateCode(state_code), DirectIngestInstance.PRIMARY
+            )
+            name = ingest_pipeline.peek("job_name", str)
+            self.assertEqual(expected_name, name)
