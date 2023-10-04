a6aab415a1d44e3828eccafcfda4b6eff456ba85
Nichelle <nichelle.a.hall@gmail.com>
2023-10-04 11:51:22 -0500

    [Justice Counts] [1/n] migrate admin panel routes that will not change to publisher (Recidiviz/recidiviz-data#24390)
    ## Description of the change
    
    This PR moves three methods `get_all_users`, `get_all_agencies` and
    `get_agency` to Publisher. These methods have not been changed from
    their original form in `justice_counts_tools`. I have also created a
    unit tests now for robustness.
    
    ## Related issues
    
    Recidiviz/recidiviz-data#24389
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: ba5a785466509bba1324962168e33329e29156b2

---
+++ recidiviz/justice_counts/control_panel/routes/admin.py
@@ -0,0 +1,99 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Implements api routes for the Justice Counts Publisher Admin Panel."""
+from typing import Callable
+
+from flask import Blueprint, Response, jsonify
+from flask_sqlalchemy_session import current_session
+
+from recidiviz.justice_counts.agency import AgencyInterface
+from recidiviz.justice_counts.user_account import UserAccountInterface
+from recidiviz.persistence.database.schema.justice_counts import schema
+
+
+def get_admin_blueprint(
+    auth_decorator: Callable,
+) -> Blueprint:
+    """API methods for Publisher Admin Panel"""
+    admin_blueprint = Blueprint("admin", __name__)
+
+    # UserAccount
+    @admin_blueprint.route("/user", methods=["GET"])
+    @auth_decorator
+    def fetch_all_users() -> Response:
+        """
+        Fetches all users and their metadata to display the User Provisioning Table
+        """
+        db_users = UserAccountInterface.get_users(session=current_session)
+        # AgencyUserAccountAssocs are loaded automatically when we
+        # query for a user in the UserAccount table.
+
+        agencies = AgencyInterface.get_agencies(session=current_session)
+        agencies_by_id = {agency.id: agency for agency in agencies}
+
+        user_json = []
+        for user in db_users:
+            user_agencies = list(
+                filter(
+                    None,
+                    [
+                        agencies_by_id.get(assoc.agency_id)
+                        for assoc in user.agency_assocs
+                    ],
+                )
+            )
+            user_json.append(user.to_json(agencies=user_agencies))
+        return jsonify({"users": user_json})
+
+    # Agency
+    @admin_blueprint.route("/agency", methods=["GET"])
+    @auth_decorator
+    def fetch_all_agencies() -> Response:
+        """
+        Fetches all agencies and their metadata to display the Agency Provisioning Table
+        """
+        agencies = AgencyInterface.get_agencies(
+            session=current_session, with_users=True, with_settings=False
+        )
+        return jsonify(
+            {
+                "agencies": [
+                    agency.to_json(with_team=True, with_settings=False)
+                    for agency in agencies
+                ],
+                # also send list of possible systems to use in the dropdown
+                # when users can assign a system role to an agency.
+                "systems": [enum.value for enum in schema.System],
+            }
+        )
+
+    @admin_blueprint.route("/agency/<agency_id>", methods=["GET"])
+    @auth_decorator
+    def get_agency(agency_id: int) -> Response:
+        """Returns metadata for an individual agency."""
+        return jsonify(
+            {
+                "agency": AgencyInterface.get_agency_by_id(
+                    session=current_session, agency_id=agency_id
+                ).to_json(),
+                # also send list of possible roles to use in the dropdown
+                # when users can assign a new role to user.
+                "roles": [role.value for role in schema.UserAccountRole],
+            }
+        )
+
+    return admin_blueprint
