48977168269e981706c153f7ca6959067db82870
Emily Chao (they/them) <emily@recidiviz.org>
2023-10-05 12:12:12 -0700

    Adding exemptions for the ingest view tree merger entity matching errors (Recidiviz/recidiviz-data#24434)
    ## Description of the change
    
    This PR adds exemptions for the IngestViewTreeMerger
    EntityMatchingErrors we're seeing for OR and PA and updates the correct
    pipeline step on how to handle those exemptions.
    
    ## Type of change
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#24283
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [X] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 0b0c8a6f456b8d06261de096aa004c28be5294a5

---
+++ recidiviz/pipelines/ingest/state/exemptions.py
@@ -0,0 +1,23 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Listing out exemptions for the state ingest pipeline."""
+from recidiviz.common.constants.states import StateCode
+
+INGEST_VIEW_TREE_MERGER_ERROR_EXEMPTIONS = {
+    # TODO(#24299) Remove this exemption once conflicts no longer appear.
+    StateCode.US_PA: {"sci_incarceration_period"},
+}
