e9dd97d5eea9a5afbb27079931e1f6777b5ec115
jazzPouls <jpouls@berkeley.edu>
2023-10-13 13:42:04 -0400

    [Spark] Create views for most recent spark output data (2nd attempt) (Recidiviz/recidiviz-data#24633)
    ## Description of the change
    
    Reintroduces Recidiviz/recidiviz-data#24216, which adds views for accessing the most recent
    version of Spark output data, bug fixes an issue where a source table
    dataset was erroneously added to the managed view dataset cleanup
    candidates list.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    N/A
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Paco Poler <pacopoler@Pacos-MacBook-Pro.local>
    Co-authored-by: Andrew Gaidus <andrewg@recidiviz.org>
    Co-authored-by: Paco Poler <paco@poler.org>
    GitOrigin-RevId: efaf32980f2dbd5978c1e178f32c4b487b0a618c

--- recidiviz/calculator/query/state/dataset_config.py
+++ recidiviz/calculator/query/state/dataset_config.py
@@ -82,6 +82,12 @@ POPULATION_PROJECTION_DATASET: str = "population_projection_data"
 # Where the population projection simulation output tables live
 POPULATION_PROJECTION_OUTPUT_DATASET: str = "population_projection_output_data"
 
+# Where the spark simulation output data tables live
+SPARK_OUTPUT_DATASET: str = "spark_public_output_data"
+
+# Contains views that select the most recent data for each spark simulation tag from spark_public_output_data
+SPARK_OUTPUT_DATASET_MOST_RECENT: str = "spark_public_output_data_most_recent"
+
 # Where the views for the vitals report live
 VITALS_REPORT_DATASET: str = "vitals_report_views"
 
