58b5b5409705e42fb23020375b9e8b1e155e8181
Chinedu Ozodi <chinedu@recidiviz.org>
2023-10-16 18:20:15 -0700

    [Airflow] Implementation of compare max update datetime and watermark in Ingest DAG (Recidiviz/recidiviz-data#24671)
    ## Description of the change
    
    Implementation of compare max update datetime and watermark in Ingest
    DAG. Still need to finish writing and updating the tests.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#23984
    Closes Recidiviz/recidiviz-data#24676
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 3fca6d4dad2fa6c9ec36394c8d42ae0c2406d32f

---
+++ recidiviz/airflow/dags/ingest/get_max_update_datetime_sql_query_generator.py
@@ -0,0 +1,65 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""The CloudSQLQueryGenerator for getting the max update datetime from direct_ingest_raw_file_metadata."""
+from typing import Dict
+
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.context import Context
+
+from recidiviz.airflow.dags.ingest.metadata import FILE_TAG, MAX_UPDATE_DATETIME
+from recidiviz.airflow.dags.operators.cloud_sql_query_operator import (
+    CloudSqlQueryGenerator,
+    CloudSqlQueryOperator,
+)
+from recidiviz.persistence.database.schema.operations.schema import (
+    DirectIngestRawFileMetadata,
+)
+
+
+class GetMaxUpdateDateTimeSqlQueryGenerator(CloudSqlQueryGenerator[Dict[str, str]]):
+    """Custom query generator for getting the max update datetime from direct_ingest_raw_file_metadata."""
+
+    def __init__(self, region_code: str, ingest_instance: str) -> None:
+        super().__init__()
+        self.region_code = region_code
+        self.ingest_instance = ingest_instance
+
+    def execute_postgres_query(
+        self,
+        operator: CloudSqlQueryOperator,
+        postgres_hook: PostgresHook,
+        context: Context,
+    ) -> Dict[str, str]:
+        """Returns the max update datetime from direct_ingest_raw_file_metadata."""
+
+        max_update_datetimes: Dict[str, str] = {
+            row[FILE_TAG]: row[MAX_UPDATE_DATETIME]
+            for _, row in postgres_hook.get_pandas_df(self.sql_query()).iterrows()
+        }
+
+        return max_update_datetimes
+
+    def sql_query(self) -> str:
+        return f"""
+            SELECT {FILE_TAG}, MAX(update_datetime) AS {MAX_UPDATE_DATETIME}
+            FROM {DirectIngestRawFileMetadata.__tablename__}
+            WHERE raw_data_instance = '{self.ingest_instance.upper()}' 
+            AND is_invalidated = false 
+            AND file_processed_time IS NOT NULL 
+            AND region_code = '{self.region_code.upper()}'
+            GROUP BY {FILE_TAG};
+        """
--- recidiviz/airflow/dags/ingest/single_ingest_pipeline_group.py
+++ recidiviz/airflow/dags/ingest/single_ingest_pipeline_group.py
@@ -17,18 +17,33 @@
 """
 Logic for state and ingest instance specific dataflow pipelines.
 """
+import logging
+from typing import Dict
+
+from airflow.decorators import task, task_group
 from airflow.models import BaseOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.utils.task_group import TaskGroup
 from airflow.utils.trigger_rule import TriggerRule
 
+from recidiviz.airflow.dags.ingest.get_max_update_datetime_sql_query_generator import (
+    GetMaxUpdateDateTimeSqlQueryGenerator,
+)
+from recidiviz.airflow.dags.ingest.get_watermark_sql_query_generator import (
+    GetWatermarkSqlQueryGenerator,
+)
 from recidiviz.airflow.dags.ingest.ingest_branching import get_ingest_branch_key
+from recidiviz.airflow.dags.operators.cloud_sql_query_operator import (
+    CloudSqlQueryOperator,
+)
 from recidiviz.airflow.dags.operators.recidiviz_kubernetes_pod_operator import (
     build_kubernetes_pod_task,
 )
+from recidiviz.airflow.dags.utils.cloud_sql import cloud_sql_conn_id_for_schema_type
 from recidiviz.common.constants.states import StateCode
 from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.persistence.database.schema_type import SchemaType
 
 # Need a disable pointless statement because Python views the chaining operator ('>>')
 # as a "pointless" statement
@@ -38,11 +53,63 @@ from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestIns
 # pylint: disable=W0106 expression-not-assigned
 
 
+@task.short_circuit(task_id="should_run_based_on_watermarks")
+def _should_run_based_on_watermarks(
+    watermarks: Dict[str, str], max_update_datetimes: Dict[str, str]
+) -> bool:
+    for raw_file_tag, watermark_for_file in watermarks.items():
+        if raw_file_tag not in max_update_datetimes:
+            raise ValueError(
+                f"Watermark file_tag '{raw_file_tag}' not found in max update datetimes: {max_update_datetimes.keys()}"
+            )
+
+        if watermark_for_file > max_update_datetimes[raw_file_tag]:
+            logging.error(
+                "Raw file %s has older data than the last time the ingest pipeline was run. Current max update_datetime: %s. Last ingest pipeline run update_datetime: %s.",
+                raw_file_tag,
+                max_update_datetimes[raw_file_tag],
+                watermark_for_file,
+            )
+            return False
+
+    return True
+
+
+@task_group(group_id="initialize_dataflow_pipeline")
 def _initialize_dataflow_pipeline(
-    _state_code: StateCode, _instance: DirectIngestInstance
-) -> BaseOperator:
-    # TODO(#23984): Check raw data max update time is less than max watermark
-    return EmptyOperator(task_id="check_raw_data_max_update_time")
+    state_code: StateCode, instance: DirectIngestInstance
+) -> None:
+    """
+    Initializes the dataflow pipeline by getting the max update datetimes and watermarks and checking if the pipeline should run.
+    """
+    operations_cloud_sql_conn_id = cloud_sql_conn_id_for_schema_type(
+        SchemaType.OPERATIONS
+    )
+
+    get_max_update_datetimes = CloudSqlQueryOperator(
+        task_id="get_max_update_datetimes",
+        cloud_sql_conn_id=operations_cloud_sql_conn_id,
+        query_generator=GetMaxUpdateDateTimeSqlQueryGenerator(
+            region_code=state_code.value,
+            ingest_instance=instance.value,
+        ),
+    )
+
+    get_watermarks = CloudSqlQueryOperator(
+        task_id="get_watermarks",
+        cloud_sql_conn_id=operations_cloud_sql_conn_id,
+        query_generator=GetWatermarkSqlQueryGenerator(
+            region_code=state_code.value,
+            ingest_instance=instance.value,
+        ),
+    )
+
+    should_run_based_on_watermarks = _should_run_based_on_watermarks(
+        watermarks=get_watermarks.output,  # type: ignore[arg-type]
+        max_update_datetimes=get_max_update_datetimes.output,  # type: ignore[arg-type]
+    )
+
+    [get_max_update_datetimes, get_watermarks] >> should_run_based_on_watermarks
 
 
 def _acquire_lock(
@@ -90,6 +157,7 @@ def create_single_ingest_pipeline_group(
     """
     Creates a dataflow pipeline operator for the given state and ingest instance.
     """
+
     with TaskGroup(get_ingest_branch_key(state_code.value, instance.value)) as dataflow:
 
         initialize_dataflow_pipeline = _initialize_dataflow_pipeline(
