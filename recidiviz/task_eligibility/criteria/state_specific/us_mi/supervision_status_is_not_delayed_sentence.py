03a9def6f08cf02701d9ed0afcce3e5d858e6f2f
Shalin Brahmbhatt <63821972+shalindb@users.noreply.github.com>
2023-10-20 12:07:25 -0700

    [US_MI] [Workflows] Added exclusionary criteria to early discharge from probation (Recidiviz/recidiviz-data#24832)
    ## Description of the change
    
    Added exclusionary criteria –
    `supervision_status_is_not_delayed_sentence` – to
    `complete_discharge_early_from_probation_supervision_request`
    
    Looking at
    `recidiviz-staging.us_mi_raw_data_up_to_date_views_secondary.COMS_Supervision_Statuses_latest`,
    reporting 722 clients are currently serving delayed sentences.
    
    Eligibility Changes:
    184 individuals **no longer eligible** in
    `complete_discharge_early_from_probation_supervision_request` due to
    supervision status being delayed.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#24818
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6744efa5aca52ec027908be212a972de5057b6b2

---
+++ recidiviz/task_eligibility/criteria/state_specific/us_mi/supervision_status_is_not_delayed_sentence.py
@@ -0,0 +1,99 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""This criteria view builder defines spans of time that a client is not on a delayed sentence
+"""
+
+from recidiviz.calculator.query.bq_utils import nonnull_end_date_clause
+from recidiviz.calculator.query.sessions_query_fragments import (
+    aggregate_adjacent_spans,
+    create_sub_sessions_with_attributes,
+)
+from recidiviz.calculator.query.state.dataset_config import (
+    NORMALIZED_STATE_DATASET,
+)
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.ingest.direct.dataset_config import raw_latest_views_dataset_for_region
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "US_MI_SUPERVISION_STATUS_IS_NOT_DELAYED_SENTENCE"
+
+_DESCRIPTION = """This criteria view builder defines spans of time that a client is not on a delayed sentence
+"""
+
+_QUERY_TEMPLATE = f"""
+WITH delayed_spans AS (
+        SELECT
+          'US_MI' AS state_code,
+          pei.person_id,
+          SAFE_CAST(SAFE_CAST(start_date AS DATETIME) AS DATE) AS start_date,
+          SAFE_CAST(SAFE_CAST(end_date AS DATETIME) AS DATE) AS end_date,
+          TRUE as is_delayed_sent,
+        FROM
+          `{{project_id}}.{{raw_data_up_to_date_views_dataset}}.COMS_Supervision_Statuses_latest`
+        INNER JOIN
+          `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` pei
+        ON
+          LTRIM(Offender_Number, '0')= pei.external_id
+          AND pei.state_code = 'US_MI'
+          AND pei.id_type = "US_MI_DOC"
+        WHERE
+          Supervision_Status LIKE '%Delayed Sentence%'
+          AND SAFE_CAST(SAFE_CAST(start_date AS DATETIME) AS DATE) != {nonnull_end_date_clause('SAFE_CAST(SAFE_CAST(end_date AS DATETIME) AS DATE)')}
+),
+{create_sub_sessions_with_attributes('delayed_spans')},
+deduped_sub_sessions AS (
+    SELECT 
+        state_code,
+        person_id,
+        start_date,
+        end_date,
+        is_delayed_sent,
+    FROM sub_sessions_with_attributes
+    GROUP BY 1,2,3,4,5)
+    SELECT 
+        state_code,
+        person_id,
+        start_date,
+        end_date,
+        FALSE AS meets_criteria,
+    TO_JSON(STRUCT(TRUE AS supervision_status_is_delayed_sentence)) AS reason,
+    FROM ({aggregate_adjacent_spans(table_name='deduped_sub_sessions', attribute=['is_delayed_sent'])})
+"""
+
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
+    StateSpecificTaskCriteriaBigQueryViewBuilder(
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+        criteria_spans_query_template=_QUERY_TEMPLATE,
+        state_code=StateCode.US_MI,
+        raw_data_up_to_date_views_dataset=raw_latest_views_dataset_for_region(
+            state_code=StateCode.US_MI,
+            instance=DirectIngestInstance.PRIMARY,
+        ),
+        normalized_state_dataset=NORMALIZED_STATE_DATASET,
+        meets_criteria_default=True,
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
