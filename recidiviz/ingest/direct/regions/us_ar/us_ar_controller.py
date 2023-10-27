40592a5c279e2dc0e8832f45e33d1eaa21888437
n-damiani <113454156+n-damiani@users.noreply.github.com>
2023-10-27 09:27:57 -0500

    [US_AR] Ingest supervision periods (Recidiviz/recidiviz-data#23346)
    ## Description of the change
    
    Adds a view and mapping for supervision periods in Arkansas. Rebased off
    of `n-damiani/ar_person_views`; files from that branch are unchanged.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#21687
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 08480d8f773be6dc1e378de5bddc871955e023f4

--- recidiviz/ingest/direct/regions/us_ar/us_ar_controller.py
+++ recidiviz/ingest/direct/regions/us_ar/us_ar_controller.py
@@ -43,4 +43,4 @@ class UsArController(BaseDirectIngestController):
         """
 
         # Add ingest view names to this list as you add mappings for them.
-        return ["person", "staff"]
+        return ["person", "staff", "supervision_period"]
