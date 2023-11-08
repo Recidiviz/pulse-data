a84a801cabf277db081d040698187ca32abd21f5
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2023-11-08 10:38:06 -0800

    [Ingest] Run CA, NC and OR in ingest DAG daily (Recidiviz/recidiviz-data#25289)
    ## Description of the change
    
    Updates ingest in dataflow gating so that US_CA, US_NC and US_OR ingest
    pipelines run daily in the ingest Airflow DAG in staging. This will have
    no downstream impact on data in the `state` dataset because the BQ
    refresh is not yet set up to read from the new pipeline outputs.
    
    Collectively these three pipelines cost ~$2 to run, so there is no
    meaningful cost to turning these on early before we are prepared to
    enable the BQ refresh with this new data.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#25225
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: bc1d5fa944405b29affe3fe9ec26a3d404ab60c7

--- recidiviz/pipelines/ingest/pipeline_utils.py
+++ recidiviz/pipelines/ingest/pipeline_utils.py
@@ -23,8 +23,9 @@ from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestIns
 # The compute region (e.g. "us-east1") ingest pipelines for a given state should be
 #  run in.
 DEFAULT_INGEST_PIPELINE_REGIONS_BY_STATE_CODE: Dict[StateCode, str] = {
-    # Fill this out as we launch pipelines, e.g.:
-    # StateCode.US_AR: "us-west1"
+    StateCode.US_CA: "us-east1",
+    StateCode.US_NC: "us-east1",
+    StateCode.US_OR: "us-west3",
 }
 
 
