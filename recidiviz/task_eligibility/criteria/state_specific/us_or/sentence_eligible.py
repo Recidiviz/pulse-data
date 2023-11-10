e46723343aebbd8b2736fab1ac5373fcaa182738
Daniel Allen <daniel@recidiviz.org>
2023-11-10 13:09:30 -0500

    [US_OR] Earned Discharge TES stubs (Recidiviz/recidiviz-data#25275)
    ## Description of the change
    
    Stubs for criteria spans and task spans.
    
    Reasons doc
    [here](https://paper.dropbox.com/doc/OR-Workflows-Reasons--CDWujxUZ_m1uX9qg4i02hAKIAg-oVhxrNil4dskRUPfk5OP9).
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#25217
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: e5e0a6c743980bdf6b86acdb3d32a83b9404f89b

---
+++ recidiviz/task_eligibility/criteria/state_specific/us_or/sentence_eligible.py
@@ -0,0 +1,54 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""Combined sentence-related criteria for OR Earned Discharge
+"""
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.placeholder_criteria_builders import (
+    state_specific_placeholder_criteria_view_builder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "US_OR_SENTENCE_ELIGIBLE"
+
+_DESCRIPTION = """Combined sentence-related criteria for OR Earned Discharge"""
+
+_REASON_QUERY = """TO_JSON(STRUCT(
+        [STRUCT(
+            DATE("9999-12-31") AS eligible_date,
+            DATE("9999-12-31") AS sentence_imposed_date,
+            DATE("9999-12-31") AS supervision_sentence_start_date,
+            DATE("9999-12-31") AS latest_conviction_date,
+            0 AS num_days_absconsion,
+            "" AS sentence_statute
+        )] AS active_sentences
+    ))"""
+
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
+    state_specific_placeholder_criteria_view_builder(
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+        reason_query=_REASON_QUERY,
+        state_code=StateCode.US_OR,
+    )
+)
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
