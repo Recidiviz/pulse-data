980469f4b0a4bd9ebc8522921953c2bc8e4e0f87
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2023-11-27 10:38:48 -0500

    [US_PA] Refactor state staff role period using roster (Recidiviz/recidiviz-data#25619)
    ## Description of the change
    
    This is still built off of three data sources for completeness (roster,
    case assignment history, and supervision contact history), and creates
    many overlapping periods for agents who have appeared as both
    supervisors and agents across those data sources. Since overlapping
    periods are a possibility for staff role periods, this view does not
    create windows when roles change; instead, it collects the earliest and
    latest instances of a staff member in each role they have held. Role
    periods constructed from the roster are assumed to be current and do not
    have valued end_dates.
    
    checks:
    - run_mappings script passes
    - confirmed deterministic
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: bb0e104765ac7254fdd359511954276cfc89a1bf

--- recidiviz/ingest/direct/regions/us_pa/ingest_views/view_supervision_staff_role_period.py
+++ recidiviz/ingest/direct/regions/us_pa/ingest_views/view_supervision_staff_role_period.py
@@ -17,14 +17,12 @@
 
 """Query containing supervision staff role period information.
 
-If a person was most recently included as a supervisor, have start_date as the first 
-date they appeared as a supervisor.
+Starts with handmade roster and uses roles from there to capture the most accurate info 
+possible for current employees. Fills in as many of the remaining officers' role info
+as possible by using the column they appear in in the agent history data (Agent_EmpNum vs. Supervisor_EmpNum)
+and PRL_AGNT_JOB_CLASSIFCTN in the supervision contacts data.
 
-If a person was most recently included as an officer, have start_date as the first date 
-they appeared as an officer.
-
-If a person appears in the roster but never appears in the case history data, they are
-currently excluded from the results of this view.
+There are overlapping periods for officers who appear as supervisors and agents concurrently. 
 """
 
 from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
@@ -34,114 +32,137 @@ from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_QUERY_TEMPLATE = """
-WITH officer_start_dates AS (
--- pull earliest dates from relagenthistory, assume all these people are still employed
-    SELECT 
-        agent_roster.Employ_Num,
-        MIN(agent_history.LastModifiedDateTime) as start_date,
-    FROM {RECIDIVIZ_REFERENCE_agent_districts} agent_roster
-    LEFT JOIN {dbo_RelAgentHistory@ALL} agent_history
-    ON (agent_roster.Employ_Num = agent_history.Agent_EmpNum)
-    WHERE NOT (UPPER(agent_roster.FirstName) LIKE '%VACANT%' AND UPPER(agent_roster.LastName) LIKE '%POSITION%')
-    GROUP BY 1
+WITH critical_dates_from_roster AS (
+    SELECT DISTINCT 
+        EmployeeID AS employeeid,
+        UPPER(Role) AS role,
+        CAST(update_datetime AS DATETIME) as edge_date,
+        MAX(CAST(roster.update_datetime AS DATETIME)) OVER (PARTITION BY TRUE) AS last_roster_update_datetime,
+        MAX(CAST(roster.update_datetime AS DATETIME)) OVER (PARTITION BY EmployeeID,role) AS last_appearance_date,
+    FROM {RECIDIVIZ_REFERENCE_staff_roster@ALL} roster
 ),
-supervisor_start_dates AS ( 
--- pull earliest dates from relagenthistory, assume all these people are still employed
-    SELECT 
-        agent_roster.Employ_Num,
-        MIN(agent_history.LastModifiedDateTime) as start_date,
-    FROM {RECIDIVIZ_REFERENCE_agent_districts} agent_roster
-    LEFT JOIN  {dbo_RelAgentHistory@ALL} agent_history
-    ON (agent_roster.Employ_Num = agent_history.Supervisor_EmpNum)
-    WHERE NOT (UPPER(agent_roster.FirstName) LIKE '%VACANT%' AND UPPER(agent_roster.LastName) LIKE '%POSITION%')
-    GROUP BY 1
+staff_from_roster AS (
+    SELECT * FROM (
+        SELECT DISTINCT 
+            EmployeeID AS employeeid,
+            UPPER(Role) AS role,
+            -- first time EmployeeID appeared in dbo_RelAgentHistory 
+            MIN(CAST(agent_history.LastModifiedDateTime AS DATETIME)) OVER person_window AS edge_date,  
+            MAX(CAST(roster.update_datetime AS DATETIME)) OVER (PARTITION BY TRUE) AS last_roster_update_datetime,
+            MAX(CAST(roster.update_datetime AS DATETIME)) OVER (PARTITION BY EmployeeID) AS last_appearance_date,
+        FROM {RECIDIVIZ_REFERENCE_staff_roster@ALL} roster
+        LEFT JOIN {dbo_RelAgentHistory@ALL} agent_history
+        ON(Agent_EmpNum = EmployeeID)
+        WHERE Role != 'Parole Spvr'
+        WINDOW person_window AS (PARTITION BY EmployeeID, Role ORDER BY roster.update_datetime)
+    ) agent_sub
+    -- person has a real start date
+    WHERE edge_date IS NOT NULL
+
+    UNION ALL 
+
+    SELECT * FROM (
+        SELECT DISTINCT 
+            EmployeeID AS employeeid,
+            UPPER(Role) AS role,
+            -- first time EmployeeID appeared in dbo_RelAgentHistory as a supervisor 
+            MIN(CAST(agent_history.LastModifiedDateTime AS DATETIME)) OVER person_window AS edge_date,
+            MAX(CAST(roster.update_datetime AS DATETIME)) OVER (PARTITION BY TRUE) AS last_roster_update_datetime,
+            MAX(CAST(roster.update_datetime AS DATETIME)) OVER (PARTITION BY EmployeeID) AS last_appearance_date,
+        FROM {RECIDIVIZ_REFERENCE_staff_roster@ALL} roster
+        LEFT JOIN {dbo_RelAgentHistory@ALL} agent_history
+        ON(agent_history.Supervisor_EmpNum = EmployeeID)
+        WHERE Role = 'Parole Spvr'
+        WINDOW person_window AS (PARTITION BY EmployeeID, Role ORDER BY roster.update_datetime)
+    ) spvr_sub
+    -- person has a real start date
+    WHERE edge_date IS NOT NULL
+
+    UNION ALL
+    -- to use as approximate edge dates when information can only be found in roster
+    SELECT * FROM critical_dates_from_roster
+),
+prelim_roster_periods AS (
+    SELECT DISTINCT 
+        employeeid,
+        role,
+        edge_date AS start_date,
+        CASE 
+        -- If there is a more recent entry for a staff member
+            WHEN LEAD(edge_date) OVER person_window IS NOT NULL THEN LEAD(edge_date) OVER person_window
+            -- If a staff member stops appearing in the roster, close their employment period
+            -- on the first date we receive a roster that does not include them
+            WHEN  LEAD(edge_date) OVER person_window IS NULL
+                AND last_appearance_date < last_roster_update_datetime THEN last_appearance_date
+            -- All currently-employed staff will appear in the latest roster
+            ELSE CAST(NULL AS DATETIME)    
+        END AS end_date
+    FROM staff_from_roster
+    WINDOW person_window AS (PARTITION BY EmployeeID, role ORDER BY edge_date)
+),
+staff_from_agent_history AS (
+    SELECT DISTINCT
+        Agent_EmpNum AS employeeid,
+        'SUPERVISION_OFFICER' AS role, 
+        -- first time EmployeeID appeared in dbo_RelAgentHistory
+        MIN(CAST(LastModifiedDateTime AS DATETIME)) AS start_date,
+        MAX(CAST(LastModifiedDateTime AS DATETIME)) AS end_date
+    FROM {dbo_RelAgentHistory@ALL} agent_history
+    WHERE AgentName NOT LIKE '%Vacant%'
+    AND Agent_EmpNum IS NOT NULL
+    AND Agent_EmpNum NOT IN (SELECT DISTINCT employeeid FROM staff_from_roster)
+    GROUP BY Agent_EmpNum
+),
+supervisors_from_agent_history AS (
+    -- can overlap with OFFICER role period for the same person
+    SELECT DISTINCT
+        Supervisor_EmpNum AS employeeid,
+        'SUPERVISION_OFFICER_SUPERVISOR' as role,
+        -- first time EmployeeID appeared in dbo_RelAgentHistory
+        MIN(CAST(LastModifiedDateTime AS DATETIME)) AS start_date,
+        MAX(CAST(LastModifiedDateTime AS DATETIME)) AS end_date
+    FROM {dbo_RelAgentHistory@ALL} agent_history
+    WHERE SupervisorName NOT LIKE '%Vacant%'
+    AND Supervisor_EmpNum IS NOT NULL
+    AND Supervisor_EmpNum NOT IN (SELECT DISTINCT employeeid FROM staff_from_roster)
+    GROUP BY Supervisor_EmpNum
+),
+staff_from_contacts AS (
+    -- Fill in role info for people who do not appear in dbo_RelAgentHistory
+    SELECT DISTINCT
+        PRL_AGNT_EMPL_NO AS employeeid,
+        UPPER(PRL_AGNT_JOB_CLASSIFCTN) as role,
+        -- first time EmployeeID appeared in dbo_PRS_FACT_PAROLEE_CNTC_SUMRY
+        MIN(CAST(START_DATE AS DATETIME)) AS start_date,
+        MAX(CAST(END_DATE AS DATETIME)) AS end_date
+    FROM {dbo_PRS_FACT_PAROLEE_CNTC_SUMRY} agent_history
+    WHERE PRL_AGNT_EMPL_NO IS NOT NULL
+    AND PRL_AGNT_EMPL_NO NOT IN (SELECT DISTINCT employeeid FROM staff_from_roster)
+    AND PRL_AGNT_EMPL_NO NOT IN (SELECT DISTINCT employeeid FROM staff_from_agent_history)
+    AND PRL_AGNT_EMPL_NO NOT IN (SELECT DISTINCT employeeid FROM supervisors_from_agent_history)
+    GROUP BY PRL_AGNT_EMPL_NO, PRL_AGNT_JOB_CLASSIFCTN
 ),
 all_periods AS (
-SELECT DISTINCT
-    officer_start_dates.Employ_Num,
-    supervisor_start_dates.start_date as sup_start,
-     -- if agent has been a supervisor AND an officer, use start date of most recent role
-    CASE WHEN
-        COALESCE(officer_start_dates.start_date, '1900-01-01') > COALESCE(supervisor_start_dates.start_date, '1900-01-01')
-        THEN officer_start_dates.start_date
-        ELSE supervisor_start_dates.start_date
-    END AS start_date,
-    CAST(NULL AS STRING) as end_date  -- assume people are still employed since they showed up in recent roster
-FROM officer_start_dates
-JOIN supervisor_start_dates
-USING(Employ_Num)
-),
-periods_with_subtypes AS (
-SELECT DISTINCT
-    Employ_Num, 
-    start_date,
-    end_date,
-    CASE 
-        WHEN start_date = all_periods.sup_start THEN 'SUPERVISION_OFFICER_SUPERVISOR'
-        WHEN start_date IS NULL THEN 'INTERNAL_UNKNOWN'
-        ELSE 'SUPERVISION_OFFICER'
-    END AS role_subtype,
-FROM all_periods
-WHERE start_date IS NOT NULL -- excludes people who appear in roster but never in case history
-),
-cntc_base AS (
-SELECT Employ_Num, start_date, end_date, role_subtype 
-FROM (
-    SELECT DISTINCT
-        PRL_AGNT_EMPL_NO as Employ_Num,
-        START_DATE AS start_date,
-        CAST(NULL AS STRING) AS end_date,
-        -- we cannot know whether these agents are still actively employed, so we assume they are.
-        UPPER(PRL_AGNT_JOB_CLASSIFCTN) AS role_subtype,
-        ROW_NUMBER() OVER (PARTITION BY PRL_AGNT_EMPL_NO, PRL_AGNT_JOB_CLASSIFCTN ORDER BY START_DATE) as rn
-    FROM {dbo_PRS_FACT_PAROLEE_CNTC_SUMRY} cntc
-    WHERE PRL_AGNT_EMPL_NO NOT IN (
-        SELECT DISTINCT Employ_Num
-        FROM periods_with_subtypes
-        )
-    AND PRL_AGNT_JOB_CLASSIFCTN IN ('Prl Agt 1', 'Prl Agt 2','Prl Supv','Prl Mgr 1')
-    ) cntc_with_row_num
-WHERE rn = 1
-),
-complete_base AS (
-SELECT DISTINCT
-    Employ_Num,
-    start_date,
-    end_date,
-    role_subtype,
-    1 as period_seq_num
-FROM periods_with_subtypes 
-
+SELECT DISTINCT * FROM prelim_roster_periods
+UNION ALL
+SELECT DISTINCT * FROM staff_from_agent_history
 UNION ALL 
-
-SELECT DISTINCT
-    *, 
-    ROW_NUMBER() OVER (
-        PARTITION BY cntc_base.Employ_Num 
-        ORDER BY cntc_base.start_date
-        ) AS period_seq_num
-    FROM cntc_base
+SELECT DISTINCT * FROM supervisors_from_agent_history
+UNION ALL
+SELECT DISTINCT * FROM staff_from_contacts
 )
 
-SELECT DISTINCT
-    COALESCE(complete_base.Employ_Num, sup.ext_id) AS Employ_Num, 
-    COALESCE(complete_base.start_date, '1900-01-01') AS start_date,
-    complete_base.end_date,
-    -- if a person supervises someone, even if they also appear in the leadership roster,
-    -- keep their role subtype as SUPERVISION_OFFICER_SUPERVISOR for product view compatibility.
-    COALESCE(complete_base.role_subtype,UPPER(sup.role)) AS role_subtype,
-    COALESCE(complete_base.period_seq_num, 1) as period_seq_num
-FROM complete_base 
-FULL OUTER JOIN {RECIDIVIZ_REFERENCE_field_supervisor_list} sup
-    ON(ext_id = Employ_Num)
-
+SELECT 
+    *, 
+    ROW_NUMBER() OVER (PARTITION BY EmployeeID ORDER BY start_date, role, end_date) AS period_seq_num
+FROM all_periods
 """
 
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
     region="us_pa",
     ingest_view_name="supervision_staff_role_period",
     view_query_template=VIEW_QUERY_TEMPLATE,
-    order_by_cols="Employ_Num",
+    order_by_cols="EmployeeID,period_seq_num",
 )
 
 if __name__ == "__main__":
