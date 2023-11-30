bdaf052d8f6d049ac2066a1702edad2b40a8031f
Maggie Hilderbran <maggie.hilderbran@me.com>
2023-11-30 13:04:37 -0500

    [US_OR] Create earned discharge eligibility stubs (Recidiviz/recidiviz-data#25749)
    ## Description of the change
    
    Created stubs in `analyst_data` for tracking eligibility for earned
    discharge in OR at the person-sentence-span level.
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#25502
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 4076dad829f750fcc1c7708db8369851a29d98d2

---
+++ recidiviz/calculator/query/state/views/analyst_data/us_or/us_or_served_6_months_supervision.py
@@ -0,0 +1,48 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Creates view to identify individuals' sentences for which they have served at least 6 months on supervision"""
+
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.calculator.query.state.dataset_config import ANALYST_VIEWS_DATASET
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+US_OR_SERVED_6_MONTHS_SUPERVISION_VIEW_NAME = "us_or_served_6_months_supervision"
+
+US_OR_SERVED_6_MONTHS_SUPERVISION_VIEW_DESCRIPTION = """Creates view to identify individuals' sentences for which they have served at least 6 months on supervision"""
+
+US_OR_SERVED_6_MONTHS_SUPERVISION_QUERY_TEMPLATE = """
+    SELECT person_id,
+        sentence_id,
+        DATE("9999-12-31") AS start_date,
+        DATE("9999-12-31") AS end_date,
+        NULL AS meets_criteria,
+    FROM `{project_id}.sessions.sentences_preprocessed_materialized`
+    LIMIT 0
+"""
+
+US_OR_SERVED_6_MONTHS_SUPERVISION_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=ANALYST_VIEWS_DATASET,
+    view_id=US_OR_SERVED_6_MONTHS_SUPERVISION_VIEW_NAME,
+    description=US_OR_SERVED_6_MONTHS_SUPERVISION_VIEW_DESCRIPTION,
+    view_query_template=US_OR_SERVED_6_MONTHS_SUPERVISION_QUERY_TEMPLATE,
+    should_materialize=False,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        US_OR_SERVED_6_MONTHS_SUPERVISION_VIEW_BUILDER.build_and_print()
