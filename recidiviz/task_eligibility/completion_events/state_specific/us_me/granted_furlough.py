b25048755226fa17505c70ca7993cde28b2c0387
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2023-12-05 12:06:36 -0600

    [US_ME] Change furlough completion events to include a new furlough dataset (Recidiviz/recidiviz-data#25735)
    ## Description of the change
    
    We started receiving a new data source for furloughs in ME. This new
    data source contains some furloughs that were not captured in
    `CIS_309_MOVEMENT`.
    
    In the following figure, `count_og` represents the furloughs captured in
    `CIS_309_MOVEMENT`, while count_new represents the ones from the new
    source. With this new addition, we will capture all furloughs we are
    aware of in the same `completion_event`.
    
    <img width="910" alt="Screenshot 2023-11-28 at 12 15 36 PM"
    src="https://github.com/Recidiviz/recidiviz-data/assets/107963194/69966094-af3d-4a3b-a224-85c9a4ccb07e">
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#23182
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 0ed78011f0c319315f7e4d0a6743eb09e6b10523

--- recidiviz/task_eligibility/completion_events/state_specific/us_me/granted_furlough.py
+++ recidiviz/task_eligibility/completion_events/state_specific/us_me/granted_furlough.py
@@ -14,7 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""Defines a view that shows furlough releases for clients in Maine.
+"""Defines a view that shows furlough releases for clients in Maine. It
+grabs information from two different tables: Furloughs_new and CIS_309_MOVEMENT_latest.
 """
 from recidiviz.calculator.query.state.dataset_config import NORMALIZED_STATE_DATASET
 from recidiviz.common.constants.states import StateCode
@@ -27,28 +28,53 @@ from recidiviz.task_eligibility.task_completion_event_big_query_view_builder imp
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
-_DESCRIPTION = """Defines a view that shows furlough releases for clients in Maine.
+_DESCRIPTION = """Defines a view that shows furlough releases for clients in Maine. It
+grabs information from two different tables: Furloughs_new and CIS_309_MOVEMENT_latest.
 """
-# TODO(#23182) Make sure were using the right dataset to catch furloughs
 
 _QUERY_TEMPLATE = """
+WITH furlough_table AS (
+  SELECT 
+    DISTINCT
+    MDOC AS external_id,
+    SAFE_CAST(LEFT(FurloughBeginDt, 10) AS DATE) AS completion_event_date,
+  FROM `{project_id}.{raw_data_up_to_date_views_dataset}.Furloughs_latest`
+  WHERE SAFE_CAST(LEFT(FurloughBeginDt, 10) AS DATE) IS NOT NULL
+),
+
+movements_table AS (
+  SELECT 
+    DISTINCT
+    mv.Cis_Client_Id AS external_id,
+    SAFE_CAST(LEFT(mv.Movement_Date, 10) AS DATE) AS completion_event_date,
+  FROM `{project_id}.{raw_data_up_to_date_views_dataset}.CIS_309_MOVEMENT_latest` mv
+  INNER JOIN `{project_id}.{raw_data_up_to_date_views_dataset}.CIS_3090_MOVEMENT_TYPE_latest` mvty
+    ON mv.Cis_3090_Movement_Type_Cd = mvty.Movement_Type_Cd
+      AND mvty.E_Movement_Type_Desc IN ('Furlough')
+      AND mv.Cis_3095_Mvmt_Direction_Cd = '2' # Movements out of facilities
+  INNER JOIN `{project_id}.{raw_data_up_to_date_views_dataset}.CIS_3093_MVMT_STATUS_latest` mvst
+    ON mv.Cis_3093_Mvmt_Status_Cd = mvst.Mvmt_Status_Cd
+      AND mvst.E_Mvmt_Status_Desc IN ('Complete')
+)
+
 SELECT 
+  DISTINCT
   peid.state_code,
   peid.person_id,
-  SAFE_CAST(LEFT(mv.Movement_Date, 10) AS DATE) AS completion_event_date,
-FROM `{project_id}.{raw_data_up_to_date_views_dataset}.CIS_309_MOVEMENT_latest` mv
-INNER JOIN `{project_id}.{raw_data_up_to_date_views_dataset}.CIS_3090_MOVEMENT_TYPE_latest` mvty
-  ON mv.Cis_3090_Movement_Type_Cd = mvty.Movement_Type_Cd
-    AND mvty.E_Movement_Type_Desc IN ('Furlough')
-    AND mv.Cis_3095_Mvmt_Direction_Cd = '2' # Movements out of facilities
-INNER JOIN `{project_id}.{raw_data_up_to_date_views_dataset}.CIS_3093_MVMT_STATUS_latest` mvst
-  ON mv.Cis_3093_Mvmt_Status_Cd = mvst.Mvmt_Status_Cd
-    AND mvst.E_Mvmt_Status_Desc IN ('Complete')
+  furloughs.completion_event_date,
+FROM (
+    SELECT *
+    FROM furlough_table
+
+    UNION ALL
+
+    SELECT *
+    FROM movements_table
+) furloughs
 INNER JOIN `{project_id}.{normalized_state_dataset}.state_person_external_id` peid
-  ON peid.external_id = mv.Cis_Client_Id
+  ON peid.external_id = furloughs.external_id
     AND peid.state_code = 'US_ME'
     AND id_type = 'US_ME_DOC'
-GROUP BY 1,2,3
 """
 
 VIEW_BUILDER: StateSpecificTaskCompletionEventBigQueryViewBuilder = (
