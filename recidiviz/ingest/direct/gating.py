1dfd70690287f1d4509cd1d62a1b426a52e97651
Emily Chao (they/them) <emily@recidiviz.org>
2023-12-06 11:13:15 -0800

    Launching US_OR to staging for ingest in Dataflow (Recidiviz/recidiviz-data#25980)
    ## Description of the change
    
    This adds US_OR to the list of states to be launched for Ingest in
    Dataflow in staging.
    
    ## Type of change
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#25236
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [X] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: d229836cd6607eaaba942cff01d794514a4ffbaf

--- recidiviz/ingest/direct/gating.py
+++ recidiviz/ingest/direct/gating.py
@@ -36,7 +36,7 @@ def is_ingest_in_dataflow_enabled(
     if environment.in_gcp_production():
         return False
 
-    staging_enabled_states = [StateCode.US_OZ, StateCode.US_CA]
+    staging_enabled_states = [StateCode.US_OZ, StateCode.US_CA, StateCode.US_OR]
     return state_code in staging_enabled_states
 
 
