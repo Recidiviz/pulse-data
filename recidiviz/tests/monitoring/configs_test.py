b4936c2b4e940bade384494b6e05f2c5d686aa38
Dan Hansen <dan@recidiviz.org>
2023-12-14 12:27:48 -0800

    [Monitoring] Fix export timeliness callback signature; validate instrument key length (Recidiviz/recidiviz-data#26247)
    ## Description of the change
    
    Observable Gauges accept one `CallbackOptions` argument for their
    callback. Some good news is that the synchronous gauge will be coming to
    `opentelemetry-python`
    [soon](https://github.com/open-telemetry/opentelemetry-python/pull/3462/files/afce221cebea1cef4376afab0c40aa84403308d3..185cc0d40f6d51c078b1825bab9db99192715c63).
    
    OpenTelemetry requires metric names to be less than 64 characters, so
    this updates some of our metric names to meet that requirement.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 0adb6f5b45766e45859307d426db885f8d02ef18

--- recidiviz/tests/monitoring/configs_test.py
+++ recidiviz/tests/monitoring/configs_test.py
@@ -18,11 +18,25 @@
 import unittest
 
 from recidiviz.monitoring.configs import MonitoringConfig
+from recidiviz.tests.utils.monitoring_test_utils import OTLMock
 
 
 class MonitoringConfigTest(unittest.TestCase):
     """Tests for configuration building / enums"""
 
+    def setUp(self) -> None:
+        self.otl_mock = OTLMock()
+        self.otl_mock.set_up()
+
+    def tearDown(self) -> None:
+        self.otl_mock.tear_down()
+
     def test_config_builds(self) -> None:
         monitoring_config = MonitoringConfig.build()
         self.assertIsNotNone(monitoring_config.instruments)
+
+    def test_all_instruments_created(self) -> None:
+        monitoring_config = MonitoringConfig.build()
+        for instrument_config in monitoring_config.instruments:
+            meter = self.otl_mock.meter_provider.get_meter("test_meter")
+            instrument_config.create_instrument(meter)
