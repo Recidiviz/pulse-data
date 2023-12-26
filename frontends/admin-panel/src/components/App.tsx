02d549293f29b27673976cf6f7abe8da51c0edb5
Dan Hansen <dan@recidiviz.org>
2023-12-26 12:53:23 -0600

    [Admin Panel] Fix environment switcher navigation (Recidiviz/recidiviz-data#26466)
    ## Description of the change
    
    Switching between environments would redirect to the ingest operations
    page as we were visiting `/admin/admin/...`
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#26169
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 2a77ee8ca294dae7ce3c466612ac10c276b3d2c0

--- frontends/admin-panel/src/components/App.tsx
+++ frontends/admin-panel/src/components/App.tsx
@@ -93,7 +93,7 @@ const ENVIRONMENT_OPTIONS: Map<EnvironmentType, EnvironmentOption> = new Map([
     "staging",
     {
       title: "Staging",
-      baseUrl: "https://admin-panel-staging.recidiviz.org/admin/",
+      baseUrl: "https://admin-panel-staging.recidiviz.org",
       backgroundColor: "#bf7474",
     },
   ],
@@ -101,7 +101,7 @@ const ENVIRONMENT_OPTIONS: Map<EnvironmentType, EnvironmentOption> = new Map([
     "production",
     {
       title: "Production",
-      baseUrl: "https://admin-panel-prod.recidiviz.org/admin/",
+      baseUrl: "https://admin-panel-prod.recidiviz.org",
       backgroundColor: "#90aeb5",
     },
   ],
