a4e5374c3c31bdae3d0b7e3d274628b6a4695d10
Emily Chao (they/them) <emily@recidiviz.org>
2024-01-05 16:37:12 -0800

    Launching US_ND to staging for ingest in Dataflow (Recidiviz/recidiviz-data#26636)
    ## Description of the change
    
    See title.
    
    ## Type of change
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#25634
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [X] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 83d5b8f740fc61d3299253c2134f210487fb02f0

--- recidiviz/ingest/direct/gating.py
+++ recidiviz/ingest/direct/gating.py
@@ -45,6 +45,7 @@ def is_ingest_in_dataflow_enabled(
         StateCode.US_IX,
         StateCode.US_MI,
         StateCode.US_PA,
+        StateCode.US_ND,
     ]
     return state_code in staging_enabled_states
 
@@ -54,8 +55,7 @@ def ingest_pipeline_can_run_in_dag(
     instance: DirectIngestInstance,  # pylint: disable=unused-argument
 ) -> bool:
     if (
-        state_code
-        in [StateCode.US_ME, StateCode.US_TN, StateCode.US_MO, StateCode.US_ND]
+        state_code in [StateCode.US_ME, StateCode.US_TN, StateCode.US_MO]
         and environment.in_gcp_production()
     ):
         return False
