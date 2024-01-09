033d831dce39449c84e0e58998496fecf478bc8d
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2024-01-09 14:18:59 -0800

    [Cleanup] Delete unused IAPHTTPRequestSensor and related code (Recidiviz/recidiviz-data#26711)
    ## Description of the change
    
    Deletes the unused `IAPHTTPRequestSensor`. In the past we have tried to
    call our own App Engine endpoints from our Airflow DAGs to do certain
    pieces of processing. This proved to be flaky and we have since
    established a better pattern where we call shared Python code from
    inside a Kubernetes pod instead, rather than hitting a web endpoint
    directly.
    
    This PR deletes the now unused `IAPHTTPRequestSensor` and underlying
    code which is no longer needed and caused some typing issues during
    Pipfile update this week.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    N/A
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 92fbabf2b17ff2442ada9ae6723e732a639d2814

--- recidiviz/cloud_functions/cloud_function_utils.py
+++ recidiviz/cloud_functions/cloud_function_utils.py
@@ -22,8 +22,7 @@ https://cloud.google.com/iap/docs/authentication-howto#iap_make_request-python
 """
 import json
 import logging
-import urllib.parse
-from typing import Any, List
+from typing import Any
 
 import google.auth
 import google.auth.app_engine
@@ -32,150 +31,12 @@ import google.auth.iam
 import google.oauth2.credentials
 import google.oauth2.service_account
 import requests
-from google.auth import crypt
 from google.auth.credentials import Credentials
 from google.auth.transport.requests import (  # type: ignore[attr-defined]
     AuthorizedSession,
-    Request,
 )
 
-IAP_CLIENT_ID = {
-    "recidiviz-staging": (
-        "984160736970-flbivauv2l7sccjsppe34p7436l6890m.apps.googleusercontent.com"
-    ),
-    "recidiviz-123": (
-        "688733534196-uol4tvqcb345md66joje9gfgm26ufqj6.apps.googleusercontent.com"
-    ),
-}
-
 GCP_PROJECT_ID_KEY = "GCP_PROJECT"
-
-_IAM_SCOPE = "https://www.googleapis.com/auth/iam"
-_OAUTH_TOKEN_URI = "https://www.googleapis.com/oauth2/v4/token"
-
-# pylint: disable=protected-access
-
-
-def make_iap_request(
-    url: str,
-    client_id: str,
-    method: str = "GET",
-    **kwargs: Any,
-) -> requests.Response:
-    """Makes a request to an application protected by Identity-Aware Proxy.
-
-    Args:
-      url: The Identity-Aware Proxy-protected URL to fetch.
-      client_id: The client ID used by Identity-Aware Proxy.
-      method: The request method to use
-              ('GET', 'OPTIONS', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE')
-      **kwargs: Any of the parameters defined for the request function:
-                https://github.com/requests/requests/blob/master/requests/api.py
-
-    Returns:
-      The page body, or raises an exception if the page couldn't be retrieved.
-    """
-    # Figure out what environment we're running in and get some preliminary
-    # information about the service account.
-    bootstrap_credentials, _ = google.auth.default(scopes=[_IAM_SCOPE])
-    if isinstance(bootstrap_credentials, google.oauth2.credentials.Credentials):
-        raise TypeError("make_iap_request is only supported for service accounts.")
-
-    # For service accounts using the Compute Engine metadata service,
-    # service_account_email isn't available until refresh is called.
-    bootstrap_credentials.refresh(Request())
-
-    signer_email = bootstrap_credentials.service_account_email
-    if isinstance(
-        bootstrap_credentials, google.auth.compute_engine.credentials.Credentials
-    ):
-        signer: crypt.Signer = google.auth.iam.Signer(
-            Request(), bootstrap_credentials, signer_email
-        )
-    else:
-        # A Signer object can sign a JWT using the service accounts key.
-        signer = bootstrap_credentials.signer
-
-    # Construct OAuth 2.0 service account credentials using the signer
-    # and email acquired from the bootstrap credentials.
-    service_account_credentials = google.oauth2.service_account.Credentials(
-        signer,
-        signer_email,
-        token_uri=_OAUTH_TOKEN_URI,
-        additional_claims={"target_audience": client_id},
-    )
-
-    # service_account_credentials gives us a JWT signed by the service
-    # account. Next, we use that to obtain an OpenID Connect token,
-    # which is a JWT signed by Google.
-    google_open_id_connect_token = get_google_open_id_connect_token(
-        service_account_credentials
-    )
-
-    # Fetch the Identity-Aware Proxy-protected URL, including an
-    # Authorization header containing "Bearer " followed by a
-    # Google-issued OpenID Connect token for the service account.
-    response = requests.request(
-        method,
-        url,
-        headers={"Authorization": f"Bearer {google_open_id_connect_token}"},
-        timeout=3600,
-        **kwargs,
-    )
-    if response.status_code == 403:
-        raise EnvironmentError(
-            f"Service account {signer_email} does not have permission to "
-            "access the IAP-protected application."
-        )
-    if response.status_code != 200:
-        raise RuntimeError(
-            f"Bad response from application: {repr(response.status_code)} / "
-            f"{repr(response.headers)} / {repr(response.text)}"
-        )
-    return response
-
-
-def get_google_open_id_connect_token(
-    service_account_credentials: google.oauth2.credentials.Credentials,
-) -> str:
-    """Get an OpenID Connect token issued by Google for the service account."""
-
-    service_account_jwt = (
-        service_account_credentials._make_authorization_grant_assertion()
-    )
-    request = Request()
-    body = {
-        "assertion": service_account_jwt,
-        "grant_type": google.oauth2._client._JWT_GRANT_TYPE,  # type: ignore
-    }
-    token_response = google.oauth2._client._token_endpoint_request(  # type: ignore
-        request, _OAUTH_TOKEN_URI, body
-    )
-    return token_response["id_token"]
-
-
-def build_query_param_string(
-    request_params: dict, accepted_query_params: List[str]
-) -> str:
-    """Given a dict of request params from the CF event JSON, it returns a query string for a URL endpoint for the
-    request params that are included in the `accepted_query_params` list.
-    If the param value is a list, it will add a query param for each value in the list.
-    If a request param key is not accepted by the endpoint, it will raise a KeyError.
-    """
-    query_tuples = []
-    for param_key, param_value in request_params.items():
-        if param_key not in accepted_query_params:
-            raise KeyError(
-                f"Unexpected key in request: [{param_key}]. "
-                f"Expected one of the following: {accepted_query_params}"
-            )
-        if isinstance(param_value, list):
-            for val in param_value:
-                query_tuples.append((param_key, val))
-        if isinstance(param_value, str):
-            query_tuples.append((param_key, param_value))
-    return f"?{urllib.parse.urlencode(query_tuples)}"
-
 
 # Following GCP best practices, these credentials should be
 # constructed at start-up time and used throughout
