cec18e325d233f530672e392a1e02dc02857a1a6
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-01-10 16:24:59 -0500

    [US_AZ] Ingest `StatePerson` (Recidiviz/recidiviz-data#26675)
    ## Description of the change
    
    ingests the `StatePerson` entity into the state db for AZ.
    
    tests:
    
    - [x] sandbox ingest pipeline runs successfully
    ([link](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-01-09_15_02_58-14841983447065344139;graphView=0?project=recidiviz-staging))
    - [x] confirmed ingest view is deterministic
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#25855
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 647f9d9f8d117d66535db0e1d1a96a6f6cd81834

--- recidiviz/ingest/direct/regions/us_az/us_az_custom_enum_parsers.py
+++ recidiviz/ingest/direct/regions/us_az/us_az_custom_enum_parsers.py
@@ -1,5 +1,5 @@
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2023 Recidiviz, Inc.
+# Copyright (C) 2024 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,3 +22,30 @@ my_enum_field:
     $raw_text: MY_CSV_COL
     $custom_parser: us_az_custom_enum_parsers.<function name>
 """
+from typing import Optional
+
+from recidiviz.common.constants.state.state_person import StateEthnicity
+
+
+def parse_ethnicity(
+    ## Some ethnicities are only included as a race, so we pipe the race field into this
+    ## parser to set ethnicities appropriately.
+    raw_text: str,
+) -> Optional[StateEthnicity]:
+    hispanic_options = [
+        "Mexican American",
+        "Mexican National",
+        "Cuban",
+        "Puerto Rican",
+        "Peru",
+        "Spain",
+        "Panama",
+        "Boliva",
+    ]
+    if raw_text:
+        ethnicity = raw_text.split("##")[0]
+        race = raw_text.split("##")[1]
+        if ethnicity in hispanic_options or race in hispanic_options:
+            return StateEthnicity.HISPANIC
+        return StateEthnicity.NOT_HISPANIC
+    return StateEthnicity.INTERNAL_UNKNOWN
