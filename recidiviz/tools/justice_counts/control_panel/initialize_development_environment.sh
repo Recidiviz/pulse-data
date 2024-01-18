9c4cef48927f80b7f4446f5ce306aaf4d42281e7
Brandon Hills <130382407+brandon-hills@users.noreply.github.com>
2024-01-18 18:46:33 -0500

    [Justice Counts] Write GH Action to test JC Docker Image creation. (Recidiviz/recidiviz-data#26547)
    ## Description of the change
    
    Write a GH Action to test that JC Docker image starts up successfully.
    
    We have a [Github Action
    workflow](https://github.com/Recidiviz/recidiviz-data/blob/main/.github/workflows/docker-test.yml)
    that runs on the main branch and tests the [main Recidiviz Docker
    image](https://github.com/Recidiviz/recidiviz-data/blob/main/Dockerfile)
    to make sure it starts successfully. We should extend this workflow to
    also test that the Justice Counts Docker image starts successfully.
    
    Context: Recidiviz/recidiviz-data#26214
    
    ## Test
    See this recent invocation of the GHA test. The Docker image was spun up
    successfully on a GH machine and passed the healthcheck.
    
    https://github.com/Recidiviz/recidiviz-data/actions/runs/7562619415/job/20593439098
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Dan Hansen <dan@recidiviz.org>
    GitOrigin-RevId: 0d144569f72768de4d94a55eb6692b67e5a2532b

--- recidiviz/tools/justice_counts/control_panel/initialize_development_environment.sh
+++ recidiviz/tools/justice_counts/control_panel/initialize_development_environment.sh
@@ -6,35 +6,26 @@ BASH_SOURCE_DIR=$(dirname "${BASH_SOURCE[0]}")
 # shellcheck source=recidiviz/tools/script_base.sh
 source "${BASH_SOURCE_DIR}/../../script_base.sh"
 
+function print_usage {
+    echo_error "usage: $0 [-t]"
+    echo_error "  -t: Write fake secrets instead of pulling from GSM. Used by GH Actions tests."
+    run_cmd exit 1
+}
+
+is_ci=false
+while getopts "t" flag; do
+    case "${flag}" in
+        t) is_ci=true;;
+        *) print_usage
+    esac
+done
+
 function write_to_file {
   echo "\"$1\" > $2" | indent_output
   echo "$1" > "$2"
 }
 
 run_cmd mkdir -p recidiviz/local/gsm/
-
-# Load staging Auth0 configuration. Uses subshell to remove additional output from gcloud util
-# This secret is used by the Control Panel frontend to access the backend
-AUTH0_CONFIGURATION=$(get_secret justice-counts-staging justice_counts_auth0)
-write_to_file "$AUTH0_CONFIGURATION" recidiviz/local/gsm/justice_counts_auth0
-
-# These secrets are used by the Admin Panel to access the Auth0 Management API and manage our users
-AUTH0_DOMAIN=$(get_secret justice-counts-staging justice_counts_auth0_api_domain)
-AUTH0_CLIENT_ID=$(get_secret justice-counts-staging justice_counts_auth0_api_client_id)
-AUTH0_CLIENT_SECRET=$(get_secret justice-counts-staging justice_counts_auth0_api_client_secret)
-write_to_file "$AUTH0_DOMAIN" recidiviz/local/gsm/justice_counts_auth0_api_domain
-write_to_file "$AUTH0_CLIENT_ID" recidiviz/local/gsm/justice_counts_auth0_api_client_id
-write_to_file "$AUTH0_CLIENT_SECRET" recidiviz/local/gsm/justice_counts_auth0_api_client_secret
-
-# These secrets are used by the `request_api` script which allows us to mock requests to our backend API
-AUTH0_M2M_CLIENT_ID=$(get_secret justice-counts-staging justice_counts_auth0_m2m_client_id)
-AUTH0_M2M_CLIENT_SECRET=$(get_secret justice-counts-staging justice_counts_auth0_m2m_client_secret)
-write_to_file "$AUTH0_M2M_CLIENT_ID" recidiviz/local/gsm/justice_counts_auth0_m2m_client_id
-write_to_file "$AUTH0_M2M_CLIENT_SECRET" recidiviz/local/gsm/justice_counts_auth0_m2m_client_secret
-
-# Load the Segment analytics public key so the Control Panel frontend can send analytics to the right destination
-SEGMENT_KEY=$(get_secret justice-counts-staging justice_counts_segment_key)
-write_to_file "$SEGMENT_KEY" recidiviz/local/gsm/justice_counts_segment_key
 
 # Database secrets
 write_to_file 'justice_counts' recidiviz/local/gsm/justice_counts_v2_cloudsql_instance_id
@@ -43,4 +34,31 @@ write_to_file 'justice_counts_user' recidiviz/local/gsm/justice_counts_v2_db_use
 write_to_file 'example' recidiviz/local/gsm/justice_counts_v2_db_password
 write_to_file '5432' recidiviz/local/gsm/justice_counts_v2_db_port
 
-write_to_file "$(python -c 'import uuid; print(uuid.uuid4().hex)')" recidiviz/local/gsm/justice_counts_secret_key
+# Do not fetch auth secrets if running from the CI pipeline.
+if [[ ! $is_ci ]]
+then
+  # Load staging Auth0 configuration. Uses subshell to remove additional output from gcloud util
+  # This secret is used by the Control Panel frontend to access the backend
+  AUTH0_CONFIGURATION=$(get_secret justice-counts-staging justice_counts_auth0)
+  write_to_file "$AUTH0_CONFIGURATION" recidiviz/local/gsm/justice_counts_auth0
+
+  # These secrets are used by the Admin Panel to access the Auth0 Management API and manage our users
+  AUTH0_DOMAIN=$(get_secret justice-counts-staging justice_counts_auth0_api_domain)
+  AUTH0_CLIENT_ID=$(get_secret justice-counts-staging justice_counts_auth0_api_client_id)
+  AUTH0_CLIENT_SECRET=$(get_secret justice-counts-staging justice_counts_auth0_api_client_secret)
+  write_to_file "$AUTH0_DOMAIN" recidiviz/local/gsm/justice_counts_auth0_api_domain
+  write_to_file "$AUTH0_CLIENT_ID" recidiviz/local/gsm/justice_counts_auth0_api_client_id
+  write_to_file "$AUTH0_CLIENT_SECRET" recidiviz/local/gsm/justice_counts_auth0_api_client_secret
+
+  # These secrets are used by the `request_api` script which allows us to mock requests to our backend API
+  AUTH0_M2M_CLIENT_ID=$(get_secret justice-counts-staging justice_counts_auth0_m2m_client_id)
+  AUTH0_M2M_CLIENT_SECRET=$(get_secret justice-counts-staging justice_counts_auth0_m2m_client_secret)
+  write_to_file "$AUTH0_M2M_CLIENT_ID" recidiviz/local/gsm/justice_counts_auth0_m2m_client_id
+  write_to_file "$AUTH0_M2M_CLIENT_SECRET" recidiviz/local/gsm/justice_counts_auth0_m2m_client_secret
+
+  # Load the Segment analytics public key so the Control Panel frontend can send analytics to the right destination
+  SEGMENT_KEY=$(get_secret justice-counts-staging justice_counts_segment_key)
+  write_to_file "$SEGMENT_KEY" recidiviz/local/gsm/justice_counts_segment_key
+
+  write_to_file "$(python -c 'import uuid; print(uuid.uuid4().hex)')" recidiviz/local/gsm/justice_counts_secret_key
+fi
