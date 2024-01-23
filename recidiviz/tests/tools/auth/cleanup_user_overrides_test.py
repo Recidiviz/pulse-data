582327328d37d9e58b9004c4479b5e81c8089d0c
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2024-01-22 16:08:45 -0800

    script to remove user overrides that match roster (Recidiviz/recidiviz-data#26979)
    ## Description of the change
    
    Now that MI rosters are syncing into the Roster table, there are many
    users who have entries in UserOverride (usually because they were added
    with the "add user" button) who don't need those entries. This adds a
    script to clean these up. Eventually it probably makes sense to add this
    logic into the endpoint itself to run after a roster syncs, but it
    seemed like it made more sense to have as a tool first so we can be
    aware when it gets run.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes https://github.com/Recidiviz/recidiviz-data/issues/25759
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 7698734ee0421ddd00df6907bae7363708ac18d0

---
+++ recidiviz/tests/tools/auth/cleanup_user_overrides_test.py
@@ -0,0 +1,314 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Implements tests for the script that cleans up extra data in the UserOverride table."""
+
+from unittest import TestCase
+
+import pytest
+from sqlalchemy import select
+
+from recidiviz.persistence.database.schema.case_triage.schema import UserOverride
+from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.persistence.database.session_factory import SessionFactory
+from recidiviz.persistence.database.sqlalchemy_database_key import SQLAlchemyDatabaseKey
+from recidiviz.persistence.database.sqlalchemy_engine_manager import (
+    SQLAlchemyEngineManager,
+)
+from recidiviz.tests.auth.helpers import (
+    generate_fake_rosters,
+    generate_fake_user_overrides,
+)
+from recidiviz.tools.auth.cleanup_user_overrides import cleanup_user_overrides
+from recidiviz.tools.postgres import local_persistence_helpers, local_postgres_helpers
+
+
+@pytest.mark.uses_db
+class CleanupUserOverridesTest(TestCase):
+    """Implements tests for the script that cleans up extra data in the UserOverride table."""
+
+    # Stores the location of the postgres DB for this test run
+    temp_db_dir: str | None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.temp_db_dir = local_postgres_helpers.start_on_disk_postgresql_database()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        local_postgres_helpers.stop_and_clear_on_disk_postgresql_database(
+            cls.temp_db_dir
+        )
+
+    def setUp(self) -> None:
+        self.database_key = SQLAlchemyDatabaseKey.for_schema(SchemaType.CASE_TRIAGE)
+        self.overridden_env_vars = (
+            local_persistence_helpers.update_local_sqlalchemy_postgres_env_vars()
+        )
+        engine = SQLAlchemyEngineManager.init_engine_for_postgres_instance(
+            database_key=self.database_key,
+            db_url=local_postgres_helpers.on_disk_postgres_db_url(),
+        )
+
+        self.database_key.declarative_meta.metadata.create_all(engine)
+        self.roster_user = generate_fake_rosters(
+            email="parameter@domain.org",
+            region_code="US_XX",
+            external_id="123",
+            role="supervision_officer",
+            district="D1",
+            first_name="Test",
+            last_name="User",
+            pseudonymized_id="pseudo-123",
+        )
+        self.session = SessionFactory.using_database(self.database_key).__enter__()
+        self.session.add(self.roster_user)
+
+    def tearDown(self) -> None:
+        self.session.__exit__(None, None, None)
+        local_postgres_helpers.restore_local_env_vars(self.overridden_env_vars)
+        local_persistence_helpers.teardown_on_disk_postgresql_database(
+            self.database_key
+        )
+
+    def test_same_external_id_set_to_null(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                external_id=self.roster_user.external_id,
+                # add one different attribute so it doesn't get deleted
+                district="different district",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user.external_id)
+
+    def test_same_district_set_to_null(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                district=self.roster_user.district,
+                # add one different attribute so it doesn't get deleted
+                external_id="different",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user.district)
+
+    def test_same_first_name_set_to_null(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                first_name=self.roster_user.first_name,
+                # add one different attribute so it doesn't get deleted
+                district="different",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user.first_name)
+
+    def test_same_first_name_different_case_set_to_null(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                first_name=self.roster_user.first_name.upper(),
+                # add one different attribute so it doesn't get deleted
+                district="different",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user.first_name)
+
+    def test_same_last_name_set_to_null(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                last_name=self.roster_user.last_name,
+                # add one different attribute so it doesn't get deleted
+                district="different",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user.last_name)
+
+    def test_same_last_name_different_case_set_to_null(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                last_name=self.roster_user.last_name.lower(),
+                # add one different attribute so it doesn't get deleted
+                district="different",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user.last_name)
+
+    def test_same_role_set_to_null(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                role=self.roster_user.role,
+                # add one different attribute so it doesn't get deleted
+                district="different",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user.role)
+
+    def test_equivalent_supervision_staff_role_set_to_null(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                role="supervision_staff",
+                # add one different attribute so it doesn't get deleted
+                district="different",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user.role)
+
+    def test_equivalent_leadership_role_set_to_null(self) -> None:
+        self.roster_user.role = "supervision_leadership"
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                role="leadership_role",
+                # add one different attribute so it doesn't get deleted
+                district="different",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user.role)
+
+    def test_non_equivalent_role_does_not_change(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                role="leadership_role",
+                # add one different attribute so it doesn't get deleted
+                district="different",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertEqual(modified_user.role, "leadership_role")
+
+    def test_delete_user_all_null_attributes(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user)
+
+    def test_delete_user_matching_attributes(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                district=self.roster_user.district,
+                role="supervision_staff",
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNone(modified_user)
+
+    def test_blocked_user_all_null_attributes_is_not_deleted(self) -> None:
+        self.session.add(
+            generate_fake_user_overrides(
+                email=self.roster_user.email_address,
+                region_code=self.roster_user.state_code,
+                blocked=True,
+            )
+        )
+        cleanup_user_overrides(self.session, dry_run=False)
+        modified_user = self.session.execute(
+            select(UserOverride).where(
+                UserOverride.email_address == self.roster_user.email_address
+            )
+        ).scalar()
+        self.assertIsNotNone(modified_user)
---
+++ recidiviz/tools/auth/cleanup_user_overrides.py
@@ -0,0 +1,189 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Tool for removing entries from the UserOverride table where the data is equivalent to a row in the
+Roster table. If a single column is equivalent, it sets it to null in the override. If all relevant
+columns are equivalent, it removes the entry.
+
+The script can be run against a local database or against one running in Cloud SQL.
+
+Usage against default development database (docker-compose v1) after `docker-compose up` has been
+run: docker exec pulse-data_case_triage_backend_1 pipenv run python -m
+recidiviz.tools.auth.cleanup_user_overrides
+
+Usage against default development database (docker-compose v2) after `docker-compose up` has been
+run: docker exec pulse-data-case_triage_backend-1 pipenv run python -m
+recidiviz.tools.auth.cleanup_user_overrides
+
+To run against Cloud SQL, specify the project id: python -m
+recidiviz.tools.auth.cleanup_user_overrides --project_id recidiviz-staging
+
+The tool can also be run in dry-run mode to see what would happen before making actual changes to
+the database: python -m recidiviz.tools.auth.cleanup_user_overrides --project_id recidiviz-staging
+--dry_run
+"""
+
+import argparse
+import logging
+import sys
+
+from sqlalchemy import and_, delete, or_, select, update
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.elements import BinaryExpression
+
+from recidiviz.persistence.database.schema.case_triage.schema import (
+    Roster,
+    UserOverride,
+)
+from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.persistence.database.session_factory import SessionFactory
+from recidiviz.persistence.database.sqlalchemy_database_key import SQLAlchemyDatabaseKey
+from recidiviz.persistence.database.sqlalchemy_engine_manager import (
+    SQLAlchemyEngineManager,
+)
+from recidiviz.tools.postgres.cloudsql_proxy_control import cloudsql_proxy_control
+from recidiviz.utils.environment import (
+    GCP_PROJECT_PRODUCTION,
+    GCP_PROJECT_STAGING,
+    in_development,
+)
+from recidiviz.utils.metadata import local_project_id_override
+
+_COLUMN_TO_COMPARISON_CLAUSE: dict[str, BinaryExpression] = {
+    "external_id": Roster.external_id == UserOverride.external_id,
+    "district": Roster.district == UserOverride.district,
+    "first_name": Roster.first_name.ilike(UserOverride.first_name),
+    "last_name": Roster.last_name.ilike(UserOverride.last_name),
+    "role": or_(
+        Roster.role == UserOverride.role,
+        and_(
+            Roster.role.in_(["supervision_officer", "supervision_officer_supervisor"]),
+            UserOverride.role == "supervision_staff",
+        ),
+        and_(
+            Roster.role.in_(["supervision_leadership", "state_leadership"]),
+            UserOverride.role == "leadership_role",
+        ),
+    ),
+}
+
+
+def _run_update_stmt(
+    session: Session, dry_run: bool, key: str, clause: BinaryExpression
+) -> None:
+    where_clause = and_(
+        Roster.email_address == UserOverride.email_address,
+        clause,
+    )
+    if dry_run:
+        results = session.execute(select(UserOverride).where(where_clause)).scalars()
+        users_to_modify = [user.email_address for user in results]
+        logging.info(
+            "[DRY RUN] would set %s to null for %d rows: %s",
+            key,
+            len(users_to_modify),
+            users_to_modify,
+        )
+    else:
+        results = session.execute(
+            update(UserOverride)
+            .where(where_clause)
+            .values(**{key: None})
+            .execution_options(synchronize_session="fetch")
+        )
+        logging.info("set %s to null for %d rows", key, results.rowcount)
+
+
+def cleanup_user_overrides(session: Session, dry_run: bool) -> None:
+    """Clean up the overrides. First, update equivalent columns to null. Then, delete any entries
+    where all relevant columns are null."""
+    for column, comparison in _COLUMN_TO_COMPARISON_CLAUSE.items():
+        _run_update_stmt(session, dry_run, column, comparison)
+
+    if dry_run:
+        similar_clauses = [
+            or_(getattr(UserOverride, column).is_(None), clause)
+            for column, clause in _COLUMN_TO_COMPARISON_CLAUSE.items()
+        ]
+        results = session.execute(
+            select(UserOverride)
+            .join(Roster, UserOverride.email_address == Roster.email_address)
+            .where(and_(*similar_clauses, UserOverride.blocked.is_not(True)))
+            .order_by(UserOverride.email_address)
+        ).scalars()
+        users_to_delete = [user.email_address for user in results]
+        logging.info(
+            "[DRY RUN] would delete %d rows: %s", len(users_to_delete), users_to_delete
+        )
+    else:
+        results = session.execute(
+            delete(UserOverride)
+            .where(
+                and_(
+                    *[
+                        getattr(UserOverride, column).is_(None)
+                        for column in _COLUMN_TO_COMPARISON_CLAUSE
+                    ],
+                    # Blocked users are kept track of in User Overrides, so a blocked user can still
+                    # have all other attributes be null
+                    UserOverride.blocked.is_not(True)
+                )
+            )
+            .execution_options(synchronize_session="fetch")
+        )
+        logging.info("deleted %d rows", results.rowcount)
+
+
+def parse_arguments(argv: list[str]) -> tuple[argparse.Namespace, list[str]]:
+    """Parses the arguments needed to call the cleanup_user_overrides function."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--project_id",
+        dest="project_id",
+        type=str,
+        choices=[GCP_PROJECT_STAGING, GCP_PROJECT_PRODUCTION],
+        required=False,
+    )
+
+    parser.add_argument("--dry_run", dest="dry_run", action="store_true")
+
+    return parser.parse_known_args(argv)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    known_args, _ = parse_arguments(sys.argv)
+
+    db_key = SQLAlchemyDatabaseKey.for_schema(SchemaType.CASE_TRIAGE)
+    if known_args.project_id:
+        with local_project_id_override(
+            known_args.project_id
+        ), cloudsql_proxy_control.connection(
+            schema_type=SchemaType.CASE_TRIAGE,
+        ), SessionFactory.for_proxy(
+            db_key
+        ) as global_session:
+            cleanup_user_overrides(global_session, known_args.dry_run)
+    else:
+        if not in_development():
+            raise RuntimeError(
+                "Expected to be called inside a docker container or with the --project_id argument. See usage in docstring"
+            )
+        SQLAlchemyEngineManager.init_engine(db_key)
+        with SessionFactory.using_database(db_key) as global_session:
+            cleanup_user_overrides(global_session, known_args.dry_run)
