c756ffe0db861e8cbf3bc85de0238e2f525a2da0
Brandon Hills <130382407+brandon-hills@users.noreply.github.com>
2024-01-29 13:15:51 -0500

    [Justice Counts] Add script to backfill missing reports from the recurring-report-creation failure. (Recidiviz/recidiviz-data#27123)
    ## Description of the change
    
    Create a new script to backfill the missing reports from the
    recurring-report-creation failure.
    
    Context Recidiviz/recidiviz-data#26815
    
    This PR extracts out duplicate logic from `create_new_reports` into a
    util file and uses that function for both the `backfill_missing_reports`
    and `create_new_reports` scripts.
    
    Usage looks like `python -m
    recidiviz.justice_counts.jobs.backfill_missing_reports --start_month {}
    --end_month_inclusive {} --year {}`.
    Backfill data for all months between `start_month`/`year` and
    `end_month_inclusive`/`year`. Represent month as integer with January as
    1 and December as 12.
    
    Note, we only support one year input (instead of spanning across
    multiple years) because the use case does not require it. We won't
    extend the logic to support that for the sake of simplicity, unless it
    is required in the future by another recurring-report-creation failure.
    
    Once this PR is submitted, we will add the `backfill_missing_reports`
    job to GCP and run it in staging (and then in prod) to fill in the
    missing entries from October+November 2023.
    
    ## Testing
    
    1. Locally tested that the new `backfill_missing_reports` method creates
    new reports in the correct time frame. Ran the script
    `
    docker exec recidiviz-data-control_panel_backend-1 pipenv run python -m
    recidiviz.justice_counts.jobs.backfill_missing_reports --start_month 1
    --end_month_inclusive 12 --year 1990
    `
    <img width="1703" alt="Screenshot 2024-01-25 at 4 32 51 PM"
    src="https://github.com/Recidiviz/recidiviz-data/assets/130382407/0d6022a0-8d52-4d98-8ff3-5c09b0621a8f">
    
    2. Locally tested that the `create_new_reports` script still operates
    properly after the refactor.
    <img width="1717" alt="Screenshot 2024-01-25 at 4 31 36 PM"
    src="https://github.com/Recidiviz/recidiviz-data/assets/130382407/eff4583b-ae28-4ea9-ba7e-b3aa5ad010bf">
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6d3a1a80e1f90822256847e383a2dbd31ae2d775

---
+++ recidiviz/justice_counts/jobs/backfill_missing_reports.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Script for backfilling missing Reports between the month ranges for each Justice Counts Agency.
+Missing reports are created at the first of every month. However, if that job fails for
+a period of time, this script can be used to backfill.
+
+Local Usage: docker exec pulse-data-control_panel_backend-1 pipenv run python -m recidiviz.justice_counts.jobs.backfill_missing_reports --start_month {} --end_month_inclusive {} --year {}
+Remote Usage: Execute the `justice-counts-backfill-missing-reports` Cloud Run Job
+"""
+
+import argparse
+import logging
+import sys
+from typing import List, Tuple
+
+import sentry_sdk
+from sqlalchemy.engine import Engine
+
+from recidiviz.justice_counts.utils.constants import JUSTICE_COUNTS_SENTRY_DSN
+from recidiviz.justice_counts.utils.create_new_report import create_new_report
+from recidiviz.persistence.database.constants import JUSTICE_COUNTS_DB_SECRET_PREFIX
+from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.persistence.database.session import Session
+from recidiviz.persistence.database.sqlalchemy_database_key import SQLAlchemyDatabaseKey
+from recidiviz.persistence.database.sqlalchemy_engine_manager import (
+    SQLAlchemyEngineManager,
+)
+
+logger = logging.getLogger(__name__)
+sentry_sdk.init(
+    dsn=JUSTICE_COUNTS_SENTRY_DSN,
+    # Enable performance monitoring
+    enable_tracing=True,
+)
+
+
+def parse_arguments(argv: List[str]) -> Tuple[argparse.Namespace, List[str]]:
+    """Parses the required arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--start_month",
+        type=int,
+        help="Backfill data for all months between `start_month`/`year` and `end_month_inclusive`/`year`. Represent month as integer with January as 1 and December as 12.",
+        required=True,
+    )
+    parser.add_argument(
+        "--end_month_inclusive",
+        type=int,
+        help="Backfill data for all months between `start_month`/`year` and `end_month_inclusive`/`year`. Represent month as integer with January as 1 and December as 12.",
+        required=True,
+    )
+    parser.add_argument(
+        "--year",
+        type=int,
+        help="Backfill data for all months between `start_month`/`year` and `end_month_inclusive`/`year`. Represent month as integer with January as 1 and December as 12.",
+        required=True,
+    )
+    return parser.parse_known_args(argv)
+
+
+def validate_input(start_month: int, end_month_inclusive: int) -> None:
+    """Validate month input ranges."""
+    if start_month < 1:
+        raise ValueError("start_month must be greater than 1.")
+    if end_month_inclusive > 12:
+        raise ValueError("end_month_inclusive must be less than or equal to 12.")
+    if start_month > end_month_inclusive:
+        raise ValueError(
+            "start_month must be less than or equal to end_month_inclusive."
+        )
+
+
+def create_new_reports(
+    engine: Engine, start_month: int, end_month_inclusive: int, year: int
+) -> None:
+    """
+    For each month in the month range [start_month, end_month_inclusive] for `year`,
+    create a new report if the previous month is missing.
+    """
+    session = Session(bind=engine)
+    logger.info("Generating new Reports in database %s", engine.url)
+    for month in range(start_month, end_month_inclusive + 1):
+        create_new_report(month, year, session, logger)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    known_args, _ = parse_arguments(sys.argv)
+    validate_input(known_args.start_month, known_args.end_month_inclusive)
+
+    database_key = SQLAlchemyDatabaseKey.for_schema(
+        SchemaType.JUSTICE_COUNTS,
+    )
+    justice_counts_engine = SQLAlchemyEngineManager.init_engine(
+        database_key=database_key,
+        secret_prefix_override=JUSTICE_COUNTS_DB_SECRET_PREFIX,
+    )
+    create_new_reports(
+        justice_counts_engine,
+        known_args.start_month,
+        known_args.end_month_inclusive,
+        known_args.year,
+    )
---
+++ recidiviz/justice_counts/utils/create_new_report.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Utilities for creating new reports."""
+
+import logging
+from typing import Any
+
+from recidiviz.justice_counts.datapoint import DatapointInterface
+from recidiviz.justice_counts.report import ReportInterface
+from recidiviz.persistence.database.schema.justice_counts import schema
+
+
+def create_new_report(
+    month: int, year: int, session: Any, logger: logging.Logger
+) -> None:
+    """For each agency, this function checks if a monthly report exists for the previous
+    month/year. If the monthly report already exists, nothing is done. If the
+    monthly report does not exist, one is created.
+
+    Additionally, for each agency, this function checks if an annual report exists for
+    the most recent previous month/year. If the annual report already exists, nothing is done. If
+    the annual report does not exists AND it is specified month to create an annual report,
+    an annual report is created."""
+
+    # Calculate most recent previous month and year
+    if month > 1:
+        previous_month = month - 1
+    else:
+        previous_month = 12
+    previous_year = year - 1
+
+    for agency in session.query(schema.Agency).filter(
+        # Skip child agencies -- safe to assume their reports will be
+        # created via bulk upload
+        schema.Agency.super_agency_id.is_(None)
+    ):
+        logger.info("Generating Reports for Agency %s", agency.name)
+
+        agency_datapoints = DatapointInterface.get_agency_datapoints(
+            session=session, agency_id=agency.id
+        )
+        metric_key_to_datapoints = DatapointInterface.build_metric_key_to_datapoints(
+            agency_datapoints
+        )
+
+        (
+            monthly_report,
+            yearly_report,
+            monthly_metric_defs,
+            annual_metric_defs,
+        ) = ReportInterface.create_new_reports(
+            session=session,
+            agency_id=agency.id,
+            user_account_id=None,
+            current_month=month,
+            current_year=year,
+            previous_month=previous_month,
+            previous_year=previous_year,
+            systems={schema.System[sys] for sys in agency.systems},
+            metric_key_to_datapoints=metric_key_to_datapoints,
+        )
+
+        if monthly_report is not None:
+            logger.info(
+                "Generated Monthly Report for Agency %s, Month %s, Year %s",
+                agency.name,
+                previous_month,
+                previous_year if previous_month == 12 else year,
+            )
+        elif monthly_report is None and len(monthly_metric_defs) > 0:
+            logger.info(
+                "Monthly Report for Agency %s, Month %s, Year %s already exists.",
+                agency.name,
+                previous_month,
+                previous_year if previous_month == 12 else year,
+            )
+        elif monthly_report is None and len(monthly_metric_defs) == 0:
+            logger.info(
+                "No metrics are included in Monthly Report for Agency %s, Month %s, Year %s.",
+                agency.name,
+                previous_month,
+                previous_year if previous_month == 12 else year,
+            )
+
+        if yearly_report is not None:
+            logger.info(
+                "Generated Annual Report for Agency %s, Month %s, Year %s",
+                agency.name,
+                month,
+                previous_year,
+            )
+        elif yearly_report is None and len(annual_metric_defs) > 0:
+            logger.info(
+                "Annual Report for Agency %s, Month %s, Year %s already exists.",
+                agency.name,
+                month,
+                previous_year,
+            )
+        elif yearly_report is None and len(annual_metric_defs) == 0:
+            logger.info(
+                "No metrics are included in Annual Report for Agency %s, Month %s, Year %s.",
+                agency.name,
+                month,
+                previous_year,
+            )
