7d376406fb44ba2dd0b1c0c0b5d780ba103ac167
Santy Mendoza <55068232+santymendoza@users.noreply.github.com>
2024-02-01 15:31:46 -0800

    [US_AZ] State sentence ingest view (Recidiviz/recidiviz-data#27168)
    ## Description of the change
    
    We are ingesting the new entity state_sentence for the state of Arizona
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#26270
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6e329c3e1fb7716fb29a699e50397adffdc0f148

---
+++ recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_sentence.py
@@ -0,0 +1,62 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query for state sentences."""
+from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
+    DirectIngestViewQueryBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+    SELECT
+        off.OFFENSE_ID as external_id,
+        CASE
+            WHEN life.DESCRIPTION IN ('Life (35 to Life)', 'Life (25 to Life)', 'Life', 'Natural Life')
+            THEN 1
+            ELSE 0
+        END AS is_life,
+        CASE
+            WHEN life.DESCRIPTION = 'Death'
+            THEN 1
+            ELSE 0
+        END AS is_capital,
+        CASE
+            WHEN parole.DESCRIPTION = 'Yes'
+            THEN 1
+            ELSE 0
+        END AS parole_possible,
+        commit.SENTENCED_DTM AS imposed_date,
+        county.DESCRIPTION AS county_code,
+        doc.PERSON_ID as person_id
+    FROM {AZ_DOC_SC_OFFENSE} off
+    LEFT JOIN {LOOKUPS} life ON life.LOOKUP_ID = off.LIFE_OR_DEATH_ID
+    LEFT JOIN {LOOKUPS} parole ON parole.LOOKUP_ID = off.PAROLE_ELIGIBILITY_ID
+    LEFT JOIN {AZ_DOC_SC_COMMITMENT} commit ON off.COMMITMENT_ID = commit.COMMITMENT_ID
+    LEFT JOIN {AZ_DOC_SC_EPISODE} sc_episode ON commit.SC_EPISODE_ID = sc_episode.SC_EPISODE_ID
+    LEFT JOIN {DOC_EPISODE} doc ON sc_episode.DOC_ID = doc.DOC_ID
+    LEFT JOIN {LOOKUPS} county ON county.LOOKUP_ID = commit.COUNTY_ID
+"""
+VIEW_BUILDER = DirectIngestViewQueryBuilder(
+    region="us_az",
+    ingest_view_name="state_sentence",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+    order_by_cols="external_id",
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
