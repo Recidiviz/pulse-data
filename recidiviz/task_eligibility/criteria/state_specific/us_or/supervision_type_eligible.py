2957bf3feb3d3781fe094a9e637bbfe81b2d9ef0
Maggie Hilderbran <maggie.hilderbran@me.com>
2024-02-05 10:51:54 -0500

    [US_OR] Exclude clients under ineligible supervision types from earned discharge (Recidiviz/recidiviz-data#26965)
    ## Description of the change
    
    Added a TES criterion for OR earned discharge that assesses whether a
    client is eligible based on their type of supervision.
    
    For reference, we've learned from OR that the following individuals
    should be excluded from eligibility for earned discharge:
    * folks on CD or DV status (haven't been convicted yet—sentenced to
    supervision and won't be convicted if supervision is completed
    successfully);
    * Second Look (SL) juveniles;
    * and clients on parole (PA) or post-parole (PS) status.
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#26680
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 18ad1534d8906b6549805d27e2c8365243fc54a0

---
+++ recidiviz/task_eligibility/criteria/state_specific/us_or/supervision_type_eligible.py
@@ -0,0 +1,84 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""Excludes individuals under ineligible supervision types from eligibility for OR earned discharge"""
+
+from recidiviz.calculator.query.bq_utils import list_to_query_string
+from recidiviz.calculator.query.sessions_query_fragments import (
+    create_sub_sessions_with_attributes,
+)
+from recidiviz.calculator.query.state.dataset_config import NORMALIZED_STATE_DATASET
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.us_or_query_fragments import (
+    OR_EARNED_DISCHARGE_INELIGIBLE_SUPERVISION_TYPES,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "US_OR_SUPERVISION_TYPE_ELIGIBLE"
+
+_DESCRIPTION = """Excludes individuals under ineligible supervision types from eligibility for OR earned discharge"""
+
+# TODO(#27324): If we change how we ingest sup. types, we may need to revise this
+# criterion query.
+# TODO(#27325): Try to break this criterion up into multiple reusable criteria (rather than a
+# single, highly-specific-to-EDIS criterion).
+_QUERY_TEMPLATE = f"""
+    WITH supervision_type_spans AS (
+        SELECT
+            state_code,
+            person_id,
+            start_date,
+            termination_date AS end_date,
+            supervision_type_raw_text,
+            (supervision_type_raw_text NOT IN ({list_to_query_string(
+                OR_EARNED_DISCHARGE_INELIGIBLE_SUPERVISION_TYPES, quoted=True
+                )})) AS is_eligible
+        FROM `{{project_id}}.{{normalized_state_dataset}}.state_supervision_period`
+        WHERE state_code='US_OR'
+    ),
+    /* Here, we sub-sessionize to handle overlapping supervision periods, because there
+    are some cases where there are overlapping periods (even with different supervision
+    types) recorded for an individual. These typically appear to be single-day periods
+    that start/end on the same day as another period. */
+    {create_sub_sessions_with_attributes("supervision_type_spans")}
+    SELECT
+        state_code,
+        person_id,
+        start_date,
+        end_date,
+        LOGICAL_AND(is_eligible) AS meets_criteria,
+        TO_JSON(STRUCT(ARRAY_AGG(DISTINCT supervision_type_raw_text ORDER BY supervision_type_raw_text) AS supervision_type)) AS reason
+    FROM sub_sessions_with_attributes
+    GROUP BY 1, 2, 3, 4
+"""
+
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
+    StateSpecificTaskCriteriaBigQueryViewBuilder(
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+        state_code=StateCode.US_OR,
+        criteria_spans_query_template=_QUERY_TEMPLATE,
+        normalized_state_dataset=NORMALIZED_STATE_DATASET,
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
