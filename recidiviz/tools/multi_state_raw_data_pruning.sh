5608e1d76f2ac6967098b1b3f2734eb95def2060
ethanoro <ethan@recidiviz.org>
2024-02-09 10:34:36 -0500

    [pruning] add bash exit when command fails (Recidiviz/recidiviz-data#27438)
    ## Description of the change
    
    When doing raw data pruning, the bash script will not hard fail when
    there is an error during the execution of the python command (like
    docker not running, etc). This PR makes it such that any non-zero exit
    code will cause the bash script to crash hard.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | **Type: Bug** | **non-breaking change that fixes an issue** |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#27310
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: c2e301e27f6123b48b2de3a6f2a37d0d74c08a16

--- recidiviz/tools/multi_state_raw_data_pruning.sh
+++ recidiviz/tools/multi_state_raw_data_pruning.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #  Usage: bash recidiviz/tools/multi_state_raw_data_pruning.sh
+BASH_SOURCE_DIR=$(dirname "${BASH_SOURCE[0]}")
+# shellcheck source=recidiviz/tools/script_base.sh
+source "${BASH_SOURCE_DIR}/script_base.sh"
 
 declare -a states=("US_TN" "US_MI" "US_ND")
 
 for state in "${states[@]}"
 do
   echo "Starting raw data pruning in STAGING for $state"
-  python -m recidiviz.tools.ingest.one_offs.clear_redundant_raw_data_on_bq --dry-run False --project-id=recidiviz-staging --state-code="$state"
+  python -m recidiviz.tools.ingest.one_offs.clear_redundant_raw_data_on_bq --dry-run False --project-id=recidiviz-staging --state-code="$state" || exit_on_fail
   echo "Starting raw data pruning in PROD for $state"
-  python -m recidiviz.tools.ingest.one_offs.clear_redundant_raw_data_on_bq --dry-run False --project-id=recidiviz-123 --state-code="$state"
+  python -m recidiviz.tools.ingest.one_offs.clear_redundant_raw_data_on_bq --dry-run False --project-id=recidiviz-123 --state-code="$state" || exit_on_fail
 done
 echo "Deleting temporary output files..."
 rm prune-prod.txt
