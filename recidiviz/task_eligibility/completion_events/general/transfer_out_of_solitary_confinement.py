944fb1da5d500007d46f6d4c2eae991639be60a9
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2024-02-20 12:56:43 -0800

    [US_MI][Workflows] Add scc criteria and completion event  (Recidiviz/recidiviz-data#27240)
    ## Description of the change
    
    This PR:
    - adds `us_mi_past_security_classificataion_committee_review_date`
    following the fines and fees logic. Example
    [here](https://docs.google.com/spreadsheets/d/1kQMu5pTQEo32maNu3UYOwJkiO2JTldK_JOkTfKznQAU/edit?usp=sharing)
    - adds the general criteria `housing_unit_type_is_solitary_confinement`
    - adds the completion event `transfer_out_of_solitary_confinement`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#24785
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: da5d8bc8d09daf599f247e97742ddce5207f2330

---
+++ recidiviz/task_eligibility/completion_events/general/transfer_out_of_solitary_confinement.py
@@ -0,0 +1,51 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2022 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines a view that shows all releases from solitary confinement.
+"""
+
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.task_eligibility.task_completion_event_big_query_view_builder import (
+    StateAgnosticTaskCompletionEventBigQueryViewBuilder,
+    TaskCompletionEventType,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Defines a view that shows all releases from solitary confinement."""
+
+_QUERY_TEMPLATE = """
+/* Get any SOLITARY_CONFINEMENT housing_unit_type_collapsed_solitary_session that ends. An end with a date_gap will
+indicate that the resident was discharged out of incarceration, and a new adjacent session will indicate the 
+housing_unit_type_collapsed_solitary_session has changed to a different type. */
+SELECT 
+    state_code,
+    person_id,
+    end_date_exclusive AS completion_event_date
+FROM `{project_id}.{sessions_dataset}.housing_unit_type_collapsed_solitary_sessions_materialized` 
+WHERE housing_unit_type_collapsed_solitary LIKE "%SOLITARY%"
+"""
+
+VIEW_BUILDER: StateAgnosticTaskCompletionEventBigQueryViewBuilder = StateAgnosticTaskCompletionEventBigQueryViewBuilder(
+    completion_event_type=TaskCompletionEventType.TRANSFER_OUT_OF_SOLITARY_CONFINEMENT,
+    description=_DESCRIPTION,
+    completion_event_query_template=_QUERY_TEMPLATE,
+    sessions_dataset=dataset_config.SESSIONS_DATASET,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
