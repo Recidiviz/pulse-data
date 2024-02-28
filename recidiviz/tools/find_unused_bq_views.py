84de5ea9bb75f10ef17d21533f1766d92c861563
Jen Overgaag <jovergaag@gmail.com>
2024-02-28 11:35:53 -0800

    Remove recidiviz_users_to_delete_from_analytics view (Recidiviz/recidiviz-data#27862)
    ## Description of the change
    
    Removes the `recidiviz_users_to_delete_from_analytics` which was used to
    identify analytics events that were from Recidiviz users impersonating
    another user - which should not contribute to our analytics metrics. The
    bad events have all been deleted from the segment tables.
    
    Snapshots from all of the segment tables exist in the recidiviz-123
    dataset `backup_pulse_dashboard_segment_metrics` . A copy of the
    materialized `recidiviz_users_to_delete_from_analytics` also lives in
    that dataset.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#27568
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6fcb7942ff789505ba55db0edd963409fbba0be7

--- recidiviz/tools/find_unused_bq_views.py
+++ recidiviz/tools/find_unused_bq_views.py
@@ -124,9 +124,6 @@ from recidiviz.calculator.query.state.views.sessions.us_tn.us_tn_parole_board_he
 from recidiviz.calculator.query.state.views.sessions.work_release_sessions import (
     WORK_RELEASE_SESSIONS_VIEW_BUILDER,
 )
-from recidiviz.calculator.query.state.views.workflows.adhoc.recidiviz_users_to_delete_from_analytics import (
-    RECIDIVIZ_USERS_TO_DELETE_FROM_ANALYTICS_VIEW_BUILDER,
-)
 from recidiviz.calculator.query.state.views.workflows.clients_opportunity_snoozed import (
     CLIENTS_OPPORTUNITY_SNOOZED_VIEW_BUILDER,
 )
@@ -310,10 +307,6 @@ UNREFERENCED_ADDRESSES_TO_KEEP_WITH_REASON = {
     ALL_TASK_TYPE_ELIGIBILITY_SPANS_VIEW_BUILDER.address: (
         "Will be referenced by a funnel status sessions view to support Workflows impact dashboards"
         " (see #26453) (Mayuka Sarukkai 2/8/24)"
-    ),
-    RECIDIVIZ_USERS_TO_DELETE_FROM_ANALYTICS_VIEW_BUILDER.address: (
-        "Ad hoc query which will be used for by another ad hoc query to delete bad analytics events next week"
-        "Will be deleted as part of #27568 (Jen Overgaag 2/14/24)"
     ),
     SUPERVISION_OFFICER_OUTLIER_STATUS_ARCHIVE_VIEW_BUILDER.address: (
         "Will be referenced to support Outliers analytics work (see #27576) (Alexa Batino 2/14/24)"
