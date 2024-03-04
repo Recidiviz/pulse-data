f73b34f207c21eb298a86572eb843c7a334d24f0
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2024-03-04 12:01:45 -0800

    [Workflows] include people only eligible in new query in CR referral record (Recidiviz/recidiviz-data#27936)
    ## Description of the change
    
    Previously, we LEFT JOINed the old/new CR queries together because the
    frontend didn't know how to handle all the new fields. Now that it does,
    we can start including people who only show up in the new style query by
    doing a FULL OUTER JOIN.
    
    If my queries are correct, this means we will start showing an
    additional 214 people as eligible or almost eligible (in staging).
    
    A different option would be to stop excluding the old records
    completely, but the implications of that not working are that we
    possibly show nobody as eligible, whereas the implications of this not
    working are we get a bunch of sentry errors. Maybe we do that next week
    after this is deployed in staging if everything goes well.
    
    Loaded to sandbox at `dana_cr`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes https://github.com/Recidiviz/recidiviz-data/issues/22521
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6da99617953c6faabd0e53bebcbe100b54da7ab7

--- recidiviz/calculator/query/state/views/workflows/firestore/compliant_reporting_referral_record.py
+++ recidiviz/calculator/query/state/views/workflows/firestore/compliant_reporting_referral_record.py
@@ -28,136 +28,139 @@ COMPLIANT_REPORTING_REFERRAL_RECORD_DESCRIPTION = """
     """
 
 COMPLIANT_REPORTING_REFERRAL_RECORD_QUERY_TEMPLATE = """
-    SELECT
-        "US_TN" AS state_code,
-        po_first_name,
-        po_last_name,
-        client_first_name,
-        client_last_name,
-        date_today,
-        tdoc_id,
-        tdoc_id AS external_id,
-        compliant_reporting_eligible,
-        physical_address,
-        current_employer,
-        drivers_license,
-        drivers_license_suspended,
-        drivers_license_revoked,
-        conviction_county,
-        conviction_counties,
-        court_name,
-        all_dockets,
-        docket_numbers,
-        current_offenses,
-        past_offenses,
-        lifetime_offenses_expired,
-        judicial_district,
-        drug_screens_past_year,
-        sanctions_past_year,
-        most_recent_arrest_check,
-        zero_tolerance_codes,
-        fines_fees_eligible,
-        special_conditions_flag,
-        supervision_type,
-        sentence_start_date,
-        sentence_length_days,
-        expiration_date,
-        supervision_fee_assessed,
-        IFNULL(supervision_fee_arrearaged, 0)=1 AS supervision_fee_arrearaged,
-        supervision_fee_arrearaged_amount,
-        supervision_fee_exemption_type,
-        supervision_fee_exemption_expir_date,
-        supervision_fee_waived,
-        IFNULL(court_costs_paid, 0)=1 AS court_costs_paid,
-        court_costs_balance,
-        court_costs_monthly_amt_1,
-        court_costs_monthly_amt_2,
-        restitution_amt,
-        restitution_monthly_payment,
-        restitution_monthly_payment_to,
-        next_special_conditions_check,
-        last_special_conditions_note,
-        special_conditions_terminated_date,
-        remaining_criteria_needed,
-        eligible_level_start,
-        offense_type_eligibility,
-        -- these fields should all be disregarded if remaining_criteria_needed is zero,
-        -- as there is additional override logic baked into that field
-        -- (i.e. the fields do not always agree and remaining_criteria_needed wins)
-        IF (
-            remaining_criteria_needed = 0,
-            NULL,
-            eligible_time_on_supervision_level_bool = 1
-        ) AS almost_eligible_time_on_supervision_level,
-        IF (
-            remaining_criteria_needed = 0,
-            NULL,
-            date_supervision_level_eligible
-        ) AS date_supervision_level_eligible,
-        IF (
-            remaining_criteria_needed = 0,
-            NULL,
-            fines_fees_eligible_bool = 1
-        ) AS almost_eligible_fines_fees,
-        IF (
-            remaining_criteria_needed = 0,
-            NULL,
-           cr_recent_rejection_eligible_bool = 1
-        ) AS almost_eligible_recent_rejection,
-        IF (
-            remaining_criteria_needed = 0,
-            NULL,
-            cr_rejections_past_3_months
-        ) AS cr_rejections_past_3_months,
-        IF (
-            remaining_criteria_needed = 0,
-            NULL,
-            eligible_serious_sanctions_bool = 1
-        ) AS almost_eligible_serious_sanctions,
-        IF (
-            remaining_criteria_needed = 0,
-            NULL,
-            date_serious_sanction_eligible
-        ) AS date_serious_sanction_eligible,
-        IFNULL(special_conditions_alc_drug_screen, 0)=1 AS special_conditions_alc_drug_screen,
-        special_conditions_alc_drug_screen_date,
-        special_conditions_alc_drug_assessment,
-        IFNULL(special_conditions_alc_drug_assessment_complete, 0)=1 AS special_conditions_alc_drug_assessment_complete,
-        special_conditions_alc_drug_assessment_complete_date,
-        IFNULL(special_conditions_alc_drug_treatment, 0)=1 AS special_conditions_alc_drug_treatment,
-        special_conditions_alc_drug_treatment_in_out,
-        IFNULL(special_conditions_alc_drug_treatment_current, 0)=1 AS special_conditions_alc_drug_treatment_current,
-        special_conditions_alc_drug_treatment_complete_date,
-        IFNULL(special_conditions_counseling, 0)=1 AS special_conditions_counseling,
-        special_conditions_counseling_anger_management,
-        IFNULL(special_conditions_counseling_anger_management_current, 0)=1 AS special_conditions_counseling_anger_management_current,
-        special_conditions_counseling_anger_management_complete_date,
-        special_conditions_counseling_mental_health,
-        special_conditions_counseling_mental_health_current,
-        special_conditions_counseling_mental_health_complete_date,
-        IFNULL(special_conditions_community_service, 0)=1 AS special_conditions_community_service,
-        special_conditions_community_service_hours,
-        IFNULL(special_conditions_community_service_current, 0)=1 AS special_conditions_community_service_current,
-        special_conditions_community_service_completion_date,
-        IFNULL(special_conditions_programming, 0)=1 AS special_conditions_programming,
-        IFNULL(special_conditions_programming_cognitive_behavior, 0)=1 AS special_conditions_programming_cognitive_behavior,
-        IFNULL(special_conditions_programming_cognitive_behavior_current, 0)=1 AS special_conditions_programming_cognitive_behavior_current,
-        special_conditions_programming_cognitive_behavior_completion_date,
-        IFNULL(special_conditions_programming_safe, 0)=1 AS special_conditions_programming_safe,
-        IFNULL(special_conditions_programming_safe_current, 0)=1 AS special_conditions_programming_safe_current,
-        special_conditions_programming_safe_completion_date,
-        IFNULL(special_conditions_programming_victim_impact, 0)=1 AS special_conditions_programming_victim_impact,
-        IFNULL(special_conditions_programming_victim_impact_current, 0)=1 AS special_conditions_programming_victim_impact_current,
-        special_conditions_programming_victim_impact_completion_date,
-        IFNULL(special_conditions_programming_fsw, 0)=1 AS special_conditions_programming_fsw,
-        IFNULL(special_conditions_programming_fsw_current, 0)=1 AS special_conditions_programming_fsw_current,
-        special_conditions_programming_fsw_completion_date,
-        n.* EXCEPT (state_code, external_id)
-    FROM `{project_id}.{analyst_dataset}.us_tn_compliant_reporting_referral_materialized` o
-    LEFT JOIN `{project_id}.{workflows_dataset}.us_tn_transfer_to_compliant_reporting_record_materialized` n
-        ON o.tdoc_id = n.external_id
-    WHERE compliant_reporting_eligible IS NOT NULL
-    AND remaining_criteria_needed <= 1
+    WITH o AS (
+        SELECT
+            "US_TN" AS state_code,
+            po_first_name,
+            po_last_name,
+            client_first_name,
+            client_last_name,
+            date_today,
+            tdoc_id,
+            tdoc_id AS external_id,
+            compliant_reporting_eligible,
+            physical_address,
+            current_employer,
+            drivers_license,
+            drivers_license_suspended,
+            drivers_license_revoked,
+            conviction_county,
+            conviction_counties,
+            court_name,
+            all_dockets,
+            docket_numbers,
+            current_offenses,
+            past_offenses,
+            lifetime_offenses_expired,
+            judicial_district,
+            drug_screens_past_year,
+            sanctions_past_year,
+            most_recent_arrest_check,
+            zero_tolerance_codes,
+            fines_fees_eligible,
+            special_conditions_flag,
+            supervision_type,
+            sentence_start_date,
+            sentence_length_days,
+            expiration_date,
+            supervision_fee_assessed,
+            IFNULL(supervision_fee_arrearaged, 0)=1 AS supervision_fee_arrearaged,
+            supervision_fee_arrearaged_amount,
+            supervision_fee_exemption_type,
+            supervision_fee_exemption_expir_date,
+            supervision_fee_waived,
+            IFNULL(court_costs_paid, 0)=1 AS court_costs_paid,
+            court_costs_balance,
+            court_costs_monthly_amt_1,
+            court_costs_monthly_amt_2,
+            restitution_amt,
+            restitution_monthly_payment,
+            restitution_monthly_payment_to,
+            next_special_conditions_check,
+            last_special_conditions_note,
+            special_conditions_terminated_date,
+            remaining_criteria_needed,
+            eligible_level_start,
+            offense_type_eligibility,
+            -- these fields should all be disregarded if remaining_criteria_needed is zero,
+            -- as there is additional override logic baked into that field
+            -- (i.e. the fields do not always agree and remaining_criteria_needed wins)
+            IF (
+                remaining_criteria_needed = 0,
+                NULL,
+                eligible_time_on_supervision_level_bool = 1
+            ) AS almost_eligible_time_on_supervision_level,
+            IF (
+                remaining_criteria_needed = 0,
+                NULL,
+                date_supervision_level_eligible
+            ) AS date_supervision_level_eligible,
+            IF (
+                remaining_criteria_needed = 0,
+                NULL,
+                fines_fees_eligible_bool = 1
+            ) AS almost_eligible_fines_fees,
+            IF (
+                remaining_criteria_needed = 0,
+                NULL,
+            cr_recent_rejection_eligible_bool = 1
+            ) AS almost_eligible_recent_rejection,
+            IF (
+                remaining_criteria_needed = 0,
+                NULL,
+                cr_rejections_past_3_months
+            ) AS cr_rejections_past_3_months,
+            IF (
+                remaining_criteria_needed = 0,
+                NULL,
+                eligible_serious_sanctions_bool = 1
+            ) AS almost_eligible_serious_sanctions,
+            IF (
+                remaining_criteria_needed = 0,
+                NULL,
+                date_serious_sanction_eligible
+            ) AS date_serious_sanction_eligible,
+            IFNULL(special_conditions_alc_drug_screen, 0)=1 AS special_conditions_alc_drug_screen,
+            special_conditions_alc_drug_screen_date,
+            special_conditions_alc_drug_assessment,
+            IFNULL(special_conditions_alc_drug_assessment_complete, 0)=1 AS special_conditions_alc_drug_assessment_complete,
+            special_conditions_alc_drug_assessment_complete_date,
+            IFNULL(special_conditions_alc_drug_treatment, 0)=1 AS special_conditions_alc_drug_treatment,
+            special_conditions_alc_drug_treatment_in_out,
+            IFNULL(special_conditions_alc_drug_treatment_current, 0)=1 AS special_conditions_alc_drug_treatment_current,
+            special_conditions_alc_drug_treatment_complete_date,
+            IFNULL(special_conditions_counseling, 0)=1 AS special_conditions_counseling,
+            special_conditions_counseling_anger_management,
+            IFNULL(special_conditions_counseling_anger_management_current, 0)=1 AS special_conditions_counseling_anger_management_current,
+            special_conditions_counseling_anger_management_complete_date,
+            special_conditions_counseling_mental_health,
+            special_conditions_counseling_mental_health_current,
+            special_conditions_counseling_mental_health_complete_date,
+            IFNULL(special_conditions_community_service, 0)=1 AS special_conditions_community_service,
+            special_conditions_community_service_hours,
+            IFNULL(special_conditions_community_service_current, 0)=1 AS special_conditions_community_service_current,
+            special_conditions_community_service_completion_date,
+            IFNULL(special_conditions_programming, 0)=1 AS special_conditions_programming,
+            IFNULL(special_conditions_programming_cognitive_behavior, 0)=1 AS special_conditions_programming_cognitive_behavior,
+            IFNULL(special_conditions_programming_cognitive_behavior_current, 0)=1 AS special_conditions_programming_cognitive_behavior_current,
+            special_conditions_programming_cognitive_behavior_completion_date,
+            IFNULL(special_conditions_programming_safe, 0)=1 AS special_conditions_programming_safe,
+            IFNULL(special_conditions_programming_safe_current, 0)=1 AS special_conditions_programming_safe_current,
+            special_conditions_programming_safe_completion_date,
+            IFNULL(special_conditions_programming_victim_impact, 0)=1 AS special_conditions_programming_victim_impact,
+            IFNULL(special_conditions_programming_victim_impact_current, 0)=1 AS special_conditions_programming_victim_impact_current,
+            special_conditions_programming_victim_impact_completion_date,
+            IFNULL(special_conditions_programming_fsw, 0)=1 AS special_conditions_programming_fsw,
+            IFNULL(special_conditions_programming_fsw_current, 0)=1 AS special_conditions_programming_fsw_current,
+            special_conditions_programming_fsw_completion_date
+        FROM `{project_id}.{analyst_dataset}.us_tn_compliant_reporting_referral_materialized`
+        WHERE compliant_reporting_eligible IS NOT NULL
+        AND remaining_criteria_needed <= 1
+    )
+    SELECT *
+    FROM o
+    FULL OUTER JOIN `{project_id}.{workflows_dataset}.us_tn_transfer_to_compliant_reporting_record_materialized` n
+        USING (state_code, external_id)
 """
 
 COMPLIANT_REPORTING_REFERRAL_RECORD_VIEW_BUILDER = SimpleBigQueryViewBuilder(
