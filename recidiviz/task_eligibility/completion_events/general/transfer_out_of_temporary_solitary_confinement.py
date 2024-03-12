2f4d457eb463f0de08fde0ba4fc67d36ba89fbe1
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2024-03-12 15:57:13 -0700

    [US_MI][Workflows] Adds temporary solitary confinement criteria, opp, and completion event (Recidiviz/recidiviz-data#28094)
    ## Description of the change
    
    Writes an opportunity to surface folks who have been in temporary
    segregation over 30 days.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#28092
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: e30b8acc2c7273661b57a7b6798f7d4054b71258

---
+++ recidiviz/task_eligibility/completion_events/general/transfer_out_of_temporary_solitary_confinement.py
@@ -0,0 +1,66 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines a view that shows all releases from temporary solitary confinement.
+"""
+
+from recidiviz.calculator.query.bq_utils import nonnull_end_date_clause
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.task_eligibility.task_completion_event_big_query_view_builder import (
+    StateAgnosticTaskCompletionEventBigQueryViewBuilder,
+    TaskCompletionEventType,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """ Defines a view that shows all releases from temporary solitary confinement.
+"""
+
+_QUERY_TEMPLATE = f"""
+/* Get any TEMPORARY_SOLITARY_CONFINEMENT housing_unit_type_session that ends in a housing_unit_type that is not 
+solitary (including nulls). */
+#TODO(#27658) Account for false transitions
+WITH housing_units AS (
+    SELECT 
+    state_code,
+    person_id,
+    end_date_exclusive AS completion_event_date,
+    housing_unit_type,
+    LEAD (housing_unit_type) OVER (PARTITION BY person_id, date_gap_id ORDER BY start_date ASC) AS next_housing_type,
+FROM `{{project_id}}.{{sessions_dataset}}.housing_unit_type_sessions_materialized` 
+)
+SELECT 
+    state_code,
+    person_id,
+    completion_event_date
+FROM housing_units
+WHERE housing_unit_type = 'TEMPORARY_SOLITARY_CONFINEMENT'
+    -- do not include open TEMPORARY_SOLITARY_CONFINEMENT periods 
+    AND {nonnull_end_date_clause('completion_event_date')}<= CURRENT_DATE('US/Eastern')
+    -- only chose sub sessions that transfer to a non solitary housing_unit_type
+    AND COALESCE(next_housing_type, '') NOT LIKE '%SOLITARY%'
+"""
+
+VIEW_BUILDER: StateAgnosticTaskCompletionEventBigQueryViewBuilder = StateAgnosticTaskCompletionEventBigQueryViewBuilder(
+    completion_event_type=TaskCompletionEventType.TRANSFER_OUT_OF_TEMPORARY_SOLITARY_CONFINEMENT,
+    description=_DESCRIPTION,
+    completion_event_query_template=_QUERY_TEMPLATE,
+    sessions_dataset=dataset_config.SESSIONS_DATASET,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
