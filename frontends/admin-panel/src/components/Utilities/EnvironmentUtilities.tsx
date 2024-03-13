037b5ec0cd99dbd3b920ffc2efd5cb442745884e
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2024-03-13 12:14:20 -0400

    [Insights][Admin Panel] Fix admin panel bugs (Recidiviz/recidiviz-data#28153)
    ## Description of the change
    
    This PR:
    - ensures the 'Create new version' is only shown in dev and staging
    - initializes columns as emtpy list when no configs exist for a state.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 05130670acf0383291b0c425caf7a8a953229343

--- frontends/admin-panel/src/components/Utilities/EnvironmentUtilities.tsx
+++ frontends/admin-panel/src/components/Utilities/EnvironmentUtilities.tsx
@@ -1,4 +1,5 @@
 export const gcpEnvironment = {
   isProduction: window.RUNTIME_GCP_ENVIRONMENT === "production",
   isStaging: window.RUNTIME_GCP_ENVIRONMENT === "staging",
+  isDevelopment: window.RUNTIME_GCP_ENVIRONMENT === "development",
 };
