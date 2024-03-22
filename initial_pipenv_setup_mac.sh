71b24a22917d8e9ba6f7b4322cfa106d40aa371c
Dan Hansen <dan@recidiviz.org>
2024-03-22 18:09:27 +0000

    Install `pylint` in `initial_pipenv_setup_mac.sh` (Recidiviz/recidiviz-data#28375)
    ## Description of the change
    
    Also adds some context to the codebase for why this isn't managed by our
    Pipfile any more.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#26243
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 35b8d351762206366e2103a49f9ae05d3c407892

--- initial_pipenv_setup_mac.sh
+++ initial_pipenv_setup_mac.sh
@@ -20,3 +20,9 @@ pipenv sync --dev --verbose
 
 # Install pre-commit hooks
 pipenv run pre-commit install --overwrite
+
+# Install pylint which is not currently managed by our Pipfile
+# There is a not easily-solved requirement conflict on `dill` between `apache-beam` and `pylint`
+# More details can be found here: https://recidiviz.slack.com/archives/C028X32LRH7/p1701458677360479
+# TODO(apache/beam#22893): This can be moved into our Pipfile once Beam moves to cloudpickle or upgrades dill
+pip install pylint
