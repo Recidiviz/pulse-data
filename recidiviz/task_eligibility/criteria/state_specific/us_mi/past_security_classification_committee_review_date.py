5c5aa89de6aab85c12fef94c82fd6ea054cf8667
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2024-03-25 11:32:35 -0700

    [US_MI][Workflows] Updates scc date logic to take the least of 30 days since last scc and expected review date  (Recidiviz/recidiviz-data#28174)
    ## Description of the change
    
    We want a resident to be eligible for an scc review when they have had
    less reviews they expected OR when their last review was more than 30
    days ago.
    
    Initially, we just surfaced residents when they had less reviews than
    expected, but this lead to residents not being eligible for months if
    they had more reviews than expected.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#28091
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 7d94b10c830d01291ab7f4781fb356ec5add563a

--- recidiviz/task_eligibility/criteria/state_specific/us_mi/past_security_classification_committee_review_date.py
+++ recidiviz/task_eligibility/criteria/state_specific/us_mi/past_security_classification_committee_review_date.py
@@ -17,17 +17,16 @@
 """Defines a criteria span view that shows spans of time during which someone is eligible
 for a security classification review"""
 
-from recidiviz.calculator.query.bq_utils import (
-    nonnull_end_date_clause,
-    nonnull_end_date_exclusive_clause,
-)
-from recidiviz.calculator.query.state.dataset_config import (
-    ANALYST_VIEWS_DATASET,
-    SESSIONS_DATASET,
-)
 from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.dataset_config import (
+    task_eligibility_criteria_state_specific_dataset,
+)
 from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
     StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.state_dataset_query_fragments import (
+    combining_several_criteria_into_one,
+    extract_object_from_json,
 )
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
@@ -42,198 +41,53 @@ This view keeps a tally of how many SCC reviews a resident should have, and subt
 see an SCC review take place. SCC reviews are summed over the solitary confinement session, so the tally
 will restart for each new session. """
 
-_QUERY_TEMPLATE = f"""
-   WITH review_dates_preprocessed AS (
-       /* This CTE gathers all solitary confinement session start dates, and associates the first scc review. 
-       If that review is within 7 days, that date is used to calculate subsequent review due dates, 
-       otherwise, 7 days from the solitary start is used. */
-        SELECT 
-            h.state_code,
-            h.person_id,
-            h.start_date,
-            h.end_date_exclusive,
-            COALESCE(s.completion_event_date, DATE_ADD(h.start_date, INTERVAL 7 DAY)) AS first_review_date,
-        FROM `{{project_id}}.{{sessions_dataset}}.us_mi_facility_housing_unit_type_collapsed_solitary_sessions` h
-        LEFT JOIN `{{project_id}}.{{analyst_views_dataset}}.us_mi_security_classification_committee_review_materialized` s
-            ON s.person_id = h.person_id
-            AND s.state_code = h.state_code
-            --join scc reviews that are done on the start or the end_date_exclusive of a solitary session
-            AND s.completion_event_date BETWEEN h.start_date AND {nonnull_end_date_clause('h.end_date_exclusive')}
-            AND DATE_DIFF(s.completion_event_date,h.start_date,  DAY) < 7
-        WHERE h.housing_unit_type_collapsed_solitary = 'SOLITARY_CONFINEMENT'
-            AND h.state_code = 'US_MI'
-        --for facility/housing unit sessions where multiple scc reviews happen within the first week
-        --take the latest scc review within that time period as the initial review date
-        QUALIFY ROW_NUMBER() OVER(PARTITION BY h.person_id, h.start_date ORDER BY s.completion_event_date DESC)=1
-    ),
-    review_dates AS(
-        /* This CTE generates the dates for which an scc review should occur. The first date is the start date of the
-        solitary confinement session. The rest of the days are 30 days after the least of the first review 
-        and 7 days after the start date, repeating.
-         
-         Additionally, due to data quality, we only include review dates after the COMS migration (2023-08-14) */
-        SELECT 
-            state_code,
-            person_id, 
-            DATE_TRUNC(DATE_ADD(first_review_date, INTERVAL offset DAY), WEEK(MONDAY)) AS change_date
-        FROM review_dates_preprocessed p,
-            UNNEST(GENERATE_ARRAY(30, 36500, 30)) AS offset
-        WHERE
-        --calculate recurring scc reviews until the solitary session ends or for 100 years
-          offset <= DATE_DIFF({nonnull_end_date_exclusive_clause('end_date_exclusive')}, start_date, DAY)
-          AND DATE_ADD(start_date, INTERVAL offset DAY) >= '2023-08-14' 
-        UNION ALL
-        SELECT 
-            state_code,
-            person_id, 
-            start_date AS change_date,
-        FROM review_dates_preprocessed 
-        WHERE start_date >= '2023-08-14'
-    ),
-    population_change_dates AS (
-    /* this CTE gathers all dates at which eligibility might change */ 
-        
-        --add 1 for each expected SCC review (this includes the start dates for solitary sessions that happen 
-        --after the COMS migration)  
-        SELECT
-            state_code,
-            person_id,
-            change_date,
-            1 AS expected_review,
-            1 AS activity_type,
-        FROM review_dates
-        
-        UNION ALL   
-        
-        --include a population change date for solitary sessions that happen before the COMS migration, 
-        --so that even though we aren't accruing expected reviews before then, we can still aggregate within sessions. 
-        SELECT 
-            state_code,
-            person_id,
-            start_date AS change_date,
-            0 AS expected_review,
-            0 AS activity_type,
-        FROM
-            `{{project_id}}.{{sessions_dataset}}.us_mi_facility_housing_unit_type_collapsed_solitary_sessions` h
-        WHERE
-            state_code = 'US_MI'
-            AND housing_unit_type_collapsed_solitary = 'SOLITARY_CONFINEMENT'
-            AND start_date <= '2023-08-14'
-       
-        UNION ALL
-        
-        -- add a change date for when the solitary session ends
-        SELECT 
-            state_code,
-            person_id,
-            h.end_date_exclusive AS change_date,
-            0 AS expected_review,
-            0 AS activity_type,
-        FROM
-            `{{project_id}}.{{sessions_dataset}}.us_mi_facility_housing_unit_type_collapsed_solitary_sessions` h
-        WHERE
-            state_code = 'US_MI'
-            AND housing_unit_type_collapsed_solitary = 'SOLITARY_CONFINEMENT'
-        
-        UNION ALL
-        
-        --add -1 for every time we observe a review 
-        SELECT 
-            state_code,
-            person_id,
-            completion_event_date AS change_date,
-            0 AS expected_review,
-            -1 AS activity_type,
-        FROM
-            `{{project_id}}.{{analyst_views_dataset}}.us_mi_security_classification_committee_review_materialized` s
+_CRITERIA_QUERY_1 = """
+    SELECT
+        * EXCEPT (reason),
+        NULL AS facility_solitary_start_date,
+        NULL AS latest_scc_review_date,
+        NULL AS number_of_expected_reviews,
+        NULL AS number_of_reviews
+    FROM `{project_id}.{task_eligibility_criteria_us_mi}.one_month_past_last_security_classification_committee_review_date_materialized`
+    """
 
-    ),
-    population_change_dates_agg AS (
-        SELECT 
-            state_code,
-            person_id,
-            change_date,
-            SUM(expected_review) AS expected_review,
-            SUM(activity_type) AS activity_type,
-        FROM
-            population_change_dates
-        GROUP BY
-            1,2,3
-    ),
-    time_spans AS (
-        SELECT 
-            p.state_code,
-            p.person_id,
-            p.change_date AS start_date,
-            LEAD(change_date) OVER (PARTITION BY p.state_code,
-                                               p.person_id
-                                  ORDER BY change_date) AS end_date,
-            p.expected_review, 
-            p.activity_type,
-        FROM
-            population_change_dates_agg p
-    ),
-    time_spans_agg AS (
-        SELECT 
-            ts.state_code,
-            ts.person_id,
-            ts.start_date,
-            ts.end_date,
-            hu.session_id,
-            hu.start_date AS housing_start_date,
-            hu.end_date_exclusive AS housing_end_date,
-            SUM(expected_review) OVER (PARTITION BY ts.state_code, 
-                                                    ts.person_id,
-                                                    hu.session_id
-                                    ORDER BY ts.start_date
-            ) AS expected_reviews,
-            SUM(activity_type) OVER (PARTITION BY ts.state_code, 
-                                                    ts.person_id,
-                                                    hu.session_id
-                                    ORDER BY ts.start_date
-            ) AS reviews_due,
-        FROM
-            time_spans ts
-        INNER JOIN
-            `{{project_id}}.{{sessions_dataset}}.us_mi_facility_housing_unit_type_collapsed_solitary_sessions` hu
-            ON ts.person_id = hu.person_id
-            AND ts.start_date < {nonnull_end_date_clause('hu.end_date_exclusive')}
-            AND hu.start_date < {nonnull_end_date_clause('ts.end_date')}
-            AND hu.housing_unit_type_collapsed_solitary = 'SOLITARY_CONFINEMENT'
-    )
-    SELECT 
-        t.state_code,
-        t.person_id,
-        t.start_date,
-        t.end_date,
-        t.reviews_due > 0 AS meets_criteria,
-        TO_JSON(STRUCT(
-                housing_start_date AS facility_solitary_start_date,
-                t.expected_reviews AS number_of_expected_reviews,
-                t.expected_reviews-t.reviews_due AS number_of_reviews,
-                p.change_date AS latest_scc_review_date
-            )) AS reason,
-    FROM time_spans_agg t
-    LEFT JOIN population_change_dates  p
-        ON p.state_code = t.state_code
-        AND p.person_id = t.person_id 
-        AND p.change_date BETWEEN housing_start_date AND {nonnull_end_date_exclusive_clause('housing_end_date')}
-        AND p.change_date < {nonnull_end_date_exclusive_clause('t.end_date')}
-        --only join SCC reviews
-        AND p.activity_type = -1
-    --pick latest SCC review within the relevant housing unit session
-    QUALIFY ROW_NUMBER() OVER(PARTITION BY t.person_id, t.start_date ORDER BY p.change_date DESC)=1
+_CRITERIA_QUERY_2 = f"""
+    SELECT
+        * EXCEPT (reason),
+        {extract_object_from_json(object_column = 'facility_solitary_start_date', 
+                                  object_type = 'DATE')} AS facility_solitary_start_date,
+        {extract_object_from_json(object_column = 'latest_scc_review_date', 
+                                  object_type = 'DATE')} AS latest_scc_review_date,           
+        {extract_object_from_json(object_column = 'number_of_expected_reviews', 
+                                  object_type = 'INT64')} AS number_of_expected_reviews,
+        {extract_object_from_json(object_column = 'number_of_reviews', 
+                                  object_type = 'INT64')} AS number_of_reviews,             
+    FROM `{{project_id}}.{{task_eligibility_criteria_us_mi}}.expected_number_of_security_classification_committee_reviews_greater_than_observed_materialized`
 """
 
-VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
-    StateSpecificTaskCriteriaBigQueryViewBuilder(
-        criteria_name=_CRITERIA_NAME,
-        description=_DESCRIPTION,
-        criteria_spans_query_template=_QUERY_TEMPLATE,
-        state_code=StateCode.US_MI,
-        sessions_dataset=SESSIONS_DATASET,
-        analyst_views_dataset=ANALYST_VIEWS_DATASET,
-    )
+
+_JSON_CONTENT = """MIN(facility_solitary_start_date) AS facility_solitary_start_date,
+                    MAX(latest_scc_review_date) AS latest_scc_review_date,
+                    MAX(number_of_expected_reviews) AS number_of_expected_reviews,
+                    MAX(number_of_reviews) AS number_of_reviews"""
+
+_QUERY_TEMPLATE = f"""
+{combining_several_criteria_into_one(
+        select_statements_for_criteria_lst=[_CRITERIA_QUERY_1,
+                                             _CRITERIA_QUERY_2],
+        meets_criteria="LOGICAL_OR(meets_criteria)",
+        json_content=_JSON_CONTENT,
+    )}
+"""
+
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = StateSpecificTaskCriteriaBigQueryViewBuilder(
+    criteria_name=_CRITERIA_NAME,
+    description=_DESCRIPTION,
+    criteria_spans_query_template=_QUERY_TEMPLATE,
+    state_code=StateCode.US_MI,
+    task_eligibility_criteria_us_mi=task_eligibility_criteria_state_specific_dataset(
+        StateCode.US_MI
+    ),
 )
 
 if __name__ == "__main__":
