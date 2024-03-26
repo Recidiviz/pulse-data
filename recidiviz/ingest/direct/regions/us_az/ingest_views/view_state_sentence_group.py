9a491307197d5c816f794a1535b44ea12be84e3e
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-03-26 16:25:29 -0400

    [US_AZ] Ingest StateSentenceGroup (Recidiviz/recidiviz-data#28427)
    ## Description of the change
    
    This entity holds information about the controlling offense for a given
    commitment to incarceration. There can be many offenses tied to a single
    commitment, but one of them controls the sentence as a whole. This
    offense is the one that determines the release dates. The sentence group
    external ID is the ADCRR system-generated ID for the _commitment_, along
    with the sequence number of this particular set of release dates for
    that commitment. The commitment ID alone can also be found on
    `state_sentence.sentence_group_external_id`, where the same
    `sentence_group_external_id` can be attached to multiple
    `state_sentence` entities when there are multiple offenses per
    commitment.
    
    The mappings are:
    
    - `parole_eligibility_date_external`: CommunitySupervisionBeginDate, the
    earliest date a person can be released to community supervision outside
    of a transition release.
    - `projected_full_term_release_date_min_external`:
    EarnedReleaseCreditDate
    - `projected_full_term_release_date_max_external`: If a person is not
    eligible for community supervision, then the latest they can be released
    from prison is their Sentence Expiration Date (SED). If they _are_
    eligible for community supervision, then the latest they can be released
    from prison is their Community Supervision End Date (CSED), which is
    115% of the SED. If a CSED exists for the controlling offense, then it
    is stored here. If no CSED exists, the SED is stored here.
    
    tests:
    - [x] ingest view is deterministic
    - [x] [sandbox ingest
    pipeline](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-03-26_08_38_18-1317953425761724098;graphView=1?project=recidiviz-staging)
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: c7992575233bc8f374b991900f7b95a2bc2874ef

---
+++ recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_sentence_group.py
@@ -0,0 +1,83 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query for state sentence groups.
+
+For each of the date fields in the entity, we coalesce three fields from 
+the source raw data. This is to accomodate the way automatic and manual overrides are
+documented in ACIS. The system prioritizes Manual Lock fields first, because these
+contain dates that were manually entered by an officer to override the automatically calculated
+date. Then, Adjust Release Date fields are prioritized. These are automatic adjustments
+made by ACIS to release dates that fall on weekends or holidays. The final field is the
+original release date calculated by ACIS based on sentence information. 
+
+We define a "sentence group" in this case as a single commitment. Many offenses can 
+be associated with a single commitment, each having their own sentencing requirements.
+There is one controlling offense that determines the final release dates for a given 
+sentence group."""
+from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
+    DirectIngestViewQueryBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+WITH base AS (
+SELECT DISTINCT
+    off.COMMITMENT_ID AS external_id,
+    doc.PERSON_ID AS person_id,
+    off.UPDT_DTM AS group_update_datetime,
+    -- See view description for explanation of datetime field hierarchy.
+    COALESCE(
+        NULLIF(COMMUNITY_SUPV_BEGIN_DTM_ML, 'NULL'),
+        NULLIF(COMMUNITY_SUPV_BEGIN_DTM_ARD, 'NULL'), 
+        NULLIF(COMMUNITY_SUPV_BEGIN_DTM, 'NULL')) AS CommunitySupervisionBeginDate, -- CSBD
+    COALESCE(
+        NULLIF(COMMUNITY_SUPV_END_DTM_ML, 'NULL'), 
+        NULLIF(COMMUNITY_SUPV_END_DTM_ARD, 'NULL'), 
+        NULLIF(COMMUNITY_SUPV_END_DTM, 'NULL')) AS CommunitySupervisionEndDate, -- CSED
+    COALESCE(
+        NULLIF(EARNED_RLS_CREDIT_DTM_ML, 'NULL'), 
+        NULLIF(EARNED_RLS_CREDIT_DTM_ARD, 'NULL'), 
+        NULLIF(EARNED_RLS_CREDIT_DTM, 'NULL')) AS EarnedReleaseCreditDate, -- ERCD
+    COALESCE(
+        NULLIF(EXPIRATION_DTM_ML, 'NULL'), 
+        NULLIF(EXPIRATION_DTM, 'NULL')) AS SentenceExpirationDate, -- SED
+FROM {AZ_DOC_SC_OFFENSE@ALL} off
+LEFT JOIN {AZ_DOC_SC_EPISODE} sc_episode ON off.OFFENSE_ID = sc_episode.FINAL_OFFENSE_ID
+LEFT JOIN {DOC_EPISODE} doc ON sc_episode.DOC_ID = doc.DOC_ID
+WHERE doc.PERSON_ID IS NOT NULL
+AND off.OFFENSE_ID = sc_episode.FINAL_OFFENSE_ID
+)
+
+SELECT 
+    *, 
+    ROW_NUMBER() OVER (
+        PARTITION BY external_id 
+        ORDER BY group_update_datetime) AS sequence_num
+FROM base
+-- This is only the case for 68 offenses that do not have any listed release dates.
+WHERE group_update_datetime IS NOT NULL
+"""
+VIEW_BUILDER = DirectIngestViewQueryBuilder(
+    region="us_az",
+    ingest_view_name="state_sentence_group",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
