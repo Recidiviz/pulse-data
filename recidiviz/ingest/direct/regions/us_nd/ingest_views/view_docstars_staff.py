36d1679dd77fa5f77c4b5885063e6d10f568459c
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-03-28 12:02:39 -0400

    [US_ND] Refactor state staff view to pull most recent officer name (Recidiviz/recidiviz-data#28483)
    ## Description of the change
    
    Previously, the `docstars_staff` ingest view that hydrates `state_staff`
    in ND was relying on the manual roster as a primary source of truth, and
    only supplementing from nightly transfers of raw data those officers who
    did not appear at all in the manual roster. We ran into an issue where
    officers have changed their names since we last received a roster, and
    were not seeing their correct names in the ET workflows tool. This would
    likely be fixed in the next roster transfer, but this change makes the
    view more resilient to these kinds of intramonth changes.
    
    Instead of relying exclusively on the roster for the most up-to-date
    information when an officer also appears in nightly transfers, we now
    compare the two sources and choose the information that is the most
    recent. There are three scenarios to gauge the impact of this change:
    - If a person appears in both data sources (the monthly roster and the
    nightly transfers), with the same information in both, this will have no
    impact.
    - If a person appears in both data sources, but has had a recent name
    change, this will get their correct information into the view faster.
    - If a person only appears in one data source, this will have no impact.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-dashboardsRecidiviz/recidiviz-data#5167
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 544655593bb444078676464d5c83704d03f1b79f

--- recidiviz/ingest/direct/regions/us_nd/ingest_views/view_docstars_staff.py
+++ recidiviz/ingest/direct/regions/us_nd/ingest_views/view_docstars_staff.py
@@ -1,5 +1,5 @@
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2020 Recidiviz, Inc.
+# Copyright (C) 2024 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,18 +26,30 @@ from recidiviz.utils.metadata import local_project_id_override
 VIEW_QUERY_TEMPLATE = """
 WITH staff_from_directory AS (
 SELECT DISTINCT
-    CAST(OFFICER AS INT64) AS OFFICER,
-    UPPER(LastName) AS LastName,
-    UPPER(FirstName) AS FirstName,
-    email
-FROM {RECIDIVIZ_REFERENCE_PP_directory}
+    OFFICER,
+    LastName,
+    FirstName,
+    email,
+    CAST(update_datetime AS DATETIME) AS RecDate
+FROM (
+    SELECT
+        CAST(OFFICER AS INT64) AS OFFICER,
+        UPPER(LastName) AS LastName,
+        UPPER(FirstName) AS FirstName,
+        email,
+        update_datetime,
+        ROW_NUMBER() OVER (PARTITION BY OFFICER ORDER BY update_datetime) AS recency_rank
+    FROM {RECIDIVIZ_REFERENCE_PP_directory@ALL}
+    )
+WHERE recency_rank = 1
 ), 
-officers_with_recency_rank AS (
-SELECT 
+staff_from_docstars AS (
+SELECT DISTINCT
     OFFICER,
     UPPER(LNAME) AS LastName,
     UPPER(FNAME) AS FirstName,
-    EMAIL 
+    EMAIL,
+    CAST(RecDate AS DATETIME) AS RecDate
 FROM (
     SELECT 
         CAST(OFFICER AS INT64) AS OFFICER,
@@ -46,26 +58,36 @@ FROM (
         -- Null out emails for entries like "Bismarck CS", which sometimes appear
         -- as terminating officers in supervision periods, but are not real people.
         IF(LOGINNAME LIKE '%% %%', NULL, CONCAT(LOWER(LOGINNAME), '@nd.gov')) AS EMAIL,
+        RecDate,
         ROW_NUMBER() OVER (
             PARTITION BY CAST(OFFICER AS INT64)
             ORDER BY CAST(RecDate AS DATETIME) DESC) AS recency_rank
     FROM {docstars_officers}
-    WHERE CAST(OFFICER AS INT64) NOT IN (
-        SELECT DISTINCT OFFICER 
-        FROM staff_from_directory)
     ) sub
 WHERE recency_rank = 1
 )
-SELECT 
+-- Choose the most recent available version of the officer's name, in case it has been
+-- changed in the nightly raw data but not yet in the manual roster.
+SELECT DISTINCT
     OFFICER,
-    LastName,
-    FirstName,
-    EMAIL
-FROM (
-    SELECT * FROM staff_from_directory
-    UNION ALL
-    SELECT * FROM officers_with_recency_rank
-) sub
+    -- If staff_from_directory.RecDate is NULL, result of comparison will be unknown and 
+    -- logic will choose staff_from_docstars.LastName. If staff_from_docstars.RecDate is NULL, 
+    -- will choose staff_from_directory.LastName because of IFNULL() statement.
+    CASE WHEN staff_from_directory.RecDate > IFNULL(staff_from_docstars.RecDate, '1000-01-01')
+        THEN staff_from_directory.LastName 
+        ELSE staff_from_docstars.LastName
+    END AS LastName,
+    CASE WHEN staff_from_directory.RecDate > IFNULL(staff_from_docstars.RecDate, '1000-01-01')
+        THEN staff_from_directory.FirstName 
+        ELSE staff_from_docstars.FirstName
+    END AS FirstName,
+    CASE WHEN staff_from_directory.RecDate > IFNULL(staff_from_docstars.RecDate, '1000-01-01')
+        THEN staff_from_directory.EMAIL 
+        ELSE staff_from_docstars.EMAIL
+    END AS EMAIL,
+FROM staff_from_directory
+FULL OUTER JOIN staff_from_docstars 
+USING(OFFICER)
 """
 
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
