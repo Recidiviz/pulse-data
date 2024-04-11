502b647bc289cf65383ca9f14b9d85f4e9f87f12
Aaron Stromberg <aaron@recidiviz.org>
2024-04-11 06:34:48 -0700

    [US_CA] Fix supervisor periods 1 (Recidiviz/recidiviz-data#28851)
    ## Description of the change
    
    Fixes 2 edge cases and adds tests to prove it
    1. Single week supervision periods were making things weird. This fixes
    them.
    2. If a staff member joined a unit right as the supervisor left (or vice
    versa), we'd incorrectly create a 0 day period for them. This change
    ensures we no longer create that period.
    
    [Pipeline
    here](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-04-10_20_48_50-3773074543428416700;graphView=1?project=recidiviz-staging&pageState=(%22dfTime%22:(%22l%22:%22dfJobMaxTime%22)))
    -- it passed!
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#25555
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 4c8f6a601e5ff6f320d68aeb93eb8a6995908e52

--- recidiviz/ingest/direct/regions/us_ca/ingest_views/view_staff_supervisor_periods.py
+++ recidiviz/ingest/direct/regions/us_ca/ingest_views/view_staff_supervisor_periods.py
@@ -42,203 +42,209 @@ from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_QUERY_TEMPLATE = """
 WITH agents AS (
+  SELECT
+    DISTINCT BadgeNumber,
+    ParoleUnit,
+    update_datetime,
+  FROM {AgentParole@ALL}
+  WHERE
+    BadgeNumber IS NOT NULL
+    AND AgentClassification IN ('Parole Agent II, AP (Supv)', 'Parole Agent III, AP')
+),
+-- This CTE will contain all update datetimes crossed with all badge numbers. We'll
+-- use this later to see which parole unit(s) each supervisor is associated with at
+-- any given time, with nulls in between. We call it possible_ParoleUnit and
+-- possible_BadgeNumber because we don't know if there was actually a supervisor then
+-- until we join back to agents in the next CTE.
+all_dates_cross_badge_units AS (
+  SELECT
+    a.BadgeNumber AS possible_BadgeNumber,
+    a.ParoleUnit AS possible_ParoleUnit,
+    ud.update_datetime AS start_date,
+    LEAD(ud.update_datetime) OVER (PARTITION BY a.BadgeNumber, a.ParoleUnit ORDER BY update_datetime) AS end_date,
+  FROM 
+    (SELECT DISTINCT update_datetime FROM {AgentParole@ALL}) ud,
+    (SELECT DISTINCT BadgeNumber, ParoleUnit FROM agents) a 
+),
+filled_all_dates AS (
+  SELECT
+    ad.start_date,
+    ad.end_date,
+    ad.possible_ParoleUnit,
+    ad.possible_BadgeNumber,
+    a.BadgeNumber, -- If there is actually a supervisor, this will be populated
+    a.ParoleUnit,
+  FROM all_dates_cross_badge_units ad
+  LEFT JOIN agents a
+  ON
+    a.BadgeNumber = ad.possible_BadgeNumber
+    AND a.update_datetime = ad.start_date
+    AND a.ParoleUnit = ad.possible_ParoleUnit 
+),
+proto_supervisor_periods AS (
+  SELECT
+    start_date,
+    end_date,
+    possible_ParoleUnit,
+    possible_BadgeNumber,
+    BadgeNumber,
+    ParoleUnit,
+    LAG(ParoleUnit) OVER (PARTITION BY possible_BadgeNumber, possible_ParoleUnit ORDER BY start_date) IS NOT NULL AS previous_unit_is_same,
+    LEAD(ParoleUnit) OVER (PARTITION BY possible_BadgeNumber, possible_ParoleUnit ORDER BY start_date) IS NOT NULL AS next_unit_is_same
+  FROM
+    filled_all_dates 
+  WHERE True
+  -- The result of this QUALIFY will be to have two proto periods that will eventually
+  -- become one. The first proto period reflects the first week this supervisor worked
+  -- within this unit, and the next protoperiod (if it exists) will be the last week
+  -- this person worked on the unit. In the next CTE, we take the start date from the
+  -- first proto period and the end date from the second to make our final period.
+  QUALIFY (
+    ParoleUnit IS NOT NULL AND (
+      -- This clause captures when a supervisor starts in a new unit. Specifically:
+      -- 1. the unit side of null -> unit transitions
+      -- 2. the B side of unit A -> unit B transitions
+      -- (previous_parole_unit_continued is null OR previous_parole_unit_continued <> ParoleUnit)
+      NOT previous_unit_is_same OR
+      -- This clause captures when a supervisor ends with a unit. Specifically:
+      -- 1. the unit side of unit -> null transitions
+      -- 2. the A side of unit A -> unit B transitions
+      NOT next_unit_is_same
+    )
+  ) 
+),
+proto_supervisor_periods_2 AS (
+  SELECT
+    start_date,
+    -- Usually we want to use the next periods end date, but if it's a single week period, 
+    -- we already have the correct end date.
+    IF(NOT previous_unit_is_same AND NOT next_unit_is_same,
+      end_date,
+      LEAD(end_date) OVER (PARTITION BY BadgeNumber, ParoleUnit ORDER BY start_date)
+    ) as end_date,
+    BadgeNumber,
+    ParoleUnit,
+    previous_unit_is_same,
+    next_unit_is_same
+  FROM
+    proto_supervisor_periods
+),
+-- This CTE simply drops the "second" proto periods since we already got the end dates
+-- from them in the proto_supervisor_periods_2. We have to use a separate CTE for this
+-- because WHERE clauses are evaluated before window functions.
+collapsed_supervisor_periods AS (
+  SELECT *
+  FROM proto_supervisor_periods_2
+  WHERE NOT previous_unit_is_same
+),
+-- This CTE starts to identify where agents have caseloads.
+staff_location_periods AS (
+  WITH staff_PersonParole AS (
     SELECT
-      DISTINCT BadgeNumber,
+      BadgeNumber,
       ParoleUnit,
       update_datetime,
-    FROM {AgentParole@ALL}
+      -- TODO(#28597) Supposedly officers can only supervise folks in a single
+      -- district at a time. However, we have 56 agents on specific days when this
+      -- does not appear to be true. In these situations, we just say the officer is
+      -- supervised in whatever unit they have the most clients in. We use the qualify
+      -- statement below to assign PAs to the case where they have the most cases.
+      COUNT(DISTINCT OffenderId) caseload_in_unit
+    FROM
+      {PersonParole@ALL}
     WHERE
       BadgeNumber IS NOT NULL
-      AND AgentClassification IN ('Parole Agent II, AP (Supv)', 'Parole Agent III, AP')
-  ),
-  -- This CTE will contain all update datetimes crossed with all badge numbers. We'll
-  -- use this later to see which parole unit(s) each supervisor is associated with at
-  -- any given time, with nulls in between. We call it possible_ParoleUnit and
-  -- possible_BadgeNumber because we don't know if there was actually a supervisor then
-  -- until we join back to agents in the next CTE.
-  all_dates_cross_badge_units AS (
-    SELECT
-      a.BadgeNumber AS possible_BadgeNumber,
-      a.ParoleUnit AS possible_ParoleUnit,
-      ud.update_datetime AS start_date,
-      LEAD(ud.update_datetime) OVER (PARTITION BY a.BadgeNumber, a.ParoleUnit ORDER BY update_datetime) AS end_date,
-    FROM 
-      (SELECT DISTINCT update_datetime FROM {PersonParole@ALL}) ud,
-      (SELECT DISTINCT BadgeNumber, ParoleUnit FROM agents) a 
-  ),
-  filled_all_dates AS (
-    SELECT
-      ad.start_date,
-      ad.end_date,
-      ad.possible_ParoleUnit,
-      ad.possible_BadgeNumber,
-      a.BadgeNumber, -- If there is actually a supervisor, this will be populated
-      a.ParoleUnit,
-    FROM all_dates_cross_badge_units ad
-    LEFT JOIN agents a
-    ON
-      a.BadgeNumber = ad.possible_BadgeNumber
-      AND a.update_datetime = ad.start_date
-      AND a.ParoleUnit = ad.possible_ParoleUnit 
-  ),
-  proto_supervisor_periods AS (
-    SELECT
-      start_date,
-      end_date,
-      possible_ParoleUnit,
-      possible_BadgeNumber,
+    GROUP BY
       BadgeNumber,
       ParoleUnit,
-      LAG(ParoleUnit) OVER (PARTITION BY possible_BadgeNumber, possible_ParoleUnit ORDER BY start_date) IS NOT NULL AS continuing_in_same_unit,
-      LEAD(ParoleUnit) OVER (PARTITION BY possible_BadgeNumber, possible_ParoleUnit ORDER BY start_date) IS NOT NULL AS continuing_onto_same_unit
+      update_datetime 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY BadgeNumber, update_datetime ORDER BY caseload_in_unit DESC) = 1 
+  ),
+  prioritized AS (
+    SELECT
+      BadgeNumber,
+      ParoleUnit,
+      update_datetime,
+      LAG(ParoleUnit) OVER (PARTITION BY BadgeNumber ORDER BY update_datetime) AS previous_parole_unit
     FROM
-      filled_all_dates 
+      staff_PersonParole 
     WHERE True
-    -- The result of this QUALIFY will be to have two proto periods that will eventually
-    -- become one. The first proto period reflects the first week this supervisor worked
-    -- within this unit, and the next protoperiod (if it exists) will be the last week
-    -- this person worked on the unit. In the next CTE, we take the start date from the
-    -- first proto period and the end date from the second to make our final period.
-    QUALIFY (
-      ParoleUnit IS NOT NULL AND (
-        -- This clause captures when a supervisor starts in a new unit. Specifically:
-        -- 1. the unit side of null -> unit transitions
-        -- 2. the B side of unit A -> unit B transitions
-        -- (previous_parole_unit_continued is null OR previous_parole_unit_continued <> ParoleUnit)
-        NOT continuing_in_same_unit OR
-        -- This clause captures when a supervisor ends with a unit. Specifically:
-        -- 1. the unit side of unit -> null transitions
-        -- 2. the A side of unit A -> unit B transitions
-        NOT continuing_onto_same_unit
-      )
-    ) 
-  ),
-  proto_supervisor_periods_2 AS (
+    QUALIFY previous_parole_unit IS NULL OR previous_parole_unit != ParoleUnit 
+  )
+  SELECT
+    BadgeNumber,
+    ParoleUnit,
+    update_datetime AS start_date,
+    LEAD(update_datetime) OVER (PARTITION BY BadgeNumber ORDER BY update_datetime) AS end_date
+  FROM prioritized 
+),
+-- This is a relatively complicated CTE! It unions two tables together, each of which
+-- starts with staff location periods (which is where agents have caseloads) and joins
+-- in supervisor periods. 
+-- 
+-- The first table in the union merges in periods during which a supervisor moves into
+-- a unit while an agent is already in the unit (therefore a new supervisor period
+-- needs to be started for this agent and others in the unit).
+--
+-- The second table in the union considers when a supervisor is already working in a
+-- unit when the staff joins the unit. The start date here will always be when the
+-- staff joined the unit, but the period may end if the supervisor changes units
+-- before the staff member does.
+supervisors_joined_to_supervisees AS (
+  WITH all_starts AS (
     SELECT
-      start_date,
-      LEAD(end_date) OVER (PARTITION BY BadgeNumber, ParoleUnit ORDER BY start_date) AS end_date,
-      BadgeNumber,
-      ParoleUnit,
-      continuing_in_same_unit
+      slp.BadgeNumber AS supervisee_BadgeNumber,
+      slp.ParoleUnit AS ParoleUnit,
+      csp_moving_into_unit.start_date AS start_date,
+      LEAST(IFNULL(csp_moving_into_unit.end_date, '9999-12-31'), IFNULL(slp.end_date, '9999-12-31')) AS end_date,
+      csp_moving_into_unit.BadgeNumber AS supervisor_BadgeNumber
     FROM
-      proto_supervisor_periods
-  ),
-  -- This CTE simply drops the "second" proto periods since we already got the end dates
-  -- from them in the proto_supervisor_periods_2. We have to use a separate CTE for this
-  -- because WHERE clauses are evaluated before window functions.
-  collapsed_supervisor_periods AS (
-    SELECT *
-    FROM proto_supervisor_periods_2
-    WHERE NOT continuing_in_same_unit
-  ),
-  -- This CTE starts to identify where agents have caseloads.
-  staff_location_periods AS (
-    WITH staff_PersonParole AS (
-      SELECT
-        BadgeNumber,
-        ParoleUnit,
-        update_datetime,
-        -- TODO(#28597) Supposedly officers can only supervise folks in a single
-        -- district at a time. However, we have 56 agents on specific days when this
-        -- does not appear to be true. In these situations, we just say the officer is
-        -- supervised in whatever unit they have the most clients in. We use the qualify
-        -- statement below to assign PAs to the case where they have the most cases.
-        COUNT(DISTINCT OffenderId) caseload_in_unit
-      FROM
-        {PersonParole@ALL}
-      WHERE
-        BadgeNumber IS NOT NULL
-      GROUP BY
-        BadgeNumber,
-        ParoleUnit,
-        update_datetime 
-      QUALIFY ROW_NUMBER() OVER (PARTITION BY BadgeNumber, update_datetime ORDER BY caseload_in_unit DESC) = 1 
-    ),
-    prioritized AS (
-      SELECT
-        BadgeNumber,
-        ParoleUnit,
-        update_datetime,
-        LAG(ParoleUnit) OVER (PARTITION BY BadgeNumber ORDER BY update_datetime) AS previous_parole_unit
-      FROM
-        staff_PersonParole 
-      WHERE True
-      QUALIFY previous_parole_unit IS NULL OR previous_parole_unit != ParoleUnit 
-    )
-    SELECT
-      BadgeNumber,
-      ParoleUnit,
-      update_datetime AS start_date,
-      LEAD(update_datetime) OVER (PARTITION BY BadgeNumber ORDER BY update_datetime) AS end_date
-    FROM prioritized 
-  ),
-  -- This is a relatively complicated CTE! It unions two tables together, each of which
-  -- starts with staff location periods (which is where agents have caseloads) and joins
-  -- in supervisor periods. 
-  -- 
-  -- The first table in the union merges in periods during which a supervisor moves into
-  -- a unit while an agent is already in the unit (therefore a new supervisor period
-  -- needs to be started for this agent and others in the unit).
-  --
-  -- The second table in the union considers when a supervisor is already working in a
-  -- unit when the staff joins the unit. The start date here will always be when the
-  -- staff joined the unit, but the period may end if the supervisor changes units
-  -- before the staff member does.
-  supervisors_joined_to_supervisees AS (
-    WITH all_starts AS (
-      SELECT
-        slp.BadgeNumber AS supervisee_BadgeNumber,
-        slp.ParoleUnit AS ParoleUnit,
-        csp_moving_into_unit.start_date AS start_date,
-        LEAST(IFNULL(csp_moving_into_unit.end_date, '9999-12-31'), IFNULL(slp.end_date, '9999-12-31')) AS end_date,
-        csp_moving_into_unit.BadgeNumber AS supervisor_BadgeNumber
-      FROM
-        staff_location_periods slp
-        -- Join in starts caused by supervisors moving into the unit
-      LEFT JOIN collapsed_supervisor_periods csp_moving_into_unit
-      ON
-        csp_moving_into_unit.ParoleUnit = slp.ParoleUnit AND (
-          csp_moving_into_unit.start_date >= slp.start_date AND (
-            csp_moving_into_unit.start_date <= slp.end_date OR slp.end_date IS NULL
-          )
+      staff_location_periods slp
+      -- Join in starts caused by supervisors moving into the unit
+    LEFT JOIN collapsed_supervisor_periods csp_moving_into_unit
+    ON
+      csp_moving_into_unit.ParoleUnit = slp.ParoleUnit AND (
+        csp_moving_into_unit.start_date >= slp.start_date AND (
+          csp_moving_into_unit.start_date < slp.end_date OR slp.end_date IS NULL
         )
-      
-      UNION DISTINCT
-      
-      SELECT
-        slp.BadgeNumber AS supervisee_BadgeNumber,
-        slp.ParoleUnit AS ParoleUnit,
-        slp.start_date AS start_date,
-        LEAST(IFNULL(csp_already_supervising_unit.end_date, '9999-12-31'), IFNULL(slp.end_date, '9999-12-31')),
-        csp_already_supervising_unit.BadgeNumber AS supervisor_BadgeNumber
-      FROM staff_location_periods slp
-        -- Join in starts caused by supervisees moving into the unit
-      LEFT JOIN collapsed_supervisor_periods csp_already_supervising_unit
-      ON
-        csp_already_supervising_unit.ParoleUnit = slp.ParoleUnit AND (
-          csp_already_supervising_unit.start_date <= slp.start_date AND (
-            csp_already_supervising_unit.end_date >= slp.start_date OR csp_already_supervising_unit.end_date IS NULL
-          )
-        ) 
-    ),
-    -- Normalizes by:
-    -- 1) removing non-supervisor periods (which is where supervisor_BadgeNumber is null)
-    -- 2) returning magic end dates to null
-    normalized AS (
-      SELECT
-        supervisee_BadgeNumber,
-        ParoleUnit,
-        start_date,
-        IF(end_date = '9999-12-31', NULL, end_date) AS end_date,
-        supervisor_BadgeNumber
-      FROM all_starts
-      WHERE supervisor_BadgeNumber IS NOT NULL 
-    )
-    SELECT *
-    FROM normalized 
+      )
+    
+    UNION DISTINCT
+    
+    SELECT
+      slp.BadgeNumber AS supervisee_BadgeNumber,
+      slp.ParoleUnit AS ParoleUnit,
+      slp.start_date AS start_date,
+      LEAST(IFNULL(csp_already_supervising_unit.end_date, '9999-12-31'), IFNULL(slp.end_date, '9999-12-31')),
+      csp_already_supervising_unit.BadgeNumber AS supervisor_BadgeNumber
+    FROM staff_location_periods slp
+      -- Join in starts caused by supervisees moving into the unit
+    LEFT JOIN collapsed_supervisor_periods csp_already_supervising_unit
+    ON
+      csp_already_supervising_unit.ParoleUnit = slp.ParoleUnit AND (
+        csp_already_supervising_unit.start_date <= slp.start_date AND (
+          csp_already_supervising_unit.end_date > slp.start_date OR csp_already_supervising_unit.end_date IS NULL
+        )
+      ) 
+  ),
+  -- Normalizes by:
+  -- 1) removing non-supervisor periods (which is where supervisor_BadgeNumber is null)
+  -- 2) returning magic end dates to null
+  normalized AS (
+    SELECT
+      supervisee_BadgeNumber,
+      ParoleUnit,
+      start_date,
+      IF(end_date = '9999-12-31', NULL, end_date) AS end_date,
+      supervisor_BadgeNumber
+    FROM all_starts
+    WHERE supervisor_BadgeNumber IS NOT NULL 
   )
   SELECT *
-  FROM supervisors_joined_to_supervisees
+  FROM normalized 
+)
+SELECT *
+FROM supervisors_joined_to_supervisees
 """
 
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
