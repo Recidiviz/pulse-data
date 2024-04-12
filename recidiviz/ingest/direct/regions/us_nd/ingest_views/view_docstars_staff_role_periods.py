b0174202214e103e0ab58a26a6ba1b92ce30801b
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-04-12 11:07:25 -0400

    [US_ND] Tweak StateStaffRolePeriod view (Recidiviz/recidiviz-data#28890)
    ## Description of the change
    
    There was a bug in the `docstars_staff_role_periods` view in ND that was
    causing officer role periods to close prematurely. There were two things
    causing this:
    1. Data from the `docstars_officers` table was not filtered to only
    include rows where an officer was active, so it included all officers in
    all updates, which threw off the `last_appearance_datetime` and
    `edge_date` fields carried forward from the `staff_from_docstars` CTE.
    2. The end_date logic in the `all_periods` CTE was checking that there
    was no subsequent critical date, and that the `edge_date <
    last_file_update_datetime` to determine that we should close a period.
    This means that it was checking if the update to a person's employment
    in the row in question happened earlier than the last time we received a
    file, and using that information to determine if a person stopped
    working. I think the correct logic here is to check if the update in the
    row in question happened earlier than the latest time _this person_
    appeared in the data, and rely more heavily on the fact that there is no
    subsequent critical date. This will hold open periods where the
    edge_date is the same as the most recent update about this person, even
    when the person does not have a subsequent critical date, which was not
    the case before this change.
    
    Tests:
    
    - [x] view query is deterministic
    - [x] external IDs are unique
    - [x] run_mappings script passes
    - [x] test ingest
    [pipeline](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-04-12_07_48_58-13169969324237089203;graphView=0?project=recidiviz-123&pageState=(%22dfTime%22:(%22l%22:%22dfJobMaxTime%22)))
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: ff0e9964a47f5167eb164977efcc0bbb8d8eeaac

--- recidiviz/ingest/direct/regions/us_nd/ingest_views/view_docstars_staff_role_periods.py
+++ recidiviz/ingest/direct/regions/us_nd/ingest_views/view_docstars_staff_role_periods.py
@@ -33,8 +33,12 @@ from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_QUERY_TEMPLATE = """
 WITH staff_from_directory AS (
+-- This CTE pulls in relevant data from the monthly P&P directories that we receive
+-- via email and upload as raw data. It uses the update_datetime on each file to 
+-- track edge dates, as well as the most recent file we received (last_file_update_datetime),
+-- and the most recent file that each employee appeared in (last_appearance_datetime).
     SELECT
-        -- make officer IDs uniform across sources 
+        -- Make officer IDs uniform across sources 
         CAST(OFFICER AS INT64) AS OFFICER,
         CAST(update_datetime AS DATETIME) AS edge_date,
         '(1)' AS STATUS, 
@@ -44,17 +48,28 @@ WITH staff_from_directory AS (
     FROM {RECIDIVIZ_REFERENCE_PP_directory@ALL}
 ),
 staff_from_docstars AS (
-    SELECT DISTINCT
-        -- make officer IDs uniform across sources 
+-- This CTE gets the officer data directly from the Docstars system into the same format
+-- as above, to the extent possible. In Docstars, RecDate is the date the row was created.
+-- There is an additional filtering step necessary here because in Docstars, officers
+-- do not stop appearing like they do in the directory when they become inactive. They
+-- appear in perpetuity with STATUS = '0'. To accurately track the date an officer
+-- became inactive, we filter the Docstars data to only include rows where STATUS='(1)'.
+    SELECT * FROM (
+        SELECT DISTINCT
+        -- Make officer IDs uniform across sources 
         CAST(OFFICER AS INT64) AS OFFICER,
         CAST(RecDate AS DATETIME) AS edge_date, 
         STATUS,
         MAX(CAST(RecDate AS DATETIME)) OVER (PARTITION BY TRUE) AS last_file_update_datetime,
         MAX(CAST(RecDate AS DATETIME)) OVER (PARTITION BY OFFICER) AS last_appearance_datetime, 
+        -- There is no JobTitle information in this table in Docstars.
         CAST(NULL AS STRING) AS JobTitle,
-    FROM {docstars_officers@ALL} 
+    FROM {docstars_officers@ALL} )
+    WHERE STATUS = '(1)' 
 ),
 combined_data AS (
+-- This CTE combines the data from the two sources. Officers often appear in both, 
+-- so we want to consider all rows from both sources when creating periods.
     SELECT 
         OFFICER,
         edge_date,
@@ -76,6 +91,10 @@ combined_data AS (
     FROM staff_from_docstars 
 ),
 critical_dates AS (
+-- This CTE filters the combined data to include only the rows that are relevant to the
+-- role periods we are constructing. These are rows where an officer just started working
+-- or changed roles. We also include all rows where the edge date is the last update
+-- we have about a given officer, to account for rows that signify an officer becoming inactive.
     SELECT *
     FROM (
         SELECT
@@ -92,28 +111,40 @@ critical_dates AS (
             last_file_update_datetime,
             last_appearance_datetime 
         FROM combined_data
-        WINDOW person_window AS (PARTITION BY OFFICER ORDER BY edge_date)
+        WINDOW person_window AS (PARTITION BY OFFICER ORDER BY edge_date, last_file_update_datetime DESC)
         ) cd
         WHERE 
         -- officer just started working
-        (cd.prev_JobTitle IS NULL AND cd.JobTitle IS NOT NULL) 
+        ((cd.prev_JobTitle IS NULL AND cd.JobTitle IS NOT NULL) 
+        OR STATUS = '(1)' AND prev_status IS NULL)
         -- officer changed roles
         OR cd.JobTitle != cd.prev_JobTitle
         -- include the latest update even if the previous two conditions are not true
         OR edge_date = last_file_update_datetime
-        WINDOW person_window AS (PARTITION BY OFFICER ORDER BY edge_date)
+        WINDOW person_window AS (PARTITION BY OFFICER ORDER BY edge_date, last_file_update_datetime DESC)
 ), all_periods AS (
+-- This CTE constructs periods using the compiled critical dates. End dates follow this 
+-- logic: 
+    -- 1. If a person stopped working, close their employment period on the last date we 
+    --    receive data that included them as an active employee.
+    -- 2. If there is a subsequent row showing a person changed roles, close the existing 
+    --    period on the date of that change.
+    -- 3. If a there is no subsequent row, and the edge date is the same as the most 
+    --    recent date the person has appeared in the data, then leave the period open
+    --    to signify that the existing period is still active.
 SELECT 
     OFFICER,
     STATUS,
     edge_date AS start_date,
     CASE 
-        -- If a staff member stops appearing in the roster, close their employment period
-        -- on the last date we receive a roster that included them
+        -- If a staff member stops appearing as active in Docstars, or stops appearing in 
+        -- the P&P directory entirely, close their employment period on the last date we 
+        -- receive data that included them as an active officer. 
         WHEN LEAD(edge_date) OVER person_window IS NULL 
-            AND edge_date < last_file_update_datetime
+            AND edge_date < last_appearance_datetime
             THEN last_appearance_datetime 
-        -- There is a more recent update to this person's location
+        -- If there is a more recent update to this person's role, close the existing
+        -- period on the date of the update.
         WHEN LEAD(edge_date) OVER person_window IS NOT NULL 
             THEN LEAD(edge_date) OVER person_window 
         -- All currently-employed staff will appear in the latest roster
@@ -122,16 +153,19 @@ SELECT
     JobTitle
 FROM critical_dates
 WHERE STATUS IS NOT NULL
-AND STATUS != '0' -- exclude periods that start with employment termination
-WINDOW person_window AS (PARTITION BY OFFICER ORDER BY edge_date, JobTitle)
+WINDOW person_window AS (PARTITION BY OFFICER ORDER BY edge_date, last_file_update_datetime DESC)
 )
 SELECT
+-- This CTE cleans up the final periods. 
     OFFICER,
     JobTitle,
     start_date,
     end_date,
     ROW_NUMBER() OVER (PARTITION BY OFFICER ORDER BY start_date) AS period_seq_num,
 FROM all_periods
+WHERE 
+    start_date != end_date
+    OR end_date IS NULL
 """
 
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
