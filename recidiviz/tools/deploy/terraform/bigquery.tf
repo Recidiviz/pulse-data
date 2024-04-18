6051c8b65a42703c9d436ac23497a8c9606e50cb
Dan Hansen <dan@recidiviz.org>
2024-04-18 12:14:01 -0700

    [Terraform] Move source table schema definitions to `recidiviz/big_query/source_tables/schema` (Recidiviz/recidiviz-data#29147)
    ## Description of the change
    As part loading our view graph into the emulator, all referenced source
    tables must be accessible in code. This moves the Terraform-defined
    schemas into our new home for source table definitions
    
    No BigQuery diffs in the Terraform plan:
    
    ```
    Terraform used the selected providers to generate the following execution plan. Resource actions are indicated with the following symbols:
      ~ update in-place
    
    Terraform will perform the following actions:
    
      # google_cloud_run_service.admin_panel will be updated in-place
      ~ resource "google_cloud_run_service" "admin_panel" {
            id                         = "locations/us-central1/namespaces/recidiviz-staging/services/admin-panel"
            name                       = "admin-panel"
            # (4 unchanged attributes hidden)
    
          ~ template {
              ~ metadata {
                  ~ annotations = {
                      - "run.googleapis.com/vpc-access-egress"    = "private-ranges-only" -> null
                        # (4 unchanged elements hidden)
                    }
                    # (2 unchanged attributes hidden)
                }
    
                # (1 unchanged block hidden)
            }
    
            # (2 unchanged blocks hidden)
        }
    
      # google_cloud_scheduler_job.hydrate_admin_panel_cache will be updated in-place
      ~ resource "google_cloud_scheduler_job" "hydrate_admin_panel_cache" {
            id               = "projects/recidiviz-staging/locations/us-central1/jobs/hydrate-admin-panel-cache"
            name             = "hydrate-admin-panel-cache"
            # (8 unchanged attributes hidden)
    
          ~ http_target {
              ~ headers     = {
                  + "User-Agent"   = "Google-Cloud-Scheduler"
                    # (1 unchanged element hidden)
                }
                # (2 unchanged attributes hidden)
    
                # (1 unchanged block hidden)
            }
    
            # (1 unchanged block hidden)
        }
    
    Plan: 0 to add, 2 to change, 0 to destroy.
    ```
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#29140
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 97c5ae8fa989366517c1ee8dbebe0618ac20e186

--- recidiviz/tools/deploy/terraform/bigquery.tf
+++ recidiviz/tools/deploy/terraform/bigquery.tf
@@ -15,6 +15,10 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 
+locals {
+  source_tables = "${local.recidiviz_root}/big_query/source_tables/schema/"
+}
+
 resource "google_project_service" "bigquery_connection_api" {
   service = "bigqueryconnection.googleapis.com"
 
@@ -35,106 +39,7 @@ resource "google_bigquery_table" "validation_results" {
   description         = "This table contains the results from data validation runs."
   deletion_protection = false
 
-  schema = <<EOF
-[
-    {
-        "name": "run_id",
-        "type": "STRING",
-        "mode": "REQUIRED"
-    },
-    {
-        "name": "run_date",
-        "type": "DATE",
-        "mode": "REQUIRED"
-    },
-    {
-        "name": "system_version",
-        "type": "STRING",
-        "mode": "REQUIRED"
-    },
-    {
-        "name": "check_type",
-        "type": "STRING",
-        "mode": "REQUIRED"
-    },
-    {
-        "name": "validation_name",
-        "type": "STRING",
-        "mode": "REQUIRED"
-    },
-    {
-        "name": "region_code",
-        "type": "STRING",
-        "mode": "REQUIRED"
-    },
-    {
-        "name": "did_run",
-        "type": "BOOLEAN",
-        "mode": "REQUIRED"
-    },
-    {
-        "name": "run_datetime",
-        "type": "DATETIME",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "validation_category",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "was_successful",
-        "type": "BOOLEAN",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "validation_result_status",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "result_details_type",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "result_details",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "trace_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "exception_log",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "failure_description",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "runtime_seconds",
-        "type": "FLOAT64",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "ingest_instance",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "sandbox_dataset_prefix",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    }
-]
-EOF
-
+  schema = jsonencode(yamldecode(file("${local.source_tables}/validation_results/validation_results.yaml"))["schema"])
 }
 
 module "supplemental_generated_dataset" {
@@ -162,51 +67,7 @@ resource "google_bigquery_table" "workflows_client_record_archive" {
     source_uris           = ["gs://${var.project_id}-practices-etl-data-archive/*/client_record.json"]
   }
 
-  schema = <<EOF
-[
-    {
-        "name": "person_external_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "pseudonymized_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "district",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "compliant_reporting_eligible",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "remaining_criteria_needed",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "officer_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "expiration_date",
-        "type": "DATE",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "all_eligible_opportunities",
-        "type": "STRING",
-        "mode": "REPEATED"
-    }
-]
-EOF
-
+  schema = jsonencode(yamldecode(file("${local.source_tables}/${module.export_archives_dataset.dataset_id}/workflows_client_record_archive.yaml"))["schema"])
 }
 
 resource "google_bigquery_table" "workflows_resident_record_archive" {
@@ -222,41 +83,7 @@ resource "google_bigquery_table" "workflows_resident_record_archive" {
     source_uris           = ["gs://${var.project_id}-practices-etl-data-archive/*/resident_record.json"]
   }
 
-  schema = <<EOF
-[
-    {
-        "name": "person_external_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "pseudonymized_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "person_id",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "officer_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "facility_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "all_eligible_opportunities",
-        "type": "STRING",
-        "mode": "REPEATED"
-    }
-]
-EOF
-
+  schema = jsonencode(yamldecode(file("${local.source_tables}/${module.export_archives_dataset.dataset_id}/workflows_resident_record_archive.yaml"))["schema"])
 }
 
 # This legacy table should be used to add any additional compliant reporting legacy fields from `client_record`
@@ -274,52 +101,7 @@ resource "google_bigquery_table" "workflows_legacy_client_record_archive" {
     source_format         = "NEWLINE_DELIMITED_JSON"
     source_uris           = ["gs://${var.project_id}-practices-etl-data-archive/*/client_record.json"]
   }
-
-  schema = <<EOF
-[
-    {
-        "name": "person_external_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "compliant_reporting_eligible",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "remaining_criteria_needed",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_time_on_supervision_level",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_drug_screen",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_fines_fees",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_recent_rejection",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_serious_sanctions",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    }
-]
-EOF
-
+  schema = jsonencode(yamldecode(file("${local.source_tables}/${module.export_archives_dataset.dataset_id}/workflows_legacy_client_record_archive.yaml"))["schema"])
 }
 
 resource "google_bigquery_table" "workflows_compliant_reporting_referral_record_archive" {
@@ -334,51 +116,7 @@ resource "google_bigquery_table" "workflows_compliant_reporting_referral_record_
     source_format         = "NEWLINE_DELIMITED_JSON"
     source_uris           = ["gs://${var.project_id}-practices-etl-data-archive/*/compliant_reporting_referral_record.json"]
   }
-
-  schema = <<EOF
-[
-    {
-        "name": "tdoc_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "remaining_criteria_needed",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "compliant_reporting_eligible",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_time_on_supervision_level",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_drug_screen",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_fines_fees",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_recent_rejection",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "almost_eligible_serious_sanctions",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    }
-]
-EOF
+  schema = jsonencode(yamldecode(file("${local.source_tables}/${module.export_archives_dataset.dataset_id}/workflows_compliant_reporting_referral_record_archive.yaml"))["schema"])
 }
 
 resource "google_bigquery_table" "outliers_supervision_officer_outlier_status_archive" {
@@ -393,61 +131,7 @@ resource "google_bigquery_table" "outliers_supervision_officer_outlier_status_ar
     source_format         = "CSV"
     source_uris           = ["gs://${var.project_id}-outliers-etl-data-archive/*/supervision_officer_outlier_status.csv"]
   }
-
-  schema = <<EOF
-[
-    {
-        "name": "state_code",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "officer_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "metric_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "period",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "end_date",
-        "type": "DATE",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "metric_rate",
-        "type": "FLOAT",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "caseload_type",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "target",
-        "type": "FLOAT",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "threshold",
-        "type": "FLOAT",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "status",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    }
-]
-EOF
+  schema = jsonencode(yamldecode(file("${local.source_tables}/${module.export_archives_dataset.dataset_id}/outliers_supervision_officer_outlier_status_archive.yaml"))["schema"])
 
 }
 
@@ -463,46 +147,7 @@ resource "google_bigquery_table" "outliers_supervision_officer_supervisors_archi
     source_format         = "CSV"
     source_uris           = ["gs://${var.project_id}-outliers-etl-data-archive/*/supervision_officer_supervisors.csv"]
   }
-
-  schema = <<EOF
-[
-    {
-        "name": "state_code",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "external_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "staff_id",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "full_name",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "pseudonymized_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "supervision_district",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "email",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    }
-]
-EOF
+  schema = jsonencode(yamldecode(file("${local.source_tables}/${module.export_archives_dataset.dataset_id}/outliers_supervision_officer_supervisors_archive.yaml"))["schema"])
 
 }
 
@@ -518,52 +163,7 @@ resource "google_bigquery_table" "outliers_supervision_officers_archive" {
     source_format         = "CSV"
     source_uris           = ["gs://${var.project_id}-outliers-etl-data-archive/*/supervision_officers.csv"]
   }
-
-  schema = <<EOF
-[
-    {
-        "name": "state_code",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "external_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "staff_id",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "full_name",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "pseudonymized_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "supervisor_external_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "supervision_district",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "specialized_caseload_type",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    }
-]
-EOF
-
+  schema = jsonencode(yamldecode(file("${local.source_tables}/${module.export_archives_dataset.dataset_id}/outliers_supervision_officers_archive.yaml"))["schema"])
 }
 
 resource "google_bigquery_table" "workflows_snooze_status_archive" {
@@ -578,55 +178,5 @@ resource "google_bigquery_table" "workflows_snooze_status_archive" {
     source_format         = "NEWLINE_DELIMITED_JSON"
     source_uris           = ["gs://${var.project_id}-snooze-statuses/*.json"]
   }
-
-  schema = <<EOF
-[
-    {
-        "name": "state_code",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "person_external_id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "opportunity_type",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "snoozed_by",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "snooze_start_date",
-        "type": "DATE",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "snooze_end_date",
-        "type": "DATE",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "denial_reasons",
-        "type": "STRING",
-        "mode": "REPEATED"
-    },
-    {
-        "name": "other_reason",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },
-    {
-        "name": "as_of",
-        "type": "DATE",
-        "mode": "NULLABLE"
-    }
-]
-EOF
-
+  schema = jsonencode(yamldecode(file("${local.source_tables}/${module.export_archives_dataset.dataset_id}/workflows_snooze_status_archive.yaml"))["schema"])
 }
