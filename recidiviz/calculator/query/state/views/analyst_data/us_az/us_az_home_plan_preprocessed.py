239555e1f20e6a9ade340687b3c98d38ff85bb89
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-04-18 13:02:13 -0400

    [US_AZ] Fix home plan preprocessed view (Recidiviz/recidiviz-data#29109)
    ## Description of the change
    There were a few issues with the view:
    1. I left the old `VIEW_BUILDER` variable in a new preprocessed view
    from the one that I copied for the template. This updates the view
    builder name so that the results will actually load to BQ.
    2. The `base` CTE referenced `APPROVAL_DATE` instead of `UPDATE_DATE`.
    That field actually represents the date that the most recent action was
    taken (i.e. home plan was approved, denied, or started), not necessarily
    the date it was approved, so `UPDATE_DATE` is a more accurate name for
    the field.
    3. A `SAFE_OFFSET` is needed in place of just an `OFFSET` in the
    `status_joined_to_IP` CTE.
    4. `CCL` was missing from the list of plan statuses that indicate a home
    plan is in progress.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 89ef78e01749de355a141e5507ee27a407f733e5

--- recidiviz/calculator/query/state/views/analyst_data/us_az/us_az_home_plan_preprocessed.py
+++ recidiviz/calculator/query/state/views/analyst_data/us_az/us_az_home_plan_preprocessed.py
@@ -20,7 +20,7 @@ in Arizona and attaches it to the relevant incarceration period."""
 from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
 from recidiviz.calculator.query.state.dataset_config import (
     ANALYST_VIEWS_DATASET,
-    STATE_BASE_DATASET,
+    NORMALIZED_STATE_DATASET,
 )
 from recidiviz.common.constants.states import StateCode
 from recidiviz.ingest.direct.dataset_config import raw_latest_views_dataset_for_region
@@ -31,7 +31,7 @@ from recidiviz.utils.metadata import local_project_id_override
 US_AZ_HOME_PLAN_PREPROCESSED_VIEW_NAME = "us_az_home_plan_preprocessed"
 
 US_AZ_HOME_PLAN_PREPROCESSED_VIEW_DESCRIPTION = """A view that gathers information about home plan preparedness for people incarcerated
-in Arizona."""
+in Arizona and attaches it to the relevant incarceration period."""
 
 US_AZ_HOME_PLAN_PREPROCESSED_QUERY_TEMPLATE = """
 WITH base AS (
@@ -41,7 +41,7 @@ SELECT DISTINCT
     HOME_PLAN_ID, 
     IS_HOMELESS_REQUEST, 
     lookups.DESCRIPTION AS APPROVAL_STATUS,
-    MAX(app.CREATE_DTM) OVER (PARTITION BY HOME_PLAN_DETAIL_ID) AS APPROVAL_DATE
+    MAX(app.CREATE_DTM) OVER (PARTITION BY HOME_PLAN_DETAIL_ID) AS UPDATE_DATE
 FROM {project_id}.{raw_data_up_to_date_views_dataset}.AZ_DOC_HOME_PLAN_latest hp
 LEFT JOIN {project_id}.{raw_data_up_to_date_views_dataset}.AZ_DOC_HOME_PLAN_DETAIL_latest det
 USING(HOME_PLAN_ID)
@@ -55,7 +55,7 @@ SELECT
     HOME_PLAN_DETAIL_ID,
     IS_HOMELESS_REQUEST,
     CASE WHEN 
-        APPROVAL_STATUS IN ('CCO SUP', 'COIII', 'CCO', 'COIV', 'Release Unit') THEN 'HOME PLAN IN PROGRESS'
+        APPROVAL_STATUS IN ('CCO SUP', 'COIII', 'CCO', 'COIV', 'Release Unit', 'CCL') THEN 'HOME PLAN IN PROGRESS'
         WHEN APPROVAL_STATUS IS NULL THEN 'HOME PLAN NOT STARTED'
         WHEN APPROVAL_STATUS = 'Address Approved' THEN 'HOME PLAN APPROVED'
         WHEN APPROVAL_STATUS = 'Address Denied' THEN 'HOME PLAN DENIED'
@@ -66,26 +66,25 @@ FROM base
 ), status_joined_to_IP AS (
     SELECT *, ip.incarceration_period_id, ip.person_id
     FROM base_with_status
-    -- Normalized state dataset is empty in AZ still, so use base state instead.
-    LEFT JOIN {project_id}.{state_dataset}.state_incarceration_period ip
-    ON(SPLIT(ip.external_id, '-')[OFFSET(1)] = base_with_status.DOC_ID)
+    LEFT JOIN {project_id}.{normalized_state_dataset}.state_incarceration_period ip
+    ON(SPLIT(ip.external_id, '-')[SAFE_OFFSET(1)] = base_with_status.DOC_ID)
 )
 SELECT * FROM status_joined_to_IP
 """
 
-INCARCERATION_INCIDENTS_PREPROCESSED_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+US_AZ_HOME_PLAN_PREPROCESSED_VIEW_BUILDER = SimpleBigQueryViewBuilder(
     dataset_id=ANALYST_VIEWS_DATASET,
     view_id=US_AZ_HOME_PLAN_PREPROCESSED_VIEW_NAME,
     description=US_AZ_HOME_PLAN_PREPROCESSED_VIEW_DESCRIPTION,
     view_query_template=US_AZ_HOME_PLAN_PREPROCESSED_QUERY_TEMPLATE,
-    state_dataset=STATE_BASE_DATASET,
     raw_data_up_to_date_views_dataset=raw_latest_views_dataset_for_region(
         state_code=StateCode.US_AZ,
         instance=DirectIngestInstance.PRIMARY,
     ),
+    normalized_state_dataset=NORMALIZED_STATE_DATASET,
     should_materialize=True,
 )
 
 if __name__ == "__main__":
     with local_project_id_override(GCP_PROJECT_STAGING):
-        INCARCERATION_INCIDENTS_PREPROCESSED_VIEW_BUILDER.build_and_print()
+        US_AZ_HOME_PLAN_PREPROCESSED_VIEW_BUILDER.build_and_print()
