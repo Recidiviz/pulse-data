e2dcf5e35454ce557b1e35d89f93b0814324fc0e
Emily Turner <149205316+emilyemilyemilyemilyemilyemily@users.noreply.github.com>
2024-04-18 08:33:33 -0700

    Add function to find views that query raw data directly (Recidiviz/recidiviz-data#28922)
    ## Description of the change
    
    https://www.notion.so/recidiviz/Enforcing-Raw-Data-Usage-Documentation-ef124923630542da92a4b8ef99a11cc9
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#28921
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x ] Tests have been written to cover the code changed/added as part
    of this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: a1bae4ee481665db9424390287a0a07be522d5b8

---
+++ recidiviz/tools/find_direct_raw_data_references.py
@@ -0,0 +1,97 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Find views that reference raw data tables/views directly.
+"""
+
+from collections import defaultdict
+from typing import Dict, List, Set
+
+from recidiviz.big_query.big_query_address import BigQueryAddress
+from recidiviz.big_query.big_query_view import BigQueryViewBuilder
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.dataset_config import (
+    raw_latest_views_dataset_for_region,
+    raw_tables_dataset_for_region,
+)
+from recidiviz.ingest.direct.regions.direct_ingest_region_utils import (
+    get_existing_direct_ingest_states,
+)
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.ingest.direct.views.direct_ingest_latest_view_collector import (
+    RAW_DATA_LATEST_VIEW_ID_SUFFIX,
+)
+
+
+def find_direct_raw_data_references(
+    view_builders: List[BigQueryViewBuilder],
+) -> Dict[StateCode, Dict[str, Set[BigQueryAddress]]]:
+    """
+    Finds direct references to raw data tables in the given list of BigQuery view builders.
+
+    A direct reference is identified when a view queries the raw data we receive from a state, rather than querying the state-agnostic format.
+
+    The returned dictionary has the following structure:
+    {
+        StateCode: {
+            FileTag: Set[BigQueryAddress]
+        }
+    }
+
+    - StateCode: Represents the state code of the source raw dataset.
+    - FileTag: Represents the table id of the raw data table. Note that there is no distinction made for latest views.
+               Ex, myFileTag_latest and myFileTag are both represented as myFileTag.
+    - BigQueryAddress: Represents the address of the BigQuery view that references the raw data table.
+
+    For example, a returned dictionary entry might look like this:
+    {
+        StateCode.US_MO: {
+            'LBAKRDTA_TAK033': {BigQueryAddress(dataset_id='analyst_data', table_id='us_mo_restrictive_housing_record')}
+        }
+    }
+    Note that the same view address may be included multiple times if it references multiple raw data tables.
+    """
+    raw_data_references: Dict[StateCode, Dict[str, Set[BigQueryAddress]]] = defaultdict(
+        lambda: defaultdict(set)
+    )
+    views = [builder.build(address_overrides=None) for builder in view_builders]
+    raw_datasets = _get_raw_datasets()
+    for view in views:
+        for parent_table in view.parent_tables:
+            # We don't count raw data references that are *in* raw data latest views
+            if view.dataset_id in raw_datasets:
+                continue
+
+            if parent_table.dataset_id in raw_datasets:
+                file_tag = parent_table.table_id.removesuffix(
+                    RAW_DATA_LATEST_VIEW_ID_SUFFIX
+                )
+                state_code = raw_datasets[parent_table.dataset_id]
+                raw_data_references[state_code][file_tag].add(view.address)
+    return raw_data_references
+
+
+def _get_raw_datasets() -> Dict[str, StateCode]:
+    raw_datasets: Dict[str, StateCode] = {}
+    for state_code in get_existing_direct_ingest_states():
+        for instance in DirectIngestInstance:
+            raw_tables_dataset = raw_tables_dataset_for_region(state_code, instance)
+            raw_latest_views_dataset = raw_latest_views_dataset_for_region(
+                state_code, instance
+            )
+            raw_datasets[raw_tables_dataset] = state_code
+            raw_datasets[raw_latest_views_dataset] = state_code
+    return raw_datasets
