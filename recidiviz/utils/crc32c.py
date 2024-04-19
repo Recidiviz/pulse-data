f33b279c031037ae795c2257a37a024a9b12d5be
ethanoro <ethan@recidiviz.org>
2024-04-19 09:55:30 -0400

    crc32_combine in python (Recidiviz/recidiviz-data#29106)
    ## Description of the change
    
    copies
    `google.cloud.storage.transfer_manager._digest_ordered_checksum_and_size_pairs`
    see
    [here](https://github.com/googleapis/python-storage/blob/6ed22ee91d4596dd27338ff8b35076d1238a603c/google/cloud/storage/transfer_manager.py#L1337)
    for originial implementation. since the it's a private function, they
    might change in any version update w/o backwards compatability. couldn't
    find another implementation of this readily available, as noted in
    Recidiviz/recidiviz-data#29093.
    
    opens Recidiviz/recidiviz-data#29093 as a todo for later if we can find an implementation that
    works for us that is externally maintained
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#28586
    Closes Recidiviz/recidiviz-data#29153
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 041d966c0c3221b28d6bdad28ac695b31020fef6

---
+++ recidiviz/utils/crc32c.py
@@ -0,0 +1,62 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Utilities for working with crc32c checksums."""
+import struct
+from typing import List, Tuple
+
+import google_crc32c
+
+MAX_CRC32C_ZERO_ARRAY_SIZE = 4 * 1024 * 1024
+
+
+# TODO(#29093): replace with package implementation for crc32c_combine
+def digest_ordered_checksum_and_size_pairs(
+    checksum_and_size_pairs: List[Tuple[int, int]]
+) -> bytes:
+    """THIS FUNCTION WAS COPY-PASTED FROM:
+    google.cloud.storage.transfer_manager._digest_ordered_checksum_and_size_pairs
+    see https://github.com/googleapis/python-storage/blob/6ed22ee91d4596dd27338ff8b35076d1238a603c/google/cloud/storage/transfer_manager.py#L1337
+    for originial implementation. function was moved here so as not to call private function
+    that might change in any version update w/o backwards compatability.
+
+    Takes in an ordered list of (checksum, size of checksumed bytes) and generates a
+    single checksum. Is useful for generating a single checksum for a full file based on
+    the checksums of a series of chunks that make up that file.
+    """
+    base_crc = None
+    zeroes = bytes(MAX_CRC32C_ZERO_ARRAY_SIZE)
+    for part_crc, size in checksum_and_size_pairs:
+        if not base_crc:
+            base_crc = part_crc
+        else:
+            base_crc ^= 0xFFFFFFFF  # precondition
+
+            # Zero pad base_crc32c. To conserve memory, do so with only
+            # MAX_CRC32C_ZERO_ARRAY_SIZE at a time. Reuse the zeroes array where
+            # possible.
+            padded = 0
+            while padded < size:
+                desired_zeroes_size = min((size - padded), MAX_CRC32C_ZERO_ARRAY_SIZE)
+                base_crc = google_crc32c.extend(base_crc, zeroes[:desired_zeroes_size])
+                padded += desired_zeroes_size
+
+            base_crc ^= 0xFFFFFFFF  # postcondition
+            base_crc ^= part_crc
+    crc_digest = struct.pack(
+        ">L", base_crc
+    )  # https://cloud.google.com/storage/docs/json_api/v1/objects#crc32c
+    return crc_digest
