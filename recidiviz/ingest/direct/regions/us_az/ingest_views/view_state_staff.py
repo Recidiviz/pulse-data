98c1d2cf57b9ff9e7db003d865ccc7e933e40e98
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-04-24 16:10:26 -0400

    [US_AZ] Ingest StateStaff (Recidiviz/recidiviz-data#29240)
    ## Description of the change
    
    Basic ingest of StateStaff and StateStaffExternalId for DOC staff in
    facilities and community corrections. There are no email addresses
    available in the raw data available at this time, so that field is not
    hydrated.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#27768
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 28cc56b000c3a98bf9b4761bd01e2c42651acb6f

---
+++ recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_staff.py
@@ -0,0 +1,45 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query containing information about facilities and community reentry staff."""
+
+from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
+    DirectIngestViewQueryBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+SELECT DISTINCT 
+    PERSON_ID, 
+    FIRST_NAME, 
+    NULLIF(MIDDLE_NAME, 'NMI') AS MIDDLE_NAME, 
+    SURNAME
+FROM {PERSON} p
+JOIN {LOOKUPS} l
+ON(PERSON_TYPE_ID = LOOKUP_ID)
+WHERE UPPER(l.description) IN ('DOC STAFF','COIII')
+"""
+
+VIEW_BUILDER = DirectIngestViewQueryBuilder(
+    region="us_az",
+    ingest_view_name="state_staff",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
