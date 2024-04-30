5baaa17859e36df3a8e2c35d7115a4dc87cc7b81
Shalin Brahmbhatt <63821972+shalindb@users.noreply.github.com>
2024-04-30 14:49:19 -0700

    [US_PA] Update to ineligible offense criteria to include all criminal history (Recidiviz/recidiviz-data#29411)
    ## Description of the change
    
    After talking with TTs, we realized we needed to include all criminal
    history as not just current charges are considered in eligibility for
    Administrative Supervision. I changed the ineligible offense criteria to
    take into account criminal history rather than just current offenses.
    
    As it is an exclusionary criteria, it makes 2970 less people eligible
    for the opportunity currently.
    Uploaded to `shalin` and verified to be passing validation views.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#29241
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 92792d6fd275b3fe8445f57da0eb748a07a9420f

--- recidiviz/task_eligibility/criteria/state_specific/us_pa/not_serving_ineligible_offense_for_admin_supervision.py
+++ recidiviz/task_eligibility/criteria/state_specific/us_pa/not_serving_ineligible_offense_for_admin_supervision.py
@@ -17,11 +17,16 @@
 """
 Defines a criteria view that shows spans of time when clients are not serving any Admin Supervision-ineligible sentences
 """
+from recidiviz.calculator.query.sessions_query_fragments import (
+    create_sub_sessions_with_attributes,
+)
 from recidiviz.calculator.query.state.dataset_config import (
     NORMALIZED_STATE_DATASET,
     SESSIONS_DATASET,
 )
 from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.dataset_config import raw_latest_views_dataset_for_region
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
 from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
     StateSpecificTaskCriteriaBigQueryViewBuilder,
 )
@@ -33,87 +38,165 @@ _CRITERIA_NAME = "US_PA_NOT_SERVING_INELIGIBLE_OFFENSE_FOR_ADMIN_SUPERVISION"
 _DESCRIPTION = """Defines a criteria view that shows spans of time when clients are not serving any
  Admin Supervision-ineligible sentences"""
 
-_REASON_QUERY = """
-    WITH ineligible_spans AS (
+_REASON_QUERY = f"""
+    WITH
+      ineligible_spans AS (
           SELECT
-          span.state_code,
-          span.person_id,
-          span.start_date,
-          span.end_date,
-          sent.projected_completion_date_max,
-          sc.description,
-          FALSE as meets_criteria,
+            span.state_code,
+            span.person_id,
+            span.start_date,
+            CAST(NULL AS DATE) AS end_date,
+            sc.description,
+            FALSE AS meets_criteria,
           FROM
-          `{project_id}.{sessions_dataset}.sentence_spans_materialized` span,
+            `{{project_id}}.{{sessions_dataset}}.sentence_spans_materialized` span,
             UNNEST (sentences_preprocessed_id_array_actual_completion) AS sentences_preprocessed_id
           INNER JOIN
-            `{project_id}.{sessions_dataset}.sentences_preprocessed_materialized` sent
+            `{{project_id}}.{{sessions_dataset}}.sentences_preprocessed_materialized` sent
           USING
             (state_code,
               person_id,
               sentences_preprocessed_id)
           LEFT JOIN
-            `{project_id}.{normalized_state_dataset}.state_charge` sc
+            `{{project_id}}.{{normalized_state_dataset}}.state_charge` sc
           ON
             span.person_id = sc.person_id
             AND span.state_code = sc.state_code
             AND sent.charge_id = sc.charge_id
           WHERE
             span.state_code = 'US_PA'
-            AND ((sc.statute LIKE 'CC25%' AND sc.description LIKE '%CRIMINAL HOMICIDE%')
-                OR (sc.statute LIKE 'CC27%' AND sc.offense_type LIKE '%ASSAULT%')
-                OR (sc.statute LIKE 'CC29%' AND sc.offense_type LIKE '%KIDNAP%')
-                OR (sc.statute LIKE 'CC31%' AND sc.description LIKE '%SEXUAL ASSAULT%')
-                OR (sc.statute LIKE 'CC33%' AND sc.description LIKE '%ARSON%')
-                OR (sc.statute LIKE 'CC37%' AND sc.offense_type LIKE '%ROBBERY%')
-                OR (sc.statute LIKE 'CC49%' AND sc.description LIKE '%INTIMIDATION%')
-                -- Relating to homicide via watercraft while under the influence
-                OR (sc.statute LIKE 'VC5502.1')
-                -- Relating to DUI/Controlled Substance in cases involving bodily injury
-                OR (sc.statute LIKE 'VC3731%')
-                -- Relating to homicide via vehicle
-                OR (sc.statute LIKE 'VC3732%')
-                OR (sc.statute IN ('VC3735','VC3735A'))
-                OR (sc.statute LIKE 'VC3735.1')
-                -- Relating to accidents involving death or personal injury
-                OR (sc.statute LIKE 'VC3742%')
-                -- Incest
-                OR (sc.statute LIKE '%4302%')
-                -- Open lewdness
-                OR (sc.statute LIKE 'CC5901')
-                -- Prostitution
-                OR (sc.statute IN ('CC5902B', 'CS5902B', 'CC5902B1', 'CC5902B3', 'CC5902B4'))
-                -- Sexual performance where victim is a minor
-                OR (sc.statute LIKE 'CC5903%')
-                -- Internet Child Pornography
-                OR (sc.statute LIKE 'CC76%')
-                -- Megan's Law Registration
-                OR (sc.statute LIKE '%9795%')
-                -- Sexual Abuse of Children
-                OR ((sc.statute LIKE 'CC6312%' OR sc.statute LIKE 'CS6312'))
-                -- Unlawful Contact with Minor
-                OR (sc.statute LIKE 'CC6318%')
-                -- Sexual Abuse of Children
-                OR (sc.statute LIKE 'CC6320%')
-                )
-          -- Removes a few cases where someone has multiple spans for the same offense so they aren't later 
-          -- aggregated unnecessarily 
-          QUALIFY ROW_NUMBER() OVER (PARTITION BY span.person_id, span.start_date, sc.description ORDER BY
-          projected_completion_date_max DESC) = 1
-    )
-    SELECT 
-      state_code,
-      person_id,
-      start_date,
-      end_date,
-      meets_criteria,
-      TO_JSON(STRUCT(
-                ARRAY_AGG(description ORDER BY COALESCE(projected_completion_date_max,'9999-01-01')) AS ineligible_offenses,
-                ARRAY_AGG(projected_completion_date_max ORDER BY COALESCE(projected_completion_date_max,'9999-01-01')) AS ineligible_sentences_expiration_date
-                )) AS reason,
-      FROM ineligible_spans
-      GROUP BY 1,2,3,4,5
-  """
+            AND ((sc.statute LIKE 'CC25%'
+                AND (sc.description LIKE '%CRIMINAL HOMICIDE%'
+                  OR sc.description LIKE '%MURDER%'))
+              OR (sc.statute LIKE 'CC27%'
+                AND sc.offense_type LIKE '%ASSAULT%')
+              OR (sc.statute LIKE 'CC29%'
+                AND sc.offense_type LIKE '%KIDNAP%')
+              OR (sc.statute LIKE 'CC31%'
+                AND (sc.description LIKE '%SEXUAL ASSAULT%'
+                    OR sc.offense_type LIKE '%RAPE%'))
+              OR (sc.statute LIKE 'CC33%'
+                AND sc.description LIKE '%ARSON%')
+              OR (sc.statute LIKE 'CC37%'
+                AND sc.offense_type LIKE '%ROBBERY%')
+              OR (sc.statute LIKE 'CC49%'
+                AND sc.description LIKE '%INTIMIDATION%')
+              -- Relating to homicide via watercraft while under the influence
+              OR (sc.statute LIKE 'VC5502.1')
+              -- Relating to DUI/Controlled Substance in cases involving bodily injury
+              OR (sc.statute LIKE 'VC3731%')
+              -- Relating to homicide via vehicle
+              OR (sc.statute LIKE 'VC3732%')
+              OR (sc.statute IN ('VC3735',
+                  'VC3735A'))
+              OR (sc.statute LIKE 'VC3735.1')
+              -- Relating to accidents involving death or personal injury
+              OR (sc.statute LIKE 'VC3742%')
+              -- Incest
+              OR (sc.statute LIKE '%4302%')
+              -- Open lewdness
+              OR (sc.statute LIKE 'CC5901')
+              -- Prostitution
+              OR (sc.statute IN ('CC5902B',
+                  'CS5902B',
+                  'CC5902B1',
+                  'CC5902B3',
+                  'CC5902B4'))
+              -- Sexual performance where victim is a minor
+              OR (sc.statute LIKE 'CC5903%')
+              -- Internet Child Pornography
+              OR (sc.statute LIKE 'CC76%')
+              -- Megan's Law Registration
+              OR (sc.statute LIKE '%9795%')
+              -- Sexual Abuse of Children
+              OR ((sc.statute LIKE 'CC6312%'
+                  OR sc.statute LIKE 'CC4304A'))
+              -- Unlawful Contact with Minor
+              OR (sc.statute LIKE 'CC6318%')
+              -- Sexual Abuse of Children
+              OR (sc.statute LIKE 'CC6320%') )
+            -- Removes a few cases where someone has multiple spans for the same offense so they aren't later
+            -- aggregated unnecessarily
+            QUALIFY ROW_NUMBER() OVER (PARTITION BY span.person_id, sc.description ORDER BY span.start_date ASC, projected_completion_date_max DESC) = 1 ),
+      -- The state_transfers CTE below is necessary to include individuals who have been transferred out of state in order to view their offenses prior 
+      state_transfers AS (
+          WITH
+            dbo_ReleaseInfo_pivoted AS (
+            SELECT
+              *
+            FROM
+              `{{project_id}}.{{raw_data_up_to_date_views_dataset}}.dbo_ReleaseInfo_latest` rel 
+              UNPIVOT(offense_code FOR offense IN (RelPBPPOffenseCode,
+                  RelPBPPOffenseCode2,
+                  RelPBPPOffenseCode3)) )
+          SELECT
+            state_code,
+            pei.person_id,
+            DATE(CAST(RelTransferDateYear AS INT),CAST(RelTransferDateMonth AS INT),CAST(RelTransferDateDay AS INT)) AS start_date,
+            CAST(NULL AS DATE) AS end_date,
+            FALSE AS meets_criteria,
+            PBPPOffenseDescription AS description
+          FROM
+            dbo_ReleaseInfo_pivoted rel
+          LEFT JOIN
+            `{{project_id}}.{{raw_data_up_to_date_views_dataset}}.RECIDIVIZ_REFERENCE_pbpp_offense_code_descriptions_latest` off1
+          ON
+            off1.PBPPOffenseCode = offense_code
+          LEFT JOIN
+            `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` pei
+          ON
+            ParoleNumber = pei.external_id
+          WHERE
+            DATE(CAST(RelTransferDateYear AS INT),CAST(RelTransferDateMonth AS INT),CAST(RelTransferDateDay AS INT)) IS NOT NULL
+            AND id_type = 'US_PA_PBPP'
+            AND state_code = 'US_PA'
+            AND (PBPPOffenseDescription LIKE '%HOMICIDE%'
+                OR PBPPOffenseDescription LIKE '%MURDER%'
+                OR PBPPOffenseDescription LIKE '%ASSAULT%'
+                OR PBPPOffenseDescription LIKE '%KIDNAP%'
+                OR PBPPOffenseDescription LIKE '%RAPE%'
+                OR PBPPOffenseDescription LIKE '%ARSON%'
+                OR PBPPOffenseDescription LIKE '%ROBBERY%'
+                OR PBPPOffenseDescription LIKE '%INTIMIDATION%')
+          GROUP BY
+            1,2,3,4,5,6
+        QUALIFY ROW_NUMBER() OVER (PARTITION BY person_id, description ORDER BY start_date ASC) = 1 ),
+      union_cte AS (
+      SELECT
+        state_code,
+        person_id,
+        start_date,
+        end_date,
+        meets_criteria,
+        description,
+      FROM
+        ineligible_spans
+      GROUP BY
+        1,2,3,4,5,6
+      UNION ALL
+      SELECT
+        state_code,
+        person_id,
+        start_date,
+        end_date,
+        meets_criteria,
+        description,
+      FROM
+        state_transfers
+      GROUP BY
+        1,2,3,4,5,6 ),
+    {create_sub_sessions_with_attributes('union_cte')},
+    dedup_cte AS (
+        SELECT * except (description),
+            TO_JSON(STRUCT( ARRAY_AGG(DISTINCT description) AS ineligible_offenses)) AS reason,
+        FROM sub_sessions_with_attributes
+        GROUP BY 1,2,3,4,5
+     )
+    SELECT
+      *
+    FROM
+      dedup_cte
+"""
 
 VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
     StateSpecificTaskCriteriaBigQueryViewBuilder(
@@ -123,6 +206,10 @@ VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
         state_code=StateCode.US_PA,
         normalized_state_dataset=NORMALIZED_STATE_DATASET,
         sessions_dataset=SESSIONS_DATASET,
+        raw_data_up_to_date_views_dataset=raw_latest_views_dataset_for_region(
+            state_code=StateCode.US_PA,
+            instance=DirectIngestInstance.PRIMARY,
+        ),
         meets_criteria_default=True,
     )
 )
