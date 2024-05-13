0b67fee7fb1f8db4414601f424388bb4ace621d6
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2024-05-13 13:13:23 -0700

    [Terraform] Configure new PagerDuty services in Terraform (Recidiviz/recidiviz-data#29513)
    ## Description of the change
    
    Adds a `RecidivizPagerDutyService` enum for tracking PagerDuty services
    that may be referenced by our alert generation code. Creates Terraform
    configurations so that all of these services are now managed by TF.
    
    Some notes:
    * All these TF-managed services are branded as "Airflow" services since
    alerts to those services will be generated based on Airflow task or DAG
    failures. Services like the "Recidiviz-Security-Team" service and "GCP
    Infrastructure" service will remain unmanaged (for now at least).
    * For all TF-managed Airflow services, we have implemented a new filter
    so that alerts can only be generated / resolved via email sent from
    `alerts+airflow-xxxx@recidiviz.org` emails.
    * The `Airflow DAG: Calculation` and `Airflow DAG: Monitoring` services
    have been imported to the `DATA_PLATFORM_AIRFLOW_SERVICE` and
    `MONITORING_AIRFLOW_SERVICE` services. You can see in the TF plan what
    actual changes are being made to these services (they are minimal).
    * There is now a service for each ingest state code. They will all need
    to be configured to send alerts to the #ingest-alerts channel via this
    page: https://recidiviz.pagerduty.com/accounts_addons/P68QE5Y
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#28642
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 1caee98f71c137bba562fdb319b5c4cad34a2319

---
+++ recidiviz/tools/deploy/terraform/modules/pagerduty-service-airflow/main.tf
@@ -0,0 +1,122 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+# Required to use the pagerduty provider inside a module.
+# See https://developer.hashicorp.com/terraform/language/modules/develop/providers.
+terraform {
+  required_providers {
+    pagerduty = {
+      source  = "PagerDuty/pagerduty"
+      version = "3.11.4"
+    }
+  }
+}
+
+locals {
+  project_name_str = var.project_id == "recidiviz-123" ? "PRODUCTION" : "STAGING"
+}
+
+# Docs: https://registry.terraform.io/providers/PagerDuty/pagerduty/latest/docs/resources/service
+resource "pagerduty_service" "service" {
+  name                    = "[${local.project_name_str}] ${var.service_base_name}"
+  description             = var.service_description
+  escalation_policy       = var.escalation_policy_id
+
+  acknowledgement_timeout = "null"
+  auto_resolve_timeout    = "null"
+
+  support_hours {
+    # Monday to Friday support hours
+    days_of_week = [1, 2, 3, 4, 5]
+    start_time = "09:00:00"
+    end_time   = "14:00:00"
+    time_zone  = "America/Los_Angeles"
+    type       = "fixed_time_per_day"
+  }
+
+  incident_urgency_rule {
+    type = "use_support_hours"
+
+    during_support_hours {
+      type    = "constant"
+      urgency = "high"
+    }
+
+    outside_support_hours {
+      type    = "constant"
+      urgency = "low"
+    }
+  }
+}
+
+# Docs: https://registry.terraform.io/providers/PagerDuty/pagerduty/latest/docs/resources/service_integration
+resource "pagerduty_service_integration" "email_integration" {
+  name              = "Email"
+  type              = "generic_email_inbound_integration"
+  # TODO(#28642): Append -${var.project_id} here instead of with each call to this module when
+  #  the imported services use new standard emails.
+  integration_email = "${var.integration_email_username}@recidiviz.pagerduty.com"
+  service           = pagerduty_service.service.id
+  email_incident_creation = "use_rules"
+  email_filter_mode       = "and-rules-email"
+  email_filter {
+    body_mode        = "always"
+    body_regex       = null
+    from_email_mode  = "match"
+    from_email_regex = var.project_id == "recidiviz-123" ? "alerts+airflow-production@recidiviz.org" : "alerts+airflow-staging@recidiviz.org"
+    subject_mode     = "match"
+    subject_regex    = null
+  }
+
+  email_parser {
+    action = "trigger"
+    match_predicate {
+      type = "any"
+      predicate {
+        matcher = "Task failure"
+        part    = "subject"
+        type    = "contains"
+      }
+    }
+    value_extractor {
+      part         = "subject"
+      type         = "between"
+      starts_after = "Task failure: "
+      ends_before  = null
+      value_name   = "incident_key"
+    }
+  }
+
+  email_parser {
+    action = "resolve"
+    match_predicate {
+      type = "any"
+      predicate {
+        matcher = "Task success"
+        part    = "subject"
+        type    = "contains"
+      }
+    }
+    value_extractor {
+      part         = "subject"
+      type         = "between"
+      starts_after = "Task success: "
+      ends_before  = null
+      value_name   = "incident_key"
+    }
+  }
+}
