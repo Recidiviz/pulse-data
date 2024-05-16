d3a2985890128b5a143514e5b7a7cf87fa5e99c1
Emily Turner <149205316+emilyemilyemilyemilyemilyemily@users.noreply.github.com>
2024-05-16 11:03:34 -0700

    Add ingest filename normalization cloud function (Recidiviz/recidiviz-data#29649)
    ## Description of the change
    
    Add cloud function code to unzip and/or rename ingest files. This code
    does not introduce any new functionality into the system and is designed
    to replace a portion of the existing handle_new_files endpoint logic.
    Control flow is as follows:
    ```mermaid
    graph LR
    
    object.v1.finalized -- filename --> bucket_pub_sub
    bucket_pub_sub --> parse_params
    
    subgraph cloud_function_rename
    	parse_params --> file_x
    	file_x --> is_filename_normalized
    	is_filename_normalized --> normalize_filename
    	is_filename_normalized --> is_zip
    	is_zip --> unpack_zip
    	unpack_zip --> archive_zip
    
    end
    
    is_zip --> finish
    archive_zip --> finish
    normalize_filename --> finish
    ```
    We can create the cloud functions with env variable DRY_RUN set to true
    so we can deploy the functions and verify theyre acting as expected
    before disabling the existing logic
    
    Error cases:
    - When we encounter an error in application code, we should log the
    error but not raise it. It doesn’t make sense to retry since we will
    encounter the same error again and again in the application code. We can
    create an alert based on error log entries to notify us if we are
    encountering unretryable exceptions.
    - When we encounter an unexpected error (ex transient gc issue) we
    should raise the exception which will cause the gcf to retry with
    exponential backoff for the next 24 hours.
    
    Also, modify file renaming logic to handle filenames that contain more
    than one period. Not necessarily an expected case but ran into the error
    during manual testing.
    
    Additional docs:
    [Raw Data
    Orchestration](https://www.notion.so/recidiviz/Raw-Data-Orchestration-77e9690a67e749ecb4bc47ec29d0d83d#e4f482c5e776467dad7f47f8dfef6f57)
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#29134
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6610ccd0d4f81dcb448653b70bdcbd38e5d86f60

---
+++ recidiviz/tests/cloud_functions/ingest_filename_normalization_test.py
@@ -0,0 +1,200 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2021 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for filename_normalization.py."""
+import os
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from cloudevents.http import CloudEvent
+from google.api_core.exceptions import GoogleAPIError
+
+from recidiviz.cloud_functions.ingest_filename_normalization import normalize_filename
+from recidiviz.cloud_storage.gcsfs_path import GcsfsDirectoryPath, GcsfsFilePath
+
+
+class TestNormalizeFilename(TestCase):
+    """Tests for normalize_filename cloud function."""
+
+    def setUp(self) -> None:
+        os.environ["STATE_CODE"] = "us_oz"
+        os.environ["INGEST_INSTANCE"] = "PRIMARY"
+        os.environ["PROJECT_ID"] = "recidiviz-test"
+        os.environ["DRY_RUN"] = "false"
+
+        self.bucket = "test_bucket"
+        self.relative_file_path = "test_file"
+        attributes = {
+            "id": "5e9f24a",
+            "type": "google.cloud.storage.object.v1.finalized",
+            "source": "sourceUrlHere",
+        }
+        self.data = {
+            "bucket": self.bucket,
+            "name": self.relative_file_path,
+            "generation": 1,
+            "metageneration": 1,
+            "timeCreated": "2021-10-10 00:00:00.000000Z",
+            "updated": "2021-11-11 00:00:00.000000Z",
+        }
+        self.event = CloudEvent(attributes, self.data)
+
+    @patch("recidiviz.cloud_functions.ingest_filename_normalization.fs")
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.DirectIngestGCSFileSystem.is_normalized_file_path",
+        return_value=False,
+    )
+    def test_successful_normalization(
+        self, _mock_is_normalized: MagicMock, mock_fs: MagicMock
+    ) -> None:
+        path_instance = GcsfsFilePath(
+            bucket_name=self.bucket, blob_name=self.relative_file_path
+        )
+
+        normalize_filename(self.event)
+        mock_fs.mv_raw_file_to_normalized_path.assert_called_with(path_instance)
+
+    @patch("recidiviz.cloud_functions.ingest_filename_normalization.fs")
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.DirectIngestGCSFileSystem.is_normalized_file_path",
+        return_value=False,
+    )
+    def test_raise_unexpected_error(
+        self, _mock_is_normalized: MagicMock, mock_fs: MagicMock
+    ) -> None:
+        mock_fs.mv_raw_file_to_normalized_path.side_effect = GoogleAPIError()
+
+        with self.assertRaises(GoogleAPIError):
+            normalize_filename(self.event)
+
+    @patch("recidiviz.cloud_functions.ingest_filename_normalization.fs")
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.DirectIngestGCSFileSystem.is_normalized_file_path",
+        return_value=False,
+    )
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.cloud_functions_log"
+    )
+    def test_swallow_nonretryable_error(
+        self,
+        mock_logging: MagicMock,
+        _mock_is_normalized: MagicMock,
+        mock_fs: MagicMock,
+    ) -> None:
+        mock_fs.mv_raw_file_to_normalized_path.side_effect = ValueError()
+
+        normalize_filename(self.event)
+        mock_logging.assert_called_once()
+
+    @patch("recidiviz.cloud_functions.ingest_filename_normalization.fs")
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.DirectIngestGCSFileSystem.is_normalized_file_path",
+        return_value=False,
+    )
+    def test_successful_normalization_dry_run(
+        self, _mock_is_normalized: MagicMock, mock_fs: MagicMock
+    ) -> None:
+        os.environ["DRY_RUN"] = "true"
+
+        normalize_filename(self.event)
+        mock_fs.mv_raw_file_to_normalized_path.assert_not_called()
+
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.cloud_functions_log"
+    )
+    def test_missing_bucket_or_name(self, mock_logging: MagicMock) -> None:
+        bad_event = self.event
+        bad_event.data["name"] = ""
+
+        normalize_filename(bad_event)
+        mock_logging.assert_called_once()
+
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.GcsfsPath.from_bucket_and_blob_name"
+    )
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.cloud_functions_log"
+    )
+    def test_incorrect_path_type(
+        self, mock_logging: MagicMock, mock_path: MagicMock
+    ) -> None:
+        mock_path.return_value = GcsfsDirectoryPath(bucket_name=self.bucket)
+
+        normalize_filename(self.event)
+        mock_logging.assert_called_once()
+
+    @patch("recidiviz.cloud_functions.ingest_filename_normalization.fs")
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.DirectIngestGCSFileSystem.is_normalized_file_path",
+        return_value=False,
+    )
+    def test_unnormalized_zip_file_handling(
+        self, _mock_is_normalized: MagicMock, mock_fs: MagicMock
+    ) -> None:
+        zip_event = self.event
+        zip_event.data["name"] = f"{self.relative_file_path}.zip"
+
+        normalize_filename(zip_event)
+        mock_fs.unzip.assert_not_called()
+        mock_fs.mv.assert_not_called()
+        mock_fs.mv_raw_file_to_normalized_path.assert_called_once()
+
+    @patch("recidiviz.cloud_functions.ingest_filename_normalization.fs")
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.DirectIngestGCSFileSystem.is_normalized_file_path",
+        return_value=True,
+    )
+    def test_zip_file_handling_dry_run(
+        self, _mock_is_normalized: MagicMock, mock_fs: MagicMock
+    ) -> None:
+        zip_event = self.event
+        zip_event.data["name"] = f"{self.relative_file_path}.zip"
+        os.environ["DRY_RUN"] = "true"
+
+        normalize_filename(zip_event)
+        mock_fs.unzip.assert_not_called()
+        mock_fs.mv.assert_not_called()
+        mock_fs.mv_raw_file_to_normalized_path.assert_not_called()
+
+    @patch("recidiviz.cloud_functions.ingest_filename_normalization.fs")
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.DirectIngestGCSFileSystem.is_normalized_file_path",
+        return_value=True,
+    )
+    def test_normalized_zip_file_handling(
+        self, _mock_is_normalized: MagicMock, mock_fs: MagicMock
+    ) -> None:
+        zip_event = self.event
+        zip_event.data["name"] = f"unprocessed{self.relative_file_path}.zip"
+
+        normalize_filename(zip_event)
+        mock_fs.unzip.assert_called_once()
+        mock_fs.mv.assert_called_once()
+        mock_fs.mv_raw_file_to_normalized_path.assert_not_called()
+
+    @patch("recidiviz.cloud_functions.ingest_filename_normalization.fs")
+    @patch(
+        "recidiviz.cloud_functions.ingest_filename_normalization.DirectIngestGCSFileSystem.is_normalized_file_path",
+        return_value=True,
+    )
+    def test_file_already_normalized(
+        self, _mock_is_normalized: MagicMock, mock_fs: MagicMock
+    ) -> None:
+        zip_event = self.event
+        zip_event.data["name"] = f"processed{self.relative_file_path}"
+
+        normalize_filename(self.event)
+        mock_fs.mv_raw_file_to_normalized_path.assert_not_called()
