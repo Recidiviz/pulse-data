3a43be3b4bcb932a05d1938c17324b7ad1fdf596
Nick Tallant <150271010+recidinick@users.noreply.github.com>
2024-05-16 14:37:51 -0500

    [US_AZ] Update Ingest for StateSentenceGroup and StateSentenceGroupLength (Recidiviz/recidiviz-data#29857)
    ## Description of the change
    
    This updates existing ingest in US_AZ to hydrate StateSentenceGroup and
    StateSentenceGroupLength entities. It also marks a TODO for
    sentence_group_external_id to just become sentence_group.
    
    GitOrigin-RevId: 049344137600771f6ff8c4ee80b7da78620c8646

--- recidiviz/persistence/entity/state/entities.py
+++ recidiviz/persistence/entity/state/entities.py
@@ -2324,6 +2324,7 @@ class StateSentence(HasExternalIdEntity, BuildableAttr, DefaultableAttr):
         default=None, validator=attr_validators.is_opt_int
     )
 
+    # TODO(#29642) Hydrate StateSentenceGroup here
     sentence_group_external_id: Optional[str] = attr.ib(
         default=None, validator=attr_validators.is_opt_str
     )
