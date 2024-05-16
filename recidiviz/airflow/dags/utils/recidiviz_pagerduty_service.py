7d773743e568337a240072d904d176ad7740d0e1
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2024-05-16 12:15:11 -0700

    [Terraform] Configure new PagerDuty services in Terraform (attempt Recidiviz/recidiviz-data#2) (Recidiviz/recidiviz-data#29717)
    ## Description of the change
    
    A second attempt at merging Recidiviz/recidiviz-data#29513, which allows us to start managing
    PagerDuty services in Terraform.
    
    The first commit in this PR is the original change, then subsequent
    commits are bugfixes. The main issue I had to work around is
    (https://github.com/PagerDuty/terraform-provider-pagerduty/issues/651)
    which prevented me from using the "text between" functionality to
    extract task ids from incident emails. I manually updated the data
    platform service integration email to use regex matching, tested that it
    worked (see
    [here](https://recidiviz.slack.com/archives/C02G720HH5W/p1715797288125709)),
    then updated the new TF config to match the configuration.
    
    The Terraform plan (see comment) shows zero changes to the imported data
    platform service and only the changes Dan and I discussed for the
    monitoring service.
    
    Also fixes a bug with the `can_send_mail()` helper.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of https://github.com/Recidiviz/recidiviz-data/issues/28642
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 8c34dd2d3cdc5c7cb16472225ab42c349fb3bafc

---
+++ recidiviz/airflow/dags/utils/recidiviz_pagerduty_service.py
@@ -0,0 +1,103 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Information about PagerDuty services."""
+
+from typing import List
+
+import attr
+
+from recidiviz.common import attr_validators
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.regions.direct_ingest_region_utils import (
+    get_existing_direct_ingest_states,
+)
+
+
+@attr.define
+class RecidivizPagerDutyService:
+    """Structure holding information about a service managed by the Recidiviz repo.
+    For the full list of services, see
+    https://recidiviz.pagerduty.com/service-directory.
+    """
+
+    project_id: str = attr.ib(validator=attr_validators.is_str)
+    service_integration_email: str = attr.ib(validator=attr_validators.is_str)
+
+    @classmethod
+    def data_platform_airflow_service(
+        cls, project_id: str
+    ) -> "RecidivizPagerDutyService":
+        """Returns the service for alerts related to state-agnostic Airflow data
+        platform task failures.
+        """
+        return RecidivizPagerDutyService(
+            project_id=project_id,
+            service_integration_email=cls._build_integration_email(
+                "data-platform-airflow", project_id=project_id
+            ),
+        )
+
+    @classmethod
+    def monitoring_airflow_service(cls, project_id: str) -> "RecidivizPagerDutyService":
+        """Returns the service for alerts related to Airflow monitoring infrastructure
+        failures.
+        """
+        return RecidivizPagerDutyService(
+            project_id=project_id,
+            service_integration_email=cls._build_integration_email(
+                "monitoring-airflow", project_id=project_id
+            ),
+        )
+
+    @classmethod
+    def airflow_service_for_state_code(
+        cls, project_id: str, state_code: StateCode
+    ) -> "RecidivizPagerDutyService":
+        """Returns the service for alerts related to state-specific Airflow data
+        platform task failures for the given state_code.
+        """
+        if state_code not in get_existing_direct_ingest_states():
+            raise ValueError(
+                f"PagerDuty Airflow service does not exist for [{state_code.value}]"
+            )
+
+        state_code_str = state_code.value.lower().replace("_", "-")
+        base_username = f"{state_code_str}-airflow"
+        return RecidivizPagerDutyService(
+            project_id=project_id,
+            service_integration_email=cls._build_integration_email(
+                base_username, project_id=project_id
+            ),
+        )
+
+    @classmethod
+    def all_services(cls, project_id: str) -> List["RecidivizPagerDutyService"]:
+        """Returns the list of all services managed for a given project."""
+        return [
+            cls.data_platform_airflow_service(project_id=project_id),
+            cls.monitoring_airflow_service(project_id=project_id),
+            *[
+                cls.airflow_service_for_state_code(
+                    project_id=project_id, state_code=state_code
+                )
+                for state_code in get_existing_direct_ingest_states()
+            ],
+        ]
+
+    @staticmethod
+    def _build_integration_email(base_username: str, project_id: str) -> str:
+        return f"{base_username}-{project_id}@recidiviz.pagerduty.com"
---
+++ recidiviz/airflow/tests/utils/recidiviz_pagerduty_service_test.py
@@ -0,0 +1,76 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for RecidivizPagerDutyService."""
+import unittest
+from typing import Dict
+
+from recidiviz.airflow.dags.utils.recidiviz_pagerduty_service import (
+    RecidivizPagerDutyService,
+)
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.regions.direct_ingest_region_utils import (
+    get_existing_direct_ingest_states,
+)
+from recidiviz.utils.environment import GCP_PROJECTS
+
+
+class TestRecidivizPagerDutyService(unittest.TestCase):
+    """Tests for RecidivizPagerDutyService."""
+
+    def test_services_for_each_deployed_state(self) -> None:
+        """Tests that every deployed ingest state has a corresponding
+        RecidivizPagerDutyService enum value defined. If this test fails and you've just
+        added a new state, the RecidivizPagerDutyService enum needs to be updated.
+        """
+        for state_code in get_existing_direct_ingest_states():
+            service = RecidivizPagerDutyService.airflow_service_for_state_code(
+                project_id="recidiviz-456",
+                state_code=state_code,
+            )
+            self.assertIsInstance(service, RecidivizPagerDutyService)
+
+    def test_service_integration_email(self) -> None:
+        self.assertEqual(
+            "data-platform-airflow-recidiviz-456@recidiviz.pagerduty.com",
+            RecidivizPagerDutyService.data_platform_airflow_service(
+                project_id="recidiviz-456"
+            ).service_integration_email,
+        )
+        self.assertEqual(
+            "us-ca-airflow-recidiviz-456@recidiviz.pagerduty.com",
+            RecidivizPagerDutyService.airflow_service_for_state_code(
+                project_id="recidiviz-456", state_code=StateCode.US_CA
+            ).service_integration_email,
+        )
+        self.assertEqual(
+            "us-id-airflow-recidiviz-456@recidiviz.pagerduty.com",
+            RecidivizPagerDutyService.airflow_service_for_state_code(
+                project_id="recidiviz-456", state_code=StateCode.US_ID
+            ).service_integration_email,
+        )
+
+    def test_services_all_have_unique_emails(self) -> None:
+        found_emails: Dict[str, RecidivizPagerDutyService] = {}
+        for project_id in GCP_PROJECTS:
+            for service in RecidivizPagerDutyService.all_services(project_id):
+                email = service.service_integration_email
+                if email in found_emails:
+                    raise ValueError(
+                        f"Found duplicate emails for services [{service}] and "
+                        f"[{found_emails[email]}]: {email}"
+                    )
+                found_emails[email] = service
