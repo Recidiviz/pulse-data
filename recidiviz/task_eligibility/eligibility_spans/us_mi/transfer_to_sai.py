31429e535c0799cd9e2bfa8600ce1ed131a57133
Sophie Bair <56454281+seb2244@users.noreply.github.com>
2024-05-30 09:42:43 -0500

    [US_MI] [Worfklows] Create stubs for SAI (Recidiviz/recidiviz-data#30194)
    ## Description of the change
    
    This is the final chapter in the saga for creating stubs for SAI
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#28729
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 3228165c06515653122831ad90c661a7181b094b

---
+++ recidiviz/task_eligibility/eligibility_spans/us_mi/transfer_to_sai.py
@@ -0,0 +1,71 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Builder for a task eligibility spans view that shows the spans of time during which
+someone in MI is eligible to transfer to the SAI (Special Alternative Incarceration) program.
+"""
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.candidate_populations.general import (
+    general_incarceration_population,
+)
+from recidiviz.task_eligibility.completion_events.general import (
+    transfer_to_treatment_in_prison,
+)
+from recidiviz.task_eligibility.criteria.general import (
+    no_violent_sexual_offenses,
+    serving_first_prison_sentence,
+)
+from recidiviz.task_eligibility.criteria.state_specific.us_mi import (
+    minimum_sentence_length_eligible_for_sai,
+    no_pending_felony_or_immigration_detainer,
+    not_serving_ineligible_offenses_for_sai,
+    screening_not_high_assault_risk,
+    served_minimum_sentence_for_controlled_substances,
+    served_minimum_sentence_for_firearm_felony,
+    true_security_level_not_iv_or_v,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Builder for a task eligibility spans view that shows the spans of time during which
+someone in MI is eligible to transfer to the SAI (Special Alternative Incarceration) program.
+"""
+
+VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
+    state_code=StateCode.US_MI,
+    task_name="TRANSFER_TO_SAI",
+    description=_DESCRIPTION,
+    candidate_population_view_builder=general_incarceration_population.VIEW_BUILDER,
+    criteria_spans_view_builders=[
+        no_violent_sexual_offenses.VIEW_BUILDER,
+        serving_first_prison_sentence.VIEW_BUILDER,
+        minimum_sentence_length_eligible_for_sai.VIEW_BUILDER,
+        no_pending_felony_or_immigration_detainer.VIEW_BUILDER,
+        not_serving_ineligible_offenses_for_sai.VIEW_BUILDER,
+        screening_not_high_assault_risk.VIEW_BUILDER,
+        served_minimum_sentence_for_controlled_substances.VIEW_BUILDER,
+        served_minimum_sentence_for_firearm_felony.VIEW_BUILDER,
+        true_security_level_not_iv_or_v.VIEW_BUILDER,
+    ],
+    completion_event_builder=transfer_to_treatment_in_prison.VIEW_BUILDER,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
