fdeb13986ab59c4ff7eded4e33ea3f3a5dd56136
Sophie Bair <56454281+seb2244@users.noreply.github.com>
2024-05-30 09:42:43 -0500

    [US_MI] [Worfklows] Create stubs for SAI (Recidiviz/recidiviz-data#30194)
    ## Description of the change
    
    This is the final chapter in the saga for creating stubs for SAI
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#28729
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 3228165c06515653122831ad90c661a7181b094b

---
+++ recidiviz/task_eligibility/criteria/state_specific/us_mi/not_serving_ineligible_offenses_for_sai.py
@@ -0,0 +1,49 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""Describes the spans of time during which someone is not serving ineligible offenses for SAI"""
+
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.placeholder_criteria_builders import (
+    state_specific_placeholder_criteria_view_builder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "US_MI_NOT_SERVING_INELIGIBLE_OFFENSES_FOR_SAI"
+
+_DESCRIPTION = """Describes the spans of time during which
+    someone is not serving ineligible offenses for SAI"""
+
+_REASON_QUERY = (
+    """TO_JSON(STRUCT(['placeholder1', 'placeholder2'] AS ineligible_offenses))"""
+)
+
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
+    state_specific_placeholder_criteria_view_builder(
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+        reason_query=_REASON_QUERY,
+        state_code=StateCode.US_MI,
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
---
+++ recidiviz/task_eligibility/criteria/state_specific/us_mi/screening_not_high_assault_risk.py
@@ -0,0 +1,48 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""Describes spans of time during which a candidate does not have a high
+    assault risk according to their most recent Assaultive Risk Screening"""
+
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.placeholder_criteria_builders import (
+    state_specific_placeholder_criteria_view_builder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "US_MI_SCREENING_NOT_HIGH_ASSAULT_RISK"
+
+_DESCRIPTION = """Describes spans of time during which a candidate does not have a high
+    assault risk according to their most recent Assaultive Risk Screening"""
+
+_REASON_QUERY = """TO_JSON(STRUCT('placeholder' AS most_recent_assault_risk))"""
+
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
+    state_specific_placeholder_criteria_view_builder(
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+        reason_query=_REASON_QUERY,
+        state_code=StateCode.US_MI,
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
