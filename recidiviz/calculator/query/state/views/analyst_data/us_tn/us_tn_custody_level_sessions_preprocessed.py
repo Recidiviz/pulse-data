677c6576bf58f75eafd28592fe3e713b2262f693
Damini Sharma <damini@recidiviz.org>
2024-06-04 12:48:37 -0400

    [US_TN] Fix end date on raw custody sessions view (Recidiviz/recidiviz-data#30336)
    ## Description of the change
    
    I merged in a change in Recidiviz/recidiviz-data#29714 which led to multiple open periods being
    open for people's `custody_level_sessions` because the end date was
    being determined within a super session ID. This PR fixes that to
    determine end dates using either the next classification OR the end of a
    super session id.
    
    Validation:
    
    This validation now returns 0 rows, previously returning 331 rows for TN
    ```
    select *
    from `recidiviz-staging.dsharm20240603_validation_views.current_impact_funnel_status_duplicate_external_ids_materialized`
    where region_code = 'US_TN'
    ```
    
    There are no longer people with multiple open periods in custody level
    sessions
    ```
    select person_id, count(*)
    from `recidiviz-staging.dsharm20240603_sessions.custody_level_sessions_materialized`
    WHERE end_date_exclusive is null
    group by 1
    having count(*)>1
    ```
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#30287
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6612a83dff7c133fbe73465d07c469ea70bfa8cf

--- recidiviz/calculator/query/state/views/analyst_data/us_tn/us_tn_custody_level_sessions_preprocessed.py
+++ recidiviz/calculator/query/state/views/analyst_data/us_tn/us_tn_custody_level_sessions_preprocessed.py
@@ -17,7 +17,11 @@
 """Computes custody levels from raw TN data"""
 
 from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
-from recidiviz.calculator.query.bq_utils import nonnull_end_date_exclusive_clause
+from recidiviz.calculator.query.bq_utils import (
+    nonnull_end_date_clause,
+    nonnull_end_date_exclusive_clause,
+    revert_nonnull_end_date_clause,
+)
 from recidiviz.calculator.query.state.dataset_config import ANALYST_VIEWS_DATASET
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
@@ -32,44 +36,52 @@ US_TN_CUSTODY_LEVEL_SESSIONS_PREPROCESSED_VIEW_DESCRIPTION = (
 
 US_TN_CUSTODY_LEVEL_SESSIONS_PREPROCESSED_QUERY_TEMPLATE = f"""
     -- TODO(#5178): Once custody level ingest issues in TN are resolved, this view can be deleted
-    SELECT
-        raw.state_code,
-        raw.person_id,
-        raw.custody_level,
-        raw.custody_level_raw_text,
-        classification_date AS start_date,
-        compartment_level_0_super_session_id,
-        LEAD(classification_date) OVER (PARTITION BY raw.person_id, compartment_level_0_super_session_id
-                                        ORDER BY classification_date ASC) AS end_date_exclusive, 
-    FROM (
+    WITH preprocessed AS (
         SELECT
-            state_code,
-            person_id,
-            CAST(CAST(ClassificationDate AS DATETIME) AS DATE) AS classification_date,
-            RecommendedCustody AS custody_level_raw_text,
-            CASE 
-                WHEN RecommendedCustody = "MAX" THEN "MAXIMUM"
-                WHEN RecommendedCustody = "CLS" THEN "CLOSE"
-                WHEN RecommendedCustody = "MED" THEN "MEDIUM"
-                WHEN RecommendedCustody LIKE "MI%" THEN "MINIMUM"
-            END AS custody_level
-        FROM `{{project_id}}.us_tn_raw_data_up_to_date_views.Classification_latest` c
-        INNER JOIN `{{project_id}}.normalized_state.state_person_external_id` pei
-            ON c.OffenderID = pei.external_id
-            AND pei.state_code = "US_TN"
-        -- Only keep classifications that were approved
-        WHERE ClassificationDecision = 'A'
-        -- Most classifications should be unique on person-date. When they're not, we keep the latest decision date
-        QUALIFY ROW_NUMBER() OVER(
-            PARTITION BY OffenderID,
-            CAST(ClassificationDate AS DATETIME)
-            ORDER BY CAST(ClassificationDecisionDate AS DATETIME) DESC
-        ) = 1    
-    ) raw
-    LEFT JOIN `{{project_id}}.sessions.compartment_level_0_super_sessions_materialized` ss
-        ON raw.person_id = ss.person_id
-        AND raw.classification_date BETWEEN ss.start_date AND {nonnull_end_date_exclusive_clause("ss.end_date_exclusive")}
-    WHERE compartment_level_0 IN ('INCARCERATION')
+            raw.state_code,
+            raw.person_id,
+            raw.custody_level,
+            raw.custody_level_raw_text,
+            classification_date AS start_date,
+            compartment_level_0_super_session_id,
+            LEAST(
+                {nonnull_end_date_clause("LEAD(classification_date) OVER (PARTITION BY raw.person_id ORDER BY classification_date ASC)")} ,
+                {nonnull_end_date_clause("ss.end_date_exclusive")}
+                ) AS end_date_exclusive,
+        FROM (
+            SELECT
+                state_code,
+                person_id,
+                CAST(CAST(ClassificationDate AS DATETIME) AS DATE) AS classification_date,
+                RecommendedCustody AS custody_level_raw_text,
+                CASE 
+                    WHEN RecommendedCustody = "MAX" THEN "MAXIMUM"
+                    WHEN RecommendedCustody = "CLS" THEN "CLOSE"
+                    WHEN RecommendedCustody = "MED" THEN "MEDIUM"
+                    WHEN RecommendedCustody LIKE "MI%" THEN "MINIMUM"
+                END AS custody_level
+            FROM `{{project_id}}.us_tn_raw_data_up_to_date_views.Classification_latest` c
+            INNER JOIN `{{project_id}}.normalized_state.state_person_external_id` pei
+                ON c.OffenderID = pei.external_id
+                AND pei.state_code = "US_TN"
+            -- Only keep classifications that were approved
+            WHERE ClassificationDecision = 'A'
+            -- Most classifications should be unique on person-date. When they're not, we keep the latest decision date
+            QUALIFY ROW_NUMBER() OVER(
+                PARTITION BY OffenderID,
+                CAST(ClassificationDate AS DATETIME)
+                ORDER BY CAST(ClassificationDecisionDate AS DATETIME) DESC
+            ) = 1    
+        ) raw
+        LEFT JOIN `{{project_id}}.sessions.compartment_level_0_super_sessions_materialized` ss
+            ON raw.person_id = ss.person_id
+            AND raw.classification_date BETWEEN ss.start_date AND {nonnull_end_date_exclusive_clause("ss.end_date_exclusive")}
+        WHERE compartment_level_0 IN ('INCARCERATION')
+    )
+    SELECT * EXCEPT(end_date_exclusive),
+            {revert_nonnull_end_date_clause("end_date_exclusive")} AS end_date_exclusive
+    FROM
+        preprocessed
         
 """
 
