cf479399fb17ee7d721029b5569f4e45bed62c5f
Brandon Hills <130382407+brandon-hills@users.noreply.github.com>
2024-06-04 15:44:44 -0400

    [Justice Counts] Add method to write to the Metric Settings table. (Recidiviz/recidiviz-data#30302)
    ## Description of the change
    
    Add method to write to the Metric Settings table.
    
    Zenhub Ticket Recidiviz/recidiviz-data#28420
    
    ## Testing
    
    Added unit tests for adding and updating, as well as unit tests
    verifying that when disaggregated_by_supervision_subsystem is modified,
    we modify the disaggregated_by_supervision_subsystem and
    is_metric_enabled fields accordingly for all supervision systems and
    subsystems for that metric.
    
    Because of the nature of the Metric Interface updates (in that we are
    overwriting the storage json with general-logic instead of custom logic
    for each field), we are not adding tests for every single Metric
    Interface field that could possibly be updated.
    
    GitOrigin-RevId: a700f2e923716ecca0c99fb90ba6ac5b81aebcff

--- recidiviz/justice_counts/metric_setting.py
+++ recidiviz/justice_counts/metric_setting.py
@@ -16,7 +16,8 @@
 # =============================================================================
 """Interface for working with the MetricSetting model."""
 
-from typing import Dict, List, Optional, Set
+from copy import deepcopy
+from typing import Any, Dict, List, Optional, Set
 
 from sqlalchemy.orm import Session
 
@@ -28,8 +29,144 @@ from recidiviz.justice_counts.metrics.metric_interface import (
 from recidiviz.persistence.database.schema.justice_counts import schema
 
 
+def apply_updates(original: Dict[str, Any], updates: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    Recursively updates the 'original' with the values from 'updates'. Does not
+    overwrite if the value of 'updates' is None.
+
+    This approach works as a way to modify metric interface storage JSONs because all
+    fields in the storage JSONs are either dictionaries or values. And in the case that
+    they are values, we always want to overwrite the original value with the update
+    value.
+
+    Arguments:
+    - original (json): The original storage json to be updated.
+    - updates (json): The updates to apply. Must also be formatted as a storage json.
+    """
+    for key, value in updates.items():
+        if isinstance(value, dict) and key in original:
+            apply_updates(original[key], value)
+        # If `key` is not in `original`, we add the full key-value pair from `updates`
+        # to `original` as-is. This applies regardless of whether `value` is a
+        # dictionary or a primitive value.
+        elif value is not None:
+            original[key] = value
+    return original
+
+
 class MetricSettingInterface:
     """Contains methods for working with the MetricSetting table."""
+
+    @staticmethod
+    def get_metric_setting_by_agency_id_and_metric_key(
+        session: Session,
+        agency_id: int,
+        metric_definition_key: str,
+    ) -> Optional[schema.MetricSetting]:
+        """Get a metric setting according to agency id and metric definition key."""
+        return (
+            session.query(schema.MetricSetting)
+            .filter(
+                schema.MetricSetting.agency_id == agency_id,
+                schema.MetricSetting.metric_definition_key == metric_definition_key,
+            )
+            .one_or_none()
+        )  # Unique by agency_id and metric_definition_key.
+
+    @staticmethod
+    def update_metric_setting(
+        session: Session,
+        agency: schema.Agency,
+        agency_metric: MetricInterface,
+    ) -> None:
+        """Overwrite an agency's metric setting with the given metric interface."""
+
+        existing_setting = (
+            MetricSettingInterface.get_metric_setting_by_agency_id_and_metric_key(
+                session=session,
+                agency_id=agency.id,
+                metric_definition_key=agency_metric.key,
+            )
+        )
+
+        if existing_setting is not None:
+            if existing_setting.metric_interface is None:
+                raise ValueError(
+                    f"metric_interface column in MetricSetting table should never be null, but is null for agency {agency.name} and metric definition key {agency_metric.key}."
+                )
+
+            updates = agency_metric.to_storage_json()
+            # Using deepcopy so that the existing metric interface is only modified once
+            # apply_updates returns.
+            existing_setting.metric_interface = apply_updates(
+                deepcopy(existing_setting.metric_interface), updates
+            )
+        else:
+            session.add(
+                schema.MetricSetting(
+                    agency_id=agency.id,
+                    metric_definition_key=agency_metric.key,
+                    metric_interface=agency_metric.to_storage_json(),
+                )
+            )
+        session.commit()
+
+    # TODO(#28469): Remove "new" from this method once we deprecate the existing
+    # add_or_update_agency_metric_setting method.
+    @staticmethod
+    def new_add_or_update_agency_metric_setting(
+        session: Session,
+        agency: schema.Agency,
+        agency_metric: MetricInterface,
+    ) -> None:
+        """Add or update an agency's metric setting with the given metric interface.
+
+        If disaggregated_by_supervision_subsystem is modified, we will also modify the
+        disaggregated_by_supervision_subsystem and is_metric_enabled fields accordingly
+        for all supervision systems and subsystems for the metric.
+        See the test_set_disaggregated_by_supervision_subsystem_to_true test as an
+        example of this behavior.
+        """
+        agency_systems = {schema.System[s] for s in agency.systems}
+        current_system = agency_metric.metric_definition.system.value
+        # First, modify the disaggregated_by_supervision_subsystems and is_metric_enabled
+        # fields for all supervision subsystems for this agency if
+        # disaggregated_by_supervision_subsystems is modified.
+        if (
+            agency_metric.disaggregated_by_supervision_subsystems is not None
+            and agency_systems.intersection(schema.System.supervision_subsystems())
+        ):
+            for system in agency.systems:
+                # Skip systems which are neither supervision systems nor subsystems.
+                if (
+                    schema.System[system] != schema.System.SUPERVISION
+                    and schema.System[system]
+                    not in schema.System.supervision_subsystems()
+                ):
+                    continue
+
+                metric_definition_key = (
+                    agency_metric.key
+                    if system == current_system
+                    else agency_metric.key.replace(current_system, system, 1)
+                )
+                MetricSettingInterface.update_metric_setting(
+                    session=session,
+                    agency=agency,
+                    agency_metric=MetricInterface(
+                        key=metric_definition_key,
+                        disaggregated_by_supervision_subsystems=agency_metric.disaggregated_by_supervision_subsystems,
+                        is_metric_enabled=agency_metric.disaggregated_by_supervision_subsystems
+                        if schema.System[system]
+                        in schema.System.supervision_subsystems()
+                        else not agency_metric.disaggregated_by_supervision_subsystems,
+                    ),
+                )
+        # Update the metric interface as-given if disaggregated_by_supervision_subsystems
+        # is not being updated, or if the agency does not have supervision subsystems.
+        MetricSettingInterface.update_metric_setting(
+            session=session, agency=agency, agency_metric=agency_metric
+        )
 
     @staticmethod
     def add_or_update_agency_metric_setting(
@@ -40,8 +177,11 @@ class MetricSettingInterface:
     ) -> None:
         """Add the agency metric interface to the MetricSetting table as a lightweight
         json representation with report datapoints removed.
-        Note: Until we start the migration process, this method will write
-        agency metrics to the Datapoint table."""
+        Note: Until we start the migration process, this method will continue to write
+        agency metrics to the Datapoint table. The `new_add_or_update_agency_metric_setting`
+        method (which writes to the MetricSetting table instead of writing agency
+        datapoints) will be used instead of this method after the migration.
+        """
         DatapointInterface.add_or_update_agency_datapoints(
             session=session,
             agency=agency,
