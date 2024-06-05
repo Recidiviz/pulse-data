8f81c93c44102cd6ab5030f6534de1c97766ce28
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-06-05 18:27:15 -0400

    [US_AZ] Add AZ supervision_population_person_level validation view (Recidiviz/recidiviz-data#30401)
    ## Description of the change
    
    This adds a view of external data to the
    supervision_population_person_level view using the
    us_az_validation_supervision_population_person_level raw data added in
    Recidiviz/recidiviz-data#30382
    
    successfully loaded view to sandbox at elise_validation_views
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 00cc1d568e7c0c4327a085d516e657d04f3be356

---
+++ recidiviz/validation/views/external_data/regions/us_az/supervision_population_person_level.py
@@ -0,0 +1,55 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""A view containing supervision population at the person level for Arizona."""
+from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.dataset_config import raw_latest_views_dataset_for_region
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+from recidiviz.validation.views import dataset_config
+
+VIEW_QUERY_TEMPLATE = """
+SELECT 
+  'US_AZ' AS region_code,
+  person.PERSON_ID AS person_external_id,
+  'US_AZ_PERSON_ID' AS external_id_type,
+  CAST(REPORT_DATE AS DATE) AS date_of_supervision,
+  OFFICE AS district,
+  OFFICER AS supervising_officer,
+  SUPV_LEV AS supervision_level
+FROM `{project_id}.{us_az_raw_data_up_to_date_dataset}.validation_supervision_population_person_level_latest` ext
+-- As of 2024-06-04, this leaves 6 people with blank PERSON_IDs who are actively on supervision,
+-- but do not appear with these ADC_NUMBER values in the PERSON table. See TODO(#30383).
+LEFT JOIN `{project_id}.{us_az_raw_data_up_to_date_dataset}.PERSON_latest` person
+USING(ADC_NUMBER)
+"""
+
+US_AZ_SUPERVISION_POPULATION_PERSON_LEVEL_VIEW_BUILDER = SimpleBigQueryViewBuilder(
+    dataset_id=dataset_config.validation_dataset_for_state(StateCode.US_AZ),
+    view_id="supervision_population_person_level",
+    description="A view detailing supervision population at the person level for North Datokta",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+    should_materialize=True,
+    us_az_raw_data_up_to_date_dataset=raw_latest_views_dataset_for_region(
+        state_code=StateCode.US_AZ, instance=DirectIngestInstance.PRIMARY
+    ),
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        US_AZ_SUPERVISION_POPULATION_PERSON_LEVEL_VIEW_BUILDER.build_and_print()
