2afe5e5da45ad364b80f236d250a076aa8f6c4d6
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2024-06-18 10:59:28 -0700

    [Utils] Refactor / bugfixes for attr_utils.py (Recidiviz/recidiviz-data#30738)
    ## Description of the change
    
    I've identified some bugs / areas for improvement in our attr field
    identification utils as I've started to implement the new
    `normalized_entities` definitions.
    
    1) We don't identify optional fields that are defined with more modern
    syntax (`str | None`) as the correct field types
    2) We have an OTHER field type that we basically never use (turns out
    the only fields that were getting classified were `float` fields.
    3) We had basically no tests for `attr_utils.py`
    4) The code in `attr_utils.py` is convoluted and hard to understand.
    
    This PR updates to remove the `BuildableAttrFieldType.OTHER` type, add a
    `BuildableAttrFieldType.FLOAT` type, add a ton more tests, support `str
    | None` syntax, and use more modern Python to determine the field types.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#30075
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: b822200e6e1c5ed2560c952d8d83ae8cfc939d1e

--- recidiviz/tests/common/attr_utils_test.py
+++ recidiviz/tests/common/attr_utils_test.py
@@ -16,12 +16,29 @@
 # =============================================================================
 """Tests for attr_utils.py."""
 import unittest
+from datetime import date, datetime
 from enum import Enum
-from typing import Optional
+from typing import ForwardRef, List, Optional
 
 import attr
+from more_itertools import one
 
-from recidiviz.common.attr_utils import get_enum_cls, is_enum, is_non_optional_enum
+from recidiviz.common.attr_utils import (
+    get_enum_cls,
+    get_non_flat_attribute_class_name,
+    get_non_optional_type,
+    is_date,
+    is_datetime,
+    is_enum,
+    is_float,
+    is_forward_ref,
+    is_int,
+    is_list,
+    is_non_optional_enum,
+    is_optional_type,
+    is_str,
+)
+from recidiviz.utils.types import non_optional
 
 
 class FakeEnum(Enum):
@@ -32,22 +49,330 @@ class FakeEnum(Enum):
 class AttrUtilsTest(unittest.TestCase):
     """Tests for attr_utils.py."""
 
+    def _check_no_utils_crash(self, attribute: attr.Attribute) -> None:
+        """Tests that none of the attr_utils helpers crash for the given attribute."""
+        mutually_exclusive: List[bool] = [
+            is_str(attribute),
+            is_int(attribute),
+            is_float(attribute),
+            is_date(attribute),
+            is_enum(attribute),
+            is_list(attribute),
+            is_forward_ref(attribute),
+        ]
+
+        try:
+            one(result for result in mutually_exclusive if result is True)
+        except ValueError as e:
+            raise ValueError(
+                f"Expected to find exactly one mutually exclusive field classifier "
+                f"that returned True for attribute [{attribute}] "
+            ) from e
+
+        _ = is_datetime(attribute)
+        _ = get_non_flat_attribute_class_name(attribute)
+        _ = get_enum_cls(attribute)
+
     def test_enum_fields(self) -> None:
         @attr.define
         class MyAttrClass:
-            my_enum: Optional[FakeEnum]
+            my_opt_enum: Optional[FakeEnum]
+            my_opt_enum_other: FakeEnum | None
             my_nonnull_enum: FakeEnum
 
         fields_dict = attr.fields_dict(MyAttrClass)
 
-        my_enum_attribute = fields_dict["my_enum"]
+        my_opt_enum_attribute = fields_dict["my_opt_enum"]
+        my_opt_enum_other_attribute = fields_dict["my_opt_enum_other"]
         my_nonnull_enum_attribute = fields_dict["my_nonnull_enum"]
 
-        self.assertTrue(is_enum(my_enum_attribute))
+        self.assertTrue(is_enum(my_opt_enum_attribute))
+        self.assertTrue(is_enum(my_opt_enum_other_attribute))
         self.assertTrue(is_enum(my_nonnull_enum_attribute))
 
-        self.assertFalse(is_non_optional_enum(my_enum_attribute))
+        self.assertFalse(is_non_optional_enum(my_opt_enum_attribute))
+        self.assertFalse(is_non_optional_enum(my_opt_enum_other_attribute))
         self.assertTrue(is_non_optional_enum(my_nonnull_enum_attribute))
 
-        self.assertEqual(FakeEnum, get_enum_cls(my_enum_attribute))
+        self.assertEqual(FakeEnum, get_enum_cls(my_opt_enum_attribute))
+        self.assertEqual(FakeEnum, get_enum_cls(my_opt_enum_other_attribute))
         self.assertEqual(FakeEnum, get_enum_cls(my_nonnull_enum_attribute))
+
+        for attribute in fields_dict.values():
+            self._check_no_utils_crash(attribute)
+
+    def test_str_fields(self) -> None:
+        @attr.define
+        class MyAttrClass:
+            my_opt_str: Optional[str]
+            my_opt_str_other: str | None
+            my_nonnull_str: str
+
+        fields_dict = attr.fields_dict(MyAttrClass)
+
+        my_opt_str_attribute = fields_dict["my_opt_str"]
+        my_opt_str_other_attribute = fields_dict["my_opt_str_other"]
+        my_nonnull_str_attribute = fields_dict["my_nonnull_str"]
+
+        self.assertTrue(is_str(my_opt_str_attribute))
+        self.assertTrue(is_str(my_opt_str_other_attribute))
+        self.assertTrue(is_str(my_nonnull_str_attribute))
+
+        self.assertTrue(is_optional_type(non_optional(my_opt_str_attribute.type)))
+        self.assertTrue(is_optional_type(non_optional(my_opt_str_other_attribute.type)))
+        self.assertFalse(is_optional_type(non_optional(my_nonnull_str_attribute.type)))
+
+        self.assertEqual(None, get_enum_cls(my_opt_str_attribute))
+        self.assertEqual(None, get_enum_cls(my_nonnull_str_attribute))
+
+        self.assertEqual(
+            str, get_non_optional_type(non_optional(my_opt_str_attribute.type))
+        )
+        self.assertEqual(
+            str, get_non_optional_type(non_optional(my_opt_str_other_attribute.type))
+        )
+
+        for attribute in fields_dict.values():
+            self._check_no_utils_crash(attribute)
+
+    def test_int_fields(self) -> None:
+        @attr.define
+        class MyAttrClass:
+            my_opt_int: Optional[int]
+            my_opt_int_other: int | None
+            my_nonnull_int: int
+
+        fields_dict = attr.fields_dict(MyAttrClass)
+
+        my_opt_int_attribute = fields_dict["my_opt_int"]
+        my_opt_int_other_attribute = fields_dict["my_opt_int_other"]
+        my_nonnull_int_attribute = fields_dict["my_nonnull_int"]
+
+        self.assertTrue(is_int(my_opt_int_attribute))
+        self.assertTrue(is_int(my_opt_int_other_attribute))
+        self.assertTrue(is_int(my_nonnull_int_attribute))
+
+        self.assertTrue(is_optional_type(non_optional(my_opt_int_attribute.type)))
+        self.assertTrue(is_optional_type(non_optional(my_opt_int_other_attribute.type)))
+        self.assertFalse(is_optional_type(non_optional(my_nonnull_int_attribute.type)))
+
+        self.assertEqual(
+            int, get_non_optional_type(non_optional(my_opt_int_attribute.type))
+        )
+        self.assertEqual(
+            int, get_non_optional_type(non_optional(my_opt_int_other_attribute.type))
+        )
+
+        for attribute in fields_dict.values():
+            self._check_no_utils_crash(attribute)
+
+    def test_float_fields(self) -> None:
+        @attr.define
+        class MyAttrClass:
+            my_opt_float: Optional[float]
+            my_opt_float_other: float | None
+            my_nonnull_float: float
+
+        fields_dict = attr.fields_dict(MyAttrClass)
+
+        my_opt_float_attribute = fields_dict["my_opt_float"]
+        my_opt_float_other_attribute = fields_dict["my_opt_float_other"]
+        my_nonnull_float_attribute = fields_dict["my_nonnull_float"]
+
+        self.assertTrue(is_float(my_opt_float_attribute))
+        self.assertTrue(is_float(my_opt_float_other_attribute))
+        self.assertTrue(is_float(my_nonnull_float_attribute))
+
+        self.assertTrue(is_optional_type(non_optional(my_opt_float_attribute.type)))
+        self.assertTrue(
+            is_optional_type(non_optional(my_opt_float_other_attribute.type))
+        )
+        self.assertFalse(
+            is_optional_type(non_optional(my_nonnull_float_attribute.type))
+        )
+
+        self.assertEqual(
+            float, get_non_optional_type(non_optional(my_opt_float_attribute.type))
+        )
+        self.assertEqual(
+            float,
+            get_non_optional_type(non_optional(my_opt_float_other_attribute.type)),
+        )
+
+        for attribute in fields_dict.values():
+            self._check_no_utils_crash(attribute)
+
+    def test_date_fields(self) -> None:
+        @attr.define
+        class MyAttrClass:
+            my_opt_date: Optional[date]
+            my_opt_date_other: date | None
+            my_nonnull_date: date
+
+        fields_dict = attr.fields_dict(MyAttrClass)
+
+        my_opt_date_attribute = fields_dict["my_opt_date"]
+        my_opt_date_other_attribute = fields_dict["my_opt_date_other"]
+        my_nonnull_date_attribute = fields_dict["my_nonnull_date"]
+
+        self.assertTrue(is_date(my_opt_date_attribute))
+        self.assertTrue(is_date(my_opt_date_other_attribute))
+        self.assertTrue(is_date(my_nonnull_date_attribute))
+
+        self.assertTrue(is_optional_type(non_optional(my_opt_date_attribute.type)))
+        self.assertTrue(
+            is_optional_type(non_optional(my_opt_date_other_attribute.type))
+        )
+        self.assertFalse(is_optional_type(non_optional(my_nonnull_date_attribute.type)))
+
+        self.assertEqual(
+            date, get_non_optional_type(non_optional(my_opt_date_attribute.type))
+        )
+        self.assertEqual(
+            date, get_non_optional_type(non_optional(my_opt_date_other_attribute.type))
+        )
+
+        for attribute in fields_dict.values():
+            self._check_no_utils_crash(attribute)
+
+    def test_datetime_fields(self) -> None:
+        @attr.define
+        class MyAttrClass:
+            my_opt_datetime: Optional[datetime]
+            my_opt_datetime_other: datetime | None
+            my_nonnull_datetime: datetime
+
+        fields_dict = attr.fields_dict(MyAttrClass)
+
+        my_opt_datetime_attribute = fields_dict["my_opt_datetime"]
+        my_opt_datetime_other_attribute = fields_dict["my_opt_datetime_other"]
+        my_nonnull_datetime_attribute = fields_dict["my_nonnull_datetime"]
+
+        self.assertTrue(is_datetime(my_opt_datetime_attribute))
+        self.assertTrue(is_datetime(my_opt_datetime_other_attribute))
+        self.assertTrue(is_datetime(my_nonnull_datetime_attribute))
+
+        self.assertTrue(is_optional_type(non_optional(my_opt_datetime_attribute.type)))
+        self.assertTrue(
+            is_optional_type(non_optional(my_opt_datetime_other_attribute.type))
+        )
+        self.assertFalse(
+            is_optional_type(non_optional(my_nonnull_datetime_attribute.type))
+        )
+
+        self.assertEqual(
+            datetime,
+            get_non_optional_type(non_optional(my_opt_datetime_attribute.type)),
+        )
+        self.assertEqual(
+            datetime,
+            get_non_optional_type(non_optional(my_opt_datetime_other_attribute.type)),
+        )
+
+        for attribute in fields_dict.values():
+            self._check_no_utils_crash(attribute)
+
+    def test_forward_ref_fields(self) -> None:
+        @attr.define
+        class MyAttrClass:
+            my_opt_forward_ref: Optional["AnotherClass"]
+            # Note: As of Python 3.11, this syntax is not supported:
+            # my_opt_datetime_other: "AnotherClass" | None
+            my_nonnull_forward_ref: "AnotherClass"
+
+        @attr.define
+        class AnotherClass:
+            pass
+
+        fields_dict = attr.fields_dict(MyAttrClass)
+
+        my_opt_forward_ref_attribute = fields_dict["my_opt_forward_ref"]
+        my_nonnull_forward_ref_attribute = fields_dict["my_nonnull_forward_ref"]
+
+        self.assertTrue(is_forward_ref(my_opt_forward_ref_attribute))
+        self.assertTrue(is_forward_ref(my_nonnull_forward_ref_attribute))
+
+        self.assertTrue(
+            is_optional_type(non_optional(my_opt_forward_ref_attribute.type))
+        )
+        self.assertFalse(
+            is_optional_type(non_optional(my_nonnull_forward_ref_attribute.type))
+        )
+
+        self.assertEqual(
+            ForwardRef("AnotherClass"),
+            get_non_optional_type(non_optional(my_opt_forward_ref_attribute.type)),
+        )
+        self.assertEqual(
+            "AnotherClass",
+            get_non_optional_type(non_optional(my_nonnull_forward_ref_attribute.type)),
+        )
+
+        self.assertEqual(
+            "AnotherClass",
+            get_non_flat_attribute_class_name(my_opt_forward_ref_attribute),
+        )
+        self.assertEqual(
+            "AnotherClass",
+            get_non_flat_attribute_class_name(my_nonnull_forward_ref_attribute),
+        )
+
+        for attribute in fields_dict.values():
+            self._check_no_utils_crash(attribute)
+
+    def test_list_fields(self) -> None:
+        @attr.define
+        class ChildClass:
+            pass
+
+        @attr.define
+        class MyAttrClass:
+            my_opt_list: Optional[List["AnotherChildClass"]]
+            my_opt_list_other: Optional[List[ChildClass]]
+            my_nonnull_list: List["AnotherChildClass"]
+            my_nonnull_list_other: List[ChildClass]
+
+        @attr.define
+        class AnotherChildClass:
+            pass
+
+        fields_dict = attr.fields_dict(MyAttrClass)
+
+        my_opt_list_attribute = fields_dict["my_opt_list"]
+        my_opt_list_other_attribute = fields_dict["my_opt_list_other"]
+        my_nonnull_list_attribute = fields_dict["my_nonnull_list"]
+        my_nonnull_list_attribute_other = fields_dict["my_nonnull_list_other"]
+
+        self.assertTrue(is_list(my_opt_list_attribute))
+        self.assertTrue(is_list(my_opt_list_other_attribute))
+        self.assertTrue(is_list(my_nonnull_list_attribute))
+        self.assertTrue(is_list(my_nonnull_list_attribute_other))
+
+        self.assertTrue(is_optional_type(non_optional(my_opt_list_attribute.type)))
+        self.assertTrue(
+            is_optional_type(non_optional(my_opt_list_other_attribute.type))
+        )
+        self.assertFalse(is_optional_type(non_optional(my_nonnull_list_attribute.type)))
+        self.assertFalse(
+            is_optional_type(non_optional(my_nonnull_list_attribute_other.type))
+        )
+
+        self.assertEqual(
+            List[ForwardRef("AnotherChildClass")],  # type: ignore[misc]
+            get_non_optional_type(non_optional(my_opt_list_attribute.type)),
+        )
+        self.assertEqual(
+            List[ChildClass],
+            get_non_optional_type(non_optional(my_opt_list_other_attribute.type)),
+        )
+        self.assertEqual(
+            List[ForwardRef("AnotherChildClass")],  # type: ignore[misc]
+            get_non_optional_type(non_optional(my_nonnull_list_attribute.type)),
+        )
+        self.assertEqual(
+            List[ChildClass],
+            get_non_optional_type(non_optional(my_nonnull_list_attribute_other.type)),
+        )
+
+        for attribute in fields_dict.values():
+            self._check_no_utils_crash(attribute)
