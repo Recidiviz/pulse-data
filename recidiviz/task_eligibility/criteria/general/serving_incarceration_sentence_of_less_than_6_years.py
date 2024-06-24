a39634e36bfde65589f7ddfdd36b4d088a875f1c
Daniel Allen <daniel@recidiviz.org>
2024-06-24 11:11:05 -0400

    [US_AR] Admin transfer TES stubs (Recidiviz/recidiviz-data#30264)
    ## Description of the change
    
    Stubs for the AR administrative transfer opportunity.
    
    Reasons doc
    [here](https://www.notion.so/recidiviz/US_AR-Workflows-Reasons-Doc-Admin-Transfer-c3db37a582d24860a20dd0ddfa212849)
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#30129
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: d03f22d9038605a5b834592d8d0683c59065b634

---
+++ recidiviz/task_eligibility/criteria/general/serving_incarceration_sentence_of_less_than_6_years.py
@@ -0,0 +1,53 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""Describes spans of time when someone is serving an incarceration sentence
+of less than 6 years."""
+
+from google.cloud import bigquery
+
+from recidiviz.task_eligibility.reasons_field import ReasonsField
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateAgnosticTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.placeholder_criteria_builders import (
+    state_agnostic_placeholder_criteria_view_builder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "SERVING_INCARCERATION_SENTENCE_OF_LESS_THAN_6_YEARS"
+
+_DESCRIPTION = """Describes spans of time when someone is serving an incarceration sentence
+of less than 6 years."""
+
+VIEW_BUILDER: StateAgnosticTaskCriteriaBigQueryViewBuilder = (
+    state_agnostic_placeholder_criteria_view_builder(
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+        reasons_fields=[
+            ReasonsField(
+                name="length_of_longest_sentence_in_days",
+                type=bigquery.enums.SqlTypeNames.INT64,
+                description="Length of longest active sentence in days.",
+            )
+        ],
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
