e0e02f8b15d2806ef8e362a527c89cbaa24236bb
ethanoro <ethan@recidiviz.org>
2024-06-24 18:15:44 -0400

    [raw data import dag] pre-migration transformations (Recidiviz/recidiviz-data#30792)
    ## Description of the change
    
    Creates a small wrapper around `BigQueryQueryBuilder` to build a query
    that will trim whitespace, filter nulls and add static fields.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#30168
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 4644d5f8dfcd79e6ecf0b78d9acf628ef5f958c6

---
+++ recidiviz/ingest/direct/raw_data/direct_ingest_raw_table_transformation_query_builder.py
@@ -0,0 +1,88 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""File responsible for generating SQL queries that lightly cleans (trims
+whitespace, removes all NULL rows) raw data that has been newly loaded from a CSV
+into a temp table and adds appropriate metadata columns so that it matches the
+expected format of our main raw data tables.
+"""
+import datetime
+from typing import Optional
+
+from recidiviz.big_query.address_overrides import BigQueryAddressOverrides
+from recidiviz.big_query.big_query_address import BigQueryAddress
+from recidiviz.big_query.big_query_query_builder import BigQueryQueryBuilder
+from recidiviz.ingest.direct.raw_data.raw_file_configs import (
+    DirectIngestRegionRawFileConfig,
+)
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+
+PRE_MIGRATION_TRANSFORMATION_QUERY = """
+SELECT {trimmed_cols}, {recidiviz_metadata_columns} \n FROM `{project_id}.{dataset_id}.{table_id}` \n WHERE {all_nulls_where}
+"""
+
+
+class DirectIngestTempRawTablePreMigrationTransformationQueryBuilder:
+    """Class responsible for generating SQL queries that lightly cleans (trims
+    whitespace, removes all NULL rows) raw data that has been newly loaded from a CSV
+    into a temp table and adds appropriate metadata columns so that it matches the
+    expected format of our main raw data tables.
+    """
+
+    def __init__(
+        self,
+        region_raw_file_config: DirectIngestRegionRawFileConfig,
+        raw_data_instance: DirectIngestInstance,
+        address_overrides: Optional[BigQueryAddressOverrides] = None,
+    ) -> None:
+        self._region_raw_file_config = region_raw_file_config
+        self._raw_data_instance = raw_data_instance
+        self._query_builder = BigQueryQueryBuilder(address_overrides=address_overrides)
+
+    def build_pre_migration_transformations_query(
+        self,
+        *,
+        project_id: str,
+        file_tag: str,
+        source_table: BigQueryAddress,
+        file_id: int,
+        update_datetime: datetime.datetime,
+        is_deleted: bool,
+    ) -> str:
+
+        raw_table_config = self._region_raw_file_config.raw_file_configs[file_tag]
+
+        trimmed_cols = ", ".join(
+            f"TRIM({col.name}) as {col.name}" for col in raw_table_config.columns
+        )
+
+        filter_all_nulls_where = "\n  OR ".join(
+            f"{col.name} IS NOT NULL" for col in raw_table_config.columns
+        )
+
+        recidiviz_metadata_columns = f"{file_id} as file_id, PARSE_DATETIME('%FT%T', '{update_datetime.strftime('%Y-%m-%dT%H:%M:%S')}') as update_datetime, {is_deleted} as is_deleted"
+
+        return self._query_builder.build_query(
+            project_id=project_id,
+            query_template=PRE_MIGRATION_TRANSFORMATION_QUERY,
+            query_format_kwargs={
+                "trimmed_cols": trimmed_cols,
+                "all_nulls_where": filter_all_nulls_where,
+                "recidiviz_metadata_columns": recidiviz_metadata_columns,
+                "dataset_id": source_table.dataset_id,
+                "table_id": source_table.table_id,
+            },
+        )
