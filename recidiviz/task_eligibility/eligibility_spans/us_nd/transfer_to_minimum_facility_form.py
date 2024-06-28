ef6d3ca2e697a089aef65c7ed25c6d4214851907
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2024-06-28 18:23:47 -0500

    [US_ND] Add not_in_btc_wtru criteria (Recidiviz/recidiviz-data#30284)
    ## Description of the change
    
    > I added a new criteria to remove folks in the WTRU unit in BTC from
    the eligible list, since they shouldn't be considered eligible.
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 9ba5594c08e214c97f938f52c485914f0b2e04b5

--- recidiviz/task_eligibility/eligibility_spans/us_nd/transfer_to_minimum_facility_form.py
+++ recidiviz/task_eligibility/eligibility_spans/us_nd/transfer_to_minimum_facility_form.py
@@ -26,16 +26,17 @@ from recidiviz.task_eligibility.candidate_populations.general import (
 from recidiviz.task_eligibility.completion_events.state_specific.us_nd import (
     transfer_to_reentry_center,
 )
-from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
-    SingleTaskEligibilitySpansBigQueryViewBuilder,
-)
-from recidiviz.task_eligibility.criteria.state_specific.us_nd import (
-    not_in_minimum_security_facility,
-    not_in_an_orientation_unit,
-)
 from recidiviz.task_eligibility.criteria.general import (
     custody_level_is_minimum,
     not_in_work_release,
+)
+from recidiviz.task_eligibility.criteria.state_specific.us_nd import (
+    not_in_an_orientation_unit,
+    not_in_minimum_security_facility,
+    not_in_wtru_btc,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
 )
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
@@ -54,6 +55,7 @@ VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
         not_in_minimum_security_facility.VIEW_BUILDER,
         not_in_work_release.VIEW_BUILDER,
         not_in_an_orientation_unit.VIEW_BUILDER,
+        not_in_wtru_btc.VIEW_BUILDER,
     ],
     completion_event_builder=transfer_to_reentry_center.VIEW_BUILDER,
 )
