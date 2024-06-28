8bae8cc60ed938be485ffc6fae93a6da092a8c6b
Santy Mendoza <55068232+santymendoza@users.noreply.github.com>
2024-06-28 09:03:07 -0700

    [US_IX] Sentencing Refactor (Recidiviz/recidiviz-data#29032)
    ## Description of the change
    
    The `state_incarceration_sentence` and the `state_supervision_sentence`
    entities are being deprecated and are being replaced by `state_sentence`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#28952
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 25f257b835c963e08ba4e6382e11cebcbc811271

--- recidiviz/ingest/direct/regions/us_ix/ingest_views/template_sentence.py
+++ recidiviz/ingest/direct/regions/us_ix/ingest_views/template_sentence.py
@@ -96,15 +96,15 @@ SENTENCE_QUERY_TEMPLATE = """
         GROUP BY SentenceId
     ),
     -- get a list of the child SentenceOrderId for each sentence order to determine later on if the child sentence order is an error correction
-    next_sentence_order as (
-        select ParentSentenceOrderId as current_SentenceOrderId,
-            SentenceOrderEventTypeId as next_SentenceOrderEventTypeId,
-            Sequence as next_Sequence,
-            ChargeId as next_ChargeId
+    next_sentence_order AS (
+        select ParentSentenceOrderId AS current_SentenceOrderId,
+            SentenceOrderEventTypeId AS next_SentenceOrderEventTypeId,
+            Sequence AS next_Sequence,
+            ChargeId AS next_ChargeId
         from {scl_SentenceOrder}
         where ParentSentenceOrderId is not null
     ),
-    final_sentences as (
+    final_sentences AS (
         SELECT 
             DISTINCT
             sent.offense_SentenceId as SentenceId,
@@ -148,6 +148,117 @@ SENTENCE_QUERY_TEMPLATE = """
     )
 """
 
+NEW_SENTENCE_QUERY_TEMPLATE = """
+    -- Links all "Sentence" sentences with their associated sentence orders and then any "Offense" sentences associated with the same sentence order
+    -- Here, the relationship between sentence_SentenceId <> offense_SentenceId is 1 <> many
+    SentenceBase AS (
+        SELECT  
+            sent.SentenceId AS offense_SentenceId, 
+            sent.OffenderId,
+            sent.TermId, 
+            ord.SentenceOrderId,
+            ord.CountyId,
+            ord.SentenceDate,
+            ord.CorrectionsCompactStartDate,
+            ord.Sequence,
+            ord.ChargeId,
+            ord_type.SentenceOrderCategoryId,
+            ord.SentenceOrderEventTypeId,
+            ord.CorrectionsCompactEndDate,
+            sentence_order_link.SentenceId AS sentence_SentenceId,
+            off.OffenseSortingOrder,
+            sent_type.OffenseSentenceTypeName,
+            loc.LocationName AS inState
+        FROM {scl_Sentence} sent
+            LEFT JOIN {scl_SentenceLink} link ON sent.SentenceId = link.SentenceId
+            LEFT JOIN {scl_SentenceLinkOffense} linkoffense on link.SentenceLinkId = linkoffense.SentenceLinkId
+            LEFT JOIN {scl_Offense} off ON linkoffense.OffenseId = off.OffenseId
+            LEFT JOIN {scl_SentenceOrder} ord ON off.SentenceOrderId = ord.SentenceOrderId
+            LEFT JOIN {scl_SentenceOrderType} ord_type ON ord.SentenceOrderTypeId = ord_type.SentenceOrderTypeId
+            LEFT JOIN {scl_Term} term ON sent.TermId = term.TermId
+            LEFT JOIN {scl_SentenceDetail} detail on sent.SentenceId = detail.SentenceId
+            LEFT JOIN {scl_SentenceLinkSentenceOrder} linkorder ON ord.SentenceOrderId = linkorder.SentenceOrderId
+            LEFT JOIN {scl_SentenceLink} sentence_order_link ON linkorder.SentenceLinkId = sentence_order_link.SentenceLinkId
+            LEFT JOIN {scl_OffenseSentenceType} sent_type ON detail.OffenseSentenceTypeId = sent_type.OffenseSentenceTypeId
+            LEFT JOIN {ref_Location} loc ON ord.StateId = loc.LocationId
+        WHERE link.SentenceLinkClassId = '1' -- keep only "Offense" sentences (AS opposed to "Sentence Order" sentences)
+            AND sent.OffenderId IS NOT NULL
+        ),
+
+    -- Makes a list of sentence relationships by SentenceId
+    --
+    -- In the scl_RelatedSentence table, each relationship is described by the relationship type, the OriginSentenceId, and theTargetSentenceId
+    -- the OriginSentenceId and the TargetSentenceId could (annoying and inexplicably) be either a "Sentence [Order]" SentenceId or an "Offense" SentenceId.
+    -- For our purposes, we want the sentence relationships in terms of "Offense" SentenceIds.  So here we'll translate the OriginSentenceId/TargetSentenceId
+    -- into the "Offense" SentenceIds if it isn't already by merging SentenceBase on by sentence_SentenceId and seeing if there's a match.  Since both types
+    -- of SentenceId come from the same table, there should only be a match if the given OriginSentenceId/TargetSentenceId is an "Sentence [Order]" SentenceId.
+    -- 
+    -- In addition, the scl_RelatedSentence table also includes "Independent" relationships where OriginSentenceId = TargetSentenceId.  Since that doesn't 
+    -- really tell us anything, we'll filter those out. 
+    -- 
+    -- Lastly, because a sentence could have relationships (e.g. concurrent) with multiple other sentences, we'll group to one row per sentence
+    RelatedSentence AS (
+        SELECT 
+            SentenceId,
+            STRING_AGG(distinct TargetSentenceId, ',') AS relationships
+        FROM (
+            SELECT
+                COALESCE(b_origin_1.offense_SentenceId, rel.OriginSentenceId) AS SentenceId,
+                ref.SentenceRelationshipDesc,
+                COALESCE(b_target_1.offense_SentenceId, rel.TargetSentenceId) AS TargetSentenceId
+            FROM {scl_RelatedSentence} rel
+            LEFT JOIN {scl_SentenceRelationship} ref on rel.SentenceRelationshipId = ref.SentenceRelationshipId
+            LEFT JOIN SentenceBase b_origin_1 on rel.OriginSentenceId = b_origin_1.sentence_SentenceId
+            LEFT JOIN SentenceBase b_target_1 on rel.TargetSentenceId = b_target_1.sentence_SentenceId
+            LEFT JOIN {scl_SentenceLink} link ON COALESCE(b_origin_1.offense_SentenceId, rel.OriginSentenceId) = link.SentenceId
+            WHERE COALESCE(b_origin_1.offense_SentenceId, rel.OriginSentenceId) <> COALESCE(b_target_1.offense_SentenceId, rel.TargetSentenceId)
+            AND link.SentenceLinkClassId = '1'
+        ) sub
+        WHERE SentenceRelationshipDesc = "Consecutive"
+        GROUP BY SentenceId
+    ),
+    -- get a list of the child SentenceOrderId for each sentence order to determine later on if the child sentence order is an error correction
+    next_sentence_order AS (
+        select ParentSentenceOrderId AS current_SentenceOrderId,
+            SentenceOrderEventTypeId AS next_SentenceOrderEventTypeId,
+            Sequence AS next_Sequence,
+            ChargeId AS next_ChargeId
+        from {scl_SentenceOrder}
+        where ParentSentenceOrderId is not null
+    ),
+    -- In final_sentences, we left join the RelatedSentences and next_sentence_order to
+    -- make sure that we only non-error correction sentence
+    final_sentences AS (
+        SELECT 
+            DISTINCT
+            sent.offense_SentenceId AS SentenceId,
+            sent.OffenderId, 
+            sent.CountyId,
+            (DATE(sent.SentenceDate)) AS SentenceDate,
+            rel.relationships AS relationships,
+            (DATE(sent.CorrectionsCompactStartDate)) AS CorrectionsCompactStartDate,
+            (DATE(sent.CorrectionsCompactEndDate)) AS CorrectionsCompactEndDate,
+            sent.SentenceOrderCategoryId,
+            sent.SentenceOrderEventTypeId,
+            sent.OffenseSentenceTypeName,
+            sent.TermId AS SentenceGroupId,
+            inState
+        FROM SentenceBase sent
+        LEFT JOIN RelatedSentence rel ON sent.offense_SentenceId = rel.SentenceId
+        LEFT JOIN next_sentence_order next ON sent.SentenceOrderId = next.current_SentenceOrderId
+        -- we want to only keep only if the next child sentence order of this corrent sentence isn't an error correction
+        -- sentence order with the same ChargeId and Sequence
+        -- i.e. we want to exclude all rows where (next_SentenceOrderEventTypeId = '3' and next_Sequence = Sequence and next_ChargeId = ChargeId)
+        WHERE (next_SentenceOrderEventTypeId <> '3' or next_SentenceOrderEventTypeId is null)
+           OR next_Sequence <> Sequence
+           OR next_ChargeId <> ChargeId    
+    )
+"""
+
 
 def sentence_view_template() -> str:
     return SENTENCE_QUERY_TEMPLATE
+
+
+def new_sentence_view_template() -> str:
+    return NEW_SENTENCE_QUERY_TEMPLATE
---
+++ recidiviz/ingest/direct/regions/us_ix/ingest_views/view_state_sentence.py
@@ -0,0 +1,55 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query for state sentences for US_IX."""
+from recidiviz.ingest.direct.regions.us_ix.ingest_views.template_sentence import (
+    new_sentence_view_template,
+)
+from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
+    DirectIngestViewQueryBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+sentences_base = new_sentence_view_template()
+
+VIEW_QUERY_TEMPLATE = f"""
+    WITH
+        {sentences_base}
+        SELECT
+            SentenceId,
+            OffenderId, 
+            CountyId,
+            SentenceDate,
+            CorrectionsCompactEndDate,
+            CorrectionsCompactStartDate,
+            relationships,
+            SentenceGroupId,
+            OffenseSentenceTypeName,
+            SentenceOrderCategoryId,
+            inState
+        FROM final_sentences
+        WHERE SentenceOrderEventTypeId IN ('1', '2', '3', '5') -- keep "Initial", "Amendment", and "Error Correction" sentences
+"""
+VIEW_BUILDER = DirectIngestViewQueryBuilder(
+    region="us_ix",
+    ingest_view_name="state_sentence",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
