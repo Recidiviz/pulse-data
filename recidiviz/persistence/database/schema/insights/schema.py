ac25470e2391aa4c3efd6f4a1cc083bd82bb69e2
Nnamdi Kenneth Ojibe <ken@recidiviz.org>
2024-07-05 19:51:19 +0200

    [Revert][Insights] Add `email` to  `SupervisionOfficer...` staff schema + Revert "add email field to `SupervisionOfficer...` staff views   (Recidiviz/recidiviz-data#30993)" (Recidiviz/recidiviz-data#31150)
    This reverts commit b495afe4e752661c8f47a2590df4c477ce88f3e7.
    
    ## Description of the change
    
    > Description here
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Gina Valderrama <40475205+Gina-Valderrama@users.noreply.github.com>
    GitOrigin-RevId: fc67a349a40e35bde53e301747eaf3d0ce2f5aeb

--- recidiviz/persistence/database/schema/insights/schema.py
+++ recidiviz/persistence/database/schema/insights/schema.py
@@ -62,7 +62,6 @@ class SupervisionOfficer(PersonBase, InsightsBase):
     __tablename__ = "supervision_officers"
 
     email = Column(String, nullable=True)
-
     supervisor_external_id = Column(String, nullable=True)
     # List of supervisors for this officer
     supervisor_external_ids = Column(ARRAY(String), nullable=True)
