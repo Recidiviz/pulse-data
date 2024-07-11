7fad35f24b5d889384849cb55779fe561d79d720
Santy Mendoza <55068232+santymendoza@users.noreply.github.com>
2024-07-11 10:00:41 -0700

    [US_IX] Adding view for charge info  (Recidiviz/recidiviz-data#31205)
    ## Description of the change
    
    There's a need for charge info to be passed downstream and this view
    collects all the distinct charges
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#31281
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 7407669f0923f9ca0f2d9cbfe5ef4af24a1b7e54

---
+++ recidiviz/calculator/query/state/views/sentencing/charge_record.py
@@ -0,0 +1,61 @@
+#  Recidiviz - a data platform for criminal justice reform
+#  Copyright (C) 2024 Recidiviz, Inc.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  =============================================================================
+"""View to prepare Sentencing charge records for PSI tools for export to the frontend."""
+from recidiviz.big_query.selected_columns_big_query_view import (
+    SelectedColumnsBigQueryViewBuilder,
+)
+from recidiviz.calculator.query.state import dataset_config
+from recidiviz.calculator.query.state.dataset_config import SESSIONS_DATASET
+from recidiviz.calculator.query.state.views.sentencing.us_ix.sentencing_charge_template import (
+    US_IX_SENTENCING_CHARGE_TEMPLATE,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+SENTENCING_CHARGE_RECORD_VIEW_NAME = "sentencing_charge_record"
+
+SENTENCING_CHARGE_RECORD_DESCRIPTION = """
+    Sentencing charge records to be exported to frontend to power PSI tools.
+    """
+
+SENTENCING_CHARGE_RECORD_QUERY_TEMPLATE = f"""
+WITH 
+    ix_charges AS 
+        ({US_IX_SENTENCING_CHARGE_TEMPLATE}), 
+    -- full_query serves as a template for when Sentencing expands to other states and we union other views
+    full_query AS 
+    (
+        SELECT * FROM ix_charges
+    ) 
+    SELECT
+        {{columns}}
+    FROM full_query
+"""
+
+SENTENCING_CHARGE_RECORD_VIEW_BUILDER = SelectedColumnsBigQueryViewBuilder(
+    view_id=SENTENCING_CHARGE_RECORD_VIEW_NAME,
+    dataset_id=dataset_config.SENTENCING_OUTPUT_DATASET,
+    view_query_template=SENTENCING_CHARGE_RECORD_QUERY_TEMPLATE,
+    description=SENTENCING_CHARGE_RECORD_DESCRIPTION,
+    sessions_dataset=SESSIONS_DATASET,
+    should_materialize=True,
+    columns=["state_code", "charge"],
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        SENTENCING_CHARGE_RECORD_VIEW_BUILDER.build_and_print()
