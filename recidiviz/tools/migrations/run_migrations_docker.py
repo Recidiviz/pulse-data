15f188eaaeddb9472f237a7b1d87ffd84e0113cc
Jen Overgaag <jovergaag@gmail.com>
2024-07-12 14:38:57 -0700

    [Insights] load_local_db updates to manage configurations/user_metadata (Recidiviz/recidiviz-data#31163)
    ## Description of the change
    
    -Adds a script (written by @ohaibbq ) to run migrations on state
    segmented databases, and uses that script for the `insights_migrations`
    action in docker-compose. This essentially enables the migrations to be
    managed by docker instead of with `load_local_db` for the
    `configurations` and `user_metadata` tables.
    -Updates the `load_local_db` script to allow it to manage the
    `configurations` and `user_metadata` tables now that they can be
    imported using json fixtures.
    
    Note: for anyone who currently has any INSIGHTS databases with
    `configurations` or `user_metadata` tables already existing, those
    tables must be deleted entirely before running `insights_migrations` in
    order for the migrations to run successfully.
    
    Steps:
    1. From this branch, run `docker compose -f docker-compose.yaml -f
    docker-compose.application-data-import.yaml up` to spin up the
    `insights_db` -- there will be errors on `insights_migrations` if any
    `configurations` or `user_metadata` tables exist. Thats OK
    2. Using Postico or any similar tool, open any state databases inside of
    your `insights_db` that may exist, and delete the `configurations` and
    `user_metadata` tables
    3. From another command prompt, run `docker compose -f
    docker-compose.yaml up insights_migrations` and verify that it finishes
    successfully.
    4. Now run the `load_local_db` script to populate those tables for any
    state `docker exec recidiviz-data-import_service-1 pipenv run python -m
    recidiviz.tools.insights.load_local_db --data_type FIXTURE --state_codes
    US_ID`
    
    I have tested that the `id` sequenced integers are recreated in the same
    sequence in the temporary id column during the migration. I have not
    tested the `downgrade` migration function.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Dan Hansen <dan@recidiviz.org>
    GitOrigin-RevId: 39f010d470b1f709fe67007c75e706983dbcd3ff

---
+++ recidiviz/tools/migrations/run_migrations_docker.py
@@ -0,0 +1,80 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2020 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+This script runs alembic migrations in the specified docker database(s).
+
+It is called from docker-compose to run migrations on state segmented databases.
+"""
+import argparse
+import logging
+import sys
+
+import alembic.config
+
+from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.persistence.database.sqlalchemy_engine_manager import (
+    SQLAlchemyEngineManager,
+)
+from recidiviz.server_config import database_keys_for_schema_type
+
+
+def create_parser() -> argparse.ArgumentParser:
+    """Returns an argument parser for the script."""
+    parser = argparse.ArgumentParser(
+        description="Run migrations against PostgresQL database."
+    )
+    parser.add_argument(
+        "--database",
+        type=SchemaType,
+        choices=list(SchemaType),
+        help="Specifies which database to run against.",
+        required=True,
+    )
+    return parser
+
+
+def main(
+    schema_type: SchemaType,
+) -> None:
+    """
+    Invokes the main code path for running migrations in the specified docker database(s).
+    """
+    # Run migrations
+    for database_key in database_keys_for_schema_type(schema_type):
+        SQLAlchemyEngineManager.update_sqlalchemy_env_vars(
+            database_key=database_key,
+        )
+        try:
+            config = alembic.config.Config(database_key.alembic_file)
+            alembic.command.upgrade(config, "head")
+        except Exception as e:
+            logging.error("Migrations failed to run: %s", e)
+            sys.exit(1)
+        finally:
+            SQLAlchemyEngineManager.teardown_engine_for_database_key(
+                database_key=database_key
+            )
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+
+    args = create_parser().parse_args()
+
+    main(
+        schema_type=args.database,
+    )
