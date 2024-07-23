7e983293264009fee1cc33da52cb0bf392d1a60d
ethanoro <ethan@recidiviz.org>
2024-07-23 14:03:38 -0400

    [raw data import dag] add task for `mv_path_to_processed_path` (Recidiviz/recidiviz-data#31643)
    ## Description of the change
    
    adds the airflow task to call
    `DirectIngestGCSFileSystem.mv_path_to_processed_path`, catching and
    batching errors
    
    the input to this task will be `processed_paths_to_rename` from Recidiviz/recidiviz-data#31540
    
    see Recidiviz/recidiviz-data#31651 to see `rename_successfully_imported_paths ` added to the dag
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#30169
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 2f3af77a642f2363e7c625bf7641569001ff7c0a

--- recidiviz/airflow/dags/raw_data/clean_up_tasks.py
+++ recidiviz/airflow/dags/raw_data/clean_up_tasks.py
@@ -25,8 +25,16 @@ from recidiviz.big_query.big_query_address import BigQueryAddress
 from recidiviz.big_query.big_query_client import BigQueryClientImpl
 from recidiviz.cloud_storage.gcsfs_factory import GcsfsFactory
 from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
+from recidiviz.ingest.direct.gcs.direct_ingest_gcs_file_system import (
+    DirectIngestGCSFileSystem,
+)
+from recidiviz.ingest.direct.gcs.directory_path_utils import (
+    gcsfs_direct_ingest_storage_directory_path_for_state,
+)
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
 
 MAX_DELETE_THREADS = 16
+MAX_RENAME_THREADS = 16
 
 
 @task
@@ -97,3 +105,54 @@ def clean_up_temporary_tables(serialized_temporary_tables: List[str]) -> None:
 
     if failed:
         raise ExceptionGroup("Errors occurred during table deletion", failed)
+
+
+@task
+def move_successfully_imported_paths_to_storage(
+    region_code: str,
+    raw_data_instance: DirectIngestInstance,
+    serialized_successfully_imported_paths: List[str],
+) -> None:
+    """Concurrently renames |serialized_successfully_imported_paths| from their
+    unprocessed to processed name in storage.
+    """
+    successfully_imported_paths = [
+        GcsfsFilePath.from_absolute_path(path)
+        for path in serialized_successfully_imported_paths
+    ]
+
+    fs = DirectIngestGCSFileSystem(GcsfsFactory.build())
+
+    storage_path = gcsfs_direct_ingest_storage_directory_path_for_state(
+        region_code=region_code, ingest_instance=raw_data_instance
+    )
+
+    with futures.ThreadPoolExecutor(max_workers=MAX_RENAME_THREADS) as executor:
+        delete_futures = [
+            executor.submit(
+                fs.mv_unprocessed_path_to_processed_path_in_storage, path, storage_path
+            )
+            for path in successfully_imported_paths
+        ]
+
+        num_successfully_moved_paths = 0
+        failed_moves: List[Exception] = []
+
+        for f in futures.as_completed(delete_futures):
+            try:
+                _ = f.result()
+                num_successfully_moved_paths += 1
+            except Exception as e:
+                failed_moves.append(e)
+
+    logging.info(
+        "Moved [%s/[%s] files to their processed path in storage",
+        num_successfully_moved_paths,
+        len(successfully_imported_paths),
+    )
+
+    if failed_moves:
+        raise ExceptionGroup(
+            "Errors occurred moving files to their processed paths in storage",
+            failed_moves,
+        )
