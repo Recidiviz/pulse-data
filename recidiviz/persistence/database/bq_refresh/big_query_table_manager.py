21fdbeeeb155c78ad8687845ab308b02c847a77f
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2024-07-30 09:59:21 -0700

    [Cleanup] Delete some unused schema building helpers (Recidiviz/recidiviz-data#31760)
    ## Description of the change
    
    Updates some helper functions related to updating schemas based on
    schema.py definitions - these are now unused.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Related to Recidiviz/recidiviz-data#29517
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 284a83af417772fca93bf6c575925b7ef3f75db5

--- recidiviz/persistence/database/bq_refresh/big_query_table_manager.py
+++ recidiviz/persistence/database/bq_refresh/big_query_table_manager.py
@@ -21,16 +21,14 @@ match that of the schema being deployed before the next CloudSqlToBQ export or d
 pipelines attempt to write these datasets. Does not perform any migrations, only adds
 and deletes columns where necessary.
 """
-from typing import List, Optional
+from typing import List
 
 from google.cloud.bigquery import SchemaField
 from sqlalchemy import Table
 
-from recidiviz.big_query.big_query_client import BigQueryClient, BigQueryClientImpl
 from recidiviz.big_query.big_query_utils import schema_for_sqlalchemy_table
 from recidiviz.persistence.database.schema_type import SchemaType
 from recidiviz.persistence.database.schema_utils import (
-    get_all_table_classes_in_schema,
     is_association_table,
     schema_has_region_code_query_support,
 )
@@ -47,73 +45,3 @@ def bq_schema_for_sqlalchemy_table(
     ) and is_association_table(table.name)
 
     return schema_for_sqlalchemy_table(table, add_state_code_field=add_state_code_field)
-
-
-def update_bq_schema_for_sqlalchemy_table(
-    bq_client: BigQueryClient, schema_type: SchemaType, dataset_id: str, table: Table
-) -> None:
-    """Updates the schema of the BigQuery table in the dataset that stores the
-    contents of the |table| to have all expected columns."""
-    bq_dataset_ref = bq_client.dataset_ref_for_id(dataset_id)
-
-    table_id = table.name
-
-    schema_for_table = bq_schema_for_sqlalchemy_table(schema_type, table)
-
-    if bq_client.table_exists(bq_dataset_ref, table_id):
-        # Compare schema derived from schema table to existing dataset and
-        # update if necessary.
-        bq_client.update_schema(
-            dataset_id,
-            table_id,
-            schema_for_table,
-        )
-    else:
-        bq_client.create_table_with_schema(
-            dataset_id,
-            table_id,
-            schema_for_table,
-        )
-
-
-def update_bq_dataset_to_match_sqlalchemy_schema(
-    schema_type: SchemaType,
-    dataset_id: str,
-    default_table_expiration_ms: Optional[int],
-    bq_region_override: Optional[str] = None,
-) -> None:
-    """For each table defined in the schema, ensures that the schema of the
-    table the provided BigQuery dataset matches the schema defined in the corresponding
-    schema.py.
-    """
-    bq_client = BigQueryClientImpl(region_override=bq_region_override)
-    bq_dataset_ref = bq_client.dataset_ref_for_id(dataset_id)
-
-    bq_client.create_dataset_if_necessary(bq_dataset_ref, default_table_expiration_ms)
-
-    for table in get_all_table_classes_in_schema(schema_type):
-        update_bq_schema_for_sqlalchemy_table(
-            bq_client=bq_client,
-            schema_type=schema_type,
-            dataset_id=dataset_id,
-            table=table,
-        )
-
-
-def update_bq_dataset_to_match_sqlalchemy_schema_for_one_table(
-    schema_type: SchemaType,
-    dataset_id: str,
-    table: Table,
-    default_table_expiration_ms: Optional[int],
-    bq_region_override: Optional[str] = None,
-) -> None:
-    bq_client = BigQueryClientImpl(region_override=bq_region_override)
-    bq_dataset_ref = bq_client.dataset_ref_for_id(dataset_id)
-
-    bq_client.create_dataset_if_necessary(bq_dataset_ref, default_table_expiration_ms)
-    update_bq_schema_for_sqlalchemy_table(
-        bq_client=bq_client,
-        schema_type=schema_type,
-        dataset_id=dataset_id,
-        table=table,
-    )
