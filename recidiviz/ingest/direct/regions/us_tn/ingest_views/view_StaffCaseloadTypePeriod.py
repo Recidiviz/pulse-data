dfb391a1f27ff38aedcfbc8b37705362ee11fd4b
Lydia Masri <102679298+lydiamasri2@users.noreply.github.com>
2024-08-06 08:23:06 -0400

    [US_TN] Adding staff caseload type period (Recidiviz/recidiviz-data#31721)
    ## Description of the change
    
    Adding caseload-type periods for TN staff
    
    ingest pipeline run successful
    
    *Bucket Numbers*
    INTERNAL_UNKNOWN: 88
    ADMINISTRATIVE_SUPERVISION: 14
    ALCOHOL_AND_DRUG: 27
    ELECTRONIC_MONITORING: 8
    GENERAL: 590
    OTHER: 32 (primarily compact in cases)
    OTHER_COURT: 52
    SEX_OFFENSE: 138
    
    The UNKNOWN are primarily cases where we do not know what the code means
    and have not gotten a clear answer from TN yet. I would also like to
    clarify the COURT mappings as some might not technically be a caseload.
    Added a ticket
    [here](https://github.com/Recidiviz/recidiviz-data/issues/31902) to
    track this and linked it in PR.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 027dfe975690cb28f0624389397488cc108d58a9

---
+++ recidiviz/ingest/direct/regions/us_tn/ingest_views/view_StaffCaseloadTypePeriod.py
@@ -0,0 +1,151 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query containing staff caseload type periods for case officers in TN
+"""
+
+from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
+    DirectIngestViewQueryBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+WITH 
+-- getting information from @ALL roster
+all_periods as (
+  SELECT 
+    external_id, 
+    location_district,
+    SupervisorID, 
+    UPPER(CaseloadType) AS CaseloadType, 
+    UPPER(Active) as Active,
+    SpecializedTeam,
+    update_datetime, 
+    FROM {RECIDIVIZ_REFERENCE_staff_supervisor_and_caseload_roster@ALL}
+), 
+-- getting rid of roles that aren't considered caseloads
+-- #TODO(#31902): Continue to clarify and refine caseload types 
+get_real_caseloads AS (
+    SELECT * FROM all_periods
+    WHERE CASELOADTYPE NOT LIKE '%NO CASELOAD%' 
+        AND CASELOADTYPE NOT LIKE '%SECRETARY%' 
+        AND CASELOADTYPE NOT LIKE '%PSI%' 
+        AND CASELOADTYPE NOT LIKE '%BHS%' --BHS are Social Workers- no caseload (Behavioral Health Specialist)
+        AND CASELOADTYPE NOT LIKE 'DATS%' --DAT are Risk and Needs assessors- they do not have a caseload.
+        AND CASELOADTYPE NOT IN ('RESIGNED',
+                                 'DAT OFFICER', --DAT are Risk and Needs assessors- they do not have a caseload.
+                                 'DAATS', --DAT are Risk and Needs assessors- they do not have a caseload.
+                                 'N/A',
+                                 'PSA', -- Sanction officer, does not carry caseload
+                                 'CA', -- Correctional Administrator, does not carry caseload
+                                 'DAT') 
+
+),
+-- First, identify the first ever reported caseloadtype for each staff member sent to us by TN
+first_reported_caseload AS (
+    SELECT
+    external_id as StaffID, 
+    CaseloadType,
+    update_datetime as UpdateDate
+FROM 
+    (SELECT
+        external_id,
+        CaseloadType,
+        update_datetime,
+        ROW_NUMBER() OVER (PARTITION BY external_id ORDER BY update_datetime ASC) as SEQ
+    FROM get_real_caseloads s
+    WHERE external_id IS NOT NULL AND CaseloadType IS NOT NULL) s 
+WHERE SEQ = 1),
+
+-- Then, determine when the caseload type sent for a given person has changed since the last time TN sent us a roster
+caseload_change_recorded AS (
+  SELECT 
+      external_id as StaffID, 
+      CaseloadType as CurrentCaseloadType,
+      LAG(CaseloadType) OVER (PARTITION BY external_id ORDER BY update_datetime ASC) as PreviousCaseloadType,
+      update_datetime as UpdateDate
+  FROM get_real_caseloads
+  WHERE external_id IS NOT NULL
+),
+-- Start all periods 2 years in the past, then add any key change dates, to get a list of all dates that a person has changed caseloads
+key_caseload_change_dates AS(
+    --arbitrary first period start dates started 2 years before first receiving caseload data
+    SELECT
+    DISTINCT StaffID, 
+    CaseloadType, 
+    CAST('2021-10-24 00:00:00' AS DATETIME) as UpdateDate
+    FROM first_reported_caseload
+    WHERE CaseloadType IS NOT NULL
+
+    UNION ALL
+    
+    SELECT 
+        StaffID, 
+        CurrentCaseloadType AS CaseloadType, 
+        UpdateDate
+    FROM caseload_change_recorded
+    WHERE CurrentCaseloadType != PreviousCaseloadType
+),
+-- creating a ranking to reduce duplicates
+ranked_rows AS(
+    SELECT 
+        *,
+        ROW_NUMBER() OVER (PARTITION BY StaffID,CaseloadType,UpdateDate ORDER BY UpdateDate DESC) as RecencyRank
+    FROM key_caseload_change_dates
+),
+-- creating unique rows
+create_unique_rows AS (
+    SELECT 
+        StaffID,
+        CaseloadType,
+        UpdateDate, 
+        ROW_NUMBER() OVER (PARTITION BY StaffID ORDER BY UpdateDate ASC) AS CaseloadChangeOrder
+    FROM ranked_rows
+    WHERE RecencyRank = 1
+),
+-- creating periods with all the key dates
+construct_periods AS (
+    SELECT 
+        StaffID,
+        CaseloadType,
+        UpdateDate as StartDate,
+        LEAD(UpdateDate) OVER person_sequence as EndDate,
+        CaseloadChangeOrder
+    FROM create_unique_rows 
+    WINDOW person_sequence AS (PARTITION BY StaffID ORDER BY CaseloadChangeOrder)
+)
+-- doing additional cleaning for output
+SELECT 
+    REGEXP_REPLACE(StaffID, r'[^A-Z0-9]', '') as StaffID, 
+    CaseloadType, 
+    StartDate,
+    EndDate,
+    CaseloadChangeOrder
+FROM construct_periods
+
+
+"""
+
+VIEW_BUILDER = DirectIngestViewQueryBuilder(
+    region="us_tn",
+    ingest_view_name="StaffCaseloadTypePeriod",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
