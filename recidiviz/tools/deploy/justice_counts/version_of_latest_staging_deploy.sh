a0fb290d9d7616291530e0d342060091a2ca34df
Brandon Hills <130382407+brandon-hills@users.noreply.github.com>
2024-08-07 14:23:27 -0400

    [Justice Counts] Fix version script to handle JSON array input. (Recidiviz/recidiviz-data#32142)
    ## Description of the change
    
    Fix version script to handle JSON array input.
    
    It turns out that `gcloud artifacts docker images list` sometimes sends
    the tag list as a JSON array instead of a comma-separated string. Handle
    both.
    
    ## Testing
    Tested on my and @mxosman's machines.
    
    GitOrigin-RevId: a4ced84fd81436d4f93fc593ea7875d08b302ac7

--- recidiviz/tools/deploy/justice_counts/version_of_latest_staging_deploy.sh
+++ recidiviz/tools/deploy/justice_counts/version_of_latest_staging_deploy.sh
@@ -7,9 +7,9 @@ Example usage:
 "
 
 STAGING_IMAGE_BASE="us-central1-docker.pkg.dev/justice-counts-staging/publisher-and-dashboard-images/main"
-
-# Find the Docker image currently deployed to staging (i.e. with the tag 'latest')
-STAGING_IMAGE_JSON=$(gcloud artifacts docker images list "${STAGING_IMAGE_BASE}" --filter="tags:latest" --format=json --include-tags)
+# Find the Docker image currently deployed to staging (i.e. with the tag 'latest').
+# The argument 2>/dev/null suppresses logging.
+STAGING_IMAGE_JSON=$(gcloud artifacts docker images list "${STAGING_IMAGE_BASE}" --filter="tags:latest" --format=json --include-tags 2>/dev/null)
 
 if [[ ${STAGING_IMAGE_JSON}  == "[]" ]]; then
     echo_error "No Docker images found in ${STAGING_IMAGE_JSON} with tag 'latest'"
@@ -19,7 +19,15 @@ fi
 # The tags of a staging image look like ({git sha}, jc.v1.XXX.0, latest), so the
 # second tag is the version number.
 STAGING_TAGS=$(jq -r '.[0].tags' <<< "${STAGING_IMAGE_JSON}")
-CURRENT_STAGING_VERSION_TAG=$(cut -d ',' -f 2 <<< "${STAGING_TAGS}" | xargs)
+
+# Check if STAGING_TAGS is a JSON array or a comma-separated string
+if echo "${STAGING_TAGS}" | jq empty 2>/dev/null; then
+  # STAGING_TAGS is a JSON array
+  CURRENT_STAGING_VERSION_TAG=$(jq -r '.[1]' <<< "${STAGING_TAGS}" | xargs)
+else
+  # STAGING_TAGS is a comma-separated string
+  CURRENT_STAGING_VERSION_TAG=$(cut -d ',' -f 2 <<< "${STAGING_TAGS}" | xargs)
+fi
 
 # We also need to remove the 'jc.' prefix from the tag to get just the version number.
 CURRENT_STAGING_VERSION="${CURRENT_STAGING_VERSION_TAG#jc.}"
