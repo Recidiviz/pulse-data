09dc63212e040e3fae07b58744a3fe1b4cc96181
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-08-12 17:10:49 -0400

    [US_AZ] Pull officer IDs into `supervision_population_person_level` validation query (Recidiviz/recidiviz-data#32314)
    ## Description of the change
    
    Since external validation data only refers to supervising officers by
    their names, and internal metrics only refer to officers by their IDs,
    this change joins officers to their IDs using raw data. There are some
    nuances to how this has to be done, and I have a ticket to investigate
    and track improvements to that logic here
    https://github.com/Recidiviz/recidiviz-data/issues/32312.
    
    Validation view tested in a sandbox yields 468 rows where the internal
    and external supervising officer IDs do not match. That brings the error
    rate down to 9%. 426 of those are accounted for by the issue documented
    in Recidiviz/recidiviz-data#32312, so once that is resolved, I expect the error rate to drop to
    <1%.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Related to Recidiviz/recidiviz-data#31837
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 39e050a186513f5783f74fc8e96ab0fee5e3e11a

--- recidiviz/validation/views/external_data/regions/us_az/supervision_population_person_level.py
+++ recidiviz/validation/views/external_data/regions/us_az/supervision_population_person_level.py
@@ -24,19 +24,52 @@ from recidiviz.utils.metadata import local_project_id_override
 from recidiviz.validation.views import dataset_config
 
 VIEW_QUERY_TEMPLATE = """
-SELECT 
+WITH base AS (
+SELECT
   'US_AZ' AS region_code,
   person.PERSON_ID AS person_external_id,
   'US_AZ_PERSON_ID' AS external_id_type,
   CAST(REPORT_DATE AS DATETIME) AS date_of_supervision,
   OFFICE AS district,
-  OFFICER AS supervising_officer,
-  SUPV_LEV AS supervision_level
+  EXT.OFFICER,
+  officer.USERID AS supervising_officer_person_id,
+  SUPV_LEV AS supervision_level,
+  officer.is_active,
+  officer.IS_CC_USER,
+  AQ_SEARCH_CRITERIA
 FROM `{project_id}.{us_az_raw_data_up_to_date_dataset}.validation_supervision_population_person_level_latest` ext
 -- As of 2024-06-04, this leaves 6 people with blank PERSON_IDs who are actively on supervision,
 -- but do not appear with these ADC_NUMBER values in the PERSON table. See TODO(#30383).
 LEFT JOIN `{project_id}.{us_az_raw_data_up_to_date_dataset}.PERSON_latest` person
 USING(ADC_NUMBER)
+LEFT JOIN `{project_id}.{us_az_raw_data_up_to_date_dataset}.MEA_PROFILES_latest` officer
+ON(
+  UPPER(CONCAT(officer.FIRSTNAME, ' ', officer.SURNAME)) = UPPER(ext.OFFICER) 
+  )
+), get_officer_id AS (
+    -- TODO(#32312): This logic leaves 3 officers with an ID that does not match the one that
+    -- they have internally, despite the fact that both IDs refer to the same person in real
+    -- life. I need to investigate further why this is happening and what is the best solution.
+SELECT DISTINCT OFFICER, supervising_officer_person_id
+FROM base
+QUALIFY (ROW_NUMBER() OVER (PARTITION BY OFFICER ORDER BY CASE
+WHEN IS_CC_USER = 'Y' AND IS_ACTIVE = 'Y' THEN 1
+WHEN IS_CC_USER IS NULL AND IS_ACTIVE = 'Y' THEN 2
+WHEN IS_CC_USER IS NULL AND IS_ACTIVE IS NULL THEN 3
+ELSE 4
+END )) = 1
+)
+SELECT 
+  region_code,
+  person_external_id,
+  external_id_type,
+  date_of_supervision,
+  district,
+  get_officer_id.supervising_officer_person_id AS supervising_officer,
+  supervision_level,
+FROM base 
+LEFT JOIN get_officer_id
+USING(OFFICER)
 """
 
 US_AZ_SUPERVISION_POPULATION_PERSON_LEVEL_VIEW_BUILDER = SimpleBigQueryViewBuilder(
