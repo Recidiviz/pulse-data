b7a7c13d8daf0543b755d69749af9bff266805ac
Justine <justine@recidiviz.org>
2024-08-12 09:38:32 -0400

    [TES][US_IX] Migrate JII LSU to almost eligible infra (Recidiviz/recidiviz-data#32246)
    ## Description of the change
    
    Move the Idaho LSU JII eligibility spans query to the almost eligible
    infrastructure. I created a new eligibility span view for the JII
    version with the more flexible almost eligible criteria conditions. This
    change has no impact on the LSU JII form record between sandbox and
    staging.
    
    ```
    python -m recidiviz.tools.load_views_to_sandbox \
            --sandbox_dataset_prefix jkunz_almost_eligible auto \
            --load_up_to_addresses workflows_views.us_ix_transfer_to_limited_supervision_jii_record
    ```
    
    ```
    python -m recidiviz.tools.calculator.compare_views \
            --dataset_original workflows_views \
            --dataset_new jkunz_almost_eligible_workflows_views \
            --view_id us_ix_transfer_to_limited_supervision_jii_record_materialized \
            --output_dataset_prefix jkunz_almost_eligible_test \
            --grouping_columns state_code \
            --primary_keys external_id,state_code
    ```
    
    ## Related issues
    
    Advances Recidiviz/recidiviz-data#21350
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6ec326f165224f7953a040c1faf5350d7bb1db46

---
+++ recidiviz/task_eligibility/eligibility_spans/us_ix/complete_transfer_to_limited_supervision_jii_form.py
@@ -0,0 +1,71 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Builder for a task eligibility spans view that shows the spans of time during which
+someone in ID is eligible or almost eligible to complete the form for transfer to limited unit supervision.
+"""
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.criteria.general import negative_da_within_90_days
+from recidiviz.task_eligibility.criteria.state_specific.us_ix import (
+    income_verified_within_3_months,
+)
+from recidiviz.task_eligibility.criteria_condition import (
+    NotEligibleCriteriaCondition,
+    PickNCompositeCriteriaCondition,
+)
+from recidiviz.task_eligibility.eligibility_spans.us_ix import (
+    complete_transfer_to_limited_supervision_form,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Shows the spans of time during which someone in ID is eligible or almost eligible
+to complete the form for transfer to limited unit supervision. These spans have a more flexible set of almost
+eligible criteria conditions since it is JII-facing.
+"""
+
+COMPLETE_TRANSFER_TO_LSU_LINE_STAFF_VERSION = (
+    complete_transfer_to_limited_supervision_form.VIEW_BUILDER
+)
+
+VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
+    state_code=StateCode.US_IX,
+    task_name="COMPLETE_TRANSFER_TO_LIMITED_SUPERVISION_JII_FORM",
+    description=_DESCRIPTION,
+    candidate_population_view_builder=COMPLETE_TRANSFER_TO_LSU_LINE_STAFF_VERSION.candidate_population_view_builder,
+    criteria_spans_view_builders=COMPLETE_TRANSFER_TO_LSU_LINE_STAFF_VERSION.criteria_spans_view_builders,
+    completion_event_builder=COMPLETE_TRANSFER_TO_LSU_LINE_STAFF_VERSION.completion_event_builder,
+    almost_eligible_condition=PickNCompositeCriteriaCondition(
+        sub_conditions_list=[
+            NotEligibleCriteriaCondition(
+                criteria=negative_da_within_90_days.VIEW_BUILDER,
+                description="Missing negative drug screen within 90 days",
+            ),
+            NotEligibleCriteriaCondition(
+                criteria=income_verified_within_3_months.VIEW_BUILDER,
+                description="Missing employment verification within 90 days",
+            ),
+        ],
+        at_least_n_conditions_true=1,
+    ),
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
