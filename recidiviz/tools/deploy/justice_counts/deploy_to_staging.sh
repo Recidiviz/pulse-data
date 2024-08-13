7cf2bfba2ec0cac0fc8d4261af9c188985161e83
lilidworkin <99899591+lilidworkin@users.noreply.github.com>
2024-08-13 10:53:11 -0400

    [Justice Counts] Remove user permissions check from staging deploy (Recidiviz/recidiviz-data#32329)
    ## Description of the change
    
    We no longer need this to run on staging since all of the checks are now
    against the production database!
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 27db06dea18c1b696ac09d46f776fef1ed8c6549

--- recidiviz/tools/deploy/justice_counts/deploy_to_staging.sh
+++ recidiviz/tools/deploy/justice_counts/deploy_to_staging.sh
@@ -143,7 +143,6 @@ echo "Updating Image for Cloud Run Jobs"
 run_cmd gcloud run jobs update recurring-report-creation --image "${LATEST_DOCKER_TAG}" --region "us-central1" --project "justice-counts-staging"
 run_cmd gcloud run jobs update recurring-new-mexico-courts-dataxchange --image "${LATEST_DOCKER_TAG}" --region "us-central1" --project "justice-counts-staging"
 run_cmd gcloud run jobs update csg-data-pull --image "${LATEST_DOCKER_TAG}" --region "us-central1" --project "justice-counts-staging"
-run_cmd gcloud run jobs update user-permission-check --image "${LATEST_DOCKER_TAG}" --region "us-central1" --project "justice-counts-staging"
 run_cmd gcloud run jobs update upload-reminder-email-job --image "${LATEST_DOCKER_TAG}" --region "us-central1" --project "justice-counts-staging"
 run_cmd gcloud run jobs update copy-superagency-metric-settings-to-child-agencies --image "${LATEST_DOCKER_TAG}" --region "us-central1" --project "justice-counts-staging"
 # TODO(#16325): Create release candidate branches to facilitate cherry-picks.
