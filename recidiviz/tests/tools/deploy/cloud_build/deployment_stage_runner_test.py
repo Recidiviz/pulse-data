4b6a5312fbc2a3e9da71e10144c17452ecd06995
Dan Hansen <dan@recidiviz.org>
2024-08-21 11:03:04 -0700

    [Deploy] Add CloudBuild integration with steps for building/tagging images, running migrations, deploying app engine (Recidiviz/recidiviz-data#32341)
    ## Description of the change
    
    This adds the scaffolding necessary to run steps of our deployment in
    Cloud Build.
    The `deployment_stage_runner` CLI uses an interactive retry loop on build
    failure.
    
    I'll be following up with the integration of calling `python -m
    recidiviz.tools.deploy.cloud_build.deployment_stage_runner` in `bash` in an
    upcoming PR.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#31461
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: e3d7c0a61120079c930ffb797a791ca90b4f15cf

---
+++ recidiviz/tests/tools/deploy/cloud_build/deployment_stage_runner_test.py
@@ -0,0 +1,67 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for deployment_step_runner"""
+import argparse
+import unittest
+
+from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.tests.utils.with_secrets import with_secrets
+from recidiviz.tools.deploy.cloud_build.artifact_registry_repository import ImageKind
+from recidiviz.tools.deploy.cloud_build.build_configuration import DeploymentContext
+from recidiviz.tools.deploy.cloud_build.deployment_stage_runner import (
+    AVAILABLE_DEPLOYMENT_STAGES,
+)
+from recidiviz.tools.deploy.cloud_build.stages.build_images import BuildImages
+from recidiviz.tools.deploy.cloud_build.stages.run_migrations_from_cloud_build import (
+    RunMigrations,
+)
+from recidiviz.tools.deploy.cloud_build.stages.tag_images import TagImages
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+
+class DeploymentStepRunnerTest(unittest.TestCase):
+    """Test case for ArtifactRegistryRepository"""
+
+    @with_secrets(
+        {
+            "workflows_cloudsql_instance_id": "123,",
+            "operations_v2_cloudsql_instance_id": "123,",
+            "case_triage_cloudsql_instance_id": "123,",
+            "insights_cloudsql_instance_id": "123,",
+            "pathways_cloudsql_instance_id": "123,",
+            "justice_counts_cloudsql_instance_id": "123,",
+        }
+    )
+    def test_parse(self) -> None:
+        stage_args = {
+            BuildImages: argparse.Namespace(images=[ImageKind.APP_ENGINE]),
+            TagImages: argparse.Namespace(images=[ImageKind.APP_ENGINE]),
+            RunMigrations: argparse.Namespace(schema_types=[SchemaType.OPERATIONS]),
+        }
+
+        with local_project_id_override(GCP_PROJECT_STAGING):
+            for deployment_stage in AVAILABLE_DEPLOYMENT_STAGES:
+                build = deployment_stage().configure_build(
+                    DeploymentContext(
+                        project_id="test-project",
+                        commit_ref="1a2b3c4d",
+                        version_tag="v1.0",
+                    ),
+                    stage_args.get(deployment_stage, argparse.Namespace()),
+                )
+                self.assertGreater(len(build.steps), 0)
