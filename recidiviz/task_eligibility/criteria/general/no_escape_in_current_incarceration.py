e3ec3f12b9a6ec546a8a28476d01ab7139704174
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2024-08-21 18:09:10 +0000

    [US_ND] Additional criteria for ATP (Recidiviz/recidiviz-data#32547)
    ## Description of the change
    
    Created new criteria based on TT feedback:
    - INCARCERATION_NOT_WITHIN_3_MONTHS_OF_FTCD: which removes folks who are
    about to be released within the next 90 months.
    - no_escape_in_current_incarceration: which removes folks who had an
    escape in the current period of incarceration
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#31549
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: c938d5041f3559b9e218732dea4d281acc4d7cfa

---
+++ recidiviz/task_eligibility/criteria/general/no_escape_in_current_incarceration.py
@@ -0,0 +1,90 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Shows the spans of time during which someone is ineligible for a task because they have
+an escape sentence in their current incarceration.
+"""
+from google.cloud import bigquery
+from recidiviz.task_eligibility.reasons_field import ReasonsField
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateAgnosticTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.calculator.query.state.dataset_config import SESSIONS_DATASET
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "NO_ESCAPE_IN_CURRENT_INCARCERATION"
+
+_DESCRIPTION = """
+Shows the spans of time during which someone is ineligible for a task because they have
+an escape sentence in their current incarceration."""
+
+_QUERY_TEMPLATE = """
+WITH escape_related_sentences AS (
+  SELECT 
+    sis.state_code,
+    sis.person_id,
+    sis.date_imposed,
+    sis.description,
+  FROM `{project_id}.{sessions_dataset}.sentences_preprocessed_materialized` sis
+  WHERE REGEXP_CONTAINS(UPPER(sis.description), r'ESCAPE')
+      AND date_imposed IS NOT NULL
+)
+
+SELECT 
+  cs.state_code,
+  cs.person_id,
+  cs.start_date,
+  cs.end_date,
+  False AS meets_criteria,
+  TO_JSON(STRUCT(
+      ARRAY_AGG(e.description) AS ineligible_offenses_descriptions,
+      MAX(e.date_imposed) AS most_recent_escape_date
+  )) AS reason,
+  ARRAY_AGG(e.description) AS ineligible_offenses_descriptions,
+  MAX(e.date_imposed) AS most_recent_escape_date
+FROM `{project_id}.{sessions_dataset}.incarceration_super_sessions_materialized` cs
+INNER JOIN escape_related_sentences e
+  ON cs.person_id = e.person_id
+    AND cs.state_code = e.state_code
+    AND e.date_imposed BETWEEN cs.start_date AND IFNULL(cs.end_date, '9999-12-31')
+GROUP BY 1,2,3,4,5
+ORDER BY start_date
+"""
+
+VIEW_BUILDER: StateAgnosticTaskCriteriaBigQueryViewBuilder = StateAgnosticTaskCriteriaBigQueryViewBuilder(
+    criteria_name=_CRITERIA_NAME,
+    criteria_spans_query_template=_QUERY_TEMPLATE,
+    description=_DESCRIPTION,
+    sessions_dataset=SESSIONS_DATASET,
+    reasons_fields=[
+        ReasonsField(
+            name="ineligible_offenses_descriptions",
+            type=bigquery.enums.StandardSqlTypeNames.ARRAY,
+            description="Ineligible offenses that make the person ineligible for the task",
+        ),
+        ReasonsField(
+            name="most_recent_escape_date",
+            type=bigquery.enums.StandardSqlTypeNames.DATE,
+            description="The most recent escape date",
+        ),
+    ],
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
