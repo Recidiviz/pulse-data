b892009e4e855bda1cf8fd1ae0e44dd4f41f010f
Roshan A <roshan.agrawal95@gmail.com>
2024-08-21 14:58:32 -0400

    [Sentencing] Send cloud storage notifications to prod Sentencing Server (Recidiviz/recidiviz-data#32569)
    ## Description of the change
    Enable prod cloud storage notifications to be sent to the prod
    sentencing server so it can trigger data import.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: dcaaec927420ce724066d318582208b798648c59

--- recidiviz/tools/deploy/terraform/cloud-storage-notifications.tf
+++ recidiviz/tools/deploy/terraform/cloud-storage-notifications.tf
@@ -103,14 +103,15 @@ module "handle_sentencing_etl" {
   minimum_backoff            = "180s"
   maximum_backoff            = "600s"
   message_retention_duration = "86400s"
-
-  count = local.is_production ? 0 : 1
 }
 
 locals {
   app_engine_url = "https://${var.project_id}.appspot.com"
   # These client IDs come from the app engine service we want to authenticate to, and can be found
   # at https://console.cloud.google.com/apis/credentials (IAP-App-Engine-app)
-  app_engine_iap_client         = local.is_production ? "688733534196-uol4tvqcb345md66joje9gfgm26ufqj6.apps.googleusercontent.com" : "984160736970-flbivauv2l7sccjsppe34p7436l6890m.apps.googleusercontent.com"
-  sentencing_cloud_run_endpoint = local.is_production ? "" : "https://sentencing-server-rithdumbtq-uc.a.run.app/trigger_import"
+  app_engine_iap_client = local.is_production ? "688733534196-uol4tvqcb345md66joje9gfgm26ufqj6.apps.googleusercontent.com" : "984160736970-flbivauv2l7sccjsppe34p7436l6890m.apps.googleusercontent.com"
+
+  # These endpoints come from the cloud run services we want to trigger
+  # They can be found at https://console.cloud.google.com/run/detail/us-central1/sentencing-server/metrics?project=recidiviz-dashboard-production and https://console.cloud.google.com/run/detail/us-central1/sentencing-server/metrics?project=recidiviz-dashboard-staging for production and staging respectively
+  sentencing_cloud_run_endpoint = local.is_production ? "https://sentencing-server-6ydpcm4cka-uc.a.run.app/trigger_import" : "https://sentencing-server-rithdumbtq-uc.a.run.app/trigger_import"
 }
