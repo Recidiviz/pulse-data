ef7a7042fc3cdbd79d83c1e5c17acabdc50ccd7b
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-08-23 11:20:41 -0600

    [US_AZ] Tweak external comparison validations (Recidiviz/recidiviz-data#32641)
    ## Description of the change
    
    This PR does three things, all related to external comparison
    validations in AZ
    1. Fixes the raw data migration that corrects the spelling of Tucson in
    the external validation data to _actually_ correct the spelling.
    2. Capitalizes district names in the internal data for AZ to more
    accurately compare to the validation data.
    3. Bumps the threshold for
    `supervision_population_person_level_external_comparison_matching_people_supervision_level`
    to 10% until we can investigate.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Related to Recidiviz/recidiviz-data#31835
    Related to Recidiviz/recidiviz-data#31836
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 723fda5dfea18e99b239ba1eaac8ac9e01e24a9a

--- recidiviz/validation/views/state/supervision_population_person_level_template.py
+++ recidiviz/validation/views/state/supervision_population_person_level_template.py
@@ -94,6 +94,7 @@ sanitized_internal_metrics AS (
       CASE
         WHEN state_code IN ('US_MO','US_TN')
           THEN level_1_supervision_location_external_id
+        WHEN state_code = 'US_AZ' THEN UPPER(level_2_supervision_location_external_id)
         ELSE level_2_supervision_location_external_id
       END AS supervising_district_external_id,
   FROM `{{project_id}}.{{materialized_metrics_dataset}}.most_recent_supervision_population_span_metrics_materialized` dataflow
