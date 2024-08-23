2df3e99b9622e2e02f7af2ab532482239ea8d11f
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2024-08-23 16:06:42 -0700

    share logic between supervision_staff_record and current_staff (Recidiviz/recidiviz-data#32652)
    ## Description of the change
    
    The workflows staff record was recently updated to read from
    `supervision_staff_attribute_sessions` and apply some district
    formatting logic to match the admin panel; this moves that logic into
    the `current_staff` view that `ingested_product_users` also pulls from
    and reads from that table in the workflows `supervision_staff_record` so
    we don't need to duplicate it.
    
    We need this logic in both views so that districts line up between the
    admin panel roster and the workflows staff record, because users without
    a staff record need to be able to search for staff in the exact district
    specified in the admin panel. Eventually we'll be able to remove a lot
    of the formatting logic, but for now it's easier to review diffs if we
    keep things the same as they have been.
    
    (also I totally want to look into using this view for insights in the
    future! maybe after the district transformation logic has been removed)
    
    Loaded into sandbox at `dana_current_staff`. No diffs to
    `ingested_product_users`, and the only diffs to
    `supervision_staff_record` are to remove some users who had null
    attributes across the board.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    https://github.com/Recidiviz/recidiviz-data/issues/32585
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 92609a7e1fa3de5f1f808619bda01503f5453134

--- recidiviz/calculator/query/state/views/reference/current_staff.py
+++ recidiviz/calculator/query/state/views/reference/current_staff.py
@@ -32,21 +32,57 @@ CURRENT_STAFF_DESCRIPTION = (
 )
 
 CURRENT_STAFF_QUERY_TEMPLATE = f"""
+    WITH attribute_sessions_mapped_ids AS (
+        -- Some staff members have multiple valid external IDs for a single external ID type,
+        -- so use `agent_multiple_ids_map` to pick a single consistent ID for use in products.
+        -- The workflows client record also uses this transformation, so this assures we can join to
+        -- it correctly in the workflows staff record.
+        SELECT
+            attrs.* EXCEPT (officer_id),
+            COALESCE(map.external_id_mapped, attrs.officer_id) AS officer_id
+        FROM `{{project_id}}.sessions.supervision_staff_attribute_sessions_materialized` attrs
+        LEFT JOIN `{{project_id}}.static_reference_tables.agent_multiple_ids_map` map
+            ON attrs.state_code = map.state_code
+            AND attrs.officer_id = map.external_id_to_map
+    )
     SELECT
         ss.state_code,
-        LOWER(email) as email,
+        email,
         CASE
+            -- Transform IX districts from the Atlas format to the pre-Atlas format so they
+            -- match the districts in the current staff record / admin panel, which makes it
+            -- easier to compare the change adding this to what currently exists and reduces
+            -- the number of changes that need to be coordinated at the moment.
+            -- TODO(#29474) Remove this transformation
+            WHEN attrs.state_code="US_IX" THEN 
+                CASE UPPER(attrs.supervision_district_name)
+                    WHEN "DISTRICT 1" THEN "DISTRICT OFFICE 1, COEUR D'ALENE"
+                    WHEN "DISTRICT 2" THEN "DISTRICT OFFICE 2, LEWISTON"
+                    WHEN "DISTRICT 3" THEN "DISTRICT OFFICE 3, CALDWELL"
+                    WHEN "DISTRICT 4" THEN "DISTRICT OFFICE 4, BOISE"
+                    WHEN "DISTRICT 5" THEN "DISTRICT OFFICE 5, TWIN FALLS"
+                    WHEN "DISTRICT 6" THEN "DISTRICT OFFICE 6, POCATELLO"
+                    WHEN "DISTRICT 7" THEN "DISTRICT OFFICE 7, IDAHO FALLS"
+                END
             -- Transform MI district IDs from "10 - CENTRAL" --> "10" and "4A" --> "4a" so they
             -- match the districts in the current staff record / admin panel, which makes it easier
             -- to compare the change adding this to what currently exists.
             -- TODO(#25398) Remove this transformation
             WHEN attrs.state_code="US_MI" THEN SPLIT(LOWER(attrs.supervision_district_id), " ")[SAFE_OFFSET(0)]
+            WHEN attrs.state_code IN ("US_CA", "US_ND") THEN
+                COALESCE(attrs.supervision_office_name, attrs.supervision_office_name_inferred)
+            -- Set ME districts to null so they match the districts in the admin panel
+            -- Set OR districts to null because "staff" in this case are actually caseloads,
+            -- and districts are not relevant.
+            WHEN attrs.state_code IN ("US_ME", "US_OR") THEN NULL
             ELSE attrs.supervision_district_id
-            END AS supervision_district_id,
-        attrs.supervision_district_name,
-        attrs.officer_id AS external_id,
+        END AS district,
+        UPPER(attrs.officer_id) AS external_id,
         JSON_VALUE(ss.full_name, "$.given_names") AS given_names,
         JSON_VALUE(ss.full_name, "$.surname") AS surname,
+        attrs.role_subtype_primary,
+        attrs.supervisor_staff_external_id_primary AS supervisor_external_id,
+        attrs.supervisor_staff_external_id_array AS supervisor_external_ids,
         officer_sessions.supervising_officer_external_id IS NOT NULL AS is_supervision_officer,
         supervisors_of_officers.supervisor_staff_external_id IS NOT NULL AS is_supervision_officer_supervisor,
         {get_pseudonymized_id_query_str("IF(ss.state_code = 'US_IX', 'US_ID', ss.state_code) || attrs.officer_id")} AS pseudonymized_id,
