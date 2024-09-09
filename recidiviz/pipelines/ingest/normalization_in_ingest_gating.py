d08cc40284aec0f392865ab14dc9ed70d1807a83
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2024-09-09 12:08:32 -0400

    [US_CO] Run normalization in the ingest pipeline (Recidiviz/recidiviz-data#33079)
    ## Description of the change
    
    Updates feature gates so the US_CO ingest pipeline will run
    normalization and output to `us_co_normalized_state_new`. Downstream
    processes will still read from `us_co_normalized_state` / `us_co_state`.
    
    By the time this PR merges, there will be a `--pre_normalization_only`
    flag you can run if you want to run sandbox pipelines without running
    normalization (e.g. because it's faster).
    
    Successful (non-crashing) pipeline run
    [here](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-09-06_10_43_25-12175957957660750421;graphView=0?project=recidiviz-staging&authuser=0&pageState=(%22dfTime%22:(%22l%22:%22dfJobMaxTime%22)))
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#29517
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: dd532a115560b41be16d064e4f1c26eeb280a9b8

--- recidiviz/pipelines/ingest/normalization_in_ingest_gating.py
+++ recidiviz/pipelines/ingest/normalization_in_ingest_gating.py
@@ -38,6 +38,7 @@ def should_run_normalization_in_ingest(state_code: StateCode) -> bool:
         *prod_launched_states,
         StateCode.US_AZ,
         StateCode.US_CA,
+        StateCode.US_CO,
         StateCode.US_NC,
         StateCode.US_MA,
         # TODO(#29517): Add states here as we launch combined pipelines to staging
