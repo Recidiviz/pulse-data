2d5126adf6611389b282e6acf4cbd9b77c462105
Lydia Masri <102679298+lydiamasri2@users.noreply.github.com>
2024-09-16 15:33:24 -0400

    [US_IA] State person  (Recidiviz/recidiviz-data#33297)
    ## Description of the change
    
    Creating state person view for Iowa.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#29008
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 91a666b2fdc9753312f10bc84f8f3034592bf7c8

---
+++ recidiviz/ingest/direct/regions/us_ia/ingest_views/view_state_person.py
@@ -0,0 +1,46 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query containing person information."""
+
+from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
+    DirectIngestViewQueryBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+SELECT 
+    OffenderCd,
+    OffenderFirstNm,
+    OffenderMiddleNm,
+    OffenderLastNm,
+    NULLIF(Sex,"NULL") AS Sex,
+    NULLIF(Race, "NULL") AS Race,
+    NULLIF(EthnicOrigin, "NULL") AS EthnicOrigin,
+    DATE(NULLIF(BirthDt, "NULL")) AS BirthDt
+FROM {IA_DOC_Offender_Details}
+"""
+
+VIEW_BUILDER = DirectIngestViewQueryBuilder(
+    region="us_ia",
+    ingest_view_name="state_person",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
