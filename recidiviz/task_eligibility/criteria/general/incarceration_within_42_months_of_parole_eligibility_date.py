f10a22f0ef69e0ef6f0fd74881f6a6f31f52616e
Daniel Allen <daniel@recidiviz.org>
2024-09-16 12:56:42 -0400

    [US_AR] Work release task eligibility spans (Recidiviz/recidiviz-data#32702)
    ## Description of the change
    
    Adds task eligibility spans and criteria (mostly stubs) for the AR work
    release opportunity.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#32006
    Closes Recidiviz/recidiviz-data#32009
    Closes Recidiviz/recidiviz-data#31996
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: f9c726ff2842c1695b754d34802f7e75609b19f4

---
+++ recidiviz/task_eligibility/criteria/general/incarceration_within_42_months_of_parole_eligibility_date.py
@@ -0,0 +1,51 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Defines a criteria span view that shows spans of time during which
+someone is incarcerated within 6 months of their parole eligibility date.
+"""
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateAgnosticTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.general_criteria_builders import (
+    is_past_completion_date_criteria_builder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "INCARCERATION_WITHIN_42_MONTHS_OF_PAROLE_ELIGIBILITY_DATE"
+
+_DESCRIPTION = """
+Defines a criteria span view that shows spans of time during which
+someone is incarcerated within 42 months of their parole eligibility date.
+"""
+
+VIEW_BUILDER: StateAgnosticTaskCriteriaBigQueryViewBuilder = (
+    is_past_completion_date_criteria_builder(
+        compartment_level_1_filter="INCARCERATION",
+        meets_criteria_leading_window_time=42,
+        date_part="MONTH",
+        critical_date_column="parole_eligibility_date",
+        critical_date_name_in_reason="parole_eligibility_date",
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
