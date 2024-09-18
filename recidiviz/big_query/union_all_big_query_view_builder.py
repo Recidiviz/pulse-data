549fd12b9665e405a745cc95a15f022aab9f3802
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2024-09-18 09:08:30 -0700

    [BigQuery] Adapt UnionAllBigQueryViewBuilder to work for source tables (Recidiviz/recidiviz-data#33428)
    ## Description of the change
    
    Updates the `UnionAllBigQueryViewBuilder` to allow for UNION ALL views
    to query a collection of source tables instead of a collection of views.
    This will allow us to build `UnionAllBigQueryViewBuilder` views that
    generate the `state` and `normalized_state` datasets by unioning
    together `us_xx_state` and `us_xx_normalized_state_new` datasets.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#29515
    Part of Recidiviz/recidiviz-data#29519
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 1123838f43025ca1bb0c43ac1bbf6a9dec41346e

--- recidiviz/big_query/union_all_big_query_view_builder.py
+++ recidiviz/big_query/union_all_big_query_view_builder.py
@@ -14,8 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""Builder for a view that unions the results from a list of parent views together."""
-
+"""Builder for a view that unions the results from a list of parent views or source
+tables together.
+"""
 import logging
 from typing import Callable, Dict, Optional, Sequence, Set
 
@@ -29,14 +30,10 @@ from recidiviz.big_query.big_query_view import (
 from recidiviz.utils import metadata
 
 
-def default_builder_to_select_statement(
-    view_builder: BigQueryViewBuilder,  # pylint: disable=unused-argument
-) -> str:
-    return "SELECT *"
-
-
 class UnionAllBigQueryViewBuilder(BigQueryViewBuilder[BigQueryView]):
-    """Builder for a view that unions the results from a list of parent views together."""
+    """Builder for a view that unions the results from a list of parent views or source
+    tables together.
+    """
 
     def __init__(
         self,
@@ -44,34 +41,50 @@ class UnionAllBigQueryViewBuilder(BigQueryViewBuilder[BigQueryView]):
         dataset_id: str,
         view_id: str,
         description: str,
-        parent_view_builders: Sequence[BigQueryViewBuilderType],
-        builder_to_select_statement: Callable[
-            [BigQueryViewBuilderType], str
-        ] = default_builder_to_select_statement,
+        parents: Sequence[BigQueryViewBuilderType] | Sequence[BigQueryAddress],
+        custom_select_statement: Optional[str] = None,
+        parent_to_select_statement: Optional[
+            Callable[[BigQueryViewBuilderType], str]
+        ] = None,
+        materialized_address_override: Optional[BigQueryAddress] = None,
     ):
         """
         Args:
             dataset_id: The view address dataset_id
             view_id: The view address table_id
             description: Description for this view
-            parent_view_builders: The list of view builders of views to select from.
-            builder_to_select_statement: A function that, for a given parent view
-                builder, returns a string SELECT statement that should be used to query
-                the provided view.
+            parents: The list of view builders or tables to select from.
+            custom_select_statement: A custom SELECT statement to be used for each
+                individual view / address query. May only be set if
+                |parent_to_select_statement| is null.
+            parent_to_select_statement: A function that, for a given parent view
+                builder or table address, returns a string SELECT statement that should
+                be used to query the provided view. May only be set if
+                |custom_select_statement| is not null.
+            materialized_address_override: If set, this view will be materialized to
+                this address rather than the default materialization address.
         """
-        if not parent_view_builders:
+        if not parents:
             raise ValueError(
-                f"Found no view builders to union for view `{dataset_id}.{view_id}`"
+                f"Found no view builders / tables to union for view "
+                f"`{dataset_id}.{view_id}`"
+            )
+        if custom_select_statement and parent_to_select_statement:
+            raise ValueError(
+                f"May only set one of |custom_select_statement| and "
+                f"|parent_to_select_statement|. Found both set for view "
+                f"{dataset_id}.{view_id}"
             )
         self.dataset_id = dataset_id
         self.view_id = view_id
         self.description = description
-        self.parent_view_builders = parent_view_builders
-        self.builder_to_select_statement = builder_to_select_statement
+        self.parents = parents
+        self.custom_select_statement = custom_select_statement
+        self.parent_to_select_statement = parent_to_select_statement
         self.materialized_address = self._build_materialized_address(
             dataset_id=dataset_id,
             view_id=view_id,
-            materialized_address_override=None,
+            materialized_address_override=materialized_address_override,
             should_materialize=True,
         )
         self.projects_to_deploy = None
@@ -81,6 +94,22 @@ class UnionAllBigQueryViewBuilder(BigQueryViewBuilder[BigQueryView]):
         self, parent_address_filter: Set[BigQueryAddress]
     ) -> None:
         self.parent_address_filter = parent_address_filter
+
+    @staticmethod
+    def _get_address(
+        parent: BigQueryViewBuilderType | BigQueryAddress,
+    ) -> BigQueryAddress:
+        if isinstance(parent, BigQueryAddress):
+            return parent
+        return parent.address
+
+    @staticmethod
+    def _get_table_for_query(
+        parent: BigQueryViewBuilderType | BigQueryAddress,
+    ) -> BigQueryAddress:
+        if isinstance(parent, BigQueryAddress):
+            return parent
+        return parent.table_for_query
 
     def _build(
         self, *, address_overrides: Optional[BigQueryAddressOverrides] = None
@@ -100,44 +129,58 @@ class UnionAllBigQueryViewBuilder(BigQueryViewBuilder[BigQueryView]):
         select_queries = []
         query_format_args: Dict[str, str] = {}
 
-        parent_view_builders_in_project = [
-            vb
-            for vb in self.parent_view_builders
-            if vb.should_deploy_in_project(metadata.project_id())
+        parents_in_project = [
+            parent
+            for parent in self.parents
+            if isinstance(parent, BigQueryAddress)
+            or parent.should_deploy_in_project(metadata.project_id())
         ]
 
-        if not parent_view_builders_in_project:
+        if not parents_in_project:
             raise ValueError(
                 f"Found no valid parent views for UNION ALL composite view "
                 f"[{self.address.to_str()}]"
             )
 
-        filtered_parent_view_builders = [
-            vb
-            for vb in parent_view_builders_in_project
+        filtered_parents = [
+            parent
+            for parent in parents_in_project
             if self.parent_address_filter is None
-            or vb.address in self.parent_address_filter
+            or self._get_address(parent) in self.parent_address_filter
         ]
 
-        if not filtered_parent_view_builders:
+        if not filtered_parents:
             logging.warning(
-                "No parent views for UNION ALL composite view [%s.%s] in the "
+                "No parent views for UNION ALL composite view [%s] in the "
                 "list of view filters. Selecting ALL parents.",
-                self.address.dataset_id,
-                self.address.table_id,
+                self.address.to_str(),
             )
-            filtered_parent_view_builders = list(self.parent_view_builders)
+            filtered_parents = list(self.parents)
 
         # Sort view builders by address so we produce a query with deterministic order
-        for vb in sorted(filtered_parent_view_builders, key=lambda vb: vb.address):
-            select_statement = self.builder_to_select_statement(vb)
-            if not vb.materialized_address:
-                raise ValueError(f"Expected view [{vb.address}] to be materialized.")
-            select_queries.append(
-                vb.materialized_address.select_query_template(
-                    select_statement=select_statement
+        for parent in sorted(filtered_parents, key=self._get_address):
+            table_for_query = self._get_table_for_query(parent)
+
+            if self.custom_select_statement:
+                custom_select_statement = self.custom_select_statement
+            elif self.parent_to_select_statement:
+                if isinstance(parent, BigQueryAddress):
+                    raise ValueError(
+                        "Can only use a custom select statement for a "
+                        "BigQueryViewBuilder type parent"
+                    )
+                custom_select_statement = self.parent_to_select_statement(parent)
+            else:
+                custom_select_statement = None
+
+            if custom_select_statement:
+                query = table_for_query.select_query_template(
+                    select_statement=custom_select_statement
                 )
-            )
+            else:
+                query = table_for_query.select_query_template()
+
+            select_queries.append(query)
 
         view_query_template = "\nUNION ALL\n".join(select_queries)
 
