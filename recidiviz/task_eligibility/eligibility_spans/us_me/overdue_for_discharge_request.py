833b9adfa7764c44aa448d48c75e3b3fb8842f84
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2024-09-18 16:55:19 -0500

    [US_ME] Create overdue_from_discharge_request form (Recidiviz/recidiviz-data#33362)
    ## Description of the change
    
    Create overdue_from_discharge_request form. Uploaded under `_hsalas` and
    16 people are eligible.
    
    ```
    python -m recidiviz.tools.load_views_to_sandbox \        --project_id recidiviz-staging\
            --sandbox_dataset_prefix hsalas manual \
            --dataset_ids_to_load task_eligibility_spans_us_me\
            --update_descendants False
    ```
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: cd0621a7ef0a44f8d036c07c3fce016d6dc49947

---
+++ recidiviz/task_eligibility/eligibility_spans/us_me/overdue_for_discharge_request.py
@@ -0,0 +1,53 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Shows the spans of time during which someone in ME is eligible
+for a dishcarge from supervision
+"""
+
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.candidate_populations.general import (
+    probation_active_supervision_and_supervision_out_of_state_population,
+)
+from recidiviz.task_eligibility.completion_events.general import full_term_discharge
+from recidiviz.task_eligibility.criteria.general import (
+    supervision_past_full_term_completion_date,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_DESCRIPTION = """Shows the spans of time during which someone in ME is eligible
+for a dishcarge from supervision
+"""
+# TODO(#33472) - Fix the discrepancy between state_supervision_sentence and task_deadline
+VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
+    state_code=StateCode.US_ME,
+    task_name="OVERDUE_FOR_DISCHARGE_REQUEST",
+    description=_DESCRIPTION,
+    candidate_population_view_builder=probation_active_supervision_and_supervision_out_of_state_population.VIEW_BUILDER,
+    criteria_spans_view_builders=[
+        supervision_past_full_term_completion_date.VIEW_BUILDER,
+    ],
+    completion_event_builder=full_term_discharge.VIEW_BUILDER,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
