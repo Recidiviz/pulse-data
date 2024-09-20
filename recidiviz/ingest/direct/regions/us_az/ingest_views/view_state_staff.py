74ea5d4bd858673626fd162b366309ab8804fba4
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-09-20 12:25:30 -0400

    [US_AZ] Refactor staff IDs (Recidiviz/recidiviz-data#33452)
    ## Description of the change
    
    For context, staff in AZ can have multiple `PERSON_ID` values over time.
    There is no way to differentiate between them - DOB is not reliably
    hydrated, and email addresses are not unique. The best way we can
    aggregate all of the `PERSON_ID`s that an individual staff member has
    ever had is using full name. This means we will necessarily be
    conflating people who have the same full name.
    
    This PR implements the following approach to AZ staff IDs:
    - Create a manually-updated, Recidiviz-managed reference file
    (`RECIDIVIZ_REFERENCE_staff_id_override`) that is designed to
    disaggregate staff members who have the same full name. We will populate
    this only based on feedback we get from the state.
    - In the ingest view, aggregate IDS by full name and any OVERRIDE_ID
    that exists in the reference file. If there are two individual staff
    members with the same full name, and we have been made aware of that,
    they will each have at least one line in
    `RECIDIVIZ_REFERENCE_staff_id_override` connecting them to their
    `PERSON_ID` with a distinct `OVERRIDE_ID`.
    - There may be many rows per person in
    `RECIDIVIZ_REFERENCE_staff_id_override` if a staff member has multiple
    `PERSON_ID` values, but there will only be one `OVERRIDE_ID` per person.
    This will allow us to aggregate based on full name and `OVERRIDE_ID` in
    the ingest view.
    
    Tests:
    - [x] confirmed via unit tests that the
    `RECIDIVIZ_REFERENCE_staff_id_override` disaggregation works as expected
    - [x] run_mappings script passes when I add a fake version of this
    reference data to `us_az_raw_data_up_to_date_views`
    - [x] sandbox ingest
    [pipeline](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-09-18_12_01_47-11331391814843386490;step=;graphView=0?project=recidiviz-123&pageState=(%22dfTime%22:(%22l%22:%22dfJobMaxTime%22)))
    succeeds when I add an empty version of this reference data to
    `us_az_raw_data` & `us_az_raw_data_up_to_date_views`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 8ad7153afd3b86d5759ac28f204a80447382bf44

--- recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_staff.py
+++ recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_staff.py
@@ -14,7 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""Query containing information about facilities and community reentry staff."""
+"""Query containing information about facilities and community reentry staff.
+
+This view will necessarily conflate staff members with the same name until they are
+manually corrected in the RECIDIVIZ_REFERENCE_staff_id_override reference data."""
 
 from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
     DirectIngestViewQueryBuilder,
@@ -23,14 +26,32 @@ from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_QUERY_TEMPLATE = """
+WITH
+-- Get all IDs associated with a staff member with a particular full name.
+staff_ids_by_name AS (
 SELECT DISTINCT 
-    STRING_AGG(DISTINCT PERSON_ID, ',' ORDER BY PERSON_ID) AS id_list,
-    NULLIF(FIRST_NAME, 'NULL') AS FIRST_NAME,
-    NULLIF(MIDDLE_NAME, 'NMI') AS MIDDLE_NAME, 
-    NULLIF(SURNAME, 'NULL') AS SURNAME,
+    PERSON_ID,
+    NULLIF(UPPER(FIRST_NAME), 'NULL') AS FIRST_NAME,
+    NULLIF(UPPER(SURNAME), 'NULL') AS SURNAME,
 FROM {PERSON}
 WHERE PERSON_TYPE_ID = '8473' -- DOC Staff
-GROUP BY FIRST_NAME, MIDDLE_NAME, SURNAME
+),
+-- Pull in a manual reference sheet with overrides that separate IDs for people with
+-- the same name. This sheet is populated based on feedback from users in AZ. 
+manual_id_overrides AS (
+    SELECT 
+        FIRST_NAME,
+        SURNAME,
+        PERSON_ID,
+        OVERRIDE_ID
+    FROM {RECIDIVIZ_REFERENCE_staff_id_override}
+)
+
+SELECT FIRST_NAME, SURNAME, STRING_AGG(PERSON_ID, ',') AS PERSON_IDS
+FROM staff_ids_by_name
+LEFT JOIN manual_id_overrides
+USING(FIRST_NAME, SURNAME, PERSON_ID)
+GROUP BY FIRST_NAME, SURNAME, OVERRIDE_ID
 """
 
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
