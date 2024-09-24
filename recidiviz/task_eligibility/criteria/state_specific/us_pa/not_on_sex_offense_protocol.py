a177b2f369491fa3d0e1ba170f30b14a91c227ca
Sophie Bair <56454281+seb2244@users.noreply.github.com>
2024-09-24 15:42:52 -0500

    [US_PA][Workflows] Create criteria for not serving on sex offense protocol  (Recidiviz/recidiviz-data#33326)
    ## Description of the change
    
    By [PA
    policy](https://docs.google.com/document/d/1uXbZswjmwsbhqRyVt2w6ExN21ma3xirO/edit)
    and confirmed with TT, "reentrants supervised under the sex offender
    protocol shall not be supervised below the medium level of supervision."
    This PR creates an additional criteria for the two PA limited
    supervision workflows to exclude clients who have a sexual offense
    protocol as one of their supervision conditions.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#33127
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: bd815de0ce85a6b1aae40f978e50c10d0981dd53

---
+++ recidiviz/task_eligibility/criteria/state_specific/us_pa/not_on_sex_offense_protocol.py
@@ -0,0 +1,90 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines a criteria span view that shows spans of time during which someone is
+not serving on a sex offense protocol"""
+
+from google.cloud import bigquery
+
+from recidiviz.calculator.query.sessions_query_fragments import (
+    aggregate_adjacent_spans,
+    create_sub_sessions_with_attributes,
+)
+from recidiviz.calculator.query.state.dataset_config import NORMALIZED_STATE_DATASET
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.reasons_field import ReasonsField
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateSpecificTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "US_PA_NOT_ON_SEX_OFFENSE_PROTOCOL"
+
+_DESCRIPTION = """Defines a criteria span view that shows spans of time during which someone is
+not serving on a sex offense protocol"""
+
+_QUERY_TEMPLATE = f"""
+WITH sex_offense_spans AS (
+    SELECT 
+        state_code,
+        person_id,
+        start_date,
+        termination_date AS end_date,
+        False AS meets_criteria,
+    FROM `{{project_id}}.{{normalized_state_dataset}}.state_supervision_period`
+    WHERE state_code = 'US_PA'
+        AND conditions like '%SEXP%'
+        AND start_date <> termination_date -- exclude zero-day sessions
+),
+{create_sub_sessions_with_attributes('sex_offense_spans')}
+, deduped_sex_offense_spans AS (
+    -- addresses overlapping spans 
+    SELECT distinct
+        state_code,
+        person_id,
+        start_date,
+        end_date,
+        meets_criteria,
+     FROM sub_sessions_with_attributes
+)
+SELECT *,
+    start_date AS sex_offense_protocol_start,
+    TO_JSON(STRUCT(start_date AS sex_offense_protocol_start)) AS reason,
+FROM({aggregate_adjacent_spans(table_name='deduped_sex_offense_spans', attribute = 'meets_criteria')})
+"""
+
+VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
+    StateSpecificTaskCriteriaBigQueryViewBuilder(
+        criteria_name=_CRITERIA_NAME,
+        description=_DESCRIPTION,
+        criteria_spans_query_template=_QUERY_TEMPLATE,
+        state_code=StateCode.US_PA,
+        normalized_state_dataset=NORMALIZED_STATE_DATASET,
+        meets_criteria_default=True,
+        reasons_fields=[
+            ReasonsField(
+                name="sex_offense_protocol_start",
+                type=bigquery.enums.StandardSqlTypeNames.DATE,
+                description="Date that someone began serving on sex offense protocol",
+            ),
+        ],
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
