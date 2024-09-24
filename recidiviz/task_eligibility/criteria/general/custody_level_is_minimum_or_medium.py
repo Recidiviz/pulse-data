210d6b2808877c3fd9ef6ae77f4bbc59914a8753
Shalin Brahmbhatt <63821972+shalindb@users.noreply.github.com>
2024-09-24 14:28:53 -0700

    [US_AZ] Created criteria stubs & task queries for AZ TPR & DTP and modified custody level criteria to include medium custody residents (Recidiviz/recidiviz-data#33608)
    ## Description of the change
    
    * Added criteria stubs for all the new criteria in policy for TPR and
    DTP
    * Also added a general criteria to include medium custody level instead
    of just minimum custody level in the eligible list
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#33378
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 5a44dfa26ac4a9393bde4884afaea8f0ce1405bf

---
+++ recidiviz/task_eligibility/criteria/general/custody_level_is_minimum_or_medium.py
@@ -0,0 +1,65 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2023 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""This criteria view builder defines spans of time where residents are
+MINIMUM or MEDIUM custody level as tracked by our `sessions` dataset.
+"""
+from google.cloud import bigquery
+
+from recidiviz.task_eligibility.reasons_field import ReasonsField
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateAgnosticTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.general_criteria_builders import (
+    custody_or_supervision_level_criteria_builder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "CUSTODY_LEVEL_IS_MINIMUM_OR_MEDIUM"
+
+_DESCRIPTION = """This criteria view builder defines spans of time where residents are
+MINIMUM or MEDIUMcustody level as tracked by our `sessions` dataset.
+"""
+
+_REASONS_COLUMNS = (
+    "custody_level AS custody_level, start_date AS custody_level_start_date"
+)
+
+VIEW_BUILDER: StateAgnosticTaskCriteriaBigQueryViewBuilder = custody_or_supervision_level_criteria_builder(
+    criteria_name=_CRITERIA_NAME,
+    description=_DESCRIPTION,
+    levels_lst=["MINIMUM", "MEDIUM"],
+    reasons_columns=_REASONS_COLUMNS,
+    reasons_fields=[
+        ReasonsField(
+            name="custody_level",
+            type=bigquery.enums.StandardSqlTypeNames.STRING,
+            description="Client's custody level",
+        ),
+        ReasonsField(
+            name="custody_level_start_date",
+            type=bigquery.enums.StandardSqlTypeNames.DATE,
+            description="Specifies when a client began on the specified custody level",
+        ),
+    ],
+    level_meets_criteria=True,
+    compartment_level_1_filter="INCARCERATION",
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
