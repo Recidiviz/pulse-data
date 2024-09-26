3bee0a8652c2f40b2e571706490fd1d0ad1e25f3
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2024-09-26 10:54:15 -0500

    [US_AZ] Criteria: is US citizen or legal permanent resident without an ICE detainer (Recidiviz/recidiviz-data#33689)
    ## Description of the change
    
    Criteria: is US citizen or legal permanent resident without an ICE
    detainer.
    
    Uploaded it all to sandbox and it's looking good (under `hsalas`).
    Eligibility changed from 4259 to 2692 in `RELEASE_TO_TPR_REQUEST`.
    ```
    python -m recidiviz.tools.load_views_to_sandbox \
        --sandbox_dataset_prefix hsalas auto \
        --load_up_to_datasets task_eligibility
    ```
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#33381
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: cabeebfd96d41738fded525734827c031fb0357d

--- recidiviz/task_eligibility/criteria/state_specific/us_az/is_us_citizen_or_legal_permanent_resident.py
+++ recidiviz/task_eligibility/criteria/state_specific/us_az/is_us_citizen_or_legal_permanent_resident.py
@@ -14,23 +14,74 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ============================================================================
-"""Describes spans of time when someone is a US citizen or a legal permanent resident"""
+"""Describes spans of time when someone is a US citizen or legal permanent resident
+without an ICE detainer"""
 from google.cloud import bigquery
 
 from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.dataset_config import raw_latest_views_dataset_for_region
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.ingest.views.dataset_config import NORMALIZED_STATE_DATASET
 from recidiviz.task_eligibility.reasons_field import ReasonsField
 from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
     StateSpecificTaskCriteriaBigQueryViewBuilder,
-)
-from recidiviz.task_eligibility.utils.placeholder_criteria_builders import (
-    state_specific_placeholder_criteria_view_builder,
 )
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
 _CRITERIA_NAME = "US_AZ_IS_US_CITIZEN_OR_LEGAL_PERMANENT_RESIDENT"
 
-_DESCRIPTION = """Describes spans of time when someone is a US citizen or a legal permanent resident"""
+_DESCRIPTION = """Describes spans of time when someone is a US citizen or legal permanent resident
+without an ICE detainer"""
+
+_QUERY_TEMPLATE = """
+WITH citizenship_status_with_priority AS (
+  SELECT 
+    *,
+  # We assume individuals with an update_date of 2019-11-30 (data migration date) have been on their current 
+  # immigration status since 1900. This assumption is not ideal, but its the best we can do.
+    IF(update_date = '2019-11-30', '1900-01-01', update_date) AS start_date,
+  FROM (
+    SELECT 
+        peid.state_code,
+        peid.person_id,
+        COUNTRY.DESCRIPTION AS citizenship_status, 
+        citizenship_status_priority,
+        SAFE_CAST(LEFT(dem.UPDT_DTM, 10) AS DATE) AS update_date,
+    -- DEMOGRAPHICS contains the codes for many demographic fields, including citizenship status,
+    -- which includes the categories in the UNNEST below. We join on the LOOKUPS table to get the
+    -- human-readable descriptions of the codes.
+    FROM `{project_id}.{raw_data_up_to_date_views_dataset}.DEMOGRAPHICS_latest` dem
+    INNER JOIN `{project_id}.{raw_data_up_to_date_views_dataset}.LOOKUPS_latest` COUNTRY
+        ON(COUNTRY_OF_CITIZENSHIP = LOOKUP_ID)
+    INNER JOIN `{project_id}.{normalized_state_dataset}.state_person_external_id` peid
+        ON peid.external_id = dem.PERSON_ID
+        AND peid.id_type = 'US_AZ_PERSON_ID'
+    LEFT JOIN (
+        SELECT *
+        FROM UNNEST(
+        -- The order of the citizenship statuses is important. We want to prioritize U.S. citizens
+        -- and legal permanent residents without an ICE detainer (LEGAL/NO DP).
+            ["U.S. CITIZEN", "LEGAL/NO DP", "LEGAL P/DP", "CRIMNL ALIEN", "UNKNOWN"]
+        ) AS citizenship_status
+        WITH OFFSET citizenship_status_priority 
+    ) AS priority
+    ON priority.citizenship_status = COUNTRY.DESCRIPTION
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY peid.state_code, peid.person_id, GREATEST(update_date) ORDER BY citizenship_status_priority) = 1
+  )
+)
+
+SELECT 
+    state_code,
+    person_id,
+    start_date,
+    LEAD(start_date) OVER(PARTITION BY state_code, person_id ORDER BY start_date) AS end_date,
+    -- We only care about US citizens and legal permanent residents without an ICE detainer
+    citizenship_status IN ("U.S. CITIZEN", "LEGAL/NO DP") AS meets_criteria,
+    TO_JSON(STRUCT(citizenship_status AS citizenship_status)) AS reason,
+    citizenship_status,
+FROM citizenship_status_with_priority
+"""
 
 _REASONS_FIELDS = [
     ReasonsField(
@@ -41,11 +92,17 @@ _REASONS_FIELDS = [
 ]
 
 VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
-    state_specific_placeholder_criteria_view_builder(
+    StateSpecificTaskCriteriaBigQueryViewBuilder(
         criteria_name=_CRITERIA_NAME,
         description=_DESCRIPTION,
-        reasons_fields=_REASONS_FIELDS,
         state_code=StateCode.US_AZ,
+        criteria_spans_query_template=_QUERY_TEMPLATE,
+        raw_data_up_to_date_views_dataset=raw_latest_views_dataset_for_region(
+            state_code=StateCode.US_AZ, instance=DirectIngestInstance.PRIMARY
+        ),
+        normalized_state_dataset=NORMALIZED_STATE_DATASET,
+        meets_criteria_default=False,
+        reasons_fields=_REASONS_FIELDS,
     )
 )
 
