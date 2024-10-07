a1466779f8b339d8d7ded21fd1d77ed83b56d7b2
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-10-07 18:45:26 -0400

    [US_AZ] Ingest `state_sentence_length` (Recidiviz/recidiviz-data#33309)
    ## Description of the change
    
    Ingests the `state_sentence_length` entity by creating a new view that
    is nearly identical to what is currently ingested into
    `state_sentence_group_length`. The new mapping improves upon the
    original one in a few ways:
    - Based on feedback from AZ, we will never want to calculate projected
    release dates off of the CSED. This PR ensures that the
    `projected_completion_date_max_external` is always SED to avoid mistakes
    there.
    - Since TR to ADD is the probation version of CSBD, the mapping now
    saves that as the `parole_eligibility_date_external` if there is no
    CSBD.
    - Since ADD is the probation version of ERCD, the mapping now saves that
    as the `parole_eligibility_date_external` if there is no CSBD.
    
    Each of these dates is assigned at the individual sentence level. Once
    this PR is merged, references to `state_sentence_group_length` entity
    fields will need to be replaced by their analogous fields on
    `state_sentence_length` for date projections in
    https://github.com/Recidiviz/recidiviz-data/pull/33955.
    
    This PR also filters invalid dates (e.g. 0100-xx-xx, 0200-xx-xx) out of
    every date field per Shalin's request
    [here](https://github.com/Recidiviz/recidiviz-data/pull/33955#discussion_r1790309066).
    
    Tests:
    - [x] run_mappings script passes
    - [x] ingest view query is deterministic
    - [x] sandbox ingest + norm
    [pipeline](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-10-07_12_16_24-872230490699787254;graphView=1?project=recidiviz-staging&pageState=(%22dfTime%22:(%22l%22:%22dfJobMaxTime%22)))
    succeeds
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#32611
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 5117c84fd1338b4f8ec85d2c787a62c1868d5731

---
+++ recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_sentence_length.py
@@ -0,0 +1,144 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Query for state sentence lengths.
+
+For each of the date fields in the entity, we coalesce three fields from 
+the source raw data. This is to accomodate the way automatic and manual overrides are
+documented in ACIS. The system prioritizes Manual Lock fields first, because these
+contain dates that were manually entered by an officer to override the automatically calculated
+date. Then, Adjust Release Date fields are prioritized. These are automatic adjustments
+made by ACIS to release dates that fall on weekends or holidays. The final field is the
+original release date calculated by ACIS based on sentence information. 
+"""
+from recidiviz.ingest.direct.regions.us_az.ingest_views.common_sentencing_views_and_utils import (
+    VALID_PEOPLE_AND_SENTENCES,
+)
+from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
+    DirectIngestViewQueryBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = f"""
+WITH 
+-- Maps epiodes to people with valid sentences
+valid_people AS ({VALID_PEOPLE_AND_SENTENCES}),
+-- The result of this CTE is one row for each combination of sentence and set of
+-- release dates for all people with valid sentences.
+-- Whenever one of the release dates changes, a row will be added
+-- with the updated information about the sentence and the date on which that information
+-- changed.
+base AS (
+SELECT DISTINCT
+    off.OFFENSE_ID,
+    valid_people.PERSON_ID,
+    CAST(NULLIF(off.UPDT_DTM,'NULL') AS DATETIME) AS UPDT_DTM,
+    -- See view description for explanation of datetime field hierarchy.
+    COALESCE(
+        NULLIF(COMMUNITY_SUPV_BEGIN_DTM_ML, 'NULL'),
+        NULLIF(COMMUNITY_SUPV_BEGIN_DTM_ARD, 'NULL'), 
+        NULLIF(COMMUNITY_SUPV_BEGIN_DTM, 'NULL')) AS CommunitySupervisionBeginDate, -- CSBD
+    COALESCE(
+        NULLIF(EARNED_RLS_CREDIT_DTM_ML, 'NULL'), 
+        NULLIF(EARNED_RLS_CREDIT_DTM_ARD, 'NULL'), 
+        NULLIF(EARNED_RLS_CREDIT_DTM, 'NULL')) AS EarnedReleaseCreditDate, -- ERCD
+    COALESCE(
+        NULLIF(EARNED_SENTENCE_EXP_DTM_ML, 'NULL'), 
+        NULLIF(EARNED_SENTENCE_EXP_DTM_ARD, 'NULL'),
+        NULLIF(EARNED_SENTENCE_EXP_DTM, 'NULL')) AS SentenceExpirationDate, -- SED
+    COALESCE(
+        NULLIF(TR_TO_ABSOLUTE_DSCHRG_DTM_ML, 'NULL'), 
+        NULLIF(TR_TO_ABSOLUTE_DSCHRG_DTM_ARD, 'NULL'),
+        NULLIF(TR_TO_ABSOLUTE_DSCHRG_DTM, 'NULL')) AS TransitionToAbsoluteDischargeDate, -- TR to ADD
+    COALESCE(
+        NULLIF(ABSOLUTE_DISCHARGE_DTM_ML, 'NULL'), 
+        NULLIF(ABSOLUTE_DISCHARGE_DTM_ARD, 'NULL'),
+        NULLIF(ABSOLUTE_DISCHARGE_DTM, 'NULL')) AS AbsoluteDischargeDate, -- Absolute Discharge Date (ADD)
+FROM 
+    {{AZ_DOC_SC_OFFENSE@ALL}} AS off
+JOIN valid_people
+    USING(OFFENSE_ID)
+),
+-- Filter malformed date values out of the query before any more processing is done.
+filter_to_valid_dates AS (
+SELECT 
+    * 
+FROM base 
+WHERE COALESCE(CommunitySupervisionBeginDate, '1900-01-01') BETWEEN '1900-01-01' AND '2100-01-01'
+AND COALESCE(EarnedReleaseCreditDate, '1900-01-01') BETWEEN '1900-01-01' AND '2100-01-01'
+AND COALESCE(SentenceExpirationDate, '1900-01-01') BETWEEN '1900-01-01' AND '2100-01-01'
+AND COALESCE(TransitionToAbsoluteDischargeDate, '1900-01-01') BETWEEN '1900-01-01' AND '2100-01-01'
+AND COALESCE(AbsoluteDischargeDate, '1900-01-01') BETWEEN '1900-01-01' AND '2100-01-01'
+),
+-- Collect the previous value of each release date for each sentence, so that we can
+-- filter these results to only include rows where a release date changed.
+previous_dates AS (
+  SELECT 
+    *,
+    LAG(CommunitySupervisionBeginDate) OVER sentence_window AS prev_CSBD,
+    LAG(EarnedReleaseCreditDate) OVER sentence_window AS prev_ERCD,
+    LAG(SentenceExpirationDate) OVER sentence_window AS prev_SED,
+    LAG(TransitionToAbsoluteDischargeDate) OVER sentence_window AS prev_TRtoADD,
+    LAG(AbsoluteDischargeDate) OVER sentence_window AS prev_ADD
+  FROM filter_to_valid_dates
+  -- There is one offense that has two updates at the exact same time. SED is included
+  -- here to sort them deterministically.
+  WINDOW sentence_window AS (PARTITION BY OFFENSE_ID ORDER BY UPDT_DTM, SentenceExpirationDate)
+)
+
+SELECT 
+  OFFENSE_ID,
+  PERSON_ID,
+  UPDT_DTM,
+  CommunitySupervisionBeginDate,
+  EarnedReleaseCreditDate,
+  SentenceExpirationDate,
+  TransitionToAbsoluteDischargeDate,
+  AbsoluteDischargeDate
+FROM previous_dates
+WHERE UPDT_DTM IS NOT NULL
+AND 
+-- any of the dates changed
+(CommunitySupervisionBeginDate != prev_CSBD
+OR EarnedReleaseCreditDate != prev_ERCD
+OR SentenceExpirationDate != prev_SED
+OR TransitionToAbsoluteDischargeDate != prev_TRtoADD
+OR AbsoluteDischargeDate != prev_ADD)
+OR
+-- any date appeared for the first time 
+((CommunitySupervisionBeginDate IS NOT NULL AND prev_CSBD IS NULL)
+OR (EarnedReleaseCreditDate IS NOT NULL AND prev_ERCD IS NULL)
+OR (SentenceExpirationDate IS NOT NULL AND prev_SED IS NULL)
+OR (TransitionToAbsoluteDischargeDate IS NOT NULL AND prev_TRtoADD IS NULL)
+OR (AbsoluteDischargeDate IS NOT NULL AND prev_ADD IS NULL))
+OR 
+-- any date was cleared
+((CommunitySupervisionBeginDate IS NULL AND prev_CSBD IS NOT NULL)
+OR (EarnedReleaseCreditDate IS NULL AND prev_ERCD IS NOT NULL)
+OR (SentenceExpirationDate IS NULL AND prev_SED IS NOT NULL)
+OR (TransitionToAbsoluteDischargeDate IS NULL AND prev_TRtoADD IS NOT NULL)
+OR (AbsoluteDischargeDate IS NULL AND prev_ADD IS NOT NULL))
+"""
+VIEW_BUILDER = DirectIngestViewQueryBuilder(
+    region="us_az",
+    ingest_view_name="state_sentence_length",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
