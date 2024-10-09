dcfe5d37561f71e9fbb90f874cff49da03256569
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2024-10-09 11:30:32 -0700

    [BigQuery] Add BigQueryAddressFormatter and BigQueryAddressFormatterProvider abstractions (Recidiviz/recidiviz-data#34062)
    ## Description of the change
    
    Introduces new abstractions for formatting `BigQueryAddress` references
    in queries (the `BigQueryAddressFormatter`) and for deciding which
    formatter to use for a given address (the
    `BigQueryAddressFormatterProvider`).
    
    I broke this change out into its own PR for the sake of sanity but you
    can see how it's used in Recidiviz/recidiviz-data#34063.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#26138
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 8455a81539478ce1514a5860e99d5c88898e2d55

---
+++ recidiviz/big_query/big_query_address_formatter.py
@@ -0,0 +1,158 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines interfaces for classes that can be used to format parent address references
+in queries. These can be used to apply parent address-specific filters or other sorts of
+transformations.
+"""
+
+import abc
+
+import attr
+
+from recidiviz.big_query.big_query_address import (
+    BigQueryAddress,
+    ProjectSpecificBigQueryAddress,
+)
+from recidiviz.common import attr_validators
+from recidiviz.common.constants.states import StateCode
+
+
+class BigQueryAddressFormatter:
+    @abc.abstractmethod
+    def format_address(self, address: ProjectSpecificBigQueryAddress) -> str:
+        """Given an address to a table or view, returns a formatted string
+        that can be used to reference that address in the context of a query.
+
+        For example, in the simplest case, this might return
+        "`my-project.my_dataset.my_table`".
+        """
+
+
+class BigQueryAddressFormatterSimple(BigQueryAddressFormatter):
+    """The most basic implementation of BigQueryAddressFormatter that just returns an
+    address in standard query format: `my-project.my_dataset.my_table`.
+    """
+
+    def format_address(self, address: ProjectSpecificBigQueryAddress) -> str:
+        return address.format_address_for_query()
+
+
+@attr.define
+class BigQueryAddressFormatterLimitZero(BigQueryAddressFormatter):
+    """A BigQueryAddressFormatter that returns a table sub-query that will select zero
+    rows from the given address.
+    """
+
+    def format_address(self, address: ProjectSpecificBigQueryAddress) -> str:
+        return f"(SELECT * FROM {address.format_address_for_query()} LIMIT 0)"
+
+
+@attr.define
+class BigQueryAddressFormatterFilterByStateCode(BigQueryAddressFormatter):
+    """A BigQueryAddressFormatter that returns a table sub-query that will select only
+    rows with a given state_code value. Should only be used on tables that have a
+    state_code column.
+    """
+
+    state_code_filter: StateCode = attr.ib(
+        validator=attr.validators.instance_of(StateCode)
+    )
+
+    def format_address(self, address: ProjectSpecificBigQueryAddress) -> str:
+        return (
+            f"(SELECT * "
+            f"FROM {address.format_address_for_query()} "
+            f'WHERE state_code = "{self.state_code_filter.value}")'
+        )
+
+
+class BigQueryAddressFormatterProvider:
+    """An interface for getting the appropriate formatter for a given address that will
+    be referenced in the context of a query.
+    """
+
+    @abc.abstractmethod
+    def get_formatter(self, address: BigQueryAddress) -> BigQueryAddressFormatter:
+        """Returns the appropriate formatter for the given address. This address should
+        be an original address in the standard deployed view graph and not a sandbox
+        address.
+        """
+
+
+class SimpleBigQueryAddressFormatterProvider(BigQueryAddressFormatterProvider):
+    """The simplest implementation of BigQueryAddressFormatterProvider that will only
+    provide a formatter that returns simple table references.
+    """
+
+    def get_formatter(self, address: BigQueryAddress) -> BigQueryAddressFormatter:
+        return BigQueryAddressFormatterSimple()
+
+
+@attr.define
+class StateFilteringBigQueryAddressFormatterProvider(BigQueryAddressFormatterProvider):
+    """An implementation of BigQueryAddressFormatterProvider that will return a
+    formatter that will filter the address down to only rows for the given state code,
+    to the best of our ability.
+    """
+
+    # Addresses will be formatted to be filtered to only rows for this state at query
+    #  time.
+    state_code_filter: StateCode = attr.ib(
+        validator=attr.validators.instance_of(StateCode)
+    )
+
+    # The list of BigQueryAddress that we may encounter which do NOT have a state_code
+    # column and cannot be filtered.
+    missing_state_code_addresses: set[BigQueryAddress] = attr.ib(
+        validator=attr_validators.is_set_of(BigQueryAddress)
+    )
+
+    # A map of table address to the pseudocolumns that are available on that table.
+    # These are used to ensure that we do not apply a filter on this table because
+    # SELECT * would exclude the psuedocolumns which might be referenced elsewhere in
+    # the query.
+    pseudocolumns_by_address: dict[BigQueryAddress, list[str]] = attr.ib(
+        validator=attr_validators.is_dict
+    )
+
+    def get_formatter(self, address: BigQueryAddress) -> BigQueryAddressFormatter:
+        pseudocolumns = self.pseudocolumns_by_address.get(address, [])
+        if pseudocolumns:
+            # We cannot do any filtering on this table because a SELECT * query on this
+            # table would exclude pseudocolumns like _FILE_NAME which may be referenced
+            # elsewhere in the query.
+            return BigQueryAddressFormatterSimple()
+
+        address_state_code = address.state_code_for_address()
+        if address_state_code:
+            if address_state_code != self.state_code_filter:
+                # This is a state-specific table for a state other than the one we're
+                # filtering to - do not select any rows
+                return BigQueryAddressFormatterLimitZero()
+
+            # This table is specific to the state we care about, do no filtering
+            return BigQueryAddressFormatterSimple()
+
+        # This is a state-agnostic table but cannot be filtered by state_code - do no
+        # filtering.
+        if address in self.missing_state_code_addresses:
+            return BigQueryAddressFormatterSimple()
+
+        # If this is a state-agnostic table - filter down to rows with the state code
+        return BigQueryAddressFormatterFilterByStateCode(
+            state_code_filter=self.state_code_filter
+        )
---
+++ recidiviz/tests/big_query/big_query_address_formatter_test.py
@@ -0,0 +1,151 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for big_query_address_formatter.py"""
+import unittest
+
+from recidiviz.big_query.big_query_address import BigQueryAddress
+from recidiviz.big_query.big_query_address_formatter import (
+    BigQueryAddressFormatterFilterByStateCode,
+    BigQueryAddressFormatterLimitZero,
+    BigQueryAddressFormatterSimple,
+    StateFilteringBigQueryAddressFormatterProvider,
+)
+from recidiviz.common.constants.states import StateCode
+
+
+class TestBigQueryAddressFormatter(unittest.TestCase):
+    """Tests for BigQueryAddressFormatter subclasses"""
+
+    def test_simple_formatter(self) -> None:
+        address = BigQueryAddress.from_str(
+            "my_dataset.my_table"
+        ).to_project_specific_address(project_id="recidiviz-456")
+        formatter = BigQueryAddressFormatterSimple()
+        formatted_address = formatter.format_address(address)
+
+        self.assertEqual(
+            "`recidiviz-456.my_dataset.my_table`",
+            formatted_address,
+        )
+
+    def test_limit_zero_formatter(self) -> None:
+        address = BigQueryAddress.from_str(
+            "my_dataset.my_table"
+        ).to_project_specific_address(project_id="recidiviz-456")
+        formatter = BigQueryAddressFormatterLimitZero()
+        formatted_address = formatter.format_address(address)
+
+        self.assertEqual(
+            "(SELECT * FROM `recidiviz-456.my_dataset.my_table` LIMIT 0)",
+            formatted_address,
+        )
+
+    def test_filter_by_state_code_formatter(self) -> None:
+        address = BigQueryAddress.from_str(
+            "my_dataset.my_table"
+        ).to_project_specific_address(project_id="recidiviz-456")
+        formatter = BigQueryAddressFormatterFilterByStateCode(
+            state_code_filter=StateCode.US_XX
+        )
+        formatted_address = formatter.format_address(address)
+
+        self.assertEqual(
+            '(SELECT * FROM `recidiviz-456.my_dataset.my_table` WHERE state_code = "US_XX")',
+            formatted_address,
+        )
+
+
+class TestBigQueryAddressFormatterProvider(unittest.TestCase):
+    """Tests for BigQueryAddressFormatterProvider subclasses"""
+
+    def test_state_filtering_provider(self) -> None:
+        formatter_provider = StateFilteringBigQueryAddressFormatterProvider(
+            state_code_filter=StateCode.US_XX,
+            missing_state_code_addresses={
+                BigQueryAddress.from_str("us_xx_dataset.missing_state_code"),
+                BigQueryAddress.from_str("us_yy_dataset.missing_state_code"),
+                BigQueryAddress.from_str("state_agnostic.missing_state_code"),
+                BigQueryAddress.from_str("external_data_table.missing_state_code"),
+            },
+            pseudocolumns_by_address={
+                BigQueryAddress.from_str("external_data_table.missing_state_code"): [
+                    "_FILE_NAME"
+                ],
+                BigQueryAddress.from_str("external_data_table.has_state_code"): [
+                    "_FILE_NAME"
+                ],
+            },
+        )
+
+        # Do not modify US_XX-specific tables
+        self.assertIsInstance(
+            formatter_provider.get_formatter(
+                BigQueryAddress.from_str("us_xx_dataset.my_table"),
+            ),
+            BigQueryAddressFormatterSimple,
+        )
+        self.assertIsInstance(
+            formatter_provider.get_formatter(
+                BigQueryAddress.from_str("us_xx_dataset.missing_state_code"),
+            ),
+            BigQueryAddressFormatterSimple,
+        )
+
+        # Always LIMIT 0 non-US_XX state-specific tables
+        self.assertIsInstance(
+            formatter_provider.get_formatter(
+                BigQueryAddress.from_str("us_yy_dataset.my_table"),
+            ),
+            BigQueryAddressFormatterLimitZero,
+        )
+        self.assertIsInstance(
+            formatter_provider.get_formatter(
+                BigQueryAddress.from_str("us_yy_dataset.missing_state_code"),
+            ),
+            BigQueryAddressFormatterLimitZero,
+        )
+
+        # Do not modify state agnostic tables with no state_code column
+        self.assertIsInstance(
+            formatter_provider.get_formatter(
+                BigQueryAddress.from_str("state_agnostic.missing_state_code"),
+            ),
+            BigQueryAddressFormatterSimple,
+        )
+
+        # Do not modify state agnostic tables with pseudocolumns, whether or not they
+        # have a state_code column
+        self.assertIsInstance(
+            formatter_provider.get_formatter(
+                BigQueryAddress.from_str("external_data_table.missing_state_code"),
+            ),
+            BigQueryAddressFormatterSimple,
+        )
+        self.assertIsInstance(
+            formatter_provider.get_formatter(
+                BigQueryAddress.from_str("external_data_table.has_state_code"),
+            ),
+            BigQueryAddressFormatterSimple,
+        )
+
+        # Filter other state-agnostic tables by state_code
+        self.assertIsInstance(
+            formatter_provider.get_formatter(
+                BigQueryAddress.from_str("my_dataset.my_table"),
+            ),
+            BigQueryAddressFormatterFilterByStateCode,
+        )
