e870109ab89d133cb81b13d08905aabc04a3a492
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-10-10 15:04:01 -0400

    [US_AZ]  Hydrate `state_sentence_group_length` with ruling sentence dates (Recidiviz/recidiviz-data#34092)
    ## Description of the change
    
    https://github.com/Recidiviz/recidiviz-data/pull/33659 restructured the
    `state_sentence_group_length` ingest view so that it only pulled dates
    that we were certain were at the group level. This PR fills in a few
    more fields on the `state_sentence_group_length` entity using what we
    now understand about sentence groups in AZ.
    
    - The sentence group level is dictated by "episodes" in AZ
    - Episodes can be comprised of many sentences
    - Every episode has exactly one "ruling offense" that dictates the
    amount of time a person actually spends incarcerated
    
    This PR hydrates date fields on `state_sentence_group_length` with the
    same dates that are used to hydrate `state_sentence_length`, but it only
    uses dates associated with the ruling offense for each episode.
    
    Tests:
    - [x] Ingest view query is deterministic
    - [x] Successful sandbox pipeline
    [here](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-10-10_06_16_26-12605968276981795314;graphView=0?project=recidiviz-staging&pageState=(%22dfTime%22:(%22l%22:%22dfJobMaxTime%22))).
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#34082
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 3fdaadf256a00433a1057cb5166ce2290d63c0fa

--- recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_sentence_group.py
+++ recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_sentence_group.py
@@ -17,11 +17,13 @@
 """
 Ingest view for state sentence group lengths.
 
-For each of the date fields in the entity, we coalesce two fields from 
+For each of the date fields in the entity, we coalesce three fields from 
 the source raw data. This is to accomodate the way automatic and manual overrides are
 documented in ACIS. The system prioritizes Manual Lock fields first, because these
 contain dates that were manually entered by an officer to override the automatically calculated
-date. The other field is the original release date calculated by ACIS based on sentence information. 
+date. Then, Adjust Release Date fields are prioritized. These are automatic adjustments
+made by ACIS to release dates that fall on weekends or holidays. The final field is the
+original release date calculated by ACIS based on sentence information. 
 
 We define a "sentence group" as a single episode. Many offenses can be associated with a 
 single commitment, and many commitments can be affiliated with a single episode. 
@@ -48,43 +50,116 @@ WITH
 
 -- Maps epiodes to people with valid sentences
 valid_people AS ({VALID_PEOPLE_AND_SENTENCES}),
-
+-- Cleans AZ_DOC_SC_OFFENSE raw data to null out fields that contain the string 'NULL'
+-- and fields that contain dates outside of a reasonable date range.
+cleaned_AZ_DOC_SC_OFFENSE_ALL AS (
+    SELECT 
+        OFFENSE_ID, 
+        UPDT_DTM,
+        IF(COMMUNITY_SUPV_BEGIN_DTM_ML BETWEEN '1900-01-01' AND '2100-01-01', COMMUNITY_SUPV_BEGIN_DTM_ML, NULL) AS COMMUNITY_SUPV_BEGIN_DTM_ML,
+        IF(COMMUNITY_SUPV_BEGIN_DTM_ARD BETWEEN '1900-01-01' AND '2100-01-01', COMMUNITY_SUPV_BEGIN_DTM_ARD, NULL) AS COMMUNITY_SUPV_BEGIN_DTM_ARD,
+        IF(COMMUNITY_SUPV_BEGIN_DTM BETWEEN '1900-01-01' AND '2100-01-01', COMMUNITY_SUPV_BEGIN_DTM, NULL) AS COMMUNITY_SUPV_BEGIN_DTM,
+        IF(EARNED_RLS_CREDIT_DTM_ML BETWEEN '1900-01-01' AND '2100-01-01', EARNED_RLS_CREDIT_DTM_ML, NULL) AS EARNED_RLS_CREDIT_DTM_ML,
+        IF(EARNED_RLS_CREDIT_DTM_ARD BETWEEN '1900-01-01' AND '2100-01-01', EARNED_RLS_CREDIT_DTM_ARD, NULL) AS EARNED_RLS_CREDIT_DTM_ARD,
+        IF(EARNED_RLS_CREDIT_DTM BETWEEN '1900-01-01' AND '2100-01-01', EARNED_RLS_CREDIT_DTM, NULL) AS EARNED_RLS_CREDIT_DTM,
+        IF(TR_TO_ABSOLUTE_DSCHRG_DTM_ML BETWEEN '1900-01-01' AND '2100-01-01', TR_TO_ABSOLUTE_DSCHRG_DTM_ML, NULL) AS TR_TO_ABSOLUTE_DSCHRG_DTM_ML,
+        IF(TR_TO_ABSOLUTE_DSCHRG_DTM_ARD BETWEEN '1900-01-01' AND '2100-01-01', TR_TO_ABSOLUTE_DSCHRG_DTM_ARD, NULL) AS TR_TO_ABSOLUTE_DSCHRG_DTM_ARD,
+        IF(TR_TO_ABSOLUTE_DSCHRG_DTM BETWEEN '1900-01-01' AND '2100-01-01', TR_TO_ABSOLUTE_DSCHRG_DTM, NULL) AS TR_TO_ABSOLUTE_DSCHRG_DTM,
+        IF(ABSOLUTE_DISCHARGE_DTM_ML BETWEEN '1900-01-01' AND '2100-01-01', ABSOLUTE_DISCHARGE_DTM_ML, NULL) AS ABSOLUTE_DISCHARGE_DTM_ML,
+        IF(ABSOLUTE_DISCHARGE_DTM_ARD BETWEEN '1900-01-01' AND '2100-01-01', ABSOLUTE_DISCHARGE_DTM_ARD, NULL) AS ABSOLUTE_DISCHARGE_DTM_ARD,
+        IF(ABSOLUTE_DISCHARGE_DTM BETWEEN '1900-01-01' AND '2100-01-01', ABSOLUTE_DISCHARGE_DTM, NULL) AS ABSOLUTE_DISCHARGE_DTM
+    FROM (
+        SELECT
+            OFFENSE_ID, 
+            NULLIF(UPDT_DTM, 'NULL') AS UPDT_DTM,
+            NULLIF(COMMUNITY_SUPV_BEGIN_DTM_ML, 'NULL') AS COMMUNITY_SUPV_BEGIN_DTM_ML,
+            NULLIF(COMMUNITY_SUPV_BEGIN_DTM_ARD, 'NULL') AS COMMUNITY_SUPV_BEGIN_DTM_ARD, 
+            NULLIF(COMMUNITY_SUPV_BEGIN_DTM, 'NULL') AS COMMUNITY_SUPV_BEGIN_DTM,
+            NULLIF(EARNED_RLS_CREDIT_DTM_ML, 'NULL') AS EARNED_RLS_CREDIT_DTM_ML,
+            NULLIF(EARNED_RLS_CREDIT_DTM_ARD, 'NULL') AS EARNED_RLS_CREDIT_DTM_ARD, 
+            NULLIF(EARNED_RLS_CREDIT_DTM, 'NULL') AS EARNED_RLS_CREDIT_DTM,
+            NULLIF(TR_TO_ABSOLUTE_DSCHRG_DTM_ML, 'NULL') AS TR_TO_ABSOLUTE_DSCHRG_DTM_ML, 
+            NULLIF(TR_TO_ABSOLUTE_DSCHRG_DTM_ARD, 'NULL') AS TR_TO_ABSOLUTE_DSCHRG_DTM_ARD,
+            NULLIF(TR_TO_ABSOLUTE_DSCHRG_DTM, 'NULL') AS TR_TO_ABSOLUTE_DSCHRG_DTM,
+            NULLIF(ABSOLUTE_DISCHARGE_DTM_ML, 'NULL') AS ABSOLUTE_DISCHARGE_DTM_ML, 
+            NULLIF(ABSOLUTE_DISCHARGE_DTM_ARD, 'NULL') AS ABSOLUTE_DISCHARGE_DTM_ARD,
+            NULLIF(ABSOLUTE_DISCHARGE_DTM, 'NULL') AS ABSOLUTE_DISCHARGE_DTM
+        FROM {{AZ_DOC_SC_OFFENSE@ALL}}
+    )
+    ),
+-- Cleans AZ_DOC_SC_EPISODE raw data to null out fields that contain the string 'NULL'
+-- and fields that contain dates outside of a reasonable date range.
+cleaned_AZ_DOC_SC_EPISODE_ALL AS (
+    SELECT 
+        SC_EPISODE_ID,
+        FINAL_OFFENSE_ID, 
+        UPDT_DTM,
+        IF(EXPIRATION_DTM_ML BETWEEN '1900-01-01' AND '2100-01-01', EXPIRATION_DTM_ML, NULL) AS EXPIRATION_DTM_ML,
+        IF(EXPIRATION_DTM BETWEEN '1900-01-01' AND '2100-01-01', EXPIRATION_DTM, NULL) AS EXPIRATION_DTM,
+    FROM (
+        SELECT
+            SC_EPISODE_ID,
+            FINAL_OFFENSE_ID, 
+            NULLIF(UPDT_DTM, 'NULL') AS UPDT_DTM,
+            NULLIF(EXPIRATION_DTM_ML, 'NULL') AS EXPIRATION_DTM_ML,
+            NULLIF(EXPIRATION_DTM, 'NULL') AS EXPIRATION_DTM, 
+        FROM {{AZ_DOC_SC_EPISODE@ALL}}
+        )
+),
 -- There are confilicting values for some episodes
 -- with the same update datetime. This CTE parses them
 -- and we take the minimum value in the output
 parsed_dates AS (
     SELECT DISTINCT
         PERSON_ID,
-        SC_EPISODE_ID, -- StateSentenceGroup.external_id
-        UPDT_DTM,      -- StateSentenceGroupLength.group_update_datetime
-        -- Earliest sentence expiration date (SED) from all active offenses 
-        -- in all active commitments in this episode
-        -- This is for projected_full_term_release_date_min_external
-        -- '_ML' columns are 'manual lock', meaning they were manually
-        -- updated by staff for a particular reason.
-        COALESCE(
-            NULLIF(EXPIRATION_DTM_ML, 'NULL'), NULLIF(EXPIRATION_DTM, 'NULL')
-        ) AS EXPIRATION_DTM
+        SC_EPISODE_ID,  -- StateSentenceGroup.external_id
+        COALESCE(ruling_offense.UPDT_DTM, episode.UPDT_DTM) AS UPDT_DTM,    -- StateSentenceGroupLength.group_update_datetime
+    COALESCE(
+        COMMUNITY_SUPV_BEGIN_DTM_ML,
+        COMMUNITY_SUPV_BEGIN_DTM_ARD,
+        COMMUNITY_SUPV_BEGIN_DTM) AS CommunitySupervisionBeginDate, -- CSBD
+    COALESCE(
+        EARNED_RLS_CREDIT_DTM_ML, 
+        EARNED_RLS_CREDIT_DTM_ARD, 
+        EARNED_RLS_CREDIT_DTM) AS EarnedReleaseCreditDate, -- ERCD
+    COALESCE(
+        TR_TO_ABSOLUTE_DSCHRG_DTM_ML, 
+        TR_TO_ABSOLUTE_DSCHRG_DTM_ARD,
+        TR_TO_ABSOLUTE_DSCHRG_DTM) AS TransitionToAbsoluteDischargeDate, -- TR to ADD
+    COALESCE(
+        ABSOLUTE_DISCHARGE_DTM_ML, 
+        ABSOLUTE_DISCHARGE_DTM_ARD,
+        ABSOLUTE_DISCHARGE_DTM) AS AbsoluteDischargeDate, -- Absolute Discharge Date (ADD)
+    COALESCE(
+        EXPIRATION_DTM_ML, 
+        EXPIRATION_DTM) AS SentenceExpirationDate -- SED
     FROM 
-        {{AZ_DOC_SC_EPISODE@ALL}} AS episode 
+        cleaned_AZ_DOC_SC_EPISODE_ALL AS episode 
     JOIN 
         valid_people
     USING
         (SC_EPISODE_ID)
-    WHERE 
-        COALESCE(
-            NULLIF(EXPIRATION_DTM_ML, 'NULL'), NULLIF(EXPIRATION_DTM, 'NULL')
-        ) IS NOT NULL
+    -- The dates associated with the Final Ruling Offense are the ones that dictate
+    -- the time a person actually spends in prison for a given group of sentences
+    -- imposed together.
+    JOIN 
+        cleaned_AZ_DOC_SC_OFFENSE_ALL AS ruling_offense
+    ON
+        (episode.FINAL_OFFENSE_ID = ruling_offense.OFFENSE_ID)
 )
 SELECT
     PERSON_ID,
     SC_EPISODE_ID,
     UPDT_DTM,
-    MIN(EXPIRATION_DTM) AS EXPIRATION_DTM
+    MIN(SentenceExpirationDate) AS SentenceExpirationDate,
+    MIN(CommunitySupervisionBeginDate) AS CommunitySupervisionBeginDate,
+    MIN(EarnedReleaseCreditDate) AS EarnedReleaseCreditDate,
+    MIN(TransitionToAbsoluteDischargeDate) AS TransitionToAbsoluteDischargeDate,
+    MIN(AbsoluteDischargeDate) AS AbsoluteDischargeDate
 FROM
     parsed_dates
-WHERE
-    EXPIRATION_DTM IS NOT NULL
+WHERE 
+    UPDT_DTM IS NOT NULL
 GROUP BY 
     PERSON_ID, SC_EPISODE_ID, UPDT_DTM
 """
