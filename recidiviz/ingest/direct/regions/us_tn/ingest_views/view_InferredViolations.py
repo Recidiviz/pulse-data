178dd978e01b593a30771c05d927a636a55fd3ac
Lydia Masri <102679298+lydiamasri2@users.noreply.github.com>
2024-10-18 12:34:25 -0400

    [US_TN] SORV and CSLR violations (Recidiviz/recidiviz-data#34156)
    ## Description of the change
    
    Adding SORV and CSLR violations to InferredViolations view.
    
    Ingest pipeline ran successfully
    
    InferredViolations results go from 380087 to 386763. We are expecting
    the jump to be big since we know edited SORV and CSLR notes are often
    re-entered with those codes instead of QQQQ but didn't want to risk
    undercounting them by arbitrarily grouping all that occur in a specific
    time period (beyond one day)
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#34099
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 3fde7fe917513faaee93636ce439c2e34353241d

--- recidiviz/ingest/direct/regions/us_tn/ingest_views/view_InferredViolations.py
+++ recidiviz/ingest/direct/regions/us_tn/ingest_views/view_InferredViolations.py
@@ -24,19 +24,19 @@ from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_QUERY_TEMPLATE = """
 WITH 
+-- This CTE is essentially the ingest view for documented violations direct from the Violations table which 
+-- will be used later on to validate/potentially disregard inferred violations when we know a violation was already 
+-- recorded in the official documentation process 
 documented_violations as 
 (SELECT 
-    # This CTE is essentially the ingest view for documented violations direct from the Violations table which 
-    # will be used later on to validate/potentially disregard inferred violations when we know a violation was already 
-    # recorded in the official documentation process 
   OffenderID,
   TriggerNumber,
   CAST(ContactNoteDate AS DATETIME) as ContactNoteDate,
   ContactNoteType
 FROM {Violations}
 ),
+-- Selects all contact notes with type ABSV, ABSR, or ABSW to not count inferred violations that occur up to 60 days after an ABS* ContactNoteType. 
 abs_contact_notes AS (
-  # Selects all contact notes with type ABSV, ABSR, or ABSW to not count inferred violations that occur up to 60 days after an ABS* ContactNoteType. 
   SELECT 
     OffenderID, 
     ContactNoteType,
@@ -44,8 +44,8 @@ abs_contact_notes AS (
   FROM {ContactNoteType} 
   WHERE ContactNoteType LIKE 'ABS%'
 ),
+-- Selects all VWAR and VRPT contact notes and groups the contact notes that occur on the same day, prioritizing VWARs over VRPTs. 
 cleaned_contact_note_type AS (
-    # Selects all VWAR and VRPT contact notes and groups the contact notes that occur on the same day, prioritizing VWARs over VRPTs. 
     SELECT 
         OffenderID,
         CAST(ContactNoteDateTime AS DATETIME) as ContactNoteDateTime,
@@ -60,18 +60,30 @@ cleaned_contact_note_type AS (
       WHERE ContactNoteType IN ('VWAR','VRPT')) c
     WHERE type_ranking = 1 
 ),
+-- Selects all SORV and CSLR contact notes and groups each of the contact notes types that occur on the same day
+contact_note_type_SORV_CSLR AS (
+    SELECT 
+        OffenderID,
+        CAST(ContactNoteDateTime AS DATETIME) as ContactNoteDateTime,
+        ContactNoteType,
+    FROM ContactNoteType_generated_view c
+    WHERE ContactNoteType IN ('SORV','CSLR') 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY OffenderID, ContactNoteType, DATE_TRUNC(CAST(ContactNoteDateTime AS DATETIME), DAY) ORDER BY ContactNoteDateTime ASC ) = 1
+
+),
+-- Assuming that if a VWAR and VRPT (or multiple of either) are filed within 30 days of each other, they are for a 
+-- related violation not for separate violations, so we assign them this flag to help with grouping in subsequent CTEs 
 ordered_vwars_vrpts AS (
 SELECT 
   *, 
   ROW_NUMBER() OVER (PARTITION BY OffenderID ORDER BY ContactNoteDateTime) as sequence,
   CASE 
-     # Assuming that if a VWAR and VRPT (or multiple of either) are filed within 30 days of each other, they are for a 
-     # related violation not for separate violations, so we assign them this flag to help with grouping in subsequent CTEs 
     WHEN ROW_NUMBER() OVER (PARTITION BY OffenderID ORDER BY ContactNoteDateTime) = 1 THEN 'first_event'
     WHEN DATE_DIFF(ContactNoteDateTime, LAG(ContactNoteDateTime) OVER (PARTITION BY OffenderID ORDER BY ContactNoteDateTime ASC), DAY) < 31 THEN 'less_than_30_days'
     ELSE 'likely_unrelated' END AS time_between_this_and_previous_event
 FROM cleaned_contact_note_type
 ),
+-- Nulls out sequence number for events that should be grouped with the previous event
 identifying_likely_related_events AS (
     SELECT 
         OffenderID,
@@ -80,11 +92,12 @@ identifying_likely_related_events AS (
         sequence,
         time_between_this_and_previous_event,
         CASE 
-            # Nulls out sequence number for events that should be grouped with the previous event
             WHEN time_between_this_and_previous_event = 'less_than_30_days' THEN NULL
             ELSE sequence END AS sequence_with_groupings_for_related_events
     FROM ordered_vwars_vrpts
 ),
+-- Here, we are looking for any sequence_with_groupings_for_related_events that are null from the previous 
+-- CTE which means they should have the sequence value of the first event in the grouped event and then assigning that sequenvce value 
 grouping_likely_related_events AS (
   SELECT 
     OffenderID,
@@ -92,20 +105,18 @@ grouping_likely_related_events AS (
         ContactNoteType,
         sequence,
         time_between_this_and_previous_event,
-        CASE 
-            # Here, we are looking for any sequence_with_groupings_for_related_events that are null from the previous 
-            # CTE which means they should have the sequence value of the first event in the grouped event and then assigning that sequenvce value 
+        CASE    
             WHEN sequence_with_groupings_for_related_events is NULL THEN LAST_VALUE(sequence_with_groupings_for_related_events IGNORE NULLS) OVER (PARTITION BY OffenderID ORDER BY ContactNoteDateTime ASC) 
             ELSE sequence_with_groupings_for_related_events END AS sequence_with_groupings_for_related_events
   FROM identifying_likely_related_events 
 ),
+-- Having min and max contact date allows us to join later on within 31 days before or after either the min or max date 
+-- of an inferred violation. (So for example, if an inferred violation has a VWAR on DAY 1, a VRPT on DAY 28, we are 
+-- checking if there are any documented violations within 31 days of day 1 OR day 28)
 all_inferred_violations AS (
 SELECT 
   OffenderID,
   'INFERRED' AS TriggerNumber,
-  # Having min and max contact date allows us to join later on within 31 days before or after either the min or max date 
-  # of an inferred violation. (So for example, if an inferred violation has a VWAR on DAY 1, a VRPT on DAY 28, we are 
-  # checking if there are any documented violations within 31 days of day 1 OR day 28)
   CAST(MIN(ContactNoteDateTime) AS DATETIME) as ContactNoteDate_min, 
   CAST(MAX(ContactNoteDateTime) AS DATETIME) as 
   ContactNoteDate_max,
@@ -123,11 +134,33 @@ SELECT
                           time_between_this_and_previous_event,
                           CAST(sequence_with_groupings_for_related_events AS STRING)) ORDER BY OffenderID, ContactNoteDateTime)
               ) as response_info,
-  'INFERRED' AS ContactNoteType
+   'INFERRED' AS ContactNoteType
 FROM grouping_likely_related_events
 GROUP BY OffenderID, sequence_with_groupings_for_related_events
+),
+-- Making null placeholders so we can join SORV and CSLR to the other violations format
+-- We have to make sequence the contactnotetype since we aren't applying the sequence logic
+-- to SORV/CSLR and one of each could happen on the same day and then they would have the 
+-- same external id for StateSupervisionViolationResponse
+ SORV_and_CSLR_info AS (
+  SELECT *, 
+    CASE WHEN ContactNoteType = "SORV" THEN "SORV"
+        WHEN ContactNoteType = "CSLR" THEN "CSLR"
+     ELSE null
+    END AS sequence, 
+    CASE 
+      WHEN ContactNoteType = "CSLR" THEN "CSLR"
+      WHEN ContactNoteType = "SORV" THEN "SORV"
+      ELSE "INFERRED"
+    END AS TriggerNumber,
+    null AS time_between_this_and_previous_event,
+    null AS sequence_with_groupings_for_related_events
+  FROM  contact_note_type_SORV_CSLR
 )
-
+-- NOTE: If TN begins adding CSLR and SORV to Violations we will want to add them earlier
+-- in the logic to avoid double counting, but they are currently not added to Violations 
+-- table by design and we do not arbitrarily want to apply the same logic to SORV/CSLR 
+-- as we do for the VWARS/VRPTS
 SELECT 
     aiv.OffenderID, 
     aiv.TriggerNumber,
@@ -142,6 +175,33 @@ LEFT JOIN abs_contact_notes acn ON aiv.OffenderID=acn.OffenderID
 WHERE (dv.TriggerNumber is null AND acn.OffenderID is null) 
     # This ensures we only bring in violations that do not have 
     # any Violations + Sanctions from documented_violations tables in TOMIS or do not have an ABS* code 90 days before the inferred violation 
+
+UNION ALL
+
+SELECT DISTINCT
+  OffenderID,
+  -- we have to make the triggerNumber the ContactNoteType for these since with the current 
+  -- methodology we see some CSLR and SORV codes being entered with the same datetime
+  TriggerNumber,
+  ContactNoteDateTime AS ContactNoteDate,
+  TO_JSON_STRING(
+                  ARRAY_AGG(STRUCT<OffenderID string,
+                                  ContactNoteDateTime datetime,
+                                  ContactNoteType string,
+                                  sequence string,
+                                  time_between_this_and_previous_event string,
+                                  sequence_with_groupings_for_related_events string>
+                          (OffenderID,
+                          ContactNoteDateTime,
+                          ContactNoteType,
+                          CAST(sequence AS STRING),
+                          CAST(time_between_this_and_previous_event AS STRING),
+                          CAST(sequence_with_groupings_for_related_events AS STRING)) ORDER BY OffenderID, ContactNoteDateTime)
+              ) as response_info,
+
+  ContactNoteType
+  FROM SORV_and_CSLR_info
+  GROUP BY OffenderID, TriggerNumber, ContactNoteDate,ContactNoteType
 """
 
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
