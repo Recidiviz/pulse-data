4283b9edb7444548bf6b3187fe8dffb6ca562958
ethanoro <ethan@recidiviz.org>
2024-10-18 09:35:29 -0400

    [boundary finder] add single, unescaped quote finding logic (Recidiviz/recidiviz-data#34223)
    ## Description of the change
    
    adds logic to find and reason about single, unescaped quotes. for now,
    we are keeping just a single quote and a future pr will add
    functionality for multiple quotes!
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#30505
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 625bfdf275f4e9a99a0136f6f53311d3247a59c8

---
+++ recidiviz/tests/utils/unescaped_quote_classification_test.py
@@ -0,0 +1,354 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Test for unescaped quote utils."""
+
+import unittest
+
+from recidiviz.common.constants.csv import (
+    CARRIAGE_RETURN,
+    DEFAULT_CSV_LINE_TERMINATOR,
+    DEFAULT_CSV_QUOTE_CHAR,
+    DEFAULT_CSV_SEPARATOR,
+)
+from recidiviz.utils.unescaped_quote_classification import (
+    SingleUnescapedQuote,
+    SingleUnescapedQuoteState,
+    find_single_first_unescaped_quote,
+)
+
+
+class TestSingleUnescapedQuote(unittest.TestCase):
+    """Tests for SingleUnescapedQuote class"""
+
+    line_term = DEFAULT_CSV_LINE_TERMINATOR.encode()
+    separator = DEFAULT_CSV_SEPARATOR.encode()
+    carriage_return = CARRIAGE_RETURN.encode()
+    quote_char = DEFAULT_CSV_QUOTE_CHAR.encode()
+
+    def test_empties(self) -> None:
+
+        empty_tests = [
+            (b"", b"", self.carriage_return),
+            (b"", b"", self.line_term),
+            (b",,", b",", self.carriage_return),
+            (b"", b",", self.carriage_return),
+            (b"", b",", self.line_term),
+        ]
+        for prev_, next_, line_term in empty_tests:
+
+            assert (
+                SingleUnescapedQuote(
+                    index=0,
+                    quote_char=self.quote_char,
+                    prev_chars=prev_,
+                    next_chars=next_,
+                ).get_quote_state(
+                    line_term=line_term,
+                    separator=self.separator,
+                )
+                == SingleUnescapedQuoteState.INDETERMINATE
+            )
+
+    def test_prev_char_sep(self) -> None:
+        _tests = {
+            (
+                self.line_term,
+                b"a,",
+                b",a",
+            ): SingleUnescapedQuoteState.START_OR_END_OF_QUOTED_CELL,
+            (
+                self.line_term,
+                b"a,",
+                b"\nx",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_CELL_OR_END_OF_QUOTED_LINE,
+            (
+                self.carriage_return,
+                b"a,",
+                b"\r\n",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_CELL_OR_END_OF_QUOTED_LINE,
+            (
+                self.carriage_return,
+                b"a,",
+                b"\r",
+            ): SingleUnescapedQuoteState.INDETERMINATE,  # \n is cutoff
+            (self.line_term, b"a,", b"a,"): SingleUnescapedQuoteState.START_QUOTED_CELL,
+            (self.line_term, b",,", b"a,"): SingleUnescapedQuoteState.START_QUOTED_CELL,
+        }
+
+        for (line_term, prev_, next_), state in _tests.items():
+
+            assert (
+                SingleUnescapedQuote(
+                    index=0,
+                    quote_char=self.quote_char,
+                    prev_chars=prev_,
+                    next_chars=next_,
+                ).get_quote_state(
+                    line_term=line_term,
+                    separator=self.separator,
+                )
+                == state
+            )
+
+    def test_prev_char_line_term(self) -> None:
+        _tests = {
+            (
+                self.line_term,
+                b"\n",
+                b",",
+            ): SingleUnescapedQuoteState.END_OF_QUOTED_CELL_OR_START_OF_QUOTED_LINE,
+            (
+                self.line_term,
+                b"\n",
+                b"\n",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_CELL_OR_END_OF_QUOTED_LINE,
+            (
+                self.carriage_return,
+                b"\r\n",
+                b"\r\n",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_CELL_OR_END_OF_QUOTED_LINE,
+            (
+                self.carriage_return,
+                b"\r\n",
+                b"a,",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_LINE,
+            (self.line_term, b"\nx", b"\r"): SingleUnescapedQuoteState.INVALID,
+        }
+
+        for (line_term, prev_, next_), state in _tests.items():
+
+            assert (
+                SingleUnescapedQuote(
+                    index=0,
+                    quote_char=self.quote_char,
+                    prev_chars=prev_,
+                    next_chars=next_,
+                ).get_quote_state(
+                    line_term=line_term,
+                    separator=self.separator,
+                )
+                == state
+            )
+
+    def test_prev_char_is_not_special_at_least_to_the_csv_reader(self) -> None:
+        _tests = {
+            (self.line_term, b"a", b",a"): SingleUnescapedQuoteState.END_QUOTED_CELL,
+            (
+                self.line_term,
+                b"a",
+                b"\nx",
+            ): SingleUnescapedQuoteState.END_OF_QUOTED_LINE,
+            (self.line_term, b"x", b"x"): SingleUnescapedQuoteState.INVALID,
+            (
+                self.carriage_return,
+                b"dd",
+                b"\r\n",
+            ): SingleUnescapedQuoteState.END_OF_QUOTED_LINE,
+            (self.line_term, b"dd", b"\r\n"): SingleUnescapedQuoteState.INVALID,
+        }
+
+        for (line_term, prev_, next_), state in _tests.items():
+
+            assert (
+                SingleUnescapedQuote(
+                    index=0,
+                    quote_char=self.quote_char,
+                    prev_chars=prev_,
+                    next_chars=next_,
+                ).get_quote_state(
+                    line_term=line_term,
+                    separator=self.separator,
+                )
+                == state
+            )
+
+    def test_multi_byte_sep(self) -> None:
+        _tests = {
+            # multiple byte separator
+            (
+                "£",
+                "a£",
+                "£a",
+            ): SingleUnescapedQuoteState.START_OR_END_OF_QUOTED_CELL,
+            (
+                "£",
+                "a£",
+                "\nx",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_CELL_OR_END_OF_QUOTED_LINE,
+            (
+                "£",
+                "a£",
+                "\na",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_CELL_OR_END_OF_QUOTED_LINE,
+            (
+                "£",
+                "a\n",
+                "£",
+            ): SingleUnescapedQuoteState.END_OF_QUOTED_CELL_OR_START_OF_QUOTED_LINE,
+            (
+                "£",
+                "a\n",
+                "a£",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_LINE,
+            # multi char
+            (
+                "££",
+                "aaa\n",
+                "a£a",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_LINE,
+            (
+                "££",
+                "a£",
+                "£a",
+            ): SingleUnescapedQuoteState.INDETERMINATE,
+            (
+                "££",
+                "££",
+                "\nxaaa",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_CELL_OR_END_OF_QUOTED_LINE,
+            (
+                "££",
+                "££",
+                "\n£d",
+            ): SingleUnescapedQuoteState.START_OF_QUOTED_CELL_OR_END_OF_QUOTED_LINE,
+            (
+                "££",
+                "££\n",
+                "££",
+            ): SingleUnescapedQuoteState.END_OF_QUOTED_CELL_OR_START_OF_QUOTED_LINE,
+        }
+
+        for (separator, prev_, next_), state in _tests.items():
+
+            print(separator, prev_.encode(), next_.encode())
+            assert (
+                SingleUnescapedQuote(
+                    index=0,
+                    quote_char=self.quote_char,
+                    prev_chars=prev_.encode(),
+                    next_chars=next_.encode(),
+                ).get_quote_state(
+                    line_term=self.line_term,
+                    separator=separator.encode(),
+                )
+                == state
+            )
+
+
+class TestFindUnescapedQuote(unittest.TestCase):
+    """Tests for find_first_unescaped_quote function"""
+
+    quote_char = DEFAULT_CSV_QUOTE_CHAR.encode()
+
+    def test_single_byte_only(self) -> None:
+        with self.assertRaisesRegex(
+            ValueError, "Cannot have a quote char that is more than a single byte"
+        ):
+            find_single_first_unescaped_quote(b"", b"to", 2)
+
+    def test_empty(self) -> None:
+        buffers = [
+            b"",
+            b"asdf",
+            b"'",
+            b'heheonlydoublequotes""nosingles',
+            b'heheonlytriplequotes""nosingles',
+        ]
+        for buffer in buffers:
+            assert (
+                find_single_first_unescaped_quote(buffer, self.quote_char, 2)
+                == SingleUnescapedQuote.none()
+            )
+
+    def test_finds_simple(self) -> None:
+        buffers = [
+            (
+                b'here is a simple case",told ya',
+                SingleUnescapedQuote(
+                    index=21,
+                    prev_chars=b"se",
+                    next_chars=b",t",
+                    quote_char=self.quote_char,
+                ),
+            ),
+            (
+                b'here is a simple case""","told ya"',
+                SingleUnescapedQuote(
+                    index=25,
+                    prev_chars=b'",',
+                    next_chars=b"to",
+                    quote_char=self.quote_char,
+                ),
+            ),
+        ]
+        for buffer, expected_result in buffers:
+            assert (
+                find_single_first_unescaped_quote(buffer, self.quote_char, 2)
+                == expected_result
+            )
+
+    def test_finds_complex(self) -> None:
+        buffers = [
+            (
+                b'here is a harder case: "" <-- that is escaped! case",told ya',
+                SingleUnescapedQuote(
+                    index=51,
+                    prev_chars=b"se",
+                    next_chars=b",t",
+                    quote_char=self.quote_char,
+                ),
+            ),
+            (
+                b'harder again: "" (escaped) and then (unescaped)""""","told ya"',
+                SingleUnescapedQuote(
+                    index=53,
+                    prev_chars=b'",',
+                    next_chars=b"to",
+                    quote_char=self.quote_char,
+                ),
+            ),
+        ]
+        for buffer, expected_result in buffers:
+            assert (
+                find_single_first_unescaped_quote(buffer, self.quote_char, 2)
+                == expected_result
+            )
+
+    def test_alt_quote(self) -> None:
+        quote_char = "'".encode()
+        buffers = [
+            (
+                b"here is a simpler case: '' <-- that is escaped! case',told ya",
+                SingleUnescapedQuote(
+                    index=52, prev_chars=b"e", next_chars=b",", quote_char=quote_char
+                ),
+            ),
+            (
+                b"harder again: '' (escaped) and then (unescaped)''''','told ya'",
+                SingleUnescapedQuote(
+                    index=53,
+                    prev_chars=b",",
+                    next_chars=b"t",
+                    quote_char=quote_char,
+                ),
+            ),
+        ]
+        for buffer, expected_result in buffers:
+            assert (
+                find_single_first_unescaped_quote(buffer, quote_char, 1)
+                == expected_result
+            )
