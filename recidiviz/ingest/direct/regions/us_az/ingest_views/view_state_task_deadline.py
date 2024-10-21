86726a6a145529f0de687f3111b8bd540bc796f9
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-10-21 15:15:13 -0400

    [US_AZ] Improve state task deadline ingest logic (Recidiviz/recidiviz-data#34308)
    ## Description of the change
    
    We are using the state_task_deadline entity in AZ to identify two
    release dates stored in their OMS (ACIS TPR and DTP dates). The last
    iteration of the view that hydrates this entity made a few mistakes that
    are corrected here:
    1. We should be partitioning by FINAL_OFFENSE_ID (sentence) instead of
    DOC_ID (person incarceration stint). The results of these should
    theoretically be the same, but since these release dates are calculated
    at the sentence group level, it is more logically sound to orient this
    view around sentences.
    2. There are more ways to deduplicate changes that were made with the
    same UPDT_DTM than were originally factored into this view. This PR adds
    deduplication using CHANGE_ID and SC_CALC_HISTORY_ID, which are two
    identifiers that only increment over time. This limits the number of
    times we need to deduplicate by the actual eligibility date to 7.
    3. In those 7 cases, we preserve the latest eligibility date rather than
    the earliest one. This is more aligned with reality based on what we
    have learned about how these dates change over time.
    
    Tests:
    - [x] ingest view query is deterministic
    - [x] run_mappings script passes
    - [x] sandbox ingest
    [pipeline](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-10-18_10_16_18-4041155279008777014;graphView=1?project=recidiviz-staging&pageState=(%22dfTime%22:(%22l%22:%22dfJobMaxTime%22)))
    succeeds
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#29234
    Related to Recidiviz/recidiviz-data#34237
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 56593d7d1ac025c0606f5dff0907fd5ab3d55a94

--- recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_task_deadline.py
+++ recidiviz/ingest/direct/regions/us_az/ingest_views/view_state_task_deadline.py
@@ -37,78 +37,87 @@ WITH
 -- the date the person is planned to be released on TPR, if one exists, as well as the date
 -- on which that release date was last updated in ACIS. 
 tpr_eligibility_dates AS (
-SELECT DISTINCT 
-  CAST(COALESCE(
-    NULLIF(TRANSITION_PROGRAM_RLS_DTM_ML, 'NULL'),
-    NULLIF(TRANSITION_PROGRAM_RLS_DTM_ARD, 'NULL'), 
-    NULLIF(TRANSITION_PROGRAM_RLS_DTM, 'NULL')) AS DATETIME) AS transition_release_eligibility_date,
-  CAST(COALESCE(
-    NULLIF(off.UPDT_DTM_ML, 'NULL'), 
-    NULLIF(off.UPDT_DTM_ARD, 'NULL'), 
-    NULLIF(off.UPDT_DTM, 'NULL')) AS DATETIME) AS update_datetime_elig,
-  ep.PERSON_ID,
-  ep.DOC_ID,
-  sc.FINAL_OFFENSE_ID,
-  sc.SC_EPISODE_ID,
-  'Standard Transition Release' AS task_subtype
-FROM {AZ_DOC_SC_OFFENSE@ALL} off
-LEFT JOIN {AZ_DOC_SC_COMMITMENT}
-USING(COMMITMENT_ID)
-LEFT JOIN {AZ_DOC_SC_EPISODE} sc
-USING(SC_EPISODE_ID)
-LEFT JOIN {DOC_EPISODE} ep
-USING(DOC_ID)
-WHERE TRANSITION_PROGRAM_STATUS_ID IN (
-  '10650', -- Approved
-  '10652' -- Tentative
-)
-AND sc.FINAL_OFFENSE_ID = off.OFFENSE_ID
--- This is only true in 52 rows out of ~30k
-AND PERSON_ID IS NOT NULL
-AND COALESCE(
-    NULLIF(TRANSITION_PROGRAM_RLS_DTM_ML, 'NULL'),
-    NULLIF(TRANSITION_PROGRAM_RLS_DTM_ARD, 'NULL'), 
-    NULLIF(TRANSITION_PROGRAM_RLS_DTM, 'NULL'), '1900-01-01') BETWEEN '1900-01-01' AND '2100-01-01'
+SELECT 
+  PERSON_ID,
+  DOC_ID,
+  FINAL_OFFENSE_ID,
+  SC_EPISODE_ID,
+  task_subtype,
+  IF(transition_release_eligibility_date BETWEEN '1900-01-01' AND '2100-01-01', transition_release_eligibility_date, NULL) AS transition_release_eligibility_date,
+  update_datetime_elig,
+  CHANGE_ID,
+  SC_CALC_HISTORY_ID
+  FROM (
+    SELECT DISTINCT 
+      CAST(COALESCE(
+        NULLIF(TRANSITION_PROGRAM_RLS_DTM_ML, 'NULL'),
+        NULLIF(TRANSITION_PROGRAM_RLS_DTM_ARD, 'NULL'), 
+        NULLIF(TRANSITION_PROGRAM_RLS_DTM, 'NULL')) AS DATETIME) AS transition_release_eligibility_date,
+      CAST(COALESCE(
+        NULLIF(off.UPDT_DTM_ML, 'NULL'), 
+        NULLIF(off.UPDT_DTM_ARD, 'NULL'), 
+        NULLIF(off.UPDT_DTM, 'NULL')) AS DATETIME) AS update_datetime_elig,
+      ep.PERSON_ID,
+      ep.DOC_ID,
+      sc.FINAL_OFFENSE_ID,
+      sc.SC_EPISODE_ID,
+      off.CHANGE_ID,
+      off.SC_CALC_HISTORY_ID,
+      'Standard Transition Release' AS task_subtype
+    FROM {AZ_DOC_SC_OFFENSE@ALL} off
+    JOIN {AZ_DOC_SC_EPISODE} sc
+      ON(sc.FINAL_OFFENSE_ID = off.OFFENSE_ID)
+    JOIN {DOC_EPISODE} ep
+      USING(DOC_ID)
+    WHERE TRANSITION_PROGRAM_STATUS_ID IN (
+      '10650', -- Approved
+      '10652' -- Tentative
+    )
+  )
 ),
 -- Return one row per person, per incarceration stint, when the person has been approved
 -- or tentatively approved for drug transition release for that stint. Each row will contain
 -- the date the person is planned to be released on DTP, if one exists, as well as the date
 -- on which that release date was last updated in ACIS. 
 dtp_eligibility_dates AS (
-SELECT DISTINCT 
-  CAST(COALESCE(
-    NULLIF(DRUG_TRANSITION_PGM_RLS_DTM_ML, 'NULL'),
-    NULLIF(DRUG_TRANSITION_PGM_RLS_DTM_ARD, 'NULL'), 
-    NULLIF(DRUG_TRANSITION_PGM_RLS_DTM, 'NULL')) AS DATETIME) AS drug_transition_release_eligibility_date,
-  CAST(COALESCE(
-    NULLIF(off.UPDT_DTM_ML, 'NULL'), 
-    NULLIF(off.UPDT_DTM_ARD, 'NULL'), 
-    NULLIF(off.UPDT_DTM, 'NULL')) AS DATETIME) AS update_datetime_elig,
-  ep.PERSON_ID,
-  ep.DOC_ID,
-  sc.FINAL_OFFENSE_ID,
-  sc.SC_EPISODE_ID,
-  'Drug Transition Release' AS task_subtype
-FROM {AZ_DOC_SC_OFFENSE@ALL} off
-LEFT JOIN {AZ_DOC_SC_COMMITMENT} comm
-USING(COMMITMENT_ID)
-LEFT JOIN {AZ_DOC_SC_EPISODE} sc
-ON(sc.SC_EPISODE_ID = comm.SC_EPISODE_ID
-AND sc.FINAL_OFFENSE_ID = off.OFFENSE_ID)
-LEFT JOIN {DOC_EPISODE} ep
-USING(DOC_ID)
-WHERE DRUG_TRANSITION_PROGRAM_STATUS_ID IN (
-  '10650', -- Approved
-  '10652' -- Tentative
-)
--- This is only true in 52 rows out of ~30k
-AND PERSON_ID IS NOT NULL
-AND COALESCE(
-    NULLIF(DRUG_TRANSITION_PGM_RLS_DTM_ML, 'NULL'),
-    NULLIF(DRUG_TRANSITION_PGM_RLS_DTM_ARD, 'NULL'), 
-    NULLIF(DRUG_TRANSITION_PGM_RLS_DTM, 'NULL'), '1900-01-01') BETWEEN '1900-01-01' AND '2100-01-01'
+SELECT 
+PERSON_ID,
+  DOC_ID,
+  FINAL_OFFENSE_ID,
+  SC_EPISODE_ID,
+  task_subtype,
+  IF(drug_transition_release_eligibility_date BETWEEN '1900-01-01' AND '2100-01-01', drug_transition_release_eligibility_date, NULL) AS drug_transition_release_eligibility_date,
+  update_datetime_elig,
+  CHANGE_ID,
+  SC_CALC_HISTORY_ID,
+  FROM (
+    SELECT DISTINCT 
+      CAST(COALESCE(
+        NULLIF(DRUG_TRANSITION_PGM_RLS_DTM_ML, 'NULL'),
+        NULLIF(DRUG_TRANSITION_PGM_RLS_DTM_ARD, 'NULL'), 
+        NULLIF(DRUG_TRANSITION_PGM_RLS_DTM, 'NULL')) AS DATETIME) AS drug_transition_release_eligibility_date,
+      CAST(COALESCE(
+        NULLIF(off.UPDT_DTM_ML, 'NULL'), 
+        NULLIF(off.UPDT_DTM_ARD, 'NULL'), 
+        NULLIF(off.UPDT_DTM, 'NULL')) AS DATETIME) AS update_datetime_elig,
+      ep.PERSON_ID,
+      ep.DOC_ID,
+      sc.FINAL_OFFENSE_ID,
+      sc.SC_EPISODE_ID,
+      off.CHANGE_ID,
+      off.SC_CALC_HISTORY_ID,
+      'Drug Transition Release' AS task_subtype
+    FROM {AZ_DOC_SC_OFFENSE@ALL} off
+    JOIN {AZ_DOC_SC_EPISODE} sc
+    ON(sc.FINAL_OFFENSE_ID = off.OFFENSE_ID)
+    JOIN {DOC_EPISODE} ep
+    USING(DOC_ID)
+    WHERE DRUG_TRANSITION_PROGRAM_STATUS_ID IN (
+      '10650', -- Approved
+      '10652' -- Tentative
+    )
+  )
 ),
-
 -- Maintain rows where the eligibility date for TPR changed; discard the rest. 
 tpr_changed_rows AS (
 -- 43,946 rows total
@@ -117,8 +126,8 @@ FROM (
   SELECT 
     *,
     LAG(transition_release_eligibility_date) OVER (
-      PARTITION BY PERSON_ID, DOC_ID 
-      ORDER BY update_datetime_elig, transition_release_eligibility_date NULLS LAST) AS prev_elig_date
+      PARTITION BY PERSON_ID, FINAL_OFFENSE_ID 
+      ORDER BY update_datetime_elig, CHANGE_ID, SC_CALC_HISTORY_ID, transition_release_eligibility_date NULLS LAST) AS prev_elig_date
   FROM tpr_eligibility_dates 
 ) sub
 -- if eligibility date changed, add a row to the ledger
@@ -135,8 +144,8 @@ FROM (
   SELECT 
     *,
     LAG(drug_transition_release_eligibility_date) OVER (
-      PARTITION BY PERSON_ID, DOC_ID 
-      ORDER BY update_datetime_elig, drug_transition_release_eligibility_date NULLS LAST) AS prev_elig_date
+      PARTITION BY PERSON_ID, FINAL_OFFENSE_ID 
+      ORDER BY update_datetime_elig, CHANGE_ID, SC_CALC_HISTORY_ID, drug_transition_release_eligibility_date NULLS LAST) AS prev_elig_date
   FROM dtp_eligibility_dates 
 ) sub
 -- if eligibility date changed, add a row to the ledger
@@ -145,17 +154,17 @@ WHERE
   OR (drug_transition_release_eligibility_date IS NULL and prev_elig_date IS NOT NULL)
   OR (drug_transition_release_eligibility_date IS NOT NULL and prev_elig_date IS NULL)
 )
--- There are a number of instances where an eligbility date is tracked in the system as 
+-- There are a number of instances where an eligibility date is tracked in the system as 
 -- having been updated 2 or 3 times at the exact same time. We deduplicate those rows
--- deterministically and maintain the earliest eligibility date entered at the time.
-SELECT *
+-- deterministically and maintain the latest eligibility date entered at the time.
+SELECT * EXCEPT (CHANGE_ID, SC_CALC_HISTORY_ID)
   FROM (
     SELECT * FROM tpr_changed_rows
     UNION ALL 
     SELECT * FROM dtp_changed_rows
   )
-QUALIFY ROW_NUMBER() OVER (PARTITION BY PERSON_ID, task_subtype, update_datetime_elig
-    ORDER BY transition_release_eligibility_date NULLS LAST) = 1
+QUALIFY ROW_NUMBER() OVER (PARTITION BY PERSON_ID, FINAL_OFFENSE_ID, task_subtype, update_datetime_elig
+    ORDER BY CHANGE_ID DESC, SC_CALC_HISTORY_ID DESC, transition_release_eligibility_date DESC NULLS LAST) = 1
 """
 
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
