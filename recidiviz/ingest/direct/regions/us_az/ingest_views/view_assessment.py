973dda0b911a97a893a4a4d82bce1d52b6b2e5ca
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-10-21 15:05:02 -0400

    [US_AZ] Ingest ACCAT data into state_assessment (Recidiviz/recidiviz-data#34313)
    ## Description of the change
    
    This PR ingests the total ACCAT score and the final supervision level
    associated with that score into `state_assessment`.
    
    ACCAT is short for Arizona Community Corrections Assessment Tool. This
    is one of a handful of assessment tools used by ADCRR; it is the one
    that is used currently to determine clients' supervision levels. There
    is possible future work to ingest other assessment types, but for the
    time being this is the most helpful assessment to have ingested for LRM
    and policy engineering work.
    
    Sandbox ingest pipeline succeeded
    [here](https://console.cloud.google.com/dataflow/jobs/us-west1/2024-10-18_12_21_25-74542768869013610;graphView=1?project=recidiviz-staging&pageState=(%22dfTime%22:(%22l%22:%22dfJobMaxTime%22))).
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#34311
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 272f5e90036aaf015b3d75fc9b38714f4b58f11e

---
+++ recidiviz/ingest/direct/regions/us_az/ingest_views/view_assessment.py
@@ -0,0 +1,61 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+""" Query for ACCAT assessment data. This is an assessment performed only on supervision
+in Arizona.
+"""
+
+from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
+    DirectIngestViewQueryBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_QUERY_TEMPLATE = """
+SELECT
+  accat.ACCAT_ID,
+  accat.DATE_ASSESSMENT,
+  ep.PERSON_ID,
+  accat.TOTAL AS TOTAL_SCORE,
+  level.DESCRIPTION AS FINAL_LEVEL,  
+  /*
+  The external ID '2' is used to denote records "created" during the system migration. 
+  These were initially created by a real user, but their ID was overwritten during the migration
+  and cannot be recovered. As of 10/18/2024 these rows account for 41% of all assessments. 
+  We do not expect to see that number increase over time. 
+  */
+  NULLIF(accat.CREATE_USERID, '2') AS CONDUCTING_STAFF_EXTERNAL_ID
+FROM
+  {AZ_CS_OMS_ACCAT} accat
+JOIN
+  {DPP_EPISODE} ep
+USING
+  (DPP_ID)
+JOIN
+  {LOOKUPS} level
+ON
+  (LEVEL_ID = LOOKUP_ID)
+"""
+
+VIEW_BUILDER = DirectIngestViewQueryBuilder(
+    region="us_az",
+    ingest_view_name="assessment",
+    view_query_template=VIEW_QUERY_TEMPLATE,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
