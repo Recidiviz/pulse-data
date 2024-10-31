559957954cfbabc8fad2f8b95a2d2dccada9c2af
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2024-10-31 11:53:53 -0500

    Create a function to upload randomized assigments to BQ (Recidiviz/recidiviz-data#31746)
    ## Description of the change
    
    I created a function (`upload_assigments_to_gbq`) to upload randomized
    assignments to BQ. I also created tests to ensure the right schema gets
    uploaded to BQ. These are meant to be used in the notebooks where we
    perform the randomization.
    
    Specifically:
    - It appends a pandas df to
    `manually_updated_source_tables.experiment_assignments_large` in both
    staging and prod.
    - It automatically creates a timestamp in the column `upload_datetime`
    for each upload.
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#31117
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Anna Geiduschek <anna@recidiviz.org>
    GitOrigin-RevId: dd667dfb42387d956c7e7fdf00617db771a0ea60

--- recidiviz/tools/analyst/randomization.py
+++ recidiviz/tools/analyst/randomization.py
@@ -18,7 +18,22 @@
 
 import numpy as np
 import pandas as pd
+from google.cloud.bigquery.enums import StandardSqlTypeNames as BigQueryFieldType
 from scipy import stats
+
+from recidiviz.big_query.big_query_address import BigQueryAddress
+from recidiviz.calculator.query.state.dataset_config import (
+    MANUALLY_UPDATED_SOURCE_TABLES_DATASET,
+)
+from recidiviz.calculator.query.state.views.analyst_data.models.metric_unit_of_analysis_type import (
+    MetricUnitOfAnalysisType,
+)
+from recidiviz.source_tables.collect_all_source_table_configs import (
+    collect_externally_managed_source_table_collections,
+)
+from recidiviz.source_tables.source_table_config import SourceTableConfig
+from recidiviz.utils.environment import GCP_PROJECTS
+from recidiviz.utils.types import assert_type
 
 
 def stratified_randomization(
@@ -115,3 +130,176 @@ def orthogonality_table(
     ] + [None] * (len(results_df.columns) - 3)
 
     return results_df.round(2)
+
+
+EXPERIMENT_ASSIGNMENTS_UNIT_ID_COLUMN = "unit_id"
+EXPERIMENT_ASSIGNMENTS_UNIT_TYPE_COLUMN = "unit_type"
+EXPERIMENT_ASSIGNMENTS_UPLOAD_DATETIME_COLUMN = "upload_datetime"
+EXPERIMENT_ASSIGNMENTS_VARIANT_DATE_COLUMN = "variant_date"
+
+_REFERENCED_EXPERIMENT_ASSIGNMENTS_COLUMNS = [
+    EXPERIMENT_ASSIGNMENTS_UNIT_ID_COLUMN,
+    EXPERIMENT_ASSIGNMENTS_UNIT_TYPE_COLUMN,
+    EXPERIMENT_ASSIGNMENTS_UPLOAD_DATETIME_COLUMN,
+    EXPERIMENT_ASSIGNMENTS_VARIANT_DATE_COLUMN,
+]
+
+
+def get_large_experiment_assignments_source_table_config() -> SourceTableConfig:
+    """Returns the SourceTableConfig with schema information about the
+    manually_updated_source_tables.experiment_assignments_large table, as defined in
+    experiment_assignments_large.yaml.
+    """
+    table_address = BigQueryAddress(
+        dataset_id=MANUALLY_UPDATED_SOURCE_TABLES_DATASET,
+        table_id="experiment_assignments_large",
+    )
+    for collection in collect_externally_managed_source_table_collections(
+        project_id=None
+    ):
+        for config in collection.source_tables:
+            if config.address != table_address:
+                continue
+            for col in _REFERENCED_EXPERIMENT_ASSIGNMENTS_COLUMNS:
+                if not config.has_column(col):
+                    raise ValueError(
+                        f"Expected [{config.address.to_str()}] to have column [{col}]"
+                    )
+            return config
+
+    raise ValueError(
+        f"Could not find source table config for [{table_address.to_str()}]."
+    )
+
+
+def upload_assignments_to_gbq(
+    df: pd.DataFrame,
+    unit_type: MetricUnitOfAnalysisType,
+) -> None:
+    """
+    Appends an experiment assignment DataFrame to the BigQuery table <experiment_assignments_large>
+    in the <manually_updated_source_tables> dataset. The final DF must have the same
+    columns as defined in the table schema definition in
+    experiment_assignments_large.yaml.
+    """
+    experiments_table_config = get_large_experiment_assignments_source_table_config()
+    df_copy = df.copy()
+
+    assert_type(unit_type, MetricUnitOfAnalysisType)
+
+    df_copy.loc[:, EXPERIMENT_ASSIGNMENTS_UNIT_TYPE_COLUMN] = unit_type.value
+    df_copy.loc[:, EXPERIMENT_ASSIGNMENTS_UPLOAD_DATETIME_COLUMN] = pd.to_datetime(
+        "today"
+    )
+    _validate_assignment_df_columns(df_copy, experiments_table_config)
+    _validate_unique_id_and_variant_per_day(df_copy)
+
+    num_rows = len(df_copy)
+    for project_id in GCP_PROJECTS:
+        project_specific_address = (
+            experiments_table_config.address.to_project_specific_address(project_id)
+        )
+        print(
+            f"Uploading [{num_rows}] rows to "
+            f"`{project_specific_address.to_str()}`..."
+        )
+        df_copy.to_gbq(
+            destination_table=experiments_table_config.address.to_str(),
+            project_id=project_id,
+            table_schema=[
+                col.to_api_repr() for col in experiments_table_config.schema_fields
+            ],
+            if_exists="append",
+        )
+        print(
+            f"Done uploading [{num_rows}] rows to "
+            f"`{project_specific_address.to_str()}`."
+        )
+
+
+def _convert_df_column_to_type_if_necessary(
+    df: pd.DataFrame, column_name: str, desired_type: BigQueryFieldType
+) -> None:
+    """Converts the values in the column with name |column_name| in the provided
+    DataFrame to a type that is compatible for uploading to a BQ table with the
+    |desired_type|.
+    """
+    try:
+        if desired_type == BigQueryFieldType.STRING:
+            if pd.api.types.is_string_dtype(df[column_name]):
+                # No modification needed
+                return
+            df[column_name] = df[column_name].astype(str)
+        elif desired_type == BigQueryFieldType.INT64:
+            if pd.api.types.is_integer_dtype(df[column_name]):
+                # No modification needed
+                return
+            df[column_name] = pd.to_numeric(df[column_name], downcast="integer")
+        elif desired_type in (BigQueryFieldType.DATE, BigQueryFieldType.TIMESTAMP):
+            if pd.api.types.is_datetime64_any_dtype(df[column_name]):
+                # No modification needed
+                return
+            df[column_name] = pd.to_datetime(df[column_name])
+        else:
+            raise ValueError(
+                f"Unsupported column type for column [{column_name}]: {desired_type}"
+            )
+    except Exception as e:
+        raise ValueError(
+            f"Column [{column_name}] cannot be converted to type [{desired_type}]: {e}"
+        ) from e
+
+
+def _validate_assignment_df_columns(
+    df: pd.DataFrame, experiments_table_config: SourceTableConfig
+) -> None:
+    """Raises a ValueError if the DataFrame does not contain the required columns, if
+    the columns are not in the correct format (or cannot be reasonably converted to the
+    correct format), or if there are extra columns.
+
+    Where possible, converts columns to the appropriate type to match the BQ schema.
+    """
+    required_columns = {
+        column.name for column in experiments_table_config.schema_fields
+    }
+    df_columns = set(df.columns)
+
+    # Check for extra columns
+    extra_columns = df_columns - required_columns
+    if extra_columns:
+        raise ValueError(f"Extra columns present: {extra_columns}")
+
+    # Check for missing columns
+    missing_columns = required_columns - df_columns
+    if missing_columns:
+        raise ValueError(f"Missing required columns: {missing_columns}")
+
+    # Validate column types
+    for col in experiments_table_config.schema_fields:
+        _convert_df_column_to_type_if_necessary(
+            df, column_name=col.name, desired_type=col.field_type
+        )
+
+
+def _validate_unique_id_and_variant_per_day(df: pd.DataFrame) -> None:
+    """
+    Checks if a single unit_id can have at most one variant_id on a given day.
+    Raises a ValueError if any unit_id has more than one variant_id on the same
+    variant_date.
+    """
+    # Group by unit_id and variant_date and count the number of unique variant_ids
+    grouped = df.groupby(
+        [
+            EXPERIMENT_ASSIGNMENTS_UNIT_ID_COLUMN,
+            EXPERIMENT_ASSIGNMENTS_VARIANT_DATE_COLUMN,
+        ]
+    ).variant_id.nunique()
+
+    # Find any groups with more than one unique variant_id
+    invalid_groups = grouped[grouped > 1]
+
+    if not invalid_groups.empty:
+        raise ValueError(
+            f"Found unit_id(s) with multiple variant_id(s) on the same day: "
+            f"{invalid_groups}"
+        )
