40094df96369ea114f88d5200ee6cf7cbca908b6
mayukas <30325262+mayukas@users.noreply.github.com>
2024-11-01 09:49:22 -0400

    Adds new login/registered user metrics to Insights looker explore (Recidiviz/recidiviz-data#34392)
    ## Description of the change
    
    Adds configurations for 3 new metrics in the insights explore:
    - distinct registered users
    - distinct logged in users
    - logins
    
    mirroring the same 3 metrics on the Workflows explore.
    
    See associated LookML autogenerated PR here:
    https://github.com/Recidiviz/looker/pull/660
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: mayukas <mayuka@stanford.edu>
    GitOrigin-RevId: 496fa1ed82a6b2ee25cf640bf3ab322a6af60889

--- recidiviz/tools/looker/aggregated_metrics/custom_insights_metrics_configurations.py
+++ recidiviz/tools/looker/aggregated_metrics/custom_insights_metrics_configurations.py
@@ -17,9 +17,49 @@
 """Configured metrics for custom insights impact metrics displayable in Looker"""
 
 import recidiviz.aggregated_metrics.models.aggregated_metric_configurations as metric_config
+from recidiviz.aggregated_metrics.models.aggregated_metric import (
+    EventCountMetric,
+    EventDistinctUnitCountMetric,
+    SpanDistinctUnitCountMetric,
+)
+from recidiviz.calculator.query.state.views.analyst_data.models.event_selector import (
+    EventSelector,
+)
+from recidiviz.calculator.query.state.views.analyst_data.models.span_selector import (
+    SpanSelector,
+)
+from recidiviz.observations.event_type import EventType
+from recidiviz.observations.span_type import SpanType
 
 ABSCONSIONS_AND_BENCH_WARRANTS_LOOKER = metric_config.ABSCONSIONS_BENCH_WARRANTS
 AVG_DAILY_POPULATION_LOOKER = metric_config.AVG_DAILY_POPULATION
+DISTINCT_REGISTERED_USERS_LOOKER = SpanDistinctUnitCountMetric(
+    name="distinct_registered_users",
+    display_name="Distinct Total Registered Primary Users (Insights)",
+    description="Number of distinct primary (supervisor) Insights users who have signed up/logged into Insights at least once",
+    span_selector=SpanSelector(
+        span_type=SpanType.INSIGHTS_USER_REGISTRATION_SESSION,
+        span_conditions_dict={},
+    ),
+)
+DISTINCT_LOGGED_IN_USERS_LOOKER = EventDistinctUnitCountMetric(
+    name="distinct_logged_in_users",
+    display_name="Distinct Primary Users Logging In (Insights)",
+    description="Number of distinct primary (supervisor) Insights users who logged into Insights",
+    event_selector=EventSelector(
+        event_type=EventType.INSIGHTS_USER_LOGIN,
+        event_conditions_dict={},
+    ),
+)
+LOGINS_LOOKER = EventCountMetric(
+    name="logins",
+    display_name="Logins (Insights)",
+    description="Number of logins performed by primary Insights users",
+    event_selector=EventSelector(
+        event_type=EventType.INSIGHTS_USER_LOGIN,
+        event_conditions_dict={},
+    ),
+)
 INCARCERATION_STARTS_LOOKER = metric_config.INCARCERATION_STARTS
 INCARCERATION_STARTS_AND_INFERRED_LOOKER = (
     metric_config.INCARCERATION_STARTS_AND_INFERRED
