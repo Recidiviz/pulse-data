fc26611e9e7c7f7a83c3b9f9bd226dbce089d304
elisegonzal <76572883+elisegonzal@users.noreply.github.com>
2024-11-11 14:56:40 -0500

    [US_ND] First drafts of PSI views (Recidiviz/recidiviz-data#34427)
    ## Description of the change
    
    As I was validating that we had the data we need, I found it easiest to
    write very rough first drafts of the views we will need for the PSI
    project in ND. This PR adds those drafts to a brand new US_ND folder of
    sentencing views!
    
    One thing to note is that while we are set up to import the
    `docstars_psi` data and are receiving 2-week incremental transfers
    nightly, something went wrong with the full historical transfer of the
    file, so there is very little data in that table as of now. The raw data
    config for that file is also currently only deployed to staging, so
    there will be no data in prod until it is deployed there next week!
    
    There is definitely still lots of work to be done to hone these - I left
    some comments and helpful hints where I could. Happy to answer any
    questions along the way!
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 59829f99be16d068695ba220a8189b5bf4f9fea9

---
+++ recidiviz/calculator/query/state/views/sentencing/us_nd/sentencing_charge_template.py
@@ -0,0 +1,49 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""View logic to prepare US_ND Sentencing charge data for PSI tools
+
+This view is identical to that used in IX. It runs, but I have not validated its results."""
+
+US_ND_SENTENCING_CHARGE_TEMPLATE = """
+    SELECT
+    description AS charge,
+    "US_ND" AS state_code,
+    -- Only count the number of times a charge appears in the past two years to determine frequency
+    SUM(
+    IF
+        (DATE_DIFF(CURRENT_DATE("US/Eastern"), offense_date, YEAR) <= 2, 1, 0)) AS frequency,
+    IF
+    -- Determine if a charge is violent based what percentage of the time it is tagged as such, rounded down or up
+    (ROUND(SUM(
+        IF
+            (is_violent, 1, 0)) / COUNT(*)) = 1,
+        TRUE,
+        FALSE) AS is_violent,
+    IF
+    -- Determine if a charge is a sex offense based what percentage of the time it is tagged as such, rounded down or up
+    (ROUND(SUM(
+        IF
+            (is_sex_offense, 1, 0)) / COUNT(*)) = 1,
+        TRUE,
+        FALSE) AS is_sex_offense
+    FROM
+      `{project_id}.normalized_state.state_charge`
+    WHERE
+    state_code = "US_ND"
+    GROUP BY
+    description
+"""
