3ba94fde37986c986990f45aedaac73ab6dab695
Damini Sharma <damini@recidiviz.org>
2024-11-15 14:50:36 -0800

    [US_TN] Exclude `TRANSITIONAL` officers from TN insights (Recidiviz/recidiviz-data#35180)
    ## Description of the change
    
    We updated our TN rosters to reflect SCU officers who are "SCU + Admin"
    - i.e. assigned to an SCU unit to track all their admin cases - to
    `TRANSITIONAL` because we don't want to associate events with them. This
    PR excludes them from being shown on the front end
    
    Validation shows below that only TN changes and sandbox has fewer
    officers since we're excluding TRANSITIONAL
    
    <img width="844" alt="Screenshot 2024-11-15 at 2 47 58 PM"
    src="https://github.com/user-attachments/assets/a0acdfce-0fa2-4733-af01-27981f8578cd">
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 63636576179f6a786aea505d4fcf98fd5a760039

--- recidiviz/outliers/outliers_configs.py
+++ recidiviz/outliers/outliers_configs.py
@@ -249,6 +249,7 @@ Denominator is the average daily caseload for the officer over the given time pe
     --TODO(#25695): Revisit this after excluding admin supervision levels    
     AND avg_daily_population BETWEEN 10 AND 175
     AND prop_period_with_critical_caseload >= 0.75""",
+        supervision_staff_exclusions="COALESCE(specialized_caseload_type_primary,'') NOT IN ('TRANSITIONAL')",
         primary_category_type=InsightsCaseloadCategoryType.SEX_OFFENSE_BINARY,
         available_specialized_caseload_categories={
             InsightsCaseloadCategoryType.SEX_OFFENSE_BINARY: [
