4dbb4f49b5d2a369de5d74f7832b6ec6e2246622
Emily Turner <149205316+emilyemilyemilyemilyemilyemily@users.noreply.github.com>
2024-11-15 10:28:30 -0800

    [calc dag] Move ingest_pipeline_should_run_in_dag out of KPO (Recidiviz/recidiviz-data#35120)
    ## Description of the change
    
    Moving `ingest_pipeline_should_run_in_dag` out of kubernetes because of
    various issues that's been causing the task to be flaky. As it stands
    the function checks 1. is ingest launched in the state 2. are there
    launchable views. Because we branch on states that have ingest launched
    [here](https://github.com/Recidiviz/recidiviz-data/blob/main/recidiviz/airflow/dags/calculation_dag.py#L221)
    the first condition will always be true so I've rebranded this check as
    `check_region_has_launchable_ingest_views`
    
    After this change is made we can remove the option to use xcom in KPO
    and close out Recidiviz/recidiviz-data#31148
    
    source visibility check is failing i'll fix it in a little bit
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#33880
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: a2a9915bacf7be8ea5acd3ac0a5c224b466be01e

--- recidiviz/airflow/dags/calculation/dataflow/single_ingest_pipeline_group.py
+++ recidiviz/airflow/dags/calculation/dataflow/single_ingest_pipeline_group.py
@@ -54,7 +54,18 @@ from recidiviz.airflow.dags.utils.dataflow_pipeline_group import (
     build_dataflow_pipeline_task_group,
 )
 from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct import direct_ingest_regions
+from recidiviz.ingest.direct.ingest_mappings.ingest_view_contents_context import (
+    IngestViewContentsContextImpl,
+)
+from recidiviz.ingest.direct.ingest_mappings.ingest_view_manifest_collector import (
+    IngestViewManifestCollector,
+)
+from recidiviz.ingest.direct.ingest_mappings.ingest_view_manifest_compiler_delegate import (
+    StateSchemaIngestViewManifestCompilerDelegate,
+)
 from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.utils import metadata
 
 # Need a disable pointless statement because Python views the chaining operator ('>>')
 # as a "pointless" statement
@@ -64,34 +75,41 @@ from recidiviz.persistence.database.schema_type import SchemaType
 # pylint: disable=W0106 expression-not-assigned
 
 
-def _ingest_pipeline_should_run_in_dag(state_code: StateCode) -> KubernetesPodOperator:
-    return build_kubernetes_pod_task(
-        task_id="ingest_pipeline_should_run_in_dag",
-        container_name="ingest_pipeline_should_run_in_dag",
-        arguments=[
-            "--entrypoint=IngestPipelineShouldRunInDagEntrypoint",
-            f"--state_code={state_code.value}",
-        ],
-        cloud_sql_connections=[SchemaType.OPERATIONS],
-        do_xcom_push=True,
+def _has_launchable_ingest_views(state_code: StateCode) -> bool:
+    region = direct_ingest_regions.get_direct_ingest_region(
+        region_code=state_code.value.lower()
+    )
+    ingest_manifest_collector = IngestViewManifestCollector(
+        region=region,
+        delegate=StateSchemaIngestViewManifestCompilerDelegate(region=region),
+    )
+    return (
+        len(
+            ingest_manifest_collector.launchable_ingest_views(
+                IngestViewContentsContextImpl.build_for_project(
+                    project_id=metadata.project_id()
+                )
+            )
+        )
+        > 0
     )
 
 
 @task.short_circuit(ignore_downstream_trigger_rules=False)
 # allows skipping of downstream tasks until trigger rule prevents it (like ALL_DONE)
-def handle_ingest_pipeline_should_run_in_dag_check(
-    should_run_ingest_pipeline: bool,
-) -> bool:
-    """Returns True if the DAG should continue, otherwise short circuits."""
-    if not isinstance(should_run_ingest_pipeline, bool):
-        raise ValueError(
-            f"Expected should_run_ingest_pipeline value to be of type bool, found type "
-            f"[{type(should_run_ingest_pipeline)}]: {should_run_ingest_pipeline}"
+def check_region_has_launchable_ingest_views(state_code: StateCode) -> bool:
+    """Returns True if the state has launchable ingest views, otherwise short circuits."""
+    if not _has_launchable_ingest_views(state_code):
+        logging.info(
+            "No launchable views found for [%s] - returning False",
+            state_code.value,
         )
-
-    if not should_run_ingest_pipeline:
-        logging.info("should_run_ingest_pipeline did not return true, do not continue.")
         return False
+
+    logging.info(
+        "State [%s] has launchable ingest views, therefore the ingest pipeline is eligible to run - returning True",
+        state_code.value,
+    )
     return True
 
 
@@ -140,15 +158,13 @@ def _initialize_ingest_pipeline(
 ) -> Tuple[TaskGroup, CloudSqlQueryOperator]:
     """
     Initializes the dataflow pipeline by getting the max update datetimes and watermarks
-    and checking if the pipeline should run. Returns the task group and the
+    and checking if the state has launachable views. Returns the task group and the
     get_max_update_datetimes task for use in downstream tasks.
     """
 
     with TaskGroup("initialize_ingest_pipeline") as initialize_ingest_pipeline:
         check_ingest_pipeline_should_run_in_dag = (
-            handle_ingest_pipeline_should_run_in_dag_check(
-                _ingest_pipeline_should_run_in_dag(state_code).output
-            )
+            check_region_has_launchable_ingest_views(state_code)
         )
 
         get_max_update_datetimes = CloudSqlQueryOperator(
