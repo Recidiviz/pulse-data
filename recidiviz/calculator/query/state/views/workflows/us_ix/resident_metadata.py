f99f165e8e703aae0eb9174acce86a67f379a56f
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2024-11-21 11:11:42 -0800

    [US_IX][Workflows] Updates CRC/XCRC facility sorting (Recidiviz/recidiviz-data#35361)
    ## Description of the change
    
    Residents should be sorted into the crc facility they are eligible for,
    or for xcrc the crc facility they are currently in.
    This PR:
    
    1. Updates CRC work release request eligibility so that XCRC/CRC are
    mutually exclusive opportunities.
    2. Updates `resident_metadata` so that residents eligible for XCRC show
    up under the facility they are currently at
    3. Updates `resident_metadata` so that residents only show up at
    facilities relevant for their gender
    
    **Eligibility changes**
    24 fewer clients are eligible for CRC work release, and all of these
    residents are in PWCC Unit 1.
    <img width="1051" alt="image"
    src="https://github.com/user-attachments/assets/1c0572ec-92a3-4f46-86ea-1514539af512">
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#35326
    Closes Recidiviz/recidiviz-data#35325
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: f54d054b7bdda5a75f803a27d21162f2604c96c5

--- recidiviz/calculator/query/state/views/workflows/us_ix/resident_metadata.py
+++ recidiviz/calculator/query/state/views/workflows/us_ix/resident_metadata.py
@@ -27,6 +27,9 @@ from recidiviz.calculator.query.state.dataset_config import (
 from recidiviz.common.constants.states import StateCode
 from recidiviz.ingest.direct.dataset_config import raw_latest_views_dataset_for_region
 from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.task_eligibility.dataset_config import (
+    task_eligibility_spans_state_specific_dataset,
+)
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
@@ -38,11 +41,36 @@ Idaho resident metadata
 
 
 US_IX_RESIDENT_METADATA_VIEW_QUERY_TEMPLATE = f"""
-WITH release_district AS (
+WITH xcrc_facility AS (
+/* identify residents eligible for xcrc so they appear sorted under their current facility */
+    SELECT
+        person_id,
+        ARRAY_AGG(
+              UPPER(JSON_EXTRACT_SCALAR(JSON_EXTRACT(reason_struct, '$.reason'), '$.facility_name'))
+              ORDER BY 
+              UPPER(JSON_EXTRACT_SCALAR(JSON_EXTRACT(reason_struct, '$.reason'), '$.facility_name'))
+            ) AS crc_facilities
+    FROM `{{project_id}}.{{task_eligibility_dataset}}.transfer_to_xcrc_request_materialized`xc, 
+    UNNEST(JSON_EXTRACT_ARRAY(reasons)) AS reason_struct
+    WHERE {today_between_start_date_and_nullable_end_date_exclusive_clause(
+            start_date_column="start_date",
+            end_date_column="end_date"
+        )}
+        AND JSON_EXTRACT_SCALAR(reason_struct, '$.criteria_name') = 'US_IX_IN_CRC_FACILITY_OR_PWCC_UNIT_1'
+        AND (is_eligible OR is_almost_eligible)
+    GROUP BY 1
+),
+release_district AS (
+/* join all residents NOT eligible for xcrc, and pull in their gender and release district */
     SELECT
         cs.person_id,
+        facility,
+        --proofing for if we get a value for gender that is not `MALE` or `FEMALE`. If we see `EXTERNAL_UNKNOWN` assume `MALE`
+        IF(sp.gender LIKE '%FEMALE', 'FEMALE', 'MALE') AS gender,
         COALESCE(JSON_VALUE(ref.location_metadata, '$.supervision_district_name'), 'UNKNOWN') AS release_district
     FROM `{{project_id}}.sessions.compartment_sessions_materialized` cs
+    LEFT JOIN `{{project_id}}.normalized_state.state_person` sp
+        ON cs.person_id = sp.person_id 
     LEFT JOIN `{{project_id}}.normalized_state.state_incarceration_period` ip
         ON cs.person_id = ip.person_id
         AND ip.release_date IS NULL
@@ -50,6 +78,8 @@ WITH release_district AS (
         ON ip.county_code = REPLACE(ref.location_external_id, 'ATLAS-', '')
         AND ref.state_code = 'US_IX'
         AND ref.location_type = 'CITY_COUNTY'
+    LEFT JOIN xcrc_facility xc
+        ON xc.person_id = cs.person_id
     WHERE 
         {today_between_start_date_and_nullable_end_date_exclusive_clause(
             start_date_column="cs.start_date",
@@ -57,18 +87,41 @@ WITH release_district AS (
         )}
         AND cs.compartment_level_1 = 'INCARCERATION'
         AND cs.state_code = "US_IX"
+        --exclude residents eligible for xcrc
+        AND xc.person_id IS NULL
     --get the latest incarceration period per resident if there are multiple open periods 
     QUALIFY ROW_NUMBER() OVER(PARTITION BY cs.person_id ORDER BY ip.admission_date DESC)=1
 ),
 crc_facility AS (
     SELECT 
-        rd.person_id,
-        ARRAY_AGG(r.CRC_FACILITY ORDER BY r.CRC_FACILITY) AS crc_facilities
-    FROM release_district rd
-    LEFT JOIN `{{project_id}}.{{raw_data_up_to_date_views_dataset}}.RECIDIVIZ_REFERENCE_release_to_crc_facility_mappings_latest` r
-        ON rd.RELEASE_DISTRICT = r.RELEASE_DISTRICT
-    WHERE r.CRC_FACILITY IS NOT NULL
-    GROUP BY 1
+        person_id,
+        ARRAY_AGG(DISTINCT crc_facility ORDER BY crc_facility) AS crc_facilities
+    FROM (
+        -- join all facilities relevant per release district
+        SELECT 
+            rd.person_id,
+            r.CRC_FACILITY AS crc_facility
+        FROM release_district rd
+        LEFT JOIN `{{project_id}}.{{raw_data_up_to_date_views_dataset}}.RECIDIVIZ_REFERENCE_release_to_crc_facility_mappings_latest` r
+            ON rd.RELEASE_DISTRICT = r.RELEASE_DISTRICT
+            AND rd.gender = r.gender
+        WHERE r.CRC_FACILITY IS NOT NULL
+    
+        UNION ALL
+        
+        -- for residents at PWCC, add PWCC as a potential CRC option since there is one crc unit at this facility
+        SELECT 
+            rd.person_id,
+            "POCATELLO WOMEN'S CORRECTIONAL CENTER" AS crc_facility
+        FROM release_district rd
+        WHERE rd.facility = "POCATELLO WOMEN'S CORRECTIONAL CENTER"
+    ) combined
+    GROUP BY person_id
+    
+    UNION ALL 
+        
+        SELECT *
+        FROM xcrc_facility
 )
 SELECT 
     c.person_id,
@@ -90,6 +143,9 @@ US_IX_RESIDENT_METADATA_VIEW_BUILDER = SimpleBigQueryViewBuilder(
     raw_data_up_to_date_views_dataset=raw_latest_views_dataset_for_region(
         state_code=StateCode.US_IX,
         instance=DirectIngestInstance.PRIMARY,
+    ),
+    task_eligibility_dataset=task_eligibility_spans_state_specific_dataset(
+        StateCode.US_IX
     ),
     view_id=US_IX_RESIDENT_METADATA_VIEW_NAME,
     view_query_template=US_IX_RESIDENT_METADATA_VIEW_QUERY_TEMPLATE,
