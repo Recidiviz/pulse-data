d44b79a8635a86ea01395a49d26ee792b18ac911
Nichelle <nichelle.a.hall@gmail.com>
2024-11-21 17:26:17 -0500

    [Justice Counts] [2/2] Save and Surface GEOIDS for Agency Jurisdictions (Recidiviz/recidiviz-data#35300)
    This PR updates the system to save geoids to the database when received
    in requests from the frontend (FE). These geoids are also surfaced back
    to the FE. Previously, the system relied on FIPS codes, but we've
    transitioned to geoids as they support finer-grained geographic data.
    [Frontend
    PR](https://github.com/Recidiviz/justice-counts/compare/nichelle-hall/update-json-goid?expand=1)
    
    Local Testing:
    
    https://github.com/user-attachments/assets/5d7a05a5-dce6-4c3b-b763-7566ed34f8b1
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6c116a8d8bd2d5e02a1800207e2f81f1833445b6

--- recidiviz/justice_counts/agency_jurisdictions.py
+++ recidiviz/justice_counts/agency_jurisdictions.py
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """Interface for working with the AgencyJurisdiction model."""
-from typing import Dict, Hashable, List, Optional
+from typing import Dict, Hashable, List
 
 import pandas as pd
 from sqlalchemy import delete
@@ -33,8 +33,8 @@ class AgencyJurisdictionInterface:
     def create_or_update_agency_jurisdictions(
         session: Session,
         agency_id: int,
-        included_jurisdiction_ids: List[str],
-        excluded_jurisdiction_ids: List[str],
+        included_geoids: List[str],
+        excluded_geoids: List[str],
     ) -> None:
         """For the specified agency, deletes all rows in the AgencyJurisdiction table.
         Then, inserts the updated rows in the AgencyJurisdiction table.
@@ -46,19 +46,19 @@ class AgencyJurisdictionInterface:
         session.execute(delete_statement)
 
         # now, insert new included rows
-        for jurisdiction_id in included_jurisdiction_ids:
+        for geoid in included_geoids:
             insert_statement = insert(schema.AgencyJurisdiction).values(
                 source_id=agency_id,
                 membership=schema.AgencyJurisdictionType.INCLUDE.value,
-                jurisdiction_id=jurisdiction_id,
+                geoid=geoid,
             )
             session.execute(insert_statement)
         # now, insert new excluded rows
-        for jurisdiction_id in excluded_jurisdiction_ids:
+        for geoid in excluded_geoids:
             insert_statement = insert(schema.AgencyJurisdiction).values(
                 source_id=agency_id,
                 membership=schema.AgencyJurisdictionType.EXCLUDE.value,
-                jurisdiction_id=jurisdiction_id,
+                geoid=geoid,
             )
             session.execute(insert_statement)
 
@@ -66,31 +66,19 @@ class AgencyJurisdictionInterface:
     def to_json(
         session: Session,
         agency_id: int,
-        is_v2: bool = False,
-        fips_code_to_geoid: Optional[Dict[str, str]] = None,
     ) -> Dict:
         all_agency_jurisdictions = (
             session.query(schema.AgencyJurisdiction).filter(
                 schema.AgencyJurisdiction.source_id == agency_id
             )
         ).all()
-
         included_ids = []
         excluded_ids = []
         for jurisdiction in all_agency_jurisdictions:
             if jurisdiction.membership == "INCLUDE":
-                included_ids.append(jurisdiction.jurisdiction_id)
+                included_ids.append(jurisdiction.geoid)
             else:
-                excluded_ids.append(jurisdiction.jurisdiction_id)
-
-        if is_v2 is True and fips_code_to_geoid is not None:
-            included_ids = [
-                fips_code_to_geoid.get(id.rstrip("0")) for id in included_ids
-            ]
-            excluded_ids = [
-                fips_code_to_geoid.get(id.rstrip("0")) for id in excluded_ids
-            ]
-
+                excluded_ids.append(jurisdiction.geoid)
         return {
             "included": included_ids,
             "excluded": excluded_ids,
