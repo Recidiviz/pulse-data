eb292b31e0461c0f245f12b88bcecdfd33d03796
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2024-11-22 11:46:46 -0800

    [US_IX][US_TN][US_MI] Add US_IX and US_MI to incarceration_incidents_preprocessed (Recidiviz/recidiviz-data#35434)
    ## Description of the change
    
    Adds `US_IX` and `US_MI` to `incarceration_incidents_preprocessed`
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 4659df1d6ed203b9a460ac88d8fd2c924a63faed

--- recidiviz/calculator/query/state/views/analyst_data/incarceration_incidents_preprocessed.py
+++ recidiviz/calculator/query/state/views/analyst_data/incarceration_incidents_preprocessed.py
@@ -18,6 +18,7 @@
 
 from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
 from recidiviz.calculator.query.state.dataset_config import ANALYST_VIEWS_DATASET
+from recidiviz.ingest.views.dataset_config import NORMALIZED_STATE_DATASET
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
@@ -27,14 +28,61 @@ INCARCERATION_INCIDENTS_PREPROCESSED_VIEW_DESCRIPTION = (
     """Materialized view for incarceration_incidents built on ingested entities"""
 )
 INCARCERATION_INCIDENTS_PREPROCESSED_QUERY_TEMPLATE = """
-    SELECT *
-    FROM `{project_id}.{analyst_dataset}.us_tn_incarceration_incidents_preprocessed` 
+    SELECT 
+    #TODO(#35459) Clean up incarceration incidents preprocessed
+        NULL AS external_id, 
+        p.state_code, 
+        p.incident_type, 
+        p.incident_type_raw_text,
+        NULL AS incident_severity, 
+        NULL AS incident_severity_raw_text, 
+        p.incident_date, 
+        NULL AS facility, 
+        NULL AS location_within_facility, 
+        p.incident_details, 
+        NULL AS incident_metadata, 
+        NULL AS incarceration_incident_id, 
+        p.person_id,
+        p.incident_class,
+        p.injury_level,
+        p.disposition,
+        p.infraction_type_raw_text,
+        p.hearing_date,
+        p.assault_score
+    FROM `{project_id}.{analyst_dataset}.us_tn_incarceration_incidents_preprocessed` p
+
+    UNION ALL 
+    
+    SELECT 
+        inc.*,
+        NULL AS incident_class,
+        NULL AS injury_level,
+        NULL AS disposition,
+        NULL AS infraction_type_raw_text,
+        NULL AS hearing_date,
+        NULL AS assault_score,
+    FROM `{project_id}.{normalized_state_dataset}.state_incarceration_incident` inc
+    WHERE state_code = 'US_MI'
+    
+    UNION ALL 
+    
+    SELECT 
+        inc.*,
+        SUBSTR(JSON_EXTRACT_SCALAR(incident_metadata, '$.OffenseCode'), 1, 1)AS  incident_class,
+        NULL AS injury_level,
+        NULL AS disposition,
+        NULL AS infraction_type_raw_text,
+        NULL AS hearing_date,
+        NULL AS assault_score,
+    FROM `{project_id}.{normalized_state_dataset}.state_incarceration_incident` inc
+    WHERE state_code = 'US_IX'
         
 """
 
 INCARCERATION_INCIDENTS_PREPROCESSED_VIEW_BUILDER = SimpleBigQueryViewBuilder(
     dataset_id=ANALYST_VIEWS_DATASET,
     analyst_dataset=ANALYST_VIEWS_DATASET,
+    normalized_state_dataset=NORMALIZED_STATE_DATASET,
     view_id=INCARCERATION_INCIDENTS_PREPROCESSED_VIEW_NAME,
     description=INCARCERATION_INCIDENTS_PREPROCESSED_VIEW_DESCRIPTION,
     view_query_template=INCARCERATION_INCIDENTS_PREPROCESSED_QUERY_TEMPLATE,
