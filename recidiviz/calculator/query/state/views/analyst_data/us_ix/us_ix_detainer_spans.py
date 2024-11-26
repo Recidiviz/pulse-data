8ef089b04eb36c870960b12df0ee357bd200f962
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2024-11-26 13:25:06 -0800

    [US_IX][Workflows] Update detainer_spans to include historical detainers (Recidiviz/recidiviz-data#35559)
    ## Description of the change
    
    For use in ID CLD and CRC/xCRC workflows, this PR updates
    `us_ix_detainer_spans` to include all current and historical active
    spans of detainers
    
    **No current eligibility changes**
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: bcacaaf4cba23872efd8015928b4cf87ff852b65

--- recidiviz/calculator/query/state/views/analyst_data/us_ix/us_ix_detainer_spans.py
+++ recidiviz/calculator/query/state/views/analyst_data/us_ix/us_ix_detainer_spans.py
@@ -1,5 +1,5 @@
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2023 Recidiviz, Inc.
+# Copyright (C) 2024 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -34,36 +34,49 @@ IX. This will be used to create criteria queries for the CRC and XCRC workflows.
 spans, just a present view of detainers """
 
 US_IX_DETAINER_SPANS_QUERY_TEMPLATE = """
+WITH all_detainers AS (
+/* This CTE queries all historical and current detainers, and orders them based on their UpdateDate */
     SELECT
-      pei.person_id,
-      pei.state_code,
-      SAFE_CAST(LEFT(IssuedDate, 10) AS DATE) AS start_date,
-      SAFE_CAST(LEFT(NULL, 10) AS DATE) AS end_date,
-      det_type.DetainerTypeDesc,
-      det_type.DetainerTypeId,
-      det_status.DetainerStatusDesc,
-      det.CriminalChargesDesc,
-      det.Comments,
-      det.AgencyContactName,
-      det_status.Abbreviation,
-    FROM
-      `{project_id}.{us_ix_raw_data_up_to_date_dataset}.scl_Detainer_latest` det
-    LEFT JOIN
-      `{project_id}.{normalized_state_dataset}.state_person_external_id` pei
-    ON
-      pei.external_id = OffenderId
-    LEFT JOIN
-      `{project_id}.{us_ix_raw_data_up_to_date_dataset}.scl_DetainerType_latest` det_type
-    ON
-      det.DetainerTypeId = det_type.DetainerTypeId
-    LEFT JOIN
-      `{project_id}.{us_ix_raw_data_up_to_date_dataset}.scl_DetainerStatus_latest` det_status
-    ON
-      det.DetainerStatusId = det_status.DetainerStatusId
-    WHERE
-      pei.state_code = 'US_IX'
-      AND pei.id_type = 'US_IX_DOC'
-      AND DetainerStatusDesc != "Closed" 
+        *, 
+        ROW_NUMBER() OVER(PARTITION BY OffenderId,DetainerId ORDER BY UpdateDate) as update_seq_no
+      FROM (
+        SELECT DISTINCT
+          person_id,
+          state_code,
+          OffenderId, 
+          DetainerId,
+          DATE(IssuedDate) as IssuedDate, 
+          DATE(d.UpdateDate) as UpdateDate,
+          CASE WHEN DetainerStatusDesc in ("Closed", "Cancelled", "Deceased") THEN 0 ELSE 1 END AS active_status,
+          DetainerTypeId,
+          DetainerTypeDesc,
+          CriminalChargesDesc,
+          Comments,
+          AgencyContactName,
+          DetainerStatusDesc,
+          ds.Abbreviation
+        FROM `{project_id}.us_ix_raw_data.scl_Detainer` d
+        LEFT JOIN `{project_id}.{us_ix_raw_data_up_to_date_dataset}.scl_DetainerStatus_latest` ds 
+            USING(DetainerStatusId)
+        LEFT JOIN `{project_id}.{us_ix_raw_data_up_to_date_dataset}.scl_DetainerType_latest` dt 
+            USING(DetainerTypeId)
+        LEFT JOIN `{project_id}.{normalized_state_dataset}.state_person_external_id` pei
+            ON pei.external_id = OffenderId
+            AND pei.state_code = 'US_IX'
+            AND pei.id_type = 'US_IX_DOC'
+      )
+),
+detainer_spans AS (
+/* This CTE creates spans of time when a detainer had certain attributes */
+    SELECT * EXCEPT(update_seq_no, IssuedDate, UpdateDate),
+      CASE WHEN update_seq_no = 1 THEN LEAST(IssuedDate, UpdateDate) ELSE UpdateDate END AS start_date,
+      LEAD(UpdateDate) OVER(PARTITION BY OffenderId,DetainerId ORDER BY UpdateDate) AS end_date
+    FROM all_detainers
+)
+SELECT *
+FROM detainer_spans
+--for the final view, only query detainers that are active during that span of time
+WHERE active_status = 1 
 """
 
 US_IX_DETAINER_SPANS_VIEW_BUILDER = SimpleBigQueryViewBuilder(
