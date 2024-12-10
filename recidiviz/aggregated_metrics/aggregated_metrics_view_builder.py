1c30b50e28cb63b6d7f33d263754bf039924f36c
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2024-12-10 10:09:49 -0800

    [Aggregated Metrics] Add AggregatedMetricsBigQueryViewBuilder (Recidiviz/recidiviz-data#35871)
    ## Description of the change
    
    Adds a view builder object which builds an view that calculates a
    collection aggregated metrics for the given population, unit of
    analysis, time period and metric class.
    
    Subsequent PRs will actually collect all relevant builders for
    PeriodEventAggregatedMetric type metrics and add them to our deployed
    views list.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#29291
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: a4299c8a4dc00a917cdd8c90662148a3f39ed90b

---
+++ recidiviz/aggregated_metrics/aggregated_metrics_view_builder.py
@@ -0,0 +1,250 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""View builder for a view that calculates metrics for the provided metric
+configurations and the specified |population_type|, |unit_of_analysis_type| and
+|time_period|.
+"""
+
+from more_itertools import one
+from pytablewriter import MarkdownTableWriter
+
+from recidiviz.aggregated_metrics.metric_time_period_config import (
+    MetricTimePeriodConfig,
+)
+from recidiviz.aggregated_metrics.models.aggregated_metric import (
+    AggregatedMetric,
+    AssignmentEventAggregatedMetric,
+    AssignmentSpanAggregatedMetric,
+    PeriodEventAggregatedMetric,
+    PeriodSpanAggregatedMetric,
+)
+from recidiviz.aggregated_metrics.models.metric_population_type import (
+    MetricPopulationType,
+)
+from recidiviz.aggregated_metrics.models.metric_unit_of_analysis_type import (
+    MetricUnitOfAnalysis,
+    MetricUnitOfAnalysisType,
+)
+from recidiviz.aggregated_metrics.query_building.aggregated_metric_query_utils import (
+    AggregatedMetricClassType,
+)
+from recidiviz.aggregated_metrics.query_building.build_aggregated_metric_query import (
+    build_aggregated_metric_query_template,
+)
+from recidiviz.big_query.big_query_view import BigQueryView, BigQueryViewBuilder
+from recidiviz.big_query.big_query_view_sandbox_context import (
+    BigQueryViewSandboxContext,
+)
+from recidiviz.utils.string_formatting import fix_indent
+from recidiviz.utils.types import assert_type
+
+
+def _metrics_description_table(metrics: list[AggregatedMetric]) -> str:
+    """Returns a formatted table with the name, output column name, and description of
+    each metric in the provided list.
+    """
+    table_data = []
+
+    observation_type_category: str = one(
+        {metric.observation_type.observation_type_category() for metric in metrics}
+    )
+    for metric in metrics:
+        table_data.append(
+            (
+                metric.display_name,
+                metric.name,
+                metric.description,
+                metric.observation_type.value,
+            )
+        )
+
+    table_writer = MarkdownTableWriter(
+        headers=[
+            "Name",
+            "Column",
+            "Description",
+            f"{observation_type_category.capitalize()} observation type",
+        ],
+        value_matrix=[row for row in table_data if row is not None],
+        # Margin values other than 0 have nondeterministic spacing. Do not change.
+        margin=0,
+    )
+
+    return table_writer.dumps()
+
+
+def aggregated_metric_view_description(
+    population_type: MetricPopulationType,
+    unit_of_analysis_type: MetricUnitOfAnalysisType,
+    metric_class: AggregatedMetricClassType,
+    time_period: MetricTimePeriodConfig,
+    metrics: list[AggregatedMetric] | None,
+) -> str:
+    """Builds a docstring for an aggregated metrics view with the given parameters."""
+    max_end_date_str = (
+        time_period.max_period_end_date.isoformat()
+        if time_period.max_period_end_date
+        else "<today's date>"
+    )
+    time_period_clause = f"""Contains metrics only for {time_period.period_name_type.value}-length time periods with
+the most recent period ending on {max_end_date_str} and the
+least recent period ending on {time_period.min_period_end_date.isoformat()}."""
+
+    if issubclass(metric_class, PeriodEventAggregatedMetric):
+        base_description = f"""
+Metrics for the {population_type.population_name_short} population calculated using
+event observations across an entire analysis period, disaggregated by {unit_of_analysis_type.short_name}.
+
+{fix_indent(time_period_clause, indent_level=0)}
+
+All end_dates are exclusive, i.e. the metric is for the range [start_date, end_date).
+"""
+    elif issubclass(metric_class, PeriodSpanAggregatedMetric):
+        raise NotImplementedError(
+            "TODO(#29291): Implement description for PeriodSpanAggregatedMetric"
+        )
+    elif issubclass(metric_class, AssignmentEventAggregatedMetric):
+        raise NotImplementedError(
+            "TODO(#29291): Implement description for AssignmentEventAggregatedMetric"
+        )
+    elif issubclass(metric_class, AssignmentSpanAggregatedMetric):
+        raise NotImplementedError(
+            "TODO(#29291): Implement description for AssignmentSpanAggregatedMetric"
+        )
+    else:
+        raise ValueError(f"Unexpected metric class type: [{metric_class}]")
+
+    if not metrics:
+        return base_description
+
+    return f"""{base_description}
+# Metrics
+{_metrics_description_table(metrics)}"""
+
+
+class AggregatedMetricsBigQueryViewBuilder(BigQueryViewBuilder[BigQueryView]):
+    """View builder for a view that calculates metrics for the provided metric
+    configurations and the specified |population_type|, |unit_of_analysis_type| and
+    |time_period|.
+    """
+
+    def __init__(
+        self,
+        *,
+        dataset_id: str,
+        population_type: MetricPopulationType,
+        unit_of_analysis_type: MetricUnitOfAnalysisType,
+        metric_class: AggregatedMetricClassType,
+        metrics: list[AggregatedMetric],
+        time_period: MetricTimePeriodConfig,
+    ) -> None:
+        self.dataset_id = dataset_id
+        self.view_id = self._build_view_id(
+            population_type=population_type,
+            unit_of_analysis_type=unit_of_analysis_type,
+            metric_class=metric_class,
+            time_period=time_period,
+        )
+        self.population_type = population_type
+        self.unit_of_analysis_type = unit_of_analysis_type
+        self.metric_class = metric_class
+        self.metrics = metrics
+        self.time_period = time_period
+
+        self.view_query_template = build_aggregated_metric_query_template(
+            population_type=population_type,
+            unit_of_analysis_type=unit_of_analysis_type,
+            metric_class=metric_class,
+            metrics=metrics,
+            time_period=time_period,
+        )
+        self.projects_to_deploy = None
+        self.materialized_address = self._build_materialized_address(
+            dataset_id=self.dataset_id,
+            view_id=self.view_id,
+            materialized_address_override=None,
+            should_materialize=True,
+        )
+
+    @staticmethod
+    def _build_view_id(
+        population_type: MetricPopulationType,
+        unit_of_analysis_type: MetricUnitOfAnalysisType,
+        metric_class: AggregatedMetricClassType,
+        time_period: MetricTimePeriodConfig,
+    ) -> str:
+        population_name = population_type.population_name_short
+        unit_of_analysis_name = unit_of_analysis_type.short_name
+        metric_class_name = metric_class.metric_class_name_lower()
+        return f"{population_name}_{unit_of_analysis_name}_{metric_class_name}_aggregated_metrics__{assert_type(time_period.config_name, str)}"
+
+    @property
+    def output_columns(self) -> list[str]:
+        return [
+            *self.unit_of_analysis.primary_key_columns,
+            "start_date",
+            "end_date",
+            f"{MetricTimePeriodConfig.METRIC_TIME_PERIOD_PERIOD_COLUMN}",
+            *sorted(metric.name for metric in self.metrics),
+        ]
+
+    @property
+    def description(self) -> str:
+        return aggregated_metric_view_description(
+            population_type=self.population_type,
+            unit_of_analysis_type=self.unit_of_analysis_type,
+            time_period=self.time_period,
+            metric_class=self.metric_class,
+            metrics=self.metrics,
+        )
+
+    @property
+    def bq_description(self) -> str:
+        return aggregated_metric_view_description(
+            population_type=self.population_type,
+            unit_of_analysis_type=self.unit_of_analysis_type,
+            time_period=self.time_period,
+            metric_class=self.metric_class,
+            # Don't list metrics to keep the description short
+            metrics=None,
+        )
+
+    @property
+    def unit_of_analysis(self) -> MetricUnitOfAnalysis:
+        return MetricUnitOfAnalysis.for_type(self.unit_of_analysis_type)
+
+    @property
+    def clustering_fields(self) -> list[str]:
+        """Cluster the output on the unit of analysis primary keys, which are the most
+        likely to be joined against in subsequent queries.
+        """
+        return self.unit_of_analysis.primary_key_columns
+
+    def _build(
+        self, *, sandbox_context: BigQueryViewSandboxContext | None
+    ) -> BigQueryView:
+        return BigQueryView(
+            dataset_id=self.dataset_id,
+            view_id=self.view_id,
+            description=self.description,
+            bq_description=self.bq_description,
+            view_query_template=self.view_query_template,
+            materialized_address=self.materialized_address,
+            clustering_fields=self.clustering_fields,
+            sandbox_context=sandbox_context,
+            should_deploy_predicate=None,
+        )
---
+++ recidiviz/tests/aggregated_metrics/aggregated_metrics_view_builder_test.py
@@ -0,0 +1,154 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for aggregated_metrics_view_builder.py"""
+import unittest
+
+from recidiviz.aggregated_metrics.aggregated_metrics_view_builder import (
+    AggregatedMetricsBigQueryViewBuilder,
+    aggregated_metric_view_description,
+)
+from recidiviz.aggregated_metrics.metric_time_period_config import (
+    MetricTimePeriodConfig,
+)
+from recidiviz.aggregated_metrics.models.aggregated_metric import (
+    EventCountMetric,
+    PeriodEventAggregatedMetric,
+)
+from recidiviz.aggregated_metrics.models.metric_population_type import (
+    MetricPopulationType,
+)
+from recidiviz.aggregated_metrics.models.metric_unit_of_analysis_type import (
+    MetricUnitOfAnalysisType,
+)
+from recidiviz.big_query.big_query_address import BigQueryAddress
+from recidiviz.observations.event_selector import EventSelector
+from recidiviz.observations.event_type import EventType
+from recidiviz.utils.types import assert_type
+
+MY_DRUG_SCREENS_METRIC = EventCountMetric(
+    name="my_drug_screens",
+    display_name="My Drug Screens",
+    description="Number of my drug screens",
+    event_selector=EventSelector(
+        event_type=EventType.DRUG_SCREEN,
+        event_conditions_dict={},
+    ),
+)
+
+MY_CONTACTS_COMPLETED_METRIC = EventCountMetric(
+    name="my_contacts_completed",
+    display_name="Contacts: Completed",
+    description="Number of completed contacts",
+    event_selector=EventSelector(
+        event_type=EventType.SUPERVISION_CONTACT,
+        event_conditions_dict={"status": ["COMPLETED"]},
+    ),
+)
+
+MY_LOGINS_BY_PRIMARY_WORKFLOWS = EventCountMetric(
+    name="my_logins_primary_workflows_user",
+    display_name="My Logins, Primary Workflows Users",
+    description="Number of logins performed by primary Workflows users",
+    event_selector=EventSelector(
+        event_type=EventType.WORKFLOWS_USER_LOGIN,
+        event_conditions_dict={},
+    ),
+)
+
+
+class TestAggregatedMetricViewDescription(unittest.TestCase):
+    """Tests for aggregated_metric_view_description()"""
+
+    def test_aggregated_metric_view_description__period_event(self) -> None:
+        docstring = aggregated_metric_view_description(
+            population_type=MetricPopulationType.SUPERVISION,
+            unit_of_analysis_type=MetricUnitOfAnalysisType.SUPERVISION_OFFICER,
+            metric_class=PeriodEventAggregatedMetric,
+            metrics=[
+                MY_DRUG_SCREENS_METRIC,
+                MY_CONTACTS_COMPLETED_METRIC,
+                MY_LOGINS_BY_PRIMARY_WORKFLOWS,
+            ],
+            time_period=MetricTimePeriodConfig.month_periods(lookback_months=12),
+        )
+
+        expected_docstring = """
+Metrics for the supervision population calculated using
+event observations across an entire analysis period, disaggregated by officer.
+
+Contains metrics only for MONTH-length time periods with
+the most recent period ending on 2024-12-01 and the
+least recent period ending on 2024-01-01.
+
+All end_dates are exclusive, i.e. the metric is for the range [start_date, end_date).
+
+# Metrics
+|               Name               |             Column             |                     Description                     |Event observation type|
+|----------------------------------|--------------------------------|-----------------------------------------------------|----------------------|
+|My Drug Screens                   |my_drug_screens                 |Number of my drug screens                            |DRUG_SCREEN           |
+|Contacts: Completed               |my_contacts_completed           |Number of completed contacts                         |SUPERVISION_CONTACT   |
+|My Logins, Primary Workflows Users|my_logins_primary_workflows_user|Number of logins performed by primary Workflows users|WORKFLOWS_USER_LOGIN  |
+"""
+        self.assertEqual(expected_docstring, docstring)
+
+    # TODO(#29291): Add tests for PeriodSpanAggregatedMetric
+    # TODO(#29291): Add tests for AssignmentEventAggregatedMetric
+    # TODO(#29291): Add tests for AssignmentSpanAggregatedMetric
+
+
+class TestAggregatedMetricsBigQueryViewBuilder(unittest.TestCase):
+    """Tests for AggregatedMetricsBigQueryViewBuilder"""
+
+    def test_builder__period_event(self) -> None:
+        builder = AggregatedMetricsBigQueryViewBuilder(
+            dataset_id="my_aggregated_metrics",
+            population_type=MetricPopulationType.SUPERVISION,
+            unit_of_analysis_type=MetricUnitOfAnalysisType.SUPERVISION_OFFICER,
+            metric_class=PeriodEventAggregatedMetric,
+            metrics=[
+                MY_DRUG_SCREENS_METRIC,
+                MY_CONTACTS_COMPLETED_METRIC,
+                MY_LOGINS_BY_PRIMARY_WORKFLOWS,
+            ],
+            time_period=MetricTimePeriodConfig.month_periods(lookback_months=12),
+        )
+
+        self.assertEqual(
+            "my_aggregated_metrics.supervision_officer_period_event_aggregated_metrics__last_12_months",
+            builder.address.to_str(),
+        )
+        self.assertEqual(
+            "my_aggregated_metrics.supervision_officer_period_event_aggregated_metrics__last_12_months_materialized",
+            assert_type(builder.materialized_address, BigQueryAddress).to_str(),
+        )
+
+        self.assertTrue(MY_DRUG_SCREENS_METRIC.name in builder.description)
+        self.assertFalse(MY_DRUG_SCREENS_METRIC.name in builder.bq_description)
+
+        self.assertEqual(
+            [
+                "state_code",
+                "officer_id",
+                "start_date",
+                "end_date",
+                "period",
+                "my_contacts_completed",
+                "my_drug_screens",
+                "my_logins_primary_workflows_user",
+            ],
+            builder.output_columns,
+        )
