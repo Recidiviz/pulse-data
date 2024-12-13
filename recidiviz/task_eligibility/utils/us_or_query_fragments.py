bda7d251f8ffde30678ba8e007d04fcdf89d843b
Maggie Hilderbran <maggie.hilderbran@me.com>
2024-12-13 13:05:18 -0500

    [US_OR] EDIS - use updated formatting for statutes (Recidiviz/recidiviz-data#35965)
    ## Description of the change
    
    Updated the statute lists used for the EDIS eligibility code to
    reference the reformatted versions of the statutes. This will keep the
    eligibility code from breaking due to reformatting of the statutes in
    ingest (which will be done by [this PR from
    Lydia](https://github.com/Recidiviz/recidiviz-data/pull/36051)).
    
    To validate the changes, I loaded views to sandbox using the following
    command, which allowed me to use Lydia's sandbox (which already reflects
    the statute reformatting) as the source:
    ```
    python -m recidiviz.tools.load_views_to_sandbox \
    --sandbox_dataset_prefix mhilderbran_statutes \
    --input_source_table_dataset_overrides_json "{\"us_or_normalized_state\": \"lydiamasri2_us_or_normalized_state\"}" \
    --state_code_filter US_OR \
    --prompt auto \
    --load_up_to_addresses analyst_data.us_or_earned_discharge_sentence_eligibility_spans
    ```
    
    For the sentence-level criterion
    `us_or_earned_discharge_sentence_eligibility_spans`, which aggregates
    the sentence-level subcriteria, we see no changes in eligibility (which
    is what we want to see, since the reformatting for statutes shouldn't
    impact eligibility at all!):
    ```
    WITH main AS (
      SELECT
        state_code,
        person_id,
        sentence_id,
        is_eligible AS is_eligible_main,
      FROM `recidiviz-staging.analyst_data.us_or_earned_discharge_sentence_eligibility_spans_materialized`
      WHERE CURRENT_DATE BETWEEN start_date AND COALESCE(DATE_SUB(end_date, INTERVAL 1 DAY), '9999-12-31')
    ),
    sandbox AS (
      SELECT
        state_code,
        person_id,
        sentence_id,
        is_eligible AS is_eligible_sandbox,
      FROM `recidiviz-staging.mhilderbran_statutes_analyst_data.us_or_earned_discharge_sentence_eligibility_spans_materialized`
      WHERE CURRENT_DATE BETWEEN start_date AND COALESCE(DATE_SUB(end_date, INTERVAL 1 DAY), '9999-12-31')
    )
    SELECT
      main.is_eligible_main,
      sandbox.is_eligible_sandbox,
      COUNT(*),
    FROM main
    FULL OUTER JOIN sandbox
      USING (state_code, person_id, sentence_id)
    WHERE (main.is_eligible_main!=sandbox.is_eligible_sandbox)
      OR (main.is_eligible_main IS NULL AND sandbox.is_eligible_sandbox IS NOT NULL)
      OR (main.is_eligible_main IS NOT NULL AND sandbox.is_eligible_sandbox IS NULL)
    GROUP BY 1, 2
    ```
    
    ## Related issues
    
    Relates to Recidiviz/recidiviz-data#35915
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 0bc259e660b6e484763d868c3c3c40dda8251508

--- recidiviz/task_eligibility/utils/us_or_query_fragments.py
+++ recidiviz/task_eligibility/utils/us_or_query_fragments.py
@@ -14,84 +14,84 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""Creates query fragments for OR."""
+"""Create query fragments for OR."""
 
 
 # ineligible statutes - both probation & post-prison
 OR_EARNED_DISCHARGE_INELIGIBLE_STATUTES = [
-    "163095",
-    # according to ODOC: 163.095 (attempted) is a Measure 11 crime that should never
-    # actually result in probation (however, leaving it as a universal exclusion here to
-    # be consistent with how the agency rule is written)
-    "163095-X",
-    "163107",
-    # according to ODOC: 163.107 (attempted) is a Measure 11 crime that should never
-    # actually result in probation (however, leaving it as a universal exclusion here to
-    # be consistent with how the agency rule is written)
-    "163107-X",
-    "163115",
-    # according to ODOC: 163.115 (attempted) is a Measure 11 crime that should never
-    # actually result in probation (however, leaving it as a universal exclusion here to
-    # be consistent with how the agency rule is written)
-    "163115-X",
-    "163118",
-    "163125",
-    "163149",
-    "163185",
-    "163225",
-    "163235",
-    "163365",
-    "163375",
-    "163395",
-    "163405",
-    "163408",
-    "163411",
-    # according to ODOC: 163.425 / 163.425 NEW are not specifically excluded by statute
+    "163.095",
+    # according to ODOC: 163.095(X) is a Measure 11 crime that should never actually
+    # result in probation (however, leaving it as a universal exclusion here to be
+    # consistent with how the agency rule is written)
+    "163.095(X)",
+    "163.107",
+    # according to ODOC: 163.107(X) is a Measure 11 crime that should never actually
+    # result in probation (however, leaving it as a universal exclusion here to be
+    # consistent with how the agency rule is written)
+    "163.107(X)",
+    "163.115",
+    # according to ODOC: 163.115(X) is a Measure 11 crime that should never actually
+    # result in probation (however, leaving it as a universal exclusion here to be
+    # consistent with how the agency rule is written)
+    "163.115(X)",
+    "163.118",
+    "163.125",
+    "163.149",
+    "163.185",
+    "163.225",
+    "163.235",
+    "163.365",
+    "163.375",
+    "163.395",
+    "163.405",
+    "163.408",
+    "163.411",
+    # according to ODOC: 163.425/163.425(NEW) are not specifically excluded by statute
     # for probation cases (only for post-prison cases, due to 144.103), but an agency
     # decision was made to exclude that crime for probation as well
-    "163425",
-    "163425-NEW",
-    "163427",
-    "163427-NEW",
-    "163670",
-    "164325",
-    "164415",
-    "167017",
+    "163.425",
+    "163.425(NEW)",
+    "163.427",
+    "163.427(NEW)",
+    "163.670",
+    "164.325",
+    "164.415",
+    "167.017",
     # the following statutes are disqualifying enhancements to crimes of conviction and
     # may or may not actually show up in the statute field
-    "161610",
-    "161725",
-    "161735",
-    "137635",
-    "137690",
-    "164061",
-    "475907",
-    "475925",
-    "475930",
+    "161.610",
+    "161.725",
+    "161.735",
+    "137.635",
+    "137.690",
+    "164.061",
+    "475.907",
+    "475.925",
+    "475.930",
     # according to ODOC: 813.010(5) is a felony DUI, and even though it's not explicitly
     # referenced by ORS number in the rule, the agency decided that they'd exclude all
     # felony DUIs to align with legislative intent (rather than having POs check each
     # judgment manually)
-    "813010-5",
-    "813011",
-    "144103",
+    "813.010(5)",
+    "813.011",
+    "144.103",
 ]
 
 # additional ineligible statutes - post-prison only
 OR_EARNED_DISCHARGE_INELIGIBLE_STATUTES_POST_PRISON = [
     # ineligible b/c these supervision terms are subject to ORS 144.103
-    "163365-X",
-    "163375-X",
-    "163395-X",
-    "163405-X",
-    "163408-X",
-    "163411-X",
-    # "163425",  # commenting out b/c already in universal exclusion list due to ODOC decision (see above comment)
-    # "163425-NEW",  # commenting out b/c already in universal exclusion list due to ODOC decision (see above comment)
-    "163425-X",
-    "163425-NEWX",
-    "163427-X",
-    "163427-NEWX",
+    "163.365(X)",
+    "163.375(X)",
+    "163.395(X)",
+    "163.405(X)",
+    "163.408(X)",
+    "163.411(X)",
+    # "163.425",  # commenting out b/c already in universal exclusion list due to ODOC decision (see above comment)
+    # "163.425(NEW)",  # commenting out b/c already in universal exclusion list due to ODOC decision (see above comment)
+    "163.425(X)",
+    "163.425(NEWX)",
+    "163.427(X)",
+    "163.427(NEWX)",
 ]
 
 # This is the list of "designated drug-related misdemeanors" (per ORS 423.478), as found
@@ -101,14 +101,14 @@ OR_EARNED_DISCHARGE_INELIGIBLE_STATUTES_POST_PRISON = [
 # eligible.
 OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2017_08_15 = [
     # new additions (based on statutes that were updated via this same bill)
-    "475752-3A",
-    "475752-3B",
-    "475824-2A",
-    "475834-2A",
-    "475854-2A",
-    "475874-2A",
-    "475884-2A",
-    "475894-2A",
+    "475.752(3A)",
+    "475.752(3B)",
+    "475.824(2A)",
+    "475.834(2A)",
+    "475.854(2A)",
+    "475.874(2A)",
+    "475.884(2A)",
+    "475.894(2A)",
 ]
 
 # This is the list of "designated drug-related misdemeanors" (per ORS 423.478), as found
@@ -119,12 +119,12 @@ OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2017_08_15 = [
 # specific offenses that this bill deemed eligible.
 OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2021_02_01 = [
     # revised subparagraphs due to changing underlying statutes
-    "475824-02C",
-    "475834-02C",
-    "475854-02C",
-    "475874-02C",
-    "475884-02C",
-    "475894-02C",
+    "475.824(02C)",
+    "475.834(02C)",
+    "475.854(02C)",
+    "475.874(02C)",
+    "475.884(02C)",
+    "475.894(02C)",
 ]
 
 # This is the list of "designated drug-related misdemeanors" (per ORS 423.478), as found
@@ -134,12 +134,12 @@ OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2021_02_01 = [
 # eligible.
 OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2021_07_19 = [
     # revised subparagraphs due to changing underlying statutes
-    "475824-02B",
-    "475834-02B",
-    "475854-02B",
-    "475874-02B",
-    "475884-02B",
-    "475894-02B",
+    "475.824(02B)",
+    "475.834(02B)",
+    "475.854(02B)",
+    "475.874(02B)",
+    "475.884(02B)",
+    "475.894(02B)",
 ]
 
 # This is the list of "designated drug-related misdemeanors" (per ORS 423.478), as found
@@ -149,7 +149,7 @@ OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2021_07_19 = [
 OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2023_07_27 = (
     [
         # new addition (with change to underlying statute)
-        "475752-8A",
+        "475.752(8A)",
     ]
     +
     # continued from existing list (without changing underlying statutes)
@@ -163,28 +163,28 @@ OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2023_07_27 = (
 OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2024_09_01 = (
     [
         # new additions (with changes to underlying statute)
-        "475752-003A",
-        "475752-003B",
-        "475752-003C",
-        "475752-003D",
+        "475.752(003A)",
+        "475.752(003B)",
+        "475.752(003C)",
+        "475.752(003D)",
         # new addition (without changing underlying statute)
-        "475752-7A",
+        "475.752(7A)",
         # new addition (with change to underlying statute)
-        "475814-002A",
+        "475.814(002A)",
         # new addition (without changing underlying statute)
-        "475814-2B",
+        "475.814(2B)",
         # new addition (with change to underlying statute)
-        "475824-002A",
+        "475.824(002A)",
         # new addition (with change to underlying statute)
-        "475834-002A",
+        "475.834(002A)",
         # new addition (with change to underlying statute)
-        "475854-002A",
+        "475.854(002A)",
         # new addition (with change to underlying statute)
-        "475874-002A",
+        "475.874(002A)",
         # new addition (with change to underlying statute)
-        "475884-002A",
+        "475.884(002A)",
         # new addition (with change to underlying statute)
-        "475894-002A",
+        "475.894(002A)",
     ]
     +
     # continued from existing list (without changing underlying statutes)
@@ -197,7 +197,7 @@ OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2024_09_01 = (
 OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2025_01_01 = (
     [
         # new addition (with change to underlying statute)
-        "166116-1E",
+        "166.116(1E)",
     ]
     +
     # continued from existing list (without changing underlying statutes)
@@ -211,9 +211,9 @@ OR_EARNED_DISCHARGE_DESIGNATED_DRUG_RELATED_MISDEMEANORS_2025_01_01 = (
 OR_EARNED_DISCHARGE_DESIGNATED_PERSON_MISDEMEANORS_2022_01_01 = [
     # TODO(#35127): Once we can identify which sentences under 163.160 and 163.190 are
     # funded and which are not, start surfacing just the funded sentences.
-    # "163160",  # only if constituting domestic violence
-    # "163190",  # only if constituting domestic violence
-    "163415-NEW",
+    # "163.160",  # only if constituting domestic violence
+    # "163.190",  # only if constituting domestic violence
+    "163.415(NEW)",
 ]
 
 # supervision types ineligible for EDIS (see OR ingest mappings to see all types)
