93580df64943caa91bee9f86b9216bfb05311663
mayukas <30325262+mayukas@users.noreply.github.com>
2024-12-16 12:38:46 -0500

    Adds login event observation join to live task types via registration sessions (Recidiviz/recidiviz-data#36131)
    ## Description of the change
    
    Adds more event attributes to the workflows user login observation
    related to task types and other associated flags by joining the
    workflows login event to the registration sessions span observation to
    get all live task types. Because this is an inner join, the observation
    now only includes login events by registered primary users (this was the
    behavior of agg metrics anyways because of the join to assignments, but
    now we're limiting the observations as well).
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#35773
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: mayukas <mayuka@stanford.edu>
    GitOrigin-RevId: e0eb1cde8a5bb738364ee487a48facdc4e1e5bf9

--- recidiviz/observations/views/events/workflows_primary_user/workflows_user_login.py
+++ recidiviz/observations/views/events/workflows_primary_user/workflows_user_login.py
@@ -16,10 +16,14 @@
 # =============================================================================
 """View with events where the workflows user logged into the dashboard
 """
+from recidiviz.calculator.query.bq_utils import nonnull_end_date_clause
 from recidiviz.observations.event_observation_big_query_view_builder import (
     EventObservationBigQueryViewBuilder,
 )
 from recidiviz.observations.event_type import EventType
+from recidiviz.observations.views.spans.workflows_primary_user.workflows_primary_user_registration_session import (
+    VIEW_BUILDER as WORKFLOWS_PRIMARY_USER_REGISTRATION_SESSION_VIEW_BUILDER,
+)
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
@@ -27,22 +31,48 @@ _VIEW_DESCRIPTION = (
     "View with events where the workflows user logged into the dashboard"
 )
 
-_QUERY_TEMPLATE = """
+_QUERY_TEMPLATE = f"""
+WITH registration_sessions AS (
+    SELECT
+        *
+    FROM
+        `{{project_id}}.{WORKFLOWS_PRIMARY_USER_REGISTRATION_SESSION_VIEW_BUILDER.table_for_query.to_str()}`
+)
+-- Join logins to all live opportunities corresponding to the system type(s)
+-- for which the user has access according to primary user registration sessions.
 SELECT
-    state_code,
-    email_address,
-    login_date,
+    logins.state_code,
+    logins.email_address,
+    logins.login_date,
+    registration_sessions.task_type,
+    registration_sessions.system_type,
+    registration_sessions.decarceral_impact_type,
+    registration_sessions.is_jii_decarceral_transition,
+    registration_sessions.task_type_is_live,
+    registration_sessions.task_type_is_fully_launched,
 FROM
-    `{project_id}.analyst_data.all_auth0_login_events_materialized`
-WHERE
-    has_workflows_access
+    `{{project_id}}.analyst_data.all_auth0_login_events_materialized` logins
+INNER JOIN
+    registration_sessions
+ON
+    logins.state_code = registration_sessions.state_code
+    AND logins.email_address = registration_sessions.email_address
+    AND logins.login_date BETWEEN registration_sessions.start_date AND {nonnull_end_date_clause("registration_sessions.end_date")}
+    AND task_type_is_live = "true"
 """
 
 VIEW_BUILDER: EventObservationBigQueryViewBuilder = EventObservationBigQueryViewBuilder(
     event_type=EventType.WORKFLOWS_USER_LOGIN,
     description=_VIEW_DESCRIPTION,
     sql_source=_QUERY_TEMPLATE,
-    attribute_cols=[],
+    attribute_cols=[
+        "task_type",
+        "system_type",
+        "decarceral_impact_type",
+        "is_jii_decarceral_transition",
+        "task_type_is_live",
+        "task_type_is_fully_launched",
+    ],
     event_date_col="login_date",
 )
 
