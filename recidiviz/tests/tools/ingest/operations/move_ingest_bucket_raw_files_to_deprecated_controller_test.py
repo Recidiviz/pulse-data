10ca7e3cb47a0295c966864bcbe51cc73aa7f0ab
Emily Turner <149205316+emilyemilyemilyemilyemilyemily@users.noreply.github.com>
2024-12-16 17:56:26 -0800

    [raw data] Add move_ingest_bucket_raw_files_to_deprecated_controller (Recidiviz/recidiviz-data#36090)
    ## Description of the change
    
    Add controller to be used by
    https://github.com/Recidiviz/recidiviz-data/pull/36089 that moves files
    in the ingest bucket to deprecated storage
    
    executes move and returns the files that succeeded and the files that
    failed
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#13378
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: b3a5c102ada4088dca772232a7e11ef60fc5e035

---
+++ recidiviz/tests/tools/ingest/operations/move_ingest_bucket_raw_files_to_deprecated_controller_test.py
@@ -0,0 +1,202 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for move_ingest_bucket_raw_files_to_deprecated_controller.py."""
+import datetime
+import unittest
+from typing import Any
+from unittest.mock import patch
+
+from recidiviz.cloud_storage.gcsfs_path import GcsfsBucketPath, GcsfsDirectoryPath
+from recidiviz.tools.ingest.operations.move_ingest_bucket_raw_files_to_deprecated_controller import (
+    MoveIngestBucketRawFilesToDeprecatedController,
+)
+
+
+class MoveIngestBucketRawFilesToDeprecatedControllerTest(unittest.TestCase):
+    """Tests for MoveIngestBucketRawFilesToDeprecatedController class"""
+
+    def setUp(self) -> None:
+        self.gsutil_ls_patcher = patch(
+            "recidiviz.tools.ingest.operations.move_ingest_bucket_raw_files_to_deprecated_controller.gsutil_ls"
+        )
+        self.gsutil_ls_mock = self.gsutil_ls_patcher.start()
+        self.files = [
+            "gs://test_bucket/unprocessed_2024-11-12T12:00:00:000000_raw_file_tag.csv",
+            "gs://test_bucket/unprocessed_2024-12-12T12:00:00:000000_raw_file_tag.csv",
+            "gs://test_bucket/unprocessed_2024-12-24T12:00:00:000000_raw_file_tag.csv",
+            "gs://test_bucket/unprocessed_2024-11-12T12:00:00:000000_raw_file_tag_2.csv",
+            "gs://test_bucket/unprocessed_2024-12-12T12:00:00:000000_raw_file_tag_2.csv",
+            "gs://test_bucket/unprocessed_2024-12-24T12:00:00:000000_raw_file_tag_2.csv",
+        ]
+        self.gsutil_ls_mock.return_value = self.files
+        self.open_patcher = patch(
+            "recidiviz.tools.ingest.operations.move_ingest_bucket_raw_files_to_deprecated_controller.open"
+        )
+        self.open_patcher.start()
+
+        self.prompt_patcher = patch(
+            "recidiviz.tools.ingest.operations.move_ingest_bucket_raw_files_to_deprecated_controller.prompt_for_confirmation"
+        )
+        self.prompt_patcher.start()
+
+    def tearDown(self) -> None:
+        self.gsutil_ls_patcher.stop()
+        self.open_patcher.stop()
+        self.prompt_patcher.stop()
+
+    def test_move_ingest_bucket_raw_files_to_deprecated_controller(
+        self,
+    ) -> None:
+
+        controller = MoveIngestBucketRawFilesToDeprecatedController(
+            source_ingest_bucket=GcsfsBucketPath("test_bucket"),
+            destination_region_deprecated_storage_dir_path=GcsfsDirectoryPath(
+                bucket_name="test_bucket_storage", relative_path="us_xx/deprecated/"
+            ),
+            start_date_bound=None,
+            end_date_bound=None,
+            file_tag_filters=None,
+            dry_run=True,
+            log_output_path="log_output_path",
+        )
+        successful_files, failed_files = controller.run()
+
+        self.assertEqual(6, len(successful_files))
+        self.assertListEqual(
+            sorted(self.files), sorted([file.uri() for file in successful_files])
+        )
+        self.assertEqual(0, len(failed_files))
+
+    def test_move_ingest_bucket_raw_files_to_deprecated_controller_with_file_tag_filter(
+        self,
+    ) -> None:
+
+        controller = MoveIngestBucketRawFilesToDeprecatedController(
+            source_ingest_bucket=GcsfsBucketPath("test_bucket"),
+            destination_region_deprecated_storage_dir_path=GcsfsDirectoryPath(
+                bucket_name="test_bucket_storage", relative_path="us_xx/deprecated/"
+            ),
+            start_date_bound=None,
+            end_date_bound=None,
+            file_tag_filters=["file_tag"],
+            dry_run=True,
+            log_output_path="log_output_path",
+        )
+        successful_files, failed_files = controller.run()
+
+        self.assertEqual(3, len(successful_files))
+        self.assertListEqual(
+            sorted(
+                [
+                    "gs://test_bucket/unprocessed_2024-11-12T12:00:00:000000_raw_file_tag.csv",
+                    "gs://test_bucket/unprocessed_2024-12-12T12:00:00:000000_raw_file_tag.csv",
+                    "gs://test_bucket/unprocessed_2024-12-24T12:00:00:000000_raw_file_tag.csv",
+                ]
+            ),
+            sorted([file.uri() for file in successful_files]),
+        )
+        self.assertEqual(0, len(failed_files))
+
+    def test_move_ingest_bucket_raw_files_to_deprecated_controller_with_date_filter(
+        self,
+    ) -> None:
+
+        controller = MoveIngestBucketRawFilesToDeprecatedController(
+            source_ingest_bucket=GcsfsBucketPath("test_bucket"),
+            destination_region_deprecated_storage_dir_path=GcsfsDirectoryPath(
+                bucket_name="test_bucket_storage", relative_path="us_xx/deprecated/"
+            ),
+            start_date_bound=datetime.date(2024, 12, 12),
+            end_date_bound=datetime.date(2024, 12, 12),
+            file_tag_filters=["file_tag"],
+            dry_run=True,
+            log_output_path="log_output_path",
+        )
+        successful_files, failed_files = controller.run()
+
+        self.assertEqual(1, len(successful_files))
+        self.assertEqual(
+            "gs://test_bucket/unprocessed_2024-12-12T12:00:00:000000_raw_file_tag.csv",
+            successful_files[0].uri(),
+        )
+        self.assertEqual(0, len(failed_files))
+
+    def test_move_ingest_bucket_raw_files_to_deprecated_controller_error(
+        self,
+    ) -> None:
+        def gsutil_mv(from_path: str, **_kwargs: Any) -> None:
+            if (
+                from_path
+                == "gs://test_bucket/unprocessed_2024-11-12T12:00:00:000000_raw_file_tag.csv"
+            ):
+                raise ValueError("Error moving file")
+
+        with patch(
+            "recidiviz.tools.ingest.operations.move_ingest_bucket_raw_files_to_deprecated_controller.gsutil_mv",
+            side_effect=gsutil_mv,
+        ):
+            controller = MoveIngestBucketRawFilesToDeprecatedController(
+                source_ingest_bucket=GcsfsBucketPath("test_bucket"),
+                destination_region_deprecated_storage_dir_path=GcsfsDirectoryPath(
+                    bucket_name="test_bucket_storage", relative_path="us_xx/deprecated/"
+                ),
+                start_date_bound=None,
+                end_date_bound=None,
+                file_tag_filters=None,
+                dry_run=False,
+                log_output_path="log_output_path",
+            )
+            successful_files, failed_files = controller.run()
+
+            self.assertEqual(5, len(successful_files))
+            self.assertListEqual(
+                sorted(
+                    [
+                        "gs://test_bucket/unprocessed_2024-12-12T12:00:00:000000_raw_file_tag.csv",
+                        "gs://test_bucket/unprocessed_2024-12-24T12:00:00:000000_raw_file_tag.csv",
+                        "gs://test_bucket/unprocessed_2024-11-12T12:00:00:000000_raw_file_tag_2.csv",
+                        "gs://test_bucket/unprocessed_2024-12-12T12:00:00:000000_raw_file_tag_2.csv",
+                        "gs://test_bucket/unprocessed_2024-12-24T12:00:00:000000_raw_file_tag_2.csv",
+                    ]
+                ),
+                sorted([file.uri() for file in successful_files]),
+            )
+            self.assertEqual(1, len(failed_files))
+            self.assertEqual(
+                "gs://test_bucket/unprocessed_2024-11-12T12:00:00:000000_raw_file_tag.csv",
+                failed_files[0].uri(),
+            )
+
+    def test_move_ingest_bucket_raw_files_to_deprecated_controller_no_matching_files(
+        self,
+    ) -> None:
+        self.gsutil_ls_mock.return_value = []
+        controller = MoveIngestBucketRawFilesToDeprecatedController(
+            source_ingest_bucket=GcsfsBucketPath("test_bucket"),
+            destination_region_deprecated_storage_dir_path=GcsfsDirectoryPath(
+                bucket_name="test_bucket_storage", relative_path="us_xx/deprecated/"
+            ),
+            start_date_bound=None,
+            end_date_bound=None,
+            file_tag_filters=None,
+            dry_run=True,
+            log_output_path="log_output_path",
+        )
+        successful_files, failed_files = controller.run()
+
+        self.assertEqual(0, len(successful_files))
+        self.assertEqual(0, len(failed_files))
---
+++ recidiviz/tools/ingest/operations/move_ingest_bucket_raw_files_to_deprecated_controller.py
@@ -0,0 +1,213 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2024 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Implements a controller used to move raw files matching the provided filters from the ingest bucket to the 
+deprecated storage directory for a given state."""
+import datetime
+import logging
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import List, Optional, Tuple
+
+import attr
+
+from recidiviz.cloud_storage.gcsfs_path import (
+    GcsfsBucketPath,
+    GcsfsDirectoryPath,
+    GcsfsFilePath,
+)
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.gcs.directory_path_utils import (
+    gcsfs_direct_ingest_bucket_for_state,
+    gcsfs_direct_ingest_deprecated_storage_directory_path_for_state,
+)
+from recidiviz.ingest.direct.gcs.filename_parts import filename_parts_from_path
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.tools.gsutil_shell_helpers import gsutil_ls, gsutil_mv
+from recidiviz.tools.utils.script_helpers import prompt_for_confirmation
+from recidiviz.utils.log_helpers import make_log_output_path
+
+MAX_THREADS = 12
+
+
+@attr.define
+class MoveIngestBucketRawFilesToDeprecatedController:
+    """Class responsible for moving raw files matching the provided filters from the ingest bucket to the
+    deprecated storage directory for a given state."""
+
+    source_ingest_bucket: GcsfsBucketPath
+    destination_region_deprecated_storage_dir_path: GcsfsDirectoryPath
+    start_date_bound: Optional[datetime.date]
+    end_date_bound: Optional[datetime.date]
+    file_tag_filters: Optional[List[str]]
+    log_output_path: str
+    dry_run: bool
+
+    @classmethod
+    def create_controller(
+        cls,
+        *,
+        state_code: StateCode,
+        ingest_instance: DirectIngestInstance,
+        project_id: str,
+        start_date_bound: Optional[datetime.date] = None,
+        end_date_bound: Optional[datetime.date] = None,
+        file_tag_filters: Optional[List[str]] = None,
+        dry_run: bool,
+    ) -> "MoveIngestBucketRawFilesToDeprecatedController":
+        """Creates a controller responsible for moving raw files from the
+        ingest bucket to the deprecated storage directory for a given state.
+        """
+        return cls(
+            source_ingest_bucket=gcsfs_direct_ingest_bucket_for_state(
+                region_code=state_code.value,
+                ingest_instance=ingest_instance,
+                project_id=project_id,
+            ),
+            destination_region_deprecated_storage_dir_path=(
+                gcsfs_direct_ingest_deprecated_storage_directory_path_for_state(
+                    region_code=state_code.value,
+                    ingest_instance=ingest_instance,
+                    deprecated_on_date=datetime.date.today(),
+                    project_id=project_id,
+                )
+            ),
+            start_date_bound=start_date_bound,
+            end_date_bound=end_date_bound,
+            file_tag_filters=file_tag_filters,
+            dry_run=dry_run,
+            log_output_path=make_log_output_path(
+                operation_name="move_ingest_bucket_raw_files",
+                region_code=state_code.value,
+                date_string=f"start_bound_{start_date_bound}_end_bound_{end_date_bound}",
+                dry_run=dry_run,
+            ),
+        )
+
+    @property
+    def destination_region_storage_uri(self) -> str:
+        return self.destination_region_deprecated_storage_dir_path.uri()
+
+    def _write_log_file(
+        self, files_moved: List[GcsfsFilePath], failed_files: List[GcsfsFilePath]
+    ) -> None:
+        with open(self.log_output_path, "w", encoding="utf-8") as f:
+            if self.dry_run:
+                f.write("[DRY RUN] Would move the following files:\n")
+            else:
+                f.write("Moved the following files:\n")
+
+            f.writelines(
+                f"    {f.uri()} -> {self.destination_region_storage_uri}\n"
+                for f in files_moved
+            )
+            if failed_files:
+                f.write("Failed to move the following files:\n")
+                f.writelines(f"    {f.uri()}\n" for f in failed_files)
+
+    def _move_file(self, raw_file: GcsfsFilePath) -> None:
+        if not self.dry_run:
+            gsutil_mv(
+                from_path=raw_file.uri(),
+                to_path=self.destination_region_storage_uri,
+                allow_empty=False,
+            )
+
+    def _file_matches_filter(self, raw_file: GcsfsFilePath) -> bool:
+        try:
+            file_parts = filename_parts_from_path(raw_file)
+        except ValueError:
+            # Skip files that don't match the expected raw file format
+            return False
+
+        if self.file_tag_filters and file_parts.file_tag not in self.file_tag_filters:
+            return False
+
+        file_upload_date = file_parts.utc_upload_datetime.date()
+        if self.start_date_bound and file_upload_date < self.start_date_bound:
+            return False
+        if self.end_date_bound and file_upload_date > self.end_date_bound:
+            return False
+
+        return True
+
+    def _find_ingest_bucket_raw_files(
+        self,
+    ) -> List[GcsfsFilePath]:
+        files_in_bucket = gsutil_ls(
+            self.source_ingest_bucket.uri(), allow_empty=True, files_only=True
+        )
+        gcsfs_paths = [GcsfsFilePath.from_absolute_path(f) for f in files_in_bucket]
+
+        return [
+            raw_file for raw_file in gcsfs_paths if self._file_matches_filter(raw_file)
+        ]
+
+    def run(self) -> Tuple[List[GcsfsFilePath], List[GcsfsFilePath]]:
+        """
+        Moves raw files matching the class's filters from the ingest bucket to the deprecated storage directory
+        for a given state. Returns a tuple of files that were successfully moved and files that failed to move.
+        """
+        prompt_for_confirmation(
+            f"Will move files from "
+            f"[{self.source_ingest_bucket.abs_path()}] to "
+            f"[{self.destination_region_deprecated_storage_dir_path.abs_path()}] - continue?",
+            dry_run=self.dry_run,
+        )
+
+        gcsfs_files_to_operate_on = self._find_ingest_bucket_raw_files()
+        if not gcsfs_files_to_operate_on:
+            logging.info("No files found to move.")
+            return [], []
+
+        prompt_for_confirmation(
+            f"Found [{len(gcsfs_files_to_operate_on)}] files to move - continue?",
+            dry_run=self.dry_run,
+        )
+
+        failed_gcsfs_files = []
+        successful_gcsfs_files = []
+        with ThreadPoolExecutor(
+            max_workers=min(MAX_THREADS, len(gcsfs_files_to_operate_on))
+        ) as executor:
+            future_to_raw_file = {
+                executor.submit(self._move_file, raw_file): raw_file
+                for raw_file in gcsfs_files_to_operate_on
+            }
+
+            for future in as_completed(future_to_raw_file):
+                raw_file = future_to_raw_file[future]
+                try:
+                    future.result()
+                    successful_gcsfs_files.append(raw_file)
+                except Exception as e:
+                    print(f"Error processing file {raw_file}: {e}")
+                    failed_gcsfs_files.append(raw_file)
+
+        self._write_log_file(successful_gcsfs_files, failed_gcsfs_files)
+
+        if self.dry_run:
+            logging.info(
+                "[DRY RUN] See results in [%s].\n"
+                "Rerun with [--dry-run False] to execute move.",
+                self.log_output_path,
+            )
+        else:
+            logging.info(
+                "Move complete! See results in [%s].\n",
+                self.log_output_path,
+            )
+
+        return successful_gcsfs_files, failed_gcsfs_files
