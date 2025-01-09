1cb4bf3ff7d5b884ae0d1c3c538616ddab331162
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2025-01-09 10:05:43 -0800

    [Utils] Add ImmutableKeyDict (Recidiviz/recidiviz-data#36972)
    ## Description of the change
    
    Adds a class, `ImmutableKeyDict`, which extends the standard Python
    `dict`, but throws if you try to set a key that has already been set.
    This can be useful when building up collections of objects that are not
    hashable but have a unique id (e.g. BigQueryAddress for
    BigQueryViewBuilders) and you want to enforce that you aren't adding
    duplicates or overwriting one with the other.
    
    This will be used in Recidiviz/recidiviz-data#36962 to enforce that aggregated metrics
    collections to not produce duplicate views with overlapping addresses.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#29291
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 26535aa7ffc2eec4538080add7d4939ce6b17bcc

---
+++ recidiviz/tests/utils/immutable_key_dict.py
@@ -0,0 +1,80 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for ImmutableKeyDict"""
+import unittest
+
+from recidiviz.utils.immutable_key_dict import ImmutableKeyDict
+
+
+class TestImmutableKeyDict(unittest.TestCase):
+    """Tests for ImmutableKeyDict"""
+
+    def test_updates_no_conflicts(self) -> None:
+        d: dict[str, int] = ImmutableKeyDict()
+        self.assertIsInstance(d, dict)
+
+        d["A"] = 1
+        d["B"] = 2
+        d.update({"C": 3, "D": 4})
+        d.update(E=5)
+        d.update([("F", 6), ("H", 7)])
+
+        self.assertEqual({"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "H": 7}, d)
+
+        # We can explicitly remove a key and then re-add a different one
+        d.pop("B")
+        d["B"] = 10
+
+        self.assertEqual({"A": 1, "B": 10, "C": 3, "D": 4, "E": 5, "F": 6, "H": 7}, d)
+
+    def test_updates_values_conflict(self) -> None:
+        d: dict[str, str] = ImmutableKeyDict()
+        self.assertIsInstance(d, dict)
+
+        d["A"] = "B"
+        # Should be no issue to have a key that matches a value
+        d["B"] = "C"
+        # Should be no issue to have a value that matches a value
+        d["C"] = "C"
+
+    def test_set_value_with_conflict(self) -> None:
+        d: dict[str, int] = ImmutableKeyDict({"A": 1})
+        self.assertIsInstance(d, dict)
+
+        with self.assertRaisesRegex(
+            KeyError, "Key 'A' already exists and cannot be overwritten."
+        ):
+            d["A"] = 2
+
+    def test_update_with_conflict(self) -> None:
+        d: dict[str, int] = ImmutableKeyDict({"A": 1})
+        self.assertIsInstance(d, dict)
+
+        with self.assertRaisesRegex(
+            KeyError, "Key 'A' already exists and cannot be overwritten."
+        ):
+            d.update({"A": 2, "B": 2})
+
+        with self.assertRaisesRegex(
+            KeyError, "Key 'A' already exists and cannot be overwritten."
+        ):
+            d.update(A=2)
+
+        with self.assertRaisesRegex(
+            KeyError, "Key 'A' already exists and cannot be overwritten."
+        ):
+            d.update([("A", 2)])
---
+++ recidiviz/utils/immutable_key_dict.py
@@ -0,0 +1,65 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines a subclass of the basic Python dict class which throws a KeyError if you
+attempt to set a key that is already set.
+"""
+from typing import Any, Generic, Iterable, Mapping, TypeVar
+
+KT = TypeVar("KT")
+VT = TypeVar("VT")
+
+
+class ImmutableKeyDict(Generic[KT, VT], dict[KT, VT]):
+    """A subclass of the basic Python dict class which throws a KeyError if you attempt
+    to set a key that is already set.
+
+    This class reimplements update() in Python and therefore update() calls will be
+    significantly slower for large inputs. This class should generally be used for small
+    collections (e.g. less than ~1000).
+    """
+
+    def __setitem__(self, key: KT, value: VT) -> None:
+        if key in self:
+            raise KeyError(f"Key '{key}' already exists and cannot be overwritten.")
+        super().__setitem__(key, value)
+
+    def update(self, *args: Any, **kwargs: VT) -> None:
+        """The standard Python dict implements update() in C for efficiency reasons, but
+        this means __setitem__ is never called. Here we reimplement update() in Python
+        so that we can enforce that no duplicate keys are added.
+        """
+        # Process positional arguments
+        if args:
+            if len(args) > 1:
+                raise TypeError(
+                    f"update() takes at most one positional argument ({len(args)} given)"
+                )
+            arg = args[0]
+            if isinstance(arg, Mapping):
+                for key, value in arg.items():
+                    self[key] = value
+            elif isinstance(arg, Iterable):
+                for key, value in arg:
+                    self[key] = value
+            else:
+                raise TypeError(
+                    f"update() argument must be a mapping or iterable, not {type(arg)}"
+                )
+
+        # Process keyword arguments
+        for key, value in kwargs.items():
+            self[key] = value
