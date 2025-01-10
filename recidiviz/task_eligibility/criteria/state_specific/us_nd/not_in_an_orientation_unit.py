6ce10b70698e7a39924349217ffe6f363366c665
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2025-01-10 11:36:44 -0600

    [US_ND] Modify ORU criteria (Recidiviz/recidiviz-data#35165)
    ## Description of the change
    
    Modify ORU criteria: now people in the ORU will become eligible after 3
    weeks there. I uploaded this into a sandbox (_hsalas) and 25 more people
    will be eligible for a transfer to min.
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 2bd94561e757ed9277ad846acdf3a23508cd46bd

--- recidiviz/task_eligibility/criteria/state_specific/us_nd/not_in_an_orientation_unit.py
+++ recidiviz/task_eligibility/criteria/state_specific/us_nd/not_in_an_orientation_unit.py
@@ -15,12 +15,16 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ============================================================================
 """Describes the spans of time during which someone in ND is not
-in an orientation unit. These are units that house people who have just arrived to 
-the DOC and are not yet eligible for many opportunities because of this.
+in an orientation unit or has been in an orientation unit for 3 weeks. 
+These are units that house people who have just arrived to the DOC and are not yet 
+eligible for many opportunities because of this.
 """
 from google.cloud import bigquery
 
-from recidiviz.calculator.query.bq_utils import nonnull_end_date_exclusive_clause
+from recidiviz.calculator.query.bq_utils import (
+    nonnull_end_date_clause,
+    nonnull_end_date_exclusive_clause,
+)
 from recidiviz.calculator.query.state.dataset_config import SESSIONS_DATASET
 from recidiviz.common.constants.states import StateCode
 from recidiviz.task_eligibility.reasons_field import ReasonsField
@@ -32,16 +36,15 @@ from recidiviz.utils.metadata import local_project_id_override
 
 _CRITERIA_NAME = "US_ND_NOT_IN_AN_ORIENTATION_UNIT"
 
-_DESCRIPTION = """Describes the spans of time during which someone in ND is not
-in an orientation unit. These are units that house people who have just arrived to 
-the DOC and are not yet eligible for many opportunities because of this."""
-
 _QUERY_TEMPLATE = f"""
 SELECT 
     state_code,
     person_id,
     start_date,
-    DATE_SUB(end_date_exclusive, INTERVAL 1 DAY) AS end_date,
+    LEAST(
+        {nonnull_end_date_clause('end_date_exclusive')},
+        DATE_ADD(start_date, INTERVAL 3 WEEK)
+    ) AS end_date,
     FALSE AS meets_criteria,
     TO_JSON(STRUCT(start_date AS housing_unit_start_date,
                    ANY_VALUE(housing_unit) AS housing_unit)) AS reason,
@@ -62,7 +65,7 @@ VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
         state_code=StateCode.US_ND,
         criteria_name=_CRITERIA_NAME,
         criteria_spans_query_template=_QUERY_TEMPLATE,
-        description=_DESCRIPTION,
+        description=__doc__,
         sessions_dataset=SESSIONS_DATASET,
         meets_criteria_default=True,
         reasons_fields=[
