0c0b757e5a0562075512be0d84a28826ad3a41a4
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2025-02-11 14:34:53 -0600

    [US_AZ] Create 35 day admin supervision opp (Recidiviz/recidiviz-data#38162)
    ## Description of the change
    
    [US_AZ] Create 35 day admin supervision opp
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: f2c1231ee9c9378558a097bd6df1031eb221be31

--- recidiviz/task_eligibility/eligibility_spans/us_az/transfer_to_35_administrative_supervision.py
+++ recidiviz/task_eligibility/eligibility_spans/us_az/transfer_to_35_administrative_supervision.py
@@ -14,61 +14,51 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-"""Shows the eligibility spans for supervision clients in AZ who are eligible 
-for a transfer to administrative supervision"""
+"""Shows the eligibility spans for residents in AZ who are eligible for a 
+supervision release directly into 35-day administrative supervision"""
 from recidiviz.common.constants.states import StateCode
 from recidiviz.task_eligibility.candidate_populations.general import (
-    active_supervision_population,
+    general_incarceration_population,
 )
 from recidiviz.task_eligibility.completion_events.general import (
     transfer_to_limited_supervision,
 )
-from recidiviz.task_eligibility.criteria.general import (
-    no_supervision_violation_within_15_months,
-    on_supervision_at_least_15_months,
-    supervision_level_is_not_limited,
-)
+from recidiviz.task_eligibility.criteria.general import not_serving_for_sexual_offense
 from recidiviz.task_eligibility.criteria.state_specific.us_az import (
     mental_health_score_3_or_below,
-    not_serving_ineligible_offense_for_admin_supervision,
+    not_eligible_or_almost_eligible_for_overdue_for_acis_dtp,
+    not_eligible_or_almost_eligible_for_overdue_for_acis_tpr,
 )
 from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
     SingleTaskEligibilitySpansBigQueryViewBuilder,
-)
-from recidiviz.task_eligibility.task_criteria_group_big_query_view_builder import (
-    AndTaskCriteriaGroup,
-    OrTaskCriteriaGroup,
 )
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
     state_code=StateCode.US_AZ,
-    task_name="TRANSFER_TO_ADMINISTRATIVE_SUPERVISION",
+    task_name="TRANSFER_TO_35_ADMINISTRATIVE_SUPERVISION",
     description=__doc__,
-    candidate_population_view_builder=active_supervision_population.VIEW_BUILDER,
+    candidate_population_view_builder=general_incarceration_population.VIEW_BUILDER,
     criteria_spans_view_builders=[
-        supervision_level_is_not_limited.VIEW_BUILDER,
-        # 1.6 Mental Health Score of 3 or below.
+        # CSED (Community Supervision End Date) - ECRD <= 35 days
+        # TODO(#38153): Add criteria
+        # 22.19.1.1.1 NO BOEC (Board of Executive Clemency) granted releases
+        # TODO(#38154): Add criteria
+        # 22.19.1.1.2 NO Transition/Drug Transition legislative program legislative early releases
+        not_eligible_or_almost_eligible_for_overdue_for_acis_dtp.VIEW_BUILDER,
+        not_eligible_or_almost_eligible_for_overdue_for_acis_tpr.VIEW_BUILDER,
+        # 22.19.1.1.3 NO Sex offenders (Codes F/N are acceptable)
+        not_serving_for_sexual_offense.VIEW_BUILDER,
+        # TODO(#38157): Make sure F/N codes are included in the criteria
+        # 22.19.1.1.4 NO Maximum, Intensive Supervision Levels, Institution G/V scores of 5 or higher
+        # TODO(#38158): Add criteria
+        # 22.19.1.1.5 NO SMI (Seriously Mentally Ill designation) or current high need General Mental Illness
         mental_health_score_3_or_below.VIEW_BUILDER,
-        # 1.8 Offenders with ineligible offenses are only eligible if they've been 15 months violation free
-        OrTaskCriteriaGroup(
-            criteria_name="US_AZ_INELIGIBLE_OFFENSES_BUT_15_MONTHS_VIOLATION_FREE",
-            sub_criteria_list=[
-                not_serving_ineligible_offense_for_admin_supervision.VIEW_BUILDER,
-                AndTaskCriteriaGroup(
-                    criteria_name="US_AZ_15_MONTHS_ON_SUPERVISION_VIOLATION_FREE",
-                    sub_criteria_list=[
-                        no_supervision_violation_within_15_months.VIEW_BUILDER,
-                        on_supervision_at_least_15_months.VIEW_BUILDER,
-                    ],
-                    allowed_duplicate_reasons_keys=[],
-                ),
-            ],
-            allowed_duplicate_reasons_keys=[],
-        ),
+        # TODO(#38159): Make sure we're including all of the relevant cases
     ],
     completion_event_builder=transfer_to_limited_supervision.VIEW_BUILDER,
+    # TODO(#38160): Add the right completion event
 )
 
 if __name__ == "__main__":
