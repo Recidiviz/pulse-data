b70b637fa74da4f4cc346245fa7001ff8d388455
Santy Mendoza <55068232+santymendoza@users.noreply.github.com>
2025-02-10 18:03:11 -0600

    [US_TX] Adding assessment scores as `SUPERVISION_LEVEL` in `SUPERVISION_PERIOD` (Recidiviz/recidiviz-data#37884)
    ## Description of the change
    
    Making two main changes in this PR:
    
    1. Changing where we get supervision levels from in supervision periods.
    Before we used to grab directly from the supervision level in the period
    raw data. Now we use the assessment level to indicate SL.
    2. Creates new supervision periods based on any changes using
    SupervisionPeriod@ALL
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#38003
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 1ec881ca9deae99a21db632f45dcb2d7850ffbe3

--- recidiviz/ingest/direct/regions/us_tx/ingest_views/view_supervision_period.py
+++ recidiviz/ingest/direct/regions/us_tx/ingest_views/view_supervision_period.py
@@ -15,13 +15,14 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 """Query for supervision periods for US_TX."""
+from recidiviz.calculator.query.sessions_query_fragments import aggregate_adjacent_spans
 from recidiviz.ingest.direct.views.direct_ingest_view_query_builder import (
     DirectIngestViewQueryBuilder,
 )
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
-VIEW_QUERY_TEMPLATE = """
+VIEW_QUERY_TEMPLATE = f"""
 WITH 
 -- In less than 1 percent of period records, there are multiple records on the same update_datetime date
 -- This CTE deterministically chooses a single record per update_datetime date. We also get rid of 
@@ -30,7 +31,7 @@ clean_cte AS (
     SELECT 
         *,
         ROW_NUMBER() OVER (PARTITION BY Period_ID_Number,update_datetime ORDER BY Supervision_Officer, Supervision_Level, Special_Conditions, Custodial_Authority, Case_Type, Start_Date, Max_Termination_Date asc) AS rn
-    FROM {SupervisionPeriod@ALL}
+    FROM `{{SupervisionPeriod@ALL}}`
     -- Confusing column naming but this means that the record is not deleted.
     WHERE Deleted_Flag = "Active"
 ),
@@ -41,7 +42,6 @@ lag_cte AS (
         SID_Number,
         Period_ID_Number,
         Supervision_Officer,
-        Supervision_Level,
         Special_Conditions,
         Custodial_Authority,
         Case_Type,
@@ -50,10 +50,10 @@ lag_cte AS (
         update_datetime,
         LAG(update_datetime) OVER w AS prev_update_datetime,
         LAG(Supervision_Officer) OVER w AS prev_Supervision_Officer,
-        LAG(Supervision_Level) OVER w AS prev_Supervision_Level,
         LAG(Special_Conditions) OVER w AS prev_Special_Conditions,
         LAG(Custodial_Authority) OVER w AS prev_Custodial_Authority,
-        LAG(Case_Type) OVER w AS prev_Case_Type
+        LAG(Case_Type) OVER w AS prev_Case_Type,
+        LAG(Max_Termination_Date) OVER w AS prev_Max_Termination_Date,
     FROM clean_cte
     WHERE rn = 1 AND Start_Date != "0001-01-01 00:00:00"
     WINDOW w AS (PARTITION BY Period_ID_Number ORDER BY update_datetime asc)
@@ -66,42 +66,185 @@ filter_cte AS
         SID_Number,
         Period_ID_Number,
         Supervision_Officer,
-        Supervision_Level,
         Special_Conditions,
         Custodial_Authority,
         Case_Type,
         Start_Date,
         Max_Termination_Date,
         update_datetime,
-        ROW_NUMBER()OVER (PARTITION BY Period_ID_Number ORDER BY update_datetime asc) AS rn,
+        ROW_NUMBER()OVER (PARTITION BY Period_ID_Number ORDER BY update_datetime DESC) AS rn,
         LAG(update_datetime) OVER (PARTITION BY Period_ID_Number ORDER BY update_datetime asc) AS prev_update_datetime,
         LEAD(update_datetime) OVER (PARTITION BY Period_ID_Number ORDER BY update_datetime asc) AS lead_update_datetime
     FROM lag_cte 
     WHERE prev_update_datetime IS NULL 
         OR Supervision_Officer IS DISTINCT FROM prev_Supervision_Officer
-        OR Supervision_Level IS DISTINCT FROM prev_Supervision_Level
         OR Special_Conditions IS DISTINCT FROM prev_Special_Conditions
         OR Custodial_Authority IS DISTINCT FROM prev_Custodial_Authority
         OR Case_Type IS DISTINCT FROM prev_Case_Type
-)
-SELECT
+        OR Max_Termination_Date IS DISTINCT FROM prev_Max_Termination_Date
+), 
+-- Creates supervision periods without supervision level. Use changes in supervision
+-- period to create smaller sub-periods. Also, since Max_Termination_Date is the
+-- expected date that supervision ends and not the actual date supervision ends
+-- we NULL Max_Termination_Date if Max_Termination_Date < CURRENT_DATE.
+sp_no_level AS
+(
+    SELECT
+        SID_Number,
+        Period_ID_Number,
+        Supervision_Officer,
+        Special_Conditions,
+        Custodial_Authority,
+        Case_Type,
+        lead_update_datetime,
+        CASE
+            WHEN prev_update_datetime IS NULL
+                THEN DATE(Start_Date)
+            ELSE DATE(update_datetime)
+        END AS Start_Date,
+        CASE 
+            WHEN lead_update_datetime IS NOT NULL
+                THEN DATE(lead_update_datetime)
+            WHEN DATE(Max_Termination_Date) < CURRENT_DATE
+                THEN DATE(Max_Termination_Date)
+            ELSE NULL
+        END AS Max_Termination_Date,
+    FROM filter_cte
+),
+-- Gathers assessment data
+assessment_cte AS (
+    SELECT
+        SID_Number,
+        assessment_level,
+        DATE(ASSESSMENT_DATE) AS ASSESSMENT_DATE
+    FROM `{{Assessment}}`
+),
+-- Left joins the assessments with their associated supervision period
+connect_cte AS (
+    SELECT 
+        p.SID_Number,
+        p.Period_ID_Number,
+        p.Supervision_Officer,
+        p.Special_Conditions,
+        p.Custodial_Authority,
+        p.Case_Type,
+        lead_update_datetime,
+        DATE(p.Start_Date) AS Start_Date,
+        DATE(p.Max_Termination_Date) AS Max_Termination_Date,
+        a.ASSESSMENT_DATE
+    FROM 
+        sp_no_level p
+    LEFT JOIN 
+        assessment_cte a
+    ON 
+        a.ASSESSMENT_DATE BETWEEN DATE(p.Start_Date) AND 
+            COALESCE(DATE(p.Max_Termination_Date),DATE("9999-12-31"))
+        AND p.SID_Number = a.SID_Number
+),
+-- Creates critical dates comprised of start, end, and assessment dates
+critical_dates AS (
+    SELECT
+        SID_Number,
+        Period_ID_Number,
+        Supervision_Officer,
+        Special_Conditions,
+        Custodial_Authority,
+        Case_Type,
+        Start_Date AS critical_date,
+        "start" as type,
+    FROM connect_cte
+    UNION ALL
+    SELECT
+        SID_Number,
+        Period_ID_Number,
+        Supervision_Officer,
+        Special_Conditions,
+        Custodial_Authority,
+        Case_Type,
+        Max_Termination_Date AS critical_date,
+        "end" as type,
+    FROM connect_cte
+    -- Only get end date as critical date if it's the last period
+    WHERE lead_update_datetime IS NULL
+    UNION ALL
+    SELECT
+        SID_Number,
+        Period_ID_Number,
+        Supervision_Officer,
+        Special_Conditions,
+        Custodial_Authority,
+        Case_Type,
+        ASSESSMENT_DATE AS critical_date,
+        "assessment" as type,
+    FROM connect_cte
+    WHERE ASSESSMENT_DATE IS NOT NULL
+),
+-- Creates periods divided by assessments
+new_periods AS (
+    SELECT
+        SID_Number,
+        Period_ID_Number,
+        Supervision_Officer,
+        Special_Conditions,
+        Custodial_Authority,
+        Case_Type,
+        critical_date AS period_start,
+        LAST_VALUE(critical_date IGNORE NULLS) OVER (
+            PARTITION BY SID_Number, Period_ID_Number
+            ORDER BY critical_date DESC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING
+        ) AS period_end,
+        type,
+        FROM critical_dates
+        WHERE critical_date IS NOT NULL
+),
+-- Finally joins on the correct supervision level given the assessment falls within that
+-- period.
+final_periods_unagg AS (
+    SELECT DISTINCT
+        np.SID_Number,
+        np.Period_ID_Number,
+        np.Supervision_Officer,
+        np.Custodial_Authority,
+        np.Special_Conditions,
+        np.Case_Type,
+        period_start AS start_date,
+        period_end AS end_date,
+        a.assessment_level,
+    FROM new_periods np 
+    LEFT JOIN assessment_cte a
+        ON np.SID_Number = a.SID_Number
+        AND a.ASSESSMENT_DATE >= np.period_start AND 
+           a.ASSESSMENT_DATE < COALESCE(DATE(period_end),DATE("9999-12-31"))
+    WHERE type != "end"
+),
+-- Aggregate above periods by period attributes
+period_info_agg AS (
+    {aggregate_adjacent_spans(
+        table_name='final_periods_unagg',
+        attribute=['assessment_level','case_type','Supervision_Officer',
+                   'Custodial_Authority','Special_Conditions'],
+        session_id_output_name='period_info_agg',
+        end_date_field_name='end_date',
+        index_columns=['Period_ID_Number','SID_Number']
+    )}
+),
+-- Assign row numbers for period external id
+final_periods AS (SELECT
     SID_Number,
     Period_ID_Number,
     Supervision_Officer,
-    Supervision_Level,
-    Special_Conditions,
     Custodial_Authority,
+    Special_Conditions,
     Case_Type,
-    CASE 
-        WHEN prev_update_datetime IS NULL
-            THEN DATE(Start_Date)
-        ELSE update_datetime
-    END AS Start_Date,
-    CASE WHEN DATE(Max_Termination_Date) < CURRENT_DATE
-        THEN Max_Termination_Date
-        ELSE NULL
-    END AS Max_Termination_Date,
-FROM filter_cte
+    start_date,
+    end_date,
+    assessment_level,
+    ROW_NUMBER() OVER (PARTITION BY SID_Number, Period_ID_Number ORDER BY start_date) AS rn
+FROM period_info_agg)
+SELECT 
+    *
+FROM final_periods
 """
 VIEW_BUILDER = DirectIngestViewQueryBuilder(
     region="us_tx",
