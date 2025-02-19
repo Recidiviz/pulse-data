198f32cae43d60613e35fe9ab8b55e3b79f9a759
samanthahuff <93396549+samanthahuff@users.noreply.github.com>
2025-02-19 11:47:29 -0800

    [US_IX][Workflows] Add ineligible_criteria and eligible_date to opp record (Recidiviz/recidiviz-data#38399)
    ## Description of the change
    
    In order to enable proper sorting and copy we need these fields on the
    opp record.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#38260
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6a873d6ccf86769eed552c3778e041705caead54

--- recidiviz/calculator/query/state/views/workflows/firestore/us_ix_custody_level_downgrade_record.py
+++ recidiviz/calculator/query/state/views/workflows/firestore/us_ix_custody_level_downgrade_record.py
@@ -19,6 +19,7 @@
 from recidiviz.big_query.big_query_view import SimpleBigQueryViewBuilder
 from recidiviz.calculator.query.bq_utils import nonnull_end_date_exclusive_clause
 from recidiviz.calculator.query.state import dataset_config
+from recidiviz.calculator.query.state.dataset_config import ANALYST_VIEWS_DATASET
 from recidiviz.common.constants.states import StateCode
 from recidiviz.ingest.views.dataset_config import NORMALIZED_STATE_DATASET
 from recidiviz.task_eligibility.dataset_config import (
@@ -37,15 +38,23 @@ US_IX_CUSTODY_LEVEL_DOWNGRADE_RECORD_QUERY_TEMPLATE = f"""
     SELECT
         pei.external_id,
         tes.state_code,
-        reasons,
-        is_eligible,
-        is_almost_eligible,
+        tes.reasons,
+        tes.is_eligible,
+        tes.is_almost_eligible,
+        tes.ineligible_criteria,
+        tes_all.start_date AS metadata_eligible_date
     FROM `{{project_id}}.{{task_eligibility_dataset}}.custody_level_downgrade_materialized` tes
     INNER JOIN `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` pei
         ON tes.state_code = pei.state_code 
             AND tes.person_id = pei.person_id
             AND pei.id_type = "US_IX_DOC"
-    WHERE CURRENT_DATE('US/Pacific') BETWEEN tes.start_date AND {nonnull_end_date_exclusive_clause('tes.end_date')}
+    --join analyst view dataset that sessionizes spans based on eligibility to get eligible start date
+    INNER JOIN `{{project_id}}.{{analyst_views_dataset}}.all_task_eligibility_spans_materialized` tes_all
+        ON tes_all.state_code = tes.state_code
+        AND tes_all.person_id = tes.person_id 
+        AND tes_all.task_name = 'CUSTODY_LEVEL_DOWNGRADE'
+        AND CURRENT_DATE('US/Pacific') BETWEEN tes_all.start_date AND {nonnull_end_date_exclusive_clause('tes_all.end_date')}
+WHERE CURRENT_DATE('US/Pacific') BETWEEN tes.start_date AND {nonnull_end_date_exclusive_clause('tes.end_date')}
         AND (tes.is_eligible OR tes.is_almost_eligible)
 """
 
@@ -55,6 +64,7 @@ US_IX_CUSTODY_LEVEL_DOWNGRADE_RECORD_VIEW_BUILDER = SimpleBigQueryViewBuilder(
     view_query_template=US_IX_CUSTODY_LEVEL_DOWNGRADE_RECORD_QUERY_TEMPLATE,
     description=US_IX_CUSTODY_LEVEL_DOWNGRADE_RECORD_DESCRIPTION,
     normalized_state_dataset=NORMALIZED_STATE_DATASET,
+    analyst_views_dataset=ANALYST_VIEWS_DATASET,
     task_eligibility_dataset=task_eligibility_spans_state_specific_dataset(
         StateCode.US_IX
     ),
