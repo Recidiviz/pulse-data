a216088b5ec219e3d1ce6a3e274c232206d7eecd
Emily Tiry <7997357+etiry@users.noreply.github.com>
2025-03-06 12:08:01 -0500

    [Auth] Add script to delete users blocked 30+ days (Recidiviz/recidiviz-data#38950)
    ## Description of the change
    
    This PR adds a script that will identify and delete users who have been
    blocked for at least 30 days and are not in Roster. The script will
    delete users from the UserOverride and PermissionsOverride tables. It
    filters users by state code and prompts for confirmation before
    deleting.
    
    I tested this with my local dev db and it works, but I wasn't able to
    test it with the Cloud SQL db, which might just be a "me" permissions
    issue? Or maybe I'm missing a step, I'm not sure. In any case, if you
    could look at that in particular that would be great!
    
    ```
    google.api_core.exceptions.PermissionDenied: 403 Permission 'secretmanager.versions.access' denied for resource 'projects/recidiviz-staging/secrets/case_triage_cloudsql_instance_id/versions/latest' (or it may not exist).
    ```
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#36604
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [X] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: c67281ec3727d9b0aba675f2a6b873a9fc2e4a1d

---
+++ recidiviz/tests/tools/auth/delete_blocked_users_test.py
@@ -0,0 +1,221 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for the script that deletes users blocked for 30+ days from UserOverride and 
+PermissionsOverride."""
+
+from datetime import datetime, timedelta, timezone
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+import pytest
+from sqlalchemy import select
+
+from recidiviz.persistence.database.schema.case_triage.schema import (
+    PermissionsOverride,
+    UserOverride,
+)
+from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.persistence.database.session_factory import SessionFactory
+from recidiviz.persistence.database.sqlalchemy_database_key import SQLAlchemyDatabaseKey
+from recidiviz.persistence.database.sqlalchemy_engine_manager import (
+    SQLAlchemyEngineManager,
+)
+from recidiviz.tests.auth.helpers import (
+    generate_fake_permissions_overrides,
+    generate_fake_rosters,
+    generate_fake_user_overrides,
+)
+from recidiviz.tools.auth.delete_blocked_users import delete_blocked_users
+from recidiviz.tools.postgres import local_persistence_helpers, local_postgres_helpers
+
+
+@pytest.mark.uses_db
+class DeleteBlockedUsersTest(TestCase):
+    """Tests for the script that deletes users blocked for 30+ days from UserOverride and
+    PermissionsOverride."""
+
+    # Stores the location of the postgres DB for this test run
+    temp_db_dir: str | None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.temp_db_dir = local_postgres_helpers.start_on_disk_postgresql_database()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        local_postgres_helpers.stop_and_clear_on_disk_postgresql_database(
+            cls.temp_db_dir
+        )
+
+    def setUp(self) -> None:
+        self.database_key = SQLAlchemyDatabaseKey.for_schema(SchemaType.CASE_TRIAGE)
+        self.overridden_env_vars = (
+            local_persistence_helpers.update_local_sqlalchemy_postgres_env_vars()
+        )
+        engine = SQLAlchemyEngineManager.init_engine_for_postgres_instance(
+            database_key=self.database_key,
+            db_url=local_postgres_helpers.on_disk_postgres_db_url(),
+        )
+
+        self.database_key.declarative_meta.metadata.create_all(engine)
+
+        self.past_blocked_on_date_more_than_30_days = datetime(
+            2025, 1, 9, 15, 0, 0
+        ).replace(tzinfo=timezone.utc)
+        self.past_blocked_on_date_less_than_30_days = (
+            datetime.now() - timedelta(weeks=1)
+        ).replace(tzinfo=timezone.utc)
+
+        self.session = SessionFactory.using_database(self.database_key).__enter__()
+
+    def tearDown(self) -> None:
+        self.session.__exit__(None, None, None)
+        local_postgres_helpers.restore_local_env_vars(self.overridden_env_vars)
+        local_persistence_helpers.teardown_on_disk_postgresql_database(
+            self.database_key
+        )
+
+    @patch("recidiviz.tools.auth.delete_blocked_users.prompt_for_confirmation")
+    def test_delete_users_blocked_more_than_30_days_not_in_roster(
+        self, mock_prompt: MagicMock
+    ) -> None:
+        mock_prompt.return_value = True
+
+        override_user = generate_fake_user_overrides(
+            email="leadership@testdomain.com",
+            region_code="US_CO",
+            blocked_on=self.past_blocked_on_date_more_than_30_days,
+        )
+        override_permissions = generate_fake_permissions_overrides(
+            email="leadership@testdomain.com", routes={}, feature_variants={}
+        )
+        self.session.add_all([override_user, override_permissions])
+
+        delete_blocked_users(self.session, state_code="US_CO")
+        user_override_result = self.session.execute(select(UserOverride)).scalar()
+        self.assertIsNone(user_override_result)
+
+        permissions_override_result = self.session.execute(
+            select(PermissionsOverride)
+        ).scalar()
+        self.assertIsNone(permissions_override_result)
+
+    @patch("recidiviz.tools.auth.delete_blocked_users.prompt_for_confirmation")
+    def test_keep_users_blocked_less_than_30_days_not_in_roster(
+        self, mock_prompt: MagicMock
+    ) -> None:
+        mock_prompt.return_value = True
+
+        override_user = generate_fake_user_overrides(
+            email="leadership@testdomain.com",
+            region_code="US_CO",
+            blocked_on=self.past_blocked_on_date_less_than_30_days,
+        )
+        override_permissions = generate_fake_permissions_overrides(
+            email="leadership@testdomain.com", routes={}, feature_variants={}
+        )
+        self.session.add_all([override_user, override_permissions])
+
+        delete_blocked_users(self.session, state_code="US_CO")
+        user_override_result = self.session.execute(select(UserOverride)).scalar()
+        self.assertEqual(override_user, user_override_result)
+
+        permissions_override_result = self.session.execute(
+            select(PermissionsOverride)
+        ).scalar()
+        self.assertEqual(override_permissions, permissions_override_result)
+
+    @patch("recidiviz.tools.auth.delete_blocked_users.prompt_for_confirmation")
+    def test_keep_users_blocked_more_than_30_days_in_roster(
+        self, mock_prompt: MagicMock
+    ) -> None:
+        mock_prompt.return_value = True
+
+        roster_user = generate_fake_rosters(
+            email="leadership@testdomain.com",
+            region_code="US_CO",
+            external_id="123",
+            roles=["leadership_role"],
+            district="D1",
+            first_name="Test",
+            last_name="User",
+            pseudonymized_id="pseudo-123",
+        )
+        override_user = generate_fake_user_overrides(
+            email="leadership@testdomain.com",
+            region_code="US_CO",
+            blocked_on=self.past_blocked_on_date_more_than_30_days,
+        )
+        override_permissions = generate_fake_permissions_overrides(
+            email="leadership@testdomain.com", routes={}, feature_variants={}
+        )
+        self.session.add_all([roster_user, override_user, override_permissions])
+
+        delete_blocked_users(self.session, state_code="US_CO")
+        user_override_result = self.session.execute(select(UserOverride)).scalar()
+        self.assertEqual(override_user, user_override_result)
+
+        permissions_override_result = self.session.execute(
+            select(PermissionsOverride)
+        ).scalar()
+        self.assertEqual(override_permissions, permissions_override_result)
+
+    @patch("recidiviz.tools.auth.delete_blocked_users.prompt_for_confirmation")
+    def test_non_specified_states_not_deleted(self, mock_prompt: MagicMock) -> None:
+        mock_prompt.return_value = True
+
+        override_user = generate_fake_user_overrides(
+            email="leadership@testdomain.com",
+            region_code="US_CO",
+            blocked_on=self.past_blocked_on_date_more_than_30_days,
+        )
+        override_permissions = generate_fake_permissions_overrides(
+            email="leadership@testdomain.com", routes={}, feature_variants={}
+        )
+        self.session.add_all([override_user, override_permissions])
+
+        delete_blocked_users(self.session, state_code="US_MI")
+        user_override_result = self.session.execute(select(UserOverride)).scalar()
+        self.assertEqual(override_user, user_override_result)
+
+        permissions_override_result = self.session.execute(
+            select(PermissionsOverride)
+        ).scalar()
+        self.assertEqual(override_permissions, permissions_override_result)
+
+    @patch("recidiviz.tools.auth.delete_blocked_users.prompt_for_confirmation")
+    def test_no_confirmation_not_deleted(self, mock_prompt: MagicMock) -> None:
+        mock_prompt.return_value = False
+
+        override_user = generate_fake_user_overrides(
+            email="leadership@testdomain.com",
+            region_code="US_CO",
+            blocked_on=self.past_blocked_on_date_more_than_30_days,
+        )
+        override_permissions = generate_fake_permissions_overrides(
+            email="leadership@testdomain.com", routes={}, feature_variants={}
+        )
+        self.session.add_all([override_user, override_permissions])
+
+        delete_blocked_users(self.session, state_code="US_CO")
+        user_override_result = self.session.execute(select(UserOverride)).scalar()
+        self.assertEqual(override_user, user_override_result)
+
+        permissions_override_result = self.session.execute(
+            select(PermissionsOverride)
+        ).scalar()
+        self.assertEqual(override_permissions, permissions_override_result)
---
+++ recidiviz/tools/auth/delete_blocked_users.py
@@ -0,0 +1,185 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Tool for deleting product users who have been blocked for at least 30 days. Blocking can
+occur either via the admin panel or during roster sync. During roster sync, if an existing
+user is not included in the ingested file, their block date is set, their information is 
+copied to the UserOverride table, and they are removed from the Roster table. If they 
+are still not in the Roster table 30 days after they are blocked, we can assume that they 
+should no longer have access and can be deleted. Users are deleted from both UserOverride
+and PermissionsOverride to prevent accidental reassignment of previous permissions if the user
+is ever re-added.
+
+The script can be run against a local database or against one running in Cloud SQL. The
+script will prompt for confirmation with a list of affected users before deleting.
+
+Usage against default development database (docker-compose v1) after `docker-compose up` has been
+run: docker exec -it pulse-data_admin_panel_backend_1 pipenv run python -m
+recidiviz.tools.auth.delete_blocked_users --state_code US_XX
+
+Usage against default development database (docker-compose v2) after `docker-compose up` has been
+run: docker exec -it pulse-data-admin_panel_backend-1 pipenv run python -m
+recidiviz.tools.auth.delete_blocked_users --state_code US_XX
+
+To run against Cloud SQL, specify the project id: python -m
+recidiviz.tools.auth.delete_blocked_users --project_id recidiviz-staging --state_code US_XX
+"""
+
+import argparse
+import logging
+import sys
+from datetime import datetime, timedelta
+from typing import List, Tuple, Type
+
+from dateutil.tz import tzlocal
+from sqlalchemy import and_, delete, select
+from sqlalchemy.orm import Session
+
+from recidiviz.common.constants.states import StateCode
+from recidiviz.persistence.database.schema.case_triage.schema import (
+    PermissionsOverride,
+    Roster,
+    UserOverride,
+)
+from recidiviz.persistence.database.schema_type import SchemaType
+from recidiviz.persistence.database.session_factory import SessionFactory
+from recidiviz.persistence.database.sqlalchemy_database_key import SQLAlchemyDatabaseKey
+from recidiviz.persistence.database.sqlalchemy_engine_manager import (
+    SQLAlchemyEngineManager,
+)
+from recidiviz.tools.postgres.cloudsql_proxy_control import cloudsql_proxy_control
+from recidiviz.tools.utils.script_helpers import prompt_for_confirmation
+from recidiviz.utils.environment import (
+    GCP_PROJECT_PRODUCTION,
+    GCP_PROJECT_STAGING,
+    in_development,
+)
+from recidiviz.utils.metadata import local_project_id_override
+
+
+def format_user_strings(users: List[Tuple[str, datetime]]) -> str:
+    indentation = " " * 4
+    user_strings = "\n".join(
+        [f"{users[0][0]} (blocked on {users[0][1].strftime('%Y-%m-%d')})"]
+        + [
+            f"{indentation}{email} (blocked on {date.strftime('%Y-%m-%d')})"
+            for email, date in users[1:]
+        ]
+    )
+    return user_strings
+
+
+def delete_users_from_table(
+    session: Session,
+    table: Type[UserOverride] | Type[PermissionsOverride],
+    users: List[Tuple[str, datetime]],
+) -> None:
+    user_emails = [email_address for email_address, _ in users]
+    results = session.execute(
+        delete(table)
+        .where(table.email_address.in_(user_emails))
+        .execution_options(synchronize_session=False)
+        .returning(table.email_address)
+    )
+    deleted_users = [user.email_address for user in results]
+    logging.info(
+        "Deleted %d rows from %s: %s",
+        len(deleted_users),
+        table.__tablename__,
+        deleted_users,
+    )
+
+
+def delete_blocked_users(session: Session, state_code: str) -> None:
+    """Delete from UserOverride and PermissionsOverride any users that have been blocked
+    for 30+ days and don't exist in Roster"""
+    thirty_days_ago = datetime.now(tzlocal()) - timedelta(days=30)
+
+    results = session.execute(
+        select(UserOverride)
+        .outerjoin(Roster, UserOverride.email_address == Roster.email_address)
+        .where(
+            and_(
+                Roster.email_address.is_(None),
+                UserOverride.state_code == state_code,
+                UserOverride.blocked_on <= thirty_days_ago,
+            )
+        )
+        .order_by(UserOverride.email_address)
+    ).scalars()
+    users_to_delete = [(user.email_address, user.blocked_on) for user in results]
+
+    if not users_to_delete:
+        logging.info("No users found to delete, exiting")
+        return
+
+    confirmation_prompt = f"""
+    Are you sure you want to delete the following {len(users_to_delete)} users?
+    {format_user_strings(users_to_delete)}
+    """
+
+    if not prompt_for_confirmation(confirmation_prompt):
+        return
+
+    delete_users_from_table(session, UserOverride, users_to_delete)
+    delete_users_from_table(session, PermissionsOverride, users_to_delete)
+
+
+def parse_arguments(argv: list[str]) -> tuple[argparse.Namespace, list[str]]:
+    """Parses the arguments needed to call the delete_blocked_users function."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--project_id",
+        dest="project_id",
+        type=str,
+        choices=[GCP_PROJECT_STAGING, GCP_PROJECT_PRODUCTION],
+        required=False,
+    )
+
+    parser.add_argument(
+        "--state_code",
+        type=StateCode,
+        choices=list(StateCode),
+        required=True,
+    )
+
+    return parser.parse_known_args(argv)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    known_args, _ = parse_arguments(sys.argv)
+
+    db_key = SQLAlchemyDatabaseKey.for_schema(SchemaType.CASE_TRIAGE)
+    if known_args.project_id:
+        with local_project_id_override(
+            known_args.project_id
+        ), cloudsql_proxy_control.connection(
+            schema_type=SchemaType.CASE_TRIAGE,
+        ), SessionFactory.for_proxy(
+            db_key
+        ) as global_session:
+            delete_blocked_users(global_session, known_args.state_code.value)
+    else:
+        if not in_development():
+            raise RuntimeError(
+                "Expected to be called inside a docker container or with the --project_id argument. See usage in docstring"
+            )
+        SQLAlchemyEngineManager.init_engine(db_key)
+        with SessionFactory.using_database(db_key) as global_session:
+            delete_blocked_users(global_session, known_args.state_code.value)
