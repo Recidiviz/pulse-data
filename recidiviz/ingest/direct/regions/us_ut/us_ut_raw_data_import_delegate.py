7d03639b4142a210081b9748dd5e14c0005a4d97
ethanoro <ethan@recidiviz.org>
2025-03-10 10:31:57 -0400

    [raw data] add chunked file logic for `sprvsn_cntct` for utah (Recidiviz/recidiviz-data#39219)
    ## Description of the change
    
    adds logic for grouping an arbitrary number of ascending suffixes for
    chunked files
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Prework for https://github.com/Recidiviz/recidiviz-data/issues/38990
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: df8beb9657e6fa4dd819a9c7f5537bc259a2f4c0

--- recidiviz/ingest/direct/regions/us_ut/us_ut_raw_data_import_delegate.py
+++ recidiviz/ingest/direct/regions/us_ut/us_ut_raw_data_import_delegate.py
@@ -20,6 +20,9 @@ from recidiviz.ingest.direct.raw_data.base_raw_data_import_delegate import (
     BaseRawDataImportDelegate,
     skipped_error_for_unrecognized_file_tag_for_chunked_files,
 )
+from recidiviz.ingest.direct.raw_data.mixins.sequential_chunked_file_mixin import (
+    SequentialChunkedFileMixin,
+)
 from recidiviz.ingest.direct.types.raw_data_import_types import (
     RawBigQueryFileMetadata,
     RawDataFilesSkippedError,
@@ -27,11 +30,17 @@ from recidiviz.ingest.direct.types.raw_data_import_types import (
 )
 
 
-class UsUtRawDataImportDelegate(BaseRawDataImportDelegate):
+class UsUtRawDataImportDelegate(BaseRawDataImportDelegate, SequentialChunkedFileMixin):
     def coalesce_chunked_files(
         self, file_tag: str, gcs_files: list[RawGCSFileMetadata]
     ) -> tuple[list[RawBigQueryFileMetadata], list[RawDataFilesSkippedError]]:
-        """As of 3/6/2025, there are no chunked files in US_UT"""
+        """sprvsn_cntc is the only chunked file in Utah."""
+
+        if file_tag == "sprvsn_cntc":
+            return SequentialChunkedFileMixin.group_files_with_sequential_suffixes(
+                file_tag=file_tag, gcs_files=gcs_files
+            )
+
         return skipped_error_for_unrecognized_file_tag_for_chunked_files(
             file_tag=file_tag, gcs_files=gcs_files
         )
