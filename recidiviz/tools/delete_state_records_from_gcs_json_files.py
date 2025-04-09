b7a66d56cd7dfc1a7aeb23b1fab4c04f2840d6af
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2025-04-09 15:35:32 -0700

    add script to scrub state data from JSON files in a GCS bucket (Recidiviz/recidiviz-data#40711)
    ## Description of the change
    
    Adds a small script to loop over all JSON files in a bucket, download
    them, get rid of any rows where the state_code matches the one passed
    in, and re-upload them.
    
    Tested by printing the values from one of the files (which had US_OR
    data) and seeing that the US_OR rows were removed and the rest remained.
    Once this is approved, I'll run it against staging to make sure the
    re-upload works, make any needed changes to the script, and then merge.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 4d67046d1cf9c149c5487e1749b81375b1900e9e

---
+++ recidiviz/tools/delete_state_records_from_gcs_json_files.py
@@ -0,0 +1,75 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Script to scrub state data from all JSON files in a bucket. Each file in the bucket must contain
+lines of JSON values, and the JSON must have a state_code attribute.
+
+Example usage:
+python -m recidiviz.tools.delete_state_records_from_gcs_json_files --bucket_name recidiviz-staging-snooze-status-archive --state_code US_OR
+"""
+import argparse
+import json
+import logging
+import sys
+
+from recidiviz.cloud_storage.gcsfs_factory import GcsfsFactory
+from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
+
+
+def delete_state_records(bucket_name: str, state_code: str) -> None:
+    gcsfs = GcsfsFactory.build()
+    files = gcsfs.ls_with_blob_prefix(bucket_name, "")
+    for file in files:
+        if not isinstance(file, GcsfsFilePath):
+            continue
+        if file.extension != "json":
+            raise RuntimeError(
+                f"Expected all files in bucket to have .json extension, found {file.file_name}"
+            )
+        new_file_contents = ""
+        with gcsfs.open(file) as f:
+            rows_to_keep = [
+                row
+                for row in f.readlines()
+                if json.loads(row).get("state_code") != state_code
+            ]
+            new_file_contents = "".join(rows_to_keep)
+        gcsfs.upload_from_string(file, new_file_contents, "application/json")
+        logging.info("Removed %s data from %s", state_code, file.abs_path())
+
+
+def parse_arguments(argv: list[str]) -> tuple[argparse.Namespace, list[str]]:
+    """Parses the required arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--bucket_name",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--state_code",
+        required=True,
+    )
+
+    return parser.parse_known_args(argv)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    known_args, _ = parse_arguments(sys.argv)
+
+    delete_state_records(known_args.bucket_name, known_args.state_code)
