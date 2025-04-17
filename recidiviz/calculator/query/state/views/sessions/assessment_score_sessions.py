c9c8edde1b65da8a22f4ba6808c0855462431e8f
Maggie Hilderbran <maggie.hilderbran@me.com>
2025-04-17 14:53:30 -0400

    [Sessions] Add STRONG-R 2.0 assessments to `assessment_score_sessions` (Recidiviz/recidiviz-data#40967)
    ## Description of the change
    
    Added STRONG-R 2.0 assessments to the `assessment_score_sessions` view.
    (TN has started using the STRONG-R 2.0, and we need this data to flow
    through the sessions view for use downstream.)
    
    To load views to sandbox for validation:
    ```
    python -m recidiviz.tools.load_views_to_sandbox \
    --sandbox_dataset_prefix mhilderbran_us_tn_strong_r2 --prompt auto \
    --load_up_to_addresses task_eligibility.all_tasks
    ```
    
    We can confirm that we now see `STRONG_R2` assessments in
    `assessment_score_sessions`:
    ```
    SELECT DISTINCT
      assessment_type,
    FROM `recidiviz-staging.mhilderbran_us_tn_strong_r2_sessions.assessment_score_sessions_materialized`
    ```
    
    The following query checks `all_tasks` to see how overall eligibility
    changes for opportunities as a result of this PR—we see some small
    changes for TN (expected) alongside some small changes for AZ
    (unexpected, but I can't trace the issue to anything in my PR so I think
    it's probably due to some data weirdness in staging):
    ```
    WITH main AS (
      SELECT
        state_code,
        task_name,
        COUNT(DISTINCT IF(is_eligible, person_id, NULL)) AS n_jii_eligible_main,
        COUNT(DISTINCT IF(is_almost_eligible, person_id, NULL)) AS n_jii_almost_eligible_main,
      FROM `recidiviz-staging.task_eligibility.all_tasks_materialized`
      WHERE CURRENT_DATE BETWEEN start_date AND COALESCE(DATE_SUB(end_date, INTERVAL 1 DAY), '9999-12-31')
      GROUP BY 1, 2
    ),
    sandbox AS (
      SELECT
        state_code,
        task_name,
        COUNT(DISTINCT IF(is_eligible, person_id, NULL)) AS n_jii_eligible_sandbox,
        COUNT(DISTINCT IF(is_almost_eligible, person_id, NULL)) AS n_jii_almost_eligible_sandbox,
      FROM `recidiviz-staging.mhilderbran_us_tn_strong_r2_task_eligibility.all_tasks_materialized`
      WHERE CURRENT_DATE BETWEEN start_date AND COALESCE(DATE_SUB(end_date, INTERVAL 1 DAY), '9999-12-31')
      GROUP BY 1, 2
    )
    SELECT *
    FROM main
    FULL OUTER JOIN sandbox
      USING (state_code, task_name)
    WHERE (COALESCE(n_jii_eligible_main, 0)!=COALESCE(n_jii_eligible_sandbox, 0))
      OR (COALESCE(n_jii_almost_eligible_main, 0)!=COALESCE(n_jii_almost_eligible_sandbox, 0))
    ORDER BY state_code, task_name
    ```
    
    ## Related issues
    
    N/A
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 62b4027f4a8b6674ddcad3148b1474d63022ad59

--- recidiviz/calculator/query/state/views/sessions/assessment_score_sessions.py
+++ recidiviz/calculator/query/state/views/sessions/assessment_score_sessions.py
@@ -56,8 +56,7 @@ WITH state_assessment AS (
         assessment_score_bucket,
         assessment_metadata,
         sequence_num,
-    FROM
-        `{project_id}.{normalized_state_dataset}.state_assessment`
+    FROM `{project_id}.{normalized_state_dataset}.state_assessment`
     WHERE
         -- keep only relevant assessment types
         (
@@ -70,7 +69,7 @@ WITH state_assessment AS (
             OR (
                 state_code != "US_MI"
                 AND (
-                    assessment_type IN ("LSIR", "STRONG_R", "CAF", "CSRA", "ACCAT", "TX_CSST", "TX_CST", "TX_RT", "TX_SRT", "LS_RNR", "CCRRA")
+                    assessment_type IN ("LSIR", "STRONG_R", "STRONG_R2", "CAF", "CSRA", "ACCAT", "TX_CSST", "TX_CST", "TX_RT", "TX_SRT", "LS_RNR", "CCRRA")
                     OR assessment_type LIKE "ORAS%"
                 )
             )
