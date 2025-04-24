dcfe73ebf8c926e32c0be14cf44242fc30456ba1
Justine <justine@recidiviz.org>
2025-04-24 09:43:52 -0400

    [US_IA] Use sentence_sessions for sentence criteria (Recidiviz/recidiviz-data#41409)
    ## Description of the change
    
    Small switch to use the sentencing tables in `sentence_sessions` instead
    of the v1 versions in `sessions` for the IA ineligible offense criteria.
    There are 100 fewer ineligible criteria spans with this change since the
    v2 version closes out older sentences more accurately (using sentence
    status snapshot) and so older completed ineligible sentences are dropped
    in this update.
    
    ## Related issues
    
    Relates to Recidiviz/recidiviz-data#39262
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [X] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 33ab0b09a6685d43e8e151b1223ac65048130b14

--- recidiviz/task_eligibility/criteria/state_specific/us_ia/not_serving_ineligible_offense_for_early_discharge.py
+++ recidiviz/task_eligibility/criteria/state_specific/us_ia/not_serving_ineligible_offense_for_early_discharge.py
@@ -20,7 +20,7 @@ and https://www.legis.iowa.gov/law/iowaCode/chapters?title=XVI&year=2025 for Iow
 
 from google.cloud import bigquery
 
-from recidiviz.calculator.query.state.dataset_config import SESSIONS_DATASET
+from recidiviz.calculator.query.state.dataset_config import SENTENCE_SESSIONS_DATASET
 from recidiviz.common.constants.states import StateCode
 from recidiviz.task_eligibility.reasons_field import ReasonsField
 from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
@@ -43,22 +43,22 @@ WITH clean_offenses AS (
   SELECT *, 
     SPLIT(REGEXP_REPLACE(statute, '[,|/(]', '#'), '#')[SAFE_OFFSET(0)] AS clean_statute, -- remove anything after parentheses/commas - for example, transform 707.3(A) -> 707.3
     CONCAT(COALESCE(statute, ''), ' ', COALESCE(description, '')) AS offense,
-  FROM `{{project_id}}.{{sessions_dataset}}.sentences_preprocessed_materialized`
+  FROM `{{project_id}}.{{sentence_sessions_dataset}}.sentences_and_charges_materialized`
   WHERE state_code = 'US_IA'
 ), preprocessed_spans as (
   SELECT
     span.state_code,
     span.person_id,
     span.start_date,
-    span.end_date,
+    span.end_date_exclusive AS end_date,
     LOGICAL_OR({is_veh_hom()}) as veh_homicide_indicator, 
     LOGICAL_OR({is_fleeing()}) as fleeing_indicator, 
     LOGICAL_OR({is_other_ed_ineligible_offense()})  AS other_ed_ineligible_offense_indicator,
     ARRAY_AGG(DISTINCT offense ORDER BY offense) AS ineligible_offenses,
-  FROM `{{project_id}}.{{sessions_dataset}}.sentence_spans_materialized` span,
-  UNNEST (sentences_preprocessed_id_array_actual_completion) AS sentences_preprocessed_id
+  FROM `{{project_id}}.{{sentence_sessions_dataset}}.overlapping_sentence_serving_periods_materialized` span,
+  UNNEST (sentence_id_array) AS sentence_id
   INNER JOIN clean_offenses
-    USING (state_code, person_id, sentences_preprocessed_id)
+    USING (state_code, person_id, sentence_id)
   WHERE span.state_code = "US_IA"
     AND ({is_veh_hom()} OR {is_fleeing()} OR {is_other_ed_ineligible_offense()})
   GROUP BY 1,2,3,4
@@ -80,7 +80,7 @@ VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = StateSpecificTaskCr
     description=_DESCRIPTION,
     state_code=StateCode.US_IA,
     criteria_spans_query_template=_REASON_QUERY,
-    sessions_dataset=SESSIONS_DATASET,
+    sentence_sessions_dataset=SENTENCE_SESSIONS_DATASET,
     meets_criteria_default=True,
     reasons_fields=[
         ReasonsField(
