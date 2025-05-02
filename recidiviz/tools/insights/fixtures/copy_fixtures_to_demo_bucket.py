de399cce4a5e4e4351f29fb19181c3720f90c5d9
Michelle Orden <82831800+morden35@users.noreply.github.com>
2025-05-02 10:54:14 -0500

    [Insights] Script to upload fixture files GCS demo bucket  (Recidiviz/recidiviz-data#41835)
    ## Description of the change
    
    Python script to copy the backend fixture files to the new demo etl
    bucket created in Recidiviz/recidiviz-data#41576
    
    I ran the script successfully, and the files can now be found
    [here](https://console.cloud.google.com/storage/browser/recidiviz-staging-insights-etl-data-demo?inv=1&invt=AbwQtg&project=recidiviz-staging&pageState=(%22StorageObjectListTable%22:(%22f%22:%22%255B%255D%22))).
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Related to https://github.com/Recidiviz/recidiviz-dashboards/issues/8120
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---------
    
    Co-authored-by: Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
    GitOrigin-RevId: 5b396f403e8ac4fa2e7e77337844527d6d2cb1fd

---
+++ recidiviz/tools/insights/fixtures/copy_fixtures_to_demo_bucket.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""
+Script that copies fixutre files to a demo GCP bucket.
+
+pipenv run python -m recidiviz.tools.insights.fixtures.copy_fixtures_to_demo_bucket
+"""
+from google.cloud import storage
+from recidiviz.calculator.query.state.views.outliers.outliers_enabled_states import (
+    get_outliers_enabled_states,
+)
+
+storage_client = storage.Client()
+bucket = storage_client.bucket("recidiviz-staging-insights-etl-data-demo")
+base_dir = "recidiviz/tools/insights/fixtures/"
+
+for state_code in get_outliers_enabled_states():
+    for file_name in [
+        "supervision_client_events.json",
+        "supervision_clients.json",
+        "supervision_district_managers.json",
+        "supervision_officer_metrics.json",
+        "supervision_officer_outlier_status.json",
+        "supervision_officer_supervisors.json",
+        "supervision_officers.json",
+        "supervision_state_metrics.json",
+        "metric_benchmarks.json",
+    ]:
+        blob = bucket.blob(f"{state_code}/{file_name}")
+        full_file_name = base_dir + file_name
+        blob.upload_from_filename(full_file_name)
+        print(f"File {full_file_name} uploaded to {state_code}/{file_name}.")
