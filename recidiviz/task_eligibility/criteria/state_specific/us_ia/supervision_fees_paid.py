5975f92b7ccf375c40bc4cdbc1eb64c18b12fa1f
Sophie Bair <56454281+seb2244@users.noreply.github.com>
2025-05-06 16:44:34 -0500

    [US_IA][Workflows] Create criterion for supervision fees paid  (Recidiviz/recidiviz-data#41865)
    ## Description of the change
    
    Write criterion for Iowa early discharge that determines whether someone
    has paid their most recent supervision fee.
    
    Note - there are still a lot of edge cases to iron out in this logic
    (hence the many TODOs) but I wanted to get something merged in by the
    end of this week due to the TT launch timeline, and this should work in
    the majority of cases.
    
    Ran a sandbox to validate changes using the following command
    ```
    python -m recidiviz.tools.load_views_to_sandbox --project_id recidiviz-staging --sandbox_dataset_prefix sbair3 --prompt auto --load_changed_views_only
    ```
    This change results in the number of people eligible for early discharge
    decreasing from 17228 -> 8466.
    ```
    select *
    from `sbair3_task_eligibility_spans_us_ia.complete_early_discharge_form_materialized`
    where is_eligible
    and current_date() between start_date and coalesce(end_date, current_date())
    ```
    Note that we have also not gotten updated fines & fees data since
    2/11/25, so hopefully this eligibility will increase a bit once we take
    into account everyone who has made payments in the past 3 months.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#39263
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 948ef01a25c749162e80eb0f10d4e6be6a242235

--- recidiviz/task_eligibility/criteria/state_specific/us_ia/supervision_fees_paid.py
+++ recidiviz/task_eligibility/criteria/state_specific/us_ia/supervision_fees_paid.py
@@ -14,18 +14,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ============================================================================
-"""Shows spans of time during which a client has paid their supervision fees in full"""
-# TODO(#39263) - create criterion for supervision fees paid
+"""Shows spans of time during which a client has paid off their most recent supervision fee"""
 
 from google.cloud import bigquery
 
+from recidiviz.calculator.query.bq_utils import (
+    MAGIC_END_DATE,
+    revert_nonnull_end_date_clause,
+)
+from recidiviz.calculator.query.sessions_query_fragments import sessionize_ledger_data
 from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.dataset_config import raw_latest_views_dataset_for_region
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.ingest.views.dataset_config import NORMALIZED_STATE_DATASET
 from recidiviz.task_eligibility.reasons_field import ReasonsField
 from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
     StateSpecificTaskCriteriaBigQueryViewBuilder,
-)
-from recidiviz.task_eligibility.utils.placeholder_criteria_builders import (
-    state_specific_placeholder_criteria_view_builder,
 )
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
@@ -34,17 +38,80 @@ _CRITERIA_NAME = "US_IA_SUPERVISION_FEES_PAID"
 
 _DESCRIPTION = __doc__
 
+_REASON_QUERY = f"""
+WITH supervision_fees AS (
+/* pulls all applicable supervision fees and their current status/balances */ 
+    SELECT state_code,
+        person_id, 
+        DATE(IntakeDate) AS IntakeDate, 
+        InitialBalance, 
+        CurrentBalance, 
+        /* if the fee was paid in full, assume it was paid on the close date (or the last update date if close date is missing) */
+        CASE WHEN FeeStatusDesc = 'Paid in Full' THEN COALESCE(DATE(CloseDate), DATE(DateOfLastUpdate)) END AS close_date,
+    FROM `{{project_id}}.{{raw_data_up_to_date_views_dataset}}.IA_DOC_Fees_latest` fee
+    INNER JOIN `{{project_id}}.{{normalized_state_dataset}}.state_person_external_id` pei 
+        ON fee.IconNumber = pei.external_id
+        AND pei.state_code = 'US_IA'
+        AND pei.id_type = 'US_IA_OFFENDERCD'
+    WHERE UPPER(FeeDescription) LIKE '%SUPERVISION%'
+        AND FeeStatusDesc IN ('Active', 'Paid in Full') -- do not include deleted fees
+        # TODO(#41880) figure out how to deal with legitimate fee waivers
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY person_id, IntakeDate ORDER BY FeeStatusDesc DESC) = 1 -- if two supervision fees were imposed on the same date, assume that they are duplicates and preferentially take the fee that was paid in full
+    # TODO(#41884) validate cases with TTs and figure out if we want to set a threshold here to exclude old supervision fees 
+), supervision_fees_clean AS (
+/* cleans up edge cases where the fee was marked paid on or before the intake date by making a fake span from the intake date -> 1 day after */
+    SELECT 
+        state_code,
+        person_id,
+        IntakeDate,
+        InitialBalance, 
+        CurrentBalance,
+        IF(close_date IS NOT NULL AND close_date <= IntakeDate, DATE_ADD(IntakeDate, INTERVAL 1 DAY), close_date) AS close_date
+    FROM supervision_fees
+), supervision_fee_spans AS (
+/* sessionizes by intake date so that only the most recent supervision fee applies at a time */
+    {sessionize_ledger_data(
+        table_name = 'supervision_fees_clean',
+        index_columns = ['state_code', 'person_id'],
+        update_column_name = 'IntakeDate',
+        attribute_columns = ['InitialBalance', 'CurrentBalance', 'close_date']
+    )}
+)
+SELECT state_code,
+    person_id,
+    start_date, 
+    /* they should stop being ineligible when the fee is paid or when the span ends, whichever comes sooner */
+    {revert_nonnull_end_date_clause(f'LEAST(IFNULL(close_date, "{MAGIC_END_DATE}"), IFNULL(end_date_exclusive, "{MAGIC_END_DATE}"))')} AS end_date,
+    FALSE AS meets_criteria,
+    InitialBalance AS initial_balance,
+    CurrentBalance AS current_balance,
+    TO_JSON(STRUCT(InitialBalance AS initial_balance, CurrentBalance AS current_balance)) AS reason,
+FROM supervision_fee_spans
+"""
+
 VIEW_BUILDER: StateSpecificTaskCriteriaBigQueryViewBuilder = (
-    state_specific_placeholder_criteria_view_builder(
+    StateSpecificTaskCriteriaBigQueryViewBuilder(
         criteria_name=_CRITERIA_NAME,
         description=_DESCRIPTION,
         state_code=StateCode.US_IA,
+        normalized_state_dataset=NORMALIZED_STATE_DATASET,
+        raw_data_up_to_date_views_dataset=raw_latest_views_dataset_for_region(
+            state_code=StateCode.US_IA,
+            instance=DirectIngestInstance.PRIMARY,
+        ),
+        criteria_spans_query_template=_REASON_QUERY,
+        meets_criteria_default=True,
         reasons_fields=[
             ReasonsField(
-                name="supervision_fee_balance",
-                type=bigquery.enums.StandardSqlTypeNames.INT64,
-                description="Remaining balance on supervision fees",
-            )
+                name="initial_balance",
+                type=bigquery.enums.StandardSqlTypeNames.FLOAT64,
+                description="Initial balance on most recent supervision fee",
+            ),
+            ReasonsField(
+                name="current_balance",
+                type=bigquery.enums.StandardSqlTypeNames.FLOAT64,
+                description="Current balance on most recent supervision fee",
+            ),
         ],
     )
 )
