28b3b1840c9d20dad89d7ae4a294db918efefd26
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2025-05-14 17:23:41 -0500

    [US_ND] Aggregate adjacent ORU spans (Recidiviz/recidiviz-data#42417)
    ## Description of the change
    
    This PR aggregates adjacent ORU spans so that we don't restart the 3
    week count if someone gets transferred from an ORU cell to another one.
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 597c6ad89106dcead705bac64bdd6c6ccb2565ab

--- recidiviz/task_eligibility/criteria/state_specific/us_nd/not_in_an_orientation_unit.py
+++ recidiviz/task_eligibility/criteria/state_specific/us_nd/not_in_an_orientation_unit.py
@@ -25,6 +25,7 @@ from recidiviz.calculator.query.bq_utils import (
     nonnull_end_date_clause,
     nonnull_end_date_exclusive_clause,
 )
+from recidiviz.calculator.query.sessions_query_fragments import aggregate_adjacent_spans
 from recidiviz.calculator.query.state.dataset_config import SESSIONS_DATASET
 from recidiviz.common.constants.states import StateCode
 from recidiviz.task_eligibility.reasons_field import ReasonsField
@@ -37,12 +38,47 @@ from recidiviz.utils.metadata import local_project_id_override
 _CRITERIA_NAME = "US_ND_NOT_IN_AN_ORIENTATION_UNIT"
 
 _QUERY_TEMPLATE = f"""
+WITH oru_sessions AS (
+    -- This query gets the sessions of people in orientation units
+    -- in the NDSP and DWCRC facilities.
+    SELECT 
+        state_code,
+        person_id,
+        start_date,
+        end_date_exclusive AS end_date,
+    FROM `{{project_id}}.{{sessions_dataset}}.housing_unit_sessions_materialized`
+    WHERE state_code = 'US_ND'
+        -- Folks on ORU in NDSP are in an orientation unit
+        AND ((facility = 'NDSP' AND REGEXP_CONTAINS(housing_unit, r'ORU'))
+        -- Folks on HZN-E in DWCRC are in an orientation unit
+            OR (facility = 'DWCRC' AND REGEXP_CONTAINS(housing_unit, r'HZN-E')))
+        AND start_date < {nonnull_end_date_exclusive_clause('end_date_exclusive')}
+    GROUP BY 1,2,3,4
+),
+oru_sessions_agg AS (
+    -- This query aggregates the sessions of people in orientation units
+    -- to get the start and end dates of their time in orientation units.
+    -- It also joins with the housing unit sessions to get the housing unit
+    -- where the person was placed.
+    SELECT 
+        oru.state_code,
+        oru.person_id,
+        oru.start_date,
+        oru.end_date,
+        STRING_AGG(hus.housing_unit ORDER BY hus.start_date) AS housing_unit,
+    FROM ({aggregate_adjacent_spans(table_name='oru_sessions')}) oru
+    INNER JOIN `{{project_id}}.{{sessions_dataset}}.housing_unit_sessions_materialized` hus
+        USING (state_code, person_id)
+    WHERE hus.start_date < {nonnull_end_date_exclusive_clause('oru.end_date')}
+        AND oru.start_date < {nonnull_end_date_exclusive_clause('hus.end_date_exclusive')}
+    GROUP BY 1,2,3,4
+)
 SELECT 
     state_code,
     person_id,
     start_date,
     LEAST(
-        {nonnull_end_date_clause('end_date_exclusive')},
+        {nonnull_end_date_clause('end_date')},
         DATE_ADD(start_date, INTERVAL 3 WEEK)
     ) AS end_date,
     FALSE AS meets_criteria,
@@ -50,13 +86,7 @@ SELECT
                    ANY_VALUE(housing_unit) AS housing_unit)) AS reason,
     start_date AS housing_unit_start_date,
     ANY_VALUE(housing_unit) AS housing_unit,
-FROM `{{project_id}}.{{sessions_dataset}}.housing_unit_sessions_materialized`
-WHERE state_code = 'US_ND'
-    -- Folks on ORU in NDSP are in an orientation unit
-    AND ((facility = 'NDSP' AND REGEXP_CONTAINS(housing_unit, r'ORU'))
-    -- Folks on HZN-E in DWCRC are in an orientation unit
-        OR (facility = 'DWCRC' AND REGEXP_CONTAINS(housing_unit, r'HZN-E')))
-    AND start_date < {nonnull_end_date_exclusive_clause('end_date_exclusive')}
+FROM oru_sessions_agg
 GROUP BY 1,2,3,4,5
 """
 
