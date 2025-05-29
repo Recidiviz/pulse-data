8963fe58e74ac2c9adca01394a70fa1c40c37fdb
Maggie Hilderbran <maggie.hilderbran@me.com>
2025-05-29 12:27:05 -0400

    [US_MO] Create TES stubs for work release (Recidiviz/recidiviz-data#43023)
    ## Description of the change
    
    Created stubs for MO work release (for the eligibility spans and for one
    of the criteria) to get us started and facilitate further development
    work.
    
    To load stub views to sandbox (to confirm that they at least don't cause
    any immediate problems):
    ```
    python -m recidiviz.tools.load_views_to_sandbox \
    --project_id recidiviz-staging \
    --sandbox_dataset_prefix mhilderbran_us_mo_work_release_stubs manual \
    --view_ids_to_load task_eligibility_criteria_us_mo.no_current_or_prior_excluded_offenses_work_release,task_eligibility_spans_us_mo.work_release \
    --update_ancestors False --update_descendants False
    ```
    
    Validation notes:
    - We can see that the
    `US_MO_NO_CURRENT_OR_PRIOR_EXCLUDED_OFFENSES_WORK_RELEASE` has
    placeholder data, as expected:
    ```
    SELECT *
    FROM `mhilderbran_us_mo_work_release_stubs_task_eligibility_criteria_us_mo.no_current_or_prior_excluded_offenses_work_release_materialized`
    LIMIT 50
    ```
    - The eligibility-spans view is empty, as expected:
    ```
    SELECT *
    FROM `mhilderbran_us_mo_work_release_stubs_task_eligibility_spans_us_mo.work_release_materialized`
    LIMIT 50
    ```
    
    ## Related issues
    
    Related to Recidiviz/recidiviz-data#42982
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6a8eec7708484a53ed69ef9f5c08ee1e0828dec1

---
+++ recidiviz/task_eligibility/eligibility_spans/us_mo/work_release.py
@@ -0,0 +1,52 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Task eligibility spans view that shows the spans of time when someone in MO is
+eligible for work release.
+"""
+
+from recidiviz.common.constants.states import StateCode
+from recidiviz.task_eligibility.candidate_populations.general import (
+    incarceration_population_state_prison,
+)
+from recidiviz.task_eligibility.completion_events.general import granted_work_release
+from recidiviz.task_eligibility.criteria.state_specific.us_mo import (
+    no_current_or_prior_excluded_offenses_work_release,
+)
+from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
+    SingleTaskEligibilitySpansBigQueryViewBuilder,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
+    state_code=StateCode.US_MO,
+    task_name="WORK_RELEASE",
+    description=__doc__,
+    # TODO(#42982): Ensure that this is the correct candidate population.
+    candidate_population_view_builder=incarceration_population_state_prison.VIEW_BUILDER,
+    # TODO(#42982): Finish adding in criteria and filling in stubs.
+    criteria_spans_view_builders=[
+        no_current_or_prior_excluded_offenses_work_release.VIEW_BUILDER,
+    ],
+    # TODO(#42982): Update this to be the correct completion event (either general or
+    # state-specific) for this opportunity.
+    completion_event_builder=granted_work_release.VIEW_BUILDER,
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
