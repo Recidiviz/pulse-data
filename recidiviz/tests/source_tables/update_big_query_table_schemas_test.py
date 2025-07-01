b75dd1cdb0e05fdd8c5e1846a1cdd3475a5ec265
Linda Liu <162385693+lindaliu0718@users.noreply.github.com>
2025-07-01 15:53:06 -0400

    [US_UT] Create "class_assignment" ingest view and mappings (Recidiviz/recidiviz-data#44728)
    ## Description of the change
    
    > This PR creates the `class_assignment` ingest view and mappings to get
    a complete picture of whether a person has made progress toward the
    goals outlined in their CAP. The three relevant raw data tables (`cls`,
    `cls_sctn`, and `ofndr_cls_sctn`) were imported in Recidiviz/recidiviz-data#44648.
    
    [Sandbox
    run](https://console.cloud.google.com/dataflow/jobs/us-east1/2025-07-01_11_37_13-17470804238784157738)
    in process
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#42346
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 570676f464c50f8f59b1b0ba2a9bc392606d59c8

--- recidiviz/tests/source_tables/update_big_query_table_schemas_test.py
+++ recidiviz/tests/source_tables/update_big_query_table_schemas_test.py
@@ -1998,6 +1998,7 @@ EXPECTED_DATAFLOW_OUTPUT_TABLES = [
     "us_tx_state.state_supervision_violation_type_entry",
     "us_tx_state.state_task_deadline",
     "us_ut_ingest_view_results.assessment",
+    "us_ut_ingest_view_results.class_assignment",
     "us_ut_ingest_view_results.drug_screen",
     "us_ut_ingest_view_results.employment_period",
     "us_ut_ingest_view_results.incarceration_incident",
