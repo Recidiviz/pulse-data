63e0f2ff6bb003e55611480bd94a85b967808be9
Dan Hansen <dan@recidiviz.org>
2025-08-20 11:38:16 -0700

    [Emulator] Bump to `v0.4.4-recidiviz.23.1` (Recidiviz/recidiviz-data#46778)
    ## Description of the change
    
    Restores the `arm64` image since the new version fixes the EOF crash
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: bdbb76dc5b6c6a3e5b915612f37ebff15e120f02

--- recidiviz/tests/utils/big_query_emulator_control.py
+++ recidiviz/tests/utils/big_query_emulator_control.py
@@ -43,9 +43,8 @@ from recidiviz.tests.test_setup_utils import (
 # TODO(#20786): Migrate back to the goccy version by removing the comment about PAT and replacing recidiviz with
 #  goccy below.
 EMULATOR_IMAGE_REPOSITORY = "ghcr.io/recidiviz/bigquery-emulator"
-EMULATOR_TAG = "0.4.4-recidiviz.21.3"
-EMULATOR_IMAGE = f"{EMULATOR_IMAGE_REPOSITORY}:{EMULATOR_TAG}"
-EMULATOR_PLATFORM = "linux/amd64"
+EMULATOR_VERSION = "0.4.4-recidiviz.23.1"
+EMULATOR_IMAGE = f"{EMULATOR_IMAGE_REPOSITORY}:{EMULATOR_VERSION}"
 
 EMULATOR_ENTRYPOINT = "/bin/bigquery-emulator"
 
@@ -81,9 +80,7 @@ class BigQueryEmulatorControl:
         try:
             self.docker_client.images.get(EMULATOR_IMAGE)
         except errors.APIError:
-            self.docker_client.images.pull(
-                EMULATOR_IMAGE_REPOSITORY, EMULATOR_TAG, platform=EMULATOR_PLATFORM
-            )
+            self.docker_client.images.pull(EMULATOR_IMAGE_REPOSITORY, EMULATOR_VERSION)
 
     def start_emulator(self, input_schema_json_path: str | None = None) -> None:
         """Starts the emulator container. Optionally mounts source tables volume"""
@@ -108,7 +105,6 @@ class BigQueryEmulatorControl:
             ports={self.port: self.port},
             detach=True,
             volumes=volumes,
-            platform=EMULATOR_PLATFORM,
         )
 
         start_time = time.perf_counter()
