f3c7dc31428874498fa85701ae4826ec66208831
Daniel Allen <daniel@recidiviz.org>
2025-09-18 10:07:41 -0400

    [US_NE] Update STABLE assessment criteria for Tasks (Recidiviz/recidiviz-data#47747)
    ## Description of the change
    
    Updates Tasks criteria to reflect that STABLE assessments are not
    required after 10 consecutive years on parole, or for women.
    
    Sandbox run:
    ```
    python -m recidiviz.tools.load_views_to_sandbox \
    --sandbox_dataset_prefix dallen_ne \
    --state_code_filter US_NE manual \
    --view_ids_to_load task_eligibility_criteria_general.on_parole_at_least_10_years,task_eligibility_criteria_general.not_on_parole_at_least_10_years,task_eligibility_criteria_us_ne.meets_stable_assessment_event_triggers,task_eligibility_criteria_us_ne.needs_stable_assessment,workflows_views.us_ne_supervision_tasks_record \
    --update_ancestors False \
    --update_descendants False
    ```
    
    Validation notes
    
    The number of open STABLE assessment tasks drops from 183 to 174.
    
    Sandbox task counts:
    ```
    select JSON_VALUE(task, '$.type') AS task_type, count(*) as count from `recidiviz-staging.dallen_ne_workflows_views.us_ne_supervision_tasks_record_materialized`, UNNEST(tasks) AS task GROUP BY 1 ORDER BY 1
    ```
    <img width="348" height="176" alt="image"
    src="https://github.com/user-attachments/assets/7926b063-46e7-4efe-826b-b3c721e2b5df"
    />
    
    Main task counts:
    ```
    select JSON_VALUE(task, '$.type') AS task_type, count(*) as count from `recidiviz-staging.workflows_views.us_ne_supervision_tasks_record_materialized`, UNNEST(tasks) AS task GROUP BY 1 ORDER BY 1
    ```
    <img width="339" height="181" alt="image"
    src="https://github.com/user-attachments/assets/fb06f0bc-9367-4e11-952c-5505ee45f9f5"
    />
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 21b35a099a6209201146957cb923987603e3de9f

---
+++ recidiviz/task_eligibility/criteria/general/on_parole_at_least_10_years.py
@@ -0,0 +1,45 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Defines a criterion span view that shows spans of time during which someone has
+served at least 10 years on parole.
+"""
+
+from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
+    StateAgnosticTaskCriteriaBigQueryViewBuilder,
+)
+from recidiviz.task_eligibility.utils.general_criteria_builders import (
+    get_minimum_time_served_criteria_query,
+)
+from recidiviz.utils.environment import GCP_PROJECT_STAGING
+from recidiviz.utils.metadata import local_project_id_override
+
+_CRITERIA_NAME = "ON_PAROLE_AT_LEAST_10_YEARS"
+
+VIEW_BUILDER: StateAgnosticTaskCriteriaBigQueryViewBuilder = (
+    get_minimum_time_served_criteria_query(
+        criteria_name=_CRITERIA_NAME,
+        description=__doc__,
+        minimum_time_served=10,
+        time_served_interval="YEAR",
+        compartment_level_1_types=["SUPERVISION", "SUPERVISION_OUT_OF_STATE"],
+        compartment_level_2_types=["PAROLE", "DUAL"],
+    )
+)
+
+if __name__ == "__main__":
+    with local_project_id_override(GCP_PROJECT_STAGING):
+        VIEW_BUILDER.build_and_print()
