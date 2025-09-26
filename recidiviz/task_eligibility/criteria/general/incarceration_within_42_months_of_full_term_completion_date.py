a6cb836288551ba35d9e37a4ceedb46500316786
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2025-09-26 12:25:53 -0500

    [US_ND] Switch to the right good time release dates (Recidiviz/recidiviz-data#48613)
    ## Description of the change
    
    We learned from ND that our current full term completion dates are
    wrong, we are not using the right good time release dates (GTRDs). This
    change will modify all the criteria queries from ND that involved GTRDs,
    so that we use the right ones.
    
    I uploaded these changes to a sandbox and they are looking as expected.
    
    ```
    python -m recidiviz.tools.load_views_to_sandbox \
            --project_id recidiviz-staging\
            --sandbox_dataset_prefix hsalas\
            --state_code_filter US_ND manual\
            --dataset_ids_to_load task_eligibility,task_eligibility_criteria_general,task_eligibility_spans_us_nd\
            --update_descendants False
    ```
    ## Related issues
    
    Closes Recidiviz/recidiviz-data#9479
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: d817784e7a1c483c9eb4e9a5ca1919f7e0a43eea

--- recidiviz/task_eligibility/criteria/general/incarceration_within_42_months_of_full_term_completion_date.py
+++ recidiviz/task_eligibility/criteria/general/incarceration_within_42_months_of_full_term_completion_date.py
@@ -18,30 +18,59 @@
 Defines a criteria span view that shows spans of time during which
 someone is incarcerated within 42 months of their full term completion date.
 """
+from google.cloud import bigquery
+
+from recidiviz.calculator.query.bq_utils import nonnull_end_date_clause
+from recidiviz.task_eligibility.reasons_field import ReasonsField
 from recidiviz.task_eligibility.task_criteria_big_query_view_builder import (
     StateAgnosticTaskCriteriaBigQueryViewBuilder,
 )
-from recidiviz.task_eligibility.utils.general_criteria_builders import (
-    is_past_completion_date_criteria_builder,
+from recidiviz.task_eligibility.utils.critical_date_query_fragments import (
+    critical_date_has_passed_spans_cte,
 )
 from recidiviz.utils.environment import GCP_PROJECT_STAGING
 from recidiviz.utils.metadata import local_project_id_override
 
+# TODO(#48612): Use is_past_completion_date_criteria_builder once discrepancy is fixed
 _CRITERIA_NAME = "INCARCERATION_WITHIN_42_MONTHS_OF_FULL_TERM_COMPLETION_DATE"
 
-_DESCRIPTION = """
-Defines a criteria span view that shows spans of time during which
-someone is incarcerated within 42 months of their full term completion date.
+_QUERY_TEMPLATE = f"""
+WITH critical_date_spans AS (
+    SELECT 
+        state_code,
+        person_id,
+        start_date AS start_datetime, 
+        end_date_exclusive AS end_datetime,
+        projected_completion_date_max AS critical_date
+    FROM `{{project_id}}.sessions.incarceration_projected_completion_date_spans_materialized`
+),
+{critical_date_has_passed_spans_cte(meets_criteria_leading_window_time = 42,
+                                    date_part='MONTH')}
+SELECT
+    state_code,
+    person_id,
+    start_date,
+    end_date,
+    critical_date_has_passed AS meets_criteria,
+    TO_JSON(STRUCT(critical_date AS full_term_completion_date)) AS reason,
+    critical_date AS full_term_completion_date,
+FROM critical_date_has_passed_spans
+WHERE start_date != {nonnull_end_date_clause('end_date')}
 """
 
 VIEW_BUILDER: StateAgnosticTaskCriteriaBigQueryViewBuilder = (
-    is_past_completion_date_criteria_builder(
-        compartment_level_1_filter="INCARCERATION",
-        meets_criteria_leading_window_time=42,
-        date_part="MONTH",
-        critical_date_name_in_reason="full_term_completion_date",
+    StateAgnosticTaskCriteriaBigQueryViewBuilder(
         criteria_name=_CRITERIA_NAME,
-        description=_DESCRIPTION,
+        criteria_spans_query_template=_QUERY_TEMPLATE,
+        description=__doc__,
+        meets_criteria_default=False,
+        reasons_fields=[
+            ReasonsField(
+                name="full_term_completion_date",
+                type=bigquery.enums.StandardSqlTypeNames.DATE,
+                description="Date when the critical date has passed",
+            ),
+        ],
     )
 )
 
