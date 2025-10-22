3869ec6ec7c349ec7f5e96d857af8f431c7bdbab
Emily Turner <149205316+emilyemilyemilyemilyemilyemily@users.noreply.github.com>
2025-10-22 14:37:18 -0700

    [operations] Add sql generator to verify bq file_ids align with operations db (Recidiviz/recidiviz-data#50725)
    ## Description of the change
    
    task to be added to the operations registration step of the import dag
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Recidiviz/recidiviz-data#8554
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [x] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 50276017c6c6fa2cf189030a450d026837ef7588

---
+++ recidiviz/airflow/dags/raw_data/verify_big_query_postgres_alignment_sql_query_generator.py
@@ -0,0 +1,234 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Generates SQL queries to verify all file_ids found in the BigQuery raw data table
+have a corresponding non-invalidated, processed entry in the `direct_ingest_raw_big_query_file_metadata`
+table in the operations postgres db.
+"""
+from collections import defaultdict
+
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.context import Context
+from more_itertools import one
+
+from recidiviz.airflow.dags.operators.cloud_sql_query_operator import (
+    CloudSqlQueryGenerator,
+    CloudSqlQueryOperator,
+)
+from recidiviz.airflow.dags.raw_data.metadata import SKIPPED_FILE_ERRORS
+from recidiviz.airflow.dags.raw_data.utils import logger
+from recidiviz.big_query.big_query_client import BigQueryClientImpl
+from recidiviz.cloud_resources.platform_resource_labels import (
+    IngestInstanceResourceLabel,
+    PlatformOrchestrationResourceLabel,
+    StateCodeResourceLabel,
+)
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.dataset_config import raw_tables_dataset_for_region
+from recidiviz.ingest.direct.gating import (
+    automatic_raw_data_pruning_enabled_for_state_and_instance,
+)
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.ingest.direct.types.raw_data_import_types import (
+    RawBigQueryFileMetadata,
+    RawDataFilesSkippedError,
+)
+from recidiviz.utils import metadata
+from recidiviz.utils.string import StrictStringFormatter
+
+POSTGRES_PROCESSED_BQ_FILE_IDS_QUERY = """
+SELECT DISTINCT file_id
+FROM direct_ingest_raw_big_query_file_metadata
+WHERE region_code = '{region_code}'
+AND raw_data_instance = '{raw_data_instance}'
+AND file_tag = '{file_tag}'
+AND is_invalidated IS FALSE
+AND file_processed_time IS NOT NULL
+"""
+
+BQ_DATA_FILE_IDS_QUERY = """
+SELECT DISTINCT file_id
+FROM `{project_id}.{raw_data_dataset_id}.{raw_data_table_id}`
+"""
+
+
+class VerifyBigQueryPostgresAlignmentSQLQueryGenerator(
+    CloudSqlQueryGenerator[list[str]]
+):
+    """Generates a SQL query to verify that the raw data table in BigQuery is aligned with the operations db."""
+
+    def __init__(
+        self,
+        state_code: StateCode,
+        raw_data_instance: DirectIngestInstance,
+        get_all_unprocessed_bq_file_metadata_task_id: str,
+    ) -> None:
+        super().__init__()
+        self._state_code = state_code
+        self._raw_data_instance = raw_data_instance
+        self._get_all_unprocessed_bq_file_metadata_task_id = (
+            get_all_unprocessed_bq_file_metadata_task_id
+        )
+        self._big_query_client = BigQueryClientImpl(
+            default_job_labels=[
+                StateCodeResourceLabel(value=self._state_code.value.lower()),
+                IngestInstanceResourceLabel(value=raw_data_instance.value.lower()),
+                PlatformOrchestrationResourceLabel.RAW_DATA_IMPORT_DAG.value,
+            ]
+        )
+        self._project_id = metadata.project_id()
+        self._raw_data_dataset_id = raw_tables_dataset_for_region(
+            self._state_code, self._raw_data_instance
+        )
+
+    def execute_postgres_query(
+        self,
+        operator: CloudSqlQueryOperator,
+        postgres_hook: PostgresHook,
+        context: Context,
+    ) -> list[str]:
+        """For each file tag pending import, verifies that all file_ids found in the BigQuery
+        raw data table have a corresponding processed, non-invalidated entry in the
+         `direct_ingest_raw_big_query_file_metadata` table in the operations postgres db.
+
+        Returns a serialized SkippedFileError for any files that fail this verification.
+        """
+        unprocessed_bq_file_metadata: list[RawBigQueryFileMetadata] = [
+            RawBigQueryFileMetadata.deserialize(xcom_metadata)
+            for xcom_metadata in operator.xcom_pull(
+                context,
+                key="return_value",
+                task_ids=self._get_all_unprocessed_bq_file_metadata_task_id,
+            )
+        ]
+
+        file_tag_to_bq_files = defaultdict(list)
+        for bq_file in unprocessed_bq_file_metadata:
+            file_tag_to_bq_files[bq_file.file_tag].append(bq_file)
+
+        skipped_file_errors: list[RawDataFilesSkippedError] = []
+        import_ready_bq_metadata: list[RawBigQueryFileMetadata] = []
+        # TODO(#8554) Run for all files once existing errors are fixed
+        # more immediate TODO(#50894) Gate on a per-file tag basis
+        should_run_validation = (
+            automatic_raw_data_pruning_enabled_for_state_and_instance(
+                self._state_code, self._raw_data_instance
+            )
+        )
+
+        for file_tag, bq_files in file_tag_to_bq_files.items():
+            try:
+                if should_run_validation:
+                    self._validate_bq_table_bq_metadata_alignment_for_file_tag(
+                        postgres_hook, file_tag
+                    )
+                import_ready_bq_metadata.extend(bq_files)
+            except Exception as e:
+                logger.error(
+                    "Error validating BigQuery/operations db alignment for file tag [%s]: %s",
+                    file_tag,
+                    e,
+                )
+                skipped_file_errors.extend(
+                    [
+                        RawDataFilesSkippedError(
+                            file_tag=bq_file.file_tag,
+                            file_paths=[
+                                gcs_file.path for gcs_file in bq_file.gcs_files
+                            ],
+                            skipped_message=str(e),
+                            update_datetime=bq_file.update_datetime,
+                        )
+                        for bq_file in bq_files
+                    ]
+                )
+
+        operator.xcom_push(
+            context=context,
+            key=SKIPPED_FILE_ERRORS,
+            value=[skipped_error.serialize() for skipped_error in skipped_file_errors],
+        )
+
+        return [metadata.serialize() for metadata in import_ready_bq_metadata]
+
+    def _validate_bq_table_bq_metadata_alignment_for_file_tag(
+        self,
+        postgres_hook: PostgresHook,
+        file_tag: str,
+    ) -> None:
+        """Validates that all file_ids found in the BigQuery raw data table have a corresponding non-invalidated
+        entry in the `direct_ingest_raw_big_query_file_metadata` table in the operations database. We don't validate
+        in the opposite direction because not every imported file will result in rows in the BigQuery raw table
+        (empty files, pruned files with no differences). However, if the BigQuery table is empty and there are non-invalidated
+        entries in the operations database, this is a condition that should never happen and we will fail.
+        """
+        bq_raw_table_file_ids = self._get_unique_bq_raw_table_file_ids(file_tag)
+
+        postgres_bq_metadata_file_ids = self._get_unique_postgres_bq_metadata_file_ids(
+            postgres_hook, file_tag
+        )
+
+        # We don't expect the first file for a file tag to be empty
+        # and it shouldn't have any data pruned out of it
+        # so we can reasonably assume something went wrong is this is the case
+        if not bq_raw_table_file_ids and postgres_bq_metadata_file_ids:
+            raise ValueError(
+                f"No file_ids found in BigQuery raw data table for file_tag [{file_tag}], "
+                f"but found non-invalidated, processed entries for file_ids {sorted(postgres_bq_metadata_file_ids)} "
+                "in the `direct_ingest_raw_big_query_file_metadata` table"
+            )
+
+        bq_file_ids_missing_postgres_entry = (
+            bq_raw_table_file_ids - postgres_bq_metadata_file_ids
+        )
+
+        if bq_file_ids_missing_postgres_entry:
+            raise ValueError(
+                f"BigQuery raw data table [{self._raw_data_dataset_id}.{file_tag}] contains file_ids {sorted(bq_file_ids_missing_postgres_entry)} "
+                f"with no corresponding non-invalidated, processed entry in the `direct_ingest_raw_big_query_file_metadata` table"
+            )
+
+        logger.info(
+            "Successfully validated %d file_id(s) for file_tag [%s]",
+            len(bq_raw_table_file_ids),
+            file_tag,
+        )
+
+    def _get_unique_bq_raw_table_file_ids(self, file_tag: str) -> set[int]:
+        query = StrictStringFormatter().format(
+            BQ_DATA_FILE_IDS_QUERY,
+            project_id=self._project_id,
+            raw_data_dataset_id=self._raw_data_dataset_id,
+            raw_data_table_id=file_tag,
+        )
+        rows = self._big_query_client.run_query_async(
+            query_str=query,
+            use_query_cache=True,
+        ).result()
+
+        return {one(row) for row in rows}
+
+    def _get_unique_postgres_bq_metadata_file_ids(
+        self, postgres_hook: PostgresHook, file_tag: str
+    ) -> set[int]:
+        query = StrictStringFormatter().format(
+            POSTGRES_PROCESSED_BQ_FILE_IDS_QUERY,
+            region_code=self._state_code.value,
+            raw_data_instance=self._raw_data_instance.value,
+            file_tag=file_tag,
+        )
+
+        return {one(row) for row in postgres_hook.get_records(query)}
---
+++ recidiviz/airflow/tests/raw_data/verify_big_query_postgres_alignment_sql_query_generator_test.py
@@ -0,0 +1,263 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Tests for VerifyBigQueryPostgresAlignmentSQLQueryGenerator"""
+import datetime
+from typing import List
+from unittest.mock import MagicMock, create_autospec, patch
+
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.context import Context
+from google.cloud.bigquery import QueryJob
+from more_itertools import one
+
+from recidiviz.airflow.dags.operators.cloud_sql_query_operator import (
+    CloudSqlQueryOperator,
+)
+from recidiviz.airflow.dags.raw_data.metadata import SKIPPED_FILE_ERRORS
+from recidiviz.airflow.dags.raw_data.verify_big_query_postgres_alignment_sql_query_generator import (
+    VerifyBigQueryPostgresAlignmentSQLQueryGenerator,
+)
+from recidiviz.airflow.tests.test_utils import CloudSqlQueryGeneratorUnitTest
+from recidiviz.cloud_storage.gcsfs_path import GcsfsFilePath
+from recidiviz.common.constants.states import StateCode
+from recidiviz.ingest.direct.types.direct_ingest_instance import DirectIngestInstance
+from recidiviz.ingest.direct.types.raw_data_import_types import (
+    RawBigQueryFileMetadata,
+    RawDataFilesSkippedError,
+    RawGCSFileMetadata,
+)
+from recidiviz.persistence.database.schema.operations.schema import OperationsBase
+
+
+class VerifyBigQueryPostgresAlignmentSQLQueryGeneratorTest(
+    CloudSqlQueryGeneratorUnitTest
+):
+    """Unit tests for VerifyBigQueryPostgresAlignmentSQLQueryGenerator"""
+
+    metas = [OperationsBase]
+    mock_operator = create_autospec(CloudSqlQueryOperator)
+    mock_context = create_autospec(Context)
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.mock_pg_hook = PostgresHook(postgres_conn_id=self.conn_id)
+
+        # Reset mocks between tests
+        self.mock_operator.reset_mock()
+        self.mock_context.reset_mock()
+
+        self.project_id_patcher = patch(
+            "recidiviz.airflow.dags.raw_data.verify_big_query_postgres_alignment_sql_query_generator.metadata.project_id"
+        )
+        self.mock_project_id = self.project_id_patcher.start()
+        self.mock_project_id.return_value = "recidiviz-testing"
+
+        self.bq_client_patcher = patch(
+            "recidiviz.airflow.dags.raw_data.verify_big_query_postgres_alignment_sql_query_generator.BigQueryClientImpl"
+        )
+        self.mock_bq_client_class = self.bq_client_patcher.start()
+        self.mock_bq_client = MagicMock()
+        self.mock_bq_client_class.return_value = self.mock_bq_client
+
+        self.generator = VerifyBigQueryPostgresAlignmentSQLQueryGenerator(
+            state_code=StateCode.US_XX,
+            raw_data_instance=DirectIngestInstance.PRIMARY,
+            get_all_unprocessed_bq_file_metadata_task_id="test_task_id",
+        )
+
+        self.pruning_enabled_patcher = patch(
+            "recidiviz.airflow.dags.raw_data.verify_big_query_postgres_alignment_sql_query_generator.automatic_raw_data_pruning_enabled_for_state_and_instance"
+        )
+        self.mock_pruning_enabled = self.pruning_enabled_patcher.start()
+        self.mock_pruning_enabled.return_value = True
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.project_id_patcher.stop()
+        self.bq_client_patcher.stop()
+        self.pruning_enabled_patcher.stop()
+
+    def _seed_bq_metadata(
+        self,
+        file_ids: List[int],
+        file_tag: str,
+        is_invalidated: bool = False,
+    ) -> None:
+        """Helper to seed the operations DB with BQ file metadata"""
+        values = [
+            f"({file_id},'US_XX','PRIMARY','{file_tag}','NOW()',{is_invalidated},'NOW()')"
+            for file_id in file_ids
+        ]
+        self.mock_pg_hook.run(
+            f"""INSERT INTO direct_ingest_raw_big_query_file_metadata
+            (file_id, region_code, raw_data_instance, file_tag, update_datetime, is_invalidated, file_processed_time)
+            VALUES {','.join(values)}
+            """
+        )
+
+    def _create_bq_file_metadata(
+        self, file_id: int, file_tag: str
+    ) -> RawBigQueryFileMetadata:
+        """Helper to create a RawBigQueryFileMetadata object"""
+        update_datetime = datetime.datetime(2024, 1, 1, 1, 1, 1, tzinfo=datetime.UTC)
+        gcs_file = RawGCSFileMetadata(
+            gcs_file_id=file_id,
+            file_id=file_id,
+            path=GcsfsFilePath.from_absolute_path(
+                f"testing/unprocessed_{update_datetime.isoformat()}_raw_{file_tag}.csv"
+            ),
+        )
+        return RawBigQueryFileMetadata(
+            gcs_files=[gcs_file],
+            file_id=file_id,
+            file_tag=file_tag,
+            update_datetime=update_datetime,
+        )
+
+    def test_no_files_to_validate(self) -> None:
+        """Test when there are no files to validate"""
+        self.mock_operator.xcom_pull.return_value = []
+
+        result = self.generator.execute_postgres_query(
+            self.mock_operator, self.mock_pg_hook, self.mock_context
+        )
+
+        assert result == []
+
+        self.mock_operator.xcom_push.assert_called_once()
+        call_args = self.mock_operator.xcom_push.call_args
+        assert call_args[1]["key"] == SKIPPED_FILE_ERRORS
+        assert call_args[1]["value"] == []
+
+    def test_pruning_not_enabled(self) -> None:
+        """Test that when pruning is not enabled, validation is skipped and all files are returned"""
+        self.mock_pruning_enabled.return_value = False
+
+        mock_query_job = MagicMock(spec=QueryJob)
+        mock_query_job.result.return_value = [[1], [2]]
+        self.mock_bq_client.run_query_async.return_value = mock_query_job
+
+        bq_files = [
+            self._create_bq_file_metadata(3, "test_tag"),
+            self._create_bq_file_metadata(4, "test_tag"),
+            self._create_bq_file_metadata(5, "test_tag"),
+        ]
+
+        self.mock_operator.xcom_pull.return_value = [f.serialize() for f in bq_files]
+
+        result = self.generator.execute_postgres_query(
+            self.mock_operator, self.mock_pg_hook, self.mock_context
+        )
+
+        assert len(result) == 3
+        returned_metadata = [RawBigQueryFileMetadata.deserialize(r) for r in result]
+        assert {m.file_id for m in returned_metadata} == {3, 4, 5}
+
+        self.mock_operator.xcom_push.assert_called_once()
+        call_args = self.mock_operator.xcom_push.call_args
+        assert call_args[1]["key"] == SKIPPED_FILE_ERRORS
+        assert call_args[1]["value"] == []
+
+    def test_all_files_valid(self) -> None:
+        self._seed_bq_metadata([1, 2, 3], file_tag="test_tag")
+
+        # Mock BigQuery to return file_ids 1 and 2
+        # we should allow file_ids in the bq metadata table to not have
+        # any corresponding rows in the big query raw data table
+        mock_query_job = MagicMock(spec=QueryJob)
+        mock_query_job.result.return_value = [[1], [2]]
+        self.mock_bq_client.run_query_async.return_value = mock_query_job
+
+        bq_files = [
+            self._create_bq_file_metadata(4, "test_tag"),
+            self._create_bq_file_metadata(5, "test_tag"),
+        ]
+
+        self.mock_operator.xcom_pull.return_value = [f.serialize() for f in bq_files]
+
+        result = self.generator.execute_postgres_query(
+            self.mock_operator, self.mock_pg_hook, self.mock_context
+        )
+
+        assert len(result) == 2
+        returned_metadata = [RawBigQueryFileMetadata.deserialize(r) for r in result]
+        assert {m.file_id for m in returned_metadata} == {4, 5}
+
+        call_args = self.mock_operator.xcom_push.call_args
+        assert call_args[1]["key"] == SKIPPED_FILE_ERRORS
+        assert call_args[1]["value"] == []
+
+    def test_partial_validation_failure(self) -> None:
+        self._seed_bq_metadata([1, 2], file_tag="tag_a")
+        self._seed_bq_metadata([11, 12], file_tag="tag_b", is_invalidated=True)
+
+        mock_query_job = MagicMock(spec=QueryJob)
+        mock_query_job.result.side_effect = [
+            [[1], [2]],  # First call for tag_a
+            [[11], [12]],  # Second call for tag_b
+        ]
+        self.mock_bq_client.run_query_async.return_value = mock_query_job
+
+        bq_files = [
+            self._create_bq_file_metadata(3, "tag_a"),
+            self._create_bq_file_metadata(13, "tag_b"),
+        ]
+
+        self.mock_operator.xcom_pull.return_value = [f.serialize() for f in bq_files]
+
+        result = self.generator.execute_postgres_query(
+            self.mock_operator, self.mock_pg_hook, self.mock_context
+        )
+
+        returned_metadata = one(
+            [RawBigQueryFileMetadata.deserialize(r) for r in result]
+        )
+        assert returned_metadata.file_tag == "tag_a"
+
+        call_args = self.mock_operator.xcom_push.call_args
+        skipped_error = one(
+            [RawDataFilesSkippedError.deserialize(e) for e in call_args[1]["value"]]
+        )
+        assert skipped_error.file_tag == "tag_b"
+
+    def test_no_file_ids_from_bigquery(self) -> None:
+        self._seed_bq_metadata([1, 2], file_tag="test_tag")
+
+        # Mock BigQuery to return no file_ids
+        mock_query_job = MagicMock(spec=QueryJob)
+        mock_query_job.result.return_value = []
+        self.mock_bq_client.run_query_async.return_value = mock_query_job
+
+        bq_files = [
+            self._create_bq_file_metadata(3, "test_tag"),
+            self._create_bq_file_metadata(4, "test_tag"),
+        ]
+
+        self.mock_operator.xcom_pull.return_value = [f.serialize() for f in bq_files]
+
+        result = self.generator.execute_postgres_query(
+            self.mock_operator, self.mock_pg_hook, self.mock_context
+        )
+
+        assert len(result) == 0
+
+        call_args = self.mock_operator.xcom_push.call_args
+        skipped_errors = [
+            RawDataFilesSkippedError.deserialize(e) for e in call_args[1]["value"]
+        ]
+        assert len(skipped_errors) == 2
+        assert all(e.file_tag == "test_tag" for e in skipped_errors)
