e569e4d3e1f920d9a17f27cf5e858bae4fa1762f
Anna Geiduschek <ageiduschek@users.noreply.github.com>
2025-11-07 12:59:34 -0800

    [Cleanup] Remove gender column from compartment_(sub_)sessions (Recidiviz/recidiviz-data#51899)
    ## Description of the change
    
    Continues work towards Recidiviz/recidiviz-data#50972 by removing the `gender` column from
    `compartment_sessions` and `compartment_sub_sessions`. This is now safe
    to do as I've merged a number of changes that remove all references to
    these column in the Looker repo. If we really want to add a `gender`
    column back to these views later, we certainly can!
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Part of Recidiviz/recidiviz-data#50972
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [x] This pull request has a descriptive title and information useful
    to a reviewer
    - [x] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 6c75ab3c049d89427f1d23ef45c70162bd97176a

--- recidiviz/view_registry/deployed_view_gender_col_exemptions.py
+++ recidiviz/view_registry/deployed_view_gender_col_exemptions.py
@@ -22,20 +22,11 @@ from recidiviz.big_query.big_query_address import BigQueryAddress
 from recidiviz.calculator.query.state.views.analyst_data.session_cohort_reincarceration import (
     SESSION_COHORT_REINCARCERATION_VIEW_BUILDER,
 )
-from recidiviz.calculator.query.state.views.dashboard.pathways.event_level.supervision_to_prison_transitions_raw import (
-    SUPERVISION_TO_PRISON_TRANSITIONS_RAW_VIEW_BUILDER,
-)
 from recidiviz.calculator.query.state.views.reference.state_resident_population import (
     STATE_RESIDENT_POPULATION_VIEW_BUILDER,
 )
 from recidiviz.calculator.query.state.views.reference.state_resident_population_combined_race_ethnicity import (
     STATE_RESIDENT_POPULATION_COMBINED_RACE_ETHNICITY_VIEW_BUILDER,
-)
-from recidiviz.calculator.query.state.views.sessions.compartment_sessions import (
-    COMPARTMENT_SESSIONS_VIEW_BUILDER,
-)
-from recidiviz.calculator.query.state.views.sessions.compartment_sub_sessions import (
-    COMPARTMENT_SUB_SESSIONS_VIEW_BUILDER,
 )
 from recidiviz.calculator.query.state.views.shared_metric.event_based_supervision_populations import (
     EVENT_BASED_SUPERVISION_VIEW_BUILDER,
@@ -46,14 +37,8 @@ from recidiviz.calculator.query.state.views.shared_metric.single_day_incarcerati
 from recidiviz.calculator.query.state.views.shared_metric.single_day_supervision_population_for_spotlight import (
     SINGLE_DAY_SUPERVISION_POPULATION_FOR_SPOTLIGHT_VIEW_BUILDER,
 )
-from recidiviz.calculator.query.state.views.shared_metric.supervision_officer_caseload import (
-    SUPERVISION_OFFICER_CASELOAD_VIEW_BUILDER,
-)
 from recidiviz.calculator.query.state.views.shared_metric.supervision_terminations_for_spotlight import (
     SUPERVISION_TERMINATIONS_FOR_SPOTLIGHT_VIEW_BUILDER,
-)
-from recidiviz.observations.views.spans.person.compartment_sub_session import (
-    VIEW_BUILDER as COMPARTMENT_SUB_SESSION_OBSERVATION_VIEW_BUILDER,
 )
 from recidiviz.utils import metadata
 from recidiviz.validation.views.state.incarceration_population_by_demographic_internal_comparison import (
@@ -67,26 +52,13 @@ from recidiviz.validation.views.state.incarceration_population_by_demographic_in
 _KNOWN_NON_EXPORT_VIEWS_WITH_GENDER_COLUMN: dict[BigQueryAddress, str] = {
     # analyst_data views
     SESSION_COHORT_REINCARCERATION_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
-    # dashboard_views views
-    SUPERVISION_TO_PRISON_TRANSITIONS_RAW_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
-    # externally_shared views
-    BigQueryAddress(
-        dataset_id="externally_shared_views",
-        table_id="csg_compartment_sessions",
-    ): "TODO(#50972): This is auto-generated - will be fixed when compartment_sessions is migrated",
-    # observations views
-    COMPARTMENT_SUB_SESSION_OBSERVATION_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
     # reference_views views
     STATE_RESIDENT_POPULATION_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
     STATE_RESIDENT_POPULATION_COMBINED_RACE_ETHNICITY_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
-    # sessions views
-    COMPARTMENT_SESSIONS_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
-    COMPARTMENT_SUB_SESSIONS_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
     # shared_metric_views views
     EVENT_BASED_SUPERVISION_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
     SINGLE_DAY_INCARCERATION_POPULATION_FOR_SPOTLIGHT_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
     SINGLE_DAY_SUPERVISION_POPULATION_FOR_SPOTLIGHT_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
-    SUPERVISION_OFFICER_CASELOAD_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
     SUPERVISION_TERMINATIONS_FOR_SPOTLIGHT_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
     # validation_views views
     INCARCERATION_POPULATION_BY_DEMOGRAPHIC_INTERNAL_COMPARISON_VIEW_BUILDER.address: "TODO(#50972): Migrate to use sex instead of gender",
