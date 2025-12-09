144d1139d457b36158495cc21d9fa800639ce8a9
Hugo S <107963194+hugosr-r@users.noreply.github.com>
2025-12-09 10:14:54 -0600

    [US_IX] CRC-like workflows: ICIO (Recidiviz/recidiviz-data#54359)
    ## Description of the change
    
    I created the TES queries to surface people eligible for CRC-like beds
    in ICIO.
    
    I uploaded it to a sandbox:
    ```
    python -m recidiviz.tools.load_views_to_sandbox \
            --project_id recidiviz-staging\
            --sandbox_dataset_prefix hsalas\
            --state_code_filter US_IX manual\
            --dataset_ids_to_load task_eligibility_criteria_us_ix,task_eligibility_spans_us_ix\
            --update_descendants False
    ```
    There are 35 people eligible for this opportunity and about 385 for the
    general CRC work-release; so a bit less than 10% of the eligible
    population for the more general oppportunity.
    
    ```
    SELECT tes.*, rr.facility_id
    FROM recidiviz-staging.hsalas_task_eligibility_spans_us_ix.transfer_to_crc_like_bed_icio_request_materialized tes
    --FROM recidiviz-staging.task_eligibility_spans_us_ix.transfer_to_crc_work_release_request_materialized tes
    LEFT JOIN recidiviz-staging.workflows_views.resident_record_materialized rr
      USING(person_id)
    WHERE is_eligible
      AND CURRENT_DATE BETWEEN start_date AND IFNULL(end_date, '9999-12-31')
    
    ```
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### ⚠️ Changing an opportunity record?
    Please [consult this
    document](https://www.notion.so/recidiviz/How-to-make-a-change-to-the-Opportunity-Record-1d67889f4d19807f8109f9f2f007dde3)
    first to see if additional changes might be required in the admin panel
    or a Zod schema. You can confirm with Polaris if unsure.
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 60bf510a8cb760c09a2ad4fd600cc15030f075f1

--- recidiviz/task_eligibility/eligibility_spans/us_ix/transfer_to_crc_like_bed_icio_request.py
+++ recidiviz/task_eligibility/eligibility_spans/us_ix/transfer_to_crc_like_bed_icio_request.py
@@ -1,5 +1,5 @@
 # Recidiviz - a data platform for criminal justice reform
-# Copyright (C) 2023 Recidiviz, Inc.
+# Copyright (C) 2025 Recidiviz, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,8 @@
 # =============================================================================
 """
 Shows the spans of time during which someone in ID is eligible
-for a transfer to a Community Reentry Center (CRC) for work-release.
+for a transfer to a Community Reentry Center (CRC)-like bed in Idaho Correctional
+Institutional Orofino (ICIO)
 """
 from recidiviz.common.constants.states import StateCode
 from recidiviz.task_eligibility.candidate_populations.general import (
@@ -27,27 +28,23 @@ from recidiviz.task_eligibility.completion_events.state_specific.us_ix import (
 )
 from recidiviz.task_eligibility.criteria.general import (
     custody_level_is_minimum,
-    incarceration_not_past_projected_parole_release_date,
-    incarceration_not_within_6_months_of_full_term_completion_date,
-    not_in_treatment_in_prison,
-    not_serving_for_sexual_offense,
     not_serving_for_violent_offense,
 )
 from recidiviz.task_eligibility.criteria.state_specific.us_ix import (
-    crc_work_release_time_based_criteria,
-    incarceration_not_within_6_months_of_upcoming_eprd,
-    no_absconsion_escape_and_eluding_police_offenses_within_10_years,
-    no_sex_offender_alert,
+    has_a_d1_or_d2_release_note,
+    in_icio,
     not_denied_for_crc,
-    not_detainers_for_xcrc_and_crc,
-    not_serving_a_rider_sentence,
 )
-from recidiviz.task_eligibility.criteria_condition import (
+from recidiviz.task_eligibility.criteria.state_specific.us_ix.crc_work_release_time_based_criteria import (
+    StateSpecificTaskCriteriaGroupBigQueryViewBuilder,
+    TaskCriteriaGroupLogicType,
+)
+from recidiviz.task_eligibility.eligibility_spans.us_ix.transfer_to_crc_work_release_request import (
+    VIEW_BUILDER as TRANSFER_TO_CRC_WORK_RELEASE_REQUEST_VIEW_BUILDER,
+)
+from recidiviz.task_eligibility.eligibility_spans.us_ix.transfer_to_crc_work_release_request import (
     NotEligibleCriteriaCondition,
     PickNCompositeCriteriaCondition,
-)
-from recidiviz.task_eligibility.eligibility_spans.us_ix.transfer_to_crc_resident_worker_request import (
-    US_IX_NOT_IN_CRC_FACILITY_VIEW_BUILDER,
 )
 from recidiviz.task_eligibility.single_task_eligiblity_spans_view_builder import (
     SingleTaskEligibilitySpansBigQueryViewBuilder,
@@ -57,26 +54,23 @@ from recidiviz.utils.metadata import local_project_id_override
 
 VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
     state_code=StateCode.US_IX,
-    task_name="TRANSFER_TO_CRC_WORK_RELEASE_REQUEST",
+    task_name="TRANSFER_TO_CRC_LIKE_BED_ICIO_REQUEST",
     description=__doc__,
     candidate_population_view_builder=general_incarceration_population.VIEW_BUILDER,
     criteria_spans_view_builders=[
-        custody_level_is_minimum.VIEW_BUILDER,
-        not_detainers_for_xcrc_and_crc.VIEW_BUILDER,
-        not_serving_for_sexual_offense.VIEW_BUILDER,
-        no_absconsion_escape_and_eluding_police_offenses_within_10_years.VIEW_BUILDER,
-        # TODO(#24043) - remove this criteria and add one where we only filter out those already on wr
-        US_IX_NOT_IN_CRC_FACILITY_VIEW_BUILDER,
-        crc_work_release_time_based_criteria.VIEW_BUILDER,
-        not_in_treatment_in_prison.VIEW_BUILDER,
-        no_sex_offender_alert.VIEW_BUILDER,
-        not_serving_for_violent_offense.VIEW_BUILDER,
-        incarceration_not_within_6_months_of_upcoming_eprd.VIEW_BUILDER,
-        incarceration_not_within_6_months_of_full_term_completion_date.VIEW_BUILDER,
-        incarceration_not_past_projected_parole_release_date.VIEW_BUILDER,
-        not_serving_a_rider_sentence.VIEW_BUILDER,
-        not_denied_for_crc.VIEW_BUILDER,
+        # All of CRC work-release criteria
+        *TRANSFER_TO_CRC_WORK_RELEASE_REQUEST_VIEW_BUILDER.criteria_spans_view_builders,
+        # Additionally, must be a resident of ICIO
+        StateSpecificTaskCriteriaGroupBigQueryViewBuilder(
+            logic_type=TaskCriteriaGroupLogicType.OR,
+            criteria_name="US_IX_IN_ICIO_OR_HAS_D1_OR_D2_RELEASE_NOTE",
+            sub_criteria_list=[
+                in_icio.VIEW_BUILDER,
+                has_a_d1_or_d2_release_note.VIEW_BUILDER,
+            ],
+        ),
     ],
+    # TODO(#54358): Find out which completion event should be used here
     completion_event_builder=granted_work_release.VIEW_BUILDER,
     almost_eligible_condition=PickNCompositeCriteriaCondition(
         sub_conditions_list=[
@@ -88,8 +82,12 @@ VIEW_BUILDER = SingleTaskEligibilitySpansBigQueryViewBuilder(
                 criteria=not_denied_for_crc.VIEW_BUILDER,
                 description="Denied for CRC eligibility",
             ),
+            NotEligibleCriteriaCondition(
+                criteria=custody_level_is_minimum.VIEW_BUILDER,
+                description="Custody level is not minimum",
+            ),
         ],
-        at_least_n_conditions_true=1,
+        at_most_n_conditions_true=1,
     ),
 )
 
