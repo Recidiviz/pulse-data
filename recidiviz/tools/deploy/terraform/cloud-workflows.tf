1315f8c19b70aa6564ee181ad916674cd9e74f13
Anthony Kesich <anthony@recidiviz.org>
2026-07-20 12:08:34 -0500

    [TF] Update iam to grant roles/cloudsql.editor to cloud workflows SAs (Recidiviz/recidiviz-data#91753)
    ## Description of the change
    
    Update the SQ to runs our cloud workflows to have editor access instead
    of viewer access. We trigger exports using this job, but starting aug 1,
    you need to have editor acces to export data, not just viewer.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [x] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### ⚠️ Changing an opportunity record?
    Please [consult this
    document](https://www.notion.so/recidiviz/How-to-make-a-change-to-the-Opportunity-Record-1d67889f4d19807f8109f9f2f007dde3)
    first to see if additional changes might be required in the admin panel
    or a Zod schema. You can confirm with Polaris if unsure.
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 68c29e5ce43086e32e7117be22be532f558c489c

--- recidiviz/tools/deploy/terraform/cloud-workflows.tf
+++ recidiviz/tools/deploy/terraform/cloud-workflows.tf
@@ -23,7 +23,7 @@ locals {
     "roles/logging.logWriter",      # for writing to logs while in a workflow
     "roles/workflows.invoker",      # for running workflows
     "roles/run.admin",              # for executing cloud run jobs from a workflows
-    "roles/cloudsql.viewer",        # for triggering cloudsql exports
+    "roles/cloudsql.editor",        # for triggering cloudsql exports
   ]
 }
 
