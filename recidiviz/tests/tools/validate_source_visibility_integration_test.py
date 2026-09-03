f097d45aa50e2b018d8920cb82a659110f65e56d
Shalin Brahmbhatt <63821972+shalindb@users.noreply.github.com>
2026-09-03 15:31:22 -0700

    [US_CO] Delete extra validation views that are currently not needed (Recidiviz/recidiviz-data#101354)
    ## Description of the change
    
    Removing credit cap and policy effective date validation tests as those
    will be tracked elsewhere in either the OMS or our code!
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    |-----------------------------
    |-----------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    ## Checklists
    
    ### Development
    
    **This box MUST be checked by the submitter prior to merging**:
    - [ ] **Double- and triple-checked that there is no Personally
    Identifiable Information (PII) being mistakenly added in this pull
    request**
    
    These boxes should be checked by the submitter prior to merging:
    - [ ] Tests have been written to cover the code changed/added as part of
    this pull request
    
    ### ⚠️ Changing an opportunity record?
    Please [consult this
    document](https://www.notion.so/recidiviz/How-to-make-a-change-to-the-Opportunity-Record-1d67889f4d19807f8109f9f2f007dde3)
    first to see if additional changes might be required in the admin panel
    or a Zod schema. You can confirm with Polaris if unsure.
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: 57a68e0daf67e741ab8602985a68c82efef1504e

--- recidiviz/tests/tools/validate_source_visibility_integration_test.py
+++ recidiviz/tests/tools/validate_source_visibility_integration_test.py
@@ -691,7 +691,6 @@ def test_admin_panel_server_dependencies() -> None:
                 "recidiviz.workflows",
                 # Thin eomis module; no imports, so safe to allow here.
                 "recidiviz.eomis.us_ar.constants",
-                "recidiviz.eomis.us_co.constants",
             }
         ),
     )
@@ -737,7 +736,6 @@ def test_application_data_import_server_dependencies() -> None:
                 "recidiviz.workflows",
                 # Thin eomis module; no imports, so safe to allow here.
                 "recidiviz.eomis.us_ar.constants",
-                "recidiviz.eomis.us_co.constants",
                 # Pulled in transitively by yaml_managed.datasets.
                 "recidiviz.datasets.static_data.terraform_managed.config",
             }
@@ -786,7 +784,6 @@ def test_entrypoint_executor_dependencies() -> None:
                 "recidiviz.segment",
                 # Thin eomis module; no imports, so safe to allow here.
                 "recidiviz.eomis.us_ar.constants",
-                "recidiviz.eomis.us_co.constants",
                 "recidiviz.source_tables",
                 "recidiviz.task_eligibility",
                 "recidiviz.utils",
@@ -829,7 +826,6 @@ def test_entrypoint_report_metric_export_timeliness_dependencies() -> None:
                 "recidiviz.datasets.static_data.views.dataset_config",
                 # Thin eomis module; no imports, so safe to allow here.
                 "recidiviz.eomis.us_ar.constants",
-                "recidiviz.eomis.us_co.constants",
                 # Pulled in transitively by yaml_managed.datasets.
                 "recidiviz.datasets.static_data.terraform_managed.config",
             }
@@ -928,7 +924,6 @@ def test_view_registry_deployed_view_graphs_dependencies() -> None:
                 "recidiviz.pipelines.supplemental.us_ix_case_note_extracted_entities.us_ix_note_title_text_analysis_configuration",
                 # Thin eomis module; no imports, so safe to allow here.
                 "recidiviz.eomis.us_ar.constants",
-                "recidiviz.eomis.us_co.constants",
             },
         ),
         explicitly_invalid_package_dependencies=["apache_beam"],
